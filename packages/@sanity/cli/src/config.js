854c871f4d9110519cc7db62d66b503c8f538002
Espen Hovlandsdal <espen@hovlandsdal.com>
2016-02-16 16:24:20 +0100

    Use actual update interval (seconds vs milliseconds)

--- packages/@sanity/cli/src/config.js
+++ packages/@sanity/cli/src/config.js
@@ -1,7 +1,7 @@
 import ConfigStore from 'configstore'
 
 const defaults = {
-  'cli.update.interval': 21600 // Every six hours
+  'cli.update.interval': 21600 * 1000 // Every six hours
 }
 
 const config = new ConfigStore('sanity', defaults, {globalConfigPath: true})
