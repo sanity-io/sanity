21ad059c2f1440899bb562a4678c8dadfa415fa0
Espen Hovlandsdal <espen@hovlandsdal.com>
2016-02-24 15:18:15 +0100

    Attempt using webpack 2 beta

--- packages/@sanity/server/bin/sanity-server.js
+++ packages/@sanity/server/bin/sanity-server.js
@@ -4,6 +4,7 @@ const pkg = require('../package.json')
 // @todo Figure out a better way to detect if we're running in "development mode"
 const serverDevMode = Boolean(pkg._id) === false
 const appDevMode = process.argv.indexOf('--dev') !== -1
+const port = process.env.PORT || 3910 // eslint-disable no-process-env
 
 if (serverDevMode) {
   require('babel-register')
@@ -13,4 +14,4 @@ const target = appDevMode ? 'devServer' : 'prodServer'
 const sourceFolder = serverDevMode ? '../src/' : '../lib/'
 const getServer = require(sourceFolder + target).default
 
-getServer().listen(3000)
+getServer().listen(port)
