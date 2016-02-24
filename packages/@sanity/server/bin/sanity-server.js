702a76c872605b4d1443b148695e48ed2088b877
Espen Hovlandsdal <espen@hovlandsdal.com>
2016-02-24 15:18:36 +0100

    Lint fix

--- packages/@sanity/server/bin/sanity-server.js
+++ packages/@sanity/server/bin/sanity-server.js
@@ -4,7 +4,7 @@ const pkg = require('../package.json')
 // @todo Figure out a better way to detect if we're running in "development mode"
 const serverDevMode = Boolean(pkg._id) === false
 const appDevMode = process.argv.indexOf('--dev') !== -1
-const port = process.env.PORT || 3910 // eslint-disable no-process-env
+const port = process.env.PORT || 3910 // eslint-disable-line no-process-env
 
 if (serverDevMode) {
   require('babel-register')
