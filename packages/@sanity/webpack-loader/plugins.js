3bf146464c30fc05223421617793fe0dcac293f5
Espen Hovlandsdal <espen@hovlandsdal.com>
2016-02-26 11:37:49 +0100

    Move role map check to prevent doing check on each getRole call

--- packages/@sanity/webpack-loader/plugins.js
+++ packages/@sanity/webpack-loader/plugins.js
@@ -5,14 +5,16 @@ Check your webpack configuration.`
 
 var roles = {'#': '#'} // eslint-disable-line no-var
 
+// The roles above is replaced by the webpack loader, if it is not enabled (user is using a custom setup),
+// give a meaningful error back to the user so it'll be easier to diagnose the problem
+if (roles['#']) {
+  throw new Error(noRolesError)
+}
+
 export function setRoles(roleMap) {
   roles = roleMap
 }
 
 export function getRole(role) {
-  if (roles['#']) {
-    throw new Error(noRolesError)
-  }
-
   return roles[role]
 }
