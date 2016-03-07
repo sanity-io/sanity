1b11e513aa559dc823a48bb68cc8e3e5ddc23242
Espen Hovlandsdal <espen@hovlandsdal.com>
2016-03-07 11:40:21 +0100

    Add support for ES5/ES6 module styles in role getter

--- src/createSanity.js
+++ src/createSanity.js
@@ -1,7 +1,18 @@
 function createSanity({roles}) {
   return {
     getRole(roleName) {
-      return roles[roleName]
+      const fulfiller = roles[roleName]
+      if (!fulfiller) {
+        return null
+      }
+
+      return fulfiller.module.__esModule && fulfiller.module.default
+        ? fulfiller.module.default
+        : fulfiller.module
+    },
+
+    getPluginForRole(roleName) {
+      return roles[roleName] && roles[roleName].plugin
     }
   }
 }
