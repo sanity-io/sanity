9eb90967007174d0cdd63d1480af357542224122
Espen Hovlandsdal <espen@hovlandsdal.com>
2016-03-14 18:14:59 +0100

    Move roles lookup into server entry in order to allow hot module reloading

--- packages/@sanity/server/src/browser/entry.js
+++ packages/@sanity/server/src/browser/entry.js
@@ -1,8 +1,23 @@
+/* eslint-disable no-process-env */
 import React from 'react'
 import ReactDOM from 'react-dom'
 import {Sanity} from '@sanity/base'
+import plugins from '@sanity/plugin-loader/plugins'
 
-ReactDOM.render(
-  <Sanity />,
-  document.getElementById('sanity')
-)
+function render(roles) {
+  ReactDOM.render(
+    <Sanity roles={roles} />,
+    document.getElementById('sanity')
+  )
+}
+
+if (process.env.NODE_ENV !== 'production') {
+  // Hot module reloading for plugins? Yes please.
+  if (module.hot) {
+    module.hot.accept('@sanity/plugin-loader/plugins', () => {
+      render(require('@sanity/plugin-loader/plugins'))
+    })
+  }
+}
+
+render(plugins)
