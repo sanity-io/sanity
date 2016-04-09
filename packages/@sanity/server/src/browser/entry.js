70d8a1d919008b50e351a4f140a73aaf6c2e8c61
Espen Hovlandsdal <espen@hovlandsdal.com>
2016-04-09 15:33:44 +0200

    Require latest Sanity base

--- packages/@sanity/server/src/browser/entry.js
+++ packages/@sanity/server/src/browser/entry.js
@@ -1,23 +1,8 @@
-/* eslint-disable no-process-env */
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {Sanity} from '@sanity/base'
-import plugins from '@sanity/plugin-loader/plugins'
+import Root from 'component:@sanity/base/root'
 
-function render(roles) {
-  ReactDOM.render(
-    <Sanity roles={roles} />,
-    document.getElementById('sanity')
-  )
-}
-
-if (process.env.NODE_ENV !== 'production') {
-  // Hot module reloading for plugins? Yes please.
-  if (module.hot) {
-    module.hot.accept('@sanity/plugin-loader/plugins', () => {
-      render(require('@sanity/plugin-loader/plugins'))
-    })
-  }
-}
-
-render(plugins)
+ReactDOM.render(
+  <Root />,
+  document.getElementById('sanity')
+)
