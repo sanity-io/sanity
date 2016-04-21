f222cd164c0f5883066ac72e11a88e07c047da75
Espen Hovlandsdal <espen@hovlandsdal.com>
2016-04-21 12:36:33 +0200

    Initial boilerplate

---
+++ packages/@sanity/desk-tool/src/DeskTool.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+class DeskTool extends React.Component {
+  render() {
+    return (
+      <div>Desk tool!</div>
+    )
+  }
+}
+
+export default DeskTool
---
+++ packages/@sanity/desk-tool/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import DeskTool from './DeskTool'
+import Icon from './Icon'
+
+export default {
+  name: 'Desk',
+  icon: Icon,
+  component: DeskTool
+}
