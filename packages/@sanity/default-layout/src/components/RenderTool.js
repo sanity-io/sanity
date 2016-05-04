dd5c045e9170e8f310d8309b0ca6fdd62f30ef40
Espen Hovlandsdal <espen@hovlandsdal.com>
2016-05-04 11:24:39 +0200

    Remove console.log

--- packages/@sanity/default-layout/src/components/RenderTool.js
+++ packages/@sanity/default-layout/src/components/RenderTool.js
@@ -1,21 +1,19 @@
 import React from 'react'
-// import {Router, Route, NotFound, Redirect} from 'router:@sanity/base/router'
 import tools from 'all:tool:@sanity/base/tool'
 
 function RenderTool(props) {
-  const activeToolName = props.params.tool
-
   if (!tools.length) {
     return <div>No tools fulfills the role <code>`tool:@sanity/base/tool`</code></div>
   }
 
+  const activeToolName = props.params.tool
   const activeTool = tools.find(tool => tool.name === activeToolName)
-  console.log('activetool', activeTool)
-
   if (!activeTool) {
     return <div>Tool not found: {activeToolName}</div>
   }
-  return <activeTool.component {...props} />
+
+  const ActiveTool = activeTool.component
+  return <ActiveTool {...props} />
 }
 
 export default RenderTool
