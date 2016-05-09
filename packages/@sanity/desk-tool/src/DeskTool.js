8b417e71fc527ae5d9ef58f1bc26b0be09e0bceb
Kristoffer Sivertsen <kristofferjoachim@gmail.com>
2016-05-10 00:16:25 +0200

    Basic styling

--- packages/@sanity/desk-tool/src/DeskTool.js
+++ packages/@sanity/desk-tool/src/DeskTool.js
@@ -2,18 +2,18 @@ import React, {PropTypes} from 'react'
 import styles from '../styles/DeskTool.css'
 import PaneResolver from 'config:desk-tool/pane-resolver'
 
+DeskTool.propTypes = {
+  location: PropTypes.shape({
+    pathname: PropTypes.string.isRequired
+  })
+}
+
 function DeskTool({location}) {
   return (
     <div className={styles.deskTool}>
       <PaneResolver location={location.pathname} />
     </div>
   )
-}
-
-DeskTool.propTypes = {
-  location: PropTypes.shape({
-    pathname: PropTypes.string.isRequired
-  })
 }
 
 export default DeskTool
