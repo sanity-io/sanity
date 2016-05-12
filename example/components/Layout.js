622d907770eaad3965106607400a22bc23ff1bc0
Bjørge Næss <bjoerge@gmail.com>
2016-05-12 20:01:05 +0200

    Set correct title

--- example/components/Layout.js
+++ example/components/Layout.js
@@ -8,13 +8,13 @@ export default React.createClass({
         <head>
           <DOMContentLoadedFix />
           <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
-          <title>Acme Inc. CMS &#x0F1C;</title>
+          <title>Form builder demo</title>
           <script src="/browser/main.js" async />
           <link rel="stylesheet" href="/stylesheets/main.css" />
           <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1, maximum-scale=1" />
         </head>
         <body>
-          <h2>Form builder playground</h2>
+          <h2>Form builder demo</h2>
           <div id="main" />
           <div id="debug" />
         </body>
