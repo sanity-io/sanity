086b517cb7b59f05b5e3bdbbe817e53f0e3aa7e9
Bjørge Næss <bjoerge@gmail.com>
2016-05-24 14:20:41 +0200

    Misc minor improvements in Array widget

--- src/field-inputs/Array.js
+++ src/field-inputs/Array.js
@@ -2,7 +2,7 @@ import React, {PropTypes} from 'react'
 import FormBuilderPropTypes from '../FormBuilderPropTypes'
 import RenderListItem from '../RenderListItem'
 import ArrayContainer from '../state/ArrayContainer'
-import {createFormBuilderState} from '../state/FormBuilderState'
+import {createFieldValue} from '../state/FormBuilderState'
 import {resolveJSType} from '../types/utils'
 import {getFieldType} from '../utils/getFieldType'
 
@@ -10,7 +10,7 @@ export default React.createClass({
   propTypes: {
     type: FormBuilderPropTypes.type,
     field: FormBuilderPropTypes.field,
-    value: PropTypes.array,
+    value: PropTypes.instanceOf(ArrayContainer),
     onChange: PropTypes.func
   },
   statics: {
@@ -24,7 +24,7 @@ export default React.createClass({
   getInitialState() {
     return {
       selecType: false,
-      addItemType: null,
+      addItemField: null,
       addItem: void 0
     }
   },
@@ -38,19 +38,20 @@ export default React.createClass({
       this.setState({selectType: true})
       return
     }
-    this.handleAddItem(getFieldType(this.context.schema, this.props.type.of[0]))
+    this.handleAddItem(this.props.type.of[0])
   },
 
-  handleAddItem(itemType) {
-    const addItemValue = createFormBuilderState(void 0, {
-      type: itemType,
+  handleAddItem(field) {
+    const addItemValue = createFieldValue(void 0, {
+      field: field,
       schema: this.context.schema,
-      resolveFieldInput: this.context.resolveFieldInput
+      // not too elegant atm.
+      resolveContainer: (_field, type) => this.context.resolveFieldInput(_field, type).container
     })
 
     this.setState({
       selectType: false,
-      addItemType: itemType,
+      addItemField: field,
       addItem: addItemValue
     })
   },
@@ -65,7 +66,7 @@ export default React.createClass({
 
   handleOK() {
     const itemValue = this.state.addItem
-    this.setState({selectType: false, addItemType: null, addItem: void 0})
+    this.setState({selectType: false, addItemField: null, addItem: void 0})
     this.props.onChange({patch: {$unshift: [itemValue]}})
   },
   renderSelectType() {
@@ -74,7 +75,7 @@ export default React.createClass({
       return (
         <button
           key={field.type}
-          onClick={() => this.handleAddItem(getFieldType(this.context.schema, field))}
+          onClick={() => this.handleAddItem(field)}
           type="button"
         >
           {field.title || field.type}
@@ -89,23 +90,23 @@ export default React.createClass({
     const patch = {[index]: event.patch}
     this.props.onChange({patch})
   },
-  renderAddItemForm(addItemType) {
+  renderAddItemForm(addItemField) {
     return (
       <div>
-        <b>Add {addItemType.title}</b>
-        <RenderListItem index={-1} field={addItemType} value={this.state.addItem} onChange={this.handleAddItemChange} />
+        <b>Add {addItemField.title}</b>
+        <RenderListItem index={-1} field={addItemField} value={this.state.addItem} onChange={this.handleAddItemChange} />
         <button type="button" onClick={this.handleOK}>OK</button>
       </div>
     )
   },
   render() {
     const {type, value} = this.props
-    const {selectType, addItemType} = this.state
+    const {selectType, addItemField} = this.state
     return (
       <div>
         <button type="button" onClick={this.handleAddBtnClick}>+</button>
         {selectType && this.renderSelectType()}
-        {addItemType && this.renderAddItemForm(addItemType)}
+        {addItemField && this.renderAddItemForm(addItemField)}
         {value && value.map((item, i) => {
           const itemValue = item.unwrap()
           const itemType = (itemValue && itemValue.$type) || resolveJSType(itemValue)
@@ -118,10 +119,10 @@ export default React.createClass({
               <div>
                 <p>Invalid type: <pre>{JSON.stringify(itemType)}</pre></p>
                 <p>Only allowed types are: <pre>{JSON.stringify(type.of)}</pre></p>
-
               </div>
             )
           }
+
           return (
             <div key={i}>
               <RenderListItem index={i} field={typeFromField} value={item} onChange={this.handleItemChange} />
