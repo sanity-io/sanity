470a823965c0443e50464429dc0364dd92a56df5
Bjørge Næss <bjoerge@gmail.com>
2016-05-26 16:22:47 +0200

    Make example-app a sanity instance

---
+++ src/field-inputs/Blocks/BlocksTooltipMenu.js
@@ -0,0 +1,37 @@
+import React, {PropTypes} from 'react'
+import BlocksTooltipMenuItem from './BlocksTooltipMenuItem'
+import {map} from 'lodash'
+
+export default React.createClass({
+  propTypes: {
+    items: PropTypes.array,
+    position: PropTypes.object,
+  },
+
+  render() {
+    const {position} = this.props
+
+    const style = {
+      position: 'absolute',
+      top: position.y,
+      left: position.x,
+      display: 'flex',
+      background: 'black',
+      borderRadius: 3,
+      transform: 'translate(-50%, calc(-100% - 0.2em))',
+      zIndex: 20
+    }
+
+    const items = map(this.props.items, (item, i) => {
+      return (
+        <BlocksTooltipMenuItem key={i} {...item} />
+      )
+    })
+
+    return (
+      <div style={style}>
+        {items}
+      </div>
+    )
+  }
+})
