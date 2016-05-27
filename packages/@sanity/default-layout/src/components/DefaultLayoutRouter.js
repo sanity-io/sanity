17dc89b3b5cf082c23d353a1e26ce961ae00fc47
Espen Hovlandsdal <espen@hovlandsdal.com>
2016-05-27 13:48:32 +0200

    Wrap IntlProvider around router

--- packages/@sanity/default-layout/src/components/DefaultLayoutRouter.js
+++ packages/@sanity/default-layout/src/components/DefaultLayoutRouter.js
@@ -2,9 +2,9 @@ import React from 'react'
 import DefaultLayout from './DefaultLayout'
 import {Router, Route, NotFound, Redirect} from 'router:@sanity/base/router'
 import locationStore from 'datastore:@sanity/base/location'
+import SanityIntlProvider from 'component:@sanity/base/sanity-intl-provider'
 
 class DefaultLayoutRouter extends React.Component {
-
   constructor() {
     super()
     this.state = {}
@@ -30,12 +30,15 @@ class DefaultLayoutRouter extends React.Component {
     if (!location) {
       return null
     }
+
     return (
-      <Router location={location} navigate={this.handleNavigate}>
-        <Route path="/:site/*" component={DefaultLayout} />
-        <Redirect path="/" to="/some-site" />
-        <NotFound component={() => <div>Not found</div>} />
-      </Router>
+      <SanityIntlProvider>
+        <Router location={location} navigate={this.handleNavigate}>
+          <Route path="/:site/*" component={DefaultLayout} />
+          <Redirect path="/" to="/some-site" />
+          <NotFound component={() => <div>Not found</div>} />
+        </Router>
+      </SanityIntlProvider>
     )
   }
 }
