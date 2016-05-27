66124df79b4e69c50d2c75341f65b742ac16b20a
Thomas Drevon <thomas@bengler.no>
2016-05-27 13:33:15 +0200

    Empty object default

--- src/locale/messageFetcher.js
+++ src/locale/messageFetcher.js
@@ -7,7 +7,7 @@ const mergedMessages = rawMessages.reduce((prev, curr) => {
 
 
 function fetchLocalizedMessages(locale) {
-  return Promise.resolve(mergedMessages[locale])
+  return Promise.resolve(mergedMessages[locale] || {})
 }
 
 function fetchAllMessages() {
