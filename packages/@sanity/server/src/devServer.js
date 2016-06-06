0bae303e3fe6ec56cedc7d9484c21fa36b5d83da
Espen Hovlandsdal <espen@hovlandsdal.com>
2016-06-06 16:11:21 +0200

    Use babel-register in order to load React components server-side

--- packages/@sanity/server/src/devServer.js
+++ packages/@sanity/server/src/devServer.js
@@ -3,6 +3,7 @@ import webpackDevMiddleware from 'webpack-dev-middleware'
 import webpackHotMiddleware from 'webpack-hot-middleware'
 import {getBaseServer, applyStaticRoutes} from './baseServer'
 import getWebpackDevConfig from './configs/webpack.config.dev'
+import registerBabel from 'babel-register'
 import find from 'lodash/find'
 import get from 'lodash/get'
 import set from 'lodash/set'
@@ -10,6 +11,10 @@ import set from 'lodash/set'
 export default function getDevServer(config = {}) {
   const app = getBaseServer()
   const webpackConfig = config.webpack || getWebpackDevConfig(config)
+
+  // Use babel-register in order to be able to load things like
+  // the document component, which can contain JSX etc
+  registerBabel()
 
   // Inject hot module reloading preset if not present already
   const babelLoader = find(webpackConfig.module.loaders, {loader: 'babel'})
