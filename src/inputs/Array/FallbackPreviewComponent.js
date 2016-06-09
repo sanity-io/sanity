c459be672eb9e29975859ceb6c18428af9d98328
Bjørge Næss <bjoerge@gmail.com>
2016-06-09 23:10:47 +0200

    Add heuristics to fallback preview component

--- src/inputs/Array/FallbackPreviewComponent.js
+++ src/inputs/Array/FallbackPreviewComponent.js
@@ -1,8 +1,48 @@
 import React, {PropTypes} from 'react'
+import {resolveJSType} from "../../types/utils";
+
+function renderPrimitive(value) {
+  return value
+}
+
+const GUESS_PROPERTIES = 'title,name,label'.split(',')
+function renderObject(value) {
+  const objectProperties = Object.keys(value)
+  const displayProperty = objectProperties.find(key => GUESS_PROPERTIES.includes(key))
+  if (displayProperty) {
+    return value[displayProperty]
+  }
+  return objectProperties
+    .filter(key => !key.startsWith('$'))
+    .slice(2)
+    .map(key => {
+      return `${key}: ${render(value[key])}`
+    })
+}
+
+function renderArray(array) {
+  return array.map(render).join(', ')
+}
+
+function render(value) {
+  if (resolveJSType(value.serialize) === 'function') {
+    return render(value.serialize())
+  }
+  const type = resolveJSType(value)
+  if (type === 'object') {
+    return renderObject(value)
+  }
+  if (type === 'array') {
+    return renderArray(value)
+  }
+  return renderPrimitive(value)
+}
 
 export default function FallbackPreviewComponent(props) {
   return (
-    <div><pre>{JSON.stringify(props.value, null, 2)}</pre></div>
+    <div>
+      {render(props.value)}
+    </div>
   )
 }
 
