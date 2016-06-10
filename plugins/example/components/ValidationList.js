f8dacd82407640068d14746ec6bedb968766c93c
Bjørge Næss <bjoerge@gmail.com>
2016-06-10 17:29:53 +0200

    Guard agianst missing validation

--- plugins/example/components/ValidationList.js
+++ plugins/example/components/ValidationList.js
@@ -7,6 +7,9 @@ class ValidationList extends React.Component {
 
   render() {
     const {messages} = this.props
+    if (!messages) {
+      return null
+    }
     return (
       <ul className={styles.validationList}>
         {messages.map((msg, i) =>
