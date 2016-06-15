ba6b3fb271744447cbb0dcb27f7a62ee290b8c19
Thomas Drevon <thomas@bengler.no>
2016-06-15 10:15:49 +0200

    Add tests for create and delete. Minor reformatting.

--- packages/@sanity/client/test/client.test.js
+++ packages/@sanity/client/test/client.test.js
@@ -6,10 +6,13 @@ const baseUrl = 'https://gradient.url'
 const client = sanityClient({url: baseUrl, dataset: 'foo'})
 
 test('can do simple requests', t => {
-  nock(baseUrl).post('/q/foo', {query: 'query'}).reply(200, {
-    ms: 123,
-    result: [{foo: 'bar'}, {bar: 'foo'}]
-  })
+  nock(baseUrl)
+    .post('/q/foo', {query: 'query'})
+    .reply(200, {
+      ms: 123,
+      result: [{foo: 'bar'}, {bar: 'foo'}]
+    })
+
   return client.fetch('query').then(res => {
     t.is(res[0].foo, 'bar')
     t.is(res[1].bar, 'foo')
@@ -24,6 +27,31 @@ test('can do update mutations', t => {
     .reply(200, {})
 
   return client.update('foo:raptor', patch)
+})
+
+test('can create something', t => {
+  const doc = {title: 'Baloney'}
+
+  nock(baseUrl)
+    .post('/m/foo?returnIds=true', {'foo:': {$$create: doc}})
+    .reply(201, {ms: 123, result: {transactionId: 'bar', docIds: ['foo:99']}
+  })
+
+  return client.create(doc).then(res => {
+    t.is(res.transactionId, 'bar')
+    t.is(res.docIds[0], 'foo:99')
+  })
+})
+
+test('can delete', t => {
+  nock(baseUrl)
+    .post('/m/foo?returnIds=true', {foo: {$$delete: null}})
+    .reply(200, {ms: 123, result: {transactionId: 'bar'}
+  })
+
+  return client.delete('foo').then(res => {
+    t.is(res.transactionId, 'bar')
+  })
 })
 
 test('request errors are captured as errors', t => {
