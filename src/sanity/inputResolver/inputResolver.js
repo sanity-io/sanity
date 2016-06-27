0c95caa0ff6c04891128dc9c3b4b92ca568bda5d
Espen Hovlandsdal <espen@hovlandsdal.com>
2016-06-27 11:58:22 +0200

    Resolve input components by role

--- src/sanity/inputResolver/inputResolver.js
+++ src/sanity/inputResolver/inputResolver.js
@@ -1,5 +1,30 @@
+import array from 'role:@sanity/form-builder/input/array?'
+import boolean from 'role:@sanity/form-builder/input/boolean?'
+import email from 'role:@sanity/form-builder/input/email?'
+import geopoint from 'role:@sanity/form-builder/input/geopoint?'
+import number from 'role:@sanity/form-builder/input/number?'
+import object from 'role:@sanity/form-builder/input/object?'
+import reference from 'role:@sanity/form-builder/input/reference?'
+import string from 'role:@sanity/form-builder/input/string?'
+import text from 'role:@sanity/form-builder/input/text?'
+import url from 'role:@sanity/form-builder/input/url?'
+
+const coreTypes = {
+  array,
+  boolean,
+  email,
+  geopoint,
+  number,
+  object,
+  reference,
+  string,
+  text,
+  url
+}
+
 const inputResolver = (field, fieldType) => {
-  return field.input
+  const inputRole = coreTypes[field.type] || coreTypes[fieldType.name]
+  return field.input || inputRole
 }
 
 export default inputResolver
