f659cc0dee2d9ed1df80793d87a1337a1403d7bc
Espen Hovlandsdal <espen@hovlandsdal.com>
2016-06-27 15:11:30 +0200

    Use action button if it exists

--- packages/@sanity/desk-tool/src/DeskTool.js
+++ packages/@sanity/desk-tool/src/DeskTool.js
@@ -1,19 +1,29 @@
 import React, {PropTypes} from 'react'
 import styles from '../styles/DeskTool.css'
 import PaneResolver from 'config:desk-tool/pane-resolver'
+import ActionButton from 'component:@sanity/base/action-button?'
+import schema from 'schema:@sanity/base/schema'
+import config from 'config:sanity'
+
+function DeskTool({location}) {
+  const actions = (schema.types || []).map(type => ({
+    url: `/${type.name}/${config.api.dataset}:`,
+    title: type.name.substr(0, 1).toUpperCase() + type.name.substr(1)
+  }))
+
+  return (
+    <div className={styles.deskTool}>
+      <PaneResolver location={location.pathname} />
+
+      {ActionButton && <ActionButton actions={actions} />}
+    </div>
+  )
+}
 
 DeskTool.propTypes = {
   location: PropTypes.shape({
     pathname: PropTypes.string.isRequired
   })
-}
-
-function DeskTool({location}) {
-  return (
-    <div className={styles.deskTool}>
-      <PaneResolver location={location.pathname} />
-    </div>
-  )
 }
 
 export default DeskTool
