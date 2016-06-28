71d8c868703b50c7530c2ae13d7476cfaa1ed466
Kristoffer Sivertsen <kristofferjoachim@gmail.com>
2016-06-28 19:38:34 +0200

    adding the switch toggle component

---
+++ src/toggles/Switch.js
@@ -0,0 +1,31 @@
+import React, {PropTypes} from 'react'
+import styles from 'style:@sanity/components/toggles/switch'
+
+export default class Switch extends React.Component {
+  static propTypes = {
+    onChange: PropTypes.func,
+    checked: PropTypes.bool,
+    disabled: PropTypes.bool,
+    label: PropTypes.string
+  }
+  render() {
+    const {disabled, checked} = this.props
+    const rootStyle = disabled ? styles.disabled : styles.enabled
+    const checkedStyle = checked ? styles.checked : styles.unchecked
+
+    return (
+      <label className={`${rootStyle} ${checkedStyle}`}>
+        <div className={styles.track} />
+        <div className={styles.thumb} />
+        <input
+          className={styles.input}
+          type="checkbox"
+          onChange={this.props.onChange}
+          checked={this.props.checked}
+        />
+        <div className={styles.label}>{this.props.label}</div>
+        <div className={styles.focusHelper} />
+      </label>
+    )
+  }
+}
