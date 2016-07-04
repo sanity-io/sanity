5ecbc8fa62365c548d6d1f07082d6a8c5369b470
Kristoffer Sivertsen <kristofferjoachim@gmail.com>
2016-07-05 00:46:31 +0200

    improvements to the native dialog

--- src/dialogs/DefaultDialog.js
+++ src/dialogs/DefaultDialog.js
@@ -2,6 +2,7 @@ import React, {PropTypes} from 'react'
 
 import styles from 'style:@sanity/components/dialogs/default'
 import Button from 'component:@sanity/components/buttons/default'
+import dialogPolyfill from 'dialog-polyfill'
 
 export default class DefaultDialog extends React.Component {
   static propTypes = {
@@ -11,6 +12,7 @@ export default class DefaultDialog extends React.Component {
     children: PropTypes.node,
     onClose: PropTypes.func.isRequired,
     isOpen: PropTypes.bool.isRequired,
+    onCloseClick: PropTypes.func,
     onAction: PropTypes.func,
     actions: PropTypes.arrayOf(PropTypes.shape(
       {
@@ -29,50 +31,74 @@ export default class DefaultDialog extends React.Component {
   constructor(...args) {
     super(...args)
     this.handleActionClick = this.handleActionClick.bind(this)
+    this.handleCloseClick = this.handleCloseClick.bind(this)
+  }
+
+  componentWillReceiveProps(props) {
+    if (props.isOpen) {
+      this.handleOpen()
+    } else {
+      this.handleClose()
+    }
   }
 
   componentDidMount() {
+    this.dialog = dialogPolyfill.registerDialog(this.dialogElement) || this.dialogElement
+    // Now dialog acts like a native <dialog>.
+  }
+
+  handleOpen() {
     this.dialog.showModal()
   }
 
-  handleActionClick(e) {
-    debugger
-    const actionId = e.currentTarget.getAttribute('data-action-id')
+  handleCloseClick() {
+    this.props.onCloseClick()
+    this.handleClose()
+  }
+
+  handleClose() {
+    this.dialog.close()
+  }
+
+  handleActionClick(event) {
+    const actionId = event.currentTarget.getAttribute('data-action-id')
     this.props.onAction(
       this.props.actions.find(action => action.id === actionId)
     )
   }
 
   render() {
-
-    const {kind, title, className, onClose, actions} = this.props
-
-    const style = styles.root
+    const {title, actions, isOpen} = this.props
+    const style = isOpen ? styles.isOpen : styles.root
 
     return (
-      <dialog className={style} ref={(ref) => this.dialog = ref} >
-        <button className={styles.closeButton} onClick={onClose} />
-        <div className={styles.inner}>
+      <dialog className={style} ref={(ref) => this.dialogElement = ref}>
+        <div className={styles.header}>
           <h1 className={styles.heading}>{title}</h1>
+          <button className={styles.closeButton} onClick={this.handleCloseClick} />
+        </div>
+        <div className={styles.inner}>
+
+
           <div className={styles.content}>
             {this.props.children}
           </div>
-        </div>
 
-        <div className={styles.functions}>
-          {
-            actions.map(action => {
-              return (
-                <Button
-                  key={action.id}
-                  onClick={this.handleActionClick}
-                  data-action-id={action.id}
-                >
-                  {action.title}
-                </Button>
-              )
-            })
-          }
+          <div className={styles.functions}>
+            {
+              actions.map(action => {
+                return (
+                  <Button
+                    key={action.id}
+                    onClick={this.handleActionClick}
+                    data-action-id={action.id}
+                  >
+                    {action.title}
+                  </Button>
+                )
+              })
+            }
+          </div>
         </div>
       </dialog>
     )
