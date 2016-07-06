4cc47b35c7b2877d47e1b82a9f3d680a322e7234
Per-Kristian Nordnes <per.kristian.nordnes@gmail.com>
2016-07-06 14:41:37 +0200

    Obtain and refresh token before doing client requests

--- src/client/index.js
+++ src/client/index.js
@@ -1,6 +1,53 @@
 import sanityClient from '@sanity/client-next'
 import config from 'config:sanity'
+import userStore from 'datastore:@sanity/base/user'
+
+const MS_BEFORE_EXPIRATION_TO_RENEW_TOKEN = 60*2*1000 // How many ms left of token expiration time before we obtain a new token
+let currentTokenPayload = null
+
+function base64urlDecode(str) {
+  return new Buffer(base64urlUnescape(str), 'base64').toString()
+}
+
+function base64urlUnescape(str) {
+  str += new Array(5 - str.length % 4).join('=')
+  return str.replace(/\-/g, '+').replace(/_/g, '/')
+}
+
+function extractTokenPayload(token) {
+  const segments = token.split('.')
+  const payloadSeg = segments[1]
+  return JSON.parse(base64urlDecode(payloadSeg))
+}
+
+function tokenIsExpired(tokenPayload) {
+  if (!tokenPayload) {
+    return true
+  }
+  const tokenTime  = currentTokenPayload.x * 1000
+  const now = new Date().getTime()
+  if ((tokenTime - now) > MS_BEFORE_EXPIRATION_TO_RENEW_TOKEN) {
+    return false
+  }
+  return true
+}
+
+function updateClientTokenPromise() {
+  return new Promise((resolve, reject) => {
+    if (!tokenIsExpired(currentTokenPayload)) {
+      return resolve()
+    }
+    userStore.currentToken
+      .map(ev => ev.token)
+      .subscribe(token => {
+        currentTokenPayload = extractTokenPayload(token)
+        client.config({token})
+        resolve()
+      })
+  })
+}
 
 const client = sanityClient(config.api)
+client.on('request', updateClientTokenPromise)
 
-module.exports = client
+export default client
