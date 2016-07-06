8ae31eadbccc30a79d018dda029dd608674f31aa
Per-Kristian Nordnes <per.kristian.nordnes@gmail.com>
2016-07-06 14:45:49 +0200

    LoginStatus wired up

--- packages/@sanity/default-layout/src/components/LoginStatus.js
+++ packages/@sanity/default-layout/src/components/LoginStatus.js
@@ -1,14 +1,63 @@
 import React from 'react'
 import styles from '../../styles/LoginStatus.css'
+import userStore from 'datastore:@sanity/base/user'
+import SanityIntlProvider from 'component:@sanity/base/sanity-intl-provider'
+import {FormattedMessage} from 'component:@sanity/base/locale/intl'
+import config from 'config:sanity'
 
-class LoginStatus extends React.Component {
+export default class LoginStatus extends React.Component {
+
+  constructor() {
+    super()
+    this.toggleLoginStatusMenu = this.toggleLoginStatusMenu.bind(this)
+    this.handleLogoutButtonClicked = this.handleLogoutButtonClicked.bind(this)
+    this.state = {user: null, menuVisible: false}
+  }
+
+  componentWillMount() {
+    this.userSubscription = userStore.currentUser
+      .map(ev => ev.user)
+      .subscribe(user => {
+        this.setState({user: user})
+      })
+  }
+
+  componentWillUnmount() {
+    this.userSubscription.unsubscribe()
+  }
+
+  toggleLoginStatusMenu() {
+    this.setState({menuVisible: !this.state.menuVisible})
+  }
+
+  handleLogoutButtonClicked(evnt) {
+    evnt.preventDefault()
+    userStore.actions.logout().progress.subscribe(ev => {
+      // Nothing to do here...
+    })
+  }
+
   render() {
+    const user = this.state.user
+    if (!user) {
+      return null
+    }
     return (
       <div className={styles.loginStatus}>
-        <img className={styles.userImage} />
+        <img onClick={this.toggleLoginStatusMenu} src={user.profileImage} className={styles.userImage} />
+        {this.state.menuVisible && (
+          <div style={{float: 'left'}}>
+            <SanityIntlProvider supportedLanguages={config.locale.supportedLanguages}>
+              <p>{user.name}</p>
+              <p>
+                <button onClick={this.handleLogoutButtonClicked}>
+                  <FormattedMessage id="logoutButtonText" />
+                </button>
+              </p>   
+            </SanityIntlProvider>
+          </div>
+        )}
       </div>
     )
   }
 }
-
-export default LoginStatus
