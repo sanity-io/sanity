7e178213b41b7d8b790194bfa7d4603f1f5eba26
Espen Hovlandsdal <espen@hovlandsdal.com>
2016-08-01 17:54:11 +0200

    Apply static loader fix to allow assets to work

--- server/storyConfig/webpack.config.js
+++ server/storyConfig/webpack.config.js
@@ -1,6 +1,35 @@
+const path = require('path')
 const sanityServer = require('@sanity/server')
 
-module.exports = sanityServer.getWebpackBaseConfig({
-  basePath: process.cwd(),
-  commonChunkPlugin: false
-})
+module.exports = (storyWpConfig, configType) => {
+  const sanityWpConfig = sanityServer.getWebpackBaseConfig({
+    basePath: process.cwd(),
+    commonChunkPlugin: false
+  })
+
+  let storybookConf = {}
+  try {
+    storybookConf = require(path.join(process.cwd(), 'sanity.json')).storybook
+  } catch (err) {
+    throw err
+  }
+
+  sanityWpConfig.module = sanityWpConfig.module || {loaders: []}
+
+  if (configType === 'DEVELOPMENT') {
+    sanityWpConfig.module.loaders = sanityServer.applyStaticLoaderFix(sanityWpConfig, {
+      listen: Object.assign({
+        hostname: 'localhost',
+        port: 9001,
+        staticPath: './static'
+      }, storybookConf)
+    })
+  }
+
+  return Object.assign({}, sanityWpConfig, storyWpConfig, {
+    plugins: [].concat(storyWpConfig.plugins, sanityWpConfig.plugins || []),
+    module: Object.assign({}, storyWpConfig.module, sanityWpConfig.module, {
+      loaders: [].concat(storyWpConfig.module.loaders, sanityWpConfig.module.loaders)
+    })
+  })
+}
