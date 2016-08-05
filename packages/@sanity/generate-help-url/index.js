50cfc42a15b7d4b5f269afd659630b6662f45146
Espen Hovlandsdal <espen@hovlandsdal.com>
2016-08-05 17:36:15 +0200

    Initial commit

---
+++ packages/@sanity/generate-help-url/index.js
@@ -0,0 +1,5 @@
+var baseUrl = 'https://docs.sanity.io/help/'
+
+module.exports = function generateHelpUrl(slug) {
+  return baseUrl + slug
+}
---
+++ packages/@sanity/generate-help-url/test/generate-help-url.test.js
@@ -0,0 +1,8 @@
+/* eslint-disable id-length */
+const test = require('tape')
+const generateHelpUrl = require('..')
+
+test('should generate valid url', t => {
+  t.equals(generateHelpUrl('foo-bar'), 'https://docs.sanity.io/help/foo-bar')
+  t.end()
+})
