96ed32c710033d5ce0fd21c1e166527d70a59546
Bjørge Næss <bjoerge@gmail.com>
2016-08-24 10:41:15 +0200

    Use latest document store

--- src/sanity/FormBuilderContainer.js
+++ src/sanity/FormBuilderContainer.js
@@ -1,32 +1,139 @@
-import React, {PropTypes} from 'react'
-import equals from 'shallow-equals'
-import SanityFormBuilder from './SanityFormBuilder'
-import QueryContainer from 'component:@sanity/base/query-container'
+// Connects the FormBuilder with various sanity roles
 
-function mapToFormbuilderProps({result, ...rest}) {
-  return {
-    initialValue: result && result.documents && result.documents[0],
-    ...rest
-  }
+import React, {PropTypes} from 'react'
+import documentStore from 'datastore:@sanity/base/document'
+import FormBuilder from './SanityFormBuilder'
+import equals from 'shallow-equals'
+import {unprefixType} from './utils/unprefixType'
+import schema from 'schema:@sanity/base/schema'
+import location from 'datastore:@sanity/base/location'
+
+const preventDefault = ev => ev.preventDefault()
+
+function createFormBuilderStateFrom(serialized, typeName) {
+  return serialized ? FormBuilder.deserialize(unprefixType(serialized), typeName) : FormBuilder.createEmpty(typeName)
 }
 
-class FormBuilderContainer extends React.Component {
+export default class SanityFormBuilder extends React.Component {
+  static propTypes = {
+    documentId: PropTypes.string,
+    typeName: PropTypes.string
+  };
+
+  constructor(props, ...rest) {
+    super(props, ...rest)
+
+    this.state = {value: FormBuilder.createEmpty(props.typeName)}
+
+    this.handleChange = this.handleChange.bind(this)
+    this.handleDocPatch = this.handleDocPatch.bind(this)
+    this.subscriptions = []
+  }
+
+  setupSubscriptions(props) {
+    this.tearDownSubscriptions()
+    const {documentId, typeName} = props
+
+    const byId = documentStore.byId(documentId)
+
+    this.setState({
+      value: FormBuilder.createEmpty(typeName)
+    })
+
+    const initialSubscription = byId
+      .first(event => event.type === 'snapshot')
+      .subscribe(event => {
+        this.setState({
+          value: createFormBuilderStateFrom(event.document, typeName)
+        })
+      })
+
+    const updateSubscription = byId
+      .filter(event => event.type === 'update')
+      .subscribe(event => {
+        this.handleDocPatch(event.patch)
+      })
+
+    this.subscriptions = [initialSubscription, updateSubscription]
+  }
+  tearDownSubscriptions() {
+    this.subscriptions.forEach(sub => sub.unsubscribe())
+  }
+  componentWillMount() {
+    this.setupSubscriptions(this.props)
+  }
+  componentWillUnmount() {
+    this.tearDownSubscriptions()
+  }
+
   shouldComponentUpdate(nextProps, nextState) {
     return !equals(this.props, nextProps) || !equals(this.state, nextState)
   }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.documentId !== this.props.documentId) {
+      this.setupSubscriptions(nextProps)
+    }
+  }
+
+  handleChange(event) {
+
+    const id = this.state.value.getFieldValue('$id')
+
+    const op = id ? this.update(id, event.patch) : this.create(event.patch)
+
+    op.subscribe(() => {
+      console.log('Document saved…')
+    })
+  }
+  update(id, patch) {
+    return documentStore.update(id, patch)
+  }
+  create(patch) {
+    if (this.creating) {
+      return
+    }
+    const {typeName} = this.props
+    const prefixedType = `${schema.name}.${typeName}`
+    const nextValue = this.state.value.patch(patch)
+    this.creating = true
+    return documentStore
+      .create(Object.assign(nextValue.serialize(), {$type: prefixedType}))
+      .delay(1000) // Need to wait for document to actual exist in ES index
+      .do(result => {
+        // todo: fix this
+        location.actions.navigate(document.location.pathname
+          .split('/')
+          .slice(0, -1)
+          .concat(result.documentId)
+          .join('/'))
+      })
+  }
+
+  handleDocPatch(patch) {
+    const nextValue = this.state.value.patch(patch)
+    this.setState({value: nextValue})
+  }
+
   render() {
-    const {documentId} = this.props
+    const {value, saving, validation} = this.state
+
     return (
-      <QueryContainer query="*[.$id == %id]" params={{id: documentId}} mapFn={mapToFormbuilderProps}>
-        <SanityFormBuilder documentId={documentId} typeName={this.props.typeName} />
-      </QueryContainer>
+      <div className="content">
+        <form className="form-container" onSubmit={preventDefault}>
+          <FormBuilder
+            value={value}
+            validation={validation}
+            onChange={this.handleChange}
+          />
+        </form>
+
+        <p>
+          <button disabled={saving} onClick={this.handleSave}>
+            {saving ? 'Saving...' : 'Save'}
+          </button>
+        </p>
+      </div>
     )
   }
 }
-
-FormBuilderContainer.propTypes = {
-  documentId: PropTypes.string,
-  typeName: PropTypes.string
-}
-
-export default FormBuilderContainer
--- src/sanity/SanityFormBuilder.js
+++ src/sanity/SanityFormBuilder.js
@@ -1,13 +1,6 @@
-// Connects the FormBuilder with various sanity roles
-
-import React, {PropTypes} from 'react'
-import documentStore from 'datastore:@sanity/base/document'
-import equals from 'shallow-equals'
 import schema from 'schema:@sanity/base/schema'
 import inputResolver from 'function:@sanity/form-builder/input-resolver'
 import ValidationList from 'component:@sanity/form-builder/validation-list'
-import {unprefixType} from './utils/unprefixType'
-
 import {
   createFormBuilder,
   Schema
@@ -15,95 +8,8 @@ import {
 
 const compiledSchema = Schema.compile(schema)
 
-const FormBuilder = createFormBuilder({
+export default createFormBuilder({
   schema: compiledSchema,
   resolveInputComponent: inputResolver,
   resolveValidationComponent: () => ValidationList
 })
-
-const preventDefault = ev => ev.preventDefault()
-
-class SanityFormBuilder extends React.Component {
-  constructor(props) {
-    super(props)
-
-    this.state = this.getStateForProps(props)
-
-    this.handleChange = this.handleChange.bind(this)
-    this.handleDocPatch = this.handleDocPatch.bind(this)
-  }
-
-  componentWillMount() {
-    documentStore.actions.update.calls.subscribe(call => {
-      const [id, patch] = call.args
-      if (id === this.props.documentId) {
-        this.handleDocPatch(patch)
-      }
-    })
-  }
-  shouldComponentUpdate(nextProps, nextState) {
-    return !equals(this.props, nextProps) || !equals(this.state, nextState)
-  }
-
-  componentWillReceiveProps(nextProps) {
-    this.setState(this.getStateForProps(nextProps))
-  }
-
-  getStateForProps(props) {
-    const value = props.initialValue
-      ? FormBuilder.deserialize(unprefixType(props.initialValue), props.typeName)
-      : FormBuilder.createEmpty(props.typeName)
-
-    return {
-      value: value,
-      changed: false,
-      saving: false
-    }
-  }
-
-  handleChange(event) {
-    const {typeName} = this.props
-    const id = this.state.value.getFieldValue('$id')
-    const prefixedType = `${schema.name}.${typeName}`
-    const mutation = id
-      ? documentStore.actions.update(id, event.patch)
-      : documentStore.actions.create(Object.assign({$type: prefixedType}, event.patch))
-
-    mutation.progress.subscribe(() => {})
-  }
-
-  handleDocPatch(patch) {
-    const nextValue = this.state.value.patch(patch)
-
-    this.setState({value: nextValue})
-  }
-
-  render() {
-    const {value, saving, validation} = this.state
-
-    return (
-      <div className="content">
-        <form className="form-container" onSubmit={preventDefault}>
-          <FormBuilder
-            value={value}
-            validation={validation}
-            onChange={this.handleChange}
-          />
-        </form>
-
-        <p>
-          <button disabled={saving} onClick={this.handleSave}>
-            {saving ? 'Saving...' : 'Save'}
-          </button>
-        </p>
-      </div>
-    )
-  }
-}
-
-SanityFormBuilder.propTypes = {
-  initialValue: PropTypes.object,
-  typeName: PropTypes.string
-}
-
-export default SanityFormBuilder
