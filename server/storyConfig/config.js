1723ac981bf8c21e0d4f6cc39f1756da00b5d9e3
Kristoffer Sivertsen <kristofferjoachim@gmail.com>
2016-08-24 10:51:35 +0200

    normalize css

--- server/storyConfig/config.js
+++ server/storyConfig/config.js
@@ -2,6 +2,7 @@ const storyBook = require('component:@sanity/storybook')
 const infoAddon = require('@sanity/react-storybook-addon-info')
 
 require('style:@sanity/base/theme/body')
+require('style:@sanity/base/theme/normalize')
 require('./styles.css')
 
 storyBook.setAddon(infoAddon.default)
