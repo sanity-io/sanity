c0b512213e31afde6609468b304339d241e12f99
Kristoffer Sivertsen <kristofferjoachim@gmail.com>
2016-08-24 15:32:20 +0200

    Providing icons

---
+++ src/components/icons/CloseIcon.js
@@ -0,0 +1,3 @@
+import CloseIcon from 'react-icons/lib/md/close'
+
+export default CloseIcon
---
+++ src/components/icons/SpinnerIcon.js
@@ -0,0 +1,3 @@
+import SpinnerIcon from 'react-icons/lib/fa/circle-o-notch'
+
+export default SpinnerIcon
