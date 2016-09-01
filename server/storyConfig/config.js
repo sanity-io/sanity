5c88a5697e6e873e1f71a0c5d8ef229bc426bda4
Kristoffer Sivertsen <kristofferjoachim@gmail.com>
2016-09-01 11:44:44 +0200

    removing infoaddon

--- server/storyConfig/config.js
+++ server/storyConfig/config.js
@@ -5,7 +5,6 @@ require('style:@sanity/base/theme/body')
 require('style:@sanity/base/theme/normalize')
 require('./styles.css')
 
-storyBook.setAddon(infoAddon.default)
 
 storyBook.configure(
   () => require('all:story:@sanity/base/component'),
