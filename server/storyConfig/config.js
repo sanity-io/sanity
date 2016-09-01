404f89ddb11d7765b7ee33bc0c4212958334cba8
Kristoffer Sivertsen <kristofferjoachim@gmail.com>
2016-09-01 11:45:06 +0200

    2.0.2

--- server/storyConfig/config.js
+++ server/storyConfig/config.js
@@ -5,6 +5,7 @@ require('style:@sanity/base/theme/body')
 require('style:@sanity/base/theme/normalize')
 require('./styles.css')
 
+storyBook.setAddon(infoAddon.default)
 
 storyBook.configure(
   () => require('all:story:@sanity/base/component'),
