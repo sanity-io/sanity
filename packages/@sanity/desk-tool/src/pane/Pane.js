3a88867a86dcc1ebf922c387de4208a3d30f24be
Espen Hovlandsdal <espen@hovlandsdal.com>
2016-09-14 14:25:49 +0200

    Make Pane stateless

--- packages/@sanity/desk-tool/src/pane/Pane.js
+++ packages/@sanity/desk-tool/src/pane/Pane.js
@@ -1,26 +1,23 @@
 import React, {PropTypes} from 'react'
 import styles from '../../styles/Pane.css'
 
-class Pane extends React.Component {
-
-  render() {
-    const {loading, items, renderItem, isActive} = this.props
-    return (
-      <div className={isActive ? styles.isActive : styles.pane}>
-        <ul className={styles.paneItems}>
-          {loading && <li>Loading...</li>}
-          {items.map((item, i) => {
-            return (
-              <li className={styles.paneItem} key={item.key}>
-                {renderItem(item, i)}
-              </li>
-            )
-          })
-          }
-        </ul>
-      </div>
-    )
-  }
+function Pane(props) {
+  const {loading, items, renderItem, isActive} = props
+  return (
+    <div className={isActive ? styles.isActive : styles.pane}>
+      <ul className={styles.paneItems}>
+        {loading && <li>Loading...</li>}
+        {items.map((item, i) => {
+          return (
+            <li className={styles.paneItem} key={item.key}>
+              {renderItem(item, i)}
+            </li>
+          )
+        })
+        }
+      </ul>
+    </div>
+  )
 }
 
 Pane.defaultProps = {
