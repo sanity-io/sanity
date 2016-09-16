04ff0922c0414bf20e1e73ac8c6520d197d3cd35
Kristoffer Sivertsen <kristofferjoachim@gmail.com>
2016-09-16 16:47:42 +0200

    removing infinity calendar

--- src/datepickers/story.js
+++ src/datepickers/story.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import {storiesOf} from 'part:@sanity/storybook'
 import DatePicker from 'part:@sanity/form-builder/input/date'
-import InfiniteCalendar from 'react-infinite-calendar'
 import styles from './styles.css'
 import 'react-infinite-calendar/styles.css'
 
@@ -18,28 +17,4 @@ storiesOf('Date pickers')
     )
   },
   {PropTables: [DatePicker], role: 'part:@sanity/form-builder/input/date'}
-)
-.addWithInfo(
-  'react-infinite-calendar',
-  `
-    react-datepicker with customized styling
-  `,
-  () => {
-    const today = new Date()
-    const minDate = Number(new Date()) - ((24 * 60 * 60 * 1000) * 7)
-
-    return (
-      <div className={styles.container}>
-        <InfiniteCalendar
-          width={400}
-          height={600}
-          selectedDate={today}
-          disabledDays={[0, 6]}
-          minDate={minDate}
-          keyboardSupport
-        />
-      </div>
-    )
-  },
-  {PropTables: [InfiniteCalendar]}
 )
