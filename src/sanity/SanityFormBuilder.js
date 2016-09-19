f49512835dad54ee139daf5656a9751b0abfd631
Bjørge Næss <bjoerge@gmail.com>
2016-09-19 14:40:52 +0200

    Don't import form builder from role

--- src/sanity/SanityFormBuilder.js
+++ src/sanity/SanityFormBuilder.js
@@ -1,10 +1,11 @@
 import schema from 'schema:@sanity/base/schema'
 import inputResolver from 'function:@sanity/form-builder/input-resolver'
 import ValidationList from 'component:@sanity/form-builder/validation-list'
+
 import {
   createFormBuilder,
   Schema
-} from 'role:@sanity/form-builder'
+} from '../index'
 
 const compiledSchema = Schema.compile(schema)
 
