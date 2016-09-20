8654db6c3031542d1302f7c7c1ac5b5f72cab8b9
Espen Hovlandsdal <espen@hovlandsdal.com>
2016-09-20 15:28:11 +0200

    Role => part

--- server/storyConfig/config.js
+++ server/storyConfig/config.js
@@ -1,13 +1,13 @@
-const storyBook = require('component:@sanity/storybook')
+const storyBook = require('part:@sanity/storybook')
 const infoAddon = require('@kadira/react-storybook-addon-info')
 
-require('style:@sanity/base/theme/body')
-require('style:@sanity/base/theme/normalize')
+require('part:@sanity/base/theme/body-style')
+require('part:@sanity/base/theme/normalize-style')
 require('./styles.css')
 
 storyBook.setAddon(infoAddon.default)
 
 storyBook.configure(
-  () => require('all:story:@sanity/base/component'),
+  () => require('all:part:@sanity/base/component'),
   module
 )
