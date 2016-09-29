22f2bd4866f8965436865e9778ae52be6c009c52
Kristoffer Sivertsen <kristofferjoachim@gmail.com>
2016-09-29 14:27:13 +0200

    More options for Spinner loader

--- src/loading/Spinner.js
+++ src/loading/Spinner.js
@@ -1,12 +1,28 @@
-import React from 'react'
+import React, {PropTypes} from 'react'
 import styles from 'part:@sanity/components/loading/spinner-style'
 import SpinnerIcon from 'part:@sanity/base/spinner-icon'
 
 export default class Spinner extends React.Component {
+  static propTypes = {
+    inline: PropTypes.bool,
+    message: PropTypes.string,
+    fullscreen: PropTypes.bool
+  }
   render() {
+    const {inline, message, fullscreen} = this.props
     return (
-      <div className={styles.root}>
-        <SpinnerIcon color="inherit" />
+      <div
+        className={`
+          ${inline ? styles.inline : styles.block}
+          ${fullscreen && styles.fullscreen}
+        `}
+      >
+        <div className={styles.inner}>
+          <SpinnerIcon color="inherit" />
+          {
+            message && <div className={styles.message}>{message}</div>
+          }
+        </div>
       </div>
     )
   }
--- src/loading/story.js
+++ src/loading/story.js
@@ -7,11 +7,72 @@ storiesOf('Loading')
 .addWithInfo(
   'Spinner',
   `
-    Spinner
+    Spinner block
   `,
   () => {
     return (
       <Spinner />
+    )
+  },
+  {
+    propTables: [Spinner],
+    role: 'part:@sanity/components/loading/spinner'
+  }
+)
+.addWithInfo(
+  'Spinner inline',
+  `
+    Spinner
+  `,
+  () => {
+    return (
+      <Spinner inline />
+    )
+  },
+  {
+    propTables: [Spinner],
+    role: 'part:@sanity/components/loading/spinner'
+  }
+)
+.addWithInfo(
+  'Spinner with message',
+  `
+    Spinner
+  `,
+  () => {
+    return (
+      <Spinner message="Loading" />
+    )
+  },
+  {
+    propTables: [Spinner],
+    role: 'part:@sanity/components/loading/spinner'
+  }
+)
+.addWithInfo(
+  'Spinner inline with message',
+  `
+    Spinner
+  `,
+  () => {
+    return (
+      <Spinner inline message="Loading" />
+    )
+  },
+  {
+    propTables: [Spinner],
+    role: 'part:@sanity/components/loading/spinner'
+  }
+)
+
+.addWithInfo(
+  'Spinner fullscreen',
+  `
+    Spinner
+  `,
+  () => {
+    return (
+      <Spinner fullscreen message="Loading" />
     )
   },
   {
