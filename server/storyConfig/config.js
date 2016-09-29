617a21a53a48aa9a7592cc0d8c67853a1d920e38
Kristoffer Sivertsen <kristofferjoachim@gmail.com>
2016-09-29 15:08:43 +0200

    Using body from base

--- server/storyConfig/config.js
+++ server/storyConfig/config.js
@@ -2,7 +2,6 @@ const storyBook = require('part:@sanity/storybook')
 const infoAddon = require('@kadira/react-storybook-addon-info')
 
 require('part:@sanity/base/theme/body-style')
-require('part:@sanity/base/theme/normalize-style')
 require('./styles.css')
 
 storyBook.setAddon(infoAddon.default)
