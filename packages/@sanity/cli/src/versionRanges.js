8ff29e089aaad27876eeccec04815493619d02be
Espen Hovlandsdal <espen@hovlandsdal.com>
2016-10-24 09:48:45 +0200

    Bump version ranges for bootstrapped packages

--- packages/@sanity/cli/src/versionRanges.js
+++ packages/@sanity/cli/src/versionRanges.js
@@ -1,10 +1,10 @@
 export default {
   // Dependencies for a default Sanity installation
   core: {
-    '@sanity/base': '^0.0.16',
-    '@sanity/components': '^0.0.10',
-    '@sanity/default-layout': '^0.0.3',
-    '@sanity/default-login': '^0.0.1',
+    '@sanity/base': '^0.0.17',
+    '@sanity/components': '^0.0.11',
+    '@sanity/default-layout': '^0.0.4',
+    '@sanity/default-login': '^0.0.2',
     '@sanity/desk-tool': '^0.1.3',
     'react': '^15.3.0',
     'react-dom': '^15.3.0'
@@ -13,7 +13,7 @@ export default {
   // Only used for Sanity-style plugins (eg, the ones we build at Sanity.io)
   plugin: {
     dev: {
-      '@sanity/check': '^0.0.1',
+      '@sanity/check': '^0.0.2',
       'babel-cli': '^6.9.0',
       'babel-eslint': '^6.0.4',
       'babel-plugin-syntax-class-properties': '^6.8.0',
