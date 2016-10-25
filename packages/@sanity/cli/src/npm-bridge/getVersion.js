3f175303b2b73291b9f977a674096a0799e8c651
Espen Hovlandsdal <espen@hovlandsdal.com>
2016-10-25 14:43:24 +0200

    Remove unused module

--- packages/@sanity/cli/src/npm-bridge/getVersion.js
+++
@@ -1,8 +0,0 @@
-import {execute} from './execute'
-
-function getVersion(pkg, opts = {}) {
-  return execute(['view', pkg, 'version', '--quiet'], opts)
-    .then(res => res.stdout.trim())
-}
-
-export default getVersion
