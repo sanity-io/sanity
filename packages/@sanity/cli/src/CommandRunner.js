a2e1900d733a32fd798d7349d726022072cee9c6
Espen Hovlandsdal <espen@hovlandsdal.com>
2016-11-03 00:01:42 +0100

    Remove duplicate commands

--- packages/@sanity/cli/src/CommandRunner.js
+++ packages/@sanity/cli/src/CommandRunner.js
@@ -1,6 +1,8 @@
 import path from 'path'
+import chalk from 'chalk'
 import groupBy from 'lodash/groupBy'
 import sortBy from 'lodash/sortBy'
+import cloneDeep from 'lodash/cloneDeep'
 import {loadJson} from '@sanity/util/lib/safeJson'
 import lazyRequire from '@sanity/util/lib/lazyRequire'
 import defaultCommands from './commands'
@@ -36,10 +38,19 @@ export default class CommandRunner {
       return Promise.resolve()
     }
 
-    const subCommand = args.argsWithoutOptions[0]
+    // We might modify arguments for easier parsing in subcommand
+    const cmdArgs = cloneDeep(args)
+
+    const subCommand = cmdArgs.argsWithoutOptions[0]
     const group = this.commandGroups[commandOrGroup] || this.commandGroups.default
     const command = group.find(cmdHasName(subCommand)) || group.find(cmdHasName(commandOrGroup))
     const isDefaultGroup = group === this.commandGroups.default
+
+    // If the command is not part of the default group, the first argument is the subcommand
+    // Since subcommands know their own name, this shouldn't be necessary to pass on
+    if (!isDefaultGroup) {
+      cmdArgs.argsWithoutOptions = args.argsWithoutOptions.slice(1)
+    }
 
     // The group exists and is not "default", but the subcommand doesn't exist
     if (!command && subCommand && !isDefaultGroup) {
@@ -75,12 +86,13 @@ export default class CommandRunner {
       prompt,
       apiClient,
       yarn,
+      chalk,
       ...options,
       commandRunner: this
     }
 
     debug(`Running command "${command.name}"`)
-    return command.action(args, context)
+    return command.action(cmdArgs, context)
   }
 }
 
--- packages/@sanity/cli/src/util/mergeCommands.js
+++ packages/@sanity/cli/src/util/mergeCommands.js
@@ -1,8 +1,27 @@
+import find from 'lodash/find'
+
 export default (baseCommands, corePath) => {
   if (!corePath) {
     return baseCommands
   }
 
   const core = require(corePath)
-  return baseCommands.concat(core.commands)
+  const merged = baseCommands.concat(core.commands).map(addDefaultGroup)
+
+  // Remove duplicate commands when within the same group,
+  // the last defined commands with the given name wins
+  return merged.reverse().reduce((cmds, cmd) => {
+    if (!find(cmds, {name: cmd.name, group: cmd.group})) {
+      cmds.push(cmd)
+    }
+    return cmds
+  }, [])
+}
+
+function addDefaultGroup(cmd) {
+  if (!cmd.group) {
+    cmd.group = 'default'
+  }
+
+  return cmd
 }
