f4d67eea4da58f7ee88eb44ce7107009471995a8
Espen Hovlandsdal <espen@hovlandsdal.com>
2016-11-09 13:01:16 +0100

    Remove 'some-site' from URLs

--- packages/@sanity/default-layout/src/defaultLayoutRouter.js
+++ packages/@sanity/default-layout/src/defaultLayoutRouter.js
@@ -1,7 +1,13 @@
 import {createRoute, createScope} from 'part:@sanity/base/router'
 import tools from 'all:part:@sanity/base/tool'
 
-export default createRoute('/some-site/:tool/*', params => {
-  const tool = tools.find(tool => tool.name === params.tool)
+export default createRoute('/:tool/*', params => {
+  const tool = tools.find(curr => curr.name === params.tool)
+
+  // @todo implement better handling of errors
+  if (!tool) {
+    throw new Error(`Tool with name "${params.tool}" not found`)
+  }
+
   return createScope(tool.name, tool.router)
 })
