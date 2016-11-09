2f7a2d94368d293f87d90f017b3eca9008d83554
Kristoffer Sivertsen <kristofferjoachim@gmail.com>
2016-11-09 11:45:18 +0100

    Removing partly support for default preview component

--- src/inputs/Array/FallbackPreviewComponent.js
+++ src/inputs/Array/FallbackPreviewComponent.js
@@ -1,7 +1,6 @@
 import React, {PropTypes} from 'react'
 import {resolveJSType} from '../../schema/types/utils'
 import styles from './styles/FallbackPreviewComponent.css'
-import SanityPreview from 'part:@sanity/components/previews/default' // eslint-disable-line
 
 function renderPrimitive(value) {
   return value
@@ -28,17 +27,6 @@ function renderArray(array) {
   return array.map(render).join(', ')
 }
 
-function resolvePreviewPath(fieldName, value) {
-  const fieldValue = value.value[fieldName] && value.value[fieldName].value
-  const type = resolveJSType(fieldValue)
-
-  if (fieldName && type == 'array') {
-    return fieldValue.join(', ')
-  }
-
-  return fieldValue
-}
-
 function render(value, fieldType, field) {
   const type = resolveJSType(value)
   if (type === 'object') {
@@ -54,20 +42,6 @@ function render(value, fieldType, field) {
 }
 
 export default function FallbackPreviewComponent(props) {
-
-  // Preview defined in schema
-  if (props.type.options && props.type.options.preview) {
-    const preview = props.type.options.preview
-    return (<SanityPreview
-      title={resolvePreviewPath(preview.title, props.value)}
-      subtitle={resolvePreviewPath(preview.subtitle, props.value)}
-      description={resolvePreviewPath(preview.description, props.value)}
-      emptyText={preview.emptyText}
-      sanityImage={false}
-            />
-    )
-  }
-
   return (
     <div className={styles.root}>
       {render(props.value, props.type, props.field) || <pre>[{props.field.type}: {JSON.stringify(props.value)}]</pre>}
