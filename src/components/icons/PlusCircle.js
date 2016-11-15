0b23f02230c3a14a3fc2f4c3937e46336156a8d9
Kristoffer Sivertsen <kristofferjoachim@gmail.com>
2016-11-15 15:32:44 +0100

    adding plus circle icons

---
+++ src/components/icons/PlusCircle.js
@@ -0,0 +1,3 @@
+import PlusCircle from 'react-icons/lib/md/plus-circle'
+
+export default PlusCircle
---
+++ src/components/icons/PlusCircleOutline.js
@@ -0,0 +1,3 @@
+import PlusCircleOutline from 'react-icons/lib/md/plus-circle-outline'
+
+export default PlusCircleOutline
