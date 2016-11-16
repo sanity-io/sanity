010293c901b11970712ab2615b8f5d2ea06d2168
Kristoffer Sivertsen <kristofferjoachim@gmail.com>
2016-11-16 12:53:34 +0100

    new fullscreen spinner

--- src/loading/Spinner.js
+++ src/loading/Spinner.js
@@ -8,6 +8,34 @@ export default class Spinner extends React.Component {
     message: PropTypes.string,
     fullscreen: PropTypes.bool
   }
+
+  renderSvg() {
+    return (
+      <svg width="126px" height="126px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid">
+        <rect x="0" y="0" width="100" height="100" fill="none" />
+        <circle
+          cx="50"
+          cy="50"
+          r="49.5"
+          strokeDasharray="202.1614872585032 108.85618544688634"
+          stroke="#000000"
+          fill="none"
+          strokeWidth="1"
+        >
+          <animateTransform
+            attributeName="transform"
+            type="rotate"
+            values="0 50 50;180 50 50;360 50 50;"
+            keyTimes="0;0.5;1"
+            dur="1s"
+            repeatCount="indefinite"
+            begin="0s"
+          />
+        </circle>
+      </svg>
+    )
+  }
+
   render() {
     const {inline, message, fullscreen} = this.props
     return (
@@ -18,7 +46,9 @@ export default class Spinner extends React.Component {
         `}
       >
         <div className={styles.inner}>
-          <SpinnerIcon color="inherit" />
+          {
+            fullscreen ? this.renderSvg() : <SpinnerIcon color="inherit" />
+          }
           {
             message && <div className={styles.message}>{message}</div>
           }
