d7d0a3e2951d04e5104558be8bbede7deaa455bb
Bjørge Næss <bjoerge@gmail.com>
2016-11-22 14:48:11 +0100

    Deprecate passing react node as children to QueryContainer, support using a function instead

--- src/components/QueryContainer.js
+++ src/components/QueryContainer.js
@@ -2,11 +2,22 @@ import React, {PropTypes} from 'react'
 import {bindAll} from 'lodash'
 import store from 'part:@sanity/base/datastore/document'
 
-// todo: make configurable
-const DEFAULT_PROPS_MAPPING = {
-  result: 'result',
-  error: 'error',
-  complete: 'complete'
+function deprecatedCheck(props, propName, componentName, ...rest) {
+  if (React.isValidElement(props[propName])) {
+    return new Error(
+      `Passing a React element as ${propName} to ${componentName} is deprecated. Use a function instead.`
+    )
+  }
+  return PropTypes.func.isRequired(props, propName, componentName, ...rest)
+}
+
+function createInitialState() {
+  return {
+    result: null,
+    complete: false,
+    loading: false,
+    error: false
+  }
 }
 
 export default class QueryContainer extends React.Component {
@@ -14,7 +25,7 @@ export default class QueryContainer extends React.Component {
   static propTypes = {
     query: PropTypes.string,
     mapFn: PropTypes.func,
-    children: PropTypes.element,
+    children: deprecatedCheck,
     params: PropTypes.object
   };
 
@@ -22,25 +33,7 @@ export default class QueryContainer extends React.Component {
     mapFn: props => props
   }
 
-  static getInitialState() {
-    return {
-      result: null,
-      complete: false,
-      error: false
-    }
-  }
-
-  constructor(...args) {
-    super(...args)
-
-    this.state = QueryContainer.getInitialState()
-
-    bindAll(this, [
-      'next',
-      'error',
-      'complete'
-    ])
-  }
+  state = createInitialState()
 
   componentWillMount() {
     this.subscribe(this.props.query, this.props.params)
@@ -51,11 +44,27 @@ export default class QueryContainer extends React.Component {
   }
 
   subscribe(query, params) {
+    this.unsubscribe()
     this._subscription = store.query(query, params).subscribe(this)
   }
 
+  next = result => {
+    this.setState({result})
+  }
+
+  error = error => {
+    console.error(error) // @todo make sure some kind of error dialog is shown, somewhere
+    this.setState({error})
+  }
+
+  complete = () => {
+    this.setState({complete: true})
+  }
+
   unsubscribe() {
-    this._subscription.unsubscribe()
+    if (this._subscription) {
+      this._subscription.unsubscribe()
+    }
   }
 
   componentWillReceiveProps(nextProps) {
@@ -63,26 +72,23 @@ export default class QueryContainer extends React.Component {
     const sameParams = nextProps.params === this.props.params
 
     if (!sameQuery || !sameParams) {
-      this.unsubscribe()
+      this.setState(createInitialState())
       this.subscribe(nextProps.query, nextProps.params)
-      this.setState(QueryContainer.getInitialState())
     }
   }
 
-  next(result) {
-    this.setState({result})
-  }
-
-  error(error) {
-    console.error(error) // @todo make sure some kind of error dialog is shown, somewhere
-    this.setState({error})
-  }
-
-  complete() {
-    this.setState({complete: true})
+  renderDeprecated() {
+    return React.cloneElement(React.Children.only(this.props.children), this.props.mapFn(this.state))
   }
 
   render() {
-    return React.cloneElement(React.Children.only(this.props.children), this.props.mapFn(this.state))
+    const {children, mapFn} = this.props
+    if (React.isValidElement(children)) {
+      return this.renderDeprecated()
+    }
+    if (!children || typeof children !== 'function') {
+      return <div>Invalid usage of QueryContainer. Expected a function as its only child</div>
+    }
+    return children(mapFn(this.state))
   }
 }
