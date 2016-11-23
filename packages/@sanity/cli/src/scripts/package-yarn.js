c4a2dac59bbd8ce7a3b7044ef19a570707ddaee5
Espen Hovlandsdal <espen@hovlandsdal.com>
2016-11-23 10:01:22 +0100

    Upgrade yarn to latest version

--- packages/@sanity/cli/src/scripts/package-yarn.js
+++ packages/@sanity/cli/src/scripts/package-yarn.js
@@ -3,7 +3,7 @@ import path from 'path'
 import fsp from 'fs-promise'
 import got from 'got'
 
-const version = '0.16.1'
+const version = '0.17.8'
 const baseUrl = 'https://github.com/yarnpkg/yarn/releases/download'
 const bundleUrl = `${baseUrl}/v${version}/yarn-legacy-${version}.js`
 const licenseUrl = 'https://raw.githubusercontent.com/yarnpkg/yarn/master/LICENSE'
@@ -22,7 +22,7 @@ async function writeHeader() {
   console.log('[package-yarn] Downloading license')
   const license = await got(licenseUrl).then(res => res.body)
   const commented = license.split('\n').map(line => ` * ${line}`).join('\n')
-  const wrapped = `/*\n${commented}*/`
+  const wrappedLicense = `/*\n${commented}*/`
 
   console.log('[package-yarn] Reading bundle')
   const bundle = await fsp.readFile(destination, writeFlags)
@@ -31,7 +31,9 @@ async function writeHeader() {
   }
 
   console.log('[package-yarn] Writing modified bundle')
-  const licensed = bundle.replace(/^(#!.*\n)/, `$1${wrapped}\n\n`)
+  const pkgDate = (new Date()).toISOString().substr(0, 10)
+  const versionString = `/* yarn v${version} - packaged ${pkgDate} */`
+  const licensed = bundle.replace(/^(#!.*\n)/, `$1${versionString}\n${wrappedLicense}\n\n`)
   await fsp.writeFile(destination, licensed, writeFlags)
 }
 
