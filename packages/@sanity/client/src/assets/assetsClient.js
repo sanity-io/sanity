bd762f98d9405cb59ceea454dadb63a289128776
Espen Hovlandsdal <espen@hovlandsdal.com>
2016-12-15 17:35:13 +0100

    Restructured assets upload method slightly

--- packages/@sanity/client/src/assets/assetsClient.js
+++ packages/@sanity/client/src/assets/assetsClient.js
@@ -5,30 +5,19 @@ function AssetsClient(client) {
   this.client = client
 }
 
-const ASSET_TYPES_TO_ENDPOINT = {
-  image: 'images',
-  file: 'files'
-}
-
 assign(AssetsClient.prototype, {
   upload(assetType, body, options = {}) {
     validators.validateAssetType(assetType)
 
     const dataset = validators.hasDataset(this.client.clientConfig)
-    const headers = ('contentType' in options)
-      ? {'Content-Type': options.contentType}
-      : {}
-
-    const assetEndpointSegment = ASSET_TYPES_TO_ENDPOINT[assetType]
-    const query = options.label ? {label: options.label} : {}
-    const url = `/assets/${assetEndpointSegment}/${dataset}`
+    const assetEndpoint = assetType === 'image' ? 'images' : 'files'
 
     return this.client.requestObservable({
       method: 'POST',
-      timeout: 0,
-      query,
-      headers,
-      uri: url,
+      timeout: options.timeout || 0,
+      query: options.label ? {label: options.label} : {},
+      uri: `/assets/${assetEndpoint}/${dataset}`,
+      headers: options.contentType ? {'Content-Type': options.contentType} : {},
       body
     })
   }
