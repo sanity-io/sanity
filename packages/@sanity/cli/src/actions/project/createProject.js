fab22f636c8717dde34fae542b28172acaeeb46b
Espen Hovlandsdal <espen@hovlandsdal.com>
2016-12-19 11:41:08 +0100

    Fix incorrect request body in init command

--- packages/@sanity/cli/src/actions/project/createProject.js
+++ packages/@sanity/cli/src/actions/project/createProject.js
@@ -5,7 +5,7 @@ export default function createProject(apiClient, options) {
   }).request({
     method: 'POST',
     uri: '/projects',
-    json: options
+    body: options
   }).then(response => ({
     projectId: response.projectId,
     displayName: options.displayName || ''
