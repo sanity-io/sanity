4d234565c3685dffd6d60f195617a125255c7977
Bjørge Næss <bjoerge@gmail.com>
2016-12-22 11:41:08 +0100

    Fix wiring of File input

--- src/sanity/inputs/File.js
+++ src/sanity/inputs/File.js
@@ -1,13 +1,22 @@
 import client from 'part:@sanity/base/client'
 import {FileInput} from '../../index'
 
-function upload(image) {
-  return client.assets.upload('file', image.file)
+function upload(file) {
+  return client.assets.upload('file', file).map(event => {
+
+    if (event.type !== 'response') {
+      return event
+    }
+    // rewrite event type
+    return {
+      type: 'complete',
+      id: event.body.document._id
+    }
+  })
 }
 
 function materializeReference(id) {
-  // eslint-disable-next-line no-console
-  return console.log('get reference not yet supported')
+  return client.fetch('*[_id == $id]', {id: id}).then(results => results[0])
 }
 
 export default FileInput.create({
