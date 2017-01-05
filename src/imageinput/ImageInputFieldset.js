81a4bd34789ea304d306e31ed07346f335c80fb1
Espen Hovlandsdal <espen@hovlandsdal.com>
2017-01-05 12:12:42 +0100

    Add 'clear image'-button

--- src/imageinput/ImageInputFieldset.js
+++ src/imageinput/ImageInputFieldset.js
@@ -1,7 +1,6 @@
 import React, {PropTypes} from 'react'
 
 import styles from 'part:@sanity/components/imageinput/fieldset-style'
-// import DefaultLabel from 'part:@sanity/components/labels/default'
 import ProgressCircle from 'part:@sanity/components/progress/circle'
 import UploadIcon from 'part:@sanity/base/upload-icon'
 import Fieldset from 'part:@sanity/components/fieldsets/default'
@@ -9,7 +8,6 @@ import createImageLoader from './common/createImageLoader'
 import _HotspotImage from '@sanity/imagetool/HotspotImage'
 import ImageSelect from 'part:@sanity/components/imageinput/image-select'
 import {DEFAULT_CROP} from '@sanity/imagetool/constants'
-// import Button from 'part:@sanity/components/buttons/default'
 
 const HotspotImage = createImageLoader(_HotspotImage, image => {
   return {srcAspectRatio: image.width / image.height}
@@ -22,7 +20,7 @@ const DEFAULT_HOTSPOT = {
   y: 0.5
 }
 
-export default class ImageInputFieldset extends React.Component {
+export default class ImageInputFieldset extends React.PureComponent {
   static propTypes = {
     status: PropTypes.oneOf(['ready', 'complete', 'pending', 'error']),
     legend: PropTypes.string,
@@ -31,6 +29,7 @@ export default class ImageInputFieldset extends React.Component {
     fieldName: PropTypes.string,
     onSelect: PropTypes.func,
     onCancel: PropTypes.func,
+    onClear: PropTypes.func,
     hotspotImage: PropTypes.shape({
       hotspot: PropTypes.object,
       crop: PropTypes.object,
@@ -50,11 +49,6 @@ export default class ImageInputFieldset extends React.Component {
       aspect: null
     }
   }
-
-  componentWillMount() {
-    // this._inputId = uniqueId('ImageInputFieldset')
-  }
-
 
   render() {
 
@@ -84,18 +78,22 @@ export default class ImageInputFieldset extends React.Component {
                 }
                 {
                   (status != 'error' && status != 'pending') && (
-                    <ImageSelect
-                      className={styles.imageSelect}
-                      name={fieldName}
-                      onSelect={this.props.onSelect}
-                    >
-                      <div className={styles.uploadInner}>
-                        <div className={styles.uploadIconContainer}>
-                          <UploadIcon className={styles.uploadIcon} />
+                    <div>
+                      <ImageSelect
+                        className={styles.imageSelect}
+                        name={fieldName}
+                        onSelect={this.props.onSelect}
+                      >
+                        <div className={styles.uploadInner}>
+                          <div className={styles.uploadIconContainer}>
+                            <UploadIcon className={styles.uploadIcon} />
+                          </div>
+                          <span className={styles.uploadIconText}>Replace image</span>
                         </div>
-                        <span className={styles.uploadIconText}>Replace image</span>
-                      </div>
-                    </ImageSelect>
+                      </ImageSelect>
+
+                      <button style={{position: 'absolute'}} onClick={this.props.onClear}>Remove image</button>
+                    </div>
                   )
                 }
               </div>
