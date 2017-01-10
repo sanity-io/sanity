626519b798e5f5da176f2d57b3c1e48f4dee3fa4
Espen Hovlandsdal <espen@hovlandsdal.com>
2017-01-10 16:12:33 +0100

    Simplify case where no login wrapper is defined

--- packages/@sanity/default-layout/src/components/DefaultLayoutContainer.js
+++ packages/@sanity/default-layout/src/components/DefaultLayoutContainer.js
@@ -2,10 +2,9 @@ import React from 'react'
 import DefaultLayout from './DefaultLayout'
 import locationStore from 'part:@sanity/base/location'
 import SanityIntlProvider from 'part:@sanity/base/sanity-intl-provider'
-import LoginWrapperPart from 'part:@sanity/base/login-wrapper?'
+import LoginWrapper from 'part:@sanity/base/login-wrapper?'
 import {RouterProvider} from 'part:@sanity/base/router'
 import config from 'config:sanity'
-import NoLogin from './NoLogin'
 import NotFound from './NotFound'
 import getOrderedTools from '../util/getOrderedTools'
 import rootRouter from '../defaultLayoutRouter'
@@ -31,19 +30,20 @@ class DefaultLayoutContainer extends React.PureComponent {
     const locale = config.locale || {}
     const tools = getOrderedTools()
     const supportedLanguages = locale.supportedLanguages || ['en-US']
-    const LoginWrapper = LoginWrapperPart || NoLogin
 
     if (!location) {
       return null
     }
 
+    const router = (
+      <RouterProvider router={rootRouter} state={rootRouter.decode(location.pathname)} onNavigate={this.handleNavigate}>
+        {rootRouter.isNotFound(location.pathname) ? <NotFound /> : <DefaultLayout tools={tools} />}
+      </RouterProvider>
+    )
+
     return (
       <SanityIntlProvider supportedLanguages={supportedLanguages}>
-        <LoginWrapper>
-          <RouterProvider router={rootRouter} state={rootRouter.decode(location.pathname)} onNavigate={this.handleNavigate}>
-            {rootRouter.isNotFound(location.pathname) ? <NotFound /> : <DefaultLayout tools={tools} />}
-          </RouterProvider>
-        </LoginWrapper>
+        {LoginWrapper ? <LoginWrapper>{router}</LoginWrapper> : router}
       </SanityIntlProvider>
     )
   }
