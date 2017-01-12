7688d513f438f053003934ea2f2c83b8360ed9b7
Bjørge Næss <bjoerge@gmail.com>
2017-01-12 23:02:27 +0100

    Minor code readability improvement

--- src/sanity/inputs/Image.js
+++ src/sanity/inputs/Image.js
@@ -4,14 +4,15 @@ import {ImageInput} from '../../index'
 function upload(image) {
   return client.assets.upload('image', image.file).map(event => {
 
-    if (event.type !== 'response') {
-      return event
+    if (event.type === 'response') {
+      // rewrite to a 'complete' event
+      return {
+        type: 'complete',
+        id: event.body.document._id
+      }
     }
-    // rewrite event type
-    return {
-      type: 'complete',
-      id: event.body.document._id
-    }
+
+    return event
   })
 }
 
