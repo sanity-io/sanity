faf683306cd6d64a7629359569eb5d629f2e7d06
Kristoffer Sivertsen <kristofferjoachim@gmail.com>
2017-01-16 14:05:08 +0100

    duplicate code into example to test how the resolver works in sanity

--- examples/schema-testbed/components/custom/resolveReferenceInput.js
+++ examples/schema-testbed/components/custom/resolveReferenceInput.js
@@ -1,27 +1,40 @@
 import {ReferenceInput} from '../../../../src'
 import {search, fetch, materializeReferences} from './mock/fetchers'
 
-const ReferenceSearchableSelect = ReferenceInput.createSearchableSelect({
-  search,
-  materializeReferences
-})
-
 const ReferenceBrowser = ReferenceInput.createBrowser({
   fetch,
   materializeReferences
 })
 
-const ReferenceSelect = ReferenceInput.createSelect({
+const ReferenceBrowserWithSearch = ReferenceInput.createBrowser({
   search,
-  fetchAll: fetch
+  fetch,
+  materializeReferences
 })
 
-export default function resolveReferenceInput(field) {
+const ReferenceSearchableSelect = ReferenceInput.createSearchableSelect({
+  search,
+  materializeReferences
+})
+
+const ReferenceSelect = ReferenceInput.createSelect({
+  fetchAll: fetch,
+  materializeReferences
+})
+
+export default function resolveReference(field) {
   const fieldOptions = field.options || {}
   if (fieldOptions.inputType === 'select') {
     return fieldOptions.searchable
       ? ReferenceSearchableSelect
       : ReferenceSelect
   }
-  return ReferenceBrowser
+
+  if (fieldOptions.inputType === 'browser') {
+    return fieldOptions.searchable
+      ? ReferenceBrowserWithSearch
+      : ReferenceBrowser
+  }
+
+  return ReferenceSearchableSelect
 }
