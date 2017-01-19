938c36322c7135c7c28355cdca3355f760e62ac9
Espen Hovlandsdal <espen@hovlandsdal.com>
2017-01-19 17:33:25 +0100

    [webpack-loader] Remove unused tests

--- packages/@sanity/webpack-loader/test/init.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-const chai = require('chai')
-const chaiAsPromised = require('chai-as-promised')
-const sinonChai = require('sinon-chai')
-
-chai.should()
-chai.use(chaiAsPromised)
-chai.use(sinonChai)
