099906a0fcd9346921c2fba893daffb6e27f7bc7
Kristoffer Sivertsen <kristofferjoachim@gmail.com>
2017-01-29 18:35:20 +0100

    [form-builder] Focus state on array of string

--- packages/@sanity/form-builder/src/inputs/Array/ArrayOfStrings.js
+++ packages/@sanity/form-builder/src/inputs/Array/ArrayOfStrings.js
@@ -3,18 +3,22 @@ import React, {PropTypes} from 'react'
 import TagInput from 'part:@sanity/components/tags/textfield'
 import FormBuilderPropTypes from '../../FormBuilderPropTypes'
 
-export default class ArrayOfStrings extends React.Component {
+export default class ArrayOfStrings extends React.PureComponent {
   static propTypes = {
-    type: FormBuilderPropTypes.type,
     type: FormBuilderPropTypes.type,
     value: PropTypes.arrayOf(PropTypes.string),
     level: PropTypes.number,
     onChange: PropTypes.func,
-    description: PropTypes.string
-  };
+    description: PropTypes.string,
+    focus: PropTypes.bool
+  }
 
   static contextTypes = {
     formBuilder: PropTypes.object
+  }
+
+  state = {
+    hasFocus: this.props.focus
   }
 
   handleRemoveItem = index => {
@@ -25,6 +29,18 @@ export default class ArrayOfStrings extends React.Component {
         type: 'set',
         value: nextVal
       }
+    })
+  }
+
+  handleFocus = () => {
+    this.setState({
+      hasFocus: true
+    })
+  }
+
+  handleBlur = () => {
+    this.setState({
+      hasFocus: false
     })
   }
 
@@ -39,16 +55,17 @@ export default class ArrayOfStrings extends React.Component {
   }
 
   render() {
-    const {type, value, level} = this.props
-
+    const {type, value, level, description} = this.props
+    const {hasFocus} = this.state
     return (
       <TagInput
         label={type.title}
         level={level}
-        description={type.description}
+        description={description}
         tags={value || []}
         onRemoveTag={this.handleRemoveItem}
         onAddTag={this.handleAddString}
+        focus={hasFocus}
       />
     )
   }
