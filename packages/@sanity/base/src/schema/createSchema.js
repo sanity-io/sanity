9a1514174e069a22c3e7113309eb31d0ee77e61c
Bjørge Næss <bjoerge@gmail.com>
2017-02-01 01:22:03 +0100

    [base] Add fileAsset to default schema

--- packages/@sanity/base/src/schema/createSchema.js
+++ packages/@sanity/base/src/schema/createSchema.js
@@ -1,10 +1,11 @@
 import geopoint from './types/geopoint'
 import imageAsset from './types/imageAsset'
+import fileAsset from './types/fileAsset'
 import Schema from '@sanity/schema'
 export default schemaDef => {
   return Schema.compile({
     name: schemaDef.name,
-    types: [...schemaDef.types, geopoint, imageAsset]
+    types: [...schemaDef.types, geopoint, imageAsset, fileAsset]
   })
 }
 
---
+++ packages/@sanity/base/src/schema/types/fileAsset.js
@@ -0,0 +1,32 @@
+export default {
+  name: 'fileAsset',
+  title: 'File asset',
+  type: 'object',
+  fields: [
+    {
+      name: 'assetId',
+      type: 'string',
+      title: 'Asset ID'
+    },
+    {
+      name: 'project',
+      type: 'string',
+      title: 'Project'
+    },
+    {
+      name: 'label',
+      type: 'string',
+      title: 'Label'
+    },
+    {
+      name: 'path',
+      type: 'string',
+      title: 'Path'
+    },
+    {
+      name: 'url',
+      type: 'string',
+      title: 'Url'
+    }
+  ]
+}
