ceb9a97acfe560f4fba3c5f64eac5329f09dce21
Bjørge Næss <bjoerge@gmail.com>
2017-02-07 14:52:06 +0100

    Deserialize and submit set-patch every few seconds

---
+++ packages/@sanity/form-builder/src/inputs/BlockEditor-slate/Syncer.js
@@ -0,0 +1,42 @@
+import React, {PropTypes} from 'react'
+import {Raw} from 'slate'
+import BlockEditor from './BlockEditor'
+import {throttle} from 'lodash'
+const EMPTY_CONTENT = [{kind: 'block', type: 'paragraph', nodes: []}]
+
+export default class Syncer extends React.PureComponent {
+  static propTypes = {
+    value: PropTypes.array,
+    onChange: PropTypes.func
+  }
+
+  constructor(props) {
+    super()
+    this.state = {
+      slateState: Raw.deserialize({
+        nodes: props.value || EMPTY_CONTENT
+      }, {terse: true})
+    }
+  }
+
+  handleChange = nextSlateState => {
+    this.setState({slateState: nextSlateState})
+    this.emitSet()
+  }
+
+  emitSet = throttle(() => {
+    const {onChange} = this.props
+    const serialized = Raw.serialize(this.state.slateState, {terse: true})
+    onChange({
+      patch: {
+        type: 'set',
+        value: (serialized && serialized.nodes) || []
+      }
+    })
+  }, 5 * 1000, {trailing: true})
+
+  render() {
+    const {slateState} = this.state
+    return <BlockEditor {...this.props} onChange={this.handleChange} value={slateState} />
+  }
+}
