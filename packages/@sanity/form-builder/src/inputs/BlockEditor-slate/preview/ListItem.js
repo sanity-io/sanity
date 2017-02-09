f4fc0ec9ad825860df7c68304bcd4a3b7bd79838
Per-Kristian Nordnes <per.kristian.nordnes@gmail.com>
2017-02-09 14:52:46 +0100

    Fix list numbering bug

--- packages/@sanity/form-builder/src/inputs/BlockEditor-slate/preview/ListItem.js
+++ packages/@sanity/form-builder/src/inputs/BlockEditor-slate/preview/ListItem.js
@@ -8,12 +8,10 @@ function ListItem(props) {
   if (!listStyles.includes(listStyle)) {
     throw new Error(`Don't know how to handle listStyle '${listStyle}'. Expected one of '${listStyles.join("', '")}'`)
   }
-  const className = `${styles[listStyle]} ${props.isFirstItem ? styles.resetListCounter : ''}`
-  return <li className={className}>{props.children}</li>
+  return <li className={styles[listStyle]}>{props.children}</li>
 }
 
 ListItem.propTypes = {
-  isFirstItem: PropTypes.bool,
   listStyle: PropTypes.oneOf(listStyles),
   children: PropTypes.node
 }
