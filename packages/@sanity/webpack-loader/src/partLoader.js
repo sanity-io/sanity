578a4b4e58028fe8cbd23d0996b21b9de3cc1110
Espen Hovlandsdal <espen@hovlandsdal.com>
2017-02-13 18:16:09 +0100

    [webpack-loader] If no NODE_ENV is set and we're in staging mode, explicitly use staging config

--- packages/@sanity/webpack-loader/src/partLoader.js
+++ packages/@sanity/webpack-loader/src/partLoader.js
@@ -5,6 +5,11 @@ const loaderUtils = require('loader-utils')
 const reduceConfig = require('@sanity/util').reduceConfig
 const multiImplementationHandler = require('./multiImplementationHandler')
 
+/* eslint-disable no-process-env */
+const staging = process.env.SANITY_STAGING
+const env = typeof process.env.NODE_ENV === 'undefined' && staging ? 'staging' : process.env.NODE_ENV
+/* eslint-enable no-process-env */
+
 function sanityPartLoader(input) {
   this.cacheable()
 
@@ -22,7 +27,6 @@ function sanityPartLoader(input) {
 
   if (request === 'config:sanity') {
     const config = JSON.parse(input)
-    const env = process.env.NODE_ENV // eslint-disable-line no-process-env
     const indent = env === 'production' ? 0 : 2
     return `module.exports = ${JSON.stringify(reduceConfig(config, env), null, indent)}\n`
   }
