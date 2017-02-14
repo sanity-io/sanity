9bec9dd156f97a1d00bf57a5badedebece295844
Per-Kristian Nordnes <per.kristian.nordnes@gmail.com>
2017-02-14 20:03:25 +0100

    [form-builder] Use link icon for link button

--- packages/@sanity/form-builder/src/inputs/BlockEditor-slate/toolbar/LinkButton.js
+++ packages/@sanity/form-builder/src/inputs/BlockEditor-slate/toolbar/LinkButton.js
@@ -1,7 +1,7 @@
 import React, {PropTypes} from 'react'
 import ToggleButton from 'part:@sanity/components/toggles/button'
 
-import LinkIcon from 'part:@sanity/base/sanity-logo-icon'
+import LinkIcon from 'part:@sanity/base/link-icon'
 import styles from './styles/LinkButton.css'
 
 export default class LinkButton extends React.Component {
@@ -16,11 +16,13 @@ export default class LinkButton extends React.Component {
   }
 
   render() {
+    const {activeLink} = this.props
     return (
       <div style={{position: 'relative'}}>
         <ToggleButton
           onClick={this.handleToggleButtonClick}
           title={'Link'}
+          selected={activeLink}
           className={styles.button}
         >
           <div className={styles.iconContainer}>
