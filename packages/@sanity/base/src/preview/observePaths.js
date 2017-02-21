84a131fe876baf13c7bcad8afc9a142cb3532aea
Bjørge Næss <bjoerge@gmail.com>
2017-02-21 23:17:44 +0100

    [previews] Optimize preview fetching

--- packages/@sanity/base/src/preview/observePaths.js
+++ packages/@sanity/base/src/preview/observePaths.js
@@ -2,36 +2,80 @@ import client from 'part:@sanity/base/client'
 import Observable from '@sanity/observable'
 import {set} from 'lodash'
 
-// Todo: figure out a way to do optimistic updates
-// const ALWAYS_INCLUDE = ['_id', '_type']
-// export default function observePaths(id, paths) {
-//   return client.listen(query, params)
-//     .subscribe(comment => {
-//       console.log(`${comment.author} commented: ${comment.text}`)
-//     })
-// }
+const globalListener = Observable
+  .from(client.listen('*[!(_id in path("_.**"))]', {}, {includeResult: false}))
+  .share()
 
-function fetchDocumentSnapshot(query, params) {
-  return client.fetch(query, params)
-    .then(result => result[0])
+function unprefixType(typeName) {
+  return typeName.split('.').slice().pop()
 }
 
-export default function observePaths(id, paths) {
-  const labeledFields = paths.map((path, i) => `"_f_${i}": ${path.join('.')}`)
-  const query = `*[_id==$id]{_id,_type,${labeledFields.join(',')}}`
-  return Observable.from(client.listen(query, {id}, {includeDocuments: true, events: ['welcome', 'change']}))
-    .concatMap(event => {
-      if (event.type === 'welcome') {
-        return fetchDocumentSnapshot(query, {id})
-      }
-      return event.documents[0]
-    })
-    .map(result => {
-      const {_id, _type, ...labels} = result
-      const res = {_id, _type}
-      Object.keys(labels).forEach((label, i) => {
-        set(res, paths[i], labels[`_f_${i}`])
+function debounceAccumulate(fn, wait) {
+  let timer
+  let pendingArgs = []
+  let observable = null
+  let observer = null
+  return function (...args) {
+    const index = pendingArgs.push(args) - 1
+    if (!observable) {
+      observable = new Observable(o => {
+        observer = o
+      }).share()
+    }
+    clearTimeout(timer)
+    timer = setTimeout(flush, wait)
+    return observable.map(value => value[index])
+  }
+  function flush() {
+    const _args = pendingArgs
+    const _observer = observer
+    pendingArgs = []
+    observer = null
+    observable = null
+    fn(_args).subscribe(_observer)
+  }
+}
+
+function listen(id) {
+  return new Observable(observer => {
+    observer.next({type: 'welcome', documentId: id})
+    return globalListener
+      .filter(event => event.documentId === id)
+      .debounceTime(2000)
+      .subscribe(observer)
+  })
+}
+
+function reproject(result, paths) {
+  const {_id, _type, ...labels} = result
+  const res = {_id, _type: unprefixType(_type)}
+  Object.keys(labels).forEach((label, i) => {
+    set(res, paths[i], labels[`_f_${i}`])
+  })
+  return res
+}
+
+function fetchAllDocumentSnapshots(selections) {
+  const optimizedParams = {}
+  const queryParts = selections.map(([id, paths], queryIndex) => {
+    optimizedParams[[`id_${queryIndex}`]] = id
+    const labeledFields = paths.map((path, i) => `"_f_${i}": ${path.join('.')}`)
+    return `*[_id==$id_${queryIndex}]{_id,_type,${labeledFields.join(',')}}`
+  })
+
+  const optimizedQuery = `[${queryParts.join(',\n')}]`
+
+  return client.observable.fetch(optimizedQuery, optimizedParams)
+    .map((result, index) => {
+      return result.map(res => {
+        return reproject(res[0], selections[index][1])
       })
-      return res
     })
+}
+
+const debouncedFetchDocumentSnapshot = debounceAccumulate(fetchAllDocumentSnapshots, 50)
+
+export default function observePaths(id, paths) {
+  return listen(id)
+    .switchMap(event => debouncedFetchDocumentSnapshot(id, paths))
 }
