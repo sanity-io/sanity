17ea1b91d53b7d31fa17dd156587ba544c75fb64
Bjørge Næss <bjoerge@gmail.com>
2017-02-21 22:39:00 +0100

    [previews] Simplify scroll position listener w/Observable

--- packages/@sanity/base/src/preview/scrollPosition.js
+++ packages/@sanity/base/src/preview/scrollPosition.js
@@ -1,45 +1,4 @@
 import Observable from '@sanity/observable'
-import {debounce} from 'lodash'
-
-// A useful pattern that can be re-used probably
-function createListener(target, eventTypes, useCapture) {
-  const listeners = []
-  return {
-    add(listener) {
-      if (listeners.length === 0) {
-        start()
-      }
-      listeners.push(listener)
-      return () => remove(listener)
-    },
-    remove: remove
-  }
-
-  function remove(listener) {
-    const idx = listeners.indexOf(listener)
-    if (idx > -1) {
-      listeners.splice(idx, 1)
-      if (listeners.length == 0) {
-        stop()
-      }
-    }
-  }
-  function emit(event) {
-    listeners.forEach(listener => listener(event))
-  }
-  function start() {
-    eventTypes.forEach(eventType => {
-      target.addEventListener(eventType, emit, useCapture)
-    })
-  }
-  function stop() {
-    eventTypes.forEach(eventType => {
-      target.removeEventListener(eventType, emit, useCapture)
-    })
-  }
-}
-
-const events = createListener(document.body, ['scroll', 'resize'], true)
 
 function getWindowDimensions() {
   return {
@@ -48,8 +7,21 @@ function getWindowDimensions() {
   }
 }
 
+const EVENTS = ['scroll', 'resize']
 export default new Observable(observer => {
-  observer.next(getWindowDimensions())
+  const listener = () => observer.next(getWindowDimensions())
 
-  return events.add(debounce(() => observer.next(getWindowDimensions()), 200))
+  listener()
+
+  EVENTS.forEach(eventType => {
+    document.body.addEventListener(eventType, listener, true)
+  })
+
+  return () => {
+    EVENTS.forEach(eventType => {
+      document.body.removeEventListener(eventType, listener, true)
+    })
+  }
 })
+  .debounceTime(100)
+  .share()
