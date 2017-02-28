0f0841d83f2f9985c3806414d3e1312f5e9717f1
Espen Hovlandsdal <espen@hovlandsdal.com>
2017-02-28 11:36:04 +0100

    [infrastructure] Improve commit message validation output

--- scripts/validateCommitMessage.js
+++ scripts/validateCommitMessage.js
@@ -1,6 +1,7 @@
 const fs = require('fs')
 const path = require('path')
 const chalk = require('chalk')
+const boxen = require('boxen')
 const childProc = require('child_process')
 
 const commitMsgPath = path.join(__dirname, '..', process.env.GIT_PARAMS)
@@ -9,7 +10,11 @@ const msg = fs.readFileSync(commitMsgPath, 'utf8').trim()
 const template = /^\[[a-z-\/]+]\s[A-Z0-9]\w+/
 
 if (!template.test(msg)) {
-  console.error(chalk.red('No, your commit message should look like this:'))
-  console.error(chalk.yellow('[package] Description of the change'))
+  console.error(boxen([
+    chalk.yellow('No, your commit message should look like this:'),
+    chalk.green('[package] Description of the change'), '',
+    'Example:',
+    chalk.green('[desk-tool] Improve performance of list rendering')
+  ].join('\n'), {padding: 1, margin: 1, borderStyle: 'double', borderColor: 'red'}))
   process.exit(1)
 }
