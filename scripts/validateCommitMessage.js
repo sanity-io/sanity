149539bd3e5ecbbbd2730fcaa28533aeebc79024
Espen Hovlandsdal <espen@hovlandsdal.com>
2017-03-02 02:28:23 +0100

    [infrastructure] Let version commits be valid commit messages

--- scripts/validateCommitMessage.js
+++ scripts/validateCommitMessage.js
@@ -8,8 +8,9 @@ const commitMsgPath = path.join(__dirname, '..', process.env.GIT_PARAMS)
 const msg = fs.readFileSync(commitMsgPath, 'utf8').trim()
 
 const template = /^\[[a-z-\/]+]\s[A-Z0-9]\w+/
+const versionTemplate = /^v\d+\.\d+.\d+(-[a-z0-9-]+)?$/
 
-if (!template.test(msg)) {
+if (!template.test(msg) && !versionTemplate.test(msg)) {
   console.error(boxen([
     chalk.yellow('No, your commit message should look like this:'),
     chalk.green('[package] Description of the change'), '',
