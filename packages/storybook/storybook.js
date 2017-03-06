479fb7a9067f7f1f6b4fc247b51d3658ead87c2d
Espen Hovlandsdal <espen@hovlandsdal.com>
2017-03-06 23:38:13 +0100

    [storybook] Stripped down storybook project even further

--- packages/storybook/storybook.js
+++ packages/storybook/storybook.js
@@ -1,1 +1,17 @@
-module.exports = require('part:@sanity/storybook/iframe')
+const React = require('react')
+const h = React.createElement // eslint-disable-line id-length
+
+function Document(props) {
+  return h('html', null,
+    h('head', null,
+      h('meta', {charSet: 'utf-8'}),
+      h('meta', {httpEquiv: 'refresh', content: '0; url=http://localhost:9001/'}),
+      h('title', null, 'Loading Storybook...')
+    ),
+    h('body', null,
+      h('div', null, 'Loading Storybook...')
+    )
+  )
+}
+
+module.exports = Document
