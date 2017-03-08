19ef32aab58f93a634ffd87550fa8012b575778e
Espen Hovlandsdal <espen@hovlandsdal.com>
2017-03-08 10:52:52 +0100

    [base] Export base client with module.exports to allow easier commonjs usage

--- packages/@sanity/base/src/client/index.js
+++ packages/@sanity/base/src/client/index.js
@@ -1,6 +1,4 @@
 import sanityClient from '@sanity/client'
 import config from 'config:sanity'
 
-const client = sanityClient(config.api)
-
-export default client
+module.exports = sanityClient(config.api)
