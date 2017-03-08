34bb22309b4c9fb1f454a7133ddbc8d53de7e305
Espen Hovlandsdal <espen@hovlandsdal.com>
2017-03-08 11:48:22 +0100

    [cli] Remove failed attempt at "holding" spinners automagically while printing

--- packages/@sanity/cli/src/outputters/cliOutputter.js
+++ packages/@sanity/cli/src/outputters/cliOutputter.js
@@ -3,33 +3,18 @@ import ora from 'ora'
 import chalk from 'chalk'
 
 let isFirstClear = true
-let activeSpinner = null
 
-const output = {
-  holdSpinner(then) {
-    if (activeSpinner) {
-      activeSpinner.stop()
-    }
-
-    then()
-
-    if (activeSpinner) {
-      activeSpinner.start()
-    }
-  },
-
+export default {
   print(...args) {
-    output.holdSpinner(() => console.log(...args))
+    console.log(...args)
   },
 
   error(...args) {
-    output.holdSpinner(() => {
-      if (args[0] instanceof Error) {
-        console.error(chalk.red(args[0].stack))
-      } else {
-        console.error(...args)
-      }
-    })
+    if (args[0] instanceof Error) {
+      console.error(chalk.red(args[0].stack))
+    } else {
+      console.error(...args)
+    }
   },
 
   clear: () => {
@@ -40,34 +25,6 @@ const output = {
   },
 
   spinner(...args) {
-    activeSpinner = spinnerProxy(...args)
-    return activeSpinner
+    return ora(...args)
   }
 }
-
-// So you're wondering why we're doing this?
-// We need a way to "hold" the spinner while printing a message, then resuming afterwards.
-// Once a spinner has been persisted, calling `start()` on it shouldn't actually restart it
-function spinnerProxy(...spinArgs) {
-  const spinner = ora(...spinArgs)
-  let persisted = false
-
-  const api = {
-    start: () => (persisted ? api : spinner.start() && api),
-    stop: () => spinner.stop() && api,
-    clear: () => () => persist().clear() && api,
-    succeed: (...args) => persist().succeed(...args) && api,
-    warn: (...args) => persist().warn(...args) && api,
-    fail: (...args) => persist().fail(...args) && api,
-    stopAndPersist: (...args) => persist().stopAndPersist(...args) && api
-  }
-
-  function persist() {
-    persisted = true
-    return spinner
-  }
-
-  return api
-}
-
-export default output
