f5d144abff50c9044cbfeeec16c8169a361aec1c
Espen Hovlandsdal <espen@hovlandsdal.com>
2017-03-10 17:47:51 +0100

    [server] Use custom way to resolve URLs for file assets that are relative

--- packages/@sanity/server/src/configs/postcssPlugins.js
+++ packages/@sanity/server/src/configs/postcssPlugins.js
@@ -1,18 +1,26 @@
+import path from 'path'
 import lost from 'lost'
 import postcssUrl from 'postcss-url'
 import postcssImport from 'postcss-import'
 import postcssCssnext from 'postcss-cssnext'
 import resolveStyleImport from '../util/resolveStyleImport'
 
+const absolute = /^(\/|\w+:\/\/)/
+const isAbsolute = url => absolute.test(url)
+
 export default options => {
   const styleResolver = resolveStyleImport({from: options.basePath})
   const importer = postcssImport({resolve: styleResolver})
+
+  const resolveUrl = (url, decl, from, dirname) => (
+    isAbsolute(url) ? url : path.resolve(dirname, url)
+  )
 
   return wp => {
     return [
       importer,
       postcssCssnext,
-      postcssUrl({url: 'rebase'}),
+      postcssUrl({url: resolveUrl}),
       lost
     ]
   }
