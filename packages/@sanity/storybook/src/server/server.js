b4f5a708d0437e9ca39f217ecd28f390f13a4e73
Espen Hovlandsdal <espen@hovlandsdal.com>
2017-03-17 13:33:21 +0100

    [storybook] Read port and enabled status from configuration

--- packages/@sanity/storybook/src/server/server.js
+++ packages/@sanity/storybook/src/server/server.js
@@ -22,20 +22,36 @@ process.on('message', msg => {
 function startServer(msg) {
   webpackConfig.setSanityContext(msg.config)
 
+  const config = tryLoadConfig(msg.config.basePath)
+  const enabled = config.enabled !== false
+  const port = config.port || 9001
+
+  if (!enabled) {
+    return
+  }
+
   app = express()
   app.use(express.static(msg.config.staticPath, {index: false}))
   app.use(storybook(path.join(__dirname, '..', 'config')))
 
-  app.listen(9001, msg.config.httpHost, error => {
+  app.listen(port, msg.config.httpHost, error => {
     if (error) {
       // @todo message back that we've exited
       throw error
     }
 
-    process.send({event: 'listening', url: `http://${msg.config.httpHost}:9001/`})
+    process.send({event: 'listening', url: `http://${msg.config.httpHost}:${port}/`})
   })
 }
 
 function unhandledMessage(msg) {
   console.error(`Unhandled message of type "${msg.event}"; %s`, JSON.stringify(msg, null, 2))
+}
+
+function tryLoadConfig(basePath) {
+  try {
+    return require(path.join(basePath, 'config', '@sanity', 'storybook.json'))
+  } catch (err) {
+    return {}
+  }
 }
