04c7a1c42d06482882d2212158e398dcc78a221d
Espen Hovlandsdal <espen@hovlandsdal.com>
2017-03-21 14:40:33 +0100

    [base] Add asset url builder abstraction

---
+++ packages/@sanity/base/src/assets/asset-url-builder.js
@@ -0,0 +1,19 @@
+const sanityUrlMatch = /^https?:\/\/cdn.sanity.\w+\/images\//
+
+export default function assetUrlBuilder(asset) {
+  const {url, width, height, fit} = asset
+  if (!sanityUrlMatch.test(url)) {
+    return url
+  }
+
+  const defaultFit = width === height ? 'crop' : 'clip'
+  const params = [
+    width && `w=${width}`,
+    height && `h=${height}`,
+    `fit=${fit ? fit : defaultFit}`,
+    'ch=DPR',
+    'q=85',
+  ].filter(Boolean).join('&')
+
+  return `${url}?${params}`
+}
