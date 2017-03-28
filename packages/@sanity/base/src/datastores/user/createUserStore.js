7f65bde848d6f6cbbc7023062ef7dfc27137d50a
Kristoffer Sivertsen <kristofferjoachim@gmail.com>
2017-03-28 11:52:48 +0200

    [base] Adding simple cache to getUser

--- packages/@sanity/base/src/datastores/user/createUserStore.js
+++ packages/@sanity/base/src/datastores/user/createUserStore.js
@@ -3,6 +3,8 @@ import createActions from '../utils/createActions'
 import pubsub from 'nano-pubsub'
 import authenticationFetcher from 'part:@sanity/base/authentication-fetcher'
 import client from 'part:@sanity/base/client'
+import {find} from 'lodash'
+
 const userChannel = pubsub()
 
 let _currentUser = null
@@ -45,12 +47,22 @@ const currentUser = new Observable(observer => {
   }
 })
 
-const getUser = id => client.request({
-  uri: `/users/${id}`,
-  withCredentials: true
-}).then(user => {
-  return user && user.id ? user : null
-})
+const userCache = {}
+
+const getUser = id => {
+
+  if (!userCache[id]) {
+    userCache[id] = client.request({
+      uri: `/users/${id}`,
+      withCredentials: true
+    })
+    .then(user => {
+      return (user && user.id) ? user : null
+    })
+  }
+
+  return userCache[id]
+}
 
 export default function createUserStore(options = {}) {
   return {
