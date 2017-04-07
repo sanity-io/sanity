ab9d6a430d1357670687753ff174c4d81875264c
Bjørge Næss <bjoerge@gmail.com>
2017-04-07 11:26:27 +0200

    [form-builder] Materialize file reference in File component

--- packages/@sanity/form-builder/src/inputs/File/createFileInput.js
+++ packages/@sanity/form-builder/src/inputs/File/createFileInput.js
@@ -2,13 +2,12 @@ import React from 'react'
 import FileInput from './FileInput'
 import {omit} from 'lodash'
 
-export default function createFileInput({upload}) {
+export default function createFileInput({upload, materializeReference}) {
   return class CustomFileInput extends React.PureComponent {
     static propTypes = omit(FileInput.propTypes, 'upload') //eslint-disable-line react/forbid-foreign-prop-types
-
     render() {
       return (
-        <FileInput {...this.props} upload={upload} />
+        <FileInput {...this.props} materializeReference={materializeReference} upload={upload} />
       )
     }
   }
