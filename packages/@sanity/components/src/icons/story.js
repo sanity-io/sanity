e0d6426d0fc0fce5c4147b40d551d8def1775539
Bjørge Næss <bjoerge@gmail.com>
2017-05-02 13:10:06 +0200

    [components] Fix brorked storybook

--- packages/@sanity/components/src/icons/story.js
+++ packages/@sanity/components/src/icons/story.js
@@ -30,7 +30,7 @@ import PlusIcon from 'part:@sanity/base/plus-icon'
 import ArrowDropDownIcon from 'part:@sanity/base/arrow-drop-down'
 import TrashIcon from 'part:@sanity/base/trash-icon'
 import UndoIcon from 'part:@sanity/base/undo-icon'
-import UnpublishIcon from 'part:@sanity/base/unpublish-icon'
+import VisibilityOffIcon from 'part:@sanity/base/visibility-off-icon'
 
 function createIconPreview(title, Icon, role) {
   return (
@@ -74,7 +74,7 @@ storiesOf('Icons')
         {createIconPreview('Arrow Drop Down', ArrowDropDownIcon, 'part:@sanity/base/arrow-drop-down')}
         {createIconPreview('Trash', TrashIcon, 'part:@sanity/base/trash-icon')}
         {createIconPreview('Undo', UndoIcon, 'part:@sanity/base/undo-icon')}
-        {createIconPreview('Unpublish', UnpublishIcon, 'part:@sanity/base/unpublish-icon')}
+        {createIconPreview('Visibility off', VisibilityOffIcon, 'part:@sanity/base/visibility-off-icon')}
 
       </ul>
     )
