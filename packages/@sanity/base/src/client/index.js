78361ee94dd31a430c28a0b948ab28651137b1dd
Bjørge Næss <bjoerge@gmail.com>
2017-05-09 11:23:14 +0200

    [base] RFC: Provide a way to configure client initially

--- packages/@sanity/base/src/client/index.js
+++ packages/@sanity/base/src/client/index.js
@@ -1,4 +1,7 @@
 import sanityClient from '@sanity/client'
 import config from 'config:sanity'
+import configureClient from 'part:@sanity/base/configure-client?'
 
-module.exports = sanityClient(config.api)
+const client = sanityClient(config.api)
+
+export default configureClient ? configureClient(sanityClient(config.api)) : client
