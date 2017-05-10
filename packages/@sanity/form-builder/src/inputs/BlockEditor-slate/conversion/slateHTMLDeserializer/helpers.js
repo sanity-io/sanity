7052525c7c9b48ebb5e094aafd7cc2e16946ebdd
Per-Kristian Nordnes <per.kristian.nordnes@gmail.com>
2017-05-10 17:22:52 +0200

    [form-builder] Block editor: paste from html - unwrap nested block tags

--- packages/@sanity/form-builder/src/inputs/BlockEditor-slate/conversion/slateHTMLDeserializer/helpers.js
+++ packages/@sanity/form-builder/src/inputs/BlockEditor-slate/conversion/slateHTMLDeserializer/helpers.js
@@ -1,10 +1,46 @@
 import cheerio from 'cheerio'
 import cleanUpWordDocument from './cleanUpWordDocument'
+import {HTML_BLOCK_TAGS, HTML_HEADER_TAGS} from './rules'
 
 const wordRegexp = /(class="?Mso|style=(?:"|')[^"]*?\bmso-|w:WordDocument|<o:\w+>|<\/font>)/
 
 function isUnwantedElement(index, node) {
   return node.type === 'comment' || node.type === 'directive'
+}
+
+function unwrapBlockTags(doc) {
+  const blockTags = Object.keys(HTML_BLOCK_TAGS).concat(Object.keys(HTML_HEADER_TAGS))
+  const blockSelect = blockTags.join(', ')
+
+  doc.root().contents()
+    .each((index, node) => {
+      const childBlocks = doc(node).find(blockSelect)
+      if (childBlocks.length && blockTags.includes(node.tagName)) {
+        doc(node).attr('data-unwrapped', 'true')
+        childBlocks.insertAfter(node)
+      }
+    })
+
+  // Remove any empty leftover empty block tags if we have unwrapped anything
+  doc.root()
+    .find('*[data-unwrapped=true]')
+    .filter((index, node) => {
+      return doc(node).text().trim() === ''
+    })
+    .remove()
+
+  return doc
+}
+
+function wrapOrphanBrs(doc) {
+  doc.root()
+    .find('br')
+    .each((index, node) => {
+      if (!node.parent) {
+        doc(node).replaceWith('<p></p>')
+      }
+    })
+  return doc
 }
 
 export function isPastedFromGoogleDocs(el) {
@@ -30,6 +66,11 @@ export function cleanupHtml(html) {
   if (isWordDocument) {
     doc = cleanUpWordDocument(doc)
   }
+
+  doc = unwrapBlockTags(doc)
+
+  doc = wrapOrphanBrs(doc)
+
   const cleanHtml = doc.html()
     .trim()   // Trim whitespace
     .replace(/[\r\n]+/g, ' ') // Remove newlines / carriage returns
