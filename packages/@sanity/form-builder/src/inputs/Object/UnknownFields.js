a1bce4773a8bff38a8b19f4b5ee302642de06908
Bjørge Næss <bjoerge@gmail.com>
2017-06-27 16:11:48 +0200

    [form-builder] Fix lint errors

--- packages/@sanity/form-builder/src/inputs/Object/UnknownFields.js
+++ packages/@sanity/form-builder/src/inputs/Object/UnknownFields.js
@@ -1,3 +1,4 @@
+// @flow
 import React from 'react'
 import Details from '../common/Details'
 import DefaultButton from 'part:@sanity/components/buttons/default'
@@ -5,6 +6,12 @@ import PatchEvent, {unset} from '../../PatchEvent'
 import styles from './styles/UnknownFields.css'
 
 export default class UnknownFields extends React.Component {
+  props: {
+    fieldNames: string[],
+    value: Object,
+    onChange: (PatchEvent) => void
+  }
+
   handleUnsetClick = fieldName => {
     this.props.onChange(PatchEvent.from(unset([fieldName])))
   }
@@ -16,11 +23,11 @@ export default class UnknownFields extends React.Component {
         <h2 className={styles.heading}>Found {fieldNames.length} unknown fields</h2>
         <div className={styles.content}>
           <Details>
-            These are not defined in schema as valid fields for this value.
+            These are not defined in the current schema as valid fields for this value.
             This could mean that the field has been removed, or that someone else has added it to their own local schema that is not yet deployed.
             {fieldNames.map(fieldName => {
               return (
-                <div>
+                <div key={fieldName}>
                   <h4>{fieldName}</h4>
                   <pre style={{border: '1px solid #aaa', maxHeight: 200, overflowY: 'scroll', backgroundColor: 'white'}}>
                     {JSON.stringify(value[fieldName], null, 2)}
