085dc76fac6e10bb5c52544bd984206b3a974cf3
Bjørge Næss <bjoerge@gmail.com>
2017-08-15 10:14:07 +0200

    [example-studio] Minor test schema tweaks

--- packages/example-studio/schemas/arrays.js
+++ packages/example-studio/schemas/arrays.js
@@ -40,56 +40,25 @@ export default {
       },
     },
     {
-      name: 'select',
-      title: 'Select string',
-      type: 'string',
+      name: 'arrayOfStringsWithLegacyList',
+      title: 'Array of strings with legacy format on lists',
+      description: 'Previously the `list` option took an array of {title, value} items. It should still work.',
+      type: 'array',
+      of: [{type: 'string'}],
       options: {
         list: [
-          {
-            title: 'One (1)',
-            value: 'one'
-          },
-          {
-            title: 'Two (2)',
-            value: 'two'
-          },
-          {
-            title: 'Three (3)',
-            value: 'three'
-          }
+          {value: 'residential', title: 'Residential'},
+          {value: 'education', title: 'Education'},
+          {value: 'commercial', title: 'Commercial'},
+          {value: 'cultural', title: 'Cultural'},
+          {value: 'display', title: 'Display'},
+          {value: 'installation', title: 'Installation'},
+          {value: 'objects', title: 'Objects'},
+          {value: 'performance', title: 'Performance'},
+          {value: 'public space', title: 'Public Space'},
+          {value: 'publications', title: 'Publications'}
         ]
       }
     },
-    {
-      name: 'selectObjectOfString',
-      title: 'Select string in object',
-      description: '',
-      type: 'string',
-      options: {
-        list: ['one', 'two', 'three']
-      }
-    },
-    {
-      name: 'radioSelect',
-      title: 'Select (layout: radio)',
-      type: 'string',
-      options: {
-        layout: 'radio',
-        list: [
-          {
-            title: 'One (1)',
-            value: 'one'
-          },
-          {
-            title: 'Two (2)',
-            value: 'two'
-          },
-          {
-            title: 'Three (3)',
-            value: 'three'
-          }
-        ]
-      }
-    }
   ]
 }
--- packages/example-studio/schemas/strings.js
+++ packages/example-studio/schemas/strings.js
@@ -26,26 +26,6 @@ export default {
       ]
     },
     {
-      name: 'arrayOfStringsWithList',
-      title: 'Array of strings with list',
-      type: 'array',
-      of: [{type: 'string'}],
-      options: {
-        list: [
-          {value: 'residential', title: 'Residential'},
-          {value: 'education', title: 'Education'},
-          {value: 'commercial', title: 'Commercial'},
-          {value: 'cultural', title: 'Cultural'},
-          {value: 'display', title: 'Display'},
-          {value: 'installation', title: 'Installation'},
-          {value: 'objects', title: 'Objects'},
-          {value: 'performance', title: 'Performance'},
-          {value: 'public space', title: 'Public Space'},
-          {value: 'publications', title: 'Publications'}
-        ]
-      }
-    },
-    {
       name: 'select',
       title: 'Select string',
       type: 'string',
