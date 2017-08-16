23ce4e351975297b61ab8806adecac8ec0fc0a6d
Espen Hovlandsdal <espen@hovlandsdal.com>
2017-08-16 16:16:09 +0200

    [base] Use credentialed requests in studio (#115)

--- packages/@sanity/base/src/client/index.js
+++ packages/@sanity/base/src/client/index.js
@@ -2,6 +2,7 @@ import sanityClient from '@sanity/client'
 import config from 'config:sanity'
 import configureClient from 'part:@sanity/base/configure-client?'
 
-const client = sanityClient(config.api)
+const apiConfig = {...config.api, withCredentials: true}
+const client = sanityClient(apiConfig)
 
-export default configureClient ? configureClient(sanityClient(config.api)) : client
+export default configureClient ? configureClient(sanityClient(apiConfig)) : client
