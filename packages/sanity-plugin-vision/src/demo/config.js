4d08f40c1fe986623ef6ec84dc464b158e968816
Espen Hovlandsdal <espen@hovlandsdal.com>
2017-08-16 15:56:30 +0200

    [vision] Remove non-sanity parts

--- packages/sanity-plugin-vision/src/demo/config.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable no-process-env */
-const apiHost = process.env.REACT_APP_API_HOST
-const apiHostCfg = apiHost ? {apiHost} : {}
-
-module.exports = {
-  client: apiHostCfg
-}
--- packages/sanity-plugin-vision/src/demo/demo.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import DemoContainer from './DemoContainer'
-
-ReactDOM.render(
-  <DemoContainer />,
-  document.getElementById('root')
-)
