d11dbc433e8c2da803fc7ad704c7184ebde5e55d
Espen Hovlandsdal <espen@hovlandsdal.com>
2017-08-23 15:36:45 +0200

    [client] Include error message in stack (#129)

--- packages/@sanity/client/src/http/request.js
+++ packages/@sanity/client/src/http/request.js
@@ -1,15 +1,12 @@
 /* eslint-disable no-empty-function, no-process-env */
 const getIt = require('get-it')
 const assign = require('object-assign')
-const createErrorClass = require('create-error-class')
 const observable = require('get-it/lib/middleware/observable')
 const jsonRequest = require('get-it/lib/middleware/jsonRequest')
 const jsonResponse = require('get-it/lib/middleware/jsonResponse')
 const SanityObservable = require('@sanity/observable/minimal')
 const progress = require('get-it/lib/middleware/progress')
-
-const ClientError = createErrorClass('ClientError', extractError)
-const ServerError = createErrorClass('ServerError', extractError)
+const {ClientError, ServerError} = require('./errors')
 
 const httpError = ({
   onResponse: res => {
@@ -22,40 +19,6 @@ const httpError = ({
     return res
   }
 })
-
-function extractError(res) {
-  const body = res.body
-  this.response = res
-  this.statusCode = res.statusCode
-  this.responseBody = stringifyBody(body, res)
-
-  // API/Boom style errors ({statusCode, error, message})
-  if (body.error && body.message) {
-    this.message = `${body.error} - ${body.message}`
-    return
-  }
-
-  // Query/database errors ({error: {description, other, arb, props}})
-  if (body.error && body.error.description) {
-    this.message = body.error.description
-    this.details = body.error
-    return
-  }
-
-  // Other, more arbitrary errors
-  this.message = body.error || body.message || httpErrorMessage(res)
-}
-
-function httpErrorMessage(res) {
-  const statusMessage = res.statusMessage ? ` ${res.statusMessage}` : ''
-  return `${res.method}-request to ${res.url} resulted in HTTP ${res.statusCode}${statusMessage}`
-}
-
-function stringifyBody(body, res) {
-  const contentType = (res.headers['content-type'] || '').toLowerCase()
-  const isJson = contentType.indexOf('application/json') !== -1
-  return isJson ? JSON.stringify(body, null, 2) : body
-}
 
 const middleware = [
   jsonRequest(),
