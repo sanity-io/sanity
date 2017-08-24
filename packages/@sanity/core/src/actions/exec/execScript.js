924d8efa38331ada8aa4ecaf0124e3044b149162
Espen Hovlandsdal <espen@hovlandsdal.com>
2017-08-24 13:20:39 +0200

    [core] Add `sanity exec` command that registers part loader + babel (#138)

---
+++ packages/@sanity/core/src/actions/exec/execScript.js
@@ -0,0 +1,24 @@
+const spawn = require('child_process').spawn
+const fsp = require('fs-promise')
+const path = require('path')
+
+module.exports = async args => {
+  const [script] = args.argsWithoutOptions
+  const scriptPath = path.resolve(script)
+
+  if (!script) {
+    throw new Error('SCRIPT must be provided. `sanity exec <script>`')
+  }
+
+  if (!await fsp.exists(scriptPath)) {
+    throw new Error(`${scriptPath} does not exist`)
+  }
+
+  const babel = require.resolve('./babel')
+  const loader = require.resolve('@sanity/plugin-loader/register')
+  const proc = spawn(process.argv[0], ['-r', babel, '-r', loader, scriptPath])
+
+  proc.stdout.pipe(process.stdout)
+  proc.stderr.pipe(process.stderr)
+  proc.on('close', process.exit)
+}
