72ece59b420741247e70c9cb89808df029e82d72
Bjørge Næss <bjoerge@gmail.com>
2017-08-24 16:38:20 +0200

    [components] Fix BlockPreview rendering (#139)

--- packages/@sanity/components/src/previews/BlockPreview.js
+++ packages/@sanity/components/src/previews/BlockPreview.js
@@ -34,38 +34,30 @@ export default class BlockPreview extends React.Component {
   render() {
     const {item, emptyText, assetSize, children, type} = this.props
 
-    if (!item) {
-      return (
-        <div className={`${styles.empty}`}>
-          {emptyText}
-        </div>
-      )
-    }
-
     return (
       <div
         className={`
           ${styles.root}
-          ${item.subtitle ? styles.hasSubtitle : ''}
+          ${(item && item.subtitle) ? styles.hasSubtitle : ''}
         `}
       >
         <div className={styles.type}>
           {type.title || type.name}
         </div>
         {
-          (item.media || item.sanityImage || item.imageUrl) && <div className={`${styles.media}`}>
+          (item && (item.media || item.sanityImage || item.imageUrl)) && <div className={`${styles.media}`}>
             <MediaRender size={assetSize} item={item} />
           </div>
         }
         <div className={styles.heading}>
           <h2 className={styles.title}>
-            {item.title || emptyText}
+            {item && item.title || emptyText}
           </h2>
           <h3 className={styles.subtitle}>
-            {item.subtitle}
+            {item && item.subtitle}
           </h3>
           {
-            item.description && (
+            item && item.description && (
               <p className={styles.description}>
                 {item.description}
               </p>
