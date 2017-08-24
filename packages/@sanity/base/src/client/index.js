506019c6f4315d3362374ed173783a5493047f1b
Espen Hovlandsdal <espen@hovlandsdal.com>
2017-08-24 09:28:32 +0200

    [base] Expose client as CommonJS

--- packages/@sanity/base/src/client/index.js
+++ packages/@sanity/base/src/client/index.js
@@ -2,7 +2,30 @@ import sanityClient from '@sanity/client'
 import config from 'config:sanity'
 import configureClient from 'part:@sanity/base/configure-client?'
 
+const deprecationMessage = `[deprecation] The Sanity client is now exposed in CommonJS format.
+
+For instance, change:
+  \`const client = require('part:@sanity/base/client').default\`
+
+To the following:
+  \`const client = require('part:@sanity/base/client')\`
+`
+
 const apiConfig = {...config.api, withCredentials: true}
 const client = sanityClient(apiConfig)
 
-export default configureClient ? configureClient(sanityClient(apiConfig)) : client
+const configuredClient = configureClient
+  ? configureClient(sanityClient(apiConfig))
+  : client
+
+// Warn when people use `.default`
+Object.defineProperty(configuredClient, 'default', {
+  get() {
+    // eslint-disable-next-line no-console
+    console.warn(deprecationMessage)
+    return configuredClient
+  }
+})
+
+// Expose as CJS to allow Node scripts to consume it without `.default`
+module.exports = configuredClient
--- packages/@sanity/base/test/init.js
+++ packages/@sanity/base/test/init.js
@@ -1,5 +1,12 @@
 import chai from 'chai'
 import chaiAsPromised from 'chai-as-promised'
+import pluginLoader from '@sanity/plugin-loader'
+
+pluginLoader({
+  overrides: {
+    'config:sanity': [{api: {projectId: 'abc123', dataset: 'hei'}}]
+  }
+})
 
 chai.should()
 chai.use(chaiAsPromised)
