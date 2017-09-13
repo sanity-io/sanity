cc25eff42117a9953475cd93c79a1ec1b4745b07
Bjørge Næss <bjoerge@gmail.com>
2017-09-13 13:01:35 +0200

    [schema] Add link to deprecation warning

--- packages/@sanity/base/src/schema/types/deprecatedDate.js
+++ packages/@sanity/base/src/schema/types/deprecatedDate.js
@@ -1,10 +1,15 @@
 import richDate from './richDate'
+import generateHelpUrl from '@sanity/generate-help-url'
 
 let hasWarned = false
 export default Object.assign({}, richDate, {
   get name() {
     if (!hasWarned) {
-      console.warn('Heads up! The `date` type has been renamed to `richDate`. Please update your schema')
+      // eslint-disable-next-line no-console
+      console.warn(
+        'Heads up! The `date` type has been renamed to `richDate`. Please update your schema. See %s for more info.',
+        generateHelpUrl('migrate-to-rich-date')
+      )
       hasWarned = true
     }
     return 'date'
