44760293136750b0a9b54de606ab188768aad768
Espen Hovlandsdal <espen@hovlandsdal.com>
2017-09-18 22:39:35 +0200

    [client] Retry failed GET/HEAD-requests

--- packages/@sanity/client/src/http/nodeMiddleware.js
+++ packages/@sanity/client/src/http/nodeMiddleware.js
@@ -1,10 +1,13 @@
-const debug = require('get-it/lib/middleware/debug')
-const headers = require('get-it/lib/middleware/headers')
+const retry = require('get-it/lib-node/middleware/retry')
+const debug = require('get-it/lib-node/middleware/debug')
+const headers = require('get-it/lib-node/middleware/headers')
+
 const pkg = require('../../package.json')
 
 const middleware = [
   debug({verbose: true, namespace: 'sanity:client'}),
-  headers({'User-Agent': `${pkg.name} ${pkg.version}`})
+  headers({'User-Agent': `${pkg.name} ${pkg.version}`}),
+  retry()
 ]
 
 module.exports = middleware
