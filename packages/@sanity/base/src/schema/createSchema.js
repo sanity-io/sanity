62e97b2c0ad47869fc407d30b6ca2fcfe46f6d5c
Espen Hovlandsdal <espen@hovlandsdal.com>
2017-09-18 20:52:10 +0200

    [base] Expose schema creator as CommonJS

--- packages/@sanity/base/src/schema/createSchema.js
+++ packages/@sanity/base/src/schema/createSchema.js
@@ -2,10 +2,8 @@ import geopoint from './types/geopoint'
 import imageAsset from './types/imageAsset'
 import fileAsset from './types/fileAsset'
 import Schema from '@sanity/schema'
-export default schemaDef => {
-  return Schema.compile({
-    name: schemaDef.name,
-    types: [...schemaDef.types, geopoint, imageAsset, fileAsset]
-  })
-}
 
+module.exports = schemaDef => Schema.compile({
+  name: schemaDef.name,
+  types: [...schemaDef.types, geopoint, imageAsset, fileAsset]
+})
