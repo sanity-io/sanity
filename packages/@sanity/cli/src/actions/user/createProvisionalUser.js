94c0048723dbf83b58d083faf45db8ec6c2ba9c6
Espen Hovlandsdal <espen@hovlandsdal.com>
2017-09-19 10:38:02 +0200

    [cli] Clean up unused code paths (#180)

--- packages/@sanity/cli/src/actions/user/createProvisionalUser.js
+++
@@ -1,9 +0,0 @@
-export default function createProvisionalUser(apiClient) {
-  return apiClient({
-    requireUser: false,
-    requireProject: false
-  }).request({
-    method: 'POST',
-    uri: '/users/provisional'
-  })
-}
