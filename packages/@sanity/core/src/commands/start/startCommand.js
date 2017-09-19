750fbb7db753f2fce002c6563f8be5878fc9b1cc
Espen Hovlandsdal <espen@hovlandsdal.com>
2017-09-19 10:04:06 +0200

    [core] Add help text for `sanity start` command (#183)

--- packages/@sanity/core/src/commands/start/startCommand.js
+++ packages/@sanity/core/src/commands/start/startCommand.js
@@ -1,8 +1,22 @@
 import lazyRequire from '@sanity/util/lib/lazyRequire'
 
+const helpText = `
+Notes
+  Changing the hostname or port number might require a new CORS-entry to be added.
+
+Options
+  --port <port> TCP port to start server on. [default: 3333]
+  --host <host> The local network interface at which to listen. [default: "127.0.0.1"]
+
+Examples
+  sanity start --host=0.0.0.0
+  sanity start --port=1942
+`
+
 export default {
   name: 'start',
-  signature: '',
+  signature: '[--port <port>] [--host <host>]',
   description: 'Starts a webserver that serves Sanity',
-  action: lazyRequire(require.resolve('../../actions/start/startAction'))
+  action: lazyRequire(require.resolve('../../actions/start/startAction')),
+  helpText
 }
