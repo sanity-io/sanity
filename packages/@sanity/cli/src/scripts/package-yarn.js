eda91be2d1081457df6f87f8be1f7c1b4d64229c
Espen Hovlandsdal <espen@hovlandsdal.com>
2017-09-20 10:19:55 +0200

    [chore] Normalize dependency versions (#184)

--- packages/@sanity/cli/src/scripts/package-yarn.js
+++ packages/@sanity/cli/src/scripts/package-yarn.js
@@ -3,7 +3,7 @@ import path from 'path'
 import fsp from 'fs-promise'
 import simpleGet from 'simple-get'
 
-const version = '0.27.5'
+const version = '1.0.2'
 const baseUrl = 'https://github.com/yarnpkg/yarn/releases/download'
 const bundleUrl = `${baseUrl}/v${version}/yarn-legacy-${version}.js`
 const licenseUrl = 'https://raw.githubusercontent.com/yarnpkg/yarn/master/LICENSE'
