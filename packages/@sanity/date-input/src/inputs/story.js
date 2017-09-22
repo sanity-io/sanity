edc4728ef8c9429711f5093f0ccda1863176ba77
Bjørge Næss <bjoerge@gmail.com>
2017-09-22 12:58:23 +0200

    [date-input] Fix wrong part name in import

--- packages/@sanity/date-input/src/inputs/story.js
+++ packages/@sanity/date-input/src/inputs/story.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {storiesOf} from 'part:@sanity/storybook'
-import RichDateInput from 'part:@sanity/form-builder/input/richDate'
+import RichDateInput from 'part:@sanity/form-builder/input/rich-date'
 
 storiesOf('Icons').add(
   'Date picker',
