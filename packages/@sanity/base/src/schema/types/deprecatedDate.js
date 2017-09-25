6a3b14f38e4c8e94c23fda321f7826755daea099
Bjørge Næss <bjoerge@gmail.com>
2017-09-25 12:50:42 +0200

    [schema] Trigger deprecation warning for 'date' type only when encountering a field that uses it

--- packages/@sanity/base/src/schema/types/deprecatedDate.js
+++ packages/@sanity/base/src/schema/types/deprecatedDate.js
@@ -1,17 +1,5 @@
 import richDate from './richDate'
-import generateHelpUrl from '@sanity/generate-help-url'
 
-let hasWarned = false
 export default Object.assign({}, richDate, {
-  get name() {
-    if (!hasWarned) {
-      // eslint-disable-next-line no-console
-      console.warn(
-        'Heads up! The `date` type has been renamed to `richDate`. Please update your schema. See %s for more info.',
-        generateHelpUrl('migrate-to-rich-date')
-      )
-      hasWarned = true
-    }
-    return 'date'
-  }
+  name: 'date'
 })
