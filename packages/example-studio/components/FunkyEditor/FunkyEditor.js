0aa19706f467b0b6d0f077c14206bfd89b7fb526
Per-Kristian Nordnes <per.kristian.nordnes@gmail.com>
2017-09-27 13:59:01 +0200

    [example-studio] Make a cleaner example of FunkyEditor

--- packages/example-studio/components/FunkyEditor/FunkyEditor.js
+++ packages/example-studio/components/FunkyEditor/FunkyEditor.js
@@ -1,6 +1,18 @@
 import PropTypes from 'prop-types'
 import React from 'react'
 import {BlockEditor} from 'part:@sanity/form-builder'
+
+function extractTextFromBlocks(blocks) {
+  return blocks
+    .filter(val => val._type === 'block')
+    .map(block => {
+      return block.children
+        .filter(child => child._type === 'span')
+        .map(span => span.text)
+        .join('')
+    }).join('')
+}
+
 export default class FunkyEditor extends React.Component {
   static propTypes = {
     type: PropTypes.shape({
@@ -11,25 +23,19 @@ export default class FunkyEditor extends React.Component {
     onChange: PropTypes.func.isRequired
   };
 
-  handleChange = event => {
-    this.props.onChange(event)
-  }
-
-  componentDidUpdate() {
-    console.log('did update')
-  }
-
   render() {
-    const {type, value, level} = this.props
+    const {type, value = [], level, onChange} = this.props
     return (
       <div>
-        <h3>Take me to Funky Town</h3>
         <BlockEditor
           type={type}
           level={level}
-          value={value === undefined ? [] : value}
-          onChange={this.handleChange}
+          value={value}
+          onChange={onChange}
         />
+        <p>
+          Your funkyness is <strong>{extractTextFromBlocks(value).length}</strong> characters long
+        </p>
       </div>
     )
   }
