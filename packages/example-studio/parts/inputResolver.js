3a5a1de97cda544920b5cdc4494aaf09535e9fce
Per-Kristian Nordnes <per.kristian.nordnes@gmail.com>
2017-09-27 14:13:12 +0200

    [example-studio] Add example on how to wrap and customize the block editor (#209)
    * [example-studio] Add example on how to wrap and customize the block editor
    
    * [example-studio] Fix errors in FunkyEditor
    
    * [example-studio] Fix localizedSlug schema bug
    
    * [example-studio] Make a cleaner example of FunkyEditor

--- packages/example-studio/parts/inputResolver.js
+++ packages/example-studio/parts/inputResolver.js
@@ -1,9 +1,14 @@
+import FunkyEditor from '../components/FunkyEditor/FunkyEditor'
 import Slider from '../components/Slider/Slider'
 import {get} from 'lodash'
 
 export default function resolveInput(type) {
   if (type.name === 'number' && get(type, 'options.range')) {
     return Slider
+  }
+
+  if (type.name === 'array' && type.of.find(ofType => ofType.name === 'block')) {
+    return FunkyEditor
   }
 
   return false
