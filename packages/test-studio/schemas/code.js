690f001fa70a53ac45c745e2d183a6fea94c9ebe
Thomas Drevon <thomas@bengler.no>
2017-09-28 12:13:08 +0200

    More flexible code input (#214)
    * [test-studio] Update test-studio schema with language alternatives
    
    * [code-input] Make code-input slightly more configurable

--- packages/test-studio/schemas/code.js
+++ packages/test-studio/schemas/code.js
@@ -13,7 +13,16 @@ export default {
       name: 'code',
       title: 'Code',
       description: 'A plain code field',
-      type: 'code'
+      type: 'code',
+      options: {
+        theme: 'github',
+        languageAlternatives: [
+          {title: 'LaTeX', value: 'latex'},
+          {title: 'JavaScript', value: 'javascript'},
+          {title: 'CSS', value: 'css'},
+          {title: 'text', value: 'text'}
+        ]
+      }
     }
   ]
 }
