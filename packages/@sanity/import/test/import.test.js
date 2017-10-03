d0294d72c1a9c05acb9f050abeef44f71439b756
Espen Hovlandsdal <espen@hovlandsdal.com>
2017-10-03 15:06:41 +0200

    [import] Don't require _id property (#227)

--- packages/@sanity/import/test/import.test.js
+++ packages/@sanity/import/test/import.test.js
@@ -18,27 +18,21 @@ const getFixture = fix => {
 }
 
 test('rejects on invalid JSON', async () => {
-  await expect(
-    importer(getFixture('invalid-json'), options)
-  ).rejects.toHaveProperty(
+  await expect(importer(getFixture('invalid-json'), options)).rejects.toHaveProperty(
     'message',
     'Failed to parse line #3: Unexpected token _ in JSON at position 1'
   )
 })
 
 test('rejects on missing `_id` property', async () => {
-  await expect(
-    importer(getFixture('missing-id'), options)
-  ).rejects.toHaveProperty(
+  await expect(importer(getFixture('invalid-id'), options)).rejects.toHaveProperty(
     'message',
-    'Failed to parse line #2: Document did not contain required "_id" property of type string'
+    'Failed to parse line #2: Document contained an invalid "_id" property - must be a string'
   )
 })
 
 test('rejects on missing `_type` property', async () => {
-  await expect(
-    importer(getFixture('missing-type'), options)
-  ).rejects.toHaveProperty(
+  await expect(importer(getFixture('missing-type'), options)).rejects.toHaveProperty(
     'message',
     'Failed to parse line #3: Document did not contain required "_type" property of type string'
   )
