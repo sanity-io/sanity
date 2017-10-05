a341226c793d0ae353d6464ad23959dfa1fa5137
Bjørge Næss <bjoerge@gmail.com>
2017-10-05 15:31:10 +0200

    [form-builder] Simplify input resolving (#234)

---
+++ packages/@sanity/form-builder/src/sanity/inputResolver/customInputs.js
@@ -0,0 +1,29 @@
+// These are inputs that may be implemented by provided parts
+import BooleanInput from 'part:@sanity/form-builder/input/boolean?'
+import EmailInput from 'part:@sanity/form-builder/input/email?'
+import GeoPointInput from 'part:@sanity/form-builder/input/geopoint?'
+import NumberInput from 'part:@sanity/form-builder/input/number?'
+import ObjectInput from 'part:@sanity/form-builder/input/object?'
+import ReferenceInput from 'part:@sanity/form-builder/input/reference?'
+import RichDateInput from 'part:@sanity/form-builder/input/rich-date?'
+import StringInput from 'part:@sanity/form-builder/input/string?'
+import TextInput from 'part:@sanity/form-builder/input/text?'
+import UrlInput from 'part:@sanity/form-builder/input/url?'
+import CodeInput from 'part:@sanity/form-builder/input/code?'
+
+const DeprecatedDateInput = RichDateInput
+
+export default {
+  object: ObjectInput,
+  boolean: BooleanInput,
+  number: NumberInput,
+  string: StringInput,
+  text: TextInput,
+  reference: ReferenceInput,
+  date: DeprecatedDateInput,
+  richDate: RichDateInput,
+  email: EmailInput,
+  geopoint: GeoPointInput,
+  code: CodeInput,
+  url: UrlInput
+}
