cc503b14bb76ff69d20c12266f15b7ac94c53291
Bjørge Næss <bjoerge@gmail.com>
2017-10-08 12:38:35 +0200

    [form-builder] Rewrite how we limit concurrent uploads (#245)

--- packages/@sanity/form-builder/src/sanity/inputs/client-adapters/assets.js
+++ packages/@sanity/form-builder/src/sanity/inputs/client-adapters/assets.js
@@ -1,19 +1,22 @@
 import client from 'part:@sanity/base/client'
+import {observeWithPaths} from 'part:@sanity/base/preview'
+import {withMaxConcurrency} from '../../utils/withMaxConcurrency'
 
-function uploadAsset(assetType, file) {
+const MAX_CONCURRENT_UPLOADS = 4
+
+function uploadSanityAsset(assetType, file) {
   return client.observable.assets.upload(assetType, file)
     .map(event => {
-      if (event.type === 'response') {
-        // rewrite to a 'complete' event
-        return {
+      return event.type === 'response'
+        ? { // rewrite to a 'complete' event
           type: 'complete',
           id: event.body.document._id,
           asset: event.body.document
-        }
-      }
-      return event
+        } : event
     })
 }
+
+const uploadAsset = withMaxConcurrency(uploadSanityAsset, MAX_CONCURRENT_UPLOADS)
 
 export const uploadImageAsset = file => uploadAsset('image', file)
 export const uploadFileAsset = file => uploadAsset('file', file)
