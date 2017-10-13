369a0d5a34835e76ef821c8b904bffec90e4b473
Thomas Drevon <thomas@bengler.no>
2017-10-13 16:44:12 +0200

    [test-studio] Add string-based date type to test studio schema

--- packages/test-studio/schemas/dates.js
+++ packages/test-studio/schemas/dates.js
@@ -2,26 +2,81 @@ export default {
   name: 'datesTest',
   type: 'object',
   title: 'Date fields test',
+  fieldsets: [
+    {name: 'richDate', title: 'Rich Date', options: {collapsable: true}},
+    {name: 'date', title: 'String Date', options: {collapsable: true}}
+  ],
+
   fields: [
     {
       name: 'title',
       type: 'string',
       title: 'Title'
     },
+
+    // ------------- string dates
+
+    {
+      name: 'myStringDateField',
+      type: 'date',
+      title: 'String date with default config',
+      description: 'A plain date field',
+      fieldset: 'date'
+    },
+    {
+      name: 'myUtcStringDateField',
+      type: 'date',
+      title: 'String date with inputUtc: true',
+      description: 'A plain date field with inputUtc: true',
+      options: {
+        inputUtc: true
+      },
+      fieldset: 'date'
+    },
+    {
+      name: 'myUtcStringDateWithoutTimeField',
+      type: 'date',
+      title: 'String date without time',
+      description: 'A plain date field without time',
+      options: {
+        inputDate: true,
+        inputTime: false,
+        dateFormat: 'Do. MMMM YYYY'
+      },
+      fieldset: 'date'
+    },
+    {
+      name: 'myUtcStringDateWithoutDateField',
+      type: 'date',
+      title: 'String date without date',
+      description: 'A plain date field with timeStep, without date',
+      options: {
+        inputDate: false,
+        inputTime: true,
+        dateFormat: 'HH:mm',
+        timeStep: 30
+      },
+      fieldset: 'date'
+    },
+
+    // ------------- rich dates
+
     {
       name: 'myDateField',
       type: 'richDate',
       title: 'Rich date with default config',
-      description: 'A plain richDate field'
+      description: 'A plain richDate field',
+      fieldset: 'richDate'
     },
     {
       name: 'myUtcDateField',
       type: 'richDate',
-      title: 'Rich date with inputUtc = true',
+      title: 'Rich date with inputUtc: true',
       description: 'A plain richDate field',
       options: {
         inputUtc: true
-      }
+      },
+      fieldset: 'richDate'
     },
 
     {
@@ -32,7 +87,8 @@ export default {
       options: {
         inputDate: true,
         inputTime: false
-      }
+      },
+      fieldset: 'richDate'
     },
     {
       name: 'myOnlyTimeField',
@@ -42,7 +98,8 @@ export default {
       options: {
         inputDate: false,
         inputTime: true
-      }
+      },
+      fieldset: 'richDate'
     },
     {
       name: 'formatedDateField',
@@ -52,13 +109,8 @@ export default {
       options: {
         dateFormat: 'MMMM Do YYYY',
         calendarTodayLabel: 'Klikk her for å gå til idag'
-      }
-    },
-    {
-      name: 'myDeprecatedDateField',
-      type: 'date',
-      title: 'A field with type: date',
-      description: 'Should still work, but display a deprecation warning'
+      },
+      fieldset: 'richDate'
     }
   ]
 }
