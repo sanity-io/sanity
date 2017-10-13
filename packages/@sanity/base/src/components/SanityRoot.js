7383988d1b7522b7241b0f820b66a5f0a611a3ba
Kristoffer J. Sivertsen <kristoffer@bengler.no>
2017-10-13 21:43:25 +0200

    Fix/new design post fix (#274)
    - Always use sanity-logo as fallback when not providing a BrandLogo
    - Making the projectName-text nicer
    - Making the main navigation background overridable with variable
    - Removing redundant use of body-style part
    - Making a fonts-style part that is included only once and can be overwritten
    
    * [default-layout] Fixing main navigation
    
    * [base] Adding a font-style part, and only use body-style port one place.
    
    * [base] Using overflow:auto. Some components (react-datepicker) dont understand overflow:overlay

--- packages/@sanity/base/src/components/SanityRoot.js
+++ packages/@sanity/base/src/components/SanityRoot.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import RootComponent from 'part:@sanity/base/root'
 import VersionChecker from './VersionChecker'
+import styles from './styles/SanityRoot.css'
 
 function SanityRoot() {
   return (
-    <div>
+    <div className={styles.root}>
       <RootComponent />
       <VersionChecker />
     </div>
