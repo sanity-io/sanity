fee608b1fb8d85c73002c59bf349b8d3526381ed
Bjørge Næss <bjoerge@gmail.com>
2017-10-27 13:56:44 +0200

    [desk-tool] Make desk tool handle edit intents without type (#303)
    * [desk-tool] Make desk tool handle edit intents without type
    
    * [desk-tool] Optimize query, and redirect on type mismatch too

--- packages/@sanity/desk-tool/src/DeskTool.js
+++ packages/@sanity/desk-tool/src/DeskTool.js
@@ -1,13 +1,36 @@
 import React from 'react'
 import styles from './styles/DeskTool.css'
 import SchemaPaneResolver from './SchemaPaneResolver'
+import client from 'part:@sanity/base/client'
+import {withRouterHOC} from 'part:@sanity/base/router'
+import PropTypes from 'prop-types'
 
-export default class DeskTool extends React.Component {
+export default withRouterHOC(class DeskTool extends React.Component {
+  static propTypes = {
+    router: PropTypes.shape({
+      state: PropTypes.object
+    }).isRequired
+  }
+  componentDidMount() {
+    const {router} = this.props
+    const {selectedType, selectedDocumentId} = router.state
+    if (selectedDocumentId && selectedType) {
+      client.fetch('*[_id == $id][0]._type', {id: selectedDocumentId})
+        .then(type => {
+          if (type !== selectedType) {
+            router.navigate({...router.state, selectedType: type})
+          }
+        })
+    }
+  }
+
   render() {
+    const {router} = this.props
+
     return (
       <div className={styles.deskTool}>
-        <SchemaPaneResolver />
+        <SchemaPaneResolver router={router} />
       </div>
     )
   }
-}
+})
