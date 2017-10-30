96dedcd79179bc9c0d3f4cd69a5a35ecb48f86f4
Espen Hovlandsdal <espen@hovlandsdal.com>
2017-10-30 16:57:04 +0100

    [webpack-integration] Resolve symlinks to real files, cache imports (#312)

--- packages/@sanity/webpack-integration/src/v3/resolveStyleImport.js
+++ packages/@sanity/webpack-integration/src/v3/resolveStyleImport.js
@@ -1,33 +1,86 @@
-const resolveParts = require('@sanity/resolver').resolveParts
+const rp = require('fs.realpath')
+const PAC = require('p-async-cache')
 const resolveNodeModule = require('resolve')
+const resolveParts = require('@sanity/resolver').resolveParts
 
-function resolveStyleImport(moduleId, opts) {
-  const id = moduleId.replace(/^\.\/(part|all:)/, '$1')
-  if (!isSanityPart(id)) {
-    return resolveNodeModule.sync(id, opts)
-  }
+const partsCache = new PAC({
+  load: resolvePartsForPath,
+  maxAge: 1000
+})
 
-  const parts = resolveParts({basePath: opts.root, sync: true})
-  const loadAll = id.indexOf('all:') === 0
-  const partName = loadAll ? id.substr(4) : id
+const sanityCache = new PAC({
+  load: resolveSanityImport,
+  maxAge: 1000
+})
 
-  const part = parts.implementations[partName]
-  if (!part) {
-    throw new Error(`No implementers of part '${partName}'`)
-  }
+const nodeCache = new PAC({
+  load: resolveNodeImport,
+  maxAge: 1000
+})
 
-  const paths = part.map(implementer => implementer.path)
-  return loadAll
-    ? paths.reverse()
-    : paths[0]
+function resolvePartsForPath(basePath) {
+  return resolveParts({basePath})
+}
+
+function resolveNodeImport(moduleId, basedir) {
+  return resolveModule(moduleId, {basedir})
+}
+
+function resolveSanityImport(id, basePath) {
+  return partsCache.get(basePath).then(cached => {
+    const parts = cached.value
+    const loadAll = id.indexOf('all:') === 0
+    const partName = loadAll ? id.substr(4) : id
+
+    const part = parts.implementations[partName]
+    if (!part) {
+      throw new Error(`No implementers of part '${partName}'`)
+    }
+
+    const paths = part.map(implementer => realPath(implementer.path))
+    return loadAll ? Promise.all(paths.reverse()) : paths[0]
+  })
 }
 
 function isSanityPart(part) {
   return /^(all:)?part:[@A-Za-z0-9_-]+\/[A-Za-z0-9_/-]+/.test(part)
 }
 
-module.exports = function getStyleResolver(opts) {
-  return function resolveStyleProxy(moduleId, basedir, styleOptions) {
-    return resolveStyleImport(moduleId, Object.assign({root: opts.from, basedir}, styleOptions))
+function resolveModule(id, opts) {
+  return new Promise((resolve, reject) => {
+    resolveNodeModule(id, opts, (err, res) => {
+      if (err) {
+        reject(err)
+        return
+      }
+
+      resolve(res)
+    })
+  })
+}
+
+function realPath(path) {
+  return new Promise((resolve, reject) => {
+    rp.realpath(path, (err, real) => {
+      if (err) {
+        reject(err)
+        return
+      }
+
+      resolve(real)
+    })
+  })
+}
+
+function getStyleResolver(opts) {
+  return function resolveStyleProxy(moduleId, basedir) {
+    const id = moduleId.replace(/^\.\/(part|all:)/, '$1')
+    const resolveStyleImport = isSanityPart(id)
+      ? sanityCache.get(id, opts.from)
+      : nodeCache.get(id, basedir)
+
+    return resolveStyleImport.then(res => res.value)
   }
 }
+
+module.exports = getStyleResolver
