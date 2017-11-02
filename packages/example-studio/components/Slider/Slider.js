41adefb123ff83a61c16a9b0bd4c9c2919a665e0
Bjørge Næss <bjoerge@gmail.com>
2017-11-02 18:48:24 +0100

    [form-builder] Add a separate part for patch-event (#327)

--- packages/example-studio/components/Slider/Slider.js
+++ packages/example-studio/components/Slider/Slider.js
@@ -1,7 +1,8 @@
 import PropTypes from 'prop-types'
 import React from 'react'
 import FormField from 'part:@sanity/components/formfields/default'
-import PatchEvent, {set, unset} from '@sanity/form-builder/PatchEvent'
+import PatchEvent, {set, unset} from 'part:@sanity/form-builder/patch-event'
+
 import styles from './Slider.css'
 
 export default class Slider extends React.Component {
