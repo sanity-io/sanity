5aa8df5427defedd8584c3182bfcca0a43dde99e
Bjørge Næss <bjoerge@gmail.com>
2017-11-03 16:12:49 +0100

    [example-studio] Simplify and fix a a few issues w/protein input example

---
+++ packages/example-studio/components/ProteinInput/PDBS.js
@@ -0,0 +1,31 @@
+
+export default [
+  {
+    title: 'Structure of the dengue virus 2\'O methyltransferase in complex with s-adenosyl homocysteine and ribavirin 5\' triphosphate',
+    id: '1R6A'
+  },
+  {
+    title: 'Adenovirus',
+    id: '3IYN'
+  },
+  {
+    title: 'Insect Flight Muscle',
+    id: '2W49'
+  },
+  {
+    title: '4HHB',
+    id: '4HHB'
+  },
+  {
+    title: 'Crystal structure of Myosin VIIa ',
+    id: '5WSU'
+  },
+  {
+    title: 'Protonation-mediated structural flexibility in the F conjugation regulatory protein.',
+    id: '2G7O'
+  },
+  {
+    title: 'Escherichia coli Hfq-RNA',
+    id: '4PNO'
+  }
+]
