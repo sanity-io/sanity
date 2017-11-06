806f1f1d57b96d0d41c807cd1087878c8a96d820
Espen Hovlandsdal <espen@hovlandsdal.com>
2017-11-06 11:58:48 +0100

    [core] Prompt for dataset on import/export (#337)

---
+++ packages/@sanity/core/src/actions/dataset/chooseDatasetPrompt.js
@@ -0,0 +1,31 @@
+import debug from '../../debug'
+import promptForDatasetName from './datasetNamePrompt'
+
+module.exports = async (context, options = {}) => {
+  const {apiClient, prompt} = context
+  const {message, allowCreation} = options
+  const client = apiClient()
+
+  const datasets = await client.datasets.list()
+  const hasProduction = datasets.find(dataset => dataset.name === 'production')
+  const datasetChoices = datasets.map(dataset => ({value: dataset.name}))
+  const selected = await prompt.single({
+    message: message || 'Select dataset to use',
+    type: 'list',
+    choices: allowCreation
+      ? [{value: 'new', name: 'Create new dataset'}, new prompt.Separator(), ...datasetChoices]
+      : datasetChoices
+  })
+
+  if (selected === 'new') {
+    debug('User wants to create a new dataset, prompting for name')
+    const newDatasetName = await promptForDatasetName(prompt, {
+      message: 'Name your dataset:',
+      default: hasProduction ? undefined : 'production'
+    })
+    await client.datasets.create(newDatasetName)
+    return newDatasetName
+  }
+
+  return selected
+}
