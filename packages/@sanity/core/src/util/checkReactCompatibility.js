4ba1b9f085736421665591f6211f5bf8606a5fab
Espen Hovlandsdal <espen@hovlandsdal.com>
2017-11-06 17:07:19 +0100

    [core] Check if installed React version is supported on startup (#340)

---
+++ packages/@sanity/core/src/util/checkReactCompatibility.js
@@ -0,0 +1,30 @@
+const path = require('path')
+const semver = require('semver')
+const resolveFrom = require('resolve-from')
+
+const supported = {
+  react: '^15.0.0',
+  'react-dom': '^15.0.0'
+}
+
+module.exports = workDir => {
+  const manifest = require(path.join(workDir, 'package.json'))
+  const dependencies = Object.assign({}, manifest.dependencies, manifest.devDependencies)
+
+  Object.keys(supported).forEach(pkg => {
+    if (!dependencies[pkg]) {
+      return
+    }
+
+    const manifestPath = resolveFrom.silent(workDir, path.join(pkg, 'package.json'))
+    const installed = manifestPath
+      ? require(manifestPath).version
+      : dependencies[pkg].replace(/[\D.]/g, '')
+
+    if (!semver.satisfies(installed, supported[pkg])) {
+      throw new Error(
+        `Sanity currently only supports ${pkg}@${supported[pkg]}. Installed version: ${installed}.`
+      )
+    }
+  })
+}
