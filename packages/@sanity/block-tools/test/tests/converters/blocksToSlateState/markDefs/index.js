06e370044d2827bbba11043b2da850f28be4a7fa
Per-Kristian Nordnes <per.kristian.nordnes@gmail.com>
2017-11-07 21:20:44 +0100

    [block-tools] Fix bug where markDefs where put incorrectly on Slate block's data

---
+++ packages/@sanity/block-tools/test/tests/converters/blocksToSlateState/markDefs/index.js
@@ -0,0 +1,11 @@
+import defaultSchema from '../../../../fixtures/defaultSchema'
+
+const blockContentType = defaultSchema.get('blogPost')
+  .fields.find(field => field.name === 'body').type
+
+
+export default (blockToSlateState, input) => {
+  const result = blockToSlateState(input, blockContentType)
+  // console.log(JSON.stringify(result))
+  return result
+}
