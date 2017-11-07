f1b61f5933bab9a634cf40a94945755c6837485e
Kristoffer J. Sivertsen <kristofferjoachim@gmail.com>
2017-11-07 21:51:50 +0100

    [components] Adding context scroll container

---
+++ packages/@sanity/components/src/utilities/ScrollContainer.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import styles from './styles/ScrollContainer.css'
+
+export default class ScrollContainer extends React.PureComponent {
+  static propTypes = {
+    className: PropTypes.string,
+    children: PropTypes.node,
+    onScroll: PropTypes.func
+  }
+
+  static defaultProps = {
+    onScroll: () => {}
+  }
+
+  getChildContext() {
+    return {
+      getScrollContainer: () => this._scrollContainerElement
+    }
+  }
+
+  static defaultProps = {
+    className: ''
+  }
+
+  componentDidMount() {
+    this._scrollContainerElement.addEventListener('scroll', this.props.onScroll, {passive: true})
+  }
+
+  static childContextTypes = {
+    getScrollContainer: PropTypes.func
+  }
+
+  setScrollContainerElement = element => {
+    this._scrollContainerElement = element
+  }
+
+  render() {
+    return (
+      <div
+        ref={this.setScrollContainerElement}
+        className={`${styles.scrollContainer} ${this.props.className}`}
+      >
+        {this.props.children}
+      </div>
+    )
+  }
+}
