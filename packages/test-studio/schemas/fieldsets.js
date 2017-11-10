111528fff31a3f093e8ead1b313804499b088c20
Bjørge Næss <bjoerge@gmail.com>
2017-11-10 13:32:46 +0100

    [test-studio] Add a fieldsets test type

---
+++ packages/test-studio/schemas/fieldsets.js
@@ -0,0 +1,70 @@
+export default {
+  name: 'fieldsetsTest',
+  type: 'document',
+  title: 'Fieldsets test',
+  preview: {
+    select: {
+      title: 'myObject.first'
+    }
+  },
+  fieldsets: [{name: 'recursive', title: 'Recursive', options: {collapsable: true}}],
+  fields: [
+    {
+      name: 'myObject',
+      type: 'myObject',
+      title: 'MyObject',
+      description: 'The first field here should be the title'
+    },
+    {
+      name: 'Person',
+      type: 'object',
+      fieldsets: [{name: 'social', title: 'Social media handles'}],
+      fields: [
+        {
+          name: 'name',
+          title: 'Name',
+          type: 'string'
+        },
+        {
+          name: 'twitter',
+          title: 'Twitter',
+          type: 'string',
+          fieldset: 'social'
+        },
+        {
+          name: 'instagram',
+          title: 'Instagram',
+          type: 'string',
+          fieldset: 'social'
+        },
+        {
+          name: 'facebook',
+          title: 'Facebook',
+          type: 'string',
+          fieldset: 'social'
+        }
+      ]
+    },
+    {
+      name: 'fieldWithObjectType',
+      title: 'Field of object type',
+      type: 'object',
+      description: 'This is a field of (anonymous, inline) object type. Values here should never get a `_type` property',
+      fields: [
+        {name: 'field1', type: 'string', description: 'This is a string field'},
+        {
+          name: 'field2',
+          type: 'myObject',
+          title: 'A field of myObject',
+          description: 'This is another field of "myObject"'
+        },
+      ]
+    },
+    {
+      name: 'recursive',
+      title: 'This field is of type objectsTest',
+      type: 'objectsTest',
+      fieldset: 'recursive'
+    }
+  ]
+}
