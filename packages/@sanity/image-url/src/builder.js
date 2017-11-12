24ad431829d0f7a13e51ad9d9c2a667341f461c7
Simen Svale Skogsrud <simen@bengler.no>
2017-11-12 21:22:42 +0100

    [image-url] Introducing the image-url package
    Quickly generate image urls from Sanity image records + a builder api for the Sanity image pipeline url options.
    
    * [image-url] Introducing the image-url package
    
    * [image-url] Documentation + configuration compliance
    
    * [image-url] README amendments

---
+++ packages/@sanity/image-url/src/builder.js
@@ -0,0 +1,145 @@
+import urlForImage from './urlForImage'
+
+class ImageUrlBuilder {
+  constructor(parent, options) {
+    if (parent) {
+      this.options = Object.assign({}, parent.options, options || {})
+    } else {
+      this.options = options || {}
+    }
+  }
+
+  _withOptions(options) {
+    return new ImageUrlBuilder(this, options)
+  }
+
+  // The image to be represented. Accepts a Sanity 'image'-document, 'asset'-document or
+  // _id of asset. To get the benefit of automatic hot-spot/crop integration with the content
+  // studio, the 'image'-document must be provided.
+  image(source) {
+    return this._withOptions({source})
+  }
+
+  // Specify the dataset
+  dataset(dataset) {
+    return this._withOptions({dataset})
+  }
+
+  // Specify the projectId
+  projectId(projectId) {
+    return this._withOptions({projectId})
+  }
+
+  // Specify the width of the image in pixels
+  width(width) {
+    return this._withOptions({width})
+  }
+
+  // Specify the height of the image in pixels
+  height(height) {
+    return this._withOptions({height})
+  }
+
+  // Specify focal point in fraction of image dimensions. Each component 0.0-1.0
+  focalPoint(x, y) {
+    return this._withOptions({focalPoint: {x, y}})
+  }
+
+  maxWidth(maxWidth) {
+    return this._withOptions({maxWidth})
+  }
+
+  minWidth(minWidth) {
+    return this._withOptions({minWidth})
+  }
+
+  maxHeight(maxHeight) {
+    return this._withOptions({maxHeight})
+  }
+
+  minHeight(minHeight) {
+    return this._withOptions({minHeight})
+  }
+
+  // Specify width and height in pixels
+  size(width, height) {
+    return this._withOptions({width, height})
+  }
+
+  // Specify blur between 0 and 100
+  blur(blur) {
+    return this._withOptions({blur})
+  }
+
+  sharpen(sharpen) {
+    return this._withOptions({sharpen})
+  }
+
+  // Specify the desired rectangle of the image
+  rect(left, top, width, height) {
+    return this._withOptions({rect: {left, top, width, height}})
+  }
+
+  // Specify the image format of the image. 'jpg', 'pjpg', 'png', 'webp'
+  format(format) {
+    return this._withOptions({format})
+  }
+
+  invert(invert) {
+    return this._withOptions({invert})
+  }
+
+  // Rotation in degrees 0, 90, 180, 270
+  orientation(orientation) {
+    return this._withOptions({orientation})
+  }
+
+  // Compression quality 0-100
+  quality(quality) {
+    return this._withOptions({quality})
+  }
+
+  // Make it a download link. Parameter is default filename.
+  forceDownload(download) {
+    return this._withOptions({download})
+  }
+
+  // Flip image horizontally
+  flipHorizontal() {
+    return this._withOptions({flipHorizontal: true})
+  }
+
+  // Flip image verically
+  flipVertical() {
+    return this._withOptions({flipVertical: true})
+  }
+
+  // Ignore crop/hotspot from image record, even when present
+  ignoreImageParams() {
+    return this._withOptions({ignoreImageParams: true})
+  }
+
+  // Gets the url based on the submitted parameters
+  url() {
+    return urlForImage(this.options)
+  }
+
+  // Synonym for url()
+  toString() {
+    return this.url()
+  }
+}
+
+export default function urlBuilder(options) {
+  // Did we get a SanityClient?
+  if (options && typeof options.clientConfig == 'object') {
+    // Inherit config from client
+    return new ImageUrlBuilder(null, {
+      projectId: options.clientConfig.projectId,
+      dataset: options.clientConfig.dataset,
+    })
+  }
+
+  // Or just accept the options as given
+  return new ImageUrlBuilder(null, options)
+}
---
+++ packages/@sanity/image-url/test/builder.test.js
@@ -0,0 +1,45 @@
+import sanityImage from '../src/builder'
+import should from 'should'
+import {croppedImage} from './fixtures'
+
+const urlFor = sanityImage().projectId('zp7mbokg').dataset('production')
+
+function stripPath(url) {
+  return url.split('?')[1]
+}
+
+const cases = [
+  {
+    name: 'constrains aspect ratio',
+    url: urlFor.image(croppedImage()).size(100, 80).url(),
+    expect: 'https://cdn.sanity.io/images/zp7mbokg/production/Tb9Ew8CXIwaY6R1kjMvI0uRR-2000x3000.jpg?rect=200,300,1600,1280&w=100&h=80'
+  },
+  {
+    name: 'all params',
+    url: stripPath(urlFor.image(croppedImage())
+      .focalPoint(10, 20)
+      .maxWidth(200)
+      .minWidth(100)
+      .maxHeight(300)
+      .minHeight(150)
+      .blur(50)
+      .rect(10, 20, 30, 40)
+      .format('png')
+      .invert(true)
+      .orientation(90)
+      .quality(50)
+      .forceDownload('a.png')
+      .flipHorizontal()
+      .flipVertical()
+      .url()),
+    expect: 'rect=10,20,30,40&fp-x=10&fp-x=20&flip=hv&fm=png&dl=a.png&blur=50&invert=true&or=90&min-h=150&max-h=300&min-w=100&max-w=200&q=50'
+  }
+]
+
+describe('builder', () => {
+  cases.forEach(testCase => {
+    it(testCase.name, () => {
+      should(testCase.url).equal(testCase.expect)
+    })
+  })
+})
