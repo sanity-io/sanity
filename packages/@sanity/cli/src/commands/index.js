ec2933c9cf98139846fdc6272c771d8df5a71ff4
Espen Hovlandsdal <espen@hovlandsdal.com>
2017-11-13 11:28:18 +0100

    [cli] Add CLI methods for manage and docs (#351)

--- packages/@sanity/cli/src/commands/index.js
+++ packages/@sanity/cli/src/commands/index.js
@@ -1,10 +1,23 @@
+import debugCommand from './debug/debugCommand'
+import docsCommand from './docs/docsCommand'
 import helpCommand from './help/helpCommand'
 import initCommand from './init/initCommand'
 import installCommand from './install/installCommand'
-import upgradeCommand from './upgrade/upgradeCommand'
 import loginCommand from './login/loginCommand'
 import logoutCommand from './logout/logoutCommand'
+import manageCommand from './manage/manageCommand'
+import upgradeCommand from './upgrade/upgradeCommand'
 import versionsCommand from './versions/versionsCommand'
-import debugCommand from './debug/debugCommand'
 
-export default [initCommand, loginCommand, logoutCommand, installCommand, upgradeCommand, versionsCommand, debugCommand, helpCommand]
+export default [
+  initCommand,
+  loginCommand,
+  logoutCommand,
+  installCommand,
+  upgradeCommand,
+  versionsCommand,
+  docsCommand,
+  manageCommand,
+  debugCommand,
+  helpCommand
+]
---
+++ packages/@sanity/cli/src/commands/manage/manageCommand.js
@@ -0,0 +1,29 @@
+import path from 'path'
+import opn from 'opn'
+import fse from 'fs-extra'
+
+export default {
+  name: 'manage',
+  signature: 'manage',
+  description: 'Opens the Sanity project management UI',
+  async action(args, context) {
+    const {workDir, output} = context
+    const {print} = output
+    const configLocation = path.join(workDir, 'sanity.json')
+
+    let projectId
+    try {
+      const config = await fse.readJson(configLocation)
+      projectId = config.api && config.api.projectId
+    } catch (err) {
+      // Noop.
+    }
+
+    const url = projectId
+      ? `https://manage.sanity.io/projects/${projectId}`
+      : 'https://manage.sanity.io/'
+
+    print(`Opening ${url}`)
+    opn(url)
+  }
+}
