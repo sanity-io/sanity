4f64450b2567419eb92ec68f88bf35f226251b8b
Bjørge Næss <bjoerge@gmail.com>
2017-11-19 17:03:10 +0100

    [form-builder] Move item remove logic to ConfirmButton

--- packages/@sanity/form-builder/src/inputs/Array/ConfirmButton.js
+++ packages/@sanity/form-builder/src/inputs/Array/ConfirmButton.js
@@ -1,39 +1,63 @@
 import PropTypes from 'prop-types'
 import React from 'react'
 import Button from 'part:@sanity/components/buttons/default'
+import styles from './styles/ConfirmButton.css'
+import TrashIcon from 'part:@sanity/base/trash-icon'
+import PopOver from 'part:@sanity/components/dialogs/popover'
 
 export default class ConfirmButton extends React.Component {
   static propTypes = {
     children: PropTypes.func,
-    onClick: PropTypes.func
+    onConfirm: PropTypes.func
   }
 
   state = {
-    doConfirm: false
-  }
-
-  handleBlur = event => {
-    this.setState({doConfirm: false})
+    showConfirmDialog: false
   }
 
   handleClick = event => {
-    if (this.state.doConfirm) {
-      this.props.onClick(event)
-    } else {
-      this.setState({doConfirm: true})
-    }
+    this.setState({
+      showConfirmDialog: true
+    })
+  }
+
+  handleConfirmPopoverClose = event => {
+    this.setState({
+      showConfirmDialog: false
+    })
   }
 
   render() {
-    const {children, ...rest} = this.props
-    const {doConfirm} = this.state
+    const {showConfirmDialog} = this.state
+    const {onConfirm, ...rest} = this.props
     return (
       <Button
         {...rest}
+        tabIndex={0}
+        kind="simple"
+        color="danger"
+        icon={TrashIcon}
         onClick={this.handleClick}
-        onBlur={this.handleBlur}
       >
-        {children(doConfirm)}
+        <div className={styles.popoverAnchor}>
+          {
+            showConfirmDialog && (
+              <PopOver
+                color="danger"
+                useOverlay={false}
+                onClose={this.handleConfirmPopoverClose}
+              >
+                <Button
+                  kind="simple"
+                  onClick={onConfirm}
+                  icon={TrashIcon}
+                >
+                  Confirm remove
+                </Button>
+              </PopOver>
+            )
+          }
+        </div>
       </Button>
     )
   }
