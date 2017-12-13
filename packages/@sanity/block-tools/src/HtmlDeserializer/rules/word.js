75dcaeb8b794a341601dca0f4ab5c182a8c19bb8
Per-Kristian Nordnes <per.kristian.nordnes@gmail.com>
2017-12-13 13:40:10 +0100

    [block-tools] Handle Word footnotes and better detection of list types

--- packages/@sanity/block-tools/src/HtmlDeserializer/rules/word.js
+++ packages/@sanity/block-tools/src/HtmlDeserializer/rules/word.js
@@ -1,15 +1,18 @@
 import {DEFAULT_BLOCK} from '../../constants'
 import {tagName} from '../helpers'
 
+// https://gist.github.com/webtobesocial/ac9d052595b406d5a5c1
+
+function notesEnabled(options) {
+  return options.enabledBlockAnnotations.includes('note')
+}
+
 function getListItemStyle(el) {
-  let style
-  if ((style = el.getAttribute('style'))) {
-    if (!style.match(/lfo\d+/)) {
-      return undefined
-    }
-    return style.match('lfo1') ? 'bullet' : 'number'
+  const symbol = el.textContent.trim()
+  if (symbol.match(/\b\./)) {
+    return 'number'
   }
-  return undefined
+  return 'bullet'
 }
 
 function getListItemLevel(el) {
@@ -25,7 +28,7 @@ function getListItemLevel(el) {
   return undefined
 }
 
-function isWordListElement(el) {
+function isListElement(el) {
   if (el.className) {
     return el.className === 'MsoListParagraphCxSpFirst'
       || el.className === 'MsoListParagraphCxSpMiddle'
@@ -34,19 +37,146 @@ function isWordListElement(el) {
   return undefined
 }
 
+function getFootnoteContentElementId(el) {
+  const style = el.getAttribute('style')
+  if (style && style === 'mso-element:footnote') {
+    return el.getAttribute('id').trim()
+  }
+  return null
+}
+
+function getFootnoteLinkElementId(el) {
+  const style = el.getAttribute('style')
+  if (style && style.match(/mso-footnote-id/)) {
+    return style.split(':')[1].trim()
+  }
+  return null
+}
+
+function getEndnoteContentElementId(el) {
+  const style = el.getAttribute('style')
+  if (style && style === 'mso-element:endnote') {
+    return el.getAttribute('id').trim()
+  }
+  return null
+}
+
+function getEndnoteLinkElementId(el) {
+  const style = el.getAttribute('style')
+  if (style && style.match(/mso-endnote-id/)) {
+    return style.split(':')[1].trim()
+  }
+  return null
+}
+
 export default function createWordRules(blockContentType, options = {}) {
 
   return [
     {
       deserialize(el, next) {
-        if (tagName(el) === 'p' && isWordListElement(el)) {
+        if (tagName(el) === 'p' && isListElement(el)) {
+          const listItem = el.querySelector("span[style='mso-list:Ignore']")
+          const listItemStyle = getListItemStyle(listItem)
+          listItem.parentNode.removeChild(listItem)
           return {
             ...DEFAULT_BLOCK,
-            listItem: getListItemStyle(el),
+            listItem: listItemStyle,
             level: getListItemLevel(el),
             style: 'normal',
             children: next(el.childNodes)
           }
+        }
+        return undefined
+      }
+    },
+    // Fotnote links
+    {
+      deserialize(el, next) {
+        let footnoteId
+        if (tagName(el) === 'a' && (footnoteId = getFootnoteLinkElementId(el))) {
+          if (!notesEnabled(options)) {
+            return undefined
+          }
+          const markDef = {
+            _key: footnoteId,
+            _type: 'note',
+            style: 'footnote'
+          }
+          return {
+            _type: '__annotation',
+            markDef: markDef,
+            children: next(el.childNodes)
+          }
+
+        }
+        return undefined
+      }
+    },
+    // Footnote content
+    {
+      deserialize(el, next, blocks, deserialize) {
+        let footnoteId
+        if (tagName(el) === 'div' && (footnoteId = getFootnoteContentElementId(el))) {
+          if (!notesEnabled(options)) {
+            return undefined
+          }
+          // Find the block where the footnote occured
+          const markDef = blocks
+            .map(blk => blk.markDefs.find(def => def._key === footnoteId))
+            .filter(Boolean)[0]
+          if (markDef) {
+            el.querySelectorAll(`a[name='_${footnoteId}']`).forEach(elm => {
+              elm.parentNode.removeChild(elm)
+            })
+            markDef.content = deserialize(el.childNodes)
+          }
+          return next([])
+        }
+        return undefined
+      }
+    },
+    // Endnote links
+    {
+      deserialize(el, next) {
+        let endnoteId
+        if (tagName(el) === 'a' && (endnoteId = getEndnoteLinkElementId(el))) {
+          if (!notesEnabled(options)) {
+            return undefined
+          }
+          const markDef = {
+            _key: endnoteId,
+            _type: 'note',
+            style: 'endnote'
+          }
+          return {
+            _type: '__annotation',
+            markDef: markDef,
+            children: next(el.childNodes)
+          }
+
+        }
+        return undefined
+      }
+    },
+    // Endnote content
+    {
+      deserialize(el, next, blocks, deserialize) {
+        let endnoteId
+        if (tagName(el) === 'div' && (endnoteId = getEndnoteContentElementId(el))) {
+          if (!notesEnabled(options)) {
+            return undefined
+          }
+          // Find the block where the footnote occured
+          const markDef = blocks
+            .map(blk => blk.markDefs.find(def => def._key === endnoteId))
+            .filter(Boolean)[0]
+          if (markDef) {
+            el.querySelectorAll(`a[name='_${endnoteId}']`).forEach(elm => {
+              elm.parentNode.removeChild(elm)
+            })
+            markDef.content = deserialize(el.childNodes)
+          }
+          return next([])
         }
         return undefined
       }
