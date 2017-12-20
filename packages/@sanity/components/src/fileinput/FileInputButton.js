839f5f7c1c69697ebe834e171db4cd44a78b6fc7
Kristoffer J. Sivertsen <kristofferjoachim@gmail.com>
2017-12-20 13:15:51 +0100

    [components] Fix "input inside button"-issue, solves upload button not working in Firefox

--- packages/@sanity/components/src/fileinput/FileInputButton.js
+++ packages/@sanity/components/src/fileinput/FileInputButton.js
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types'
 import React from 'react'
 
-import Button from 'part:@sanity/components/buttons/default'
+import FakeButton from 'part:@sanity/components/buttons/fake'
 import FileInput from 'part:@sanity/components/fileinput/default'
 
 export default class FileInputButton extends React.PureComponent {
@@ -10,14 +10,14 @@ export default class FileInputButton extends React.PureComponent {
   }
   render() {
     return (
-      <Button ripple={false}>
+      <FakeButton>
         <div style={{position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', opacity: 0}}>
           <FileInput {...this.props} style={{height: '100%', width: '100%', display: 'block'}}>
             {this.props.children}
           </FileInput>
         </div>
         {this.props.children}
-      </Button>
+      </FakeButton>
     )
   }
 }
