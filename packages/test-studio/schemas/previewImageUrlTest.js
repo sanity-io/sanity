a3f8e009ae7fa9408e7093e12210eec964c98696
Bjørge Næss <bjoerge@gmail.com>
2018-01-03 14:14:01 +0100

    [test-studio] Refine image test document types (#446)
    * [test-studio] Refine image test document types
    
    * [test-studio] Add hotspot to media preview test

---
+++ packages/test-studio/schemas/previewImageUrlTest.js
@@ -0,0 +1,29 @@
+export default {
+  name: 'previewImageUrlTest',
+  type: 'document',
+  title: 'Preview: Using imageUrl in preview',
+  preview: {
+    select: {
+      title: 'title',
+      imageUrl: 'image.asset.url'
+    },
+  },
+  fields: [
+    {
+      name: 'title',
+      title: 'Title',
+      type: 'string'
+    },
+    {
+      name: 'image',
+      title: 'Image',
+      type: 'image'
+    },
+    {
+      name: 'array',
+      type: 'array',
+      title: 'Array',
+      of: [{type: 'reference', to: {type: 'previewImageUrlTest'}}]
+    }
+  ]
+}
