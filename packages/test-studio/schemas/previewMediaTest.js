805e40eb47a51b5ad8238d29e46bc291734502d0
Bjørge Næss <bjoerge@gmail.com>
2018-01-03 14:14:01 +0100

    [test-studio] Refine image test document types (#446)
    * [test-studio] Refine image test document types
    
    * [test-studio] Add hotspot to media preview test

---
+++ packages/test-studio/schemas/previewMediaTest.js
@@ -0,0 +1,30 @@
+export default {
+  name: 'previewMediaTest',
+  type: 'document',
+  title: 'Preview: Using media in preview',
+  preview: {
+    select: {
+      title: 'title',
+      media: 'image'
+    },
+  },
+  fields: [
+    {
+      name: 'title',
+      title: 'Title',
+      type: 'string'
+    },
+    {
+      name: 'image',
+      title: 'Image',
+      type: 'image',
+      options: {hotspot: true} // tweaking the hotspot should be reflected in the preview
+    },
+    {
+      name: 'array',
+      type: 'array',
+      title: 'Array',
+      of: [{type: 'reference', to: {type: 'previewMediaTest'}}]
+    }
+  ]
+}
