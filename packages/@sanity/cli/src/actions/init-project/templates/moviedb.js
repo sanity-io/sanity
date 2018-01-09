7d0e7e49ef8e30825d317f1a9cab074fb6270097
Espen Hovlandsdal <espen@hovlandsdal.com>
2018-01-09 14:22:38 +0100

    [cli] Show prompt on whether or not to import data for a template

--- packages/@sanity/cli/src/actions/init-project/templates/moviedb.js
+++ packages/@sanity/cli/src/actions/init-project/templates/moviedb.js
@@ -1,7 +1,5 @@
-import path from 'path'
-
-const datasetUrl = 'https://public.sanity.io/moviesdb.ndjson'
-const indent = line => `  ${line}`
+export const importPrompt = 'Import a sampling of sci-fi movies to go with your movie schema?'
+export const datasetUrl = 'https://public.sanity.io/moviesdb.ndjson'
 
 export const dependencies = {
   '@sanity/google-maps-input': '^0.99.0',
@@ -11,18 +9,3 @@ export const generateSanityManifest = base => ({
   ...base,
   plugins: base.plugins.concat(['@sanity/google-maps-input'])
 })
-
-export const getSuccessMessage = (opts, context) => {
-  const {chalk} = context
-  const cwd = process.cwd()
-  const inDir = opts.outputDir === cwd
-  const cdCmd = inDir ? '' : `cd ${path.relative(cwd, opts.outputDir)}`
-
-  return [
-    `${chalk.cyan('Note:')} If you want to import some sample data to your new project, run:`,
-    '',
-    chalk.cyan(cdCmd),
-    chalk.cyan(`sanity dataset import ${datasetUrl} ${opts.dataset}`),
-    ''
-  ].map(indent).join('\n')
-}
