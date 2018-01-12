cb6fd8dc4f237740b7f62e874fe9b82701136cca
Kristoffer J. Sivertsen <kristoffer@bengler.no>
2018-01-12 12:55:03 +0100

    [components] Visual improvements - input components (#488)
    * [components] Aligning right side functions on selects
    
    * [components] Cleaning up style select
    
    * [components] Minor tweaks on dropdown and textinput
    
    * [components] Better buttons. No seondary kind (use simple)
    
    * [components] Focus on functions on searchable select
    
    * [components] Button tweak
    
    * [components] Styling fieldsets (and add hasError prop)
    
    * [base] New brand color
    
    * [components] Button story
    
    * [components] Last button fixes
    
    * [pr] Change requests from bjoerge

--- packages/@sanity/components/src/selects/StyleSelect.js
+++ packages/@sanity/components/src/selects/StyleSelect.js
@@ -6,18 +6,16 @@ import FaAngleDown from 'part:@sanity/base/angle-down-icon'
 import enhanceWithClickOutside from 'react-click-outside'
 import CircleThinIcon from 'part:@sanity/base/circle-thin-icon'
 import CircleCheckIcon from 'part:@sanity/base/circle-check-icon'
+import Escapable from '../utilities/Escapable'
+import {List, Item} from 'part:@sanity/components/lists/default'
 
 class StyleSelect extends React.Component {
   static propTypes = {
-    description: PropTypes.string,
     placeholder: PropTypes.string,
     onChange: PropTypes.func,
     onOpen: PropTypes.func,
-    onFocus: PropTypes.func,
-    onBlur: PropTypes.func,
     onClose: PropTypes.func,
     value: PropTypes.array,
-    error: PropTypes.bool,
     renderItem: PropTypes.func,
     className: PropTypes.string,
     transparent: PropTypes.bool,
@@ -26,65 +24,33 @@ class StyleSelect extends React.Component {
         title: PropTypes.string,
         active: PropTypes.bool
       })
-    ),
+    )
   }
 
   static defaultProps = {
-    placeholder: 'Type to search…',
-    loading: false,
     className: '',
     onChange() {},
-    onBlur() {},
     onOpen() {},
     onClose() {}
   }
 
-  constructor(props, context) {
-    super(props, context)
-
-    this.state = {
-      hasFocus: false,
-      inputValue: this.props.value && this.props.value.title,
-      inputSelected: false,
-      arrowNavigationPosition: 0
-    }
+  state = {
+    showList: false
   }
 
   handleClickOutside = () => {
     this.handleCloseList()
   }
 
-  // componentWillReceiveProps(nextProps) {
-  //   if (nextProps.items != this.props.items) {
-  //     this.setState({
-  //       searchResult: this.props.items,
-  //       showList: true
-  //     })
-  //   }
-  //   if (nextProps.value != this.props.value) {
-  //     this.setState({
-  //       inputValue: nextProps.value.title,
-  //       inputSelected: true,
-  //       value: nextProps.value.title,
-  //       showList: false
-  //     })
-  //   }
-  // }
-
-  handleFocus = event => {
-    this.setState({
-      hasFocus: true,
-      inputSelected: true
-    })
-
-    this.props.onFocus(event)
-  }
-
-  handleBlur = event => {
-    this.props.onBlur(event)
-  }
-
-  handleSelect = item => {
+  handleSelect = event => {
+    const index = event.currentTarget.dataset.index
+    if (!index) {
+      return
+    }
+    const item = this.props.items[index]
+    if (!item) {
+      return
+    }
     this.props.onChange(item)
     this.handleCloseList()
   }
@@ -111,63 +77,20 @@ class StyleSelect extends React.Component {
     }
   }
 
-
-  handleKeyDown = event => {
-    const {items} = this.props
-    const {arrowNavigationPosition} = this.state
-    if (items) {
-      if (event.key == 'ArrowUp' && arrowNavigationPosition > 0) {
-        this.setState({
-          arrowNavigationPosition: arrowNavigationPosition - 1,
-          inputValue: items[arrowNavigationPosition - 1].title,
-          showList: true
-        })
-        return false
-      }
-
-      if (event.key == 'ArrowDown' && arrowNavigationPosition < items.length - 1) {
-        this.setState({
-          arrowNavigationPosition: arrowNavigationPosition + 1,
-          inputValue: items[arrowNavigationPosition + 1].title,
-          showList: true
-        })
-        return false
-      }
-    }
-    return true
-  }
-
-  handleKeyUp = event => {
-    const {items} = this.props
-    const {arrowNavigationPosition} = this.state
-    if (event.key == 'Enter' && arrowNavigationPosition) {
-      this.handleSelect(items[arrowNavigationPosition])
-      this.setState({
-        hasFocus: false
-      })
-      return false
-    }
-    return true
-  }
-
-  componentWillMount() {
-    this._inputId = uniqueId('CustomSelect')
+  handleRootKeyPress = () => {
+    this.handleOpenList()
   }
 
   render() {
-    const {error, value, description, items, className, transparent, placeholder} = this.props
-    const {hasFocus, showList} = this.state
-
+    const {value, transparent, items, className, placeholder, renderItem} = this.props
+    const {showList} = this.state
 
     return (
       <div
-        className={`
-          ${styles.root}
-          ${hasFocus ? styles.focused : ''}
-          ${error ? styles.error : ''}
-          ${transparent ? styles.transparent : ''}
-          ${className || ''}
-        `}
+        className={`${styles.root} ${className || ''} ${transparent ? styles.transparent : ''}`}
+        onBlur={this.handleCloseList}
+        onKeyPress={this.handleRootKeyPress}
+        tabIndex={0}
       >
         <div className={styles.inner} onClick={this.handleInnerClick}>
           <div className={styles.selectContainer}>
@@ -176,39 +99,47 @@ class StyleSelect extends React.Component {
               {value && value.length == 1 && value[0].title}
               {!value && placeholder}
             </span>
-            <div className={styles.icon}>
+            <div className={styles.arrow}>
               <FaAngleDown color="inherit" />
             </div>
           </div>
         </div>
-
-        <div className={`${showList ? styles.listContainer : styles.listContainerHidden}`}>
-          {
-            items.map(item => {
-              const isSemiSelected = value && value.length > 1 && includes(value, item)
-              const isSelected = value && value.length === 1 && value[0] == item
-              const classNames = `
-                ${isSelected ? styles.itemSelected : styles.item}
-                ${isSemiSelected ? styles.itemSemiSelected : ''}
-              `
-              return (
-                <a className={classNames} key={item.key} title={item.title} onClick={() => this.handleSelect(item)}>
-                  <div className={styles.itemIcon}>
-                    {
-                      isSelected && <CircleCheckIcon />
-                    }
-                    {
-                      isSemiSelected && <CircleThinIcon />
-                    }
-                  </div>
-                  <div className={styles.itemContent}>
-                    {this.props.renderItem(item)}
-                  </div>
-                </a>
-              )
-            })
-          }
-        </div>
+        <Escapable onEscape={this.handleCloseList} />
+        {
+          showList && (
+            <List className={styles.list}>
+              {items.map((item, index) => {
+                const isSemiSelected = value && value.length > 1 && includes(value, item)
+                const isSelected = value && value.length === 1 && value[0] == item
+                const classNames = `
+                  ${isSelected ? styles.itemSelected : styles.item}
+                  ${isSemiSelected ? styles.itemSemiSelected : ''}
+                `
+                return (
+                  <Item
+                    key={item.key}
+                    title={item.title}
+                    data-index={index}
+                    onClick={this.handleSelect}
+                    className={classNames}
+                  >
+                    <div className={styles.itemIcon}>
+                      {
+                        isSelected && <CircleCheckIcon />
+                      }
+                      {
+                        isSemiSelected && <CircleThinIcon />
+                      }
+                    </div>
+                    <div className={styles.itemContent}>
+                      {renderItem(item)}
+                    </div>
+                  </Item>
+                )
+              })}
+            </List>
+          )
+        }
       </div>
     )
   }
