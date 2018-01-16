07e91db51d56512873e94d64a298b16c5b29bf62
Kristoffer J. Sivertsen <kristofferjoachim@gmail.com>
2018-01-16 10:39:16 +0100

    [google-maps-input] Refreshing with media preview and fix for dialog

--- packages/test-studio/schemas/geopoint.js
+++ packages/test-studio/schemas/geopoint.js
@@ -1,4 +1,6 @@
+import React from 'react'
 import icon from 'react-icons/lib/md/pin-drop'
+import config from 'config:@sanity/google-maps-input'
 
 export default {
   name: 'geopointTest',
@@ -17,5 +19,23 @@ export default {
       title: 'A geopoint',
       description: 'This is a geopoint field',
     }
-  ]
+  ],
+  preview: {
+    select: {
+      title: 'title',
+      location: 'location'
+    },
+    prepare({title, location}) {
+      const {apiKey} = config
+      return {
+        title: title,
+        subtitle: location && `${Number(location.lat).toPrecision(5)}, ${Number(location.lng).toPrecision(5)}`,
+        media: ({dimensions}) => {
+          if (location && location.lat && location.lng && apiKey) {
+            return <img src={`https://maps.googleapis.com/maps/api/staticmap?zoom=11&center=${location.lat},${location.lng}&size=${dimensions.width}x${dimensions.height}&key=${apiKey}`} alt={title} />
+          }
+        }
+      }
+    }
+  }
 }
