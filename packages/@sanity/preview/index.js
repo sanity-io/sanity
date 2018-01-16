ae895f6c51ab18df4e3d6b8bd3585b4d8c3ae145
Bjørge Næss <bjoerge@gmail.com>
2018-01-16 14:30:12 +0100

    [preview] Expose a default materializePaths function (#492)

--- packages/@sanity/preview/index.js
+++ packages/@sanity/preview/index.js
@@ -1,2 +1,3 @@
 exports.observeForPreview = require('./lib/observeForPreview').default
 exports.observeWithPaths = require('./lib/observeWithPaths').default
+exports.materializePaths = require('./lib/materializePaths').default
---
+++ packages/@sanity/preview/src/materializePaths.js
@@ -0,0 +1,4 @@
+import observeWithPaths from './observeWithPaths'
+import createPathMaterializer from './createPathMaterializer'
+
+export default createPathMaterializer(observeWithPaths)
