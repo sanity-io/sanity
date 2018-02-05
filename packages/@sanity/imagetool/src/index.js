469569c781fc594a03f32540889d5f101dd52383
Bjørge Næss <bjoerge@gmail.com>
2018-02-05 17:56:22 +0700

    [imagetool] Scale down large images before passing to ImageTool (#567)
    * [imagetool] Scale down large images before passing to ImageTool
    
    * [imagetool] Remove canvas element on cWU
    
    * [imagetool] Fix incorrect class name

--- packages/@sanity/imagetool/src/index.js
+++ packages/@sanity/imagetool/src/index.js
@@ -1,13 +1,32 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import ImageLoader from './ImageLoader'
 import ImageTool from './ImageTool'
-import createImageLoadProxy from './createImageLoadProxy'
+import Resize from './Resize'
 
-/* eslint-disable react/no-multi-comp, react/prop-types */
-export default createImageLoadProxy(ImageTool, {
-  loading(props) {
-    return <div>Loading...</div>
-  },
-  error(props) {
-    return <div>Could not load image "{props.src}" {props.error.message}</div>
-  }
-})
+export default function ImageToolWrapper(props) {
+  return (
+    <ImageLoader src={props.src}>
+      {({isLoading, image, error}) => {
+        if (isLoading) {
+          return <div>Loading...</div>
+        }
+        if (error) {
+          return <div>{error.message}</div>
+        }
+        if (image) {
+          return (
+            <Resize image={image} maxHeight={500} maxWidth={1000}>
+              {canvas => <ImageTool image={canvas} {...props} />}
+            </Resize>
+          )
+        }
+        return null
+      }}
+    </ImageLoader>
+  )
+}
+
+ImageToolWrapper.propTypes = {
+  src: PropTypes.string.isRequired
+}
