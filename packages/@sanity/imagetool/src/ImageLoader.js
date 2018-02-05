a6c7f05c574a90327b6bc3a98644b3bd95d947c1
Bjørge Næss <bjoerge@gmail.com>
2018-02-05 15:10:06 +0700

    [imagetool] Scale down large images before passing to ImageTool

---
+++ packages/@sanity/imagetool/src/ImageLoader.js
@@ -0,0 +1,55 @@
+import PropTypes from 'prop-types'
+import React from 'react'
+
+export default class ImageLoader extends React.Component {
+  static propTypes = {
+    src: PropTypes.string.isRequired,
+    children: PropTypes.func.isRequired
+  }
+
+  state = {
+    isLoading: true,
+    image: null,
+    error: null
+  }
+
+  componentWillMount() {
+    this.loadImage(this.props.src)
+  }
+
+  loadImage(src) {
+    const image = new Image()
+    this.setState({
+      image: null,
+      error: null
+    })
+
+    image.onload = () => {
+      this.setState({
+        image: image,
+        error: null,
+        isLoading: false
+      })
+    }
+
+    image.onerror = () => {
+      this.setState({
+        error: new Error(`Could not load image from ${JSON.stringify(this.props.src)}`),
+        isLoading: false
+      })
+    }
+
+    image.src = src
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.src !== this.props.src) {
+      this.loadImage(nextProps.src)
+    }
+  }
+
+  render() {
+    const {error, image, isLoading} = this.state
+    return this.props.children({image, error, isLoading})
+  }
+}
---
+++ packages/@sanity/imagetool/src/Resize.js
@@ -0,0 +1,44 @@
+import PropTypes from 'prop-types'
+import React from 'react'
+
+export default class ImageLoader extends React.Component {
+  static propTypes = {
+    image: PropTypes.instanceOf(HTMLImageElement).isRequired,
+    maxWidth: PropTypes.number.isRequired,
+    maxHeight: PropTypes.number.isRequired,
+    children: PropTypes.func.isRequired
+  }
+
+  getCanvas() {
+    if (!this._canvas) {
+      this._canvas = document.createElement('canvas')
+      document.body.appendChild(this._canvas)
+      this._canvas.style.display = 'none'
+    }
+    return this._canvas
+  }
+
+  resize(image, maxHeight, maxWidth) {
+    const canvas = this.getCanvas()
+    const ratio = image.width / image.height
+    const width = Math.min(image.width, maxWidth)
+    const height = Math.min(image.height, maxHeight)
+
+    const landscape = image.width > image.height
+    const targetWidth = landscape ? width : height * ratio
+    const targetHeight = landscape ? width / ratio : height
+
+    canvas.width = targetWidth
+    canvas.height = targetHeight
+
+    const ctx = canvas.getContext('2d')
+
+    ctx.drawImage(image, 0, 0, image.width, image.height, 0, 0, targetWidth, targetHeight)
+    return canvas
+  }
+
+  render() {
+    const {image, maxHeight, maxWidth, children} = this.props
+    return children(this.resize(image, maxHeight, maxWidth))
+  }
+}
