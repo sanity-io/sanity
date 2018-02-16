8b1492783791d9e8170ba90e5b6ddb9d66f0476e
Espen Hovlandsdal <espen@hovlandsdal.com>
2018-02-16 12:50:30 +0700

    [client] Always reconnect listeners, even when auto-reconnect fails

--- packages/@sanity/client/test/helpers/sseServer.js
+++ packages/@sanity/client/test/helpers/sseServer.js
@@ -9,7 +9,10 @@ const SseChannel = require('sse-channel')
 module.exports = (onRequest, cb) => {
   const server = http.createServer((request, response) => {
     let channel
-    if (request.url.indexOf('/v1/data/listen/') === 0 || request.url.indexOf('/listen/beerns?query=') === 0) {
+    if (
+      request.url.indexOf('/v1/data/listen/') === 0 ||
+      request.url.indexOf('/listen/beerns?query=') === 0
+    ) {
       channel = new SseChannel({jsonEncode: true})
       channel.addClient(request, response)
     }
