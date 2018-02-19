8c92b3d0254de3e2081e7bcb2f624ea0e3e77faf
Kristoffer J. Sivertsen <kristofferjoachim@gmail.com>
2018-02-19 15:36:46 +0700

    [form-builder] Not check slug agains unpublished slugs

--- packages/@sanity/form-builder/src/sanity/inputs/Slug.js
+++ packages/@sanity/form-builder/src/sanity/inputs/Slug.js
@@ -15,20 +15,19 @@ export function sanitySlugify(type, slugSourceText, finalize = false) {
 
 export function validateSlug(type, slug, myDocId) {
   let query
+
   if (myDocId) {
     query = `*[${type.name}.current == $slug && _id != $id]`
   } else {
     query = `*[${type.name}.current == $slug`
   }
-  return client.fetch(query, {slug: slug, id: myDocId})
-    .then(results => {
-      if (results[0]) {
-        const foundDocId = results[0]._id
-        return `There is already a document (${foundDocId}) `
-          + `in the dataset with the slug '${slug}'.`
-      }
-      return null
-    })
+  return client.fetch(query, {slug: slug, id: myDocId}).then(results => {
+    if (results[0] && results[0]._id && !results[0]._id.startsWith('drafts.')) {
+      const foundDocId = results[0]._id
+      return `There is already a document (${foundDocId}) in the dataset with the slug '${slug}'.`
+    }
+    return null
+  })
 }
 
 export default SlugInput.create({validate: validateSlug, slugify: sanitySlugify})
