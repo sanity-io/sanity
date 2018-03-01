d803394c4caa32f0095a37cb1a905d3380835092
Bjørge Næss <bjoerge@gmail.com>
2018-03-01 12:50:40 +0100

    [eslint-config-sanity] Import eslint config package (#625)

---
+++ packages/eslint-config-sanity/README.md
@@ -0,0 +1,7 @@
+# eslint-config-sanity
+
+Shared eslint rules for Sanity
+
+# Usage
+
+`npm i -D eslint-config-sanity`
---
+++ packages/eslint-config-sanity/bin/diff.js
@@ -0,0 +1,49 @@
+/* eslint-disable no-console */
+const difference = require('lodash.difference')
+const latest = Object.keys(require('eslint/conf/eslint-all').rules)
+const base = Object.keys(require('..').rules)
+const latestReactRules = Object.keys(require('eslint-plugin-react').rules).map(ruleName => {
+  return `react/${ruleName}`
+})
+const existingReactRules = Object.keys(require('../react').rules)
+
+const replacements = require('eslint/conf/replacements.json').rules
+
+const addedRules = difference(latest, base)
+let removedRules = difference(base, latest)
+
+const replacedRules = removedRules.filter(removed => {
+  return removed in replacements
+})
+
+removedRules = difference(removedRules, replacedRules)
+
+const replacedRuleMapping = replacedRules.map(removed => {
+  return {from: removed, to: replacements[removed]}
+})
+
+function printRules(rules) {
+  if (!rules.length) {
+    return '  None'
+  }
+  return `  ${rules.join('\n  ')}`
+}
+
+console.log('New rules: \n%s', printRules(addedRules))
+console.log('Removed rules: \n%s', printRules(removedRules))
+console.log(
+  'Replaced rules: \n%s',
+  printRules(
+    replacedRuleMapping.map(repl => {
+      return `${repl.from} => ${repl.to}`
+    })
+  )
+)
+
+console.log()
+
+const addedReactRules = difference(latestReactRules, existingReactRules)
+const removeReactRules = difference(existingReactRules, latestReactRules)
+console.log('--- eslint-plugin-react ---')
+console.log('New rules: \n%s', printRules(addedReactRules))
+console.log('Removed rules: \n%s', printRules(removeReactRules))
