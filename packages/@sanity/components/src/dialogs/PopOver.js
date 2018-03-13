6abdee874a83e5377c7aa81a4d734ffa1879632f
Kristoffer J. Sivertsen <kristofferjoachim@gmail.com>
2018-03-13 13:04:30 +0100

    [components] Cleanup popover nesting. Fixes react15 breakage

--- packages/@sanity/components/src/dialogs/PopOver.js
+++ packages/@sanity/components/src/dialogs/PopOver.js
@@ -9,8 +9,7 @@ import CaptureOutsideClicks from '../utilities/CaptureOutsideClicks'
 import Escapable from '../utilities/Escapable'
 import {Manager, Target, Popper, Arrow} from 'react-popper'
 
-export default class EditItemPopOver extends React.Component {
-
+export default class EditItemPopOver extends React.PureComponent {
   static propTypes = {
     title: PropTypes.string,
     children: PropTypes.node.isRequired,
@@ -19,11 +18,13 @@ export default class EditItemPopOver extends React.Component {
     onAction: PropTypes.func,
     modifiers: PropTypes.object,
     color: PropTypes.oneOf(['default', 'danger']),
-    actions: PropTypes.arrayOf(PropTypes.shape({
-      kind: PropTypes.string,
-      title: PropTypes.string,
-      key: PropTypes.string
-    }))
+    actions: PropTypes.arrayOf(
+      PropTypes.shape({
+        kind: PropTypes.string,
+        title: PropTypes.string,
+        key: PropTypes.string
+      })
+    )
   }
 
   static defaultProps = {
@@ -41,96 +42,74 @@ export default class EditItemPopOver extends React.Component {
     }
   }
 
-  setTargetRef = element => {
-    this.target = element
-    return element
-  }
-
-  setPopperRef = element => {
-    this.popper = element
-    return element
+  renderPopper(isActive) {
+    const {title, color, children, actions, onClose, modifiers} = this.props
+    return (
+      <Popper
+        className={`${styles.popper} ${styles[`color_${color}`]}`}
+        placement="auto"
+        modifiers={modifiers}
+      >
+        <Arrow className={title ? styles.filledArrow : styles.arrow} />
+        <Escapable onEscape={event => (isActive || event.shiftKey) && onClose()} />
+        <CaptureOutsideClicks onClickOutside={isActive ? onClose : undefined}>
+          <div className={styles.popover}>
+            {onClose && (
+              <button
+                className={title ? styles.closeInverted : styles.close}
+                type="button"
+                onClick={onClose}
+              >
+                <CloseIcon />
+              </button>
+            )}
+            {title && <h3 className={styles.title}>{title}</h3>}
+            <div className={actions.length > 0 ? styles.contentWithActions : styles.content}>
+              {children}
+            </div>
+            {actions.length > 0 && (
+              <div className={styles.footer}>
+                <div className={styles.actions}>
+                  {actions.map((action, i) => {
+                    return (
+                      <Button
+                        key={i}
+                        onClick={() => this.props.onAction(action)}
+                        data-action-index={i}
+                        color={action.color}
+                        disabled={action.disabled}
+                        kind={action.kind}
+                        autoFocus={action.autoFocus}
+                        className={action.secondary ? styles.actionSecondary : ''}
+                      >
+                        {action.title}
+                      </Button>
+                    )
+                  })}
+                </div>
+              </div>
+            )}
+          </div>
+        </CaptureOutsideClicks>
+      </Popper>
+    )
   }
 
   render() {
-    const {
-      title,
-      color,
-      children,
-      actions,
-      onClose,
-      modifiers,
-      onClickOutside
-    } = this.props
-
     return (
-      <Stacked>
-        {isActive => (
-          <div>
-            <Escapable onEscape={event => ((isActive || event.shiftKey) && onClose())} />
-            <Manager>
-              <Target className={styles.target} innerRef={this.setTargetRef} />
-              <Portal>
+      <Manager>
+        <Target className={styles.target} />
+        <Portal>
+          <Stacked>
+            {isActive => (
+              <div>
                 <div className={styles.overlay} />
-                <Popper
-                  innerRef={this.setPopperRef}
-                  className={`${styles.popper} ${styles[`color_${color}`]}`}
-                  placement="auto"
-                  modifiers={modifiers}
-                >
-                  <Arrow className={title ? styles.filledArrow : styles.arrow} />
-                  <CaptureOutsideClicks onClickOutside={isActive ? onClickOutside || onClose : null}>
-                    <div className={styles.popover}>
-                      {
-                        onClose && (
-                          <button className={title ? styles.closeInverted : styles.close} type="button" onClick={onClose}>
-                            <CloseIcon />
-                          </button>
-                        )
-                      }
-                      {
-                        title && (
-                          <h3 className={styles.title}>
-                            {title}
-                          </h3>
-                        )
-                      }
-                      <div className={styles.content}>
-                        {children}
-                      </div>
-                      {
-                        actions.length > 0 && (
-                          <div className={styles.footer}>
-                            <div className={styles.actions}>
-                              {
-                                actions.map((action, i) => {
-                                  return (
-                                    <Button
-                                      key={i}
-                                      onClick={() => this.props.onAction(action)}
-                                      data-action-index={i}
-                                      color={action.color}
-                                      disabled={action.disabled}
-                                      kind={action.kind}
-                                      autoFocus={action.autoFocus}
-                                      className={action.secondary ? styles.actionSecondary : ''}
-                                    >
-                                      {action.title}
-                                    </Button>
-                                  )
-                                })
-                              }
-                            </div>
-                          </div>
-                        )
-                      }
-                    </div>
-                  </CaptureOutsideClicks>
-                </Popper>
-              </Portal>
-            </Manager>
-          </div>
-        )}
-      </Stacked>
+                {this.renderPopper(isActive)}
+              </div>
+            )}
+          </Stacked>
+        </Portal>
+      </Manager>
     )
   }
 }
