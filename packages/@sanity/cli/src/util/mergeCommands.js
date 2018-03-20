db50eb557e0d614f4c7d0b27ea64788655b22f11
Espen Hovlandsdal <espen@hovlandsdal.com>
2018-03-20 15:05:29 +0100

    [cli] Allow @sanity/core to declare CLI version requirement

--- packages/@sanity/cli/src/util/mergeCommands.js
+++ packages/@sanity/cli/src/util/mergeCommands.js
@@ -1,12 +1,32 @@
 import {find} from 'lodash'
+import semver from 'semver'
+import chalk from 'chalk'
+import {version} from '../../package.json'
 import dynamicRequire from './dynamicRequire'
+import getUpgradeCommand from './getUpgradeCommand'
 
-export default (baseCommands, corePath) => {
+export default (baseCommands, corePath, options = {}) => {
   if (!corePath) {
     return baseCommands
   }
 
+  const {cwd, workDir} = options
   const core = dynamicRequire(corePath)
+
+  if (core.requiredCliVersionRange && !semver.satisfies(version, core.requiredCliVersionRange)) {
+    const upgradeCmd = chalk.yellow(getUpgradeCommand({cwd, workDir}))
+    /* eslint-disable no-console, no-process-exit */
+    console.error(
+      `The version of @sanity/core installed in this project requires @sanity/cli @ ${chalk.green(
+        core.requiredCliVersionRange
+      )}. Currently installed version is ${chalk.red(
+        version
+      )}.\n\nPlease upgrade by running:\n\n  ${upgradeCmd}\n\n`
+    )
+    process.exit(1)
+    /* eslint-enable no-console, no-process-exit */
+  }
+
   const merged = baseCommands.concat(core.commands).map(addDefaultGroup)
 
   // Remove duplicate commands when within the same group,
--- packages/@sanity/core/src/index.js
+++ packages/@sanity/core/src/index.js
@@ -1,6 +1,8 @@
 import commands from './commands'
+import requiredCliVersionRange from './requiredCliVersionRange'
 
 // Must be module.exports (commonjs)
 module.exports = {
-  commands
+  commands,
+  requiredCliVersionRange
 }
