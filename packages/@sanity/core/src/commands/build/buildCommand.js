561fca917dc14d9d4a2dc804b0b7df6a0565b054
dan-dr <danrosenshain@gmail.com>
2018-03-26 19:14:35 +0300

    [core] Add ability to build in unattended mode (no prompts) (#700)

--- packages/@sanity/core/src/commands/build/buildCommand.js
+++ packages/@sanity/core/src/commands/build/buildCommand.js
@@ -4,6 +4,7 @@ const helpText = `
 Options
   --source-maps Enable source maps for built bundles (increases size of bundle)
   --no-minify Skip minifying built Javascript (speeds up build, increases size of bundle)
+  -y, --yes Use unattended mode, accepting defaults and using only flags for choices
 
 Examples
   sanity build
