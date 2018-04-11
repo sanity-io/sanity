71651bea4e057f9df1bb926bed5a06ed004f8747
Kristoffer J. Sivertsen <kristofferjoachim@gmail.com>
2018-04-11 13:05:27 +0200

    [form-builder] Responsive block toolbar

--- packages/@sanity/form-builder/src/inputs/BlockEditor/Toolbar/Toolbar.js
+++ packages/@sanity/form-builder/src/inputs/BlockEditor/Toolbar/Toolbar.js
@@ -11,6 +11,10 @@ import CloseIcon from 'part:@sanity/base/close-icon'
 import DecoratorButtons from './DecoratorButtons'
 import FullscreenIcon from 'part:@sanity/base/fullscreen-icon'
 import ListItemButtons from './ListItemButtons'
+import {ContainerQuery} from 'react-container-query'
+import classnames from 'classnames'
+import ArrowIcon from 'part:@sanity/base/angle-down-icon'
+import enhanceWithClickOutside from 'react-click-outside'
 
 import styles from './styles/Toolbar.css'
 
@@ -18,13 +22,42 @@ type Props = {
   blockContentFeatures: BlockContentFeatures,
   editorValue: SlateValue,
   focusPath: [],
+  style: {},
   fullscreen: boolean,
   onChange: (change: SlateChange) => void,
   onFocus: (nextPath: []) => void,
   onToggleFullScreen: void => void
 }
 
-export default class Toolbar extends React.PureComponent<Props> {
+const query = {
+  [styles.largeContainer]: {
+    minWidth: 600
+  }
+}
+
+class Toolbar extends React.PureComponent<Props> {
+  state = {
+    expanded: false
+  }
+
+  handleExpand = () => {
+    this.setState({
+      expanded: true
+    })
+  }
+
+  handleContract = () => {
+    this.setState({
+      expanded: false
+    })
+  }
+
+  handleClickOutside = () => {
+    this.setState({
+      expanded: false
+    })
+  }
+
   render() {
     const {
       blockContentFeatures,
@@ -33,75 +66,91 @@ export default class Toolbar extends React.PureComponent<Props> {
       focusPath,
       onChange,
       onFocus,
-      onToggleFullScreen
+      onToggleFullScreen,
+      style
     } = this.props
-    const className = `${styles.root}${fullscreen ? ` ${styles.fullscreen}` : ''}`
+
+    const {expanded} = this.state
 
     return (
-      <div className={className}>
-        <div className={styles.blockFormatContainer}>
-          <BlockStyleSelect
-            editorValue={editorValue}
-            onChange={onChange}
-            blockContentFeatures={blockContentFeatures}
-          />
-        </div>
-
-        <div className={styles.canBeMinimized}>
-          <div className={styles.formatButtonsContainer}>
-            {blockContentFeatures.decorators.length > 0 && (
-              <div className={styles.decoratorButtonsContainer}>
-                <DecoratorButtons
-                  editorValue={editorValue}
-                  onChange={onChange}
-                  blockContentFeatures={blockContentFeatures}
-                />
-              </div>
-            )}
-
-            {blockContentFeatures.lists.length > 0 && (
-              <div className={styles.decoratorButtonsContainer}>
-                <ListItemButtons
-                  editorValue={editorValue}
-                  onChange={onChange}
-                  blockContentFeatures={blockContentFeatures}
-                />
-              </div>
-            )}
-          </div>
-
-          {blockContentFeatures.annotations.length > 0 && (
-            <div className={styles.annotationButtonsContainer}>
-              <AnnotationButtons
+      <ContainerQuery query={query}>
+        {params => (
+          <div
+            className={`
+              ${styles.root}
+              ${classnames(params)}
+              ${fullscreen ? ` ${styles.fullscreen}` : ''}
+            `}
+            style={style}
+          >
+            <div className={styles.blockFormatContainer} onClick={this.handleContract}>
+              <BlockStyleSelect
                 editorValue={editorValue}
                 onChange={onChange}
-                onFocus={onFocus}
-                focusPath={focusPath}
                 blockContentFeatures={blockContentFeatures}
               />
             </div>
-          )}
-        </div>
+            <Button className={styles.expandButton} onClick={this.handleExpand} kind="simple">
+              More&nbsp;
+              <span className={styles.arrow}>
+                <ArrowIcon color="inherit" />
+              </span>
+            </Button>
+            <div className={`${styles.compactable} ${expanded ? styles.expanded : ''}`} onClick={this.handleContract}>
+              {blockContentFeatures.decorators.length > 0 && (
+                <div className={styles.decoratorButtonsContainer}>
+                  <DecoratorButtons
+                    editorValue={editorValue}
+                    onChange={onChange}
+                    blockContentFeatures={blockContentFeatures}
+                  />
+                </div>
+              )}
+              {blockContentFeatures.lists.length > 0 && (
+                <div className={styles.decoratorButtonsContainer}>
+                  <ListItemButtons
+                    editorValue={editorValue}
+                    onChange={onChange}
+                    blockContentFeatures={blockContentFeatures}
+                  />
+                </div>
+              )}
+              {blockContentFeatures.annotations.length > 0 && (
+                <div className={styles.annotationButtonsContainer}>
+                  <AnnotationButtons
+                    editorValue={editorValue}
+                    onChange={onChange}
+                    onFocus={onFocus}
+                    focusPath={focusPath}
+                    blockContentFeatures={blockContentFeatures}
+                  />
+                </div>
+              )}
 
-        {blockContentFeatures.blockObjectTypes.length > 0 && (
-          <div className={styles.insertContainer}>
-            <BlockObjectsMenu
-              types={blockContentFeatures.blockObjectTypes}
-              editorValue={editorValue}
-              onChange={onChange}
-              onFocus={onFocus}
-            />
+              {blockContentFeatures.blockObjectTypes.length > 0 && (
+                <div className={styles.insertContainer}>
+                  <BlockObjectsMenu
+                    types={blockContentFeatures.blockObjectTypes}
+                    editorValue={editorValue}
+                    onChange={onChange}
+                    onFocus={onFocus}
+                  />
+                </div>
+              )}
+            </div>
+
+            <div className={styles.fullscreenButtonContainer} onClick={this.handleContract}>
+              <Button
+                kind="simple"
+                onClick={onToggleFullScreen}
+                icon={fullscreen ? CloseIcon : FullscreenIcon}
+              />
+            </div>
           </div>
         )}
-
-        <div className={styles.fullscreenButtonContainer}>
-          <Button
-            kind="simple"
-            onClick={onToggleFullScreen}
-            icon={fullscreen ? CloseIcon : FullscreenIcon}
-          />
-        </div>
-      </div>
+      </ContainerQuery>
     )
   }
 }
+
+export default enhanceWithClickOutside(Toolbar)
