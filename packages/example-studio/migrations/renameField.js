8f576a4c669632a539cc877e03a52e1f35e176db
Bjørge Næss <bjoerge@gmail.com>
2018-04-20 15:28:18 +0200

    [example-studio] Keep on migrating until no more pending documents + explainer

--- packages/example-studio/migrations/renameField.js
+++ packages/example-studio/migrations/renameField.js
@@ -1,43 +1,66 @@
+/* eslint-disable no-console */
 import baseClient from 'part:@sanity/base/client'
 
-// Remove when we get support for sanity exec --with-user-token
+// Run this script with: `sanity exec migrations/renameField.js`
+//
+// This example shows how you may write a migration script that renames a field (name => fullname)
+// on a specific document type (author).
+// This will migrate documents in batches of 100 and continue patching until no more documents are
+// returned from the query.
+//
+// This migration script can safely be run even if others are currently editing content.
+// If a document has been edited in between the document is fetched and migrated, the migration will
+// fail, but can safely be re-run until it passes.
+
+// A few things to note:
+// - This script will exit if any of the mutations fail due to a revision mismatch (which means the
+//   document was edited between fetch => update)
+// - The query must eventually return an empty set, or else this script will continue indefinitely
+
+// @todo: Remove when we release support for sanity exec --with-user-token
+// See (https://github.com/sanity-io/sanity/pull/746)
 import ConfigStore from 'configstore'
 const authToken = new ConfigStore('sanity', {}, {globalConfigPath: true}).get('authToken')
 const client = baseClient.config({token: authToken})
 // ---
 
-// Fetching documents that matches the precondition for the migration
+// Fetching documents that matches the precondition for the migration.
+// NOTE: This query should eventually return an empty set of documents to mark the migration
+// as complete
 const fetchDocuments = () =>
-  client.fetch(`*[_type == 'author' && defined(fullname)] {_id, _rev, fullname}`)
+  client.fetch(`*[_type == 'author' && defined(name)][0...100] {_id, _rev, name}`)
 
 const buildPatches = docs =>
   docs.map(doc => ({
     id: doc._id,
     patch: {
-      set: {name: doc.fullname},
-      unset: doc.name,
-      // this will cause the migration to fail if the document has been
+      set: {fullname: doc.name},
+      unset: ['name'],
+      // this will cause the migration to fail if any of the documents has been
       // modified since it was fetched.
       ifRevisionID: doc._rev
     }
   }))
-
-const printSummary = patches => {
-  console.log(
-    `Patching:\n %s`,
-    patches.map(patch => `${patch.id} => ${JSON.stringify(patch.patch)}`).join('\n')
-  )
-  return patches
-}
 
 const createTransaction = patches =>
   patches.reduce((tx, patch) => tx.patch(patch.id, patch.patch), client.transaction())
 
 const commitTransaction = tx => tx.commit()
 
-fetchDocuments()
-  .then(buildPatches)
-  .then(printSummary)
-  .then(createTransaction)
-  .then(commitTransaction)
-  .then(console.log, console.error)
+const migrateNextBatch = async () => {
+  const documents = await fetchDocuments()
+  const patches = buildPatches(documents)
+  if (patches.length === 0) {
+    console.log('No more documents to migrate!')
+    return null
+  }
+  console.log(
+    `Migrating batch:\n %s`,
+    patches.map(patch => `${patch.id} => ${JSON.stringify(patch.patch)}`).join('\n')
+  )
+  const transaction = createTransaction(patches)
+  await commitTransaction(transaction)
+  return migrateNextBatch()
+}
+
+migrateNextBatch().catch(console.error)
