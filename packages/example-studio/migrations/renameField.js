8f35178d73b7b2d62fb19ccb02a1cc04179a246c
Bjørge Næss <bjoerge@gmail.com>
2018-04-20 15:58:45 +0200

    [example-studio] Add an example of how to rename a document field (#745)
    * [example-studio] Add an example of how to rename a document field
    
    * [example-studio] Keep on migrating until no more pending documents + explainer
    
    * [example-studio] Minor copy edits explainer

---
+++ packages/example-studio/migrations/renameField.js
@@ -0,0 +1,62 @@
+/* eslint-disable no-console */
+import client from 'part:@sanity/base/client'
+
+// Run this script with: `sanity exec --with-user-token migrations/renameField.js`
+//
+// This example shows how you may write a migration script that renames a field (name => fullname)
+// on a specific document type (author).
+// This will migrate documents in batches of 100 and continue patching until no more documents are
+// returned from the query.
+//
+// This script can safely be run, even if documents are being concurrently modified by others.
+// If a document gets modified in the time between fetch => submit patch, this script will fail,
+// but can safely be re-run multiple times until it eventually runs out of documents to migrate.
+
+// A few things to note:
+// - This script will exit if any of the mutations fail due to a revision mismatch (which means the
+//   document was edited between fetch => update)
+// - The query must eventually return an empty set, or else this script will continue indefinitely
+
+// Fetching documents that matches the precondition for the migration.
+// NOTE: This query should eventually return an empty set of documents to mark the migration
+// as complete
+const fetchDocuments = () =>
+  client.fetch(`*[_type == 'author' && defined(name)][0...100] {_id, _rev, name}`)
+
+const buildPatches = docs =>
+  docs.map(doc => ({
+    id: doc._id,
+    patch: {
+      set: {fullname: doc.name},
+      unset: ['name'],
+      // this will cause the migration to fail if any of the documents has been
+      // modified since it was fetched.
+      ifRevisionID: doc._rev
+    }
+  }))
+
+const createTransaction = patches =>
+  patches.reduce((tx, patch) => tx.patch(patch.id, patch.patch), client.transaction())
+
+const commitTransaction = tx => tx.commit()
+
+const migrateNextBatch = async () => {
+  const documents = await fetchDocuments()
+  const patches = buildPatches(documents)
+  if (patches.length === 0) {
+    console.log('No more documents to migrate!')
+    return null
+  }
+  console.log(
+    `Migrating batch:\n %s`,
+    patches.map(patch => `${patch.id} => ${JSON.stringify(patch.patch)}`).join('\n')
+  )
+  const transaction = createTransaction(patches)
+  await commitTransaction(transaction)
+  return migrateNextBatch()
+}
+
+migrateNextBatch().catch(err => {
+  console.error(err)
+  process.exit(1)
+})
