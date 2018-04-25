0f70e0da9465efe1a40bf85b64596e787a182dad
Kristoffer J. Sivertsen <kristofferjoachim@gmail.com>
2018-04-25 15:36:28 +0200

    [cli] Adding new data and adding option

--- packages/@sanity/cli/src/actions/init-project/templates/ecommerce.js
+++ packages/@sanity/cli/src/actions/init-project/templates/ecommerce.js
@@ -1,9 +1,7 @@
 export const importPrompt = 'Import a sampling of products to start your e-commerce project?'
-export const datasetUrl = 'https://public.sanity.io/ecommerce-2018-04-24.tar.gz'
+export const datasetUrl = 'https://public.sanity.io/ecommerce-2018-04-25.tar.gz'
 
 export const dependencies = {
-  canvas: '^1.6.10',
-  jsbarcode: '^3.9.0',
   'react-barcode': '^1.3.2'
 }
 
