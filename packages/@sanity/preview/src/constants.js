04af3e2037cfe2edc2ff54cfb2991e737a7db8b8
Bjørge Næss <bjoerge@gmail.com>
2018-04-26 16:05:22 +0200

    [preview] Use symbol instead of empty object

--- packages/@sanity/preview/src/constants.js
+++ packages/@sanity/preview/src/constants.js
@@ -1,4 +1,4 @@
 export const INCLUDE_FIELDS_QUERY = ['_id', '_rev', '_type']
 export const INCLUDE_FIELDS = [...INCLUDE_FIELDS_QUERY, '_key']
 
-export const INVALID_PREVIEW_CONFIG = {}
+export const INVALID_PREVIEW_CONFIG = Symbol('invalid preview config')
