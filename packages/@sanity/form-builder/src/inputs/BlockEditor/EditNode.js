2f89d404e486e960227620520871037909a34730
Per-Kristian Nordnes <per.kristian.nordnes@gmail.com>
2018-04-26 14:50:19 +0200

    [form-bulder] Block editor: ensure that annotations are dereferenced from spans when unset from the block's markDefs

--- packages/@sanity/form-builder/src/inputs/BlockEditor/EditNode.js
+++ packages/@sanity/form-builder/src/inputs/BlockEditor/EditNode.js
@@ -1,5 +1,5 @@
 // @flow
-import type {Block, SlateChange, Marker} from './typeDefs'
+import type {Block, Marker} from './typeDefs'
 
 import React from 'react'
 import {get} from 'lodash'
@@ -10,27 +10,48 @@ import Popover from 'part:@sanity/components/dialogs/popover'
 import EditItemFold from 'part:@sanity/components/edititem/fold'
 
 import {FormBuilderInput} from '../../FormBuilderInput'
+import {set} from '../../PatchEvent'
 
 import styles from './styles/EditNode.css'
 
 type Props = {
   focusPath: [],
-  onChange: (change: SlateChange) => void,
-  onFocus: (nextPath: []) => void,
-  path: [],
   markers: Marker[],
+  nodeValue: Block,
+  onFocus: (nextPath: []) => void,
+  onPatch: (event: PatchEvent) => void,
+  path: [],
   type: Type,
   value: Block[]
 }
 
 export default class EditNode extends React.Component<Props> {
   handleChange = patchEvent => {
-    const {onChange, path} = this.props
+    const {onPatch, path, value} = this.props
     let _patchEvent = patchEvent
     path.reverse().forEach(segment => {
       _patchEvent = _patchEvent.prefixAll(segment)
     })
-    onChange(_patchEvent)
+    // Intercept patches that unsets markDefs.
+    // The node using the markDef must have that mark removed,
+    // so create patches that rewrite that block without the mark
+    const unsetMarkDefPatches = _patchEvent.patches.filter(
+      patch => patch.path[1] === 'markDefs' && patch.type === 'unset'
+    )
+    if (unsetMarkDefPatches.length) {
+      unsetMarkDefPatches.forEach(patch => {
+        const block = value.find(blk => blk._key === patch.path[0]._key)
+        const _block = {...block}
+        const markKey = patch.path.slice(-1)[0]._key
+        _block.children.forEach(child => {
+          if (child._type === 'span' && child.marks.includes(markKey)) {
+            child.marks = child.marks.filter(mark => mark !== markKey)
+          }
+        })
+        _patchEvent.patches.push(set(_block, [{_key: _block._key}]))
+      })
+    }
+    onPatch(_patchEvent)
   }
 
   handleClose = () => {
@@ -39,8 +60,8 @@ export default class EditNode extends React.Component<Props> {
   }
 
   render() {
-    const {value, type, onFocus, focusPath, path, markers} = this.props
-    if (!value) {
+    const {nodeValue, type, onFocus, focusPath, path, markers} = this.props
+    if (!nodeValue) {
       return <div>No value???</div>
     }
     const editModalLayout = get(type.options, 'editModal')
@@ -50,7 +71,7 @@ export default class EditNode extends React.Component<Props> {
         <FormBuilderInput
           type={type}
           level={1}
-          value={value}
+          value={nodeValue}
           onChange={this.handleChange}
           onFocus={onFocus}
           focusPath={focusPath}
