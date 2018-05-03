cda8a87cb2603bf900fcdd8499902d0acb94548b
Kristoffer J. Sivertsen <kristofferjoachim@gmail.com>
2018-05-03 17:01:09 +0200

    [components] Fixing styleselect with react-popper

--- packages/@sanity/components/src/selects/StyleSelect.js
+++ packages/@sanity/components/src/selects/StyleSelect.js
@@ -1,13 +1,15 @@
 import PropTypes from 'prop-types'
 import React from 'react'
 import styles from 'part:@sanity/components/selects/style-style'
-import {uniqueId, includes} from 'lodash'
+import {includes} from 'lodash'
 import FaAngleDown from 'part:@sanity/base/angle-down-icon'
 import enhanceWithClickOutside from 'react-click-outside'
 import CircleThinIcon from 'part:@sanity/base/circle-thin-icon'
 import CircleCheckIcon from 'part:@sanity/base/circle-check-icon'
 import Escapable from '../utilities/Escapable'
 import {List, Item} from 'part:@sanity/components/lists/default'
+import {Manager, Target, Popper} from 'react-popper'
+import {Portal} from '../utilities/Portal'
 
 class StyleSelect extends React.Component {
   static propTypes = {
@@ -38,8 +40,10 @@ class StyleSelect extends React.Component {
     showList: false
   }
 
-  handleClickOutside = () => {
-    this.handleCloseList()
+  handleClickOutside = event => {
+    if (this._popperElement && !this._popperElement.contains(event.target)) {
+      this.handleCloseList()
+    }
   }
 
   handleSelect = event => {
@@ -81,58 +85,72 @@ class StyleSelect extends React.Component {
     this.handleOpenList()
   }
 
+  setPopperElement = element => {
+    this._popperElement = element
+  }
+
   render() {
     const {value, transparent, items, className, placeholder, renderItem} = this.props
     const {showList} = this.state
 
     return (
-      <div
-        className={`${styles.root} ${className || ''} ${transparent ? styles.transparent : ''}`}
-        onBlur={this.handleCloseList}
-        onKeyPress={this.handleRootKeyPress}
-        tabIndex={0}
-      >
-        <div className={styles.inner} onClick={this.handleInnerClick}>
-          <div className={styles.selectContainer}>
-            <span className={styles.text}>
-              {value && value.length > 1 && 'Multiple'}
-              {value && value.length == 1 && value[0].title}
-              {!value && placeholder}
-            </span>
-            <div className={styles.arrow}>
-              <FaAngleDown color="inherit" />
+      <Manager>
+        <div
+          className={`${styles.root} ${className || ''} ${transparent ? styles.transparent : ''}`}
+          // onBlur={this.handleCloseList}
+          onKeyPress={this.handleRootKeyPress}
+          tabIndex={0}
+        >
+          <Target>
+            <div className={styles.inner} onClick={this.handleInnerClick}>
+              <div className={styles.selectContainer}>
+                <span className={styles.text}>
+                  {value && value.length > 1 && 'Multiple'}
+                  {value && value.length == 1 && value[0].title}
+                  {!value && placeholder}
+                </span>
+                <div className={styles.arrow}>
+                  <FaAngleDown color="inherit" />
+                </div>
+              </div>
             </div>
-          </div>
+          </Target>
+          <Portal className={styles.portal}>
+            <Escapable onEscape={this.handleCloseList} />
+            {showList && (
+              <Popper placement="bottom-start">
+                <div ref={this.setPopperElement}>
+                  <List className={styles.list}>
+                    {items.map((item, index) => {
+                      const isSemiSelected = value && value.length > 1 && includes(value, item)
+                      const isSelected = value && value.length === 1 && value[0] == item
+                      const classNames = `
+                          ${isSelected ? styles.itemSelected : styles.item}
+                          ${isSemiSelected ? styles.itemSemiSelected : ''}
+                        `
+                      return (
+                        <div
+                          key={item.key}
+                          title={item.title}
+                          data-index={index}
+                          onClick={this.handleSelect}
+                          className={classNames}
+                        >
+                          <div className={styles.itemIcon}>
+                            {isSelected && <CircleCheckIcon />}
+                            {isSemiSelected && <CircleThinIcon />}
+                          </div>
+                          <div className={styles.itemContent}>{renderItem(item)}</div>
+                        </div>
+                      )
+                    })}
+                  </List>
+                </div>
+              </Popper>
+            )}
+          </Portal>
         </div>
-        <Escapable onEscape={this.handleCloseList} />
-        {showList && (
-          <List className={styles.list}>
-            {items.map((item, index) => {
-              const isSemiSelected = value && value.length > 1 && includes(value, item)
-              const isSelected = value && value.length === 1 && value[0] == item
-              const classNames = `
-                  ${isSelected ? styles.itemSelected : styles.item}
-                  ${isSemiSelected ? styles.itemSemiSelected : ''}
-                `
-              return (
-                <Item
-                  key={item.key}
-                  title={item.title}
-                  data-index={index}
-                  onClick={this.handleSelect}
-                  className={classNames}
-                >
-                  <div className={styles.itemIcon}>
-                    {isSelected && <CircleCheckIcon />}
-                    {isSemiSelected && <CircleThinIcon />}
-                  </div>
-                  <div className={styles.itemContent}>{renderItem(item)}</div>
-                </Item>
-              )
-            })}
-          </List>
-        )}
-      </div>
+      </Manager>
     )
   }
 }
