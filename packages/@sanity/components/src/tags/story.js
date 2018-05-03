fc19a5c57d4ee30543a373ac4783f55b121a7b99
Kristoffer J. Sivertsen <kristofferjoachim@gmail.com>
2018-05-03 15:30:14 +0200

    [components] Make tags textfield display readOnly correct

--- packages/@sanity/components/src/tags/story.js
+++ packages/@sanity/components/src/tags/story.js
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types'
 import React from 'react'
 import {storiesOf, action} from 'part:@sanity/storybook'
 import TagsTextField from 'part:@sanity/components/tags/textfield'
-import {withKnobs, array, text} from 'part:@sanity/storybook/addons/knobs'
+import {withKnobs, array, text, boolean} from 'part:@sanity/storybook/addons/knobs'
 import Sanity from 'part:@sanity/storybook/addons/sanity'
 
 class DefaultTextFieldTagsImplementation extends React.Component {
@@ -43,6 +43,7 @@ storiesOf('Tags')
       <Sanity part="part:@sanity/components/tags/textfield" propTables={[TagsTextField]}>
         <TagsTextField
           label={text('label (prop)', 'Tags')}
+          readOnly={boolean('readOnly (prop)', false)}
           placeholder={text('placeholder (prop)', 'This is the placeholder')}
           value={array('value (prop)', tags)}
           onChange={action('onChange')}
