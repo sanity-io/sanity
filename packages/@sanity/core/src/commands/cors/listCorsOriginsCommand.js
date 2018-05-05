172b092a9c243fcfe03c324bf82f191847e746df
Espen Hovlandsdal <espen@hovlandsdal.com>
2018-05-05 10:55:59 +0200

    [core] Add CLI-commands for managing CORS-origins

---
+++ packages/@sanity/core/src/commands/cors/listCorsOriginsCommand.js
@@ -0,0 +1,19 @@
+const helpText = `
+Examples
+  sanity cors list
+`
+
+export default {
+  name: 'list',
+  group: 'cors',
+  signature: '[ORIGIN]',
+  helpText,
+  description: 'List all origins allowed to access the API for this project',
+  action: async (args, context) => {
+    const {output} = context
+    const {apiClient} = context
+    const client = apiClient({requireUser: true, requireProject: true})
+    const origins = await client.request({url: '/cors'})
+    output.print(origins.map(origin => origin.origin).join('\n'))
+  }
+}
