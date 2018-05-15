eb924c709a8908f08b1fdd44f59f07c0d83a0db9
Bjørge Næss <bjoerge@gmail.com>
2018-05-15 17:27:39 +0200

    [form-builder] Fix tab/focus on confirm delete button (#783)
    * [form-builder] Fix tab/focus on confirm delete button
    
    * [components] Always insert element with tabindex after portal content

--- packages/@sanity/components/src/utilities/Portal/index.js
+++ packages/@sanity/components/src/utilities/Portal/index.js
@@ -1,6 +1,1 @@
-import ReactDOM from 'react-dom'
-
-import {ModernPortal} from './ModernPortal'
-import {LegacyPortal} from './LegacyPortal'
-
-export const Portal = ReactDOM.createPortal ? ModernPortal : LegacyPortal
+export {Portal} from './Portal'
