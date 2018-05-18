e039159c89d1bf243d28c91b1538769a0447908a
Espen Hovlandsdal <espen@hovlandsdal.com>
2018-05-18 10:31:28 +0200

    [util] Merge studio base path from env var if available

--- packages/@sanity/util/src/reduceConfig.js
+++ packages/@sanity/util/src/reduceConfig.js
@@ -1,9 +1,14 @@
+/* eslint-disable no-process-env */
 import {mergeWith} from 'lodash'
 
-const sanityEnv = process.env.SANITY_ENV || 'production' // eslint-disable-line no-process-env
+const sanityEnv = process.env.SANITY_ENV || 'production'
 const apiHosts = {
   staging: 'https://api.sanity.work',
   development: 'http://api.sanity.wtf'
+}
+
+const processEnvConfig = {
+  project: process.env.STUDIO_BASEPATH ? {basePath: process.env.STUDIO_BASEPATH} : {}
 }
 
 function merge(objValue, srcValue, key) {
@@ -19,7 +24,7 @@ export default (rawConfig, env = 'development') => {
   const apiHost = apiHosts[sanityEnv]
   const sanityConf = apiHost ? {api: {apiHost}} : {}
   const envConfig = (rawConfig.env || {})[env] || {}
-  const config = mergeWith({}, rawConfig, envConfig, sanityConf, merge)
+  const config = mergeWith({}, rawConfig, envConfig, sanityConf, processEnvConfig, merge)
   delete config.env
   return config
 }
