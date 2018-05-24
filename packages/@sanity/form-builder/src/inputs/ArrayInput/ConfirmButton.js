006b803d9a176b5b13f7b7abdec67c45bb736fc4
Bjørge Næss <bjoerge@gmail.com>
2018-05-24 16:41:05 +0200

    [components] Prevent inner element in button from stealing focus

--- packages/@sanity/form-builder/src/inputs/ArrayInput/ConfirmButton.js
+++ packages/@sanity/form-builder/src/inputs/ArrayInput/ConfirmButton.js
@@ -68,12 +68,7 @@ export default class ConfirmButton extends React.Component {
         />
         <div className={styles.popoverAnchor}>
           {showConfirmDialog && (
-            <PopOver
-              color="danger"
-              useOverlay={false}
-              onEscape={this.handleConfirmPopoverClose}
-              onClickOutside={this.handleConfirmPopoverClose}
-            >
+            <PopOver color="danger" useOverlay={false} onEscape={this.handleConfirmPopoverClose}>
               <div className={styles.wrapper}>
                 <Button
                   color="white"
