10f78f0aba0a1543097abf5849584a9c5cbfe33b
Bjørge Næss <bjoerge@gmail.com>
2018-05-29 10:33:19 +0200

    [preview] Refactor and expose WithVisibility helper component (#840)

--- packages/@sanity/preview/components.js
+++ packages/@sanity/preview/components.js
@@ -2,3 +2,4 @@
 exports.PreviewFields = require('./lib/components/PreviewFields').default
 exports.SanityPreview = require('./lib/components/SanityPreview').default
 exports.PreviewSubscriber = require('./lib/components/PreviewSubscriber').default
+exports.WithVisibility = require('./lib/components/WithVisibility').default
---
+++ packages/@sanity/preview/src/components/WithVisibility.js
@@ -0,0 +1,83 @@
+import React from 'react'
+import {defer, concat, of as observableOf} from 'rxjs'
+
+import intersectionObservableFor from '../streams/intersectionObservableFor'
+import visibilityChange$ from '../streams/visibilityChange'
+import {
+  map,
+  tap,
+  switchMap,
+  distinctUntilChanged,
+  refCount,
+  publishReplay,
+  delay
+} from 'rxjs/operators'
+
+const isVisible$ = visibilityChange$.pipe(map(event => !event.target.hidden))
+
+const STYLE = {minHeight: 1, minWidth: 1}
+
+// A stream of booleans to signal whether preview component should keep
+// subscriptions active or not
+const documentVisibility$ = concat(defer(() => observableOf(!document.hidden)), isVisible$).pipe(
+  distinctUntilChanged(),
+  publishReplay(1),
+  refCount()
+)
+
+type Props = {
+  // How long to wait before signalling hide
+  hideDelay: Number,
+  element: string,
+  style: {},
+  children: (isVisible: boolean) => React.Element<*>
+}
+
+type State = {
+  isVisible: boolean
+}
+
+export default class WithVisibility extends React.Component<Props, State> {
+  element = React.createRef()
+
+  state = {isVisible: null}
+
+  componentDidMount() {
+    const {hideDelay = 0} = this.props
+    const inViewport$ = intersectionObservableFor(this.element.current).pipe(
+      map(event => event.isIntersecting)
+    )
+
+    this.subscription = documentVisibility$
+      .pipe(
+        switchMap(isVisible => (isVisible ? inViewport$ : observableOf(false))),
+        switchMap(
+          isVisible => (isVisible ? observableOf(true) : observableOf(false).pipe(delay(hideDelay)))
+        ),
+        distinctUntilChanged(),
+        tap(isVisible => {
+          this.setState({isVisible})
+        })
+      )
+      .subscribe()
+  }
+
+  componentWillUnmount() {
+    this.subscription.unsubscribe()
+    this.subscription = null
+  }
+
+  render() {
+    const {isVisible} = this.state
+    const {children, style = {}, element = 'span', hideDelay, ...rest} = this.props
+    return React.createElement(
+      element,
+      {
+        ref: this.element,
+        style: {...STYLE, ...style},
+        ...rest
+      },
+      children(isVisible)
+    )
+  }
+}
