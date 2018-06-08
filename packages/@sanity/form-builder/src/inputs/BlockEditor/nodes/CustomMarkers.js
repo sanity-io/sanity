d4b5a584316513c89a76f5702700c4460b23916a
Per-Kristian Nordnes <per.kristian.nordnes@gmail.com>
2018-06-08 13:48:09 +0200

    [form-builder] Block editor: move block errors to the marker display

---
+++ packages/@sanity/form-builder/src/inputs/BlockEditor/nodes/CustomMarkers.js
@@ -0,0 +1,49 @@
+// @flow
+import React from 'react'
+
+import {Tooltip} from '@sanity/react-tippy'
+import CommentIcon from 'part:@sanity/base/comment-icon'
+
+import styles from './styles/CustomMarkers.css'
+
+type marker = {}
+
+type Props = {
+  markers: marker[]
+}
+
+export default class Markers extends React.Component<Props> {
+  static defaultProps = {
+    markers: []
+  }
+
+  handleCustomMarkerClick = event => {
+    event.preventDefault()
+    event.stopPropagation()
+    const {markers} = this.props
+    console.log(markers) // eslint-disable-line no-console
+  }
+
+  render() {
+    const {markers} = this.props
+    const text = `${markers.length === 1 ? 'One' : markers.length} custom ${
+      markers.length > 1 ? 'markers' : 'marker'
+    }, click to log to console.`
+
+    return (
+      <div className={styles.marker}>
+        <Tooltip
+          title={text}
+          trigger="mouseenter focus"
+          animation="scale"
+          arrow
+          theme="light"
+          distance="2"
+          duration={50}
+        >
+          <CommentIcon onClick={this.handleCustomMarkerClick} />
+        </Tooltip>
+      </div>
+    )
+  }
+}
--- packages/@sanity/form-builder/src/inputs/BlockEditor/nodes/Markers.js
+++ packages/@sanity/form-builder/src/inputs/BlockEditor/nodes/Markers.js
@@ -1,16 +1,20 @@
 // @flow
-import type {Node} from 'react'
+import type {SlateChange, SlateValue} from './typeDefs'
 import React from 'react'
+import classNames from 'classnames'
 
-import {Tooltip} from '@sanity/react-tippy'
-import CommentIcon from 'part:@sanity/base/comment-icon'
+import CustomMarkers from 'part:@sanity/form-builder/input/block-editor/custom-markers'
+import ValidationStatus from 'part:@sanity/components/validation/status'
 
 import styles from './styles/Markers.css'
 
 type marker = {}
 
 type Props = {
-  markers: marker[]
+  editorValue: SlateValue,
+  markers: marker[],
+  onFocus: void => void,
+  onChange: (change: SlateChange) => void
 }
 
 export default class Markers extends React.Component<Props> {
@@ -18,37 +22,63 @@ export default class Markers extends React.Component<Props> {
     markers: []
   }
 
-  handleClick = event => {
+  handleValidationMarkerClick = (event: SyntheticMouseEvent<*>) => {
     event.preventDefault()
     event.stopPropagation()
+    const {onFocus, onChange, editorValue} = this.props
+    const change = editorValue.change()
+    change.blur()
+    onChange(change, onFocus(this.getValidationMarkers()[0].path))
+  }
+
+  getValidationMarkers() {
     const {markers} = this.props
-    console.log(markers) // eslint-disable-line no-console
+    const validation = markers.filter(mrkr => mrkr.type === 'validation')
+    return validation.map(mrkr => {
+      if (mrkr.path.length <= 1) {
+        return mrkr
+      }
+      const level = mrkr.level === 'error' ? 'errors' : 'warnings'
+      return Object.assign({}, mrkr, {
+        item: mrkr.item.cloneWithMessage(`Contains ${level}`)
+      })
+    })
+  }
+
+  handleCancelEvent = event => {
+    event.preventDefault()
+    event.stopPropagation()
   }
 
   render() {
     const {markers} = this.props
-    const customMarkers = markers.filter(mrkr => mrkr.type !== 'validation')
-    if (customMarkers.length === 0) {
+    if (markers.length === 0) {
       return <div className={styles.root} contentEditable={false} />
     }
-    const text = `${customMarkers.length} custom ${
-      customMarkers.length > 1 ? 'markers' : 'marker'
-    }, click to log to console.`
+    const customMarkers = markers.filter(mrkr => mrkr.type !== 'validation')
+
+    const scopedValidation = this.getValidationMarkers()
+    const errors = scopedValidation.filter(mrkr => mrkr.level === 'error')
+    const warnings = scopedValidation.filter(mrkr => mrkr.level === 'warning')
+
     return (
-      <div className={styles.markers} contentEditable={false}>
+      <div
+        className={classNames([
+          styles.markers,
+          errors.length > 0 && styles.markersWithError,
+          warnings.length > 0 && !errors.length && styles.markersWithWarning
+        ])}
+        contentEditable={false}
+        onClick={this.handleCancelEvent}
+      >
         {customMarkers.length > 0 && (
-          <div className={styles.marker}>
-            <Tooltip
-              title={text}
-              trigger="mouseenter focus"
-              animation="scale"
-              arrow
-              theme="light"
-              distance="2"
-              duration={50}
-            >
-              <CommentIcon onClick={this.handleClick} />
-            </Tooltip>
+          <div className={styles.marker} onClick={this.handleCancelEvent}>
+            <CustomMarkers markers={customMarkers} />
+          </div>
+        )}
+        {scopedValidation.length > 0 && (
+          <div className={styles.marker} onClick={this.handleValidationMarkerClick}>
+            <ValidationStatus markers={scopedValidation} />
           </div>
         )}
       </div>
