47d351d3e4f99d3c523e9b108f6295d91e940b2e
Thomas Drevon <thomas@bengler.no>
2018-06-13 13:04:12 +0200

    [cli] Update moviesdb path to point at new tarball

--- packages/@sanity/cli/src/actions/init-project/templates/moviedb.js
+++ packages/@sanity/cli/src/actions/init-project/templates/moviedb.js
@@ -1,5 +1,5 @@
 export const importPrompt = 'Import a sampling of sci-fi movies to go with your movie schema?'
-export const datasetUrl = 'https://public.sanity.io/moviesdb-2018-03-06.tar.gz'
+export const datasetUrl = 'https://public.sanity.io/moviesdb-2018-06-13.tar.gz'
 
 export const dependencies = {
   '@sanity/google-maps-input': '^0.99.0',
