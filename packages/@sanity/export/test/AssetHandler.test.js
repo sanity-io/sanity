01c58dc61093edb83f218f428274f7a552345c72
Bjørge Næss <bjoerge@gmail.com>
2018-06-13 14:00:30 +0200

    [export] Add regression test to assert sibling fields are exported

--- packages/@sanity/export/test/AssetHandler.test.js
+++ packages/@sanity/export/test/AssetHandler.test.js
@@ -15,9 +15,9 @@ describe('asset handler', () => {
   test('can rewrite documents / queue downloads', done => {
     // prettier-ignore
     const docs = [
-      {_id: 'doc1', _type: 'bike', name: 'Scooter', image: {asset: {_ref: 'image-idx_abc123-3360x840-png'}}},
-      {_id: 'doc2', _type: 'bike', name: 'Dupe', image: {asset: {_ref: 'image-idx_abc123-3360x840-png'}}},
-      {_id: 'doc3', _type: 'bike', name: 'Tandem', image: {asset: {_ref: 'image-idx_abc456-310x282-jpg'}}},
+      {_id: 'doc1', _type: 'bike', name: 'Scooter', image: {_type: 'image', caption: 'Scooter bike', asset: {_ref: 'image-idx_abc123-3360x840-png'}}},
+      {_id: 'doc2', _type: 'bike', name: 'Dupe', image: {_type: 'image', asset: {_ref: 'image-idx_abc123-3360x840-png'}}},
+      {_id: 'doc3', _type: 'bike', name: 'Tandem', image: {_type: 'image', asset: {_ref: 'image-idx_abc456-310x282-jpg'}}},
       {_id: 'old4', _type: 'bike', name: 'Cool', image: {asset: {_ref: 'mzFgq1cvHSEeGscxBsRFoqKG'}}},
       {_id: 'mzFgq1cvHSEeGscxBsRFoqKG', _type: 'sanity.imageAsset', url: 'https://cdn.sanity.io/images/__fixtures__/__test__/mzFgq1cvHSEeGscxBsRFoqKG-2048x1364.jpg'},
       {_id: 'image-idx_abc123-dads3360x840-png', _type: 'sanity.imageAsset', url: 'https://cdn.sanity.io/images/__fixtures__/__test__/idx_abc123-3360x840.png'},
@@ -48,9 +48,9 @@ describe('asset handler', () => {
   test('can remove asset documents', done => {
     // prettier-ignore
     const docs = [
-      {_id: 'doc1', _type: 'bike', name: 'Scooter', image: {asset: {_ref: 'image-idx_abc123-3360x840-png'}}},
-      {_id: 'doc2', _type: 'bike', name: 'Dupe', image: {asset: {_ref: 'image-idx_abc123-3360x840-png'}}},
-      {_id: 'doc3', _type: 'bike', name: 'Tandem', image: {asset: {_ref: 'image-idx_abc456-310x282-jpg'}}},
+      {_id: 'doc1', _type: 'bike', name: 'Scooter', image: {_type: 'image', caption: 'Scooter bike', asset: {_ref: 'image-idx_abc123-3360x840-png'}}},
+      {_id: 'doc2', _type: 'bike', name: 'Dupe', image: {_type: 'image', asset: {_ref: 'image-idx_abc123-3360x840-png'}}},
+      {_id: 'doc3', _type: 'bike', name: 'Tandem', image: {_type: 'image', asset: {_ref: 'image-idx_abc456-310x282-jpg'}}},
       {_id: 'old4', _type: 'bike', name: 'Cool', image: {asset: {_ref: 'mzFgq1cvHSEeGscxBsRFoqKG'}}},
       {_id: 'mzFgq1cvHSEeGscxBsRFoqKG', _type: 'sanity.imageAsset', url: 'https://cdn.sanity.io/images/__fixtures__/__test__/mzFgq1cvHSEeGscxBsRFoqKG-2048x1364.jpg'},
       {_id: 'image-idx_abc123-3360x840-png', _type: 'sanity.imageAsset', url: 'https://cdn.sanity.io/images/__fixtures__/__test__/idx_abc123-3360x840.png'},
@@ -105,9 +105,9 @@ describe('asset handler', () => {
   test('can skip asset documents', done => {
     // prettier-ignore
     const docs = [
-      {_id: 'doc1', _type: 'bike', name: 'Scooter', image: {asset: {_ref: 'image-idx_abc123-3360x840-png'}}},
-      {_id: 'doc2', _type: 'bike', name: 'Dupe', image: {asset: {_ref: 'image-idx_abc123-3360x840-png'}}},
-      {_id: 'doc3', _type: 'bike', name: 'Tandem', image: {asset: {_ref: 'image-idx_abc456-310x282-jpg'}}},
+      {_id: 'doc1', _type: 'bike', name: 'Scooter', image: {_type: 'image', caption: 'Scooter bike', asset: {_ref: 'image-idx_abc123-3360x840-png'}}},
+      {_id: 'doc2', _type: 'bike', name: 'Dupe', image: {_type: 'image', asset: {_ref: 'image-idx_abc123-3360x840-png'}}},
+      {_id: 'doc3', _type: 'bike', name: 'Tandem', image: {_type: 'image', asset: {_ref: 'image-idx_abc456-310x282-jpg'}}},
       {_id: 'mzFgq1cvHSEeGscxBsRFoqKG', _type: 'sanity.imageAsset', url: 'https://cdn.sanity.io/images/__fixtures__/__test__/mzFgq1cvHSEeGscxBsRFoqKG-2048x1364.jpg'},
       {_id: 'image-idx_abc123-3360x840-png', _type: 'sanity.imageAsset', url: 'https://cdn.sanity.io/images/__fixtures__/__test__/idx_abc123-3360x840.png'},
       {_id: 'image-idx_abc456-310x282-jpg', _type: 'sanity.imageAsset', url: 'https://cdn.sanity.io/images/__fixtures__/__test__/idx_abc456-310x282.jpg'},
