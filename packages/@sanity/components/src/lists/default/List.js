d3fc45871529c801e8b91d9ff945340f1e659c7e
Kristoffer J. Sivertsen <kristoffer@bengler.no>
2018-06-28 14:17:39 +0200

    [form-builder] Not send inn the defaultList class in arrayInput (#878)

--- packages/@sanity/components/src/lists/default/List.js
+++ packages/@sanity/components/src/lists/default/List.js
@@ -1,6 +1,8 @@
 // @flow
 import React from 'react'
+import styles from '../styles/DefaultList.css'
+import classNames from 'classnames'
 
 export default function DefaultList(props) {
-  return <ul {...props} />
+  return <ul {...props} className={classNames([styles.root, props.className])} />
 }
--- packages/@sanity/components/src/lists/default/ListItem.js
+++ packages/@sanity/components/src/lists/default/ListItem.js
@@ -1,6 +1,8 @@
 // @flow
 import React from 'react'
+import styles from '../styles/DefaultListItem.css'
+import classNames from 'classnames'
 
 export default function CoreListItem(props: any) {
-  return <li {...props} />
+  return <li {...props} className={classNames([styles.root, props.className])} />
 }
