d9647a86e8a3fe97ae88ee8a67be3162533a41ca
Espen Hovlandsdal <espen@hovlandsdal.com>
2018-07-11 13:03:45 +0200

    [components] Clarify the role of SplitPaneWrapper with some comments

--- packages/@sanity/components/src/panes/SplitPaneWrapper.js
+++ packages/@sanity/components/src/panes/SplitPaneWrapper.js
@@ -1,18 +1,26 @@
 import PropTypes from 'prop-types'
 import React from 'react'
 
-export default class SplitPaneWrapper extends React.Component {
+/**
+ * SplitController plucks the props from this component and distributes panes,
+ * so while this component might seem useless, it has a purpose.
+ * @todo See if this can be done in a more intuitive way without this extra layer
+ */
+export default class SplitPaneWrapper extends React.PureComponent {
+  /* eslint-disable react/no-unused-prop-types */
   static propTypes = {
     minWidth: PropTypes.number,
     maxWidth: PropTypes.number,
     defaultWidth: PropTypes.number,
     children: PropTypes.node
   }
+  /* eslint-enable react/no-unused-prop-types */
 
   static defaultProps = {
     minWidth: 0,
     maxWidth: 0,
-    defaultWidth: 0
+    defaultWidth: 0,
+    children: undefined
   }
 
   render() {
