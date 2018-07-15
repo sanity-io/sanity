43286b988118069612fbb032fc007439229f395d
Espen Hovlandsdal <espen@hovlandsdal.com>
2018-07-15 15:29:23 +0200

    [components] Refactor panes to render menu items & actions from props

--- packages/@sanity/components/src/panes/story.js
+++ packages/@sanity/components/src/panes/story.js
@@ -1,72 +1,82 @@
 /* eslint-disable react/no-multi-comp */
 import React from 'react'
+import {range} from 'lodash'
 import {storiesOf, action} from 'part:@sanity/storybook'
 import {withKnobs, object, boolean, number, text} from 'part:@sanity/storybook/addons/knobs'
+import {RouterProvider, route} from 'part:@sanity/base/router'
 import Sanity from 'part:@sanity/storybook/addons/sanity'
-import {range} from 'lodash'
 import DefaultPane from 'part:@sanity/components/panes/default'
 import PanesController from 'part:@sanity/components/panes/controller'
 import SplitController from 'part:@sanity/components/panes/split-controller'
 import SplitPaneWrapper from 'part:@sanity/components/panes/split-pane-wrapper'
-import Menu from 'part:@sanity/components/menus/default'
-import MenuDivider from 'part:@sanity/components/menus/divider'
 import PlusIcon from 'part:@sanity/base/plus-icon'
 import TrashIcon from 'part:@sanity/base/trash-outline-icon'
 import Button from 'part:@sanity/components/buttons/default'
-import renderFunctionsStyles from './styles/renderFunctions.css'
+import renderActionsStyles from './styles/renderActions.css'
 
 const menuItems = [
   {
+    action: 'first',
     title: 'First item',
     key: '1'
   },
   {
+    action: 'second',
     title: 'Second item',
-    key: '1'
+    key: '2'
   },
   {
+    action: 'third',
     title: 'Third item',
-    key: '3'
+    key: '3',
+    params: {some: 'param'}
   },
-  MenuDivider,
   {
+    action: 'fourth',
     title: 'Extra item',
-    key: '4'
+    key: '4',
+    group: 'extra'
+  },
+  {
+    title: 'Add',
+    key: '5',
+    group: 'actions',
+    icon: PlusIcon,
+    showAsAction: true,
+    action: () => {
+      console.log('Function attached to action!')
+    }
   }
 ]
+
+const menuItemsWhenCollapsed = menuItems.map(
+  item =>
+    item.showAsAction ? Object.assign({}, item, {showAsAction: {whenCollapsed: true}}) : item
+)
 
 const handleMenuAction = menuAction => {
   console.log('action', menuAction)
 }
 
-const renderMenu = isCollapsed => {
-  return (
-    <Menu
-      items={menuItems}
-      origin={isCollapsed ? 'top-left' : 'top-right'}
-      onAction={handleMenuAction}
-      isOpen
-      onClickOutside={action('Close menu')}
-    />
-  )
-}
+const router = route('/')
+const handleNavigate = () => null
 
-const renderFunctions = isCollapsed => {
+const renderActions = isCollapsed => {
   return (
-    <div className={renderFunctionsStyles.root}>
+    <div className={renderActionsStyles.root}>
       <Button
         kind="simple"
         icon={PlusIcon}
         color="primary"
         title="Add"
-        className={renderFunctionsStyles.button}
+        className={renderActionsStyles.button}
       />
       <Button
         kind="simple"
         icon={TrashIcon}
         color="danger"
         title="Delete"
-        className={renderFunctionsStyles.button}
+        className={renderActionsStyles.button}
       />
     </div>
   )
@@ -77,56 +87,25 @@ storiesOf('Panes')
   .add('Pane', () => {
     return (
       <Sanity part="part:@sanity/components/panes/default" propTables={[DefaultPane]}>
-        <DefaultPane
-          title={text('title (prop)', 'This is the default pane title')}
-          isCollapsed={boolean('isCollapsed (prop)', false)}
-          onExpand={action('onExpand')}
-          onCollapse={action('onCollapse')}
-          minWidth={number('minWidth (prop)', 300)}
-          renderFunctions={renderFunctions}
-          renderMenu={renderMenu}
-        />
+        <RouterProvider
+          router={router}
+          onNavigate={handleNavigate}
+          state={router.decode(location.pathname)}
+        >
+          <DefaultPane
+            title={text('title (prop)', 'This is the default pane title')}
+            isCollapsed={boolean('isCollapsed (prop)', false)}
+            onExpand={action('onExpand')}
+            onCollapse={action('onCollapse')}
+            minWidth={number('minWidth (prop)', 300)}
+            onAction={handleMenuAction}
+            menuItems={menuItems}
+            renderActions={renderActions}
+          />
+        </RouterProvider>
       </Sanity>
     )
   })
-
-  // .add(
-  //   'Controller',
-  //   () => {
-  //     const panes = range(number('#Panes', 4)).map((pane, i) => {
-  //       return {
-  //         title: `Pane ${i} is the best pane`,
-  //         layout: i > 3 ? 'default' : 'main'
-  //       }
-  //     })
-  //
-  //     const selectedPaneIndex = number('Selected pane', 1)
-  //     const knobsPanes = object('Panes', panes)
-  //
-  //     return (
-  //       <Sanity part="part:@sanity/components/panes/controller" propTables={[PanesController]}>
-  //         <PanesController selectedIndex={selectedPaneIndex}>
-  //           {
-  //             knobsPanes.map((pane, i) => {
-  //               return (
-  //                 <DefaultPane
-  //                   title={pane.title}
-  //                   key={pane.title}
-  //                   renderFunctions={renderFunctions}
-  //                   renderMenu={renderMenu}
-  //                   onExpand={action('expand')}
-  //                   onCollapse={action('onCollapse')}
-  //                   onMenuToggle={action('onMenuToggle')}
-  //                   minWidth={300}
-  //                 />
-  //               )
-  //             })
-  //           }
-  //         </PanesController>
-  //       </Sanity>
-  //     )
-  //   }
-  // )
 
   .add('Split', () => {
     const panes = range(number('#Panes', 2)).map((pane, i) => {
@@ -149,51 +128,47 @@ storiesOf('Panes')
 
     const selectedPaneIndex = number('Selected pane', 1)
     const knobsPanes = object('Panes', panes)
-    const showMenu = boolean('Show menu', false)
-
-    const renderSplitMenu = isCollapsed => {
-      return (
-        <Menu
-          items={menuItems}
-          origin={isCollapsed ? 'top-left' : 'top-right'}
-          onAction={handleMenuAction}
-          isOpen={showMenu}
-          onClickOutside={action('Close menu')}
-        />
-      )
-    }
+    const actionsWhenCollapsed = boolean('Show actions when collapsed', false)
+    const customActionRenderer = boolean('Custom action rendering', false)
 
     return (
       <Sanity part="part:@sanity/components/panes/controller" propTables={[PanesController]}>
-        <SplitController
-          selectedIndex={selectedPaneIndex}
-          onCollapse={handleControllerCollapse}
-          onUnCollapse={handleControllerUnCollapse}
+        <RouterProvider
+          router={router}
+          onNavigate={handleNavigate}
+          state={router.decode(location.pathname)}
         >
-          {knobsPanes.map((pane, i) => {
-            return (
-              <SplitPaneWrapper
-                minWidth={pane.minWidth}
-                defaultWidth={pane.defaultWidth}
-                key={pane.key}
-                isCollapsed={pane.isCollapsed}
-              >
-                <DefaultPane
-                  title={pane.title}
-                  renderFunctions={renderFunctions}
-                  renderMenu={renderSplitMenu}
-                  onExpand={action('expand')}
-                  onCollapse={action('onCollapse')}
+          <SplitController
+            selectedIndex={selectedPaneIndex}
+            onCollapse={handleControllerCollapse}
+            onUnCollapse={handleControllerUnCollapse}
+          >
+            {knobsPanes.map((pane, i) => {
+              return (
+                <SplitPaneWrapper
+                  minWidth={pane.minWidth}
+                  defaultWidth={pane.defaultWidth}
+                  key={pane.key}
                   isCollapsed={pane.isCollapsed}
-                  onMenuToggle={action('onMenuToggle')}
                 >
-                  <div>defaultWidth: {pane.defaultWidth}</div>
-                  <div>minWidth: {pane.minWidth}</div>
-                </DefaultPane>
-              </SplitPaneWrapper>
-            )
-          })}
-        </SplitController>
+                  <DefaultPane
+                    title={pane.title}
+                    renderActions={customActionRenderer ? renderActions : undefined}
+                    onAction={handleMenuAction}
+                    menuItems={actionsWhenCollapsed ? menuItemsWhenCollapsed : menuItems}
+                    onExpand={action('expand')}
+                    onCollapse={action('onCollapse')}
+                    isCollapsed={pane.isCollapsed}
+                    onMenuToggle={action('onMenuToggle')}
+                  >
+                    <div>defaultWidth: {pane.defaultWidth}</div>
+                    <div>minWidth: {pane.minWidth}</div>
+                  </DefaultPane>
+                </SplitPaneWrapper>
+              )
+            })}
+          </SplitController>
+        </RouterProvider>
       </Sanity>
     )
   })
