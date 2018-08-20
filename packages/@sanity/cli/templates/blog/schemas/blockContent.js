de4bebd245f1cbf84ded93d9933f9adf8e18a4f1
Knut Melvær <knut.melvaer@gmail.com>
2018-08-20 07:44:47 +0200

    [cli] Add documentation to blockContent

--- packages/@sanity/cli/templates/blog/schemas/blockContent.js
+++ packages/@sanity/cli/templates/blog/schemas/blockContent.js
@@ -1,7 +1,7 @@
 /**
  * This is the schema definition for the rich text fields used for
  * for this blog studio. When you import it in schemas.js it can be
- * reused in other parts of the studio by:
+ * reused in other parts of the studio with:
  *  {
  *    name: 'someName',
  *    title: 'Some title',
@@ -16,7 +16,7 @@ export default {
     {
       title: 'Block',
       type: 'block',
-      // Styles let you set what your user can mark up text with. These
+      // Styles let you set what your user can mark up blocks with. These
       // corrensponds with HTML tags, but you can set any title or value
       // you want and decide how you want to deal with it where you want to
       // use your content.
@@ -29,10 +29,12 @@ export default {
         {title: 'Quote', value: 'blockquote'}
       ],
       lists: [{title: 'Bullet', value: 'bullet'}],
+      // Marks let you mark up inline text in the block editor.
       marks: {
-        // Decorators are...
+        // Decorators usually describe a single property – e.g. a typographic
+        // preference or highlighting by editors.
         decorators: [{title: 'Strong', value: 'strong'}, {title: 'Emphasis', value: 'em'}],
-        // Annotations is typically…
+        // Annotations can be any object structure – e.g. a link or a footnote.
         annotations: [
           {
             title: 'URL',
