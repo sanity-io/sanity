06479710bad26b6b3c0353419c58a18282c83317
Per-Kristian Nordnes <per.kristian.nordnes@gmail.com>
2018-09-05 13:33:52 +0200

    [form-builder] Delete old block editor code

--- packages/@sanity/form-builder/src/inputs/BlockEditor-slate/constants.js
+++
@@ -1,9 +0,0 @@
-export const BLOCK_DEFAULT_STYLE = 'normal'
-export const SLATE_SPAN_TYPE = 'span'
-export const SLATE_DEFAULT_BLOCK = Object.freeze({
-  kind: 'block',
-  type: 'contentBlock',
-  data: {
-    style: BLOCK_DEFAULT_STYLE
-  }
-})
--- packages/@sanity/form-builder/src/inputs/BlockEditor-slate/util/mapToObject.js
+++
@@ -1,7 +0,0 @@
-export default function mapToObject(array, producer) {
-  return array.reduce((acc, el, i) => {
-    const [key, value] = producer(el, i, acc)
-    acc[key] = value
-    return acc
-  }, {})
-}
