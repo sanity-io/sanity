f36246c2934ddd6ef9277c66b9c3d4707d8393ef
Per-Kristian Nordnes <per.kristian.nordnes@gmail.com>
2018-09-05 13:33:52 +0200

    [form-builder] Delete old block editor code

--- packages/@sanity/form-builder/src/inputs/BlockEditor-slate/createBlockNode.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import FormBuilderBlock from './FormBuilderBlock'
-
-export default function createBlockNode(type, onPatch) {
-  return function BlockNode(props) {
-    return <FormBuilderBlock type={type} {...props} onPatch={onPatch} />
-  }
-}
--- packages/@sanity/form-builder/src/inputs/BlockEditor-slate/createInlineNode.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import FormBuilderInline from './FormBuilderInline'
-
-export default function createInlineNode(type, onPatch) {
-  return function InlineNode(props) {
-    return <FormBuilderInline type={type} {...props} onPatch={onPatch} />
-  }
-}
