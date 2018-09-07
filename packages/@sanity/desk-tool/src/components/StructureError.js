89dc76c6b95f0431789d15af23cfa0524f13bcec
Espen Hovlandsdal <espen@hovlandsdal.com>
2018-09-07 13:57:34 +0200

    [desk-tool] Use error message instead of stack trace on HMR errors

--- packages/@sanity/desk-tool/src/components/StructureError.js
+++ packages/@sanity/desk-tool/src/components/StructureError.js
@@ -5,12 +5,32 @@ import {SerializeError} from '@sanity/structure'
 import ErrorIcon from 'part:@sanity/base/error-icon'
 import styles from './styles/StructureError.css'
 
+function formatStack(stack) {
+  return (
+    stack
+      // Prettify builder functions
+      .replace(/\(\.\.\.\)\./g, '(...)\n  .')
+      // Remove webpack cruft from function names
+      .replace(/__WEBPACK_IMPORTED_MODULE_\d+_+/g, '')
+      // Remove default export postfix from function names
+      .replace(/___default\./g, '.')
+      // Replace full host path, leave only path to JS-file
+      .replace(new RegExp(` \\(https?:\\/\\/${window.location.host}`, 'g'), ' (')
+  )
+}
+
 export default function StructureError(props) {
   const {path, helpId, message, stack} = props.error
-  const showStack = !(props.error instanceof SerializeError)
+
+  // Serialize errors are well-formatted and should be readable, in these cases a stack trace is
+  // usually not helpful. Build errors in dev (with HMR) usually also contains a bunch of garbage
+  // instead of an actual error message, so make sure we show the message in these cases as well
+  const showStack =
+    !(props.error instanceof SerializeError) && !message.includes('Module build failed:')
+
   return (
     <div className={styles.root}>
-      <h2 className={styles.title}>Uh oh… encountered an error while reading structure</h2>
+      <h2 className={styles.title}>Hmm… encountered an error while reading structure</h2>
 
       <div className={styles.body}>
         <h2 className={styles.path}>
@@ -29,7 +49,7 @@ export default function StructureError(props) {
             <span className={styles.problemSeverityText}>Error</span>
           </div>
           <div className={styles.problemContent}>
-            <div className={styles.problemMessage}>{showStack ? stack : message}</div>
+            <div className={styles.problemMessage}>{showStack ? formatStack(stack) : message}</div>
             {helpId && (
               <a
                 className={styles.problemLink}
