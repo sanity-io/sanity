187746eb59b0f6e0c36fb87476fd23d155b9555e
Kristoffer J. Sivertsen <kristoffer@sanity.io>
2018-09-27 14:05:37 +0200

    [components] Dropdown button cleanup (#959)

--- packages/@sanity/components/src/lists/default/ListItem.js
+++ packages/@sanity/components/src/lists/default/ListItem.js
@@ -3,6 +3,12 @@ import React from 'react'
 import styles from '../styles/DefaultListItem.css'
 import classNames from 'classnames'
 
-export default function CoreListItem(props: any) {
-  return <li {...props} className={classNames([styles.root, props.className])} />
+export default class CoreListItem extends React.Component<any> {
+  ref = React.createRef()
+  focus() {
+    this.ref.current.focus()
+  }
+  render() {
+    return <li {...this.props} ref={this.ref} className={classNames([styles.root, this.props.className])} />
+  }
 }
