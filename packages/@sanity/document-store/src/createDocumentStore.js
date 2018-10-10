f816369f481e5ffb965f7468ddafc079cbae63f2
Bjørge Næss <bjoerge@gmail.com>
2018-10-10 13:05:26 +0200

    [document-store] Refactor document store (#1003)

--- packages/@sanity/document-store/src/createDocumentStore.js
+++ packages/@sanity/document-store/src/createDocumentStore.js
@@ -1,150 +1,40 @@
-import {Observable, merge} from 'rxjs'
-import {share, filter, map, concat, switchMap, tap} from 'rxjs/operators'
-
-import {omit} from 'lodash'
-import pubsub from 'nano-pubsub'
-import {BufferedDocument, Mutation} from '@sanity/mutator'
-
-const NOOP = () => {}
+import {merge, Observable} from 'rxjs'
+import {filter, share} from 'rxjs/operators'
+import {createObservableBufferedDocument} from './createObservableBufferedDocument'
 
 function createBufferedDocument(documentId, serverEvents$, doCommit) {
+  const bufferedDocument = createObservableBufferedDocument(serverEvents$, doCommit)
+
   const reconnects$ = serverEvents$.pipe(filter(event => event.type === 'reconnect'))
 
-  const saves = pubsub()
-
-  const bufferedDocs$ = serverEvents$.pipe(
-    filter(event => event.type === 'snapshot'),
-    map(event => event.document),
-    map(snapshot => {
-      const bufferedDocument = new BufferedDocument(snapshot || null)
-
-      bufferedDocument.commitHandler = function commitHandler(opts) {
-        const payload = opts.mutation.params
-
-        // TODO:
-        // right now the BufferedDocument just commits fire-and-forget-ish
-        // We should be able to handle failures and retry here
-
-        doCommit(omit(payload, 'resultRev')).subscribe({
-          next: res => {
-            opts.success(res)
-            saves.publish()
-          },
-          error: opts.failure
-        })
-      }
-
-      const rebase$ = new Observable(rebaseObserver => {
-        bufferedDocument.onRebase = edge => {
-          rebaseObserver.next({type: 'rebase', document: edge})
-        }
-        return () => {
-          bufferedDocument.onRebase = NOOP
-        }
-      }).pipe(share())
-
-      const mutation$ = new Observable(mutationObserver => {
-        bufferedDocument.onMutation = ({mutation, remote}) => {
-          mutationObserver.next({
-            type: 'mutation',
-            document: bufferedDocument.LOCAL,
-            mutations: mutation.mutations,
-            origin: remote ? 'remote' : 'local'
-          })
-        }
-
-        const serverMutations = serverEvents$
-          .pipe(filter(event => event.type === 'mutation'))
-          .subscribe(event => bufferedDocument.arrive(new Mutation(event)))
-
-        return () => {
-          serverMutations.unsubscribe()
-          bufferedDocument.onMutation = NOOP
-        }
-      }).pipe(share())
-
-      return {
-        events: new Observable(observer => {
-          observer.next({type: 'snapshot', document: bufferedDocument.LOCAL})
-          observer.complete()
-        }).pipe(concat(merge(mutation$, rebase$, reconnects$))),
-
-        patch(patches) {
-          const mutations = patches.map(patch => ({patch: {...patch, id: documentId}}))
-
-          bufferedDocument.add(new Mutation({mutations: mutations}))
-        },
-        create(document) {
-          const mutation = {
-            create: Object.assign({id: documentId}, document)
-          }
-          bufferedDocument.add(new Mutation({mutations: [mutation]}))
-        },
-        createIfNotExists(document) {
-          bufferedDocument.add(new Mutation({mutations: [{createIfNotExists: document}]}))
-        },
-        createOrReplace(document) {
-          bufferedDocument.add(new Mutation({mutations: [{createOrReplace: document}]}))
-        },
-        delete() {
-          bufferedDocument.add(new Mutation({mutations: [{delete: {id: documentId}}]}))
-        },
-        commit() {
-          return new Observable(observer => {
-            // todo: connect observable with request from bufferedDocument.commit somehow
-            bufferedDocument.commit()
-            return saves.subscribe(() => {
-              observer.next()
-              observer.complete()
-            })
-          })
-        }
-      }
-    }),
-    share()
-  )
-
-  let currentBuffered
-  const cachedBuffered = new Observable(observer => {
-    if (currentBuffered) {
-      observer.next(currentBuffered)
-      observer.complete()
-    }
-    return bufferedDocs$
-      .pipe(
-        tap(doc => {
-          currentBuffered = doc
-        })
-      )
-      .subscribe(observer)
-  })
-
   return {
-    events: cachedBuffered.pipe(switchMap(bufferedDoc => bufferedDoc.events)),
+    events: merge(reconnects$, bufferedDocument.updates$),
     patch(patches) {
-      cachedBuffered.subscribe(bufferedDoc => bufferedDoc.patch(patches))
+      bufferedDocument.addMutations(patches.map(patch => ({patch: {...patch, id: documentId}})))
     },
     create(document) {
-      cachedBuffered.subscribe(bufferedDoc => bufferedDoc.create(document))
+      bufferedDocument.addMutation({
+        create: Object.assign({id: documentId}, document)
+      })
     },
     createIfNotExists(document) {
-      cachedBuffered.subscribe(bufferedDoc => bufferedDoc.createIfNotExists(document))
+      bufferedDocument.addMutation({createIfNotExists: document})
     },
     createOrReplace(document) {
-      cachedBuffered.subscribe(bufferedDoc => bufferedDoc.createOrReplace(document))
+      bufferedDocument.addMutation({createOrReplace: document})
     },
     delete() {
-      cachedBuffered.subscribe(bufferedDoc => bufferedDoc.delete())
+      bufferedDocument.addMutation({delete: {id: documentId}})
     },
     commit() {
-      return cachedBuffered.pipe(switchMap(bufferedDoc => bufferedDoc.commit()))
+      return bufferedDocument.commit()
     }
   }
 }
 
-const isDocId = id => event => event.documentId === id
+const isEventForDocId = id => event => event.type === 'reconnect' || event.documentId === id
 
-module.exports = function createDocumentStore({serverConnection}) {
+export default function createDocumentStore({serverConnection}) {
   return {
     byId,
     byIds,
@@ -181,12 +71,13 @@ module.exports = function createDocumentStore({serverConnection}) {
 
     const draft = createBufferedDocument(
       draftId,
-      serverEvents$.pipe(filter(isDocId(draftId))),
+      serverEvents$.pipe(filter(isEventForDocId(draftId))),
       doCommit
     )
+
     const published = createBufferedDocument(
       publishedId,
-      serverEvents$.pipe(filter(isDocId(publishedId))),
+      serverEvents$.pipe(filter(isEventForDocId(publishedId))),
       doCommit
     )
     return {draft, published}
