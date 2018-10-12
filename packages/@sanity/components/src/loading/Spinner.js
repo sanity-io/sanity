4ebf0794e746336f07b9721cc97c3c112d3b1059
Kristoffer J. Sivertsen <kristoffer@sanity.io>
2018-10-12 14:38:30 +0200

    [components] Smooth spinner with proper fullscreen

--- packages/@sanity/components/src/loading/Spinner.js
+++ packages/@sanity/components/src/loading/Spinner.js
@@ -2,8 +2,9 @@ import PropTypes from 'prop-types'
 import React from 'react'
 import styles from 'part:@sanity/components/loading/spinner-style'
 import SpinnerIcon from 'part:@sanity/base/spinner-icon'
+import {Portal} from '../utilities/Portal'
 
-export default class Spinner extends React.Component {
+export default class Spinner extends React.PureComponent {
   static propTypes = {
     inline: PropTypes.bool,
     message: PropTypes.string,
@@ -11,40 +12,7 @@ export default class Spinner extends React.Component {
     center: PropTypes.bool
   }
 
-  renderSvg() {
-    return (
-      <svg
-        width="126px"
-        height="126px"
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 100 100"
-        preserveAspectRatio="xMidYMid"
-      >
-        <rect x="0" y="0" width="100" height="100" fill="none" />
-        <circle
-          cx="50"
-          cy="50"
-          r="49.5"
-          strokeDasharray="202.1614872585032 108.85618544688634"
-          stroke="currentColor"
-          fill="none"
-          strokeWidth="1"
-        >
-          <animateTransform
-            attributeName="transform"
-            type="rotate"
-            values="0 50 50;180 50 50;360 50 50;"
-            keyTimes="0;0.5;1"
-            dur="1s"
-            repeatCount="indefinite"
-            begin="0s"
-          />
-        </circle>
-      </svg>
-    )
-  }
-
-  render() {
+  renderSpinner() {
     const {inline, message, fullscreen, center} = this.props
     return (
       <div
@@ -55,10 +23,17 @@ export default class Spinner extends React.Component {
         `}
       >
         <div className={styles.inner}>
-          {fullscreen || center ? this.renderSvg() : <SpinnerIcon color="inherit" />}
+          <span className={styles.icon}>
+            <SpinnerIcon />
+          </span>
           {message && <div className={styles.message}>{message}</div>}
         </div>
       </div>
     )
+  }
+
+  render() {
+    const {fullscreen} = this.props
+    return fullscreen ? <Portal>{this.renderSpinner()}</Portal> : this.renderSpinner()
   }
 }
