dba80097a8a75157a47b14108aff2c38e286517a
Kristoffer J. Sivertsen <kristoffer@sanity.io>
2018-10-15 12:35:20 +0200

    [components] Smooth spinner with proper fullscreen (#1012)

--- packages/@sanity/base/src/components/icons/SpinnerIcon.js
+++ packages/@sanity/base/src/components/icons/SpinnerIcon.js
@@ -1,3 +1,17 @@
-import SpinnerIcon from 'react-icons/lib/fa/circle-o-notch'
+import React from 'react'
+
+const SpinnerIcon = () => (
+  <svg
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    preserveAspectRatio="xMidYMid"
+  >
+    <path
+      d="M22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12C2 7.84072 4.53924 4.27455 8.15208 2.76711L7.38316 0.920257C3.04741 2.72903 0 7.00861 0 12C0 18.6274 5.37258 24 12 24C18.6274 24 24 18.6274 24 12C24 7.00842 20.9524 2.72869 16.6163 0.920044L15.8464 2.76647C19.46 4.27354 22 7.84013 22 12Z"
+      fill="currentColor"
+    />
+  </svg>
+)
 
 export default SpinnerIcon
