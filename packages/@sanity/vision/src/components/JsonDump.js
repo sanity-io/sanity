63c89ebe199c66ecb9024a90a99e4c00c2cdc555
Knut Melvær <knut.melvaer@gmail.com>
2018-10-16 12:56:32 +0200

    [vision] Replace JSON viewer (#1020)
    This replaces the JS(ON)-viewer in Vision with the [react-json-view](https://www.npmjs.com/package/react-json-view) component. The previous had formatting errors (strings in arrays). This also features a copy-function so that you can actually get sensible data out of vision.
    
    [vision] Fix visual hover bug

--- packages/@sanity/vision/src/components/JsonDump.js
+++ packages/@sanity/vision/src/components/JsonDump.js
@@ -1,41 +1,14 @@
 /* eslint-disable react/prop-types, react/no-multi-comp */
-import React from 'react'
+import React, {Fragment} from 'react'
 import PropTypes from 'prop-types'
-import tokenize from 'json-lexer'
+import ReactJson from 'react-json-view'
 
-const punctuator = token => <span className={token.className}>{token.raw}</span>
-const number = token => <span className={token.className}>{token.raw}</span>
-const string = token => <span className={token.className}>{token.raw}</span>
-const key = token => <span className={token.className}>{token.raw.slice(1, -1)}</span>
-const formatters = {punctuator, key, string, number}
-
-class JsonBlock extends React.PureComponent {
+class JsonBlock extends React.Component {
   render() {
     const styles = this.context.styles.jsonDump
-    const json = JSON.stringify(this.props.data, null, 2)
-    const tokens = tokenize(json).map((token, i, arr) => {
-      const prevToken = i === 0 ? token : arr[i - 1]
-      if (
-        token.type === 'string' &&
-        prevToken.type === 'whitespace' &&
-        /^\n\s+$/.test(prevToken.value)
-      ) {
-        token.type = 'key'
-      }
-
-      return token
-    })
-
     return (
       <pre className={styles.block}>
-        {tokens.map((token, i) => {
-          const Formatter = formatters[token.type]
-          return Formatter ? (
-            <Formatter key={i} className={styles[token.type]} raw={token.raw} />
-          ) : (
-            token.raw
-          )
-        })}
+        <ReactJson displayDataTypes={false} src={this.props.data} />
       </pre>
     )
   }
@@ -51,8 +24,10 @@ export default function JsonDump(props) {
   }
 
   return (
-    <code>
-      {props.data.map((row, i) => <JsonBlock key={row._id || row.eventId || i} data={row} />)}
-    </code>
+    <Fragment>
+      {props.data.map((row, i) => (
+        <JsonBlock key={row._id || row.eventId || i} data={row} />
+      ))}
+    </Fragment>
   )
 }
