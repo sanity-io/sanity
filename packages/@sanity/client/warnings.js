b19c048f9bfede5f4250c8fe8018e2d23d64c5f8
Bjørge Næss <bjoerge@gmail.com>
2018-10-18 13:32:05 +0200

    [client] Remove test script and add test suite for warnings

--- packages/@sanity/client/warnings.js
+++
@@ -1,7 +0,0 @@
-const SanityClient = require('./src/sanityClient')
-
-global.window = {
-  location: {hostname: 'prod.com'}
-}
-// should warn
-new SanityClient({projectId: 'skajfie', useCdn: false, token: 'foo'})
