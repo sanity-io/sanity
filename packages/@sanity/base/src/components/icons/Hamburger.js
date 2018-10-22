dea846d7b1d5b5d9763c07e33492fd83a5490ff2
Marius Lundgård <studio@mariuslundgard.com>
2018-10-22 15:40:23 +0200

    [base] Replace Hamburger icon

--- packages/@sanity/base/src/components/icons/Hamburger.js
+++ packages/@sanity/base/src/components/icons/Hamburger.js
@@ -1,3 +1,20 @@
-import FaBars from 'react-icons/lib/fa/bars'
+import React from 'react'
 
-export default FaBars
+const strokeStyle = {vectorEffect: 'non-scaling-stroke'}
+
+const HamburgerIcon = () => (
+  <svg
+    viewBox="0 0 25 25"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    preserveAspectRatio="xMidYMid"
+    width="1em"
+    height="1em"
+  >
+    <path d="M5 7.5H20" stroke="currentColor" style={strokeStyle} />
+    <path d="M5 12.5H20" stroke="currentColor" style={strokeStyle} />
+    <path d="M5 17.5H20" stroke="currentColor" style={strokeStyle} />
+  </svg>
+)
+
+export default HamburgerIcon
