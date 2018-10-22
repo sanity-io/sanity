151910bc3c5d4ba7760a3209b9b28a4b7a89a9f7
Marius Lundgård <studio@mariuslundgard.com>
2018-10-22 11:45:57 +0200

    [base] Add updated icons: Plus, Search, ViewColumn

--- packages/@sanity/base/src/components/icons/Plus.js
+++ packages/@sanity/base/src/components/icons/Plus.js
@@ -1,3 +1,19 @@
-import GoPlus from 'react-icons/lib/go/plus'
+import React from 'react'
 
-export default GoPlus
+const strokeStyle = {vectorEffect: 'non-scaling-stroke'}
+
+const PlusIcon = () => (
+  <svg
+    viewBox="0 0 25 25"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    preserveAspectRatio="xMidYMid"
+    width="1em"
+    height="1em"
+  >
+    <line x1="12.5" y1="4" x2="12.5" y2="21" stroke="currentColor" style={strokeStyle} />
+    <line x1="4" y1="12.5" x2="21" y2="12.5" stroke="currentColor" style={strokeStyle} />
+  </svg>
+)
+
+export default PlusIcon
--- packages/@sanity/base/src/components/icons/Search.js
+++ packages/@sanity/base/src/components/icons/Search.js
@@ -1,3 +1,26 @@
-import Search from 'react-icons/lib/fa/search'
+import React from 'react'
 
-export default Search
+const strokeStyle = {vectorEffect: 'non-scaling-stroke'}
+
+const SearchIcon = () => (
+  <svg
+    viewBox="0 0 25 25"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    preserveAspectRatio="xMidYMid"
+    width="1em"
+    height="1em"
+  >
+    <circle cx="10.5" cy="10.5" r="6" stroke="currentColor" style={strokeStyle} />
+    <line
+      x1="14.6036"
+      y1="14.6464"
+      x2="20.6069"
+      y2="20.6498"
+      stroke="currentColor"
+      style={strokeStyle}
+    />
+  </svg>
+)
+
+export default SearchIcon
