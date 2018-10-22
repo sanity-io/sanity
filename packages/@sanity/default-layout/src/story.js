7cd8b4a6e51e0e9a099ae86e03e5588aa0044368
Marius Lundgård <studio@mariuslundgard.com>
2018-10-22 14:33:42 +0200

    [default-layout] Add complete example story

--- packages/@sanity/default-layout/src/story.js
+++ packages/@sanity/default-layout/src/story.js
@@ -1,6 +1,14 @@
+/* eslint-disable no-console */
+/* eslint-disable react/jsx-no-bind */
+
 import React from 'react'
+import Ink from 'react-ink'
 import {storiesOf} from 'part:@sanity/storybook'
-import {withKnobs, text, boolean, object, select, color} from 'part:@sanity/storybook/addons/knobs'
+import {withKnobs, text, boolean, object} from 'part:@sanity/storybook/addons/knobs'
+import Button from 'part:@sanity/components/buttons/default'
+import HamburgerIcon from 'part:@sanity/base/hamburger-icon'
+import PlusIcon from 'part:@sanity/base/plus-icon'
+import PluginIcon from 'part:@sanity/base/plugin-icon'
 import ViewColumnIcon from 'part:@sanity/base/view-column-icon'
 import Branding from './components/Branding'
 import ToolSwitcherWidget from './components/ToolSwitcherWidget'
@@ -13,6 +21,71 @@ import DefaultLayoutStyles from './components/styles/DefaultLayout.css'
 
 storiesOf('Default layout')
   .addDecorator(withKnobs)
+  .add('Example', () => {
+    return (
+      <div className={DefaultLayoutStyles.navBar}>
+        <div className={NavBarStyles.root}>
+          <div className={NavBarStyles.hamburger}>
+            <Button
+              kind="simple"
+              onClick={() => console.log('onClick()')}
+              title="Menu"
+              icon={HamburgerIcon}
+            />
+          </div>
+          <a className={NavBarStyles.branding} href="#" onClick={evt => evt.preventDefault()}>
+            <Branding />
+          </a>
+          <a className={NavBarStyles.createButton} onClick={() => console.log('onClick()')}>
+            <span className={NavBarStyles.createButtonIcon}>
+              <PlusIcon />
+            </span>
+            <span className={NavBarStyles.createButtonText}>New</span>
+            <Ink duration={200} opacity={0.1} radius={200} />
+          </a>
+          <div className={NavBarStyles.search}>
+            <Search
+              inputValue={text('inputValue', '', 'props')}
+              onInputChange={event => console.log('onInputChange', event)}
+              isOpen={boolean('isOpen', false, 'props')}
+              isSearching={boolean('isSearching', false, 'props')}
+              renderItem={(i, index) => <div>Item</div>}
+              hits={object('hits', [{index: 0, _id: 'id0', name: 'Test'}], undefined, 'props')}
+            />
+          </div>
+          <div className={NavBarStyles.toolSwitcher}>
+            <ToolSwitcherWidget
+              onSwitchTool={event => console.log('onSwitchTool()', event)}
+              tools={[
+                {
+                  name: 'Desk tool',
+                  icon: ViewColumnIcon
+                },
+                {
+                  name: 'Plugin 1',
+                  icon: PluginIcon
+                },
+                {
+                  name: 'Plugin 2',
+                  icon: PluginIcon
+                }
+              ]}
+              renderItem={tool => <ToolSwitcherItem title={tool.name} icon={tool.icon} />}
+            />
+          </div>
+          <div className={NavBarStyles.loginStatus}>
+            <LoginStatus
+              user={{
+                name: 'John Doe',
+                profileImage: 'https://randomuser.me/api/portraits/men/12.jpg'
+              }}
+              onLogout={() => console.log('logout')}
+            />
+          </div>
+        </div>
+      </div>
+    )
+  })
   .add('Branding', () => {
     return (
       <div className={DefaultLayoutStyles.navBar}>
@@ -37,12 +110,12 @@ storiesOf('Default layout')
                   icon: ViewColumnIcon
                 },
                 {
-                  name: 'Desk tool 2',
-                  icon: ViewColumnIcon
+                  name: 'Plugin 1',
+                  icon: PluginIcon
                 },
                 {
-                  name: 'Desk tool 3',
-                  icon: ViewColumnIcon
+                  name: 'Plugin 2',
+                  icon: PluginIcon
                 }
               ]}
               renderItem={tool => <ToolSwitcherItem title={tool.name} icon={tool.icon} />}
@@ -90,8 +163,11 @@ storiesOf('Default layout')
         <div className={NavBarStyles.root}>
           <div className={NavBarStyles.loginStatus}>
             <LoginStatus
-              user={{name: 'John Doe', profileImage: 'https://randomuser.me/api/portraits/men/12.jpg'}}
-              onLogout={() => alert('logout')}
+              user={{
+                name: 'John Doe',
+                profileImage: 'https://randomuser.me/api/portraits/men/12.jpg'
+              }}
+              onLogout={() => console.log('logout')}
             />
           </div>
         </div>
