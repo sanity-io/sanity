d1f0cf6aec9df865fab4a92031071bb9ebbfd80c
Marius Lundgård <studio@mariuslundgard.com>
2018-10-22 11:45:57 +0200

    [base] Add updated icons: Plus, Search, ViewColumn

--- packages/@sanity/base/src/components/icons/ViewColumn.js
+++ packages/@sanity/base/src/components/icons/ViewColumn.js
@@ -1,3 +1,20 @@
-import MdViewColumn from 'react-icons/lib/md/view-column'
+import React from 'react'
 
-export default MdViewColumn
+const strokeStyle = {vectorEffect: 'non-scaling-stroke'}
+
+const ViewColumnIcon = () => (
+  <svg
+    viewBox="0 0 25 25"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    preserveAspectRatio="xMidYMid"
+    width="1em"
+    height="1em"
+  >
+    <rect x="0.5" y="4.5" width="24" height="16" stroke="currentColor" style={strokeStyle} />
+    <line x1="8.5" y1="5" x2="8.5" y2="20" stroke="currentColor" style={strokeStyle} />
+    <line x1="16.5" y1="5" x2="16.5" y2="20" stroke="currentColor" style={strokeStyle} />
+  </svg>
+)
+
+export default ViewColumnIcon
