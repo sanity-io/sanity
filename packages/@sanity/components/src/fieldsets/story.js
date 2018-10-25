a8286c9096acd4de790a328f6520dd54440ae476
Kristoffer J. Sivertsen <kristoffer@sanity.io>
2018-10-25 15:09:53 +0200

    [components] Story cleanup on fieldsets and fileinput

--- packages/@sanity/components/src/fieldsets/story.js
+++ packages/@sanity/components/src/fieldsets/story.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {Fragment} from 'react'
 import {storiesOf} from 'part:@sanity/storybook'
 import {withKnobs, text, number, boolean} from 'part:@sanity/storybook/addons/knobs'
 import Fieldset from 'part:@sanity/components/fieldsets/default'
@@ -6,6 +6,10 @@ import DefaultTextField from 'part:@sanity/components/textfields/default'
 import Chance from 'chance'
 const chance = new Chance()
 import Sanity from 'part:@sanity/storybook/addons/sanity'
+
+const wrapperStyles = {
+  margin: '1rem'
+}
 
 storiesOf('Fieldsets')
   .addDecorator(withKnobs)
@@ -16,37 +20,41 @@ storiesOf('Fieldsets')
     // `,
     () => {
       return (
-        <Sanity part="part:@sanity/components/fieldsets/default" propTables={[Fieldset]}>
-          <Fieldset
-            legend={text('legend (prop)', 'This is the legend')}
-            description={text('description (prop)', 'This is the description')}
-            level={number('level (prop)', 1)}
-            transparent={boolean('transparent (prop)', false)}
-            tabIndex={boolean('tabIndex', false) ? 0 : false}
-          >
-            {text('children (prop)', 'Put content here')}
-          </Fieldset>
-        </Sanity>
+        <div style={wrapperStyles}>
+          <Sanity part="part:@sanity/components/fieldsets/default" propTables={[Fieldset]}>
+            <Fieldset
+              legend={text('legend (prop)', 'This is the legend')}
+              description={text('description (prop)', 'This is the description')}
+              level={number('level (prop)', 1)}
+              transparent={boolean('transparent (prop)', false)}
+              tabIndex={boolean('tabIndex', false) ? 0 : false}
+            >
+              {text('children (prop)', 'Put content here')}
+            </Fieldset>
+          </Sanity>
+        </div>
       )
     }
   )
   .add('Nested (demo)', () => {
     return (
-      <Sanity part="part:@sanity/components/fieldsets/default" propTables={[Fieldset]}>
-        <Fieldset
-          legend="This is the legend"
-          description="Fieldsets supports beeing inside itselfs, and get new styling!"
-          level={1}
-        >
+      <div style={wrapperStyles}>
+        <Sanity part="part:@sanity/components/fieldsets/default" propTables={[Fieldset]}>
           <Fieldset
-            legend="This is the legend in a nested fieldset"
-            description={chance.paragraph()}
-            level={2}
+            legend="This is the legend"
+            description="Fieldsets supports beeing inside itselfs, and get new styling!"
+            level={1}
           >
-            A nested fieldset
+            <Fieldset
+              legend="This is the legend in a nested fieldset"
+              description={chance.paragraph()}
+              level={2}
+            >
+              A nested fieldset
+            </Fieldset>
           </Fieldset>
-        </Fieldset>
-      </Sanity>
+        </Sanity>
+      </div>
     )
   })
 
@@ -57,44 +65,46 @@ storiesOf('Fieldsets')
     // `,
     () => {
       return (
-        <Sanity part="part:@sanity/components/fieldsets/default" propTables={[Fieldset]}>
-          <Fieldset
-            legend="Dude, I heard you like fieldsets…"
-            description={chance.paragraph()}
-            level={1}
-            tabIndex="0"
-          >
+        <div style={wrapperStyles}>
+          <Sanity part="part:@sanity/components/fieldsets/default" propTables={[Fieldset]}>
             <Fieldset
-              legend="So I put a fieldset in a fieldset…"
+              legend="Dude, I heard you like fieldsets…"
               description={chance.paragraph()}
-              level={2}
+              level={1}
               tabIndex="0"
             >
               <Fieldset
-                legend="In a fieldset…"
+                legend="So I put a fieldset in a fieldset…"
                 description={chance.paragraph()}
-                level={3}
+                level={2}
                 tabIndex="0"
               >
                 <Fieldset
-                  legend="In a fieldset!"
+                  legend="In a fieldset…"
                   description={chance.paragraph()}
-                  level={4}
+                  level={3}
                   tabIndex="0"
                 >
                   <Fieldset
                     legend="In a fieldset!"
                     description={chance.paragraph()}
-                    level={5}
+                    level={4}
                     tabIndex="0"
                   >
-                    Content
+                    <Fieldset
+                      legend="In a fieldset!"
+                      description={chance.paragraph()}
+                      level={5}
+                      tabIndex="0"
+                    >
+                      Content
+                    </Fieldset>
                   </Fieldset>
                 </Fieldset>
               </Fieldset>
             </Fieldset>
-          </Fieldset>
-        </Sanity>
+          </Sanity>
+        </div>
       )
     }
   )
@@ -103,60 +113,62 @@ storiesOf('Fieldsets')
     const level = number('start level', 1)
     const isCollapsible = boolean('isCollapsible', true)
     return (
-      <Sanity part="part:@sanity/components/fieldsets/default" propTables={[Fieldset]}>
-        <div>
-          <Fieldset
-            isCollapsible={isCollapsible}
-            legend="Noooo, pick me, pick me!"
-            description={chance.sentence()}
-            level={level}
-          >
-            <DefaultTextField label="Content" level={level + 1} />
-            <DefaultTextField label="Content" level={level + 1} description="Test this one" />
-            <DefaultTextField label="Content" level={level + 1} />
-            <DefaultTextField label="Content" level={level + 1} />
-          </Fieldset>
-          <Fieldset
-            legend="Fieldsets can be collapsed"
-            description={chance.sentence()}
-            level={level}
-          >
-            <Fieldset
-              isCollapsible={isCollapsible}
-              legend="Open me"
-              description={chance.sentence()}
-              level={level + 1}
-            >
-              <DefaultTextField label="Content" level={level + 2} />
-              <DefaultTextField label="Content" level={level + 2} description="Test this one" />
-              <DefaultTextField label="Content" level={level + 2} />
-              <DefaultTextField label="Content" level={level + 2} />
-            </Fieldset>
-            <Fieldset
-              isCollapsible={isCollapsible}
-              legend="No, open me!"
-              description={chance.sentence()}
-              level={level + 1}
-            >
-              <DefaultTextField label="Content" level={level + 2} />
-              <DefaultTextField label="Content" level={level + 2} description="Test this one" />
-              <DefaultTextField label="Content" level={level + 2} />
-              <DefaultTextField label="Content" level={level + 2} />
-            </Fieldset>
+      <div style={wrapperStyles}>
+        <Sanity part="part:@sanity/components/fieldsets/default" propTables={[Fieldset]}>
+          <Fragment>
             <Fieldset
               isCollapsible={isCollapsible}
               legend="Noooo, pick me, pick me!"
               description={chance.sentence()}
-              level={level + 1}
+              level={level}
             >
-              <DefaultTextField label="Content" level={level + 2} />
-              <DefaultTextField label="Content" level={level + 2} description="Test this one" />
-              <DefaultTextField label="Content" level={level + 2} />
-              <DefaultTextField label="Content" level={level + 2} />
+              <DefaultTextField label="Content" level={level + 1} />
+              <DefaultTextField label="Content" level={level + 1} description="Test this one" />
+              <DefaultTextField label="Content" level={level + 1} />
+              <DefaultTextField label="Content" level={level + 1} />
             </Fieldset>
-          </Fieldset>
-        </div>
-      </Sanity>
+            <Fieldset
+              legend="Fieldsets can be collapsed"
+              description={chance.sentence()}
+              level={level}
+            >
+              <Fieldset
+                isCollapsible={isCollapsible}
+                legend="Open me"
+                description={chance.sentence()}
+                level={level + 1}
+              >
+                <DefaultTextField label="Content" level={level + 2} />
+                <DefaultTextField label="Content" level={level + 2} description="Test this one" />
+                <DefaultTextField label="Content" level={level + 2} />
+                <DefaultTextField label="Content" level={level + 2} />
+              </Fieldset>
+              <Fieldset
+                isCollapsible={isCollapsible}
+                legend="No, open me!"
+                description={chance.sentence()}
+                level={level + 1}
+              >
+                <DefaultTextField label="Content" level={level + 2} />
+                <DefaultTextField label="Content" level={level + 2} description="Test this one" />
+                <DefaultTextField label="Content" level={level + 2} />
+                <DefaultTextField label="Content" level={level + 2} />
+              </Fieldset>
+              <Fieldset
+                isCollapsible={isCollapsible}
+                legend="Noooo, pick me, pick me!"
+                description={chance.sentence()}
+                level={level + 1}
+              >
+                <DefaultTextField label="Content" level={level + 2} />
+                <DefaultTextField label="Content" level={level + 2} description="Test this one" />
+                <DefaultTextField label="Content" level={level + 2} />
+                <DefaultTextField label="Content" level={level + 2} />
+              </Fieldset>
+            </Fieldset>
+          </Fragment>
+        </Sanity>
+      </div>
     )
   })
 
@@ -164,23 +176,25 @@ storiesOf('Fieldsets')
     const columns = number('columns', 3)
     const level = number('start level', 1)
     return (
-      <Sanity part="part:@sanity/components/fieldsets/default" propTables={[Fieldset]}>
-        <Fieldset
-          legend={`${columns}columns`}
-          columns={columns}
-          description={chance.paragraph()}
-          level={level}
-        >
-          <Fieldset legend="Test" description={chance.paragraph()} level={level + 1} />
-          <Fieldset legend="Test 2" description={chance.paragraph()} level={level + 1} />
-          <Fieldset legend="Test 3" description={chance.paragraph()} level={level + 1} />
-        </Fieldset>
-      </Sanity>
+      <div style={wrapperStyles}>
+        <Sanity part="part:@sanity/components/fieldsets/default" propTables={[Fieldset]}>
+          <Fieldset
+            legend={`${columns}columns`}
+            columns={columns}
+            description={chance.paragraph()}
+            level={level}
+          >
+            <Fieldset legend="Test" description={chance.paragraph()} level={level + 1} />
+            <Fieldset legend="Test 2" description={chance.paragraph()} level={level + 1} />
+            <Fieldset legend="Test 3" description={chance.paragraph()} level={level + 1} />
+          </Fieldset>
+        </Sanity>
+      </div>
     )
   })
   .add('Spacing test', () => {
     return (
-      <div style={{margin: '1rem'}}>
+      <div style={wrapperStyles}>
         <Fieldset legend="Test" description={chance.paragraph()}>
           <DefaultTextField label="Label" placeholder="Placeholder" />
           <DefaultTextField label="Label" placeholder="Placeholder" />
--- packages/@sanity/components/src/fileinput/story.js
+++ packages/@sanity/components/src/fileinput/story.js
@@ -1,26 +1,41 @@
 import React from 'react'
 import {storiesOf, action} from 'part:@sanity/storybook'
-import {withKnobs, text, boolean} from 'part:@sanity/storybook/addons/knobs'
+import {withKnobs} from 'part:@sanity/storybook/addons/knobs'
 
 import FileInput from 'part:@sanity/components/fileinput/default'
 import FileInputButton from 'part:@sanity/components/fileinput/button'
 import Sanity from 'part:@sanity/storybook/addons/sanity'
 
+const centerStyle = {
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  height: '100%',
+  width: '100%',
+  position: 'absolute',
+  top: 0,
+  left: 0
+}
+
 storiesOf('File Input')
   .addDecorator(withKnobs)
   .add('Default', () => {
     return (
-      <Sanity part="part:@sanity/components/fileinput/default" propTables={[FileInput]}>
-        <FileInput onSelect={action('onSelect')}>
-          All this content triggers a file select from device
-        </FileInput>
-      </Sanity>
+      <div style={centerStyle}>
+        <Sanity part="part:@sanity/components/fileinput/default" propTables={[FileInput]}>
+          <FileInput onSelect={action('onSelect')}>
+            All this content triggers a file select from device
+          </FileInput>
+        </Sanity>
+      </div>
     )
   })
   .add('Button', () => {
     return (
-      <Sanity part="part:@sanity/components/fileinput/button" propTables={[FileInputButton]}>
-        <FileInputButton onSelect={action('onSelect')}>Upload file…</FileInputButton>
-      </Sanity>
+      <div style={centerStyle}>
+        <Sanity part="part:@sanity/components/fileinput/button" propTables={[FileInputButton]}>
+          <FileInputButton onSelect={action('onSelect')}>Upload file…</FileInputButton>
+        </Sanity>
+      </div>
     )
   })
