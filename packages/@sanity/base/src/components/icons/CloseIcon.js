ea541251416784f79bca3c12e0dfa653dbc4a5fa
Marius Lundgård <studio@mariuslundgard.com>
2018-10-26 15:26:41 +0200

    [base] Replace CloseIcon

--- packages/@sanity/base/src/components/icons/CloseIcon.js
+++ packages/@sanity/base/src/components/icons/CloseIcon.js
@@ -1,3 +1,18 @@
-import CloseIcon from 'react-icons/lib/md/close'
+import React from 'react'
+
+const strokeStyle = {vectorEffect: 'non-scaling-stroke'}
+
+const CloseIcon = () => (
+  <svg
+    viewBox="0 0 25 25"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    preserveAspectRatio="xMidYMid"
+    width="1em"
+    height="1em"
+  >
+    <path d="M19 6L6 19M6 6L19 19" stroke="currentColor" style={strokeStyle} />
+  </svg>
+)
 
 export default CloseIcon
