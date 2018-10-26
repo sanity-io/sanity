5f8e703385bdae5891789507033bf0eec4c17770
Kristoffer J. Sivertsen <kristoffer@sanity.io>
2018-10-26 13:50:07 +0200

    [components] InProgress indication on progress-bar/circle

--- packages/@sanity/components/src/progress/story.js
+++ packages/@sanity/components/src/progress/story.js
@@ -6,23 +6,40 @@ import {withKnobs, number, boolean, text} from 'part:@sanity/storybook/addons/kn
 import {storiesOf} from 'part:@sanity/storybook'
 import Sanity from 'part:@sanity/storybook/addons/sanity'
 
+const centerStyle = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: '100%',
+  maxWidth: '600px',
+  padding: '1rem'
+}
+
 storiesOf('Progress')
   .addDecorator(withKnobs)
   .add('Progress bar', () => (
-    <Sanity part="part:@sanity/components/progress/bar" propTables={[ProgressBar]}>
-      <ProgressBar
-        percent={number('percentage (prop)', 10, {range: true, min: 0, max: 100, step: 1})}
-        showPercent={boolean('showPercent (prop)', false)}
-        text={text('text (prop)', 'Downloaded 5.1 of 8.2Mb')}
-      />
-    </Sanity>
+    <div style={centerStyle}>
+      <Sanity part="part:@sanity/components/progress/bar" propTables={[ProgressBar]}>
+        <ProgressBar
+          isComplete={boolean('isComplete', false, 'props')}
+          percent={number('percentage', 10, {range: true, min: 0, max: 100, step: 1}, 'props')}
+          showPercent={boolean('showPercent', false, 'props')}
+          text={text('text', 'Downloaded 5.1 of 8.2Mb', 'props')}
+          isInProgress={boolean('isInProgress', false, 'props')}
+        />
+      </Sanity>
+    </div>
   ))
   .add('Progress circle', () => (
-    <Sanity part="part:@sanity/components/progress/circle" propTables={[ProgressCircle]}>
-      <ProgressCircle
-        percent={number('percent (prop)', 10, {range: true, min: 0, max: 100, step: 1})}
-        showPercent={boolean('showPercent (prop)', true)}
-        text={text('text (prop)', 'Uploaded')}
-      />
-    </Sanity>
+    <div style={centerStyle}>
+      <Sanity part="part:@sanity/components/progress/circle" propTables={[ProgressCircle]}>
+        <ProgressCircle
+          isComplete={boolean('isComplete', false, 'props')}
+          percent={number('percent', 10, {range: true, min: 0, max: 100, step: 1}, 'props')}
+          showPercent={boolean('showPercent', true, 'props')}
+          text={text('text', 'Uploaded', 'props')}
+        />
+      </Sanity>
+    </div>
   ))
