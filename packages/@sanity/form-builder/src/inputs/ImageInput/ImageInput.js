c16f099ae4d9bebdd5f7911cf7c920c30bd93ca5
Kristoffer J. Sivertsen <kristoffer@sanity.io>
2018-10-29 13:02:48 +0100

    [form-builder] Move image functions close to image

--- packages/@sanity/form-builder/src/inputs/ImageInput/ImageInput.js
+++ packages/@sanity/form-builder/src/inputs/ImageInput/ImageInput.js
@@ -373,9 +373,6 @@ export default class ImageInput extends React.PureComponent<Props, State> {
               <UploadPlaceholder hasFocus={hasFocus} />
             )}
           </div>
-          {highlightedFields.length > 0 && (
-            <div className={styles.fieldsWrapper}>{this.renderFields(highlightedFields)}</div>
-          )}
         </div>
         <div className={styles.functions}>
           <ButtonCollection>
@@ -412,6 +409,9 @@ export default class ImageInput extends React.PureComponent<Props, State> {
               )}
           </ButtonCollection>
         </div>
+        {highlightedFields.length > 0 && (
+          <div className={styles.fieldsWrapper}>{this.renderFields(highlightedFields)}</div>
+        )}
         {isAdvancedEditOpen && this.renderAdvancedEdit(otherFields)}
         {isSelectAssetOpen && (
           <Dialog title="Select image" onClose={this.handleCloseSelectAsset} isOpen>
