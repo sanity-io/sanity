19f39ee7b2f1494b118468028ac2723c9b286e51
Marius Lundgård <studio@mariuslundgard.com>
2018-10-29 17:19:42 +0100

    [base] Replace MoreVert icon

--- packages/@sanity/base/src/components/icons/MoreVert.js
+++ packages/@sanity/base/src/components/icons/MoreVert.js
@@ -1,3 +1,20 @@
-import MdMoreVert from 'react-icons/lib/md/more-vert'
+import React from 'react'
 
-export default MdMoreVert
+const strokeStyle = {vectorEffect: 'non-scaling-stroke'}
+
+const MoreVertIcon = () => (
+  <svg
+    viewBox="0 0 25 25"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    preserveAspectRatio="xMidYMid"
+    width="1em"
+    height="1em"
+  >
+    <circle cx="12.5" cy="6.5" r="1.5" fill="currentColor" style={strokeStyle} />
+    <circle cx="12.5" cy="12.5" r="1.5" fill="currentColor" style={strokeStyle} />
+    <circle cx="12.5" cy="18.5" r="1.5" fill="currentColor" style={strokeStyle} />
+  </svg>
+)
+
+export default MoreVertIcon
