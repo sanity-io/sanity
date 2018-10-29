0ffa22aec92f524b09f3162c30e83f51634ddf66
Marius Lundgård <studio@mariuslundgard.com>
2018-10-29 13:19:28 +0100

    [default-layout] Implement revised menu and search

--- packages/@sanity/default-layout/src/components/NavBar.js
+++ packages/@sanity/default-layout/src/components/NavBar.js
@@ -6,17 +6,30 @@ import config from 'config:sanity'
 import PlusIcon from 'part:@sanity/base/plus-icon'
 import HamburgerIcon from 'part:@sanity/base/hamburger-icon'
 import ToolSwitcher from 'part:@sanity/default-layout/tool-switcher'
+import SearchIcon from 'part:@sanity/base/search-icon'
+import {StateLink} from 'part:@sanity/base/router'
 import Branding from './Branding'
 import LoginStatus from './LoginStatus'
-import SearchController from './SearchController'
+import SearchContainer from './SearchContainer'
 import styles from './styles/NavBar.css'
-import {StateLink} from 'part:@sanity/base/router'
 
 function NavBar(props) {
-  const {onCreateButtonClick, onToggleMenu, onSwitchTool, onUserLogout, router, tools, user} = props
-
+  const {
+    searchIsOpen,
+    onCreateButtonClick,
+    onToggleMenu,
+    onSwitchTool,
+    onUserLogout,
+    onSearchOpen,
+    onSearchClose,
+    router,
+    tools,
+    user
+  } = props
+  let searchClassName = styles.search
+  if (searchIsOpen) searchClassName += ` ${styles.searchIsOpen}`
   return (
-    <div className={styles.root}>
+    <div className={styles.root} data-search-open={searchIsOpen}>
       <div className={styles.hamburger}>
         <button
           className={styles.hamburgerButton}
@@ -52,22 +65,38 @@ function NavBar(props) {
           onSwitchTool={onSwitchTool}
         />
       </div>
-      <div className={styles.search}>
-        <SearchController />
+      <div className={searchClassName}>
+        <div>
+          <SearchContainer
+            shouldBeFocused={searchIsOpen}
+            onOpen={onSearchOpen}
+            onClose={onSearchClose}
+          />
+        </div>
       </div>
       <div className={styles.extras}>{/* Insert plugins here */}</div>
       <div className={styles.loginStatus}>
         <LoginStatus onLogout={onUserLogout} user={user} />
       </div>
+      <a className={styles.searchButton} onClick={onSearchOpen}>
+        <span className={styles.searchButtonIcon}>
+          <SearchIcon />
+        </span>
+        <span className={styles.searchButtonText}>Search</span>
+        <Ink duration={200} opacity={0.1} radius={200} />
+      </a>
     </div>
   )
 }
 
 NavBar.propTypes = {
+  searchIsOpen: PropTypes.bool.isRequired,
   onCreateButtonClick: PropTypes.func.isRequired,
   onToggleMenu: PropTypes.func.isRequired,
   onSwitchTool: PropTypes.func.isRequired,
   onUserLogout: PropTypes.func.isRequired,
+  onSearchOpen: PropTypes.func.isRequired,
+  onSearchClose: PropTypes.func.isRequired,
   router: PropTypes.shape({
     state: PropTypes.shape({tool: PropTypes.string}),
     navigate: PropTypes.func
--- packages/@sanity/default-layout/src/story.js
+++ packages/@sanity/default-layout/src/story.js
@@ -4,7 +4,7 @@
 import React from 'react'
 import Ink from 'react-ink'
 import {storiesOf} from 'part:@sanity/storybook'
-import {withKnobs, text, boolean, object} from 'part:@sanity/storybook/addons/knobs'
+import {withKnobs, text, boolean, number} from 'part:@sanity/storybook/addons/knobs'
 import HamburgerIcon from 'part:@sanity/base/hamburger-icon'
 import PlusIcon from 'part:@sanity/base/plus-icon'
 import PluginIcon from 'part:@sanity/base/plugin-icon'
@@ -13,75 +13,96 @@ import Branding from './components/Branding'
 import ToolSwitcherWidget from './components/ToolSwitcherWidget'
 import NotFound from './components/NotFound'
 import LoginStatus from './components/LoginStatus'
-import Search from './components/SearchWidget'
+import SearchField from './components/SearchField'
+import SearchResults from './components/SearchResults'
 import ToolSwitcherItem from './components/ToolSwitcherItem'
+
 import NavBarStyles from './components/styles/NavBar.css'
 import DefaultLayoutStyles from './components/styles/DefaultLayout.css'
 
 storiesOf('Default layout')
   .addDecorator(withKnobs)
   .add('Example', () => {
+    const menuIsOpen = boolean('menuIsOpen', false, 'props')
+    const searchIsOpen = boolean('searchIsOpen', false, 'props')
+    const isOverlayVisible = menuIsOpen || searchIsOpen
+    let className = DefaultLayoutStyles.root
+    if (isOverlayVisible) className += ` ${DefaultLayoutStyles.isOverlayVisible}`
     return (
-      <div className={DefaultLayoutStyles.navBar}>
-        <div className={NavBarStyles.root}>
-          <div className={NavBarStyles.hamburger}>
-            <button
-              className={NavBarStyles.hamburgerButton}
-              type="button"
-              title="Menu"
-              onClick={() => console.log('onClick()')}
-            >
-              <HamburgerIcon />
-            </button>
-          </div>
-          <a className={NavBarStyles.branding} href="#" onClick={evt => evt.preventDefault()}>
-            <Branding />
-          </a>
-          <a className={NavBarStyles.createButton} onClick={() => console.log('onClick()')}>
-            <span className={NavBarStyles.createButtonIcon}>
-              <PlusIcon />
-            </span>
-            <span className={NavBarStyles.createButtonText}>New</span>
-            <Ink duration={200} opacity={0.1} radius={200} />
-          </a>
-          <div className={NavBarStyles.search}>
-            <Search
-              inputValue={text('inputValue', '', 'props')}
-              onInputChange={event => console.log('onInputChange', event)}
-              isOpen={boolean('isOpen', false, 'props')}
-              isSearching={boolean('isSearching', false, 'props')}
-              renderItem={(i, index) => <div>Item</div>}
-              hits={object('hits', [{index: 0, _id: 'id0', name: 'Test'}], undefined, 'props')}
-            />
-          </div>
-          <div className={NavBarStyles.toolSwitcher}>
-            <ToolSwitcherWidget
-              onSwitchTool={event => console.log('onSwitchTool()', event)}
-              tools={[
-                {
-                  name: 'Desk tool',
-                  icon: ViewColumnIcon
-                },
-                {
-                  name: 'Plugin 1',
-                  icon: PluginIcon
-                },
-                {
-                  name: 'Plugin 2',
-                  icon: PluginIcon
+      <div className={className}>
+        <div className={DefaultLayoutStyles.navBar}>
+          <div className={NavBarStyles.root}>
+            <div className={NavBarStyles.hamburger}>
+              <button
+                className={NavBarStyles.hamburgerButton}
+                type="button"
+                title="Menu"
+                onClick={() => console.log('onClick()')}
+              >
+                <HamburgerIcon />
+              </button>
+            </div>
+            <a className={NavBarStyles.branding} href="#" onClick={evt => evt.preventDefault()}>
+              <Branding />
+            </a>
+            <a className={NavBarStyles.createButton} onClick={() => console.log('onClick()')}>
+              <span className={NavBarStyles.createButtonIcon}>
+                <PlusIcon />
+              </span>
+              <span className={NavBarStyles.createButtonText}>New</span>
+              <Ink duration={200} opacity={0.1} radius={200} />
+            </a>
+            <div className={NavBarStyles.search}>
+              <SearchField
+                hotkeys={['F']}
+                isFocused={boolean('searchIsFocused', false, 'props')}
+                isOpen={searchIsOpen}
+                results={
+                  <SearchResults
+                    activeIndex={number('searchActiveIndex', -1, 'props')}
+                    isLoading={boolean('searchIsLoading', false, 'props')}
+                    items={[]}
+                    query={''}
+                    renderItem={key => (
+                      <div key={key} style={{padding: '0.5em 0.75em'}}>
+                        {key}
+                      </div>
+                    )}
+                  />
                 }
-              ]}
-              renderItem={tool => <ToolSwitcherItem title={tool.name} icon={tool.icon} />}
-            />
-          </div>
-          <div className={NavBarStyles.loginStatus}>
-            <LoginStatus
-              user={{
-                name: 'John Doe',
-                profileImage: 'https://randomuser.me/api/portraits/men/12.jpg'
-              }}
-              onLogout={() => console.log('logout')}
-            />
+                value={''}
+                onChange={() => console.log('change')}
+              />
+            </div>
+            <div className={NavBarStyles.toolSwitcher}>
+              <ToolSwitcherWidget
+                onSwitchTool={event => console.log('onSwitchTool()', event)}
+                tools={[
+                  {
+                    name: 'Desk tool',
+                    icon: ViewColumnIcon
+                  },
+                  {
+                    name: 'Plugin 1',
+                    icon: PluginIcon
+                  },
+                  {
+                    name: 'Plugin 2',
+                    icon: PluginIcon
+                  }
+                ]}
+                renderItem={tool => <ToolSwitcherItem title={tool.name} icon={tool.icon} />}
+              />
+            </div>
+            <div className={NavBarStyles.loginStatus}>
+              <LoginStatus
+                user={{
+                  name: 'John Doe',
+                  profileImage: 'https://randomuser.me/api/portraits/men/12.jpg'
+                }}
+                onLogout={() => console.log('logout')}
+              />
+            </div>
           </div>
         </div>
       </div>
@@ -140,24 +161,6 @@ storiesOf('Default layout')
   .add('Not Found', () => {
     return <NotFound />
   })
-  .add('Search', () => {
-    return (
-      <div className={DefaultLayoutStyles.navBar}>
-        <div className={NavBarStyles.root}>
-          <div className={NavBarStyles.search}>
-            <Search
-              inputValue={text('inputValue', '', 'props')}
-              onInputChange={event => console.log('onInputChange', event)}
-              isOpen={boolean('isOpen', false, 'props')}
-              isSearching={boolean('isSearching', false, 'props')}
-              renderItem={(i, index) => <div>Item</div>}
-              hits={object('hits', [{index: 0, _id: 'id0', name: 'Test'}], undefined, 'props')}
-            />
-          </div>
-        </div>
-      </div>
-    )
-  })
   .add('Login Status', () => {
     return (
       <div className={DefaultLayoutStyles.navBar}>
@@ -170,6 +173,74 @@ storiesOf('Default layout')
               }}
               onLogout={() => console.log('logout')}
             />
+          </div>
+        </div>
+      </div>
+    )
+  })
+  .add('Search Field (mobile)', () => {
+    const hasResults = boolean('hasResults', false, 'props')
+    const items = hasResults ? ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'] : []
+    const query = text('query', '', 'props')
+
+    return (
+      <div style={{background: '#444', height: '100vh'}}>
+        <div style={{background: '#fff', margin: '0 auto', position: 'relative'}}>
+          <SearchField
+            isBleeding
+            isFocused={boolean('isFocused', false, 'props')}
+            isOpen={boolean('isOpen', false, 'props')}
+            results={
+              <SearchResults
+                activeIndex={number('activeIndex', -1, 'props')}
+                isLoading={boolean('isLoading', false, 'props')}
+                items={items}
+                query={query}
+                renderItem={key => (
+                  <div key={key} style={{padding: '0.75em 1em'}}>
+                    {key}
+                  </div>
+                )}
+              />
+            }
+            value={query}
+            onChange={() => console.log('change')}
+          />
+        </div>
+      </div>
+    )
+  })
+  .add('Search Field (desktop)', () => {
+    const hasResults = boolean('hasResults', false, 'props')
+    const items = hasResults ? ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'] : []
+    const query = text('query', '', 'props')
+
+    return (
+      <div>
+        <div className={DefaultLayoutStyles.navBar}>
+          <div className={NavBarStyles.root}>
+            <div className={NavBarStyles.search}>
+              <SearchField
+                hotkeys={['F']}
+                isFocused={boolean('isFocused', false, 'props')}
+                isOpen={boolean('isOpen', false, 'props')}
+                results={
+                  <SearchResults
+                    activeIndex={number('activeIndex', -1, 'props')}
+                    isLoading={boolean('isLoading', false, 'props')}
+                    items={items}
+                    query={query}
+                    renderItem={key => (
+                      <div key={key} style={{padding: '0.5em 0.75em'}}>
+                        {key}
+                      </div>
+                    )}
+                  />
+                }
+                value={query}
+                onChange={() => console.log('change')}
+              />
+            </div>
           </div>
         </div>
       </div>
