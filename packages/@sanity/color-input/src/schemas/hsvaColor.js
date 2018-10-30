af377c2100cdad9a15a2bf92e014a83c8e78ea07
Espen Hovlandsdal <espen@hovlandsdal.com>
2018-10-30 21:35:20 +0100

    [color-input] Hoist color types, add _type to objects

---
+++ packages/@sanity/color-input/src/schemas/hsvaColor.js
@@ -0,0 +1,11 @@
+export default {
+  title: 'Hue Saturation Value',
+  name: 'hsvaColor',
+  type: 'object',
+  fields: [
+    {name: 'h', type: 'number', title: 'Hue'},
+    {name: 's', type: 'number', title: 'Saturation'},
+    {name: 'v', type: 'number', title: 'Value'},
+    {name: 'a', type: 'number', title: 'Alpha'}
+  ]
+}
