0b9aec7d6e666d07ce5e2d0d56faa65e2b44f667
Marius Lundgård <studio@mariuslundgard.com>
2018-10-30 10:34:12 +0100

    [base] Replace SignOut icon

--- packages/@sanity/base/src/components/icons/SignOut.js
+++ packages/@sanity/base/src/components/icons/SignOut.js
@@ -1,3 +1,19 @@
-import FaSignOut from 'react-icons/lib/fa/sign-out'
+import React from 'react'
 
-export default FaSignOut
+const strokeStyle = {vectorEffect: 'non-scaling-stroke'}
+
+const SignOutIcon = () => (
+  <svg
+    viewBox="0 0 25 25"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    preserveAspectRatio="xMidYMid"
+    width="1em"
+    height="1em"
+  >
+    <rect x="3.5" y="5.5" width="14" height="14" stroke="currentColor" style={strokeStyle} />
+    <path d="M8 12.5H23M21 10L23.5 12.5L21 15" stroke="currentColor" style={strokeStyle} />
+  </svg>
+)
+
+export default SignOutIcon
