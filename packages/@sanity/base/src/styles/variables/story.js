04f28a51a68da4139abc8cb51583e0543709bdec
Kristoffer J. Sivertsen <kristoffer@sanity.io>
2018-10-31 11:01:38 +0100

    [base] Variables cleanup

--- packages/@sanity/base/src/styles/variables/story.js
+++ packages/@sanity/base/src/styles/variables/story.js
@@ -1,12 +1,49 @@
 import React from 'react'
 import {storiesOf} from 'part:@sanity/storybook'
-import brandColors from 'part:@sanity/base/theme/variables/brand-colors-style'
+import shadowStyles from 'part:@sanity/base/theme/shadows-style'
+import {range} from 'lodash'
+import styles from './story.css'
 
-storiesOf('Variables').add('Brand colors', () => {
-  console.log(brandColors)
-  return (
-    <div>
-      Test
-    </div>
-  )
-})
+storiesOf('Variables')
+  .add('Brand colors', () => {
+    return (
+      <div>
+        <div className={styles.colors}>
+          <div className={styles.grayBase} />
+          <div className={styles.brandPrimary} />
+          <div className={styles.brandSecondary} />
+        </div>
+      </div>
+    )
+  })
+  .add('Selectable item', () => {
+    return (
+      <div>
+        <div className={styles.colors}>
+          <div className={styles.selectableItemBase} />
+          <div className={styles.selectableItem} />
+          <div className={styles.selectableItemHover} />
+          <div className={styles.selectableItemFocus} />
+          <div className={styles.selectableItemActive} />
+          <div className={styles.selectableItemHighlighted} />
+          <div className={styles.selectedItem} />
+          <div className={styles.selectedHover} />
+        </div>
+      </div>
+    )
+  })
+  .add('Shadows', () => {
+    return (
+      <div>
+        <div className={styles.shadows}>
+          {range(0, 25).map(i => {
+            return (
+              <div key={i} className={shadowStyles[`shadow-${i}dp`]}>
+                shadow-{i}dp
+              </div>
+            )
+          })}
+        </div>
+      </div>
+    )
+  })
