748b6f8b56d6efa790e111c64520341009a696f0
Espen Hovlandsdal <espen@hovlandsdal.com>
2018-10-31 10:44:46 +0100

    [color-input] Hoist color types, add _type to objects (#1036)

---
+++ packages/@sanity/color-input/src/schemas/hslaColor.js
@@ -0,0 +1,11 @@
+export default {
+  title: 'Hue Saturation Lightness',
+  name: 'hslaColor',
+  type: 'object',
+  fields: [
+    {name: 'h', type: 'number', title: 'Hue'},
+    {name: 's', type: 'number', title: 'Saturation'},
+    {name: 'l', type: 'number', title: 'Lightness'},
+    {name: 'a', type: 'number', title: 'Alpha'}
+  ]
+}
---
+++ packages/@sanity/color-input/src/schemas/rgbaColor.js
@@ -0,0 +1,11 @@
+export default {
+  title: 'Red Green Blue (rgb)',
+  name: 'rgbaColor',
+  type: 'object',
+  fields: [
+    {name: 'r', type: 'number', title: 'Red'},
+    {name: 'g', type: 'number', title: 'Green'},
+    {name: 'b', type: 'number', title: 'Blue'},
+    {name: 'a', type: 'number', title: 'Alpha'}
+  ]
+}
