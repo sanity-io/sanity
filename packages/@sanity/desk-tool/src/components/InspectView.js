51b3de92ee948ffe71cb831a431b08dd579b5bc8
Bjørge Næss <bjoerge@gmail.com>
2018-11-14 13:06:51 +0100

    [desk-tool] Inspect view improvements

--- packages/@sanity/desk-tool/src/components/InspectView.js
+++ packages/@sanity/desk-tool/src/components/InspectView.js
@@ -1,13 +1,19 @@
 import PropTypes from 'prop-types'
 import React from 'react'
+import {combineLatest} from 'rxjs'
+import {map} from 'rxjs/operators'
+// eslint-disable-next-line import/no-unassigned-import
 import './styles/JSONInspector.css'
 import styles from './styles/InspectView.css'
 import JSONInspector from 'react-json-inspector'
-import DefaultDialog from 'part:@sanity/components/dialogs/default'
-import ToggleButtons from 'part:@sanity/components/toggles/buttons'
+import FullScreenDialog from 'part:@sanity/components/dialogs/fullscreen'
+import RadioButtons from 'part:@sanity/components/selects/radio'
+
 import {isObject} from 'lodash'
 import HLRU from 'hashlru'
-import {getPublishedId} from 'part:@sanity/base/util/draft-utils'
+import settings from '../settings'
+import {withPropsStream} from 'react-props-stream'
+import DocTitle from './DocTitle'
 
 const lru = HLRU(1000)
 
@@ -51,68 +57,67 @@ function maybeSelectAll(event) {
   selectElement(event.currentTarget)
 }
 
-const VIEW_MODE_PARSED = {value: 'parsed', title: 'Parsed'}
-const VIEW_MODE_RAW = {value: 'raw', title: 'Raw'}
-
+const VIEW_MODE_PARSED = {id: 'parsed', title: 'Parsed'}
+const VIEW_MODE_RAW = {id: 'raw', title: 'JSON'}
 const VIEW_MODES = [VIEW_MODE_PARSED, VIEW_MODE_RAW]
 
-const VIEW_MODE_LS_KEY = 'desk-tool-inspect-view-preferred-view-mode'
+const viewModeSettings = settings.forKey('inspect-view-preferred-view-mode')
 
-function getPreferredViewMode() {
-  const preferredViewMode = localStorage.getItem(VIEW_MODE_LS_KEY)
-  return preferredViewMode && VIEW_MODES.find(mode => mode.value === preferredViewMode)
+function mapReceivedPropsToChildProps(props$) {
+  const onViewModeChange = nextViewMode => viewModeSettings.set(nextViewMode.id)
+
+  const viewModeSetting$ = viewModeSettings
+    .listen('parsed')
+    .pipe(map(id => VIEW_MODES.find(mode => mode.id === id)))
+
+  return combineLatest(props$, viewModeSetting$).pipe(
+    map(([props, viewMode]) => ({...props, viewMode, onViewModeChange}))
+  )
 }
 
-export default class InspectView extends React.PureComponent {
-  state = {
-    viewMode: getPreferredViewMode() || VIEW_MODE_PARSED
-  }
-
-  handleChangeViewMode = viewMode => {
-    this.setState({viewMode: viewMode})
-    localStorage.setItem(VIEW_MODE_LS_KEY, viewMode.value)
-  }
-
-  render() {
-    const {value, onClose} = this.props
-    const {viewMode} = this.state
-    return (
-      <DefaultDialog
-        isOpen
-        showHeader
-        title={`Inspecting ${getPublishedId(value._id)}`}
-        onClose={onClose}
-      >
-        <div className={styles.content}>
-          <div className={styles.toolbar}>
-            <ToggleButtons
-              label="View"
-              value={viewMode}
-              items={VIEW_MODES}
-              onChange={this.handleChangeViewMode}
-            />
-          </div>
-          {viewMode === VIEW_MODE_PARSED && (
-            <JSONInspector isExpanded={isExpanded} onClick={toggleExpanded} data={value} />
-          )}
-          {viewMode === VIEW_MODE_RAW && (
-            <pre
-              className={styles.raw}
-              tabIndex={0}
-              onKeyDown={maybeSelectAll}
-              onDoubleClick={select}
-              onFocus={select}
-            >
-              {JSON.stringify(value, null, 2)}
-            </pre>
-          )}
-        </div>
-      </DefaultDialog>
-    )
-  }
+function InspectView(props) {
+  const {value, viewMode, onClose, onViewModeChange} = props
+  return (
+    <FullScreenDialog
+      showHeader
+      title={
+        <span>
+          Inspecting{' '}
+          <em>
+            <DocTitle document={value} />
+          </em>
+        </span>
+      }
+      onClose={onClose}
+    >
+      <div className={styles.toolbar}>
+        <RadioButtons value={viewMode} items={VIEW_MODES} onChange={onViewModeChange} />
+      </div>
+      <div className={styles.content}>
+        {viewMode === VIEW_MODE_PARSED && (
+          <JSONInspector isExpanded={isExpanded} onClick={toggleExpanded} data={value} />
+        )}
+        {viewMode === VIEW_MODE_RAW && (
+          <pre
+            className={styles.raw}
+            tabIndex={0}
+            onKeyDown={maybeSelectAll}
+            onDoubleClick={select}
+            onFocus={select}
+          >
+            {JSON.stringify(value, null, 2)}
+          </pre>
+        )}
+      </div>
+    </FullScreenDialog>
+  )
 }
 
 InspectView.propTypes = {
   value: PropTypes.object,
-  onClose: PropTypes.func
+  onClose: PropTypes.func,
+  onViewModeChange: PropTypes.func,
+  viewMode: PropTypes.shape({id: PropTypes.string, title: PropTypes.string})
 }
+
+export default withPropsStream(mapReceivedPropsToChildProps, InspectView)
