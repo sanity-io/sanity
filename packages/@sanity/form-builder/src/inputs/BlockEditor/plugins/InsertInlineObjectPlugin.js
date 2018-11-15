5b1e22abfe03a206de9dcd717b79d0aa0a1b32f5
Per-Kristian Nordnes <per.kristian.nordnes@gmail.com>
2018-11-15 10:28:31 +0100

    [form-builder] Block editor: update InsertInlinePlugin

--- packages/@sanity/form-builder/src/inputs/BlockEditor/plugins/InsertInlineObjectPlugin.js
+++ packages/@sanity/form-builder/src/inputs/BlockEditor/plugins/InsertInlineObjectPlugin.js
@@ -1,9 +1,6 @@
 // @flow
-
-import {randomKey, editorValueToBlocks, blocksToEditorValue} from '@sanity/block-tools'
+import {Inline} from 'slate'
 import type {SlateEditor, Type} from '../typeDefs'
-import {VALUE_TO_JSON_OPTS} from '../utils/createOperationToPatches'
-import {Block} from 'slate'
 
 export default function InsertInlineObjectPlugin(blockContentType: Type) {
   return {
@@ -13,34 +10,19 @@ export default function InsertInlineObjectPlugin(blockContentType: Type) {
       }
       const options = command.args[0] || {}
       const {objectType} = options
-      const key = options.key || randomKey(12)
-      const inline = {
+      const {focusBlock, focusText} = editor.value
+      const key = options.key || `${focusBlock.key}${focusBlock.nodes.indexOf(focusText) + 1}`
+      const inlineProps = {
+        key,
         type: objectType.name,
         isVoid: true,
-        key: key,
         data: {
           _key: key,
-          value: {_type: objectType.name, _key: key, _oldKey: key}
+          value: {_type: objectType.name, _key: key}
         }
       }
+      const inline = Inline.create(inlineProps)
       editor.insertInline(inline)
-      const {value} = editor
-      const {focusBlock} = value
-      const appliedBlocks = editorValueToBlocks(
-        {document: {nodes: [focusBlock.toJSON(VALUE_TO_JSON_OPTS)]}},
-        blockContentType
-      )
-      const newBlock = Block.fromJSON(
-        blocksToEditorValue(appliedBlocks, blockContentType).document.nodes[0]
-      )
-      const inlineObject = newBlock.nodes.find(
-        node => node.data && node.data.get('value') && node.data.get('value')._oldKey === key
-      )
-      const newData = inlineObject.data.toObject()
-      delete newData.value._oldKey
-      editor.replaceNodeByKey(focusBlock.key, newBlock.toJSON(VALUE_TO_JSON_OPTS))
-      editor.setNodeByKey(inlineObject.key, {data: newData})
-      editor.moveToEndOfNode(inlineObject)
       return editor
     }
   }
