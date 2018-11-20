68aeb8f7389c8368fe78ec24f42e80555f153a32
Bjørge Næss <bjoerge@gmail.com>
2018-11-20 14:42:02 +0100

    [default-layout] Only select _id and _type when searching (#1057)

--- packages/@sanity/default-layout/src/components/SearchContainer.js
+++ packages/@sanity/default-layout/src/components/SearchContainer.js
@@ -60,7 +60,7 @@ function search(query) {
     uniqueFields.map(joinedPath => `${joinedPath} match $t${i}`)
   )
   const constraintString = constraints.map(constraint => `(${constraint.join('||')})`).join('&&')
-  return client.observable.fetch(`*[${constraintString}][0...100]`, params)
+  return client.observable.fetch(`*[${constraintString}][0...100] {_id, _type}`, params)
 }
 
 class SearchContainer extends React.PureComponent {
