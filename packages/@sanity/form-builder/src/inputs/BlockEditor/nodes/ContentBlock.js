830a145d30aa3907f6e1e30e7013fb2d16b984e1
Per-Kristian Nordnes <per.kristian.nordnes@gmail.com>
2018-11-27 15:41:18 +0100

    [form-builder] Block editor: fix (hack) triple click on text selection bug

--- packages/@sanity/form-builder/src/inputs/BlockEditor/nodes/ContentBlock.js
+++ packages/@sanity/form-builder/src/inputs/BlockEditor/nodes/ContentBlock.js
@@ -26,37 +26,55 @@ type Props = {
 }
 
 // eslint-disable-next-line complexity
-export default function ContentBlock(props: Props & SlateComponentProps) {
-  const {attributes, blockContentFeatures, children, node} = props
-  const data = node.data
-  const listItem = data ? data.get('listItem') : null
-  const level = data ? data.get('level') : 1
-  const style = data ? data.get('style') : 'normal'
+export default class Markers extends React.Component<Props & SlateComponentProps> {
+  _clickCounter = 0
+  _clickTimeout = null
 
-  // Should we render a custom style?
-  let styleComponent
-  const customStyle =
-    blockContentFeatures && style
-      ? blockContentFeatures.styles.find(item => item.value === style)
-      : null
-  if (customStyle) {
-    styleComponent = customStyle.blockEditor && customStyle.blockEditor.render
+  handleMouseDown = () => {
+    if (this._clickTimeout) {
+      clearTimeout(this._clickTimeout)
+    }
+    this._clickTimeout = setTimeout(() => {
+      if (this._clickCounter > 2) {
+        this.props.editor.moveToRangeOfNode(this.props.editor.value.anchorBlock)
+      }
+      this._clickCounter = 0
+    }, 500)
+    this._clickCounter++
   }
 
-  if (listItem) {
+  render() {
+    const {attributes, blockContentFeatures, children, node} = this.props
+    const data = node.data
+    const listItem = data ? data.get('listItem') : null
+    const level = data ? data.get('level') : 1
+    const style = data ? data.get('style') : 'normal'
+
+    // Should we render a custom style?
+    let styleComponent
+    const customStyle =
+      blockContentFeatures && style
+        ? blockContentFeatures.styles.find(item => item.value === style)
+        : null
+    if (customStyle) {
+      styleComponent = customStyle.blockEditor && customStyle.blockEditor.render
+    }
+
+    if (listItem) {
+      return (
+        <ListItem attributes={attributes} level={level} listStyle={listItem}>
+          <Text style={style} styleComponent={styleComponent}>
+            <div onMouseDown={this.handleMouseDown}>{children}</div>
+          </Text>
+        </ListItem>
+      )
+    }
     return (
-      <ListItem attributes={attributes} level={level} listStyle={listItem}>
+      <div className={styles.textBlock} {...attributes} onMouseDown={this.handleMouseDown}>
         <Text style={style} styleComponent={styleComponent}>
           {children}
         </Text>
-      </ListItem>
+      </div>
     )
   }
-  return (
-    <div className={styles.textBlock} {...attributes}>
-      <Text style={style} styleComponent={styleComponent}>
-        {children}
-      </Text>
-    </div>
-  )
 }
