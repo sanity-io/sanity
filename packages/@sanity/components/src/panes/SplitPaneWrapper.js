a07200d16d14c8a86f0b5854e7e453cacd4cc110
Kristoffer J. Sivertsen <kristoffer@sanity.io>
2018-11-29 22:41:19 +0100

    Auto collapse panes (#1059)
    Panes can now auto-collapse if there is not enough space in the viewport.
    Last pane will be prioritized.
    User collapsed panes will stay collapsed on resize even if there is space
    
    * [all] Upgrade react-split-pane
    * [components] Pane story example
    * [desk-tool] Use the new pane changes in desk-tool
    * [components] Autocollapse in splitController
    * [components] Fixing collapsed pane shadow
    * [desk-tool] Ability to turn auto-collapse on/off
    * [test-studio] Add a deep pane example to test auto collapsing
    * [components] Move logic to DeskToolPanes

--- packages/@sanity/components/src/panes/SplitPaneWrapper.js
+++ packages/@sanity/components/src/panes/SplitPaneWrapper.js
@@ -6,20 +6,22 @@ import React from 'react'
  * so while this component might seem useless, it has a purpose.
  * @todo See if this can be done in a more intuitive way without this extra layer
  */
+
 export default class SplitPaneWrapper extends React.PureComponent {
   /* eslint-disable react/no-unused-prop-types */
   static propTypes = {
-    minWidth: PropTypes.number,
-    maxWidth: PropTypes.number,
-    defaultWidth: PropTypes.number,
-    children: PropTypes.node
+    minSize: PropTypes.number,
+    maxSize: PropTypes.number,
+    defaultSize: PropTypes.number,
+    children: PropTypes.node,
+    index: PropTypes.number
   }
   /* eslint-enable react/no-unused-prop-types */
 
   static defaultProps = {
-    minWidth: 0,
-    maxWidth: 0,
-    defaultWidth: 0,
+    minSize: 100,
+    maxSize: 500,
+    defaultSize: 321,
     children: undefined
   }
 
