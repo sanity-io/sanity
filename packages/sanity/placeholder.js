14b9f3bed3925267792d49a6e523950782916d6b
Bjørge Næss <bjoerge@gmail.com>
2018-12-01 16:31:31 +0100

    [sanity] Add sanity package (placeholder)

---
+++ packages/sanity/placeholder.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-console */
+console.log('👋 This is an empty placeholder package')
+console.log()
+console.log('To install Sanity, run:')
+console.log('  npm i -g @sanity/cli')
+console.log()
+console.log(
+  '👉 Read more about getting started at https://sanity.io/docs/introduction/getting-started'
+)
