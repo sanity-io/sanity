3e8758563650303031952e0c551a085d24e86acc
Bjørge Næss <bjoerge@gmail.com>
2018-12-03 19:47:13 +0100

    [create-sanity] Add engines field and change var => const

--- packages/create-sanity/index.js
+++ packages/create-sanity/index.js
@@ -1,6 +1,5 @@
 #!/usr/bin/env node
-var cli = require.resolve('@sanity/cli/bin/sanity')
-var child_process = require('child_process')
-child_process.fork(cli, ['init'], {
-  stdio: 'inherit'
-})
+const cli = require.resolve('@sanity/cli/bin/sanity')
+const child_process = require('child_process')
+
+child_process.fork(cli, ['init'], {stdio: 'inherit'})
