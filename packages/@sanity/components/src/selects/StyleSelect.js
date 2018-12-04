b602081c6a83ac1735c2f23d99982935f3620b38
Kristoffer J. Sivertsen <kristoffer@sanity.io>
2018-12-04 14:38:35 +0100

    [components] Sync styleselect with realtime block editor branch (#1079)

--- packages/@sanity/components/src/selects/StyleSelect.js
+++ packages/@sanity/components/src/selects/StyleSelect.js
@@ -1,17 +1,18 @@
 /* eslint-disable complexity */
 import PropTypes from 'prop-types'
 import React from 'react'
+import ArrowKeyNavigation from 'boundless-arrow-key-navigation/build'
 import styles from 'part:@sanity/components/selects/style-style'
-import FaAngleDown from 'part:@sanity/base/angle-down-icon'
+import ArrowIcon from 'part:@sanity/base/angle-down-icon'
 import CircleThinIcon from 'part:@sanity/base/circle-thin-icon'
 import CircleCheckIcon from 'part:@sanity/base/circle-check-icon'
-import {List, Item} from 'part:@sanity/components/lists/default'
-import CaptureOutsideClicks from '../utilities/CaptureOutsideClicks'
+import {List} from 'part:@sanity/components/lists/default'
 import Poppable from 'part:@sanity/components/utilities/poppable'
 
 const modifiers = {
   preventOverflow: {
-    padding: 0
+    padding: 0,
+    boundariesElement: 'viewport'
   },
   offset: {
     offset: '0, 0'
@@ -40,27 +41,36 @@ class StyleSelect extends React.PureComponent {
     value: PropTypes.array,
     renderItem: PropTypes.func,
     className: PropTypes.string,
-    transparent: PropTypes.bool,
     items: PropTypes.arrayOf(
       PropTypes.shape({
         title: PropTypes.string,
         active: PropTypes.bool
       })
-    )
+    ),
+    transparent: PropTypes.bool
   }
 
   static defaultProps = {
     className: '',
     onChange() {},
     onOpen() {},
-    onClose() {}
+    onClose() {},
+    items: [],
+    transparent: false
   }
 
   state = {
     showList: false
   }
 
+  buttonElement = React.createRef()
+  firstItemElement = React.createRef()
+  keyboardNavigation = false
+  menuHasKeyboardFocus = false
+
   handleSelect = event => {
+    event.preventDefault()
+    event.stopPropagation()
     const index = event.currentTarget.dataset.index
     if (!index) {
       return
@@ -71,92 +81,131 @@ class StyleSelect extends React.PureComponent {
     }
     this.props.onChange(item)
     this.handleCloseList()
+    this.keyboardNavigation = false
   }
 
   handleOpenList = () => {
-    this.setState({
-      showList: true
-    })
-    this.props.onOpen()
+    this.setState(
+      {
+        showList: true
+      },
+      () => {
+        this.menuHasKeyboardFocus = true
+        this.keyboardNavigation = true
+        this.firstItemElement.current.focus()
+        this.props.onOpen()
+      }
+    )
   }
 
   handleCloseList = () => {
-    this.setState({
-      showList: false
-    })
-    this.props.onClose()
+    this.buttonElement.current.focus()
+    this.setState(
+      {
+        showList: false
+      },
+      () => {
+        this.props.onClose()
+      }
+    )
   }
 
-  handleInnerClick = () => {
+  handleButtonClick = event => {
     if (this.state.showList) {
       this.handleCloseList()
     } else {
       this.handleOpenList()
     }
+    this.keyboardNavigation = event.detail == 0
   }
 
-  handleRootKeyPress = () => {
-    this.handleOpenList()
+  handleButtonKeyDown = event => {
+    if (event.key == 'Enter') {
+      this.handleOpenList()
+    }
+  }
+
+  handleButtonBlur = event => {
+    if (this.state.showList && !this.menuHasKeyboardFocus && this.keyboardNavigation) {
+      this.handleCloseList()
+    }
+  }
+
+  handleMenuBlur = event => {
+    this.menuHasKeyboardFocus = false
+    this.buttonElement.current.focus()
+    this.handleCloseList()
+  }
+
+  handleItemKeyPress = event => {
+    if (event.key === 'Enter') {
+      this.handleSelect(event)
+    }
   }
 
   render() {
-    const {value, transparent, items, className, placeholder, renderItem} = this.props
+    const {value, items, className, placeholder, renderItem, transparent} = this.props
     const {showList} = this.state
-
-    const target = (
-      <div className={styles.inner} onClick={this.handleInnerClick}>
-        <div className={styles.selectContainer}>
-          <span className={styles.text}>
-            {value && value.length > 1 && 'Multiple'}
-            {value && value.length == 1 && value[0].title}
-            {!value && placeholder}
-          </span>
-          <div className={styles.arrow}>
-            <FaAngleDown color="inherit" />
-          </div>
-        </div>
-      </div>
-    )
 
     return (
       <div
-        className={`${styles.root} ${className || ''} ${transparent ? styles.transparent : ''}`}
-        onKeyPress={this.handleRootKeyPress}
         tabIndex={0}
+        onClick={this.handleButtonClick}
+        onBlur={this.handleButtonBlur}
+        onKeyPress={this.handleButtonKeyDown}
+        className={`${styles.root} ${className || ''} ${transparent ? styles.transparent : ''}`}
       >
-        <Poppable target={target} onEscape={this.handleCloseList} modifiers={modifiers}>
+        <div className={styles.inner} ref={this.buttonElement}>
+          <div className={styles.selectContainer}>
+            <span className={styles.title}>
+              {value && value.length > 1 && 'Multiple'}
+              {value && value.length == 1 && value[0].title}
+              {!value && placeholder}
+            </span>
+            <span className={styles.arrow}>
+              <ArrowIcon color="inherit" />
+            </span>
+          </div>
+        </div>
+        <Poppable
+          onEscape={this.handleCloseList}
+          modifiers={modifiers}
+          onClickOutside={this.handleCloseList}
+          popperClassName={styles.popper}
+        >
           {showList && (
-            <div className={styles.popper}>
-              <CaptureOutsideClicks onClickOutside={showList ? this.handleCloseList : undefined}>
-                <div>
-                  <List className={styles.list}>
-                    {items.map((item, index) => {
-                      const isSemiSelected = value && value.length > 1 && value.includes(item)
-                      const isSelected = value && value.length === 1 && value[0] == item
-                      const classNames = `
-                          ${isSelected ? styles.itemSelected : styles.item}
-                          ${isSemiSelected ? styles.itemSemiSelected : ''}
-                        `
-                      return (
-                        <div
-                          key={item.key}
-                          title={item.title}
-                          data-index={index}
-                          onClick={this.handleSelect}
-                          className={classNames}
-                        >
-                          <div className={styles.itemIcon}>
-                            {isSelected && <CircleCheckIcon />}
-                            {isSemiSelected && <CircleThinIcon />}
-                          </div>
-                          <div className={styles.itemContent}>{renderItem(item)}</div>
+            <React.Fragment>
+              <List className={styles.list}>
+                <ArrowKeyNavigation>
+                  {items.map((item, index) => {
+                    const isSemiSelected = value && value.length > 1 && value.includes(item)
+                    const isSelected = value && value.length === 1 && value[0].key == item.key
+                    const classNames = `
+                        ${isSelected ? styles.itemSelected : styles.item}
+                        ${isSemiSelected ? styles.itemSemiSelected : ''}
+                      `
+                    return (
+                      <div
+                        key={`${item.key}${index}`}
+                        title={item.title}
+                        data-index={index}
+                        onClick={this.handleSelect}
+                        className={classNames}
+                        onKeyPress={this.handleItemKeyPress} //eslint-disable-line react/jsx-no-bind
+                        ref={index === 0 && this.firstItemElement}
+                      >
+                        <div className={styles.itemIcon}>
+                          {isSelected && <CircleCheckIcon />}
+                          {isSemiSelected && <CircleThinIcon />}
                         </div>
-                      )
-                    })}
-                  </List>
-                </div>
-              </CaptureOutsideClicks>
-            </div>
+                        <div className={styles.itemContent}>{renderItem(item)}</div>
+                      </div>
+                    )
+                  })}
+                </ArrowKeyNavigation>
+              </List>
+              <div tabIndex={0} onFocus={this.handleMenuBlur} />
+            </React.Fragment>
           )}
         </Poppable>
       </div>
