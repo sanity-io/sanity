5335dbf77eb9fa2d226b53edf4310f92bf4082c4
Kristoffer J. Sivertsen <kristoffer@sanity.io>
2018-12-06 11:21:35 +0100

    [components] Deprecate button collection (#1083)

---
+++ packages/@sanity/components/src/buttons/ButtonCollection.js
@@ -0,0 +1,5 @@
+import ButtonGrid from './ButtonGrid'
+
+console.warn('part:@sanity/components/buttons/button-collection is deprecated. Use part:@sanity/components/buttons/button-grid')
+
+export default ButtonGrid
