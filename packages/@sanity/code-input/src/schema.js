942828dd5b8a4f7687f5dc638995cc89093cdd5d
Espen Hovlandsdal <espen@hovlandsdal.com>
2018-12-12 16:28:45 +0100

    [code-input] Implement schema part, better readme, use lazy requires (#1098)

--- packages/@sanity/code-input/src/schema.js
+++ packages/@sanity/code-input/src/schema.js
@@ -1,9 +1,21 @@
-import Preview from './Preview'
+/* eslint-disable react/no-multi-comp */
+import React from 'react'
+
+const Input = props => {
+  const CodeInput = require('./CodeInput').default
+  return <CodeInput {...props} />
+}
+
+const Preview = props => {
+  const CodePreview = require('./Preview').default
+  return <CodePreview {...props} />
+}
 
 export default {
   name: 'code',
   type: 'object',
   title: 'Code',
+  inputComponent: Input,
   fields: [
     {
       title: 'Code',
