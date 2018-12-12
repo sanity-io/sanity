fa5701de37166bf15fb89c134cdd8cec1cb8077f
Espen Hovlandsdal <espen@hovlandsdal.com>
2018-12-12 11:55:38 +0100

    [core] Add support for wildcard + credential-less CORS-origins (#1093)

--- packages/@sanity/core/src/actions/cors/addCorsOrigin.js
+++ packages/@sanity/core/src/actions/cors/addCorsOrigin.js
@@ -1,10 +1,28 @@
 const url = require('url')
+const logSymbols = require('log-symbols')
+const oneline = require('oneline')
 
-module.exports = async function addCorsOrigin(givenOrigin, context) {
-  const {apiClient, prompt} = context
+const wildcardReplacement = 'a-wild-card-r3pl4c3m3n7-a'
+const portReplacement = ':7777777'
+
+module.exports = async function addCorsOrigin(givenOrigin, flags, context) {
+  const {apiClient, prompt, output} = context
   const origin = await (givenOrigin
     ? filterAndValidateOrigin(givenOrigin)
     : promptForOrigin(prompt))
+
+  const hasWildcard = origin.includes('*')
+  if (hasWildcard && !(await promptForWildcardConfirmation(origin, context))) {
+    return false
+  }
+  const allowCredentials =
+    typeof flags.credentials === 'undefined'
+      ? await promptForCredentials(hasWildcard, context)
+      : Boolean(flags.credentials)
+
+  if (givenOrigin !== origin) {
+    output.print(`Normalized origin to ${origin}`)
+  }
 
   const client = apiClient({
     requireUser: true,
@@ -14,8 +32,69 @@ module.exports = async function addCorsOrigin(givenOrigin, context) {
   return client.request({
     method: 'POST',
     url: '/cors',
-    body: {origin},
+    body: {origin, allowCredentials},
     maxRedirects: 0
+  })
+}
+
+function promptForCredentials(hasWildcard, context) {
+  const {prompt, output, chalk} = context
+
+  output.print('')
+  if (hasWildcard) {
+    output.print(oneline`
+      ${chalk.yellow(`${logSymbols.warning} Warning:`)}
+      We ${chalk.red(chalk.underline('HIGHLY'))} recommend NOT allowing credentials
+      on origins containing wildcards. If you are logged in to a studio, people will
+      be able to send requests ${chalk.underline('on your behalf')} to read and modify
+      data, from any matching origin. Please tread carefully!
+    `)
+  } else {
+    output.print(oneline`
+      ${chalk.yellow(`${logSymbols.warning} Warning:`)}
+      Should this origin be allowed to send requests using authentication tokens or
+      session cookies? Be aware that any script on this origin will be able to send
+      requests ${chalk.underline('on your behalf')} to read and modify data if you
+      are logged in to a Sanity studio. If this origin hosts a studio, you will need
+      this, otherwise you should probably answer "No" (n).
+    `)
+  }
+
+  output.print('')
+
+  return prompt.single({
+    type: 'confirm',
+    message: oneline`
+      Allow credentials to be sent from this origin? Please read the warning above.
+    `,
+    default: false
+  })
+}
+
+function promptForWildcardConfirmation(origin, context) {
+  const {prompt, output, chalk} = context
+
+  output.print('')
+  output.print(chalk.yellow(`${logSymbols.warning} Warning: Examples of allowed origins:`))
+
+  if (origin === '*') {
+    output.print('- http://www.some-malicious.site')
+    output.print('- https://not.what-you-were-expecting.com')
+    output.print('- https://high-traffic-site.com')
+    output.print('- http://192.168.1.1:8080')
+  } else {
+    output.print(`- ${origin.replace(/:\*/, ':1234').replace(/\*/g, 'foo')}`)
+    output.print(`- ${origin.replace(/:\*/, ':3030').replace(/\*/g, 'foo.bar')}`)
+  }
+
+  output.print('')
+
+  return prompt.single({
+    type: 'confirm',
+    message: oneline`
+      Using wildcards can be ${chalk.red('risky')}.
+      Are you ${chalk.underline('absolutely sure')} you want to allow this origin?`,
+    default: false
   })
 }
 
@@ -29,25 +108,33 @@ function promptForOrigin(prompt) {
 }
 
 function filterOrigin(origin) {
-  if (origin === '*') {
-    return '*'
+  if (origin === '*' || origin === 'file:///*') {
+    return origin
   }
 
   try {
-    const parsed = url.parse(origin)
+    const example = origin
+      .replace(/([^:])\*/g, `$1${wildcardReplacement}`)
+      .replace(/:\*/, portReplacement)
+
+    const parsed = url.parse(example)
     if (!/^https?:$/.test(parsed.protocol || '')) {
       return null
     }
 
-    const host = parsed.host.replace(/:(80|443)$/, '')
+    const host = parsed.host
+      .replace(/:(80|443)$/, '')
+      .replace(portReplacement, ':*')
+      .replace(new RegExp(wildcardReplacement, 'g'), '*')
+
     return `${parsed.protocol}//${host}`
   } catch (err) {
     return null
   }
 }
 
-function validateOrigin(origin) {
-  if (origin === '*') {
+function validateOrigin(origin, givenOrigin) {
+  if (origin === '*' || origin === 'file:///*') {
     return true
   }
 
@@ -58,12 +145,16 @@ function validateOrigin(origin) {
     // Fall-through to error
   }
 
-  return 'Invalid origin, must include protocol (http://some.host)'
+  if (/^file:\/\//.test(givenOrigin)) {
+    return `Only a local file wildcard is currently allowed: file:///*`
+  }
+
+  return `Invalid origin "${givenOrigin}", must include protocol (https://some.host)`
 }
 
 function filterAndValidateOrigin(givenOrigin) {
   const origin = filterOrigin(givenOrigin)
-  const result = validateOrigin(origin)
+  const result = validateOrigin(origin, givenOrigin)
   if (result !== true) {
     throw new Error(result)
   }
