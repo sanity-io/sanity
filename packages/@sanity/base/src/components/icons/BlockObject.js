46b9db4043e823ea85079aedad091accfadc45e1
Kristoffer J. Sivertsen <kristoffer@sanity.io>
2018-12-13 17:57:21 +0100

    [base] Icons, part definitions CSS for the new block editor

---
+++ packages/@sanity/base/src/components/icons/BlockObject.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const BlockObjectIcon = () => (
+  <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <g clipPath="url(#clip0)">
+      <rect x="2.5" y="7.5" width="19" height="11" stroke="currentColor" />
+      <line x1="2" y1="5.5" x2="22" y2="5.5" stroke="currentColor" />
+      <line x1="2" y1="20.5" x2="22" y2="20.5" stroke="currentColor" />
+    </g>
+  </svg>
+)
+
+export default BlockObjectIcon
