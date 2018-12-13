5cfebf599677190bd58e82469525a6123809c59d
Per-Kristian Nordnes <per.kristian.nordnes@gmail.com>
2018-12-13 16:24:58 +0100

    [form-builder] Remove old block editor folder

--- packages/@sanity/form-builder/src/inputs/BlockEditor-slate/createSpanNode.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import FormBuilderSpan from './FormBuilderSpan'
-
-export default function createSpanNode(type) {
-  return function SpanNode(props) {
-    return <FormBuilderSpan type={type} {...props} />
-  }
-}
