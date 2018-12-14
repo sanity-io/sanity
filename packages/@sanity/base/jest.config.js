f812d4069429d014f3e8445adb02afb82ebe29cf
Per-Kristian Nordnes <per.kristian.nordnes@gmail.com>
2018-12-14 11:29:54 +0100

    [base] Mock 'part:@sanity/base/schema?' to pass test

--- packages/@sanity/base/jest.config.js
+++ packages/@sanity/base/jest.config.js
@@ -1,4 +1,8 @@
 module.exports = {
   transform: {'^.+\\.jsx?$': 'babel-jest'},
-  testRegex: 'test\\/.*\\.test\\.js'
+  testRegex: 'test\\/.*\\.test\\.js',
+  testURL: 'http://localhost/',
+  moduleNameMapper: {
+    '^part:@sanity/base/schema?': '<rootDir>/test/mocks/schema.js'
+  }
 }
