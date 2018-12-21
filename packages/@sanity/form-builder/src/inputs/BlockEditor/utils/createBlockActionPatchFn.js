30364b711ca849e47f798d324989a9d4c51d55c6
Per-Kristian Nordnes <per.kristian.nordnes@gmail.com>
2018-12-21 20:21:35 +0100

    [form-builder] Block editor: improve block action patch functions (#1138)

--- packages/@sanity/form-builder/src/inputs/BlockEditor/utils/createBlockActionPatchFn.js
+++ packages/@sanity/form-builder/src/inputs/BlockEditor/utils/createBlockActionPatchFn.js
@@ -2,26 +2,24 @@
 
 import type {FormBuilderValue, Block} from '../typeDefs'
 import PatchEvent, {insert, unset, set} from '../../../../PatchEvent'
-import {randomKey, normalizeBlock} from '@sanity/block-tools'
+import {normalizeBlock} from '@sanity/block-tools'
 
 export default function createBlockActionPatchFn(
   type: string,
   block: FormBuilderValue,
   onPatch: PatchEvent => void
 ) {
+  let toInsert
   return (givenBlock: Block) => {
     switch (type) {
       case 'set':
-        return onPatch(PatchEvent.from(set(givenBlock, [{_key: block._key}])))
+        return onPatch(PatchEvent.from(set(normalizeBlock(givenBlock), [{_key: block._key}])))
       case 'unset':
         return onPatch(PatchEvent.from(unset([{_key: block._key}])))
       case 'insert':
-        // Make sure the given block key's are unique and normalized
-        givenBlock._key = randomKey(12)
-        if (givenBlock._type === 'block') {
-          normalizeBlock(givenBlock)
-        }
-        return onPatch(PatchEvent.from(insert([givenBlock], 'after', [{_key: block._key}])))
+        toInsert = Array.isArray(givenBlock) ? givenBlock : [givenBlock]
+        toInsert = toInsert.map(blk => normalizeBlock(blk))
+        return onPatch(PatchEvent.from(insert(toInsert, 'after', [{_key: block._key}])))
       default:
         throw new Error(`Patch type ${type} not supported`)
     }
