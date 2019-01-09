f3f6ce43518602ece470422b4f61abf7e41b997f
Kristoffer J. Sivertsen <kristofferjoachim@gmail.com>
2019-01-09 15:38:43 +0100

    [backstop-test-studio] Adding studio

---
+++ packages/backstop-test-studio/schemas/empty.js
@@ -0,0 +1,13 @@
+export default {
+  name: 'empty',
+  type: 'document',
+  title: 'Empty',
+  description: 'Do not put content here',
+  fields: [
+    {
+      name: 'name',
+      title: 'Name',
+      type: 'string'
+    }
+  ]
+}
---
+++ packages/backstop-test-studio/src/components/PertEstimate.js
@@ -0,0 +1,2 @@
+export {default as Input} from './PertEstimateInput'
+export {default as schema} from './PertEstimateSchema'
