c19a554d988df1a6572625fb139b48c096e4d988
Kristoffer J. Sivertsen <kristofferjoachim@gmail.com>
2019-01-09 17:20:37 +0100

    [ci] Backstop tweaks

--- scripts/backstop.sh
+++ scripts/backstop.sh
@@ -4,6 +4,7 @@ set -u
 # Deploy test studio
 cd /drone/src/github.com/sanity-io/sanity/packages/backstop-test-studio
 SANITY_AUTH_TOKEN=${BACKSTOP_AUTH_TOKEN} /drone/src/github.com/sanity-io/sanity/packages/@sanity/cli/bin/sanity deploy
+sleep 10
 # Do the visual backstop test
 cd /drone/src/github.com/sanity-io/sanity
 npm run test:backstop
@@ -14,10 +15,10 @@ if [ $test_exit_code -ne 0 ]; then
   cp -R /drone/src/github.com/sanity-io/sanity/backstop_data /drone/src/github.com/sanity-io/sanity/packages/backstop-test-studio/static/.
   cd /drone/src/github.com/sanity-io/sanity/packages/backstop-test-studio
   SANITY_AUTH_TOKEN=${BACKSTOP_AUTH_TOKEN} /drone/src/github.com/sanity-io/sanity/packages/@sanity/cli/bin/sanity deploy
-  echo "Go to https://backstop.sanity.studio/static/backstop_data/htmlReport/index.html to view diff"
+  echo "### VISUAL TEST FAILED ### Go to https://backstop.sanity.studio/static/backstop_data/htmlReport/index.html to view diff"
   # Notify the design team
   curl -sSf -XPOST "$SLACK_WEBHOOK" --data-urlencode 'payload={
-    "channel": "#design",
+    "channel": "#design-issues",
     "username": "Backstop tester",
     "text": "Visual issues. '"<$DRONE_BUILD_LINK|$DRONE_REPO_NAME #$DRONE_BUILD_NUMBER> ($DRONE_BRANCH) by $DRONE_COMMIT_AUTHOR: <$DRONE_COMMIT_LINK|#${DRONE_COMMIT:0:7}>"' Go to https://backstop.sanity.studio/static/backstop_data/htmlReport/index.html to view diff"
   }'
