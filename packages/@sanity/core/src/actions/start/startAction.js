1e43ac4afaeabd7ab6bfe4ac39e06ba86132495f
Espen Hovlandsdal <espen@hovlandsdal.com>
2019-01-15 23:07:49 +0100

    [core] Prompt for projectId/dataset on start if missing

--- packages/@sanity/core/src/actions/start/startAction.js
+++ packages/@sanity/core/src/actions/start/startAction.js
@@ -1,15 +1,22 @@
 import path from 'path'
 import chalk from 'chalk'
+import fse from 'fs-extra'
+import {isPlainObject} from 'lodash'
 import {promisify} from 'es6-promisify'
 import {getDevServer} from '@sanity/server'
 import getConfig from '@sanity/util/lib/getConfig'
+import chooseDatasetPrompt from '../dataset/chooseDatasetPrompt'
 import {tryInitializePluginConfigs} from '../../actions/config/reinitializePluginConfigs'
 import checkReactCompatibility from '../../util/checkReactCompatibility'
+import debug from '../../debug'
 import {formatMessage, isLikelyASyntaxError} from './formatMessage'
 
 export default async (args, context) => {
   const flags = args.extOptions
   const {output, workDir} = context
+
+  await ensureProjectConfig(context)
+
   const sanityConfig = getConfig(workDir)
   const config = sanityConfig.get('server')
   const {port, hostname} = config
@@ -81,9 +88,7 @@ export default async (args, context) => {
       printWarnings(output, warnings)
     }
 
-    output.print(
-      chalk.green(`Content Studio listening on http://${httpHost}:${httpPort}`)
-    )
+    output.print(chalk.green(`Content Studio listening on http://${httpHost}:${httpPort}`))
   })
 
   function resetSpinner() {
@@ -95,6 +100,71 @@ export default async (args, context) => {
   }
 }
 
+async function ensureProjectConfig(context) {
+  const {workDir, output} = context
+  const manifestPath = path.join(workDir, 'sanity.json')
+  const projectManifest = await fse.readJson(manifestPath)
+  const apiConfig = projectManifest.api
+  if (typeof apiConfig !== 'undefined' && !isPlainObject(apiConfig)) {
+    throw new Error('Invalid `api` property in `sanity.json` - should be an object')
+  }
+
+  let displayName = projectManifest.project && projectManifest.project.displayName
+  let {projectId, dataset} = apiConfig || {}
+  const configMissing = !projectId || !dataset
+  if (!configMissing) {
+    return
+  }
+
+  output.print('Project configuration required before starting studio')
+  output.print('')
+
+  if (!projectId) {
+    const selected = await getOrCreateProject(context)
+    projectId = selected.projectId
+    displayName = selected.displayName
+  }
+
+  if (!dataset) {
+    const client = context
+      .apiClient({requireUser: true, requireProject: false})
+      .config({projectId, useProjectHostname: true})
+
+    const apiClient = () => client
+    const projectContext = {...context, apiClient}
+    dataset = await chooseDatasetPrompt(projectContext, {allowCreation: true})
+  }
+
+  // Rewrite project manifest (sanity.json)
+  const projectInfo = projectManifest.project || {}
+  const newProps = {
+    root: true,
+    api: {
+      ...(projectManifest.api || {}),
+      projectId,
+      dataset
+    },
+    project: {
+      ...projectInfo,
+      // Keep original name if present
+      name: projectInfo.name || displayName
+    }
+  }
+
+  // Ensure root, api and project keys are at top to follow sanity.json key order convention
+  await fse.outputJSON(
+    manifestPath,
+    {
+      ...newProps,
+      ...projectManifest,
+      ...newProps
+    },
+    {spaces: 2}
+  )
+
+  output.print('Project ID / dataset configured')
+}
+
 function resolveStaticPath(rootDir, config) {
   const {staticPath} = config
   return path.isAbsolute(staticPath) ? staticPath : path.resolve(path.join(rootDir, staticPath))
@@ -102,9 +172,7 @@ function resolveStaticPath(rootDir, config) {
 
 function gracefulDeath(httpHost, config, err) {
   if (err.code === 'EADDRINUSE') {
-    throw new Error(
-      'Port number is already in use, configure `server.port` in `sanity.json`'
-    )
+    throw new Error('Port number is already in use, configure `server.port` in `sanity.json`')
   }
 
   if (err.code === 'EACCES') {
@@ -113,7 +181,9 @@ function gracefulDeath(httpHost, config, err) {
         ? 'port numbers below 1024 requires root privileges'
         : `do you have access to listen to the given host (${httpHost})?`
 
-    throw new Error(`The Content Studio server does not have access to listen to given port - ${help}`) // eslint-disable-line max-len
+    throw new Error(
+      `The Content Studio server does not have access to listen to given port - ${help}`
+    ) // eslint-disable-line max-len
   }
 
   throw err
@@ -138,8 +208,71 @@ function printWarnings(output, warnings) {
   output.print(chalk.yellow('Compiled with warnings.'))
   output.print()
 
-  warnings.map(message => `Warning in ${formatMessage(message)}`).forEach(message => {
-    output.print(message)
-    output.print()
+  warnings
+    .map(message => `Warning in ${formatMessage(message)}`)
+    .forEach(message => {
+      output.print(message)
+      output.print()
+    })
+}
+
+async function getOrCreateProject(context) {
+  const {prompt, apiClient} = context
+
+  let projects
+  try {
+    projects = await apiClient({requireProject: false}).projects.list()
+  } catch (err) {
+    throw new Error(`Failed to communicate with the Sanity API:\n${err.message}`)
+  }
+
+  if (projects.length === 0) {
+    debug('No projects found for user, prompting for name')
+    const projectName = await prompt.single({message: 'Project name'})
+    return createProject(apiClient, {displayName: projectName})
+  }
+
+  debug(`User has ${projects.length} project(s) already, showing list of choices`)
+
+  const projectChoices = projects.map(project => ({
+    value: project.id,
+    name: `${project.displayName} [${project.id}]`
+  }))
+
+  const selected = await prompt.single({
+    message: 'Select project to use',
+    type: 'list',
+    choices: [{value: 'new', name: 'Create new project'}, new prompt.Separator(), ...projectChoices]
   })
+
+  if (selected === 'new') {
+    debug('User wants to create a new project, prompting for name')
+    return createProject(apiClient, {
+      displayName: await prompt.single({
+        message: 'Informal name for your project'
+      })
+    })
+  }
+
+  debug(`Returning selected project (${selected})`)
+  return {
+    projectId: selected,
+    displayName: projects.find(proj => proj.id === selected).displayName
+  }
+}
+
+function createProject(apiClient, options) {
+  return apiClient({
+    requireUser: true,
+    requireProject: false
+  })
+    .request({
+      method: 'POST',
+      uri: '/projects',
+      body: options
+    })
+    .then(response => ({
+      projectId: response.projectId || response.id,
+      displayName: options.displayName || ''
+    }))
 }
