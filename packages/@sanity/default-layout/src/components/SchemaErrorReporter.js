15ab1f238346373cd16eaeb460972d4727a01089
Per-Kristian Nordnes <per.kristian.nordnes@gmail.com>
2019-01-25 12:32:45 +0100

    Validate array type of types for object without a name when used in combination with a block type (#1171)
    * [default-layout] Fix unique keys and add propTypes for SchemaErrorReporter
    
    * [schema] Don't allow object types without a name when used with a block type

--- packages/@sanity/default-layout/src/components/SchemaErrorReporter.js
+++ packages/@sanity/default-layout/src/components/SchemaErrorReporter.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import schema from 'part:@sanity/base/schema'
-import {SchemaErrors} from './SchemaErrors'
+import SchemaErrors from './SchemaErrors'
 
 function renderPath(path) {
   return path
