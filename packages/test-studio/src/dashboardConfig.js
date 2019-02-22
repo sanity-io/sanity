aee1c6645e5df25cd1a944f90d985d39b5b59ad1
Thomas Drevon <thomas@sanity.io>
2019-02-22 17:18:22 +0700

    [test-studio] Sample configuration of project-info widget

--- packages/test-studio/src/dashboardConfig.js
+++ packages/test-studio/src/dashboardConfig.js
@@ -3,6 +3,17 @@ export default {
     {name: 'stack-overview', layout: {width: 'full'}},
     {name: 'document-count'},
     {name: 'cats', layout: {width: 'medium'}, options: {imageWidth: 50}},
-    {name: 'cats', layout: {width: 'medium'}, options: {imageWidth: 150}}
+    {name: 'cats', layout: {width: 'medium'}, options: {imageWidth: 150}},
+    {
+      name: 'project-info',
+      options: {
+        data: [
+          {title: 'Frontend', value: 'https://asdf.heroku.com/greedy-goblin', category: 'apps'},
+          {title: 'Strange endpoint', value: 'https://example.com/v1/strange', category: 'apis'},
+          {title: 'With strawberry jam?', value: 'Yes', category: 'Waffles'}
+        ]
+      }
+    },
+    {name: 'cats', options: {imageWidth: 90}}
   ]
 }
