5fad35c2eebdbd2699b0e28d191aa3782ee34da1
Thomas Drevon <thomas@sanity.io>
2019-02-22 13:05:55 +0700

    [test-studio] Update format of dashboardConfig

--- packages/test-studio/src/dashboardConfig.js
+++ packages/test-studio/src/dashboardConfig.js
@@ -1,11 +1,8 @@
 export default {
-  sections: [
-    {name: 'stack-overview'},
-    [
-      {name: 'cats', options: {imageWidth: 90}},
-      {name: 'cats', options: {imageWidth: 120}},
-      {name: 'cats', options: {imageWidth: 10}}
-    ],
-    {name: 'document-count'}
+  widgets: [
+    {name: 'stack-overview', layout: {fullWidth: true}},
+    {name: 'document-count'},
+    {name: 'cats', options: {imageWidth: 50}},
+    {name: 'cats', options: {imageWidth: 150}}
   ]
 }
