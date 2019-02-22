fe1a7a2dd3f1c0b5bda8fd602ec024c91795d00e
Thomas Drevon <thomas@sanity.io>
2019-02-22 12:28:20 +0700

    [test-studio] Add some plugins to make the dashboard tool show something

---
+++ packages/test-studio/src/dashboardConfig.js
@@ -0,0 +1,11 @@
+export default {
+  sections: [
+    {name: 'stack-overview'},
+    [
+      {name: 'cats', options: {imageWidth: 90}},
+      {name: 'cats', options: {imageWidth: 120}},
+      {name: 'cats', options: {imageWidth: 10}}
+    ],
+    {name: 'document-count'}
+  ]
+}
