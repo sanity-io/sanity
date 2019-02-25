70dafdba25fcb4cd4e7bd50bd522257b0b54d69f
Thomas Drevon <thomas@sanity.io>
2019-02-25 12:13:27 +0100

    [dashboard] First pass at project-users widget

--- packages/@sanity/dashboard/src/dashboardConfig.js
+++ packages/@sanity/dashboard/src/dashboardConfig.js
@@ -1,3 +1,8 @@
 export default {
-  widgets: [{name: 'sanity-tutorials', layout: {width: 'full'}}]
+  widgets: [
+    {name: 'sanity-tutorials', layout: {width: 'full'}},
+    {name: 'project-info'},
+    {name: 'project-users'},
+    {name: 'document-list'}
+  ]
 }
