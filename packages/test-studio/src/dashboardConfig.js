93c1b4db34c649ab8fb43b87cd3e1ef44d694f23
Kristoffer Sivertsen <kristoffer@sanity.io>
2019-02-27 16:35:25 +0700

    [dashboard] Remove stack-overview

--- packages/test-studio/src/dashboardConfig.js
+++ packages/test-studio/src/dashboardConfig.js
@@ -4,8 +4,6 @@ export default {
     {name: 'document-list'},
     {name: 'document-list', options: {title: 'Last edited', order: '_updatedAt desc'}},
     {name: 'document-list', options: {title: 'Last created books', types: ['book']}},
-    {name: 'document-list', options: {title: 'Last created authors', types: ['author']}},
-    {name: 'document-count'},
     {name: 'project-users'},
     {
       name: 'project-info',
@@ -19,8 +17,6 @@ export default {
           {title: 'With strawberry jam?', value: 'Yes', category: 'Waffles'}
         ]
       }
-    },
-    // {name: 'cats', options: {imageWidth: 90}},
-    {name: 'stack-overview', layout: {width: 'full'}}
+    }
   ]
 }
