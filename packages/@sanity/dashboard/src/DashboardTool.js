6e29a35222641c15d5eb30f77153a63e8e36dbbe
Kristoffer Sivertsen <kristoffer@sanity.io>
2019-02-28 15:44:02 +0100

    [dashboard-tool] Adding correct icon

--- packages/@sanity/dashboard/src/DashboardTool.js
+++ packages/@sanity/dashboard/src/DashboardTool.js
@@ -1,5 +1,16 @@
-import Icon from 'react-icons/lib/go/dashboard'
+import React from 'react'
 import DashboardLayout from './components/DashboardLayout'
+
+const Icon = () => (
+  <svg width="25" height="25" viewBox="0 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M5 5H12V10V11V14H5V5ZM12 15H5V20H12V15ZM12 21H5H4V20V15V14V5V4H5H12H13H20H21V5V10V11V20V21H20H13H12ZM20 11H13V14V15V20H20V11ZM20 10V5H13V10H20Z"
+      fill="currentColor"
+    />
+  </svg>
+)
 
 export default {
   title: 'Dashboard',
