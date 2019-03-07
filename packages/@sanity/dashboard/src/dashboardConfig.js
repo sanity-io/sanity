b0a9f1d5f941f688314f314d8b3892586731995b
Thomas Drevon <thomas@sanity.io>
2019-03-07 13:49:16 +0100

    [dashboard] Extract DocumentList widget to its own repo

--- packages/@sanity/dashboard/src/dashboardConfig.js
+++ packages/@sanity/dashboard/src/dashboardConfig.js
@@ -2,7 +2,6 @@ export default {
   widgets: [
     {name: 'sanity-tutorials', layout: {width: 'full'}},
     {name: 'project-info'},
-    {name: 'project-users'},
-    {name: 'document-list'}
+    {name: 'project-users'}
   ]
 }
