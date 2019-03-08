f6b4ae7e4818df38d1bb0622ef5e5520ba13c333
Marius Lundgård <studio@mariuslundgard.com>
2019-03-08 11:26:02 +0100

    [dashboard] Add design tweaks

--- packages/@sanity/dashboard/src/widgets/sanityTutorials/Tutorial.js
+++ packages/@sanity/dashboard/src/widgets/sanityTutorials/Tutorial.js
@@ -2,26 +2,9 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import styles from './Tutorial.css'
 
-const PlayIcon = (
-  <svg
-    width="44"
-    height="44"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-    className={styles.playIcon}
-  >
-    <circle cx="22" cy="22" r="21.5" stroke="currentColor" />
-    <path
-      d="M30.643 22.848L17.53 31.044a1 1 0 0 1-1.53-.848V13.804a1 1 0 0 1 1.53-.848l13.113 8.196a1 1 0 0 1 0 1.696z"
-      fill="currentColor"
-    />
-  </svg>
-)
-
 class Tutorial extends React.PureComponent {
   static propTypes = {
     title: PropTypes.string.isRequired,
-    hasVideo: PropTypes.bool.isRequired,
     posterURL: PropTypes.string,
     href: PropTypes.string.isRequired,
     presenterName: PropTypes.string.isRequired,
@@ -32,13 +15,12 @@ class Tutorial extends React.PureComponent {
   }
 
   render() {
-    const {title, posterURL, href, presenterName, presenterSubtitle, hasVideo} = this.props
+    const {title, posterURL, href, presenterName, presenterSubtitle} = this.props
 
     return (
       <a className={styles.root} href={href}>
         <div className={styles.posterContainer}>
           <img className={styles.poster} src={posterURL} />
-          {hasVideo && PlayIcon}
         </div>
         <h3 className={styles.title}>{title}</h3>
         <div className={styles.byLine}>
