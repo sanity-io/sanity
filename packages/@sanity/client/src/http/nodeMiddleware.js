63c56254c69cbbe25a7a9cd8da3e3a9f7d0dd95d
Espen Hovlandsdal <espen@hovlandsdal.com>
2019-03-13 15:49:08 +0100

    [client] Introduce keepAlive in client (#1235)

--- packages/@sanity/client/src/http/nodeMiddleware.js
+++ packages/@sanity/client/src/http/nodeMiddleware.js
@@ -1,13 +1,15 @@
 const retry = require('get-it/lib-node/middleware/retry')
 const debug = require('get-it/lib-node/middleware/debug')
 const headers = require('get-it/lib-node/middleware/headers')
+const keepAlive = require('get-it/lib-node/middleware/keepAlive')
 
 const pkg = require('../../package.json')
 
 const middleware = [
   debug({verbose: true, namespace: 'sanity:client'}),
   headers({'User-Agent': `${pkg.name} ${pkg.version}`}),
-  retry({maxRetries: 3})
+  retry({maxRetries: 3}),
+  keepAlive({ms: 75000})
 ]
 
 module.exports = middleware
