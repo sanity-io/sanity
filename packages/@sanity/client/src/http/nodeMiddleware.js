d155bce714b93909d506d97067146557f409d505
Espen Hovlandsdal <espen@hovlandsdal.com>
2019-03-15 14:50:30 +0100

    [client] Roll back keep-alive change for now

--- packages/@sanity/client/src/http/nodeMiddleware.js
+++ packages/@sanity/client/src/http/nodeMiddleware.js
@@ -1,15 +1,13 @@
 const retry = require('get-it/lib-node/middleware/retry')
 const debug = require('get-it/lib-node/middleware/debug')
 const headers = require('get-it/lib-node/middleware/headers')
-const keepAlive = require('get-it/lib-node/middleware/keepAlive')
 
 const pkg = require('../../package.json')
 
 const middleware = [
   debug({verbose: true, namespace: 'sanity:client'}),
   headers({'User-Agent': `${pkg.name} ${pkg.version}`}),
-  retry({maxRetries: 3}),
-  keepAlive({ms: 75000})
+  retry({maxRetries: 3})
 ]
 
 module.exports = middleware
