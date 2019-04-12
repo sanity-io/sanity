6176f97f9d94361094088d5aaaaaa4ce253d4d17
Espen Hovlandsdal <espen@hovlandsdal.com>
2019-04-11 17:53:22 -0700

    [default-login] Fix synchronous setState in constructor

--- packages/@sanity/default-login/src/LoginWrapper.js
+++ packages/@sanity/default-login/src/LoginWrapper.js
@@ -12,6 +12,7 @@ import UnauthorizedUser from './UnauthorizedUser'
 
 const isProjectLogin = client.config().useProjectHostname
 const projectId = isProjectLogin && client.config().projectId ? client.config().projectId : null
+
 export default class LoginWrapper extends React.PureComponent {
   static propTypes = {
     children: PropTypes.oneOfType([PropTypes.node, PropTypes.func]).isRequired,
@@ -36,10 +37,25 @@ export default class LoginWrapper extends React.PureComponent {
 
   constructor(props) {
     super(props)
+
+    let sync = true
+
     this.userSubscription = userStore.currentUser.subscribe({
-      next: evt => this.setState({user: evt.user, error: evt.error, isLoading: false}),
+      next: evt => {
+        // Because observables _can_ be syncronous, it's not safe to call `setState` as it is a noop
+        // We must therefore explicitly check whether or not we were call syncronously
+        const newState = {user: evt.user, error: evt.error, isLoading: false}
+        if (sync) {
+          // eslint-disable-next-line react/no-direct-mutation-state
+          this.state = {...this.state, ...newState}
+        } else {
+          this.setState(newState)
+        }
+      },
       error: error => this.setState({error, isLoading: false})
     })
+
+    sync = false
   }
 
   componentWillUnmount() {
