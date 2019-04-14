8a4744b3a2631408204936ea2d81f5c373a5c8c4
Espen Hovlandsdal <espen@hovlandsdal.com>
2019-04-14 12:41:49 -0700

    [form-builder] Support additional filter to reference input

--- packages/@sanity/form-builder/src/sanity/inputs/client-adapters/reference.ts
+++ packages/@sanity/form-builder/src/sanity/inputs/client-adapters/reference.ts
@@ -7,8 +7,8 @@ export function getPreviewSnapshot(value, referenceType) {
   return observeForPreview(value, referenceType).pipe(map((result: any) => result.snapshot))
 }
 
-export function search(textTerm, referenceType) {
-  const doSearch = createWeightedSearch(referenceType.to, client)
+export function search(textTerm, referenceType, options) {
+  const doSearch = createWeightedSearch(referenceType.to, client, options)
   return doSearch(textTerm, {includeDrafts: false}).pipe(
     map((results: any[]) => results.map(res => res.hit))
   )
