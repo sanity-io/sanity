9438935ba46abde5f61b6da05e27dd8cff1dfa1a
Per-Kristian Nordnes <per.kristian.nordnes@gmail.com>
2019-05-16 15:51:52 +0200

    Fix block editor inline bug (#1300)
    * [form-builder] Block editor: make TogglePlaceHolderPlugin toggle on insertInlineObject commands
    
    * [form-builder] Block editor: Make sure that the editors block keys matches doc data  when inserting InlineObjects

--- packages/@sanity/form-builder/src/inputs/BlockEditor/plugins/InsertInlineObjectPlugin.js
+++ packages/@sanity/form-builder/src/inputs/BlockEditor/plugins/InsertInlineObjectPlugin.js
@@ -1,9 +1,24 @@
 // @flow
 import {Inline} from 'slate'
 import {randomKey} from '@sanity/block-tools'
+import {editorValueToBlocks, blocksToEditorValue} from '@sanity/block-tools'
 import type {SlateEditor, Type} from '../typeDefs'
+import {VALUE_TO_JSON_OPTS} from '../utils/createOperationToPatches'
 
 export default function InsertInlineObjectPlugin(blockContentType: Type) {
+  function normalizeBlock(block) {
+    return blocksToEditorValue(
+      editorValueToBlocks(
+        {
+          document: {
+            nodes: [block.toJSON(VALUE_TO_JSON_OPTS)]
+          }
+        },
+        blockContentType
+      ),
+      blockContentType
+    ).document.nodes[0]
+  }
   return {
     onCommand(command: any, editor: SlateEditor, next: void => void) {
       if (command.type !== 'insertInlineObject') {
@@ -23,6 +38,12 @@ export default function InsertInlineObjectPlugin(blockContentType: Type) {
       }
       const inline = Inline.create(inlineProps)
       editor.insertInline(inline)
+
+      // Normalize the keys in the block nodes to match what is sent to gradient
+      const inlinePath = editor.value.selection.focus.path
+      const block = editor.value.focusBlock
+      editor.replaceNodeByKey(block.key, normalizeBlock(block))
+      editor.moveTo(inlinePath, 0)
       return editor
     }
   }
