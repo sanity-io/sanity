26e24691685a0cc1a9e68310d256f4585a4bc10c
Espen Hovlandsdal <espen@hovlandsdal.com>
2019-05-24 17:20:18 +0200

    [core] Cast document IDs to string (#1317)

--- packages/@sanity/core/src/commands/documents/deleteDocumentsCommand.js
+++ packages/@sanity/core/src/commands/documents/deleteDocumentsCommand.js
@@ -19,9 +19,9 @@ export default {
   helpText,
   description: 'Delete a document by ID',
   action: async (args, context) => {
-    const {apiClient, output} = context
+    const {apiClient, output, chalk} = context
     const {dataset} = args.extOptions
-    const [id] = args.argsWithoutOptions
+    const [id] = args.argsWithoutOptions.map(str => `${str}`)
 
     if (!id) {
       throw new Error('Document ID must be specified')
@@ -38,7 +38,7 @@ export default {
       if (results.length > 0 && results[0].operation === 'delete') {
         output.print('Document deleted')
       } else {
-        output.print(`Document with ID "${id}" not found`)
+        output.error(chalk.red(`Document with ID "${id}" not found`))
       }
     } catch (err) {
       throw new Error(`Failed to delete document:\n${err.message}`)
