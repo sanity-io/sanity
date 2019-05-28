5fe84efa5b37174afbc6407aca49ac3348461fd6
Thomas Drevon <thomas@sanity.io>
2019-05-28 13:40:38 +0200

    [desk-tool] Display EmptyListPand in case of no document types in schema

--- packages/@sanity/desk-tool/src/defaultStructure.js
+++ packages/@sanity/desk-tool/src/defaultStructure.js
@@ -1,7 +1,15 @@
+import React from 'react'
 import Structure from '../structure-builder'
+import EmptyListPane from './pane/EmptyListPane'
 
 export default () => {
   const items = Structure.documentTypeListItems()
+  if (items.length === 0) {
+    return Structure.component({
+      id: 'empty-list-pane',
+      component: <EmptyListPane />
+    })
+  }
   return Structure.list()
     .id('__root__')
     .title('Content')
