8b37da28c69999f6f611c4e086f024d9b1cdbfd1
Thomas Drevon <thomas@sanity.io>
2019-05-29 17:14:07 +0200

    [history-store] Set apiHost depending on environment

--- packages/@sanity/history-store/src/utils/Client.ts
+++ packages/@sanity/history-store/src/utils/Client.ts
@@ -1,11 +1,5 @@
 import getDefaultModule from './getDefaultModule'
 
-const clientConfig = {
-  apiHost: 'https://api.sanity.work',
-  useProjectHostname: true,
-  useCdn: false
-}
-
 interface SanityClient {
   request(options: { [key: string]: any }): Promise<any>
   fetch(query: string, params: { [key: string]: any }): Promise<any>
@@ -13,10 +7,25 @@ interface SanityClient {
   clientConfig: any
 }
 
+const sanityEnv = process.env.SANITY_ENV || 'production'
+const apiHosts = {
+  production: 'https://api.sanity.io',
+  staging: 'https://api.sanity.work',
+  development: 'http://api.sanity.wtf'
+}
+
+const getClientConfig = (): { [key: string]: any } => {
+  return {
+    apiHost: (apiHosts as any)[sanityEnv],
+    useProjectHostname: true,
+    useCdn: false
+  }
+}
+
 // We are lazy-loading the part to work around typescript trying to resolve it
 const client = ((): SanityClient => {
   const client: SanityClient = getDefaultModule(require('part:@sanity/base/client'))
-  return client.config(clientConfig)
+  return client.config(getClientConfig())
 })()
 
 export { client }
