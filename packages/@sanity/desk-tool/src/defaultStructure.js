17b3e1f053d736f4e46c45e4d0f7ee2b2c76e55b
Thomas Drevon <thomas@sanity.io>
2019-05-29 14:04:18 +0200

    [desk-tool] Rename empty list message component and refrain from using any Pane

--- packages/@sanity/desk-tool/src/defaultStructure.js
+++ packages/@sanity/desk-tool/src/defaultStructure.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import Structure from '../structure-builder'
-import EmptyListPane from './pane/EmptyListPane'
+import MissingDocumentTypesMessage from './components/MissingDocumentTypesMessage'
 
 export default () => {
   const items = Structure.documentTypeListItems()
   if (items.length === 0) {
     return Structure.component({
       id: 'empty-list-pane',
-      component: <EmptyListPane />
+      component: <MissingDocumentTypesMessage />
     })
   }
   return Structure.list()
