039255309026581ae05077699014b06fcdcf3ac0
Thomas Drevon <thomas@sanity.io>
2019-05-31 13:48:26 +0200

    [histoy-store] Throw error if getHistory receives both time and rev params

--- packages/@sanity/history-store/src/HistoryFetcher.ts
+++ packages/@sanity/history-store/src/HistoryFetcher.ts
@@ -3,11 +3,19 @@ import { client } from './utils/Client'
 export function getHistory(documentIds: string | string[], options: { [key: string]: any } = {}): Promise<any> {
   const ids = Array.isArray(documentIds) ? documentIds : [documentIds]
   const { time, revision } = options
+
+  if (time && revision) {
+    throw new Error(`getHistory can't handle both time and revision parameters`)
+  }
+
   const dataset = client.clientConfig.dataset
-  const timestamp = time || new Date().toISOString()
-  let url = `/data/history/${dataset}/documents/${ids.join(',')}?time=${timestamp}`
+  let url = `/data/history/${dataset}/documents/${ids.join(',')}`
+
   if (revision) {
-    url = `${url}&revision=${revision}`
+    url = `${url}?revision=${revision}`
+  } else {
+    const timestamp = time || new Date().toISOString()
+    url = `${url}?time=${timestamp}`
   }
 
   return client.request({ url })
