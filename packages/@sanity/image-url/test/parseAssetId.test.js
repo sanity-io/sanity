07093716bdd6bcf6813852a0ffa748440eff8cbc
Espen Hovlandsdal <espen@hovlandsdal.com>
2019-06-13 10:40:55 +0200

    [image-url] Move module out of monorepo (#1359)

--- packages/@sanity/image-url/test/parseAssetId.test.js
+++
@@ -1,9 +0,0 @@
-import parseAssetId from '../src/parseAssetId'
-
-test('throws on invalid document id', () => {
-  expect(() => parseAssetId('moop')).toThrowErrorMatchingSnapshot()
-})
-
-test('throws on invalid dimensions', () => {
-  expect(() => parseAssetId('image-assetId-mooxmoo-png')).toThrowErrorMatchingSnapshot()
-})
