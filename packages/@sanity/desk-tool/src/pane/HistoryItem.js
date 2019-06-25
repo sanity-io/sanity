8e963d3bbd0885883029d2d71d1462a2660c2992
Kristoffer Sivertsen <kristoffer@sanity.io>
2019-06-25 13:33:22 +0200

    [desk-tool] Add history components
    Co-authored-by: Bjørge Næss <bjoerge@gmail.com>
    Co-authored-by: Per-Kristian Nordnes <per.kristian.nordnes@gmail.com>

---
+++ packages/@sanity/desk-tool/src/pane/HistoryItem.js
@@ -0,0 +1,106 @@
+import PropTypes from 'prop-types'
+import React from 'react'
+import HistoryListItem from 'part:@sanity/components/history/list-item'
+import UserStore from 'part:@sanity/base/user'
+import {format, isYesterday, isToday} from 'date-fns'
+
+const {getUsers} = UserStore
+
+const dateFormat = 'MMM D, YYYY, hh:mm A'
+
+function getDateString(date) {
+  if (isToday(date)) {
+    return `Today, ${format(date, 'hh:mm A')}`
+  }
+  if (isYesterday(date)) {
+    return `Yesterday, ${format(date, 'hh:mm A')}`
+  }
+  return format(date, dateFormat)
+}
+
+function getHumanReadableStatus(type) {
+  if (type === 'discardDraft') return 'Discarded Edits'
+  if (type === 'truncated') return 'Truncated Events'
+  if (type === 'unknown') return 'Edited'
+  return type
+}
+
+export default class HistoryItem extends React.PureComponent {
+  static defaultProps = {
+    displayDocumentId: undefined,
+    isSelected: false,
+    userIds: undefined
+  }
+
+  static propTypes = {
+    displayDocumentId: PropTypes.string,
+    endTime: PropTypes.string.isRequired,
+    isCurrentVersion: PropTypes.bool.isRequired,
+    isSelected: PropTypes.bool,
+    onClick: PropTypes.func.isRequired,
+    onSelectNext: PropTypes.func,
+    onSelectPrev: PropTypes.func,
+    rev: PropTypes.string.isRequired,
+    type: PropTypes.oneOf([
+      'created',
+      'discardDraft',
+      'edited',
+      'published',
+      'unpublished',
+      'truncated',
+      'unknown'
+    ]).isRequired,
+    userIds: PropTypes.arrayOf(PropTypes.string)
+  }
+
+  componentDidMount() {
+    const {userIds} = this.props
+    if (!userIds) {
+      return
+    }
+    getUsers(userIds).then(users => {
+      this.setState({users})
+    })
+  }
+
+  handleKeyUp = event => {
+    const {onClick} = this.props
+    if (event.key === 'Enter') {
+      onClick()
+    }
+  }
+
+  handleKeyDown = event => {
+    // Prevent arrow keypress scrolling
+    const {onSelectPrev, onSelectNext} = this.props
+    if (event.key === 'ArrowDown') {
+      onSelectNext()
+      event.preventDefault()
+    } else if (event.key === 'ArrowUp') {
+      onSelectPrev()
+      event.preventDefault()
+    }
+  }
+
+  state = {users: []}
+
+  render() {
+    const {type, endTime, isSelected, isCurrentVersion, rev, onClick} = this.props
+    const {users} = this.state
+    return (
+      <HistoryListItem
+        isCurrentVersion={isCurrentVersion}
+        status={getHumanReadableStatus(type)}
+        type={type}
+        title={getDateString(endTime)}
+        tooltip={format(endTime, dateFormat)}
+        rev={rev}
+        users={users}
+        onClick={onClick}
+        onKeyUp={this.handleKeyUp}
+        onKeyDown={this.handleKeyDown}
+        isSelected={isSelected}
+      />
+    )
+  }
+}
