149493978ab340342165a31810d33453e3443bba
Kristoffer Sivertsen <kristoffer@sanity.io>
2019-07-11 13:26:34 +0200

    [components] Queue snackbar story

--- packages/@sanity/components/src/snackbar/story.js
+++ packages/@sanity/components/src/snackbar/story.js
@@ -1,10 +1,31 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import Snackbar from 'part:@sanity/components/snackbar/default'
 import {storiesOf, action} from 'part:@sanity/storybook'
-import {withKnobs, select, text, number} from 'part:@sanity/storybook/addons/knobs'
+import {withKnobs, select, text, number, button} from 'part:@sanity/storybook/addons/knobs'
 import Sanity from 'part:@sanity/storybook/addons/sanity'
+import SnackbarProvider from 'part:@sanity/components/snackbar/provider'
 
-const getKinds = () => select('Kind', ['success', 'error', 'warning', 'info'])
+const getKinds = () => select('Kind', ['success', 'error', 'warning', 'info'], 'success')
+
+class SnackQueue extends React.PureComponent {
+  static propTypes = {
+    snack: PropTypes.object
+  }
+  static contextTypes = {
+    addToSnackQueue: PropTypes.func
+  }
+
+  addToQueue = () => {
+    const {snack} = this.props
+    this.context.addToSnackQueue(snack)
+  }
+
+  render() {
+    button('Add to queue', this.addToQueue, 'test')
+    return <div />
+  }
+}
 
 storiesOf('Snackbar')
   .addDecorator(withKnobs)
@@ -29,5 +50,21 @@ storiesOf('Snackbar')
           {text('children', 'This is the content', 'props')}
         </Snackbar>
       </Sanity>
+    )
+  })
+  .add('Queue', () => {
+    const snack = {
+      kind: getKinds(),
+      message: text('message', 'Message', 'props'),
+      children: text('children', 'Children', 'props'),
+      onHide: () => action('onHide'),
+      onAction: () => action('action'),
+      actionTitle: text('actionTitle', 'action', 'props')
+    }
+
+    return (
+      <SnackbarProvider>
+        <SnackQueue snack={snack} />
+      </SnackbarProvider>
     )
   })
