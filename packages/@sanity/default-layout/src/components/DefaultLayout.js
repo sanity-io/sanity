d67b45c221d92f3f14a059d94900082f6c5a8f7d
Espen Hovlandsdal <espen@hovlandsdal.com>
2019-07-31 15:04:18 +0200

    [default-layout] Use templates for global action modal
    [structure] Fix signature of hasIcon
    [dashboard] Don't implicitly depend on @sanity/client being installed
    [base] Fix incorrect require after refactoring
    [structure] Fix Boolean/boolean mismatch
    [structure] Remove template tests from structure

--- packages/@sanity/default-layout/src/components/DefaultLayout.js
+++ packages/@sanity/default-layout/src/components/DefaultLayout.js
@@ -1,32 +1,24 @@
 import PropTypes from 'prop-types'
 import React from 'react'
-import {startCase} from 'lodash'
 import schema from 'part:@sanity/base/schema'
-import DataAspectsResolver from 'part:@sanity/data-aspects/resolver?'
 import AppLoadingScreen from 'part:@sanity/base/app-loading-screen'
 import {RouteScope, withRouterHOC} from 'part:@sanity/base/router'
 import absolutes from 'all:part:@sanity/base/absolutes'
 import {isActionEnabled} from 'part:@sanity/base/util/document-action-utils'
 import userStore from 'part:@sanity/base/user'
 import styles from './styles/DefaultLayout.css'
+import SideMenu from './SideMenu'
 import RenderTool from './RenderTool'
 import ActionModal from './ActionModal'
 import NavBarContainer from './NavBarContainer'
 import {SchemaErrorReporter} from './SchemaErrorReporter'
-import SideMenu from './SideMenu'
-
-let dataAspects
-if (DataAspectsResolver) {
-  dataAspects = new DataAspectsResolver(schema)
-}
+import {getTemplatesBySchemaType} from '@sanity/base/initial-value-templates'
 
 function getDocumentTypeNames() {
-  return dataAspects
-    ? dataAspects.getInferredTypes()
-    : schema.getTypeNames().filter(typeName => {
-        const schemaType = schema.get(typeName)
-        return schemaType.type && schemaType.type.name === 'document'
-      })
+  return schema.getTypeNames().filter(typeName => {
+    const schemaType = schema.get(typeName)
+    return schemaType.type && schemaType.type.name === 'document'
+  })
 }
 
 export default withRouterHOC(
@@ -147,13 +139,16 @@ export default withRouterHOC(
       const modalActions = getDocumentTypeNames()
         .map(typeName => schema.get(typeName))
         .filter(type => isActionEnabled(type, 'create'))
-        .map(type => ({
-          title: dataAspects
-            ? dataAspects.getDisplayName(type.name)
-            : type.title || startCase(type.name),
-          icon: dataAspects ? dataAspects.getIcon(type.name) : type.icon,
-          params: {type: type.name}
-        }))
+        .reduce((actions, type) => {
+          const templates = getTemplatesBySchemaType(type.name)
+          return actions.concat(
+            templates.map(tpl => ({
+              title: tpl.title,
+              icon: tpl.icon || type.icon,
+              params: {template: tpl.id, type: tpl.schemaType}
+            }))
+          )
+        }, [])
 
       const isOverlayVisible = menuIsOpen || searchIsOpen
       let className = styles.root
