ef9443cf06a1c15ba7146c8d5e953e0874036ab6
Kristoffer J. Sivertsen <kristoffer@sanity.io>
2019-08-02 12:46:46 +0200

    [default-layout] Prettier styling of broken avatar image (#1419)
    * [default-layout] Prettier styling of broken avatar image
    
    Co-Authored-By: Bjørge Næss <bjoerge@gmail.com>

--- packages/@sanity/default-layout/src/components/LoginStatus.js
+++ packages/@sanity/default-layout/src/components/LoginStatus.js
@@ -64,6 +64,7 @@ class LoginStatus extends React.PureComponent {
                 src={user.profileImage}
                 className={styles.userImage}
                 alt={`${user.name}'s profile image`}
+                data-initials={(user.name || user.email || '?').charAt(0)}
               />
             ) : (
               <div className={styles.userImageMissing}>
