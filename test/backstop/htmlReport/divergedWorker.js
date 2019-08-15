6f7a79109d356c4a1ac0370cec401a2c23b0b178
Bjørge Næss <bjoerge@gmail.com>
2019-08-15 14:07:00 +0200

    [chore] Remove the backstop test folder as well

--- test/backstop/htmlReport/divergedWorker.js
+++
@@ -1,6 +0,0 @@
-importScripts('diff.js');
-importScripts('diverged.js');
-self.addEventListener('message', function(e) {
-  self.postMessage(diverged(...e.data.divergedInput));
-  self.close();
-}, false);
