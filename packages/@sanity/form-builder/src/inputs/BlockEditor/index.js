6b6860ef3fef29b4d614773934f7dcf10a5150ef
Espen Hovlandsdal <espen@hovlandsdal.com>
2019-08-16 22:39:27 +0200

    [form-builder] Add error boundary around portable text editor (#1450)

--- packages/@sanity/form-builder/src/inputs/BlockEditor/index.js
+++ packages/@sanity/form-builder/src/inputs/BlockEditor/index.js
@@ -1,8 +1,17 @@
 import {KeyUtils} from 'slate'
+import React from 'react'
 import {randomKey} from '@sanity/block-tools'
+import FormBuilderErrorBoundary from '../../FormBuilderErrorBoundary'
+import SyncWrapper from './SyncWrapper'
 
 // Set our own key generator for Slate (as early as possible)
 const keyGenerator = () => randomKey(12)
 KeyUtils.setGenerator(keyGenerator)
 
-export {default} from './SyncWrapper'
+const FormBuilderRoot = props => (
+  <FormBuilderErrorBoundary>
+    <SyncWrapper {...props} />
+  </FormBuilderErrorBoundary>
+)
+
+export default FormBuilderRoot
