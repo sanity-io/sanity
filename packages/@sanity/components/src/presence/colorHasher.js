903ccbb6325476f534ef873118d82a6a381b8a1c
Bjørge Næss <bjoerge@gmail.com>
2019-08-26 16:55:39 +0200

    [components] Make sure color hashing doesn't fail on non-transpiled var() css properties
    This provides a fix for #1447 by resolving the actual value of `var(--brand-primary)` at runtime if it isn't already transpiled to a valid color in css import.

--- packages/@sanity/components/src/presence/colorHasher.js
+++ packages/@sanity/components/src/presence/colorHasher.js
@@ -1,16 +1,32 @@
 import stringHash from 'string-hash'
 import palx from 'palx'
+import chroma from 'chroma-js'
+import {memoize} from 'lodash'
 import styles from './colorHasher.css'
 
-const brandPrimary = styles.brandPrimary || '#fcc'
+const readVar = varName =>
+  getComputedStyle(document.documentElement)
+    .getPropertyValue(varName)
+    .trim()
 
-const pal = palx(brandPrimary)
+const getBrandPrimary = memoize(() => {
+  if (chroma.valid(styles.brandPrimary)) {
+    return styles.brandPrimary
+  }
+  const fromVar = readVar('--brand-primary')
+  return fromVar && chroma.valid(fromVar) ? fromVar : '#fcc'
+})
+
+const getPalette = memoize(palx)
 
 // Picks strong colors from a palette created from the brand primary color
 function toColor(str) {
+  const brandPrimary = getBrandPrimary()
   if (!str) {
     return brandPrimary
   }
+
+  const pal = getPalette(brandPrimary)
   const hashFloat = stringHash(str) / Math.pow(2, 32)
   // ignore base and black from palx
   const hue = Object.keys(pal).slice(2)[Math.floor(hashFloat * (Object.keys(pal).length - 2))]
