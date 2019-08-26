b51e882fb921aa3721550901eeea25ae99b6736e
Espen Hovlandsdal <espen@hovlandsdal.com>
2019-08-26 14:28:54 -0700

    [test-studio] Add example of deep pane using initial value template

--- packages/test-studio/src/templates.js
+++ packages/test-studio/src/templates.js
@@ -8,8 +8,17 @@ export default [
     title: 'Developer',
     description: `Selects the role "Developer" for you, so you don't have to`,
     schemaType: 'author',
-    value: {
-      role: 'developer'
-    }
+    value: params => ({role: 'developer'})
+  }),
+
+  T.template({
+    id: 'book-by-author',
+    title: 'Book by author',
+    description: 'Book by a specific author',
+    schemaType: 'book',
+    parameters: [{name: 'authorId', type: 'string'}],
+    value: params => ({
+      author: {_type: 'reference', _ref: params.authorId}
+    })
   })
 ]
