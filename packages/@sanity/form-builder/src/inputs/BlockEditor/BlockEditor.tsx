sanity-io/sanity

commit 21af6baffe88d57db32d0a05e048ef7d3d671523

[form-builder] Migrate from Flow to TypeScript

packages/@sanity/form-builder/src/inputs/BlockEditor/BlockEditor.tsx
@@ -1,0 +1,400 @@
+import React, {RefObject} from 'react'
+import {debounce} from 'lodash'
+import ActivateOnFocus from 'part:@sanity/components/utilities/activate-on-focus'
+import {Portal} from 'part:@sanity/components/utilities/portal'
+import StackedEscapeable from 'part:@sanity/components/utilities/stacked-escapable'
+import Button from 'part:@sanity/components/buttons/default'
+import CloseIcon from 'part:@sanity/base/close-icon'
+import FullscreenIcon from 'part:@sanity/base/fullscreen-icon'
+import Spinner from 'part:@sanity/components/loading/spinner'
+import EditNode from './EditNode'
+import Editor from './Editor'
+import Toolbar from './Toolbar/Toolbar'
+import styles from './styles/BlockEditor.css'
+import IS_MAC from './utils/isMac'
+import {
+  BlockContentFeatures,
+  FormBuilderValue,
+  Marker,
+  RenderBlockActions,
+  RenderCustomMarkers,
+  SlateEditor,
+  SlateNode,
+  SlateValue,
+  Type,
+  UndoRedoStack
+} from './typeDefs'
+
+import {PatchEvent} from '../../PatchEvent'
+import {Path} from '../../typedefs/path'
+import {getKey} from './utils/getKey'
+
+type Props = {
+  blockContentFeatures: BlockContentFeatures
+  editorValue: SlateValue
+  fullscreen: boolean
+  isActive: boolean
+  focusPath: Path
+  markers: Marker[]
+  onPatch: (event: PatchEvent) => void
+  isLoading: boolean
+  onChange: (editor: SlateEditor, callback?: (arg0: void) => void) => void
+  onBlur: () => void
+  onFocus: (arg0: Path) => void
+  onLoading: (props: {}) => void
+  onPaste?: (arg0: {
+    event: React.SyntheticEvent
+    path: []
+    type: Type
+    value: FormBuilderValue[] | null
+  }) => {
+    insert?: FormBuilderValue[]
+    path?: []
+  }
+  onToggleFullScreen: (event: React.SyntheticEvent<any>) => void
+  readOnly?: boolean
+  renderBlockActions?: RenderBlockActions
+  renderCustomMarkers?: RenderCustomMarkers
+  setFocus: (arg0: void) => void
+  type: Type
+  value: FormBuilderValue[] | null
+  undoRedoStack: UndoRedoStack
+  userIsWritingText: boolean
+}
+
+// eslint-disable-next-line complexity
+function findEditNode(focusPath: Path, editorValue: SlateValue, editor: SlateEditor) {
+  const focusBlockKey = getKey(focusPath[0])
+  const isVoidRootBlock =
+    focusBlockKey &&
+    editorValue &&
+    editorValue.document &&
+    editorValue.document.size > 0 &&
+    editor.query('isVoid', editorValue.document.getDescendant(focusBlockKey))
+  const focusInlineKey =
+    !isVoidRootBlock &&
+    focusPath[1] &&
+    focusPath[1] === 'children' &&
+    focusPath[2] &&
+    getKey(focusPath[2])
+  const markDefKey =
+    !isVoidRootBlock && focusPath[2] && focusPath[1] === 'markDefs' && getKey(focusPath[2])
+  let key
+  if (markDefKey) {
+    const block = editorValue.document.getDescendant(focusBlockKey)
+    if (!block) {
+      return null
+    }
+    const span = block
+      .filterDescendants(desc => desc.type === 'span')
+      .find(node => {
+        const annotations = node.data.get('annotations') || {}
+        return Object.keys(annotations).find(
+          aKey => annotations[aKey] && annotations[aKey]._key === markDefKey
+        )
+      })
+    return span
+  } else if (focusInlineKey) {
+    key = focusInlineKey
+  } else {
+    key = focusBlockKey
+  }
+  return editorValue.document.getDescendant(key)
+}
+
+type State = {
+  preventScroll: boolean
+  isDragging: boolean
+}
+
+export default class BlockEditor extends React.PureComponent<Props, State> {
+  state = {
+    preventScroll: false,
+    isDragging: false
+  }
+  static defaultProps = {
+    readOnly: false,
+    onPaste: undefined,
+    renderBlockActions: undefined,
+    renderCustomMarkers: undefined
+  }
+  scrollContainer: RefObject<HTMLDivElement> = React.createRef()
+  editor: RefObject<Editor> = React.createRef()
+  editorWrapper: RefObject<HTMLDivElement> = React.createRef()
+
+  componentDidUpdate() {
+    this.checkScrollHeight()
+  }
+
+  componentWillUnmount() {
+    this.handleOnDragLeave.cancel()
+  }
+
+  renderNodeEditor() {
+    const {blockContentFeatures, editorValue, focusPath} = this.props
+    const slateNode = findEditNode(focusPath, editorValue, this.getEditor())
+    if (!slateNode || slateNode.type === 'contentBlock') {
+      return null
+    }
+    let value
+    let type
+    if (slateNode.type === 'span') {
+      const annotations = slateNode.data.get('annotations')
+      const focusedAnnotationName = Object.keys(annotations).find(
+        key => annotations[key]._key === getKey(focusPath[2])
+      )
+      if (!focusedAnnotationName) {
+        return null
+      }
+      value = annotations[focusedAnnotationName]
+      type = blockContentFeatures.annotations.find(an => an.value === focusedAnnotationName)
+      if (type) {
+        return this.renderEditNode(
+          value,
+          type.type,
+          [focusPath[0], 'markDefs', {_key: value._key}],
+          slateNode
+        )
+      }
+    }
+    value = slateNode.data.get('value')
+    const findType = obj => obj.name === value._type
+    if (slateNode.object === 'inline') {
+      type = blockContentFeatures.types.inlineObjects.find(findType)
+      if (type) {
+        return this.renderEditNode(
+          value,
+          type,
+          [focusPath[0], 'children', {_key: value._key}],
+          slateNode
+        )
+      }
+    }
+    type = blockContentFeatures.types.blockObjects.find(findType)
+    if (type) {
+      return this.renderEditNode(value, type, [{_key: value._key}], slateNode)
+    }
+    return null
+  }
+
+  renderEditNode(nodeValue: any, type: Type, path: Path, slateNode: SlateNode) {
+    const {focusPath, fullscreen, markers, onFocus, onPatch, readOnly, value} = this.props
+    return (
+      <EditNode
+        editor={this.getEditor()}
+        focusPath={focusPath}
+        fullscreen={fullscreen}
+        markers={markers}
+        nodeValue={nodeValue}
+        node={slateNode}
+        onFocus={onFocus}
+        onPatch={onPatch}
+        path={path}
+        readOnly={readOnly}
+        type={type}
+        value={value}
+      />
+    )
+  }
+
+  checkScrollHeight = () => {
+    if (this.scrollContainer && this.scrollContainer.current && this.editorWrapper.current) {
+      const preventScroll =
+        this.scrollContainer.current.offsetHeight < this.editorWrapper.current.offsetHeight
+      if (this.state.preventScroll !== preventScroll) {
+        this.setState({
+          preventScroll
+        })
+      }
+    }
+  }
+
+  getEditor() {
+    if (this.editor && this.editor.current) {
+      return this.editor.current.getEditor()
+    }
+    return null
+  }
+
+  handleOnDragEnter = (event: React.DragEvent) => {
+    this.setState({isDragging: true})
+  }
+  handleOnDragLeave = debounce((event: React.DragEvent) => {
+    this.setState({isDragging: false})
+  }, 1500)
+
+  renderEditor() {
+    const {
+      blockContentFeatures,
+      editorValue,
+      focusPath,
+      fullscreen,
+      markers,
+      onBlur,
+      onFocus,
+      onChange,
+      onLoading,
+      onPatch,
+      onPaste,
+      readOnly,
+      renderBlockActions,
+      renderCustomMarkers,
+      setFocus,
+      type,
+      undoRedoStack,
+      userIsWritingText,
+      value
+    } = this.props
+    return (
+      <Editor
+        blockContentFeatures={blockContentFeatures}
+        editorValue={editorValue}
+        focusPath={focusPath}
+        fullscreen={fullscreen}
+        markers={markers}
+        onBlur={onBlur}
+        onChange={onChange}
+        onFocus={onFocus}
+        onLoading={onLoading}
+        onPaste={onPaste}
+        onPatch={onPatch}
+        onToggleFullScreen={this.handleToggleFullscreen}
+        readOnly={readOnly}
+        ref={this.editor}
+        renderBlockActions={renderBlockActions}
+        renderCustomMarkers={renderCustomMarkers}
+        scrollContainer={this.scrollContainer}
+        setFocus={setFocus}
+        type={type}
+        undoRedoStack={undoRedoStack}
+        userIsWritingText={userIsWritingText}
+        value={value}
+      />
+    )
+  }
+
+  renderReadOnlyFullscreenButton() {
+    const {readOnly, fullscreen} = this.props
+    if (!readOnly) {
+      return null
+    }
+    return (
+      <div className={styles.readOnlyFullscreenButtonContainer}>
+        <Button
+          kind="simple"
+          onClick={this.handleToggleFullscreen}
+          title={`Open in fullscreen`}
+          icon={fullscreen ? CloseIcon : FullscreenIcon}
+        />
+      </div>
+    )
+  }
+
+  // eslint-disable-next-line complexity
+  renderBlockEditor() {
+    const {
+      blockContentFeatures,
+      editorValue,
+      fullscreen,
+      focusPath,
+      isActive,
+      isLoading,
+      markers,
+      onFocus,
+      setFocus,
+      readOnly,
+      type,
+      userIsWritingText
+    } = this.props
+    const isEditingNode = (focusPath || []).length > 1
+    return (
+      <div>
+        {!readOnly && (
+          <Toolbar
+            blockContentFeatures={blockContentFeatures}
+            editor={this.getEditor()}
+            editorValue={editorValue}
+            fullscreen={fullscreen}
+            markers={markers}
+            onFocus={onFocus}
+            onToggleFullScreen={this.handleToggleFullscreen}
+            isDragging={this.state.isDragging}
+            type={type}
+            userIsWritingText={userIsWritingText}
+          />
+        )}
+
+        {isLoading && (
+          <div className={styles.loading}>
+            <Spinner center />
+          </div>
+        )}
+
+        <ActivateOnFocus
+          isActive={!this.state.preventScroll || fullscreen || isActive}
+          html={
+            <div className={styles.activeOnFocus}>
+              <h3>Click to edit</h3>
+              <div>or</div>
+              <div>
+                <Button onClick={this.handleToggleFullscreen} color="primary">
+                  Open in fullscreen
+                </Button>
+              </div>
+              <p className={styles.keyboardShortcut}>
+                Tip: <br />
+                <strong>
+                  {IS_MAC ? '⌘' : 'ctrl'}
+                  &nbsp;+&nbsp;enter
+                </strong>{' '}
+                while editing to go in fullscreen
+              </p>
+            </div>
+          }
+          onActivate={setFocus}
+        >
+          <div className={styles.scrollContainer} ref={this.scrollContainer}>
+            <div className={styles.editorWrapper} ref={this.editorWrapper}>
+              {this.renderEditor()}
+              {isEditingNode && fullscreen && this.renderNodeEditor()}
+            </div>
+          </div>
+        </ActivateOnFocus>
+        {isEditingNode && !fullscreen && this.renderNodeEditor()}
+      </div>
+    )
+  }
+
+  handleToggleFullscreen = (event: React.SyntheticEvent<any>) => {
+    const {onToggleFullScreen} = this.props
+    onToggleFullScreen(event)
+  }
+
+  render() {
+    const {fullscreen} = this.props
+    return (
+      <div className={styles.root}>
+        {fullscreen && (
+          <Portal>
+            <StackedEscapeable onEscape={this.handleToggleFullscreen}>
+              <div
+                className={styles.fullscreen}
+                onDragLeave={this.handleOnDragLeave}
+                onDragEnter={this.handleOnDragEnter}
+              >
+                {this.renderReadOnlyFullscreenButton()}
+                {this.renderBlockEditor()}
+              </div>
+            </StackedEscapeable>
+          </Portal>
+        )}
+        {!fullscreen && (
+          <div>
+            {this.renderReadOnlyFullscreenButton()}
+            {this.renderBlockEditor()}
+          </div>
+        )}
+      </div>
+    )
+  }
+}

packages/@sanity/form-builder/src/inputs/BlockEditor/Toolbar/Toolbar.tsx
@@ -1,0 +1,249 @@
+/* eslint-disable complexity */
+import React, {RefObject} from 'react'
+import {Tooltip} from 'react-tippy'
+import Measure from 'react-measure'
+import ArrowIcon from 'part:@sanity/base/angle-down-icon'
+import Button from 'part:@sanity/components/buttons/default'
+import ChevronDown from 'part:@sanity/base/chevron-down-icon'
+import CloseIcon from 'part:@sanity/base/close-icon'
+import FullscreenIcon from 'part:@sanity/base/fullscreen-icon'
+import ValidationList from 'part:@sanity/components/validation/list'
+import WarningIcon from 'part:@sanity/base/warning-icon'
+import Poppable from 'part:@sanity/components/utilities/poppable'
+import {debounce, xor} from 'lodash'
+import {BlockContentFeatures, Marker, SlateEditor, SlateValue, Type} from '../typeDefs'
+import IS_MAC from '../utils/isMac'
+import PrimaryGroup from './PrimaryGroup'
+import styles from './styles/Toolbar.css'
+import {Path} from '../../../typedefs/path'
+
+const collapsibleGroups = ['insertMenu', 'annotationButtons', 'decoratorButtons', 'listItemButtons']
+const BREAKPOINT_SCREEN_MEDIUM = 512
+
+type Props = {
+  blockContentFeatures: BlockContentFeatures
+  editor: SlateEditor
+  editorValue: SlateValue
+  fullscreen: boolean
+  onFocus: (path: Path) => void
+  onToggleFullScreen: (event: React.SyntheticEvent<any>) => void
+  markers: Marker[]
+  type: Type
+  isDragging: boolean
+  userIsWritingText: boolean
+}
+
+type ToolbarState = {
+  collapsePrimaryIsOpen: boolean
+  collapsePrimary: boolean
+  showValidationTooltip: boolean
+  collapsedGroups: any[]
+  lastContentWidth: number
+  isMobile: boolean
+}
+
+class Toolbar extends React.PureComponent<Props, ToolbarState> {
+  state = {
+    collapsePrimaryIsOpen: false,
+    collapsePrimary: false,
+    showValidationTooltip: false,
+    collapsedGroups: [],
+    lastContentWidth: -1,
+    isMobile: false
+  }
+  _primaryToolbar: RefObject<any> = React.createRef()
+
+  constructor(props: Props) {
+    super(props)
+    if (window) {
+      this.state = {
+        ...this.state,
+        isMobile: window.innerWidth < BREAKPOINT_SCREEN_MEDIUM
+      }
+    }
+  }
+
+  handleOpenPrimary = () => {
+    this.setState({
+      collapsePrimaryIsOpen: true
+    })
+  }
+  handleClosePrimary = () => {
+    this.setState({
+      collapsePrimaryIsOpen: false
+    })
+  }
+  handleClickOutsidePrimary = () => {
+    this.setState({
+      collapsePrimaryIsOpen: false
+    })
+  }
+  handleFocus = (focusPath: []) => {
+    const {onFocus} = this.props
+    onFocus(focusPath)
+  }
+  handleCloseValidationResults = () => {
+    this.setState({showValidationTooltip: false})
+  }
+  handleToggleValidationResults = () => {
+    this.setState(prevState => ({
+      showValidationTooltip: !prevState.showValidationTooltip
+    }))
+  }
+  handleResize = debounce(() => {
+    if (this.state.isMobile) return
+    const {_primaryToolbar} = this
+    const {collapsedGroups, lastContentWidth, collapsePrimary} = this.state
+    if (!_primaryToolbar || !_primaryToolbar.current) return
+    const width = _primaryToolbar.current.offsetWidth
+    const contentWidth = _primaryToolbar.current.scrollWidth
+    if (contentWidth > width && !collapsePrimary) {
+      const groupToCollapse = xor(collapsibleGroups, collapsedGroups)[0]
+      this.setState(
+        {
+          collapsedGroups: [...collapsedGroups, groupToCollapse],
+          lastContentWidth: contentWidth
+        },
+        () => {
+          if (contentWidth > width && collapsedGroups.length != collapsibleGroups.length) {
+            this.handleResize()
+          } else if (collapsedGroups.length === collapsibleGroups.length && contentWidth > width) {
+            this.setState({collapsePrimary: true})
+          }
+        }
+      )
+    }
+    if (collapsePrimary && lastContentWidth < width) {
+      this.setState({
+        collapsePrimary: false,
+        collapsePrimaryIsOpen: false
+      })
+    }
+    if (width >= lastContentWidth && collapsedGroups.length != 0) {
+      this.setState({
+        collapsedGroups: []
+      })
+    }
+  }, 50)
+
+  render() {
+    const {
+      blockContentFeatures,
+      editor,
+      fullscreen,
+      isDragging,
+      markers,
+      onToggleFullScreen,
+      type
+    } = this.props
+    if (!editor) {
+      return null
+    }
+    const {showValidationTooltip, isMobile} = this.state
+    const insertItems = blockContentFeatures.types.inlineObjects.concat(
+      blockContentFeatures.types.blockObjects
+    )
+    const validation = markers.filter(marker => marker.type === 'validation')
+    const errors = validation.filter(marker => marker.level === 'error')
+    const warnings = validation.filter(marker => marker.level === 'warning')
+    const {collapsedGroups, collapsePrimary, collapsePrimaryIsOpen} = this.state
+    return (
+      <Measure offset scroll onResize={this.handleResize}>
+        {({measureRef}) => (
+          <div
+            ref={measureRef}
+            className={styles.root}
+            style={{pointerEvents: isDragging ? 'none' : 'unset'}}
+          >
+            <div className={styles.primary} ref={this._primaryToolbar}>
+              {collapsePrimary && (
+                <Button
+                  className={styles.showMoreButton}
+                  onClick={this.handleOpenPrimary}
+                  kind="simple"
+                >
+                  Show menu&nbsp;
+                  <span>
+                    <ArrowIcon color="inherit" />
+                  </span>
+                  <Poppable
+                    onClickOutside={this.handleClosePrimary}
+                    onEscape={this.handleClosePrimary}
+                  >
+                    {collapsePrimaryIsOpen && (
+                      <PrimaryGroup
+                        {...this.props}
+                        isPopped
+                        collapsedGroups={collapsedGroups}
+                        insertItems={insertItems}
+                      />
+                    )}
+                  </Poppable>
+                </Button>
+              )}
+              <div className={styles.primaryInner}>
+                {!collapsePrimary && (
+                  <PrimaryGroup
+                    {...this.props}
+                    collapsedGroups={collapsedGroups}
+                    insertItems={insertItems}
+                    isMobile={isMobile}
+                  />
+                )}
+              </div>
+            </div>
+            <div className={styles.secondary}>
+              {fullscreen && (errors.length > 0 || warnings.length > 0) && (
+                <Tooltip
+                  arrow
+                  duration={100}
+                  html={
+                    <ValidationList
+                      markers={validation}
+                      showLink
+                      isOpen={showValidationTooltip}
+                      documentType={type}
+                      onClose={this.handleCloseValidationResults}
+                      onFocus={this.handleFocus}
+                    />
+                  }
+                  interactive
+                  onRequestClose={this.handleCloseValidationResults}
+                  open={showValidationTooltip}
+                  position="bottom"
+                  style={{padding: 0}}
+                  theme="light noPadding"
+                  trigger="click"
+                >
+                  <Button
+                    color="danger"
+                    icon={WarningIcon}
+                    kind="simple"
+                    onClick={this.handleToggleValidationResults}
+                    padding="small"
+                  >
+                    {errors.length}
+                    <span style={{paddingLeft: '0.5em'}}>
+                      <ChevronDown />
+                    </span>
+                  </Button>
+                </Tooltip>
+              )}
+              <div className={styles.fullscreenButtonContainer}>
+                <Button
+                  kind="simple"
+                  onClick={onToggleFullScreen}
+                  title={`Open in fullscreen (${IS_MAC ? 'cmd' : 'ctrl'}+enter)`}
+                  icon={fullscreen ? CloseIcon : FullscreenIcon}
+                  bleed
+                />
+              </div>
+            </div>
+          </div>
+        )}
+      </Measure>
+    )
+  }
+}
+
+export default Toolbar

packages/@sanity/form-builder/src/inputs/BlockEditor/Toolbar/styles/Toolbar.css
@@ -1,4 +1,4 @@
-@import "part:@sanity/base/theme/variables-style";
+@import 'part:@sanity/base/theme/variables-style';
 
 .root {
   position: relative;
@@ -53,7 +53,10 @@
   height: 100%;
   width: 1em;
   z-index: 2;
-  background: radial-gradient(var(--component-border-color), color(var(--component-border-color) a(0%)) 70%);
+  background: radial-gradient(
+    var(--component-border-color),
+    color(var(--component-border-color) a(0%)) 70%
+  );
   background-position: 0.5em 0;
   background-repeat: no-repeat;
   border-right: 1px solid color(var(--component-border-color) a(5%));

commit 99720ed4892d396e936e589c2c6d55f2c1fd5671

[form-builder] Block editor: fix design regressions from #1492

packages/@sanity/form-builder/src/inputs/BlockEditor/BlockEditor.tsx
@@ -303,10 +303,17 @@ export default class BlockEditor extends React.PureComponent<Props, State> {
       onFocus,
       setFocus,
       readOnly,
+      renderBlockActions,
       type,
       userIsWritingText
     } = this.props
+    const hasMarkers = markers.filter(marker => marker.path.length > 0).length > 0
     const isEditingNode = (focusPath || []).length > 1
+    const scrollContainerClassNames = [
+      styles.scrollContainer,
+      renderBlockActions || hasMarkers ? styles.hasBlockExtras : null
+    ]
+
     return (
       <div>
         {!readOnly && (
@@ -353,7 +360,7 @@ export default class BlockEditor extends React.PureComponent<Props, State> {
           }
           onActivate={setFocus}
         >
-          <div className={styles.scrollContainer} ref={this.scrollContainer}>
+          <div className={scrollContainerClassNames.join(' ')} ref={this.scrollContainer}>
             <div className={styles.editorWrapper} ref={this.editorWrapper}>
               {this.renderEditor()}
               {isEditingNode && fullscreen && this.renderNodeEditor()}
@@ -371,7 +378,8 @@ export default class BlockEditor extends React.PureComponent<Props, State> {
   }
 
   render() {
-    const {fullscreen} = this.props
+    const {focusPath, fullscreen, readOnly} = this.props
+    const isFocused = (focusPath || []).length
     return (
       <div className={styles.root}>
         {fullscreen && (
@@ -389,7 +397,7 @@ export default class BlockEditor extends React.PureComponent<Props, State> {
           </Portal>
         )}
         {!fullscreen && (
-          <div>
+          <div className={isFocused && !readOnly ? styles.focus : ''}>
             {this.renderReadOnlyFullscreenButton()}
             {this.renderBlockEditor()}
           </div>

packages/@sanity/form-builder/src/inputs/BlockEditor/Toolbar/Toolbar.tsx
@@ -151,8 +151,11 @@ class Toolbar extends React.PureComponent<Props, ToolbarState> {
       <Measure offset scroll onResize={this.handleResize}>
         {({measureRef}) => (
           <div
+            className={`
+              ${styles.root}
+              ${fullscreen ? ` ${styles.fullscreen}` : ''}
+            `}
             ref={measureRef}
-            className={styles.root}
             style={{pointerEvents: isDragging ? 'none' : 'unset'}}
           >
             <div className={styles.primary} ref={this._primaryToolbar}>

packages/@sanity/form-builder/src/inputs/BlockEditor/Toolbar/styles/Toolbar.css
@@ -53,10 +53,7 @@
   height: 100%;
   width: 1em;
   z-index: 2;
-  background: radial-gradient(
-    var(--component-border-color),
-    color(var(--component-border-color) a(0%)) 70%
-  );
+  background: radial-gradient(var(--component-border-color), color(var(--component-border-color) a(0%)) 70%);
   background-position: 0.5em 0;
   background-repeat: no-repeat;
   border-right: 1px solid color(var(--component-border-color) a(5%));