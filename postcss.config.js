7ea7af5f05d8dd201b9b0df9284438f323965226
Bjørge Næss <bjoerge@gmail.com>
2019-09-30 10:20:03 +0200

    [chore] Add postcss config for typescript-plugin-css-modules
    The typescript-plugin-css-modules is using postcss in order to parse css files and extract named exports (class names). However, it did not pick up our postcss config file, therefore did not add postcss-cssnext plugin, therefore did not parse @nested syntax and therefore did not add nested classes to css file's exported class names. This fixes this issue by adding a postcss config that will get picked up by the plugin (as it looks for postcss.config in `cwd`).

---
+++ postcss.config.js
@@ -0,0 +1,14 @@
+/* eslint-disable import/no-commonjs */
+/*
+This file is here for the typescript-plugin-css-modules to pick up our postcss config
+The plugin will look for postcss config it current working directory (cwd)
+ */
+const config = require('./packages/@sanity/server/src/configs/postcss.config')
+
+module.exports = {
+  ...config,
+  // postcss-import is an async plugin and the typescript plugin currently does
+  // not support async processing, so we need to remove it
+  // see this issue: https://github.com/mrmckeb/typescript-plugin-css-modules/issues/46
+  plugins: config.plugins.filter(plugin => plugin.postcssPlugin !== 'postcss-import')
+}
