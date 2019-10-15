de6394326b87926c0089dcc8d88a64831104ea6d
Sindre Bøyum <sindre.boyum@gmail.com>
2019-10-15 15:40:57 +0200

    Shorten groq to its bare minimum `groq` does nothing to the input string, which is exactly the same as `String.raw` does. Not many bytes are saved, and the discussion in #1109 might suggest that there are plans to extend `groq`'s capabilities, however until then, perhaps shortening it as much as possibly is a viable idea.

--- packages/groq/src/groq.js
+++ packages/groq/src/groq.js
@@ -1,8 +1,2 @@
 /* eslint-disable import/no-commonjs */
-module.exports = function groq(strings, ...keys) {
-  const lastIndex = strings.length - 1
-  return (
-    strings.slice(0, lastIndex).reduce((acc, str, i) => acc + str + keys[i], '') +
-    strings[lastIndex]
-  )
-}
+module.exports = String.raw
