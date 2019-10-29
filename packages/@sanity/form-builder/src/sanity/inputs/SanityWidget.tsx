5750dbc3ad7c5b44c2f6c95b244f955edb24dc55
Kristoffer Sivertsen <kristoffer@sanity.io>
2019-10-29 11:31:25 +0100

    [form-builder] Update widget type

--- packages/@sanity/form-builder/src/sanity/inputs/SanityWidget.tsx
+++ packages/@sanity/form-builder/src/sanity/inputs/SanityWidget.tsx
@@ -1,24 +1,26 @@
 import React from 'react'
 import {Type} from '../../typedefs'
-const ENABLE_CONTEXT = () => {}
+import FormField from 'part:@sanity/components/formfields/default'
 
 type Props = {
   type: Type
 }
 
 export default class SanityWidget extends React.Component<Props> {
-  static contextTypes = {
-    formBuilder: ENABLE_CONTEXT
-  }
   render() {
     const {type} = this.props
-    const doc = this.context.formBuilder.getDocument()
-    const selectedValues = {}
-    Object.keys(type.select).forEach(k => selectedValues[k] = doc[type.select[k]])
 
     if (type.component) {
       const Component = type.component
-      return <Component {...this.props} value={selectedValues} />
+      return (
+        <FormField
+          label={type.title}
+          description={type.description}
+        >
+          <Component {...this.props} />
+        </FormField>
+
+      )
     }
     return (
       <div>No component in widget</div>
