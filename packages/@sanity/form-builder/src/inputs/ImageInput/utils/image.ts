646066cb51bf1a0d6dade2c5b9fd40511dc663ed
Per-Kristian Nordnes <per.kristian.nordnes@gmail.com>
2019-11-06 12:39:21 +0100

    [form-builder] Image field utils

---
+++ packages/@sanity/form-builder/src/inputs/ImageInput/utils/image.ts
@@ -0,0 +1,48 @@
+import UUID from '@sanity/uuid'
+
+export function urlToFile(url: string, filename?: string): Promise<File> {
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest()
+    xhr.onload = () => {
+      const reader = new FileReader()
+      reader.onloadend = () => {
+        const string = reader.result.toString()
+        const ext = string.substring('data:image/'.length, string.indexOf(';base64'))
+        if (!ext && !filename) {
+          return reject(new Error('Could not find mime type for image'))
+        }
+        resolve(dataURLtoFile(reader.result, filename || `${UUID()}.${ext}`))
+      }
+      reader.readAsDataURL(xhr.response)
+    }
+    xhr.onerror = error => {
+      reject(error)
+    }
+    xhr.open('GET', url)
+    xhr.responseType = 'blob'
+    xhr.send()
+  })
+}
+
+export function base64ToFile(base64Data: string | ArrayBuffer, filename?: string): Promise<File> {
+  return new Promise((resolve, reject) => {
+    const string = base64Data.toString()
+    const ext = string.substring('data:image/'.length, string.indexOf(';base64'))
+    if (!ext && !filename) {
+      return reject(new Error('Could not find mime type for image'))
+    }
+    resolve(dataURLtoFile(base64Data, filename || `${UUID()}.${ext}`))
+  })
+}
+
+function dataURLtoFile(dataurl, filename) {
+  const arr = dataurl.split(',')
+  const mime = arr[0].match(/:(.*?);/)[1]
+  const bstr = atob(arr[1])
+  let n = bstr.length
+  const u8arr = new Uint8Array(n)
+  while (n--) {
+    u8arr[n] = bstr.charCodeAt(n)
+  }
+  return new File([u8arr], filename, {type: mime})
+}
