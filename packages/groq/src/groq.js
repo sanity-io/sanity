77572adb93700b2b86f48ef0801ea2005b94a9e7
Espen Hovlandsdal <espen@hovlandsdal.com>
2019-11-12 10:55:02 -0800

    [groq] Don't use arrow functions, fixes IE11 compatibility

--- packages/groq/src/groq.js
+++ packages/groq/src/groq.js
@@ -2,7 +2,8 @@
 module.exports = function groq(strings, ...keys) {
   const lastIndex = strings.length - 1
   return (
-    strings.slice(0, lastIndex).reduce((acc, str, i) => acc + str + keys[i], '') +
-    strings[lastIndex]
+    strings.slice(0, lastIndex).reduce(function(acc, str, i) {
+      return acc + str + keys[i]
+    }, '') + strings[lastIndex]
   )
 }
