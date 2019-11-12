8998ce7ef0509c44e5e739c2dce2ec84cfc35032
Marius Lundgård <studio@mariuslundgard.com>
2019-11-12 13:06:04 +0100

    [components] Remove unused scroll UI logic

--- packages/@sanity/components/src/panes/DefaultPane.js
+++ packages/@sanity/components/src/panes/DefaultPane.js
@@ -22,51 +22,6 @@ function noActionFn() {
   console.warn('No handler defined for action')
 }
 
-// This is the height of the global navigation bar
-// TODO: Turn this into a prop that DefaultPane receives
-const GLOBAL_NAV_BAR_HEIGHT = 49
-
-const getScrollShadowState = (scrollTop, prevState) => {
-  const {headerStyleRatio} = prevState
-  const threshold = 30
-
-  if (scrollTop < threshold) {
-    // Round off the calculation to cut down rerenders that are not visible to the human eye
-    // Example: 0.53 -> 0.55 or 0.91 -> 0.9
-    const ratio = Math.round((scrollTop / threshold) * 20) / 20
-    if (ratio === headerStyleRatio) {
-      return null
-    }
-
-    return {
-      headerStyleRatio: ratio,
-      headerStyle: {
-        boxShadow: `0 0 ${2 * ratio}px rgba(0, 0, 0, ${ratio * 0.2})`
-      }
-    }
-  }
-
-  if (scrollTop < 0 && headerStyleRatio !== -1) {
-    return {
-      headerStyleRatio: -1,
-      headerStyle: {
-        boxShadow: 'none'
-      }
-    }
-  }
-
-  if (headerStyleRatio !== 1) {
-    return {
-      headerStyleRatio: 1,
-      headerStyle: {
-        boxShadow: '0 0px 2px rgba(0, 0, 0, 0.2)'
-      }
-    }
-  }
-
-  return null
-}
-
 const noop = () => {
   /* intentional noop */
 }
@@ -75,7 +30,7 @@ const isActionButton = item => item.showAsAction
 const isMenuButton = negate(isActionButton)
 
 // eslint-disable-next-line
-class Pane extends React.Component {
+class Pane extends React.PureComponent {
   static propTypes = {
     hasTabs: PropTypes.bool,
     tabIdPrefix: PropTypes.string,
@@ -87,7 +42,6 @@ class Pane extends React.Component {
     children: PropTypes.node,
     isSelected: PropTypes.bool,
     isScrollable: PropTypes.bool,
-    scrollTop: PropTypes.number,
     onAction: PropTypes.func,
     renderActions: PropTypes.func,
     menuItems: PropTypes.arrayOf(
@@ -124,7 +78,6 @@ class Pane extends React.Component {
     title: 'Untitled',
     isCollapsed: false,
     isSelected: false,
-    scrollTop: undefined,
     isScrollable: true,
     renderActions: undefined,
     styles: {},
@@ -138,12 +91,7 @@ class Pane extends React.Component {
 
   state = {
     menuIsOpen: false,
-    templateSelectionIsOpen: false,
-    headerStyleRatio: -1,
-    headerStyle: {
-      boxShadow: 'none'
-    },
-    scrollTop: 0
+    templateSelectionIsOpen: false
   }
 
   actionHandlers = {
@@ -170,69 +118,9 @@ class Pane extends React.Component {
       .substr(2, 6)
   }
 
-  static getDerivedStateFromProps(props, state) {
-    if (typeof props.scrollTop === 'undefined') {
-      return null
-    }
-
-    return getScrollShadowState(props.scrollTop, state)
-  }
-
-  componentDidMount() {
-    this.scrollFrame()
-  }
-
   componentWillUnmount() {
     if (this.closeRequest) {
       cancelAnimationFrame(this.closeRequest)
-    }
-
-    if (this.scrollFrameId) {
-      cancelAnimationFrame(this.scrollFrameId)
-    }
-  }
-
-  shouldComponentUpdate(nextProps, nextState) {
-    // The pane header has a styling which gradually adds more shadow and tunes the opacity when
-    // scrolling. In the case of "managed" lists (infinite scroll and such), the scroll position
-    // is passed as a prop (`scrollTop`). However, passed a certain threshold we no longer need to
-    // update, since the styling turns static. To prevent the prop from forcing a re-render,
-    // explicitly check for a difference in the state here to short-circuit in this common scenario
-    const scrollPropChanged = nextProps.scrollTop !== this.props.scrollTop
-    const headerStyleChanged = nextState.headerStyleRatio !== this.state.headerStyleRatio
-    if (scrollPropChanged && !headerStyleChanged) {
-      return false
-    }
-
-    return (
-      scrollPropChanged ||
-      headerStyleChanged ||
-      !shallowEquals(nextProps, this.props) ||
-      !shallowEquals(nextState, this.state)
-    )
-  }
-
-  scrollFrame = () => {
-    const winScrollTop = (window.pageYOffset || document.scrollTop || 0) - (document.clientTop || 0)
-    const scrollTop = Math.max(winScrollTop - GLOBAL_NAV_BAR_HEIGHT, 0)
-
-    if (this.state.scrollTop !== scrollTop) {
-      const shadowState = getScrollShadowState(scrollTop, this.state)
-      if (shadowState) {
-        shadowState.scrollTop = scrollTop
-        this.setState(shadowState)
-      } else {
-        this.setState({scrollTop: scrollTop})
-      }
-    }
-
-    this.scrollFrameId = requestAnimationFrame(this.scrollFrame)
-  }
-
-  handleContentScroll = event => {
-    const shadowState = getScrollShadowState(event.target.scrollTop, this.state)
-    if (shadowState) {
-      this.setState(shadowState)
     }
   }
 
@@ -267,7 +155,7 @@ class Pane extends React.Component {
 
   handleMenuAction = item => {
     // When closing the menu outright, the menu button will be focused and the "enter" keypress
-    // will bouble up to it and trigger a re-open of the menu. To work around this, use rAF to
+    // will bubble up to it and trigger a re-open of the menu. To work around this, use rAF to
     // ensure the current event is completed before closing the menu
     this.closeRequest = requestAnimationFrame(() => this.handleCloseMenu())
 
@@ -445,7 +333,7 @@ class Pane extends React.Component {
     const mainChildren = (
       <>
         {isScrollable ? (
-          <ScrollContainer className={styles.scrollContainer} onScroll={this.handleContentScroll}>
+          <ScrollContainer className={styles.scrollContainer}>
             <div style={contentMaxWidth ? {maxWidth: `${contentMaxWidth}px`} : {}}>{children}</div>
           </ScrollContainer>
         ) : (
