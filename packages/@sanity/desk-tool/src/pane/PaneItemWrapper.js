b3de6b507a7a6477973ff27bc20b4be33a0f2c69
Espen Hovlandsdal <espen@hovlandsdal.com>
2019-11-13 14:51:55 -0800

    [desk-tool] Use context hook for PaneItemWrapper

--- packages/@sanity/desk-tool/src/pane/PaneItemWrapper.js
+++ packages/@sanity/desk-tool/src/pane/PaneItemWrapper.js
@@ -1,28 +1,25 @@
-import styles from './styles/PaneItem.css'
-import listStyles from './styles/ListView.css'
 import PropTypes from 'prop-types'
-import React from 'react'
+import React, {useContext} from 'react'
 import {Item as GridListItem} from 'part:@sanity/components/lists/grid'
 import {PaneRouterContext} from '../index'
+import styles from './styles/PaneItem.css'
+import listStyles from './styles/ListView.css'
 
-const PaneItemWrapper = props => (
-  <PaneRouterContext.Consumer>
-    {({ChildLink}) => {
-      const {id, useGrid, layout, isSelected} = props
-      const link = (
-        <ChildLink childId={id} className={isSelected ? styles.linkIsSelected : styles.link}>
-          {props.children}
-        </ChildLink>
-      )
+const PaneItemWrapper = props => {
+  const {ChildLink} = useContext(PaneRouterContext)
+  const {id, useGrid, layout, isSelected} = props
+  const link = (
+    <ChildLink childId={id} className={isSelected ? styles.linkIsSelected : styles.link}>
+      {props.children}
+    </ChildLink>
+  )
 
-      return useGrid ? (
-        <GridListItem className={listStyles[`${layout}ListItem`]}>{link}</GridListItem>
-      ) : (
-        <div className={isSelected ? styles.selected : styles.item}>{link}</div>
-      )
-    }}
-  </PaneRouterContext.Consumer>
-)
+  return useGrid ? (
+    <GridListItem className={listStyles[`${layout}ListItem`]}>{link}</GridListItem>
+  ) : (
+    <div className={isSelected ? styles.selected : styles.item}>{link}</div>
+  )
+}
 
 PaneItemWrapper.propTypes = {
   id: PropTypes.string.isRequired,
