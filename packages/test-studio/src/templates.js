9ad68bf69822104746433303d43d5d255de8ff06
Espen Hovlandsdal <espen@hovlandsdal.com>
2019-11-12 18:54:30 -0800

    [test-studio] Add examples of promise/observable initial value templates

--- packages/test-studio/src/templates.js
+++ packages/test-studio/src/templates.js
@@ -1,4 +1,7 @@
+import {createProgressEvent} from '@sanity/base/initial-value-templates'
 import T from '@sanity/base/initial-value-template-builder'
+import {of, from, concat} from 'rxjs'
+import {delay, concatMap} from 'rxjs/operators'
 
 export default [
   ...T.defaults(),
@@ -20,5 +23,25 @@ export default [
     value: params => ({
       author: {_type: 'reference', _ref: params.authorId}
     })
+  }),
+
+  T.template({
+    id: 'slow-color-promise',
+    title: 'Slow color (promise)',
+    schemaType: 'colorTest',
+    value: () => new Promise(resolve => setTimeout(resolve, 5000, {title: 'Really slow color!'}))
+  }),
+
+  T.template({
+    id: 'slow-color-observable',
+    title: 'Slow color (observable)',
+    schemaType: 'colorTest',
+    value: () =>
+      concat(
+        from([5, 4, 3, 2, 1, 0]).pipe(
+          concatMap(num => of(createProgressEvent(`Resolving in... ${num}`)).pipe(delay(1000)))
+        ),
+        of({title: 'Wow, that took a while.'})
+      )
   })
 ]
