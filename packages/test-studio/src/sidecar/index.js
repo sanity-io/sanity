8122599042464b5216a4325055dcb23e1eaf6f04
Thomas Drevon <thomas@sanity.io>
2019-11-18 14:34:38 +0100

    [test-studio] Update alternative sidecar implementation

--- packages/test-studio/src/sidecar/index.js
+++ packages/test-studio/src/sidecar/index.js
@@ -7,12 +7,23 @@
 /* eslint-disable react/no-multi-comp */
 import React from 'react'
 import ToggleSidecarButton from 'react-icons/lib/ti/weather-snow'
+import {toggleSidecarOpenState} from 'part:@sanity/default-layout/sidecar-datastore'
 import Confetti from 'react-confetti'
+
+export function isSidecarEnabled() {
+  return true
+}
 
 export function SidecarLayout() {
   return <Confetti numberOfPieces={400} opacity={0.7} />
 }
 
 export function SidecarToggleButton() {
-  return <ToggleSidecarButton />
+  return (
+    <button onClick={toggleSidecarOpenState} type="button" aria-label="Toggle sidecar">
+      <div>
+        <ToggleSidecarButton />
+      </div>
+    </button>
+  )
 }
