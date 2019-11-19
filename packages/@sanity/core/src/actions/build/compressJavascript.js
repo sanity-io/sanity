33d310c40422f7f8e1553829f32bab0c96c0745a
Espen Hovlandsdal <espen@hovlandsdal.com>
2019-11-19 13:27:43 -0800

    [core] Upgrade to Terser 4.4, execute out-of-process (#1608)

--- packages/@sanity/core/src/actions/build/compressJavascript.js
+++ packages/@sanity/core/src/actions/build/compressJavascript.js
@@ -1,60 +1,20 @@
+import path from 'path'
+import execa from 'execa'
 import fse from 'fs-extra'
-import Terser, {minify as minifyJs} from 'terser'
+import resolveBin from 'resolve-bin'
 
 export default async inputFile => {
-  const content = await fse.readFile(inputFile, 'utf8')
-  const minified = await minify(content, inputFile)
-  await fse.outputFile(inputFile, minified)
-}
-
-function minify(content, fileName) {
-  return new Promise((resolve, reject) => {
-    // Terser introduced a breaking API change in a patch version
-    // In case they revert or people are using an older version,
-    // try both combinations
-    let result
-    if (minifyJs) {
-      result = minifyJs(content)
-    } else if (Terser.minify) {
-      result = Terser.minify(content)
-    } else {
-      return reject(new Error('Breaking change in Terser - `minify` function not found'))
-    }
-
-    if (result.error) {
-      reject(formatError(result.error, fileName, content))
-    } else {
-      resolve(result.code)
-    }
-  })
-}
-
-function formatError(err, fileName, content) {
-  let msg = `Parse error at ${fileName}:${err.line},${err.col}`
-
-  const limit = 70
-  const lines = content.split(/\r?\n/)
-  let col = err.col
-  let line = lines[err.line - 1]
-
-  if (!line && !col) {
-    line = lines[err.line - 2]
-    col = line.length
+  const terserBin = resolveBin.sync('terser')
+  if (!terserBin) {
+    throw new Error(`Can't find terser binary, cannot compress bundles`)
   }
 
-  if (line) {
-    if (col > limit) {
-      line = line.slice(col - limit)
-      col = limit
-    }
-
-    msg += '\n\n'
-    msg += line.slice(0, 80)
-    msg += '\n'
-    msg += line.slice(0, col).replace(/\S/g, ' ')
-    msg += '^'
+  const outPath = `${inputFile}.min`
+  const {stderr, stdout, code} = await execa(terserBin, ['-c', '-m', '-o', outPath, inputFile])
+  if (code > 0) {
+    throw new Error(`Failed to minify bundle (${path.basename(inputFile)}):\n\n${stderr || stdout}`)
   }
 
-  err.message = msg
-  return err
+  await fse.unlink(inputFile)
+  await fse.move(outPath, inputFile)
 }
