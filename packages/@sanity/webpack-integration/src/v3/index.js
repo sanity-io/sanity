1ca6c4667adb1cb9c6d2c04dfadd33647438724b
Espen Hovlandsdal <espen@hovlandsdal.com>
2019-11-19 17:50:00 -0800

    [webpack-integration] Expose SANITY_STUDIO_* environment variables

--- packages/@sanity/webpack-integration/src/v3/index.js
+++ packages/@sanity/webpack-integration/src/v3/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-process-env */
 const postcssImport = require('postcss-import')
 const postcssCssnext = require('postcss-cssnext')
 const PartResolverPlugin = require('@sanity/webpack-loader')
@@ -9,13 +10,30 @@ function getPartResolverPlugin(options) {
   return new PartResolverPlugin(options)
 }
 
+function getEnvVars(isProd) {
+  return Object.keys(process.env).reduce(
+    (acc, key) => {
+      if (key.startsWith('SANITY_STUDIO_')) {
+        acc[`process.env.${key}`] = JSON.stringify(process.env[key])
+      }
+      return acc
+    },
+    {
+      'process.env.NODE_ENV': JSON.stringify(isProd ? 'production' : 'development'),
+      'process.env': JSON.stringify({})
+    }
+  )
+}
+
 function getEnvPlugin(options) {
-  // eslint-disable-next-line no-process-env
   const bundleEnv = options.bundleEnv || process.env.BUNDLE_ENV || 'development'
   const env = options.env || 'development'
   const isProd = env === 'production'
   const webpack = options.webpack || require('webpack')
-  return new webpack.DefinePlugin({__DEV__: !isProd && bundleEnv === 'development'})
+  return new webpack.DefinePlugin({
+    __DEV__: !isProd && bundleEnv === 'development',
+    ...getEnvVars(isProd)
+  })
 }
 
 function getPlugins(options) {
