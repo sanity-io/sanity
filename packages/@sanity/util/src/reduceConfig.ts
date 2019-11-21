e203698e064954b234a8d99e94861bfdd8ba0a63
Espen Hovlandsdal <espen@hovlandsdal.com>
2019-11-20 17:27:19 -0800

    [util] Allow setting projectId/dataset through environment variables

--- packages/@sanity/util/src/reduceConfig.ts
+++ packages/@sanity/util/src/reduceConfig.ts
@@ -2,13 +2,21 @@
 import {mergeWith} from 'lodash'
 
 const sanityEnv = process.env.SANITY_ENV || 'production'
+const basePath = process.env.SANITY_STUDIO_BASEPATH || process.env.STUDIO_BASEPATH
 const apiHosts = {
   staging: 'https://api.sanity.work',
   development: 'http://api.sanity.wtf'
 }
 
+const projectId = process.env.SANITY_STUDIO_PROJECT_ID || undefined
+const dataset = process.env.SANITY_STUDIO_DATASET || undefined
+
 const processEnvConfig = {
-  project: process.env.STUDIO_BASEPATH ? {basePath: process.env.STUDIO_BASEPATH} : {}
+  project: basePath ? {basePath} : {}
+}
+
+function clean(obj) {
+  return Object.keys(obj).reduce((acc, key) => (obj[key] ? {...acc, [key]: obj[key]} : acc), {})
 }
 
 function merge(objValue, srcValue, key) {
@@ -22,7 +30,8 @@ function merge(objValue, srcValue, key) {
 
 export default (rawConfig, env = 'development') => {
   const apiHost = apiHosts[sanityEnv]
-  const sanityConf = apiHost ? {api: {apiHost}} : {}
+  const api = clean({apiHost, projectId, dataset})
+  const sanityConf = {api}
   const envConfig = (rawConfig.env || {})[env] || {}
   const config = mergeWith({}, rawConfig, envConfig, sanityConf, processEnvConfig, merge)
   delete config.env
