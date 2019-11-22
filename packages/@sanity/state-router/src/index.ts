eb6485bda8952f959d7c32076c2ecd854c046a96
Espen Hovlandsdal <espen@hovlandsdal.com>
2019-11-22 13:54:54 -0800

    [state-router] Provide router state as hook: useRouterState()

--- packages/@sanity/state-router/src/index.ts
+++ packages/@sanity/state-router/src/index.ts
@@ -2,7 +2,7 @@ import resolveStateFromPath from './resolveStateFromPath'
 import resolvePathFromState from './resolvePathFromState'
 
 export {default as route} from './route'
-export {RouterContext, useRouter} from './RouterContext'
+export {RouterContext, useRouter, useRouterState} from './RouterContext'
 
 export {resolveStateFromPath}
 export {resolvePathFromState}
