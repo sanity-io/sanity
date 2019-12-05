b283b1cd75d69cf796d6457b9512385a92fa6f02
Espen Hovlandsdal <espen@hovlandsdal.com>
2019-12-05 15:38:26 -0800

    [desk-tool] Add missing `setPayload` method to pane router context (#1645)
    * [desk-tool] Add missing `setPayload` method to pane router context
    
    * [test-studio] Add example of using setPayload

--- packages/test-studio/src/previews/developer/DeveloperPreview.js
+++ packages/test-studio/src/previews/developer/DeveloperPreview.js
@@ -1,14 +1,20 @@
 /* eslint-disable react/prop-types */
 
-import React from 'react'
+import React, {useContext} from 'react'
 import JSONPretty from 'react-json-pretty'
 import monikai from 'react-json-pretty/dist/monikai'
 import styles from './DeveloperPreview.module.css'
+import {PaneRouterContext} from '@sanity/desk-tool'
 
 function DeveloperPreview(props) {
+  const pane = useContext(PaneRouterContext)
+  const payload = {count: 0, ...pane.payload}
   const {displayed} = props.document
   return (
     <div className={styles.root}>
+      <button type="button" onClick={() => pane.setPayload({count: payload.count + 1})}>
+        + {payload.count}
+      </button>
       <JSONPretty data={displayed} theme={monikai} mainStyle="white-space: pre-wrap" />
     </div>
   )
