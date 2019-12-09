301a1a2c24f837037f30e043b2606edb7f0dbc78
Thomas Drevon <thomas@sanity.io>
2019-12-09 11:04:47 +0100

    [studio-hints] Remove unused sanity client and set version number to the current plublishable value

--- packages/@sanity/studio-hints/src/client.js
+++
@@ -1,7 +0,0 @@
-import SanityClient from '@sanity/client'
-
-export default new SanityClient({
-  projectId: '3do82whm',
-  dataset: 'next',
-  useCdn: false // switch this to true when we're out of dev mode
-})
