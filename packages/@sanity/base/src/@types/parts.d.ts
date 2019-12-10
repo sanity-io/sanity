fc6f4ca22a0c0a8b81544fe0e0b20cd9feff6883
Bjørge Næss <bjoerge@gmail.com>
2019-12-10 10:20:04 +0100

    Fix typescript issues

--- packages/@sanity/base/src/@types/parts.d.ts
+++ packages/@sanity/base/src/@types/parts.d.ts
@@ -1,8 +1,2 @@
-declare module 'part:*' {
-  const shim = any
-  export default shim
-}
-declare module 'all:part:*' {
-  const shim = any
-  export default shim
-}
+declare module 'part:*'
+declare module 'all:part:*'
