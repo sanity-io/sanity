3a3195edaa79f20d7332a750e9135781fe2b360a
Bjørge Næss <bjoerge@gmail.com>
2019-12-16 15:59:21 +0100

    Improve how we layers the document/edit stores

--- packages/@sanity/base/src/datastores/document/editor-document/documentEvents.ts
+++ packages/@sanity/base/src/datastores/document/editor-document/documentEvents.ts
@@ -1,22 +1,12 @@
-import {merge, Observable} from 'rxjs'
-import {map, switchMap} from 'rxjs/operators'
+import {merge} from 'rxjs'
+import {switchMap} from 'rxjs/operators'
 import {IdPair} from '../types'
-import {BufferedDocumentEvent} from '../buffered-doc/createBufferedDocument'
 import {cachedPair} from './cachedPair'
 
-type BufferedDocumentEventWithTarget = BufferedDocumentEvent & {target: 'draft' | 'published'}
-
-function setTarget(target: 'draft' | 'published') {
-  return (ev: BufferedDocumentEvent) => ({...ev, target})
-}
-
-export function documentPairEventsFor(idPair: IdPair): Observable<BufferedDocumentEventWithTarget> {
+// A stream of all events related to either published or draft, each event comes with a 'target'
+// that specifies which version (draft|published) the event is about
+export function documentPairEventsFor(idPair: IdPair) {
   return cachedPair(idPair).pipe(
-    switchMap(({draft, published}) =>
-      merge(
-        draft.events.pipe(map(setTarget('draft'))),
-        published.events.pipe(map(setTarget('published')))
-      )
-    )
+    switchMap(({draft, published}) => merge(draft.events, published.events))
   )
 }
--- packages/@sanity/base/src/datastores/document/editor-document/editOps.ts
+++ packages/@sanity/base/src/datastores/document/editor-document/editOps.ts
@@ -1,11 +1,10 @@
 import {getDraftId, isDraftId} from 'part:@sanity/base/util/draft-utils'
 import {omit} from 'lodash'
-import {merge, Observable} from 'rxjs'
-import {filter, map, scan} from 'rxjs/operators'
+import {combineLatest, merge, Observable, concat, of} from 'rxjs'
+import {map, switchMap} from 'rxjs/operators'
 import client from 'part:@sanity/base/client'
-import {documentPairEventsFor} from './documentEvents'
 import schema from 'part:@sanity/base/schema'
-import {DocumentMutationEvent, DocumentRebaseEvent, SnapshotEvent} from '../buffered-doc/types'
+import {snapshotPair} from './snapshotPair'
 
 interface EditorDocumentOperations {
   publish: () => void
@@ -15,13 +14,16 @@ interface EditorDocumentOperations {
   commit: () => void
 }
 
-function hasSnapshot(
-  event
-): event is (SnapshotEvent | DocumentRebaseEvent | DocumentMutationEvent) & {
-  target: 'published' | 'draft'
-} {
-  return event.type === 'snapshot' || event.type === 'rebase' || event.type === 'mutation'
-}
+const GUARDED = ['publish', 'delete', 'create', 'patch', 'discardDraft', 'commit'].reduce(
+  (a, name) => {
+    a[name] = () => {
+      // todo: improve this error message
+      throw new Error(`Premature invocation of ${name}`)
+    }
+    return a
+  },
+  {}
+) as EditorDocumentOperations
 
 export function editOpsOf(
   publishedId: string,
@@ -33,82 +35,86 @@ export function editOpsOf(
 
   const draftId = getDraftId(publishedId)
 
-  return documentPairEventsFor({publishedId, draftId}).pipe(
-    filter(hasSnapshot),
-    scan((targets, event) => ({...targets, [event.target]: event}), {draft: null, published: null}),
-    filter(({draft, published}) => draft && published),
-    map(({draft, published}) => {
-      const schemaType = schema.get(typeName)
-      const liveEdit = !!schemaType.liveEdit
-      return {
-        publish: () => {
-          if (liveEdit) {
-            throw new Error('Cannot publish when liveEdit is enabled')
-          }
+  return concat(
+    of(GUARDED),
+    snapshotPair({publishedId, draftId}).pipe(
+      switchMap(({draft, published}) => {
+        return combineLatest([draft.snapshots$, published.snapshots$]).pipe(
+          map(([draftSnapshot, publishSnapshot]) => {
+            const schemaType = schema.get(typeName)
+            const liveEdit = !!schemaType.liveEdit
+            return {
+              publish: () => {
+                if (liveEdit) {
+                  throw new Error('Cannot publish when liveEdit is enabled')
+                }
 
-          if (draft.document === null) {
-            throw new Error(`Can't publish an empty draft`)
-          }
+                if (draftSnapshot === null) {
+                  throw new Error(`Can't publish an empty draft`)
+                }
 
-          const tx = client.observable.transaction()
+                const tx = client.observable.transaction()
 
-          if (!published || !published.document) {
-            // If the document has not been published, we want to create it - if it suddenly exists
-            // before being created, we don't want to overwrite if, instead we want to yield an error
-            tx.create({
-              ...omit(draft.document, '_updatedAt'),
-              _id: publishedId
-            })
-          } else {
-            // If it exists already, we only want to update it if the revision on the remote server
-            // matches what our local state thinks it's at
-            tx.patch(publishedId, {
-              // Hack until other mutations support revision locking
-              unset: ['_reserved_prop_'],
-              ifRevisionID: published.document._rev
-            }).createOrReplace({
-              ...omit(draft.document, '_updatedAt'),
-              _id: publishedId
-            })
-          }
+                if (!published || !publishSnapshot) {
+                  // If the document has not been published, we want to create it - if it suddenly exists
+                  // before being created, we don't want to overwrite if, instead we want to yield an error
+                  tx.create({
+                    ...omit(draftSnapshot, '_updatedAt'),
+                    _id: publishedId
+                  })
+                } else {
+                  // If it exists already, we only want to update it if the revision on the remote server
+                  // matches what our local state thinks it's at
+                  tx.patch(publishedId, {
+                    // Hack until other mutations support revision locking
+                    unset: ['_reserved_prop_'],
+                    ifRevisionID: publishSnapshot._rev
+                  }).createOrReplace({
+                    ...omit(draftSnapshot, '_updatedAt'),
+                    _id: publishedId
+                  })
+                }
 
-          tx.delete(draftId)
+                tx.delete(draftId)
 
-          tx.commit().subscribe()
-        },
-        delete() {
-          published.delete()
-          draft.delete()
-        },
-        create(document) {
-          const target = liveEdit ? published : draft
-          target.create(document)
-        },
-        patch(patches) {
-          // const initialValue = this.getInitialValue()
-          if (liveEdit) {
-            // No drafting, patch and commit the published document
-            published.createIfNotExists({
-              _id: publishedId,
-              _type: typeName
-            })
-            published.patch(patches)
-          } else {
-            draft.createIfNotExists({
-              ...omit(published.document, '_updatedAt'),
-              _id: draftId,
-              _type: typeName
-            })
-            draft.patch(patches)
-          }
-        },
-        discardDraft() {
-          draft.delete()
-        },
-        commit() {
-          return merge(published.commit(), draft.commit()).subscribe()
-        }
-      }
-    })
+                tx.commit().subscribe()
+              },
+              delete() {
+                published.delete()
+                draft.delete()
+              },
+              create(document) {
+                const target = liveEdit ? published : draft
+                target.create(document)
+              },
+              patch(patches) {
+                // const initialValue = this.getInitialValue()
+                if (liveEdit) {
+                  // No drafting, patch and commit the published document
+                  published.createIfNotExists({
+                    _id: publishedId,
+                    _type: typeName
+                  })
+                  published.patch(patches)
+                } else {
+                  draft.createIfNotExists({
+                    ...omit(publishSnapshot, '_updatedAt'),
+                    _id: draftId,
+                    _type: typeName
+                  })
+                  draft.patch(patches)
+                }
+              },
+              discardDraft() {
+                draft.delete()
+              },
+              commit() {
+                return merge(published.commit(), draft.commit()).subscribe()
+              }
+            }
+          })
+        )
+      })
+    )
   )
 }
