3bd96cc24a98c2c03519bdd440bfa4e4274fb5c4
Anders Stensaas <stensaasanders@gmail.com>
2019-12-16 11:41:12 +0100

    [core] Add a tag option when interacting with GraphQL endpoints (#1606)

--- packages/@sanity/core/src/commands/graphql/deleteGraphQLAPICommand.js
+++ packages/@sanity/core/src/commands/graphql/deleteGraphQLAPICommand.js
@@ -1,8 +1,20 @@
 import lazyRequire from '@sanity/util/lib/lazyRequire'
 
+const helpText = `
+Options
+  --dataset <dataset> Delete GraphQL API for the given dataset
+  --tag <tag> Delete GraphQL API for the given tag (defaults to 'default')
+
+Examples
+  sanity graphql undeploy
+  sanity graphql undeploy --dataset staging
+  sanity graphql undeploy --dataset staging --tag next
+`
+
 export default {
   name: 'undeploy',
   group: 'graphql',
   description: 'Remove a deployed GraphQL API',
-  action: lazyRequire(require.resolve('../../actions/graphql/deleteApiAction'))
+  action: lazyRequire(require.resolve('../../actions/graphql/deleteApiAction')),
+  helpText
 }
