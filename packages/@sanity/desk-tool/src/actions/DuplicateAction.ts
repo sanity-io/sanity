fe3e2c054a2ac5d21570e138cea94f778119442e
Bjørge Næss <bjoerge@gmail.com>
2019-12-19 20:36:31 +0100

    Navigate after duping

--- packages/@sanity/desk-tool/src/actions/DuplicateAction.ts
+++ packages/@sanity/desk-tool/src/actions/DuplicateAction.ts
@@ -1,17 +1,26 @@
+import * as React from 'react'
 import {useDocumentOperation} from '@sanity/react-hooks'
 import {createAction} from 'part:@sanity/base/actions/utils'
 import ContentCopyIcon from 'part:@sanity/base/content-copy-icon'
+import {useRouter} from 'part:@sanity/base/router'
+import {getDraftId} from 'part:@sanity/base/util/draft-utils'
 
-export const DuplicateAction = createAction(function DuplicateAction(docInfo) {
-  const {duplicate}: any = useDocumentOperation(docInfo.id, docInfo.type)
+export const DuplicateAction = createAction(function DuplicateAction({id, type, onComplete}) {
+  const {duplicate}: any = useDocumentOperation(id, type)
+  const router = useRouter()
 
+  const [isDuplicating, setDuplicating] = React.useState(false)
   return {
     icon: ContentCopyIcon,
-    disabled: duplicate.disabled,
-    label: 'Duplicate',
+    disabled: isDuplicating || duplicate.disabled,
+    label: isDuplicating ? 'Duplicating…' : 'Duplicate',
     title: duplicate.disabled ? `Cannot duplicate: ${duplicate.disabled}` : 'Duplicate',
     onHandle: () => {
-      duplicate.execute()
+      setDuplicating(true)
+      duplicate.execute().then(copy => {
+        router.navigateIntent('edit', {id: getDraftId(copy._id), type: copy._type})
+        onComplete()
+      })
     }
   }
 })
