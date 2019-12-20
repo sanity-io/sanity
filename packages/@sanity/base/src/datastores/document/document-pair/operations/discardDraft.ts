4b101ad152433cd3fe571f1d2cadba14de78fe31
Per-Kristian Nordnes <per.kristian.nordnes@gmail.com>
2019-12-20 12:41:32 +0100

    [base] discardDraft action should be disabled if no published version

--- packages/@sanity/base/src/datastores/document/document-pair/operations/discardDraft.ts
+++ packages/@sanity/base/src/datastores/document/document-pair/operations/discardDraft.ts
@@ -1,7 +1,7 @@
 import {OperationArgs} from '../../types'
 
 export const discardDraft = {
-  disabled: ({snapshots}: OperationArgs) => !snapshots.draft,
+  disabled: ({snapshots}: OperationArgs) => !snapshots.published || !snapshots.draft,
   execute: ({versions}: OperationArgs) => {
     versions.draft.delete()
   }
