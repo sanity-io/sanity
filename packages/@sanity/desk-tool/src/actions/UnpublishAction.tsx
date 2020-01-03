57ff43033b4d3be7c2993d08be234cc44860aa96
Per-Kristian Nordnes <per.kristian.nordnes@gmail.com>
2020-01-03 15:42:02 +0100

    [desk-tool] Snackbar error feedback for UnpublishAction

--- packages/@sanity/desk-tool/src/actions/UnpublishAction.tsx
+++ packages/@sanity/desk-tool/src/actions/UnpublishAction.tsx
@@ -1,10 +1,10 @@
 import {useDocumentOperation} from '@sanity/react-hooks'
 import {createAction} from 'part:@sanity/base/actions/utils'
 import CloseIcon from 'part:@sanity/base/close-icon'
-
+import React from 'react'
 export const UnpublishAction = createAction(function UnpublishAction({id, type, onComplete}) {
   const {unpublish}: any = useDocumentOperation(id, type)
-
+  const [error, setError] = React.useState<Error | null>(null)
   return {
     icon: CloseIcon,
     disabled: Boolean(unpublish.disabled),
@@ -13,8 +13,14 @@ export const UnpublishAction = createAction(function UnpublishAction({id, type, 
       ? `Cannot unpublish. ${unpublish.disabled}`
       : 'Unpublish this document',
     onHandle: () => {
-      unpublish.execute()
-      onComplete()
+      setError(null)
+      unpublish.execute().then(onComplete, err => setError(err))
+    },
+    dialog: error && {
+      type: 'error',
+      onClose: () => setError(null),
+      title: 'An error occured',
+      content: error.message
     }
   }
 })
