98856cfc72fcb6ce406d91f51f09a03a96020e03
Per-Kristian Nordnes <per.kristian.nordnes@gmail.com>
2020-01-03 15:41:33 +0100

    [desk-tool] Snackbar feedback for error and success with PublishAction

--- packages/@sanity/desk-tool/src/actions/PublishAction.tsx
+++ packages/@sanity/desk-tool/src/actions/PublishAction.tsx
@@ -2,18 +2,38 @@ import * as React from 'react'
 import {useDocumentOperation} from '@sanity/react-hooks'
 import {createAction} from 'part:@sanity/base/actions/utils'
 
-export const PublishAction = createAction(function PublishAction(props) {
-  const {publish}: any = useDocumentOperation(props.id, props.type)
+export const PublishAction = createAction(function PublishAction({id, type, onComplete}) {
+  const {publish}: any = useDocumentOperation(id, type)
   const [publishing, setPublishing] = React.useState(false)
-
+  const [didPublish, setDidPublish] = React.useState(false)
+  const [error, setError] = React.useState<Error | null>(null)
   return {
     disabled: Boolean(publishing || publish.disabled),
     label: publishing ? 'Publishing…' : 'Publish',
     title: publish.disabled ? publish.disabled : '',
-    onHandle: async () => {
+    onHandle: () => {
       setPublishing(true)
-      await publish.execute()
-      props.onComplete()
-    }
+      setError(null)
+      setDidPublish(false)
+      publish.execute().then(
+        () => setDidPublish(true),
+        err => setError(err)
+      )
+    },
+    dialog:
+      (error && {
+        type: 'error',
+        onClose: () => setError(null),
+        title: 'An error occured when publishing the document',
+        content: error.message
+      }) ||
+      (didPublish && {
+        type: 'success',
+        onClose: () => {
+          setDidPublish(false)
+          onComplete()
+        },
+        title: 'Succesfully published document'
+      })
   }
 })
