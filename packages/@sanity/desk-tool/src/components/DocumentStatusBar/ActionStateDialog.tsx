3cc1f9424c7642efb167884603ec29542c2750fe
Per-Kristian Nordnes <per.kristian.nordnes@gmail.com>
2020-01-03 15:40:37 +0100

    [desk-tool] Success and error dialogs (Snackbar) for actionStates

--- packages/@sanity/desk-tool/src/components/DocumentStatusBar/ActionStateDialog.tsx
+++ packages/@sanity/desk-tool/src/components/DocumentStatusBar/ActionStateDialog.tsx
@@ -1,9 +1,12 @@
+/* eslint-disable react/jsx-handler-names */
+
 import * as React from 'react'
 import Dialog from 'part:@sanity/components/dialogs/default'
 import DialogContent from 'part:@sanity/components/dialogs/content'
 import Button from 'part:@sanity/components/buttons/default'
 import ButtonGrid from 'part:@sanity/components/buttons/button-grid'
 import PopOverDialog from 'part:@sanity/components/dialogs/popover'
+import Snackbar from 'part:@sanity/components/snackbar/default'
 
 import styles from './DocumentStatusBar.css'
 
@@ -36,8 +39,28 @@ interface LegacyDialogProps {
   onClose: () => void
 }
 
+interface ErrorDialogProps {
+  type: 'error'
+  title: string
+  content: React.ReactNode
+  onClose: () => void
+}
+
+interface SuccessDialogProps {
+  type: 'success'
+  title: string
+  content: React.ReactNode
+  onClose: () => void
+}
+
 interface Props {
-  dialog: ConfirmDialogProps | LegacyDialogProps | ModalDialogProps | PopOverDialogProps
+  dialog:
+    | ConfirmDialogProps
+    | LegacyDialogProps
+    | ModalDialogProps
+    | PopOverDialogProps
+    | ErrorDialogProps
+    | SuccessDialogProps
 }
 
 export function ActionStateDialog(props: Props) {
@@ -90,6 +113,29 @@ export function ActionStateDialog(props: Props) {
       >
         {dialog.content}
       </PopOverDialog>
+    )
+  }
+
+  if (dialog.type === 'success') {
+    return (
+      <Snackbar
+        kind="success"
+        isPersisted={false}
+        isCloseable={false}
+        timeout={2000}
+        onClose={dialog.onClose}
+        title={dialog.title}
+      >
+        {dialog.content}
+      </Snackbar>
+    )
+  }
+
+  if (dialog.type === 'error') {
+    return (
+      <Snackbar isCloseable kind="error" onClose={dialog.onClose} title={dialog.title}>
+        {dialog.content}
+      </Snackbar>
     )
   }
 
