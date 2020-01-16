c2dd7c38582ff99ad80ea565cef3db52aa82a9f0
Anders Stensaas <stensaasanders@gmail.com>
2020-01-16 14:09:41 +0100

    [core] GraphQL: Explicitly add all filters for all types for clarity

---
+++ packages/@sanity/core/src/actions/graphql/filters/booleanFilters.js
@@ -0,0 +1,20 @@
+function createBooleanFilters() {
+  return {
+    name: 'BooleanFilter',
+    kind: 'InputObject',
+    fields: [
+      {
+        fieldName: 'eq',
+        type: 'Boolean',
+        description: 'Checks if the value is equal to the given input.'
+      },
+      {
+        fieldName: 'neq',
+        type: 'Boolean',
+        description: 'Checks if the value is not equal to the given input.'
+      }
+    ]
+  }
+}
+
+module.exports = createBooleanFilters
