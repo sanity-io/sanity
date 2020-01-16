657f8f85f152d705a46c7ce6ae17858caf01255e
Espen Hovlandsdal <espen@hovlandsdal.com>
2020-01-15 19:10:51 -0800

    [all] Remove unused interdependencies (#1700)
    * [google-maps-input] Remove example + interdependencies
    
    * [vision] Remove unused dependencies
    
    * [components] Remove demo + interdependencies
    
    * [default-layout] Remove example + interdependencies
    
    * [desk-tool] Remove examples + interdependencies
    
    * [form-builder] Remove examples + interdependencies
    
    * [studio-hints] Synchronize dependency versions

--- packages/@sanity/default-layout/example/previewResolver.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const Preview = () => <div>Preview</div>
-
-export default function previewResolver(field) {
-  return Preview
-}
--- packages/@sanity/form-builder/examples/schema-testbed/lib/utils.js
+++
@@ -1,8 +0,0 @@
-export function preventDefault(event) {
-  event.preventDefault()
-}
-
-export function parseParams(pathname) {
-  const [schemaName, typeName] = pathname.split('/').filter(Boolean)
-  return {schemaName, typeName}
-}
