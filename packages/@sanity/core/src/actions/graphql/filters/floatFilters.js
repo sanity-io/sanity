618180d2641de40104d88282bd040df85d558c02
Anders Stensaas <stensaasanders@gmail.com>
2020-01-16 14:09:41 +0100

    [core] GraphQL: Explicitly add all filters for all types for clarity

---
+++ packages/@sanity/core/src/actions/graphql/filters/floatFilters.js
@@ -0,0 +1,40 @@
+function createFloatFilters() {
+  return {
+    name: 'FloatFilter',
+    kind: 'InputObject',
+    fields: [
+      {
+        fieldName: 'eq',
+        type: 'Float',
+        description: 'Checks if the value is equal to the given input.'
+      },
+      {
+        fieldName: 'neq',
+        type: 'Float',
+        description: 'Checks if the value is not equal to the given input.'
+      },
+      {
+        fieldName: 'gt',
+        type: 'Float',
+        description: 'Checks if the value is greater than the given input.'
+      },
+      {
+        fieldName: 'gte',
+        type: 'Float',
+        description: 'Checks if the value is greater than or equal to the given input.'
+      },
+      {
+        fieldName: 'lt',
+        type: 'Float',
+        description: 'Checks if the value is lesser than the given input.'
+      },
+      {
+        fieldName: 'lte',
+        type: 'Float',
+        description: 'Checks if the value is lesser than or equal to the given input.'
+      }
+    ]
+  }
+}
+
+module.exports = createFloatFilters
