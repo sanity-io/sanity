ab9ee8365c373204496fd2595d18c4656750ee7d
Bjørge Næss <bjoerge@gmail.com>
2020-01-27 16:39:40 +0100

    [form-builder] Deprecate checkoutPair and move receiveEvent to SanityFormBuilderContext

--- packages/@sanity/form-builder/src/sanity/SanityFormBuilderContext.tsx
+++ packages/@sanity/form-builder/src/sanity/SanityFormBuilderContext.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import FormBuilderContext from '../FormBuilderContext'
 import SanityPreview from 'part:@sanity/base/preview'
 import inputResolver from './inputResolver/inputResolver'
+import * as gradientPatchAdapter from './utils/gradientPatchAdapter'
 
 const previewResolver = () => SanityPreview
 type Props = {
@@ -24,4 +25,43 @@ export default function SanityFormBuilderContext(props: Props) {
     </FormBuilderContext>
   )
 }
-SanityFormBuilderContext.createPatchChannel = FormBuilderContext.createPatchChannel
+
+function prepareMutationEvent(event) {
+  const patches = event.mutations.map(mut => mut.patch).filter(Boolean)
+  return {
+    ...event,
+    patches: gradientPatchAdapter.toFormBuilder(event.origin, patches)
+  }
+}
+
+function prepareRebaseEvent(event) {
+  const patches = [
+    {
+      id: event.document._id,
+      set: event.document
+    }
+  ]
+  return {
+    type: 'mutation',
+    document: event.document,
+    mutations: patches.map(patch => ({
+      patch
+    })),
+    patches: gradientPatchAdapter.toFormBuilder('internal', patches)
+  }
+}
+
+SanityFormBuilderContext.createPatchChannel = () => {
+  const patchChannel = FormBuilderContext.createPatchChannel()
+  return {
+    receiveEvent: event => {
+      if (event.type !== 'mutation' && event.type !== 'rebase') {
+        return
+      }
+      patchChannel.receivePatches(
+        event.type === 'mutation' ? prepareMutationEvent(event) : prepareRebaseEvent(event)
+      )
+    },
+    onPatch: patchChannel.onPatch
+  }
+}
