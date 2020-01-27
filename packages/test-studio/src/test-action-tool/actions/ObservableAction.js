c4451ae07f7bcc8a4f60fd1504ccb1399bba7cf7
Bjørge Næss <bjoerge@gmail.com>
2020-01-27 16:48:55 +0100

    [test-studio] Add an actions playground tool

---
+++ packages/test-studio/src/test-action-tool/actions/ObservableAction.js
@@ -0,0 +1,13 @@
+import {useObservable} from '@sanity/react-hooks'
+import {timer} from 'rxjs'
+import {map} from 'rxjs/operators'
+
+export default function ObservableAction(docInfo) {
+  return useObservable(
+    timer(0, 1000).pipe(
+      map(n => ({
+        label: n % 2 === 0 ? 'Tick' : 'Tack'
+      }))
+    )
+  )
+}
---
+++ packages/test-studio/src/test-action-tool/actions/SaveAction.js
@@ -0,0 +1,11 @@
+import {useDocumentOperation} from '@sanity/react-hooks'
+
+export default function SaveAction(docInfo) {
+  const {commit} = useDocumentOperation(docInfo.id, docInfo.type)
+  return {
+    label: 'Save',
+    onHandle: () => {
+      commit.execute()
+    }
+  }
+}
