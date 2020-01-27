7667f9787088f2511bbb11e3feff8fdac82150ff
Bjørge Næss <bjoerge@gmail.com>
2020-01-27 16:49:55 +0100

    [test-studio] Add some custom document actions and badges to test-studio

---
+++ packages/test-studio/src/resolveDocumentBadges.js
@@ -0,0 +1,13 @@
+import defaultResolve from 'part:@sanity/base/document-badges'
+
+function CustomBadge() {
+  return {
+    label: 'Hello World',
+    title: 'Hello I am a custom document badge',
+    color: 'success'
+  }
+}
+
+export default function resolveDocumentBadges(props) {
+  return [...defaultResolve(props), CustomBadge]
+}
