ac7cd7399269f349e8192b2222f6d5d56eb64c25
Espen Hovlandsdal <espen@hovlandsdal.com>
2020-02-04 15:00:13 -0800

    [core] GraphQL: Move document filters to separate `_` property/type

---
+++ packages/@sanity/core/src/actions/graphql/v2/filters/documentFilters.js
@@ -0,0 +1,21 @@
+function createDocumentFilters() {
+  return {
+    name: 'DocumentFilter',
+    kind: 'InputObject',
+    isConstraintFilter: true,
+    fields: [
+      {
+        fieldName: 'references',
+        type: 'ID',
+        description: 'All documents referencing the given document ID.'
+      },
+      {
+        fieldName: 'is_draft',
+        type: 'Boolean',
+        description: 'All documents that are drafts.'
+      }
+    ]
+  }
+}
+
+module.exports = createDocumentFilters
