ccd5524595c219130c4d56d49f7ea00a51db19bf
Espen Hovlandsdal <espen@hovlandsdal.com>
2020-02-07 16:34:18 -0800

    [base] Include typescript definition stubs for parts + css modules

---
+++ packages/@sanity/base/types/parts.d.ts
@@ -0,0 +1,24 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+declare module 'all:*' {
+  const anyArray: any[]
+
+  export default anyArray
+}
+
+declare module 'config:*' {
+  const pluginConfig: {[key: string]: any}
+
+  export default pluginConfig
+}
+
+declare module 'part:*' {
+  const any: any
+
+  export default any
+}
+
+declare module '*.css' {
+  const cssModule: {[key: string]: string}
+
+  export default cssModule
+}
