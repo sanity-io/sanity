407f675e9158d0bad8e7b128943e37a724276898
Bjørge Næss <bjoerge@gmail.com>
2020-02-19 09:40:32 +0100

    [base] Allow passing initialValue to patch

--- packages/@sanity/base/src/datastores/document/document-pair/operations/patch.ts
+++ packages/@sanity/base/src/datastores/document/document-pair/operations/patch.ts
@@ -1,20 +1,27 @@
 import {OperationArgs} from '../../types'
 import {isLiveEditEnabled} from '../utils/isLiveEditEnabled'
 
+// todo: we could also consider exposing 'mutate' directly
 export const patch = {
   disabled: (): false => false,
-  execute: ({snapshots, idPair, draft, published, typeName}: OperationArgs, patches = []): void => {
+  execute: (
+    {snapshots, idPair, draft, published, typeName}: OperationArgs,
+    patches = [],
+    initialValue
+  ): void => {
     if (isLiveEditEnabled(typeName)) {
       // No drafting, so patch and commit the published document
       published.mutate([
         published.createIfNotExists({
-          _type: typeName
+          _type: typeName,
+          ...initialValue
         }),
         ...published.patch(patches)
       ])
     } else {
       draft.mutate([
         draft.createIfNotExists({
+          ...initialValue,
           ...snapshots.published,
           _id: idPair.draftId,
           _type: typeName
