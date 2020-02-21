b96332bda03cb8bd2883a8be1a124365e5c6e63a
Anders Stensaas <stensaasanders@gmail.com>
2020-02-21 11:19:17 +0100

    [core] GraphQL: Rename from "vX" to "genX" in GraphQL
    As this might confuse users, as "v1" is in the URL, and actually has nothing to do with the generation at all. We decided to change this to "genX" so they will be properly separated by name.

--- packages/@sanity/core/src/commands/graphql/deployGraphQLAPICommand.js
+++ packages/@sanity/core/src/commands/graphql/deployGraphQLAPICommand.js
@@ -4,7 +4,7 @@ const helpText = `
 Options
   --dataset <dataset> Deploy API for the given dataset
   --tag <tag> Deploy API to given tag (defaults to 'default')
-  --generation <generation> API generation to deploy (defaults to 'v2')
+  --generation <generation> API generation to deploy (defaults to 'gen2')
   --playground Deploy a GraphQL playground for easily testing queries (public)
   --no-playground Skip playground prompt (do not deploy a playground)
   --force Deploy API without confirming breaking changes
@@ -12,7 +12,7 @@ Options
 Examples
   sanity graphql deploy
   sanity graphql deploy --playground
-  sanity graphql deploy --generation v1
+  sanity graphql deploy --generation gen1
   sanity graphql deploy --dataset staging --no-playground
   sanity graphql deploy --dataset staging --tag next --no-playground
   sanity graphql deploy --no-playground --force
