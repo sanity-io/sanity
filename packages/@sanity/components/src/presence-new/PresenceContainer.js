e5c3fe09c90de5b1f12b8dc11a75deda00a850c0
Per-Kristian Nordnes <per.kristian.nordnes@gmail.com>
2020-02-28 13:52:19 +0100

    Fix cruft

--- packages/@sanity/components/src/presence-new/PresenceContainer.js
+++ packages/@sanity/components/src/presence-new/PresenceContainer.js
@@ -3,8 +3,8 @@ import Avatar from './Avatar'
 
 export default function PresenceContainer({presence}) {
   return (
-    <div data-presence-container={presence && presence.map(u => u.id)}>
-      {presence && presence.map(user => <Avatar key={user.id} id={user.id} />)}
+    <div data-presence-container={presence && presence.map(u => u.identity)}>
+      {presence && presence.map(item => <Avatar key={item.identity} id={item.identity} />)}
     </div>
   )
 }
