e030eaa3fc620739effc0b1942f9bae1e969cf01
Bjørge Næss <bjoerge@gmail.com>
2020-03-20 15:25:22 +0100

    [base] Add sessionId to presence entries

--- packages/@sanity/base/src/hooks/usePresence.tsx
+++ packages/@sanity/base/src/hooks/usePresence.tsx
@@ -1,6 +1,8 @@
 import {useState, useEffect} from 'react'
 import {clients$} from 'part:@sanity/base/presence'
 import * as PathUtils from '@sanity/util/paths.js'
+import {distinctUntilChanged, map, tap} from 'rxjs/operators'
+import shallowEquals from 'shallow-equals'
 
 type KeyedSegment = {
   _key: string
@@ -19,25 +21,30 @@ interface PresenceFilter {
 export default function usePresence(filter: PresenceFilter) {
   const [presence, setPresence] = useState([])
   useEffect(() => {
-    const subscription = clients$.subscribe(clients => {
-      setPresence(
-        clients.filter(client =>
-          client.sessions.some(
-            session =>
-              session.state &&
-              session.state.some(
-                item =>
-                  item.namespace === filter.namespace &&
-                  item.documentId === filter.documentId &&
-                  (!filter.path || PathUtils.startsWith(filter.path, item.path))
-              )
+    const subscription = clients$
+      .pipe(
+        map(clients =>
+          clients.filter(client =>
+            client.sessions.some(
+              session =>
+                session.state &&
+                session.state.some(
+                  item =>
+                    item.namespace === filter.namespace &&
+                    item.documentId === filter.documentId &&
+                    (!filter.path || PathUtils.startsWith(filter.path, item.path))
+                )
+            )
           )
-        )
+        ),
+        distinctUntilChanged(shallowEquals)
       )
-    })
+      .subscribe(clients => {
+        setPresence(clients)
+      })
     return () => {
       subscription.unsubscribe()
     }
-  }, [clients$])
+  }, [filter.documentId, filter.namespace, filter.path])
   return presence
 }
