975fa3ac6f4b9f3ef819ae03af755935e2787171
Bjørge Næss <bjoerge@gmail.com>
2020-03-23 11:35:51 +0100

    Remove styled components

--- packages/@sanity/form-builder/src/sanity/AbsoluteOverlayRenderer.tsx
+++ packages/@sanity/form-builder/src/sanity/AbsoluteOverlayRenderer.tsx
@@ -1,30 +1,30 @@
 import * as React from 'react'
-import styled from 'styled-components'
 
-const Item = styled.div`
-  display: inline-block;
-  transition-property: all;
-  transition-duration: 1s;
-  background: rgba(255, 0, 0, 0.25);
-  overflow: hidden;
-  text-align: right;
-  pointer-events: all;
-  position: absolute;
-  outline: 1px solid #f00;
-`
+const ITEM_STYLE = {
+  display: 'inline-block',
+  transitionProperty: 'all',
+  transitionDuration: '1s',
+  background: 'rgba(255, 0, 0, 0.25)',
+  overflow: 'hidden',
+  textAlign: 'right',
+  pointerEvents: 'all',
+  position: 'absolute',
+  outline: '1px solid #f00'
+}
 
 export function AbsoluteOverlayRenderer(props) {
   const {items} = props
   return items.map(item => {
     return (
-      <Item
+      <div
         key={item.id}
         style={{
+          ...ITEM_STYLE,
           ...item.rect
         }}
       >
         {item.id}
-      </Item>
+      </div>
     )
   })
 }
