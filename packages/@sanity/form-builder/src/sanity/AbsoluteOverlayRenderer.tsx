b5c0a3463eb3ccadd14e73d05ac32aae59b282b0
Bjørge Næss <bjoerge@gmail.com>
2020-03-30 16:03:37 +0200

    [overlayer] Use offsets

--- packages/@sanity/form-builder/src/sanity/AbsoluteOverlayRenderer.tsx
+++ packages/@sanity/form-builder/src/sanity/AbsoluteOverlayRenderer.tsx
@@ -1,9 +1,17 @@
 import * as React from 'react'
+import {sortBy} from 'lodash'
+
+const OVERLAY_STYLE: React.CSSProperties = {
+  position: 'absolute',
+  top: 0,
+  left: 0,
+  right: 0,
+  bottom: 0,
+  pointerEvents: 'none'
+}
 
 const ITEM_STYLE = {
   display: 'inline-block',
-  transitionProperty: 'all',
-  transitionDuration: '1s',
   background: 'rgba(255, 0, 0, 0.25)',
   overflow: 'hidden',
   textAlign: 'right',
