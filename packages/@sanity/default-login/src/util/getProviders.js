49f87f035c40ced2e82405f047b6e0c316218986
Espen Hovlandsdal <espen@hovlandsdal.com>
2020-04-01 18:05:48 -0700

    [default-login] Treat official provider URLs as non-custom

--- packages/@sanity/default-login/src/util/getProviders.js
+++ packages/@sanity/default-login/src/util/getProviders.js
@@ -10,17 +10,27 @@ export default function getProviders() {
     })
     .then(res => {
       const {providers, thirdPartyLogin} = res
-      const customProviders = (config.providers && config.providers.entries
-        ? config.providers.entries
-        : []
-      ).map(provider => {
-        provider.custom = true
-        provider.supported = thirdPartyLogin
-        return provider
-      })
-      if (customProviders.length && config.providers && config.providers.mode === 'replace') {
-        return Promise.resolve(customProviders)
+      const customProviders = (config.providers && config.providers.entries) || []
+      if (customProviders.length === 0) {
+        return providers
       }
-      return providers.concat(customProviders)
+
+      const custom = customProviders.map(provider => {
+        // A user may want to remove certain login options (eg GitHub) and thus provide "official"
+        // login options through the config. These shouldn't be treated as custom login providers
+        // which require the third-party login feature, but as the official provider
+        const isOfficial = providers.some(official => official.url === provider.url)
+        return {...provider, custom: !isOfficial, supported: isOfficial || thirdPartyLogin}
+      })
+
+      if (config.providers.mode === 'replace') {
+        return custom
+      }
+
+      // Append to the list of official providers, but replace any provider that has
+      // the same URL with the custom one (allows customizing the title, name)
+      return providers
+        .filter(official => custom.some(provider => provider.url !== official.url))
+        .concat(custom)
     })
 }
