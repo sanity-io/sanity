9753c18d7f55cbc749e8980cf6bb2d54a1d6f6cb
Per-Kristian Nordnes <per.kristian.nordnes@gmail.com>
2020-04-03 23:38:06 +0200

    [test-studio] Add simple block editor document

---
+++ packages/test-studio/schemas/simpleBlock.js
@@ -0,0 +1,18 @@
+export default {
+  name: 'simpleBlock',
+  title: 'Simple block',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      title: 'Title',
+      type: 'string'
+    },
+    {
+      name: 'body',
+      title: 'Body',
+      type: 'array',
+      of: [{type: 'block'}]
+    }
+  ]
+}
