91199ab97f526ab85b28fa1524e873e87919b7c2
vicbergquist <victoria.bergquist@gmail.com>
2020-04-07 11:19:37 +0200

    [presence] start adding stack

--- packages/@sanity/components/src/presence-new/PresenceContainer.js
+++ packages/@sanity/components/src/presence-new/PresenceContainer.js
@@ -1,19 +1,22 @@
 import React from 'react'
 import styles from './styles/PresenceContainer.css'
 import Avatar from './Avatar'
+import AvatarStack from './AvatarStack'
 
 export default function PresenceContainer({presence, position}) {
   return (
     <div className={styles.root}>
-      {presence &&
-        presence.map(item => (
-          <Avatar
-            key={item.sessionId}
-            position={position}
-            userId={item.identity}
-            sessionId={item.sessionId}
-          />
-        ))}
+      <AvatarStack>
+        {presence &&
+          presence.map(item => (
+            <Avatar
+              key={item.sessionId}
+              position={position}
+              userId={item.identity}
+              sessionId={item.sessionId}
+            />
+          ))}
+      </AvatarStack>
     </div>
   )
 }
