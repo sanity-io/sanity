93460f31277e294b984a414e9d834d8526803c50
Per-Kristian Nordnes <per.kristian.nordnes@gmail.com>
2020-04-15 02:53:48 +0200

    [form-builder] More PortableTextInput

--- packages/@sanity/form-builder/src/inputs/PortableText/PortableTextInput.tsx
+++ packages/@sanity/form-builder/src/inputs/PortableText/PortableTextInput.tsx
@@ -2,16 +2,19 @@ import React from 'react'
 import {
   PortableTextEditor,
   PortableTextBlock,
+  PortableTextChild,
   EditorSelection,
   Type,
   EditorChange,
-  compactPatches
+  InvalidValue as InvalidEditorValue
 } from '@sanity/portable-text-editor'
 import FormField from 'part:@sanity/components/formfields/default'
-import PatchEvent, {set, unset} from '../../PatchEvent'
+import InvalidValue from './InvalidValue'
+import PatchEvent from '../../PatchEvent'
 import {Marker} from '../../typedefs'
-import {Subject, Subscription} from 'rxjs'
-import {distinctUntilChanged} from 'rxjs/operators'
+import {Patch} from '../../typedefs/patch'
+import withPatchSubscriber from '../../utils/withPatchSubscriber'
+import {Subject} from 'rxjs'
 
 type Props = {
   type: Type
@@ -31,104 +34,191 @@ type Props = {
     insert?: PortableTextBlock[]
     path?: []
   }
+  subscribe: (arg0: ({patches: PatchEvent}) => void) => void
 }
 
 type State = {
   selection: EditorSelection
+  isLoading: boolean
+  invalidValue: InvalidEditorValue | null
+  valueWithError: PortableTextBlock[] | undefined
 }
 
-const HOTKEYS = {
-  marks: {
-    'mod+b': 'strong',
-    'mod+i': 'em',
-    'mod+´': 'code'
-  }
+type PatchWithOrigin = Patch & {
+  origin: 'local' | 'remote' | 'internal'
+  timestamp: Date
 }
 
-export default class PortableTextInput extends React.PureComponent<Props, State> {
-  private _editor: PortableTextEditor | null
-  private _pendingPatches = []
-  private _isThrottling = false
-  private changes$: Subject<EditorChange> = new Subject()
-  private changes: Subscription
-  state = {
-    selection: null
-  }
-  constructor(props: Props) {
-    super(props)
-    this.changes = this.changes$.pipe(distinctUntilChanged()).subscribe(this.handleChange)
-  }
-
-  private commit(): void {
-    const cPatches = compactPatches(this._pendingPatches)
-    this.props.onChange(PatchEvent.from(cPatches))
-    this._pendingPatches = []
-  }
-
-  private setEditor = (editor: PortableTextEditor | null) => {
-    this._editor = editor
-  }
-
-  componentWillUnmount() {
-    this.changes$.unsubscribe()
-  }
-
-  handleChange = (next: EditorChange): void => {
-    switch (next.type) {
-      case 'mutation':
-        if (!this._isThrottling) {
-          this.commit()
-        } else {
-          this._pendingPatches = [...this._pendingPatches, ...next.patches]
-        }
-        break
-      case 'selection':
-        this.setState({selection: next.selection})
-        break
-      case 'throttle':
-        if (next.throttle) {
-          this._isThrottling = true
-        } else {
-          this._isThrottling = false
-          // Commit pending patches (compacted)
-          if (this._pendingPatches.length > 0) {
-            this.commit()
-          }
-        }
-        break
-      default:
-        throw new Error('Unhandled editor event')
+export default withPatchSubscriber(
+  class PortableTextInput extends React.PureComponent<Props, State> {
+    private editor: PortableTextEditor | null
+    private incoming: PatchWithOrigin[] = []
+    private usubscribe: any
+    private patche$: any = new Subject()
+    private hotkeys = {
+      marks: {
+        'mod+b': 'strong',
+        'mod+i': 'em',
+        'mod+´': 'code'
+      }
     }
-  }
-
-  focus(): void {
-    if (this._editor) {
-      this._editor.focus()
+    state = {
+      selection: null,
+      isLoading: false,
+      invalidValue: null,
+      valueWithError: undefined
     }
-  }
 
-  render() {
-    const {value, readOnly, type, markers, level, onFocus, onBlur} = this.props
-    const validation = markers.filter(marker => marker.type === 'validation')
-    const errors = validation.filter(marker => marker.level === 'error')
-    const {selection} = this.state
-    return (
-      <FormField markers={markers} level={level} label={type.title} description={type.description}>
-        <div style={{overflowY: 'auto', maxHeight: '300px', overflowX: 'hidden'}}>
-          <PortableTextEditor
-            ref={this.setEditor}
-            placeholderText=""
-            type={type}
-            changes={this.changes$}
-            selection={selection}
-            hotkeys={HOTKEYS}
-            value={value}
-            maxBlocks={-1} // TODO: from schema
-            spellCheck={false}
-            readOnly={readOnly}
-          />
+    static getDerivedStateFromProps(nextProps: Props, prevState: State): {} | null {
+      if (nextProps.value !== prevState.valueWithError && prevState.invalidValue) {
+        return {invalidValue: null}
+      }
+      return null
+    }
+
+    constructor(props: Props) {
+      super(props)
+      this.usubscribe = props.subscribe(this.handleDocumentPatches)
+    }
+
+    componentWillUnmount(): void {
+      this.usubscribe()
+    }
+
+    private setEditor = (editor: PortableTextEditor | null): void => {
+      this.editor = editor
+    }
+
+    private handleDocumentPatches = ({
+      patches
+    }: {
+      patches: PatchWithOrigin[]
+      snapshot: PortableTextBlock[] | undefined
+    }): void => {
+      const selection =
+        patches && patches.length > 0 && patches.filter(patch => patch.origin !== 'local')
+      if (selection) {
+        this.incoming = this.incoming.concat(selection)
+        selection.map(patch => this.patche$.next(patch))
+      }
+    }
+
+    private handleChange = (change: EditorChange): void => {
+      switch (change.type) {
+        case 'mutation':
+          this.props.onChange(PatchEvent.from(change.patches))
+          break
+        case 'selection':
+          this.setState({selection: change.selection})
+          break
+        case 'focus':
+          this.props.onFocus && this.props.onFocus()
+        case 'blur':
+          this.props.onBlur && this.props.onBlur()
+          break
+        case 'loading':
+          this.setState({isLoading: change.isLoading})
+          break
+        case 'undo':
+        case 'redo':
+          this.props.onChange(PatchEvent.from(change.patches))
+          break
+        case 'invalidValue':
+          this.setState({invalidValue: change, valueWithError: this.props.value})
+          break
+        case 'patch':
+        case 'value':
+        case 'unset':
+          break
+        default:
+          throw new Error(`Unhandled editor change ${JSON.stringify(change)}`)
+      }
+    }
+
+    focus(): void {
+      if (this.editor) {
+        this.editor.focus()
+      }
+    }
+
+    renderBlock = (
+      block: PortableTextBlock,
+      attributes: {focused: boolean; selected: boolean}
+    ): JSX.Element => {
+      if (!this.editor) {
+        return null
+      }
+      // Offload rendering text to the editor
+      if (block._type === this.editor.getPortableTextFeatures().types.block.name) {
+        return undefined
+      }
+      // Render object blocks (images, etc)
+      return (
+        <div
+          style={{color: '#999', fontFamily: 'monospace', padding: '1em', backgroundColor: '#eee'}}
+        >
+          {JSON.stringify(block)} {JSON.stringify(attributes)}
         </div>
-      </FormField>
-    )
+      )
+    }
+
+    renderChild = (child: PortableTextChild): JSX.Element => {
+      if (!this.editor) {
+        return null
+      }
+      // Offload rendering text to the editor
+      if (child._type === this.editor.getPortableTextFeatures().types.span.name) {
+        return undefined
+      }
+      // Render object childs (images, etc)
+      return (
+        <span style={{fontFamily: 'monospace', padding: '1em', backgroundColor: '#eee'}}>
+          {JSON.stringify(child)}
+        </span>
+      )
+    }
+
+    render(): React.ReactNode {
+      const {value, readOnly, type, markers, level, onFocus, onBlur} = this.props
+      const validation = markers.filter(marker => marker.type === 'validation')
+      const errors = validation.filter(marker => marker.level === 'error')
+      const {isLoading, selection, invalidValue} = this.state
+      if (invalidValue) {
+        return (
+          <InvalidValue
+            onChange={this.handleChange}
+            resolution={invalidValue.resolution}
+            value={value}
+          />
+        )
+      }
+      return (
+        <FormField
+          markers={markers}
+          level={level}
+          label={type.title}
+          description={type.description}
+        >
+          <div style={{overflowY: 'auto', maxHeight: '300px', overflowX: 'hidden'}}>
+            <PortableTextEditor
+              hotkeys={this.hotkeys}
+              maxBlocks={-1} // TODO: from schema
+              onChange={this.handleChange}
+              incomingPatche$={this.patche$}
+              placeholderText={value ? '' : '[No value]'}
+              readOnly={readOnly}
+              ref={this.setEditor}
+              renderBlock={this.renderBlock}
+              renderChild={this.renderChild}
+              searchAndReplace
+              selection={selection}
+              spellCheck={false}
+              type={type}
+              value={value}
+            />
+          </div>
+        </FormField>
+      )
+    }
   }
-}
+)
