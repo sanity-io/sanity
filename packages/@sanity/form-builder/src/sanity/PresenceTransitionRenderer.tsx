c35f41d636434c8b6d9e1ae20b9a7eaf928626ae
Bjørge Næss <bjoerge@gmail.com>
2020-04-15 14:45:14 +0200

    Top group with collapsing

--- packages/@sanity/form-builder/src/sanity/PresenceTransitionRenderer.tsx
+++ packages/@sanity/form-builder/src/sanity/PresenceTransitionRenderer.tsx
@@ -5,23 +5,32 @@ import * as React from 'react'
 import {StickyOverlayRenderer} from './StickyOverlayRenderer'
 import {groupBy, orderBy} from 'lodash'
 import {CSSProperties} from 'react'
+import AvatarProvider from '@sanity/components/lib/presence-new/AvatarProvider'
+import Avatar from '@sanity/components/lib/presence-new/Avatar'
 
 const split = (array, index) => [array.slice(0, index), array.slice(index)]
+const splitRight = (array, index) => {
+  const idx = Math.max(0, array.length - index)
+  return [array.slice(0, idx), array.slice(idx)]
+}
 
-const MAX_AVATARS = 1
+const MAX_AVATARS = 2
 
-const ITEM_STYLE: CSSProperties = {
-  zIndex: 1100,
+const ITEM_TRANSITION: CSSProperties = {
   transition: 'transform',
   transitionDuration: '200ms',
-  transitionTimingFunction: 'ease-in-out',
+  transitionTimingFunction: 'ease-in-out'
+}
+const ITEM_STYLE: CSSProperties = {
+  ...ITEM_TRANSITION,
+  zIndex: 1100,
   position: 'sticky',
   pointerEvents: 'all',
   top: 8,
   bottom: 8
 }
 
-const RenderInside = props => {
+const RenderItem = props => {
   const {childComponent: ChildComponent, presence, ...rest} = props
   return <ChildComponent presence={presence} {...rest} />
 }
@@ -69,50 +78,65 @@ function StickyPresenceTransitionRenderer(props) {
         )
         const grouped = group(entries)
         return [
-          renderInside(grouped.top, maxRight),
-          renderInside(grouped.inside, maxRight),
-          renderInside(grouped.bottom, maxRight)
+          renderTop(grouped.top, maxRight),
+          ...renderGroup(grouped.inside, maxRight),
+          ...renderGroup(grouped.bottom, maxRight)
         ]
       }}
     />
   )
 }
 
+const Spacer = ({height, ...rest}) => <div style={{height: Math.max(0, height), ...rest?.style}} />
+
 function renderTop(entries, maxRight) {
-  return entries.map((entry, i) => {
-    const {
-      childComponent: ChildComponent,
-      avatarComponent: AvatarComponent,
-      presence,
-      ...rest
-    } = entry.item.props
-    return (
-      <React.Fragment key={entry.item.id}>
-        <div style={{height: Math.max(0, entry.spacerHeight)}} />
-        {presence?.map((p, i) => {
-          return (
-            <>
-              <div
-                style={{
-                  ...ITEM_STYLE,
-                  backgroundColor: 'black',
-                  transform: `translate3d(${maxRight -
-                    entry.item.rect.width / presence.length -
-                    entry.indent -
-                    (entry.item.rect.width / presence.length) * i}px, 0px, 0px)`,
-                  height: entry.item.rect.height,
-                  width: entry.item.rect.width / presence.length
-                }}
-              >
-                <AvatarComponent {...p} userId={p.identity} />
-              </div>
-            </>
-          )
-        })}
-      </React.Fragment>
-    )
-  })
+  const spacerSum = entries.reduce((total, entry) => total + entry.spacerHeight, 0)
+
+  const allPresenceItems = entries.flatMap(entry => entry.item.props.presence || [])
+
+  const [collapsed, visible] = splitRight(allPresenceItems, MAX_AVATARS)
+
+  return (
+    <React.Fragment>
+      <Spacer height={spacerSum} />
+      <div
+        style={{
+          ...ITEM_STYLE,
+          display: 'flex',
+          justifyContent: 'flex-end'
+        }}
+      >
+        {collapsed.length > 0 && (
+          <div
+            key={collapsed.slice(-1)[0].sessionId}
+            style={{
+              ...ITEM_TRANSITION,
+              position: 'absolute',
+              transform: `translate3d(${-25 * MAX_AVATARS}px, 0px, 0px)`
+            }}
+          >
+            <Avatar label={collapsed.map(a => a.displayName).join(', ')} color="salmon">
+              +{collapsed.length}
+            </Avatar>
+          </div>
+        )}
+        {visible.map((p, i) => (
+          <div
+            key={p.sessionId}
+            style={{
+              ...ITEM_TRANSITION,
+              position: 'absolute',
+              transform: `translate3d(${-25 * (visible.length - i -1)}px, 0px, 0px)`
+            }}
+          >
+            <AvatarProvider userId={p.identity} {...p} />
+          </div>
+        ))}
+      </div>
+    </React.Fragment>
+  )
 }
+
 //  keep for debugging purposes
 // function renderAbsolute(entry) {
 //   return (
@@ -123,10 +147,10 @@ function renderTop(entries, maxRight) {
 //   )
 // }
 
-function renderInside(entries, maxRight) {
+function renderGroup(entries, maxRight) {
   return entries.map(entry => (
     <React.Fragment key={entry.item.id}>
-      <div style={{height: Math.max(0, entry.spacerHeight)}} />
+      <Spacer height={entry.spacerHeight} />
       <div
         style={{
           ...ITEM_STYLE,
@@ -135,12 +159,11 @@ function renderInside(entries, maxRight) {
               ? maxRight - entry.item.rect.width - entry.indent
               : entry.item.rect.left
           }px, 0px, 0px)`,
-
           height: entry.item.rect.height,
           width: entry.item.rect.width
         }}
       >
-        <RenderInside {...entry.item.props} />
+        <RenderItem {...entry.item.props} />
       </div>
     </React.Fragment>
   ))
