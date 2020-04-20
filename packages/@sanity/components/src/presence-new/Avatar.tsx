e7ede2ebee9b65db38549539693464c53d9ad2b8
vicbergquist <victoria.bergquist@gmail.com>
2020-04-20 20:30:41 +0200

    [components] add presence list to stack, update avatar tooltip

--- packages/@sanity/components/src/presence-new/Avatar.tsx
+++ packages/@sanity/components/src/presence-new/Avatar.tsx
@@ -28,68 +28,64 @@ export default function Avatar({
 }: Props) {
   const elementId = useId()
   return (
-    <div className={styles.root} data-dock={position} style={{color}} aria-label={label}>
-      <Tooltip
-        disabled={!label || size === 'medium'}
-        title={label}
-        position="bottom"
-        animation="scale"
-        arrow
-        theme="light"
-        duration={50}
-      >
-        <div className={`${styles.avatar}`} data-status={status} data-size={size}>
-          <div className={styles.avatarInner}>
-            <svg viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <ellipse
-                cx="16"
-                cy="16"
-                rx="15"
-                ry="15"
-                transform="rotate(90 16 16)"
-                stroke="currentColor"
-                className={`
+    <div
+      className={styles.root}
+      data-dock={position}
+      style={{color}}
+      aria-label={label}
+      title={label}
+    >
+      <div className={`${styles.avatar}`} data-status={status} data-size={size}>
+        <div className={styles.avatarInner}>
+          <svg viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <ellipse
+              cx="16"
+              cy="16"
+              rx="15"
+              ry="15"
+              transform="rotate(90 16 16)"
+              stroke="currentColor"
+              className={`
                 ${styles.border}
                 ${isAnimating && styles.isAnimating}
               `}
-              />
-              <ellipse cx="16" cy="16" rx="14" ry="14" transform="rotate(-90 16 16)" fill="white" />
-              <circle
-                cx="16"
-                cy="16"
-                r="13"
-                fill={imageUrl ? `url(#${elementId}-image-url)` : 'currentColor'}
-              />
-              <defs>
-                <pattern
-                  id={`${elementId}-image-url`}
-                  patternContentUnits="objectBoundingBox"
-                  width="1"
-                  height="1"
-                >
-                  {imageUrl && (
-                    <image
-                      href={imageUrl}
-                      width="1"
-                      height="1"
-                      onError={() => onImageLoadError(new Error('Image failed to load'))}
-                    />
-                  )}
-                </pattern>
-              </defs>
-            </svg>
-            {children && <div className={styles.avatarInitials}>{children}</div>}
-          </div>
-          <div className={styles.arrow} data-dock={position}>
-            <svg viewBox="0 0 10 7" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path
-                d="M0 6.4H9.6L5.44 0.853333C5.12 0.426666 4.48 0.426666 4.16 0.853333L0 6.4Z"
-                fill="currentColor"
-              />
-            </svg>
-          </div>
+            />
+            <ellipse cx="16" cy="16" rx="14" ry="14" transform="rotate(-90 16 16)" fill="white" />
+            <circle
+              cx="16"
+              cy="16"
+              r="13"
+              fill={imageUrl ? `url(#${elementId}-image-url)` : 'currentColor'}
+            />
+            <defs>
+              <pattern
+                id={`${elementId}-image-url`}
+                patternContentUnits="objectBoundingBox"
+                width="1"
+                height="1"
+              >
+                {imageUrl && (
+                  <image
+                    href={imageUrl}
+                    width="1"
+                    height="1"
+                    onError={() => onImageLoadError(new Error('Image failed to load'))}
+                  />
+                )}
+              </pattern>
+            </defs>
+          </svg>
+          {children && <div className={styles.avatarInitials}>{children}</div>}
         </div>
-      </Tooltip>
+        <div className={styles.arrow} data-dock={position}>
+          <svg viewBox="0 0 10 7" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path
+              d="M0 6.4H9.6L5.44 0.853333C5.12 0.426666 4.48 0.426666 4.16 0.853333L0 6.4Z"
+              fill="currentColor"
+            />
+          </svg>
+        </div>
+      </div>
     </div>
   )
 }
