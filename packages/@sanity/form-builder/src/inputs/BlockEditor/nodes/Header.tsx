7383c552bc668bdbdea296319e399ec13a386962
Per-Kristian Nordnes <per.kristian.nordnes@gmail.com>
2020-04-30 11:52:40 +0200

    [form-builder] Remove old BlockEditor input

--- packages/@sanity/form-builder/src/inputs/BlockEditor/nodes/Header.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import styles from './styles/Header.css'
-type Props = {
-  style: string
-  children: React.ReactNode
-}
-export default function Header(props: Props) {
-  return <div className={styles[props.style]}>{props.children}</div>
-}
--- packages/@sanity/form-builder/src/inputs/BlockEditor/utils/deserialize.ts
+++
@@ -1,7 +0,0 @@
-import {blocksToEditorValue} from '@sanity/block-tools'
-import {Value} from 'slate'
-import {SlateValue, Type} from '../typeDefs'
-
-export default function deserialize(value: SlateValue, type: Type) {
-  return Value.fromJSON(blocksToEditorValue(value, type))
-}
