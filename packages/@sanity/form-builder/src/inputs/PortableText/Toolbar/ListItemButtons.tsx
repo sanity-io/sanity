f0bba5551940188bc7fe66986dd99f6c1190491f
Per-Kristian Nordnes <per.kristian.nordnes@gmail.com>
2020-05-04 12:24:32 +0200

    [form-builder] Toolbar files

--- packages/@sanity/form-builder/src/inputs/PortableText/Toolbar/ListItemButtons.tsx
+++ packages/@sanity/form-builder/src/inputs/PortableText/Toolbar/ListItemButtons.tsx
@@ -3,22 +3,25 @@ import FormatListBulletedIcon from 'part:@sanity/base/format-list-bulleted-icon'
 import FormatListNumberedIcon from 'part:@sanity/base/format-list-numbered-icon'
 import SanityLogoIcon from 'part:@sanity/base/sanity-logo-icon'
 import ToggleButton from 'part:@sanity/components/toggles/button'
-import {BlockContentFeature, BlockContentFeatures, SlateEditor, SlateValue} from '../typeDefs'
 import CustomIcon from './CustomIcon'
-import ToolbarClickAction from './ToolbarClickAction'
 import CollapsibleButtonGroup from './CollapsibleButtonGroup'
-import styles from './styles/ListItemButtons.css'
-type ListItem = BlockContentFeature & {
+import styles from './ListItemButtons.css'
+import {
+  PortableTextFeature,
+  PortableTextEditor,
+  EditorSelection
+} from '@sanity/portable-text-editor'
+
+type ListItem = PortableTextFeature & {
   active: boolean
   disabled: boolean
 }
 type Props = {
-  blockContentFeatures: BlockContentFeatures
-  editor: SlateEditor
-  editorValue: SlateValue
+  editor: PortableTextEditor
+  selection: EditorSelection
   collapsed: boolean
 }
-function getIcon(type: string) {
+function getIcon(type: string): JSX.ElementClass {
   switch (type) {
     case 'number':
       return FormatListNumberedIcon
@@ -28,77 +31,56 @@ function getIcon(type: string) {
       return SanityLogoIcon
   }
 }
-const NOOP = () => {}
 export default class ListItemButtons extends React.Component<Props, {}> {
-  shouldComponentUpdate(nextProps: Props) {
-    const nextFocusBlock = nextProps.editorValue.focusBlock
-    const currentFocusBlock = this.props.editorValue.focusBlock
-    // Always update if we have selected more than one block
-    if (nextProps.editorValue.blocks.size > 1) {
+  shouldComponentUpdate(nextProps: Props): boolean {
+    if (nextProps.collapsed !== this.props.collapsed) {
       return true
     }
-    if (nextProps.collapsed != this.props.collapsed) {
-      return true
-    }
-    // Update if we have navigated to another block, or the block's data litItem prop is changed
-    if (
-      (nextFocusBlock && nextFocusBlock.key) !== (currentFocusBlock && currentFocusBlock.key) ||
-      (nextFocusBlock && nextFocusBlock.data.get('listItem')) !==
-        (currentFocusBlock && currentFocusBlock.data.get('listItem'))
-    ) {
+    if (nextProps.selection !== this.props.selection) {
       return true
     }
     return false
   }
-  hasListItem(listItemName: string) {
+  getItems(): ListItem[] {
     const {editor} = this.props
-    return editor.value.blocks.some(block => {
-      return block.data.get('listItem') === listItemName
-    })
-  }
-  getItems() {
-    const {editor, blockContentFeatures} = this.props
-    const {focusBlock} = editor.value
-    const disabled = focusBlock ? editor.query('isVoid', focusBlock) : false
-    return blockContentFeatures.lists.map((listItem: BlockContentFeature) => {
+    const ptFeatures = PortableTextEditor.getPortableTextFeatures(editor)
+    const focusBlock = PortableTextEditor.focusBlock(editor)
+    return ptFeatures.lists.map((listItem: PortableTextFeature) => {
       return {
         ...listItem,
-        active: editor.query('hasListItem', listItem.value),
-        disabled
+        active: focusBlock ? focusBlock.listItem === listItem.value : false,
+        disabled: focusBlock ? ptFeatures.types.block.name !== focusBlock._type : false
       }
     })
   }
-  handleClick = (item: ListItem) => {
+  handleClick = (item: ListItem): void => {
     const {editor} = this.props
-    editor.command('toggleListItem', item.value)
-    this.forceUpdate()
+    PortableTextEditor.toggleList(editor, item.value)
   }
-  renderListItemButton = (item: ListItem) => {
-    const {editor} = this.props
+  renderListItemButton = (item: ListItem): JSX.Element => {
     let Icon
     const icon = item.blockEditor ? item.blockEditor.icon : null
     if (icon) {
       if (typeof icon === 'string') {
-        Icon = () => <CustomIcon icon={icon} active={!!item.active} />
+        Icon = (): JSX.Element => <CustomIcon icon={icon} active={!!item.active} />
       } else if (typeof icon === 'function') {
         Icon = icon
       }
     }
     Icon = Icon || getIcon(item.value)
-    const onAction = () => this.handleClick(item)
+    const onAction = (): void => this.handleClick(item)
     return (
-      <ToolbarClickAction onAction={onAction} editor={editor} key={`listItemButton${item.value}`}>
-        <ToggleButton
-          selected={item.active}
-          disabled={item.disabled}
-          onClick={NOOP}
-          title={item.title}
-          icon={Icon}
-        />
-      </ToolbarClickAction>
+      <ToggleButton
+        selected={item.active}
+        disabled={item.disabled}
+        onClick={onAction}
+        aria-label={`Toggle '${item.title}' list`}
+        title={`Toggle '${item.title}' list`}
+        icon={Icon}
+      />
     )
   }
-  render() {
+  render(): JSX.Element {
     const {collapsed} = this.props
     const items = this.getItems()
     const icon = items[0].blockEditor ? items[0].blockEditor.icon : null
