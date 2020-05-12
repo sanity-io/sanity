48b07010ba833269cbfb5cb4ae8ae395a6c0b846
vicbergquist <victoria.bergquist@gmail.com>
2020-05-12 12:27:43 +0200

    [components] update checkboxes

--- packages/@sanity/form-builder/src/inputs/BooleanInput.tsx
+++ packages/@sanity/form-builder/src/inputs/BooleanInput.tsx
@@ -1,52 +1,52 @@
-import React from 'react'
+import React, {useRef} from 'react'
 import Switch from 'part:@sanity/components/toggles/switch'
 import Checkbox from 'part:@sanity/components/toggles/checkbox'
+import ValidationStatus from 'part:@sanity/components/validation/status'
 import PatchEvent, {set} from '../PatchEvent'
-import {Type} from '../typedefs'
+import {Type, Marker} from '../typedefs'
+import styles from './BooleanInput.css'
+
 type Props = {
   type: Type
   value: boolean | null
   readOnly: boolean | null
   onFocus: () => void
   onChange: (arg0: PatchEvent) => void
+  markers: Marker[]
 }
 // Todo: support indeterminate state, see https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/checkbox
-export default class BooleanInput extends React.Component<Props> {
-  _input: (Checkbox | Switch) | null
-  handleChange = (event: React.SyntheticEvent<HTMLInputElement>) => {
-    this.props.onChange(PatchEvent.from(set(event.currentTarget.checked)))
+
+export default function BooleanInput(props: Props) {
+  const handleChange = (event: React.SyntheticEvent<HTMLInputElement>) => {
+    props.onChange(PatchEvent.from(set(event.currentTarget.checked)))
   }
-  focus() {
-    if (this._input) {
-      this._input.focus()
-    }
-  }
-  setInput = (input: (Checkbox | Switch) | null) => {
-    this._input = input
-  }
-  render() {
-    const {value, type, readOnly, onFocus} = this.props
-    const isCheckbox = type.options && type.options.layout === 'checkbox'
-    return isCheckbox ? (
-      <Checkbox
-        readOnly={readOnly}
-        onChange={this.handleChange}
-        checked={value}
-        ref={this.setInput}
-        description={type.description}
-      >
-        {type.title}
-      </Checkbox>
-    ) : (
-      <Switch
-        readOnly={readOnly}
-        checked={value}
-        label={type.title}
-        description={type.description}
-        onChange={this.handleChange}
-        onFocus={onFocus}
-        ref={this.setInput}
-      />
-    )
-  }
+  const {value, type, readOnly, onFocus, markers} = props
+  const isCheckbox = type.options && type.options.layout === 'checkbox'
+  return (
+    <div className={styles.root}>
+      {isCheckbox ? (
+        <Checkbox
+          label={type.title}
+          readOnly={readOnly}
+          onChange={handleChange}
+          onFocus={onFocus}
+          checked={value}
+          description={type.description}
+        >
+          <ValidationStatus markers={markers} />
+        </Checkbox>
+      ) : (
+        <Switch
+          readOnly={readOnly}
+          checked={value}
+          label={type.title}
+          description={type.description}
+          onChange={handleChange}
+          onFocus={onFocus}
+        >
+          <ValidationStatus markers={markers} />
+        </Switch>
+      )}
+    </div>
+  )
 }
