7f8835368ab8a8e3fc47eb5337a87f0bc07683f7
vicbergquist <victoria.bergquist@gmail.com>
2020-05-15 15:58:45 +0200

    [components] Add presence components
    Co-authored-by: Bjørge Næss <bjoerge@gmail.com>
    Co-authored-by: Marius Lundgård <studio@mariuslundgard.com>

---
+++ packages/@sanity/components/src/presence/utils.ts
@@ -0,0 +1,15 @@
+export const splitRight = <T>(array: T[], max): [T[], T[]] => {
+  const indexFromMax = array.length > max ? max - 1 : max
+  const idx = Math.max(0, array.length - indexFromMax)
+  return [array.slice(0, idx), array.slice(idx)]
+}
+
+// export const splitRight = <T>(array: T[], index: number): [T[], T[]] => {
+//   const idx = Math.max(0, array.length - index)
+//   return [array.slice(0, idx), array.slice(idx)]
+// }
+//
+export const split = <T>(array: T[], index: number): [T[], T[]] => {
+  const idx = Math.max(0, index)
+  return [array.slice(0, idx), array.slice(idx)]
+}
