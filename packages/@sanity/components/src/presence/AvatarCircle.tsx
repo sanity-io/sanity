cc3f17224ee6e013fa3e4ffd53abb3e891254f25
Marius Lundgård <studio@mariuslundgard.com>
2020-05-15 15:32:51 +0200

    [components] Tweak avatar component (#1883)

--- packages/@sanity/components/src/presence/AvatarCircle.tsx
+++ packages/@sanity/components/src/presence/AvatarCircle.tsx
@@ -1,3 +1,4 @@
+import classNames from 'classnames'
 import React, {useState, useEffect} from 'react'
 import {useId} from '@reach/auto-id'
 import styles from './AvatarCircle.css'
@@ -5,8 +6,6 @@ import {Position, Status, Size} from './types'
 
 type Props = {
   borderColor: string
-  fillColor?: string
-  showFill?: boolean
   imageUrl?: string
   label: string
   isAnimating?: boolean
@@ -15,12 +14,14 @@ type Props = {
   position?: Position
   status?: Status
   size?: Size
+  tone?: 'navbar'
 }
+
+const W = 21
+const H = 21
 
 export default function AvatarCircle({
   borderColor = 'currentColor',
-  fillColor = 'white',
-  showFill = true,
   imageUrl,
   label,
   isAnimating = false,
@@ -28,7 +29,8 @@ export default function AvatarCircle({
   onImageLoadError,
   position = 'inside',
   status = 'online',
-  size = 'small'
+  size = 'small',
+  tone
 }: Props) {
   const elementId = useId()
   const [arrowPosition, setArrowPosition] = useState('inside')
@@ -43,37 +45,50 @@ export default function AvatarCircle({
   }, [])
 
   return (
-    <div className={styles.root} data-dock={arrowPosition} aria-label={label} title={label}>
-      <div className={`${styles.avatar}`} data-status={status} data-size={size}>
+    <div
+      className={styles.root}
+      data-dock={arrowPosition}
+      data-tone={tone}
+      aria-label={label}
+      title={label}
+    >
+      <div className={styles.avatar} data-status={status} data-size={size}>
+        <div className={styles.arrow} data-dock={arrowPosition}>
+          <svg viewBox="0 0 11 7" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path
+              d="M6.67948 1.50115L11 7L0 7L4.32052 1.50115C4.92109 0.736796 6.07891 0.736795 6.67948 1.50115Z"
+              fill="currentColor"
+            />
+          </svg>
+        </div>
         <div className={styles.avatarInner}>
-          <svg viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <svg viewBox={`0 0 ${W} ${H}`} fill="none" xmlns="http://www.w3.org/2000/svg">
             <ellipse
               data-avatar-border
-              cx="16"
-              cy="16"
-              rx="15"
-              ry="15"
-              transform="rotate(90 16 16)"
+              cx={W / 2}
+              cy={H / 2}
+              rx={W / 2 - 1}
+              ry={H / 2 - 1}
+              transform={`rotate(90 ${W / 2} ${H / 2})`}
               stroke={borderColor}
-              className={`
-                ${styles.border}
-                ${isAnimating && styles.isAnimating}
-              `}
+              className={classNames(styles.border, isAnimating && styles.isAnimating)}
             />
-            <ellipse
-              data-avatar-fill
-              cx="16"
-              cy="16"
-              rx="14"
-              ry="14"
-              transform="rotate(-90 16 16)"
-              fill={fillColor}
-            />
+            {imageUrl && (
+              <ellipse
+                className={styles.backgroundFill}
+                data-avatar-fill
+                cx={W / 2}
+                cy={H / 2}
+                rx={W / 2 - 2}
+                ry={H / 2 - 2}
+                transform={`rotate(90 ${W / 2} ${H / 2})`}
+              />
+            )}
             <circle
               data-avatar-image
-              cx="16"
-              cy="16"
-              r={showFill ? 13 : 14.2}
+              cx={W / 2}
+              cy={H / 2}
+              r={W / 2 - 2}
               fill={imageUrl ? `url(#${elementId}-image-url)` : 'currentColor'}
             />
             <defs>
@@ -95,14 +110,6 @@ export default function AvatarCircle({
             </defs>
           </svg>
           {children && <div className={styles.avatarInitials}>{children}</div>}
-        </div>
-        <div className={styles.arrow} data-dock={arrowPosition}>
-          <svg viewBox="0 0 10 7" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path
-              d="M0 6.4H9.6L5.44 0.853333C5.12 0.426666 4.48 0.426666 4.16 0.853333L0 6.4Z"
-              fill="currentColor"
-            />
-          </svg>
         </div>
       </div>
     </div>
