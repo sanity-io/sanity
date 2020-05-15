a5e0b14783219b7b96238b058716c1a539fb758d
Marius Lundgård <studio@mariuslundgard.com>
2020-05-15 15:32:51 +0200

    [components] Tweak avatar component (#1883)

--- packages/@sanity/components/src/presence/StackCounter.tsx
+++ packages/@sanity/components/src/presence/StackCounter.tsx
@@ -4,11 +4,12 @@ import styles from './StackCounter.css'
 type Props = {
   count: number
   isGlobal?: boolean
+  tone?: 'navbar'
 }
 
-export default function StackCounter({count, isGlobal = false}: Props) {
+export default function StackCounter({count, isGlobal = false, tone}: Props) {
   return (
-    <div className={styles.root} key="counter">
+    <div className={styles.root} data-tone={tone} key="counter">
       <div className={`${styles.counter} ${isGlobal ? styles.isGlobal : ''}`}>{count}</div>
     </div>
   )
