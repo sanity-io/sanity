c40deaa450d403b24dfa6b0e742ec9310c43b179
vicbergquist <victoria.bergquist@gmail.com>
2020-05-15 15:58:45 +0200

    [components] Add presence components
    Co-authored-by: Bjørge Næss <bjoerge@gmail.com>
    Co-authored-by: Marius Lundgård <studio@mariuslundgard.com>

---
+++ packages/@sanity/components/src/presence/overlay/Overlay.tsx
@@ -0,0 +1,23 @@
+import {StickyOverlayRenderer} from './StickyOverlayRenderer'
+import React from 'react'
+import {DISABLE_OVERLAY} from '../constants'
+import {Tracker} from '../overlay-reporter'
+
+interface Props {
+  children: React.ReactNode
+  margins: [number, number, number, number]
+}
+
+function OverlayEnabled(props: Props) {
+  return (
+    <Tracker component={StickyOverlayRenderer} componentProps={{margins: props.margins}}>
+      {props.children}
+    </Tracker>
+  )
+}
+
+function OverlayDisabled(props: Props) {
+  return props.children
+}
+
+export const Overlay = DISABLE_OVERLAY ? OverlayDisabled : OverlayEnabled
