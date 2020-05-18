e6fd7f89051a34df597f5e0efe94f96b44953298
vicbergquist <victoria.bergquist@gmail.com>
2020-05-18 16:16:02 +0200

    [components] update radio ui

---
+++ packages/@sanity/components/src/selects/RadioSelect.tsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import styles from './styles/RadioSelect.css'
+import RadioButton from '../radiobutton/RadioButtonDefault'
+
+type Props = {
+  name: string
+  direction: 'horizontal' | 'vertical'
+  onFocus?: () => void
+  onChange?: (any) => void
+  value: any
+  readOnly: boolean
+  items: {title: string}[]
+}
+
+export default function RadioSelect({
+  name,
+  direction,
+  onChange = () => {},
+  onFocus,
+  value,
+  readOnly,
+  items = []
+}: Props) {
+  const handleRadioChange = item => {
+    onChange(item)
+  }
+
+  return (
+    <div
+      className={`
+        ${direction == 'vertical' ? styles.vertical : styles.horizontal}
+       ${styles.root}`}
+    >
+      <div className={styles.radioContainer} role="group">
+        {items.map((item, i) => {
+          return (
+            <div className={styles.item} key={i}>
+              <RadioButton
+                disabled={readOnly}
+                name={name}
+                label={item.title}
+                item={item}
+                onChange={handleRadioChange}
+                checked={value === item}
+                onFocus={onFocus}
+              />
+            </div>
+          )
+        })}
+      </div>
+    </div>
+  )
+}
