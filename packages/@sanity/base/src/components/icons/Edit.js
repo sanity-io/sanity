04baf41f7187cc1e4f7cfc88a89627fb822e406d
Marius Lundgård <studio@mariuslundgard.com>
2020-05-20 16:09:18 +0200

    [base] Update icons

--- packages/@sanity/base/src/components/icons/Edit.js
+++ packages/@sanity/base/src/components/icons/Edit.js
@@ -1,3 +1,23 @@
-import MdEdit from 'react-icons/lib/md/edit'
+import React from 'react'
 
-export default MdEdit
+const strokeStyle = {
+  stroke: 'currentColor',
+  strokeWidth: 1.2,
+  vectorEffect: 'non-scaling-stroke'
+}
+
+export default function Edit() {
+  return (
+    <svg
+      viewBox="0 0 25 25"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      preserveAspectRatio="xMidYMid"
+      width="1em"
+      height="1em"
+    >
+      <path d="M7 15L6 19L10 18L20 8L17 5L7 15Z" style={strokeStyle} />
+      <path d="M15 7L18 10" style={strokeStyle} />
+    </svg>
+  )
+}
--- packages/@sanity/base/src/components/icons/Eye.js
+++ packages/@sanity/base/src/components/icons/Eye.js
@@ -1,3 +1,34 @@
-import FaEye from 'react-icons/lib/fa/eye'
+import React from 'react'
 
-export default FaEye
+const strokeStyle = {
+  vectorEffect: 'non-scaling-stroke'
+}
+
+export default function Eye() {
+  return (
+    <svg
+      viewBox="0 0 25 25"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      preserveAspectRatio="xMidYMid"
+      width="1em"
+      height="1em"
+    >
+      <path
+        d="M12.5 7.5C8.5 7.5 6 10 4.5 12.5C6 15 8.5 17.5 12.5 17.5C16.5 17.5 19 15 20.5 12.5C19 10 16.5 7.5 12.5 7.5Z"
+        stroke="currentColor"
+        stroke-width="1.2"
+        style={strokeStyle}
+      />
+      <circle
+        cx="12.5"
+        cy="12.5"
+        r="2.5"
+        fill="currentColor"
+        stroke="currentColor"
+        stroke-width="1.2"
+        style={strokeStyle}
+      />
+    </svg>
+  )
+}
