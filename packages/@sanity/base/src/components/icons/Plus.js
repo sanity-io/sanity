0986a2d93fbeb63f45ec30f168fe2d00b62cd5e5
Marius Lundgård <studio@mariuslundgard.com>
2020-05-20 16:09:18 +0200

    [base] Update icons

--- packages/@sanity/base/src/components/icons/Plus.js
+++ packages/@sanity/base/src/components/icons/Plus.js
@@ -1,18 +1,22 @@
 import React from 'react'
 
-const strokeStyle = {vectorEffect: 'non-scaling-stroke'}
+const strokeStyle = {
+  stroke: 'currentColor',
+  strokeWidth: 1.2,
+  vectorEffect: 'non-scaling-stroke'
+}
 
-const PlusIcon = () => (
-  <svg
-    viewBox="0 0 25 25"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-    preserveAspectRatio="xMidYMid"
-    width="1em"
-    height="1em"
-  >
-    <path d="M12.5 4V21M4 12.5H21" stroke="currentColor" style={strokeStyle} />
-  </svg>
-)
-
-export default PlusIcon
+export default function Plus() {
+  return (
+    <svg
+      viewBox="0 0 25 25"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      preserveAspectRatio="xMidYMid"
+      width="1em"
+      height="1em"
+    >
+      <path d="M12.5 6V19M6 12.5H19" style={strokeStyle} />
+    </svg>
+  )
+}
