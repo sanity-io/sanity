cbc55dec5730e1084fb018b70f2c18318391f250
Marius Lundgård <studio@mariuslundgard.com>
2020-05-20 14:04:12 +0200

    [default-layout] Update NotFound component

--- packages/@sanity/default-layout/src/components/NotFound.js
+++ packages/@sanity/default-layout/src/components/NotFound.js
@@ -2,14 +2,23 @@ import React from 'react'
 import {withRouterHOC, StateLink} from 'part:@sanity/base/router'
 import {HAS_SPACES} from '../util/spaces'
 
+import styles from './NotFound.css'
+
 export default withRouterHOC(function NotFound(props) {
   const router = props.router
   const rootState = HAS_SPACES && router.state.space ? {space: router.state.space} : {}
+
   return (
-    <div>
-      <h2>Page not found</h2>
-      {props.children}
-      <StateLink state={rootState}>Go to index</StateLink>
+    <div className={styles.root}>
+      <header className={styles.header}>
+        <h2>Page not found</h2>
+      </header>
+
+      <div className={styles.content}>{props.children}</div>
+
+      <div className={styles.footer}>
+        <StateLink state={rootState}>Go to index</StateLink>
+      </div>
     </div>
   )
 })
