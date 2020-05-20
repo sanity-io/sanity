b9f10c6722c51cb7c8b767d379d4053a0f0ff34c
Marius Lundgård <studio@mariuslundgard.com>
2020-05-20 16:11:53 +0200

    [studio-hints] Update CSS and icons

---
+++ packages/@sanity/studio-hints/src/components/icons/HelpCircle.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const strokeStyle = {
+  stroke: 'currentColor',
+  strokeWidth: 1.2,
+  vectorEffect: 'non-scaling-stroke'
+}
+
+export default function HelpCircle() {
+  return (
+    <svg
+      viewBox="0 0 25 25"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      preserveAspectRatio="xMidYMid"
+      width="1em"
+      height="1em"
+    >
+      <circle cx="12.5" cy="12.5" r="8" style={strokeStyle} />
+      <path d="M12.5 16V14.5" style={strokeStyle} />
+      <path
+        d="M12.5 13C12.5 11 14 11.5 14 10C14 9.34375 13.5 8.5 12.5 8.5C11.5 8.5 11 9 10.5 9.5"
+        style={strokeStyle}
+      />
+    </svg>
+  )
+}
