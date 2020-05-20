606b24acae2eec455a5901865eec10c87a7c40bc
Marius Lundgård <studio@mariuslundgard.com>
2020-05-20 16:09:18 +0200

    [base] Update icons

--- packages/@sanity/base/src/components/icons/Plug.js
+++ packages/@sanity/base/src/components/icons/Plug.js
@@ -1,25 +1,28 @@
 import React from 'react'
 
-const strokeStyle = {vectorEffect: 'non-scaling-stroke'}
+const strokeStyle = {
+  stroke: 'currentColor',
+  strokeWidth: 1.2,
+  vectorEffect: 'non-scaling-stroke'
+}
 
-const PlugIcon = () => (
-  <svg
-    viewBox="0 0 25 25"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-    preserveAspectRatio="xMidYMid"
-    width="1em"
-    height="1em"
-  >
-    <path
-      d="M8 13L6 15L9 18L11 16C13.5 17.5 15 18 17 16L18.5 14.5L9.5 5.5L8 7C6 9 6.5 10.5 8 13Z"
-      stroke="currentColor"
-      style={strokeStyle}
-    />
-    <path d="M12 8L17 3" stroke="currentColor" style={strokeStyle} />
-    <path d="M16 12L21 7" stroke="currentColor" style={strokeStyle} />
-    <path d="M7.5 16.5L2.5 21.5" stroke="currentColor" style={strokeStyle} />
-  </svg>
-)
-
-export default PlugIcon
+export default function Plug() {
+  return (
+    <svg
+      viewBox="0 0 25 25"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      preserveAspectRatio="xMidYMid"
+      width="1em"
+      height="1em"
+    >
+      <path
+        d="M9 12L7 14L10 17L12 15C14 16.5 15 16 16 15L17.5 13.5L10.5 6.5L9 8C8 9 7.5 10 9 12Z"
+        style={strokeStyle}
+      />
+      <path d="M12.5 8.5L16 5" style={strokeStyle} />
+      <path d="M15.5 11.5L19 8" style={strokeStyle} />
+      <path d="M8.5 15.5L4.5 19.5" style={strokeStyle} />
+    </svg>
+  )
+}
