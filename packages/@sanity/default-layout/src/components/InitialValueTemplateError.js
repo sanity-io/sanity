7391ce55a536b39f8540778d8f6803f2bfbf5907
Marius Lundgård <studio@mariuslundgard.com>
2020-05-20 14:03:11 +0200

    [default-layout] Use new FullscreenMessageDialog in InitialValueTemplateError

--- packages/@sanity/default-layout/src/components/InitialValueTemplateError.js
+++ packages/@sanity/default-layout/src/components/InitialValueTemplateError.js
@@ -1,22 +1,17 @@
 import PropTypes from 'prop-types'
 import React from 'react'
-import FullscreenDialog from 'part:@sanity/components/dialogs/fullscreen'
-import DialogContent from 'part:@sanity/components/dialogs/content'
+import FullscreenMessageDialog from 'part:@sanity/components/dialogs/fullscreen-message'
 
 export default function InitialValueTemplateError({errors}) {
   return (
-    <FullscreenDialog color="danger" title="Initial value template error" isOpen centered>
-      {
-        <DialogContent size="medium" padding="none">
-          <p>Failed to load initial value templates:</p>
-          {errors.map((error, i) => (
-            <p key={error.message}>
-              <code>{error.message}</code>
-            </p>
-          ))}
-        </DialogContent>
-      }
-    </FullscreenDialog>
+    <FullscreenMessageDialog color="danger" title="Initial value template error">
+      <p>Failed to load initial value templates:</p>
+      {errors.map((error, i) => (
+        <p key={error.message}>
+          <code>{error.message}</code>
+        </p>
+      ))}
+    </FullscreenMessageDialog>
   )
 }
 
