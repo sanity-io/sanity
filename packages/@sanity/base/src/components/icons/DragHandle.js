10e700e9f46fe41f59529e11c6684c4824e64aba
Marius Lundgård <studio@mariuslundgard.com>
2020-05-26 13:04:03 +0200

    [base] Add DragHandle icon

---
+++ packages/@sanity/base/src/components/icons/DragHandle.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default function DragHandle() {
+  return (
+    <svg
+      viewBox="0 0 25 25"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      preserveAspectRatio="xMidYMid"
+      width="1em"
+      height="1em"
+    >
+      <circle cx="9.5" cy="6.5" r="1.5" fill="currentColor" />
+      <circle cx="9.5" cy="12.5" r="1.5" fill="currentColor" />
+      <circle cx="9.5" cy="18.5" r="1.5" fill="currentColor" />
+      <circle cx="15.5" cy="6.5" r="1.5" fill="currentColor" />
+      <circle cx="15.5" cy="12.5" r="1.5" fill="currentColor" />
+      <circle cx="15.5" cy="18.5" r="1.5" fill="currentColor" />
+    </svg>
+  )
+}
