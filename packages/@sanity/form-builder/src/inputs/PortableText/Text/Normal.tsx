b1901ccdf4adf95f6d1481fa516b60d8195a7b45
Per-Kristian Nordnes <per.kristian.nordnes@gmail.com>
2020-05-26 13:54:55 +0200

    [form-builder] PTE: fix design issues (padding/margins)

--- packages/@sanity/form-builder/src/inputs/PortableText/Text/Normal.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import styles from './Normal.css'
-type Props = {
-  children: React.ReactNode
-}
-export default function Normal(props: Props) {
-  return <div className={styles.root}>{props.children}</div>
-}
