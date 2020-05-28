ff4cf6237697fa68bbe205535efd132eb4fc6a92
Marius Lundgård <studio@mariuslundgard.com>
2020-05-28 23:16:14 +0200

    [components] Update button component

--- packages/@sanity/components/src/buttons/stories/default.js
+++ packages/@sanity/components/src/buttons/stories/default.js
@@ -1,7 +1,7 @@
 import Button from 'part:@sanity/components/buttons/default'
 import {action} from 'part:@sanity/storybook/addons/actions'
 import {text, select, boolean, button} from 'part:@sanity/storybook/addons/knobs'
-import SanityLogoIcon from 'part:@sanity/base/sanity-logo-icon'
+import TrashIcon from 'part:@sanity/base/trash-icon'
 import Sanity from 'part:@sanity/storybook/addons/sanity'
 import {CenteredContainer} from 'part:@sanity/storybook/components'
 import React from 'react'
@@ -30,21 +30,22 @@ export function DefaultStory() {
       <Sanity part="part:@sanity/components/buttons/default" propTables={[Button]}>
         <Button
           {...actionsProps}
-          kind={select('kind', ['default', 'simple', 'secondary'], 'default', 'props')}
-          disabled={boolean('disabled', false, 'props')}
-          inverted={boolean('inverted', false, 'props')}
+          kind={select('Kind', ['default', 'simple', 'secondary'], 'default', 'props')}
+          disabled={boolean('Disabled', false, 'props')}
+          inverted={boolean('Inverted', false, 'props')}
           color={select(
-            'color',
+            'Color',
             [undefined, 'primary', 'success', 'warning', 'danger', 'white'],
             undefined,
             'props'
           )}
-          loading={boolean('loading', false, 'props')}
-          icon={boolean('icon', false, 'props') ? SanityLogoIcon : false}
+          loading={boolean('Loading', false, 'props')}
+          icon={boolean('Icon', false, 'props') ? TrashIcon : false}
+          padding={select('Padding', ['large', 'default', 'small', 'none'], 'default', 'props')}
           ref={setConfirmButton}
-          bleed={boolean('bleed', false, 'props')}
+          bleed={boolean('Bleed', false, 'props')}
         >
-          {text('children', 'Touch Me!', 'props')}
+          {text('Text', 'Touch Me!', 'props')}
         </Button>
       </Sanity>
     </CenteredContainer>
