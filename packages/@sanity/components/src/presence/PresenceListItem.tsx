8da4863addd7b7cfa48b206e38a2e0c41438b5a7
vicbergquist <victoria.bergquist@gmail.com>
2020-06-02 18:28:13 +0200

    [components] update presence intent links in list

--- packages/@sanity/components/src/presence/PresenceListItem.tsx
+++ packages/@sanity/components/src/presence/PresenceListItem.tsx
@@ -1,26 +1,38 @@
 import styles from './PresenceListItem.css'
 import UserAvatar from './UserAvatar'
 import React from 'react'
-import {Size, Status, User} from './types'
+import {Size, Status, User, Location} from './types'
+import LinkIcon from 'part:@sanity/base/link-icon'
+import {IntentLink} from 'part:@sanity/base/router'
 
 type Props = {
   user: User
   status: Status
   size?: Size
   onClick?: () => void
+  locations?: Location[]
 }
 
-export function PresenceListItem({user, status, size, onClick}: Props) {
+export function PresenceListItem({user, status, size, onClick, locations = []}: Props) {
+  const locationWithDocumentId = locations.find(location => location.documentId)
   return (
-    <div className={styles.inner} data-size={size} onClick={onClick}>
+    <div className={styles.root} data-size={size} onClick={onClick}>
       <div className={styles.avatar}>
         <UserAvatar user={user} size={size} status={status} />
       </div>
-      <div className={styles.userInfo}>
-        <span className={styles.name} title={user.displayName}>
-          {user.displayName}
-        </span>
+      <div className={styles.inner}>
+        <div className={styles.userName}>{user.displayName}</div>
       </div>
+      {locationWithDocumentId && (
+        <IntentLink
+          className={styles.intentLink}
+          title={`Go to ${user.displayName}`}
+          intent="edit"
+          params={{id: locationWithDocumentId.documentId}}
+        >
+          <LinkIcon />
+        </IntentLink>
+      )}
     </div>
   )
 }
