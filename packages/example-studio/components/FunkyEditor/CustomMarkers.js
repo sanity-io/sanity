7ccaf387805f70405249dc5fb3f6cd093d23a57d
Marius Lundgård <studio@mariuslundgard.com>
2020-06-03 13:54:58 +0200

    [example-studio] Update custom inputs

--- packages/example-studio/components/FunkyEditor/CustomMarkers.js
+++ packages/example-studio/components/FunkyEditor/CustomMarkers.js
@@ -1,8 +1,10 @@
 import React from 'react'
 
+import styles from './CustomMarkers.css'
+
 export default function renderCustomMarkers(markers) {
   return (
-    <div>
+    <div className={styles.root}>
       {markers.map((marker, index) => {
         if (marker.type === 'customMarkerTest') {
           return <div key={`marker${index}`}>Marker!</div>
