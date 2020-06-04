410a96d35f1e6b4dc43fa5a92eabe406d5cd2847
Marius Lundgård <studio@mariuslundgard.com>
2020-06-04 13:57:13 +0200

    [form-builder] Refactor PTEToolbar to a functional component

--- packages/@sanity/form-builder/src/inputs/PortableText/Toolbar/Toolbar.tsx
+++ packages/@sanity/form-builder/src/inputs/PortableText/Toolbar/Toolbar.tsx
@@ -1,5 +1,3 @@
-/* eslint-disable complexity */
-
 import classNames from 'classnames'
 import React from 'react'
 import {uniq} from 'lodash'
@@ -19,7 +17,7 @@ const denyFocusChange = (event: React.SyntheticEvent<HTMLDivElement>): void => {
   event.preventDefault()
 }
 
-type Props = {
+interface PTEToolbarProps {
   editor: PortableTextEditor
   hotkeys: HotkeyOptions
   isFullscreen: boolean
@@ -29,59 +27,42 @@ type Props = {
   selection: EditorSelection
 }
 
-type ToolbarState = {
-  isMobile: boolean
-}
+function PTEToolbar(props: PTEToolbarProps): JSX.Element {
+  const {editor, hotkeys, isFullscreen, isReadOnly, onFocus, renderBlock, selection} = props
+  const [isMobile] = React.useState(
+    typeof window !== 'undefined' ? window.innerWidth < BREAKPOINT_SCREEN_MEDIUM : true
+  )
 
-export default class Toolbar extends React.Component<Props, ToolbarState> {
-  state = {
-    isMobile: false
-  }
+  // NOTE: do not use any hooks afer this point
+  if (!editor) return null
 
-  constructor(props: Props) {
-    super(props)
+  const features = PortableTextEditor.getPortableTextFeatures(editor)
+  const insertItems = uniq(features.types.inlineObjects.concat(features.types.blockObjects))
+  const rootClassNames = classNames(styles.root, isFullscreen && styles.fullscreen)
 
-    this.state = {
-      isMobile: typeof window !== 'undefined' ? window.innerWidth < BREAKPOINT_SCREEN_MEDIUM : true
-    }
-  }
-
-  shouldComponentUpdate(nextProps: Props): boolean {
-    return this.props.selection !== nextProps.selection || this.props.editor !== nextProps.editor
-  }
-
-  render(): JSX.Element {
-    const {editor, hotkeys, isFullscreen, isReadOnly, onFocus, renderBlock, selection} = this.props
-    if (!editor) {
-      return null
-    }
-    const {isMobile} = this.state
-    const ptFeatures = PortableTextEditor.getPortableTextFeatures(editor)
-    const insertItems = uniq(ptFeatures.types.inlineObjects.concat(ptFeatures.types.blockObjects))
-    const rootClassNames = classNames(styles.root, isFullscreen && styles.fullscreen)
-
-    return (
-      <div
-        className={rootClassNames}
-        // Ensure the editor doesn't lose focus when interacting
-        // with the toolbar (prevent focus click events)
-        onMouseDown={denyFocusChange}
-        onKeyPress={denyFocusChange}
-      >
-        <div className={styles.inner}>
-          <PrimaryGroup
-            editor={editor}
-            hotkeys={hotkeys}
-            insertItems={insertItems}
-            isFullscreen={isFullscreen}
-            isMobile={isMobile}
-            isReadOnly={isReadOnly}
-            onFocus={onFocus}
-            renderBlock={renderBlock}
-            selection={selection}
-          />
-        </div>
+  return (
+    <div
+      className={rootClassNames}
+      // Ensure the editor doesn't lose focus when interacting
+      // with the toolbar (prevent focus click events)
+      onMouseDown={denyFocusChange}
+      onKeyPress={denyFocusChange}
+    >
+      <div className={styles.inner}>
+        <PrimaryGroup
+          editor={editor}
+          hotkeys={hotkeys}
+          insertItems={insertItems}
+          isFullscreen={isFullscreen}
+          isMobile={isMobile}
+          isReadOnly={isReadOnly}
+          onFocus={onFocus}
+          renderBlock={renderBlock}
+          selection={selection}
+        />
       </div>
-    )
-  }
+    </div>
+  )
 }
+
+export default PTEToolbar
