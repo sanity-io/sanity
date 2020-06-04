98d6f89ac743cb56db95e5dfef4bc78608015a8e
Marius Lundgård <studio@mariuslundgard.com>
2020-06-04 11:37:59 +0200

    [desk-tool] Disable react-tippy because of a bug

--- packages/@sanity/desk-tool/src/components/DraftStatus.js
+++ packages/@sanity/desk-tool/src/components/DraftStatus.js
@@ -1,27 +1,29 @@
 import EditIcon from 'part:@sanity/base/edit-icon'
 import React from 'react'
-import {Tooltip} from 'react-tippy'
+// import {Tooltip} from 'react-tippy'
 
 import styles from './ItemStatus.css'
 
 const DraftStatus = () => (
-  <Tooltip
-    tabIndex={-1}
-    className={styles.itemStatus}
-    html={
-      <div className={styles.tooltipWrapper}>
-        <span>Unpublished changes</span>
-      </div>
-    }
-    arrow
-    theme="light"
-    sticky
-    size="small"
-  >
-    <div className={styles.draftBadge} role="image" aria-label="There are unpublished edits">
-      <EditIcon />
-    </div>
-  </Tooltip>
+  // NOTE: We're experiencing a bug with `react-tippy` here
+  // @todo: Replace react-tippy with `react-popper` or something
+  // <Tooltip
+  //   tabIndex={-1}
+  //   className={styles.itemStatus}
+  //   html={
+  //     <div className={styles.tooltipWrapper}>
+  //       <span>Unpublished changes</span>
+  //     </div>
+  //   }
+  //   arrow
+  //   theme="light"
+  //   sticky
+  //   size="small"
+  // >
+  <div className={styles.draftBadge} role="image" aria-label="There are unpublished edits">
+    <EditIcon />
+  </div>
+  // </Tooltip>
 )
 
 export default DraftStatus
