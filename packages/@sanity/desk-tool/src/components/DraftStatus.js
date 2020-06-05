d4413054a75a35c76ec0f829a2a3879a80e5634d
Marius Lundgård <studio@mariuslundgard.com>
2020-06-05 21:09:41 +0200

    [desk-tool] Add title attribute to DraftStatus

--- packages/@sanity/desk-tool/src/components/DraftStatus.js
+++ packages/@sanity/desk-tool/src/components/DraftStatus.js
@@ -4,25 +4,14 @@ import React from 'react'
 import styles from './ItemStatus.css'
 
 const DraftStatus = () => (
-  // NOTE: We're experiencing a bug with `react-tippy` here
-  // @todo: Replace react-tippy with `react-popper` or something
-  // <Tooltip
-  //   tabIndex={-1}
-  //   className={styles.itemStatus}
-  //   html={
-  //     <div className={styles.tooltipWrapper}>
-  //       <span>Unpublished changes</span>
-  //     </div>
-  //   }
-  //   arrow
-  //   theme="light"
-  //   sticky
-  //   size="small"
-  // >
-  <div className={styles.draftBadge} role="image" aria-label="There are unpublished edits">
+  <div
+    className={styles.draftBadge}
+    role="image"
+    aria-label="There are unpublished edits"
+    title="There are unpublished edits"
+  >
     <EditIcon />
   </div>
-  // </Tooltip>
 )
 
 export default DraftStatus
