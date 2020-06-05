4fbd16fe04998f02f1e63bced844ff662cd623f2
Per-Kristian Nordnes <per.kristian.nordnes@gmail.com>
2020-06-05 20:38:40 +0200

    [default-layout] Fix router state bug (NotFound component) (#1786)
    * [default-layout] Fix null bug in NotFound component
    
    * [default-layout] Give padding to NotFound component

--- packages/@sanity/default-layout/src/components/NotFound.js
+++ packages/@sanity/default-layout/src/components/NotFound.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import {withRouterHOC, StateLink} from 'part:@sanity/base/router'
 import {HAS_SPACES} from '../util/spaces'
+import styles from './styles/NotFound.css'
 
 export default withRouterHOC(function NotFound(props) {
   const router = props.router
-  const rootState = HAS_SPACES && router.state.space ? {space: router.state.space} : {}
+  const rootState =
+    HAS_SPACES && router.state && router.state.space ? {space: router.state.space} : {}
   return (
-    <div>
+    <div className={styles.root}>
       <h2>Page not found</h2>
       {props.children}
       <StateLink state={rootState}>Go to index</StateLink>
