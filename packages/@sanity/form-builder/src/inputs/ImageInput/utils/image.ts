f696469c311a845f78e59b5797b3a7f5f9014359
Rune Botten <rbotten@gmail.com>
2020-06-08 17:10:22 +0200

    Handle application/octet-stream when constructing filename for image uploads. Also avoid unneccesary stringification when we are passed a filename.

--- packages/@sanity/form-builder/src/inputs/ImageInput/utils/image.ts
+++ packages/@sanity/form-builder/src/inputs/ImageInput/utils/image.ts
@@ -1,17 +1,40 @@
 import UUID from '@sanity/uuid'
 
+function extFromData(string: string): string | undefined {
+  let ext: string
+
+  if (string.match(/^data:application\/octet-stream/)) {
+    ext = 'bin'
+  }
+
+  if (string.match(/^data:image/)) {
+    ext = string.substring('data:image/'.length, string.indexOf(';base64'))
+  }
+
+  return ext
+}
+
 export function urlToFile(url: string, filename?: string): Promise<File> {
   return new Promise((resolve, reject) => {
     const xhr = new XMLHttpRequest()
     xhr.onload = () => {
       const reader = new FileReader()
       reader.onloadend = () => {
+        // Return early if we have a filename for this this file data
+        if (filename) {
+          return resolve(dataURLtoFile(reader.result, filename))
+        }
+
+        // Otherwise we need to dig into the string representation of the data to
+        // determine what extension to use when constructing a filename
         const string = reader.result.toString()
-        const ext = string.substring('data:image/'.length, string.indexOf(';base64'))
-        if (!ext && !filename) {
+        const ext = extFromData(string)
+
+        if (!ext) {
           return reject(new Error('Could not find mime type for image'))
         }
-        resolve(dataURLtoFile(reader.result, filename || `${UUID()}.${ext}`))
+
+        resolve(dataURLtoFile(reader.result, `${UUID()}.${ext}`))
       }
       reader.readAsDataURL(xhr.response)
     }
@@ -26,15 +49,21 @@ export function urlToFile(url: string, filename?: string): Promise<File> {
 
 export function base64ToFile(base64Data: string | ArrayBuffer, filename?: string): Promise<File> {
   return new Promise((resolve, reject) => {
+    // Return early if we have a filename for this this file data
+    if (filename) {
+      return resolve(dataURLtoFile(base64Data, filename))
+    }
     const string = base64Data.toString()
-    const ext = string.substring('data:image/'.length, string.indexOf(';base64'))
-    if (!ext && !filename) {
+    const ext = extFromData(string)
+    if (!ext) {
       return reject(new Error('Could not find mime type for image'))
     }
-    resolve(dataURLtoFile(base64Data, filename || `${UUID()}.${ext}`))
+    resolve(dataURLtoFile(base64Data, `${UUID()}.${ext}`))
   })
 }
 
+// FIXME: What happens if ArrayBuffer  is passed into here as dataurl?
+// (base64Data in function base64ToFile is passed into dataurl param)
 function dataURLtoFile(dataurl, filename) {
   const arr = dataurl.split(',')
   const mime = arr[0].match(/:(.*?);/)[1]
