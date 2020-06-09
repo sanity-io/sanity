2d38375747d026ac8121d22767e2f85ca4cb81ce
Marius Lundgård <studio@mariuslundgard.com>
2020-06-09 09:09:41 +0200

    [default-layout] Update default layout

--- packages/@sanity/default-layout/src/navbar/actionButton/ActionModal.tsx
+++ packages/@sanity/default-layout/src/navbar/actionButton/ActionModal.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Dialog from 'part:@sanity/components/dialogs/fullscreen'
+import FullscreenDialog from 'part:@sanity/components/dialogs/fullscreen'
 import CreateDocumentList from 'part:@sanity/components/lists/create-document'
 import FileIcon from 'part:@sanity/base/file-icon'
 import styles from './ActionModal.css'
@@ -14,21 +14,19 @@ function ActionModal(props: Props) {
   const {title, actions, onClose} = props
 
   return (
-    <Dialog className={styles.modal} onClose={onClose} title={title} isOpen>
-      <div className={styles.listContainer}>
-        {actions.length > 0 ? (
-          <CreateDocumentList
-            items={actions.map(action => ({
-              ...action,
-              icon: action.icon || FileIcon,
-              onClick: onClose
-            }))}
-          />
-        ) : (
-          <h3>No initial value templates are configured.</h3>
-        )}
-      </div>
-    </Dialog>
+    <FullscreenDialog cardClassName={styles.card} onClose={onClose} title={title} isOpen>
+      {actions.length > 0 ? (
+        <CreateDocumentList
+          items={actions.map(action => ({
+            ...action,
+            icon: action.icon || FileIcon,
+            onClick: onClose
+          }))}
+        />
+      ) : (
+        <h3>No initial value templates are configured.</h3>
+      )}
+    </FullscreenDialog>
   )
 }
 
