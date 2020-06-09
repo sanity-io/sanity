b2d68d2d9b6af3ba447f05b380c7d568036c9897
Marius Lundgård <studio@mariuslundgard.com>
2020-06-09 09:07:12 +0200

    [components] Update fullscreen dialogs

--- packages/@sanity/components/src/dialogs/FullscreenDialog.js
+++ packages/@sanity/components/src/dialogs/FullscreenDialog.js
@@ -1,11 +1,11 @@
-import PropTypes from 'prop-types'
-import React from 'react'
-
+import classNames from 'classnames'
+import {partition} from 'lodash'
 import styles from 'part:@sanity/components/dialogs/fullscreen-style'
 import CloseIcon from 'part:@sanity/base/close-icon'
 import Button from 'part:@sanity/components/buttons/default'
 import ButtonGrid from 'part:@sanity/components/buttons/button-grid'
-import {partition} from 'lodash'
+import PropTypes from 'prop-types'
+import React from 'react'
 import {Portal} from '../utilities/Portal'
 import StackedEscapable from '../utilities/StackedEscapable'
 
@@ -13,32 +13,41 @@ const noop = () => {}
 
 export default class FullScreenDialog extends React.PureComponent {
   static propTypes = {
-    color: PropTypes.oneOf(['default', 'warning', 'info', 'success', 'danger']),
+    cardClassName: PropTypes.string,
     className: PropTypes.string,
     title: PropTypes.node,
     children: PropTypes.node,
     onClose: PropTypes.func,
     onEscape: PropTypes.func,
-    centered: PropTypes.bool,
     isOpen: PropTypes.bool,
     onAction: PropTypes.func,
-    padding: PropTypes.oneOf(['none', 'small', 'medium', 'large']),
     actions: PropTypes.arrayOf(
       PropTypes.shape({
         title: PropTypes.string.isRequired,
         tooltip: PropTypes.string,
         kind: PropTypes.string,
-        autoFocus: PropTypes.bool
+        autoFocus: PropTypes.bool,
+        disabled: PropTypes.bool,
+        inverted: PropTypes.bool
       })
-    )
+    ),
+
+    // deprecated
+    /* eslint-disable react/no-unused-prop-types */
+    color: PropTypes.oneOf(['default', 'warning', 'info', 'success', 'danger']),
+    padding: PropTypes.oneOf(['none', 'small', 'medium', 'large'])
+    /* eslint-enable react/no-unused-prop-types */
   }
 
   static defaultProps = {
-    color: 'default',
+    cardClassName: undefined,
     isOpen: true,
-    padding: 'large',
     onAction() {},
-    actions: []
+    actions: [],
+
+    // deprecated
+    color: 'default',
+    padding: 'medium'
   }
 
   createButtonFromAction = (action, i) => {
@@ -49,6 +58,7 @@ export default class FullScreenDialog extends React.PureComponent {
         data-action-index={i}
         color={action.color}
         disabled={action.disabled}
+        inverted={action.inverted}
         kind={action.kind}
         autoFocus={action.autoFocus}
         className={action.secondary ? styles.actionSecondary : ''}
@@ -73,46 +83,40 @@ export default class FullScreenDialog extends React.PureComponent {
   }
 
   render() {
-    const {
-      color,
-      title,
-      className,
-      onClose,
-      onEscape,
-      isOpen,
-      actions,
-      padding,
-      centered
-    } = this.props
+    const {title, cardClassName, className, onClose, onEscape, isOpen, actions} = this.props
 
-    const classNames = [
-      styles[color] || styles.default,
-      isOpen ? styles.isOpen : styles.isClosed,
-      centered && styles.centered,
-      className
-    ]
-      .filter(Boolean)
-      .join(' ')
+    console.log(this.props)
 
     return (
       <StackedEscapable onEscape={onEscape || onClose || noop}>
         <Portal>
-          <div className={classNames}>
-            <div className={styles.inner}>
-              {onClose && (
-                <button className={styles.closeButton} onClick={onClose} type="button">
-                  <CloseIcon color="inherit" />
-                </button>
-              )}
-              <div className={styles[`padding_${padding}`]}>
+          <section
+            className={classNames(styles.root, isOpen ? styles.isOpen : styles.isClosed, className)}
+          >
+            <div className={classNames(styles.inner, cardClassName)}>
+              <header className={styles.header}>
                 {title && <h1 className={styles.title}>{title}</h1>}
-                <div className={styles.content}>
-                  {this.props.children}
-                  <div className={styles.actionsWrapper}>{this.renderActions(actions)}</div>
-                </div>
-              </div>
+
+                {onClose && (
+                  <div className={styles.actions}>
+                    <Button
+                      className={styles.closeButton}
+                      icon={CloseIcon}
+                      kind="simple"
+                      onClick={onClose}
+                      padding="small"
+                    />
+                  </div>
+                )}
+              </header>
+
+              <div className={styles.content}>{this.props.children}</div>
+
+              {actions && actions.length > 0 && (
+                <div className={styles.actionsWrapper}>{this.renderActions(actions)}</div>
+              )}
             </div>
-          </div>
+          </section>
         </Portal>
       </StackedEscapable>
     )
