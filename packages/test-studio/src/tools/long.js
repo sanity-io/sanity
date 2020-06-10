c03206626a78d4ba784a4608f8ac707618392404
Marius Lundgård <studio@mariuslundgard.com>
2020-06-10 13:50:22 +0200

    [test-studio] Add a custom tool that tests scrollability

---
+++ packages/test-studio/src/tools/long.js
@@ -0,0 +1,121 @@
+import React from 'react'
+
+function LongTool() {
+  return (
+    <div style={{padding: '1em', textAlign: 'center'}}>
+      <h1>This should be scrollable</h1>
+      <h1>This should be scrollable</h1>
+      <h1>This should be scrollable</h1>
+      <h1>This should be scrollable</h1>
+      <h1>This should be scrollable</h1>
+      <h1>This should be scrollable</h1>
+      <h1>This should be scrollable</h1>
+      <h1>This should be scrollable</h1>
+      <h1>This should be scrollable</h1>
+      <h1>This should be scrollable</h1>
+      <h1>This should be scrollable</h1>
+      <h1>This should be scrollable</h1>
+      <h1>This should be scrollable</h1>
+      <h1>This should be scrollable</h1>
+      <h1>This should be scrollable</h1>
+      <h1>This should be scrollable</h1>
+      <h1>This should be scrollable</h1>
+      <h1>This should be scrollable</h1>
+      <h1>This should be scrollable</h1>
+      <h1>This should be scrollable</h1>
+      <h1>This should be scrollable</h1>
+      <h1>This should be scrollable</h1>
+      <h1>This should be scrollable</h1>
+      <h1>This should be scrollable</h1>
+      <h1>This should be scrollable</h1>
+      <h1>This should be scrollable</h1>
+      <h1>This should be scrollable</h1>
+      <h1>This should be scrollable</h1>
+      <h1>This should be scrollable</h1>
+      <h1>This should be scrollable</h1>
+      <h1>This should be scrollable</h1>
+      <h1>This should be scrollable</h1>
+      <h1>This should be scrollable</h1>
+      <h1>This should be scrollable</h1>
+      <h1>This should be scrollable</h1>
+      <h1>This should be scrollable</h1>
+      <h1>This should be scrollable</h1>
+      <h1>This should be scrollable</h1>
+      <h1>This should be scrollable</h1>
+      <h1>This should be scrollable</h1>
+      <h1>This should be scrollable</h1>
+      <h1>This should be scrollable</h1>
+      <h1>This should be scrollable</h1>
+      <h1>This should be scrollable</h1>
+      <h1>This should be scrollable</h1>
+      <h1>This should be scrollable</h1>
+      <h1>This should be scrollable</h1>
+      <h1>This should be scrollable</h1>
+      <h1>This should be scrollable</h1>
+      <h1>This should be scrollable</h1>
+      <h1>This should be scrollable</h1>
+      <h1>This should be scrollable</h1>
+      <h1>This should be scrollable</h1>
+      <h1>This should be scrollable</h1>
+      <h1>This should be scrollable</h1>
+      <h1>This should be scrollable</h1>
+      <h1>This should be scrollable</h1>
+      <h1>This should be scrollable</h1>
+      <h1>This should be scrollable</h1>
+      <h1>This should be scrollable</h1>
+      <h1>This should be scrollable</h1>
+      <h1>This should be scrollable</h1>
+      <h1>This should be scrollable</h1>
+      <h1>This should be scrollable</h1>
+      <h1>This should be scrollable</h1>
+      <h1>This should be scrollable</h1>
+      <h1>This should be scrollable</h1>
+      <h1>This should be scrollable</h1>
+      <h1>This should be scrollable</h1>
+      <h1>This should be scrollable</h1>
+      <h1>This should be scrollable</h1>
+      <h1>This should be scrollable</h1>
+      <h1>This should be scrollable</h1>
+      <h1>This should be scrollable</h1>
+      <h1>This should be scrollable</h1>
+      <h1>This should be scrollable</h1>
+      <h1>This should be scrollable</h1>
+      <h1>This should be scrollable</h1>
+      <h1>This should be scrollable</h1>
+      <h1>This should be scrollable</h1>
+      <h1>This should be scrollable</h1>
+      <h1>This should be scrollable</h1>
+      <h1>This should be scrollable</h1>
+      <h1>This should be scrollable</h1>
+      <h1>This should be scrollable</h1>
+      <h1>This should be scrollable</h1>
+      <h1>This should be scrollable</h1>
+      <h1>This should be scrollable</h1>
+      <h1>This should be scrollable</h1>
+      <h1>This should be scrollable</h1>
+      <h1>This should be scrollable</h1>
+      <h1>This should be scrollable</h1>
+      <h1>This should be scrollable</h1>
+      <h1>This should be scrollable</h1>
+      <h1>This should be scrollable</h1>
+      <h1>This should be scrollable</h1>
+      <h1>This should be scrollable</h1>
+      <h1>This should be scrollable</h1>
+      <h1>This should be scrollable</h1>
+      <h1>This should be scrollable</h1>
+      <h1>This should be scrollable</h1>
+      <h1>This should be scrollable</h1>
+      <h1>This should be scrollable</h1>
+      <h1>This should be scrollable</h1>
+      <h1>This should be scrollable</h1>
+      <h1>This should be scrollable</h1>
+      <h1>This should be scrollable</h1>
+    </div>
+  )
+}
+
+export default {
+  name: 'long',
+  title: 'Long',
+  component: LongTool
+}
