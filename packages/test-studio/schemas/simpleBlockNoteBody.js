a5ec467602f16cfe7f96e87012da5693dddb039e
Marius Lundgård <studio@mariuslundgard.com>
2020-06-11 12:42:50 +0200

    [test-studio] Add schema config to test PTE

---
+++ packages/test-studio/schemas/simpleBlockNoteBody.js
@@ -0,0 +1,41 @@
+export default {
+  name: 'simpleBlockNoteBody',
+  title: 'Body',
+  type: 'array',
+  of: [
+    {
+      type: 'simpleBlockNoteUrl',
+      name: 'ul',
+      title: 'URL'
+    },
+    {
+      type: 'block',
+      of: [
+        {
+          type: 'reference',
+          to: [{type: 'author'}]
+        }
+      ]
+    },
+    {
+      title: 'Code Block',
+      name: 'code',
+      type: 'code'
+    },
+    {
+      title: 'Image',
+      name: 'image',
+      type: 'image',
+      fields: [
+        {
+          title: 'Caption',
+          name: 'caption',
+          type: 'string',
+          options: {
+            isHighlighted: true
+          }
+        }
+      ]
+    }
+  ]
+}
