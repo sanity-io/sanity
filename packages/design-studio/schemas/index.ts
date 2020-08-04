a9d9b99afde883db15bd9b2922013a4e90f4fa9d
Marius Lundgård <studio@mariuslundgard.com>
2020-08-04 10:13:04 +0200

    [design-studio] Add array examples

--- packages/design-studio/schemas/index.ts
+++ packages/design-studio/schemas/index.ts
@@ -1,8 +1,11 @@
 import schemaTypes from 'all:part:@sanity/base/schema-type'
 import createSchema from 'part:@sanity/base/schema-creator'
+
+// document types
 import allInputs from './allInputs'
+import author from './author'
 
 export default createSchema({
   name: 'design-studio',
-  types: schemaTypes.concat([allInputs])
+  types: schemaTypes.concat([allInputs, author])
 })
