e2b975cd2161c78a075a6e18db102cbfc8428031
Espen Hovlandsdal <espen@hovlandsdal.com>
2020-08-07 10:32:09 +0200

    [desk-tool] Move change list builder to separate file

--- packages/@sanity/desk-tool/src/panes/documentPane/changesPanel/ChangesPanel.tsx
+++ packages/@sanity/desk-tool/src/panes/documentPane/changesPanel/ChangesPanel.tsx
@@ -1,99 +1,16 @@
 /* eslint-disable max-depth */
 import React from 'react'
-import {toString as pathToString} from '@sanity/util/paths'
-import {Diff, NoDiff, ObjectDiff, Path, FieldDiff} from '@sanity/diff'
+import {Diff, NoDiff} from '@sanity/diff'
 import {FallbackDiff} from '../../../components/diffs/FallbackDiff'
 import {resolveDiffComponent} from '../../../components/diffs/resolveDiffComponent'
 import {Annotation} from '../history/types'
 import {SchemaType, ChangeNode, FieldChangeNode, GroupChangeNode} from '../types'
+import {buildChangeList} from './buildChangeList'
 import styles from './ChangesPanel.css'
 
 type Props = {
   diff: Diff<Annotation> | NoDiff | null
   schemaType: SchemaType
-}
-
-function getDiffAtPath(diff: ObjectDiff<Annotation>, path: Path): Diff<Annotation> | null {
-  let node: Diff<Annotation> = diff
-
-  for (const segment of path) {
-    if (node.type === 'object' && typeof segment === 'string') {
-      const fieldDiff: FieldDiff<Annotation> = node.fields[segment]
-      if (!fieldDiff || fieldDiff.type === 'unchanged') {
-        return null
-      }
-
-      if (fieldDiff.type === 'added' || fieldDiff.type === 'removed') {
-        // @todo how do we want to handle this?
-        return null
-      }
-
-      node = fieldDiff.diff
-    } else {
-      throw new Error(
-        `Mismatch between path segment (${typeof segment}) and diff type (${diff.type})`
-      )
-    }
-  }
-
-  return node
-}
-
-function buildChangeList(
-  schemaType: SchemaType,
-  diff: ObjectDiff<Annotation>,
-  path: Path,
-  titlePath: string[]
-): ChangeNode[] {
-  const list: ChangeNode[] = []
-
-  const diffComponent = resolveDiffComponent(schemaType)
-  if (diffComponent) {
-    const fieldDiff = getDiffAtPath(diff, path)
-    if (fieldDiff) {
-      list.push({
-        type: 'field',
-        diff: fieldDiff,
-        key: pathToString(path),
-        path,
-        titlePath,
-        schemaType
-      })
-    }
-  } else {
-    schemaType.fields.forEach(field => {
-      const fieldPath = path.concat([field.name])
-      const fieldTitlePath = titlePath.concat([field.type.title || field.name])
-      if (field.type.jsonType === 'object') {
-        const objectChanges = buildChangeList(field.type, diff, fieldPath, fieldTitlePath)
-        if (objectChanges.length > 1) {
-          list.push({
-            type: 'group',
-            key: pathToString(fieldPath),
-            path: fieldPath,
-            titlePath: fieldTitlePath,
-            changes: objectChanges
-          })
-        } else {
-          list.push(...objectChanges)
-        }
-      } else {
-        const fieldDiff = getDiffAtPath(diff, fieldPath)
-        if (fieldDiff) {
-          list.push({
-            type: 'field',
-            diff: fieldDiff,
-            key: fieldPath.join('.'),
-            path: fieldPath,
-            titlePath: fieldTitlePath,
-            schemaType: field.type
-          })
-        }
-      }
-    })
-  }
-
-  return list
 }
 
 export function ChangesPanel({diff, schemaType}: Props) {
