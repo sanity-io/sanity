9fdbd500046a58910a6d2828272584429b67b789
Marius Lundgård <studio@mariuslundgard.com>
2020-08-07 14:19:08 +0200

    [desk-tool] Add basic user-based annotation colors

---
+++ packages/@sanity/base/src/user-color/provider.tsx
@@ -0,0 +1,25 @@
+import React, {createContext, useContext} from 'react'
+import {UserColorManager} from './types'
+
+interface ProviderProps {
+  children: React.ReactNode
+  manager: UserColorManager
+}
+
+const UserColorManagerContext = createContext<UserColorManager | null>(null)
+
+export function useUserColorManager(): UserColorManager {
+  const manager = useContext(UserColorManagerContext)
+
+  if (!manager) {
+    throw new Error(`missing user color manager in context`)
+  }
+
+  return manager
+}
+
+export function UserColorManagerProvider({children, manager}: ProviderProps) {
+  return (
+    <UserColorManagerContext.Provider value={manager}>{children}</UserColorManagerContext.Provider>
+  )
+}
