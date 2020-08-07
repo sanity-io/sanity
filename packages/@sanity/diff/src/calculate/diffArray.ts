817231a76dd946ea994baeba8f3bb4086611ed76
Magnus Holm <judofyr@gmail.com>
2020-08-07 13:07:54 +0200

    [diff] Improve array diffing
    - Assign hasMoved:false to items which are part of the LCS
    - Fix sorting of items (it was reversed)
    - Use the smart diffing for arrays of strings, numbers, booleans, nulls and keyed objects
    - Correctly detect when the array has changed

--- packages/@sanity/diff/src/calculate/diffArray.ts
+++ packages/@sanity/diff/src/calculate/diffArray.ts
@@ -1,59 +1,87 @@
 import {ArrayDiff, ArrayInput, Input, ItemDiff, DiffOptions, NoDiff} from '../types'
 import {diffInput} from './diffInput'
+import {getLongestCommonSubsequence} from './lcs'
 
 export function diffArray<A>(
   fromInput: ArrayInput<A>,
   toInput: ArrayInput<A>,
   options: DiffOptions
 ): ArrayDiff<A> | NoDiff {
+  const fromValue = fromInput.value
+  const toValue = toInput.value
+  const items = diffArrayItems(fromInput, toInput, options)
+
+  return items
+    ? {
+        type: 'array',
+        isChanged: true,
+        fromValue,
+        toValue,
+        items
+      }
+    : {
+        type: 'unchanged',
+        isChanged: false,
+        fromValue,
+        toValue
+      }
+}
+
+/** Returns the items array for the diff, or `undefined` if there are no changes. */
+function diffArrayItems<A>(
+  fromInput: ArrayInput<A>,
+  toInput: ArrayInput<A>,
+  options: DiffOptions
+): ItemDiff<A>[] | undefined {
+  if (fromInput === toInput) return
+
   const keyedA = indexByKey(fromInput)
   const keyedB = indexByKey(toInput)
 
-  const items =
-    keyedA && keyedB ? diffArrayByKey(keyedA, keyedB) : diffArrayByReinsert(fromInput, toInput)
-
-  const fromValue = fromInput.value
-  const toValue = toInput.value
-
-  if (!items) {
-    return {
-      type: 'unchanged',
-      isChanged: false,
-      fromValue,
-      toValue
-    }
-  }
-
-  return {
-    type: 'array',
-    isChanged: true,
-    fromValue,
-    toValue,
-    items
-  }
+  return keyedA && keyedB
+    ? diffArrayByKey(fromInput, keyedA, toInput, keyedB)
+    : diffArrayByReinsert(fromInput, toInput)
 }
 
-function diffArrayByReinsert<A>(fromInput: ArrayInput<A>, toInput: ArrayInput<A>): ItemDiff<A>[] {
+function diffArrayByReinsert<A>(
+  fromInput: ArrayInput<A>,
+  toInput: ArrayInput<A>
+): ItemDiff<A>[] | undefined {
   const items: ItemDiff<A>[] = []
+  let mightBeSame = fromInput.length === toInput.length
 
   for (let idx = 0; idx < toInput.length; idx++) {
+    let input = toInput.at(idx)
+
+    if (mightBeSame) {
+      let diff = diffInput(fromInput.at(idx), input)
+      if (diff.isChanged) {
+        mightBeSame = false
+      }
+    }
+
     items.push({
       type: 'added',
       isChanged: true,
       fromIndex: undefined,
       fromValue: undefined,
       toIndex: idx,
-      toValue: toInput.at(idx).value,
+      toValue: input.value,
       hasMoved: false
     })
   }
 
+  // If `mightBeSame` is still true, then they are indeed equivalent.
+  if (mightBeSame) return
+
   for (let idx = 0; idx < fromInput.length; idx++) {
+    let input = fromInput.at(idx)
+
     items.push({
       type: 'removed',
       isChanged: true,
       fromIndex: idx,
-      fromValue: fromInput.at(idx).value,
+      fromValue: input.value,
       toIndex: undefined,
       toValue: undefined,
       hasMoved: false
@@ -63,95 +91,121 @@ function diffArrayByReinsert<A>(fromInput: ArrayInput<A>, toInput: ArrayInput<A>
   return items
 }
 
+type Key = string | number | boolean
+
 /**
  * Diff an array when all the elements have _key in the same position.
  */
 function diffArrayByKey<A>(
-  fromKeyIndex: KeyIndex<A>,
-  toKeyIndex: KeyIndex<A>
+  fromArray: ArrayInput<A>,
+  fromKeyIndex: KeyIndex,
+  toArray: ArrayInput<A>,
+  toKeyIndex: KeyIndex
 ): ItemDiff<A>[] | undefined {
   const items: ItemDiff<A>[] = []
   let isChanged = false
 
-  // TODO: use LCS to figure out which element which has moved
+  function diffCommon(key: Key, fromIndex: number, toIndex: number, hasMoved: boolean) {
+    deletePositionInIndex(fromKeyIndex.index, key, fromIndex)
+    deletePositionInIndex(toKeyIndex.index, key, toIndex)
 
-  for (let key of fromKeyIndex.keys) {
-    let fromEntry = fromKeyIndex.index.get(key)!
-    const fromValue = fromEntry.item.value
+    let fromInput = fromArray.at(fromIndex)
+    let toInput = toArray.at(toIndex)
 
-    let toEntry = toKeyIndex.index.get(key)
-
-    if (toEntry) {
-      const toValue = toEntry.item.value
-
-      let diff = diffInput(fromEntry.item, toEntry.item)
-      if (diff.isChanged) {
-        items.push({
-          type: 'changed',
-          isChanged: true,
-          fromIndex: fromEntry.index,
-          fromValue,
-          toValue,
-          toIndex: toEntry.index,
-          diff,
-          hasMoved: false
-        })
-        isChanged = true
-      } else {
-        items.push({
-          type: 'unchanged',
-          isChanged: false,
-          fromIndex: fromEntry.index,
-          fromValue,
-          toValue,
-          toIndex: toEntry.index,
-          hasMoved: false
-        })
-      }
-    } else {
+    let diff = diffInput(fromInput, toInput)
+    if (diff.isChanged) {
       items.push({
-        type: 'removed',
+        type: 'changed',
         isChanged: true,
-        fromIndex: fromEntry.index,
-        fromValue,
-        toValue: undefined,
-        toIndex: undefined,
-        hasMoved: false
+        fromIndex,
+        fromValue: fromInput.value,
+        toIndex,
+        toValue: toInput.value,
+        diff,
+        hasMoved
       })
       isChanged = true
+    } else {
+      items.push({
+        type: 'unchanged',
+        isChanged: false,
+        fromIndex,
+        fromValue: fromInput.value,
+        toIndex,
+        toValue: toInput.value,
+        hasMoved
+      })
+
+      if (fromIndex !== toIndex) {
+        isChanged = true
+      }
     }
   }
 
-  for (let key of toKeyIndex.keys) {
-    // Handled above
-    if (fromKeyIndex.index.has(key)) continue
+  let lcs = getLongestCommonSubsequence(fromKeyIndex.keys, toKeyIndex.keys)
 
-    let toEntry = toKeyIndex.index.get(key)!
+  for (let fromIndex = 0; fromIndex < fromKeyIndex.keys.length; fromIndex++) {
+    let key = fromKeyIndex.keys[fromIndex]
+
+    let subsequenceIdx = lcs.prevIndices.indexOf(fromIndex)
+    if (subsequenceIdx !== -1) {
+      // Part of the common subsequence => hasMoved:false
+      diffCommon(key, fromIndex, lcs.nextIndices[subsequenceIdx], false)
+      continue
+    }
+
+    let toIndex = toKeyIndex.index.get(key)?.[0]
+    if (toIndex !== undefined) {
+      // Not a part of the subsequence, but is present in the to-version => hasMoved:true
+      diffCommon(key, fromIndex, toIndex, true)
+      continue
+    }
+
+    // Not a part of the to-value => removed
     items.push({
-      type: 'added',
+      type: 'removed',
       isChanged: true,
-      fromIndex: undefined,
-      fromValue: undefined,
-      toValue: toEntry.item.value,
-      toIndex: toEntry.index,
+      fromIndex,
+      fromValue: fromArray.at(fromIndex).value,
+      toValue: undefined,
+      toIndex: undefined,
       hasMoved: false
     })
+
+    isChanged = true
+  }
+
+  // The remaining data in toKeyIndex are the new elements which has been added
+  for (let positions of toKeyIndex.index.values()) {
+    for (let toIndex of positions) {
+      items.push({
+        type: 'added',
+        isChanged: true,
+        fromIndex: undefined,
+        fromValue: undefined,
+        toIndex,
+        toValue: toArray.at(toIndex).value,
+        hasMoved: false
+      })
+    }
+
     isChanged = true
   }
 
   if (!isChanged) return
 
   items.sort(compareItemDiff)
+
   return items
 }
 
 function compareItemDiff<A>(a: ItemDiff<A>, b: ItemDiff<A>): number {
   if (a.toIndex !== undefined && b.toIndex !== undefined) {
-    return b.toIndex - a.toIndex
+    return a.toIndex - b.toIndex
   }
 
   if (a.fromIndex !== undefined && b.fromIndex !== undefined) {
-    return b.fromIndex - a.fromIndex
+    return a.fromIndex - b.fromIndex
   }
 
   if (a.fromIndex !== undefined && b.toIndex !== undefined) {
@@ -167,14 +221,23 @@ function compareItemDiff<A>(a: ItemDiff<A>, b: ItemDiff<A>): number {
   throw new Error('invalid item diff comparison')
 }
 
-type KeyIndex<A> = {
-  keys: (string | number)[]
-  index: Map<string | number, ItemEntry<A>>
+function deletePositionInIndex(index: Map<Key, number[]>, key: Key, pos: number) {
+  let positions = index.get(key)!
+  deleteArrayValue(positions, pos)
+  if (positions.length === 0) {
+    index.delete(key)
+  }
 }
 
-type ItemEntry<A> = {
-  item: Input<A>
-  index: number
+function deleteArrayValue<E>(arr: E[], value: E) {
+  let idx = arr.indexOf(value)
+  if (idx === -1) throw new Error('value not found')
+  arr.splice(idx, 1)
+}
+
+type KeyIndex = {
+  keys: Key[]
+  index: Map<Key, number[]>
 }
 
 /**
@@ -184,35 +247,50 @@ type ItemEntry<A> = {
  * - Strings
  * - Numbers
  */
-function indexByKey<A>(arr: ArrayInput<A>): KeyIndex<A> | undefined {
-  let index = new Map<string | number, ItemEntry<A>>()
-  let keys: (string | number)[] = []
+function indexByKey<A>(arr: ArrayInput<A>): KeyIndex | undefined {
+  let index = new Map<Key, number[]>()
+  let keys: Key[] = []
   let length = arr.length
 
   for (let i = 0; i < length; i++) {
     let item = arr.at(i)
 
-    let key: string | number | null = null
+    let key: Key | null = null
 
-    if (item.type === 'string') {
-      key = 's' + item.value
-    } else if (item.type === 'number') {
-      key = item.value
-    } else if (item.type === 'object') {
-      const keyField = item.get('_key')
-      if (keyField && keyField.type === 'string') {
-        key = 'k' + keyField.value
+    switch (item.type) {
+      case 'string':
+        key = 's' + item.value
+        break
+      case 'number':
+        key = item.value
+        break
+      case 'boolean':
+        key = item.value
+        break
+      case 'null':
+        key = 'n'
+        break
+      case 'object': {
+        const keyField = item.get('_key')
+        if (keyField && keyField.type === 'string') {
+          key = 'k' + keyField.value
+
+          // We do not handle duplicate _key
+          if (index.has(key)) return
+        }
       }
     }
 
     // No key => abort
     if (key === null) return
 
-    // We've already seen the same key => abort
-    if (index.has(key)) return
-
     keys.push(key)
-    index.set(key, {item, index: i})
+    let positions = index.get(key)
+    if (!positions) {
+      positions = []
+      index.set(key, positions)
+    }
+    positions.push(i)
   }
 
   // All is good.
