b74ff2848bf54c1f1f0c6706fbbcacc418e2d272
Marius Lundgård <studio@mariuslundgard.com>
2020-08-07 12:57:01 +0200

    [desk-tool] Add basic diff styling

--- packages/@sanity/desk-tool/src/components/diffs/StringFieldDiff.tsx
+++ packages/@sanity/desk-tool/src/components/diffs/StringFieldDiff.tsx
@@ -1,8 +1,10 @@
 /* eslint-disable react/no-multi-comp, react/prop-types */
+
 import * as React from 'react'
 import {StringDiffSegment, StringDiff} from '@sanity/diff'
 import {Annotation} from '../../panes/documentPane/history/types'
 import {DiffComponent} from './types'
+
 import styles from './StringFieldDiff.css'
 
 export function StringSegment({
@@ -11,22 +13,22 @@ export function StringSegment({
   segment: StringDiffSegment<Annotation>
 }): React.ReactElement {
   if (segment.type === 'added') {
-    return <span className={styles.add}>{segment.text}</span>
+    return <ins className={styles.add}>{segment.text}</ins>
   }
 
   if (segment.type === 'removed') {
-    return <span className={styles.remove}>{segment.text}</span>
+    return <del className={styles.remove}>{segment.text}</del>
   }
 
-  return <span>{segment.text}</span>
+  return <>{segment.text}</>
 }
 
 export const StringFieldDiff: DiffComponent<StringDiff<Annotation>> = ({diff}) => {
   return (
-    <>
+    <div className={styles.root}>
       {(diff.segments || []).map((segment, idx) => (
         <StringSegment key={String(idx)} segment={segment} />
       ))}
-    </>
+    </div>
   )
 }
