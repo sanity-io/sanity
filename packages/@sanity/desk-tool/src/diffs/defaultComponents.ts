1f29f9b11de7950928f02dba0d4295d5010a1f49
Espen Hovlandsdal <espen@hovlandsdal.com>
2020-08-10 13:16:53 +0200

    [desk-tool] Add placeholder reference field diff

--- packages/@sanity/desk-tool/src/diffs/defaultComponents.ts
+++ packages/@sanity/desk-tool/src/diffs/defaultComponents.ts
@@ -1,8 +1,10 @@
 import {DiffComponent} from './types'
 import {NumberFieldDiff} from './NumberFieldDiff'
 import {StringFieldDiff} from './StringFieldDiff'
+import {ReferenceFieldDiff} from './ReferenceFieldDiff'
 
 export const defaultComponents: {[key: string]: DiffComponent<any>} = {
   string: StringFieldDiff,
-  number: NumberFieldDiff
+  number: NumberFieldDiff,
+  reference: ReferenceFieldDiff
 }
