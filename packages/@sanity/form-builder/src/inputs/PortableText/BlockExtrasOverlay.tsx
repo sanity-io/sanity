58ac332ba96148371e306ee3d7ca19e4846e001d
Per-Kristian Nordnes <per.kristian.nordnes@gmail.com>
2020-08-10 12:31:02 +0200

    [form-builder] PTE: simplify BlockExtrasOverlay

--- packages/@sanity/form-builder/src/inputs/PortableText/BlockExtrasOverlay.tsx
+++ packages/@sanity/form-builder/src/inputs/PortableText/BlockExtrasOverlay.tsx
@@ -1,17 +1,20 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import BlockExtras from 'part:@sanity/form-builder/input/block-editor/block-extras'
+import {
+  PortableTextBlock,
+  PortableTextEditor,
+  usePortableTextEditor
+} from '@sanity/portable-text-editor'
 import PatchEvent from '../../../PatchEvent'
-import createBlockActionPatchFn from './utils/createBlockActionPatchFn'
-import {RenderBlockActions, RenderCustomMarkers} from './types'
 import {Path} from '../../typedefs/path'
 import {Marker} from '../../typedefs'
-import {PortableTextBlock, PortableTextEditor} from '@sanity/portable-text-editor'
+import createBlockActionPatchFn from './utils/createBlockActionPatchFn'
+import {RenderBlockActions, RenderCustomMarkers} from './types'
 
 import styles from './BlockExtrasOverlay.css'
 
 type Props = {
   isFullscreen: boolean
-  editor: PortableTextEditor
   markers: Marker[]
   onFocus: (path: Path) => void
   onChange: (event: PatchEvent) => void
@@ -20,67 +23,42 @@ type Props = {
   value: PortableTextBlock[] | undefined
 }
 
-type State = {
-  visible: boolean
-}
-export default class BlockExtrasOverlay extends React.Component<Props, State> {
-  _setVisibleTimer: any
-  _setVisibleRequest: number
-  state = {
-    visible: false
-  }
-  componentDidMount(): void {
-    // Wait for things to get finshed rendered before rendering the aboslute positions
-    this._setVisibleTimer = setTimeout(() => {
-      this._setVisibleRequest = window.requestAnimationFrame(() => {
-        this.setState({visible: true})
-        this._setVisibleTimer = setTimeout(() => {
-          this.setState({visible: true})
-        }, 200)
-      })
-    }, 0)
-  }
-  componentWillUnmount(): void {
-    clearTimeout(this._setVisibleTimer)
-    window.cancelAnimationFrame(this._setVisibleRequest)
-  }
+export default function BlockExtrasOverlay(props: Props) {
+  const {
+    onFocus,
+    renderCustomMarkers,
+    renderBlockActions,
+    onChange,
+    isFullscreen,
+    markers,
+    value
+  } = props
 
-  shouldComponentUpdate(nextProps: Props, nextState: State): boolean {
-    return (
-      nextProps.value !== this.props.value ||
-      nextState.visible !== this.state.visible ||
-      nextProps.markers !== this.props.markers
-    )
-  }
+  const editor = usePortableTextEditor()
+  const ptFeatures = PortableTextEditor.getPortableTextFeatures(editor)
 
-  // eslint-disable-next-line complexity
-  renderBlockExtras = (block: PortableTextBlock): JSX.Element => {
-    const {
-      editor,
-      onFocus,
-      renderCustomMarkers,
-      renderBlockActions,
-      onChange,
-      isFullscreen
-    } = this.props
-    const markers = this.props.markers.filter(
+  function renderBlockExtras(block: PortableTextBlock): JSX.Element {
+    // Find block markers
+    const myMarkers = Array.from(markers).filter(
       marker => typeof marker.path[0] === 'object' && marker.path[0]._key === block._key
     )
-    if (markers.length === 0 && !renderBlockActions) {
+
+    // Return if no markers
+    if (myMarkers.length === 0 && !renderBlockActions) {
       return null
     }
-    let element
-    try {
-      element = PortableTextEditor.findDOMNode(editor, block)
-    } catch (err) {
+
+    // Try to find DOMNode and return if not
+    const element = PortableTextEditor.findDOMNode(editor, block) as HTMLElement
+
+    if (!element) {
       return null
     }
+
     const rect = element.getBoundingClientRect()
     let actions = null
-    const value = this.props.value || []
     if (renderBlockActions) {
       const RenderComponent = renderBlockActions
-      const ptFeatures = PortableTextEditor.getPortableTextFeatures(editor)
       if (block) {
         actions = (
           <RenderComponent
@@ -94,7 +72,7 @@ export default class BlockExtrasOverlay extends React.Component<Props, State> {
       }
     }
 
-    if (markers.length === 0 && !actions) {
+    if (myMarkers.length === 0 && !actions) {
       return null
     }
 
@@ -119,12 +97,6 @@ export default class BlockExtrasOverlay extends React.Component<Props, State> {
     )
   }
 
-  render(): JSX.Element {
-    const {value} = this.props
-    const {visible} = this.state
-    if (!visible || !value || !Array.isArray(value)) {
-      return null
-    }
-    return <>{value.map(this.renderBlockExtras)}</>
-  }
+  // Render overlay for each block
+  return <>{(value || []).map(renderBlockExtras)}</>
 }
