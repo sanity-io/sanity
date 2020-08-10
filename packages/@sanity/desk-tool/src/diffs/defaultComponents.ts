10b43afe3e9ec81cceb1ff18881535365aaf3cc0
Espen Hovlandsdal <espen@hovlandsdal.com>
2020-08-10 14:30:56 +0200

    [desk-tool] Add placeholder slug diff component

--- packages/@sanity/desk-tool/src/diffs/defaultComponents.ts
+++ packages/@sanity/desk-tool/src/diffs/defaultComponents.ts
@@ -4,11 +4,13 @@ import {StringFieldDiff} from './StringFieldDiff'
 import {ReferenceFieldDiff} from './ReferenceFieldDiff'
 import {ImageFieldDiff} from './ImageFieldDiff'
 import {BooleanFieldDiff} from './BooleanFieldDiff'
+import {SlugFieldDiff} from './SlugFieldDiff'
 
 export const defaultComponents: {[key: string]: DiffComponent<any>} = {
   string: StringFieldDiff,
   number: NumberFieldDiff,
   reference: ReferenceFieldDiff,
   image: ImageFieldDiff,
-  boolean: BooleanFieldDiff
+  boolean: BooleanFieldDiff,
+  slug: SlugFieldDiff
 }
