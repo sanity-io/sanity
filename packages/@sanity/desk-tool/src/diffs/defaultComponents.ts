ae63f5626d384ea050e499f11d98b516951a508a
Espen Hovlandsdal <espen@hovlandsdal.com>
2020-08-10 13:58:56 +0200

    [desk-tool] Add placeholder image field diff component

--- packages/@sanity/desk-tool/src/diffs/defaultComponents.ts
+++ packages/@sanity/desk-tool/src/diffs/defaultComponents.ts
@@ -2,9 +2,11 @@ import {DiffComponent} from './types'
 import {NumberFieldDiff} from './NumberFieldDiff'
 import {StringFieldDiff} from './StringFieldDiff'
 import {ReferenceFieldDiff} from './ReferenceFieldDiff'
+import {ImageFieldDiff} from './ImageFieldDiff'
 
 export const defaultComponents: {[key: string]: DiffComponent<any>} = {
   string: StringFieldDiff,
   number: NumberFieldDiff,
-  reference: ReferenceFieldDiff
+  reference: ReferenceFieldDiff,
+  image: ImageFieldDiff
 }
