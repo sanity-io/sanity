35bbd95ff1ff8a23a56eba487d1a17a30ae59a8f
Espen Hovlandsdal <espen@hovlandsdal.com>
2020-08-13 00:39:24 +0200

    [google-maps-input] Provide diff component

--- packages/@sanity/google-maps-input/src/map/Marker.tsx
+++ packages/@sanity/google-maps-input/src/map/Marker.tsx
@@ -5,20 +5,28 @@ interface Props {
   api: typeof window.google.maps
   map: google.maps.Map
   onMove?: (event: google.maps.MouseEvent) => void
+  onClick?: (event: google.maps.MouseEvent) => void
+  onMouseOver?: (event: google.maps.MouseEvent) => void
+  onMouseOut?: (event: google.maps.MouseEvent) => void
   position: LatLng | google.maps.LatLng
   zIndex?: number
   opacity?: number
   label?: string
+  markerRef?: React.MutableRefObject<google.maps.Marker | undefined>
 }
 
 export class Marker extends React.Component<Props> {
   marker: google.maps.Marker | undefined
 
-  moveHandler: google.maps.MapsEventListener | undefined
+  eventHandlers: {
+    move?: google.maps.MapsEventListener
+    click?: google.maps.MapsEventListener
+  } = {}
 
   componentDidMount() {
-    const {position, api, map, onMove, zIndex, opacity, label} = this.props
+    const {position, api, map, onMove, zIndex, opacity, label, markerRef} = this.props
     const {Marker: GMarker} = api
+
     this.marker = new GMarker({
       position,
       map,
@@ -28,7 +36,12 @@ export class Marker extends React.Component<Props> {
       label
     })
 
+    if (markerRef) {
+      markerRef.current = this.marker
+    }
+
     this.attachMoveHandler()
+    this.attachClickHandler()
   }
 
   componentDidUpdate(prevProps: Props) {
@@ -36,7 +49,7 @@ export class Marker extends React.Component<Props> {
       return
     }
 
-    const {position, onMove, label, zIndex, opacity} = this.props
+    const {position, onMove, label, zIndex, opacity, map} = this.props
 
     if (prevProps.onMove !== onMove) {
       this.attachMoveHandler()
@@ -57,23 +70,39 @@ export class Marker extends React.Component<Props> {
     if (prevProps.opacity !== opacity) {
       this.marker.setOpacity(opacity || null)
     }
+
+    if (prevProps.map !== map) {
+      this.marker.setMap(map)
+    }
   }
 
   componentWillUnmount() {
-    if (this.moveHandler) {
-      this.moveHandler.remove()
+    if (this.eventHandlers.move) {
+      this.eventHandlers.move.remove()
+    }
+
+    if (this.marker) {
+      this.marker.setMap(null)
     }
   }
 
   attachMoveHandler() {
     const {api, onMove} = this.props
-
-    if (this.moveHandler) {
-      this.moveHandler.remove()
+    if (this.eventHandlers.move) {
+      this.eventHandlers.move.remove()
     }
-
     if (this.marker && onMove) {
-      this.moveHandler = api.event.addListener(this.marker, 'dragend', onMove)
+      this.eventHandlers.move = api.event.addListener(this.marker, 'dragend', onMove)
+    }
+  }
+
+  attachClickHandler() {
+    const {api, onClick} = this.props
+    if (this.eventHandlers.click) {
+      this.eventHandlers.click.remove()
+    }
+    if (this.marker && onClick) {
+      this.eventHandlers.click = api.event.addListener(this.marker, 'click', onClick)
     }
   }
 
