296daac5e32c2685b2067571c8783bfb5dc69744
Espen Hovlandsdal <espen@hovlandsdal.com>
2020-08-12 13:54:24 +0200

    [google-maps-input] Rewrite to typescript

---
+++ packages/@sanity/google-maps-input/src/GoogleMapsLoadProxy.tsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import {loadGoogleMapsApi} from './loadGoogleMapsApi'
+
+interface LoadProps {
+  apiKey: string
+  locale?: string
+  children: (api: typeof window.google.maps) => React.ReactElement
+}
+
+interface LoadState {
+  loading: boolean
+  error?: Error
+  api?: typeof window.google.maps
+}
+
+export class GoogleMapsLoadProxy extends React.Component<LoadProps, LoadState> {
+  constructor(props: LoadProps) {
+    super(props)
+
+    const api =
+      typeof window !== 'undefined' && window.google && window.google.maps
+        ? window.google.maps
+        : undefined
+
+    this.state = {loading: !api, api}
+  }
+
+  componentDidMount() {
+    if (this.state.api) {
+      // Already loaded
+      return
+    }
+
+    const {apiKey, locale} = this.props
+    loadGoogleMapsApi(apiKey, locale)
+      .then(api => this.setState({loading: false, api}))
+      .catch(error => this.setState({error}))
+  }
+
+  render() {
+    const {error, loading, api} = this.state
+    if (error) {
+      return <div>Load error: {error.stack}</div>
+    }
+
+    if (loading) {
+      return <div>Loading Google Maps API</div>
+    }
+
+    if (api) {
+      return this.props.children(api)
+    }
+
+    return null
+  }
+}
