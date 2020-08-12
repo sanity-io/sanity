aecbf936528a5a6bb89d9637ee9c477ca472d03a
Espen Hovlandsdal <espen@hovlandsdal.com>
2020-08-12 14:55:25 +0200

    [desk-tool] Allow defining diff component resolvers

--- packages/@sanity/desk-tool/src/diffs/resolveDiffComponent.ts
+++ packages/@sanity/desk-tool/src/diffs/resolveDiffComponent.ts
@@ -1,10 +1,23 @@
+import {ComponentType} from 'react'
+import diffResolvers from 'all:part:@sanity/base/diff-resolver'
 import {defaultComponents} from './defaultComponents'
 import {SchemaType, DiffComponent} from './types'
+
+function tryResolve(type: SchemaType): DiffComponent | undefined {
+  let component: ComponentType | undefined
+  for (const resolver of diffResolvers) {
+    component = resolver(type)
+    if (component) {
+      return component as DiffComponent
+    }
+  }
+  return undefined
+}
 
 export function resolveDiffComponent(type: SchemaType): DiffComponent | undefined {
   let itType: SchemaType | undefined = type
   while (itType) {
-    const resolved = itType.diffComponent || defaultComponents[itType.name]
+    const resolved = itType.diffComponent || tryResolve(itType) || defaultComponents[itType.name]
     if (resolved) {
       return resolved
     }
