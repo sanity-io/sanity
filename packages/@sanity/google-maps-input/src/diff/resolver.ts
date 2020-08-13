1ee95dd7fa70d8c1fcddbca5764c47988f759523
Espen Hovlandsdal <espen@hovlandsdal.com>
2020-08-13 13:46:24 +0200

    [google-maps-input] Add diff component for geopoint array

--- packages/@sanity/google-maps-input/src/diff/resolver.ts
+++ packages/@sanity/google-maps-input/src/diff/resolver.ts
@@ -1,9 +1,22 @@
-import {GeopointSchemaType} from '../types'
+import {GeopointSchemaType, ArraySchemaType} from '../types'
 import {GeopointFieldDiff} from './GeopointFieldDiff'
+import {GeopointArrayDiff} from './GeopointArrayDiff'
 
-export default function diffResolver({schemaType}: {schemaType: GeopointSchemaType}) {
+export default function diffResolver({
+  schemaType
+}: {
+  schemaType: GeopointSchemaType | ArraySchemaType
+}) {
   if (schemaType.name === 'geopoint') {
     return GeopointFieldDiff
+  }
+
+  if (
+    schemaType.name === 'array' &&
+    schemaType.of.length === 1 &&
+    schemaType.of[0].name === 'geopoint'
+  ) {
+    return GeopointArrayDiff
   }
 
   return undefined
