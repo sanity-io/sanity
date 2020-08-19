1188acba89e9f2aa79219ea0d125d7bfbc5de880
Espen Hovlandsdal <espen@hovlandsdal.com>
2020-08-19 15:18:20 +0200

    [base] Provide user store to user color singleton

--- packages/@sanity/base/src/user-color/singleton.ts
+++ packages/@sanity/base/src/user-color/singleton.ts
@@ -1,3 +1,4 @@
+import userStore from 'part:@sanity/base/user'
 import {createUserColorManager} from './manager'
 
-export const userColorManager = createUserColorManager()
+export const userColorManager = createUserColorManager({userStore})
