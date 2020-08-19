b55a4a3527f98bcf98ea31f971a17d23f3b56dd2
Espen Hovlandsdal <espen@hovlandsdal.com>
2020-08-19 15:13:24 +0200

    [base] Change return value from user color manager

--- packages/@sanity/react-hooks/src/useUserColor.ts
+++ packages/@sanity/react-hooks/src/useUserColor.ts
@@ -1,7 +1,12 @@
-import {useUserColorManager} from '@sanity/base/user-color'
-import {useObservable} from './utils/useObservable'
+import {useUserColorManager, UserColor} from '@sanity/base/user-color'
 
-export function useUserColor(userId: string | null): string | null {
+export function useUserColor(userId: string | null): UserColor | null {
   const manager = useUserColorManager()
-  return userId ? useObservable(manager.listen(userId)) : null
+  return userId ? manager.get(userId) : null
+
+  // Note: When we implement rebalancing, use the listen interface
+  // this currently adds overhead (state/hooks) that we don't need,
+  // so for now let's not use it
+
+  // return userId ? useObservable(manager.listen(userId)) : null
 }
