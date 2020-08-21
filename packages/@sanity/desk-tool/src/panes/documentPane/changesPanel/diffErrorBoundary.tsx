ca32f8af217d8f0be5556cf072fa06a24456e40a
Marius Lundgård <studio@mariuslundgard.com>
2020-08-21 14:08:20 +0200

    [desk-tool] Refactor DocumentPane

---
+++ packages/@sanity/desk-tool/src/panes/documentPane/changesPanel/diffErrorBoundary.tsx
@@ -0,0 +1,49 @@
+import * as React from 'react'
+
+import styles from './diffErrorBoundary.css'
+
+declare const __DEV__: boolean
+
+interface BoundaryProps {
+  children: React.ReactNode
+}
+
+interface BoundaryState {
+  error?: Error
+}
+
+export class DiffErrorBoundary extends React.Component<BoundaryProps, BoundaryState> {
+  constructor(props: BoundaryProps) {
+    super(props)
+    this.state = {error: undefined}
+  }
+
+  static getDerivedStateFromError(error) {
+    return {error}
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  componentDidCatch(error: Error) {
+    /* eslint-disable no-console */
+    console.error('Error rendering diff component: ')
+    console.error(error)
+    /* eslint-enable no-console */
+  }
+
+  render() {
+    const isDev = __DEV__ === true
+    const {error} = this.state
+    if (error) {
+      const help = isDev ? <p>Check the developer console for more information.</p> : null
+      return (
+        <div className={styles.root}>
+          <h2>Error</h2>
+          <p>The component responsible for showing the changes of this field crashed.</p>
+          {help}
+        </div>
+      )
+    }
+
+    return this.props.children
+  }
+}
---
+++ packages/@sanity/desk-tool/src/panes/documentPane/documentPanel/statusBar/index.ts
@@ -0,0 +1,3 @@
+export * from './actionStateDialog'
+export * from './documentStatusBar'
+export * from './historyStatusBar'
