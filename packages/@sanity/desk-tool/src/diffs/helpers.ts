44d654a08a3c160ce26925acb5289345497dbe59
Espen Hovlandsdal <espen@hovlandsdal.com>
2020-08-21 14:58:29 +0200

    [field] Expose annotated tooltip components, refactor diff components

--- packages/@sanity/desk-tool/src/diffs/helpers.ts
+++ packages/@sanity/desk-tool/src/diffs/helpers.ts
@@ -1,22 +1,4 @@
-import {UserColorManager, UserColor} from '@sanity/base/user-color'
-import {Annotation} from '@sanity/field/diff'
 import {TypedObject, KeyedObject} from '../panes/documentPane/changesPanel/types'
-
-export function getAnnotationColor(
-  colorManager: UserColorManager,
-  annotation: Annotation
-): UserColor {
-  if (!annotation) {
-    return {
-      background: '#fcc',
-      text: '#f00',
-      // @todo CHANGE BEFORE RELEASING - USING RED TO INDICATE MISSING ANNOTATION
-      border: 'red'
-    }
-  }
-
-  return colorManager.get(annotation.author)
-}
 
 export function getObjectKey(obj: unknown, fallback: string | number) {
   if (isKeyedObject(obj)) {
---
+++ packages/@sanity/field/src/diff/annotations/AnnotatedStringDiff.tsx
@@ -0,0 +1,38 @@
+import * as React from 'react'
+import {StringDiffSegment, StringDiff} from '../types'
+import {DiffAnnotation} from '../annotations'
+import styles from './AnnotatedStringDiff.css'
+
+export function AnnotatedStringDiffSegment({
+  segment
+}: {
+  segment: StringDiffSegment
+}): React.ReactElement {
+  if (segment.type === 'added') {
+    return (
+      <DiffAnnotation annotation={segment.annotation} as="ins" className={styles.add}>
+        {segment.text}
+      </DiffAnnotation>
+    )
+  }
+
+  if (segment.type === 'removed') {
+    return (
+      <DiffAnnotation annotation={segment.annotation} as="del" className={styles.remove}>
+        {segment.text}
+      </DiffAnnotation>
+    )
+  }
+
+  return <>{segment.text}</>
+}
+
+export function AnnotatedStringDiff({diff}: {diff: StringDiff}) {
+  return (
+    <>
+      {(diff.segments || []).map((segment, idx) => (
+        <AnnotatedStringDiffSegment key={String(idx)} segment={segment} />
+      ))}
+    </>
+  )
+}
