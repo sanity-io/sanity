b7cebc457aa0507b34427e2cf9f5320f0e97df85
Espen Hovlandsdal <espen@hovlandsdal.com>
2020-08-21 14:58:29 +0200

    [field] Expose annotated tooltip components, refactor diff components

--- packages/@sanity/desk-tool/src/diffs/image/ImageFieldDiff.tsx
+++ packages/@sanity/desk-tool/src/diffs/image/ImageFieldDiff.tsx
@@ -1,17 +1,6 @@
 import * as React from 'react'
-import {useUserColorManager} from '@sanity/base/user-color'
-import {
-  Annotation,
-  DiffComponent,
-  ObjectDiff,
-  ReferenceDiff,
-  StringDiff,
-  StringSegmentChanged
-} from '@sanity/field/diff'
+import {DiffComponent, ObjectDiff, DiffAnnotation} from '@sanity/field/diff'
 import Preview from 'part:@sanity/base/preview?'
-import {AnnotationTooltip} from '../annotationTooltip'
-import {getAnnotationColor} from '../helpers'
-
 import styles from './ImageFieldDiff.css'
 
 interface Image {
@@ -47,15 +36,12 @@ export const ImageFieldDiff: DiffComponent<ObjectDiff<Image>> = ({diff, schemaTy
     )
   }
 
-  const userColorManager = useUserColorManager()
   const {fromValue, toValue} = diff
   const prev = fromValue?.asset?._ref
   const next = toValue?.asset?._ref
-  const annotation = getRefFieldAnnotation(diff)
-  const color = getAnnotationColor(userColorManager, annotation)
 
-  const content = (
-    <div className={styles.root} style={{background: color.background, color: color.text}}>
+  return (
+    <DiffAnnotation as="div" className={styles.root} diff={diff} path="asset._ref">
       {prev && (
         <div className={styles.removed}>
           <Preview type={schemaType} value={fromValue} layout="card" />
@@ -65,39 +51,6 @@ export const ImageFieldDiff: DiffComponent<ObjectDiff<Image>> = ({diff, schemaTy
       {prev && <div>⇩</div>}
 
       {next && <Preview type={schemaType} value={toValue} layout="card" />}
-    </div>
+    </DiffAnnotation>
   )
-
-  return annotation ? (
-    <AnnotationTooltip annotation={annotation}>{content}</AnnotationTooltip>
-  ) : (
-    content
-  )
-}
-
-function getStringFieldAnnotation(diff: StringDiff): Annotation | null {
-  const changed = diff.segments.find(
-    (segment): segment is StringSegmentChanged =>
-      (segment.type === 'added' || segment.type === 'removed') && Boolean(segment.annotation)
-  )
-
-  return changed ? changed.annotation : null
-}
-
-function getRefFieldAnnotation(diff: ObjectDiff<Image>): Annotation | null {
-  const assetFieldDiff = diff.fields.asset as ReferenceDiff
-  if (!assetFieldDiff || assetFieldDiff.action === 'unchanged') {
-    return null
-  }
-
-  if (assetFieldDiff.type !== 'object') {
-    return null
-  }
-
-  const refField = assetFieldDiff.fields._ref
-  if (!refField || refField.type !== 'string') {
-    return null
-  }
-
-  return getStringFieldAnnotation(refField)
 }
