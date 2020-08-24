2f34d6a376bc9b1f266228ac48ae01e3e91b0d84
Bjørge Næss <bjoerge@gmail.com>
2020-08-24 13:18:41 +0200

    [react-hooks] Make sure useObservable re-subscribes when the passed observable changes

--- packages/@sanity/react-hooks/src/utils/use-observable.ts
+++ packages/@sanity/react-hooks/src/utils/use-observable.ts
@@ -33,13 +33,22 @@ export function toObservable<T, K>(
   return setup ? setup(o) : o
 }
 
-export function useObservable<T>(observable$: Observable<T>): T | null
+const isCallable = (val: any): val is (...args: unknown[]) => unknown => typeof val === 'function'
+
+function getValue<T>(value: T): T
+function getValue<T>(value: T | (() => T)): T {
+  return isCallable(value) ? value() : value
+}
+
+export function useObservable<T>(observable$: Observable<T>): T | undefined
 export function useObservable<T>(observable$: Observable<T>, initialValue: T): T
-export function useObservable<T>(observable$: Observable<T>, initialValue?: T): T | null {
+export function useObservable<T>(observable$: Observable<T>, initialValue: () => T): T
+export function useObservable<T>(observable$: Observable<T>, initialValue?: T): T | undefined {
   const subscription = React.useRef<Subscription>()
-  const [value, setState] = React.useState<T | null>(() => {
+  const isInitial = React.useRef(true)
+  const [value, setState] = React.useState<T | undefined>(() => {
     let isSync = true
-    let syncVal = typeof initialValue === 'undefined' ? null : initialValue
+    let syncVal = getValue(initialValue)
     subscription.current = observable$.subscribe(nextVal => {
       if (isSync) {
         syncVal = nextVal
@@ -51,14 +60,18 @@ export function useObservable<T>(observable$: Observable<T>, initialValue?: T): 
     return syncVal
   })
 
-  React.useEffect(
-    () => () => {
+  React.useEffect(() => {
+    // when the observable$ changes after initial (possibly sync render)
+    if (!isInitial.current) {
+      subscription.current = observable$.subscribe(nextVal => setState(nextVal))
+    }
+    isInitial.current = false
+    return () => {
       if (subscription.current) {
         subscription.current.unsubscribe()
       }
-    },
-    []
-  )
+    }
+  }, [observable$])
 
   return value
 }
