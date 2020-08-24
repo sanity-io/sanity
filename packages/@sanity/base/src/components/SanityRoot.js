00c4bb5fce91dcc8eac7140f0ec5697622c72d91
Marius Lundgård <studio@mariuslundgard.com>
2020-08-24 12:59:58 +0200

    [base] Add the global portal provider

--- packages/@sanity/base/src/components/SanityRoot.js
+++ packages/@sanity/base/src/components/SanityRoot.js
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, {useRef, useEffect} from 'react'
 import config from 'config:sanity'
 import RootComponent from 'part:@sanity/base/root'
+import {PortalProvider} from 'part:@sanity/components/portal'
 import SnackbarProvider from 'part:@sanity/components/snackbar/provider'
 import {userColorManager, UserColorManagerProvider} from '../user-color'
 import ErrorHandler from './ErrorHandler'
@@ -11,21 +12,41 @@ import DevServerStatus from './DevServerStatus'
 
 function SanityRoot() {
   const {projectId, dataset} = config.api || {}
+  const rootRef = useRef(null)
+  const portalRef = useRef(document.createElement('div'))
+
+  // attach the global portal element
+  useEffect(() => {
+    // set a data attribute for debugging
+    portalRef.current.setAttribute('data-portal', '')
+
+    if (rootRef.current) {
+      rootRef.current.appendChild(portalRef.current)
+    }
+
+    return () => {
+      if (rootRef.current) {
+        rootRef.current.removeChild(portalRef.current)
+      }
+    }
+  }, [])
 
   if (!projectId || !dataset) {
     return <MissingProjectConfig />
   }
 
   return (
-    <div className={styles.root}>
-      <UserColorManagerProvider manager={userColorManager}>
-        <SnackbarProvider>
-          <DevServerStatus />
-          <ErrorHandler />
-          <RootComponent />
-          <VersionChecker />
-        </SnackbarProvider>
-      </UserColorManagerProvider>
+    <div className={styles.root} ref={rootRef}>
+      <PortalProvider element={portalRef.current}>
+        <UserColorManagerProvider manager={userColorManager}>
+          <SnackbarProvider>
+            <DevServerStatus />
+            <ErrorHandler />
+            <RootComponent />
+            <VersionChecker />
+          </SnackbarProvider>
+        </UserColorManagerProvider>
+      </PortalProvider>
     </div>
   )
 }
