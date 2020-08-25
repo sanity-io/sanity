13d6b73f809b61e42c136656c58e2abca2a37529
vicbergquist <victoria.bergquist@gmail.com>
2020-08-25 16:04:16 +0200

    [desk-tool] add boolean diff

--- packages/@sanity/desk-tool/src/diffs/boolean/BooleanFieldDiff.tsx
+++ packages/@sanity/desk-tool/src/diffs/boolean/BooleanFieldDiff.tsx
@@ -1,19 +1,30 @@
-import React from 'react'
-import {DiffComponent, BooleanDiff, DiffAnnotation} from '@sanity/field/diff'
+import React, {useRef} from 'react'
+import {DiffComponent, BooleanDiff, DiffAnnotationTooltip} from '@sanity/field/diff'
+import SwitchInput from 'part:@sanity/components/toggles/switch'
+import CheckboxInput from 'part:@sanity/components/toggles/checkbox'
+import ArrowIcon from 'part:@sanity/base/arrow-right'
 import styles from './BooleanFieldDiff.css'
 
-export const BooleanFieldDiff: DiffComponent<BooleanDiff> = ({diff}) => {
+export const BooleanFieldDiff: DiffComponent<BooleanDiff> = ({diff, schemaType}) => {
   const {fromValue, toValue} = diff
-
+  const inputRefFrom = useRef<any>(fromValue)
+  const {title, options} = schemaType
+  const Input = options?.layout === 'checkbox' ? CheckboxInput : SwitchInput
   return (
-    <DiffAnnotation as="div" className={styles.root} diff={diff}>
-      {fromValue !== undefined && fromValue !== null && (
-        <input type="checkbox" checked={fromValue} readOnly />
-      )}
-      {diff.action === 'changed' && <span>&rarr;</span>}
+    <DiffAnnotationTooltip as="div" className={styles.root} diff={diff}>
+      <div className={styles.input}>
+        <Input checked={fromValue} ref={inputRefFrom} />
+      </div>
       {toValue !== undefined && toValue !== null && (
-        <input type="checkbox" checked={toValue} readOnly />
+        <>
+          <div className={styles.arrow}>
+            <ArrowIcon />
+          </div>
+          <div className={styles.input}>
+            <Input checked={toValue} label={title} />
+          </div>
+        </>
       )}
-    </DiffAnnotation>
+    </DiffAnnotationTooltip>
   )
 }
