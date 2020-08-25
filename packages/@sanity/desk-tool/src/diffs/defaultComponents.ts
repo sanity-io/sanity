d23af91bac0ce3c2850a5df92d1221ad8387ca7f
Espen Hovlandsdal <espen@hovlandsdal.com>
2020-08-25 17:07:20 +0200

    [desk-tool] Remove unused array field diff component

--- packages/@sanity/desk-tool/src/diffs/defaultComponents.ts
+++ packages/@sanity/desk-tool/src/diffs/defaultComponents.ts
@@ -1,5 +1,4 @@
 import {DiffComponent} from '@sanity/field/diff'
-import {ArrayFieldDiff} from './array'
 import {NumberFieldDiff} from './number'
 import {StringFieldDiff} from './string'
 import {ReferenceFieldDiff} from './reference'
@@ -9,7 +8,6 @@ import {BooleanFieldDiff} from './boolean'
 import {SlugFieldDiff} from './slug'
 
 export const defaultComponents: {[key: string]: DiffComponent<any>} = {
-  array: ArrayFieldDiff,
   string: StringFieldDiff,
   number: NumberFieldDiff,
   reference: ReferenceFieldDiff,
