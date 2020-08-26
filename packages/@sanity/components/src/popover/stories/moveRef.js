1448efe619551932f4c07503affce83e6e9f9986
Marius Lundgård <studio@mariuslundgard.com>
2020-08-26 15:32:57 +0200

    [desk-tool] Add Timeline menu animation

---
+++ packages/@sanity/components/src/popover/stories/moveRef.js
@@ -0,0 +1,61 @@
+/* eslint-disable react/prop-types */
+
+import {Popover} from 'part:@sanity/components/popover'
+import {select} from 'part:@sanity/storybook/addons/knobs'
+import Sanity from 'part:@sanity/storybook/addons/sanity'
+import {CenteredContainer} from 'part:@sanity/storybook/components'
+import React, {useRef} from 'react'
+
+export function MoveRefStory() {
+  const refIndex = select('Reference element', {'0': 'First', '1': 'Second'}, '0', 'Debug')
+
+  return (
+    <CenteredContainer>
+      <Sanity part="part:@sanity/components/dialogs/default" propTables={[Popover]}>
+        <PopoverExample refIndex={Number(refIndex)} />
+      </Sanity>
+    </CenteredContainer>
+  )
+}
+
+function PopoverExample({refIndex}) {
+  const ref1 = useRef(null)
+  const ref2 = useRef(null)
+  const open = true
+  const placement = 'bottom'
+  const ref = refIndex === 0 ? ref1 : ref2
+
+  return (
+    <Popover
+      content={<div style={{padding: '1em'}}>popover content</div>}
+      open={open}
+      placement={placement}
+      style={{transition: 'transform 200ms'}}
+      targetElement={ref.current}
+    >
+      <div>
+        <span
+          ref={ref1}
+          style={{
+            display: 'inline-block',
+            padding: '0.25em',
+            background: 'rgba(127, 127, 127, 0.5)'
+          }}
+        >
+          reference 1
+        </span>
+
+        <span
+          ref={ref2}
+          style={{
+            display: 'inline-block',
+            padding: '0.25em',
+            background: 'rgba(127, 127, 127, 0.5)'
+          }}
+        >
+          reference 2
+        </span>
+      </div>
+    </Popover>
+  )
+}
--- packages/@sanity/desk-tool/src/panes/documentPane/documentPane.tsx
+++ packages/@sanity/desk-tool/src/panes/documentPane/documentPane.tsx
@@ -1,6 +1,8 @@
+import {ClickOutside} from 'part:@sanity/components/click-outside'
+import {Popover} from 'part:@sanity/components/popover'
 import * as PathUtils from '@sanity/util/paths'
 import classNames from 'classnames'
-import React, {useCallback} from 'react'
+import React, {useCallback, useRef, useState} from 'react'
 import {usePaneRouter} from '../../contexts/PaneRouterContext'
 import {useDeskToolFeatures} from '../../features'
 import {ChangesPanel} from './changesPanel'
@@ -8,6 +10,7 @@ import {useDocumentHistory} from './documentHistory'
 import {DocumentPanel, getProductionPreviewItem} from './documentPanel'
 import {InspectDialog} from './inspectDialog'
 import {DocumentActionShortcuts, isInspectHotkey, isPreviewHotkey} from './keyboardShortcuts'
+import {Timeline} from './timeline'
 import {Doc, DocumentViewType, MenuItemGroup} from './types'
 
 import styles from './documentPane.css'
@@ -58,8 +61,10 @@ export function DocumentPane(props: DocumentPaneProps) {
 
   const features = useDeskToolFeatures()
   const {startTime} = useDocumentHistory()
-  const [showValidationTooltip, setShowValidationTooltip] = React.useState<boolean>(false)
+  const [showValidationTooltip, setShowValidationTooltip] = useState<boolean>(false)
   const paneRouter = usePaneRouter()
+  const [isTimelineOpen, setTimelineOpen] = useState(false)
+  const [timelineMode, setTimelineMode] = useState<'version' | 'changesSince'>('version')
   const activeViewId = paneRouter.params.view || (views[0] && views[0].id)
   const initialFocusPath = paneRouter.params.path
     ? PathUtils.fromString(paneRouter.params.path)
@@ -118,52 +123,87 @@ export function DocumentPane(props: DocumentPaneProps) {
     paneRouter.duplicateCurrent()
   }, [paneRouter])
 
+  const changesSinceSelectRef = useRef<HTMLDivElement | null>(null)
+  const versionSelectRef = useRef<HTMLDivElement | null>(null)
+
+  const handleTimelineOpen = useCallback((mode: 'version' | 'changesSince') => {
+    setTimelineMode(mode)
+    setTimelineOpen(true)
+  }, [])
+
+  const handleTimelineClose = useCallback(() => {
+    setTimelineOpen(false)
+  }, [])
+
   return (
-    <DocumentActionShortcuts
-      id={documentIdRaw}
-      type={documentType}
-      onKeyUp={handleKeyUp}
-      className={classNames([
-        styles.root,
-        isCollapsed && styles.isCollapsed,
-        isSelected ? styles.isActive : styles.isDisabled
-      ])}
+    <Popover
+      content={
+        <ClickOutside onClickOutside={handleTimelineClose}>
+          {ref => (
+            <Timeline onModeChange={setTimelineMode} onSelect={handleTimelineClose} ref={ref} />
+          )}
+        </ClickOutside>
+      }
+      open={isTimelineOpen}
+      placement="bottom"
+      style={{transition: isTimelineOpen ? 'transform 200ms' : undefined}}
+      targetElement={
+        timelineMode === 'version' ? versionSelectRef.current : changesSinceSelectRef.current
+      }
     >
-      <div className={styles.documentContainer}>
-        {isInspectOpen && <InspectDialog value={value} onClose={handleInspectClose} />}
+      <DocumentActionShortcuts
+        id={documentIdRaw}
+        type={documentType}
+        onKeyUp={handleKeyUp}
+        className={classNames([
+          styles.root,
+          isCollapsed && styles.isCollapsed,
+          isSelected ? styles.isActive : styles.isDisabled
+        ])}
+      >
+        <div className={styles.documentContainer}>
+          {isInspectOpen && <InspectDialog value={value} onClose={handleInspectClose} />}
 
-        <DocumentPanel
-          activeViewId={activeViewId}
-          connectionState={connectionState}
-          documentId={documentId}
-          documentType={documentType}
-          idPrefix={paneKey}
-          initialFocusPath={initialFocusPath}
-          initialValue={initialValue}
-          isClosable={isClosable}
-          isCollapsed={isCollapsed}
-          isHistoryOpen={isHistoryOpen}
-          markers={markers}
-          menuItemGroups={menuItemGroups}
-          onChange={onChange}
-          onCloseView={handleClosePane}
-          onCollapse={onCollapse}
-          onExpand={onExpand}
-          onSetActiveView={handleSetActiveView}
-          onSplitPane={handleSplitPane}
-          paneTitle={paneTitle}
-          schemaType={schemaType}
-          toggleInspect={toggleInspect}
-          value={value}
-          views={views}
-        />
-      </div>
-
-      {features.reviewChanges && !isCollapsed && isHistoryOpen && (
-        <div className={styles.changesContainer}>
-          <ChangesPanel documentId={documentId} schemaType={schemaType} />
+          <DocumentPanel
+            activeViewId={activeViewId}
+            connectionState={connectionState}
+            documentId={documentId}
+            documentType={documentType}
+            idPrefix={paneKey}
+            initialFocusPath={initialFocusPath}
+            initialValue={initialValue}
+            isClosable={isClosable}
+            isCollapsed={isCollapsed}
+            isHistoryOpen={isHistoryOpen}
+            markers={markers}
+            menuItemGroups={menuItemGroups}
+            onChange={onChange}
+            onCloseView={handleClosePane}
+            onCollapse={onCollapse}
+            onExpand={onExpand}
+            onSetActiveView={handleSetActiveView}
+            onSplitPane={handleSplitPane}
+            onTimelineOpen={handleTimelineOpen}
+            paneTitle={paneTitle}
+            schemaType={schemaType}
+            toggleInspect={toggleInspect}
+            value={value}
+            versionSelectRef={versionSelectRef}
+            views={views}
+          />
         </div>
-      )}
-    </DocumentActionShortcuts>
+
+        {features.reviewChanges && !isCollapsed && isHistoryOpen && (
+          <div className={styles.changesContainer}>
+            <ChangesPanel
+              documentId={documentId}
+              changesSinceSelectRef={changesSinceSelectRef}
+              onTimelineOpen={handleTimelineOpen}
+              schemaType={schemaType}
+            />
+          </div>
+        )}
+      </DocumentActionShortcuts>
+    </Popover>
   )
 }
