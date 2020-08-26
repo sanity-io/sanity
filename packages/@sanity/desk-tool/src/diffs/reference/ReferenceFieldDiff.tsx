b48768e94eae9bb5e7bc066558e7d41f0bb9db9a
vicbergquist <victoria.bergquist@gmail.com>
2020-08-26 15:59:22 +0200

    [desk-tool] update reference diff styling

--- packages/@sanity/desk-tool/src/diffs/reference/ReferenceFieldDiff.tsx
+++ packages/@sanity/desk-tool/src/diffs/reference/ReferenceFieldDiff.tsx
@@ -1,24 +1,54 @@
-import * as React from 'react'
-import Preview from 'part:@sanity/base/preview?'
+import React from 'react'
 import {DiffComponent, ReferenceDiff, DiffAnnotation} from '@sanity/field/diff'
+import LinkIcon from 'part:@sanity/base/link-icon'
+import ArrowIcon from 'part:@sanity/base/arrow-right'
+import {useRefPreview} from '../hooks'
 import styles from './ReferenceFieldDiff.css'
+
+const ReferenceDetails = ({diff, title, action}) => {
+  return (
+    <DiffAnnotation as="div" diff={diff} path="_ref" className={styles.meta} data-action={action}>
+      <div className={styles.icon}>
+        <LinkIcon />
+      </div>
+      <div className={styles.info}>
+        <h3 className={styles.title} title={title}>
+          {title}
+        </h3>
+        <div>{action}</div>
+      </div>
+    </DiffAnnotation>
+  )
+}
 
 export const ReferenceFieldDiff: DiffComponent<ReferenceDiff> = ({diff, schemaType}) => {
   const {fromValue, toValue} = diff
-  const prev = fromValue && fromValue._ref
-  const next = toValue && toValue._ref
+  const prev = fromValue && useRefPreview(fromValue, schemaType)
+  const next = toValue && useRefPreview(toValue, schemaType)
 
   return (
-    <DiffAnnotation as="div" className={styles.root} diff={diff} path="_ref">
+    <div className={styles.root} data-diff-layout={prev && next ? 'double' : 'single'}>
       {prev && (
-        <div className={styles.removed}>
-          <Preview type={schemaType} value={fromValue} layout="default" />
+        <ReferenceDetails
+          diff={diff}
+          title={prev.title || 'Untitled'}
+          action={prev && next ? 'changed' : 'removed'}
+        />
+      )}
+
+      {prev && next && (
+        <div className={styles.arrow}>
+          <ArrowIcon />
         </div>
       )}
 
-      {prev && <div>⇩</div>}
-
-      {next && <Preview type={schemaType} value={toValue} layout="default" />}
-    </DiffAnnotation>
+      {next && (
+        <ReferenceDetails
+          diff={diff}
+          title={next.title || 'Untitled'}
+          action={prev && next ? 'changed' : 'added'}
+        />
+      )}
+    </div>
   )
 }
