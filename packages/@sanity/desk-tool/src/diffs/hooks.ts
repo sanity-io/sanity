bbe84f93dfde985f0a21f2e2d2828ae9c95f8843
vicbergquist <victoria.bergquist@gmail.com>
2020-08-26 15:59:22 +0200

    [desk-tool] update reference diff styling

--- packages/@sanity/desk-tool/src/diffs/hooks.ts
+++ packages/@sanity/desk-tool/src/diffs/hooks.ts
@@ -1,5 +1,12 @@
 import {useState, useEffect} from 'react'
 import client from 'part:@sanity/base/client'
+import {observeForPreview} from 'part:@sanity/base/preview'
+
+type PreviewSnapshot =
+  | {
+      title: string
+    }
+  | undefined
 
 export function getRefValue(refId: string | undefined) {
   const [value, setValue] = useState(undefined)
@@ -15,4 +22,23 @@ export function getRefValue(refId: string | undefined) {
     return () => {}
   }, [refId])
   return value
+}
+
+export function useRefPreview(value, schemaType): PreviewSnapshot {
+  const [preview, setPreview] = useState(undefined)
+  useEffect(() => {
+    let subscription
+    if (value && schemaType) {
+      subscription = observeForPreview(value, schemaType).subscribe(result => {
+        setPreview(result.snapshot)
+      })
+    }
+
+    return () => {
+      if (subscription) {
+        subscription.unsubscribe()
+      }
+    }
+  }, [value])
+  return preview
 }
