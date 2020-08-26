3979456df1f2d7728b7f919692bf5915ef4fbd67
Espen Hovlandsdal <espen@hovlandsdal.com>
2020-08-26 10:45:34 +0200

    [google-maps-input] Handle presence + focus state

--- packages/@sanity/google-maps-input/src/input/GeopointSelect.tsx
+++ packages/@sanity/google-maps-input/src/input/GeopointSelect.tsx
@@ -10,7 +10,7 @@ const fallbackLatLng: LatLng = {lat: 40.7058254, lng: -74.1180863}
 interface SelectProps {
   api: typeof window.google.maps
   value?: Geopoint
-  onChange: (latLng: google.maps.LatLng) => void
+  onChange?: (latLng: google.maps.LatLng) => void
   defaultLocation?: LatLng
   defaultZoom?: number
 }
@@ -46,11 +46,13 @@ export class GeopointSelect extends React.PureComponent<SelectProps> {
   }
 
   setValue(geoPoint: google.maps.LatLng) {
-    this.props.onChange(geoPoint)
+    if (this.props.onChange) {
+      this.props.onChange(geoPoint)
+    }
   }
 
   render() {
-    const {api, defaultZoom, value} = this.props
+    const {api, defaultZoom, value, onChange} = this.props
     return (
       <div className={styles.wrapper}>
         <GoogleMap
@@ -63,7 +65,12 @@ export class GeopointSelect extends React.PureComponent<SelectProps> {
             <>
               <SearchInput api={api} map={map} onChange={this.handlePlaceChanged} />
               {value && (
-                <Marker api={api} map={map} position={value} onMove={this.handleMarkerDragEnd} />
+                <Marker
+                  api={api}
+                  map={map}
+                  position={value}
+                  onMove={onChange ? this.handleMarkerDragEnd : undefined}
+                />
               )}
             </>
           )}
