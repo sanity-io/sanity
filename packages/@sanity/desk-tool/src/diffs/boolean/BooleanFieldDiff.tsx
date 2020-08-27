b019b0d9f791cb7f225516ca55c3733a5aeac95b
vicbergquist <victoria.bergquist@gmail.com>
2020-08-27 14:24:01 +0200

    [desk-tool] fix color function in boolean diff

--- packages/@sanity/desk-tool/src/diffs/boolean/BooleanFieldDiff.tsx
+++ packages/@sanity/desk-tool/src/diffs/boolean/BooleanFieldDiff.tsx
@@ -3,17 +3,17 @@ import {
   DiffComponent,
   BooleanDiff,
   DiffAnnotationTooltip,
-  useAnnotationColor
+  useDiffAnnotationColor
 } from '@sanity/field/diff'
 import ArrowIcon from 'part:@sanity/base/arrow-right'
 import {Checkbox, Switch} from './BooleanInput'
 import styles from './BooleanFieldDiff.css'
 
-export const BooleanFieldDiff /* : DiffComponent<BooleanDiff> */ = ({diff, schemaType}) => {
-  const {fromValue, toValue, annotation} = diff
+export const BooleanFieldDiff: DiffComponent<BooleanDiff> = ({diff, schemaType}) => {
+  const {fromValue, toValue} = diff
   const {title, options} = schemaType
   const Input = options?.layout === 'checkbox' ? Checkbox : Switch
-  const userColor = useAnnotationColor(annotation)
+  const userColor = useDiffAnnotationColor(diff, []) || {background: '', text: '', border: ''}
   return (
     <DiffAnnotationTooltip as="div" className={styles.root} diff={diff}>
       <Input checked={fromValue} color={userColor} />
