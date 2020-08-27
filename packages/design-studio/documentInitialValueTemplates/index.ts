508f8cc447bb80addc67b4c62a6fb1989d83b590
Marius Lundgård <studio@mariuslundgard.com>
2020-08-27 14:24:52 +0200

    [design-studio] Add author templates

---
+++ packages/design-studio/documentInitialValueTemplates/index.ts
@@ -0,0 +1,32 @@
+import T from '@sanity/base/initial-value-template-builder'
+
+export default [
+  ...T.defaults(),
+
+  T.template({
+    id: 'author-developer',
+    title: 'Developer',
+    schemaType: 'author',
+    value: {
+      role: 'developer'
+    }
+  }),
+
+  T.template({
+    id: 'author-designer',
+    title: 'Designer',
+    schemaType: 'author',
+    value: {
+      role: 'designer'
+    }
+  }),
+
+  T.template({
+    id: 'author-manager',
+    title: 'Manager',
+    schemaType: 'author',
+    value: {
+      role: 'manager'
+    }
+  })
+]
