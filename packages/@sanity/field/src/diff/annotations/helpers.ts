1e3e59de8bb05989da20a11d4a2e813431ce8b3c
Espen Hovlandsdal <espen@hovlandsdal.com>
2020-08-27 13:45:05 +0200

    [desk-tool] Move changes logic from @sanity/desk-tool to @sanity/field

--- packages/@sanity/field/src/diff/annotations/helpers.ts
+++ packages/@sanity/field/src/diff/annotations/helpers.ts
@@ -1,9 +1,7 @@
 import {UserColorManager, UserColor} from '@sanity/base/user-color'
-import {Annotation, Diff, Path, PathSegment, KeyedSegment, ItemDiff} from '../types'
+import {Annotation, Diff, Path, KeyedSegment, ItemDiff} from '../types'
+import {stringToPath, pathToString, isIndexSegment, isKeySegment} from '../paths/helpers'
 import {FALLBACK_DIFF_COLOR} from './contants'
-
-const rePropName = /[^.[\]]+|\[(?:(-?\d+(?:\.\d+)?)|(["'])((?:(?!\2)[^\\]|\\.)*?)\2)\]|(?=(?:\.|\[\])(?:\.|\[\]|$))/g
-const reKeySegment = /_key\s*==\s*['"](.*)['"]/
 
 export function getAnnotationColor(
   colorManager: UserColorManager,
@@ -74,78 +72,8 @@ function getDiffAt(diff: Diff, path: Path, parentPath: Path = []): Diff | undefi
   return getDiffAt(fieldDiff, tail, parentPath.concat(segment))
 }
 
-function normalizePathSegment(segment: string): PathSegment {
-  if (isIndexSegment(segment)) {
-    return normalizeIndexSegment(segment)
-  }
-
-  if (isKeySegment(segment)) {
-    return normalizeKeySegment(segment)
-  }
-
-  return segment
-}
-
-function normalizeIndexSegment(segment: string): PathSegment {
-  return Number(segment.replace(/[^\d]/g, ''))
-}
-
-function normalizeKeySegment(segment: string): KeyedSegment {
-  const segments = segment.match(reKeySegment)
-  if (!segments) {
-    throw new Error('Invalid key segment')
-  }
-
-  return {_key: segments[1]}
-}
-
-function isIndexSegment(segment: any): segment is number {
-  return typeof segment === 'number' || /^\[\d+\]$/.test(segment)
-}
-
-function isKeySegment(segment: any): segment is KeyedSegment {
-  if (typeof segment === 'string') {
-    return reKeySegment.test(segment.trim())
-  }
-
-  return segment && segment._key
-}
-
 function warn(msg: string) {
   console.warn(msg)
-}
-
-function pathToString(path: Path): string {
-  if (!Array.isArray(path)) {
-    throw new Error('Path is not an array')
-  }
-
-  return path.reduce<string>((target, segment, i) => {
-    const segmentType = typeof segment
-    if (segmentType === 'number') {
-      return `${target}[${segment}]`
-    }
-
-    if (segmentType === 'string') {
-      const separator = i === 0 ? '' : '.'
-      return `${target}${separator}${segment}`
-    }
-
-    if (isKeySegment(segment) && segment._key) {
-      return `${target}[_key=="${segment._key}"]`
-    }
-
-    throw new Error(`Unsupported path segment \`${JSON.stringify(segment)}\``)
-  }, '')
-}
-
-function stringToPath(path: string): Path {
-  const segments = path.match(rePropName)
-  if (!segments) {
-    throw new Error('Invalid path string')
-  }
-
-  return segments.map(normalizePathSegment)
 }
 
 function itemMatchesKey(item: ItemDiff, key: KeyedSegment) {
