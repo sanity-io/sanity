d9d8843b61e5390b7c3b40249b05d08c739106ba
vicbergquist <victoria.bergquist@gmail.com>
2020-08-27 14:12:40 +0200

    [desk-tool] update number diff

--- packages/@sanity/desk-tool/src/diffs/number/NumberFieldDiff.tsx
+++ packages/@sanity/desk-tool/src/diffs/number/NumberFieldDiff.tsx
@@ -1,20 +1,24 @@
 import React from 'react'
-import {DiffComponent, NumberDiff, DiffAnnotation} from '@sanity/field/diff'
-
+import {DiffComponent, NumberDiff, DiffAnnotationTooltip, useDiffAnnotationColor} from '@sanity/field/diff'
+import ArrowIcon from 'part:@sanity/base/arrow-right'
 import styles from './NumberFieldDiff.css'
 
 export const NumberFieldDiff: DiffComponent<NumberDiff> = ({diff}) => {
   const {fromValue, toValue} = diff
+  const color = useDiffAnnotationColor(diff, [])
+  const style =  color ? {background: color.background, color: color.text} : {}
 
   return (
-    <DiffAnnotation as="div" className={styles.root} diff={diff}>
+    <DiffAnnotationTooltip className={styles.root} diff={diff}>
       {fromValue !== undefined && (
-        <>
-          <del>{fromValue}</del>
-          <span>&rarr;</span>
-        </>
+       <del className={`${styles.segment} ${styles.add}`} style={style}>{fromValue}</del>
       )}
-      <ins>{toValue}</ins>
-    </DiffAnnotation>
+      {fromValue && toValue &&
+        <div className={styles.arrow}>
+        <ArrowIcon />
+      </div>
+      }
+      {toValue && <ins className={`${styles.segment} ${styles.remove}`} style={style}>{toValue}</ins>}
+    </DiffAnnotationTooltip>
   )
 }
