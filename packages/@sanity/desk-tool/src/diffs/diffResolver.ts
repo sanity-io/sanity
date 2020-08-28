4c9af9d15b9e1af171766aaf7420b264db2303cd
vicbergquist <victoria.bergquist@gmail.com>
2020-08-28 13:34:10 +0200

    [desk-tool] add URL diff component

--- packages/@sanity/desk-tool/src/diffs/diffResolver.ts
+++ packages/@sanity/desk-tool/src/diffs/diffResolver.ts
@@ -1,10 +1,14 @@
 import {DiffComponentResolver} from '@sanity/field/diff'
 import {DatetimeFieldDiff} from './datetime'
+import {UrlFieldDiff} from './url'
 
 const diffResolver: DiffComponentResolver = function diffResolver({schemaType}) {
   // datetime or date
   if (['datetime', 'date'].includes(schemaType.name)) {
     return DatetimeFieldDiff
+  }
+  if (schemaType.name === 'url') {
+    return UrlFieldDiff
   }
   return undefined
 }
