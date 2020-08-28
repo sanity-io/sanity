64705dbdacf56a7894a277b2f313cfa27446c965
vicbergquist <victoria.bergquist@gmail.com>
2020-08-28 12:40:19 +0200

    [desk-tool] add datetime diff component

---
+++ packages/@sanity/desk-tool/src/diffs/datetime/helpers.ts
@@ -0,0 +1,19 @@
+import formatDate from 'date-fns/format'
+
+export function getDateFormat(value, type = '', options){
+  let format = 'YYYY-MM-DD'
+  if (options && options.dateFormat) {
+    format = options.dateFormat
+  }
+  if (type === 'date') {
+    // If the type is date only
+    return formatDate(value, format)
+  }
+  // If the type is datetime
+  if (options && options.timeFormat) {
+    format += ` ${options.timeFormat}`
+  } else {
+    format += ' HH:mm'
+  }
+  return formatDate(value, format)
+}
