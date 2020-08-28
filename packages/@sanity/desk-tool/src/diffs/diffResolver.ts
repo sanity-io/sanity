2a0e044373343fe3e436516a96295182b75e73e7
vicbergquist <victoria.bergquist@gmail.com>
2020-08-28 13:58:52 +0200

    [desk-tool] update slug field diff

--- packages/@sanity/desk-tool/src/diffs/diffResolver.ts
+++ packages/@sanity/desk-tool/src/diffs/diffResolver.ts
@@ -1,6 +1,7 @@
 import {DiffComponentResolver} from '@sanity/field/diff'
 import {DatetimeFieldDiff} from './datetime'
 import {UrlFieldDiff} from './url'
+import {SlugFieldDiff} from './slug'
 
 const diffResolver: DiffComponentResolver = function diffResolver({schemaType}) {
   // datetime or date
@@ -9,6 +10,9 @@ const diffResolver: DiffComponentResolver = function diffResolver({schemaType}) 
   }
   if (schemaType.name === 'url') {
     return UrlFieldDiff
+  }
+  if (schemaType.name === 'slug') {
+    return SlugFieldDiff
   }
   return undefined
 }
