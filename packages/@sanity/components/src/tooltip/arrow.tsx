18c4d29166ed064b8dfca6736f500cbca1759d76
Marius Lundgård <studio@mariuslundgard.com>
2020-08-31 14:08:34 +0200

    [components] Fix tooltip and popover arrows

--- packages/@sanity/components/src/tooltip/arrow.tsx
+++ packages/@sanity/components/src/tooltip/arrow.tsx
@@ -3,15 +3,16 @@ import React, {forwardRef} from 'react'
 import styles from './arrow.css'
 
 export const Arrow = forwardRef(
-  (props: React.HTMLProps<HTMLDivElement> & {tone?: 'navbar'}, ref) => (
-    <div {...props} className={styles.root} data-tone={props.tone} ref={ref as any}>
-      <svg width="15" height="6" viewBox="0 0 15 6" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <g className={styles.outlineGroup}>
-          <path d="M11.5851 5H15C13.4011 5 11.8867 4.28234 10.8742 3.04489L9.82187 1.75866C8.62156 0.29161 6.37844 0.291607 5.17813 1.75866L4.12576 3.04489C3.1133 4.28234 1.59886 5 0 5H3.41494C3.97027 4.64429 4.4726 4.20015 4.89971 3.67813L5.95209 2.39189C6.75229 1.41386 8.24771 1.41386 9.04791 2.3919L10.1003 3.67813C10.5274 4.20016 11.0297 4.64429 11.5851 5Z" />
-        </g>
+  ({tone, ...restProps}: React.HTMLProps<HTMLDivElement> & {tone?: 'navbar'}, ref) => (
+    <div {...restProps} className={styles.root} data-tone={tone} ref={ref as any}>
+      <svg width="15" height="15" viewBox="0 0 15 15">
         <path
-          d="M14.8433 6C13.0437 6 11.3391 5.19224 10.1995 3.79943L9.04791 2.3919C8.24771 1.41386 6.75229 1.41386 5.95209 2.39189L4.80047 3.79943C3.66089 5.19224 1.95632 6 0.156725 6H0H15H14.8433Z"
-          fill="currentColor"
+          className={styles.border}
+          d="M11.5266 1C11.032 1.32802 10.5837 1.73105 10.1995 2.20057L9.04792 3.6081C8.24771 4.58614 6.7523 4.58614 5.95209 3.6081L4.80047 2.20057C4.41632 1.73105 3.96796 1.32802 3.47341 1H0.156727C1.65639 1 3.07687 1.67313 4.02651 2.83381L5.17813 4.24134C6.37844 5.70839 8.62156 5.70839 9.82187 4.24134L10.9735 2.83381C11.9231 1.67313 13.3436 1 14.8433 1H11.5266Z"
+        />
+        <path
+          className={styles.shape}
+          d="M0.156725 0C1.95632 0 3.66089 0.80776 4.80047 2.20057L5.95209 3.6081C6.75229 4.58614 8.24771 4.58614 9.04791 3.6081L10.1995 2.20057C11.3391 0.80776 13.0437 0 14.8433 0H15H0H0.156725Z"
         />
       </svg>
     </div>
