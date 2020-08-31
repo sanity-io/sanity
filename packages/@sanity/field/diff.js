7a13a96455aa85e62c0981edfe182d4406f19f97
Espen Hovlandsdal <espen@hovlandsdal.com>
2020-08-31 15:29:15 +0200

    [field] Use lazy getter for field diff export

--- packages/@sanity/field/diff.js
+++ packages/@sanity/field/diff.js
@@ -1,1 +1,17 @@
-module.exports = require('./lib/diff')
+'use strict'
+
+Object.defineProperty(exports, '__esModule', {
+  value: true
+})
+
+var _diff = require('./lib/diff')
+
+Object.keys(_diff).forEach(function(key) {
+  if (key === 'default' || key === '__esModule') return
+  Object.defineProperty(exports, key, {
+    enumerable: true,
+    get: function get() {
+      return _diff[key]
+    }
+  })
+})
