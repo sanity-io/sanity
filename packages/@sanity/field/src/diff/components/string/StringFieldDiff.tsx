adc46ee745efc29d4cdb18787f0d68005779eba0
vicbergquist <victoria.bergquist@gmail.com>
2020-08-31 12:35:21 +0200

    [field] fix diff for strings with list option

--- packages/@sanity/field/src/diff/components/string/StringFieldDiff.tsx
+++ packages/@sanity/field/src/diff/components/string/StringFieldDiff.tsx
@@ -1,12 +1,38 @@
 import React from 'react'
 import {DiffComponent, StringDiff} from '../../types'
-import {AnnotatedStringDiff} from '../../annotations'
-import styles from './StringFieldDiff.css'
+import {AnnotatedStringDiff, DiffAnnotationTooltip, useDiffAnnotationColor} from '../../annotations'
+import styles from '../shared/BlockSegmentStyles.css'
+import {DiffLayout} from '../shared'
 
-export const StringFieldDiff: DiffComponent<StringDiff> = ({diff}) => {
+export const StringFieldDiff: DiffComponent<StringDiff> = ({diff, schemaType}) => {
+  const {fromValue, toValue} = diff
+  const {options} = schemaType
+  const color = useDiffAnnotationColor(diff, [])
+  const style = color ? {background: color.background, color: color.text} : {}
   return (
     <div className={styles.root}>
-      <AnnotatedStringDiff diff={diff} />
+      {options?.list ? (
+        <DiffAnnotationTooltip className={styles.root} diff={diff}>
+          <DiffLayout
+            renderFrom={
+              fromValue && (
+                <del className={`${styles.segment} ${styles.add}`} style={style}>
+                  {fromValue}
+                </del>
+              )
+            }
+            renderTo={
+              toValue && (
+                <ins className={`${styles.segment} ${styles.remove}`} style={style}>
+                  {toValue}
+                </ins>
+              )
+            }
+          />
+        </DiffAnnotationTooltip>
+      ) : (
+        <AnnotatedStringDiff diff={diff} />
+      )}
     </div>
   )
 }
