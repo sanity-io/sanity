9ecde82931faf0ced03393255a8cc5e5554a8df6
Espen Hovlandsdal <espen@hovlandsdal.com>
2020-08-31 12:29:18 +0200

    [field] Add object field diff component

--- packages/@sanity/field/src/diff/components/defaultComponents.ts
+++ packages/@sanity/field/src/diff/components/defaultComponents.ts
@@ -5,8 +5,10 @@ import {ReferenceFieldDiff} from './reference'
 import {ImageFieldDiff} from './image'
 import {FileFieldDiff} from './file'
 import {BooleanFieldDiff} from './boolean'
+import {ObjectFieldDiff} from './object'
 
 export const defaultComponents: {[key: string]: DiffComponent<any>} = {
+  object: ObjectFieldDiff,
   string: StringFieldDiff,
   number: NumberFieldDiff,
   reference: ReferenceFieldDiff,
