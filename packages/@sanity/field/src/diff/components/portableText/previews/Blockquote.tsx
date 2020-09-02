6851adbcb587a012d63ba7d3afffc037ca1de00a
Per-Kristian Nordnes <per.kristian.nordnes@gmail.com>
2020-09-02 18:26:37 +0200

    [field] PT: make more context available for the preview components

--- packages/@sanity/field/src/diff/components/portableText/previews/Blockquote.tsx
+++ packages/@sanity/field/src/diff/components/portableText/previews/Blockquote.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import styles from './Blockquote.css'
+import {PortableTextBlock} from '../types'
 
 type Props = {
+  block: PortableTextBlock
   children: React.ReactNode
 }
 export default function Blockquote(props: Props) {
--- packages/@sanity/field/src/diff/components/portableText/previews/Decorator.tsx
+++ packages/@sanity/field/src/diff/components/portableText/previews/Decorator.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import styles from './Decorator.css'
+import {PortableTextBlock, PortableTextChild} from '../types'
 
 type Props = {
-  mark: string
+  block: PortableTextBlock
   children: React.ReactNode
+  mark: string
+  span: PortableTextChild
 }
 export default function Decorator(props: Props) {
-  return <span className={`${styles[props.mark]}`}>{props.children}</span>
+  return (
+    <span key={`decorator-${props.span._key}-${props.mark}`} className={`${styles[props.mark]}`}>
+      {props.children}
+    </span>
+  )
 }
