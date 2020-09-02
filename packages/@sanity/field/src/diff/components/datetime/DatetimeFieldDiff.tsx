6574dc635d2c1b8370f615d1f08f6495fe657244
Marius Lundgård <studio@mariuslundgard.com>
2020-09-02 13:01:21 +0200

    [field] Add DiffAnnotationCard building block component

--- packages/@sanity/field/src/diff/components/datetime/DatetimeFieldDiff.tsx
+++ packages/@sanity/field/src/diff/components/datetime/DatetimeFieldDiff.tsx
@@ -1,31 +1,35 @@
 import React from 'react'
 import {DiffComponent, StringDiff} from '../../types'
-import {useDiffAnnotationColor, DiffAnnotationTooltip} from '../../annotations'
+import {DiffAnnotationCard, DiffAnnotationTooltip} from '../../annotations'
 import {DiffLayout} from '../shared'
 import {getDateFormat} from './helpers'
+
 import styles from '../shared/BlockSegmentStyles.css'
 
 export const DatetimeFieldDiff: DiffComponent<StringDiff> = ({diff, schemaType}) => {
   const {fromValue, toValue} = diff
-  const color = useDiffAnnotationColor(diff, [])
-  const style = color ? {background: color.background, color: color.text} : {}
   const fromDate = fromValue && getDateFormat(fromValue, schemaType.name, schemaType.options)
   const toDate = toValue && getDateFormat(toValue, schemaType.name, schemaType.options)
+
   return (
     <DiffAnnotationTooltip className={styles.root} diff={diff}>
       <DiffLayout
         renderFrom={
           fromValue !== undefined && (
-            <del className={`${styles.segment} ${styles.add}`} style={style}>
+            <DiffAnnotationCard as="del" className={`${styles.segment} ${styles.add}`} diff={diff}>
               {fromDate}
-            </del>
+            </DiffAnnotationCard>
           )
         }
         renderTo={
           toValue && (
-            <ins className={`${styles.segment} ${styles.remove}`} style={style}>
+            <DiffAnnotationCard
+              as="ins"
+              className={`${styles.segment} ${styles.remove}`}
+              diff={diff}
+            >
               {toDate}
-            </ins>
+            </DiffAnnotationCard>
           )
         }
       />
