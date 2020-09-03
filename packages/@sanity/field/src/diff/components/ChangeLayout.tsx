b3292db01fd22e06f33ff9f7db82f9bde43ca16b
Espen Hovlandsdal <espen@hovlandsdal.com>
2020-09-04 00:35:33 +0200

    [field] Rename DiffLayout to ChangeLayout

--- packages/@sanity/field/src/diff/components/ChangeLayout.tsx
+++ packages/@sanity/field/src/diff/components/ChangeLayout.tsx
@@ -1,25 +1,25 @@
 import React from 'react'
 import ArrowIcon from 'part:@sanity/base/arrow-right'
-import styles from './DiffLayout.css'
+import styles from './ChangeLayout.css'
 
-export interface DiffLayoutProps {
+export interface ChangeLayoutProps {
   layout?: 'grid' | 'inline'
   from?: React.ReactElement | boolean | null | ''
   to?: React.ReactElement | boolean | null | ''
   children?: React.ReactNode
 }
 
-export const DiffArrow = () => (
+export const ChangeArrow = () => (
   <div className={styles.arrow}>
     <ArrowIcon />
   </div>
 )
 
-export function DiffLayout({layout = 'inline', from, to, children}: DiffLayoutProps) {
+export function ChangeLayout({layout = 'inline', from, to, children}: ChangeLayoutProps) {
   return (
     <div className={styles.root} data-layout={layout}>
       {from}
-      {from && to && <DiffArrow />}
+      {from && to && <ChangeArrow />}
       {to}
       {children}
     </div>
