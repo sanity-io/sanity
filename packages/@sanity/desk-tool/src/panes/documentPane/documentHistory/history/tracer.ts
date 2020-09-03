7fbeaf1ad4bf164e1018acfa4e1759026c700116
Magnus Holm <judofyr@gmail.com>
2020-09-03 15:25:05 +0200

    [desk-tool] Add helper function for replaying a timeline trace

--- packages/@sanity/desk-tool/src/panes/documentPane/documentHistory/history/tracer.ts
+++ packages/@sanity/desk-tool/src/panes/documentPane/documentHistory/history/tracer.ts
@@ -1,4 +1,5 @@
 import {Doc, RemoteMutationWithVersion, TransactionLogEvent} from './types'
+import {Timeline} from './timeline'
 
 export type TraceEvent =
   | {
@@ -11,3 +12,40 @@ export type TraceEvent =
   | {type: 'addTranslogEntry'; event: TransactionLogEvent}
   | {type: 'didReachEarliestEntry'}
   | {type: 'updateChunks'}
+
+export function replay(events: TraceEvent[]): Timeline {
+  const fst = events[0]
+  if (fst?.type !== 'initial') throw new Error('no initial event')
+
+  const timeline = new Timeline({
+    publishedId: fst.publishedId,
+    draft: fst.draft,
+    published: fst.published
+  })
+
+  console.log('Replaying')
+  console.log({events})
+
+  for (let i = 1; i < events.length; i++) {
+    const event = events[i]
+
+    switch (event.type) {
+      case 'initial':
+        throw new Error('unexpected initial event')
+      case 'addRemoteMutation':
+        timeline.addRemoteMutation(event.event)
+        break
+      case 'addTranslogEntry':
+        timeline.addTranslogEntry(event.event)
+        break
+      case 'didReachEarliestEntry':
+        timeline.didReachEarliestEntry()
+        break
+      case 'updateChunks':
+        timeline.updateChunks()
+        break
+    }
+  }
+
+  return timeline
+}
