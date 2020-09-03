df43c436f6295e17634298dbd12eb328cbe80fd1
Espen Hovlandsdal <espen@hovlandsdal.com>
2020-09-03 23:16:32 +0200

    [base] Remove unnecessary useMemo

--- packages/@sanity/base/src/user-color/useUserColor.ts
+++ packages/@sanity/base/src/user-color/useUserColor.ts
@@ -2,11 +2,8 @@ import {of} from 'rxjs'
 import {useObservable} from '../util/useObservable'
 import {useUserColorManager} from './provider'
 import {UserColor} from './types'
-import React from 'react'
 
 export function useUserColor(userId: string | null): UserColor | null {
   const manager = useUserColorManager()
-  return useObservable(
-    userId === null ? of(null) : React.useMemo(() => manager.listen(userId), [userId])
-  )
+  return useObservable(userId === null ? of(null) : manager.listen(userId))
 }
