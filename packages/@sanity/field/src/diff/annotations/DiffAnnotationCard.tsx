d1eecec8cda1ef7f8cd64f61d875fc5f383cf077
Espen Hovlandsdal <espen@hovlandsdal.com>
2020-09-04 16:33:03 +0200

    [field] Fix diff/path/annotation passed down to DOM element

--- packages/@sanity/field/src/diff/annotations/DiffAnnotationCard.tsx
+++ packages/@sanity/field/src/diff/annotations/DiffAnnotationCard.tsx
@@ -2,9 +2,14 @@ import React, {createElement} from 'react'
 import {useAnnotationColor} from './hooks'
 import {AnnotationProps, AnnotatedDiffProps} from './DiffAnnotation'
 import {getAnnotationAtPath} from './helpers'
+import {Diff, Annotation} from '../../types'
+import {Path} from '../../paths'
 
 interface BaseDiffAnnotationCardProps {
   as?: React.ElementType | keyof JSX.IntrinsicElements
+  diff?: Diff
+  path?: Path | string
+  annotation?: Annotation
 }
 
 export type DiffAnnotationCardProps = (AnnotationProps | AnnotatedDiffProps) &
@@ -13,10 +18,17 @@ export type DiffAnnotationCardProps = (AnnotationProps | AnnotatedDiffProps) &
 export function DiffAnnotationCard(
   props: DiffAnnotationCardProps & React.HTMLProps<HTMLDivElement>
 ): React.ReactElement {
-  const {as = 'div', children, style = {}, ...restProps} = props
-  const annotation =
-    'diff' in props ? getAnnotationAtPath(props.diff, props.path || []) : props.annotation
+  const {
+    as = 'div',
+    children,
+    path,
+    diff,
+    annotation: specifiedAnnotation,
+    style = {},
+    ...restProps
+  } = props
 
+  const annotation = diff ? getAnnotationAtPath(diff, path || []) : specifiedAnnotation
   const color = useAnnotationColor(annotation)
   const colorStyle = color ? {background: color.background, color: color.text} : {}
   return createElement(as, {...restProps, style: {...colorStyle, ...style}}, children)
