8390d422c3fedac2aa1e81ce4da96899ba5a4023
Espen Hovlandsdal <espen@hovlandsdal.com>
2020-09-04 14:41:32 +0200

    [field] Add annotation to array segments where possible

--- packages/@sanity/field/src/diff/changes/ChangeTitleSegment.tsx
+++ packages/@sanity/field/src/diff/changes/ChangeTitleSegment.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react'
 import {FromToIndex} from '../../types'
+import {DiffAnnotation} from '../annotations'
 import styles from './ChangeTitleSegment.css'
 
 export function ChangeTitleSegment({segment}: {segment: string | FromToIndex}) {
@@ -11,12 +12,39 @@ export function ChangeTitleSegment({segment}: {segment: string | FromToIndex}) {
     )
   }
 
-  const {hasMoved, fromIndex, toIndex} = segment
-  return (
-    <span className={styles.indexGroup}>
-      {hasMoved && <span className={styles.index}>{fromIndex}</span>}
-      {hasMoved && ' → '}
-      <span className={styles.index}>{toIndex}</span>
-    </span>
-  )
+  const {hasMoved, fromIndex, toIndex, annotation} = segment
+  const created = typeof fromIndex === 'undefined'
+  const deleted = typeof toIndex === 'undefined'
+  if (created) {
+    // Item was created
+    return (
+      <DiffAnnotation annotation={annotation || null} className={styles.indexGroup} as="ins">
+        #{toIndex}
+      </DiffAnnotation>
+    )
+  }
+
+  if (deleted) {
+    return (
+      <DiffAnnotation annotation={annotation || null} className={styles.indexGroup} as="del">
+        #{fromIndex}
+      </DiffAnnotation>
+    )
+  }
+
+  if (hasMoved && typeof toIndex !== 'undefined' && typeof fromIndex !== 'undefined') {
+    const indexDiff = toIndex - fromIndex
+    const indexSymbol = indexDiff < 0 ? '↑' : '↓'
+    return (
+      <>
+        <span className={styles.indexGroup}>#{toIndex}</span>
+        <DiffAnnotation annotation={annotation || null} className={styles.indexGroup} as="span">
+          {indexSymbol}
+          {Math.abs(indexDiff)}
+        </DiffAnnotation>
+      </>
+    )
+  }
+
+  return <span className={styles.indexGroup}>#{toIndex}</span>
 }
