3843dd8e5b601207bdae75050a1b9802d14cca5a
Marius Lundgård <studio@mariuslundgard.com>
2020-09-07 16:14:17 +0200

    [storybook] Add tone selection to container

--- packages/@sanity/storybook/src/components/centeredContainer.js
+++ packages/@sanity/storybook/src/components/centeredContainer.js
@@ -1,19 +1,31 @@
 import classNames from 'classnames'
-import {color, number} from 'part:@sanity/storybook/addons/knobs'
+import {number, select} from 'part:@sanity/storybook/addons/knobs'
 import React from 'react'
 
 import styles from './centeredContainer.css'
 
 export function CenteredContainer({children, className: classNameProp, style, ...restProps}) {
-  const backgroundColor = color('View color', 'rgba(255, 255, 255, 0', 'test')
-  const fontSize = number('Font size (rem)', 1, {range: true, min: 0.5, max: 3, step: 0.25})
+  const fontSize = number(
+    'Font size (rem)',
+    1,
+    {range: true, min: 0.5, max: 3, step: 0.25},
+    'Container'
+  )
   const className = classNames(styles.root, classNameProp)
+  const tone =
+    select(
+      'Background tone',
+      {'': '(none)', component: 'Component', navbar: 'Navbar'},
+      'component',
+      'Container'
+    ) || undefined
 
   return (
     <div
       {...restProps}
       className={className}
-      style={{backgroundColor, fontSize: `${fontSize}rem`, ...(style || {})}}
+      data-tone={tone}
+      style={{fontSize: `${fontSize}rem`, ...(style || {})}}
     >
       {children}
     </div>
