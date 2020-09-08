290c2d87094d948121359bc527882ff8b7a08db1
Espen Hovlandsdal <espen@hovlandsdal.com>
2020-09-08 16:02:38 +0200

    [field] Iterate over object field values when validating

--- packages/@sanity/field/src/validation/index.ts
+++ packages/@sanity/field/src/validation/index.ts
@@ -1,4 +1,4 @@
-import {SchemaType} from '../diff'
+import {SchemaType, ObjectSchemaType} from '../diff'
 
 export function getValueError(value: unknown, schemaType: SchemaType) {
   const {jsonType} = schemaType
@@ -12,5 +12,17 @@ export function getValueError(value: unknown, schemaType: SchemaType) {
     return {error: `Value is ${valueType}, expected ${jsonType}`, value}
   }
 
+  if (isObjectType(schemaType) && isObjectValue(value)) {
+    return schemaType.fields.find(field => getValueError(value[field.name], field.type))
+  }
+
   return undefined
+}
+
+function isObjectType(schemaType: SchemaType): schemaType is ObjectSchemaType {
+  return schemaType.jsonType === 'object'
+}
+
+function isObjectValue(value: unknown): value is Record<string, unknown> {
+  return value !== null && !Array.isArray(value) && typeof value === 'object'
 }
