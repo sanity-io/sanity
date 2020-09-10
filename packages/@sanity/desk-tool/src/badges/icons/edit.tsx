d8264c3f384716e5327f8fb21276568af2d73276
Mikolaj Dobrucki <5467602+mikolajdobrucki@users.noreply.github.com>
2020-09-10 15:01:15 +0200

    [desk-tool] Add custom sparkline icons

---
+++ packages/@sanity/desk-tool/src/badges/icons/edit.tsx
@@ -0,0 +1,17 @@
+// part:@sanity/base/edit-icon
+
+import React from 'react'
+
+const strokeStyle = {
+  stroke: 'currentColor',
+  strokeWidth: 1
+}
+
+export default function EditIcon() {
+  return (
+    <svg width="17" height="17" viewBox="0 0 17 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M4.5 10.5L4 13L6.5 12.5L14 5L12 3L4.5 10.5Z" style={strokeStyle}/>
+      <path d="M10 5L12 7" style={strokeStyle}/>
+    </svg>
+  )
+}
---
+++ packages/@sanity/desk-tool/src/badges/icons/publish.tsx
@@ -0,0 +1,18 @@
+// part:@sanity/base/publish-icon
+
+import React from 'react'
+
+const strokeStyle = {
+  stroke: 'currentColor',
+  strokeWidth: 1
+}
+
+export default function PublishIcon() {
+  return (
+    <svg width="17" height="17" viewBox="0 0 17 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M3 3.5H14" style={strokeStyle}/>
+        <path d="M8.5 6V14" style={strokeStyle}/>
+        <path d="M5 9.5L8.5 6L12 9.5" style={strokeStyle}/>
+    </svg>
+  )
+}
