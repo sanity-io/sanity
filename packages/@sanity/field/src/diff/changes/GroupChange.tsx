1b77fcb418139ea32fa5be4b7145ce2cb39b197b
Espen Hovlandsdal <espen@hovlandsdal.com>
2020-09-10 14:58:00 +0200

    [field] Fix nesting of fields in change list

--- packages/@sanity/field/src/diff/changes/GroupChange.tsx
+++ packages/@sanity/field/src/diff/changes/GroupChange.tsx
@@ -1,22 +1,39 @@
+import {useDocumentOperation} from '@sanity/react-hooks'
 import * as React from 'react'
-import {GroupChangeNode} from '../../types'
+import {useHover} from '../../utils/useHover'
+import {GroupChangeNode, OperationsAPI} from '../../types'
 import {ChangeBreadcrumb} from './ChangeBreadcrumb'
 import {ChangeResolver} from './ChangeResolver'
-
+import {DocumentChangeContext} from './DocumentChangeContext'
+import {RevertChangesButton} from './RevertChangesButton'
+import {undoChange} from './undoChange'
 import styles from './GroupChange.css'
 
-export function GroupChange({change: group}: {change: GroupChangeNode}) {
+export function GroupChange({change: group}: {change: GroupChangeNode}): React.ReactElement {
   const {titlePath, changes} = group
+  const {documentId, schemaType} = React.useContext(DocumentChangeContext)
+  const [hoverRef, isHoveringRevert] = useHover<HTMLDivElement>()
+  const docOperations = useDocumentOperation(documentId, schemaType.name) as OperationsAPI
+
+  const handleRevertChanges = React.useCallback(
+    () => undoChange(group.diff, group.path, docOperations),
+    [documentId, group.key, group.diff]
+  )
+
   return (
     <div className={styles.groupChange}>
       <div className={styles.changeHeader}>
         <ChangeBreadcrumb titlePath={titlePath} />
       </div>
 
-      <div className={styles.changeList}>
+      <div className={isHoveringRevert ? styles.changeListOutlined : styles.changeList}>
         {changes.map(change => (
           <ChangeResolver key={change.key} change={change} />
         ))}
+
+        <div ref={hoverRef} className={styles.revertChangesButtonContainer}>
+          <RevertChangesButton onClick={handleRevertChanges} />
+        </div>
       </div>
     </div>
   )
