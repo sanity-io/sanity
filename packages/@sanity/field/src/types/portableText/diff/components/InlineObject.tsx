2b1b35761d5db30e5362b8bd852a4654b7d09a13
Per-Kristian Nordnes <per.kristian.nordnes@gmail.com>
2020-09-11 13:02:23 +0200

    [field] PT: render diffs better

--- packages/@sanity/field/src/types/portableText/diff/components/InlineObject.tsx
+++ packages/@sanity/field/src/types/portableText/diff/components/InlineObject.tsx
@@ -1,15 +1,20 @@
 import React, {SyntheticEvent} from 'react'
-import styles from './InlineObject.css'
 import {PortableTextChild} from '../types'
 import {DiffAnnotationTooltip, ObjectDiff, useDiffAnnotationColor} from '../../../../diff'
+import styles from './InlineObject.css'
+
+const noop = () => {
+  // Nothing
+}
 
 type Props = {
   diff?: ObjectDiff
+  blockDiff?: ObjectDiff
   object: PortableTextChild
   onClick?: (event: SyntheticEvent<HTMLSpanElement>) => void
   children?: React.ReactNode
 }
-export default function InlineObject(props: Props) {
+export default function InlineObject(props: Props): JSX.Element {
   const children = props.children || props.object._type
   const {diff, onClick} = props
   let returned = <span className={styles.root}>{children}</span>
@@ -25,7 +30,7 @@ export default function InlineObject(props: Props) {
       ? (event: SyntheticEvent<HTMLSpanElement>) => {
           onClick(event)
         }
-      : () => {}
+      : noop
 
     // Wrap in inline object
     returned = (
--- packages/@sanity/field/src/types/portableText/diff/helpers.ts
+++ packages/@sanity/field/src/types/portableText/diff/helpers.ts
@@ -1,26 +1,35 @@
-import {ArrayDiff, ObjectDiff, StringDiff} from '../../../diff'
 import {startCase} from 'lodash'
-import {ChildMap, PortableTextBlock, PortableTextChild, SpanTypeSchema} from './types'
+import {ArrayDiff, ObjectDiff, StringDiff} from '../../../diff'
 import {SchemaType, ObjectSchemaType} from '../../../types'
+import {
+  ChildMap,
+  PortableTextBlock,
+  PortableTextDiff,
+  PortableTextChild,
+  SpanTypeSchema
+} from './types'
 
 export const UNKNOWN_TYPE_NAME = '_UNKOWN_TYPE_'
 
-export function isPTSchemaType(schemaType: SchemaType) {
+export function isPTSchemaType(schemaType: SchemaType): boolean {
   return schemaType.jsonType === 'object' && schemaType.name === 'block'
 }
-export function isHeader(node: PortableTextBlock) {
+export function isHeader(node: PortableTextBlock): boolean {
   return !!node.style && ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'].includes(node.style)
 }
 
-export function createChildMap(blockDiff: ObjectDiff, schemaType: ObjectSchemaType) {
+export function createChildMap(
+  blockDiff: PortableTextDiff,
+  schemaType: ObjectSchemaType
+): ChildMap {
   // Create a map from span to diff
-  const block = (diffDidRemove(blockDiff)
-    ? blockDiff.fromValue
-    : blockDiff.toValue) as PortableTextBlock
+  const block = blockDiff.displayValue
+  // Add removed children
   const childMap: ChildMap = {}
   const children = block.children || []
+  // eslint-disable-next-line complexity
   children.forEach(child => {
-    let summary: string[] = []
+    const summary: string[] = []
     // Fallback type for renderer (unkown types)
     if (typeof child !== 'object' || typeof child._type !== 'string') {
       child._type = UNKNOWN_TYPE_NAME
@@ -31,6 +40,7 @@ export function createChildMap(blockDiff: ObjectDiff, schemaType: ObjectSchemaTy
     if (cDiff) {
       const textDiff = cDiff.fields.text as StringDiff
       if (textDiff && textDiff.isChanged) {
+        // eslint-disable-next-line max-depth
         if (textDiff.action === 'changed') {
           summary.push(`Changed '${textDiff.fromValue}' to  '${textDiff.toValue}'`)
         } else {
@@ -110,11 +120,11 @@ function isRemoveInlineObject(cDiff: ObjectDiff) {
   )
 }
 
-function isAddMark(cDiff: ObjectDiff, cSchemaType?: SchemaType) {
+export function isAddMark(cDiff: ObjectDiff, cSchemaType?: SchemaType): boolean {
   if (!cSchemaType) {
     return false
   }
-  return (
+  return !!(
     cDiff.fields.marks &&
     cDiff.fields.marks.isChanged &&
     cDiff.fields.marks.action === 'added' &&
@@ -127,11 +137,26 @@ function isAddMark(cDiff: ObjectDiff, cSchemaType?: SchemaType) {
   )
 }
 
-function isAddAnnotation(cDiff: ObjectDiff, cSchemaType?: SchemaType) {
+export function isRemoveMark(cDiff: ObjectDiff, cSchemaType?: SchemaType): boolean {
   if (!cSchemaType) {
     return false
   }
-  return (
+  return !!(
+    cDiff.fields.marks &&
+    cDiff.fields.marks.isChanged &&
+    cDiff.fields.marks.action === 'removed' &&
+    Array.isArray(cDiff.fields.marks.fromValue) &&
+    cDiff.fields.marks.fromValue.some(
+      mark => typeof mark === 'string' && cSchemaType && isDecorator(mark, cSchemaType)
+    )
+  )
+}
+
+function isAddAnnotation(cDiff: ObjectDiff, cSchemaType?: SchemaType): boolean {
+  if (!cSchemaType) {
+    return false
+  }
+  return !!(
     cDiff.fields.marks &&
     cDiff.fields.marks.isChanged &&
     cDiff.fields.marks.action === 'added' &&
@@ -144,11 +169,11 @@ function isAddAnnotation(cDiff: ObjectDiff, cSchemaType?: SchemaType) {
   )
 }
 
-function isRemoveAnnotation(cDiff: ObjectDiff, cSchemaType?: SchemaType) {
+function isRemoveAnnotation(cDiff: ObjectDiff, cSchemaType?: SchemaType): boolean {
   if (!cSchemaType) {
     return false
   }
-  return (
+  return !!(
     cDiff.fields.marks &&
     cDiff.fields.marks.isChanged &&
     cDiff.fields.marks.action === 'removed' &&
@@ -173,26 +198,211 @@ function getChildSchemaType(fields: any[], child: PortableTextChild) {
   return cSchemaType
 }
 
-export function diffDidRemove(blockDiff: ObjectDiff) {
-  const childrenDiff = blockDiff.fields.children as ArrayDiff
-  return (
-    blockDiff.action === 'removed' ||
-    (childrenDiff && childrenDiff.items.some(item => item.diff.action === 'removed'))
-  )
+export function diffDidRemove(blockDiff: ObjectDiff): boolean {
+  return blockDiff.action === 'removed'
 }
 
-export function getDecorators(spanSchemaType: SpanTypeSchema) {
+export function getDecorators(spanSchemaType: SpanTypeSchema): {title: string; value: string}[] {
   if (spanSchemaType.decorators) {
     return spanSchemaType.decorators
   }
   return []
 }
 
-export function isDecorator(name: string, schemaType: SpanTypeSchema) {
+export function isDecorator(name: string, schemaType: SpanTypeSchema): boolean {
   return getDecorators(schemaType).some(dec => dec.value === name)
 }
 
-export function childIsSpan(child: PortableTextChild) {
+export function childIsSpan(child: PortableTextChild): boolean {
   const isObject = typeof child === 'object'
   return isObject && typeof child._type === 'string' && child._type === 'span'
+}
+
+export function didChangeMarksOnly(diff: ObjectDiff): boolean {
+  const from = blockToText(diff.fromValue as PortableTextBlock)
+  const to = blockToText(diff.toValue as PortableTextBlock)
+  const childrenDiff = diff.fields.children as ArrayDiff
+  const hasMarkDiffs =
+    !!childrenDiff &&
+    childrenDiff.items.every(
+      item => item.diff.isChanged && item.diff.type === 'object' && item.diff.fields.marks
+    )
+  return from === to && hasMarkDiffs
+}
+
+export function marksAreChangedByAction(
+  diff: ObjectDiff,
+  action: 'added' | 'removed' | 'changed'
+): boolean {
+  const childrenDiff = diff.fields.children as ArrayDiff
+  const hasMarkDiffs =
+    !!childrenDiff &&
+    childrenDiff.items.some(
+      item =>
+        item.diff.isChanged &&
+        item.diff.type === 'object' &&
+        item.diff.fields.marks &&
+        item.diff.fields.marks.action === action
+    )
+  return hasMarkDiffs
+}
+
+export function blockToText(block: PortableTextBlock | undefined | null): string {
+  if (!block) {
+    return ''
+  }
+  return block.children.map(child => child.text || '').join('')
+}
+
+export function prepareDiffForPortableText(diff: ObjectDiff): PortableTextDiff {
+  const _diff: PortableTextDiff = {
+    ...diff,
+    displayValue:
+      diff.action === 'removed'
+        ? (diff.fromValue as PortableTextBlock)
+        : (diff.toValue as PortableTextBlock)
+  } // Make a copy so we don't manipulate the original diff object
+
+  // Add children that are removed to the display value
+  if (_diff.action !== 'removed') {
+    const childrenDiff = _diff.fields.children as ArrayDiff
+    const newChildren = [...(_diff?.toValue?.children || [])] as PortableTextChild[]
+    const removedChildrenDiffs =
+      (childrenDiff &&
+        childrenDiff.items.filter(item => item.diff && item.diff.action === 'removed')) ||
+      []
+    removedChildrenDiffs.forEach(rDiff => {
+      if (rDiff.fromIndex !== undefined) {
+        const fromValue = rDiff.diff.fromValue as PortableTextChild
+        if (fromValue._key) {
+          newChildren.splice(rDiff.fromIndex + 1, 0, fromValue)
+        }
+      }
+    })
+    _diff.displayValue = {..._diff.toValue, children: newChildren} as PortableTextBlock
+  }
+  // Special condition when the only change is adding marks (then just remove all the other diffs - like created new spans)
+  const onlyMarksAreChanged = didChangeMarksOnly(_diff)
+  if (onlyMarksAreChanged) {
+    const childrenItem = _diff.fields.children
+    if (childrenItem && childrenItem.type === 'array') {
+      childrenItem.items.forEach(item => {
+        if (item.diff.type === 'object') {
+          const itemDiff = item.diff as ObjectDiff
+          Object.keys(itemDiff.fields).forEach(key => {
+            if (key !== 'marks') {
+              delete itemDiff.fields[key]
+            }
+          })
+        }
+      })
+    }
+  }
+
+  // EXPERIMENTAL APPROACH TO BETTER SHOW DECORATOR CHANGES WHEN
+  // THERE ARE ALSO CHANGES IN THE TEXT.
+
+  // else if (marksAreChangedByAction(_diff, 'added') && _diff.toValue) {
+  //   console.log('Marks added, but there is more!')
+  //   // Is child split to add new span with mark?
+  //   const block = _diff.toValue as PortableTextBlock
+  //   // Find the span which has an added mark
+  //   const childrenDiff = _diff.fields.children as ArrayDiff
+  //   // console.log(
+  //   //   JSON.stringify(
+  //   //     childrenDiff.items.map(item => item.diff),
+  //   //     null,
+  //   //     2
+  //   //   )
+  //   // )
+  //   const addMarkItems = childrenDiff.items.filter(
+  //     item =>
+  //       item.diff.isChanged &&
+  //       item.diff.type === 'object' &&
+  //       item.diff.fields.marks &&
+  //       item.diff.fields.marks.toValue &&
+  //       Array.isArray(item.diff.fields.marks.toValue) &&
+  //       item.diff.fields.marks.toValue.length > 0
+  //   )
+  //   console.log('diff', _diff)
+  //   console.log('addMarkItems', addMarkItems)
+  //   // eslint-disable-next-line complexity
+  //   addMarkItems.forEach(item => {
+  //     const span = item.diff.toValue as PortableTextChild
+  //     let spanBeforeDiff
+  //     if (span) {
+  //       const spanIndex = block.children.findIndex(child => child._key === span._key)
+  //       const spanBefore = block.children[spanIndex - 1]
+  //       if (spanBefore) {
+  //         // Remove the text diff segment that is removed from the spanBefore but exists on the current span
+  //         const spanDiff = childrenDiff.items.find(i => i.diff.toValue === spanBefore)?.diff
+  //         // eslint-disable-next-line max-depth
+  //         if (spanDiff) {
+  //           const textDiff =
+  //             spanDiff.type === 'object' &&
+  //             !!spanDiff.fields.text &&
+  //             spanDiff.fields.text.type === 'string' &&
+  //             spanDiff.fields.text
+  //           // eslint-disable-next-line max-depth
+  //           if (textDiff && textDiff.segments[textDiff.segments.length - 1].action === 'removed') {
+  //             spanBeforeDiff = textDiff
+  //             textDiff.segments = textDiff.segments.slice(0, textDiff.segments.length - 1)
+  //           }
+  //         }
+  //       } else if (
+  //         item.diff.type === 'object' &&
+  //         item.diff.fields.text &&
+  //         item.diff.fields.text.type === 'string'
+  //       ) {
+  //         item.diff.fields.text.segments = item.diff.fields.text.segments.filter(
+  //           segment => segment.action !== 'removed'
+  //         )
+  //       }
+  //       const spanAfter = block.children[spanIndex + 1]
+  //       if (spanAfter) {
+  //         // Remove the text diff segment that is removed from the spanBefore but exists on the current span
+  //         const spanDiff = childrenDiff.items.find(i => i.diff.toValue === spanAfter)?.diff
+  //         // eslint-disable-next-line max-depth
+  //         if (spanDiff) {
+  //           const textDiff =
+  //             spanDiff.type === 'object' &&
+  //             !!spanDiff.fields.text &&
+  //             spanDiff.fields.text.type === 'string' &&
+  //             spanDiff.fields.text
+  //           // eslint-disable-next-line max-depth
+  //           if (textDiff && textDiff.segments[textDiff.segments.length - 1].action === 'added') {
+  //             // eslint-disable-next-line max-depth
+  //             if (
+  //               spanBeforeDiff &&
+  //               textDiff.segments.length === 1 &&
+  //               textDiff.segments[0].action === 'added' &&
+  //               spanBeforeDiff.fromValue.indexOf(
+  //                 textDiff.segments[textDiff.segments.length - 1].text
+  //               ) > -1 &&
+  //               spanBeforeDiff.fromValue.substring(
+  //                 spanBeforeDiff.fromValue.indexOf(
+  //                   textDiff.segments[textDiff.segments.length - 1].text
+  //                 )
+  //               ) === textDiff.segments[textDiff.segments.length - 1].text
+  //             ) {
+  //               textDiff.segments[textDiff.segments.length - 1].action = 'unchanged'
+  //             } else {
+  //               // Diff what's changed here!
+  //               console.log('diffing what is changed in the spanAfter', textDiff.segments)
+  //               console.log('spanBeforeDiff', spanBeforeDiff)
+  //               const blockFromValueText = blockToText(_diff.fromValue as PortableTextBlock)
+  //               const blockToValueText = blockToText(_diff.toValue as PortableTextBlock)
+  //               console.log('Value before:', blockFromValueText)
+  //               console.log('Value now:', blockToValueText)
+  //               // textDiff.segments = textDiff.segments.filter(seg => {
+  //               //   seg.text
+  //               // })
+  //             }
+  //           }
+  //         }
+  //       }
+  //     }
+  //   })
+  // }
+  return _diff as PortableTextDiff
 }
