1ba973271b740ab22834954c701ca44a93b7dc83
Espen Hovlandsdal <espen@hovlandsdal.com>
2020-09-14 15:35:24 +0200

    [field] Resolve PT diff component through default components list

--- packages/@sanity/field/src/diff/resolve/diffResolver.ts
+++ packages/@sanity/field/src/diff/resolve/diffResolver.ts
@@ -1,7 +1,6 @@
 import {DatetimeFieldDiff} from '../../types/datetime/diff'
 import {UrlFieldDiff} from '../../types/url/diff'
 import {SlugFieldDiff} from '../../types/slug/diff'
-import {isPTSchemaType, PTDiff} from '../../types/portableText/diff'
 import {DiffComponentResolver} from '../../types'
 
 export const diffResolver: DiffComponentResolver = function diffResolver({schemaType}) {
@@ -16,10 +15,6 @@ export const diffResolver: DiffComponentResolver = function diffResolver({schema
 
   if (schemaType.name === 'slug') {
     return SlugFieldDiff
-  }
-
-  if (isPTSchemaType(schemaType)) {
-    return PTDiff
   }
 
   return undefined
