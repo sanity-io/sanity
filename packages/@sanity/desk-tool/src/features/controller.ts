47c3bd474fbb594786b40443e9627b2fa12e6914
Marius Lundgård <studio@mariuslundgard.com>
2020-09-15 07:30:12 +0200

    [desk-tool] Improve dialogs on narrow screens

--- packages/@sanity/desk-tool/src/features/controller.ts
+++ packages/@sanity/desk-tool/src/features/controller.ts
@@ -13,13 +13,21 @@ export function createDeskToolFeaturesController() {
   // determine if "reviewChanges" are available
   const reviewChanges$ = isNarrowScreen$.pipe(map(val => !val))
 
+  // determine if "splitPanes" are available
+  const splitPanes$ = isNarrowScreen$.pipe(map(val => !val))
+
   // determine if "splitViews" are available
   const splitViews$ = isNarrowScreen$.pipe(map(val => !val))
 
   // combine streams of features flags
-  const state$: Observable<DeskToolFeatures> = combineLatest([reviewChanges$, splitViews$]).pipe(
-    map(([reviewChanges, splitViews]) => ({
+  const state$: Observable<DeskToolFeatures> = combineLatest([
+    reviewChanges$,
+    splitPanes$,
+    splitViews$
+  ]).pipe(
+    map(([reviewChanges, splitPanes, splitViews]) => ({
       reviewChanges,
+      splitPanes,
       splitViews
     }))
   )
--- packages/@sanity/desk-tool/src/features/provider.tsx
+++ packages/@sanity/desk-tool/src/features/provider.tsx
@@ -1,11 +1,11 @@
 import React, {useEffect, useMemo, useState} from 'react'
 import {DeskToolFeaturesContext} from './context'
 import {createDeskToolFeaturesController} from './controller'
-import {DeskToolFeatures} from './types'
+import {DEFAULT_STATE} from './constants'
 
 export function DeskToolFeaturesProvider({children}: {children: React.ReactNode}) {
   const controller = useMemo(() => createDeskToolFeaturesController(), [])
-  const [state, setState] = useState<DeskToolFeatures>({reviewChanges: false, splitViews: false})
+  const [state, setState] = useState(DEFAULT_STATE)
 
   useEffect(() => {
     const sub = controller.state$.subscribe(setState)
