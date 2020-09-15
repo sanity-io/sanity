f30c8f7bdf81a5f49e6e616bd2a241a213510b37
Marius Lundgård <studio@mariuslundgard.com>
2020-09-15 09:32:39 +0200

    [design-studio] Update JSON preview

--- packages/design-studio/documentViews/jsonPreview.tsx
+++ packages/design-studio/documentViews/jsonPreview.tsx
@@ -1,8 +1,5 @@
-import {useEditState} from '@sanity/react-hooks'
 import React from 'react'
 
 export function JSONPreviewDocumentView(props: any) {
-  const editState: any = useEditState(props.documentId, props.schemaType.name)
-
-  return <pre>{JSON.stringify(editState.draft || editState.published, null, 2)}</pre>
+  return <pre>{JSON.stringify(props.document, null, 2)}</pre>
 }
