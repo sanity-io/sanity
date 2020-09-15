4ef78cc09de5c5cf128f73b82fe77be7fac79bfa
vicbergquist <victoria.bergquist@gmail.com>
2020-09-15 12:06:37 +0200

    wip reference pickerr fixes

--- packages/@sanity/components/src/selects/StatelessSearchableSelect.tsx
+++ packages/@sanity/components/src/selects/StatelessSearchableSelect.tsx
@@ -1,18 +1,15 @@
 /* eslint-disable complexity, max-depth */
 
-import React from 'react'
+import React, {useState, useRef} from 'react'
 import styles from 'part:@sanity/components/selects/searchable-style'
 import FaAngleDown from 'part:@sanity/base/angle-down-icon'
 import DefaultTextInput from 'part:@sanity/components/textinputs/default'
 import Spinner from 'part:@sanity/components/loading/spinner'
 import CloseIcon from 'part:@sanity/base/close-icon'
-import {Manager, Reference, Popper} from 'react-popper'
-import {get} from 'lodash'
-import Stacked from '../utilities/Stacked'
 import CaptureOutsideClicks from '../utilities/CaptureOutsideClicks'
 import Escapable from '../utilities/Escapable'
-import {Portal} from '../utilities/Portal'
 import SelectMenu from './SelectMenu'
+import {usePopper} from 'react-popper'
 
 // interface Item {}
 
@@ -44,94 +41,98 @@ interface StatelessSearchableSelectProps {
   inputId?: string
 }
 
-export default class StatelessSearchableSelect extends React.PureComponent<
-  StatelessSearchableSelectProps & React.HTMLProps<HTMLInputElement>
-> {
-  _input: DefaultTextInput | null = null
+const StatelessSearchableSelect = React.forwardRef(
+  (props: StatelessSearchableSelectProps & React.HTMLProps<HTMLInputElement>, ref) => {
+    const [referenceElement, setReferenceElement] = useState<HTMLElement | null>(null)
+    const [popperElement, setPopperElement] = useState<HTMLElement | null>(null)
+    const popper = usePopper(referenceElement, popperElement, {
+      placement: 'bottom-start',
+      modifiers: [
+        {
+          name: 'preventOverflow',
+          options: {
+            altAxis: true,
+            boundary: undefined,
+            padding: 8
+          }
+        }
+      ]
+    })
 
-  handleSelect = (item: Item) => {
-    if (this.props.onChange) this.props.onChange(item)
-  }
-
-  handleArrowClick = (event?: React.MouseEvent<HTMLDivElement>) => {
-    const {isOpen, onOpen} = this.props
-    if (isOpen) {
-      this.handleClose()
-    } else if (onOpen) onOpen()
-  }
-
-  handleArrowKeyPress = (event: React.KeyboardEvent<HTMLDivElement>) => {
-    if (event.key === 'Enter') {
-      this.handleArrowClick()
-    }
-  }
-
-  handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    if (this.props.onInputChange) this.props.onInputChange(event.target.value)
-  }
-
-  handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
-    const {items, highlightIndex = -1, onHighlightIndexChange, isOpen, onOpen} = this.props
-
-    if (event.key === 'ArrowDown' && !isOpen) {
-      if (onOpen) onOpen()
+    const handleSelect = (item: Item) => {
+      if (props.onChange) props.onChange(item)
     }
 
-    if (!items || items.length === 0) {
-      return
+    const handleArrowClick = (event?: React.MouseEvent<HTMLDivElement>) => {
+      const {isOpen, onOpen} = props
+      if (isOpen) {
+        handleClose()
+      } else if (onOpen) onOpen()
     }
 
-    const lastIndex = items.length - 1
-    if (event.key === 'ArrowUp') {
-      event.preventDefault()
-      const nextIndex = highlightIndex - 1
-      if (onHighlightIndexChange) {
-        onHighlightIndexChange(nextIndex < 0 ? lastIndex : nextIndex)
+    const handleArrowKeyPress = (event: React.KeyboardEvent<HTMLDivElement>) => {
+      if (event.key === 'Enter') {
+        handleArrowClick()
       }
     }
-    if (event.key === 'ArrowDown') {
-      event.preventDefault()
-      if (!isOpen) {
+
+    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+      if (props.onInputChange) props.onInputChange(event.target.value)
+    }
+
+    const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+      const {items, highlightIndex = -1, onHighlightIndexChange, isOpen, onOpen} = props
+
+      if (event.key === 'ArrowDown' && !isOpen) {
         if (onOpen) onOpen()
       }
-      const nextIndex = highlightIndex + 1
-      if (onHighlightIndexChange) {
-        onHighlightIndexChange(nextIndex > lastIndex ? 0 : nextIndex)
+
+      if (!items || items.length === 0) {
+        return
+      }
+
+      const lastIndex = items.length - 1
+      if (event.key === 'ArrowUp') {
+        event.preventDefault()
+        const nextIndex = highlightIndex - 1
+        if (onHighlightIndexChange) {
+          onHighlightIndexChange(nextIndex < 0 ? lastIndex : nextIndex)
+        }
+      }
+      if (event.key === 'ArrowDown') {
+        event.preventDefault()
+        if (!isOpen) {
+          if (onOpen) onOpen()
+        }
+        const nextIndex = highlightIndex + 1
+        if (onHighlightIndexChange) {
+          onHighlightIndexChange(nextIndex > lastIndex ? 0 : nextIndex)
+        }
       }
     }
-  }
 
-  handleKeyUp = (event: React.KeyboardEvent<HTMLInputElement>) => {
-    const {items = [], onChange, highlightIndex = -1} = this.props
-    if (event.key === 'Enter' && highlightIndex > -1 && items[highlightIndex]) {
-      if (onChange) onChange(items[highlightIndex])
+    const handleKeyUp = (event: React.KeyboardEvent<HTMLInputElement>) => {
+      const {items = [], onChange, highlightIndex = -1} = props
+      if (event.key === 'Enter' && highlightIndex > -1 && items[highlightIndex]) {
+        if (onChange) onChange(items[highlightIndex])
+      }
     }
-  }
 
-  handleClose = (event?: MouseEvent) => {
-    if (this.props.onClose) this.props.onClose(event)
-  }
+    const handleClose = (event?: MouseEvent) => {
+      if (props.onClose) props.onClose(event)
+    }
 
-  setInput = (ref: DefaultTextInput | null) => {
-    this._input = ref
-  }
+    const renderItem = (item: Item) => {
+      return <div className={styles.item}>{props.renderItem(item)}</div>
+    }
 
-  focus() {
-    if (this._input) this._input.focus()
-  }
-
-  renderItem = (item: Item) => {
-    return <div className={styles.item}>{this.props.renderItem(item)}</div>
-  }
-
-  render() {
     const {
       onClear,
       placeholder,
       isLoading,
       value,
       items = [],
-      renderItem,
+      // renderItem,
       isOpen,
       highlightIndex,
       isInputSelected,
@@ -146,133 +147,100 @@ export default class StatelessSearchableSelect extends React.PureComponent<
       openItemElement,
       readOnly,
       ...rest
-    } = this.props
+    } = props
+
+    console.log('popper', popper)
 
     return (
-      <Manager>
-        <Reference>
-          {({ref}) => (
-            <div
-              ref={ref}
-              className={disabled ? styles.selectContainerDisabled : styles.selectContainer}
-            >
-              <DefaultTextInput
-                {...rest}
-                className={styles.select}
-                placeholder={placeholder}
-                onChange={this.handleInputChange}
-                onKeyDown={this.handleKeyDown}
-                onKeyUp={this.handleKeyUp}
-                value={inputValue || ''}
-                selected={isInputSelected}
-                disabled={disabled}
-                ref={this.setInput as any}
-                spellCheck="false"
-                readOnly={readOnly}
-              />
-              <div className={styles.functions}>
-                {openItemElement && value && (
-                  <span className={styles.openItem}>{openItemElement(value)}</span>
+      <div className={styles.root}>
+        <div
+          ref={setReferenceElement}
+          className={disabled ? styles.selectContainerDisabled : styles.selectContainer}
+        >
+          <DefaultTextInput
+            {...rest}
+            className={styles.select}
+            placeholder={placeholder}
+            onChange={handleInputChange}
+            onKeyDown={handleKeyDown}
+            onKeyUp={handleKeyUp}
+            value={inputValue || ''}
+            selected={isInputSelected}
+            disabled={disabled}
+            ref={ref as any}
+            spellCheck="false"
+            readOnly={readOnly}
+          />
+          <div className={styles.functions}>
+            {openItemElement && value && (
+              <span className={styles.openItem}>{openItemElement(value)}</span>
+            )}
+            {!readOnly && onClear && value && (
+              <button type="button" className={styles.clearButton} onClick={onClear}>
+                <CloseIcon />
+              </button>
+            )}
+            {!readOnly && (
+              <div className={styles.arrowAndSpinnerContainer}>
+                {!isLoading && (
+                  <div
+                    className={styles.arrow}
+                    onClick={disabled ? undefined : handleArrowClick}
+                    tabIndex={0}
+                    onKeyPress={disabled ? undefined : handleArrowKeyPress}
+                  >
+                    <FaAngleDown />
+                  </div>
                 )}
-                {!readOnly && onClear && value && (
-                  <button type="button" className={styles.clearButton} onClick={onClear}>
-                    <CloseIcon />
-                  </button>
-                )}
-                {!readOnly && (
-                  <div className={styles.arrowAndSpinnerContainer}>
-                    {!isLoading && (
-                      <div
-                        className={styles.arrow}
-                        onClick={disabled ? undefined : this.handleArrowClick}
-                        tabIndex={0}
-                        onKeyPress={disabled ? undefined : this.handleArrowKeyPress}
-                      >
-                        <FaAngleDown />
-                      </div>
-                    )}
-                    {isLoading && (
-                      <div className={styles.spinner}>
-                        <Spinner />
-                      </div>
-                    )}
+                {isLoading && (
+                  <div className={styles.spinner}>
+                    <Spinner />
                   </div>
                 )}
               </div>
-            </div>
-          )}
-        </Reference>
+            )}
+          </div>
+        </div>
         {isOpen && (
-          <Stacked>
-            {isActive => (
-              <Portal>
-                <Popper
-                  placement="bottom"
-                  modifiers={
-                    {
-                      preventOverflow: {
-                        boundariesElement: 'viewport'
-                      },
-                      customStyle: {
-                        enabled: true,
-                        fn: data => {
-                          const width = get(data, 'instance.reference.clientWidth') || 500
-                          data.styles = {
-                            ...data.styles,
-                            width: width
-                          }
-                          return data
-                        }
-                      }
-                    } as any
+          <div
+            ref={setPopperElement}
+            className={styles.popper}
+            style={popper.styles.popper}
+            {...popper.attributes.popper}
+          >
+            <CaptureOutsideClicks
+              onClickOutside={/* isActive && */ isOpen ? handleClose : undefined}
+            >
+              <div
+                className={items.length === 0 ? styles.listContainerNoResult : styles.listContainer}
+              >
+                <Escapable onEscape={event => /* isActive ||  */ event.shiftKey && handleClose()} />
+                <div
+                  className={
+                    items.length === 0 && !isLoading
+                      ? styles.noResultText
+                      : styles.noResultTextHidden
                   }
                 >
-                  {({ref, style, placement, arrowProps}) => (
-                    <div
-                      ref={ref}
-                      data-placement={placement}
-                      style={style}
-                      className={styles.popper}
-                    >
-                      <CaptureOutsideClicks
-                        onClickOutside={isActive && isOpen ? this.handleClose : undefined}
-                      >
-                        <div
-                          className={
-                            items.length === 0 ? styles.listContainerNoResult : styles.listContainer
-                          }
-                        >
-                          <Escapable
-                            onEscape={event => (isActive || event.shiftKey) && this.handleClose()}
-                          />
-                          <div
-                            className={
-                              items.length === 0 && !isLoading
-                                ? styles.noResultText
-                                : styles.noResultTextHidden
-                            }
-                          >
-                            No results
-                          </div>
-                          {items.length > 0 && (
-                            <SelectMenu
-                              items={items}
-                              value={value}
-                              onSelect={this.handleSelect}
-                              renderItem={this.renderItem}
-                              highlightIndex={highlightIndex}
-                            />
-                          )}
-                        </div>
-                      </CaptureOutsideClicks>
-                    </div>
-                  )}
-                </Popper>
-              </Portal>
-            )}
-          </Stacked>
+                  No results
+                </div>
+                {items.length > 0 && (
+                  <SelectMenu
+                    items={items}
+                    value={value}
+                    onSelect={handleSelect}
+                    renderItem={renderItem}
+                    highlightIndex={highlightIndex}
+                  />
+                )}
+              </div>
+            </CaptureOutsideClicks>
+          </div>
         )}
-      </Manager>
+      </div>
     )
   }
-}
+)
+
+StatelessSearchableSelect.displayName = 'StatelessSearchableSelect'
+export default StatelessSearchableSelect
