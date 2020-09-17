2b89e7215ca335dfc376b5bc0267174d37916fa1
Marius Lundgård <studio@mariuslundgard.com>
2020-09-17 21:24:13 +0200

    [desk-tool] Style the "No changes" state

--- packages/@sanity/desk-tool/src/panes/documentPane/changesPanel/content/empty.tsx
+++ packages/@sanity/desk-tool/src/panes/documentPane/changesPanel/content/empty.tsx
@@ -4,15 +4,17 @@ import styles from './empty.css'
 
 export function EmptyContent() {
   return (
-    <>
-      <h3 className={styles.heading}>There are no changes to this document</h3>
+    <div className={styles.root}>
+      <div className={styles.box}>
+        <h3 className={styles.heading}>There are no changes to this document</h3>
 
-      <div className={styles.content}>
-        <p>
-          Either edit the document or change the selection in the timeline menu to see the changes
-          appear in this panel.
-        </p>
+        <div className={styles.content}>
+          <p>
+            Either edit the document or change the selection in the timeline menu to see the changes
+            appear in this panel.
+          </p>
+        </div>
       </div>
-    </>
+    </div>
   )
 }
