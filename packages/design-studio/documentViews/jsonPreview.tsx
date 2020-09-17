41e65dc712cd659379f77b597aeb038f25d1b747
Marius Lundgård <studio@mariuslundgard.com>
2020-09-17 21:00:05 +0200

    [design-studio] Add JSON preview styling

--- packages/design-studio/documentViews/jsonPreview.tsx
+++ packages/design-studio/documentViews/jsonPreview.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import styles from './jsonPreview.css'
+
 export function JSONPreviewDocumentView(props: any) {
-  return <pre>{JSON.stringify(props.document, null, 2)}</pre>
+  return <pre className={styles.root}>{JSON.stringify(props.document.displayed, null, 2)}</pre>
 }
