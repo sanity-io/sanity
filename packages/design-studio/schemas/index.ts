f7eb257f1862bb1c2bc02b9ee96ee15143e2ea28
Marius Lundgård <studio@mariuslundgard.com>
2020-09-17 18:41:26 +0200

    [design-studio] Add PT document type

--- packages/design-studio/schemas/index.ts
+++ packages/design-studio/schemas/index.ts
@@ -5,8 +5,9 @@ import createSchema from 'part:@sanity/base/schema-creator'
 import allInputs from './allInputs'
 import author from './author'
 import live from './live'
+import pt from './pt'
 
 export default createSchema({
   name: 'design-studio',
-  types: schemaTypes.concat([allInputs, author, live])
+  types: schemaTypes.concat([allInputs, author, live, pt])
 })
