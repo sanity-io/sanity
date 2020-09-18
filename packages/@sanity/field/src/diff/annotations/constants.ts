a1617991b4afc46a459d2be896b86f5d5b92f746
Marius Lundgård <studio@mariuslundgard.com>
2020-09-18 13:09:04 +0200

    [field] Update user color application

--- packages/@sanity/field/src/diff/annotations/constants.ts
+++
@@ -1,8 +0,0 @@
-import {color} from '@sanity/color'
-import {UserColor} from '@sanity/base/user-color'
-
-export const FALLBACK_DIFF_COLOR: UserColor = {
-  background: color.gray[100].hex,
-  border: color.gray[300].hex,
-  text: color.gray[700].hex
-}
