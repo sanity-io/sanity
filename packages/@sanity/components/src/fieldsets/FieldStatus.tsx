cada735df7fa30fd7833fff8cdea658de6f71132
Marius Lundgård <studio@mariuslundgard.com>
2020-09-18 17:20:55 +0200

    [components] Various UI tweaks

--- packages/@sanity/components/src/fieldsets/FieldStatus.tsx
+++ packages/@sanity/components/src/fieldsets/FieldStatus.tsx
@@ -4,12 +4,13 @@ import styles from './FieldStatus.css'
 
 interface FieldStatusProps {
   children?: React.ReactNode
+  maxAvatars?: number
   position?: 'top' | 'bottom'
 }
 
-export default function FieldStatus({position = 'bottom', children}: FieldStatusProps) {
+export default function FieldStatus({children, maxAvatars, position = 'bottom'}: FieldStatusProps) {
   return (
-    <div className={styles.root} data-position={position}>
+    <div className={styles.root} data-max-avatars={maxAvatars} data-position={position}>
       {children}
     </div>
   )
