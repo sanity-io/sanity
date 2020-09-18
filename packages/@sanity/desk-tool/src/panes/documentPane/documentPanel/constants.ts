f9847f04b43b9c46d6df4365cb33fc2f4d84443a
Marius Lundgård <studio@mariuslundgard.com>
2020-09-18 17:21:46 +0200

    [desk-tool] Adjust presence margins

--- packages/@sanity/desk-tool/src/panes/documentPane/documentPanel/constants.ts
+++ packages/@sanity/desk-tool/src/panes/documentPane/documentPanel/constants.ts
@@ -3,4 +3,4 @@ type Margins = [number, number, number, number]
 export const DEFAULT_MARGINS: Margins = [0, 0, 1, 0]
 
 export const MARGINS_NARROW_SCREEN_WITH_TABS: Margins = [74, 0, 64, 0]
-export const MARGINS_NARROW_SCREEN_WITHOUT_TABS: Margins = [53, 0, 64, 0]
+export const MARGINS_NARROW_SCREEN_WITHOUT_TABS: Margins = [49, 0, 64, 0]
