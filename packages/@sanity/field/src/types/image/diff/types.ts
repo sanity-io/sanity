01510ed4583ce5ff54da32461211875ecf4d4e91
Espen Hovlandsdal <espen@hovlandsdal.com>
2020-09-18 17:15:45 +0200

    [field] Provide loading and error states for image diff

--- packages/@sanity/field/src/types/image/diff/types.ts
+++ packages/@sanity/field/src/types/image/diff/types.ts
@@ -1,45 +1,19 @@
+import {Image, ImageHotspot, ImageCrop, ImageDimensions} from '@sanity/types'
 import {ObjectDiff} from '../../../diff'
 
-export interface Hotspot {
-  height: number
-  width: number
-  x: number
-  y: number
-}
-
-export interface Crop {
-  bottom: number
-  left: number
-  right: number
-  top: number
-}
-
-export interface Image {
-  [key: string]: any
-  asset?: {_ref: string; _weak?: boolean}
-  crop?: Crop
-  hotspot?: Hotspot
-}
-
-export interface SanityImageAsset {
-  _id: string
-  url: string
-  path: string
-  originalFilename?: string
-  metadata: {
-    dimensions: {
-      width: number
-      height: number
-      aspectRatio: number
-    }
-  }
-}
-
 export interface ImagePreviewProps {
+  id: string
   diff: ObjectDiff<Image>
-  asset: SanityImageAsset
-  hotspot?: Hotspot
-  crop?: Crop
+  hotspot?: ImageHotspot
+  crop?: ImageCrop
   is: 'from' | 'to'
   action?: 'changed' | 'added' | 'removed'
+}
+
+export interface MinimalAsset {
+  url: string
+  originalFilename: string
+  metadata: {
+    dimensions: ImageDimensions
+  }
 }
