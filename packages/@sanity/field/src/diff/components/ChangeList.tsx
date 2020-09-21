92eec3ba72257d77546fef15ee6a39d0830f63fc
Espen Hovlandsdal <espen@hovlandsdal.com>
2020-09-21 16:51:35 +0200

    [field] Optimize ChangeList change computation

--- packages/@sanity/field/src/diff/components/ChangeList.tsx
+++ packages/@sanity/field/src/diff/components/ChangeList.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react'
-import {Path} from '@sanity/types'
 import {useDocumentOperation} from '@sanity/react-hooks'
 import UndoIcon from 'part:@sanity/base/undo-icon'
 import Button from 'part:@sanity/components/buttons/default'
@@ -9,6 +8,7 @@ import {buildObjectChangeList} from '../changes/buildChangeList'
 import {undoChange} from '../changes/undoChange'
 import {ChangeResolver} from './ChangeResolver'
 import {DocumentChangeContext} from './DocumentChangeContext'
+import styles from './ChangeList.css'
 
 interface Props {
   schemaType: ObjectSchemaType
@@ -20,37 +20,38 @@ export function ChangeList({diff, fields, schemaType}: Props): React.ReactElemen
   const {documentId} = React.useContext(DocumentChangeContext)
   const docOperations = useDocumentOperation(documentId, schemaType.name) as OperationsAPI
   const {path} = React.useContext(DiffContext)
-  const allChanges = buildObjectChangeList(schemaType, diff, path, [])
-
-  const handleRevertAllChanges = React.useCallback(
-    () => undoChange(allChanges[0], diff, docOperations),
-    [allChanges[0], diff, docOperations]
-  )
 
   if (schemaType.jsonType !== 'object') {
     throw new Error(`Only object schema types are allowed in ChangeList`)
   }
 
-  const changes = React.useMemo(() => getFlatChangeList(schemaType, diff, path, fields), [
+  const allChanges = React.useMemo(() => buildObjectChangeList(schemaType, diff, path, fields), [
     schemaType,
     fields,
     path,
     diff
   ])
 
+  const changes = fields && fields.length === 0 ? [] : maybeFlatten(allChanges)
+
+  const rootChange = allChanges[0]
+  const handleRevertAllChanges = React.useCallback(
+    () => undoChange(rootChange, diff, docOperations),
+    [rootChange, diff, docOperations]
+  )
+
   if (changes.length === 0) {
     return null
   }
-  // @todo move?
-  const revertAllChangesContainerStyle = {display: 'grid', marginTop: '1rem'}
 
   return (
     <>
       {changes.map(change => (
         <ChangeResolver change={change} key={change.key} />
       ))}
-      {changes.length > 1 && (
-        <div style={revertAllChangesContainerStyle}>
+
+      {path.length === 0 && changes.length > 1 && (
+        <div className={styles.revertAllContainer}>
           <Button icon={UndoIcon} kind="secondary" onClick={handleRevertAllChanges}>
             Revert all changes
           </Button>
@@ -58,19 +59,6 @@ export function ChangeList({diff, fields, schemaType}: Props): React.ReactElemen
       )}
     </>
   )
-}
-
-function getFlatChangeList(
-  schemaType: ObjectSchemaType,
-  diff: ObjectDiff,
-  path: Path,
-  fields?: string[]
-) {
-  if (fields && fields.length === 0) {
-    return []
-  }
-
-  return maybeFlatten(buildObjectChangeList(schemaType, diff, path, [], {fieldFilter: fields}))
 }
 
 function maybeFlatten(changes: ChangeNode[]) {
