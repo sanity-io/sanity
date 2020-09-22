be7549be65c573243275e84d48f91b60503f58e5
Marius Lundgård <studio@mariuslundgard.com>
2020-09-22 14:30:45 +0200

    [field] Update diff layout

--- packages/@sanity/field/src/types/slug/diff/SlugFieldDiff.tsx
+++ packages/@sanity/field/src/types/slug/diff/SlugFieldDiff.tsx
@@ -2,12 +2,20 @@ import React from 'react'
 import {DiffComponent, DiffFromTo, ObjectDiff} from '../../../diff'
 import {SlugPreview} from '../preview'
 
+import styles from './SlugFieldDiff.css'
+
 interface Slug {
   current?: string
 }
 
 export const SlugFieldDiff: DiffComponent<ObjectDiff<Slug>> = ({diff, schemaType}) => {
   return (
-    <DiffFromTo layout="grid" diff={diff} schemaType={schemaType} previewComponent={SlugPreview} />
+    <DiffFromTo
+      cardClassName={styles.card}
+      layout="inline"
+      diff={diff}
+      schemaType={schemaType}
+      previewComponent={SlugPreview}
+    />
   )
 }
--- packages/@sanity/field/src/types/slug/preview/SlugPreview.tsx
+++ packages/@sanity/field/src/types/slug/preview/SlugPreview.tsx
@@ -2,10 +2,8 @@ import React from 'react'
 import {PreviewComponent} from '../../../preview/types'
 import {Slug} from '../types'
 
-import styles from './SlugPreview.css'
-
 export const SlugPreview: PreviewComponent<Slug> = props => {
   const {value} = props
 
-  return <span className={styles.root}>{value.current}</span>
+  return <>{value.current}</>
 }
