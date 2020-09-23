4c602ce5e28f27c1e8a2a46bf9b948359d71992b
Espen Hovlandsdal <espen@hovlandsdal.com>
2020-09-23 17:43:51 +0200

    [base] Add ability to use experimental API version (localStorage flag)

--- packages/@sanity/base/src/client/index.ts
+++ packages/@sanity/base/src/client/index.ts
@@ -1,6 +1,6 @@
 import config from 'config:sanity'
 import configureClient from 'part:@sanity/base/configure-client?'
-import sanityClient from '@sanity/client'
+import sanityClient, {SanityClient} from '@sanity/client'
 
 const deprecationMessage = `[deprecation] The Sanity client is now exposed in CommonJS format.
 
@@ -15,7 +15,29 @@ const fallbackConfig = {projectId: 'UNSPECIFIED', dataset: 'UNSPECIFIED'}
 const apiConfig = {...fallbackConfig, ...config.api, withCredentials: true, useCdn: false}
 const client = sanityClient(apiConfig)
 
-const configuredClient = configureClient ? configureClient(sanityClient(apiConfig)) : client
+const configuredClient = experimental(
+  configureClient ? configureClient(sanityClient(apiConfig)) : client
+)
+
+function experimental(original: SanityClient): SanityClient {
+  let useExperimental = false
+  try {
+    useExperimental = Boolean(window.localStorage.vx)
+  } catch (err) {
+    // nah
+  }
+
+  if (!useExperimental) {
+    return original
+  }
+
+  ;[original.clientConfig as any, original.observable.clientConfig as any].forEach(cfg => {
+    cfg.url = cfg.url.replace(/\/v1$/, '/vX')
+    cfg.cdnUrl = cfg.cdnUrl.replace(/\/v1$/, '/vX')
+  })
+
+  return original
+}
 
 // Warn when people use `.default`
 Object.defineProperty(configuredClient, 'default', {
