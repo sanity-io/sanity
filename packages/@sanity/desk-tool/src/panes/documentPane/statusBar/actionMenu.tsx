b4fbf086dd4e438206fa96b830ef6d78fc35f83b
Marius Lundgård <studio@mariuslundgard.com>
2020-09-23 21:07:25 +0200

    [desk-tool] Update document actions UI

--- packages/@sanity/desk-tool/src/panes/documentPane/statusBar/actionMenu.tsx
+++ packages/@sanity/desk-tool/src/panes/documentPane/statusBar/actionMenu.tsx
@@ -1,12 +1,13 @@
 import {useId} from '@reach/auto-id'
 import Button from 'part:@sanity/components/buttons/default'
+import {Popover} from 'part:@sanity/components/popover'
 import Hotkeys from 'part:@sanity/components/typography/hotkeys'
 import ChevronDownIcon from 'part:@sanity/base/chevron-down-icon'
 import React, {createElement, useCallback, useEffect, useRef, useState} from 'react'
 import useOnClickOutside from 'use-onclickoutside'
 import {ActionStateDialog} from './actionStateDialog'
 
-import styles from './documentStatusBarActions.css'
+import styles from './actionMenu.css'
 
 function getNext<T>(array: T[], fromIndex: number, dir = 1): T {
   const next = fromIndex + dir
@@ -28,16 +29,20 @@ export function ActionMenu({actionStates, onOpen, onClose, disabled, isOpen}: Pr
   const listRef = useRef<HTMLUListElement>(null)
   const idPrefix = useId()
 
-  useOnClickOutside(clickOutsideRef, () => {
+  const handleCloseMenu = useCallback(() => {
     if (!isOpen) {
       return
     }
+
     // this is a bit hacky, but if there is a modal open, we should not close on outside clicks
     const hasOpenDialog = actionStates.some(state => state.dialog)
+
     if (!hasOpenDialog) {
       onClose()
     }
-  })
+  }, [actionStates, isOpen, onClose])
+
+  useOnClickOutside(clickOutsideRef, handleCloseMenu)
 
   const [activeAction, setActiveAction] = useState(actionStates.find(s => !s.disabled))
 
@@ -48,6 +53,7 @@ export function ActionMenu({actionStates, onOpen, onClose, disabled, isOpen}: Pr
   useEffect(() => {
     if (listRef.current) {
       const el: HTMLUListElement | null = listRef.current.querySelector('[data-has-focus]')
+
       if (el) {
         el.focus()
       }
@@ -56,76 +62,98 @@ export function ActionMenu({actionStates, onOpen, onClose, disabled, isOpen}: Pr
 
   const handleKeyDown = useCallback(
     event => {
+      if (event.key === 'Escape') {
+        handleCloseMenu()
+        return
+      }
+
       if (event.key !== 'ArrowUp' && event.key !== 'ArrowDown') {
         return
       }
+
       const dir = event.key === 'ArrowUp' ? -1 : 1
       const enabledActions = actionStates.filter(state => state === activeAction || !state.disabled)
 
       setActiveAction(getNext(enabledActions, enabledActions.indexOf(activeAction), dir))
     },
-    [activeAction, actionStates]
+    [activeAction, actionStates, handleCloseMenu]
+  )
+
+  const popoverContent = (
+    <div className={styles.popoverContent}>
+      <ul
+        aria-labelledby={`${idPrefix}-button`}
+        className={styles.menu}
+        id={`${idPrefix}-menu`}
+        role="menu"
+        ref={listRef}
+      >
+        {actionStates.map((actionState, idx) => (
+          <ActionMenuListItem
+            actionState={actionState}
+            activeAction={activeAction}
+            disabled={disabled}
+            // eslint-disable-next-line react/no-array-index-key
+            key={idx}
+          />
+        ))}
+      </ul>
+    </div>
   )
 
   return (
     <div className={styles.actionsDropDown} ref={clickOutsideRef} onKeyDown={handleKeyDown}>
-      <Button
-        aria-controls={`${idPrefix}-menu`}
-        aria-haspopup="true"
-        aria-label="Actions"
-        className={styles.actionsDropDownButton}
-        disabled={disabled}
-        icon={ChevronDownIcon}
-        id={`${idPrefix}-button`}
-        kind="secondary"
-        onClick={isOpen ? onClose : onOpen}
-      />
-
-      {isOpen && (
-        <div className={styles.menuContainer}>
-          <ul
-            aria-labelledby={`${idPrefix}-button`}
-            className={styles.menu}
-            id={`${idPrefix}-menu`}
-            role="menu"
-            ref={listRef}
-          >
-            {actionStates.map((actionState, idx) => {
-              return (
-                // eslint-disable-next-line react/no-array-index-key
-                <li key={idx} className={styles.menuItem} role="presentation">
-                  <button
-                    {...(actionState === activeAction && {'data-has-focus': true})}
-                    aria-label={actionState.label}
-                    className={styles.menuItemButton}
-                    disabled={disabled || Boolean(actionState.disabled)}
-                    title={actionState.title}
-                    onClick={actionState.onHandle}
-                    role="menuitem"
-                    tabIndex={-1}
-                    type="button"
-                  >
-                    <div tabIndex={-1}>
-                      {actionState.icon && (
-                        <span className={styles.menuItemIcon}>
-                          {createElement(actionState.icon)}
-                        </span>
-                      )}
-                      <span className={styles.menuItemLabel}>{actionState.label}</span>
-                      {actionState.shortcut && (
-                        <span className={styles.menuItemHotkeys}>
-                          <Hotkeys keys={String(actionState.shortcut).split('+')} size="small" />
-                        </span>
-                      )}
-                    </div>
-                  </button>
-                  {actionState.dialog && <ActionStateDialog dialog={actionState.dialog} />}
-                </li>
-              )
-            })}
-          </ul>
+      <Popover content={popoverContent} open={isOpen} placement="top-end">
+        <div>
+          <Button
+            aria-controls={`${idPrefix}-menu`}
+            aria-haspopup="true"
+            aria-label="Actions"
+            disabled={disabled}
+            icon={ChevronDownIcon}
+            id={`${idPrefix}-button`}
+            kind="secondary"
+            onClick={isOpen ? onClose : onOpen}
+          />
         </div>
-      )}
+      </Popover>
     </div>
+  )
+}
+
+function ActionMenuListItem({actionState, activeAction, disabled}) {
+  const [buttonElement, setButtonElement] = useState<HTMLElement | null>(null)
+
+  return (
+    <li className={styles.menuItem} role="presentation">
+      <button
+        {...(actionState === activeAction && {'data-has-focus': true})}
+        aria-label={actionState.label}
+        className={styles.menuItemButton}
+        disabled={disabled || Boolean(actionState.disabled)}
+        title={actionState.title}
+        onClick={actionState.onHandle}
+        ref={setButtonElement}
+        role="menuitem"
+        tabIndex={-1}
+        type="button"
+      >
+        <div tabIndex={-1}>
+          {actionState.icon && (
+            <span className={styles.menuItemIcon}>{createElement(actionState.icon)}</span>
+          )}
+          <span className={styles.menuItemLabel}>{actionState.label}</span>
+          {actionState.shortcut && (
+            <span className={styles.menuItemHotkeys}>
+              <Hotkeys keys={String(actionState.shortcut).split('+')} size="small" />
+            </span>
+          )}
+        </div>
+      </button>
+
+      {actionState.dialog && (
+        <ActionStateDialog dialog={actionState.dialog} referenceElement={buttonElement} />
+      )}
+    </li>
   )
 }
