f9235b9464556e64e3875def85abbb64672a0cf4
Marius Lundgård <studio@mariuslundgard.com>
2020-09-23 13:28:02 +0200

    [components] Work around tsconfig issue

--- packages/@sanity/components/src/portal/context.ts
+++ packages/@sanity/components/src/portal/context.ts
@@ -1,19 +1,8 @@
 import {createContext} from 'react'
+import {defaultContextValue} from './defaultContextValue'
 
 export interface PortalContextInterface {
   element: HTMLElement
-}
-
-let globalElement: HTMLDivElement | null = null
-
-const defaultContextValue = {
-  get element() {
-    if (globalElement) return globalElement
-    globalElement = document.createElement('div')
-    globalElement.setAttribute('data-portal', 'default')
-    document.body.appendChild(globalElement)
-    return globalElement
-  }
 }
 
 export const PortalContext = createContext<PortalContextInterface>(defaultContextValue)
