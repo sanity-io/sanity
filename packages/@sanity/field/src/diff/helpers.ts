f3f36ce85a922c14d9c8c8bb5dd86a25f5551e05
Espen Hovlandsdal <espen@hovlandsdal.com>
2020-09-23 10:57:04 +0200

    [field] Add helpers for narrowing item diff types

--- packages/@sanity/field/src/diff/helpers.ts
+++ packages/@sanity/field/src/diff/helpers.ts
@@ -1,4 +1,4 @@
-import {ChangeNode, Diff, FieldChangeNode, GroupChangeNode} from '../types'
+import {ChangeNode, Diff, FieldChangeNode, GroupChangeNode, ItemDiff} from '../types'
 
 export function getChangeVerb(diff: Diff): 'Added' | 'Removed' | 'Changed' {
   const hadPrevValue = hasValue(diff.fromValue)
@@ -23,6 +23,22 @@ export function isFieldChange(change: ChangeNode): change is FieldChangeNode {
 
 export function isGroupChange(change: ChangeNode): change is GroupChangeNode {
   return change.type === 'group'
+}
+
+export function isAddedItemDiff(
+  item: ItemDiff
+): item is ItemDiff & {diff: Diff & {action: 'added'}} {
+  return item.diff.action === 'added'
+}
+
+export function isRemovedItemDiff(
+  item: ItemDiff
+): item is ItemDiff & {diff: Diff & {action: 'removed'}} {
+  return item.diff.action === 'removed'
+}
+
+export function isUnchangedDiff(diff: Diff): diff is Diff & {action: 'unchanged'} {
+  return diff.action === 'unchanged'
 }
 
 function hasValue(value: unknown) {
