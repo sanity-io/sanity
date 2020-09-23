80b7fc44e2e3270294f8ccaa03e27897b64269ec
Espen Hovlandsdal <espen@hovlandsdal.com>
2020-09-23 10:54:11 +0200

    [field] Fix FromTo showing arrow regardless of "from" value

--- packages/@sanity/field/src/diff/components/FromTo.tsx
+++ packages/@sanity/field/src/diff/components/FromTo.tsx
@@ -4,35 +4,29 @@ import {FromToArrow} from './FromToArrow'
 
 import styles from './FromTo.css'
 
-export interface FromToProps {
+export type FromToProps = {
   align?: 'top' | 'center' | 'bottom'
   layout?: 'grid' | 'inline'
   from?: React.ReactNode
   to?: React.ReactNode
-}
+} & Omit<React.HTMLProps<HTMLDivElement>, 'children'>
 
-export const FromTo = forwardRef(
-  (
-    {
-      align = 'top',
-      className,
-      layout = 'inline',
-      from,
-      to,
-      ...restProps
-    }: FromToProps & Omit<React.HTMLProps<HTMLDivElement>, 'children'>,
-    ref
-  ) => {
+export const FromTo = forwardRef<HTMLDivElement, FromToProps>(
+  ({align = 'top', className, layout = 'inline', from, to, ...restProps}, ref) => {
     return (
       <div
         {...restProps}
         className={classNames(styles.root, className)}
         data-from-to-align={align}
         data-from-to-layout={layout}
-        ref={ref as any}
+        ref={ref}
       >
-        <div className={styles.column}>{from}</div>
-        <FromToArrow className={styles.arrow} />
+        {from && (
+          <>
+            <div className={styles.column}>{from}</div>
+            <FromToArrow className={styles.arrow} />
+          </>
+        )}
         <div className={styles.column}>{to}</div>
       </div>
     )
