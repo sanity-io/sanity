3be8404c791b923f4a973c5e2e2069fb1e48baa1
Marius Lundgård <studio@mariuslundgard.com>
2020-09-24 15:48:44 +0200

    [base] Wrap ScrollContainer in forwardRef

--- packages/@sanity/base/src/components/ScrollContainer.tsx
+++ packages/@sanity/base/src/components/ScrollContainer.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {createElement, forwardRef, useCallback, useEffect, useContext, useRef} from 'react'
 
 type ScrollEventHandler = (event: Event) => void
 interface ScrollContextValue {
@@ -18,6 +18,7 @@ export function ScrollMonitor({
   const handleScroll = React.useCallback(
     (event: Event) => {
       onScroll(event)
+
       if (parentContext.onScroll) {
         parentContext.onScroll(event)
       }
@@ -28,43 +29,60 @@ export function ScrollMonitor({
   return <Context.Provider value={{onScroll: handleScroll}}>{children}</Context.Provider>
 }
 
+interface ScrollContainerProps {
+  as?: React.ElementType | keyof JSX.IntrinsicElements
+  onScroll?: ScrollEventHandler
+}
+
 /**
  * This provides a utility function for use within Sanity Studios to create scrollable containers
  * Note: this is used by different studio utilities to track positions of elements on screen
  * Note: It will call any given `onScroll` callback with a Native DOM Event, and not a React Synthetic event
  * Note: It will not make sure the element is actually scrollable, this still needs to be done with css as usual
  */
-export function ScrollContainer<T extends React.ElementType = 'div'>({
-  onScroll,
-  as = 'div',
-  ...props
-}: React.ComponentProps<T> & {
-  onScroll?: ScrollEventHandler
-  as?: T
-}) {
-  const containerRef = React.useRef<HTMLDivElement>(null)
-  const parentContext = React.useContext(Context)
+export const ScrollContainer = forwardRef(
+  (props: ScrollContainerProps & Omit<React.HTMLProps<HTMLElement>, 'onScroll'>, ref) => {
+    const {as = 'div', onScroll, ...restProps} = props
+    const rootRef = useRef<HTMLElement | null>(null)
+    const parentContext = useContext(Context)
 
-  const handleScroll = React.useCallback(
-    (event: Event) => {
-      if (onScroll) {
-        onScroll(event)
-      }
-      if (parentContext.onScroll) {
-        parentContext.onScroll(event)
-      }
-    },
-    [onScroll]
-  )
+    const handleScroll = useCallback(
+      (event: Event) => {
+        if (onScroll) {
+          onScroll(event)
+        }
+        if (parentContext.onScroll) {
+          parentContext.onScroll(event)
+        }
+      },
+      [onScroll, parentContext]
+    )
 
-  React.useEffect(() => {
-    containerRef.current!.addEventListener('scroll', handleScroll, {
-      passive: true
-    })
-    return () => {
-      containerRef.current!.removeEventListener('scroll', handleScroll)
+    useEffect(() => {
+      const rootElement = rootRef.current
+
+      if (rootElement) {
+        rootElement.addEventListener('scroll', handleScroll, {
+          passive: true
+        })
+      }
+
+      return () => {
+        if (rootElement) {
+          rootElement.removeEventListener('scroll', handleScroll)
+        }
+      }
+    }, [handleScroll])
+
+    const setRef = (el: HTMLElement | null) => {
+      console.log('setRef', el)
+      rootRef.current = el
+      if (typeof ref === 'function') ref(el)
+      else if (ref && typeof ref === 'object') ref.current = el
     }
-  }, [onScroll])
 
-  return React.createElement(as, {ref: containerRef, ...props})
-}
+    return createElement(as, {...restProps, ref: setRef})
+  }
+)
+
+ScrollContainer.displayName = 'ScrollContainer'
