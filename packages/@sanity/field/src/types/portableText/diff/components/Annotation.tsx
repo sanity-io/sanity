e111ccc2a839bfd8287cffb3ccee75ff71e00a5a
Per-Kristian Nordnes <per.kristian.nordnes@gmail.com>
2020-09-25 00:25:28 +0200

    [field] PT: let Decorator and Annotation comp. get ready baked diffs

--- packages/@sanity/field/src/types/portableText/diff/components/Annotation.tsx
+++ packages/@sanity/field/src/types/portableText/diff/components/Annotation.tsx
@@ -2,35 +2,15 @@ import React from 'react'
 import {DiffCard, ObjectDiff} from '../../../../diff'
 import styles from './Annotation.css'
 
-export default function Annotation({
-  diff,
-  mark,
-  children
-}: {
-  diff: ObjectDiff
-  mark: string
-  children: JSX.Element
-}) {
+export default function Annotation({diff, children}: {diff?: ObjectDiff; children: JSX.Element}) {
   let returned = children
-  const annotationDiff =
-    diff.fields.markDefs &&
-    diff.fields.markDefs.isChanged &&
-    diff.fields.markDefs.type === 'array' &&
-    diff.fields.markDefs.items.find(
-      item =>
-        item.diff &&
-        item.diff.type === 'object' &&
-        item.diff.toValue &&
-        item.diff.toValue._key &&
-        item.diff.toValue._key === mark
-    )?.diff
   returned = (
     <span className={styles.root}>
-      {annotationDiff && annotationDiff.action !== 'unchanged' ? (
+      {diff && diff.action !== 'unchanged' ? (
         <DiffCard
-          annotation={annotationDiff.annotation}
+          annotation={diff.annotation}
           as="ins"
-          tooltip={{description: `Annotation ${annotationDiff.action} by`}}
+          tooltip={{description: `Annotation ${diff.action} by`}}
         >
           {returned}
         </DiffCard>
