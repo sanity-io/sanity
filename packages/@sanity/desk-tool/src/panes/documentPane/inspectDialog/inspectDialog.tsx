6cc1e3b9db19224ea4aa158e77a3630892954f6e
Marius Lundgård <studio@mariuslundgard.com>
2020-09-24 10:27:45 +0200

    [desk-tool] Improve inspect dialog

--- packages/@sanity/desk-tool/src/panes/documentPane/inspectDialog/inspectDialog.tsx
+++ packages/@sanity/desk-tool/src/panes/documentPane/inspectDialog/inspectDialog.tsx
@@ -1,92 +1,59 @@
-/* eslint-disable @typescript-eslint/explicit-function-return-type */
-/* eslint-disable @typescript-eslint/no-explicit-any */
-
-import PropTypes from 'prop-types'
-import React from 'react'
+import {SanityDocument} from '@sanity/types'
+import FullScreenDialog from 'part:@sanity/components/dialogs/fullscreen'
+import Tab from 'part:@sanity/components/tabs/tab'
+import TabList from 'part:@sanity/components/tabs/tab-list'
+import TabPanel from 'part:@sanity/components/tabs/tab-panel'
+import React, {useCallback} from 'react'
+import JSONInspector from 'react-json-inspector'
+import {withPropsStream} from 'react-props-stream'
 import {combineLatest, Observable} from 'rxjs'
 import {map} from 'rxjs/operators'
-import JSONInspector from 'react-json-inspector'
-import FullScreenDialog from 'part:@sanity/components/dialogs/fullscreen'
-import TabList from 'part:@sanity/components/tabs/tab-list'
-import Tab from 'part:@sanity/components/tabs/tab'
-import TabPanel from 'part:@sanity/components/tabs/tab-panel'
-import {isObject} from 'lodash'
-import HLRU from 'hashlru'
-import {withPropsStream} from 'react-props-stream'
-import settings from '../../../settings'
 import DocTitle from '../../../components/DocTitle'
+import settings from '../../../settings'
+import {VIEW_MODE_PARSED, VIEW_MODE_RAW, VIEW_MODES} from './constants'
+import {isExpanded, maybeSelectAll, select, toggleExpanded} from './helpers'
+import {InspectViewMode} from './types'
 
 import styles from './inspectDialog.css'
 
-const lru = HLRU(1000)
-
-function isExpanded(keyPath: any, value: any) {
-  const cached = lru.get(keyPath)
-  if (cached === undefined) {
-    lru.set(keyPath, Array.isArray(value) || isObject(value))
-    return isExpanded(keyPath, value)
-  }
-  return cached
+interface InspectDialogProps {
+  idPrefix: string
+  onClose: () => void
+  value: SanityDocument | null
 }
 
-function toggleExpanded(event: any) {
-  const {path} = event
-  const current = lru.get(path)
-  if (current === undefined) {
-    // something is wrong
-    return
-  }
-  lru.set(path, !current)
+interface InnerInspectDialogProps extends InspectDialogProps {
+  onViewModeChange: (viewMode: InspectViewMode) => void
+  viewMode: InspectViewMode
 }
-
-function selectElement(element: HTMLElement) {
-  const sel = window.getSelection()
-
-  if (sel) {
-    const range = document.createRange()
-
-    sel.removeAllRanges()
-    range.selectNodeContents(element)
-    sel.addRange(range)
-  }
-}
-
-function select(event: any) {
-  selectElement(event.currentTarget)
-}
-
-function maybeSelectAll(event: any) {
-  const selectAll = event.keyCode === 65 && (event.metaKey || event.ctrlKey)
-  if (!selectAll) {
-    return
-  }
-  event.preventDefault()
-  selectElement(event.currentTarget)
-}
-
-const VIEW_MODE_PARSED = {id: 'parsed', title: 'Parsed'}
-const VIEW_MODE_RAW = {id: 'raw', title: 'JSON'}
-const VIEW_MODES = [VIEW_MODE_PARSED, VIEW_MODE_RAW]
 
 const viewModeSettings = settings.forKey('inspect-view-preferred-view-mode')
 
-function mapReceivedPropsToChildProps(props$: Observable<any>) {
-  const onViewModeChange = (nextViewMode: any) => viewModeSettings.set(nextViewMode.id)
+function mapReceivedPropsToChildProps(
+  props$: Observable<InspectDialogProps>
+): Observable<InnerInspectDialogProps> {
+  const onViewModeChange = (nextViewMode: InspectViewMode) => viewModeSettings.set(nextViewMode.id)
 
-  const viewModeSetting$ = viewModeSettings
+  const viewModeSetting$: Observable<InspectViewMode> = viewModeSettings
     .listen('parsed')
-    .pipe(map((id: any) => VIEW_MODES.find(mode => mode.id === id)))
+    .pipe(map((id: string) => VIEW_MODES.find(mode => mode.id === id)))
 
   return combineLatest(props$, viewModeSetting$).pipe(
     map(([props, viewMode]) => ({...props, viewMode, onViewModeChange}))
   )
 }
 
-function InspectDialogComponent(props) {
+function InspectDialogComponent(props: InnerInspectDialogProps) {
   const {idPrefix, onClose, onViewModeChange, value, viewMode} = props
-
-  // @todo: prefix with pane id
   const tabIdPrefix = `${idPrefix}_inspect_`
+
+  const setParsedViewMode = useCallback(() => {
+    onViewModeChange(VIEW_MODE_PARSED)
+  }, [onViewModeChange])
+
+  const setRawViewMode = useCallback(() => {
+    onViewModeChange(VIEW_MODE_RAW)
+  }, [onViewModeChange])
 
   return (
     <FullScreenDialog
@@ -107,17 +74,15 @@ function InspectDialogComponent(props) {
               aria-controls={`${tabIdPrefix}tabpanel`}
               id={`${tabIdPrefix}tab-${VIEW_MODE_PARSED.id}`}
               isActive={viewMode === VIEW_MODE_PARSED}
-              label="Parsed"
-              // eslint-disable-next-line react/jsx-no-bind
-              onClick={() => onViewModeChange(VIEW_MODE_PARSED)}
+              label={VIEW_MODE_PARSED.title}
+              onClick={setParsedViewMode}
             />
             <Tab
               aria-controls={`${tabIdPrefix}tabpanel`}
-              id={`${tabIdPrefix}tab-${VIEW_MODE_PARSED.id}`}
+              id={`${tabIdPrefix}tab-${VIEW_MODE_RAW.id}`}
               isActive={viewMode === VIEW_MODE_RAW}
-              label="Raw JSON"
-              // eslint-disable-next-line react/jsx-no-bind
-              onClick={() => onViewModeChange(VIEW_MODE_RAW)}
+              label={VIEW_MODE_RAW.title}
+              onClick={setRawViewMode}
             />
           </TabList>
         </div>
@@ -149,21 +114,7 @@ function InspectDialogComponent(props) {
   )
 }
 
-InspectDialogComponent.propTypes = {
-  idPrefix: PropTypes.string.isRequired,
-  onClose: PropTypes.func,
-  onViewModeChange: PropTypes.func.isRequired,
-  // eslint-disable-next-line react/forbid-prop-types
-  value: PropTypes.object,
-  viewMode: PropTypes.shape({
-    id: PropTypes.string.isRequired,
-    title: PropTypes.string.isRequired
-  }).isRequired
-}
-
-InspectDialogComponent.defaultProps = {
-  onClose: undefined,
-  value: undefined
-}
-
-export const InspectDialog = withPropsStream(mapReceivedPropsToChildProps, InspectDialogComponent)
+export const InspectDialog = withPropsStream<InspectDialogProps, InnerInspectDialogProps>(
+  mapReceivedPropsToChildProps,
+  InspectDialogComponent
+)
