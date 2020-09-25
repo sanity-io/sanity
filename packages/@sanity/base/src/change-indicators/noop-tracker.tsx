53dc1073cd2f1d673c80e32d9e65f03a714eba1b
Bjørge Næss <bjoerge@gmail.com>
2020-09-25 13:19:43 +0200

    [base] Support for disabling connectors

---
+++ packages/@sanity/base/src/change-indicators/noop-tracker.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import {Reported} from './tracker'
+import {IsEqualFunction} from '../components/react-track-elements/createUseReporter'
+
+// eslint-disable-next-line @typescript-eslint/no-empty-function
+function noop() {}
+
+function NoopTracker({children}: {children: React.ReactNode}) {
+  return <>{children}</>
+}
+export function createNoopTracker<T>() {
+  return {
+    useReportedValues: noop as () => Reported<T>[],
+    Tracker: NoopTracker,
+    useReporter: noop as (id: string, value: T | (() => T), isEqual: IsEqualFunction<T>) => void
+  }
+}
