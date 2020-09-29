fe2272c8c6c34886b969d0c7e2b843d24f112bc0
Per-Kristian Nordnes <per.kristian.nordnes@gmail.com>
2020-09-29 11:22:57 +0200

    [field] PT: Render annotation objects with popover diff

--- packages/@sanity/field/src/types/portableText/diff/components/Annotation.tsx
+++ packages/@sanity/field/src/types/portableText/diff/components/Annotation.tsx
@@ -1,24 +1,87 @@
-import React from 'react'
-import {DiffCard, ObjectDiff} from '../../../../diff'
+import classNames from 'classnames'
+import ChevronDownIcon from 'part:@sanity/base/chevron-down-icon'
+import {ClickOutside} from 'part:@sanity/components/click-outside'
+import {Popover} from 'part:@sanity/components/popover'
+import React, {useCallback, useState} from 'react'
+import {ChangeList, ObjectDiff, ObjectSchemaType, useDiffAnnotationColor} from '../../../../diff'
+import {PortableTextChild} from '../types'
+import {isEmptyObject} from '../helpers'
+
 import styles from './Annotation.css'
 
-export default function Annotation({diff, children}: {diff?: ObjectDiff; children: JSX.Element}) {
-  let returned = children
-  returned = (
-    <span className={styles.root}>
-      {/* {diff && diff.action !== 'unchanged' ? (
-        <DiffCard
-          annotation={diff.annotation}
-          as="ins"
-          tooltip={{description: `Annotation ${diff.action} by`}}
-        >
-          {returned}
-        </DiffCard>
-      ) : (
-        returned
-      )} */}
-      {returned}
-    </span>
+interface AnnotationProps {
+  diff?: ObjectDiff
+  object: PortableTextChild
+  children: JSX.Element
+  schemaType?: ObjectSchemaType
+}
+
+export function Annotation({
+  diff,
+  schemaType,
+  ...restProps
+}: AnnotationProps & Omit<React.HTMLProps<HTMLSpanElement>, 'onClick'>) {
+  if (!schemaType) {
+    return (
+      <span {...restProps} className={styles.root}>
+        Unknown schema type
+      </span>
+    )
+  }
+
+  if (diff) {
+    return <AnnnotationWithDiff {...restProps} diff={diff} schemaType={schemaType} />
+  }
+  return <span {...restProps} />
+}
+
+interface AnnnotationWithDiffProps {
+  diff: ObjectDiff
+  object: PortableTextChild
+  schemaType: ObjectSchemaType
+}
+
+function AnnnotationWithDiff({
+  diff,
+  children,
+  object,
+  schemaType,
+  ...restProps
+}: AnnnotationWithDiffProps & Omit<React.HTMLProps<HTMLSpanElement>, 'onClick'>) {
+  const color = useDiffAnnotationColor(diff, [])
+  const style = color ? {background: color.background, color: color.text} : {}
+  const className = classNames(styles.root, diff.action === 'removed' && styles.removed)
+  const [open, setOpen] = useState(false)
+  const emptyObject = object && isEmptyObject(object)
+
+  const handleClick = useCallback(() => {
+    setOpen(true)
+  }, [])
+
+  const popoverContent = (
+    <div className={styles.popoverContent}>
+      {emptyObject && <span>Empty {schemaType.title}</span>}
+      {!emptyObject && <ChangeList diff={diff} schemaType={schemaType} />}
+    </div>
   )
-  return returned
+
+  const handleClickOutside = useCallback(() => {
+    setOpen(false)
+  }, [])
+  if (!diff || diff.action === 'unchanged') {
+    return <>children</>
+  }
+  return (
+    <ClickOutside onClickOutside={handleClickOutside}>
+      {ref => (
+        <span {...restProps} className={className} onClick={handleClick} ref={ref} style={style}>
+          <Popover content={popoverContent} open={open}>
+            <span className={styles.previewContainer}>
+              {children} <ChevronDownIcon />
+            </span>
+          </Popover>
+        </span>
+      )}
+    </ClickOutside>
+  )
 }
