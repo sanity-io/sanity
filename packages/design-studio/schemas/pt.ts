2c73f982318f70119c923d5d0c6e5c22da6c8236
Per-Kristian Nordnes <per.kristian.nordnes@gmail.com>
2020-09-30 02:43:14 +0200

    [design-studio] Edit pt schema

--- packages/design-studio/schemas/pt.ts
+++ packages/design-studio/schemas/pt.ts
@@ -45,7 +45,8 @@ export default {
                     validation: Rule => Rule.required()
                   }
                 ]
-              }
+              },
+              objectExample
             ]
           }
         },
