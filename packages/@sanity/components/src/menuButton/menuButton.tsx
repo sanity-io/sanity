606e288ec2d6507c6ef07ad2d418156e4885fa45
Marius Lundgård <studio@mariuslundgard.com>
2020-10-02 11:18:17 +0200

    [components] Add `portal` prop to MenuButton and improve click outside

--- packages/@sanity/components/src/menuButton/menuButton.tsx
+++ packages/@sanity/components/src/menuButton/menuButton.tsx
@@ -1,55 +1,65 @@
 import Button from 'part:@sanity/components/buttons/default'
-import {ClickOutside} from 'part:@sanity/components/click-outside'
 import {Popover} from 'part:@sanity/components/popover'
 import Escapable from 'part:@sanity/components/utilities/escapable'
-import React, {useCallback} from 'react'
+import React, {forwardRef, useCallback, useState} from 'react'
 import {ButtonProps} from '../buttons'
+import {useClickOutside} from '../hooks'
 import {Placement} from '../types'
 
 interface MenuButtonProps {
   boundaryElement?: HTMLElement | null
   buttonProps?: ButtonProps
   menu?: React.ReactNode
-  placement?: Placement
   open?: boolean
+  placement?: Placement
+  portal?: boolean
   setOpen: (val: boolean) => void
 }
 
-export function MenuButton(props: MenuButtonProps & React.HTMLProps<HTMLDivElement>) {
-  const {
-    boundaryElement,
-    buttonProps,
-    children,
-    menu,
-    open,
-    placement,
-    setOpen,
-    ...restProps
-  } = props
+export const MenuButton = forwardRef(
+  (props: MenuButtonProps & React.HTMLProps<HTMLDivElement>, ref) => {
+    const {
+      boundaryElement,
+      buttonProps,
+      children,
+      menu,
+      open,
+      placement,
+      portal,
+      setOpen,
+      ...restProps
+    } = props
 
-  const handleClose = useCallback(() => setOpen(false), [setOpen])
-  const handleButtonClick = useCallback(() => setOpen(!open), [open, setOpen])
+    const handleClose = useCallback(() => setOpen(false), [setOpen])
+    const handleButtonClick = useCallback(() => setOpen(!open), [open, setOpen])
+    const [buttonContainerElement, setButtonContainerElement] = useState<HTMLDivElement | null>(
+      null
+    )
+    const [popoverElement, setPopoverElement] = useState<HTMLDivElement | null>(null)
 
-  return (
-    <ClickOutside onClickOutside={handleClose}>
-      {ref => (
-        <div {...restProps} ref={ref}>
-          <Popover
-            boundaryElement={boundaryElement}
-            content={menu}
-            open={open}
-            placement={placement}
-          >
-            <div>
-              <Button {...buttonProps} onClick={handleButtonClick}>
-                {children}
-              </Button>
-            </div>
-          </Popover>
+    useClickOutside(handleClose, [buttonContainerElement, popoverElement])
 
-          {open && <Escapable onEscape={handleClose} />}
-        </div>
-      )}
-    </ClickOutside>
-  )
-}
+    return (
+      <div {...restProps} ref={ref as any}>
+        <Popover
+          boundaryElement={boundaryElement}
+          content={menu}
+          open={open}
+          placement={placement}
+          portal={portal}
+          ref={setPopoverElement}
+        >
+          <div ref={setButtonContainerElement}>
+            <Button {...buttonProps} onClick={handleButtonClick}>
+              {children}
+            </Button>
+          </div>
+        </Popover>
+
+        {open && <Escapable onEscape={handleClose} />}
+      </div>
+    )
+  }
+)
+
+MenuButton.displayName = 'MenuButton'
