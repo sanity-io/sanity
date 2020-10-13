ab34f4a6d3280e4844d18a588a3b8384295a2210
Marius Lundgård <studio@mariuslundgard.com>
2020-10-13 16:37:36 +0200

    [design-studio] Add schemas for debugging

--- packages/design-studio/schemas/index.ts
+++ packages/design-studio/schemas/index.ts
@@ -2,13 +2,23 @@ import schemaTypes from 'all:part:@sanity/base/schema-type'
 import createSchema from 'part:@sanity/base/schema-creator'
 
 // document types
+// NOTE: keep this alphabetized
 import allInputs from './allInputs'
 import author from './author'
+import complexArrays from './complexArrays'
 import live from './live'
 import pt from './pt'
 import settings from './settings'
 
 export default createSchema({
   name: 'design-studio',
-  types: schemaTypes.concat([allInputs, author, live, pt, settings])
+  types: schemaTypes.concat([
+    // NOTE: keep this alphabetized
+    allInputs,
+    author,
+    complexArrays,
+    live,
+    pt,
+    settings
+  ])
 })
