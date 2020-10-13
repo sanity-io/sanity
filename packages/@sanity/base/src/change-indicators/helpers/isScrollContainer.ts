a40acea910f6872727eb62f8f3489f10a3997270
Bjørge Næss <bjoerge@gmail.com>
2020-10-13 17:02:34 +0200

    [base] Use more robust way of determining whether element is scroll container

--- packages/@sanity/base/src/change-indicators/helpers/isScrollContainer.ts
+++ packages/@sanity/base/src/change-indicators/helpers/isScrollContainer.ts
@@ -1,3 +1,8 @@
-const isScrollContainer = el => el.scrollHeight !== el.offsetHeight
-
+const isScrollContainer = el => {
+  if (el.scrollHeight !== el.offsetHeight || el.scrollWidth !== el.offsetWidth) {
+    return true
+  }
+  const overflow = getComputedStyle(el).overflow
+  return overflow.includes('auto') || overflow.includes('scroll')
+}
 export default isScrollContainer
