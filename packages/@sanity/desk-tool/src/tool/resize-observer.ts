4825c6c1376f41f19b70fce8e594080217a94e33
Bjørge Næss <bjoerge@gmail.com>
2020-10-15 17:06:36 +0200

    [desk-tool] Use shared resizeObserver wrapper from base

--- packages/@sanity/desk-tool/src/tool/resize-observer.ts
+++
@@ -1,3 +0,0 @@
-// Todo: this can be safely removed at some point
-import {ResizeObserver as Polyfill} from '@juggle/resize-observer'
-export const ResizeObserver = (window as any).ResizeObserver || Polyfill
