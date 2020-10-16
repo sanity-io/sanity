f762d3c53b38c0436b5b1e2678e4a5fb42e9f2b0
Per-Kristian Nordnes <per.kristian.nordnes@gmail.com>
2020-10-16 12:40:17 +0200

    [form-builder] PTE: fix issue where edit object modal would not save pending when closed

--- packages/@sanity/form-builder/src/inputs/PortableText/Objects/EditObject.tsx
+++ packages/@sanity/form-builder/src/inputs/PortableText/Objects/EditObject.tsx
@@ -4,6 +4,7 @@ import {isKeySegment, Path, Marker} from '@sanity/types'
 import {FormFieldPresence} from '@sanity/base/presence'
 import {
   PortableTextBlock,
+  PortableTextChild,
   Type,
   PortableTextEditor,
   compactPatches,
@@ -52,66 +53,13 @@ export const EditObject = ({
 }: Props) => {
   const editor = usePortableTextEditor()
   const ptFeatures = PortableTextEditor.getPortableTextFeatures(editor)
-  const {formBuilderPath, editorPath, kind} = objectEditData
-
-  function findObjectAndType() {
-    let object
-    let type: Type
-
-    // Try finding the relevant block
-    const blockKey =
-      Array.isArray(formBuilderPath) && isKeySegment(formBuilderPath[0]) && formBuilderPath[0]._key
-
-    const block =
-      value && blockKey && Array.isArray(value) && value.find(blk => blk._key === blockKey)
-    const child =
-      block &&
-      block.children &&
-      block.children.find(cld => isKeySegment(editorPath[2]) && cld._key === editorPath[2]._key)
-
-    if (block) {
-      // Get object, type, and relevant editor element
-      switch (kind) {
-        case 'blockObject':
-          object = block
-          type = ptFeatures.types.blockObjects.find(t => t.name === block._type)
-          break
-        case 'inlineObject':
-          object = child
-          // eslint-disable-next-line max-depth
-          if (object) {
-            type = ptFeatures.types.inlineObjects.find(t => t.name === child._type)
-          }
-          break
-        case 'annotation':
-          // eslint-disable-next-line max-depth
-          if (child) {
-            const markDef =
-              child.marks &&
-              block.markDefs &&
-              block.markDefs.find(def => child.marks.includes(def._key))
-            // eslint-disable-next-line max-depth
-            if (markDef) {
-              type = ptFeatures.types.annotations.find(t => t.name === markDef._type)
-              object = markDef
-            }
-          }
-          break
-        default:
-        // Nothing
-      }
-    }
-    return [object, type]
-  }
-
-  const objectAndType = useMemo(findObjectAndType, [value])
-  const [object, setObject] = useState(objectAndType[0])
-  const type = objectAndType[1]
+  const [_object, type] = useMemo(() => findObjectAndType(objectEditData, value, ptFeatures), [
+    objectEditData,
+    ptFeatures,
+    value
+  ])
+  const [object, setObject] = useState(_object)
   const [timeoutInstance, setTimeoutInstance] = useState(undefined)
-
-  function handleClose(): void {
-    onClose()
-  }
 
   // Initialize weakmaps on mount, and send patches on unmount
   useEffect(() => {
@@ -125,8 +73,17 @@ export const EditObject = ({
   }, [])
 
   useLayoutEffect(() => {
-    setObject(objectAndType[0])
-  }, [value])
+    setObject(_object)
+  }, [_object])
+
+  if (!objectEditData) {
+    return null
+  }
+  const {formBuilderPath, kind} = objectEditData
+
+  function handleClose(): void {
+    onClose()
+  }
 
   const editModalLayout: ModalType = get(type, 'options.editModal')
 
@@ -218,4 +175,62 @@ export const EditObject = ({
       type={type}
     />
   )
+}
+
+function findObjectAndType(
+  objectEditData,
+  value,
+  ptFeatures
+): [PortableTextChild | undefined, Type | undefined] {
+  if (!objectEditData) {
+    return [undefined, undefined]
+  }
+  const {editorPath, formBuilderPath, kind} = objectEditData
+  let object: PortableTextChild
+  let type: Type
+
+  // Try finding the relevant block
+  const blockKey =
+    Array.isArray(formBuilderPath) && isKeySegment(formBuilderPath[0]) && formBuilderPath[0]._key
+
+  const block =
+    value && blockKey && Array.isArray(value) && value.find(blk => blk._key === blockKey)
+  const child =
+    block &&
+    block.children &&
+    block.children.find(cld => isKeySegment(editorPath[2]) && cld._key === editorPath[2]._key)
+
+  if (block) {
+    // Get object, type, and relevant editor element
+    switch (kind) {
+      case 'blockObject':
+        object = block
+        type = ptFeatures.types.blockObjects.find(t => t.name === block._type)
+        break
+      case 'inlineObject':
+        object = child
+        // eslint-disable-next-line max-depth
+        if (object) {
+          type = ptFeatures.types.inlineObjects.find(t => t.name === child._type)
+        }
+        break
+      case 'annotation':
+        // eslint-disable-next-line max-depth
+        if (child) {
+          const markDef =
+            child.marks &&
+            block.markDefs &&
+            block.markDefs.find(def => child.marks.includes(def._key))
+          // eslint-disable-next-line max-depth
+          if (markDef) {
+            type = ptFeatures.types.annotations.find(t => t.name === markDef._type)
+            object = markDef
+          }
+        }
+        break
+      default:
+      // Nothing
+    }
+  }
+  return [object, type]
 }
