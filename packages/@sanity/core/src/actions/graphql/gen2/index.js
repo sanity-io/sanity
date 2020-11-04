04821bfc7ab10592957545e1e132d04735d4dca0
Espen Hovlandsdal <espen@hovlandsdal.com>
2020-11-04 21:52:44 +0100

    [core] Do not include document interface as type

--- packages/@sanity/core/src/actions/graphql/gen2/index.js
+++ packages/@sanity/core/src/actions/graphql/gen2/index.js
@@ -8,7 +8,7 @@ module.exports = (extracted) => {
   const filters = generateTypeFilters(types)
   const sortings = generateTypeSortings(types)
   const queries = generateTypeQueries(types, filters, sortings)
-  const graphqlTypes = types.concat(filters).concat(sortings)
+  const graphqlTypes = extracted.types.concat(filters).concat(sortings)
 
   return {types: graphqlTypes, queries, interfaces: extracted.interfaces, generation: 'gen2'}
 }
