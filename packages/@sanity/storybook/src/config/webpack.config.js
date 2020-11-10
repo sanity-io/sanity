56a857ddb5f61853bfecbe6a56463614e2eae1ba
Marius Lundgård <studio@mariuslundgard.com>
2020-11-10 15:49:06 +0100

    [storybook] Inherit JSX/TSX loader from @sanity/server

--- packages/@sanity/storybook/src/config/webpack.config.js
+++ packages/@sanity/storybook/src/config/webpack.config.js
@@ -3,8 +3,9 @@ const sanityServer = require('@sanity/server')
 const wpIntegration = require('@sanity/webpack-integration/v3')
 const genDefaultConfig = require('@storybook/react/dist/server/config/defaults/webpack.config.js')
 
-const skipCssLoader = (rule) => !rule.test || (rule.test && !rule.test.toString().includes('.css'))
-const isCssLoader = (rule) => rule.test && rule.test.toString().includes('.css')
+const skipCssLoader = rule => !rule.test || (rule.test && !rule.test.toString().includes('.css'))
+const isCssLoader = rule => rule.test && rule.test.toString().includes('.css')
+const isJsxLoader = rule => rule.test && rule.test.toString().includes('jsx')
 
 // This is very hacky, but I couldn't figure out a way to pass config from
 // the parent task onto this configuration, which we need to infer the base
@@ -33,14 +34,14 @@ function getWebpackConfig(baseConfig, env) {
   config.module.rules = config.module.rules.filter(skipCssLoader)
   config.module.rules.unshift(sanityWpConfig.module.rules.find(isCssLoader))
 
-  const jsonLoaderAt = config.module.rules.findIndex((rule) =>
+  const jsonLoaderAt = config.module.rules.findIndex(rule =>
     (rule.loader || '').includes('json-loader')
   )
 
   const jsonHackLoader = {
     test: /\.json$/,
     resourceQuery: /sanityPart=/,
-    loader: require.resolve('./jsonHackLoader.js'),
+    loader: require.resolve('./jsonHackLoader.js')
   }
 
   if (jsonLoaderAt !== -1) {
@@ -48,12 +49,23 @@ function getWebpackConfig(baseConfig, env) {
   }
 
   config.resolve = Object.assign({}, config.resolve, sanityWpConfig.resolve, {
-    alias: Object.assign({}, config.resolve.alias || {}, sanityWpConfig.resolve.alias || {}),
+    alias: Object.assign({}, config.resolve.alias || {}, sanityWpConfig.resolve.alias || {})
   })
+
+  const storybookJsxLoader = config.module.rules.find(isJsxLoader)
+  const sanityJsxLoader = sanityWpConfig.module.rules.find(isJsxLoader)
+
+  if (storybookJsxLoader && sanityJsxLoader) {
+    const jsxLoaderIndex = config.module.rules.indexOf(storybookJsxLoader)
+
+    // replace with Sanity's JSX/TSX loader
+    config.module.rules.splice(jsxLoaderIndex, 1, sanityJsxLoader)
+  }
+
   return config
 }
 
-getWebpackConfig.setSanityContext = (context) => {
+getWebpackConfig.setSanityContext = context => {
   sanityContext = context
 }
 
