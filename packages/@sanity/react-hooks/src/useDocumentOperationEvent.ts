ac028ee33787a5f455b50b098a5f91873f7d052f
Bjørge Næss <bjoerge@gmail.com>
2020-11-10 15:02:46 +0100

    [react-hooks] Enable TypeScript strict mode

--- packages/@sanity/react-hooks/src/useDocumentOperationEvent.ts
+++ packages/@sanity/react-hooks/src/useDocumentOperationEvent.ts
@@ -2,7 +2,7 @@ import documentStore from 'part:@sanity/base/datastore/document'
 import {useObservable} from './utils/useObservable'
 import React from 'react'
 
-export function useDocumentOperationEvent(publishedDocId, docTypeName) {
+export function useDocumentOperationEvent(publishedDocId: string, docTypeName: string) {
   return useObservable(
     React.useMemo(() => documentStore.pair.operationEvents(publishedDocId, docTypeName), [
       publishedDocId,
--- packages/@sanity/react-hooks/src/useEditState.ts
+++ packages/@sanity/react-hooks/src/useEditState.ts
@@ -2,7 +2,7 @@ import documentStore from 'part:@sanity/base/datastore/document'
 import {useObservable} from './utils/useObservable'
 import React from 'react'
 
-export function useEditState(publishedDocId, docTypeName) {
+export function useEditState(publishedDocId: string, docTypeName: string) {
   return useObservable(
     React.useMemo(() => documentStore.pair.editState(publishedDocId, docTypeName), [
       publishedDocId,
