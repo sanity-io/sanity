ca34d937e13e5d263cd8439921c30a8e45b8e953
Espen Hovlandsdal <espen@hovlandsdal.com>
2020-11-11 09:52:59 +0100

    [chore] Remove unused modules (#2114)

--- packages/@sanity/document-window/src/sortOrderToOperator.js
+++
@@ -1,9 +0,0 @@
-const operators = {asc: '>', desc: '<'}
-const inverted = {asc: '<', desc: '>'}
-
-module.exports = (order, options) => {
-  const {invert, orEqual} = options
-  const suffix = orEqual ? '=' : ''
-  const source = invert ? inverted : operators
-  return `${source[order]}${suffix}`
-}
