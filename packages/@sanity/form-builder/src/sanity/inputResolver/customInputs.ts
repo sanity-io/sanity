ad64e6c1cee4b7d1aa18d70bbf5071b75e0c3759
Bjørge Næss <bjoerge@gmail.com>
2020-11-12 15:43:49 +0100

    [chore] Move the the rich-date-input package out of the monorepo
    Also, remove the long deprecated date-input package

--- packages/@sanity/form-builder/src/sanity/inputResolver/customInputs.ts
+++ packages/@sanity/form-builder/src/sanity/inputResolver/customInputs.ts
@@ -6,7 +6,6 @@ import GeoPointInput from 'part:@sanity/form-builder/input/geopoint?'
 import NumberInput from 'part:@sanity/form-builder/input/number?'
 import ObjectInput from 'part:@sanity/form-builder/input/object?'
 import ReferenceInput from 'part:@sanity/form-builder/input/reference?'
-import RichDateInput from 'part:@sanity/form-builder/input/rich-date?'
 import StringInput from 'part:@sanity/form-builder/input/string?'
 import TextInput from 'part:@sanity/form-builder/input/text?'
 import UrlInput from 'part:@sanity/form-builder/input/url?'
@@ -19,7 +18,6 @@ export default {
   text: TextInput,
   reference: ReferenceInput,
   datetime: DateTimeInput,
-  richDate: RichDateInput,
   email: EmailInput,
   geopoint: GeoPointInput,
   url: UrlInput,
