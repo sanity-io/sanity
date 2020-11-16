7da5e2e0c395ac2afa3fbdd33308c2b70c0998c7
Marius Lundgård <studio@mariuslundgard.com>
2020-11-16 20:49:18 +0100

    [form-builder] Fix import path

--- packages/@sanity/form-builder/src/inputs/PortableText/utils/createBlockActionPatchFn.tsx
+++ packages/@sanity/form-builder/src/inputs/PortableText/utils/createBlockActionPatchFn.tsx
@@ -1,6 +1,6 @@
-import PatchEvent, {insert, unset, set} from '../../../../PatchEvent'
 import {normalizeBlock} from '@sanity/block-tools'
 import {PortableTextBlock, PortableTextFeatures} from '@sanity/portable-text-editor'
+import PatchEvent, {insert, unset, set} from '../../../PatchEvent'
 
 type UnsetFunction = () => void
 type SetFunction = (args0: PortableTextBlock) => void
