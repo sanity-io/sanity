6ecced93ca4b081466d77b6edcb2a952cbaa7433
Bjørge Næss <bjoerge@gmail.com>
2020-11-17 22:01:07 +0100

    [form-builder] Use TextInput from Sanity UI for most primitive input components

--- packages/@sanity/form-builder/src/inputs/NumberInput.tsx
+++ packages/@sanity/form-builder/src/inputs/NumberInput.tsx
@@ -1,56 +1,56 @@
 import React from 'react'
-import {uniqueId} from 'lodash'
 import {NumberSchemaType} from '@sanity/types'
-import TextInput from 'part:@sanity/components/textinputs/default'
-import FormField from 'part:@sanity/components/formfields/default'
+import {TextInput} from '@sanity/ui'
+import {useId} from '@reach/auto-id'
+import {FormField} from '@sanity/base/components'
 import {getValidationRule} from '../utils/getValidationRule'
 import PatchEvent, {set, unset} from '../PatchEvent'
 import {Props} from './types'
 
-export default class NumberInput extends React.Component<Props<number, NumberSchemaType>> {
-  _input: TextInput | null
-  _inputId = uniqueId('NumberInput')
-  handleChange = (event: React.SyntheticEvent<HTMLInputElement>) => {
-    const nextValue = event.currentTarget.value
-    this.props.onChange(PatchEvent.from(nextValue === '' ? unset() : set(Number(nextValue))))
-  }
-  focus() {
-    if (this._input) {
-      this._input.focus()
-    }
-  }
-  setInput = (input: TextInput | null) => {
-    this._input = input
-  }
-  render() {
-    const {value = '', readOnly, markers, type, level, onFocus, presence} = this.props
-    const validation = markers.filter((marker) => marker.type === 'validation')
-    const errors = validation.filter((marker) => marker.level === 'error')
-    // Show numpad on mobile if only positive numbers is preferred
-    const minRule = getValidationRule(type, 'min')
-    const onlyPositiveNumber = minRule && minRule.constraint >= 0
-    return (
-      <FormField
-        markers={markers}
-        level={level}
-        label={type.title}
-        description={type.description}
-        labelFor={this._inputId}
-        presence={presence}
-      >
-        <TextInput
-          inputId={this._inputId}
-          customValidity={errors && errors.length > 0 ? errors[0].item.message : ''}
-          type="number"
-          value={value}
-          readOnly={readOnly}
-          placeholder={type.placeholder}
-          onChange={this.handleChange}
-          onFocus={onFocus}
-          ref={this.setInput}
-          pattern={onlyPositiveNumber ? '[d]*' : undefined}
-        />
-      </FormField>
-    )
-  }
-}
+const NumberInput = React.forwardRef(function NumberInput(
+  props: Props<number, NumberSchemaType>,
+  forwardedRef: React.ForwardedRef<HTMLInputElement>
+) {
+  const {value = '', readOnly, markers, type, level, onFocus, onChange, presence} = props
+  const validation = markers.filter((marker) => marker.type === 'validation')
+  const errors = validation.filter((marker) => marker.level === 'error')
+  const id = useId()
+
+  // Show numpad on mobile if only positive numbers is preferred
+  const minRule = getValidationRule(type, 'min')
+  const onlyPositiveNumber = minRule?.constraint >= 0
+
+  const handleChange = React.useCallback(
+    (event: React.SyntheticEvent<HTMLInputElement>) => {
+      const nextValue = event.currentTarget.value
+      onChange(PatchEvent.from(nextValue === '' ? unset() : set(Number(nextValue))))
+    },
+    [onChange]
+  )
+  return (
+    <FormField
+      level={level}
+      __unstable_markers={markers}
+      title={type.title}
+      description={type.description}
+      inputId={id}
+      __unstable_presence={presence}
+    >
+      <TextInput
+        type="number"
+        inputMode={onlyPositiveNumber ? 'numeric' : 'text'}
+        id={id}
+        customValidity={errors && errors.length > 0 ? errors[0].item.message : ''}
+        value={value || ''}
+        readOnly={Boolean(readOnly)}
+        placeholder={type.placeholder}
+        onChange={handleChange}
+        onFocus={onFocus}
+        ref={forwardedRef}
+        pattern={onlyPositiveNumber ? '[d]*' : undefined}
+      />
+    </FormField>
+  )
+})
+
+export default NumberInput
