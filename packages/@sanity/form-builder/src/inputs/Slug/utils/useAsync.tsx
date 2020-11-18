bcce64968cf74d9a250152fb7d1ba88449b2f700
Bjørge Næss <bjoerge@gmail.com>
2020-11-18 13:34:02 +0100

    [form-builder] Migrate SlugInput to use Sanity UI
    Co-authored-by: Marius Lundgård <studio@mariuslundgard.com>

---
+++ packages/@sanity/form-builder/src/inputs/Slug/utils/useAsync.tsx
@@ -0,0 +1,56 @@
+import React from 'react'
+
+export type AsyncCompleteState<T> = {
+  status: 'complete'
+  result: T
+}
+export type AsyncPendingState = {
+  status: 'pending'
+}
+export type AsyncErrorState = {
+  status: 'error'
+  error: Error
+}
+
+export type AsyncState<T> = AsyncPendingState | AsyncCompleteState<T> | AsyncErrorState
+
+/**
+ * Takes an async function and returns a [AsyncState<value>, callback] pair.
+ * Whenever the callback is invoked, a new AsyncState is returned.
+ * If the returned callback is called again before the previous callback has settled, the resolution of the previous one will be ignored, thus preventing race conditions.
+ * @param fn an async function that returns a value
+ * @param dependencies list of dependencies that will return a new [value, callback] pair
+ */
+export function useAsync<T, U>(
+  fn: (arg: U) => Promise<T>,
+  dependencies: React.DependencyList
+): [null | AsyncState<T>, (arg: U) => void] {
+  const [state, setState] = React.useState<AsyncState<T> | null>(null)
+
+  const lastId = React.useRef(0)
+
+  const wrappedCallback = React.useCallback(
+    (arg: U) => {
+      const asyncId = ++lastId.current
+      setState({status: 'pending'})
+
+      Promise.resolve()
+        .then(() => fn(arg))
+        .then(
+          (res) => {
+            if (asyncId === lastId.current) {
+              setState({status: 'complete', result: res})
+            }
+          },
+          (err) => {
+            if (asyncId === lastId.current) {
+              setState({status: 'error', error: err})
+            }
+          }
+        )
+    },
+    [fn, ...dependencies]
+  )
+
+  return [state, wrappedCallback]
+}
