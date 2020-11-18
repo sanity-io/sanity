7b1f7d3360020cb88ef1fcf6a7e9d6e9f3c53e38
Bjørge Næss <bjoerge@gmail.com>
2020-11-18 13:34:02 +0100

    Refactor SlugInput to use @sanity/ui

--- packages/@sanity/form-builder/src/inputs/Slug/SlugInput.tsx
+++ packages/@sanity/form-builder/src/inputs/Slug/SlugInput.tsx
@@ -1,36 +1,23 @@
 import React from 'react'
-import {uniqueId} from 'lodash'
-import speakingurl from 'speakingurl'
 import {
   Path,
   SanityDocument,
   SlugParent,
-  SlugifierFn,
   SlugSchemaType,
   Marker,
   isValidationErrorMarker,
 } from '@sanity/types'
 import {ChangeIndicatorCompareValueProvider} from '@sanity/base/lib/change-indicators/ChangeIndicator'
 import * as PathUtils from '@sanity/util/paths'
-import Button from 'part:@sanity/components/buttons/default'
+import {TextInput, Button, Flex, Box, Card, Stack, Badge} from '@sanity/ui'
+
 import FormField from 'part:@sanity/components/formfields/default'
-import TextInput from 'part:@sanity/components/textinputs/default'
+import {useId} from '@reach/auto-id'
 import {PatchEvent, set, setIfMissing, unset} from '../../PatchEvent'
 import withDocument from '../../utils/withDocument'
 import withValuePath from '../../utils/withValuePath'
-import styles from './styles/SlugInput.css'
-
-// Fallback slugify function if not defined in field options
-const defaultSlugify: SlugifierFn = (value, type) => {
-  const maxLength = (type.options && type.options.maxLength) || 200
-  const slugifyOpts = {truncate: maxLength, symbols: true}
-  return value ? speakingurl(value, slugifyOpts) : ''
-}
-
-const defaultState = {
-  inputText: undefined,
-  loading: false,
-}
+import {slugify} from './utils/slugify'
+import {useAsyncCallback} from './utils/useAsync'
 
 interface Slug {
   _type: 'slug'
@@ -42,169 +29,120 @@ type Props = {
   level: number
   value?: Slug
   compareValue?: Slug
-  focusPath: Path
   readOnly?: boolean
   document: SanityDocument
   onChange: (ev: PatchEvent) => void
-  onFocus: (path: Path) => void
   getValuePath: () => Path
   markers: Marker[]
   presence: any
 }
 
-export default withValuePath(
-  withDocument(
-    class SlugInput extends React.Component<Props> {
-      _textInput: any
-      _isMounted: boolean
-      _inputId = uniqueId('SlugInput')
-
-      static defaultProps = {
-        value: {current: undefined},
-        readOnly: false,
-        onChange() {},
-        markers: [],
-      }
-
-      state = defaultState
-
-      componentDidMount() {
-        this._isMounted = true
-      }
-
-      componentWillUnmount() {
-        this._isMounted = false
-      }
-
-      updateCurrent(current) {
-        const {onChange, type} = this.props
-        if (!current) {
-          onChange(PatchEvent.from(unset([])))
-          return
-        }
-
-        onChange(PatchEvent.from(setIfMissing({_type: type.name}), set(current, ['current'])))
-      }
-
-      slugify(sourceValue) {
-        if (!sourceValue) {
-          return Promise.resolve(sourceValue)
-        }
-        const {type} = this.props
-        const slugify = type.options.slugify || defaultSlugify
-        return Promise.resolve(slugify(sourceValue, type))
-      }
-
-      UNSAFE_componentWillReceiveProps(nextProps) {
-        const {document} = nextProps
-        // Reset state if document is changed
-        const oldDocId = this.props.document._id
-        const newDocId = document._id
-        if (oldDocId !== newDocId) {
-          this.setState(defaultState)
-        }
-      }
-
-      focus() {
-        if (this._textInput) {
-          this._textInput.focus()
-        }
-      }
-
-      setTextInput = (input) => {
-        this._textInput = input
-      }
-
-      handleChange = (event) => {
-        this.updateCurrent(event.target.value)
-      }
-
-      handleGenerateSlug = () => {
-        const {type} = this.props
-        const source = type.options?.source
-        if (!source) {
-          // eslint-disable-next-line no-console
-          console.error(`Source is missing. Check source on type "${type.name}" in schema`)
-          return
-        }
-
-        this.setState({loading: true})
-        this.getNewFromSource()
-          .then((newFromSource) => this.slugify(newFromSource || ''))
-          .then((newSlug) => this.updateCurrent(newSlug))
-          .catch((err) => {
-            // eslint-disable-next-line no-console
-            console.error(`An error occured while slugifying:\n${err.message}\n${err.stack}`)
-          })
-          .then(() => this._isMounted && this.setState({loading: false}))
-      }
-
-      hasSource = (): boolean => {
-        const {type, document} = this.props
-        const source = type.options?.source
-
-        return typeof source === 'function' ? true : Boolean(PathUtils.get(document, source))
-      }
-
-      getNewFromSource = (): Promise<string | undefined> => {
-        const {getValuePath, type, document} = this.props
-        const parentPath = getValuePath().slice(0, -1)
-        const parent = PathUtils.get(document, parentPath) as SlugParent
-        const source = type.options?.source || ([] as Path)
-        return Promise.resolve(
-          typeof source === 'function'
-            ? source(document, {parentPath, parent})
-            : (PathUtils.get(document, source) as string | undefined)
-        )
-      }
-
-      render() {
-        const {value, compareValue, type, level, markers, readOnly, presence} = this.props
-        const {loading, inputText} = this.state
-        const hasSourceField = type.options && type.options.source
-        const formFieldProps = {
-          label: type.title,
-          description: type.description,
-          level: level,
-          markers,
-          presence,
-          labelFor: this._inputId,
-        }
-        const errors = markers.filter(isValidationErrorMarker)
-        return (
-          <ChangeIndicatorCompareValueProvider
-            value={value?.current}
-            compareValue={compareValue?.current}
-          >
-            <FormField {...formFieldProps}>
-              <div className={styles.wrapper}>
-                <div className={styles.input}>
-                  <TextInput
-                    inputId={this._inputId}
-                    ref={this.setTextInput}
-                    customValidity={errors.length > 0 ? errors[0].item.message : ''}
-                    disabled={loading}
-                    onChange={this.handleChange}
-                    value={typeof inputText === 'string' ? inputText : value.current}
-                    readOnly={readOnly}
-                  />
-                </div>
-                {hasSourceField && (
-                  <Button
-                    className={styles.button}
-                    disabled={readOnly || loading || !this.hasSource()}
-                    loading={loading}
-                    onClick={this.handleGenerateSlug}
-                    kind="simple"
-                  >
-                    Generate
-                  </Button>
-                )}
-              </div>
-            </FormField>
-          </ChangeIndicatorCompareValueProvider>
-        )
-      }
-    }
+function getNewFromSource(source, valuePath, document) {
+  const parentPath = valuePath.slice(0, -1)
+  const parent = PathUtils.get(document, parentPath) as SlugParent
+  return Promise.resolve(
+    typeof source === 'function'
+      ? source(document, {parentPath, parent})
+      : (PathUtils.get(document, source) as string | undefined)
   )
-)
+}
+
+const SlugInput = React.forwardRef(function SlugInput(
+  props: Props,
+  forwardedRef: React.ForwardedRef<HTMLInputElement>
+) {
+  const {
+    value,
+    compareValue,
+    type,
+    level,
+    markers,
+    onChange,
+    getValuePath,
+    document,
+    readOnly,
+    presence,
+  } = props
+
+  const sourceField = type.options?.source
+
+  const inputId = useId()
+  const errors = markers.filter(isValidationErrorMarker)
+
+  const updateSlug = React.useCallback(
+    (nextSlug) => {
+      if (!nextSlug) {
+        onChange(PatchEvent.from(unset([])))
+        return
+      }
+
+      onChange(PatchEvent.from(setIfMissing({_type: type.name}), set(nextSlug, ['current'])))
+    },
+    [onChange, type.name]
+  )
+
+  const [generateState, handleGenerateSlug] = useAsyncCallback(() => {
+    if (!sourceField) {
+      return Promise.reject(
+        new Error(`Source is missing. Check source on type "${type.name}" in schema`)
+      )
+    }
+
+    return getNewFromSource(sourceField, getValuePath(), document)
+      .then((newFromSource) => slugify(newFromSource || '', type))
+      .then((newSlug) => updateSlug(newSlug))
+  }, [getValuePath, updateSlug, document, type])
+
+  const isUpdating = generateState?.status === 'pending'
+
+  return (
+    <ChangeIndicatorCompareValueProvider
+      value={value?.current}
+      compareValue={compareValue?.current}
+    >
+      <FormField
+        label={type.title}
+        description={type.description}
+        level={level}
+        markers={markers}
+        presence={presence}
+        labelFor={inputId}
+      >
+        <Stack space={3}>
+          <Flex>
+            <Box flex={1}>
+              <TextInput
+                id={inputId}
+                ref={forwardedRef}
+                customValidity={errors.length > 0 ? errors[0].item.message : ''}
+                disabled={isUpdating}
+                onChange={React.useCallback((event) => updateSlug(event.currentTarget.value), [
+                  updateSlug,
+                ])}
+                value={value?.current || ''}
+                readOnly={readOnly}
+              />
+              {generateState?.status === 'error' && (
+                <Card padding={2} tone="critical">
+                  {generateState.error.message}
+                </Card>
+              )}
+            </Box>
+            <Box marginLeft={1}>
+              <Button
+                mode="ghost"
+                type="button"
+                disabled={readOnly || isUpdating}
+                onClick={handleGenerateSlug}
+              >
+                {generateState?.status === 'pending' ? 'Generating…' : 'Generate'}
+              </Button>
+            </Box>
+          </Flex>
+        </Stack>
+      </FormField>
+    </ChangeIndicatorCompareValueProvider>
+  )
+})
+
+export default withValuePath(withDocument(SlugInput))
