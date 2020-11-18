4572d36f8ebca9697adaf7d6c95eb0b02e2a0f22
Marius Lundgård <studio@mariuslundgard.com>
2020-11-18 19:47:49 +0100

    [base] Remove base color workaround

--- packages/@sanity/base/src/components/SanityRoot.tsx
+++ packages/@sanity/base/src/components/SanityRoot.tsx
@@ -1,4 +1,4 @@
-import {Card, CardProvider, ThemeProvider} from '@sanity/ui'
+import {Card, ThemeProvider} from '@sanity/ui'
 import config from 'config:sanity'
 import RootComponent from 'part:@sanity/base/root'
 import {LayerProvider} from 'part:@sanity/components/layer'
@@ -8,15 +8,13 @@ import React, {useState} from 'react'
 import Refractor from 'react-refractor'
 import jsx from 'refractor/lang/jsx'
 import styled from 'styled-components'
-import studioTheme from '../theme'
+import theme from '../theme'
 import {userColorManager, UserColorManagerProvider} from '../user-color'
 import ErrorHandler from './ErrorHandler'
 import DevServerStatus from './DevServerStatus'
 import {GlobalStyle} from './GlobalStyle'
 import MissingProjectConfig from './MissingProjectConfig'
 import VersionChecker from './VersionChecker'
-
-import styles from './SanityRoot.css'
 
 Refractor.registerLanguage(jsx)
 
@@ -27,32 +25,30 @@ const Root = styled(Card).attrs({tone: 'transparent'})`
 function SanityRoot() {
   const {projectId, dataset} = config.api || {}
   const [portalElement, setPortalElement] = useState(() => document.createElement('div'))
-  const themeMode = 'light'
+  const colorScheme = 'light'
 
   if (!projectId || !dataset) {
     return <MissingProjectConfig />
   }
 
   return (
-    <ThemeProvider theme={studioTheme}>
-      <CardProvider scheme={themeMode}>
-        <UserColorManagerProvider manager={userColorManager}>
-          <PortalProvider element={portalElement}>
-            <LayerProvider>
-              <SnackbarProvider>
-                <GlobalStyle scheme={themeMode} />
-                <Root className={styles.root}>
-                  <DevServerStatus />
-                  <ErrorHandler />
-                  <RootComponent />
-                  <VersionChecker />
-                </Root>
-                <div data-portal="" ref={setPortalElement} />
-              </SnackbarProvider>
-            </LayerProvider>
-          </PortalProvider>
-        </UserColorManagerProvider>
-      </CardProvider>
+    <ThemeProvider theme={theme}>
+      <UserColorManagerProvider manager={userColorManager}>
+        <PortalProvider element={portalElement}>
+          <LayerProvider>
+            <SnackbarProvider>
+              <GlobalStyle scheme={colorScheme} />
+              <Root scheme={colorScheme}>
+                <DevServerStatus />
+                <ErrorHandler />
+                <RootComponent />
+                <VersionChecker />
+              </Root>
+              <div data-portal="" ref={setPortalElement} />
+            </SnackbarProvider>
+          </LayerProvider>
+        </PortalProvider>
+      </UserColorManagerProvider>
     </ThemeProvider>
   )
 }
