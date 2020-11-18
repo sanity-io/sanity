f83cf46244bc83071c79c744f9548b027f5f459e
Marius Lundgård <studio@mariuslundgard.com>
2020-11-18 19:42:04 +0100

    [base] Use body colors to define card color scheme

--- packages/@sanity/base/src/theme/index.ts
+++ packages/@sanity/base/src/theme/index.ts
@@ -1,8 +1,41 @@
-import {studioTheme} from '@sanity/ui'
+import {studioTheme as defaultTheme, ThemeCardColor} from '@sanity/ui'
 import legacyTheme from 'sanity:css-custom-properties'
 
+const defaultLightScheme = defaultTheme.color.light
+
+const customCard: ThemeCardColor = {
+  tones: {
+    ...defaultLightScheme.card.tones,
+    default: {
+      ...defaultLightScheme.card.tones.default,
+      enabled: {
+        ...defaultLightScheme.card.tones.default.enabled,
+        bg: legacyTheme['--body-bg'],
+        fg: legacyTheme['--body-text'],
+      },
+    },
+    transparent: {
+      ...defaultLightScheme.card.tones.transparent,
+      enabled: {
+        ...defaultLightScheme.card.tones.transparent.enabled,
+        bg: legacyTheme['--body-bg'],
+        fg: legacyTheme['--body-text'],
+      },
+    },
+  },
+}
+
+const lightScheme = {
+  ...defaultLightScheme,
+  card: customCard,
+}
+
 export default {
-  ...studioTheme,
+  ...defaultTheme,
+  color: {
+    ...defaultTheme.color,
+    light: lightScheme,
+  },
   media: [
     parseInt(legacyTheme['--screen-medium-break'], 10) || 512,
     parseInt(legacyTheme['--screen-default-break'], 10) || 640,
