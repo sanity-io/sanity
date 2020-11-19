8e5ca3409b88918e7fa51d09c40e61486b93525a
Bjørge Næss <bjoerge@gmail.com>
2020-11-19 16:28:39 +0100

    Refactor SlugInput to use @sanity/ui

--- packages/@sanity/form-builder/src/inputs/SelectInput.tsx
+++ packages/@sanity/form-builder/src/inputs/SelectInput.tsx
@@ -1,13 +1,11 @@
 import React from 'react'
-import {uniqueId, capitalize} from 'lodash'
+import {capitalize} from 'lodash'
 import {isTitledListValue, TitledListValue} from '@sanity/types'
 import FormField from 'part:@sanity/components/formfields/default'
-import Select from 'part:@sanity/components/selects/default'
-import RadioSelect from 'part:@sanity/components/selects/radio'
+import {Inline, Stack, Card, Text, Select, Flex, Radio, Box} from '@sanity/ui'
+import {useId} from '@reach/auto-id'
 import PatchEvent, {set, unset} from '../PatchEvent'
 import {Props} from './types'
-
-const EMPTY_ITEM = {title: '', value: undefined}
 
 function toSelectItem(
   option: TitledListValue<string | number> | string | number
@@ -15,72 +13,99 @@ function toSelectItem(
   return isTitledListValue(option) ? option : {title: capitalize(`${option}`), value: option}
 }
 
-export default class SelectInput extends React.Component<Props<string | number>> {
-  _input: (RadioSelect | Select) | null
-  name = uniqueId('RadioName')
-  static defaultProps = {
-    value: '',
-  }
+const EMPTY_ITEM = {title: '', value: undefined}
 
-  handleChange = (item: string | number | TitledListValue<string | number>) => {
-    const {onChange} = this.props
-    const newValue = typeof item === 'string' || typeof item === 'number' ? item : item.value
-    onChange(PatchEvent.from(typeof newValue === 'undefined' ? unset() : set(newValue)))
-  }
+const SelectInput = React.forwardRef(function SelectInput(
+  props: Props<string | number>,
+  forwardedRef: React.ForwardedRef<HTMLSelectElement | HTMLInputElement>
+) {
+  const {value, readOnly, markers, type, level, onChange, presence} = props
+  const items = ((type.options?.list || []) as unknown[]).map(toSelectItem)
+  const currentItem = items.find((item) => item.value === value)
+  const isRadio = type.options && type.options.layout === 'radio'
+  const validation = markers.filter((marker) => marker.type === 'validation')
+  const errors = validation.filter((marker) => marker.level === 'error')
 
-  focus() {
-    if (this._input) {
-      this._input.focus()
-    }
-  }
+  const inputId = useId()
+  const handleChange = React.useCallback(
+    (nextItem) => {
+      onChange(
+        PatchEvent.from(typeof nextItem.value === 'undefined' ? unset() : set(nextItem.value))
+      )
+    },
+    [onChange]
+  )
+  return (
+    <FormField
+      labelFor={inputId}
+      markers={markers}
+      level={level}
+      label={type.title}
+      description={type.description}
+      presence={presence}
+    >
+      {isRadio ? (
+        <RadioSelect
+          items={items}
+          value={currentItem}
+          onChange={handleChange}
+          direction={type.options.direction || 'vertical'}
+          ref={forwardedRef as React.ForwardedRef<HTMLInputElement>}
+          readOnly={readOnly}
+          customValidity={errors?.[0]?.item.message}
+        />
+      ) : (
+        <Select
+          onChange={handleChange}
+          id={inputId}
+          ref={forwardedRef as React.ForwardedRef<HTMLSelectElement>}
+          customValidity={errors?.[0]?.item.message}
+        >
+          {[EMPTY_ITEM, ...items].map((item, i) => (
+            <option key={i} value={item.value}>
+              {item.title}
+            </option>
+          ))}
+        </Select>
+      )}
+    </FormField>
+  )
+})
 
-  setInput = (input: (RadioSelect | Select) | null) => {
-    this._input = input
-  }
+export default SelectInput
 
-  render() {
-    const {value, readOnly, markers, type, level, onFocus, presence} = this.props
-    const items = ((type.options?.list || []) as unknown[]).map(toSelectItem)
-    const currentItem = items.find((item) => item.value === value)
-    const isRadio = type.options && type.options.layout === 'radio'
-    const validation = markers.filter((marker) => marker.type === 'validation')
-    return (
-      <FormField
-        labelFor={this.name}
-        markers={markers}
-        level={level}
-        label={type.title}
-        description={type.description}
-        presence={presence}
-        onFocus={onFocus}
-      >
-        {isRadio ? (
-          <RadioSelect
-            name={this.name}
-            legend={type.title}
-            items={items}
-            onChange={this.handleChange}
-            onFocus={onFocus}
-            value={currentItem}
-            direction={type.options.direction || 'vertical'}
-            ref={this.setInput}
-            readOnly={readOnly}
-          />
-        ) : (
-          <Select
-            id={this.name}
-            label={type.title}
-            value={currentItem}
-            placeholder={type.placeholder}
-            onChange={this.handleChange}
-            onFocus={onFocus}
-            items={[EMPTY_ITEM].concat(items)}
-            ref={this.setInput}
-            readOnly={readOnly}
-            hasError={validation.length >= 1}
-          />
-        )}
-      </FormField>
-    )
-  }
-}
+const RadioSelect = React.forwardRef(function RadioSelect(
+  props: {
+    items: TitledListValue<string | number>[]
+    value: TitledListValue<string | number>
+    direction: 'horizontal' | 'vertical'
+    readOnly: boolean
+    onChange: (value: TitledListValue<string | number> | null) => void
+    customValidity?: string
+  },
+  forwardedRef: React.ForwardedRef<HTMLInputElement>
+) {
+  const {items, value, onChange, readOnly, customValidity, direction} = props
+
+  const Flow = direction === 'horizontal' ? Inline : Stack
+  return (
+    <Card border padding={4} radius={1}>
+      <Flow space={4} role="group">
+        {items.map((item, index) => (
+          <Flex key={index} as="label" align="center">
+            <Radio
+              ref={index === 0 ? forwardedRef : null}
+              checked={value === item}
+              onClick={() => onChange(item)}
+              readOnly={readOnly}
+              customValidity={customValidity}
+            />
+            <Box marginLeft={3}>
+              <Text>{item.title}</Text>
+            </Box>
+          </Flex>
+        ))}
+      </Flow>
+    </Card>
+  )
+})
