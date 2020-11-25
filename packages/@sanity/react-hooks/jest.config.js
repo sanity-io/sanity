3cb2ee39ce4e9aac837333d4f36327a7e0352b68
Bjørge Næss <bjoerge@gmail.com>
2020-11-25 12:30:25 +0100

    [chore] Switch to project references instead of path aliases

--- packages/@sanity/react-hooks/jest.config.js
+++ packages/@sanity/react-hooks/jest.config.js
@@ -1,10 +1,5 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  globals: {
-    'ts-jest': {
-      tsconfig: 'tsconfig.dist.json',
-    },
-  },
   modulePathIgnorePatterns: ['<rootDir>/lib/'],
 }
