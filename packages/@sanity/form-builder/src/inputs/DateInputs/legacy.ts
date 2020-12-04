1bd2cf301e0be88280753eb847a20e589a46efe9
Bjørge Næss <bjoerge@gmail.com>
2020-12-04 12:14:45 +0100

    [form-builder] Rewrite date inputs to use date picker based on UI
    Co-authored-by: Benedicte Emilie Brækken <benedicte@sanity.io>
    Co-authored-by: Marius Lundgård <studio@mariuslundgard.com>

---
+++ packages/@sanity/form-builder/src/inputs/DateInputs/legacy.ts
@@ -0,0 +1,18 @@
+/* eslint-disable no-shadow */
+import moment from 'moment'
+import {ParseResult} from './types'
+
+// todo: find a way to get rid of moment there.
+// note: the format comes form peoples schemas, so we need to deprecate it for a while and
+// find a way to tell people that they need to change it
+export function format(input: Date, format: string) {
+  return moment(input).format(format)
+}
+
+export function parse(dateString: string, format: string): ParseResult {
+  const parsed = moment(dateString, format, true)
+  if (parsed.isValid()) {
+    return {isValid: true, date: parsed.toDate()}
+  }
+  return {isValid: false, error: `Invalid date. Must be on the format "${format}"`}
+}
