5279d8e0f10cd83da75afaca4229064671fe2279
Bjørge Næss <bjoerge@gmail.com>
2020-12-15 12:06:09 +0100

    [form-builder] Declare list/item component pairs in outer scope

--- packages/@sanity/form-builder/src/inputs/ArrayInput/resolveListComponents.ts
+++ packages/@sanity/form-builder/src/inputs/ArrayInput/resolveListComponents.ts
@@ -3,17 +3,22 @@ import {
   List as SortableList,
   Item as SortableListItem,
 } from 'part:@sanity/components/lists/sortable'
+
 import {List as GridList, Item as GridListItem} from 'part:@sanity/components/lists/grid'
+
 import {
   List as SortableGridList,
   Item as SortableGridListItem,
 } from 'part:@sanity/components/lists/sortable-grid'
 
+const SORTABLE_GRID = {List: SortableGridList, Item: SortableGridListItem}
+const SORTABLE_LIST = {List: SortableList, Item: SortableListItem}
+const NON_SORTABLE_GRID = {List: GridList, Item: GridListItem}
+const NON_SORTABLE_LIST = {List: DefaultList, Item: DefaultListItem}
+
 export default function resolveListComponents(isSortable: boolean, isGrid: boolean) {
   if (isSortable) {
-    return isGrid
-      ? {List: SortableGridList, Item: SortableGridListItem}
-      : {List: SortableList, Item: SortableListItem}
+    return isGrid ? SORTABLE_GRID : SORTABLE_LIST
   }
-  return isGrid ? {List: GridList, Item: GridListItem} : {List: DefaultList, Item: DefaultListItem}
+  return isGrid ? NON_SORTABLE_GRID : NON_SORTABLE_LIST
 }
