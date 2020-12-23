64d00345388a8d9b5ef6d15d17542ce0d7d368a3
Bjørge Næss <bjoerge@gmail.com>
2020-12-23 10:31:57 +0100

    [form-builder] Refactor missing item type warning, improve explanation and make it more graceful

---
+++ packages/@sanity/form-builder/src/inputs/ArrayInput/item/ItemWithMissingType.tsx
@@ -0,0 +1,75 @@
+import {Box, Card, Flex, Popover, Text, useClickOutside} from '@sanity/ui'
+import React from 'react'
+import {BulbOutlineIcon, UnknownIcon} from '@sanity/icons'
+
+type Props = {value: any; onFocus?: () => void}
+export function ItemWithMissingType(props: Props) {
+  const {value, onFocus} = props
+  const [showDetails, setShowDetails] = React.useState(false)
+  const [popoverRef, setPopoverRef] = React.useState<HTMLElement | null>(null)
+
+  useClickOutside(() => setShowDetails(false), [popoverRef])
+
+  const handleKeyDown = React.useCallback((e) => {
+    if (e.key === 'Escape' || e.key === 'Tab') {
+      setShowDetails(false)
+    }
+  }, [])
+
+  const typeName = value?._type || 'object'
+  return (
+    <Popover
+      open={showDetails}
+      ref={setPopoverRef}
+      onKeyDown={handleKeyDown}
+      content={
+        <Card
+          margin={1}
+          padding={1}
+          onKeyDown={handleKeyDown}
+          tabIndex={0}
+          style={{outline: 'none'}}
+        >
+          <Box>
+            <b>Why is this happening?</b>
+          </Box>
+          The current schema does not declare items of type <code>{typeName}</code> as valid for
+          this list. This could mean that the type has been removed as a valid item type, or that
+          someone else has added it to their own local schema that is not yet deployed.
+          <Box marginTop={4}>
+            <Text>
+              <BulbOutlineIcon /> You can still move or delete this item, but it cannot be edited
+              since the schema definition for its type is nowhere to be found.
+            </Text>
+          </Box>
+          <Box marginTop={4}>JSON representation of this item:</Box>
+          <Card overflow="auto" border>
+            <Text size={1}>
+              <pre>{JSON.stringify(value, null, 2)}</pre>
+            </Text>
+          </Card>
+        </Card>
+      }
+    >
+      <Card
+        as="button"
+        radius={2}
+        padding={2}
+        onFocus={onFocus}
+        onClick={() => setShowDetails(true)}
+        onKeyDown={handleKeyDown}
+      >
+        <Flex>
+          <Text size={1}>
+            <UnknownIcon />
+          </Text>
+          <Box marginLeft={2} flex={1}>
+            <Text size={1}>
+              Item type <code>{value?._type || 'object'}</code> not defined for this list
+            </Text>
+          </Box>
+        </Flex>
+      </Card>
+    </Popover>
+  )
+}
