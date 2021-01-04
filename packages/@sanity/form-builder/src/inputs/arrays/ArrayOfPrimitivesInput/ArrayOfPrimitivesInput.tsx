dc9b8d916b4498b2ef5977641ab40aaf06c938a0
Bjørge Næss <bjoerge@gmail.com>
2021-01-04 15:31:37 +0100

    [form-builder] Migrate Array of primitives to use @sanity/ui

--- packages/@sanity/form-builder/src/inputs/arrays/ArrayOfPrimitivesInput/ArrayOfPrimitivesInput.tsx
+++ packages/@sanity/form-builder/src/inputs/arrays/ArrayOfPrimitivesInput/ArrayOfPrimitivesInput.tsx
@@ -2,18 +2,14 @@ import React from 'react'
 import {get} from 'lodash'
 import {startsWith} from '@sanity/util/paths'
 import {ArraySchemaType, Marker, Path, SchemaType} from '@sanity/types'
-import {Item as DefaultItem, List as DefaultList} from 'part:@sanity/components/lists/default'
-import {Item as SortableItem, List as SortableList} from 'part:@sanity/components/lists/sortable'
-import ArrayFunctions from 'part:@sanity/form-builder/input/array/functions'
-import {Button} from '@sanity/ui'
+import {Box} from '@sanity/ui'
 import {PatchEvent, set, unset} from '../../../PatchEvent'
 import {resolveTypeName} from '../../../utils/resolveTypeName'
-import Warning from '../../Warning'
 import {Fieldset} from '../../../transitional/Fieldset'
+import {Item, List} from '../common/list'
+import {ArrayFunctions} from '../../../legacyImports'
 import getEmptyValue from './getEmptyValue'
-import Item from './Item'
-
-import styles from './ArrayOfPrimitivesInput.css'
+import {ItemRow} from './ItemRow'
 
 type Primitive = string | number | boolean
 
@@ -34,7 +30,7 @@ function insertAt(arr: Primitive[], index: number, item: Primitive): Primitive[]
 }
 
 type Props = {
-  type: ArraySchemaType<Primitive>
+  type?: ArraySchemaType<Primitive>
   value: Primitive[]
   compareValue?: Primitive[]
   level: number
@@ -116,71 +112,6 @@ export default class ArrayOfPrimitivesInput extends React.PureComponent<Props> {
     )
   }
 
-  renderItem = (item: Primitive, index: number) => {
-    const {
-      type,
-      level = 1,
-      markers,
-      compareValue,
-      focusPath,
-      onFocus,
-      readOnly,
-      onBlur,
-      presence,
-    } = this.props
-
-    const typeName = resolveTypeName(item)
-    const itemMemberType = this.getMemberType(typeName)
-    if (!itemMemberType) {
-      return null
-    }
-
-    const isSortable = get(type, 'options.sortable') !== false
-    const ListItem = isSortable ? SortableItem : DefaultItem
-    const filteredMarkers = markers.filter((marker) => startsWith([index], marker.path))
-    const childPresence = presence.filter((pItem) => startsWith([index], pItem.path))
-
-    return (
-      <ListItem key={index} index={index} className={styles.item}>
-        <Item
-          level={level + 1}
-          index={index}
-          value={item}
-          compareValue={(compareValue || [])[index]}
-          readOnly={readOnly}
-          markers={filteredMarkers.length === 0 ? NO_MARKERS : filteredMarkers}
-          isSortable={isSortable}
-          type={itemMemberType}
-          focusPath={focusPath}
-          onFocus={onFocus}
-          onBlur={onBlur}
-          onEnterKey={this.handleItemEnterKey}
-          onEscapeKey={this.handleItemEscapeKey}
-          onChange={this.handleItemChange}
-          onRemove={this.handleRemoveItem}
-          presence={childPresence}
-        />
-      </ListItem>
-    )
-  }
-
-  renderList(value: Primitive[]) {
-    const {type} = this.props
-    const isSortable = get(type, 'options.sortable') !== false
-    return isSortable ? (
-      <SortableList
-        className={styles.list}
-        onSortEnd={this.handleSortEnd}
-        helperClass="ArrayOfPrimitivesInput__moving"
-        useDragHandle
-      >
-        {value.map(this.renderItem)}
-      </SortableList>
-    ) : (
-      <DefaultList decoration="divider">{value.map(this.renderItem)}</DefaultList>
-    )
-  }
-
   setElement = (el: Focusable | null) => {
     this._element = el
   }
@@ -195,62 +126,27 @@ export default class ArrayOfPrimitivesInput extends React.PureComponent<Props> {
     this.props.onFocus([index])
   }
 
-  renderUnknownValueTypes = () => {
-    const {value, readOnly} = this.props
-
-    // Find values with types not specified in the schema
-    const unknownValues = (value || [])
-      .filter((v) => {
-        const typeName = resolveTypeName(v)
-        const itemMemberType = this.getMemberType(typeName)
-        return !itemMemberType
-      })
-      .map((v) => ({value: v, type: resolveTypeName(v)}))
-
-    if (!unknownValues || unknownValues.length === 0) {
-      return null
-    }
-
-    const message = (
-      <>
-        <p>
-          These are not defined in the current schema as valid types for this array. This could mean
-          that the type has been removed, or that someone else has added it to their own local
-          schema that is not yet deployed.
-        </p>
-
-        {unknownValues.map((item) => (
-          <div key={item.type}>
-            <h4>{item.type}</h4>
-            <pre>{JSON.stringify(item.value, null, 2)}</pre>
-            {readOnly ? (
-              <div>
-                This array is <em>read only</em> according to its enclosing schema type and values
-                cannot be unset. If you want to unset a value, make sure you remove the{' '}
-                <strong>readOnly</strong> property from the enclosing type.
-              </div>
-            ) : (
-              <Button
-                onClick={() => this.handleRemoveItem(value.findIndex((v) => v === item.value))}
-                tone="critical"
-                text={`Unset ${item.value}`}
-              />
-            )}
-          </div>
-        ))}
-      </>
-    )
-
-    return <Warning values={unknownValues} message={message} />
-  }
-
   render() {
-    const {type, value, level = 1, markers, readOnly, onChange, onFocus, presence} = this.props
+    const {
+      type,
+      value,
+      level = 1,
+      markers,
+      readOnly,
+      onChange,
+      onFocus,
+      presence,
+      compareValue,
+      focusPath,
+      onBlur,
+    } = this.props
+
+    const isSortable = get(type, 'options.sortable') !== false
 
     return (
       <Fieldset
-        legend={type.title}
-        description={type.description}
+        legend={type?.title}
+        description={type?.description}
         level={level - 1}
         tabIndex={0}
         onFocus={onFocus}
@@ -259,10 +155,39 @@ export default class ArrayOfPrimitivesInput extends React.PureComponent<Props> {
         presence={presence.filter((item) => item.path[0] === '$' || item.path.length === 0)}
         changeIndicator={false}
       >
-        <div className={styles.root}>
-          {value && value.length > 0 && <div className={styles.list}>{this.renderList(value)}</div>}
-          {this.renderUnknownValueTypes()}
-          <div className={styles.functions}>
+        <Box>
+          {value && value.length > 0 && (
+            <List onSortEnd={this.handleSortEnd} isSortable={isSortable}>
+              {value.map((item, index) => {
+                const filteredMarkers = markers.filter((marker) => startsWith([index], marker.path))
+                const childPresence = presence.filter((pItem) => startsWith([index], pItem.path))
+
+                return (
+                  <Item key={index} index={index} isSortable={isSortable}>
+                    <ItemRow
+                      level={level + 1}
+                      index={index}
+                      value={item}
+                      compareValue={(compareValue || [])[index]}
+                      readOnly={readOnly}
+                      markers={filteredMarkers.length === 0 ? NO_MARKERS : filteredMarkers}
+                      isSortable={isSortable}
+                      type={this.getMemberType(resolveTypeName(item))}
+                      focusPath={focusPath}
+                      onFocus={onFocus}
+                      onBlur={onBlur}
+                      onEnterKey={this.handleItemEnterKey}
+                      onEscapeKey={this.handleItemEscapeKey}
+                      onChange={this.handleItemChange}
+                      onRemove={this.handleRemoveItem}
+                      presence={childPresence}
+                    />
+                  </Item>
+                )
+              })}
+            </List>
+          )}
+          <Box marginTop={1}>
             <ArrayFunctions
               type={type}
               value={value}
@@ -273,8 +198,8 @@ export default class ArrayOfPrimitivesInput extends React.PureComponent<Props> {
               onCreateValue={getEmptyValue}
               onChange={onChange}
             />
-          </div>
-        </div>
+          </Box>
+        </Box>
       </Fieldset>
     )
   }
