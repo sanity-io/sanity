2eb4e1caf0f6a0bef39ac3971b72438413fef017
Marius Lundgård <studio@mariuslundgard.com>
2021-01-14 19:40:19 +0100

    [form-builder] New TagInput component

--- packages/@sanity/form-builder/src/inputs/TagsArrayInput.tsx
+++ packages/@sanity/form-builder/src/inputs/TagsArrayInput.tsx
@@ -1,44 +1,47 @@
 import React from 'react'
 import {uniqueId} from 'lodash'
-import FormField from 'part:@sanity/components/formfields/default'
 import TagInput from 'part:@sanity/components/tags/textfield'
 import {FormField} from '@sanity/base/components'
+import {TagInput} from '../components/tagInput'
 import PatchEvent, {set, unset} from '../PatchEvent'
 import {Props} from './types'
 
 export default class TagsArrayInput extends React.PureComponent<Props<string[]>> {
-  _input: TagInput
+  _input: HTMLInputElement
   _inputId = uniqueId('TagsArrayInput')
   set(nextValue: string[]) {
     const patch = nextValue.length === 0 ? unset() : set(nextValue)
     this.props.onChange(PatchEvent.from(patch))
   }
-  handleChange = (nextValue: Array<string>) => {
-    this.set(nextValue)
+  handleChange = (nextValue: {value: string}[]) => {
+    this.set(nextValue.map((v) => v.value))
   }
   focus() {
     if (this._input) {
       this._input.focus()
     }
   }
-  setInput = (el: TagInput | null) => {
+  setInput = (el: HTMLInputElement | null) => {
     this._input = el
   }
   render() {
     const {type, value, readOnly, level, markers, onFocus, presence} = this.props
+
+    const tagInputValue = value.map((v) => ({value: v}))
+
     return (
       <FormField
         level={level}
         title={type.title}
         description={type.description}
-        presence={presence}
-        htmlFor={this._inputId}
-        markers={markers}
+        __unstable_presence={presence}
+        inputId={this._inputId}
+        __unstable_markers={markers}
       >
         <TagInput
-          inputId={this._inputId}
+          id={this._inputId}
           readOnly={readOnly}
-          value={value}
+          value={tagInputValue}
           onChange={this.handleChange}
           onFocus={onFocus}
           ref={this.setInput}
