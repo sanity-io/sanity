120b5b68be902aad8c6f0b4f3076fe6c39b8584a
Bjørge Næss <bjoerge@gmail.com>
2021-01-18 10:02:45 +0100

    [form-builder] Move default asset source to sanity folder

--- packages/@sanity/form-builder/src/sanity/inputs/SanityImageInput.tsx
+++ packages/@sanity/form-builder/src/sanity/inputs/SanityImageInput.tsx
@@ -1,29 +1,36 @@
-import React from 'react'
-import ImageInput, {Props} from '../../inputs/files/ImageInput'
+import React, {ForwardedRef, LegacyRef} from 'react'
+
+// what's the idea behind this, can it be replaced by a local import?
+import defaultAssetSources from 'all:part:@sanity/form-builder/input/image/asset-source'
+
+import userDefinedAssetSources from 'part:@sanity/form-builder/input/image/asset-sources?'
+import formBuilderConfig from 'config:@sanity/form-builder'
+import ImageInput from '../../inputs/files/ImageInput'
 import resolveUploader from '../uploads/resolveUploader'
 import withDocument from '../../utils/withDocument'
 import {materializeReference} from './client-adapters/assets'
 
+const globalAssetSources = userDefinedAssetSources ? userDefinedAssetSources : defaultAssetSources
+
+const SUPPORT_DIRECT_UPLOADS = formBuilderConfig?.images?.directUploads !== false
+
+type Props = Omit<React.ComponentProps<typeof ImageInput>, 'assetSources'>
+
 export default withDocument(
-  class SanityImageInput extends React.Component<Props> {
-    _input: any
-    focus() {
-      if (this._input) {
-        this._input.focus()
-      }
-    }
-    setInput = (input) => {
-      this._input = input
-    }
-    render() {
-      return (
-        <ImageInput
-          {...this.props}
-          resolveUploader={resolveUploader}
-          materialize={materializeReference}
-          ref={this.setInput}
-        />
-      )
-    }
-  }
+  React.forwardRef(function SanityImageInput(props: Props, forwardedRef: any) {
+    // note: type.options.sources may be an empty array and in that case we're
+    // disabling selecting images from asset source  (it's a feature, not a bug)
+    const assetSources = props.type.options?.sources || globalAssetSources
+
+    return (
+      <ImageInput
+        {...props}
+        resolveUploader={resolveUploader}
+        materialize={materializeReference}
+        assetSources={assetSources}
+        directUploads={SUPPORT_DIRECT_UPLOADS}
+        ref={forwardedRef}
+      />
+    )
+  })
 )
