2de9e0386a5517694ac800890f135ae94d7e0822
Marius Lundgård <studio@mariuslundgard.com>
2021-01-18 11:36:25 +0100

    [form-builder] Migrate Alert and Details to Sanity UI

--- packages/@sanity/form-builder/src/components/Alert.tsx
+++ packages/@sanity/form-builder/src/components/Alert.tsx
@@ -1,51 +1,47 @@
+import {ErrorOutlineIcon, WarningOutlineIcon} from '@sanity/icons'
+import {Box, Card, Flex, Text, ThemeColorToneKey} from '@sanity/ui'
 import React from 'react'
-import {Details} from '../components/Details'
-import styles from './Alert.css'
+import styled from 'styled-components'
 
-type Props = {
-  heading?: any
-  message?: any
-  values?: any[]
-  children?: any
+interface AlertProps {
+  children?: React.ReactNode
+  title: React.ReactNode
+  status?: 'warning' | 'error'
+  suffix?: React.ReactNode
 }
 
-export function Alert({heading, message, values = [], children}: Props) {
-  const len = values.length
+const STATUS_TONES: {[key: string]: ThemeColorToneKey} = {
+  warning: 'caution',
+  error: 'critical',
+}
+
+const SuffixBox = styled(Box)`
+  border-top: 1px solid var(--card-border-color);
+`
+
+export function Alert(props: AlertProps) {
+  const {children, status = 'warning', suffix, title} = props
+
   return (
-    <div className={styles.root}>
-      <h2 className={styles.heading}>
-        {heading ? (
-          heading
-        ) : (
-          <>
-            Found {len === 1 ? <>a</> : len} {len === 1 ? <>value</> : <>values</>} with{' '}
-            {len === 1 && <>an</>} unknown {len === 1 ? <>type</> : <>types</>}
-          </>
-        )}
-      </h2>
-      <div>
-        <Details>
-          <div>
-            {message ? (
-              message
-            ) : (
-              <>
-                These are not defined in the current schema as valid types for this array. This
-                could mean that the type has been removed, or that someone else has added it to
-                their own local schema that is not yet deployed.
-                {values.map((item) => {
-                  return (
-                    <div key={item}>
-                      <pre>{JSON.stringify(item)}</pre>
-                    </div>
-                  )
-                })}
-              </>
-            )}
-          </div>
-          <div>{children && children}</div>
-        </Details>
-      </div>
-    </div>
+    <Card radius={2} tone={STATUS_TONES[status]}>
+      <Flex padding={4}>
+        <Box>
+          <Text size={1}>
+            {status === 'warning' && <WarningOutlineIcon />}
+            {status === 'error' && <ErrorOutlineIcon />}
+          </Text>
+        </Box>
+
+        <Box flex={1} marginLeft={3}>
+          <Text size={1} weight="semibold">
+            {title}
+          </Text>
+
+          {children && <Box marginTop={3}>{children}</Box>}
+        </Box>
+      </Flex>
+
+      {suffix && <SuffixBox>{suffix}</SuffixBox>}
+    </Card>
   )
 }
