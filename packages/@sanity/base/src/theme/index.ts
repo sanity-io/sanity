3f8bdd7da3b599338ea2d841f18e41027c2cbef9
Marius Lundgård <studio@mariuslundgard.com>
2021-01-21 17:24:21 +0100

    [base] Use legacy theming for Sanity UI inputs

--- packages/@sanity/base/src/theme/index.ts
+++ packages/@sanity/base/src/theme/index.ts
@@ -1,5 +1,6 @@
-import {RootTheme, studioTheme as defaults, ThemeColorSchemes} from '@sanity/ui'
+import {RootTheme, studioTheme as defaults} from '@sanity/ui'
 import legacyTheme from 'sanity:css-custom-properties'
+import {color} from './color'
 
 // For debugging purposes
 declare global {
@@ -7,52 +8,8 @@ declare global {
     __sanityLegacyTheme: Record<string, string>
   }
 }
-window.__sanityLegacyTheme = legacyTheme
 
-// NOTE: This mapping is needed only in a transition period between legacy CSS custom properties,
-// and the new Theme API provided by Sanity UI.
-const color: ThemeColorSchemes = {
-  ...defaults.color,
-  light: {
-    ...defaults.color.light,
-    default: {
-      ...defaults.color.light.default,
-      base: {
-        ...defaults.color.light.default.base,
-        bg: legacyTheme['--component-bg'],
-        fg: legacyTheme['--component-text-color'],
-        border: legacyTheme['--hairline-color'],
-      },
-      card: {
-        ...defaults.color.light.default.card,
-        enabled: {
-          ...defaults.color.light.default.card.enabled,
-          bg: legacyTheme['--component-bg'],
-          fg: legacyTheme['--component-text-color'],
-          border: legacyTheme['--hairline-color'],
-        },
-      },
-    },
-    transparent: {
-      ...defaults.color.light.transparent,
-      base: {
-        ...defaults.color.light.transparent.base,
-        bg: legacyTheme['--body-bg'],
-        fg: legacyTheme['--body-text'],
-        border: legacyTheme['--hairline-color'],
-      },
-      card: {
-        ...defaults.color.light.transparent.card,
-        enabled: {
-          ...defaults.color.light.transparent.card.enabled,
-          bg: legacyTheme['--body-bg'],
-          fg: legacyTheme['--body-text'],
-          border: legacyTheme['--hairline-color'],
-        },
-      },
-    },
-  },
-}
+window.__sanityLegacyTheme = legacyTheme
 
 export const theme: RootTheme = {
   ...defaults,
