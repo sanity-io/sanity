7cf1108ef2e98d3a9198b241278672b75eb5fc85
Benedicte Emilie Brækken <benedicte@sanity.io>
2021-01-25 11:04:24 +0100

    [form-builder] Add value to SelectInput
    This fixes a bug where the select html element would not update when
    undoing changes because it had no way of separating between values.
    
    Also fixes a bug where onChange would not send a correct patch on
    changes because it was assumed radio and select onChange methods had the
    same signature.
    
    Hack: We use the items index directly to identify options. Bad for
    semantics. Could use some kind of unique ID with a readable name.

--- packages/@sanity/form-builder/src/inputs/SelectInput.tsx
+++ packages/@sanity/form-builder/src/inputs/SelectInput.tsx
@@ -26,15 +26,34 @@ const SelectInput = React.forwardRef(function SelectInput(
   const validation = markers.filter((marker) => marker.type === 'validation')
   const errors = validation.filter((marker) => marker.level === 'error')
 
+  const itemFromOptionValue = (optionValue) => {
+    const index = Number(optionValue)
+
+    return items[index]
+  }
+
+  const optionValueFromItem = (item) => {
+    return items.indexOf(item)
+  }
+
   const inputId = useId()
   const handleChange = React.useCallback(
-    (nextItem) => {
+    (nextItem: TitledListValue<string | number>) => {
       onChange(
         PatchEvent.from(typeof nextItem.value === 'undefined' ? unset() : set(nextItem.value))
       )
     },
     [onChange]
   )
+
+  const handleSelectChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const nextItem = itemFromOptionValue(event.currentTarget.value)
+
+    if (nextItem) {
+      handleChange(nextItem)
+    }
+  }
+
   return (
     <FormField
       inputId={inputId}
@@ -56,14 +75,15 @@ const SelectInput = React.forwardRef(function SelectInput(
         />
       ) : (
         <Select
-          onChange={handleChange}
+          onChange={handleSelectChange}
           id={inputId}
           ref={forwardedRef as React.ForwardedRef<HTMLSelectElement>}
           readOnly={readOnly}
           customValidity={errors?.[0]?.item.message}
+          value={optionValueFromItem(currentItem)}
         >
           {[EMPTY_ITEM, ...items].map((item, i) => (
-            <option key={i} value={item.value}>
+            <option key={`${i - 1}`} value={i - 1}>
               {item.title}
             </option>
           ))}
