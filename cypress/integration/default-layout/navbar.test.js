269150e45dd7a2bf7d06484a79f87ff9d1b362dc
Marius Lundgård <studio@mariuslundgard.com>
2021-01-28 12:47:37 +0100

    [cypress] Add test for navbar

---
+++ cypress/integration/default-layout/navbar.test.js
@@ -0,0 +1,19 @@
+describe('@sanity/default-layout: Navbar', () => {
+  it('should render ActionModal on top of Desk Tool’s pane headers', () => {
+    cy.visit('/test/desk')
+
+    cy.get('[data-test="default-layout-global-create-button"]').click()
+
+    cy.get('[data-test="default-layout-global-create-dialog"]').should(
+      'have.css',
+      'z-index',
+      '500401'
+    )
+
+    cy.get('[data-test="desk-tool-list-pane"] [data-test="components-default-pane-header"]').should(
+      'have.css',
+      'z-index',
+      '101'
+    )
+  })
+})
