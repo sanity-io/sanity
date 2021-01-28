0ea6475b13970e8ad27cda5e222cff4809f10f39
Bjørge Næss <bjoerge@gmail.com>
2021-01-28 20:47:32 +0100

    [chore] Set up github action for running performance test as part of ci

--- perf/suite.ts
+++ perf/suite.ts
@@ -2,8 +2,18 @@ import os from 'os'
 import getRepoInfo from 'git-repo-info'
 import {uuid} from '@sanity/uuid'
 import {omit} from 'lodash'
-import {testTypingSpeed} from './type-speed'
-import {sanity} from './sanity'
+import createClient from '@sanity/client'
+import {testTypingSpeed} from './typingSpeed'
+
+const writeToken = process.env.SANITY_PERF_STUDIO_WRITE_TOKEN
+const userToken = process.env.SANITY_PERF_STUDIO_USER_TOKEN
+
+export const sanity = createClient({
+  token: writeToken,
+  projectId: 'ppsg7ml5',
+  dataset: 'perf',
+  useCdn: false,
+})
 
 function getInstanceInfo() {
   return {
@@ -18,31 +28,49 @@ function getInstanceInfo() {
   }
 }
 
+function getWorkflowInfo() {
+  const workflowName = process.env.GITHUB_WORKFLOW
+  if (!workflowName) {
+    return undefined
+  }
+  return {
+    workflow: workflowName,
+    action: process.env.GITHUB_ACTION,
+    runId: process.env.GITHUB_RUN_ID,
+  }
+}
+
+if (!writeToken || !userToken) {
+  throw new Error('Missing required environment variables')
+}
+
 const baseVersion = require('../lerna.json').version
 
 const instance = getInstanceInfo()
 const repoInfo = getRepoInfo()
 const workflowInfo = getWorkflowInfo()
 
-testTypingSpeed().then((result) => {
-  const docId = uuid()
-  sanity.createIfNotExists({
-    _id: docId,
-    _type: 'typingPerfSample',
-    baseVersion,
-    git: {
-      branch: repoInfo.branch,
-      tag: repoInfo.tag,
-      commit: {
-        sha: repoInfo.sha,
-        author: repoInfo.author,
-        committer: repoInfo.committer,
-        authoredAt: repoInfo.authorDate,
-        committedAt: repoInfo.committerDate,
+testTypingSpeed({userToken: userToken}).then(
+  (result) => {
+    const docId = uuid()
+    return sanity.create({
+      _id: docId,
+      _type: 'typingPerfRun',
+      baseVersion,
+      git: {
+        branch: repoInfo.branch || undefined,
+        tag: repoInfo.tag || undefined,
+        sha: repoInfo.sha || undefined,
       },
-    },
-    result: result,
-    ci: process.env.CI === 'true',
-    instance,
-  })
-})
+      github: workflowInfo,
+      result: result,
+      ci: process.env.CI === 'true',
+      instance,
+    })
+  },
+  (err) => {
+    // eslint-disable-next-line no-console
+    console.error(err)
+    process.exit(1)
+  }
+)
