b3db23240bf02892529861d0ed6ad67abfbc8e48
Benedicte Emilie Brækken <benedicte@sanity.io>
2021-01-28 10:59:50 +0100

    [chore] Add end-to-end testing with Cypress (#2261)
    Can be used to write automated tests which actually utilize a browser.
    
    Will produce videos as artifacts.
    
    Can also be run locally.
    
    Uses an environment variable for logging in.

---
+++ cypress/support/commands.js
@@ -0,0 +1,29 @@
+Cypress.Commands.add('login', (sanitySessionToken) => {
+  const token = sanitySessionToken ? sanitySessionToken : Cypress.env('SANITY_SESSION_TOKEN')
+
+  cy.intercept({url: 'v1/users/me', method: 'GET'}).as('getUser')
+
+  return cy.visit('/').then(() => {
+    cy.wait('@getUser').then((interception) => {
+      const domain = new URL(interception.response.url).hostname
+
+      cy.setCookie('sanitySession', token, {
+        secure: true,
+        httpOnly: true,
+        sameSite: 'None',
+        domain: `.${domain}`,
+      })
+    })
+  })
+})
+
+Cypress.Commands.add('getField', (fieldName) => {
+  // TODO(@benedicteb, 2021-01-26) Add <data-qa=..." /> or something to html to make select super robust
+  return cy.get(`[data-focus-path=${fieldName}]`)
+})
+
+Cypress.Commands.add('getFieldInput', (fieldName) => {
+  return cy.getField(fieldName).within(($field) => {
+    return cy.get('input')
+  })
+})
