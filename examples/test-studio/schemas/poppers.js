769c2dcc36e01b8c451927ccd8e211733480fca1
Marius Lundgård <studio@mariuslundgard.com>
2021-01-29 12:51:50 +0100

    [examples] Add more examples of poppers

--- examples/test-studio/schemas/poppers.js
+++ examples/test-studio/schemas/poppers.js
@@ -1,3 +1,61 @@
+const objectsWithReference = {
+  type: 'array',
+  name: 'objectsWithReference',
+  title: 'Objects with reference',
+  of: [
+    {
+      type: 'object',
+      name: 'objectWithReference',
+      title: 'Object with reference',
+      fields: [
+        {type: 'string', name: 'title', title: 'Title'},
+        {type: 'array', name: 'primitives', title: 'Primitives', of: [{type: 'string'}]},
+        {type: 'text', name: 'description', title: 'Description', rows: 20},
+        {type: 'reference', name: 'reference', title: 'Reference', to: [{type: 'book'}]},
+      ],
+    },
+  ],
+}
+
+const arraysInArrays = {
+  type: 'array',
+  name: 'arraysInArrays',
+  title: 'Arrays in arrays',
+  of: [
+    {
+      type: 'object',
+      name: 'object',
+      title: 'Object',
+      fields: [
+        {
+          type: 'string',
+          name: 'title',
+          title: 'Title',
+        },
+        {
+          type: 'array',
+          name: 'array',
+          title: 'Arrays in arrays',
+          of: [
+            {
+              type: 'object',
+              name: 'object',
+              title: 'Object',
+              fields: [
+                {
+                  type: 'string',
+                  name: 'title',
+                  title: 'Title',
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    },
+  ],
+}
+
 export default {
   type: 'document',
   name: 'poppers',
@@ -8,22 +66,8 @@ export default {
       name: 'title',
       title: 'Title',
     },
-    {
-      type: 'array',
-      name: 'objectsWithReference',
-      title: 'Objects with reference',
-      of: [
-        {
-          type: 'object',
-          name: 'objectWithReference',
-          title: 'Object with reference',
-          fields: [
-            {type: 'string', name: 'title', title: 'Title'},
-            {type: 'text', name: 'description', title: 'Description', rows: 20},
-            {type: 'reference', name: 'reference', title: 'Reference', to: [{type: 'book'}]},
-          ],
-        },
-      ],
-    },
+    {type: 'array', name: 'primitives', title: 'Primitives', of: [{type: 'string'}]},
+    objectsWithReference,
+    arraysInArrays,
   ],
 }
