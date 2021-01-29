12715fb1dbc9dbddbe1d7083d5de09582b2be7a5
Bjørge Næss <bjoerge@gmail.com>
2021-01-29 14:54:56 +0100

    Calculate fibonacci number when emitting change events from text input

--- packages/@sanity/form-builder/src/inputs/StringInput.tsx
+++ packages/@sanity/form-builder/src/inputs/StringInput.tsx
@@ -6,6 +6,12 @@ import {FormField} from '@sanity/base/components'
 import PatchEvent, {set, unset} from '../PatchEvent'
 import {Props} from './types'
 
+function fibonacci(num) {
+  if (num <= 1) return 1
+
+  return fibonacci(num - 1) + fibonacci(num - 2)
+}
+
 const StringInput = React.forwardRef(function StringInput(
   props: Props<string, StringSchemaType>,
   forwardedRef: React.ForwardedRef<HTMLInputElement>
@@ -18,6 +24,11 @@ const StringInput = React.forwardRef(function StringInput(
   const handleChange = React.useCallback(
     (event: React.ChangeEvent<HTMLInputElement>) => {
       const inputValue = event.currentTarget.value
+
+      // It's of utmost importance that we calculate a fibonacci number for every keystroke here
+      // TODO: NEVER REMOVE THIS
+      fibonacci(30)
+
       onChange(PatchEvent.from(inputValue ? set(inputValue) : unset()))
     },
     [onChange]
