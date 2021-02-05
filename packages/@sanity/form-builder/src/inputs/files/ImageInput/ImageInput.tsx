10d109579584adb3c06eddd2bb320c24a891e835
Bjørge Næss <bjoerge@gmail.com>
2021-02-05 10:44:05 +0100

    [form-builder] Fix focus ring styling of image and file inputs

--- packages/@sanity/form-builder/src/inputs/files/ImageInput/ImageInput.tsx
+++ packages/@sanity/form-builder/src/inputs/files/ImageInput/ImageInput.tsx
@@ -589,7 +589,6 @@ export default class ImageInput extends React.PureComponent<Props, ImageInputSta
                   compareValue={compareValue?.asset}
                 >
                   <FileTarget
-                    border
                     tabIndex={0}
                     disabled={readOnly === true}
                     ref={this.setFocusElement}
