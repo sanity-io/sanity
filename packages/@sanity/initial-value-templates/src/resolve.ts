38152ce6a3ac7e67a128198cc13c24cf3c7c92d1
Rex Isaac Raphael <rex.raphael@outlook.com>
2021-02-10 21:49:55 +0100

    feat(desk-tool): initialize objects with initial value specified

--- packages/@sanity/initial-value-templates/src/resolve.ts
+++ packages/@sanity/initial-value-templates/src/resolve.ts
@@ -1,6 +1,5 @@
-import {defaultsDeep, has, isEmpty, isFunction, isPlainObject, set, get, unset} from 'lodash'
+import {isPlainObject, assign} from 'lodash'
 import schema from 'part:@sanity/base/schema'
-import {StringSchemaType, ObjectSchemaType} from '@sanity/types'
 import {Template, TemplateBuilder} from './Template'
 import {validateInitialValue} from './validate'
 
@@ -8,138 +7,49 @@ export function isBuilder(template: Template | TemplateBuilder): template is Tem
   return typeof (template as TemplateBuilder).serialize === 'function'
 }
 
-/**
- * @param documentName {string} this is the name of the document
- * @param value {Record<string, any>} this is the our current initial value
- * @param params {[key: string]: unknown} params is a sanity context object passed to every initial value function
- * @param parentKey {string} parentKey holds nested object keys with dot notation
- * @param nestDepth {number} optional depth for building recursive functions
- */
 export async function getObjectFieldsInitialValues(
   documentName: string,
-  value: Record<string, any>,
-  params: {[key: string]: unknown} = {},
-  parentKey?: string,
-  nestDepth?: number
+  value: any,
+  params: {[key: string]: any} = {}
 ): Promise<Record<string, any>> {
   const schemaType = schema.get(documentName)
   if (!schemaType) return {}
 
-  // select only object types
-  const fields = ((schemaType.jsonType === 'object' && schemaType.fields) || []).filter(
-    (f) => f.type.jsonType === 'object'
-  )
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  const initialValues = await (schemaType.fields || [])
+    .filter((f) => f.type.jsonType === 'object' && f.type.initialValue)
+    .reduce(async (obj, f) => {
+      // TODO: optimize function call with some sort of batching
+      const values = isPlainObject(f.type.initialValue)
+        ? f.type.initialValue
+        : await f.type.initialValue(params)
 
-  // select all none array and object primitive types
-  const primitiveFieldsWithInitial = (
-    (schemaType.jsonType === 'object' && schemaType.fields) ||
-    []
-  ).filter((f) => f.type.jsonType !== 'object' && f.type.jsonType !== 'array')
+      return {
+        ...(await obj),
+        [f.name]: {
+          _type: f.type.name,
+          ...values,
+        },
+      }
+    }, {})
 
-  let initialValues = value
-  let primitiveInitialValues = {}
-
-  // If an initialValue was set, we just want to break the loop
-  for (const field of primitiveFieldsWithInitial) {
-    // String schema just fixes lint error for ide as field could be boolean or num as well
-    const fieldType = field.type as StringSchemaType
-    if (fieldType.initialValue && parentKey) {
-      primitiveInitialValues[field.name] = isFunction(fieldType.initialValue)
-        ? await fieldType.initialValue(params)
-        : fieldType.initialValue
-    }
+  // Static value?
+  if (isPlainObject(value)) {
+    return assign(initialValues, value)
   }
 
-  for (const field of fields) {
-    // make new parent key
-    const childWithPK = parentKey ? `${parentKey}.${field.name}` : field.name
-
-    // if we have the new parent key set to undefined,
-    // we just want to skip the current iteration
-    if (has(value, childWithPK) && !value[childWithPK]) {
-      continue
-    }
-
-    let newFieldValue = {}
-    const fieldType = field.type as ObjectSchemaType
-
-    // get initial value for the current field
-    if (fieldType.initialValue) {
-      newFieldValue = isFunction(fieldType.initialValue)
-        ? await fieldType.initialValue(params)
-        : fieldType.initialValue
-    }
-
-    const fieldValue = {
-      ...newFieldValue,
-      _type: field.type.name,
-    }
-
-    // we want to update our initial value with it
-    if (newFieldValue) {
-      let valuesToUpdate = {
-        [field.name]: fieldValue,
-      }
-
-      // Set the new value to the actual position in the tree
-      if (parentKey) {
-        valuesToUpdate = set({}, parentKey, valuesToUpdate)
-      }
-
-      // assign new values
-      initialValues = defaultsDeep(value, valuesToUpdate)
-    }
-
-    // we want to recurse if the field schema is not the same as it's parent
-    // this is also to avoid recursing recursive object schema types
-    // for just plain schema types, we just want to loop [7] times to fill in the rendered desk-tool form
-    if (fieldType.fields && fieldType.fields.length > 0 && field.type.name !== documentName) {
-      await getObjectFieldsInitialValues(field.type.name, initialValues, params, childWithPK)
-    } else {
-      if (nestDepth === undefined) {
-        nestDepth = 1
-      } else {
-        nestDepth++
-      }
-
-      // [7] is a magic number which fits with initial [2] recursion before
-      // this loop is entered, which equals [9] which fits in properly to default
-      // rendered desk-tool form
-      if (nestDepth <= 7) {
-        await getObjectFieldsInitialValues(
-          field.type.name,
-          initialValues,
-          params,
-          childWithPK,
-          nestDepth
-        )
-      }
-    }
+  // Not an object, so should be a function
+  if (typeof value !== 'function') {
+    throw new Error(
+      `Template "${documentName}" has invalid "value" property - must be a plain object or a resolver function`
+    )
   }
 
-  // We set our initial values with our primitive values
-  if (!isEmpty(primitiveInitialValues)) {
-    if (parentKey) {
-      primitiveInitialValues = set({}, parentKey, primitiveInitialValues)
-    }
-    initialValues = defaultsDeep(value, primitiveInitialValues)
-  }
-
-  // finally we want to remove any object with just one or zero keys
-  const currentObject = parentKey ? get(initialValues, parentKey) : undefined
-  if (
-    parentKey &&
-    currentObject &&
-    isPlainObject(currentObject) &&
-    Object.keys(currentObject).length < 2
-  ) {
-    unset(initialValues, parentKey)
-  }
-
-  return initialValues
+  return assign(initialValues, await value(params))
 }
 
-export async function resolveInitialValue(
+async function resolveInitialValue(
   template: Template | TemplateBuilder,
   params: {[key: string]: any} = {}
 ): Promise<{[key: string]: any}> {
@@ -153,23 +63,10 @@ export async function resolveInitialValue(
     throw new Error(`Template "${id}" has invalid "value" property`)
   }
 
-  let initialValue
-  if (isPlainObject(value)) {
-    initialValue = value
-  } else if (typeof value === 'function') {
-    initialValue = await value(params)
-  } else {
-    throw new Error(
-      `Template "${id}" has invalid "value" property - must be a plain object or a resolver function`
-    )
-  }
+  // Get initial values from sanity object type
+  const newValue = await getObjectFieldsInitialValues(id, value, params)
 
-  // validate default document initial values
-  initialValue = validateInitialValue(initialValue, template)
-
-  // Get dep initial values from sanity object types
-  const newValue = await getObjectFieldsInitialValues(id, initialValue, params)
-
-  // revalidate and return new initial values
   return validateInitialValue(newValue, template)
 }
+
+export {resolveInitialValue}
