bf18231ce8d182a6367474c60518ed6da6ac639e
Rex Isaac Raphael <rex.raphael@outlook.com>
2021-02-10 21:49:55 +0100

    feat(desk-tool): initialize objects with initial value specified

--- packages/@sanity/initial-value-templates/src/resolve.ts
+++ packages/@sanity/initial-value-templates/src/resolve.ts
@@ -1,9 +1,52 @@
-import {isPlainObject} from 'lodash'
+import {isPlainObject, assign} from 'lodash'
+import schema from 'part:@sanity/base/schema'
 import {Template, TemplateBuilder} from './Template'
 import {validateInitialValue} from './validate'
 
 export function isBuilder(template: Template | TemplateBuilder): template is TemplateBuilder {
   return typeof (template as TemplateBuilder).serialize === 'function'
+}
+
+export async function getObjectFieldsInitialValues(
+  documentName: string,
+  value: any,
+  params: {[key: string]: any} = {}
+): Promise<Record<string, any>> {
+  const schemaType = schema.get(documentName)
+  if (!schemaType) return {}
+
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  const initialValues = await (schemaType.fields || [])
+    .filter((f) => f.type.jsonType === 'object' && f.type.initialValue)
+    .reduce(async (obj, f) => {
+      // TODO: optimize function call with some sort of batching
+      const values = isPlainObject(f.type.initialValue)
+        ? f.type.initialValue
+        : await f.type.initialValue(params)
+
+      return {
+        ...(await obj),
+        [f.name]: {
+          _type: f.type.name,
+          ...values,
+        },
+      }
+    }, {})
+
+  // Static value?
+  if (isPlainObject(value)) {
+    return assign(initialValues, value)
+  }
+
+  // Not an object, so should be a function
+  if (typeof value !== 'function') {
+    throw new Error(
+      `Template "${documentName}" has invalid "value" property - must be a plain object or a resolver function`
+    )
+  }
+
+  return assign(initialValues, await value(params))
 }
 
 async function resolveInitialValue(
@@ -20,20 +63,10 @@ async function resolveInitialValue(
     throw new Error(`Template "${id}" has invalid "value" property`)
   }
 
-  // Static value?
-  if (isPlainObject(value)) {
-    return validateInitialValue(value, template)
-  }
+  // Get initial values from sanity object type
+  const newValue = await getObjectFieldsInitialValues(id, value, params)
 
-  // Not an object, so should be a function
-  if (typeof value !== 'function') {
-    throw new Error(
-      `Template "${id}" has invalid "value" property - must be a plain object or a resolver function`
-    )
-  }
-
-  const resolved = await value(params)
-  return validateInitialValue(resolved, template)
+  return validateInitialValue(newValue, template)
 }
 
 export {resolveInitialValue}
