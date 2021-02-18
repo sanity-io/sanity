5c2f8745c31bea695925426960c7759be9d16333
Bjørge Næss <bjoerge@gmail.com>
2021-02-18 11:13:02 +0100

    perf(form-builder): integrate perf fixes from main

--- packages/@sanity/form-builder/src/sanity/inputs/SanityImageInput.tsx
+++ packages/@sanity/form-builder/src/sanity/inputs/SanityImageInput.tsx
@@ -12,30 +12,31 @@ const SUPPORT_DIRECT_UPLOADS = formBuilderConfig?.images?.directUploads !== fals
 
 type Props = Omit<React.ComponentProps<typeof ImageInput>, 'assetSources'>
 
-export default withDocument(
-  React.forwardRef(function SanityImageInput(props: Props, forwardedRef: any) {
-    const sourcesFromSchema = props.type.options?.sources
+export default React.forwardRef(function SanityImageInput(props: Props, forwardedRef: any) {
+  const sourcesFromSchema = props.type.options?.sources
 
-    // note: type.options.sources may be an empty array and in that case we're
-    // disabling selecting images from asset source  (it's a feature, not a bug)
-    const assetSources = React.useMemo(
-      () =>
-        (sourcesFromSchema || globalAssetSources).map((source) => ({
-          ...source,
-          component: withDocument(source.component),
-        })),
-      [sourcesFromSchema]
-    )
+  // note: type.options.sources may be an empty array and in that case we're
+  // disabling selecting images from asset source  (it's a feature, not a bug)
+  const assetSources = React.useMemo(
+    () =>
+      (sourcesFromSchema || globalAssetSources).map((source) => ({
+        ...source,
+        // Note: The asset source plugin get's passed the enclosing document by default.
+        // This is a potential performance hog, so we should consider some alternatives here
+        // e.g. we could offer a way to declare the path for the values in the document you're interested in instead
+        component: withDocument(source.component),
+      })),
+    [sourcesFromSchema]
+  )
 
-    return (
-      <ImageInput
-        {...props}
-        resolveUploader={resolveUploader}
-        materialize={materializeReference}
-        assetSources={assetSources}
-        directUploads={SUPPORT_DIRECT_UPLOADS}
-        ref={forwardedRef}
-      />
-    )
-  })
-)
+  return (
+    <ImageInput
+      {...props}
+      resolveUploader={resolveUploader}
+      materialize={materializeReference}
+      assetSources={assetSources}
+      directUploads={SUPPORT_DIRECT_UPLOADS}
+      ref={forwardedRef}
+    />
+  )
+})
