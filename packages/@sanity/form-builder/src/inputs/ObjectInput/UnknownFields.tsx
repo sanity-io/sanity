0402bbc2bfcc540defc699c6be6dc2ed6fe4f617
Marius Lundgård <studio@mariuslundgard.com>
2021-02-18 15:21:41 +0100

    refactor(form-builder): Migrate UnknownFields to `@sanity/ui`

--- packages/@sanity/form-builder/src/inputs/ObjectInput/UnknownFields.tsx
+++ packages/@sanity/form-builder/src/inputs/ObjectInput/UnknownFields.tsx
@@ -1,68 +1,135 @@
-import React from 'react'
-import {Button, Code, Heading, Text} from '@sanity/ui'
+import React, {useCallback} from 'react'
+import {Box, Button, Card, Code, Stack, Text} from '@sanity/ui'
+import {TrashIcon} from '@sanity/icons'
 import {Details} from '../../components/Details'
 import PatchEvent, {unset} from '../../PatchEvent'
 import {ActivateOnFocus} from '../../transitional/ActivateOnFocus'
-import styles from './styles/UnknownFields.css'
+import {Alert} from '../../components/Alert'
+
+declare const __DEV__: boolean
 
 type Props = {
   fieldNames: string[]
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   value: Record<string, any>
   onChange: (event: PatchEvent) => void
   readOnly?: boolean
 }
 
-export default class UnknownFields extends React.PureComponent<Props> {
-  handleUnsetClick = (fieldName) => {
-    this.props.onChange(PatchEvent.from(unset([fieldName])))
-  }
+export function UnknownFields(props: Props) {
+  const {fieldNames, onChange, readOnly, value} = props
+  const fieldsLen = fieldNames.length
 
-  render() {
-    const {fieldNames, value, readOnly} = this.props
-    const len = fieldNames.length
+  const handleUnsetClick = useCallback(
+    (fieldName) => {
+      onChange(PatchEvent.from(unset([fieldName])))
+    },
+    [onChange]
+  )
 
-    return (
-      <div className={styles.root}>
-        <h2 className={styles.heading}>
-          Found {len === 1 ? <>an</> : len} unknown {len === 1 ? <>field</> : <>fields</>}
-        </h2>
+  return (
+    <Alert
+      status="warning"
+      title={
+        <>
+          {fieldsLen === 1 && <>This document contains an unknown property</>}
+          {fieldsLen > 1 && <>This document contains unknown properties</>}
+        </>
+      }
+    >
+      <Text as="p" muted size={1}>
+        {fieldsLen === 1 && (
+          <>Encountered a property that is not defined in the document’s schema.</>
+        )}
+        {fieldsLen > 1 && (
+          <>Encountered {fieldsLen} properties that are not defined in the document’s schema.</>
+        )}
+      </Text>
 
-        <Details>
-          <Text as="p">
-            These are not defined in the current schema as valid fields for this value. This could
-            mean that the field has been removed, or that someone else has added it to their own
-            local schema that is not yet deployed.
-          </Text>
+      <Details marginTop={4} open={__DEV__} title={<>Developer info</>}>
+        <Box marginBottom={3}>
+          {fieldsLen === 1 && (
+            <Text as="p" muted size={1}>
+              This field is not defined in the document’s schema, which could mean that the field
+              definition has been removed or that someone else has added it to their own local
+              project and have not deployed their changes yet.
+            </Text>
+          )}
 
+          {fieldsLen > 1 && (
+            <Text as="p" muted size={1}>
+              These fields are not defined in the document’s schema, which could mean that the field
+              definitions have been removed or that someone else has added them to their own local
+              project and have not deployed their changes yet.
+            </Text>
+          )}
+        </Box>
+
+        <Stack as="ul" space={3}>
           {fieldNames.map((fieldName) => {
             return (
-              <div key={fieldName}>
-                <Heading as="h4">{fieldName}</Heading>
-
-                <ActivateOnFocus>
-                  <Code className={styles.inspectValue}>
-                    {JSON.stringify(value[fieldName], null, 2)}
-                  </Code>
-                </ActivateOnFocus>
-
-                {readOnly ? (
-                  <Text as="p">
-                    This value is <em>read only</em> according to its enclosing schema type and
-                    cannot be unset. If you want to unset, make sure you remove the{' '}
-                    <strong>readOnly</strong> property from the enclosing type
-                  </Text>
-                ) : (
-                  <Button
-                    onClick={() => this.handleUnsetClick(fieldName)}
-                    tone="critical"
-                    text={`Unset ${fieldName}`}
-                  />
-                )}
-              </div>
+              <UnknownField
+                key={fieldName}
+                fieldName={fieldName}
+                onUnsetClient={handleUnsetClick}
+                readOnly={readOnly}
+                value={value[fieldName]}
+              />
             )
           })}
-        </Details>
-      </div>
-    )
-  }
+        </Stack>
+      </Details>
+    </Alert>
+  )
+}
+
+function UnknownField({
+  fieldName,
+  onUnsetClient,
+  readOnly,
+  value,
+}: {
+  fieldName: string
+  // eslint-disable-next-line no-shadow
+  onUnsetClient: (fieldName: string) => void
+  readOnly: boolean
+  value: any
+}) {
+  const handleUnsetClick = useCallback(() => {
+    onUnsetClient(fieldName)
+  }, [fieldName, onUnsetClient])
+
+  return (
+    <Card as="li" padding={3} radius={2} shadow={1} tone="caution">
+      <Stack space={2}>
+        <Card border radius={1}>
+          <ActivateOnFocus>
+            <Card borderBottom padding={3}>
+              <Code weight="medium">{fieldName}</Code>
+            </Card>
+            <Box padding={3}>
+              <Code language="json">{JSON.stringify(value, null, 2)}</Code>
+            </Box>
+          </ActivateOnFocus>
+        </Card>
+
+        {readOnly && (
+          <Text as="p" muted size={1}>
+            This property is <strong>read only</strong> according to the document’s schema and
+            cannot be unset. If you want to be able to unset this in Studio, make sure you remove
+            the <code>readOnly</code> property from the enclosing type in the schema.
+          </Text>
+        )}
+
+        {!readOnly && (
+          <Button
+            icon={TrashIcon}
+            onClick={handleUnsetClick}
+            tone="critical"
+            text={<>Remove property</>}
+          />
+        )}
+      </Stack>
+    </Card>
+  )
 }
