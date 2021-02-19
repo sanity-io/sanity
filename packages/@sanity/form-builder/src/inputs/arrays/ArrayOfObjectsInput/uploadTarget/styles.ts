668f154841475b347f9432753790de46d8fc2b47
Bjørge Næss <bjoerge@gmail.com>
2021-02-19 09:30:35 +0100

    fix(form-builder): restore uploadTarget for arrays

--- packages/@sanity/form-builder/src/inputs/arrays/ArrayOfObjectsInput/uploadTarget/styles.ts
+++ packages/@sanity/form-builder/src/inputs/arrays/ArrayOfObjectsInput/uploadTarget/styles.ts
@@ -5,10 +5,11 @@ export const Overlay: React.ComponentType = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
+  top: -2px;
+  left: -2px;
+  right: -2px;
+  bottom: -2px;
   background-color: var(--card-bg-color);
   z-index: 3;
+  opacity: 0.8;
 `
