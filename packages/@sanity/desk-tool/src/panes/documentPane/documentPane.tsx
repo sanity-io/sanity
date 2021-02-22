fc39e8419b7cd5c4c59cc19567520bef25a49e9a
Marius Lundgård <studio@mariuslundgard.com>
2021-02-22 14:25:15 +0100

    fix(desk-tool): improve z-offsets
    * Make sure the pane header and footer are always on top of pane content

--- packages/@sanity/desk-tool/src/panes/documentPane/documentPane.tsx
+++ packages/@sanity/desk-tool/src/panes/documentPane/documentPane.tsx
@@ -237,13 +237,15 @@ export function DocumentPane(props: DocumentPaneProps) {
           )}
         </ChangeConnectorRoot>
 
-        <Layer className={styles.footerContainer}>
-          <DocumentStatusBar
-            id={documentId}
-            type={documentType}
-            lastUpdated={value && value._updatedAt}
-          />
-        </Layer>
+        <LegacyLayerProvider zOffset="paneFooter">
+          <Layer className={styles.footerContainer}>
+            <DocumentStatusBar
+              id={documentId}
+              type={documentType}
+              lastUpdated={value && value._updatedAt}
+            />
+          </Layer>
+        </LegacyLayerProvider>
 
         {connectionState === 'reconnecting' && (
           <Snackbar kind="warning" isPersisted title="Connection lost. Reconnecting…" />
@@ -251,14 +253,16 @@ export function DocumentPane(props: DocumentPaneProps) {
 
         <DocumentOperationResults id={documentId} type={documentType} />
 
-        <TimelinePopover
-          onClose={handleTimelineClose}
-          open={isTimelineOpen}
-          placement="bottom"
-          targetElement={
-            timelineMode === 'rev' ? versionSelectRef.current : changesSinceSelectRef.current
-          }
-        />
+        <LegacyLayerProvider zOffset="paneHeader">
+          <TimelinePopover
+            onClose={handleTimelineClose}
+            open={isTimelineOpen}
+            placement="bottom"
+            targetElement={
+              timelineMode === 'rev' ? versionSelectRef.current : changesSinceSelectRef.current
+            }
+          />
+        </LegacyLayerProvider>
       </DocumentActionShortcuts>
     </LegacyLayerProvider>
   )
