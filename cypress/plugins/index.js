1f35fe18eb96fed43c0171c13fdc1cbc0e11ac0d
Bjørge Næss <bjoerge@gmail.com>
2021-03-02 21:07:06 +0100

    ci(cypress): read env vars from local .env

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,3 +1,5 @@
+const dotenvPlugin = require('cypress-dotenv')
+
 /// <reference types="cypress" />
 // ***********************************************************
 // This example plugins/index.js can be used to load plugins
@@ -15,7 +17,9 @@
 /**
  * @type {Cypress.PluginConfig}
  */
+
 module.exports = (on, config) => {
+  return dotenvPlugin(config)
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
 }
