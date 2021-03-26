39d4f1a12fceff9d29b089b7c7c6e62ac5be51ef
Bjørge Næss <bjoerge@gmail.com>
2021-03-26 12:51:56 +0100

    feat(core): minify using esbuild --compress

--- packages/@sanity/core/src/actions/build/compressJavascript.js
+++ packages/@sanity/core/src/actions/build/compressJavascript.js
@@ -3,7 +3,7 @@ import execa from 'execa'
 import fse from 'fs-extra'
 import resolveBin from 'resolve-bin'
 
-export default async (inputFile) => {
+async function minifyTerser(inputFile) {
   const terserBin = resolveBin.sync('terser')
   if (!terserBin) {
     throw new Error(`Can't find terser binary, cannot compress bundles`)
@@ -18,3 +18,25 @@ export default async (inputFile) => {
   await fse.unlink(inputFile)
   await fse.move(outPath, inputFile)
 }
+
+async function minifyEsbuild(inputFile) {
+  const esbuildBin = resolveBin.sync('esbuild')
+  if (!esbuildBin) {
+    throw new Error(`Can't find esbuild binary, cannot compress bundles`)
+  }
+
+  const outPath = `${inputFile}.min`
+  const {stderr, stdout, code} = await execa(esbuildBin, [
+    inputFile,
+    '--minify',
+    `--outfile=${outPath}`,
+  ])
+  if (code > 0) {
+    throw new Error(`Failed to minify bundle (${path.basename(inputFile)}):\n\n${stderr || stdout}`)
+  }
+
+  await fse.unlink(inputFile)
+  await fse.move(outPath, inputFile)
+}
+
+export default minifyEsbuild
