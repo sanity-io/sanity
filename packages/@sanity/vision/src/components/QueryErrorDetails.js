197534a5e40990f1837edcf627c9629d42de2dc0
Espen Hovlandsdal <espen@hovlandsdal.com>
2021-04-01 19:03:45 -0700

    fix(vision): extract and display line/column info on errors

--- packages/@sanity/vision/src/components/QueryErrorDetails.js
+++ packages/@sanity/vision/src/components/QueryErrorDetails.js
@@ -2,15 +2,15 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 function QueryErrorDetails(props) {
-  const details = props.error.details
-  if (!details || !details.line) {
+  const details = {...props.error.details, ...mapToLegacyDetails(props.error.details)}
+  if (!details.line) {
     return null
   }
 
   return (
     <div>
       <pre>
-        <code>{`${details.line}\n${dashLine(details.column)}`}</code>
+        <code>{`${details.line}\n${dashLine(details.column, details.columnEnd)}`}</code>
       </pre>
       <pre>
         <code>{`Line:   ${details.lineNumber}\nColumn: ${details.column}`}</code>
@@ -19,9 +19,25 @@ function QueryErrorDetails(props) {
   )
 }
 
-function dashLine(ln) {
-  const line = new Array(ln + 1).join('-')
-  return `${line}^`
+function mapToLegacyDetails(details) {
+  if (!details || typeof details.query !== 'string' || typeof details.start !== 'number') {
+    return {}
+  }
+
+  const {query, start, end} = details
+  const lineStart = query.slice(0, start).lastIndexOf('\n') + 1
+  const lineNumber = (query.slice(0, lineStart).match(/\n/g) || []).length
+  const line = query.slice(lineStart, query.indexOf('\n', lineStart))
+  const column = start - lineStart
+  const columnEnd = typeof end === 'number' ? end - lineStart : undefined
+
+  return {line, lineNumber, column, columnEnd}
+}
+
+function dashLine(column, columnEnd) {
+  const line = '-'.repeat(column)
+  const hats = `^`.repeat(columnEnd ? columnEnd - column : 1)
+  return `${line}${hats}`
 }
 
 QueryErrorDetails.propTypes = {
@@ -29,7 +45,10 @@ QueryErrorDetails.propTypes = {
     details: PropTypes.shape({
       line: PropTypes.string,
       lineNumber: PropTypes.number,
-      column: PropTypes.column,
+      column: PropTypes.number,
+      start: PropTypes.number,
+      end: PropTypes.number,
+      query: PropTypes.string,
     }),
   }),
 }
