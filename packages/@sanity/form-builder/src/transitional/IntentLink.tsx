7c5cadb11a8b57b5b043576cd01e6c050d0343ae
Bjørge Næss <bjoerge@gmail.com>
2021-04-08 11:38:49 +0200

    chore(form-builder): use strict mode for transitional components

--- packages/@sanity/form-builder/src/transitional/IntentLink.tsx
+++ packages/@sanity/form-builder/src/transitional/IntentLink.tsx
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, {ComponentProps} from 'react'
 import {useRouter} from '../legacyParts'
 
-export function IntentLink(props) {
+export function IntentLink(
+  props: Omit<ComponentProps<'a'>, 'href'> & {intent: string; params: Record<string, string>}
+) {
   const router = useRouter()
 
   return <a {...props} href={router.resolveIntentLink(props.intent, props.params)} />
