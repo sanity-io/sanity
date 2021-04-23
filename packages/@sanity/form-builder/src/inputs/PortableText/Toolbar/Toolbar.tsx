1bd6332831ce36692ba87a3ea8a9927ae24a9441
Bjørge Næss <bjoerge@gmail.com>
2021-04-23 12:37:30 +0200

    feat(form-builder): add support for initial values in portable text editor

--- packages/@sanity/form-builder/src/inputs/PortableText/Toolbar/Toolbar.tsx
+++ packages/@sanity/form-builder/src/inputs/PortableText/Toolbar/Toolbar.tsx
@@ -7,17 +7,23 @@ import {
   RenderBlockFunction,
   usePortableTextEditor,
   usePortableTextEditorSelection,
+  Type,
   PortableTextEditor,
 } from '@sanity/portable-text-editor'
 import classNames from 'classnames'
-import React from 'react'
-import {Path} from '@sanity/types'
+import React, {useCallback} from 'react'
+import {Path, SchemaType} from '@sanity/types'
+import {FOCUS_TERMINATOR} from '@sanity/util/paths'
+import {resolveInitialValueForType} from '@sanity/initial-value-templates'
+import {useToast} from '@sanity/ui'
 import ActionMenu from './ActionMenu'
 import BlockStyleSelect from './BlockStyleSelect'
 import InsertMenu from './InsertMenu'
 import {getBlockStyleSelectProps, getInsertMenuItems, getPTEToolbarActionGroups} from './helpers'
 
 import styles from './Toolbar.css'
+
+const SLOW_INITIAL_VALUE_LIMIT = 300
 
 interface Props {
   hotkeys: HotkeyOptions
@@ -32,18 +38,95 @@ function PTEToolbar(props: Props) {
   const editor = usePortableTextEditor()
   const selection = usePortableTextEditorSelection()
   const disabled = !selection
+
+  const toast = useToast()
+
+  const resolveInitialValue = useCallback(
+    (type: Type) => {
+      let isSlow = false
+      const slowTimer = setTimeout(() => {
+        isSlow = true
+        toast.push({
+          id: 'resolving-initial-value',
+          status: 'info',
+          title: 'Resolving initial value…',
+        })
+      }, SLOW_INITIAL_VALUE_LIMIT)
+      return resolveInitialValueForType((type as any) as SchemaType)
+        .then((value) => {
+          if (isSlow) {
+            // I found no way to close an existing toast, so this will replace the message in the
+            // "Resolving initial value…"-toast and then make sure it gets closed.
+            toast.push({
+              id: 'resolving-initial-value',
+              status: 'info',
+              duration: 500,
+              title: 'Initial value resolved',
+            })
+          }
+          return value
+        })
+        .catch((error) => {
+          toast.push({
+            title: `Could not resolve initial value`,
+            id: 'resolving-initial-value',
+            description: `Unable to resolve initial value for type: ${type.name}: ${error.message}.`,
+            status: 'error',
+          })
+          return undefined
+        })
+        .finally(() => clearTimeout(slowTimer))
+    },
+    [toast]
+  )
+
+  const handleInsertBlock = useCallback(
+    async (type: Type) => {
+      const initialValue = await resolveInitialValue(type)
+      const path = PortableTextEditor.insertBlock(editor, type, initialValue)
+
+      setTimeout(() => onFocus(path.concat(FOCUS_TERMINATOR)), 0)
+    },
+    [editor, onFocus, resolveInitialValue]
+  )
+
+  const handleInsertInline = useCallback(
+    async (type: Type) => {
+      const initialValue = await resolveInitialValue(type)
+      const path = PortableTextEditor.insertChild(editor, type, initialValue)
+
+      setTimeout(() => onFocus(path.concat(FOCUS_TERMINATOR)), 0)
+    },
+    [editor, onFocus, resolveInitialValue]
+  )
+
+  const handleInsertAnnotation = useCallback(
+    async (type: Type) => {
+      const initialValue = await resolveInitialValue(type)
+
+      const paths = PortableTextEditor.addAnnotation(editor, type, initialValue)
+      if (paths && paths.markDefPath) {
+        onFocus(paths.markDefPath.concat(FOCUS_TERMINATOR))
+      }
+    },
+    [editor, onFocus, resolveInitialValue]
+  )
+
   const actionGroups = React.useMemo(
-    () => (editor ? getPTEToolbarActionGroups(editor, selection, onFocus, hotkeys) : []),
-    [editor, selection, onFocus, hotkeys]
+    () =>
+      editor ? getPTEToolbarActionGroups(editor, selection, handleInsertAnnotation, hotkeys) : [],
+    [editor, selection, handleInsertAnnotation, hotkeys]
   )
   const actionsLen = actionGroups.reduce((acc, x) => acc + x.actions.length, 0)
   const blockStyleSelectProps = React.useMemo(
     () => (editor ? getBlockStyleSelectProps(editor) : null),
-    [selection]
+    [editor]
   )
+
   const insertMenuItems = React.useMemo(
-    () => (editor ? getInsertMenuItems(editor, selection, onFocus) : []),
-    [selection]
+    () =>
+      editor ? getInsertMenuItems(editor, selection, handleInsertBlock, handleInsertInline) : [],
+    [editor, handleInsertBlock, handleInsertInline, selection]
   )
 
   return (
