7f8eaee41f09494bc7fb4b3bbe41d094d34c912b
Bjørge Næss <bjoerge@gmail.com>
2021-05-07 20:55:19 +0200

    make realtime

--- dev/test-studio/src/BuildSwitcher/BuildSwitcher.tsx
+++ dev/test-studio/src/BuildSwitcher/BuildSwitcher.tsx
@@ -13,14 +13,23 @@ import {
   Text,
 } from '@sanity/ui'
 import {defer} from 'rxjs'
-import {shareReplay} from 'rxjs/operators'
-import {ArrowTopRightIcon, ChevronRightIcon, CircleIcon} from '@sanity/icons'
+import {shareReplay, switchMapTo, throttleTime} from 'rxjs/operators'
+import {ArrowTopRightIcon, ChevronRightIcon, ArrowDownIcon, CircleIcon} from '@sanity/icons'
 import {metricsStudioClient} from './metricsClient'
 
-function fetchBuildHistory() {
-  return metricsStudioClient.fetch(
-    '*[_type=="branch"] | order(_updatedAt desc) [0..10] | {_id, name, "latestDeployments": *[_type == "deployment" && name=="test-studio" && references(^._id)] | order(_updatedAt desc)[0..10]}'
+function listenBuildHistory() {
+  const fetch$ = defer(() =>
+    metricsStudioClient.fetch(
+      '*[_type=="branch"] | order(_updatedAt desc) [0..10] | {_id, name, "latestDeployments": *[_type == "deployment" && name=="test-studio" && references(^._id)] | order(_updatedAt desc)[0..10]}'
+    )
   )
+  return defer(() =>
+    metricsStudioClient.listen(
+      `*[_type=="branch" || _type == "latestDeployments"]`,
+      {},
+      {events: ['mutation', 'welcome']}
+    )
+  ).pipe(throttleTime(50, undefined, {trailing: true}), switchMapTo(fetch$))
 }
 
 function getGithubUserUrl(username) {
@@ -35,6 +44,13 @@ const COLORS = {
   pending: '#f5a623',
   error: '#f46e64',
 }
+
+const BADGE_COLORS = {
+  success: 'positive',
+  pending: 'caution',
+  error: 'critical',
+} as const
+
 function getDeploymentStatusColor(deployment) {
   if (!deployment) {
     return undefined
@@ -42,7 +58,7 @@ function getDeploymentStatusColor(deployment) {
   if (deployment?.status === 'pending') {
     return COLORS.pending
   }
-  if (deployment?.status === 'build-complete') {
+  if (deployment?.status === 'ready') {
     return COLORS.success
   }
   if (deployment?.status === 'error') {
@@ -87,7 +103,7 @@ function getPath() {
   return document.location.pathname
 }
 
-const branches$ = defer(() => fetchBuildHistory()).pipe(
+const branches$ = defer(() => listenBuildHistory()).pipe(
   shareReplay({refCount: true, bufferSize: 1})
 )
 
@@ -110,7 +126,7 @@ export function BuildSwitcher() {
     branch.latestDeployments.find((deployment) => deployment?.url === document.location.hostname)
   )
 
-  const [isPopoverOpen, setPopoverOpen] = useState(true)
+  const [isPopoverOpen, setPopoverOpen] = useState(false)
   const [selectedBranch, setSelectedBranch] = useState(null)
 
   return (
@@ -126,30 +142,58 @@ export function BuildSwitcher() {
           id="menu-button-example"
           portal
           content={
-            <Box marginTop={4} marginBottom={2} marginX={2}>
-              <Heading size={1} as="h4">
-                Active branches
-              </Heading>
-              <Grid marginTop={2} columns={1} gap={2}>
-                {branches.map((branch) => {
-                  const latestDeployment = branch.latestDeployments[0]
-                  return (
-                    <Box key={branch.name} paddingY={1}>
-                      <Popover
-                        portal
-                        open={selectedBranch === branch._id}
-                        placement="right"
-                        content={
-                          <Box marginTop={4} marginBottom={2} marginX={2}>
-                            <Heading size={1} as="h4">
-                              Deployments
-                            </Heading>
-                            <Grid marginTop={2} columns={1} gap={2}>
-                              {branch.latestDeployments.map((build) => {
-                                return (
-                                  <Flex key={build._id} paddingY={1} paddingX={2}>
-                                    <Box>
-                                      <Text size={1}>
+            <Box marginBottom={2} marginX={2}>
+              {isLocal && (
+                <Box marginY={4}>
+                  <Button
+                    as="a"
+                    icon={ArrowDownIcon}
+                    tone="primary"
+                    mode="ghost"
+                    href="http://localhost:3333"
+                    text="Go to localhost"
+                  />
+                </Box>
+              )}
+              <Box marginTop={2} padding={1}>
+                <Text size={2} weight="semibold" muted>
+                  Branches
+                </Text>
+              </Box>
+              {branches.map((branch) => {
+                const latestDeployment = branch.latestDeployments[0]
+                return (
+                  <Box key={branch.name} paddingY={1}>
+                    <Popover
+                      portal
+                      open={selectedBranch === branch._id}
+                      placement="right"
+                      content={
+                        <Stack space={3} padding={3}>
+                          <Box padding={1}>
+                            <Text align="center" size={2} weight="semibold" muted>
+                              Latest deployments of <code>{branch.name}</code>
+                            </Text>
+                          </Box>
+                          {branch.latestDeployments.map((build) => {
+                            return (
+                              <Flex align="center" key={build._id} padding={2} gap={3}>
+                                <Stack flex={1} space={2}>
+                                  <Text weight="semibold">{build.meta.githubCommitMessage}</Text>
+                                  <Flex gap={2} align="center">
+                                    <Badge tone={getDeploymentStatusTone(build)}>
+                                      {getDeploymentStatusText(build)}
+                                    </Badge>
+                                    <Box flex={1}>
+                                      <Text size={1} muted>
+                                        <a
+                                          href={`https://github.com/${build.meta.githubCommitAuthorLogin}`}
+                                          target="_blank"
+                                          rel="noreferrer"
+                                        >
+                                          {build.meta.githubCommitAuthorLogin}
+                                        </a>{' '}
+                                        (
                                         <a
                                           href={getGithubCommitUrlFromMetaData(build.meta)}
                                           target="_blank"
@@ -178,57 +222,59 @@ export function BuildSwitcher() {
                                     </Box>
                                     <Box>
                                       <Inline space={2}>
-                                        <Button
-                                          as="a"
-                                          icon={ArrowTopRightIcon}
-                                          tone="primary"
-                                          mode="ghost"
-                                          href={`https://${build.url}${getPath()}`}
-                                          target="_blank"
-                                          rel="noreferrer"
-                                        />
+                                        {build.status === 'error' ? (
+                                          <Button
+                                            as="a"
+                                            icon={ArrowTopRightIcon}
+                                            tone="caution"
+                                            mode="ghost"
+                                            href={build.inspectorUrl}
+                                            title="Show error"
+                                          />
+                                        ) : (
+                                          <Button
+                                            as="a"
+                                            icon={ArrowTopRightIcon}
+                                            tone="primary"
+                                            mode="ghost"
+                                            href={`https://${build.url}${getPath()}`}
+                                            title="Open built studio"
+                                          />
+                                        )}
                                       </Inline>
                                     </Box>
                                   </Flex>
-                                )
-                              })}
-                            </Grid>
-                          </Box>
-                        }
-                      >
-                        <Flex paddingY={0} paddingX={2}>
-                          <Flex flex={1} direction="column" marginRight={2}>
-                            <Box>
-                              <Text weight="semibold">{branch.name}</Text>
-                            </Box>
-                            <Box marginTop={2} marginLeft={1}>
-                              <Text size={1} style={{marginLeft: '-0.4rem'}}>
-                                {branch.latestDeployments
-                                  .slice()
-                                  .reverse()
-                                  .map((deployment, i) => (
-                                    <CircleIcon
-                                      style={{
-                                        marginLeft: i > 0 ? '-0.6rem' : 0,
-                                        marginRight: '-0.3rem',
-                                      }}
-                                      key={`status-${deployment._id}`}
-                                      fill={getDeploymentStatusColor(deployment)}
-                                    />
-                                  ))}{' '}
-                                {latestDeployment && (
-                                  <>
-                                    {latestDeployment?.meta?.githubCommitAuthorLogin} (
-                                    <a
-                                      href={getGithubCommitUrlFromMetaData(latestDeployment?.meta)}
-                                    >
-                                      {latestDeployment.meta?.githubCommitSha.substring(0, 6)})
-                                    </a>
-                                  </>
-                                )}
-                              </Text>
-                            </Box>
-                          </Flex>
+                                </Stack>
+                                <Box>
+                                  <Inline space={2}>
+                                    {build.status === 'error' ? (
+                                      <Button
+                                        as="a"
+                                        icon={InfoOutlineIcon}
+                                        tone="critical"
+                                        mode="ghost"
+                                        href={build.inspectorUrl}
+                                        title="Show error"
+                                      />
+                                    ) : (
+                                      <Button
+                                        as="a"
+                                        icon={LaunchIcon}
+                                        mode="ghost"
+                                        href={`https://${build.url}${getPath()}`}
+                                        title="Switch to this deployed Studio"
+                                      />
+                                    )}
+                                  </Inline>
+                                </Box>
+                              </Flex>
+                            )
+                          })}
+                        </Stack>
+                      }
+                    >
+                      <Flex paddingY={0} paddingX={2}>
+                        <Flex flex={1} direction="column" marginRight={2}>
                           <Box>
                             <Text weight="semibold">{branch.name}</Text>
                           </Box>
@@ -291,7 +337,8 @@ export function BuildSwitcher() {
         >
           <Button
             type="button"
-            mode="bleed"
+            mode="ghost"
+            paddingY={2}
             onClick={(e) => {
               e.stopPropagation()
               e.preventDefault()
