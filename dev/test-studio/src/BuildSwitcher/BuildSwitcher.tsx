b61352bfb9a3d2636f2772ee62ffd6274364a347
Bjørge Næss <bjoerge@gmail.com>
2021-05-07 15:09:33 +0200

    status colors

--- dev/test-studio/src/BuildSwitcher/BuildSwitcher.tsx
+++ dev/test-studio/src/BuildSwitcher/BuildSwitcher.tsx
@@ -1,11 +1,26 @@
 import React, {useEffect, useState} from 'react'
-import {metricsStudioClient} from './metricsClient'
 import {MenuButton, Button, Menu, MenuItem, MenuDivider, Layer} from '@sanity/ui'
+import {CircleIcon, CheckmarkCircleIcon} from '@sanity/icons'
+import {metricsStudioClient} from './metricsClient'
+import {deployment} from '../../../metrics-studio/schemas/deployment'
 
 function fetchBuildHistory() {
   return metricsStudioClient.fetch(
     '*[_type=="branch"] | order(updatedAt desc) | {_id, name, "latestDeployment": *[_type == "deployment" && references(^._id)][0]}'
   )
+}
+
+function getDeploymentStatusColor(deployment: any) {
+  if (!deployment) {
+    return 'transparent'
+  }
+  if (deployment.status === 'pending') {
+    return '#ccc970'
+  }
+  if (deployment.status === 'completed') {
+    return '#70cc70'
+  }
+  return '#e53935'
 }
 
 export function BuildSwitcher() {
@@ -28,6 +43,7 @@ export function BuildSwitcher() {
     <MenuButton
       button={
         <Button
+          type="button"
           mode="bleed"
           text={isLocal ? <>Localhost</> : <>{currentBranch?.name || 'unknown branch'}</>}
         />
@@ -36,15 +52,18 @@ export function BuildSwitcher() {
       portal
       menu={
         <Menu>
-          {branches.map((branch) => (
-            <MenuItem
-              key={branch.name}
-              onClick={() =>
-                (document.location.href = `https://${branch.latestDeployment.url}${document.location.pathname}`)
-              }
-              text={<>{branch.name}</>}
-            />
-          ))}
+          {branches.map((branch) => {
+            return (
+              <MenuItem
+                icon={() => <CircleIcon fill={getDeploymentStatusColor(branch.latestDeployment)} />}
+                key={branch.name}
+                onClick={() =>
+                  (document.location.href = `https://${branch.latestDeployment.url}${document.location.pathname}`)
+                }
+                text={<>{branch.name}</>}
+              />
+            )
+          })}
         </Menu>
       }
     />
