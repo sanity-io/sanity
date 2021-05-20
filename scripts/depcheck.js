a98184c8306823f49f811bbdee3b7a7dd52a03d5
Bjørge Næss <bjoerge@gmail.com>
2021-05-20 23:34:34 +0200

    chore: add depcheck script to catch unused dependencies

---
+++ scripts/depcheck.js
@@ -0,0 +1,139 @@
+#!/usr/bin/env node
+
+const depcheck = require('depcheck')
+const path = require('path')
+const fs = require('fs')
+const chalk = require('chalk')
+
+const cwd = path.resolve(process.cwd(), process.argv[2] || '.')
+
+const MONOREPO_ROOT = process.env.LERNA_ROOT_PATH
+
+// eslint-disable-next-line import/no-dynamic-require
+const rootPkg = require(`${MONOREPO_ROOT}/package.json`)
+// eslint-disable-next-line import/no-dynamic-require
+const package = require(`${cwd}/package.json`)
+
+const options = {
+  ignoreMatches: [
+    '@types/jest',
+    'typescript-plugin-css-modules',
+    'ts-node',
+    'config:*',
+    'part:*',
+    'all:part:*',
+    'sanity:*',
+    ...getProjectIgnores(cwd),
+  ],
+  ignoreDirs: ['lib'],
+  detectors: [
+    depcheck.detector.exportDeclaration,
+    depcheck.detector.extract,
+    depcheck.detector.importCallExpression,
+    depcheck.detector.importDeclaration,
+    depcheck.detector.requireCallExpression,
+    depcheck.detector.requireResolveCallExpression,
+    depcheck.detector.typescriptImportEqualsDeclaration,
+    depcheck.detector.typescriptImportType,
+  ],
+  specials: [
+    depcheck.special.bin,
+    depcheck.special.babel,
+    depcheck.special.webpack,
+    depcheck.special.jest,
+    eslint,
+    sanityJSONParser,
+    implicitDepsParser,
+  ],
+  package,
+}
+
+async function eslint(filePath, deps, dir) {
+  const detected = await depcheck.special.eslint(filePath, deps, dir)
+  return detected.filter((res) => deps.includes(res) || !rootPkg.devDependencies[res])
+}
+
+depcheck(cwd, options).then((unused) => {
+  const hasUnusedDeps = unused.dependencies.length > 0 || unused.devDependencies.length > 0
+  const missing = Object.keys(unused.missing).map((dep) => ({
+    name: dep,
+    usages: unused.missing[dep],
+  }))
+  const hasMissingDeps = missing.length > 0
+  const hasInvalidFiles = unused.invalidFiles.length > 0
+  const hasInvalidDirs = unused.invalidDirs.length > 0
+  if (hasUnusedDeps) {
+    console.error(
+      [
+        chalk.bold('Unused dependencies'),
+        ...unused.dependencies.map((dep) => `- ${dep}`),
+        ...unused.devDependencies.map((dep) => `- ${dep} (dev)`),
+      ].join('\n')
+    )
+  }
+  if (hasMissingDeps) {
+    console.error(
+      [
+        chalk.bold('Missing dependencies'),
+        ...missing.flatMap((dep) => [
+          `- ${dep.name}`,
+          '  used by',
+          ...dep.usages.map((u) => `    -- ${path.relative(cwd, u)}`),
+        ]),
+      ].join('\n')
+    )
+  }
+  if (hasInvalidFiles) {
+    console.error(
+      [chalk.bold('Invalid files'), ...unused.invalidFiles.map((file) => `- ${file}`)].join('\n')
+    )
+  }
+  if (hasInvalidDirs) {
+    console.error(
+      [chalk.bold('Invalid dirs'), ...unused.invalidDirs.map((file) => `- ${file}`)].join('\n')
+    )
+  }
+  if (hasMissingDeps || hasUnusedDeps || hasInvalidFiles) {
+    process.exit(1)
+  }
+})
+
+const IMPLICIT_DEPS = {
+  '@sanity/cli': ['@sanity/core'],
+}
+
+function implicitDepsParser(filePath, deps) {
+  return deps.flatMap((dep) => IMPLICIT_DEPS[dep] || [])
+}
+
+function getProjectIgnores(baseDir) {
+  try {
+    return JSON.parse(fs.readFileSync(`${baseDir}/.depcheckignore.json`, 'utf-8'))?.ignore || []
+  } catch {
+    return []
+  }
+}
+
+function sanityJSONParser(filePath, deps, dir) {
+  const filename = path.basename(filePath)
+  if (filename === 'sanity.json') {
+    const sanityConfig = JSON.parse(fs.readFileSync(filePath, 'utf-8'))
+    return deps
+      .concat(sanityConfig.plugins.filter(isLocalPlugin(filePath)))
+      .filter((dep) =>
+        sanityConfig.plugins.some((plugin) => plugin === dep || dep === `sanity-plugin-${plugin}`)
+      )
+  }
+  return []
+}
+
+function isLocalPlugin(basePath) {
+  return (pluginName) => {
+    try {
+      fs.accessSync(path.join(basePath, pluginName, 'sanity.json'), fs.constants.R_OK)
+      return true
+    } catch {
+      return false
+    }
+  }
+}
