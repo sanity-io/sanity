c19f2cf1454db781ce1a5c2a8ebe58cef1a4bc04
Bjørge Næss <bjoerge@gmail.com>
2021-05-25 11:48:20 +0200

    feat(base): add support for sticky debug params

---
+++ packages/@sanity/base/src/datastores/debugParams/debugParams.ts
@@ -0,0 +1,27 @@
+import {concat, of, fromEvent} from 'rxjs'
+import {distinctUntilChanged, map} from 'rxjs/operators'
+
+const DEBUG_PREFIX = '_debug_'
+
+export const debugParams$ = concat(of(0), fromEvent(window, 'hashchange')).pipe(
+  map(() => document.location.hash),
+  distinctUntilChanged(),
+  map((hash) =>
+    hash
+      .substring(1)
+      .split(';')
+      .filter((p) => p.toLowerCase().startsWith(DEBUG_PREFIX))
+      .map((param) => param.substring(DEBUG_PREFIX.length))
+  )
+)
+
+export const debugRolesParam$ = debugParams$.pipe(
+  map((args) => args.find((arg) => arg.startsWith('roles='))),
+  map(
+    (arg) =>
+      arg
+        ?.split('roles=')[1]
+        .split(',')
+        .map((r) => r.trim()) || []
+  )
+)
