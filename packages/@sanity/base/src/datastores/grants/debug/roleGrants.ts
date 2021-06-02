05699ca7c8005660e85bd93f1bd9a89d0e6b6fe6
Rico Kahler <ricokahler@me.com>
2021-06-02 07:57:37 -0400

    refactor(base): update to new acl endpoint

--- packages/@sanity/base/src/datastores/grants/debug/roleGrants.ts
+++ packages/@sanity/base/src/datastores/grants/debug/roleGrants.ts
@@ -1,10 +1,10 @@
 import {map} from 'rxjs/operators'
 import {debugRolesParam$} from '../../debugParams'
-import {DatasetGrants} from '../types'
+import {Grant} from '../types'
 import * as grants from './exampleGrants'
 import {ExampleRoleName} from './exampleRoles'
 
-const DEBUG_ROLE_GRANTS_MAP: Record<ExampleRoleName, DatasetGrants> = {
+const DEBUG_ROLE_GRANTS_MAP: Record<ExampleRoleName, Grant[]> = {
   // basic
   administrator: grants.administrator,
   editor: grants.editor,
@@ -18,26 +18,15 @@ const DEBUG_ROLE_GRANTS_MAP: Record<ExampleRoleName, DatasetGrants> = {
   // legacy
   read: grants.viewer,
   write: grants.editor,
-} as const
-
-function isRoleName(value: string): value is ExampleRoleName {
-  return Boolean(value) && value in DEBUG_ROLE_GRANTS_MAP
 }
-
-export const debugGrants$ = debugRolesParam$.pipe(
-  map((roles) => {
-    return roles.length > 0 ? getGrantsForRoles(roles) : null
-  })
-)
 
 // todo: merge other resource keys(?)
-export function getGrantsForRoles(roles: string[]) {
-  return {
-    'sanity.document.filter': roles
-      .filter(isRoleName)
-      .flatMap(
-        (roleName: ExampleRoleName) =>
-          DEBUG_ROLE_GRANTS_MAP[roleName]['sanity.document.filter'] || []
-      ),
-  }
-}
+export const debugGrants$ = debugRolesParam$.pipe(
+  map((roles) => {
+    if (!roles.length) return null
+
+    return roles
+      .filter((value) => Boolean(value) && value in DEBUG_ROLE_GRANTS_MAP)
+      .flatMap((roleName: ExampleRoleName) => DEBUG_ROLE_GRANTS_MAP[roleName] || [])
+  })
+)
