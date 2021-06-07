c1a240153d04d527e6a67207174870f5710707f1
vicbergquist <victoria.bergquist@gmail.com>
2021-06-07 15:24:35 +0200

    refactor(field): migrate no changes component to sanity ui

--- packages/@sanity/field/src/diff/components/NoChanges.tsx
+++ packages/@sanity/field/src/diff/components/NoChanges.tsx
@@ -1,20 +1,16 @@
+import {Stack, Text} from '@sanity/ui'
 import React from 'react'
-
-import styles from './NoChanges.css'
 
 export function NoChanges() {
   return (
-    <div className={styles.root}>
-      <div className={styles.box}>
-        <h3 className={styles.heading}>There are no changes</h3>
-
-        <div className={styles.content}>
-          <p>
-            Edit the document or select an older version in the timeline to see a list of changes
-            appear in this panel.
-          </p>
-        </div>
-      </div>
-    </div>
+    <Stack space={3}>
+      <Text size={1} weight="semibold" as="h3">
+        There are no changes
+      </Text>
+      <Text as="p" size={1} muted>
+        Edit the document or select an older version in the timeline to see a list of changes appear
+        in this panel.
+      </Text>
+    </Stack>
   )
 }
