fb1541a424178e411c3455acf75b02d9d88c3a97
Bjørge Næss <bjoerge@gmail.com>
2021-06-08 17:45:52 +0200

    refactor(form-builder): use resolveUploader to determine whether file can be uploaded or not

--- packages/@sanity/form-builder/src/sanity/inputs/SanityArrayInput.tsx
+++ packages/@sanity/form-builder/src/sanity/inputs/SanityArrayInput.tsx
@@ -2,14 +2,27 @@ import React, {ForwardedRef, forwardRef} from 'react'
 import formBuilderConfig from 'config:@sanity/form-builder'
 import ArrayFunctions from 'part:@sanity/form-builder/input/array/functions'
 import {resolveInitialValueForType} from '@sanity/initial-value-templates'
+import {SchemaType} from '@sanity/types'
 import resolveUploader from '../uploads/resolveUploader'
 import ArrayInput, {Props} from '../../inputs/arrays/ArrayOfObjectsInput'
 import {
   ArrayOfPrimitivesInput,
   Props as PrimitiveArrayInputProps,
 } from '../../inputs/arrays/ArrayOfPrimitivesInput'
+import * as is from '../../utils/is'
+import {FileLike} from '../uploads/types'
 
-const SUPPORT_DIRECT_UPLOADS = formBuilderConfig?.images?.directUploads
+const arrayResolveUploader = (type: SchemaType, file: FileLike) => {
+  const SUPPORT_DIRECT_IMAGE_UPLOADS = formBuilderConfig?.images?.directUploads
+  const SUPPORT_DIRECT_FILE_UPLOADS = formBuilderConfig?.files?.directUploads
+  if (is.type('image', type) && !SUPPORT_DIRECT_IMAGE_UPLOADS) {
+    return null
+  }
+  if (is.type('file', type) && !SUPPORT_DIRECT_FILE_UPLOADS) {
+    return null
+  }
+  return resolveUploader(type, file)
+}
 
 export const SanityArrayInput = forwardRef(function SanityArrayInput(
   props: Props,
@@ -19,7 +32,7 @@ export const SanityArrayInput = forwardRef(function SanityArrayInput(
     <ArrayInput
       {...props}
       ref={ref}
-      resolveUploader={resolveUploader}
+      resolveUploader={arrayResolveUploader}
       resolveInitialValue={resolveInitialValueForType}
       ArrayFunctionsImpl={ArrayFunctions}
     />
