d3a4670242295f41952eb2ae25caefb0022978a0
vicbergquist <victoria.bergquist@gmail.com>
2021-06-08 16:21:04 +0200

    refactor(field): migrate fromto to sanity ui [ch2438]

--- packages/@sanity/field/src/diff/components/FromTo.tsx
+++ packages/@sanity/field/src/diff/components/FromTo.tsx
@@ -1,34 +1,51 @@
-import classNames from 'classnames'
-import React, {forwardRef} from 'react'
+import React, {forwardRef, useMemo} from 'react'
+import {Flex, Grid} from '@sanity/ui'
 import {FromToArrow} from './FromToArrow'
-
-import styles from './FromTo.css'
 
 export type FromToProps = {
   align?: 'top' | 'center' | 'bottom'
   layout?: 'grid' | 'inline'
   from?: React.ReactNode
   to?: React.ReactNode
-} & Omit<React.HTMLProps<HTMLDivElement>, 'children'>
+} & Omit<React.HTMLProps<HTMLDivElement>, 'children' | 'as' | 'height' | 'wrap'>
 
 export const FromTo = forwardRef<HTMLDivElement, FromToProps>(
-  ({align = 'top', className, layout = 'inline', from, to, ...restProps}, ref) => {
+  ({align = 'top', layout = 'inline', from, to, style, ...restProps}, ref) => {
+    const flexAlign: Record<string, 'flex-start' | 'center' | 'flex-end'> = {
+      top: 'flex-start',
+      center: 'center',
+      bottom: 'flex-end',
+      default: 'flex-start',
+    }
+    const Layout = layout === 'inline' ? Flex : Grid
+    const layoutStyles = useMemo(
+      () => ({
+        ...style,
+        ...(layout === 'inline'
+          ? {maxWidth: '100%', display: 'inline-flex'}
+          : {gridTemplateColumns: `minmax(0, 1fr) 26px minmax(0, 1fr)`}),
+      }),
+      [layout, style]
+    )
+
+    const columnStyles = useMemo(() => (layout === 'inline' ? {flexShrink: 0} : {}), [layout])
+
     return (
-      <div
-        {...restProps}
-        className={classNames(styles.root, className)}
-        data-from-to-align={align}
-        data-from-to-layout={layout}
-        ref={ref}
-      >
+      <Layout {...restProps} ref={ref} style={layoutStyles} data-from-to-layout>
         {from && (
           <>
-            <div className={styles.column}>{from}</div>
-            <FromToArrow className={styles.arrow} />
+            <Flex align={flexAlign[align]} style={columnStyles}>
+              {from}
+            </Flex>
+            <Flex align="center" justify="center" padding={2}>
+              <FromToArrow />
+            </Flex>
           </>
         )}
-        <div className={styles.column}>{to}</div>
-      </div>
+        <Flex align={flexAlign[align]} style={columnStyles}>
+          {to}
+        </Flex>
+      </Layout>
     )
   }
 )
