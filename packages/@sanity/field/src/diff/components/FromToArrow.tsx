3299fddf69c0986e555a8b0085727f1208719c9b
vicbergquist <victoria.bergquist@gmail.com>
2021-06-09 16:17:09 +0200

    refactor(field): migrate fromto arrow to sanity ui

--- packages/@sanity/field/src/diff/components/FromToArrow.tsx
+++ packages/@sanity/field/src/diff/components/FromToArrow.tsx
@@ -1,22 +1,11 @@
-import classNames from 'classnames'
+import {Text} from '@sanity/ui'
 import React from 'react'
+import {ArrowRightIcon} from '@sanity/icons'
 
-import styles from './FromToArrow.css'
-
-export const FromToArrow = ({
-  className,
-  ...restProps
-}: Omit<React.HTMLProps<HTMLDivElement>, 'children'>) => (
-  <div {...restProps} className={classNames(styles.root, className)}>
-    <svg
-      width="1em"
-      height="1em"
-      viewBox="0 0 17 17"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <path d="M4 8.5L12 8.5" stroke="currentColor" strokeWidth="1.2" />
-      <path d="M9 5.5L12 8.5L9 11.5" stroke="currentColor" strokeWidth="1.2" />
-    </svg>
-  </div>
+export const FromToArrow = (
+  props: Omit<React.HTMLProps<HTMLDivElement>, 'children' | 'as' | 'ref'>
+) => (
+  <Text muted size={1} {...props}>
+    <ArrowRightIcon />
+  </Text>
 )
