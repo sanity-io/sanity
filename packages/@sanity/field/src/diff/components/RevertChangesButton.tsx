fe34085bddc27c20c9cb92aa13f1a47eb96135ef
vicbergquist <victoria.bergquist@gmail.com>
2021-06-11 13:52:27 +0200

    refactor(field): migrate revert changes button to sanity ui

--- packages/@sanity/field/src/diff/components/RevertChangesButton.tsx
+++ packages/@sanity/field/src/diff/components/RevertChangesButton.tsx
@@ -1,25 +1,45 @@
-import classNames from 'classnames'
 import React, {forwardRef} from 'react'
-import UndoIcon from 'part:@sanity/base/undo-icon'
+import {RevertIcon} from '@sanity/icons'
+import {Button, ButtonProps} from '@sanity/ui'
+import styled from 'styled-components'
 
-import styles from './RevertChangesButton.css'
+const RevertButton = styled.button`
+  [data-ui='Text'] {
+    font-weight: normal;
+  }
+
+  div[data-ui='Box'] {
+    display: none;
+  }
+
+  &:not([data-disabled='true']):hover,
+  &:not([data-disabled='true']):focus {
+    --card-fg-color: ${({theme}) => theme.sanity.color.solid.critical.enabled.bg};
+    --card-bg-color: transparent;
+    --card-border-color: transparent;
+
+    div[data-ui='Box'] {
+      display: block;
+    }
+  }
+`
 
 export const RevertChangesButton = forwardRef(
-  (props: Omit<React.HTMLProps<HTMLButtonElement>, 'type'>, ref): React.ReactElement => {
-    const {className, selected, ...restProps} = props
-
+  (props: ButtonProps & React.HTMLProps<HTMLButtonElement>, ref): React.ReactElement => {
+    const {selected, ...restProps} = props
     return (
-      <button
-        {...restProps}
-        className={classNames(styles.root, selected && styles.selected, className)}
+      <Button
+        icon={RevertIcon}
+        selected={selected}
+        text="Revert changes"
+        mode="bleed"
+        padding={2}
+        fontSize={1}
+        space={2}
+        as={RevertButton}
         ref={ref as any}
-        type="button"
-      >
-        <span className={styles.iconContainer}>
-          <UndoIcon />
-        </span>
-        <span className={styles.text}>Revert changes</span>
-      </button>
+        {...restProps}
+      />
     )
   }
 )
