87595f3e6f2e54200eddfa3a05810165a048c359
Marius Lundgård <studio@mariuslundgard.com>
2021-06-18 11:39:44 +0200

    fix(validator): disable eslint for regex

--- packages/@sanity/validation/src/validators/dateValidator.js
+++ packages/@sanity/validation/src/validators/dateValidator.js
@@ -2,6 +2,7 @@ import formatDate from 'date-fns/format'
 import ValidationError from '../ValidationError'
 import genericValidator from './genericValidator'
 
+// eslint-disable-next-line no-useless-escape
 const isoDate = /^(?:[-+]\d{2})?(?:\d{4}(?!\d{2}\b))(?:(-?)(?:(?:0[1-9]|1[0-2])(?:\1(?:[12]\d|0[1-9]|3[01]))?|W(?:[0-4]\d|5[0-2])(?:-?[1-7])?|(?:00[1-9]|0[1-9]\d|[12]\d{2}|3(?:[0-5]\d|6[1-6])))(?![T]$|[T][\d]+Z$)(?:[T\s](?:(?:(?:[01]\d|2[0-3])(?:(:?)[0-5]\d)?|24\:?00)(?:[.,]\d+(?!:))?)(?:\2[0-5]\d(?:[.,]\d+)?)?(?:[Z]|(?:[+-])(?:[01]\d|2[0-3])(?::?[0-5]\d)?)?)?)?$/
 
 const getFormattedDate = (type = '', value, options) => {
