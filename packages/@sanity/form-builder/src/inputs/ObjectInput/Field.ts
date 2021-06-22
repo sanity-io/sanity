d6f50d924ae38e3a2f4a3c8127728062b165c100
Bjørge Næss <bjoerge@gmail.com>
2021-06-22 10:15:50 +0200

    fix(form-builder): bring back old internal Field API

---
+++ packages/@sanity/form-builder/src/inputs/ObjectInput/Field.ts
@@ -0,0 +1,3 @@
+// This file was renamed in v2.12.0, but broke the intl-plugin, so keep this file around until
+// it can be safely deleted without causing breakage
+export {ObjectInputField as default} from './ObjectInputField'
