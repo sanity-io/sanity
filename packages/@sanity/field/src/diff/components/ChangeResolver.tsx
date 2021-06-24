75822ff23bbc325b53990f9ee185a25d34d53d50
vicbergquist <victoria.bergquist@gmail.com>
2021-06-24 11:16:59 +0200

    feat(field): migrate fieldchange, groupchange to sanity ui

--- packages/@sanity/field/src/diff/components/ChangeResolver.tsx
+++ packages/@sanity/field/src/diff/components/ChangeResolver.tsx
@@ -3,13 +3,16 @@ import {ChangeNode} from '../../types'
 import {FieldChange} from './FieldChange'
 import {GroupChange} from './GroupChange'
 
-export function ChangeResolver({change}: {change: ChangeNode}) {
+export function ChangeResolver({
+  change,
+  ...restProps
+}: {change: ChangeNode} & React.HTMLAttributes<HTMLDivElement>) {
   if (change.type === 'field') {
-    return <FieldChange change={change} />
+    return <FieldChange change={change} {...restProps} />
   }
 
   if (change.type === 'group') {
-    return <GroupChange change={change} />
+    return <GroupChange change={change} {...restProps} />
   }
 
   return <div>Unknown change type: {(change as any).type}</div>
