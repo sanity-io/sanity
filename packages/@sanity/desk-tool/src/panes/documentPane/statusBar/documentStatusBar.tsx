6b94b1865c75000e831251f075031f9131856085
vicbergquist <victoria.bergquist@gmail.com>
2021-06-24 13:25:49 +0200

    refactor(desk-tool): migrate DocumentStatusBar to @sanity/ui

--- packages/@sanity/desk-tool/src/panes/documentPane/statusBar/documentStatusBar.tsx
+++ packages/@sanity/desk-tool/src/panes/documentPane/statusBar/documentStatusBar.tsx
@@ -1,11 +1,23 @@
 import React from 'react'
 import {useEditState} from '@sanity/react-hooks'
 import resolveDocumentBadges from 'part:@sanity/base/document-badges/resolver'
+import styled from 'styled-components'
+import {Box, Card, Grid} from '@sanity/ui'
 import {useDocumentHistory} from '../documentHistory'
 import {DocumentStatusBarActions, HistoryStatusBarActions} from './documentStatusBarActions'
 import {DocumentSparkline} from './documentSparkline'
 import {DocumentStatusBarProps} from './types'
-import styles from './documentStatusBar.css'
+
+const StatusBarLayout = styled(Grid)`
+  grid-template-columns: repeat(2, minmax(0, 1fr));
+  align-items: center;
+`
+
+const ActionsWrapper = styled.div`
+  position: relative;
+  max-width: 15em;
+  margin-left: auto;
+`
 
 export function DocumentStatusBar(props: DocumentStatusBarProps) {
   const {historyController} = useDocumentHistory()
@@ -15,17 +27,19 @@ export function DocumentStatusBar(props: DocumentStatusBarProps) {
   const revision = historyController.revTime?.id || ''
 
   return (
-    <div className={styles.root}>
-      <DocumentSparkline editState={editState} badges={badges} lastUpdated={props.lastUpdated} />
-      <div className={styles.actions}>
-        <div className={styles.actionsWrapper}>
-          {showingRevision ? (
-            <HistoryStatusBarActions id={props.id} type={props.type} revision={revision} />
-          ) : (
-            <DocumentStatusBarActions id={props.id} type={props.type} />
-          )}
-        </div>
-      </div>
-    </div>
+    <Card paddingX={3} paddingY={2}>
+      <StatusBarLayout>
+        <DocumentSparkline editState={editState} badges={badges} lastUpdated={props.lastUpdated} />
+        <Box>
+          <ActionsWrapper>
+            {showingRevision ? (
+              <HistoryStatusBarActions id={props.id} type={props.type} revision={revision} />
+            ) : (
+              <DocumentStatusBarActions id={props.id} type={props.type} />
+            )}
+          </ActionsWrapper>
+        </Box>
+      </StatusBarLayout>
+    </Card>
   )
 }
