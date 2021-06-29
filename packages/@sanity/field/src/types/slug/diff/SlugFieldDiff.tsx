51915caa67dc9915be3a4c7e561f65431186edb8
vicbergquist <victoria.bergquist@gmail.com>
2021-06-29 14:44:28 +0200

    refactor(field): migrate slug diff to @sanity/ui

--- packages/@sanity/field/src/types/slug/diff/SlugFieldDiff.tsx
+++ packages/@sanity/field/src/types/slug/diff/SlugFieldDiff.tsx
@@ -2,8 +2,6 @@ import React from 'react'
 import {DiffComponent, DiffFromTo, ObjectDiff} from '../../../diff'
 import {SlugPreview} from '../preview'
 
-import styles from './SlugFieldDiff.css'
-
 interface Slug {
   current?: string
 }
@@ -11,7 +9,6 @@ interface Slug {
 export const SlugFieldDiff: DiffComponent<ObjectDiff<Slug>> = ({diff, schemaType}) => {
   return (
     <DiffFromTo
-      cardClassName={styles.card}
       layout="inline"
       diff={diff}
       schemaType={schemaType}
--- packages/@sanity/field/src/types/slug/preview/SlugPreview.tsx
+++ packages/@sanity/field/src/types/slug/preview/SlugPreview.tsx
@@ -1,9 +1,21 @@
+import {Box} from '@sanity/ui'
 import React from 'react'
+import styled from 'styled-components'
+import {Slug} from '@sanity/types'
 import {PreviewComponent} from '../../../preview/types'
-import {Slug} from '../types'
+
+const SlugWrapper = styled.div`
+  display: inline-block;
+  word-break: break-all;
+  white-space: pre-wrap;
+`
 
 export const SlugPreview: PreviewComponent<Slug> = (props) => {
   const {value} = props
 
-  return <>{value.current}</>
+  return (
+    <Box as={SlugWrapper} paddingX={2} paddingY={1}>
+      {value.current}
+    </Box>
+  )
 }
