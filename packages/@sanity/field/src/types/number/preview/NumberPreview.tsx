2c4c35cbf7dbf479e846650dde837f91a6cdef7f
vicbergquist <victoria.bergquist@gmail.com>
2021-06-29 14:24:22 +0200

    refactor(field): migrate number diff to @sanity/ui

--- packages/@sanity/field/src/types/number/preview/NumberPreview.tsx
+++ packages/@sanity/field/src/types/number/preview/NumberPreview.tsx
@@ -1,8 +1,19 @@
+import {Box} from '@sanity/ui'
 import React from 'react'
+import styled from 'styled-components'
 import {PreviewComponent} from '../../../preview/types'
+
+const NumberWrapper = styled.div`
+  display: inline-block;
+  word-break: break-all;
+`
 
 export const NumberPreview: PreviewComponent<string> = (props) => {
   const {value} = props
 
-  return <>{value}</>
+  return (
+    <Box as={NumberWrapper} paddingX={2} paddingY={1}>
+      {value}
+    </Box>
+  )
 }
