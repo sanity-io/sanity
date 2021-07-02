7ab0f91012934a6aa03826b10560461cb3e002d7
Simeon Griggs <simeon@hey.com>
2021-07-02 11:29:11 +0100

    add optional chaining

--- packages/@sanity/desk-tool/src/panes/documentPane/statusBar/actionStateDialog.tsx
+++ packages/@sanity/desk-tool/src/panes/documentPane/statusBar/actionStateDialog.tsx
@@ -141,8 +141,8 @@ export function ActionStateDialog(props: Props) {
         onClose={dialog.onClose}
         onClickOutside={dialog.onClose}
         showCloseButton={dialog.showCloseButton}
-        size={dialog.size ?? 'medium'}
-        padding={dialog.padding ?? 'large'}
+        size={dialog?.size ?? 'medium'}
+        padding={dialog?.padding ?? 'large'}
       >
         {dialog.content}
       </Dialog>
