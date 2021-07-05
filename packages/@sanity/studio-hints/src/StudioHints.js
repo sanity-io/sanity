c25f921327bb27ab986ad55fe98be52367bc4c2b
hermanwikner <wiknerherman@hotmail.com>
2021-07-05 15:45:32 +0200

    refactor(studio-hints): migrate `StudioHints` to `@sanity/ui`

--- packages/@sanity/studio-hints/src/StudioHints.js
+++ packages/@sanity/studio-hints/src/StudioHints.js
@@ -1,9 +1,16 @@
 /* eslint-disable react/no-multi-comp */
 import React from 'react'
 import studioHintsConfig from 'part:@sanity/default-layout/studio-hints-config?'
+import styled from 'styled-components'
+import {Card, ThemeProvider, studioTheme} from '@sanity/ui'
 import ToggleSidecarButton from './components/ToggleSidecarButton'
 import HintsPackage from './components/HintsPackage'
-import styles from './StudioHints.css'
+
+const Root = styled(Card)`
+  height: 100%;
+  width: 100%;
+  min-width: 420px;
+`
 
 const isMobile = () => {
   return typeof window !== 'undefined' && window.innerWidth < 512
@@ -13,9 +20,11 @@ export const isSidecarEnabled = () => studioHintsConfig && !isMobile()
 
 export const SidecarLayout = () => {
   return (
-    <div className={styles.root}>
-      <HintsPackage />
-    </div>
+    <ThemeProvider theme={studioTheme} scheme="dark">
+      <Root display="flex" overflow="auto" sizing="border" tone="transparent" borderTop borderLeft>
+        <HintsPackage />
+      </Root>
+    </ThemeProvider>
   )
 }
 
