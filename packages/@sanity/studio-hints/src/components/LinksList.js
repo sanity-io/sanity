38db973a051d9873096673b9ef0b124869bf164e
hermanwikner <wiknerherman@hotmail.com>
2021-07-05 15:49:03 +0200

    refactor(studio-hints): migrate `LinksList` to `@sanity/ui`

--- packages/@sanity/studio-hints/src/components/LinksList.js
+++ packages/@sanity/studio-hints/src/components/LinksList.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import LaunchIcon from 'part:@sanity/base/launch-icon'
-import styles from './LinksList.css'
+import {Card, Text, Stack, Flex, Box, Label} from '@sanity/ui'
+import {LaunchIcon} from '@sanity/icons'
 import HintCard from './HintCard'
 
 function CardLinks(props) {
@@ -10,34 +10,52 @@ function CardLinks(props) {
     return null
   }
 
+  if (type === 'card') {
+    return (
+      <Stack space={4}>
+        <Label muted>{title}</Label>
+        <Stack space={2} as="ul">
+          {links.map((link) => (
+            <Box as="li" key={link._key}>
+              <HintCard card={link} repoId={repoId} />
+            </Box>
+          ))}
+        </Stack>
+      </Stack>
+    )
+  }
+
   return (
-    <div className={styles.root}>
-      <h3 className={styles.listTitle}>{title}</h3>
-      <ul
-        className={`${styles.linksList} ${type === 'card' ? styles.cardList : styles.simpleList}`}
-      >
-        {links.map((link) => {
-          return type === 'card' ? (
-            <HintCard key={link._key} card={link} repoId={repoId} />
-          ) : (
-            <li className={styles.linkTitle} key={link.title}>
-              <a
-                className={styles.link}
-                href={`${link.url}?utm_source=hints&utm_medium=${repoId}`}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                {/* TODO: handle inserting icon */}
-                <span className={styles.linkTitleText}>{link.title}</span>
-                <span className={styles.externalIcon}>
-                  <LaunchIcon />
-                </span>
-              </a>
-            </li>
-          )
-        })}
-      </ul>
-    </div>
+    <Stack space={4}>
+      <Label muted as="h3">
+        {title}
+      </Label>
+      <Card radius={2} border tone="inherit" overflow="hidden">
+        <Stack as="ul">
+          {links.map((link) => {
+            return (
+              <Box as="li" key={link._key}>
+                <Card
+                  tone="inherit"
+                  padding={4}
+                  as="a"
+                  href={`${link.url}?utm_source=hints&utm_medium=${repoId}`}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <Flex justify="space-between" align="center">
+                    <Text muted>{link.title}</Text>
+                    <Text muted>
+                      <LaunchIcon />
+                    </Text>
+                  </Flex>
+                </Card>
+              </Box>
+            )
+          })}
+        </Stack>
+      </Card>
+    </Stack>
   )
 }
 
