767dc98ac67daa006dda8418521f9e82bd65f5da
Espen Hovlandsdal <espen@hovlandsdal.com>
2021-07-07 16:44:34 +0200

    feat(types): add image url query parameter types

---
+++ packages/@sanity/types/src/images/types.ts
@@ -0,0 +1,53 @@
+/**
+ * Note: These are query parameters, so they will eventually be encoded as strings.
+ * However, since most/all query parameter encoders will accept numbers and encode
+ * them as strings, we'll use `string| number` where applicable, as it makes it easier
+ * to use in places that do calculations and such.
+ */
+export interface ImageUrlParams {
+  bg?: string
+  dpr?: number | string
+  w?: number | string
+  h?: number | string
+  q?: number | string
+  dl?: string
+  dlRaw?: string
+  'fp-x'?: number | string
+  'fp-y'?: number | string
+  'max-w'?: number | string
+  'max-h'?: number | string
+  'min-w'?: number | string
+  'min-h'?: number | string
+  blur?: number | string
+  sharp?: number | string
+  rect?: string // <x>,<y>,<w>,<h>
+  fm?: ImageUrlFormat
+  or?: ImageUrlOrientation
+  fit?: ImageUrlFitMode
+  crop?: ImageUrlCropMode
+  auto?: ImageUrlAutoMode
+  invert?: 'true' | 'false'
+  quality?: number | string
+  flip?: 'h' | 'v' | 'hv'
+  sat?: number | string
+  pad?: number | string
+  colorquant?: number | string
+  border?: string // <width>,<color>
+}
+
+export type ImageUrlFormat = 'jpg' | 'pjpg' | 'png' | 'webp'
+
+export type ImageUrlFitMode = 'clip' | 'crop' | 'fill' | 'fillmax' | 'max' | 'scale' | 'min'
+
+export type ImageUrlCropMode =
+  | 'top'
+  | 'bottom'
+  | 'left'
+  | 'right'
+  | 'center'
+  | 'focalpoint'
+  | 'entropy' // EXPERIMENTAL
+
+export type ImageUrlAutoMode = 'format'
+
+export type ImageUrlOrientation = '0' | '90' | '180' | '270'
