bd9f90845f6c82a645414dface6de0107b8abc85
Bjørge Næss <bjoerge@gmail.com>
2021-07-26 12:26:15 +0200

    fix(form-builder): prevent studio crash when component passed to `withDocument()` is missing a focus() method

--- packages/@sanity/form-builder/src/utils/withDocument.tsx
+++ packages/@sanity/form-builder/src/utils/withDocument.tsx
@@ -1,8 +1,23 @@
 import PropTypes from 'prop-types'
 import React from 'react'
+
+function getDisplayName(component) {
+  return component.displayName || component.name || '<Anonymous>'
+}
+
+function warnMissingFocusMethod(ComposedComponent) {
+  console.warn(
+    `withDocument(${getDisplayName(
+      ComposedComponent
+    )}): The passed component did not expose a ".focus()" method. Either implement an imperative focus method on the component instance, or forward it's received ref to an element that exposes a .focus() method. The component passed to withDocument was: %O`,
+    ComposedComponent
+  )
+}
+
 export default function withDocument(ComposedComponent: any) {
   return class WithDocument extends React.PureComponent {
     _input: any
+    _didShowFocusWarning = false
     static displayName = `withDocument(${ComposedComponent.displayName || ComposedComponent.name})`
     static contextTypes = {
       formBuilder: PropTypes.any,
@@ -26,7 +41,12 @@ export default function withDocument(ComposedComponent: any) {
       this.unsubscribe()
     }
     focus() {
-      this._input.focus()
+      if (typeof this._input?.focus === 'function') {
+        this._input.focus()
+      } else if (!this._didShowFocusWarning) {
+        warnMissingFocusMethod(ComposedComponent)
+        this._didShowFocusWarning = true
+      }
     }
     setInput = (input) => {
       this._input = input
