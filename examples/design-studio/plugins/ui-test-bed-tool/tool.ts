335604feca7aedf34d650cc84aa245511640033e
Marius Lundgård <studio@mariuslundgard.com>
2021-08-03 11:46:24 +0200

    refactor(examples): impove `ui-test-bed-tool`

--- examples/design-studio/plugins/ui-test-bed-tool/tool.ts
+++
@@ -1,7 +0,0 @@
-import {UITestBedTool} from './UITestBedTool'
-
-export default {
-  name: 'ui-testbed',
-  title: 'UI Test Bed',
-  component: UITestBedTool,
-}
---
+++ examples/design-studio/plugins/ui-test-bed-tool/tool.tsx
@@ -0,0 +1,14 @@
+// import {UITestBedTool} from './UITestBedTool'
+
+import React from 'react'
+import {ColorCanvas} from './ColorCanvas'
+
+function UITestBedTool() {
+  return <ColorCanvas />
+}
+
+export default {
+  name: 'ui-testbed',
+  title: 'UI Test Bed',
+  component: UITestBedTool,
+}
