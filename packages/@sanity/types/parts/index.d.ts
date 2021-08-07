f6678d27257ea96619a29403e04612e87f7df1c0
Bjørge Næss <bjoerge@gmail.com>
2021-08-07 16:01:53 +0200

    refactor(types): remove typeRoots, make part-typedef directory names windows safe
    Up until now we have included type defitions for part-imports using `typeRoots`. This required the filesystem to match the actual import (e.g. given the typeRoot `["packages/@sanity/types/parts"]`, `import 'part:@sanity/foo'`, would make TypeScript look for the type definitions in a directory `foo` under the `packages/part:@sanity` directory). This forced us to have directory names including colons, which again caused issues on windows. This PR removes the need for `typeRoots` configuration, by instead requiring part typings to be imported using the triple-slash directive `///<reference types="@sanity/types/parts" />`. This also seems like a better way going forward as they can be specified on a source file basis, which facilitates easier maintenance and removal.
    
    One gotcha to note is that the triple-slash directive will affect the whole compiler step for each package, so a single source file with it will make it effective for all other files within the same package.
    
    TL;DR: if you reliy on getting typings for `part`-imports by configuring `typeRoots` in `tsconfig.json`, you should now instead add
    `///<reference types="@sanity/types/parts" />` to the beginning of all source files that includes part-imports. (Note: only applies to TypeScript (.ts/.tsx) files, not CSS or plain JavaScript files)

---
+++ packages/@sanity/types/parts/index.d.ts
@@ -0,0 +1,6 @@
+export * from './all.part.@sanity'
+export * from './config.@sanity'
+export * from './config.sanity'
+export * from './part.@sanity'
+export * from './sanity.css-custom-properties'
+export * from './sanity.versions'
