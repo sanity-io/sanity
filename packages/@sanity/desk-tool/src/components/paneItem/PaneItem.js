eb43ce262d0dff77c2e3c3f502e7249c2650b0c7
Marius Lundgård <studio@mariuslundgard.com>
2021-08-09 15:59:59 +0200

    refactor(desk-tool): simplify pane item UI

--- packages/@sanity/desk-tool/src/components/paneItem/PaneItem.js
+++ packages/@sanity/desk-tool/src/components/paneItem/PaneItem.js
@@ -1,12 +1,11 @@
 import React, {useCallback, useContext} from 'react'
 import PropTypes from 'prop-types'
-import {Card, Text, Box} from '@sanity/ui'
+import {Card, Text} from '@sanity/ui'
 import {ChevronRightIcon} from '@sanity/icons'
 import schema from 'part:@sanity/base/schema'
 import {SanityDefaultPreview} from 'part:@sanity/base/preview'
 import folderIcon from 'part:@sanity/base/folder-icon'
 import fileIcon from 'part:@sanity/base/file-icon'
-import styled from 'styled-components'
 import DocumentPaneItemPreview from '../../components/DocumentPaneItemPreview'
 import getIconWithFallback from '../../utils/getIconWithFallback'
 import MissingSchemaType from '../../components/MissingSchemaType'
@@ -40,11 +39,6 @@ PaneItem.defaultProps = {
   schemaType: null,
 }
 
-const PaneItemActive = styled(Card)`
-  background: ${({theme}) => theme.sanity.color.card.selected.bg};
-  color: ${({theme}) => theme.sanity.color.card.selected.fg};
-`
-
 export default function PaneItem(props) {
   const {id, isSelected, schemaType, layout, icon, value, isActive} = props
   const {ChildLink} = useContext(PaneRouterContext)
@@ -66,11 +60,9 @@ export default function PaneItem(props) {
     preview = (
       <SanityDefaultPreview
         status={
-          <Box padding={1}>
-            <Text style={{color: 'inherit', opacity: 0.25}}>
-              <ChevronRightIcon />
-            </Text>
-          </Box>
+          <Text muted size={1}>
+            <ChevronRightIcon />
+          </Text>
         }
         icon={getIconWithFallback(icon, schemaType, folderIcon)}
         layout={layout}
@@ -86,24 +78,15 @@ export default function PaneItem(props) {
     [ChildLink, id]
   )
 
-  if (isSelected && isActive) {
-    return (
-      <PaneItemActive paddingX={2} paddingY={1} radius={2} aria-selected>
-        {preview}
-      </PaneItemActive>
-    )
-  }
-
-  if (isSelected) {
-    return (
-      <Card paddingX={2} paddingY={1} radius={2} data-as="a" selected>
-        {preview}
-      </Card>
-    )
-  }
-
   return (
-    <Card paddingX={2} paddingY={1} radius={2} data-as="a" as={LinkComponent}>
+    <Card
+      as={LinkComponent}
+      data-as="a"
+      padding={2}
+      radius={2}
+      pressed={!isActive && isSelected}
+      selected={isActive && isSelected}
+    >
       {preview}
     </Card>
   )
