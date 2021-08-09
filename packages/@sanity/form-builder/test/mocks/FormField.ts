5a28a728a508a11ff5f178485b0341e6d5b543a5
Bjørge Næss <bjoerge@gmail.com>
2021-08-09 17:39:36 +0200

    test(form-builder): allow mock imports to non-typed path

--- packages/@sanity/form-builder/test/mocks/FormField.ts
+++ packages/@sanity/form-builder/test/mocks/FormField.ts
@@ -1,1 +1,2 @@
+//@ts-expect-error (it's ok, the path actually exists after sanity build, it's just not shipped with types)
 export {default} from '@sanity/base/lib/__legacy/@sanity/components/formfields/DefaultFormField'
