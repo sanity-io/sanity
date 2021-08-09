c21b36fb208799fd7a2b4f9f2f4abe802f49f458
Bjørge Næss <bjoerge@gmail.com>
2021-08-09 10:39:14 +0200

    feat(cli): add codemod for inserting parts reference types directive in ts/tsx files that has part imports

---
+++ packages/@sanity/cli/codemods/partsTypeDirective.js
@@ -0,0 +1,45 @@
+const path = require('path')
+partsTypeDirective.parser = 'tsx'
+module.exports = partsTypeDirective
+
+const EXTS = ['.ts', '.tsx']
+
+function partsTypeDirective(fileInfo, api) {
+  const j = api.jscodeshift
+  const root = j(fileInfo.source)
+
+  if (!EXTS.includes(path.extname(fileInfo.path))) {
+    return fileInfo.source
+  }
+  const partImports = root.find(api.jscodeshift.ImportDeclaration, (node) =>
+    isSanityPart(node.source.value)
+  )
+  if (partImports.length === 0) {
+    return fileInfo.source
+  }
+
+  const existingDirectives = root.find(api.jscodeshift.Comment, (node) =>
+    isSanityTypesReferenceDirective(node)
+  )
+  if (existingDirectives.length > 0) {
+    // already added
+    return fileInfo.source
+  }
+
+  return `// @todo: remove the following line when part imports has been removed from this file
+///<reference types="@sanity/types/parts" />
+
+${fileInfo.source}`
+}
+
+const partMatcher = /^(all:)?part:[@A-Za-z0-9_-]+\/[A-Za-z0-9_/-]+/
+const configMatcher = /^config:(@?[A-Za-z0-9_-]+\/[A-Za-z0-9_-]+|[A-Za-z0-9_-]+)$/
+const sanityMatcher = /^sanity:/
+
+const isSanityPart = (path) =>
+  [partMatcher, configMatcher, sanityMatcher].some((match) => match.test(path))
+
+const isSanityTypesReferenceDirective = (node) =>
+  node.type === 'CommentLine' &&
+  // note: the leading '/' is intentional (it's a triple-slash directive)
+  node.value === '/<reference types="@sanity/types/parts" />'
