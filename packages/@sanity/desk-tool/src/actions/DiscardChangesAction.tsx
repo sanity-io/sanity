01e752f0ae885cf1aff20b5c1791836269605b0a
Marius Lundgård <studio@mariuslundgard.com>
2021-08-10 15:25:31 +0200

    fixup! refactor(desk-tool): add colors to document actions

--- packages/@sanity/desk-tool/src/actions/DiscardChangesAction.tsx
+++ packages/@sanity/desk-tool/src/actions/DiscardChangesAction.tsx
@@ -34,7 +34,7 @@ export function DiscardChangesAction({id, type, published, liveEdit, onComplete}
     () =>
       isConfirmDialogOpen && {
         type: 'confirm',
-        color: 'danger',
+        color: 'warning',
         onCancel: onComplete,
         onConfirm: handleConfirm,
         message: <>Are you sure you want to discard all changes since last published?</>,
@@ -62,7 +62,7 @@ export function DiscardChangesAction({id, type, published, liveEdit, onComplete}
   }
 
   return {
-    color: 'danger',
+    color: 'warning',
     icon: ResetIcon,
     disabled: Boolean(discardChanges.disabled),
     title: (discardChanges.disabled && DISABLED_REASON_TITLE[discardChanges.disabled]) || '',
