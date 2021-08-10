f618f2e3a96a6ee05779560cc09c0b0aecb67fdf
Marius Lundgård <studio@mariuslundgard.com>
2021-08-10 11:30:16 +0200

    fix(desk-tool): remove unused default badges

--- packages/@sanity/desk-tool/src/badges/defaultDocumentBadges.ts
+++ packages/@sanity/desk-tool/src/badges/defaultDocumentBadges.ts
@@ -1,15 +1,19 @@
-import {LiveEditBadge} from './LiveEditBadge'
-import {PublishedStatusBadge} from './PublishedStatusBadge'
-import {DraftStatusBadge} from './DraftStatusBadge'
-
 import schema from 'part:@sanity/base/schema'
+import {LiveEditBadge} from './LiveEditBadge'
 
+// @todo: remove these 2 exports (should not be used)
 export {PublishedStatusBadge} from './PublishedStatusBadge'
 export {DraftStatusBadge} from './DraftStatusBadge'
+
 export {LiveEditBadge} from './LiveEditBadge'
 
 export default function defaultResolveDocumentBadges(props) {
   const schemaType = schema.get(props.type)
-  const isLiveEditEnabled = schemaType?.liveEdit === true
-  return isLiveEditEnabled ? [LiveEditBadge] : [PublishedStatusBadge, DraftStatusBadge]
+
+  // Show a `Live` badge
+  if (schemaType?.liveEdit === true) {
+    return [LiveEditBadge]
+  }
+
+  return []
 }
