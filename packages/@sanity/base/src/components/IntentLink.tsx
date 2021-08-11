755ddce9a6e681dac1750a862f2dae6ad45856b3
Bjørge Næss <bjoerge@gmail.com>
2021-08-11 17:45:06 +0200

    refactor(form-builder): use IntentButton and IntentLink from @sanity/components

--- packages/@sanity/base/src/components/IntentLink.tsx
+++ packages/@sanity/base/src/components/IntentLink.tsx
@@ -1,6 +1,9 @@
 import React, {ComponentProps} from 'react'
 import {IntentLink as RouterIntentLink} from '../router'
 
+/**
+ * @beta
+ */
 export function IntentLink(
   props: Omit<ComponentProps<'a'>, 'href' | 'ref'> & {
     intent: string
