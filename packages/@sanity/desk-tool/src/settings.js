4165ddd23cb6ce6964dfa69a18347ce2dc8d48e1
Marius Lundgård <studio@mariuslundgard.com>
2021-08-15 13:15:14 +0200

    feat(desk-tool): add settings hook for simpler usage

--- packages/@sanity/desk-tool/src/settings.js
+++ packages/@sanity/desk-tool/src/settings.js
@@ -1,3 +1,43 @@
-import settings from 'part:@sanity/base/settings'
+// @todo: remove the following line when part imports has been removed from this file
+///<reference types="@sanity/types/parts" />
 
-export default settings.forNamespace('desk-tool')
+import {Observable} from 'rxjs'
+import settings from 'part:@sanity/base/settings'
+import {useCallback, useEffect, useMemo, useRef, useState} from 'react'
+
+/**
+ * @internal
+ */
+export const deskToolSettings = settings.forNamespace('desk-tool')
+
+/**
+ * @internal
+ */
+export function useDeskToolSetting<ValueType>(
+  namespace: string | null,
+  key: string,
+  defaultValue?: ValueType
+): [ValueType | undefined, (_value: ValueType) => void] {
+  const [value, setValue] = useState<ValueType | undefined>(defaultValue)
+  const settingRef = useRef<{
+    listen: (_defaultValue?: ValueType) => Observable<ValueType>
+    set: (_value: ValueType) => void
+  } | null>(null)
+
+  useEffect(() => {
+    const settingsNamespace = deskToolSettings.forNamespace(namespace)
+
+    settingRef.current = settingsNamespace.forKey(key)
+
+    const sub = settingRef.current.listen(defaultValue).subscribe(setValue)
+
+    return () => sub.unsubscribe()
+  }, [defaultValue, key, namespace])
+
+  const set = useCallback((newValue: ValueType) => {
+    setValue(newValue)
+    settingRef.current?.set(newValue)
+  }, [])
+
+  return useMemo(() => [value, set], [set, value])
+}
