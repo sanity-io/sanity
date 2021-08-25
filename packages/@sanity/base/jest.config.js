8ffa519944de60363dcd9a3f42b4f88420fa46ec
Marius Lundgård <studio@mariuslundgard.com>
2021-08-25 10:19:22 +0200

    test(base): add mock for user store

--- packages/@sanity/base/jest.config.js
+++ packages/@sanity/base/jest.config.js
@@ -8,5 +8,6 @@ module.exports = {
     '^part:@sanity/base/configure-client?': '<rootDir>/test/mocks/undefined.js',
     '^part:@sanity/base/initial-value-templates?': '<rootDir>/test/mocks/templates.js',
     '^part:@sanity/base/schema?': '<rootDir>/test/mocks/schema.js',
+    '^part:@sanity/base/user': '<rootDir>/test/mocks/user.js',
   },
 }
