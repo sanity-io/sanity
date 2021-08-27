3e0af9bd67e19c95519883b86092e256d11b7178
RitaDias <rita@sanity.io>
2021-08-27 10:42:24 +0200

    refactor(imagetool): use styled-components instead of css modules (#2695)
    * refactor(imagetool): Refactor ImageTool

--- packages/@sanity/imagetool/src/ImageTool.styles.ts
+++ packages/@sanity/imagetool/src/ImageTool.styles.ts
@@ -1,16 +1,21 @@
-.root {
+import styled from 'styled-components'
+import makeDragAware from './makeDragAware'
+
+const DragAwareCanvas = makeDragAware('canvas')
+
+export const RootContainer = styled.div`
   width: 100%;
   height: 100%;
   position: relative;
   display: flex;
   align-items: center;
   justify-content: center;
-}
+`
 
-.canvas {
+export const CanvasContainer = styled(DragAwareCanvas)`
   display: block;
   position: relative;
   max-width: calc(100% - 0.5em); /* to prevent overlap with change bar */
   max-height: calc(100% + 1em);
   user-select: none;
-}
+`
