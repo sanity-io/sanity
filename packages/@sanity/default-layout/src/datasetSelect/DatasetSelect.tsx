27b3018eb8996e83d2b3a5e1af2a1e556743f88f
hermanwikner <wiknerherman@hotmail.com>
2021-08-31 10:43:24 +0200

    refactor(default-layout): refactor `DataSelect` to functional component

---
+++ packages/@sanity/default-layout/src/datasetSelect/DatasetSelect.tsx
@@ -0,0 +1,51 @@
+import React, {useCallback, useEffect, useRef, useState} from 'react'
+import {Subscription} from 'rxjs'
+import {useRouter} from '@sanity/base/router'
+import {map} from 'rxjs/operators'
+import {Select} from '@sanity/ui'
+import {state as urlState} from '../datastores/urlState'
+import {CONFIGURED_SPACES} from '../util/spaces'
+
+export function DatasetSelect(props) {
+  const router = useRouter()
+  const [currentSpace, setCurrentSpace] = useState<{name: string} | null>(null)
+  const currentSpaceSubscription: React.MutableRefObject<Subscription | undefined> = useRef()
+
+  const currentSpace$ = urlState.pipe(
+    map((event) => event.state && event.state.space),
+    map((spaceName) => CONFIGURED_SPACES.find((sp) => sp.name === spaceName))
+  )
+
+  useEffect(() => {
+    currentSpaceSubscription.current = currentSpace$.subscribe((space) => {
+      setCurrentSpace(space)
+    })
+
+    return () => {
+      currentSpaceSubscription.current.unsubscribe()
+    }
+  }, [currentSpace$])
+
+  const handleChange = useCallback(
+    (event: React.ChangeEvent<HTMLSelectElement>) => {
+      router.navigate({space: event.target.value})
+      window.location.reload()
+    },
+    [router]
+  )
+
+  return (
+    <Select
+      onChange={handleChange}
+      value={(currentSpace && currentSpace.name) || undefined}
+      radius={2}
+      {...props}
+    >
+      {CONFIGURED_SPACES.map((space) => (
+        <option key={space.name} value={space.name}>
+          {space.title}
+        </option>
+      ))}
+    </Select>
+  )
+}
