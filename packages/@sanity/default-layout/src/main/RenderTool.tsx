8d403a7d15616a2a2f8572f4d1a04e886c7d4fda
hermanwikner <wiknerherman@hotmail.com>
2021-08-31 12:50:38 +0200

    fix(default-layout): update `main` exports and imports

--- packages/@sanity/default-layout/src/main/RenderTool.tsx
+++ packages/@sanity/default-layout/src/main/RenderTool.tsx
@@ -4,7 +4,7 @@
 import React from 'react'
 import {Box, Card, Container, Heading, Stack, Text} from '@sanity/ui'
 import tools from 'all:part:@sanity/base/tool'
-import ErrorScreen from './ErrorScreen'
+import {ErrorScreen} from '.'
 
 declare const __DEV__: boolean
 
