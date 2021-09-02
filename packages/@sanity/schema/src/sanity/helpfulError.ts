07af468a8d0afa84e167d8cb197ea7aec44b09b7
Marius Lundgård <studio@mariuslundgard.com>
2021-09-02 13:55:19 +0200

    refactor(schema): improve types

--- packages/@sanity/schema/src/sanity/helpfulError.ts
+++ packages/@sanity/schema/src/sanity/helpfulError.ts
@@ -1,5 +1,5 @@
 import generateHelpUrl from '@sanity/generate-help-url'
 
-export function createHelpfulError(message: string, slug: string) {
+export function createHelpfulError(message: string, slug: string): Error {
   return new Error(`${message}. For more info see ${generateHelpUrl(slug)}`)
 }
