a3d83b0dfaa927561a608c123896048b60f2fd02
Marius Lundgård <studio@mariuslundgard.com>
2021-09-02 16:39:17 +0200

    refactor(base): improve default z-offsets

--- packages/@sanity/base/src/components/zOffsets/defaults.ts
+++ packages/@sanity/base/src/components/zOffsets/defaults.ts
@@ -7,20 +7,24 @@ export const defaults: ZIndexContextValue = {
   navbar: 200,
   navbarPopover: 500000,
   navbarDialog: 500001,
+
+  // pane
   pane: 100,
   paneHeader: [110, 15000],
   paneFooter: [120, 20000],
-  paneResizer: 150,
-  portal: 200,
+  paneResizer: [130, 25000],
+
+  //
   popover: 200,
   modal: 200,
   movingItem: 10000,
   drawershade: 1000000,
   drawer: 1000001,
   fullscreen: 1200000,
-  toast: 11000,
+  toast: [100, 11000],
 
   // NOT IN USE
+  portal: 200,
   dropdown: 200,
   navbarFixed: 1010,
   fullscreenEdit: 1050,
