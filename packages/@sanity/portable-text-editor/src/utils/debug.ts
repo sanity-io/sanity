d66b7c261218ccef81221d12dbcc8bc928b42d4e
Per-Kristian Nordnes <per.kristian.nordnes@gmail.com>
2021-09-06 08:54:33 +0200

    refactor(portable-text-editor): remove uneccesary string usage

--- packages/@sanity/portable-text-editor/src/utils/debug.ts
+++ packages/@sanity/portable-text-editor/src/utils/debug.ts
@@ -2,8 +2,8 @@ import debug from 'debug'
 
 const rootName = 'sanity-pte:'
 
-export default debug('sanity-pte:')
-export function debugWithName(name: string) {
+export default debug(rootName)
+export function debugWithName(name: string): debug.Debugger {
   const namespace = `${rootName}${name}`
   if (debug && debug.enabled(namespace)) {
     return debug(namespace)
