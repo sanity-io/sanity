6362b30da92865e22d4e4f4f2a9f33839ec8b6c9
Marius Lundgård <studio@mariuslundgard.com>
2021-09-09 11:42:49 +0200

    feat(design-studio): add document view with styled component

---
+++ examples/design-studio/parts/schema/documentWithViews/index.ts
@@ -0,0 +1,1 @@
+export {documentWithViews as default} from './schema'
--- examples/design-studio/parts/schema/index.ts
+++ examples/design-studio/parts/schema/index.ts
@@ -1,30 +1,34 @@
 import schemaTypes from 'all:part:@sanity/base/schema-type'
 import createSchema from 'part:@sanity/base/schema-creator'
-
-// document types
-// NOTE: keep this alphabetized
 import allInputs from './allInputs'
 import arrayInArray from './arrayInArray'
 import author from './author'
 import complexArrays from './complexArrays'
+import documentWithViews from './documentWithViews'
 import live from './live'
 import pt from './pt'
 import radioInputs from './radioInputs'
 import selectInputs from './selectInputs'
 import settings from './settings'
 
+// NOTE: keep this alphabetized
+const documentTypes = [
+  allInputs,
+  arrayInArray,
+  author,
+  complexArrays,
+  documentWithViews,
+  live,
+  pt,
+  radioInputs,
+  selectInputs,
+  settings,
+]
+
+// NOTE: keep this alphabetized
+const objectTypes: any[] = []
+
 export default createSchema({
   name: 'design-studio',
-  types: schemaTypes.concat([
-    // NOTE: keep this alphabetized
-    allInputs,
-    arrayInArray,
-    author,
-    complexArrays,
-    live,
-    pt,
-    radioInputs,
-    selectInputs,
-    settings,
-  ]),
+  types: schemaTypes.concat([...documentTypes, ...objectTypes]),
 })
