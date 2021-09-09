5791530f390bb889d72117e45817f83b128c9fe2
Espen Hovlandsdal <espen@hovlandsdal.com>
2021-09-09 15:55:45 +0200

    refactor(base): import preview-resolver part from central location

---
+++ packages/@sanity/base/src/legacyParts.ts
@@ -0,0 +1,6 @@
+// @todo: remove the following line when part imports has been removed from this file
+///<reference types="@sanity/types/parts" />
+
+import previewResolver from 'part:@sanity/base/preview-resolver?'
+
+export {previewResolver}
