fc238222482e3441d7ee99afa4786ad039706406
Marius Lundgård <studio@mariuslundgard.com>
2021-09-09 11:42:49 +0200

    feat(design-studio): add document view with styled component

---
+++ examples/design-studio/parts/schema/documentWithViews/view/styledTest.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Thing = styled.div`
+  background: #fe0;
+  height: 100%;
+  padding: 20px;
+  box-sizing: border-box;
+`
+
+export function StyledTestView() {
+  // throw new Error('called?')
+
+  return (
+    <Thing key="test">
+      Styled with <code>styled-components</code>
+    </Thing>
+  )
+}
