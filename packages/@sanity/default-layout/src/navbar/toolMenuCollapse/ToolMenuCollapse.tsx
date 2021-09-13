2e358d05d3a1b734a4472d7b220f4c54cd593e29
hermanwikner <wiknerherman@hotmail.com>
2021-09-13 15:01:10 +0200

    refactor(default-layout): improve `ToolCollapseMenu`

--- packages/@sanity/default-layout/src/navbar/toolMenuCollapse/ToolMenuCollapse.tsx
+++ packages/@sanity/default-layout/src/navbar/toolMenuCollapse/ToolMenuCollapse.tsx
@@ -1,60 +1,43 @@
 import {PlugIcon} from '@sanity/icons'
-import {StateLink} from '@sanity/state-router/components'
-import React, {forwardRef, useMemo} from 'react'
+import {StateLink} from '@sanity/base/router'
+import React, {useMemo} from 'react'
+import {CollapseMenu, CollapseMenuButton} from '@sanity/base/components'
 import {Tool, Router} from '../../types'
-import {CollapseMenu, CollapseMenuButton} from '../components'
 
 export function ToolMenuCollapse({tools, router}: {tools: Tool[]; router: Router}) {
-  const tool = router.state?.tool || ''
+  const activeTool = router.state.tool || ''
 
-  const LinkComponent = useMemo(
-    () =>
-      // eslint-disable-next-line no-shadow
-      forwardRef(function LinkComponent(
-        props: {children: React.ReactNode; tool: Tool},
-        ref: React.ForwardedRef<HTMLAnchorElement>
-      ) {
-        const {name} = props.tool as Tool
-        const linkProps = {...props, router: null, tool: null, title: null}
+  return useMemo(
+    () => (
+      <CollapseMenu menuPopoverProps={{scheme: 'light'}} gap={1}>
+        {tools?.map((tool) => {
+          const Link = (props: any) => (
+            <StateLink
+              {...props}
+              state={{
+                ...router.state,
+                tool: tool?.name,
+                [tool?.name]: undefined,
+              }}
+            />
+          )
 
-        return (
-          <StateLink
-            {...linkProps}
-            state={{...router.state, tool: name, [name]: undefined}}
-            ref={ref}
-          />
-        )
-      }),
-    [router.state]
-  )
-
-  const toolsOptions = useMemo(
-    () =>
-      tools.map((t) => {
-        return {
-          ...t,
-          icon: t?.icon || PlugIcon,
-          text: t.title || t.name,
-          tool: t,
-          selected: tool === t.name,
-        }
-      }),
-    [tool, tools]
-  )
-
-  return (
-    <CollapseMenu menuScheme="light">
-      {toolsOptions.map((t) => (
-        <CollapseMenuButton
-          key={t?.name}
-          as={LinkComponent}
-          data-as="a"
-          mode="bleed"
-          tooltipScheme="light"
-          tool={t}
-          {...t}
-        />
-      ))}
-    </CollapseMenu>
+          return (
+            <CollapseMenuButton
+              // eslint-disable-next-line react/jsx-no-bind
+              as={Link}
+              key={tool?.name}
+              data-as="a"
+              buttonProps={{mode: 'bleed'}}
+              tooltipProps={{scheme: 'light'}}
+              text={tool.title}
+              icon={tool?.icon || PlugIcon}
+              selected={activeTool === tool?.name}
+            />
+          )
+        })}
+      </CollapseMenu>
+    ),
+    [activeTool, router.state, tools]
   )
 }
