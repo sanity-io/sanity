07921ab3178da815f0d700ec8b259e5b01197d32
Per-Kristian Nordnes <per.kristian.nordnes@gmail.com>
2021-09-14 15:27:13 +0200

    refactor(base): export types as Type

--- packages/@sanity/base/src/_exports/_internal.ts
+++ packages/@sanity/base/src/_exports/_internal.ts
@@ -5,10 +5,10 @@ export {default as FieldStatus} from '../__legacy/@sanity/components/fieldsets/F
 
 export {resizeObserver} from '../util/resizeObserver'
 
-export {RemoteSnapshotVersionEvent} from '../datastores/document/document-pair/checkoutPair'
-export {DocumentRemoteMutationEvent} from '../datastores/document/buffered-doc/types'
+export type {RemoteSnapshotVersionEvent} from '../datastores/document/document-pair/checkoutPair'
+export type {DocumentRemoteMutationEvent} from '../datastores/document/buffered-doc/types'
 export {remoteSnapshots} from '../datastores/document/document-pair/remoteSnapshots'
 
-export {EditStateFor} from '../datastores/document/document-pair/editState'
+export type {EditStateFor} from '../datastores/document/document-pair/editState'
 
 export * from '../actions/utils/types'
