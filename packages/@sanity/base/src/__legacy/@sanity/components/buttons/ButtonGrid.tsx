5ae13e445641cf00d8410432959efbfde798bab3
Alexis Hope <alexis@cvofhope.com>
2021-09-15 09:53:13 +1000

    refactor(base): ButtonGrid to functional component

--- packages/@sanity/base/src/__legacy/@sanity/components/buttons/ButtonGrid.tsx
+++ packages/@sanity/base/src/__legacy/@sanity/components/buttons/ButtonGrid.tsx
@@ -3,46 +3,42 @@
 
 import classNames from 'classnames'
 import styles from 'part:@sanity/components/buttons/button-grid-style'
-import React from 'react'
+import React, {FunctionComponent} from 'react'
 import {childrenToElementArray} from '../helpers'
 
-interface ButtonGridProps {
+interface ButtonGridProps extends React.HTMLProps<HTMLDivElement> {
   secondary?: React.ReactNode
   align?: 'start' | 'end'
 }
 
-// @todo: refactor to functional component
-export default class ButtonGrid extends React.PureComponent<
-  ButtonGridProps & React.HTMLProps<HTMLDivElement>
-> {
-  render() {
-    const {
-      align = 'start',
-      children: childrenProp,
-      className: classNameProp,
-      secondary,
-      ...restProps
-    } = this.props
-    const children = childrenToElementArray(childrenProp)
-    const secondaryChildren = childrenToElementArray(secondary)
-    const len = children.length + secondaryChildren.length
+const ButtonGrid: FunctionComponent<ButtonGridProps> = (props) => {
+  const {
+    align = 'start',
+    children: childrenProp,
+    className: classNameProp,
+    secondary,
+    ...restProps
+  } = props
+  const children = childrenToElementArray(childrenProp)
+  const secondaryChildren = childrenToElementArray(secondary)
+  const len = children.length + secondaryChildren.length
 
-    if (len === 0) return null
+  if (len === 0) return null
 
-    const className = classNames(
-      classNameProp,
-      align === 'start' ? styles.alignStart : styles.alignEnd
-    )
+  const className = classNames(
+    classNameProp,
+    align === 'start' ? styles.alignStart : styles.alignEnd
+  )
 
-    return (
-      <div {...restProps} className={className} data-buttons={len}>
-        {children}
-        {secondaryChildren.map((child, childIndex) => (
-          <div className={styles.secondary} key={String(childIndex)}>
-            {child}
-          </div>
-        ))}
-      </div>
-    )
-  }
+  return (
+    <div {...restProps} className={className} data-buttons={len}>
+      {children}
+      {secondaryChildren.map((child, childIndex) => (
+        <div className={styles.secondary} key={String(childIndex)}>
+          {child}
+        </div>
+      ))}
+    </div>
+  )
 }
+export default ButtonGrid
