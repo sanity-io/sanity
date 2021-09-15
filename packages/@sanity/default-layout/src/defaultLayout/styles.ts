ae3b3cb337d063f7b3dc48dd2777ad93b5b37614
hermanwikner <wiknerherman@hotmail.com>
2021-09-15 15:44:07 +0200

    refactor(default-layout): update `DefaultLayout` styles

--- packages/@sanity/default-layout/src/defaultLayout/styles.ts
+++ packages/@sanity/default-layout/src/defaultLayout/styles.ts
@@ -1,56 +1,41 @@
 import styled, {css} from 'styled-components'
 import {Box, Flex} from '@sanity/ui'
 
-export const Root = styled(Flex)<{$isOverlayVisible: boolean}>`
-  box-sizing: border-box;
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-
+export const RootFlex = styled(Flex)<{$isOverlayVisible: boolean}>`
   ${({$isOverlayVisible}) =>
     $isOverlayVisible &&
     css`
       overflow: hidden;
-      height: 100%;
     `}
 
   @media (min-width: ${({theme}) => theme.sanity.media[1]}px) {
     overflow: hidden;
     width: 100%;
-    height: 100%;
   }
 `
 
-export const MainArea = styled(Flex)<{$isOverlayVisible: boolean}>`
-  flex: 1;
-  min-height: 0;
+export const MainAreaFlex = styled(Flex)`
   position: relative;
-
-  ${({$isOverlayVisible}) =>
-    $isOverlayVisible &&
-    css`
-      overflow: hidden;
-      height: 100%;
-    `}
 `
 
 export const PortalBox = styled(Box)`
   position: absolute;
-  inset: 0;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
 `
 
-export const ToolContainer = styled.div`
+export const ToolBox = styled(Box)`
   position: relative;
-  flex: 1;
   min-width: 0;
-  height: 100%;
 
   @media (min-width: ${({theme}) => theme.sanity.media[1]}px) {
     overflow: auto;
   }
 `
 
-export const SidecarContainer = styled.div`
+export const SidecarBox = styled(Box)`
   position: relative;
 
   @media (max-width: ${({theme}) => theme.sanity.media[1]}px) {
