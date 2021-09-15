00f66654e8e2a63cd6c113063bcbd33688dd9370
Marius Lundgård <studio@mariuslundgard.com>
2021-09-15 15:08:55 +0200

    refactor(studio-hints): improve toggle button

--- packages/@sanity/studio-hints/src/components/ToggleSidecarButton.js
+++ packages/@sanity/studio-hints/src/components/ToggleSidecarButton.js
@@ -1,23 +1,10 @@
-/* eslint-disable prefer-template */
-
 import React from 'react'
 import {
   isSidecarOpenSetting,
   toggleSidecarOpenState,
 } from 'part:@sanity/default-layout/sidecar-datastore'
-import {Button as UIButton, studioTheme, ThemeProvider} from '@sanity/ui'
+import {Button} from '@sanity/ui'
 import {HelpCircleIcon} from '@sanity/icons'
-import styled from 'styled-components'
-
-const Button = styled(UIButton)`
-  width: 33px;
-  height: 33px;
-
-  &:not(:hover):not(:focus):not([data-selected]) {
-    background: transparent;
-    box-shadow: unset;
-  }
-`
 
 export default class ToggleSidecarButton extends React.PureComponent {
   state = {
@@ -40,18 +27,14 @@ export default class ToggleSidecarButton extends React.PureComponent {
     const {isOpen} = this.state
 
     return (
-      <ThemeProvider theme={studioTheme} scheme="dark">
-        <Button
-          padding={0}
-          aria-label="Toggle sidecar"
-          aria-pressed={isOpen}
-          icon={HelpCircleIcon}
-          onClick={toggleSidecarOpenState}
-          selected={isOpen}
-          tone="primary"
-          mode="bleed"
-        />
-      </ThemeProvider>
+      <Button
+        aria-label="Toggle sidecar"
+        aria-pressed={isOpen}
+        icon={HelpCircleIcon}
+        mode="bleed"
+        onClick={toggleSidecarOpenState}
+        selected={isOpen}
+      />
     )
   }
 }
