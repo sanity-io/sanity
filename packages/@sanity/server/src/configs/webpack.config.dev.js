7eefcb0284ef5658fabb46c676016a225f7ef55d
Espen Hovlandsdal <espen@hovlandsdal.com>
2021-09-20 13:43:59 +0200

    chore(server): use monorepo aliases if applicable for all webpack configs

--- packages/@sanity/server/src/configs/webpack.config.dev.js
+++ packages/@sanity/server/src/configs/webpack.config.dev.js
@@ -1,7 +1,6 @@
 import webpack from 'webpack'
 import applyStaticLoaderFix from '../util/applyStaticLoaderFix'
 import getBaseConfig from './webpack.config'
-import {getMonorepoAliases} from './monorepoAliases'
 import {getModulePath} from './getModulePath'
 import {applyLocalWebpackConfig} from './applyLocalWebpackConfig'
 
@@ -18,15 +17,10 @@ export default (config) => {
       ].concat(baseConfig.entry.vendor),
     }),
     resolve: {
-      alias: Object.assign(
-        {},
-        baseConfig.resolve.alias,
-        {
-          'react-dom': getModulePath('@hot-loader/react-dom'),
-          'webpack-hot-middleware/client': require.resolve('../browser/hot-client'),
-        },
-        config.isSanityMonorepo ? getMonorepoAliases() : {}
-      ),
+      alias: Object.assign({}, baseConfig.resolve.alias, {
+        'react-dom': getModulePath('@hot-loader/react-dom'),
+        'webpack-hot-middleware/client': require.resolve('../browser/hot-client'),
+      }),
       extensions: baseConfig.resolve.extensions,
     },
     plugins: (baseConfig.plugins || []).concat([
