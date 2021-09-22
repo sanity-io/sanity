93bdc51efd2261bd4856d719f42abc06166bbf8a
Marius Lundgård <studio@mariuslundgard.com>
2021-09-22 14:52:06 +0200

    refactor(test-studio): improve `DeveloperPreview`

--- examples/test-studio/src/previews/developer/DeveloperPreview.js
+++ examples/test-studio/src/previews/developer/DeveloperPreview.js
@@ -1,22 +1,34 @@
-/* eslint-disable react/prop-types */
-
-import React, {useContext} from 'react'
-import JSONPretty from 'react-json-pretty'
-import monikai from 'react-json-pretty/dist/monikai'
-import styles from './DeveloperPreview.module.css'
 import {PaneRouterContext} from '@sanity/desk-tool'
+import {AddIcon, RemoveIcon} from '@sanity/icons'
+import {Button, Card, Code, Inline, Label, Stack, Text} from '@sanity/ui'
+import React, {useCallback, useContext} from 'react'
 
 function DeveloperPreview(props) {
-  const pane = useContext(PaneRouterContext)
-  const payload = {count: 0, ...pane.payload}
+  const {payload = {}, setPayload} = useContext(PaneRouterContext)
   const {displayed} = props.document
+  const {count = 0} = payload
+
+  const handleDecr = useCallback(() => setPayload({count: count - 1}), [count, setPayload])
+  const handleIncr = useCallback(() => setPayload({count: count + 1}), [count, setPayload])
+
   return (
-    <div className={styles.root}>
-      <button type="button" onClick={() => pane.setPayload({count: payload.count + 1})}>
-        + {payload.count}
-      </button>
-      <JSONPretty data={displayed} theme={monikai} mainStyle="white-space: pre-wrap" />
-    </div>
+    <Card height="fill" overflow="auto" padding={4} sizing="border" tone="transparent">
+      <Stack space={5}>
+        <Stack space={3}>
+          <Label>Count payload</Label>
+          <Inline space={2}>
+            <Button icon={RemoveIcon} mode="ghost" padding={2} type="button" onClick={handleDecr} />
+            <Text>{count}</Text>
+            <Button icon={AddIcon} mode="ghost" padding={2} type="button" onClick={handleIncr} />
+          </Inline>
+        </Stack>
+
+        <Stack space={3}>
+          <Label>Displayed document</Label>
+          <Code language="json">{JSON.stringify(displayed, null, 2)}</Code>
+        </Stack>
+      </Stack>
+    </Card>
   )
 }
 
