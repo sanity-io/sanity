8c3af65d7073fd8dcf450954b4fc203280f11c56
Marius Lundgård <studio@mariuslundgard.com>
2021-09-22 14:02:28 +0200

    refactor(desk-tool): improve `UserComponentPane`

--- packages/@sanity/desk-tool/src/panes/userComponent/UserComponentPane.tsx
+++ packages/@sanity/desk-tool/src/panes/userComponent/UserComponentPane.tsx
@@ -1,7 +1,11 @@
-import React, {createElement, isValidElement, useCallback, useRef, useState} from 'react'
+import React, {createElement, isValidElement, useCallback, useRef} from 'react'
 import {MenuItem, MenuItemGroup} from '@sanity/base/__legacy/@sanity/components'
+import {Button} from '@sanity/ui'
 import {isValidElementType} from 'react-is'
+import {ArrowLeftIcon} from '@sanity/icons'
 import {PaneContextMenuButton, Pane, PaneHeader} from '../../components/pane'
+import {useDeskTool} from '../../contexts/deskTool'
+import {BackLink} from '../../contexts/paneRouter'
 import {BaseDeskToolPaneProps} from '../types'
 import {DeskToolPaneActionHandler} from '../../types'
 
@@ -18,8 +22,8 @@ type UserComponentPaneProps = BaseDeskToolPaneProps<{
  */
 export function UserComponentPane(props: UserComponentPaneProps) {
   const {index, isSelected, pane, ...restProps} = props
+  const {features} = useDeskTool()
   const {component, menuItems = [], menuItemGroups = [], title = ''} = pane
-  const [rootElement, setRootElement] = useState<HTMLDivElement | null>(null)
   const userComponent = useRef<{
     actionHandlers?: Record<string, DeskToolPaneActionHandler>
   } | null>(null)
@@ -45,17 +49,19 @@ export function UserComponentPane(props: UserComponentPaneProps) {
   }, [])
 
   const actions = menuItems.length > 0 && (
-    <PaneContextMenuButton
-      boundaryElement={rootElement}
-      items={menuItems}
-      itemGroups={menuItemGroups}
-      onAction={handleAction}
-    />
+    <PaneContextMenuButton items={menuItems} itemGroups={menuItemGroups} onAction={handleAction} />
   )
 
   return (
-    <Pane data-index={index} minWidth={320} selected={isSelected} ref={setRootElement}>
-      <PaneHeader actions={actions} title={title} />
+    <Pane data-index={index} minWidth={320} selected={isSelected}>
+      <PaneHeader
+        actions={actions}
+        backButton={
+          features.backButton &&
+          index > 0 && <Button as={BackLink} data-as="a" icon={ArrowLeftIcon} mode="bleed" />
+        }
+        title={title}
+      />
 
       {isValidElementType(component) &&
         createElement(component, {...restProps, ref: userComponent})}
