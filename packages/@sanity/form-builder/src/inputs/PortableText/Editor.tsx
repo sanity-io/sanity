bfa04797d8f984060b5db30a011bced54479a039
Per-Kristian Nordnes <per.kristian.nordnes@gmail.com>
2021-09-23 18:02:04 +0200

    refactor(form-builder): optimize pte component rendering with hooks
    refactor(form-builder): optimize rendering of editor with hooks
    
    refactor(form-builder): optimize rendering of input with hooks
    
    refactor(form-builder): optimize rendering of block object with hooks
    
    refactor(form-builder): optimize rendering of block object preview with hooks
    
    refactor(form-builder): optimize rendering of edit object with hooks
    
    refactor(form-builder): optimize rendering of inline object with hooks

--- packages/@sanity/form-builder/src/inputs/PortableText/Editor.tsx
+++ packages/@sanity/form-builder/src/inputs/PortableText/Editor.tsx
@@ -15,16 +15,15 @@ import {
 } from '@sanity/portable-text-editor'
 import {Marker} from '@sanity/types'
 import {useLayer} from '@sanity/ui'
-import {FOCUS_TERMINATOR} from '@sanity/util/paths'
 import {ScrollContainer} from '@sanity/base/components'
-import React, {useMemo, useCallback, useEffect, useState} from 'react'
+import React, {useMemo, useEffect, useCallback} from 'react'
 import PatchEvent from '../../PatchEvent'
 import styles from './PortableTextInput.module.css'
 import Toolbar from './Toolbar/Toolbar'
 import {ExpandCollapseButton} from './expandCollapseButton'
-import BlockExtrasOverlay from './BlockExtrasOverlay'
 import {RenderBlockActions, RenderCustomMarkers} from './types'
 import Decorator from './Text/Decorator'
+import BlockExtrasOverlay from './BlockExtrasOverlay'
 
 type Props = {
   initialSelection?: EditorSelection
@@ -60,7 +59,6 @@ function PortableTextSanityEditor(props: Props) {
     markers,
     onCopy,
     onFocus,
-    onFormBuilderChange,
     onPaste,
     onToggleFullscreen,
     readOnly,
@@ -68,115 +66,117 @@ function PortableTextSanityEditor(props: Props) {
     renderBlock,
     renderBlockActions,
     renderChild,
-    renderCustomMarkers,
     setPortalElement,
     setScrollContainerElement,
     value,
   } = props
 
   const editor = usePortableTextEditor()
-  const ptFeatures = useMemo(() => PortableTextEditor.getPortableTextFeatures(editor), [])
+  const ptFeatures = useMemo(() => PortableTextEditor.getPortableTextFeatures(editor), [editor])
   const {isTopLayer} = useLayer()
 
-  // eslint-disable-next-line no-unused-vars
-  const handleOpenObjectHotkey = (
-    event: React.BaseSyntheticEvent,
-    ptEditor: PortableTextEditor
-  ) => {
-    const selection = PortableTextEditor.getSelection(ptEditor)
-    if (selection) {
-      event.preventDefault()
-      event.stopPropagation()
-      const {focus} = selection
-      const activeAnnotations = PortableTextEditor.activeAnnotations(ptEditor)
-      const focusBlock = PortableTextEditor.focusBlock(ptEditor)
-      const focusChild = PortableTextEditor.focusChild(ptEditor)
-      if (activeAnnotations.length > 0) {
-        onFocus([
-          ...focus.path.slice(0, 1),
-          'markDefs',
-          {_key: activeAnnotations[0]._key},
-          FOCUS_TERMINATOR,
-        ])
-        return
-      }
-      if (focusChild && PortableTextEditor.isVoid(ptEditor, focusChild)) {
-        onFocus([...focus.path, FOCUS_TERMINATOR])
-        return
-      }
-      if (focusBlock && PortableTextEditor.isVoid(ptEditor, focusBlock)) {
-        onFocus([...focus.path.slice(0, 1), FOCUS_TERMINATOR])
-      }
-    }
-  }
-  const customFromProps: HotkeyOptions = {
-    custom: {
-      'mod+enter': props.onToggleFullscreen,
-      // 'mod+o': handleOpenObjectHotkey, // TODO: disabled for now, enable when we agree on the hotkey
-      ...(props.hotkeys || {}).custom,
-    },
-  }
-  const defaultHotkeys = {marks: {}}
-  ptFeatures.decorators.forEach((dec) => {
-    switch (dec.value) {
-      case 'strong':
-        defaultHotkeys.marks['mod+b'] = dec.value
-        break
-      case 'em':
-        defaultHotkeys.marks['mod+i'] = dec.value
-        break
-      case 'underline':
-        defaultHotkeys.marks['mod+u'] = dec.value
-        break
-      case 'code':
-        defaultHotkeys.marks["mod+'"] = dec.value
-        break
-      default:
-      // Nothing
-    }
-  })
-  const marksFromProps: HotkeyOptions = {
-    marks: {
-      ...defaultHotkeys.marks,
-      ...(props.hotkeys || {}).marks,
-    },
-  }
-  const hotkeys: HotkeyOptions = {
-    ...marksFromProps,
-    ...customFromProps,
-  }
+  // TODO: Enable when we agree upon the hotkey for opening edit object interface when block object is focused
+  //
+  // const handleOpenObjectHotkey = (
+  //   event: React.BaseSyntheticEvent,
+  //   ptEditor: PortableTextEditor
+  // ) => {
+  //   const selection = PortableTextEditor.getSelection(ptEditor)
+  //   if (selection) {
+  //     event.preventDefault()
+  //     event.stopPropagation()
+  //     const {focus} = selection
+  //     const activeAnnotations = PortableTextEditor.activeAnnotations(ptEditor)
+  //     const focusBlock = PortableTextEditor.focusBlock(ptEditor)
+  //     const focusChild = PortableTextEditor.focusChild(ptEditor)
+  //     if (activeAnnotations.length > 0) {
+  //       onFocus([
+  //         ...focus.path.slice(0, 1),
+  //         'markDefs',
+  //         {_key: activeAnnotations[0]._key},
+  //         FOCUS_TERMINATOR,
+  //       ])
+  //       return
+  //     }
+  //     if (focusChild && PortableTextEditor.isVoid(ptEditor, focusChild)) {
+  //       onFocus([...focus.path, FOCUS_TERMINATOR])
+  //       return
+  //     }
+  //     if (focusBlock && PortableTextEditor.isVoid(ptEditor, focusBlock)) {
+  //       onFocus([...focus.path.slice(0, 1), FOCUS_TERMINATOR])
+  //     }
+  //   }
+  // }
 
-  const hasMarkers = markers.length > 0
-  const scClassNames = [
-    styles.scrollContainer,
-    ...(renderBlockActions || hasMarkers ? [styles.hasBlockExtras] : [styles.hasNoBlockExtras]),
-  ].join(' ')
-  const editorWrapperClassNames = [styles.editorWrapper].join(' ')
-  const editorClassNames = [
-    styles.editor,
-    ...(renderBlockActions || hasMarkers ? [styles.hasBlockExtras] : [styles.hasNoBlockExtras]),
-  ].join(' ')
-
-  const blockExtras = useCallback(
-    () => (
-      <BlockExtrasOverlay
-        isFullscreen={isFullscreen}
-        markers={markers}
-        onFocus={onFocus}
-        onChange={onFormBuilderChange}
-        renderBlockActions={readOnly ? undefined : renderBlockActions}
-        renderCustomMarkers={renderCustomMarkers}
-        value={value}
-      />
-    ),
-    [isFullscreen, value]
+  const customFromProps: HotkeyOptions = useMemo(
+    () => ({
+      custom: {
+        'mod+enter': props.onToggleFullscreen,
+        // 'mod+o': handleOpenObjectHotkey, // TODO: disabled for now, enable when we agree on the hotkey
+        ...(props.hotkeys || {}).custom,
+      },
+    }),
+    [props.hotkeys, props.onToggleFullscreen]
   )
 
-  // Needed for rendering the overlay in the correct place when toggling fullscreen.
-  const [forceUpdate, setForceUpdate] = useState(0)
-  useEffect(() => {
-    setForceUpdate(forceUpdate + 1)
-  }, [])
+  const defaultHotkeys = useMemo(() => {
+    const def = {marks: {}}
+    ptFeatures.decorators.forEach((dec) => {
+      switch (dec.value) {
+        case 'strong':
+          def.marks['mod+b'] = dec.value
+          break
+        case 'em':
+          def.marks['mod+i'] = dec.value
+          break
+        case 'underline':
+          def.marks['mod+u'] = dec.value
+          break
+        case 'code':
+          def.marks["mod+'"] = dec.value
+          break
+        default:
+        // Nothing
+      }
+    })
+    return def
+  }, [ptFeatures.decorators])
+
+  const marksFromProps: HotkeyOptions = useMemo(
+    () => ({
+      marks: {
+        ...defaultHotkeys.marks,
+        ...(props.hotkeys || {}).marks,
+      },
+    }),
+    [props.hotkeys, defaultHotkeys]
+  )
+  const hotkeys: HotkeyOptions = useMemo(
+    () => ({
+      ...marksFromProps,
+      ...customFromProps,
+    }),
+    [marksFromProps, customFromProps]
+  )
+
+  const hasMarkers = useMemo(() => markers.length > 0, [markers])
+  const scClassNames = useMemo(
+    () =>
+      [
+        styles.scrollContainer,
+        ...(renderBlockActions || hasMarkers ? [styles.hasBlockExtras] : [styles.hasNoBlockExtras]),
+      ].join(' '),
+    [hasMarkers, renderBlockActions]
+  )
+  const editorWrapperClassNames = useMemo(() => [styles.editorWrapper].join(' '), [])
+  const editorClassNames = useMemo(
+    () =>
+      [
+        styles.editor,
+        ...(renderBlockActions || hasMarkers ? [styles.hasBlockExtras] : [styles.hasNoBlockExtras]),
+      ].join(' '),
+    [hasMarkers, renderBlockActions]
+  )
 
   useEffect(() => {
     if (!isTopLayer || !isFullscreen) return undefined
@@ -196,7 +196,30 @@ function PortableTextSanityEditor(props: Props) {
     }
   }, [isFullscreen, isTopLayer, onToggleFullscreen])
 
-  const _editor = useMemo(
+  const blockExtras = useCallback(
+    () => (
+      <BlockExtrasOverlay
+        isFullscreen={isFullscreen}
+        markers={markers}
+        onFocus={onFocus}
+        onChange={props.onFormBuilderChange}
+        renderBlockActions={readOnly ? undefined : renderBlockActions}
+        renderCustomMarkers={props.renderCustomMarkers}
+        value={value}
+      />
+    ),
+    [
+      isFullscreen,
+      markers,
+      onFocus,
+      props.onFormBuilderChange,
+      props.renderCustomMarkers,
+      readOnly,
+      renderBlockActions,
+      value,
+    ]
+  )
+  const sanityEditor = useMemo(
     () => (
       <div className={styles.editorBox}>
         <div className={styles.header}>
@@ -241,9 +264,28 @@ function PortableTextSanityEditor(props: Props) {
         </div>
       </div>
     ),
-    [initialSelection, isFullscreen, value, readOnly, forceUpdate]
+    [
+      blockExtras,
+      editorClassNames,
+      editorWrapperClassNames,
+      hotkeys,
+      initialSelection,
+      isFullscreen,
+      onCopy,
+      onFocus,
+      onPaste,
+      onToggleFullscreen,
+      readOnly,
+      renderAnnotation,
+      renderBlock,
+      renderChild,
+      scClassNames,
+      setPortalElement,
+      setScrollContainerElement,
+      value,
+    ]
   )
-  return _editor
+  return sanityEditor
 }
 
 export default PortableTextSanityEditor
