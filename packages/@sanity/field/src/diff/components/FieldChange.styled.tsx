1aaefb82d5b4527a4f74c71da0dd843e44df0ac1
Rita <rita@sanity.io>
2021-09-24 14:26:57 +0200

    doc(ChangeList): add comment to explain popover styled component
    doc(FieldChange): add comment to explain popover styled component
    
    doc(GroupChange): add comment to explain popover styled component

--- packages/@sanity/field/src/diff/components/FieldChange.styled.tsx
+++ packages/@sanity/field/src/diff/components/FieldChange.styled.tsx
@@ -34,6 +34,9 @@ export const DiffBorder = styled.div`
 `
 
 export const PopoverWrapper = styled(Popover)`
+  /* hides the popover when the target of it has left the visible part of the window.
+   without it, the popover will be on top of the headers (document title & changes)
+   and footers (changed notifications, publish button etc)*/
   &[data-popper-reference-hidden='true'] {
     display: none;
   }
--- packages/@sanity/field/src/diff/components/GroupChange.styled.tsx
+++ packages/@sanity/field/src/diff/components/GroupChange.styled.tsx
@@ -32,6 +32,9 @@ export const GroupChangeContainer = styled.div`
 `
 
 export const PopoverWrapper = styled(Popover)`
+  /* hides the popover when the target of it has left the visible part of the window.
+   without it, the popover will be on top of the headers (document title & changes)
+   and footers (changed notifications, publish button etc)*/
   &[data-popper-reference-hidden='true'] {
     display: none;
   }
