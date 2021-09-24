8718b13d04c77ebc8c3dadf2d994e8230af26d9a
Rita <rita@sanity.io>
2021-09-24 13:14:07 +0200

    fix(GroupChange): fix issue when scrolling to the bottom of the page where the popover dragged to the right
    Also moved the styled components to a separate styled file

--- packages/@sanity/field/src/diff/components/GroupChange.tsx
+++ packages/@sanity/field/src/diff/components/GroupChange.tsx
@@ -1,8 +1,7 @@
 import {useDocumentOperation} from '@sanity/react-hooks'
 import React, {useCallback, useContext, useState} from 'react'
 import {unstable_useCheckDocumentPermission as useCheckDocumentPermission} from '@sanity/base/hooks'
-import {Box, rem, Stack, Button, Grid, Popover, Text, useClickOutside} from '@sanity/ui'
-import styled from 'styled-components'
+import {Box, Stack, Button, Grid, Popover, Text, useClickOutside} from '@sanity/ui'
 import {undoChange} from '../changes/undoChange'
 import {isFieldChange} from '../helpers'
 import {isPTSchemaType} from '../../types/portableText/diff'
@@ -15,35 +14,7 @@ import {ChangeResolver} from './ChangeResolver'
 import {DocumentChangeContext} from './DocumentChangeContext'
 import {RevertChangesButton} from './RevertChangesButton'
 
-const ChangeListWrapper = styled.div`
-  display: grid;
-  grid-template-columns: minmax(0, 1fr);
-`
-
-const GroupChangeContainer = styled.div`
-  --field-change-error: ${({theme}) => theme.sanity.color.solid.critical.enabled.bg};
-  --diff-inspect-padding-xsmall: ${({theme}) => rem(theme.sanity.space[1])};
-  --diff-inspect-padding-small: ${({theme}) => rem(theme.sanity.space[2])};
-
-  position: relative;
-  padding: var(--diff-inspect-padding-xsmall) var(--diff-inspect-padding-small);
-
-  &::before {
-    content: '';
-    display: block;
-    position: absolute;
-    top: 0;
-    left: 0;
-    bottom: 0;
-    border-left: 1px solid var(--card-border-color);
-  }
-
-  &[data-error]:hover::before,
-  &[data-revert-group-hover]:hover::before,
-  &[data-revert-all-groups-hover]::before {
-    border-left: 2px solid var(--field-change-error);
-  }
-`
+import {ChangeListWrapper, GroupChangeContainer, PopoverWrapper} from './GroupChange.styled'
 
 export function GroupChange({
   change: group,
@@ -136,7 +107,7 @@ export function GroupChange({
         </FieldWrapper>
       )}
 
-      <Popover
+      <PopoverWrapper
         content={
           <Box>
             Are you sure you want to revert the changes?
