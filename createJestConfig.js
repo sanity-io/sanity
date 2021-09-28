2780ebc4a7f3d1f9773a15d0ccfb08b969f93574
Rico Kahler <ricokahler@gmail.com>
2021-09-28 08:33:20 -0400

    test: mock useElementRect in jest-setup

--- createJestConfig.js
+++ createJestConfig.js
@@ -124,7 +124,7 @@ function createProjectConfig({
     },
     testMatch: [...testMatch, '<rootDir>/**/*.{test,spec}.{js,ts,tsx}'],
     testPathIgnorePatterns: [...testPathIgnorePatterns, '/(node_modules|lib|dist|bin|coverage)/'],
-    setupFiles: [...setupFiles, require.resolve('./test/jest-setup.js')],
+    setupFiles: [...setupFiles, path.resolve(__dirname, './test/jest-setup.ts')],
     testURL: 'http://localhost:3333',
     testEnvironment: 'jsdom',
     globals: {__DEV__: false, ...globals},
