2ab997ee967421331423cadf79dbefc908456058
Per-Kristian Nordnes <per.kristian.nordnes@gmail.com>
2021-09-28 19:04:05 +0200

    test(form-builder): add to story values

--- packages/@sanity/form-builder/src/inputs/PortableText/__workshop__/default/values.ts
+++ packages/@sanity/form-builder/src/inputs/PortableText/__workshop__/default/values.ts
@@ -16,6 +16,66 @@ export const values = {
         },
       ],
     },
+    {
+      _type: 'block',
+      _key: 'b',
+      style: 'normal',
+      markDefs: [],
+      listItem: 'bullet',
+      children: [
+        {
+          _type: 'span',
+          _key: 'a1',
+          text: 'This is the first bullet list item',
+          marks: [],
+        },
+      ],
+    },
+    {
+      _type: 'block',
+      _key: 'c',
+      style: 'normal',
+      markDefs: [],
+      listItem: 'bullet',
+      children: [
+        {
+          _type: 'span',
+          _key: 'c1',
+          text: 'This is the second bullet list item',
+          marks: [],
+        },
+      ],
+    },
+    {
+      _type: 'block',
+      _key: 'd',
+      style: 'normal',
+      markDefs: [],
+      listItem: 'bullet',
+      children: [
+        {
+          _type: 'span',
+          _key: 'd1',
+          text: 'This is the third bullet list item',
+          marks: [],
+        },
+      ],
+    },
+    {
+      _type: 'block',
+      _key: 'e',
+      style: 'normal',
+      markDefs: [],
+      children: [
+        {
+          _type: 'span',
+          _key: 'e1',
+          text:
+            'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+          marks: [],
+        },
+      ],
+    },
   ],
 }
 
