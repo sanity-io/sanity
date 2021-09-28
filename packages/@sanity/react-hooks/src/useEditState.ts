09c7e11263fe8f7dd8efe73751c3e26aeb02f172
Marius Lundgård <studio@mariuslundgard.com>
2021-09-28 18:48:12 +0200

    fix(react-hooks): improve reset and memoization of `useEditState`

--- packages/@sanity/react-hooks/src/useEditState.ts
+++ packages/@sanity/react-hooks/src/useEditState.ts
@@ -1,12 +1,23 @@
 // @todo: remove the following line when part imports has been removed from this file
 ///<reference types="@sanity/types/parts" />
 
+import type {EditStateFor} from '@sanity/base/_internal'
 import documentStore from 'part:@sanity/base/datastore/document'
 import {useMemoObservable} from 'react-rx'
+import {startWith} from 'rxjs/operators'
 
-export function useEditState(publishedDocId: string, docTypeName: string) {
-  return useMemoObservable(() => documentStore.pair.editState(publishedDocId, docTypeName), [
-    publishedDocId,
-    docTypeName,
-  ])
+export function useEditState(publishedDocId: string, docTypeName: string): EditStateFor {
+  return useMemoObservable(
+    () =>
+      documentStore.pair.editState(publishedDocId, docTypeName).pipe(
+        startWith({
+          id: publishedDocId,
+          type: docTypeName,
+          draft: null,
+          published: null,
+          liveEdit: false,
+        })
+      ),
+    [publishedDocId, docTypeName]
+  ) as EditStateFor
 }
