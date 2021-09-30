07a2595d87b0e70d5d109a2477e7af6d35429588
Per-Kristian Nordnes <per.kristian.nordnes@gmail.com>
2021-09-30 16:09:06 +0200

    refactor(form-builder): migrate PTE focus skipper button to Sanity/UI

---
+++ packages/@sanity/form-builder/src/inputs/PortableText/VisibleOnFocusButton.tsx
@@ -0,0 +1,35 @@
+import React, {useCallback, useMemo, CSSProperties} from 'react'
+
+import styled from 'styled-components'
+import {Button, useLayer} from '@sanity/ui'
+
+const StyledButton = styled(Button)`
+  position: absolute;
+  height: 1px;
+  width: 1px;
+  overflow: hidden;
+  clip: rect(1px, 1px, 1px, 1px);
+
+  &:focus {
+    width: auto;
+    height: auto;
+    clip: auto;
+  }
+`
+type Props = {
+  children: React.ReactNode
+  onClick: () => void
+  style: CSSProperties
+}
+export default function VisibleOnFocusButton({children, onClick, style}: Props) {
+  const {zIndex} = useLayer()
+  const zIndexStyle = useMemo(() => ({zIndex: zIndex + 3, ...style}), [zIndex, style])
+  const handleClick = useCallback(() => {
+    onClick()
+  }, [onClick])
+  return (
+    <StyledButton mode="ghost" onClick={handleClick} style={zIndexStyle}>
+      {children}
+    </StyledButton>
+  )
+}
