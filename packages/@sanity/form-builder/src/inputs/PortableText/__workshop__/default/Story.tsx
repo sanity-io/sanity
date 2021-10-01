aab9ccac28edf21bb15c2939dfc54ba48d45ff73
Marius Lundgård <studio@mariuslundgard.com>
2021-10-01 12:47:29 +0200

    test(form-builder): improve PT workshop

--- packages/@sanity/form-builder/src/inputs/PortableText/__workshop__/default/Story.tsx
+++ packages/@sanity/form-builder/src/inputs/PortableText/__workshop__/default/Story.tsx
@@ -12,7 +12,7 @@ export default function Story() {
 
   return (
     <LayerProvider>
-      <Card height="fill" padding={4}>
+      <Card height="fill" padding={4} sizing="border">
         <Flex align="center" height="fill" justify="center">
           <Container width={1}>
             <TestInput readOnly={readOnly} value={value} type={portableTextType} schema={schema} />
