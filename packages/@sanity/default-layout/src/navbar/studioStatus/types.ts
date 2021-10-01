54a9188f8b350af41cc6ef0f2484d41d48d214a2
Espen Hovlandsdal <espen@hovlandsdal.com>
2021-10-01 14:28:38 +0200

    feat(default-layout): replace VersionChecker with useModuleStatus hook

--- packages/@sanity/default-layout/src/navbar/studioStatus/types.ts
+++ packages/@sanity/default-layout/src/navbar/studioStatus/types.ts
@@ -3,6 +3,6 @@ export type Severity = 'notice' | 'low' | 'medium' | 'high'
 export interface Package {
   name: string
   latest: string
-  severity: Severity
   version: string
+  severity?: Severity
 }
