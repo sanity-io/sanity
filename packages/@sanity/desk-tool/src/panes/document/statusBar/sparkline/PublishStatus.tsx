9e1d9900c5599e97edac6c7d6ed51edc93f05ebb
Mikolaj Dobrucki <5467602+mikolajdobrucki@users.noreply.github.com>
2021-10-05 10:14:18 +0200

    feat(desk-tool): redesign document published status

--- packages/@sanity/desk-tool/src/panes/document/statusBar/sparkline/PublishStatus.tsx
+++ packages/@sanity/desk-tool/src/panes/document/statusBar/sparkline/PublishStatus.tsx
@@ -1,6 +1,7 @@
+/* eslint-disable complexity */
 import {useTimeAgo} from '@sanity/base/hooks'
 
-import {Box, Flex, Text} from '@sanity/ui'
+import {Box, Button, Flex, Stack, Text, Tooltip} from '@sanity/ui'
 import {PlayIcon, PublishIcon} from '@sanity/icons'
 import React from 'react'
 import styled from 'styled-components'
@@ -20,30 +21,46 @@ const Root = styled(Flex)`
 export function PublishStatus(props: PublishStatusProps) {
   const {disabled, lastPublished, lastUpdated, liveEdit} = props
 
-  const lastPublishedTimeAgo = useTimeAgo(lastPublished || '', {
-    minimal: true,
-    agoSuffix: true,
-  })
+  const lastPublishedTimeAgo = useTimeAgo(lastPublished || '', {minimal: true, agoSuffix: true})
+  const lastPublishedTime = useTimeAgo(lastPublished || '', {minimal: true})
 
   const lastUpdatedTimeAgo = useTimeAgo(lastUpdated || '', {minimal: true, agoSuffix: true})
+  const lastUpdatedTime = useTimeAgo(lastUpdated || '', {minimal: true})
 
   return (
-    <Root align="center" data-ui="SessionLayout" padding={2} sizing="border">
-      {liveEdit && <IconBadge disabled={disabled} icon={PlayIcon} tone="critical" />}
-      {!liveEdit && <IconBadge disabled={disabled} icon={PublishIcon} tone="positive" />}
-
-      <Box flex={1} marginLeft={2}>
-        <Text muted size={0} weight="semibold">
-          Published
-        </Text>
-
-        <Box marginTop={1}>
-          <Text muted size={0}>
-            {liveEdit && <>{lastUpdated ? lastUpdatedTimeAgo : lastPublishedTimeAgo}</>}
-            {!liveEdit && <>{lastPublishedTimeAgo}</>}
-          </Text>
-        </Box>
-      </Box>
+    <Root align="center" data-ui="SessionLayout" sizing="border">
+      <Tooltip
+        content={
+          <Stack padding={3} space={3}>
+            <Text size={1} muted>
+              {liveEdit && (
+                <>Last updated {lastUpdated ? lastUpdatedTimeAgo : lastPublishedTimeAgo}</>
+              )}
+              {!liveEdit && <>Last published {lastPublishedTimeAgo}</>}
+            </Text>
+          </Stack>
+        }
+      >
+        <Button
+          mode="bleed"
+          tone={liveEdit ? 'critical' : 'positive'}
+          tabIndex={-1}
+          disabled={disabled}
+        >
+          <Flex align="center">
+            <Box marginRight={3}>
+              <Text size={3}>
+                {liveEdit && <PlayIcon />}
+                {!liveEdit && <PublishIcon />}
+              </Text>
+            </Box>
+            <Text size={1} weight="medium">
+              {liveEdit && <>{lastUpdated ? lastUpdatedTime : lastPublishedTime}</>}
+              {!liveEdit && lastPublishedTime}
+            </Text>
+          </Flex>
+        </Button>
+      </Tooltip>
     </Root>
   )
 }
