1c80c8c1ed3d50e3e50399bddc45bfdb577e6db7
Marius Lundgård <studio@mariuslundgard.com>
2021-10-05 15:36:13 +0200

    refactor(desk-tool): migrate `FullscreenObjectEditing` to `@sanity/ui`

--- packages/@sanity/form-builder/src/inputs/PortableText/Objects/renderers/FullscreenObjectEditing.tsx
+++ packages/@sanity/form-builder/src/inputs/PortableText/Objects/renderers/FullscreenObjectEditing.tsx
@@ -1,12 +1,7 @@
-// @todo: remove the following line when part imports has been removed from this file
-///<reference types="@sanity/types/parts" />
-
-/* eslint-disable react/prop-types */
-
 import {PortableTextBlock, Type, PortableTextChild} from '@sanity/portable-text-editor'
 import {FormFieldPresence, PresenceOverlay} from '@sanity/base/presence'
 import {Path, Marker, SchemaType} from '@sanity/types'
-import FullscreenDialog from 'part:@sanity/components/dialogs/fullscreen'
+import {Box, Dialog} from '@sanity/ui'
 import React, {useCallback} from 'react'
 import {FormBuilderInput} from '../../../../FormBuilderInput'
 import {PatchEvent} from '../../../../PatchEvent'
@@ -17,7 +12,7 @@ interface FullscreenObjectEditingProps {
   object: PortableTextBlock | PortableTextChild
   onBlur: () => void
   onChange: (patchEvent: PatchEvent, path: Path) => void
-  onClose: (event: React.SyntheticEvent) => void
+  onClose: () => void
   onFocus: (path: Path) => void
   path: Path
   presence: FormFieldPresence[]
@@ -43,28 +38,31 @@ export function FullscreenObjectEditing({
     path,
   ])
 
-  const handleEscape = useCallback((event: React.SyntheticEvent): void => onClose(event), [onClose])
-
   return (
-    <FullscreenDialog isOpen onClose={onClose} onEscape={handleEscape} title={type.title}>
-      {/* @todo: styling */}
-      {/* <div className={styles.formBuilderInputWrapper}> */}
-
+    <Dialog
+      header={type.title}
+      // @todo
+      id=""
+      onClose={onClose}
+      width="auto"
+    >
       <PresenceOverlay margins={[0, 0, 1, 0]}>
-        <FormBuilderInput
-          focusPath={focusPath}
-          level={0}
-          markers={markers}
-          onBlur={onBlur}
-          onChange={handleChange}
-          onFocus={onFocus}
-          path={path}
-          presence={presence}
-          readOnly={readOnly || type.readOnly}
-          type={type as SchemaType}
-          value={object}
-        />
+        <Box padding={4}>
+          <FormBuilderInput
+            focusPath={focusPath}
+            level={0}
+            markers={markers}
+            onBlur={onBlur}
+            onChange={handleChange}
+            onFocus={onFocus}
+            path={path}
+            presence={presence}
+            readOnly={readOnly || type.readOnly}
+            type={type as SchemaType}
+            value={object}
+          />
+        </Box>
       </PresenceOverlay>
-    </FullscreenDialog>
+    </Dialog>
   )
 }
