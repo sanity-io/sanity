aaed9e9a0103d1eb0023269eab4160c5aabd25e3
Per-Kristian Nordnes <per.kristian.nordnes@gmail.com>
2021-10-06 13:28:36 +0200

    test(form-builder): add to PT input workshop values

--- packages/@sanity/form-builder/src/inputs/PortableText/__workshop__/default/values.ts
+++ packages/@sanity/form-builder/src/inputs/PortableText/__workshop__/default/values.ts
@@ -5,13 +5,30 @@ export const values = {
       _type: 'block',
       _key: 'a',
       style: 'normal',
-      markDefs: [],
+      markDefs: [
+        {
+          _key: '123',
+          _type: 'link',
+        },
+      ],
       children: [
         {
           _type: 'span',
           _key: 'a1',
+          text: 'Lorem ipsum dolor sit amet, ',
+          marks: [],
+        },
+        {
+          _type: 'span',
+          _key: 'a2',
+          text: 'consectetur',
+          marks: ['123'],
+        },
+        {
+          _type: 'span',
+          _key: 'a3',
           text:
-            'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+            ' adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
           marks: [],
         },
       ],
@@ -22,11 +39,24 @@ export const values = {
       style: 'normal',
       markDefs: [],
       listItem: 'bullet',
+      level: 1,
       children: [
         {
           _type: 'span',
-          _key: 'a1',
-          text: 'This is the first bullet list item',
+          _key: 'b1',
+          text: 'This is the ',
+          marks: [],
+        },
+        {
+          _type: 'span',
+          _key: 'b2',
+          text: 'first',
+          marks: ['strong'],
+        },
+        {
+          _type: 'span',
+          _key: 'b2',
+          text: ' bullet list item',
           marks: [],
         },
       ],
@@ -37,11 +67,24 @@ export const values = {
       style: 'normal',
       markDefs: [],
       listItem: 'bullet',
+      level: 1,
       children: [
         {
           _type: 'span',
           _key: 'c1',
-          text: 'This is the second bullet list item',
+          text: 'This is the ',
+          marks: [],
+        },
+        {
+          _type: 'span',
+          _key: 'c2',
+          text: 'second',
+          marks: ['strong'],
+        },
+        {
+          _type: 'span',
+          _key: 'c2',
+          text: ' bullet list item',
           marks: [],
         },
       ],
@@ -52,11 +95,24 @@ export const values = {
       style: 'normal',
       markDefs: [],
       listItem: 'bullet',
+      level: 1,
       children: [
         {
           _type: 'span',
           _key: 'd1',
-          text: 'This is the third bullet list item',
+          text: 'This is the ',
+          marks: [],
+        },
+        {
+          _type: 'span',
+          _key: 'd2',
+          text: 'third',
+          marks: ['strong'],
+        },
+        {
+          _type: 'span',
+          _key: 'd2',
+          text: ' bullet list item',
           marks: [],
         },
       ],
