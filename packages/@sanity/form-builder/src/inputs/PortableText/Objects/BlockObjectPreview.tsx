e4c9dadde911a853f4206c23b2d71e751c13083b
hermanwikner <wiknerherman@hotmail.com>
2021-10-12 12:11:22 +0200

    refactor(form-builder): fix menu button and UI issues in `BlockObjectPreview`

--- packages/@sanity/form-builder/src/inputs/PortableText/Objects/BlockObjectPreview.tsx
+++ packages/@sanity/form-builder/src/inputs/PortableText/Objects/BlockObjectPreview.tsx
@@ -1,8 +1,9 @@
 import {IntentLink} from '@sanity/base/router'
-import {EditIcon, LinkIcon, TrashIcon, EyeOpenIcon, ChevronDownIcon} from '@sanity/icons'
+import {EditIcon, LinkIcon, TrashIcon, EyeOpenIcon, EllipsisVerticalIcon} from '@sanity/icons'
 import {PortableTextBlock, Type} from '@sanity/portable-text-editor'
-import {Button, Menu, MenuButton, MenuButtonProps, MenuItem, ThemeColorProvider} from '@sanity/ui'
-import React, {useCallback} from 'react'
+import {Button, Menu, MenuButton, MenuButtonProps, MenuItem} from '@sanity/ui'
+import React, {forwardRef, useMemo} from 'react'
+import {useId} from '@reach/auto-id'
 import Preview from '../../../Preview'
 
 interface BlockObjectPreviewProps {
@@ -17,51 +18,54 @@ const POPOVER_PROPS: MenuButtonProps['popover'] = {
   constrainSize: true,
   placement: 'bottom',
   portal: true,
+  tone: 'default',
 }
 
 export function BlockObjectPreview(props: BlockObjectPreviewProps) {
   const {value, type, readOnly, onClickingEdit, onClickingDelete} = props
+  const menuButtonId = useId()
 
-  const ReferenceLink = useCallback(
-    (linkProps: any) => <IntentLink {...linkProps} intent="edit" params={{id: value._ref}} />,
+  const ReferenceLink = useMemo(
+    () =>
+      // eslint-disable-next-line no-shadow
+      forwardRef(function ReferenceLink(linkProps: any, ref: any) {
+        return <IntentLink {...linkProps} intent="edit" params={{id: value._ref}} ref={ref} />
+      }),
     [value?._ref]
   )
 
   return (
     <Preview
       actions={
-        <ThemeColorProvider tone="default">
-          <MenuButton
-            button={
-              <Button
-                fontSize={1}
-                iconRight={ChevronDownIcon}
-                mode="ghost"
-                text={type ? type.title || type.name : 'Unknown'}
-              />
-            }
-            // @todo
-            id=""
-            menu={
-              <Menu>
-                {value?._ref && (
-                  <MenuItem as={ReferenceLink} icon={LinkIcon} text="Open reference" />
-                )}
-                {readOnly && <MenuItem icon={EyeOpenIcon} onClick={onClickingEdit} text="View" />}
-                {!readOnly && <MenuItem icon={EditIcon} onClick={onClickingEdit} text="Edit" />}
-                {!readOnly && (
-                  <MenuItem
-                    icon={TrashIcon}
-                    onClick={onClickingDelete}
-                    text="Delete"
-                    tone="critical"
-                  />
-                )}
-              </Menu>
-            }
-            popover={POPOVER_PROPS}
-          />
-        </ThemeColorProvider>
+        <MenuButton
+          button={
+            <Button
+              fontSize={1}
+              iconRight={EllipsisVerticalIcon}
+              mode="bleed"
+              aria-label="Open menu"
+            />
+          }
+          id={menuButtonId}
+          menu={
+            <Menu>
+              {value?._ref && (
+                <MenuItem as={ReferenceLink} data-as="a" icon={LinkIcon} text="Open reference" />
+              )}
+              {readOnly && <MenuItem icon={EyeOpenIcon} onClick={onClickingEdit} text="View" />}
+              {!readOnly && <MenuItem icon={EditIcon} onClick={onClickingEdit} text="Edit" />}
+              {!readOnly && (
+                <MenuItem
+                  icon={TrashIcon}
+                  onClick={onClickingDelete}
+                  text="Delete"
+                  tone="critical"
+                />
+              )}
+            </Menu>
+          }
+          popover={POPOVER_PROPS}
+        />
       }
       type={type}
       value={value}
