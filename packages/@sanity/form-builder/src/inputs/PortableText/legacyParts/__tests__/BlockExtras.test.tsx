7e7470e5184ef56672ec1f13093acf13a47b25ed
Per-Kristian Nordnes <per.kristian.nordnes@gmail.com>
2021-10-12 14:29:58 +0200

    test(form-builder): test PT input block extras customizations.

---
+++ packages/@sanity/form-builder/src/inputs/PortableText/legacyParts/__tests__/BlockExtras.test.tsx
@@ -0,0 +1,131 @@
+// eslint-disable-next-line import/no-unassigned-import
+import '@testing-library/jest-dom/extend-expect'
+import {render, waitFor} from '@testing-library/react'
+import React from 'react'
+
+import {LayerProvider, studioTheme, ThemeProvider, ToastProvider} from '@sanity/ui'
+import Schema from '@sanity/schema'
+import {PortableTextInputProps} from '../../PortableTextInput'
+import {TestInput} from '../../__workshop__/TestInput'
+import {portableTextType} from './schema'
+
+const schema = Schema.compile({
+  name: 'test',
+  types: [portableTextType],
+})
+
+const value = [
+  {
+    _type: 'myTestBlockType',
+    _key: 'a',
+    style: 'normal',
+    markDefs: [
+      {
+        _key: '123',
+        _type: 'link',
+      },
+    ],
+    children: [
+      {
+        _type: 'span',
+        _key: 'a1',
+        text: 'Lorem ipsum dolor sit amet, ',
+        marks: [],
+      },
+      {
+        _type: 'span',
+        _key: 'a2',
+        text: 'consectetur',
+        marks: ['123'],
+      },
+      {
+        _type: 'span',
+        _key: 'a3',
+        text:
+          ' adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        marks: [],
+      },
+    ],
+  },
+  {
+    _type: 'myTestBlockType',
+    _key: 'b',
+    style: 'normal',
+    markDefs: [],
+    children: [
+      {
+        _type: 'span',
+        _key: 'b1',
+        text: 'Yo',
+        marks: [],
+      },
+    ],
+  },
+]
+const noop = () => {
+  // noop
+}
+const subscribe = () => noop
+
+function renderCustomMarkers(markers) {
+  return markers.map((marker, index) => {
+    if (marker.type === 'customMarkerTest') {
+      return (
+        <div key={`marker-${index}`} data-testid="custom-marker-test">
+          Marker!
+        </div>
+      )
+    }
+    return null
+  })
+}
+
+function renderBlockActions() {
+  return <div data-testid="block-action-test">Action!</div>
+}
+
+const markers = [{type: 'customMarkerTest', path: [{_key: value[0]._key}]}]
+
+function renderInput(props: Partial<PortableTextInputProps> = {}) {
+  const onBlur = jest.fn()
+  const onFocus = jest.fn()
+  const onChange = jest.fn()
+
+  const {queryByTestId, queryAllByTestId} = render(
+    <ThemeProvider scheme="light" theme={studioTheme}>
+      <LayerProvider>
+        <ToastProvider>
+          <TestInput
+            focusPath={[]}
+            type={schema.get('body')}
+            onBlur={onBlur}
+            onFocus={onFocus}
+            onChange={onChange}
+            markers={markers}
+            level={0}
+            renderCustomMarkers={renderCustomMarkers}
+            renderBlockActions={renderBlockActions}
+            readOnly={false}
+            presence={[]}
+            subscribe={subscribe}
+            value={value}
+            schema={schema}
+            {...(props || {})}
+          />
+        </ToastProvider>
+      </LayerProvider>
+    </ThemeProvider>
+  )
+  return {onChange, onFocus, queryByTestId, queryAllByTestId}
+}
+
+describe('Portable Text Editor Block Extras', () => {
+  test('custom markers', async () => {
+    const {queryByTestId} = renderInput()
+    await waitFor(() => expect(queryByTestId('custom-marker-test')).toBeTruthy())
+  })
+  test('block actions', async () => {
+    const {queryAllByTestId} = renderInput()
+    await waitFor(() => expect(queryAllByTestId('block-action-test').length).toBe(2))
+  })
+})
---
+++ packages/@sanity/form-builder/src/inputs/PortableText/legacyParts/__tests__/schema.ts
@@ -0,0 +1,32 @@
+const imageType = {
+  type: 'image',
+  name: 'blockImage',
+}
+
+export const someObject = {
+  type: 'object',
+  name: 'someObject',
+  fields: [{type: 'string', name: 'color'}],
+}
+
+export const blockType = {
+  type: 'block',
+  name: 'myTestBlockType',
+  styles: [
+    {title: 'Normal', value: 'normal'},
+    {title: 'H1', value: 'h1'},
+    {title: 'H2', value: 'h2'},
+    {title: 'H3', value: 'h3'},
+    {title: 'H4', value: 'h4'},
+    {title: 'H5', value: 'h5'},
+    {title: 'H6', value: 'h6'},
+    {title: 'Quote', value: 'blockquote'},
+  ],
+  of: [someObject, imageType],
+}
+
+export const portableTextType = {
+  type: 'array',
+  name: 'body',
+  of: [blockType, someObject],
+}
