97d5333df5a01c2358c73adf6b666e1870b51e3a
Mikolaj Dobrucki <5467602+mikolajdobrucki@users.noreply.github.com>
2021-10-12 15:00:38 +0200

    fix(default-layout): fix glitches in app loader
    – try removing animation glitches from logo when app is laoding
    
    – also change loader background to white

--- packages/@sanity/base/src/components/AppLoadingScreen.js
+++ packages/@sanity/base/src/components/AppLoadingScreen.js
@@ -6,7 +6,7 @@ const AppLoadingScreenStyles = `
 .sanity-app-loading-screen {
   font-family: system-ui, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
   text-align: center;
-  background-color: #F1F3F6;
+  background-color: white;
   color: #1C2430;
   display: flex;
   width: 100vw;
@@ -36,11 +36,11 @@ const AppLoadingScreenStyles = `
   from {
     transform: scale3d(0.75, 0.75, 0.75);
   }
-  
+
   50% {
     transform: scale3d(1, 1, 1);
   }
-  
+
   to {
     transform: scale3d(0.75, 0.75, 0.75);
   }
@@ -53,25 +53,25 @@ const AppLoaderStyles = `
     stroke: #F03E2F;
     opacity: 0;
   }
-  
+
   .fillShape--bottom {
     stroke-dasharray: 90;
     stroke-dashoffset: 85;
     animation: bottom var(--time) ease-in infinite;
   }
-  
+
   .fillShape--middle {
     stroke-dasharray: 115;
     stroke-dashoffset: 110;
     animation: middle var(--time) linear infinite;
   }
-  
+
   .fillShape--top {
     stroke-dasharray: 77;
     stroke-dashoffset: 72;
     animation: top var(--time) ease-out infinite;
   }
-  
+
   @keyframes bottom {
     0%,
     85% {
@@ -145,9 +145,9 @@ export default class AppLoadingScreen extends React.PureComponent {
                 mask-type="alpha"
                 maskUnits="userSpaceOnUse"
                 x="0"
-                y="62"
-                width="67"
-                height="33"
+                y="0"
+                width="83"
+                height="105"
               >
                 <path
                   d="M56.4905 62.9807C63.6434 67.5637 66.7972 73.9744 66.7972 83.168C60.7987 90.7262 50.4577 95 38.2203 95C17.6963 95 3.17759 84.8102 0.0100098 67.282H19.7851C22.3343 75.3761 29.0748 79.1209 38.0622 79.1209C49.056 79.1209 56.3462 73.2598 56.4905 62.9807"
@@ -165,10 +165,10 @@ export default class AppLoadingScreen extends React.PureComponent {
                 id="middle"
                 mask-type="alpha"
                 maskUnits="userSpaceOnUse"
-                x="2"
-                y="12"
-                width="71"
-                height="72"
+                x="0"
+                y="0"
+                width="83"
+                height="105"
               >
                 <path
                   d="M7.73317 12.1206C7.73317 24.8871 15.6418 32.5484 31.4729 36.568L48.2521 40.4571C63.238 43.8926 72.3834 52.4472 72.3834 66.3818C72.493 72.4436 70.5187 78.3594 66.7904 83.1404C66.7904 69.2402 59.5963 61.7232 42.2605 57.2157L25.7698 53.471C12.5842 50.4683 2.40806 43.4323 2.40806 28.2952C2.33733 22.4523 4.21324 16.7521 7.74004 12.0931"
@@ -186,10 +186,10 @@ export default class AppLoadingScreen extends React.PureComponent {
                 id="top"
                 mask-type="alpha"
                 maskUnits="userSpaceOnUse"
-                x="7"
+                x="0"
                 y="0"
-                width="65"
-                height="32"
+                width="83"
+                height="105"
               >
                 <path
                   d="M18.0122 31.1124C11.1411 26.7149 7.70557 20.524 7.70557 12.1138C13.4636 4.59677 23.3648 0 35.5198 0C56.4973 0 68.6317 11.09 71.6275 26.6737H52.5945C50.4988 20.5309 45.2562 15.7348 35.6641 15.7348C25.4399 15.7348 18.4863 21.6852 18.0122 31.1124"
