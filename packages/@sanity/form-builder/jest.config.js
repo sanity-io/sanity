4263ade4ceeda4570e11131cf53394b5e4a7e0f2
Per-Kristian Nordnes <per.kristian.nordnes@gmail.com>
2021-10-12 14:29:09 +0200

    test(form-builder): resolve parts with Jest required by PT input tests

--- packages/@sanity/form-builder/jest.config.js
+++ packages/@sanity/form-builder/jest.config.js
@@ -6,6 +6,18 @@ module.exports = createConfig({
   globalSetup: '<rootDir>/test/setup/global.ts',
   setupFiles: ['<rootDir>/test/setup/environment.ts'],
   moduleNameMapper: {
+    'part:@sanity/form-builder/input/block-editor/block-extras': path.resolve(
+      __dirname,
+      'src/inputs/PortableText/legacyParts/BlockExtras.tsx'
+    ),
+    'part:@sanity/form-builder/input/block-editor/block-markers-custom-default': path.resolve(
+      __dirname,
+      'src/inputs/PortableText/legacyParts/CustomMarkers.tsx'
+    ),
+    'part:@sanity/form-builder/input/block-editor/block-markers': path.resolve(
+      __dirname,
+      'src/inputs/PortableText/legacyParts/Markers.tsx'
+    ),
     '^part:@sanity/form-builder*': path.resolve(__dirname, '../../../test/undefined'),
   },
 })
