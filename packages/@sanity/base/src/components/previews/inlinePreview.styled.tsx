b9e58fec9de8ce75c11f7ce3c5808abb8ce1a3ce
hermanwikner <wiknerherman@hotmail.com>
2021-10-13 18:46:53 +0200

    refactor(form-builder): design updates in `InlinePreview`

--- packages/@sanity/base/src/components/previews/inlinePreview.styled.tsx
+++ packages/@sanity/base/src/components/previews/inlinePreview.styled.tsx
@@ -1,50 +1,68 @@
-import {Box} from '@sanity/ui'
 import styled from 'styled-components'
 
-export const Root = styled.span`
-  display: inline-block;
-  position: relative;
+export const RootDiv = styled.div`
+  display: inline-flex;
+  align-items: center;
+  vertical-align: bottom;
   line-height: 1;
+  max-width: 100%;
 `
 
-export const MediaWrapper = styled(Box)`
+export const MediaDiv = styled.div`
   display: inline-block;
-  vertical-align: top;
-  position: relative;
+  vertical-align: bottom;
+  width: 1em;
 
-  &:not([hidden]) {
-    display: inline-block;
+  &:empty {
+    display: none;
   }
 
-  > img {
-    height: calc(14 / 16 * 1em);
-    margin: 1px 0;
+  & img {
+    width: 1em;
     display: block;
     object-fit: cover;
+    border-radius: ${({theme}) => theme.sanity.radius[1]}px;
+    margin-right: ${({theme}) => theme.sanity.space[1]}px;
   }
 
-  > svg {
+  & svg {
     display: block;
     font-size: calc(14 / 16 * 1em);
     margin: 1px 0;
 
-    &[data-sanity-icon='true'] {
+    &[data-sanity-icon] {
       font-size: calc(18 / 16 * 1em);
       margin: calc(1px + (2 / 18 * -1em)) 0;
     }
   }
-
-  > div {
-    display: inline-block;
-  }
 `
 
-export const InheritedText = styled(Box).attrs({as: 'span'})`
-  font-size: inherit;
-  line-height: inherit;
-  vertical-align: top;
+export const TextSpan = styled.div`
+  font-size: calc(14 / 16 * 1em);
+  font-weight: ${({theme}) => theme.sanity.fonts.text.weights.medium};
+  overflow: hidden;
+  padding: 0.1em;
+  margin: -0.1em;
+  display: inline-flex;
+  align-items: center;
 
-  &:empty {
-    display: none;
+  & > div {
+    display: inline-block;
+    width: 100%;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+    overflow: hidden;
+    padding: 0.1em;
+    margin: -0.1em;
+
+    span {
+      margin-left: 0.25em;
+      margin-right: 0.25em;
+      vertical-align: top;
+
+      &:empty {
+        display: none;
+      }
+    }
   }
 `
--- packages/@sanity/base/src/components/previews/inlinePreview.tsx
+++ packages/@sanity/base/src/components/previews/inlinePreview.tsx
@@ -1,8 +1,7 @@
 import React from 'react'
-import {Box} from '@sanity/ui'
 import {getDevicePixelRatio} from 'use-device-pixel-ratio'
 import {MediaDimensions, PreviewProps} from './types'
-import {MediaWrapper, InheritedText, Root} from './inlinePreview.styled'
+import {RootDiv, MediaDiv, TextSpan} from './inlinePreview.styled'
 
 const DEFAULT_MEDIA_DIMENSIONS: MediaDimensions = {
   width: 32,
@@ -13,33 +12,24 @@ const DEFAULT_MEDIA_DIMENSIONS: MediaDimensions = {
 }
 
 export const InlinePreview: React.FunctionComponent<PreviewProps<'inline'>> = (props) => {
-  const {title, media, mediaDimensions = DEFAULT_MEDIA_DIMENSIONS, children} = props
-
-  if (!title && !media) {
-    return <span />
-  }
+  const {title, media, mediaDimensions = DEFAULT_MEDIA_DIMENSIONS} = props
 
   return (
-    <Root>
-      {media && (
-        <MediaWrapper marginRight={1}>
-          {typeof media === 'function' &&
-            media({
-              dimensions: mediaDimensions,
-              layout: 'inline',
-            })}
-          {typeof media !== 'function' && media}
-          {React.isValidElement(media) && media}
-        </MediaWrapper>
-      )}
-      <InheritedText marginRight={1}>
-        {typeof title === 'function'
-          ? title({
-              layout: 'inline',
-            })
-          : title}
-      </InheritedText>
-      {children && <Box as="span">{children}</Box>}
-    </Root>
+    <RootDiv>
+      <MediaDiv>
+        {typeof media === 'function' &&
+          media({
+            dimensions: mediaDimensions,
+            layout: 'inline',
+          })}
+        {typeof media !== 'function' && media}
+        {React.isValidElement(media) && media}
+      </MediaDiv>
+      <TextSpan forwardedAs="span">
+        <div>
+          <span>{title}</span>
+        </div>
+      </TextSpan>
+    </RootDiv>
   )
 }
