cf7ccd6827e49e284f42357f2ecf3b7797feb1d4
Mikolaj Dobrucki <5467602+mikolajdobrucki@users.noreply.github.com>
2021-10-15 11:25:53 +0200

    refactor(base): remove unused components

--- packages/@sanity/base/src/change-indicators/ChangeBar.styled.tsx
+++ packages/@sanity/base/src/change-indicators/ChangeBar.styled.tsx
@@ -1,7 +1,5 @@
-import React from 'react'
 import styled, {css} from 'styled-components'
 import {Theme} from '@sanity/ui'
-import {EditIcon} from '@sanity/icons'
 
 interface ThemeContext {
   theme: Theme
@@ -13,17 +11,6 @@ interface RootProps {
   changed: boolean
   isReviewChangeOpen: boolean
   disabled: boolean
-}
-
-const Shape = (props: Omit<React.SVGProps<SVGElement>, 'ref'>) => {
-  return (
-    <svg {...props} viewBox="0 0 20 27" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path
-        d="M9 0.448608C9 2.49663 7.38382 4.13678 5.57253 5.09261C2.55605 6.68443 0.5 9.8521 0.5 13.5C0.5 17.1479 2.55605 20.3155 5.57253 21.9074C7.38382 22.8632 9 24.5033 9 26.5514V27H11V26.5514C11 24.5033 12.6162 22.8632 14.4275 21.9074C17.4439 20.3155 19.5 17.1479 19.5 13.5C19.5 9.8521 17.4439 6.68443 14.4275 5.09261C12.6162 4.13678 11 2.49663 11 0.448608V0H9V0.448608Z"
-        fill="currentColor"
-      />
-    </svg>
-  )
 }
 
 const animationSpeed = 150
@@ -76,75 +63,70 @@ export const BadgeWrapper = styled.div(({theme}: ThemeContext) => {
   `
 })
 
-export const EditIconWrapper = styled(EditIcon)`
-  display: block;
-  position: relative;
-  margin: 0;
-  font-size: calc(19 / 16 * 1rem);
-  color: var(--card-bg-color);
-  opacity: 0;
-  transition: opacity ${animationSpeed}ms;
-
-  & path {
-    stroke-width: 1.5px;
-  }
-`
-
-export const ShapeWrapper = styled.div(({theme}: ThemeContext) => {
+export const HitAreaButton = styled.button(({theme}: ThemeContext) => {
   /* these colours aren't freely available on the current theme */
   const notSelectedColor = theme.sanity.color.spot.yellow
-  const maxScreenMedium = theme.sanity.media[0] - 1
 
   return css`
+    appearance: none;
+    border: 0;
+    outline: 0;
     display: block;
+    padding: 0;
+    background: transparent;
+    opacity: 0;
     position: absolute;
-    width: 15px;
     height: 100%;
-    border-radius: 12px;
-    background: ${notSelectedColor};
-    color: ${notSelectedColor};
+    cursor: pointer;
+    pointer-events: all;
+    left: -0.25rem;
+    width: 1rem;
+    transition: opacity ${animationSpeed}ms;
 
-    @media (max-width: ${maxScreenMedium}px) {
-      /* hide on mobile */
-      display: none;
+    &:focus {
+      border: 0;
+      outline: 0;
+    }
+
+    &:after {
+      content: '';
+      width: 16px;
+      height: calc(100% + 16px);
+      display: block;
+      position: absolute;
+      top: -8px;
+      left: -4px;
+      border-radius: 16px;
+      background: ${notSelectedColor};
+    }
+
+    &:focus {
+      border: 0;
+      outline: 0;
+    }
+
+    &:hover {
+      opacity: 0.2;
     }
   `
 })
 
-export const HitAreaButton = styled.button`
-  appearance: none;
-  border: 0;
-  outline: 0;
-  display: block;
-  padding: 0;
-  background: 0;
-  position: absolute;
-  height: 100%;
-  cursor: pointer;
-  pointer-events: all;
-  left: -0.25rem;
-  width: 1rem;
-
-  &:focus {
-    border: 0;
-    outline: 0;
-  }
-`
-
-// For when the shape and icon need to appear on the page
+/* for when the shape and icon need to appear on the page */
 const BadgeOpen = css`
   ${BadgeWrapper} {
     opacity: 0.2;
     transition: opacity ${animationSpeed}ms;
   }
-
-  ${EditIconWrapper} {
-    opacity: 1;
-  }
 `
 
 export const ChangeBarWrapper = styled.div(
-  ({focus, hover, changed, disabled, isReviewChangeOpen}: RootProps) => {
+  ({
+    // focus,
+    hover,
+    changed,
+    disabled,
+    isReviewChangeOpen,
+  }: RootProps) => {
     if (disabled)
       return css`
         ${TooltipTriggerWrapper} {
@@ -161,23 +143,6 @@ export const ChangeBarWrapper = styled.div(
           z-index: 10;
         }
       }
-
-      /* on focus */
-
-      /* ${focus &&
-      css`
-        ${ShapeWrapper} {
-          color: var(--card-focus-ring-color);
-        }
-
-        ${BarWrapper},
-        ${TooltipTriggerWrapper} {
-          &:focus-within {
-            ${BadgeOpen}
-          }
-          background-color: var(--card-focus-ring-color);
-        }
-      `} */
 
       /* on hover */
 
--- packages/@sanity/base/src/change-indicators/ChangeIndicator.tsx
+++ packages/@sanity/base/src/change-indicators/ChangeIndicator.tsx
@@ -34,10 +34,9 @@ const ChangeBarWrapper = memo(function ChangeBarWrapper(
     hasFocus: boolean
     fullPath: Path
     disabled?: boolean
-    withBadge?: boolean
   }
 ) {
-  const {children, className, fullPath, hasFocus, isChanged, disabled, withBadge} = props
+  const {children, className, fullPath, hasFocus, isChanged, disabled} = props
   const layer = useLayer()
   const [hasHover, setHover] = React.useState(false)
   const onMouseEnter = React.useCallback(() => setHover(true), [])
@@ -60,12 +59,7 @@ const ChangeBarWrapper = memo(function ChangeBarWrapper(
 
   return (
     <div ref={ref} className={className} onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave}>
-      <ChangeBar
-        hasFocus={hasFocus}
-        isChanged={isChanged}
-        disabled={disabled}
-        withBadge={withBadge}
-      >
+      <ChangeBar hasFocus={hasFocus} isChanged={isChanged} disabled={disabled}>
         {children}
       </ChangeBar>
     </div>
@@ -138,7 +132,6 @@ interface CoreProps {
   hasFocus: boolean
   compareValue: unknown
   children?: React.ReactNode
-  withBadge?: boolean
 }
 
 export function CoreChangeIndicator(props: CoreProps) {
@@ -151,7 +144,6 @@ export function CoreChangeIndicator(props: CoreProps) {
     hasFocus,
     compareDeep,
     children,
-    withBadge,
   } = props
   // todo: lazy compare debounced (possibly with intersection observer)
   const isChanged =
@@ -165,7 +157,6 @@ export function CoreChangeIndicator(props: CoreProps) {
       fullPath={fullPath}
       hasFocus={hasFocus}
       disabled={disabled}
-      withBadge={withBadge}
     >
       {children}
     </ChangeBarWrapper>
@@ -209,13 +200,12 @@ interface ChangeIndicatorWithProvidedFullPathProps {
   hasFocus: boolean
   compareDeep?: boolean
   children?: React.ReactNode
-  withBadge?: boolean
 }
 
 export function ChangeIndicatorWithProvidedFullPath(
   props: ChangeIndicatorWithProvidedFullPathProps
 ) {
-  const {className, disabled, path, value, hasFocus, compareDeep, children, withBadge} = props
+  const {className, disabled, path, value, hasFocus, compareDeep, children} = props
   const parentContext = React.useContext(ChangeIndicatorContext)
 
   const fullPath = React.useMemo(() => PathUtils.pathFor(parentContext.fullPath.concat(path)), [
@@ -232,7 +222,6 @@ export function ChangeIndicatorWithProvidedFullPath(
       hasFocus={hasFocus}
       fullPath={fullPath}
       compareDeep={compareDeep}
-      withBadge={withBadge}
     >
       {children}
     </CoreChangeIndicator>
