4550d2d7ec5c55cb4fd2024de9ae63c32c3d3c94
hermanwikner <wiknerherman@hotmail.com>
2021-10-15 16:15:14 +0200

    test(form-builder): add marker test to `TextBlock` workshop

--- packages/@sanity/form-builder/src/inputs/PortableText/Text/TextBlock/__workshop__/test.tsx
+++ packages/@sanity/form-builder/src/inputs/PortableText/Text/TextBlock/__workshop__/test.tsx
@@ -6,66 +6,103 @@ import {TextBlock} from '../TextBlock'
 
 export default function TestStory() {
   const hasErrors = useBoolean('Has errors', false)
+  const hasMarkers = useBoolean('Has markers', false)
 
   return (
     <Flex align="center" height="fill" justify="center" padding={4} sizing="border">
       <Container width={1}>
         <Card padding={3} shadow={1} overflow="auto">
-          <TextBlock hasError={hasErrors} style="h1">
+          <TextBlock hasError={hasErrors} hasMarker={hasMarkers} style="h1">
             Heading 1
           </TextBlock>
 
-          <TextBlock hasError={hasErrors} style="normal">
+          <TextBlock hasError={hasErrors} hasMarker={hasMarkers} style="normal">
             Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum
             has been the industry's standard dummy text ever since the 1500s, when an unknown
             printer took a galley of type and scrambled it to make a type specimen book.
           </TextBlock>
 
-          <TextBlock hasError={hasErrors} style="normal" listItem="number" level={1}>
+          <TextBlock
+            hasError={hasErrors}
+            hasMarker={hasMarkers}
+            style="normal"
+            listItem="number"
+            level={1}
+          >
             Lorem Ipsum is simply dummy text of the printing and typesetting industry.
           </TextBlock>
 
-          <TextBlock hasError={hasErrors} style="normal" listItem="number" level={2}>
+          <TextBlock
+            hasError={hasErrors}
+            hasMarker={hasMarkers}
+            style="normal"
+            listItem="number"
+            level={2}
+          >
             Lorem Ipsum is simply dummy text of the printing and typesetting industry.
           </TextBlock>
 
-          <TextBlock hasError={hasErrors} style="normal" listItem="number" level={3}>
+          <TextBlock
+            hasError={hasErrors}
+            hasMarker={hasMarkers}
+            style="normal"
+            listItem="number"
+            level={3}
+          >
             Lorem Ipsum is simply dummy text of the printing and typesetting industry.
           </TextBlock>
 
-          <TextBlock hasError={hasErrors} style="normal" listItem="number" level={3}>
+          <TextBlock
+            hasError={hasErrors}
+            hasMarker={hasMarkers}
+            style="normal"
+            listItem="number"
+            level={3}
+          >
             Lorem Ipsum is simply dummy text of the printing and typesetting industry.
           </TextBlock>
 
-          <TextBlock hasError={hasErrors} style="h2">
+          <TextBlock hasError={hasErrors} hasMarker={hasMarkers} style="h2">
             Heading 2
           </TextBlock>
 
-          <TextBlock hasError={hasErrors} style="normal">
+          <TextBlock hasError={hasErrors} hasMarker={hasMarkers} style="normal">
             Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum
             has been the industry's standard dummy text ever since the 1500s, when an unknown
             printer took a galley of type and scrambled it to make a type specimen book.
           </TextBlock>
 
-          <TextBlock hasError={hasErrors} style="normal" listItem="bullet" level={1}>
+          <TextBlock
+            hasError={hasErrors}
+            hasMarker={hasMarkers}
+            style="normal"
+            listItem="bullet"
+            level={1}
+          >
             Lorem Ipsum is simply dummy text of the printing and typesetting industry.
           </TextBlock>
 
-          <TextBlock hasError={hasErrors} style="normal" listItem="bullet" level={2}>
+          <TextBlock
+            hasError={hasErrors}
+            hasMarker={hasMarkers}
+            style="normal"
+            listItem="bullet"
+            level={2}
+          >
             Lorem Ipsum is simply dummy text of the printing and typesetting industry.
           </TextBlock>
 
-          <TextBlock hasError={hasErrors} style="h3">
+          <TextBlock hasError={hasErrors} hasMarker={hasMarkers} style="h3">
             Heading 3
           </TextBlock>
 
-          <TextBlock hasError={hasErrors} style="normal">
+          <TextBlock hasError={hasErrors} hasMarker={hasMarkers} style="normal">
             Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum
             has been the industry's standard dummy text ever since the 1500s, when an unknown
             printer took a galley of type and scrambled it to make a type specimen book.
           </TextBlock>
 
-          <TextBlock hasError={hasErrors} style="blockquote">
+          <TextBlock hasError={hasErrors} hasMarker={hasMarkers} style="blockquote">
             Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum
             has been the industry's standard dummy text ever since the 1500s, when an unknown
             printer took a galley of type and scrambled it to make a type specimen book.
