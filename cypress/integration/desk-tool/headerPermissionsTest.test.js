0727dc52d9b96ab4a22be75ff5d616f0d67d1b34
Rita <rita@sanity.io>
2021-10-20 12:08:29 +0200

    test(cypress): add two tests for two levels of permissions when single action exists in the document list header

---
+++ cypress/integration/desk-tool/headerPermissionsTest.test.js
@@ -0,0 +1,19 @@
+describe('@sanity/desk-tool: header permissions', () => {
+  it('is active if user has permissions', () => {
+    cy.visit('/test/desk/custom;deep;book')
+
+    // eslint-disable-next-line cypress/no-unnecessary-waiting
+    cy.wait(1000)
+
+    cy.get('[data-cy=action-intent]').should('not.have.attr', 'disabled')
+  })
+
+  it('are disabled if user is restricted (read only)', () => {
+    cy.visit('/test/desk/custom;deep;book#_debug_roles=restricted')
+
+    // eslint-disable-next-line cypress/no-unnecessary-waiting
+    cy.wait(1000)
+
+    cy.get('[data-cy=action-intent]').should('have.attr', 'disabled')
+  })
+})
