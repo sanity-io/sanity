8ff842b74fe0e2df37d4f21663ff70ba6eb7d87f
Rita <rita@sanity.io>
2021-10-20 15:29:29 +0200

    test(cypress): change definition of tests & separate between single actions and multiple

--- cypress/integration/desk-tool/headerPermissionsTest.test.js
+++ cypress/integration/desk-tool/headerPermissionsTest.test.js
@@ -1,4 +1,4 @@
-describe('@sanity/desk-tool: test which header permissions exist', () => {
+describe('@sanity/desk-tool: when a single action exists on the header, which permissions exist', () => {
   it('when the user is admin: the create button is active', () => {
     cy.visit('/test/desk/custom;deep;book#_debug_roles=administrator')
 
@@ -16,8 +16,10 @@ describe('@sanity/desk-tool: test which header permissions exist', () => {
 
     cy.get('[data-testid=action-intent-button]').should('have.attr', 'disabled')
   })
+})
 
-  it('when the user is requiresApproval (based on schema property): from the list of create (tooltip) only the last action is enabled', () => {
+describe('@sanity/desk-tool: when multiple actions exist on the header, which permissions exist', () => {
+  it('when the user is admin: all actions are enabled', () => {
     cy.visit('/test/desk/author#_debug_roles=requiresApproval')
 
     // eslint-disable-next-line cypress/no-unnecessary-waiting
@@ -28,5 +30,31 @@ describe('@sanity/desk-tool: test which header permissions exist', () => {
     cy.get('[data-testid=action-intent-button-0').should('have.attr', 'disabled')
     cy.get('[data-testid=action-intent-button-1').should('have.attr', 'disabled')
     cy.get('[data-testid=action-intent-button-2').should('not.have.attr', 'disabled') // the only menu is affected by the locked property
+  })
+
+  it('when the user is requiresApproval (based on schema property): only the last action is enabled', () => {
+    cy.visit('/test/desk/author#_debug_roles=requiresApproval')
+
+    // eslint-disable-next-line cypress/no-unnecessary-waiting
+    cy.wait(1000)
+
+    cy.get('[data-testid=multi-action-intent-button]').click()
+
+    cy.get('[data-testid=action-intent-button-0').should('have.attr', 'disabled')
+    cy.get('[data-testid=action-intent-button-1').should('have.attr', 'disabled')
+    cy.get('[data-testid=action-intent-button-2').should('not.have.attr', 'disabled') // the only menu is affected by the locked property
+  })
+
+  it('when the user is restricted (read only): all the actions are disabled', () => {
+    cy.visit('/test/desk/author#_debug_roles=restricted')
+
+    // eslint-disable-next-line cypress/no-unnecessary-waiting
+    cy.wait(1000)
+
+    cy.get('[data-testid=multi-action-intent-button]').click()
+
+    cy.get('[data-testid=action-intent-button-0').should('have.attr', 'disabled')
+    cy.get('[data-testid=action-intent-button-1').should('have.attr', 'disabled')
+    cy.get('[data-testid=action-intent-button-2').should('have.attr', 'disabled') // the only menu is affected by the locked property
   })
 })
