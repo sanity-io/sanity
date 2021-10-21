a616057ea1e2eba43afd405ef28cf4c0a61c751a
Espen Hovlandsdal <espen@hovlandsdal.com>
2021-10-21 16:28:05 +0200

    test(base): add more tests for useDocumentType hook

--- packages/@sanity/base/src/datastores/document/hooks/__tests__/useDocumentType.test.tsx
+++ packages/@sanity/base/src/datastores/document/hooks/__tests__/useDocumentType.test.tsx
@@ -36,10 +36,48 @@ test('should resolve document type from API on undefined type (with loading stat
   expect(result.current).toEqual({isLoaded: true, documentType: 'book'})
 })
 
+test('should return correct document type on argument transition', () => {
+  let documentType = 'book'
+  const client = {observable: {fetch: jest.fn().mockImplementation(shouldNotBeCalled)}}
+  const {result, rerender} = renderHook(() => useDocumentType('abc123', documentType, {client}))
+  expect(result.current).toEqual({isLoaded: true, documentType: 'book'})
+
+  documentType = 'author'
+  rerender()
+
+  expect(result.current).toEqual({isLoaded: true, documentType: 'author'})
+})
+
+test('should return correct document type on document ID transition', async () => {
+  const responseGrrm = defer(() => of(['author']).pipe(observeOn(asyncScheduler)))
+  const responseGot = defer(() => of(['book']).pipe(observeOn(asyncScheduler)))
+  const fetch = jest.fn().mockImplementation((query, params) => {
+    return params.documentId === 'grrm' ? responseGrrm : responseGot
+  })
+
+  let documentId = 'grrm'
+  const client = {observable: {fetch}}
+  const {result, rerender, waitForNextUpdate} = renderHook(() =>
+    useDocumentType(documentId, undefined, {client})
+  )
+
+  // First lookup (author)
+  expect(result.current).toEqual({isLoaded: false, documentType: undefined})
+  await waitForNextUpdate()
+  expect(result.current).toEqual({isLoaded: true, documentType: 'author'})
+
+  // Change to look up a book instead
+  documentId = 'agot'
+  rerender()
+
+  // Second lookup (book)
+  expect(result.current).toEqual({isLoaded: false, documentType: undefined})
+  await waitForNextUpdate()
+  expect(result.current).toEqual({isLoaded: true, documentType: 'book'})
+})
+
 /**
  * @todo add tests for:
- * - Transition from one document ID to a different one
  * - Transition from an undefined specified type to a specified type
- * - Transition from a specified type to a different specified type
  * - Transition from a specified type to an undefined specified type
  */
