6d10981882cbdaf9f4eef5f7299b39373115f118
hermanwikner <wiknerherman@hotmail.com>
2021-10-21 13:52:46 +0200

    fix(desk-tool): add correct color on `UserAvatar` initials in timeline

--- packages/@sanity/desk-tool/src/panes/document/timeline/userAvatarStack.tsx
+++ packages/@sanity/desk-tool/src/panes/document/timeline/userAvatarStack.tsx
@@ -1,20 +1,25 @@
-// @todo: remove the following line when part imports has been removed from this file
-///<reference types="@sanity/types/parts" />
-
 import {UserAvatar} from '@sanity/base/components'
 import React from 'react'
-import {AvatarStack} from '@sanity/ui'
+import {AvatarStack, Card} from '@sanity/ui'
+import styled from 'styled-components'
 
 interface UserAvatarStackProps {
   maxLength?: number
   userIds: string[]
 }
 
+// This Card is added in order to get the correct color on avatar initials
+const AvatarCard = styled(Card)`
+  background: transparent;
+`
+
 export function UserAvatarStack({maxLength, userIds}: UserAvatarStackProps) {
   return (
     <AvatarStack maxLength={maxLength}>
       {userIds.map((userId) => (
-        <UserAvatar key={userId} userId={userId} withTooltip />
+        <AvatarCard key={userId}>
+          <UserAvatar userId={userId} withTooltip />
+        </AvatarCard>
       ))}
     </AvatarStack>
   )
