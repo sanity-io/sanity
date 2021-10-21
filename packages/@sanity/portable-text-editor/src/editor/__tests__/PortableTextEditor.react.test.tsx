a98840791c3811216f611cc7daad8d01aea2948c
Per-Kristian Nordnes <per.kristian.nordnes@gmail.com>
2021-10-21 15:44:53 +0200

    test(portable-text-editor): test remove annotations and resulting normalization

--- packages/@sanity/portable-text-editor/src/editor/__tests__/PortableTextEditor.react.test.tsx
+++ packages/@sanity/portable-text-editor/src/editor/__tests__/PortableTextEditor.react.test.tsx
@@ -186,3 +186,140 @@ describe('initialization', () => {
     expect(PortableTextEditor.getSelection(editorRef.current)).toEqual(initialSelection)
   })
 })
+
+describe('normalization', () => {
+  it('merges adjacent spans correctly when removing annotations', () => {
+    const editorRef: React.RefObject<PortableTextEditor> = React.createRef()
+    const initialValue = [
+      {
+        _key: '5fc57af23597',
+        _type: 'myTestBlockType',
+        children: [
+          {
+            _key: 'be1c67c6971a',
+            _type: 'span',
+            marks: [],
+            text: 'This is a ',
+          },
+          {
+            _key: '11c8c9f783a8',
+            _type: 'span',
+            marks: ['fde1fd54b544'],
+            text: 'link',
+          },
+          {
+            _key: '576c748e0cd2',
+            _type: 'span',
+            marks: [],
+            text: ', this is ',
+          },
+          {
+            _key: 'f3d73d3833bf',
+            _type: 'span',
+            marks: ['7b6d3d5de30c'],
+            text: 'another',
+          },
+          {
+            _key: '73b01f13c2ec',
+            _type: 'span',
+            marks: [],
+            text: ', and this is ',
+          },
+          {
+            _key: '13eb0d467c82',
+            _type: 'span',
+            marks: ['93a1d24eade0'],
+            text: 'a third',
+          },
+        ],
+        markDefs: [
+          {
+            _key: 'fde1fd54b544',
+            _type: 'link',
+            url: '1',
+          },
+          {
+            _key: '7b6d3d5de30c',
+            _type: 'link',
+            url: '2',
+          },
+          {
+            _key: '93a1d24eade0',
+            _type: 'link',
+            url: '3',
+          },
+        ],
+        style: 'normal',
+      },
+    ]
+    const onChange = jest.fn()
+    render(
+      <PortableTextEditorTester
+        onChange={onChange}
+        ref={editorRef}
+        type={bodyType}
+        value={initialValue}
+      />
+    )
+    if (!editorRef.current) {
+      throw new Error('No editor')
+    }
+    PortableTextEditor.focus(editorRef.current)
+    PortableTextEditor.select(editorRef.current, {
+      focus: {path: [{_key: '5fc57af23597'}, 'children', {_key: '11c8c9f783a8'}], offset: 4},
+      anchor: {path: [{_key: '5fc57af23597'}, 'children', {_key: '11c8c9f783a8'}], offset: 0},
+    })
+    const linkType = PortableTextEditor.getPortableTextFeatures(editorRef.current).annotations.find(
+      (a) => a.type.name === 'link'
+    )?.type
+    if (!linkType) {
+      throw new Error('No link type found')
+    }
+    PortableTextEditor.removeAnnotation(editorRef.current, linkType)
+    expect(PortableTextEditor.getValue(editorRef.current)).toEqual([
+      {
+        _key: '5fc57af23597',
+        _type: 'myTestBlockType',
+        children: [
+          {
+            _key: 'be1c67c6971a',
+            _type: 'span',
+            marks: [],
+            text: 'This is a link, this is ',
+          },
+          {
+            _key: 'f3d73d3833bf',
+            _type: 'span',
+            marks: ['7b6d3d5de30c'],
+            text: 'another',
+          },
+          {
+            _key: '73b01f13c2ec',
+            _type: 'span',
+            marks: [],
+            text: ', and this is ',
+          },
+          {
+            _key: '13eb0d467c82',
+            _type: 'span',
+            marks: ['93a1d24eade0'],
+            text: 'a third',
+          },
+        ],
+        markDefs: [
+          {
+            _key: '7b6d3d5de30c',
+            _type: 'link',
+            url: '2',
+          },
+          {
+            _key: '93a1d24eade0',
+            _type: 'link',
+            url: '3',
+          },
+        ],
+        style: 'normal',
+      },
+    ])
+  })
+})
