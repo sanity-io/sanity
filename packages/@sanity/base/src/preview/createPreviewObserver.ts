7f609fec4b275a37cdb53188125742b8f0ab2c14
Bjørge Næss <bjoerge@gmail.com>
2021-10-26 22:43:16 +0200

    chore(base): remove soon to be obsolete _internalMeta property for ref type previews

--- packages/@sanity/base/src/preview/createPreviewObserver.ts
+++ packages/@sanity/base/src/preview/createPreviewObserver.ts
@@ -1,17 +1,15 @@
-import {of as observableOf, Observable} from 'rxjs'
-import {map, switchMap, catchError} from 'rxjs/operators'
-import {isReferenceSchemaType, PreviewValue, ReferenceSchemaType, SchemaType} from '@sanity/types'
+import {Observable, of as observableOf} from 'rxjs'
+import {map, switchMap} from 'rxjs/operators'
+import {isReferenceSchemaType, PreviewValue, SchemaType} from '@sanity/types'
 import {isPlainObject} from 'lodash'
 import prepareForPreview, {invokePrepare} from './utils/prepareForPreview'
-import {Reference, PrepareViewOptions, Path} from './types'
-import {INSUFFICIENT_PERMISSIONS_FALLBACK, InsufficientPermissionsError} from './constants'
+import {Path, PrepareViewOptions, Reference} from './types'
 import {getPreviewPaths} from './utils/getPreviewPaths'
-
-const INSUFFICIENT_PERMISSIONS = Symbol('INSUFFICIENT_PERMISSIONS')
+import {observeDocumentTypeFromId} from './observeDocumentTypeFromId'
 
 export interface PreparedSnapshot {
   type?: SchemaType
-  snapshot: null | PreviewValue
+  snapshot: undefined | PreviewValue
 }
 
 export function isRecord(value: unknown): value is Record<string, unknown> {
@@ -19,13 +17,7 @@ export function isRecord(value: unknown): value is Record<string, unknown> {
 }
 
 // Takes a value and its type and prepares a snapshot for it that can be passed to a preview component
-export function createPreviewObserver(
-  observePaths: (value: any, paths: Path[]) => any,
-  resolveRefType: (
-    value: Reference,
-    ownerType: ReferenceSchemaType
-  ) => Observable<SchemaType | undefined>
-) {
+export function createPreviewObserver(observePaths: (value: any, paths: Path[]) => any) {
   return function observeForPreview(
     value: Reference,
     type: SchemaType,
@@ -33,33 +25,24 @@ export function createPreviewObserver(
   ): Observable<PreparedSnapshot> {
     if (isReferenceSchemaType(type)) {
       // if the value is of type reference, but has no _ref property, we cannot prepare any value for the preview
-      // and the most sane thing to do is to return `null` for snapshot
+      // and the most sane thing to do is to return `undefined` for snapshot
       if (!value._ref) {
-        return observableOf({snapshot: null})
+        return observableOf({snapshot: undefined})
       }
       // Previewing references actually means getting the referenced value,
       // and preview using the preview config of its type
       // todo: We need a way of knowing the type of the referenced value by looking at the reference record alone
-      return resolveRefType(value, type).pipe(
-        catchError((e) => {
-          if (e instanceof InsufficientPermissionsError) {
-            return observableOf(INSUFFICIENT_PERMISSIONS)
+      return observeDocumentTypeFromId(value._ref).pipe(
+        switchMap((typeName) => {
+          if (typeName) {
+            const refType = type.to.find((toType) => toType.name === typeName)
+            return observeForPreview(value, refType)
           }
-          throw e
-        }),
-        switchMap((refType) => {
-          if (refType === INSUFFICIENT_PERMISSIONS) {
-            return observableOf<PreparedSnapshot>({
-              type,
-              snapshot: INSUFFICIENT_PERMISSIONS_FALLBACK,
-            })
-          }
-
-          if (!refType) {
-            return observableOf<PreparedSnapshot>({type, snapshot: null})
-          }
-
-          return observeForPreview(value, refType)
+          // todo: in case we can't read the document type, we can figure out the reason why e.g. whether it's because
+          //  the document doesn't exist or it's not readable due to lack of permission.
+          //  We can use the "observeDocumentAvailability" function
+          //  for this, but currently not sure if needed
+          return observableOf({snapshot: undefined})
         })
       )
     }
