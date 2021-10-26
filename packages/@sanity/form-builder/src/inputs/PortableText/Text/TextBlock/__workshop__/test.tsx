7de0c5a2279a01e3bf95431e35183861bbc7c367
Per-Kristian Nordnes <per.kristian.nordnes@gmail.com>
2021-10-26 13:28:26 +0200

    test(form-builder): update TextBlock workshop

--- packages/@sanity/form-builder/src/inputs/PortableText/Text/TextBlock/__workshop__/test.tsx
+++ packages/@sanity/form-builder/src/inputs/PortableText/Text/TextBlock/__workshop__/test.tsx
@@ -4,105 +4,117 @@ import {useBoolean} from '@sanity/ui-workshop'
 import React from 'react'
 import {TextBlock} from '../TextBlock'
 
+const errorMarker = {
+  type: 'validation',
+  level: 'error',
+  path: [{_key: 'a'}],
+  item: {message: 'There is an error with this text block'},
+}
+const customMarker = {
+  type: 'test',
+  path: [{_key: 'a'}],
+  item: {message: '1 comment'},
+}
+
+const value = {
+  _type: 'myBlockType',
+  _key: 'a',
+  style: 'normal',
+  markDefs: [],
+  children: [
+    {
+      _type: 'span',
+      _key: 'a1',
+      text:
+        "This is a custom portable text block above an empty image block! It's the block below. There should be a nice margin below me?",
+      marks: [],
+    },
+  ],
+}
+
 export default function TestStory() {
   const hasErrors = useBoolean('Has errors', false)
   const hasMarkers = useBoolean('Has markers', false)
-
+  const markers = [
+    ...(hasErrors ? [errorMarker] : []),
+    ...(hasMarkers ? [customMarker] : []),
+  ] as any
   return (
     <Flex align="center" height="fill" justify="center" padding={4} sizing="border">
       <Container width={1}>
         <Card padding={3} shadow={1} overflow="auto">
-          <TextBlock hasError={hasErrors} hasMarker={hasMarkers} style="h1">
+          <TextBlock markers={markers} value={{...value, style: 'h1'}}>
             Heading 1
           </TextBlock>
 
-          <TextBlock hasError={hasErrors} hasMarker={hasMarkers} style="normal">
+          <TextBlock markers={markers} value={{...value, style: 'normal'}}>
             Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum
             has been the industry's standard dummy text ever since the 1500s, when an unknown
             printer took a galley of type and scrambled it to make a type specimen book.
           </TextBlock>
 
           <TextBlock
-            hasError={hasErrors}
-            hasMarker={hasMarkers}
-            style="normal"
-            listItem="number"
-            level={1}
+            markers={markers}
+            value={{...value, style: 'normal', listItem: 'number', level: 1}}
+          >
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+          </TextBlock>
+          <TextBlock
+            markers={markers}
+            value={{...value, style: 'normal', listItem: 'number', level: 1}}
           >
             Lorem Ipsum is simply dummy text of the printing and typesetting industry.
           </TextBlock>
 
           <TextBlock
-            hasError={hasErrors}
-            hasMarker={hasMarkers}
-            style="normal"
-            listItem="number"
-            level={2}
+            markers={markers}
+            value={{...value, style: 'normal', listItem: 'number', level: 3}}
           >
             Lorem Ipsum is simply dummy text of the printing and typesetting industry.
           </TextBlock>
 
           <TextBlock
-            hasError={hasErrors}
-            hasMarker={hasMarkers}
-            style="normal"
-            listItem="number"
-            level={3}
+            markers={markers}
+            value={{...value, style: 'normal', listItem: 'number', level: 3}}
           >
             Lorem Ipsum is simply dummy text of the printing and typesetting industry.
           </TextBlock>
 
-          <TextBlock
-            hasError={hasErrors}
-            hasMarker={hasMarkers}
-            style="normal"
-            listItem="number"
-            level={3}
-          >
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-          </TextBlock>
-
-          <TextBlock hasError={hasErrors} hasMarker={hasMarkers} style="h2">
+          <TextBlock markers={markers} value={{...value, style: 'h2'}}>
             Heading 2
           </TextBlock>
 
-          <TextBlock hasError={hasErrors} hasMarker={hasMarkers} style="normal">
+          <TextBlock markers={markers} value={{...value, style: 'normal'}}>
             Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum
             has been the industry's standard dummy text ever since the 1500s, when an unknown
             printer took a galley of type and scrambled it to make a type specimen book.
           </TextBlock>
 
           <TextBlock
-            hasError={hasErrors}
-            hasMarker={hasMarkers}
-            style="normal"
-            listItem="bullet"
-            level={1}
+            markers={markers}
+            value={{...value, style: 'normal', listItem: 'bullet', level: 1}}
           >
             Lorem Ipsum is simply dummy text of the printing and typesetting industry.
           </TextBlock>
 
           <TextBlock
-            hasError={hasErrors}
-            hasMarker={hasMarkers}
-            style="normal"
-            listItem="bullet"
-            level={2}
+            markers={markers}
+            value={{...value, style: 'normal', listItem: 'bullet', level: 2}}
           >
             Lorem Ipsum is simply dummy text of the printing and typesetting industry.
           </TextBlock>
 
-          <TextBlock hasError={hasErrors} hasMarker={hasMarkers} style="h3">
+          <TextBlock value={{...value, style: 'h3'}} markers={[]}>
             Heading 3
           </TextBlock>
 
-          <TextBlock hasError={hasErrors} hasMarker={hasMarkers} style="normal">
+          <TextBlock value={{...value, style: 'normal'}} markers={markers}>
             Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum
             has been the industry's standard dummy text ever since the 1500s, when an unknown
             printer took a galley of type and scrambled it to make a type specimen book.
           </TextBlock>
 
-          <TextBlock hasError={hasErrors} hasMarker={hasMarkers} style="blockquote">
+          <TextBlock value={{...value, style: 'blockquote'}} markers={markers}>
             Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum
             has been the industry's standard dummy text ever since the 1500s, when an unknown
             printer took a galley of type and scrambled it to make a type specimen book.
