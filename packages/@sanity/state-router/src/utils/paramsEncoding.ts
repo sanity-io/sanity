a0c4a665445f45c9998e8ceaa06773841f37f5e5
Bjørge Næss <bjoerge@gmail.com>
2021-10-26 23:26:29 +0200

    feat(structure): support parent ref path

--- packages/@sanity/state-router/src/utils/paramsEncoding.ts
+++ packages/@sanity/state-router/src/utils/paramsEncoding.ts
@@ -1,13 +1,13 @@
-export function decodeParams(pathsegment) {
-  return pathsegment.split(';').reduce((params, pair) => {
+export function decodeParams(pathSegment: string): Record<string, string> {
+  return pathSegment.split(';').reduce<Record<string, string>>((params, pair) => {
     const [key, value] = pair.split('=')
-    params[key] = value
+    params[decodeURIComponent(key)] = decodeURIComponent(value)
     return params
   }, {})
 }
 
-export function encodeParams(params) {
+export function encodeParams(params: Record<string, string>): string {
   return Object.keys(params)
-    .map((key) => `${key}=${params[key]}`)
+    .map((key) => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
     .join(';')
 }
