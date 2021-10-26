9124490a2e6bf47ed5d63a284161f80903329dd0
Per-Kristian Nordnes <per.kristian.nordnes@gmail.com>
2021-10-26 13:28:08 +0200

    fix(form-builder): blockRef is optional, use optional chaning

--- packages/@sanity/form-builder/src/inputs/PortableText/Text/TextBlock/TextBlock.tsx
+++ packages/@sanity/form-builder/src/inputs/PortableText/Text/TextBlock/TextBlock.tsx
@@ -258,7 +258,7 @@ export function TextBlock(props: TextBlockProps): React.ReactElement {
     hasErrors || (hasMarkers && renderCustomMarkers) ? (
       <Tooltip
         placement="top"
-        boundaryElement={blockRef.current}
+        boundaryElement={blockRef?.current}
         portal
         content={
           <Stack space={3} padding={2} style={{maxWidth: 250}}>
