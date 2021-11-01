9f1386488d1396239eacf8e2e27181b79112d4e4
Marius Lundgård <studio@mariuslundgard.com>
2021-11-01 15:31:44 +0100

    chore(workshop): define `__DEV__` global

--- dev/workshop/vite.config.ts
+++ dev/workshop/vite.config.ts
@@ -54,6 +54,7 @@ export default defineConfig({
     // sourcemap: true,
   },
   define: {
+    // __DEV__: JSON.stringify(true),
     'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development'),
   },
   plugins: [
