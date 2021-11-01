1d9b63402b9fbbe89f1b4284e15cfa4559e6446c
Rita <rita@sanity.io>
2021-11-01 15:08:06 +0100

    test(cypress): add delete permission tests.

--- cypress/integration/desk-tool/actionPermission.test.js
+++ cypress/integration/desk-tool/actionPermission.test.js
@@ -86,3 +86,41 @@ describe('@sanity/desk-tool: duplicate permission', () => {
     cy.get('[data-testid=action-Duplicate]').should('have.attr', 'disabled')
   })
 })
+
+describe('@sanity/desk-tool: delete permission', () => {
+  it('as an administrator user, the duplicate button will be active', () => {
+    cy.visit(
+      '/test/desk/input-standard;booleansTest;1053af2f-84af-49a1-b42b-e2156470bb77%2Ctemplate%3DbooleansTest#_debug_roles=administrator'
+    )
+
+    cy.get('[data-testid=action-menu-button').click()
+    cy.get('[data-testid=action-Delete]').should('not.have.attr', 'disabled')
+  })
+
+  it('as an restricted user, the duplicate button will be disabled', () => {
+    cy.visit(
+      '/test/desk/input-standard;booleansTest;1053af2f-84af-49a1-b42b-e2156470bb77%2Ctemplate%3DbooleansTest#_debug_roles=restricted'
+    )
+
+    cy.get('[data-testid=action-menu-button').click()
+    cy.get('[data-testid=action-Delete]').should('have.attr', 'disabled')
+  })
+
+  it('as a requiresApproval user on an approved published document, the duplicate button will be active', () => {
+    cy.visit(
+      '/test/desk/author;914bcbf4-9ead-4be1-b797-8d2995c50380%2Ctemplate%3Dauthor-unlocked#_debug_roles=requiresApproval'
+    )
+
+    cy.get('[data-testid=action-menu-button').click()
+    cy.get('[data-testid=action-Delete]').should('not.have.attr', 'disabled')
+  })
+
+  it('as a requiresApproval user on a non-approved document, the duplicate button will be disabled', () => {
+    cy.visit(
+      '/test/desk/input-standard;booleansTest;1053af2f-84af-49a1-b42b-e2156470bb77%2Ctemplate%3DbooleansTest#_debug_roles=requiresApproval'
+    )
+
+    cy.get('[data-testid=action-menu-button').click()
+    cy.get('[data-testid=action-Delete]').should('have.attr', 'disabled')
+  })
+})
