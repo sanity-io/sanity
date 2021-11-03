4e18075708434e29bc67b8432a72f96a1ea18ead
Marius Lundgård <studio@mariuslundgard.com>
2021-11-03 11:43:07 +0100

    fix(form-builder): improve decorator styling

--- packages/@sanity/form-builder/src/inputs/PortableText/Text/Decorator.tsx
+++ packages/@sanity/form-builder/src/inputs/PortableText/Text/Decorator.tsx
@@ -1,41 +1,48 @@
-import React, {useMemo} from 'react'
+import React from 'react'
 import styled from 'styled-components'
 
-type Props = {
+interface DecoratorProps {
   mark: string
   children: React.ReactNode
 }
 
 const Root = styled.span`
-  display: inline;
+  /* Make sure the annotation styling is visible */
+  [data-annotation] &[data-decorator='code'] {
+    mix-blend-mode: multiply;
+    color: inherit;
+  }
 `
 
-export default function Decorator(props: Props) {
+/**
+ * @todo: Consider adding `data-mark` to all return paths
+ */
+export default function Decorator(props: DecoratorProps) {
   const {mark, children} = props
 
-  const child = useMemo(() => {
-    if (mark === 'em') {
-      return <em>{children}</em>
-    }
+  if (mark === 'em') {
+    return <Root as="em">{children}</Root>
+  }
 
-    if (mark === 'strike-through') {
-      return <s>{children}</s>
-    }
+  if (mark === 'strike-through') {
+    return <Root as="s">{children}</Root>
+  }
 
-    if (mark === 'underline') {
-      return <u>{children}</u>
-    }
+  if (mark === 'underline') {
+    return <Root as="u">{children}</Root>
+  }
 
-    if (mark === 'strong') {
-      return <strong>{children}</strong>
-    }
+  if (mark === 'strong') {
+    return <Root as="strong">{children}</Root>
+  }
 
-    if (mark === 'code') {
-      return <code>{children}</code>
-    }
+  if (mark === 'code') {
+    return (
+      <Root as="code" data-mark={mark}>
+        {children}
+      </Root>
+    )
+  }
 
-    return children
-  }, [children, mark])
-
-  return <Root>{child}</Root>
+  return <Root>{children}</Root>
 }
