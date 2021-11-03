9e24a5334d9d060231d40f6bcf723515f097be83
hermanwikner <wiknerherman@hotmail.com>
2021-11-03 11:58:21 +0100

    refactor(form-builder): `TextBlock/textStyles`: fix block quote styling, add default `as` prop

--- packages/@sanity/form-builder/src/inputs/PortableText/Text/TextBlock/textStyles.tsx
+++ packages/@sanity/form-builder/src/inputs/PortableText/Text/TextBlock/textStyles.tsx
@@ -1,61 +1,68 @@
-import {Box, Heading, Text} from '@sanity/ui'
+import {Heading, Text} from '@sanity/ui'
 import React from 'react'
 import styled from 'styled-components'
 
-const BlockQuoteBox = styled(Box)`
+const BlockQuoteDiv = styled.div`
   position: relative;
+  display: block;
+  margin: 0;
+  padding-left: ${({theme}) => theme.sanity.space[3]}px;
 
-  &:before {
+  &::before {
     content: '';
-    display: block;
     position: absolute;
-    top: -4px;
     left: 0;
+    top: -4px;
     bottom: -4px;
-    border-left: 3px solid var(--card-border-color);
+    width: 3px;
+    background: var(--card-border-color);
   }
 `
 
 export const Heading1 = ({children, ...rest}) => (
-  <Heading size={5} {...rest}>
+  <Heading as="h1" data-testid="heading1" size={5} {...rest}>
     {children}
   </Heading>
 )
 
 export const Heading2 = ({children, ...rest}) => (
-  <Heading size={4} {...rest}>
+  <Heading as="h2" data-testid="heading2" size={4} {...rest}>
     {children}
   </Heading>
 )
 
 export const Heading3 = ({children, ...rest}) => (
-  <Heading size={3} {...rest}>
+  <Heading as="h3" data-testid="heading3" size={3} {...rest}>
     {children}
   </Heading>
 )
 
 export const Heading4 = ({children, ...rest}) => (
-  <Heading size={2} {...rest}>
+  <Heading as="h4" data-testid="heading4" size={2} {...rest}>
     {children}
   </Heading>
 )
 
 export const Heading5 = ({children, ...rest}) => (
-  <Heading size={1} {...rest}>
+  <Heading as="h5" data-testid="heading5" size={1} {...rest}>
     {children}
   </Heading>
 )
 
 export const Heading6 = ({children, ...rest}) => (
-  <Heading muted size={0} {...rest}>
+  <Heading as="h6" data-testid="heading6" size={0} {...rest}>
     {children}
   </Heading>
 )
 
-export const Normal = ({children, ...rest}) => <Text {...rest}>{children}</Text>
+export const Normal = ({children, ...rest}) => (
+  <Text as="p" data-testid="normal" {...rest}>
+    {children}
+  </Text>
+)
 
 export const BlockQuote = ({children, ...rest}) => (
-  <BlockQuoteBox forwardedAs="blockquote" paddingLeft={3} {...rest}>
+  <BlockQuoteDiv as="blockquote" data-testid="blockquote" {...rest}>
     <Text>{children}</Text>
-  </BlockQuoteBox>
+  </BlockQuoteDiv>
 )
