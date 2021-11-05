100b1ad673ba019965a2c22a4e2e0e438dd858e7
Marius Lundgård <studio@mariuslundgard.com>
2021-11-05 12:14:19 +0100

    refactor(form-builder): clean up `VisibleOnFocusButton`

--- packages/@sanity/form-builder/src/inputs/PortableText/VisibleOnFocusButton.tsx
+++ packages/@sanity/form-builder/src/inputs/PortableText/VisibleOnFocusButton.tsx
@@ -1,35 +1,31 @@
-import React, {useCallback, useMemo, CSSProperties} from 'react'
-
+import {Button} from '@sanity/ui'
+import React from 'react'
 import styled from 'styled-components'
-import {Button, useLayer} from '@sanity/ui'
 
-const StyledButton = styled(Button)`
-  position: absolute;
-  height: 1px;
-  width: 1px;
-  overflow: hidden;
-  clip: rect(1px, 1px, 1px, 1px);
-
-  &:focus {
-    width: auto;
-    height: auto;
-    clip: auto;
-  }
-`
-type Props = {
+interface VisibleOnFocusButtonProps {
   children: React.ReactNode
   onClick: () => void
-  style: CSSProperties
 }
-export function VisibleOnFocusButton({children, onClick, style}: Props) {
-  const {zIndex} = useLayer()
-  const zIndexStyle = useMemo(() => ({zIndex: zIndex + 3, ...style}), [zIndex, style])
-  const handleClick = useCallback(() => {
-    onClick()
-  }, [onClick])
+
+const Root = styled(Button)`
+  position: absolute;
+  z-index: 3;
+  margin: 1px;
+
+  &:not(:focus) {
+    height: 1px;
+    width: 1px;
+    overflow: hidden;
+    clip: rect(1px, 1px, 1px, 1px);
+  }
+`
+
+export function VisibleOnFocusButton(props: VisibleOnFocusButtonProps) {
+  const {children, onClick} = props
+
   return (
-    <StyledButton mode="ghost" onClick={handleClick} style={zIndexStyle}>
+    <Root mode="ghost" onClick={onClick}>
       {children}
-    </StyledButton>
+    </Root>
   )
 }
