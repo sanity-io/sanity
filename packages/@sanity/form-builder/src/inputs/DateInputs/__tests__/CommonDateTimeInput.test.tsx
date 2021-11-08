3969680566e7a671c76bd4536f6b7d520376b474
Rico Kahler <ricokahler@gmail.com>
2021-11-08 15:05:45 -0500

    fix(form-builder): use Intl.DateTimeFormat instead

--- packages/@sanity/form-builder/src/inputs/DateInputs/__tests__/CommonDateTimeInput.test.tsx
+++ packages/@sanity/form-builder/src/inputs/DateInputs/__tests__/CommonDateTimeInput.test.tsx
@@ -66,13 +66,7 @@ function renderInput() {
 // Note: for the tests to be deterministic we need this to ensure tests are run in a predefined timezone
 // see globalSetup in jest config for details about how this is set up
 test('timezone for the test environment should be set to America/Los_Angeles', () => {
-  const timezoneOffset = new Date().getTimezoneOffset()
-  expect(
-    // this is true during DST
-    timezoneOffset === 420 ||
-      // this is true with no DST
-      timezoneOffset === 480
-  ).toBe(true)
+  expect(Intl.DateTimeFormat().resolvedOptions().timeZone).toBe('America/Los_Angeles')
 })
 
 test('does not emit onChange after invalid value has been typed', () => {
