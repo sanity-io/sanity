953d8acc3a64658e3b6dfb23e03429de0ed0d158
Fred Carlsen <fred@sjelfull.no>
2021-11-08 11:33:20 +0100

    fix(vision): add padding to prevent results cutoff

--- packages/@sanity/vision/src/components/ResultView.styled.js
+++ packages/@sanity/vision/src/components/ResultView.styled.js
@@ -2,6 +2,14 @@ import styled from 'styled-components'
 import {rem} from '@sanity/ui'
 
 export const ReactJsonViewContainer = styled.div`
+  padding-bottom: ${({theme}) =>
+    rem(
+      theme.sanity.space[3] * 2 +
+        theme.sanity.fonts.text.sizes[2].lineHeight -
+        theme.sanity.fonts.text.sizes[2].ascenderHeight -
+        theme.sanity.fonts.text.sizes[2].descenderHeight
+    )};
+
   .react-json-view {
     font-family: ${({theme}) => theme.sanity.fonts.code.family} !important;
     font-size: ${({theme}) => rem(theme.sanity.fonts.code.sizes[1].fontSize)} !important;
