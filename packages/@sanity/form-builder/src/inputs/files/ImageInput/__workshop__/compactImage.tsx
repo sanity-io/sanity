f95ceff5d20bdab1d5d23adaa1c0970888913147
jorngeorg <968695+jorngeorg@users.noreply.github.com>
2021-11-08 11:52:31 +0100

    add expand dialog

--- packages/@sanity/form-builder/src/inputs/files/ImageInput/__workshop__/compactImage.tsx
+++ packages/@sanity/form-builder/src/inputs/files/ImageInput/__workshop__/compactImage.tsx
@@ -17,12 +17,14 @@ import {
   Flex,
   MenuDivider,
   rgba,
+  Dialog,
 } from '@sanity/ui'
 import {
   ChevronDownIcon,
   ChevronRightIcon,
   EditIcon,
   EllipsisVerticalIcon,
+  ExpandIcon,
   FolderIcon,
   ImageIcon,
   PublishIcon,
@@ -98,138 +100,172 @@ export default function CompactImage(props) {
   const drag = useBoolean('Drag file', false, 'Props')
   const assetSources = useBoolean('Asset sources', false, 'Props')
   const padImage = useBoolean('Pad image', false, 'Props')
+  const [showExpandDialog, setShowExpandDialog] = useState(false)
 
   return (
-    <Container width={1}>
-      <Stack space={2} marginTop={6} paddingX={[3, 3, 0, 0]}>
-        <Text weight="semibold" size={1}>
-          Image input
-        </Text>
-        <Card border style={{borderStyle: hasImage ? 'solid' : 'dashed'}}>
-          <Resize direction="vertical" defaultSize={{height: '30vh'}} minHeight={200}>
-            {hasImage && (
-              <>
-                <Card data-container padding={padImage ? 3 : 0} tone="transparent" sizing="border">
-                  <img src={'https://picsum.photos/1200/900'} />
-                </Card>
-                <Overlay justify="flex-end" padding={3} drag={drag}>
-                  {drag && (
-                    <Flex
-                      direction="column"
-                      align="center"
-                      justify="center"
-                      style={{position: 'absolute', top: 0, left: 0, bottom: 0, right: 0}}
-                    >
-                      <Box marginBottom={3}>
-                        <Heading>
-                          <ImageIcon />
-                        </Heading>
-                      </Box>
-                      <Text size={1}>Drop file to upload</Text>
-                    </Flex>
-                  )}
-                  <Inline space={1}>
-                    <Tooltip
-                      content={
-                        <Box padding={2}>
-                          <Text muted size={1}>
-                            Edit details
-                          </Text>
-                        </Box>
-                      }
-                    >
-                      <Button mode="ghost" icon={EditIcon} />
-                    </Tooltip>
-                    {/* <Tooltip
-                      content={(
-                        <Box padding={2}>
-                          <Text muted size={1}>Remove image</Text>
-                        </Box>
-                      )}
-                    >
-                      <Button mode="ghost" icon={TrashIcon} tone="critical" onClick={() => setHasImage(false)}/>
-                    </Tooltip> */}
-                    <MenuButton
-                      id="image-menu"
-                      button={<Button icon={EllipsisVerticalIcon} mode="ghost" />}
-                      portal
-                      menu={
-                        <Menu>
-                          <MenuItem icon={UploadIcon} text="Upload" />
-                          {!assetSources && <MenuItem icon={SearchIcon} text="Browse" />}
-                          {assetSources && (
-                            <MenuGroup text="Browse">
-                              <MenuItem icon={ImageIcon} text="Media" />
-                              <MenuItem icon={ImageIcon} text="Unsplash" />
-                            </MenuGroup>
-                          )}
-                          <MenuDivider />
-                          <MenuItem icon={TrashIcon} text="Remove" tone="critical" />
-                        </Menu>
-                      }
-                    />
-                  </Inline>
-                </Overlay>
-                {/* {drag && (
-                  <HoverOverlay>
-                    <Card tone="transparent" height="stretch" padding={4}>
-                      <Text>drag!</Text>
-                    </Card>
-                  </HoverOverlay>
-                )} */}
-              </>
-            )}
-            {!hasImage && (
-              <Box data-container>
-                <Stack space={3}>
-                  <Flex justify="center">
-                    <Card padding={4} radius={2}>
-                      <Stack space={3}>
-                        <Flex justify="center">
-                          <Text size={4} muted>
+    <>
+      <Container width={1}>
+        <Stack space={2} marginTop={6} paddingX={[3, 3, 0, 0]}>
+          <Text weight="semibold" size={1}>
+            Image input
+          </Text>
+          <Card border style={{borderStyle: hasImage ? 'solid' : 'dashed'}}>
+            <Resize direction="vertical" defaultSize={{height: '30vh'}} minHeight={200}>
+              {hasImage && (
+                <>
+                  <Card data-container padding={padImage ? 3 : 0} tone="transparent" sizing="border">
+                    <img src={'https://source.unsplash.com/random'} />
+                  </Card>
+                  <Overlay justify="flex-end" padding={3} drag={drag}>
+                    {drag && (
+                      <Flex
+                        direction="column"
+                        align="center"
+                        justify="center"
+                        style={{position: 'absolute', top: 0, left: 0, bottom: 0, right: 0}}
+                      >
+                        <Box marginBottom={3}>
+                          <Heading>
                             <ImageIcon />
-                          </Text>
-                        </Flex>
-                        <Text size={1} muted>Drag or paste image here</Text>
-                      </Stack>
-                    </Card>
-                  </Flex>
-
-                  <Inline space={1}>
-                    <Button
-                      onClick={() => setHasImage(true)}
-                      text="Upload"
-                      mode="ghost"
-                      icon={UploadIcon}
-                    />
-                    {!assetSources && <Button text="Browse media" mode="ghost" icon={SearchIcon} />}
-                    {assetSources && (
-                      <MenuButton
-                        id="asset-source-menubutton"
-                        button={
-                          <Button
-                            text="Browse"
-                            icon={SearchIcon}
-                            iconRight={ChevronDownIcon}
-                            mode="ghost"
-                          />
+                          </Heading>
+                        </Box>
+                        <Text size={1}>Drop file to upload</Text>
+                      </Flex>
+                    )}
+                    <Inline space={1}>
+                      <Tooltip
+                        content={
+                          <Box padding={2}>
+                            <Text muted size={1}>
+                              Edit details
+                            </Text>
+                          </Box>
                         }
+                      >
+                        <Button mode="ghost" icon={EditIcon} />
+                      </Tooltip>
+                      <Tooltip
+                        content={
+                          <Box padding={2}>
+                            <Text muted size={1}>
+                              Expand
+                            </Text>
+                          </Box>
+                        }
+                      >
+                        <Button
+                          mode="ghost"
+                          icon={ExpandIcon}
+                          onClick={() => setShowExpandDialog(true)}
+                        />
+                      </Tooltip>
+                      {/* <Tooltip
+                        content={(
+                          <Box padding={2}>
+                            <Text muted size={1}>Remove image</Text>
+                          </Box>
+                        )}
+                      >
+                        <Button mode="ghost" icon={TrashIcon} tone="critical" onClick={() => setHasImage(false)}/>
+                      </Tooltip> */}
+                      <MenuButton
+                        id="image-menu"
+                        button={<Button icon={EllipsisVerticalIcon} mode="ghost" />}
+                        portal
                         menu={
-                          <Menu icon={FolderIcon} text="Browse">
-                            <MenuItem icon={ImageIcon} text="Media" />
-                            <MenuItem icon={ImageIcon} text="Unsplash" />
+                          <Menu>
+                            <MenuItem icon={UploadIcon} text="Upload" />
+                            {!assetSources && <MenuItem icon={SearchIcon} text="Browse" />}
+                            {assetSources && (
+                              <MenuGroup text="Browse">
+                                <MenuItem icon={ImageIcon} text="Media" />
+                                <MenuItem icon={ImageIcon} text="Unsplash" />
+                              </MenuGroup>
+                            )}
+                            <MenuDivider />
+                            <MenuItem icon={TrashIcon} text="Remove" tone="critical" />
                           </Menu>
                         }
-                        portal
                       />
-                    )}
-                  </Inline>
-                </Stack>
-              </Box>
-            )}
-          </Resize>
-        </Card>
-      </Stack>
-    </Container>
+                    </Inline>
+                  </Overlay>
+                  {/* {drag && (
+                    <HoverOverlay>
+                      <Card tone="transparent" height="stretch" padding={4}>
+                        <Text>drag!</Text>
+                      </Card>
+                    </HoverOverlay>
+                  )} */}
+                </>
+              )}
+              {!hasImage && (
+                <Box data-container>
+                  <Stack space={3}>
+                    <Flex justify="center">
+                      <Card padding={4} radius={2}>
+                        <Stack space={3}>
+                          <Flex justify="center">
+                            <Text size={4} muted>
+                              <ImageIcon />
+                            </Text>
+                          </Flex>
+                          <Text size={1} muted>Drag or paste image here</Text>
+                        </Stack>
+                      </Card>
+                    </Flex>
+
+                    <Inline space={1}>
+                      <Button
+                        onClick={() => setHasImage(true)}
+                        text="Upload"
+                        mode="ghost"
+                        icon={UploadIcon}
+                      />
+                      {!assetSources && <Button text="Browse media" mode="ghost" icon={SearchIcon} />}
+                      {assetSources && (
+                        <MenuButton
+                          id="asset-source-menubutton"
+                          button={
+                            <Button
+                              text="Browse"
+                              icon={SearchIcon}
+                              iconRight={ChevronDownIcon}
+                              mode="ghost"
+                            />
+                          }
+                          menu={
+                            <Menu icon={FolderIcon} text="Browse">
+                              <MenuItem icon={ImageIcon} text="Media" />
+                              <MenuItem icon={ImageIcon} text="Unsplash" />
+                            </Menu>
+                          }
+                          portal
+                        />
+                      )}
+                    </Inline>
+                  </Stack>
+                </Box>
+              )}
+            </Resize>
+          </Card>
+        </Stack>
+      </Container>
+      {showExpandDialog && (
+        <Dialog
+          header="Image title"
+          id="expanded-image-dialog"
+          zOffset={1000}
+          width={4}
+          onClose={() => setShowExpandDialog(false)}
+          style={{maxHeight: '100%'}}
+        >
+          <Card tone="transparent" padding={4} style={{height: 'calc(100% - 2.5rem)'}}>
+            <Flex height="fill" align="center" justify="center">
+              <img src="https://source.unsplash.com/random" style={{objectFit:'contain', maxWidth: '100%', maxHeight: '100%'}} />
+            </Flex>
+          </Card>
+        </Dialog>
+      )}
+    </>
   )
 }
