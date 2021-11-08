39cc89ed4fb110d733eaff0b57b11e02c65f90b2
hermanwikner <wiknerherman@hotmail.com>
2021-11-08 17:02:48 +0100

    test(base): `CollapseMenuStory`: add `disableRestoreFocusOnClose` and `onMenuClose`

--- packages/@sanity/base/src/components/__workshop__/CollapseMenuStory.tsx
+++ packages/@sanity/base/src/components/__workshop__/CollapseMenuStory.tsx
@@ -1,5 +1,5 @@
 import React, {useCallback, useMemo, useState} from 'react'
-import {Button, Card, Container} from '@sanity/ui'
+import {Button, Card, Container, useToast} from '@sanity/ui'
 import {
   BoldIcon,
   CodeIcon,
@@ -16,15 +16,25 @@ import {CollapseMenu, CollapseMenuButton} from '..'
 const GAP_OPTIONS = {'0': 0, '1': 1, '2': 2, '3': 3}
 
 export default function CollapseMenuStory() {
+  const {push} = useToast()
   const [selected, setSelected] = useState<string>('')
   const collapseText = useBoolean('Collapse text', true)
   const customMenuButton = useBoolean('Custom menu button', false)
   const gap = useSelect('Gap', GAP_OPTIONS, 1)
   const collapsed = useBoolean('Collapsed', false)
+  const disableRestoreFocusOnClose = useBoolean('Disable restore focus on close', false)
+  const onMenuClose = useBoolean('onMenuClose', false)
 
   const handleSelect = useCallback((id: string) => {
     setSelected(id)
   }, [])
+
+  const handleMenuClose = useCallback(() => {
+    push({
+      title: 'Menu closed',
+      status: 'success',
+    })
+  }, [push])
 
   const menuButton = useMemo(
     () => (customMenuButton ? <Button tone="primary" icon={CogIcon} /> : undefined),
@@ -34,7 +44,13 @@ export default function CollapseMenuStory() {
   return (
     <Container padding={4} width={4} sizing="border">
       <Card shadow={1} padding={1} radius={3}>
-        <CollapseMenu gap={gap} menuButton={menuButton} collapsed={collapsed}>
+        <CollapseMenu
+          collapsed={collapsed}
+          gap={gap}
+          menuButton={menuButton}
+          onMenuClose={onMenuClose ? handleMenuClose : undefined}
+          disableRestoreFocusOnClose={disableRestoreFocusOnClose}
+        >
           <CollapseMenuButton
             buttonProps={{mode: 'bleed'}}
             collapseText={collapseText}
