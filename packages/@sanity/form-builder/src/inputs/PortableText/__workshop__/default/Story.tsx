0014efff40b94167d732506b6e700eb6ed25893d
Marius Lundgård <studio@mariuslundgard.com>
2021-11-09 12:37:52 +0100

    test(form-builder): improve PTE workshop

--- packages/@sanity/form-builder/src/inputs/PortableText/__workshop__/default/Story.tsx
+++ packages/@sanity/form-builder/src/inputs/PortableText/__workshop__/default/Story.tsx
@@ -11,8 +11,8 @@ export default function Story() {
   const value = values[selectedValue]
 
   return (
-    <LayerProvider>
-      <Card height="fill" padding={4} sizing="border">
+    <LayerProvider zOffset={100}>
+      <Card height="fill" overflow="hidden" padding={4} sizing="border">
         <Flex align="center" height="fill" justify="center">
           <Container width={1}>
             <TestInput readOnly={readOnly} value={value} type={portableTextType} schema={schema} />
