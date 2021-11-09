8a588a9ecd61cb1c0909413a4f4306db11ec8e9e
Marius Lundgård <studio@mariuslundgard.com>
2021-11-09 18:11:32 +0100

    chore: fix lint issues

--- dev/example-studio/schemas/videoEmbed.js
+++ dev/example-studio/schemas/videoEmbed.js
@@ -1,4 +1,3 @@
-import {FaVideoCamera as icon} from 'react-icons/fa'
 import VideoEmbedPreview from '../components/VideoEmbedPreview/VideoEmbedPreview'
 import VideoEmbedInput from '../components/VideoEmbedInput'
 
@@ -6,7 +5,6 @@ export default {
   type: 'object',
   name: 'videoEmbed',
   title: 'Video',
-  icon,
   fields: [
     {
       name: 'service',
