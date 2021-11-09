81835be465c16142b5354a005a91e7333ed501fe
Marius Lundgård <studio@mariuslundgard.com>
2021-11-09 12:37:38 +0100

    refactor(form-builder): begin migrate to `@sanity/ui`

---
+++ packages/@sanity/form-builder/src/inputs/PortableText/Input.styles.tsx
@@ -0,0 +1,55 @@
+import {Layer} from '@sanity/ui'
+import styled, {css} from 'styled-components'
+import {focusRingBorderStyle, focusRingStyle} from '../../components/withFocusRing/helpers'
+
+export const Root = styled.div(({theme}) => {
+  const {focusRing, input} = theme.sanity
+  const base = theme.sanity.color.base
+  const color = theme.sanity.color.input
+  const border = {
+    color: color.default.enabled.border,
+    width: input.border.width,
+  }
+
+  return css`
+    --input-box-shadow: ${focusRingBorderStyle(border)};
+
+    position: relative;
+
+    & [data-wrapper] {
+      overflow: hidden;
+      position: relative;
+      z-index: 1;
+      padding: ${input.border.width}px;
+    }
+
+    & [data-border] {
+      position: absolute;
+      top: 0;
+      left: 0;
+      right: 0;
+      bottom: 0;
+      box-shadow: var(--input-box-shadow);
+      z-index: 2;
+      border-radius: 1px;
+      pointer-events: none;
+    }
+
+    &:not([data-read-only])[data-focused] [data-border] {
+      --input-box-shadow: ${focusRingStyle({
+        base,
+        border,
+        focusRing,
+      })};
+    }
+  `
+})
+
+// This element only wraps the input when in "fullscreen" mode
+export const ExpandedLayer = styled(Layer)`
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+`
