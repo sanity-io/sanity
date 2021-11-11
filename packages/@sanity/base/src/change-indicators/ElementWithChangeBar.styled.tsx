76d3cfc09674b3eba391d4a803ae3f5c48beedc4
Marius Lundgård <studio@mariuslundgard.com>
2021-11-11 13:15:44 +0100

    refactor(base): adjust `ElementWithChangeBar`

--- packages/@sanity/base/src/change-indicators/ElementWithChangeBar.styled.tsx
+++ packages/@sanity/base/src/change-indicators/ElementWithChangeBar.styled.tsx
@@ -15,80 +15,6 @@ interface RootProps {
 
 const animationSpeed = 250
 
-export const ChangeBar = styled.div`
-  position: relative;
-  opacity: 1;
-  transition: opacity 100ms;
-`
-
-export const ChangeBarMarker = styled.div(({theme}: ThemeContext) => {
-  /* these colours aren't freely available on the current theme */
-  const notSelectedColor = theme.sanity.color.spot.yellow
-  const screenMedium = theme.sanity.media[0]
-
-  return css`
-    position: absolute;
-    top: 0;
-    left: 2px;
-    width: 2px;
-    bottom: 0;
-    background-color: ${notSelectedColor};
-    border-radius: 1px;
-
-    @media (min-width: ${screenMedium}px) {
-      display: unset;
-    }
-  `
-})
-
-export const ChangeBarButton = styled.button(({theme}: ThemeContext) => {
-  /* these colours aren't freely available on the current theme */
-  const notSelectedColor = theme.sanity.color.spot.yellow
-
-  return css`
-    appearance: none;
-    border: 0;
-    outline: 0;
-    display: block;
-    padding: 0;
-    background: transparent;
-    opacity: 0;
-    position: absolute;
-    height: 100%;
-    cursor: pointer;
-    pointer-events: all;
-    left: -0.25rem;
-    width: 1rem;
-    transition: opacity ${animationSpeed}ms;
-
-    &:focus {
-      border: 0;
-      outline: 0;
-    }
-
-    &:after {
-      content: '';
-      width: 16px;
-      height: calc(100% + 14px);
-      display: block;
-      position: absolute;
-      top: -7px;
-      left: -5px;
-      border-radius: 16px;
-      background: ${notSelectedColor};
-    }
-
-    &:focus {
-      border: 0;
-      outline: 0;
-    }
-
-    &:hover {
-      opacity: 0.2;
-    }
-  `
-})
-
 export const ChangeBarWrapper = styled.div(({changed, disabled, isReviewChangeOpen}: RootProps) => {
   if (disabled)
     return css`
@@ -98,6 +24,8 @@ export const ChangeBarWrapper = styled.div(({changed, disabled, isReviewChangeOp
     `
 
   return css`
+    --change-bar-offset: 2px;
+
     display: flex;
     position: relative;
 
@@ -130,3 +58,77 @@ export const FieldWrapper = styled.div`
   flex-grow: 1;
   min-width: 0;
 `
+
+export const ChangeBar = styled.div`
+  position: relative;
+  opacity: 1;
+  transition: opacity 100ms;
+`
+
+export const ChangeBarMarker = styled.div(({theme}: ThemeContext) => {
+  /* these colours aren't freely available on the current theme */
+  const notSelectedColor = theme.sanity.color.spot.yellow
+  const screenMedium = theme.sanity.media[0]
+
+  return css`
+    position: absolute;
+    top: 0;
+    left: var(--change-bar-offset);
+    width: 2px;
+    bottom: 0;
+    background-color: ${notSelectedColor};
+    border-radius: 1px;
+
+    @media (min-width: ${screenMedium}px) {
+      display: unset;
+    }
+  `
+})
+
+export const ChangeBarButton = styled.button(({theme}: ThemeContext) => {
+  /* these colours aren't freely available on the current theme */
+  const notSelectedColor = theme.sanity.color.spot.yellow
+
+  return css`
+    appearance: none;
+    border: 0;
+    outline: 0;
+    display: block;
+    padding: 0;
+    background: transparent;
+    opacity: 0;
+    position: absolute;
+    height: 100%;
+    cursor: pointer;
+    pointer-events: all;
+    left: calc(-0.25rem + var(--change-bar-offset));
+    width: 1rem;
+    transition: opacity ${animationSpeed}ms;
+
+    &:focus {
+      border: 0;
+      outline: 0;
+    }
+
+    &:after {
+      content: '';
+      width: 16px;
+      height: calc(100% + 14px);
+      display: block;
+      position: absolute;
+      top: -7px;
+      left: -3px;
+      border-radius: 8px;
+      background: ${notSelectedColor};
+    }
+
+    &:focus {
+      border: 0;
+      outline: 0;
+    }
+
+    &:hover {
+      opacity: 0.2;
+    }
+  `
+})
