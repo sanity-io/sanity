02334332e315b0c3213317a4681bfe3c77a8b90e
Bjørge Næss <bjoerge@gmail.com>
2021-11-18 11:55:29 +0100

    chore(test-studio): add an image to species type

--- dev/test-studio/schema/species.js
+++ dev/test-studio/schema/species.js
@@ -18,15 +18,22 @@ export default {
       title: 'Species',
       type: 'string',
     },
+    {
+      name: 'image',
+      title: 'Image',
+      type: 'image',
+    },
   ],
   preview: {
     select: {
       name: 'name',
       genus: 'genus',
       species: 'species',
+      media: 'image',
     },
     prepare(value) {
       return {
+        ...value,
         title: [value.genus, value.species, value.name && `(${value.name})`]
           .filter(Boolean)
           .join(' '),
