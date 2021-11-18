320ba36d2710209ff03f51c904be1f5bd6ee3ea5
Bjørge Næss <bjoerge@gmail.com>
2021-11-18 11:58:16 +0100

    feat(form-builder) support inline editing of reference items
    Co-authored-by: Mikolaj Dobrucki <5467602+mikolajdobrucki@users.noreply.github.com>
    Co-authored-by: Rico Kahler <ricokahler@gmail.com>

--- packages/@sanity/form-builder/src/inputs/ReferenceInput/TextWithTone.tsx
+++ packages/@sanity/form-builder/src/inputs/ReferenceInput/TextWithTone.tsx
@@ -1,17 +1,17 @@
 import styled, {css} from 'styled-components'
-import {Text, Theme, ThemeColorToneKey} from '@sanity/ui'
+import {Text, Theme, ThemeColorButtonTones} from '@sanity/ui'
 
-export const TextWithTone = styled(Text)<{$tone: ThemeColorToneKey}>(
-  ({$tone, theme}: {$tone: ThemeColorToneKey; theme: Theme}) => {
+type ButtonToneKeys = keyof ThemeColorButtonTones
+
+export const TextWithTone = styled(Text)<{$tone: ButtonToneKeys}>(
+  ({$tone, theme}: {$tone: ButtonToneKeys; theme: Theme}) => {
     const tone = theme.sanity.color.button.bleed[$tone]
     return css`
       &:not(*[data-selected]) {
         --card-fg-color: ${tone ? tone.enabled.fg : undefined};
-        --card-muted-fg-color: ${tone ? tone.enabled.fg : undefined};
       }
       [data-ui='Card']:disabled & {
         --card-fg-color: inherit;
-        --card-muted-fg-color: inherit;
       }
     `
   }
