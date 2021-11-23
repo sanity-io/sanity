02d2146230d183dfebf7fafc9d20048b232d3313
Rita <rita@sanity.io>
2021-11-23 10:03:18 +0100

    test(cypress): remove some e2e tests in the desk-tool folder
    since some of these - specifically focused on the actions - have been turned into unit tests, they've been turned into smoke screen.

--- cypress/integration/desk-tool/actionPermission.test.js
+++ cypress/integration/desk-tool/actionPermission.test.js
@@ -12,41 +12,6 @@ describe('@sanity/desk-tool: publish permission', () => {
 
     cy.get('[data-testid=action-Publish]').should('not.have.attr', 'disabled')
   })
-
-  it('as an restricted user, the publish button will be disabled', () => {
-    cy.visit(
-      '/test/desk/input-standard;booleansTest;1053af2f-84af-49a1-b42b-e2156470bb77%2Ctemplate%3DbooleansTest#_debug_roles=restricted'
-    )
-
-    // this whole document will be disabled and is outside of the scope of this test
-
-    cy.get('[data-testid=action-Publish]').should('have.attr', 'disabled')
-  })
-
-  it('as a requiresApproval user on an approved published document, the publish button will be active', () => {
-    cy.visit(
-      '/test/desk/author;914bcbf4-9ead-4be1-b797-8d2995c50380%2Ctemplate%3Dauthor-unlocked#_debug_roles=requiresApproval'
-    )
-
-    // eslint-disable-next-line cypress/no-unnecessary-waiting
-    cy.wait(4500)
-
-    // as the publish button works right now, unless changes are made to the document once opened, the button will always be disaled
-    // this is expected behaviour for now.
-    cy.get('input').eq(1).type(' ')
-
-    cy.get('[data-testid=action-Publish]').should('not.have.attr', 'disabled')
-  })
-
-  it('as a requiresApproval user on a non-approved document, the publish button will be disabled', () => {
-    cy.visit(
-      '/test/desk/input-standard;booleansTest;1053af2f-84af-49a1-b42b-e2156470bb77%2Ctemplate%3DbooleansTest#_debug_roles=requiresApproval'
-    )
-
-    // this whole document will be disabled and is outside of the scope of this test
-
-    cy.get('[data-testid=action-Publish]').should('have.attr', 'disabled')
-  })
 })
 
 describe('@sanity/desk-tool: duplicate permission', () => {
@@ -57,36 +22,6 @@ describe('@sanity/desk-tool: duplicate permission', () => {
 
     cy.get('[data-testid=action-menu-button').click()
     cy.get('[data-testid=action-Duplicate]').should('not.have.attr', 'disabled')
-  })
-
-  it('as an restricted user, the duplicate button will be disabled', () => {
-    cy.visit(
-      '/test/desk/input-standard;booleansTest;1053af2f-84af-49a1-b42b-e2156470bb77%2Ctemplate%3DbooleansTest#_debug_roles=restricted'
-    )
-
-    cy.get('[data-testid=action-menu-button').click()
-    cy.get('[data-testid=action-Duplicate]').should('have.attr', 'disabled')
-  })
-
-  it('as a requiresApproval user on an approved published document, the duplicate button will be active', () => {
-    cy.visit(
-      '/test/desk/author;914bcbf4-9ead-4be1-b797-8d2995c50380%2Ctemplate%3Dauthor-unlocked#_debug_roles=requiresApproval'
-    )
-
-    // eslint-disable-next-line cypress/no-unnecessary-waiting
-    cy.wait(4500)
-
-    cy.get('[data-testid=action-menu-button').click()
-    cy.get('[data-testid=action-Duplicate]').should('not.have.attr', 'disabled')
-  })
-
-  it('as a requiresApproval user on a non-approved document, the duplicate button will be disabled', () => {
-    cy.visit(
-      '/test/desk/input-standard;booleansTest;1053af2f-84af-49a1-b42b-e2156470bb77%2Ctemplate%3DbooleansTest#_debug_roles=requiresApproval'
-    )
-
-    cy.get('[data-testid=action-menu-button').click()
-    cy.get('[data-testid=action-Duplicate]').should('have.attr', 'disabled')
   })
 })
 
@@ -99,36 +34,6 @@ describe('@sanity/desk-tool: delete permission', () => {
     cy.get('[data-testid=action-menu-button').click()
     cy.get('[data-testid=action-Delete]').should('not.have.attr', 'disabled')
   })
-
-  it('as an restricted user, the duplicate button will be disabled', () => {
-    cy.visit(
-      '/test/desk/input-standard;booleansTest;1053af2f-84af-49a1-b42b-e2156470bb77%2Ctemplate%3DbooleansTest#_debug_roles=restricted'
-    )
-
-    cy.get('[data-testid=action-menu-button').click()
-    cy.get('[data-testid=action-Delete]').should('have.attr', 'disabled')
-  })
-
-  it('as a requiresApproval user on an approved published document, the duplicate button will be active', () => {
-    cy.visit(
-      '/test/desk/author;914bcbf4-9ead-4be1-b797-8d2995c50380%2Ctemplate%3Dauthor-unlocked#_debug_roles=requiresApproval'
-    )
-
-    // eslint-disable-next-line cypress/no-unnecessary-waiting
-    cy.wait(4500)
-
-    cy.get('[data-testid=action-menu-button').click()
-    cy.get('[data-testid=action-Delete]').should('not.have.attr', 'disabled')
-  })
-
-  it('as a requiresApproval user on a non-approved document, the duplicate button will be disabled', () => {
-    cy.visit(
-      '/test/desk/input-standard;booleansTest;1053af2f-84af-49a1-b42b-e2156470bb77%2Ctemplate%3DbooleansTest#_debug_roles=requiresApproval'
-    )
-
-    cy.get('[data-testid=action-menu-button').click()
-    cy.get('[data-testid=action-Delete]').should('have.attr', 'disabled')
-  })
 })
 
 describe('@sanity/desk-tool: unpublish permission', () => {
@@ -140,36 +45,6 @@ describe('@sanity/desk-tool: unpublish permission', () => {
     cy.get('[data-testid=action-menu-button').click()
     cy.get('[data-testid=action-Unpublish]').should('not.have.attr', 'disabled')
   })
-
-  it('as an restricted user, the unpublish button will be disabled', () => {
-    cy.visit(
-      '/test/desk/input-standard;booleansTest;1053af2f-84af-49a1-b42b-e2156470bb77%2Ctemplate%3DbooleansTest#_debug_roles=restricted'
-    )
-
-    cy.get('[data-testid=action-menu-button').click()
-    cy.get('[data-testid=action-Unpublish]').should('have.attr', 'disabled')
-  })
-
-  it('as a requiresApproval user on an approved published document, the unpublish button will be active', () => {
-    cy.visit(
-      '/test/desk/author;914bcbf4-9ead-4be1-b797-8d2995c50380%2Ctemplate%3Dauthor-unlocked#_debug_roles=requiresApproval'
-    )
-
-    // eslint-disable-next-line cypress/no-unnecessary-waiting
-    cy.wait(4500)
-
-    cy.get('[data-testid=action-menu-button').click()
-    cy.get('[data-testid=action-Unpublish]').should('not.have.attr', 'disabled')
-  })
-
-  it('as a requiresApproval user on a non-approved document, the unpublish button will be disabled', () => {
-    cy.visit(
-      '/test/desk/input-standard;booleansTest;1053af2f-84af-49a1-b42b-e2156470bb77%2Ctemplate%3DbooleansTest#_debug_roles=requiresApproval'
-    )
-
-    cy.get('[data-testid=action-menu-button').click()
-    cy.get('[data-testid=action-Unpublish]').should('have.attr', 'disabled')
-  })
 })
 
 describe('@sanity/desk-tool: discardDraft permission', () => {
@@ -180,35 +55,5 @@ describe('@sanity/desk-tool: discardDraft permission', () => {
 
     cy.get('[data-testid=action-menu-button').click()
     cy.get('[data-testid=action-Discardchanges]').should('not.have.attr', 'disabled')
-  })
-
-  it('as an restricted user, the discard changes button will be disabled', () => {
-    cy.visit(
-      '/test/desk/input-standard;booleansTest;1053af2f-84af-49a1-b42b-e2156470bb77%2Ctemplate%3DbooleansTest#_debug_roles=restricted'
-    )
-
-    cy.get('[data-testid=action-menu-button').click()
-    cy.get('[data-testid=action-Discardchanges]').should('have.attr', 'disabled')
-  })
-
-  it('as a requiresApproval user on an approved published document, the discard changes button will be active', () => {
-    cy.visit(
-      '/test/desk/author;914bcbf4-9ead-4be1-b797-8d2995c50380%2Ctemplate%3Dauthor-unlocked#_debug_roles=requiresApproval'
-    )
-
-    // eslint-disable-next-line cypress/no-unnecessary-waiting
-    cy.wait(4500)
-
-    cy.get('[data-testid=action-menu-button').click()
-    cy.get('[data-testid=action-Discardchanges]').should('not.have.attr', 'disabled')
-  })
-
-  it('as a requiresApproval user on a non-approved document, the discard changes button will be disabled', () => {
-    cy.visit(
-      '/test/desk/input-standard;booleansTest;1053af2f-84af-49a1-b42b-e2156470bb77%2Ctemplate%3DbooleansTest#_debug_roles=requiresApproval'
-    )
-
-    cy.get('[data-testid=action-menu-button').click()
-    cy.get('[data-testid=action-Discardchanges]').should('have.attr', 'disabled')
   })
 })
