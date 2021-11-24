2a2acecf8f61192c99bf8571bc78989c9769ab1f
Bjørge Næss <bjoerge@gmail.com>
2021-11-24 16:29:59 +0000

    refactor(form-builder): create a shared reference autocomplete

---
+++ packages/@sanity/form-builder/src/inputs/ReferenceInput/ReferenceAutocomplete.tsx
@@ -0,0 +1,37 @@
+import React, {ComponentProps, ForwardedRef, forwardRef, RefObject, useCallback} from 'react'
+import {Autocomplete, Popover} from '@sanity/ui'
+import styled from 'styled-components'
+import {AutocompleteHeightFix} from './utils/AutocompleteHeightFix'
+
+const StyledPopover = styled(Popover)`
+  width: 100%;
+  & > div {
+    overflow: auto;
+    -webkit-overflow-scrolling: touch;
+  }
+`
+
+const MARGINS: [number, number, number, number] = [-2, 0, 0, 0]
+
+export const ReferenceAutocomplete = forwardRef(function ReferenceAutocomplete(
+  props: ComponentProps<typeof Autocomplete> & {popoverRef?: RefObject<HTMLDivElement>},
+  ref: ForwardedRef<HTMLInputElement>
+) {
+  const hasHits = props.options.length > 0
+  const renderPopover = useCallback(
+    ({content, hidden, inputElement}) => (
+      <StyledPopover
+        __unstable_margins={MARGINS}
+        placement="bottom-start"
+        arrow={false}
+        constrainSize
+        content={content}
+        open={hasHits && !props.loading && !hidden}
+        ref={props.popoverRef}
+        referenceElement={inputElement}
+      />
+    ),
+    [props.loading, hasHits, props.popoverRef]
+  )
+  return <Autocomplete {...props} ref={ref} renderPopover={renderPopover} />
+})
