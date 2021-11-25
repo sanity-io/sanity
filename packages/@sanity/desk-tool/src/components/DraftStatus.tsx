1ba41152c9d6db481675d6291ca1a6386c5ed877
hermanwikner <wiknerherman@hotmail.com>
2021-11-25 12:47:32 +0100

    feat(desk-tool): update `DraftStatus` with `TextWithTone` and `Tooltip`

--- packages/@sanity/desk-tool/src/components/DraftStatus.tsx
+++ packages/@sanity/desk-tool/src/components/DraftStatus.tsx
@@ -1,9 +1,26 @@
-import {EditIcon} from '@sanity/icons'
-import {Text} from '@sanity/ui'
 import React from 'react'
+import {Box, Text, Tooltip} from '@sanity/ui'
+import {TextWithTone} from '@sanity/base/components'
+import {EditIcon} from '@sanity/icons'
+import {SanityDocument} from '@sanity/types'
+import {TimeAgo} from './TimeAgo'
 
-export const DraftStatus = () => (
-  <Text size={1} as="span" muted title="There are unpublished edits">
-    <EditIcon aria-label="There are unpublished edits" />
-  </Text>
+export const DraftStatus = ({document}: {document?: SanityDocument | null}) => (
+  <Tooltip
+    content={
+      <Box padding={2}>
+        <Text size={1}>
+          {document ? (
+            <>Edited {document?._updatedAt && <TimeAgo time={document?._updatedAt} />}</>
+          ) : (
+            <>No unpublished edits</>
+          )}
+        </Text>
+      </Box>
+    }
+  >
+    <TextWithTone tone="caution" dimmed={!document} muted={!document} size={1}>
+      <EditIcon />
+    </TextWithTone>
+  </Tooltip>
 )
