03c82f9f6ac6c3ddf6830ae6267b80ff0e4d11c2
hermanwikner <wiknerherman@hotmail.com>
2021-11-25 13:03:50 +0100

    feat(form-builder): update `ReferenceInput` so that `TextWithTone` inherits correct color

--- packages/@sanity/form-builder/src/inputs/ReferenceInput/ReferenceInput.tsx
+++ packages/@sanity/form-builder/src/inputs/ReferenceInput/ReferenceInput.tsx
@@ -40,6 +40,7 @@ import {FormFieldPresence} from '@sanity/base/presence'
 import {getPublishedId} from '@sanity/base/_internal'
 import {useObservableCallback} from 'react-rx'
 import {uuid} from '@sanity/uuid'
+import styled from 'styled-components'
 import PatchEvent, {set, setIfMissing, unset} from '../../PatchEvent'
 import {EMPTY_ARRAY} from '../../utils/empty'
 import {useDidUpdate} from '../../hooks/useDidUpdate'
@@ -87,6 +88,18 @@ const NO_FILTER = () => true
 function nonNullable<T>(v: T): v is NonNullable<T> {
   return v !== null
 }
+
+const OptionCard = styled(Card)`
+  /* TextWithTone uses its own logic to set color, and we therefore need 
+  to override this logic in order to set the correct color in different states */
+  &[data-selected],
+  &[data-pressed],
+  &:active {
+    [data-ui='TextWithTone'] {
+      color: inherit;
+    }
+  }
+`
 
 const REF_PATH = ['_ref']
 export const ReferenceInput = forwardRef(function ReferenceInput(
@@ -308,11 +321,11 @@ export const ReferenceInput = forwardRef(function ReferenceInput(
     (option) => {
       const id = option.hit.draft?._id || option.hit.published?._id
       return (
-        <Card as="button" type="button" radius={2}>
+        <OptionCard forwardedAs="button" type="button" radius={2}>
           <Box paddingX={3} paddingY={1}>
             <OptionPreview type={type} id={id} getReferenceInfo={getReferenceInfoMemo} />
           </Box>
-        </Card>
+        </OptionCard>
       )
     },
     [type, getReferenceInfoMemo]
@@ -457,13 +470,12 @@ export const ReferenceInput = forwardRef(function ReferenceInput(
               }
             >
               <Flex align="center" padding={1}>
-                <Card
+                <OptionCard
                   flex={1}
                   padding={1}
                   paddingRight={3}
                   radius={2}
-                  as={EditReferenceLink}
-                  //@ts-expect-error issue with styled components "as" polymorphism
+                  forwardedAs={EditReferenceLink as any}
                   documentId={value?._ref}
                   documentType={refType?.name}
                   data-as="a"
@@ -484,7 +496,7 @@ export const ReferenceInput = forwardRef(function ReferenceInput(
                     type={type}
                     selected={selected}
                   />
-                </Card>
+                </OptionCard>
                 <Inline paddingX={1}>
                   <MenuButton
                     button={<Button padding={2} mode="bleed" icon={EllipsisVerticalIcon} />}
