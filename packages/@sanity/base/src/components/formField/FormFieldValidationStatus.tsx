715e1c93ea5a7c80da0179fa8513ccb6994f78e9
hermanwikner <wiknerherman@hotmail.com>
2021-11-26 15:32:53 +0100

    feat(base): add info validation support in `FormFieldValidationStatus`

--- packages/@sanity/base/src/components/formField/FormFieldValidationStatus.tsx
+++ packages/@sanity/base/src/components/formField/FormFieldValidationStatus.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable camelcase */
 
 import {hues} from '@sanity/color'
-import {ErrorOutlineIcon, WarningOutlineIcon} from '@sanity/icons'
+import {ErrorOutlineIcon, InfoOutlineIcon, WarningOutlineIcon} from '@sanity/icons'
 import {
   isValidationErrorMarker,
   isValidationMarker,
@@ -10,7 +10,7 @@ import {
   ValidationMarker,
 } from '@sanity/types'
 import {Box, Flex, Placement, Stack, Text, Tooltip} from '@sanity/ui'
-import React, {createElement} from 'react'
+import React from 'react'
 import {markersToValidationList} from './helpers'
 import {FormFieldValidation} from './types'
 
@@ -28,6 +28,18 @@ export interface FormFieldValidationStatusProps {
   portal?: boolean
 }
 
+const VALIDATION_COLORS: Record<'error' | 'warning' | 'info', string> = {
+  error: hues.red[500].hex,
+  warning: hues.yellow[500].hex,
+  info: hues.blue[500].hex,
+}
+
+const VALIDATION_ICONS: Record<'error' | 'warning' | 'info', React.ReactElement> = {
+  error: <ErrorOutlineIcon />,
+  warning: <WarningOutlineIcon />,
+  info: <InfoOutlineIcon />,
+}
+
 export function FormFieldValidationStatus(props: FormFieldValidationStatusProps) {
   const {
     __unstable_markers: markers = [],
@@ -39,9 +51,26 @@ export function FormFieldValidationStatus(props: FormFieldValidationStatusProps)
   const validationMarkers = markers.filter(isValidationMarker)
   const validation = markersToValidationList(validationMarkers)
   const errors = validation.filter((v) => v.type === 'error')
+  const warnings = validation.filter((v) => v.type === 'warning')
+  const info = validation.filter((v) => v.type === 'info')
+
   const hasErrors = errors.length > 0
-  const statusIcon = hasErrors ? ErrorOutlineIcon : WarningOutlineIcon
-  const statusColor = hasErrors ? hues.red[500].hex : hues.yellow[500].hex
+  const hasWarnings = warnings.length > 0
+  const hasInfo = info.length > 0
+
+  const statusIcon = () => {
+    if (hasErrors) return VALIDATION_ICONS.error
+    if (hasWarnings) return VALIDATION_ICONS.warning
+    if (hasInfo) return VALIDATION_ICONS.info
+    return null
+  }
+
+  const statusColor = () => {
+    if (hasErrors) return VALIDATION_COLORS.error
+    if (hasWarnings) return VALIDATION_COLORS.warning
+    if (hasInfo) return VALIDATION_COLORS.info
+    return null
+  }
 
   return (
     <Tooltip
@@ -64,8 +93,8 @@ export function FormFieldValidationStatus(props: FormFieldValidationStatusProps)
       fallbackPlacements={['bottom', 'right', 'left']}
     >
       <div>
-        <Text muted size={fontSize} weight="semibold" style={{color: statusColor}}>
-          {createElement(statusIcon)}
+        <Text muted size={fontSize} weight="semibold" style={{color: statusColor()}}>
+          {statusIcon()}
         </Text>
       </div>
     </Tooltip>
@@ -74,14 +103,26 @@ export function FormFieldValidationStatus(props: FormFieldValidationStatusProps)
 
 function FormFieldValidationStatusItem(props: {item: FormFieldValidation}) {
   const {item} = props
-  const statusIcon = item.type === 'error' ? ErrorOutlineIcon : WarningOutlineIcon
-  const statusColor = item.type === 'error' ? hues.red[500].hex : hues.yellow[500].hex
+
+  const statusIcon = () => {
+    if (item.type === 'error') return VALIDATION_ICONS.error
+    if (item.type === 'warning') return VALIDATION_ICONS.warning
+    if (item.type === 'info') return VALIDATION_ICONS.info
+    return null
+  }
+
+  const statusColor = () => {
+    if (item.type === 'error') return VALIDATION_COLORS.error
+    if (item.type === 'warning') return VALIDATION_COLORS.warning
+    if (item.type === 'info') return VALIDATION_COLORS.info
+    return null
+  }
 
   return (
     <Flex>
       <Box marginRight={2}>
-        <Text size={1} style={{color: statusColor}}>
-          {createElement(statusIcon)}
+        <Text size={1} style={{color: statusColor()}}>
+          {statusIcon()}
         </Text>
       </Box>
       <Box flex={1}>
