2d097f7fe7b1e6b8388a76bff175e43cd2bf85ae
Bjørge Næss <bjoerge@gmail.com>
2021-12-07 12:24:00 +0100

    refactor(form-builder): make reference input responsive based on available width

--- packages/@sanity/form-builder/src/inputs/ReferenceInput/AutocompleteContainer.tsx
+++ packages/@sanity/form-builder/src/inputs/ReferenceInput/AutocompleteContainer.tsx
@@ -1,27 +1,43 @@
-import {Grid, Theme} from '@sanity/ui'
+import {Grid, useElementRect, useForwardedRef} from '@sanity/ui'
+import React, {ReactNode, ForwardedRef, forwardRef, useCallback, useState} from 'react'
 import styled, {css} from 'styled-components'
-import React, {ForwardedRef, forwardRef} from 'react'
-import type {ReactNode} from 'react'
 
-const Root = styled(Grid)((props: {theme: Theme}) => {
-  const {media} = props.theme.sanity
-  return css`
-    @media screen and (min-width: ${media[0]}px) {
-      grid-template-columns: repeat(auto-fill, minmax(100px, 1fr));
-    }
-    @media screen and (min-width: ${media[1]}px) {
-      grid-template-columns: 1fr min-content;
-    }
-  `
-})
+const NARROW_LAYOUT = css`
+  grid-template-columns: minmax(0px, 1fr);
+`
+
+const WIDE_LAYOUT = css`
+  grid-template-columns: 1fr min-content;
+`
+
+const Root = styled(Grid)<{$narrow: boolean}>((props: {$narrow: boolean}) =>
+  props.$narrow ? NARROW_LAYOUT : WIDE_LAYOUT
+)
+
 export const AutocompleteContainer = forwardRef(function AutocompleteContainer(
   props: {
     children: ReactNode
   },
-  ref: ForwardedRef<HTMLDivElement>
+  forwardedRef: ForwardedRef<HTMLDivElement>
 ) {
+  const ref = useForwardedRef(forwardedRef)
+
+  const [rootElement, setRootElement] = useState<HTMLDivElement>(ref.current)
+
+  const handleNewRef = useCallback(
+    (element: HTMLDivElement) => {
+      // there's a bit of "double bookkeeping" here, since useElementRef and useForwardedRef doesn't compose all that well
+      // (useElementRect requires the state to be updated whenever the element change)
+      ref.current = element
+      setRootElement(element)
+    },
+    [ref]
+  )
+
+  const inputWrapperRect = useElementRect(rootElement)
+
   return (
-    <Root gap={1} ref={ref}>
+    <Root ref={handleNewRef} gap={1} $narrow={inputWrapperRect?.width < 480}>
       {props.children}
     </Root>
   )
