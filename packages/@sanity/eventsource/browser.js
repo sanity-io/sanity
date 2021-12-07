2f089395af4a4c91e0d7a8ceb0d3bfe9755694c0
Espen Hovlandsdal <espen@hovlandsdal.com>
2021-12-07 14:23:32 +0100

    fix(eventsource): check for window before using it (#2982)

--- packages/@sanity/eventsource/browser.js
+++ packages/@sanity/eventsource/browser.js
@@ -1,4 +1,9 @@
 /* eslint-disable no-var */
 var evs = require('@rexxars/eventsource-polyfill')
 
-module.exports = window.EventSource || evs.EventSource
+module.exports =
+  typeof window === 'undefined' || !window.EventSource
+    ? // Use polyfill in non-browser/legacy environments
+      evs.EventSource
+    : // Use native EventSource when we can
+      window.EventSource
