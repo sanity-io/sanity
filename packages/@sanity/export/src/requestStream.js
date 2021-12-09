cf19cbeaf9a71ec72000540e10feaabc393dcee8
Espen Hovlandsdal <espen@hovlandsdal.com>
2021-12-10 00:00:26 +0100

    feat(export): use get-it for requests

--- packages/@sanity/export/src/requestStream.js
+++ packages/@sanity/export/src/requestStream.js
@@ -1,40 +1,13 @@
-const simpleGet = require('simple-get')
-const HttpAgent = require('agentkeepalive')
+const getIt = require('get-it')
+const {keepAlive, promise} = require('get-it/middleware')
 const debug = require('./debug')
 
-const HttpsAgent = HttpAgent.HttpsAgent
-const httpAgent = new HttpAgent()
-const httpsAgent = new HttpsAgent()
+const request = getIt([keepAlive(), promise({onlyBody: true})])
 const socketsWithTimeout = new WeakSet()
 
 const CONNECTION_TIMEOUT = 15 * 1000 // 15 seconds
 const READ_TIMEOUT = 3 * 60 * 1000 // 3 minutes
 const MAX_RETRIES = 5
-
-// Just a promisified simpleGet
-function getStream(options) {
-  return new Promise((resolve, reject) => {
-    let rejected = false
-    const openTimeout = setTimeout(() => {
-      rejected = true
-      reject(new Error(`Connection timed out after ${CONNECTION_TIMEOUT} ms`))
-    }, CONNECTION_TIMEOUT)
-
-    simpleGet(options, (err, res) => {
-      clearTimeout(openTimeout)
-      if (rejected) {
-        return
-      }
-
-      if (err) {
-        reject(err)
-        return
-      }
-
-      resolve(res)
-    })
-  })
-}
 
 function delay(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms))
@@ -42,12 +15,15 @@ function delay(ms) {
 
 /* eslint-disable no-await-in-loop, max-depth */
 module.exports = async (options) => {
-  const agent = options.url.startsWith('https:') ? httpsAgent : httpAgent
-  const reqOptions = {...options, followRedirects: false, agent}
   let error
   for (let i = 0; i < MAX_RETRIES; i++) {
     try {
-      const response = await getStream(reqOptions)
+      const response = await request({
+        ...options,
+        stream: true,
+        maxRedirects: 0,
+        timeout: {connect: CONNECTION_TIMEOUT, socket: READ_TIMEOUT},
+      })
 
       if (
         response.connection &&
@@ -66,7 +42,7 @@ module.exports = async (options) => {
     } catch (err) {
       error = err
 
-      if (err.statusCode && err.statusCode < 500) {
+      if (err.response && err.response.statusCode && err.response.statusCode < 500) {
         break
       }
 
