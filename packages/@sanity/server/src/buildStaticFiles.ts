759e0510916df3df9692233a76c3e15703e2a0d5
Espen Hovlandsdal <espen@hovlandsdal.com>
2021-12-13 15:26:23 +0100

    feat(core): copy files from source /static to output /static

--- packages/@sanity/server/src/buildStaticFiles.ts
+++ packages/@sanity/server/src/buildStaticFiles.ts
@@ -1,3 +1,4 @@
+import {promises as fs} from 'fs'
 import path from 'path'
 import {build} from 'vite'
 import {getViteConfig, SanityViteConfig} from './getViteConfig'
@@ -51,7 +52,8 @@ export async function buildStaticFiles(
 
   const bundle = await build(viteConfig)
 
-  // @todo copy files
+  // Copy files placed in /static to the built /static
+  await copyDir(path.join(cwd, 'static'), path.join(outputDir, 'static'))
 
   // For typescript only - this shouldn't ever be the case given we're not watching
   if (Array.isArray(bundle) || !('output' in bundle)) {
@@ -81,4 +83,22 @@ export async function buildStaticFiles(
   })
 
   return {chunks: stats}
+}
+
+async function copyDir(srcDir: string, destDir: string): Promise<void> {
+  await fs.mkdir(destDir, {recursive: true})
+  for (const file of await fs.readdir(srcDir)) {
+    const srcFile = path.resolve(srcDir, file)
+    if (srcFile === destDir) {
+      continue
+    }
+
+    const destFile = path.resolve(destDir, file)
+    const stat = await fs.stat(srcFile)
+    if (stat.isDirectory()) {
+      await copyDir(srcFile, destFile)
+    } else {
+      await fs.copyFile(srcFile, destFile)
+    }
+  }
 }
