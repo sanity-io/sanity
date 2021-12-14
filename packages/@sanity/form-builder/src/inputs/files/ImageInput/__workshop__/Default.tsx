65396c9632cb303f0350b7bb1219777e51591499
jorngeorg <968695+jorngeorg@users.noreply.github.com>
2021-12-14 16:55:39 +0100

    fix: update compact file

--- packages/@sanity/form-builder/src/inputs/files/ImageInput/__workshop__/Default.tsx
+++ packages/@sanity/form-builder/src/inputs/files/ImageInput/__workshop__/Default.tsx
@@ -1,4 +1,4 @@
-import {SearchIcon, ChevronDownIcon, FolderIcon, ImageIcon, UploadIcon} from '@sanity/icons'
+import {SearchIcon, ChevronDownIcon, FolderIcon, ImageIcon, UploadIcon, ReadOnlyIcon} from '@sanity/icons'
 import {Card, Stack, Text, Flex, Box, Button, Menu, MenuItem, MenuButton} from '@sanity/ui'
 import React from 'react'
 
@@ -8,25 +8,24 @@ export function Default(props) {
   return (
     <Card
       data-container
-      tone={drag ? 'primary' : 'default'}
+      tone={drag ? 'primary' : readOnly ? 'transparent' : 'default'}
       padding={4}
       border
       style={{borderStyle: drag ? 'solid' : 'dashed'}}
     >
       <Stack space={4}>
-        {!readOnly && (
-          <Flex justify="center">
-            <Text size={1} muted={!drag}>
-              <ImageIcon /> &nbsp;
-              {drag ? 'Drop image here' : 'Drag or paste image here'}
-            </Text>
-          </Flex>
-        )}
+        <Flex justify="center">
+          <Text size={1} muted={!drag}>
+            {readOnly ? <ReadOnlyIcon /> : <ImageIcon />} &nbsp;
+            {readOnly ? 'Read only' : drag ? 'Drop image here' : 'Drag or paste image here'}
+            {}
+          </Text>
+        </Flex>
 
         <Flex data-buttons gap={1} justify="center">
           {!assetSources && (
             <Button
-              text="Browse media"
+              text="Browse"
               mode="ghost"
               icon={SearchIcon}
               disabled={readOnly || drag}
@@ -40,7 +39,6 @@ export function Default(props) {
                 <Button
                   text="Browse"
                   icon={SearchIcon}
-                  iconRight={ChevronDownIcon}
                   mode="ghost"
                   disabled={readOnly || drag}
                   fontSize={2}
