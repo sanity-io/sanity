f15c9631fda50b0e0bd28c5767a6fc7de06b9762
jorngeorg <968695+jorngeorg@users.noreply.github.com>
2021-12-14 16:55:54 +0100

    fix: update compact file

--- packages/@sanity/form-builder/src/inputs/files/FileInput/__workshop__/compactFile.tsx
+++ packages/@sanity/form-builder/src/inputs/files/FileInput/__workshop__/compactFile.tsx
@@ -13,9 +13,12 @@ import {
   MenuItem,
   MenuDivider,
   Label,
+  Dialog,
+  TextInput,
 } from '@sanity/ui'
 import {
   BinaryDocumentIcon,
+  ClipboardIcon,
   DocumentIcon,
   DownloadIcon,
   EditIcon,
@@ -27,6 +30,7 @@ import {
 } from '@sanity/icons'
 import {useBoolean} from '@sanity/ui-workshop'
 import styled from 'styled-components'
+import {useState} from 'react'
 
 const Overlay = styled(Flex)`
   position: absolute;
@@ -38,6 +42,8 @@ const Overlay = styled(Flex)`
 
 export default function CompactFile() {
   const hasFile = useBoolean('File', false, 'Props')
+  const readOnly = useBoolean('Read only', false, 'Props')
+  const [dialogOpen, setDialogOpen] = useState(false)
 
   return (
     <Container width={[0, 1, 1]}>
@@ -45,12 +51,12 @@ export default function CompactFile() {
         {/* Alt 0. */}
         <Stack space={3} marginTop={6}>
           <Text weight="semibold" size={1}>
-            Alternative 0
+            File input
           </Text>
           <Card
             border
-            padding={3}
-            tone={hasFile ? 'transparent' : 'default'}
+            padding={hasFile ? 1 : 3}
+            tone={readOnly ? 'transparent' : 'default'}
             style={{
               position: 'relative',
               borderStyle: hasFile ? 'solid' : 'dashed',
@@ -79,50 +85,65 @@ export default function CompactFile() {
               </Flex>
             )}
             {hasFile && (
-              <Flex align="center" justify="space-between">
-                <Inline space={3} flex={1}>
-                  <Text muted>
-                    <BinaryDocumentIcon />
-                  </Text>
-                  <Text size={2} muted>
-                    document-file-name.pdf
-                  </Text>
-                </Inline>
-                <Box>
-                  <Inline space={2}>
-                    <Card>
-                      <Button mode="ghost" icon={EditIcon} />
-                    </Card>
-                    <Card>
-                      <MenuButton
-                        id="compact-file-menu-button"
-                        button={<Button icon={EllipsisVerticalIcon} mode="ghost" />}
-                        menu={
-                          <Menu>
-                            <Box padding={2}>
-                              <Label muted size={1}>
-                                Replace
-                              </Label>
-                            </Box>
-                            <MenuItem text="Select" icon={SearchIcon} />
-                            <MenuItem text="Upload" icon={UploadIcon} />
-                            <MenuDivider />
-                            <MenuItem text="Download file" icon={DownloadIcon} />
-                            <MenuDivider />
-                            <MenuItem text="Clear field" icon={ResetIcon} tone="critical" />
-                          </Menu>
-                        }
-                      />
-                    </Card>
-                  </Inline>
-                </Box>
+              <Flex align="center" justify="space-between" paddingRight={2}>
+                <Card flex={1} tone={readOnly ? 'transparent' : 'default'}>
+                  <Button
+                    mode="bleed"
+                    style={{width: '100%'}}
+                    padding={2}
+                    disabled={readOnly}
+                    onClick={() => setDialogOpen(true)}
+                  >
+                    <Inline space={3} flex={1}>
+                      <Card padding={3} tone="transparent" shadow={1} radius={1}>
+                        <Text muted={readOnly}>
+                          <BinaryDocumentIcon />
+                        </Text>
+                      </Card>
+                      <Stack space={2}>
+                        <Text size={2}>document-file-name.pdf</Text>
+                        <Text size={1} muted>
+                          2.3MB
+                        </Text>
+                      </Stack>
+                    </Inline>
+                  </Button>
+                </Card>
+                <Card tone={readOnly ? 'transparent' : 'default'}>
+                  <MenuButton
+                    id="compact-file-menu-button"
+                    button={<Button icon={EllipsisVerticalIcon} mode="bleed" padding={2} />}
+                    popover={{tone: 'default'}}
+                    menu={
+                      <Menu>
+                        <Box padding={2}>
+                          <Label muted size={1}>
+                            Replace
+                          </Label>
+                        </Box>
+                        <MenuItem text="Select" icon={SearchIcon} disabled={readOnly} />
+                        <MenuItem text="Upload" icon={UploadIcon} disabled={readOnly} />
+                        <MenuDivider />
+                        <MenuItem text="Download file" icon={DownloadIcon} />
+                        <MenuItem text="Copy URL" icon={ClipboardIcon} />
+                        <MenuDivider />
+                        <MenuItem
+                          text="Clear field"
+                          icon={ResetIcon}
+                          tone="critical"
+                          disabled={readOnly}
+                        />
+                      </Menu>
+                    }
+                  />
+                </Card>
               </Flex>
             )}
           </Card>
         </Stack>
 
         {/* Alt 1. */}
-        <Stack space={3} marginTop={6}>
+        {/* <Stack space={3} marginTop={6}>
           <Text weight="semibold" size={1}>
             Alternative 1
           </Text>
@@ -176,7 +197,7 @@ export default function CompactFile() {
                         <MenuDivider />
                         <MenuItem text="Download file" icon={DownloadIcon} />
                         <MenuDivider />
-                        <MenuItem text="Clear field" icon={ResetIcon} tone="critical" />
+                        <MenuItem text="Clear field" disabled icon={ResetIcon} tone="critical" />
                       </Menu>
                     }
                   />
@@ -184,10 +205,10 @@ export default function CompactFile() {
               </Flex>
             )}
           </Card>
-        </Stack>
+        </Stack> */}
 
         {/* Alt 2. */}
-        <Stack space={3} marginTop={6}>
+        {/* <Stack space={3} marginTop={6}>
           <Text weight="semibold" size={1}>
             Alternative 2
           </Text>
@@ -266,7 +287,7 @@ export default function CompactFile() {
               </Flex>
             )}
           </Card>
-        </Stack>
+        </Stack> */}
 
         {/* <Stack space={3} marginTop={6}>
           <Text weight="semibold" size={1}>
@@ -307,6 +328,16 @@ export default function CompactFile() {
           </Card>
         </Stack> */}
       </Stack>
+      {dialogOpen && (
+        <Dialog header="Edit" onClose={() => setDialogOpen(false)}>
+          <Box padding={4}>
+            <Stack space={2}>
+              <Text size={1} weight="semibold">Title</Text>
+              <TextInput />
+            </Stack>
+          </Box>
+        </Dialog>
+      )}
     </Container>
   )
 }
