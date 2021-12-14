53ae532baab600bd5b51ed13711a9f8c533f9712
Rita <rita@sanity.io>
2021-12-14 15:38:23 +0100

    refactor(form-builder): fix upload menu item hover

--- packages/@sanity/form-builder/src/inputs/files/common/FileInputButton/styles.ts
+++ packages/@sanity/form-builder/src/inputs/files/common/FileInputButton/styles.ts
@@ -1,38 +1,42 @@
-import {Button, Theme} from '@sanity/ui'
+import {Button, Theme, MenuItem} from '@sanity/ui'
 import styled, {css} from 'styled-components'
 import {focusRingStyle} from '../../../../components/withFocusRing/helpers'
 
-export const FileButton = styled(Button)(({theme, noBorder}: {theme: Theme; noBorder: boolean}) => {
+export const invisibleInput = css`
+  & input {
+    overflow: hidden;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    min-width: 0;
+    display: block;
+    appearance: none;
+    padding: 0;
+    margin: 0;
+    border: 0;
+    opacity: 0;
+  }
+`
+
+export const FileMenuItem = styled(MenuItem)`
+  position: relative;
+  ${invisibleInput}
+`
+
+export const FileButton = styled(Button)(({theme}: {theme: Theme}) => {
   const {focusRing} = theme.sanity
   const base = theme.sanity.color.base
   const border = {width: 1, color: 'var(--card-border-color)'}
 
   return css`
-    ${noBorder &&
-    css`
-      box-shadow: none;
-    `}
-
     &:not([data-disabled='true']) {
       &:focus-within {
         box-shadow: ${focusRingStyle({base, border, focusRing})};
       }
     }
 
-    & input {
-      overflow: hidden;
-      top: 0;
-      left: 0;
-      width: 100%;
-      height: 100%;
-      position: absolute;
-      min-width: 0;
-      display: block;
-      appearance: none;
-      padding: 0;
-      margin: 0;
-      border: 0;
-      opacity: 0;
-    }
+    ${invisibleInput}
   `
 })
