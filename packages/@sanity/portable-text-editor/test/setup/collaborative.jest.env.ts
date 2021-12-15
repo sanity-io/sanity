a2174c9be3ca074588e3620ce17bba539ebd7e2c
Per-Kristian Nordnes <per.kristian.nordnes@gmail.com>
2021-12-15 19:54:12 +0100

    test(portable-text-edior): increase timeouts as CI struggles with Win testing

--- packages/@sanity/portable-text-editor/test/setup/collaborative.jest.env.ts
+++ packages/@sanity/portable-text-editor/test/setup/collaborative.jest.env.ts
@@ -17,8 +17,8 @@ const WEB_SERVER_ROOT_URL = 'http://localhost:3000'
 const DEBUG = process.env.DEBUG || false
 
 // Wait this long for selections and a new doc revision to appear on the clients
-const SELECTION_TIMEOUT_MS = 150 // This will also be an indicator of the performance in the editor. Set it as low as possible without breaking the tests.
-const REVISION_TIMEOUT_MS = 800 // 800 seems to be the limit for the doc patching to go full circle (increase this if tests starts to time out)
+const SELECTION_TIMEOUT_MS = 200 // This will also be an indicator of the performance in the editor. Set it as low as possible without breaking the tests.
+const REVISION_TIMEOUT_MS = 1000 // 800 seems to be the limit for the doc patching to go full circle (increase this if tests starts to time out)
 
 let testId: string
 
