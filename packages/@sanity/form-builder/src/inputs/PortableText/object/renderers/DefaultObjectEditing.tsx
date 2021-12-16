586a9e0435f495ad26b7e5366e4c4b6255dd0773
Marius Lundgård <studio@mariuslundgard.com>
2021-12-16 16:51:13 +0100

    fix(form-builder): do not close edit dialogs on click-outside (PTE)

--- packages/@sanity/form-builder/src/inputs/PortableText/object/renderers/DefaultObjectEditing.tsx
+++ packages/@sanity/form-builder/src/inputs/PortableText/object/renderers/DefaultObjectEditing.tsx
@@ -73,7 +73,6 @@ export function DefaultObjectEditing(props: DefaultObjectEditingProps) {
     <Dialog
       id={dialogId || ''}
       onClose={onClose}
-      onClickOutside={onClose}
       header={type.title}
       portal="default"
       ref={setRootElement}
