8df6711b4b4320122928b3b2206a93d7c037a2b2
Rita <rita@sanity.io>
2021-12-20 12:27:01 +0100

    refactor(form-builder): update ui for linear progress

--- packages/@sanity/form-builder/src/components/progress/LinearProgress.tsx
+++ packages/@sanity/form-builder/src/components/progress/LinearProgress.tsx
@@ -1,24 +1,19 @@
 import {hues} from '@sanity/color'
-import {Theme} from '@sanity/ui'
+import {Theme, Card} from '@sanity/ui'
 import React from 'react'
 import styled, {css} from 'styled-components'
 
-const STROKE_WIDTH = 3
+const STROKE_WIDTH = 0.5
 
-const Root = styled.div(({theme}: {theme: Theme}) => {
+const Root = styled(Card)`
+  overflow: hidden;
+`
+
+const Bar = styled(Card)(({theme}: {theme: Theme}) => {
   const {color} = theme.sanity
 
   return css`
-    overflow: hidden;
-    background-color: ${hues.gray[color.dark ? 900 : 100].hex};
-  `
-})
-
-const Bar = styled.div(({theme}: {theme: Theme}) => {
-  const {color} = theme.sanity
-
-  return css`
-    height: ${STROKE_WIDTH}px;
+    height: ${STROKE_WIDTH}rem;
     background: ${hues.blue[color.dark ? 400 : 500].hex};
     transition: transform 75ms;
   `
@@ -28,8 +23,8 @@ export function LinearProgress(props: {value: number}) {
   const {value} = props
 
   return (
-    <Root>
-      <Bar style={{transform: `translate3d(${value - 100}%, 0, 0)`}} />
+    <Root radius={5}>
+      <Bar radius={5} style={{transform: `translate3d(${value - 100}%, 0, 0)`}} />
     </Root>
   )
 }
