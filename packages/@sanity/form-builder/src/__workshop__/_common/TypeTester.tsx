b6b0427e0664106a9a75078494f3e3894365d2a9
Fred Carlsen <fred@sjelfull.no>
2021-12-20 15:19:22 +0100

    feat(form-builder): list focused input and classes

--- packages/@sanity/form-builder/src/__workshop__/_common/TypeTester.tsx
+++ packages/@sanity/form-builder/src/__workshop__/_common/TypeTester.tsx
@@ -1,21 +1,51 @@
 import React, {useCallback, useEffect, useRef, useState, useMemo} from 'react'
-import {Button, Card, Label, Stack, TextArea} from '@sanity/ui'
+import {Button, Card, Label, Stack, Text, TextArea} from '@sanity/ui'
+import styled, {css, createGlobalStyle} from 'styled-components'
 import {runTest} from './typer'
 
+const HIGHLIGHT_CLASSNAME = 'js-highlight-selected-input'
+
+const GlobalStyle = createGlobalStyle`
+  .${HIGHLIGHT_CLASSNAME} {
+    outline: 2px solid red;
+  }
+`
+
+function getNameFromInput(input: HTMLInputElement | HTMLTextAreaElement) {
+  const classNames = input.classList
+    .toString()
+    .split(' ')
+    .filter((name) => name !== HIGHLIGHT_CLASSNAME)
+    .join('.')
+  return `${input.tagName.toLowerCase()}.${classNames}`
+}
+
+const Sticky = styled.div`
+  position: sticky;
+  top: 1rem;
+`
+
 export function TypeTester() {
+  const textAreaRef = useRef()
   const [focusedInput, setFocusedInput] = useState<HTMLInputElement | HTMLTextAreaElement>()
   const [testOutput, setTestOutput] = useState('')
   const [isRunning, setIsRunning] = useState(false)
   const cancelTypeTester = useRef<() => boolean>(() => true)
 
-  const handleFocus = useCallback((e: FocusEvent) => {
-    if (
-      (e.type === 'focusin' && e.target instanceof HTMLInputElement) ||
-      e.target instanceof HTMLTextAreaElement
-    ) {
-      setFocusedInput(e.target)
-    }
-  }, [])
+  const handleFocus = useCallback(
+    (e: FocusEvent) => {
+      if (textAreaRef.current && e.target === textAreaRef.current) {
+        return
+      }
+      if (
+        (e.type === 'focusin' && e.target instanceof HTMLInputElement) ||
+        e.target instanceof HTMLTextAreaElement
+      ) {
+        setFocusedInput(e.target)
+      }
+    },
+    [textAreaRef]
+  )
 
   useEffect(() => {
     document.addEventListener('focusin', handleFocus)
@@ -43,17 +73,54 @@ export function TypeTester() {
     })
   }, [focusedInput])
 
+  const handleHighlightMouseOver = useCallback(
+    (event) => {
+      if (event.type === 'mouseover') {
+        focusedInput.classList.add(HIGHLIGHT_CLASSNAME)
+      }
+      if (event.type === 'mouseout') {
+        focusedInput.classList.remove(HIGHLIGHT_CLASSNAME)
+      }
+    },
+    [focusedInput]
+  )
+
+  const handleHighlightClick = useCallback(() => {
+    if (focusedInput) {
+      focusedInput.focus()
+    }
+  }, [focusedInput])
+
   const isDisabled = useMemo(() => {
     return isRunning || !focusedInput || !['INPUT', 'TEXTAREA'].includes(focusedInput.tagName)
   }, [focusedInput, isRunning])
 
   return (
-    <Card padding={4} tone="default" border>
-      <Stack space={4}>
-        <Label size={0}>Type Performance Tester</Label>
-        <TextArea readOnly rows={6} value={testOutput} />
-        <Button text="Run test" onClick={handleRunTest} disabled={isDisabled} loading={isRunning} />
-      </Stack>
-    </Card>
+    <Sticky>
+      <Card padding={4} tone="default" border>
+        <Stack space={4}>
+          <Label size={0}>Type Performance Tester</Label>
+          <Text
+            title={isDisabled ? undefined : `Click to focus input`}
+            size={1}
+            onClick={handleHighlightClick}
+            onMouseOver={handleHighlightMouseOver}
+            onMouseOut={handleHighlightMouseOver}
+          >
+            {isDisabled && !isRunning
+              ? 'No input selected'
+              : `Selected input: ${getNameFromInput(focusedInput)}`}
+          </Text>
+          <TextArea ref={textAreaRef} readOnly rows={6} value={testOutput} />
+          <Button
+            text="Run test"
+            onClick={handleRunTest}
+            disabled={isDisabled}
+            loading={isRunning}
+          />
+        </Stack>
+      </Card>
+      <GlobalStyle />
+    </Sticky>
   )
 }
