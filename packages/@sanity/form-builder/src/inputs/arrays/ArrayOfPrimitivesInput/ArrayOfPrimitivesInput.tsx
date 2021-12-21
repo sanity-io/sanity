c156a4124981a01d9ccc5a9f913c25d496e37394
Bjørge Næss <bjoerge@gmail.com>
2021-12-21 13:57:22 +0100

    feat(form-builder): add insert menu to array of primitive values

--- packages/@sanity/form-builder/src/inputs/arrays/ArrayOfPrimitivesInput/ArrayOfPrimitivesInput.tsx
+++ packages/@sanity/form-builder/src/inputs/arrays/ArrayOfPrimitivesInput/ArrayOfPrimitivesInput.tsx
@@ -12,8 +12,7 @@ import ArrayFunctions from '../common/ArrayFunctions'
 import {ConditionalReadOnlyField} from '../../common'
 import getEmptyValue from './getEmptyValue'
 import {ItemRow} from './ItemRow'
-
-type Primitive = string | number | boolean
+import {PrimitiveValue} from './types'
 
 const NO_MARKERS: Marker[] = []
 
@@ -25,16 +24,25 @@ function move<T>(arr: T[], from: number, to: number): T[] {
   return copy
 }
 
-function insertAt<T>(arr: T[], index: number, item: T): T[] {
+/**
+ * @param index index to insert item after. An index of -1 will prepend the item
+ * @param arr the array to insert the item into
+ * @param item the item to insert
+ *
+ * example:
+ *  insertAfter(-1, ["one", "two"], "hello")
+ *  => ["hello", "one", "two"]
+ */
+function insertAfter<T>(index: number, arr: T[], item: T): T[] {
   const copy = arr.slice()
   copy.splice(index + 1, 0, item)
   return copy
 }
 
 export interface Props {
-  type: ArraySchemaType<Primitive>
-  value: Primitive[]
-  compareValue?: Primitive[]
+  type: ArraySchemaType<PrimitiveValue>
+  value: PrimitiveValue[]
+  compareValue?: PrimitiveValue[]
   level: number
   onChange: (event: PatchEvent) => void
   onFocus: (path: Path) => void
@@ -51,7 +59,7 @@ export class ArrayOfPrimitivesInput extends React.PureComponent<Props> {
   _element: Focusable | null = null
   _lastAddedIndex = -1
 
-  set(nextValue: Primitive[]) {
+  set(nextValue: PrimitiveValue[]) {
     this._lastAddedIndex = -1
     const patch = nextValue.length === 0 ? unset() : set(nextValue)
     this.props.onChange(PatchEvent.from(patch))
@@ -63,30 +71,37 @@ export class ArrayOfPrimitivesInput extends React.PureComponent<Props> {
     this.props.onFocus([Math.max(0, index - 1)])
   }
 
-  handleAppend = (itemValue: Primitive) => {
+  handleAppend = (itemValue: PrimitiveValue) => {
     const {value = [], onFocus} = this.props
     this.set(value.concat(itemValue))
     onFocus([value.length])
   }
 
-  handlePrepend = (itemValue: Primitive) => {
+  handlePrepend = (itemValue: PrimitiveValue) => {
     const {value = [], onFocus} = this.props
     this.set([itemValue].concat(value))
     onFocus([value.length])
   }
 
-  insertAt(index: number, type: SchemaType) {
+  insertAfter(index: number, type: SchemaType) {
     const {value = [], onFocus} = this.props
     const emptyValue = getEmptyValue(type)
     if (emptyValue === undefined) {
       throw new Error(`Cannot create empty primitive value from ${type.name}`)
     }
-    this.set(insertAt(value, index, emptyValue))
+    this.set(insertAfter(index, value, emptyValue))
     onFocus([index + 1])
   }
 
   handleRemoveItem = (index: number) => {
     this.removeAt(index)
+  }
+
+  handleInsert = (pos: 'before' | 'after', index: number, item: PrimitiveValue) => {
+    const {value = [], onFocus} = this.props
+    const insertIndex = index + (pos === 'before' ? -1 : 0)
+    this.set(insertAfter(insertIndex, value, item))
+    onFocus([insertIndex + 1])
   }
 
   handleItemChange = (event: PatchEvent) => {
@@ -97,7 +112,7 @@ export class ArrayOfPrimitivesInput extends React.PureComponent<Props> {
   handleItemEnterKey = (index: number) => {
     const firstType = this.props.type?.of[0]
     if (firstType) {
-      this.insertAt(index, firstType)
+      this.insertAfter(index, firstType)
       this._lastAddedIndex = index + 1
     }
   }
@@ -110,9 +125,10 @@ export class ArrayOfPrimitivesInput extends React.PureComponent<Props> {
   }
 
   handleSortEnd = (event: {oldIndex: number; newIndex: number}) => {
-    const {value} = this.props
+    const {value, onFocus} = this.props
     const {oldIndex, newIndex} = event
     this.set(move(value, oldIndex, newIndex))
+    onFocus([newIndex])
   }
 
   getMemberType(typeName: string) {
@@ -132,11 +148,17 @@ export class ArrayOfPrimitivesInput extends React.PureComponent<Props> {
     }
   }
 
-  handleFocusRoot = () => {
-    this.props.onFocus([])
+  handleFocusRoot = (event) => {
+    // We want to handle focus when the array input *itself* element receives
+    // focus, not when a child element receives focus, but React has decided
+    // to let focus bubble, so this workaround is needed
+    // Background: https://github.com/facebook/react/issues/6410#issuecomment-671915381
+    if (event.currentTarget === event.target && event.currentTarget === this._element) {
+      this.props.onFocus([])
+    }
   }
 
-  handleFocusItem = (item: Primitive, index: number) => {
+  handleFocusItem = (item: PrimitiveValue, index: number) => {
     this.props.onFocus([index])
   }
 
@@ -184,10 +206,15 @@ export class ArrayOfPrimitivesInput extends React.PureComponent<Props> {
                     const childPresence = presence.filter((pItem) =>
                       startsWith([index], pItem.path)
                     )
+                    const memberType = this.getMemberType(resolveTypeName(item))
 
+                    // Best effort attempt to make a stable key for each item in the array
+                    // Since items may be reordered and change at any time, there's no way to reliably address each item uniquely
+                    // This is a "best effort"-attempt at making sure we don't re-use internal state for item inputs
+                    // when items gets added or removed to the array
+                    const key = memberType.name + String(index) + String(value.length)
                     return (
-                      // eslint-disable-next-line react/no-array-index-key
-                      <Item key={index} index={index} isSortable={isSortable}>
+                      <Item key={key} index={index} isSortable={isSortable}>
                         <ConditionalReadOnlyField
                           readOnly={readOnly || this.getMemberType(resolveTypeName(item))?.readOnly}
                           value={item}
@@ -201,13 +228,15 @@ export class ArrayOfPrimitivesInput extends React.PureComponent<Props> {
                             readOnly={readOnly}
                             markers={filteredMarkers.length === 0 ? NO_MARKERS : filteredMarkers}
                             isSortable={isSortable}
-                            type={this.getMemberType(resolveTypeName(item))}
+                            type={memberType}
                             focusPath={focusPath}
                             onFocus={onFocus}
                             onBlur={onBlur}
+                            insertableTypes={type.of}
                             onEnterKey={this.handleItemEnterKey}
                             onEscapeKey={this.handleItemEscapeKey}
                             onChange={this.handleItemChange}
+                            onInsert={this.handleInsert}
                             onRemove={this.handleRemoveItem}
                             presence={childPresence}
                           />
@@ -219,7 +248,7 @@ export class ArrayOfPrimitivesInput extends React.PureComponent<Props> {
               </Card>
             )}
           </Stack>
-          <ArrayFunctionsImpl<Primitive>
+          <ArrayFunctionsImpl<PrimitiveValue>
             type={type}
             value={value}
             readOnly={readOnly}
