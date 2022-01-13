95494bfa4f381ba11170c21cd40dd3703d9fe60f
Per-Kristian Nordnes <per.kristian.nordnes@gmail.com>
2022-01-13 12:53:43 +0100

    feature(portable-text-editor): put basic plainText and HTML on clipboard

--- packages/@sanity/portable-text-editor/src/editor/plugins/createWithInsertData.ts
+++ packages/@sanity/portable-text-editor/src/editor/plugins/createWithInsertData.ts
@@ -1,6 +1,7 @@
-import {Node, Element, Transforms, Editor, Descendant} from 'slate'
+import {Node, Element, Transforms, Editor, Descendant, Range} from 'slate'
 import {htmlToBlocks, normalizeBlock} from '@sanity/block-tools'
-import {PortableTextFeatures, PortableTextBlock} from '../../types/portableText'
+import {ReactEditor} from '@sanity/slate-react'
+import {PortableTextFeatures, PortableTextBlock, PortableTextChild} from '../../types/portableText'
 import {EditorChanges, PortableTextSlateEditor} from '../../types/editor'
 import {fromSlateValue, toSlateValue} from '../../utils/values'
 import {validateValue} from '../../utils/validateValue'
@@ -21,16 +22,100 @@ export function createWithInsertData(
     const blockTypeName = portableTextFeatures.types.block.name
     const spanTypeName = portableTextFeatures.types.span.name
 
-    editor.setFragmentData = (data: DataTransfer) => {
-      if (editor.selection) {
-        debug('Set fragment data')
-        const fragment = Node.fragment(editor, editor.selection)
-        const portableText = fromSlateValue(fragment, portableTextFeatures.types.block.name)
-        const asJSON = JSON.stringify(portableText)
-        data.clearData()
-        data.setData('application/x-portable-text', asJSON)
-        data.setData('application/json', asJSON)
+    const toPlainText = (blocks: PortableTextBlock[]) => {
+      return blocks
+        .map((block) => {
+          if (block._type === blockTypeName) {
+            return block.children
+              .map((child: PortableTextChild) => {
+                if (child._type === spanTypeName) {
+                  return child.text
+                }
+                return `[${
+                  portableTextFeatures.types.inlineObjects.find((t) => t.name === child._type)
+                    ?.title || 'Object'
+                }]`
+              })
+              .join('')
+          }
+          return `[${
+            portableTextFeatures.types.blockObjects.find((t) => t.name === block._type)?.title ||
+            'Object'
+          }]`
+        })
+        .join('\n\n')
+    }
+
+    editor.setFragmentData = (data: DataTransfer, originEvent) => {
+      const {selection} = editor
+
+      if (!selection) {
+        return
       }
+
+      const [start, end] = Range.edges(selection)
+      const startVoid = Editor.void(editor, {at: start.path})
+      const endVoid = Editor.void(editor, {at: end.path})
+
+      if (Range.isCollapsed(selection) && !startVoid) {
+        return
+      }
+
+      // Create a fake selection so that we can add a Base64-encoded copy of the
+      // fragment to the HTML, to decode on future pastes.
+      const domRange = ReactEditor.toDOMRange(editor, selection)
+      let contents = domRange.cloneContents()
+      // COMPAT: If the end node is a void node, we need to move the end of the
+      // range from the void node's spacer span, to the end of the void node's
+      // content, since the spacer is before void's content in the DOM.
+      if (endVoid) {
+        const [voidNode] = endVoid
+        const r = domRange.cloneRange()
+        const domNode = ReactEditor.toDOMNode(editor, voidNode)
+        r.setEndAfter(domNode)
+        contents = r.cloneContents()
+      }
+      // Remove any zero-width space spans from the cloned DOM so that they don't
+      // show up elsewhere when pasted.
+      Array.from(contents.querySelectorAll('[data-slate-zero-width]')).forEach((zw) => {
+        const isNewline = zw.getAttribute('data-slate-zero-width') === 'n'
+        zw.textContent = isNewline ? '\n' : ''
+      })
+      // Clean up the clipboard HTML for editor spesific attributes
+      Array.from(contents.querySelectorAll('*')).forEach((elm) => {
+        elm.removeAttribute('contentEditable')
+        elm.removeAttribute('data-slate-inline')
+        elm.removeAttribute('data-slate-leaf')
+        elm.removeAttribute('data-slate-node')
+        elm.removeAttribute('data-slate-spacer')
+        elm.removeAttribute('data-slate-string')
+        elm.removeAttribute('data-slate-zero-width')
+        elm.removeAttribute('draggable')
+        for (const key in elm.attributes) {
+          if (elm.hasAttribute(key)) {
+            elm.removeAttribute(key)
+          }
+        }
+      })
+      const div = contents.ownerDocument.createElement('div')
+      div.appendChild(contents)
+      div.setAttribute('hidden', 'true')
+      contents.ownerDocument.body.appendChild(div)
+      const asHTML = div.innerHTML
+      contents.ownerDocument.body.removeChild(div)
+      const fragment = editor.getFragment()
+      const portableText = fromSlateValue(fragment as Node[], portableTextFeatures.types.block.name)
+
+      const asJSON = JSON.stringify(portableText)
+      const asPlainText = toPlainText(portableText)
+      data.clearData()
+      data.setData('text/plain', asPlainText)
+      data.setData('text/html', asHTML)
+      data.setData('application/json', asJSON)
+      data.setData('application/x-portable-text', asJSON)
+      debug('text', asPlainText)
+      data.setData('application/x-portable-text-event-origin', originEvent || 'external')
+      debug('Set fragment data', asJSON, asHTML)
     }
 
     editor.insertPortableTextData = (data: DataTransfer): boolean => {
@@ -38,7 +123,8 @@ export function createWithInsertData(
         return false
       }
       const pText = data.getData('application/x-portable-text')
-      debug('Inserting portable text from clipboard', pText)
+      const origin = data.getData('application/x-portable-text-event-origin')
+      debug(`Inserting portable text from ${origin} event`, pText)
       if (pText) {
         const parsed = JSON.parse(pText) as PortableTextBlock[]
         if (Array.isArray(parsed) && parsed.length > 0) {
@@ -48,7 +134,6 @@ export function createWithInsertData(
             keyGenerator,
             spanTypeName
           )
-          debug('Inserting portable text from clipboard', slateValue)
           // Validate the result
           const validation = validateValue(parsed, portableTextFeatures, keyGenerator)
           // Bail out if it's not valid
