f232b920839e8b8a36dea71cbd74016f38fceeb9
Marius Lundgård <studio@mariuslundgard.com>
2022-01-14 16:42:56 +0100

    feat(desk-tool): expose internal for debugging

--- packages/@sanity/desk-tool/src/_exports/_internal.ts
+++ packages/@sanity/desk-tool/src/_exports/_internal.ts
@@ -2,3 +2,8 @@ export type {RouterPanes, UnresolvedPaneNode} from '../types'
 
 export {useResolvedPanes} from '../structure/useResolvedPanes'
 export {validateStructure} from '../structure/validateStructure'
+
+// @todo: move to main export?
+export {DeskToolProvider} from '../contexts/deskTool'
+export {DocumentPaneProvider} from '../panes/document/DocumentPaneProvider'
+export type {DocumentPaneNode} from '../types'
