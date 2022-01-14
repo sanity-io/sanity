e8a23bbfb9e0a744ef021475f32b29a04725dfee
Per-Kristian Nordnes <per.kristian.nordnes@gmail.com>
2022-01-14 20:08:21 +0100

    test(portable-text-editor): add test for marks normalization

--- packages/@sanity/portable-text-editor/src/editor/plugins/__tests__/withPortableTextMarkModelNormalization.test.tsx
+++ packages/@sanity/portable-text-editor/src/editor/plugins/__tests__/withPortableTextMarkModelNormalization.test.tsx
@@ -259,4 +259,141 @@ describe('plugin:withPortableTextMarksModel: normalization', () => {
       ]
     `)
   })
+  it('merges children correctly when toggling marks in various ranges', () => {
+    const editorRef: React.RefObject<PortableTextEditor> = React.createRef()
+    const initialValue = [
+      {
+        _key: 'a',
+        _type: 'myTestBlockType',
+        children: [
+          {
+            _key: 'a1',
+            _type: 'span',
+            marks: [],
+            text: '1234',
+          },
+        ],
+        markDefs: [],
+        style: 'normal',
+      },
+    ]
+    const onChange = jest.fn()
+    act(() => {
+      render(
+        <PortableTextEditorTester
+          onChange={onChange}
+          ref={editorRef}
+          type={type}
+          value={initialValue}
+        />
+      )
+    })
+    if (!editorRef.current) {
+      throw new Error('No editor')
+    }
+    act(() => {
+      if (editorRef.current) {
+        PortableTextEditor.focus(editorRef.current)
+        PortableTextEditor.select(editorRef.current, {
+          focus: {path: [{_key: 'a'}, 'children', {_key: 'a1'}], offset: 0},
+          anchor: {path: [{_key: 'a'}, 'children', {_key: 'a1'}], offset: 4},
+        })
+      }
+    })
+    act(() => {
+      if (editorRef.current) {
+        PortableTextEditor.toggleMark(editorRef.current, 'bold')
+      }
+    })
+    expect(PortableTextEditor.getValue(editorRef.current)).toMatchInlineSnapshot(`
+      Array [
+        Object {
+          "_key": "a",
+          "_type": "myTestBlockType",
+          "children": Array [
+            Object {
+              "_key": "a1",
+              "_type": "span",
+              "marks": Array [
+                "bold",
+              ],
+              "text": "1234",
+            },
+          ],
+          "markDefs": Array [],
+          "style": "normal",
+        },
+      ]
+    `)
+    act(() => {
+      if (editorRef.current) {
+        PortableTextEditor.select(editorRef.current, {
+          focus: {path: [{_key: 'a'}, 'children', {_key: 'a1'}], offset: 1},
+          anchor: {path: [{_key: 'a'}, 'children', {_key: 'a1'}], offset: 3},
+        })
+        PortableTextEditor.toggleMark(editorRef.current, 'bold')
+      }
+    })
+    expect(PortableTextEditor.getValue(editorRef.current)).toMatchInlineSnapshot(`
+      Array [
+        Object {
+          "_key": "a",
+          "_type": "myTestBlockType",
+          "children": Array [
+            Object {
+              "_key": "a1",
+              "_type": "span",
+              "marks": Array [
+                "bold",
+              ],
+              "text": "1",
+            },
+            Object {
+              "_key": "2",
+              "_type": "span",
+              "marks": Array [],
+              "text": "23",
+            },
+            Object {
+              "_key": "1",
+              "_type": "span",
+              "marks": Array [
+                "bold",
+              ],
+              "text": "4",
+            },
+          ],
+          "markDefs": Array [],
+          "style": "normal",
+        },
+      ]
+    `)
+    act(() => {
+      if (editorRef.current) {
+        PortableTextEditor.select(editorRef.current, {
+          focus: {path: [{_key: 'a'}, 'children', {_key: 'a1'}], offset: 0},
+          anchor: {path: [{_key: 'a'}, 'children', {_key: '1'}], offset: 1},
+        })
+        PortableTextEditor.toggleMark(editorRef.current, 'bold')
+      }
+    })
+    expect(PortableTextEditor.getValue(editorRef.current)).toMatchInlineSnapshot(`
+      Array [
+        Object {
+          "_key": "a",
+          "_type": "myTestBlockType",
+          "children": Array [
+            Object {
+              "_key": "a1",
+              "_type": "span",
+              "marks": Array [],
+              "text": "1234",
+            },
+          ],
+          "markDefs": Array [],
+          "style": "normal",
+        },
+      ]
+    `)
+  })
 })
