8fb6d3c53ccd1cbd5c87ac5da0d0691b0d489182
Marius Lundgård <studio@mariuslundgard.com>
2022-01-18 12:12:49 +0100

    refactor(base): simplify preview constants

--- packages/@sanity/base/src/preview/constants.tsx
+++ packages/@sanity/base/src/preview/constants.tsx
@@ -1,8 +1,6 @@
-import React from 'react'
-import styled from 'styled-components'
-import {Flex, Text} from '@sanity/ui'
 import {WarningOutlineIcon} from '@sanity/icons'
 import {PreviewValue} from '@sanity/types'
+import React from 'react'
 import {AvailabilityReason} from './types'
 
 export const INCLUDE_FIELDS_QUERY = ['_id', '_rev', '_type']
@@ -23,28 +21,8 @@ export const AVAILABILITY_NOT_FOUND = {
   reason: AvailabilityReason.NOT_FOUND,
 } as const
 
-// NOTE: have to use color inherit to make it work correctly with the
-// `isSelected` state in the list pane
-const IconSizer = styled(Text)`
-  color: inherit;
-`
-
-function IconWrapper({children}: {children: React.ReactNode}) {
-  return (
-    <Flex>
-      <IconSizer size={3}>{children}</IconSizer>
-    </Flex>
-  )
-}
-
 export const INVALID_PREVIEW_FALLBACK: PreviewValue = {
-  // The `<small>` element is used for more compatibility
-  // with the different downstream preview components.
-  title: <small>Invalid preview config</small>,
-  subtitle: <small>Check the error log in the console</small>,
-  media: (
-    <IconWrapper>
-      <WarningOutlineIcon />
-    </IconWrapper>
-  ),
+  title: 'Invalid preview config',
+  subtitle: 'Check the error log in the console',
+  media: <WarningOutlineIcon />,
 }
