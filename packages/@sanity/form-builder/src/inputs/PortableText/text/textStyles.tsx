44bd011feda704660b1e36f9e0c34a266b49939e
Per-Kristian Nordnes <per.kristian.nordnes@gmail.com>
2022-01-18 19:34:15 +0100

    fix(form-builder): fix styled components syntax

--- packages/@sanity/form-builder/src/inputs/PortableText/text/textStyles.tsx
+++ packages/@sanity/form-builder/src/inputs/PortableText/text/textStyles.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 import styled from 'styled-components'
 
 // Without this container, editing with Android breaks due to how Text is styled via responsiveFont in @sanity/ui
-const TextContainer = styled('div')`
+const TextContainer = styled.div`
   display: block;
 `
 
