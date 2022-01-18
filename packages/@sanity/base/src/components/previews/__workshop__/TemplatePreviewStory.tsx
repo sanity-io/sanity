6d87ae10e0da2983aaaeb1c05bcf4cb11caad6fb
Marius Lundgård <studio@mariuslundgard.com>
2022-01-18 16:13:35 +0100

    test(base): rename to `TemplatePreview`

--- packages/@sanity/base/src/components/previews/__workshop__/TemplatePreviewStory.tsx
+++ packages/@sanity/base/src/components/previews/__workshop__/TemplatePreviewStory.tsx
@@ -1,10 +1,8 @@
 import {DocumentIcon} from '@sanity/icons'
 import {Card, Container, Flex, Text} from '@sanity/ui'
-import {useAction, useBoolean, useSelect, useString, useText} from '@sanity/ui-workshop'
-import {omit} from 'lodash'
-import React, {createElement, useMemo} from 'react'
-import {CreateDocumentPreview, CreateDocumentPreviewProps} from '../template/CreateDocumentPreview'
-import {route, RouterProvider} from '../../../router'
+import {useBoolean, useSelect, useString, useText} from '@sanity/ui-workshop'
+import React, {createElement} from 'react'
+import {TemplatePreview, TemplatePreviewProps} from '../template/TemplatePreview'
 
 type LayoutKey = 'createDocument'
 
@@ -23,16 +21,8 @@ const mediaValues: Record<string, React.ReactNode> = {
   icon: <DocumentIcon />,
 }
 
-const previewComponents: Record<LayoutKey, React.ComponentType<CreateDocumentPreviewProps>> = {
-  createDocument: CreateDocumentPreview,
-}
-
-function decodeParams(str: string) {
-  return {...JSON.parse(str), intent: 'create'}
-}
-
-function encodeParams(obj: Record<string, unknown>) {
-  return JSON.stringify(omit(obj, 'intent'))
+const previewComponents: Record<LayoutKey, React.ComponentType<TemplatePreviewProps>> = {
+  createDocument: TemplatePreview,
 }
 
 export default function TemplatePreviewStory() {
@@ -45,21 +35,7 @@ export default function TemplatePreviewStory() {
   const description = useText('Description', undefined, 'Props')
 
   const media = mediaValues[mediaKey] || false
-  const params = useMemo(() => ({intent: 'create' as const, type: 'book'}), [])
   const component = previewComponents[layout]
-
-  const router = useMemo(
-    () =>
-      route('/:intent', [
-        route(':params', {transform: {params: {toState: decodeParams, toPath: encodeParams}}}, [
-          route(':payload', {transform: {payload: {toState: decodeParams, toPath: encodeParams}}}),
-        ]),
-      ]),
-    []
-  )
-
-  const routerState = useMemo(() => ({}), [])
-  const handleNavigate = useAction('onNavigate')
 
   if (!component) {
     return (
@@ -70,23 +46,20 @@ export default function TemplatePreviewStory() {
   }
 
   return (
-    <RouterProvider onNavigate={handleNavigate} router={router} state={routerState}>
-      <Card height="fill" tone="transparent">
-        <Flex align="center" height="fill" justify="center" padding={4} sizing="border">
-          <Container width={0}>
-            <Card padding={2} radius={2}>
-              {createElement(component, {
-                description,
-                isPlaceholder,
-                media,
-                params,
-                title,
-                subtitle,
-              })}
-            </Card>
-          </Container>
-        </Flex>
-      </Card>
-    </RouterProvider>
+    <Card height="fill" tone="transparent">
+      <Flex align="center" height="fill" justify="center" padding={4} sizing="border">
+        <Container width={0}>
+          <Card padding={3} radius={2}>
+            {createElement(component, {
+              description,
+              isPlaceholder,
+              media,
+              title,
+              subtitle,
+            })}
+          </Card>
+        </Container>
+      </Flex>
+    </Card>
   )
 }
