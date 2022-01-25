5140dce5e5b7e15fe20a369db3a40459e2aa09a9
Rita <rita@sanity.io>
2022-01-25 12:04:47 +0100

    refactor(form-builder): add loading for in between state for fetching the referenced asset

--- packages/@sanity/form-builder/src/utils/WithReferencedAsset.tsx
+++ packages/@sanity/form-builder/src/utils/WithReferencedAsset.tsx
@@ -2,6 +2,7 @@ import React, {ReactNode} from 'react'
 import {Observable} from 'rxjs'
 import {Reference} from '@sanity/types'
 import {useMemoObservable} from 'react-rx'
+import {Box, Spinner} from '@sanity/ui'
 
 interface Props<AssetDoc> {
   reference: Reference
@@ -13,5 +14,15 @@ export function WithReferencedAsset<Asset>(props: Props<Asset>) {
   const {reference, children, observeAsset} = props
   const documentId = reference?._ref
   const asset = useMemoObservable(() => observeAsset(documentId), [documentId])
-  return <>{documentId && asset ? children(asset) : null}</>
+  return (
+    <>
+      {documentId && asset ? (
+        children(asset)
+      ) : (
+        <Box padding={2}>
+          <Spinner muted />
+        </Box>
+      )}
+    </>
+  )
 }
