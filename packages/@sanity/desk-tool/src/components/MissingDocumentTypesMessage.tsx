d78dacae46986f97f6983e2177abb6c0d8dd99ea
Herman Wikner <wiknerherman@gmail.com>
2022-01-28 10:27:16 +0100

    feat(desk-tool): improve `MissingDocumentTypesMessage` design

--- packages/@sanity/desk-tool/src/components/MissingDocumentTypesMessage.tsx
+++ packages/@sanity/desk-tool/src/components/MissingDocumentTypesMessage.tsx
@@ -1,23 +1,28 @@
-import {Heading, Stack, Text} from '@sanity/ui'
 import React from 'react'
+import {Card, Container, Heading, Stack, Text} from '@sanity/ui'
 
 export function MissingDocumentTypesMessage() {
   return (
-    <Stack space={4}>
-      <Heading as="h2">Empty schema</Heading>
-      <Text as="p">
-        Your schema does not contain any document types. If it did, those types would be listed
-        here.{' '}
-        <a
-          title="Schema documentation"
-          target="_blank"
-          rel="noopener noreferrer"
-          href="https://www.sanity.io/docs/content-studio/the-schema"
-        >
-          Read more about how to add schema types
-        </a>
-        .
-      </Text>
-    </Stack>
+    <Card height="fill" paddingX={[5, 5, 7]} paddingY={[5, 5, 6]} sizing="border">
+      <Container>
+        <Stack space={5}>
+          <Heading as="h1">Empty schema</Heading>
+
+          <Text as="p">
+            Your schema does not contain any document types. If it did, those types would be listed
+            here.{' '}
+            <a
+              title="Schema documentation"
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://www.sanity.io/docs/content-studio/the-schema"
+            >
+              Read more about how to add schema types
+            </a>
+            .
+          </Text>
+        </Stack>
+      </Container>
+    </Card>
   )
 }
