55022000133a9a002002b82f2a89b9574a283da6
Rico Kahler <ricokahler@gmail.com>
2022-01-31 16:57:23 -0500

    feat(desk-tool): new confirm delete dialog for cross dataset references

---
+++ packages/@sanity/desk-tool/src/components/confirmDeleteDialog/ConfirmDeleteDialog.tsx
@@ -0,0 +1,113 @@
+import React, {useMemo} from 'react'
+import styled from 'styled-components'
+import {Box, Dialog, Button, Text, Spinner, Grid, Flex} from '@sanity/ui'
+import {useId} from '@reach/auto-id'
+import {DocTitle} from '../DocTitle'
+import {useReferringDocuments} from './useReferringDocuments'
+import {ConfirmDeleteDialogBody} from './ConfirmDeleteDialogBody'
+
+export const DialogBody = styled(Flex).attrs({
+  padding: 4,
+  direction: 'column',
+  height: 'fill',
+})`
+  box-sizing: border-box;
+`
+
+export const LoadingContainer = styled(Flex).attrs({
+  align: 'center',
+  direction: 'column',
+  justify: 'center',
+})`
+  height: 300px;
+`
+
+interface ReferencingDocumentsProps {
+  /**
+   * Incoming document ID used to find other referencing documents. This
+   * field respects draft IDs (e.g. if you pass in a published ID when one
+   * doesn't exist the document title may not show up).
+   */
+  id: string
+  /**
+   * The schema typename of the incoming document
+   */
+  type: string
+  /**
+   * The name of the action being done. (e.g. the `'unpublish'` action requires
+   * the same document deletion confirmation).
+   */
+  action?: string
+  onCancel: () => void
+  onConfirm: () => void
+}
+
+/**
+ * A confirmation dialog used to prevent unwanted document deletes. Loads all
+ * the referencing internal and cross-data references prior to showing the
+ * delete button.
+ */
+export function ConfirmDeleteDialog({
+  id,
+  type,
+  action = 'delete',
+  onCancel,
+  onConfirm,
+}: ReferencingDocumentsProps) {
+  const dialogId = `deletion-confirmation-${useId()}`
+  const {
+    internalReferences,
+    crossDatasetReferences,
+    isLoading,
+    totalCount: total,
+  } = useReferringDocuments(id)
+  const capitalizedAction = `${action.substring(0, 1).toUpperCase()}${action.substring(1)}`
+
+  const documentTitle = <DocTitle document={useMemo(() => ({_id: id, _type: type}), [id, type])} />
+  const showConfirmButton = !isLoading
+
+  return (
+    <Dialog
+      width={1}
+      id={dialogId}
+      header={`${capitalizedAction} document?`}
+      footer={
+        <Grid columns={showConfirmButton ? 2 : 1} gap={2} paddingX={4} paddingY={3}>
+          <Button mode="ghost" onClick={onCancel} text="Cancel" />
+
+          {showConfirmButton && (
+            <Button
+              data-testid="confirm-delete-button"
+              text={total > 0 ? `${capitalizedAction} anyway` : `${capitalizedAction} now`}
+              tone="critical"
+              onClick={onConfirm}
+            />
+          )}
+        </Grid>
+      }
+      onClose={onCancel}
+    >
+      <DialogBody>
+        {crossDatasetReferences && internalReferences && !isLoading ? (
+          <ConfirmDeleteDialogBody
+            crossDatasetReferences={crossDatasetReferences}
+            internalReferences={internalReferences}
+            documentTitle={documentTitle}
+            isLoading={isLoading}
+            totalCount={total}
+            action={action}
+          />
+        ) : (
+          <LoadingContainer data-testid="loading-container">
+            <Spinner muted />
+            <Box marginTop={3}>
+              <Text align="center" muted size={1}>
+                Looking for referring documents…
+              </Text>
+            </Box>
+          </LoadingContainer>
+        )}
+      </DialogBody>
+    </Dialog>
+  )
+}
---
+++ packages/@sanity/desk-tool/src/components/confirmDeleteDialog/index.tsx
@@ -0,0 +1,37 @@
+import React, {useCallback, useState} from 'react'
+import {Box, Flex, Button, Dialog, Text, ErrorBoundary} from '@sanity/ui'
+import {useId} from '@reach/auto-id'
+import {ConfirmDeleteDialog} from './ConfirmDeleteDialog'
+
+type ArgType<T> = T extends (arg: infer U) => unknown ? U : never
+type ErrorInfo = ArgType<React.ComponentProps<typeof ErrorBoundary>['onCatch']>
+
+function ConfirmDeleteDialogContainer(props: React.ComponentProps<typeof ConfirmDeleteDialog>) {
+  const id = useId()
+  const [error, setError] = useState<ErrorInfo | null>(null)
+  const handleRetry = useCallback(() => setError(null), [])
+
+  return error ? (
+    <Dialog
+      id={`dialog-error-${id}`}
+      data-testid="confirm-delete-error-dialog"
+      header="Error"
+      footer={
+        <Flex paddingX={4} paddingY={3} direction="column">
+          <Button mode="ghost" text="Retry" onClick={handleRetry} />
+        </Flex>
+      }
+      onClose={props.onCancel}
+    >
+      <Box padding={4}>
+        <Text>An error occurred while loading referencing documents.</Text>
+      </Box>
+    </Dialog>
+  ) : (
+    <ErrorBoundary onCatch={setError}>
+      <ConfirmDeleteDialog {...props} />
+    </ErrorBoundary>
+  )
+}
+
+export {ConfirmDeleteDialogContainer as ConfirmDeleteDialog}
