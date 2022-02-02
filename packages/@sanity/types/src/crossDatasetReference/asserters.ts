b1e89024a12bf52c69e740acbc7ccc14a001fccd
Bjørge Næss <bjoerge@gmail.com>
2022-02-02 12:44:49 +0100

    feat(types): add support for cross dataset references

---
+++ packages/@sanity/types/src/crossDatasetReference/asserters.ts
@@ -0,0 +1,10 @@
+import {isObject} from '../helpers'
+import {CrossDatasetReference} from './types'
+
+export function isCrossDatasetReference(reference: unknown): reference is CrossDatasetReference {
+  return (
+    isObject(reference) &&
+    typeof reference._ref === 'string' &&
+    typeof reference._dataset === 'string'
+  )
+}
