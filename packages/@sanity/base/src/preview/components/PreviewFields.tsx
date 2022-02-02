2e77b22544c5692521e60e6a937b4687ce3ec36b
Rico Kahler <ricokahler@gmail.com>
2022-02-02 15:19:34 -0500

    refactor(base): remove unused `fields`

--- packages/@sanity/base/src/preview/components/PreviewFields.tsx
+++ packages/@sanity/base/src/preview/components/PreviewFields.tsx
@@ -2,23 +2,15 @@ import {SchemaType} from '@sanity/types'
 import React from 'react'
 import PreviewSubscriber from './PreviewSubscriber'
 
-function arrify<T>(val: T | T[]) {
-  if (Array.isArray(val)) {
-    return val
-  }
-  return typeof val === undefined ? [] : [val]
-}
-
 interface PreviewFieldsProps {
   document: Document
-  fields: string | string[]
   type: SchemaType
   children: (snapshot: Document) => React.ReactChildren
 }
 
 export default function PreviewFields(props: PreviewFieldsProps) {
   return (
-    <PreviewSubscriber value={props.document} type={props.type} fields={arrify(props.fields)}>
+    <PreviewSubscriber value={props.document} type={props.type}>
       {({snapshot}) => <>{snapshot ? props.children(snapshot) : null}</>}
     </PreviewSubscriber>
   )
