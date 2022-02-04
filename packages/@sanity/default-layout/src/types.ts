469a41a5d2bc14fd2db8acbfc9199b3fe2b0f5cd
Espen Hovlandsdal <espen@hovlandsdal.com>
2022-02-04 10:43:52 -0800

    fix(default-layout): include `tool` in tool component props

--- packages/@sanity/default-layout/src/types.ts
+++ packages/@sanity/default-layout/src/types.ts
@@ -9,7 +9,7 @@ export interface Tool {
     params: Record<string, any>,
     state: Record<string, any>
   ) => void
-  component?: React.ComponentType
+  component?: React.ComponentType<{tool: string}>
   icon?: React.ComponentType
   getIntentState?: (
     intent: Record<string, any>,
