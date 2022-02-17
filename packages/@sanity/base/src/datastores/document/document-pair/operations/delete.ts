c877cda2a80114199080a7b0e70484132b560765
Rico Kahler <ricokahler@gmail.com>
2022-02-17 12:20:41 -0500

    feat(base): include `skipCrossDatasetReferenceValidation` in delete-like operations

--- packages/@sanity/base/src/datastores/document/document-pair/operations/delete.ts
+++ packages/@sanity/base/src/datastores/document/document-pair/operations/delete.ts
@@ -11,6 +11,12 @@ export const del = {
       return tx.commit({tag: 'document.delete'})
     }
 
-    return tx.delete(idPair.draftId).commit({tag: 'document.delete'})
+    return tx.delete(idPair.draftId).commit({
+      tag: 'document.delete',
+      // this disables referential integrity for cross-dataset references. we
+      // have this set because we warn against deletes in the `ConfirmDeleteDialog`
+      // UI. This operation is run when "delete anyway" is clicked
+      skipCrossDatasetReferenceValidation: true,
+    })
   },
 }
