ba7dbce242b938ce5a0de3721831432133d3f023
Bjørge Næss <bjoerge@gmail.com>
2022-02-18 19:20:09 +0100

    chore(test-studio): add example for docs dataset

--- packages/@sanity/form-builder/src/sanity/inputs/crossDatasetReferenceInput/SanityCrossDatasetReferenceInput.tsx
+++ packages/@sanity/form-builder/src/sanity/inputs/crossDatasetReferenceInput/SanityCrossDatasetReferenceInput.tsx
@@ -158,8 +158,8 @@ const SanityCrossDatasetReferenceInput = forwardRef(function SanityCrossDatasetR
           <Stack space={3}>
             <Text size={1}>
               This cross dataset reference field requires a cross dataset token to be registered.
-              Please provide a value for token with id "<b>{type.tokenId}</b>" for dataset "
-              <b>{type.dataset}</b>" in project "<b>{type.projectId}</b>".
+              Please configure a token with ID <b>{type.tokenId}</b> for project{' '}
+              <b>{type.projectId}</b> that has read access to the <b>{type.dataset}</b>-dataset.
             </Text>
             <Text size={1}>
               See the documentation for{' '}
