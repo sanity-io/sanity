4e1263bf4e1643ef9d7b1256479437bdbd996b19
Espen Hovlandsdal <espen@hovlandsdal.com>
2022-03-03 21:24:39 -0800

    feat(cli): run yarn install command with `--check-files` flag

--- packages/@sanity/cli/src/commands/install/installDependencies.ts
+++ packages/@sanity/cli/src/commands/install/installDependencies.ts
@@ -3,6 +3,6 @@ import type {CliCommandAction} from '../../types'
 export const installDependencies: CliCommandAction = (args, context) => {
   const {output, workDir, yarn} = context
   const {extOptions} = args
-  const flags = extOptions.offline ? ['--offline'] : []
+  const flags = ['--check-files'].concat(extOptions.offline ? ['--offline'] : [])
   return yarn(['install'].concat(flags), {...output, rootDir: workDir})
 }
