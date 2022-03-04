154fa929faff9a62e8d654dda8db6293099dc5ce
Espen Hovlandsdal <espen@hovlandsdal.com>
2022-03-03 21:08:53 -0800

    feat(cli): make manage command work with both v2 and v3

--- packages/@sanity/cli/src/commands/manage/manageCommand.ts
+++ packages/@sanity/cli/src/commands/manage/manageCommand.ts
@@ -1,6 +1,4 @@
-import path from 'path'
 import open from 'open'
-import fse from 'fs-extra'
 import type {CliCommandDefinition} from '../../types'
 
 const manageCommand: CliCommandDefinition = {
@@ -9,25 +7,16 @@ const manageCommand: CliCommandDefinition = {
   helpText: '',
   description: 'Opens the Sanity project management UI',
   async action(args, context) {
-    const {workDir, output} = context
+    const {output, cliConfig} = context
     const {print} = output
-    const configLocation = path.join(workDir, 'sanity.json')
-
-    // @todo v2/v3 compat
-    let projectId
-    try {
-      const config = await fse.readJson(configLocation)
-      projectId = config.api && config.api.projectId
-    } catch (err) {
-      // Noop.
-    }
+    const projectId = cliConfig?.api?.projectId
 
     const url = projectId
       ? `https://manage.sanity.io/projects/${projectId}`
       : 'https://manage.sanity.io/'
 
     print(`Opening ${url}`)
-    open(url)
+    await open(url)
   },
 }
 
