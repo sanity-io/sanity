d1c9d6a2f4c539354d2497e820edf99afaaaeea4
Espen Hovlandsdal <espen@hovlandsdal.com>
2022-03-07 14:34:47 -0800

    feat(schema): warn when providing superflous image metadata props

--- packages/@sanity/schema/src/sanity/validation/types/image.ts
+++ packages/@sanity/schema/src/sanity/validation/types/image.ts
@@ -1,29 +1,43 @@
-import {error, HELP_IDS} from '../createValidationResult'
+import {error, warning, HELP_IDS} from '../createValidationResult'
 import {validateFields, validateField} from './object'
+
+const autoMeta = ['dimensions', 'hasAlpha', 'isOpaque']
 
 export default (typeDef, visitorContext) => {
   const problems = []
-  let fields = typeDef.fields
+  const fields = typeDef.fields
 
   if (fields) {
     problems.push(...validateFields(fields, {allowEmpty: true}))
   }
 
-  if (
-    typeDef.options &&
-    typeof typeDef.options.metadata !== 'undefined' &&
-    !Array.isArray(typeDef.options.metadata)
-  ) {
+  let options = typeDef.options
+  const metadata = options?.metadata
+  const superfluousMeta = Array.isArray(metadata)
+    ? metadata.filter((meta) => autoMeta.includes(meta))
+    : []
+
+  if (typeof metadata !== 'undefined' && !Array.isArray(metadata)) {
     problems.push(
       error(
         `Invalid type for image \`metadata\` field - must be an array of strings`,
         HELP_IDS.ASSET_METADATA_FIELD_INVALID
       )
     )
+  } else if (superfluousMeta.length > 0) {
+    problems.push(
+      warning(
+        `Image \`metadata\` field contains superfluous properties (they are always included): ${superfluousMeta.join(
+          ', '
+        )}`
+      )
+    )
+    options = {...options, metadata: metadata.filter((meta) => !autoMeta.includes(meta))}
   }
 
   return {
     ...typeDef,
+    options,
     fields: (Array.isArray(fields) ? fields : []).map((field, index) => {
       const {name, ...fieldTypeDef} = field
       const {_problems, ...fieldType} = visitorContext.visit(fieldTypeDef, index)
