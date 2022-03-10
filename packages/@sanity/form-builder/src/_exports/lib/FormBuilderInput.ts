ec6b45b2676d187489966308f7cacd154d84814c
Marius Lundgård <studio@mariuslundgard.com>
2022-03-10 21:27:48 +0100

    chore(form-builder): remove deprecated export

--- packages/@sanity/form-builder/src/_exports/lib/FormBuilderInput.ts
+++
@@ -1,2 +0,0 @@
-// Note this is only for exposing types for package consumers through the officially supported import '@sanity/form-builder/lib/FormBuilderInput'
-export * from '../../FormBuilderInput'
