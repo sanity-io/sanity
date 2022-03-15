013076052c066b3691e78f9cc8113e9d5e4b4dbd
Marius Lundgård <studio@mariuslundgard.com>
2022-03-15 17:51:09 +0100

    fix(mutator): fix TS issues

--- packages/@sanity/mutator/perf/run.js
+++ packages/@sanity/mutator/perf/run.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 const {BufferedDocument, Mutation} = require('../lib')
 const mutations = require('./fixtures/patches')
 const snapshot = require('./fixtures/snapshot')
