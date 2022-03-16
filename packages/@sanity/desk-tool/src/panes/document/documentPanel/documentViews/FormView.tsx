43bce4cae9d17ef1077d76aa8ca3046bfbced79e
Marius Lundgård <studio@mariuslundgard.com>
2022-03-16 13:02:39 +0100

    WIP: refactor(desk-tool): fix after rebase

--- packages/@sanity/desk-tool/src/panes/document/documentPanel/documentViews/FormView.tsx
+++ packages/@sanity/desk-tool/src/panes/document/documentPanel/documentViews/FormView.tsx
@@ -1,15 +1,24 @@
 import {useDatastores, useSource} from '@sanity/base'
 import {
+  DocumentMutationEvent,
+  DocumentRebaseEvent,
+  fromMutationPatches,
+} from '@sanity/base/_internal'
+import {
   useDocumentPresence,
   unstable_useConditionalProperty as useConditionalProperty,
 } from '@sanity/base/hooks'
 import {PresenceOverlay} from '@sanity/base/presence'
 import {SanityDocument} from '@sanity/client'
 import {SanityFormBuilder} from '@sanity/form-builder'
-import {FormBuilderFilterFieldFn} from '@sanity/form-builder/_internal'
+import {
+  createPatchChannel,
+  FormBuilderFilterFieldFn,
+  PatchMsg,
+} from '@sanity/form-builder/_internal'
 import {isActionEnabled} from '@sanity/schema/_internal'
 import {Box, Container, Flex, Spinner, Text} from '@sanity/ui'
-import React, {useCallback, useEffect, useMemo, useRef, useState} from 'react'
+import React, {useCallback, useEffect, useMemo, useState} from 'react'
 import {tap} from 'rxjs/operators'
 import {useDocumentPane} from '../../useDocumentPane'
 import {Delay} from '../../../../components/Delay'
@@ -57,11 +66,11 @@ export function FormView(props: FormViewProps) {
   const hasTypeMismatch = value !== null && value._type !== documentSchema.name
   const isNonExistent = !value || !value._id
 
-  // Create a patch channel for each document ID
-  const patchChannelRef = useRef<any>()
-  if (!patchChannelRef.current) {
-    patchChannelRef.current = SanityFormBuilder.createPatchChannel()
-  }
+  // The `patchChannel` is an INTERNAL publish/subscribe channel that we use to notify form-builder
+  // nodes about both remote and local patches.
+  // - Used by the Portable Text input to modify selections.
+  // - Used by `withDocument` to reset value.
+  const patchChannel = useMemo(() => createPatchChannel(), [])
 
   const readOnly = useConditionalProperty({
     document: value as SanityDocument,
@@ -98,13 +107,23 @@ export function FormView(props: FormViewProps) {
   useEffect(() => {
     const sub = documentStore.pair
       .documentEvents(documentId, documentType)
-      .pipe(tap((event) => patchChannelRef.current.receiveEvent(event)))
+      .pipe(
+        tap((event) => {
+          if (event.type === 'mutation') {
+            patchChannel.publish(prepareMutationEvent(event))
+          }
+
+          if (event.type === 'rebase') {
+            patchChannel.publish(prepareRebaseEvent(event))
+          }
+        })
+      )
       .subscribe()
 
     return () => {
       sub.unsubscribe()
     }
-  }, [documentId, documentStore, documentType, patchChannelRef])
+  }, [documentId, documentStore, documentType, patchChannel])
 
   const hasRev = Boolean(value?._rev)
   useEffect(() => {
@@ -113,7 +132,11 @@ export function FormView(props: FormViewProps) {
       // stuck at the first initial value.
       // This effect is triggered only when the document goes from not having a revision, to getting one
       // so it will kick in as soon as the document is received from the backend
-      patchChannelRef.current.receiveEvent({type: 'mutation', mutations: [], document: value})
+      patchChannel.publish({
+        type: 'mutation',
+        patches: [],
+        snapshot: value,
+      })
     }
     // React to changes in hasRev only
     // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -153,7 +176,7 @@ export function FormView(props: FormViewProps) {
               onBlur={handleBlur}
               onChange={isReadOnly ? noop : handleChange}
               onFocus={handleFocus}
-              patchChannel={patchChannelRef.current}
+              __internal_patchChannel={patchChannel}
               presence={presence}
               readOnly={isReadOnly}
               schema={schema}
@@ -187,12 +210,13 @@ export function FormView(props: FormViewProps) {
     hasTypeMismatch,
     margins,
     markers,
-    patchChannelRef,
+    patchChannel,
     presence,
     ready,
     isReadOnly,
     value,
     changesOpen,
+    schema,
   ])
 
   const after = useMemo(
@@ -219,4 +243,27 @@ export function FormView(props: FormViewProps) {
       {after}
     </Container>
   )
+}
+
+function prepareMutationEvent(event: DocumentMutationEvent): PatchMsg {
+  const patches = event.mutations.map((mut) => mut.patch).filter(Boolean)
+
+  return {
+    type: 'mutation',
+    snapshot: event.document,
+    patches: fromMutationPatches(event.origin, patches),
+  }
+}
+
+function prepareRebaseEvent(event: DocumentRebaseEvent): PatchMsg {
+  const remotePatches = event.remoteMutations.map((mut) => mut.patch).filter(Boolean)
+  const localPatches = event.localMutations.map((mut) => mut.patch).filter(Boolean)
+
+  return {
+    type: 'rebase',
+    snapshot: event.document,
+    patches: fromMutationPatches('remote', remotePatches).concat(
+      fromMutationPatches('local', localPatches)
+    ),
+  }
 }
