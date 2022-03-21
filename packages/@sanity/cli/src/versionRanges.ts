3cdec57d238aaf2bd6169fe1f9c66d7c701cf890
Espen Hovlandsdal <espen@hovlandsdal.com>
2022-03-21 11:18:46 -0700

    feat(cli): bootstrap new studios with v3 tag (temporary)

--- packages/@sanity/cli/src/versionRanges.ts
+++ packages/@sanity/cli/src/versionRanges.ts
@@ -1,33 +1,20 @@
+const V3_TAG = 'purple-unicorn'
+
 export default {
   // Dependencies for a default Sanity installation
   core: {
-    '@sanity/base': 'latest',
-    '@sanity/core': 'latest',
-    '@sanity/default-layout': 'latest',
-    '@sanity/default-login': 'latest',
-    '@sanity/desk-tool': 'latest',
-    '@sanity/eslint-config-studio': 'latest',
-    '@sanity/vision': 'latest',
-    eslint: '^8.6.0',
-    'prop-types': '^15.7',
-    react: '^17.0',
-    'react-dom': '^17.0',
-    'styled-components': '^5.2.0',
-  },
+    '@sanity/base': V3_TAG,
+    '@sanity/core': V3_TAG,
+    '@sanity/desk-tool': V3_TAG,
+    '@sanity/vision': V3_TAG,
 
-  // Only used for Sanity-style plugins (eg, the ones we build at Sanity.io)
-  plugin: {
-    dev: {
-      'babel-cli': '^6.9.0',
-      'babel-eslint': '^6.0.4',
-      'babel-plugin-syntax-class-properties': '^6.8.0',
-      'babel-plugin-transform-class-properties': '^6.9.1',
-      'babel-preset-es2015': '^6.9.0',
-      'babel-preset-react': '^6.5.0',
-      eslint: '^3.4.0',
-      'eslint-config-sanity': '^1.1.3',
-      'eslint-plugin-react': '^6.3.0',
-      rimraf: '^2.5.2',
-    },
+    // Non-Sanity dependencies
+    react: '^17.0.0',
+    'react-dom': '^17.0.0',
+    'styled-components': '^5.2.0',
+
+    // Linting
+    '@sanity/eslint-config-studio': 'latest',
+    eslint: '^8.6.0',
   },
 }
