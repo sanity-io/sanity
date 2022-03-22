41192c9d591a7ef8219e8afc93ed8e8f07fef25c
Per-Kristian Nordnes <per.kristian.nordnes@gmail.com>
2022-03-22 13:20:38 +0100

    feat(cli): store metadata on template used with project
    When running 'sanity init' with the --template options, store on project.metadata.initialTemplate the template used.

--- packages/@sanity/cli/src/actions/init-project/bootstrapTemplate.js
+++ packages/@sanity/cli/src/actions/init-project/bootstrapTemplate.js
@@ -1,4 +1,5 @@
 import path from 'path'
+import chalk from 'chalk'
 import fse from 'fs-extra'
 import {union, difference} from 'lodash'
 import debug from '../../debug'
@@ -8,19 +9,19 @@ import {createPackageManifest, createSanityManifest} from './createManifest'
 import templates from './templates'
 
 export default async (opts, context) => {
-  const {output, cliRoot} = context
+  const {apiClient, cliRoot, output} = context
   const templatesDir = path.join(cliRoot, 'templates')
   const sourceDir = path.join(templatesDir, opts.template)
-  const outputDir = opts.outputDir
+  const {outputDir, projectId, template: templateName} = opts
 
   // Check that we have a template info file (dependencies, plugins etc)
-  const template = templates[opts.template]
+  const template = templates[templateName]
   if (!template) {
-    throw new Error(`Template "${opts.template}" not defined`)
+    throw new Error(`Template "${templateName}" not defined`)
   }
 
   // Copy template files
-  debug('Copying files from template "%s" to "%s"', opts.template, outputDir)
+  debug('Copying files from template "%s" to "%s"', templateName, outputDir)
   let spinner = output.spinner('Bootstrapping files from template').start()
   await fse.copy(sourceDir, outputDir, {
     overwrite: false,
@@ -64,6 +65,21 @@ export default async (opts, context) => {
     ),
   ])
 
+  // Store template name metadata on project
+  try {
+    await apiClient({api: {projectId}}).request({
+      method: 'PATCH',
+      uri: `/projects/${projectId}`,
+      body: {metadata: {initialTemplate: `cli-${templateName}`}},
+    })
+  } catch (err) {
+    if (err.statusCode === 401) {
+      output.warn(`\n${chalk.yellow('⚠')} Unauthorized to update metadata for this project`)
+    } else {
+      output.warn(`\n${chalk.red('⚠')} ${err.message}`)
+    }
+  }
+
   // Finish up by providing init process with template-specific info
   spinner.succeed()
   return template
@@ -74,7 +90,7 @@ export default async (opts, context) => {
       await fse.writeFile(filePath, content, {flag: 'wx'})
     } catch (err) {
       if (err.code === 'EEXIST') {
-        output.print(`\n[WARN] File "${filePath}" already exists, skipping`)
+        output.warn(`\n${chalk.yellow('⚠')} File "${filePath}" already exists, skipping`)
       } else {
         throw err
       }
