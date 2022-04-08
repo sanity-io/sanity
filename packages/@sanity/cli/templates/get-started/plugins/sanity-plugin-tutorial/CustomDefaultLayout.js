4b92ad7cebcc04a946994128631c2e4821b4bfa3
Per-Kristian Nordnes <per.kristian.nordnes@gmail.com>
2022-04-08 14:01:15 +0200

    refactor(cli): hide empty schema msg for get-started template

--- packages/@sanity/cli/templates/get-started/plugins/sanity-plugin-tutorial/CustomDefaultLayout.js
+++ packages/@sanity/cli/templates/get-started/plugins/sanity-plugin-tutorial/CustomDefaultLayout.js
@@ -2,14 +2,23 @@ import React from 'react'
 import StudioRoot from 'part:@sanity/default-layout/root'
 import {useCurrentUser} from '@sanity/base/hooks'
 import HelloSanityTutorial from './HelloSanityTutorial'
+import styled from 'styled-components'
+
+
+// Hide the empty schema message
+const Wrapper = styled.div`
+  *[data-testid="missing-document-types-message"] {
+    display: none;
+  }
+`
 
 export default function CustomDefaultLayout() {
   const currentUser = useCurrentUser()
 
   return (
-    <>
+    <Wrapper>
       {currentUser.value && <HelloSanityTutorial />}
       <StudioRoot />
-    </>
+    </Wrapper>
   )
 }
