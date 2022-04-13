575cddaf0f873975f49ab2c3dd98980bad330c45
Geoff Ball <geoff@gball.dev>
2022-04-13 17:35:26 -0600

    chore(cli): Update login docs

--- packages/@sanity/cli/src/commands/login/loginCommand.js
+++ packages/@sanity/cli/src/commands/login/loginCommand.js
@@ -1,8 +1,17 @@
 import login from '../../actions/login/login'
 
+const helpText = `
+Options
+  --sso Authenticate against a third-party identity provider
+
+Examples
+  # Login with SAML SSO
+  sanity login sso <slug>
+`
 export default {
   name: 'login',
   signature: '',
-  description: 'Authenticates against the Sanity.io API',
+  description: 'Authenticates against the Sanity.io API or a third-party identity provider',
+  helpText,
   action: login,
 }
