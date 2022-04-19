44b425761998b1faefc28a201dd97ab3b2d38e8b
Geoff Ball <geoff@sanity.io>
2022-04-19 10:57:49 -0600

    chore(cli): Update login docs (#3247)
    * chore(cli): Update login docs to reflect `--sso` option.
    * chore(cli): fix linting
    
    Co-authored-by: Rita <rita@sanity.io>

--- packages/@sanity/cli/src/commands/login/loginCommand.js
+++ packages/@sanity/cli/src/commands/login/loginCommand.js
@@ -1,8 +1,21 @@
 import login from '../../actions/login/login'
 
+const helpText = `
+Options
+  --sso <slug> Authenticate against a third-party identity provider
+
+Examples
+  # Login against the Sanity.io API
+  sanity login
+
+  # Login with SAML SSO
+  sanity login --sso org-slug
+`
 export default {
   name: 'login',
-  signature: '',
-  description: 'Authenticates against the Sanity.io API',
+  signature: '[--sso <slug>]',
+  description:
+    'Authenticates against the Sanity.io API (no flag) or a third-party identity provider (with --sso flag)',
+  helpText,
   action: login,
 }
