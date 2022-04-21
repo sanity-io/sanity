bc8faa4b01e1756f4e244c7d2a658d566e148eb0
Kristofer Giltvedt Selbekk <selbekk@users.noreply.github.com>
2022-04-21 12:59:04 +0200

    Just use the timestamp directly

--- packages/@sanity/desk-tool/src/panes/document/timeline/timelineItem.tsx
+++ packages/@sanity/desk-tool/src/panes/document/timeline/timelineItem.tsx
@@ -33,7 +33,6 @@ export function TimelineItem(props: {
   const iconComponent = getTimelineEventIconComponent(type)
   const authorUserIds = Array.from(chunk.authors)
   const timeAgo = useTimeAgo(timestamp, {minimal: true})
-  const formattedTimestamp = new Date(timestamp).toISOString()
 
   const isSelected = state === 'selected'
   const isWithinSelection = state === 'withinSelection'
@@ -67,7 +66,7 @@ export function TimelineItem(props: {
       data-selection-bottom={isSelectionBottom}
       data-selection-top={isSelectionTop}
       onClick={handleClick}
-      title={formattedTimestamp}
+      title={timestamp}
     >
       <div
         // eslint-disable-next-line react/jsx-no-bind
