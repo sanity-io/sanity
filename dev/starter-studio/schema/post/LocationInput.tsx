238df86504b2ab1a8dc44ae2866cd4913c712d85
Marius Lundgård <studio@mariuslundgard.com>
2022-04-26 22:34:51 +0200

    WIP: dev

--- dev/starter-studio/schema/post/LocationInput.tsx
+++ dev/starter-studio/schema/post/LocationInput.tsx
@@ -1,7 +1,6 @@
 import {Card, Grid} from '@sanity/ui'
 import React from 'react'
-import {FieldMember, FormNode, ObjectInputProps} from 'sanity/form'
-import {CoordinateInput} from './CoordinateInput'
+import {FormFieldMember, ObjectInputProps} from 'sanity/form'
 
 export interface LocationValue {
   lat?: number
@@ -12,25 +11,12 @@ export interface LocationValue {
 // and inform it that we are working with a value of `LocationValue`
 export type LocationInputProps = ObjectInputProps<LocationValue>
 
-export function LocationInput(props: LocationInputProps) {
-  const {members} = props
-  const fieldMembers = members.filter((member) => member.type === 'field') as FieldMember[]
-
-  // Find the field members we want to render below
-  // NOTE: this is cumbersome, and something we need to improve
-  const latFieldMember = fieldMembers.find((member) => member.field.name === 'lat')
-  const lngFieldMember = fieldMembers.find((member) => member.field.name === 'lng')
-
+export function LocationInput() {
   return (
     <Card padding={2} radius={2} tone="positive">
       <Grid columns={2} gap={3}>
-        {latFieldMember && (
-          <FormNode component={CoordinateInput as any} fieldProps={latFieldMember.field} />
-        )}
-
-        {lngFieldMember && (
-          <FormNode component={CoordinateInput as any} fieldProps={lngFieldMember.field} />
-        )}
+        <FormFieldMember name="lat" />
+        <FormFieldMember name="lng" />
       </Grid>
     </Card>
   )
