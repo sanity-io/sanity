fc99c6114c6605665bfb3dbb2eb28620e9a15eb6
Marius Lundgård <studio@mariuslundgard.com>
2022-05-09 17:47:53 +0200

    refactor(base): refactor `PopoverDialog`

--- packages/@sanity/base/src/components/popoverDialog/PopoverDialog.tsx
+++ packages/@sanity/base/src/components/popoverDialog/PopoverDialog.tsx
@@ -1,38 +1,39 @@
 import {CloseIcon} from '@sanity/icons'
-import {Box, Button, Card, Flex, Placement, Popover, Text} from '@sanity/ui'
-import React from 'react'
+import {Box, Button, Card, Container, Flex, Popover, PopoverProps, Text} from '@sanity/ui'
+import React, {HTMLProps} from 'react'
 
-export function PopoverDialog(props: {
-  children?: React.ReactNode
-  onClose: () => void
-  placement?: Placement
-  referenceElement?: HTMLElement | null
-  title?: React.ReactNode
-}) {
-  const {children, onClose, placement, referenceElement, title} = props
+export function PopoverDialog(
+  props: Omit<PopoverProps, 'content' | 'width'> &
+    Omit<HTMLProps<HTMLDivElement>, 'ref' | 'width'> & {
+      header?: React.ReactNode
+      onClose: () => void
+      width?: number | number[] | 'auto'
+    }
+) {
+  const {children, header, onClose, width, ...restProps} = props
 
   return (
     <Popover
+      {...restProps}
       content={
-        <Flex direction="column">
-          {/* @todo: use `flex="none"` property */}
-          <Card style={{flex: 'none'}}>
-            <Flex>
-              <Box flex={1}>
-                <Text weight="semibold">{title}</Text>
-              </Box>
-              <Box>
-                <Button icon={CloseIcon} onClick={onClose} />
-              </Box>
-            </Flex>
-          </Card>
-          <Card flex={1} overflow="auto">
-            {children}
-          </Card>
-        </Flex>
+        <Container width={width}>
+          <Flex direction="column">
+            <Card flex="none">
+              <Flex>
+                <Box flex={1}>
+                  <Text weight="semibold">{header}</Text>
+                </Box>
+                <Box>
+                  <Button icon={CloseIcon} onClick={onClose} />
+                </Box>
+              </Flex>
+            </Card>
+            <Card flex={1} overflow="auto">
+              {children}
+            </Card>
+          </Flex>
+        </Container>
       }
-      placement={placement}
-      referenceElement={referenceElement}
     />
   )
 }
