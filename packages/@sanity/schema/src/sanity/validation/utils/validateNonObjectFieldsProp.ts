a772c01921d3497dbe49eed23367a9177a5dca72
Espen Hovlandsdal <espen@hovlandsdal.com>
2022-05-09 15:06:52 +0200

    fix(schema): prevent crash on validating non-object schema type

--- packages/@sanity/schema/src/sanity/validation/utils/validateNonObjectFieldsProp.ts
+++ packages/@sanity/schema/src/sanity/validation/utils/validateNonObjectFieldsProp.ts
@@ -1,12 +1,17 @@
+import type {SchemaType} from '@sanity/types'
+import type {ValidationResult} from '../../typedefs'
 import {error} from '../createValidationResult'
 
-export function validateNonObjectFieldsProp(typeDef, visitorContext) {
-  if (!typeDef.fields) {
+export function validateNonObjectFieldsProp(
+  typeDef: SchemaType,
+  visitorContext
+): ValidationResult[] {
+  if (!('fields' in typeDef)) {
     return []
   }
 
   let type = typeDef
-  while (!type.jsonType && type) {
+  while (type && !type.jsonType) {
     type = visitorContext.getType(type.type)
   }
 
