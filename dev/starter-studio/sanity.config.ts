d9a68ab885bda1be8287438ff5cdb9d9696a2467
Marius Lundgård <studio@mariuslundgard.com>
2022-05-12 16:11:10 +0200

    WIP: starter-studio

--- dev/starter-studio/sanity.config.ts
+++ dev/starter-studio/sanity.config.ts
@@ -1,11 +1,13 @@
-import {codeInput} from '@sanity/code-input'
 import {createConfig} from 'sanity'
 import {deskTool} from 'sanity/desk'
 
 export default createConfig({
   dataset: 'test',
-  plugins: [codeInput(), deskTool()],
+  plugins: [deskTool()],
   name: 'default',
   projectId: 'ppsg7ml5',
   title: 'Starter Studio',
+  schema: {
+    types: [],
+  },
 })
