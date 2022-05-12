a655ae58815670f3425f2c821f3d33ed9a08cd14
Rita <rita@sanity.io>
2022-05-12 11:02:00 +0200

    screenshot test

--- dev/test-studio/schema/standard/images.js
+++ dev/test-studio/schema/standard/images.js
@@ -30,10 +30,8 @@ export default {
     },
     {
       name: 'mainImage',
-      title: 'Image',
+      title: 'Main Image',
       type: 'image',
-      description:
-        'Image hotspot should be possible to change. Caption should be visible in image field, full description should be editable in modal',
       options: {
         hotspot: true,
       },
@@ -48,19 +46,10 @@ export default {
           validation: (Rule) => Rule.required(),
         },
         {
-          name: 'detailedCaption',
-          type: 'string',
-          title: 'Detailed caption',
-          options: {
-            isHighlighted: true,
-          },
-          hidden: ({parent}) => !parent?.caption,
-        },
-        {
           name: 'foo',
           type: 'string',
-          title:
-            'This is a rather longish title for a field. It should still work. This is a rather longish title for a field. It should still work.',
+          title: 'Alternative text',
+          description: 'Important for SEO and accessibility.',
           options: {
             isHighlighted: true,
           },
@@ -71,127 +60,6 @@ export default {
           title: 'Full description',
         },
       ],
-    },
-    {
-      name: 'someFile',
-      title: 'A simple file',
-      type: 'file',
-    },
-    {
-      name: 'fileWithFields',
-      title: 'File with additional fields',
-      type: 'file',
-      fields: [
-        {
-          title: 'Description',
-          name: 'description',
-          type: 'string',
-          options: {isHighlighted: true},
-        },
-        {
-          title: '👀',
-          name: 'description_eyes',
-          type: 'string',
-          options: {isHighlighted: true},
-        },
-        {
-          title: 'Not so important',
-          name: 'notsoimportant',
-          type: 'string',
-        },
-        {
-          title: 'not so important but electric boogaloo',
-          name: 'notsoimportant2',
-          type: 'string',
-        },
-      ],
-    },
-    {
-      name: 'myImage',
-      title: 'Field of custom image type',
-      type: 'myImage',
-      description: 'Should be like myImage',
-      required: true,
-    },
-    {
-      name: 'pngImage',
-      title: 'PNG image',
-      type: 'image',
-      description: 'Should not accept other image types than png',
-      options: {
-        hotspot: true,
-        accept: 'image/png',
-      },
-    },
-    {
-      name: 'jpegImageWithLqip',
-      title: 'JPEG image',
-      type: 'image',
-      description: 'Should only accept JPEGs and will extract only LQIP and location metadata',
-      options: {
-        accept: 'image/jpeg',
-        metadata: ['location', 'lqip'],
-      },
-    },
-    {
-      name: 'imageWithImage',
-      title: 'Image with image',
-      type: 'image',
-      description: 'This is a weird example of an image that has an image as one of its fields',
-      options: {
-        hotspot: true,
-      },
-      fields: [
-        {
-          name: 'caption',
-          type: 'string',
-          title: 'Caption',
-          options: {
-            isHighlighted: true,
-          },
-        },
-        {
-          name: 'foo',
-          type: 'string',
-          title:
-            'This is a rather longish title for a field. It should still work. This is a rather longish title for a field. It should still work.',
-          options: {
-            isHighlighted: true,
-          },
-        },
-        {
-          name: 'image',
-          type: 'image',
-          title: 'Image in image',
-          options: {
-            isHighlighted: true,
-          },
-        },
-        {
-          name: 'description',
-          type: 'string',
-          title: 'Full description',
-        },
-        {
-          name: 'extraImage',
-          type: 'image',
-          title: 'Image in image behind edit',
-        },
-      ],
-    },
-    {
-      name: 'customAssetSource',
-      type: 'image',
-      title: 'Custom asset source',
-      options: {
-        sources: [petsAssetSource, noopAssetSource],
-      },
-    },
-    {
-      name: 'noAssetSource',
-      type: 'image',
-      title: 'No asset source',
-      options: {sources: []},
     },
   ],
 }
