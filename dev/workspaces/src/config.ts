4f3d400cc4b9a1e97791e031f2c64bed073a71c3
Marius Lundgård <studio@mariuslundgard.com>
2022-05-24 15:06:09 +0200

    WIP: workspaces

--- dev/workspaces/src/config.ts
+++
@@ -1,5 +0,0 @@
-export const config = {
-  sanity: {
-    projectId: 'ppsg7ml5',
-  },
-}
--- dev/workspaces/src/empty/sanity.config.ts
+++
@@ -1,9 +0,0 @@
-import {createConfig} from '@sanity/base'
-
-export default createConfig({
-  project: {
-    basePath: '/empty',
-    name: 'Empty',
-  },
-  sources: [],
-})
