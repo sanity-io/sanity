d7a8da2ee084a46946da81556cf467c5c9caefbf
Marius Lundgård <studio@mariuslundgard.com>
2022-05-24 15:05:57 +0200

    WIP: multi-source-studio

--- dev/multi-source-studio/internal/deskTool.ts
+++
@@ -1,9 +0,0 @@
-import {deskTool} from '@sanity/desk-tool'
-import {LockIcon} from '@sanity/icons'
-
-export const internalDeskTool = deskTool({
-  source: 'internal',
-  name: 'internal',
-  title: 'Internal',
-  icon: LockIcon,
-})
--- dev/multi-source-studio/tools/index.ts
+++
@@ -1,4 +0,0 @@
-import {dataTool} from './data'
-import {debugTool} from './debug'
-
-export const tools = [dataTool, debugTool]
