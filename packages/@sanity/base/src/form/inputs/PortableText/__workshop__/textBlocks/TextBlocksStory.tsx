e0a0d494349831593fba03abf4e99a3962091bcf
Bjørge Næss <bjoerge@gmail.com>
2022-06-02 15:43:32 +0200

    chore(base): add presence and validation to members

--- packages/@sanity/base/src/form/inputs/PortableText/__workshop__/textBlocks/TextBlocksStory.tsx
+++ packages/@sanity/base/src/form/inputs/PortableText/__workshop__/textBlocks/TextBlocksStory.tsx
@@ -4,7 +4,6 @@ import React, {useMemo} from 'react'
 import {NodeValidation, PortableTextMarker} from '../../../../types'
 import {FormBuilderProvider} from '../../../../FormBuilderProvider'
 import {TextBlock} from '../../text'
-import {useSource} from '../../../../../studio'
 
 const errorMarker: NodeValidation = {
   level: 'error',
