be4fd1b37f987a89b6eb9d7b184d6f200bb9fe1e
Marius Lundgård <studio@mariuslundgard.com>
2022-06-03 09:11:45 +0200

    chore(base): delete unused file

--- packages/@sanity/base/src/TODO.ts
+++
@@ -1,7 +0,0 @@
-import {SchemaType} from '@sanity/types'
-
-/**
- * @todo
- * This used to be configurable by add the optional 'part:@sanity/base/preview-resolver' part
- */
-export const customPreviewResolver: ((schemaType: SchemaType) => unknown) | undefined = undefined
