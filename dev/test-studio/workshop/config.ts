cb43ad3535bfe3917f0eedea72ead7c11dd2b445
Marius Lundgård <studio@mariuslundgard.com>
2022-06-10 20:22:23 +0200

    WIP: test-studio

--- dev/test-studio/workshop/config.ts
+++ dev/test-studio/workshop/config.ts
@@ -2,33 +2,8 @@ import {a11yPlugin, defineConfig, perfPlugin} from '@sanity/ui-workshop'
 import {scopes} from './scopes'
 
 export const config = defineConfig({
-  collections: [
-    {
-      name: 'sanity',
-      title: 'sanity',
-    },
-    {
-      name: 'default-layout',
-      title: '@sanity/default-layout',
-    },
-    {
-      name: 'desk-tool',
-      title: '@sanity/desk-tool',
-    },
-    {
-      name: 'field',
-      title: '@sanity/field',
-    },
-    {
-      name: 'form-builder',
-      title: '@sanity/form-builder',
-    },
-  ],
-  features: {
-    // navbar: false,
-  },
   frameUrl: '/workshop/frame/',
   plugins: [perfPlugin(), a11yPlugin()],
   scopes: scopes.filter(Boolean),
-  title: 'Studio Workshop',
+  title: 'Sanity Workshop',
 })
