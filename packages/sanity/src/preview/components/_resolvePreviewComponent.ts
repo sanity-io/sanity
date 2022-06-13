7694f8e4cc2b15bfb24e99678cd489e93d1c1425
Marius Lundgård <studio@mariuslundgard.com>
2022-06-13 22:41:58 +0200

    fix(sanity): fix preview component resolution

--- packages/sanity/src/preview/components/_resolvePreviewComponent.ts
+++ packages/sanity/src/preview/components/_resolvePreviewComponent.ts
@@ -1,21 +1,13 @@
 import {SchemaType} from '@sanity/types'
-import {get} from 'lodash'
 import {ComponentType} from 'react'
 import {PreviewProps} from '../../components/previews'
 import {SanityDefaultPreview} from './SanityDefaultPreview'
 
 export function _resolvePreviewComponent(type: SchemaType): ComponentType<
   PreviewProps & {
-    error: Error | null
     icon?: ComponentType
     schemaType?: SchemaType
   }
 > {
-  const fromPreview = get(type, 'preview.component')
-
-  if (fromPreview) {
-    return fromPreview
-  }
-
-  return SanityDefaultPreview
+  return type.components?.preview || SanityDefaultPreview
 }
