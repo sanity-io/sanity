92764b7947d8519746fd860b46e8e2307d5e80a3
Espen Hovlandsdal <espen@hovlandsdal.com>
2022-06-14 12:44:40 +0200

    chore(cli): disable unmigrated v2 templates for now

--- packages/@sanity/cli/src/actions/init-project/templates/index.ts
+++ packages/@sanity/cli/src/actions/init-project/templates/index.ts
@@ -2,17 +2,21 @@ import type {ProjectTemplate} from '../initProject'
 import blog from './blog'
 import clean from './clean'
 import moviedb from './moviedb'
+
+/*
+// @todo DISABLED UNTIL THEY WORK WITH v3
 import ecommerce from './ecommerce'
 import getStarted from './get-started'
 import shopify from './shopify'
+*/
 
 const templates: Record<string, ProjectTemplate | undefined> = {
   blog,
   clean,
   moviedb,
-  ecommerce,
-  shopify,
-  'get-started': getStarted,
+  //ecommerce,
+  //shopify,
+  //'get-started': getStarted,
 }
 
 export default templates
