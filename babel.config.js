455129ad6bceb639e80e0a3ab7d2e32942b0867d
Espen Hovlandsdal <espen@hovlandsdal.com>
2022-06-17 14:47:14 +0200

    chore: drop unnecessary eslint rule overrides

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,10 @@
-/* eslint-disable @typescript-eslint/no-var-requires, no-sync, strict */
+/* eslint-disable no-sync, strict */
 
 'use strict'
 
 const fs = require('fs')
 const path = require('path')
 
-// eslint-disable-next-line no-sync
 const babelrc = JSON.parse(fs.readFileSync(path.join(__dirname, '.babelrc'), 'utf8'))
 
 module.exports = {
