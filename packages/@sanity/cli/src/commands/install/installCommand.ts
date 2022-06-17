83201a5de186f512e38619f4578de0316ed5a1de
Espen Hovlandsdal <espen@hovlandsdal.com>
2022-06-17 14:19:58 +0200

    feat(cli): throw error on v2 plugin install with no `@sanity/core` installed

--- packages/@sanity/cli/src/commands/install/installCommand.ts
+++ packages/@sanity/cli/src/commands/install/installCommand.ts
@@ -1,20 +1,38 @@
-import {CliCommandDefinition} from '../../types'
-import {installDependencies} from './installDependencies'
-
-const help = `
-Looks for a "package.json" and/or a "yarn.lock"-file in the current directory,
-then installs or updates dependencies according to these files.
-
-When working in the context of a project, this command transforms to allow
-installation of Sanity-plugins as well.
-`
+import type {CliCommandDefinition} from '../../types'
+import {
+  getPackageManagerChoice,
+  installDeclaredPackages,
+  installNewPackages,
+} from '../../packageManager'
 
 const installCommand: CliCommandDefinition = {
   name: 'install',
   signature: '',
+  helpText: '',
   description: 'Installs dependencies of the current project',
-  action: installDependencies,
-  helpText: help,
+  action: async (args, context) => {
+    const {workDir, prompt, sanityMajorVersion} = context
+    const packages = args.argsWithoutOptions
+    if (packages.length > 0 && sanityMajorVersion === 2) {
+      /**
+       * Note: `@sanity/core` (only used in v2) takes over the `sanity install`
+       * command and provides the ability to install plugins. In v3, we don't
+       * want this, which is why this error differs based on major version.
+       **/
+      throw new Error('Re-run this command with `@sanity/core` installed')
+    }
+
+    const pkgManager = await getPackageManagerChoice(workDir, {prompt})
+
+    if (packages.length > 0) {
+      await installNewPackages(
+        {packageManager: pkgManager.chosen, packages: args.argsWithoutOptions},
+        context
+      )
+    } else {
+      await installDeclaredPackages(workDir, pkgManager.chosen, context)
+    }
+  },
 }
 
 export default installCommand
--- packages/@sanity/cli/src/commands/install/installDependencies.ts
+++
@@ -1,8 +0,0 @@
-import type {CliCommandAction} from '../../types'
-
-export const installDependencies: CliCommandAction = (args, context) => {
-  const {output, workDir, yarn} = context
-  const {extOptions} = args
-  const flags = ['--check-files'].concat(extOptions.offline ? ['--offline'] : [])
-  return yarn(['install'].concat(flags), {...output, rootDir: workDir})
-}
