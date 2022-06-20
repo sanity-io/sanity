97b72acd4449e085b8680c0fd5546e5f412de62a
Per-Kristian Nordnes <per.kristian.nordnes@gmail.com>
2022-06-20 12:21:13 +0200

    refactor(sanity): simplify scrollToFocus hook

--- packages/sanity/src/form/inputs/PortableText/hooks/useScrollToFocusFromOutside.tsx
+++ packages/sanity/src/form/inputs/PortableText/hooks/useScrollToFocusFromOutside.tsx
@@ -1,13 +1,7 @@
-import {
-  PortableTextEditor,
-  usePortableTextEditor,
-  usePortableTextEditorSelection,
-} from '@sanity/portable-text-editor'
+import {PortableTextEditor, usePortableTextEditor} from '@sanity/portable-text-editor'
 import {Path} from '@sanity/types'
-import {isEqual} from '@sanity/util/paths'
 import {useEffect} from 'react'
 import scrollIntoView from 'scroll-into-view-if-needed'
-import {_isBlockType} from '../_helpers'
 import {usePortableTextMemberItems} from './usePortableTextMembers'
 
 interface Props {
@@ -21,11 +15,10 @@ export function useScrollToFocusFromOutside(props: Props): void {
   const {fieldPath, scrollElement, onCloseItem} = props
   const portableTextMemberItems = usePortableTextMemberItems()
   const editor = usePortableTextEditor()
-  const selection = usePortableTextEditorSelection()
 
   // This will scroll to the relevant editor node which has a member that is opened.
   useEffect(() => {
-    // Find the openened member that is most spesific (longest path).
+    // Find the opened member that is most spesific (longest path).
     const memberItem = portableTextMemberItems
       .filter((item) => item.member.open)
       .sort((a, b) => b.member.item.path.length - a.member.item.path.length)[0]
@@ -37,23 +30,17 @@ export function useScrollToFocusFromOutside(props: Props): void {
         boundary: scrollElement,
         scrollMode: 'if-needed',
       })
+      // Set selection in the editor if this is a block
       const relativePath = memberItem.member.item.path.slice(fieldPath.length)
-      // Only set selection and focus if it's not pointing to embedded data (annotations, objects etc)
-      const setSelectionAndFocus =
-        !PortableTextEditor.isObjectPath(editor, relativePath) &&
-        (selection === null ||
-          (selection.focus.path.length && !isEqual(relativePath, selection.focus.path)))
-      if (setSelectionAndFocus) {
+      if (memberItem.kind === 'textBlock') {
         PortableTextEditor.select(editor, {
           anchor: {path: relativePath, offset: 0},
           focus: {path: relativePath, offset: 0},
         })
         PortableTextEditor.focus(editor)
-      }
-      // Auto-close regular blocks
-      if (_isBlockType(memberItem.member.item.schemaType)) {
+        // Auto-close normal textBlock as there is no interface for doing so.
         onCloseItem()
       }
     }
-  }, [editor, selection, fieldPath.length, onCloseItem, portableTextMemberItems, scrollElement])
+  }, [editor, fieldPath.length, onCloseItem, portableTextMemberItems, scrollElement])
 }
