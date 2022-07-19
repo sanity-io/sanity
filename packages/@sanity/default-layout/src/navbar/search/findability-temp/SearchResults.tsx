b5cd2feb4226320e4874b7f9c577971bed1405cb
Robin Pyon <robin.pyon@gmail.com>
2022-07-19 15:21:27 +0100

    feat: add search results error state, focus previous search result when load more is pressed

--- packages/@sanity/default-layout/src/navbar/search/findability-temp/SearchResults.tsx
+++ packages/@sanity/default-layout/src/navbar/search/findability-temp/SearchResults.tsx
@@ -1,6 +1,8 @@
-import {useRovingFocus} from '@sanity/base/components'
-import {Box, Button, Stack} from '@sanity/ui'
-import React, {useCallback, useState} from 'react'
+import {TextWithTone, useRovingFocus} from '@sanity/base/components'
+import {WarningOutlineIcon} from '@sanity/icons'
+import {Box, Button, Flex, Stack} from '@sanity/ui'
+import React, {MouseEvent, useCallback, useState} from 'react'
+import styled from 'styled-components'
 import {Instructions} from './Instructions'
 import {addSearchTerm} from './local-storage/search-store'
 import {SearchResultItem} from './SearchResultItem'
@@ -21,9 +23,20 @@ export function SearchResults(props: SearchResultsProps) {
     state: {terms, result},
   } = useOmnisearch()
 
-  const handleLoadMore = useCallback(() => {
-    dispatch({type: 'PAGE_INCREMENT'})
-  }, [dispatch])
+  // Load next page and focus previous sibling
+  const handleLoadMore = useCallback(
+    (event: MouseEvent<HTMLButtonElement>) => {
+      dispatch({type: 'PAGE_INCREMENT'})
+
+      const previousSibling = event?.currentTarget?.previousElementSibling as HTMLElement
+      if (previousSibling) {
+        previousSibling.focus()
+        previousSibling.setAttribute('aria-selected', 'true')
+        previousSibling.setAttribute('tabIndex', '0')
+      }
+    },
+    [dispatch]
+  )
 
   const handleResultClick = useCallback(() => {
     addSearchTerm(terms)
@@ -40,37 +53,52 @@ export function SearchResults(props: SearchResultsProps) {
   })
 
   return (
-    <Box>
-      {!!result.hits.length && (
-        // (Has search results)
-        <Stack
-          padding={1}
-          ref={setFocusRootElement}
-          space={1}
-          style={{
-            opacity: result.loading ? 0.5 : 1,
-            transition: '300ms opacity',
-          }}
-        >
-          {result.hits.map((hit) => (
-            <SearchResultItem key={hit.hit._id} hit={hit} onClick={handleResultClick} />
-          ))}
-          {result.hasMore && (
-            <Button
-              disabled={result.loading}
-              mode="bleed"
-              onClick={handleLoadMore}
-              text="More"
-              title="Load more search results"
-            />
+    <SearchResultsWrapper loading={result.loading}>
+      {result.error ? (
+        <Flex align="center" direction="column" gap={3} marginY={2} padding={4}>
+          <Box marginBottom={1}>
+            <TextWithTone tone="critical">
+              <WarningOutlineIcon />
+            </TextWithTone>
+          </Box>
+          <TextWithTone size={2} tone="critical" weight="semibold">
+            Something went wrong while searching
+          </TextWithTone>
+          <TextWithTone size={1} tone="critical">
+            Please try again or check your connection
+          </TextWithTone>
+        </Flex>
+      ) : (
+        <>
+          {!!result.hits.length && (
+            // (Has search results)
+            <Stack padding={1} ref={setFocusRootElement} space={1}>
+              {result.hits.map((hit) => (
+                <SearchResultItem key={hit.hit._id} hit={hit} onClick={handleResultClick} />
+              ))}
+              {result.hasMore && (
+                <Button
+                  disabled={result.loading}
+                  mode="bleed"
+                  onClick={handleLoadMore}
+                  text="More"
+                  title="Load more search results"
+                />
+              )}
+            </Stack>
           )}
-        </Stack>
-      )}
 
-      {!result.hits.length && result.loaded && (
-        // (No results)
-        <Instructions />
+          {!result.hits.length && result.loaded && (
+            // (No results)
+            <Instructions />
+          )}
+        </>
       )}
-    </Box>
+    </SearchResultsWrapper>
   )
 }
+
+const SearchResultsWrapper = styled.div<{loading: boolean}>`
+  opacity: ${(props) => (props.loading ? 0.5 : 1)};
+  transition: 300ms opacity;
+`
