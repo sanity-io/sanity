2ed41826ebd5ce04b39d926e731f0bcbdb51fd28
Robin Pyon <robin.pyon@gmail.com>
2022-07-21 23:45:50 +0100

    feat: add support for custom text input background colors, tweak padding in type filters

--- packages/@sanity/default-layout/src/navbar/search/components/CustomTextInput.tsx
+++ packages/@sanity/default-layout/src/navbar/search/components/CustomTextInput.tsx
@@ -1,25 +1,57 @@
+import {hues} from '@sanity/color'
 import {Box, TextInput} from '@sanity/ui'
 import React, {ComponentProps, forwardRef} from 'react'
-import styled from 'styled-components'
+import styled, {css} from 'styled-components'
+
+type BackgroundTone = 'dark' | 'darker'
 
 interface CustomTextInputProps extends ComponentProps<typeof TextInput> {
+  backgroundTone?: BackgroundTone
   smallClearButton?: boolean
 }
 
-export const CustomTextInput = forwardRef<HTMLDivElement, CustomTextInputProps>(
+export const CustomTextInput = forwardRef<HTMLInputElement, CustomTextInputProps>(
   function CustomTextInput(props, ref) {
-    const {smallClearButton, ...rest} = props
+    const {backgroundTone, smallClearButton, ...rest} = props
+
     return (
-      <CustomTextInputWrapper smallClearButton={smallClearButton} ref={ref}>
-        <TextInput {...rest} />
+      <CustomTextInputWrapper backgroundTone={backgroundTone} smallClearButton={smallClearButton}>
+        <TextInput {...rest} ref={ref} />
       </CustomTextInputWrapper>
     )
   }
 )
 
-const CustomTextInputWrapper = styled(Box)<{smallClearButton: boolean}>`
-  [data-qa='clear-button'] {
-    display: flex; /* TODO: hack, currently used to vertically center <TextInput>'s clearButton */
-    transform: ${({smallClearButton}) => (smallClearButton ? 'scale(0.7)' : 'scale(1)')};
+const CustomTextInputWrapper = styled(Box)(
+  ({
+    backgroundTone,
+    smallClearButton,
+  }: {
+    backgroundTone?: BackgroundTone
+    smallClearButton: boolean
+  }) => {
+    let backgroundColor: string
+    if (backgroundTone === 'dark') {
+      backgroundColor = hues.gray[50].hex
+    }
+    if (backgroundTone === 'darker') {
+      backgroundColor = hues.gray[100].hex
+    }
+
+    return css`
+      ${backgroundColor &&
+      css`
+        input + span {
+          background: ${backgroundColor};
+        }
+      `}
+
+      [data-qa='clear-button'] {
+        background: none;
+        box-shadow: none;
+        display: flex; /* TODO: hack, currently used to vertically center <TextInput>'s clearButton */
+        transform: ${smallClearButton ? 'scale(0.7)' : 'scale(1)'};
+      }
+    `
   }
-`
+)
