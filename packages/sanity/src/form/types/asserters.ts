adec4af45639d5fbc6071e4584ae51b05be1f08c
Espen Hovlandsdal <espen@hovlandsdal.com>
2022-07-31 23:07:53 -0700

    feat(form): implement input props type asserters

--- packages/sanity/src/form/types/asserters.ts
+++ packages/sanity/src/form/types/asserters.ts
@@ -1,6 +1,50 @@
-import {isObjectSchemaType} from '@sanity/types'
+import {
+  isArrayOfObjectsSchemaType,
+  isArrayOfPrimitivesSchemaType,
+  isBooleanSchemaType,
+  isNumberSchemaType,
+  isObjectSchemaType,
+  isStringSchemaType,
+} from '@sanity/types'
+import {
+  ArrayOfObjectsInputProps,
+  ArrayOfPrimitivesInputProps,
+  BooleanInputProps,
+  InputProps,
+  NumberInputProps,
+  ObjectInputProps,
+  StringInputProps,
+} from './inputProps'
 import {ItemProps, ObjectItemProps} from './itemProps'
 
 export function isObjectItemProps(item: ItemProps): item is ObjectItemProps {
   return isObjectSchemaType(item.schemaType)
+}
+
+export function isObjectInputProps(inputProps: InputProps): inputProps is ObjectInputProps {
+  return isObjectSchemaType(inputProps.schemaType)
+}
+
+export function isStringInputProps(inputProps: InputProps): inputProps is StringInputProps {
+  return isStringSchemaType(inputProps.schemaType)
+}
+
+export function isNumberInputProps(inputProps: InputProps): inputProps is NumberInputProps {
+  return isNumberSchemaType(inputProps.schemaType)
+}
+
+export function isBooleanInputProps(inputProps: InputProps): inputProps is BooleanInputProps {
+  return isBooleanSchemaType(inputProps.schemaType)
+}
+
+export function isArrayOfObjectsInputProps(
+  inputProps: InputProps
+): inputProps is ArrayOfObjectsInputProps {
+  return isArrayOfObjectsSchemaType(inputProps.schemaType)
+}
+
+export function isArrayOfPrimitivesInputProps(
+  inputProps: InputProps
+): inputProps is ArrayOfPrimitivesInputProps {
+  return isArrayOfPrimitivesSchemaType(inputProps.schemaType)
 }
