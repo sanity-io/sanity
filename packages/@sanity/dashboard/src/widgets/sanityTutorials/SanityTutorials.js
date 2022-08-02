9103669370713454af6f3356b6fe313e850f0056
Victoria Bergquist <victoria.bergquist@gmail.com>
2022-08-02 22:33:32 +0200

    feat(dashboard): add docs banner to sanity tutorials widget (#3446)

--- packages/@sanity/dashboard/src/widgets/sanityTutorials/SanityTutorials.js
+++ packages/@sanity/dashboard/src/widgets/sanityTutorials/SanityTutorials.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import {Flex, Grid, Stack, Heading, Container} from '@sanity/ui'
+import {Flex, Grid, Stack, Heading, Container, Card, Text, Box, Button} from '@sanity/ui'
 import Tutorial from './Tutorial'
 import dataAdapter from './dataAdapter'
 
@@ -67,6 +67,7 @@ class SanityTutorials extends React.Component {
 
   render() {
     const {title = 'Learn about Sanity', feedItems} = this.state
+    const {templateRepoId} = this.props
 
     // Filter out items and sections for layout purposes
     const sections = feedItems.filter((i) => i._type === 'feedSection')
@@ -76,7 +77,30 @@ class SanityTutorials extends React.Component {
 
     return (
       <Container width={4}>
-        <Stack space={5} paddingBottom={4}>
+        <Stack space={6} paddingBottom={4}>
+          <Card tone="primary" padding={4} radius={2} border marginTop={4}>
+            <Flex direction={['column', 'column', 'row']}>
+              <Stack space={4} flex={1} paddingRight={[0, 0, 4]}>
+                <Heading>Getting started Guide</Heading>
+                <Text>
+                  It’s time to learn how to build schemas, create content and connect it with other
+                  applications.
+                </Text>
+              </Stack>
+              <Flex paddingTop={[4, 4, 0]} align="center">
+                <Stack flex={1}>
+                  <Button
+                    paddingY={3}
+                    paddingX={5}
+                    tone="primary"
+                    as="a"
+                    href={`https://www.sanity.io/docs?ref=dashboard-${templateRepoId || 'plugin'}`}
+                    text="Go go docs"
+                  />
+                </Stack>
+              </Flex>
+            </Flex>
+          </Card>
           {sections &&
             sections?.length > 0 &&
             sections.map((section) => {
