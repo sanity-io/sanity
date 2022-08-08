26fc71ee2451df064c11842bf96cc1e5900bb3fa
Robin Pyon <robin.pyon@gmail.com>
2022-08-09 00:40:20 +0100

    fix(default-layout): re-use getSearchableTypes from base, dont include internal sanity types in document type list

--- packages/@sanity/default-layout/src/navbar/search/contexts/search/selectors.ts
+++ packages/@sanity/default-layout/src/navbar/search/contexts/search/selectors.ts
@@ -1,38 +1,21 @@
-import {ObjectSchemaType, SchemaType} from '@sanity/types'
+import {ObjectSchemaType, Schema, SchemaType} from '@sanity/types'
+import {getSearchableTypes} from '@sanity/base/_internal'
 
 /**
  * Returns a list of all available document types filtered by a search string.
  * Types containing the search string in its `title` or `name` will be returned.
  */
-export function getSelectableTypes(
-  schema: {
-    get: (typeName: string) => SchemaType | undefined
-    getTypeNames(): string[]
-  },
-  // selectedTypes: SchemaType[],
-  typeFilter: string
-): ObjectSchemaType[] {
-  return (
-    schema
-      .getTypeNames()
-      .map((n) => schema.get(n))
-      .filter((s): s is ObjectSchemaType => s && s.jsonType === 'object')
-      .filter((s) => getRootType(s)?.name === 'document' && s.name !== 'document')
-      //.filter((s) => !selectedTypes.includes(s))
-      .filter(
-        (t) => !typeFilter || (t.title ?? t.name).toLowerCase().includes(typeFilter?.toLowerCase())
-      )
-      .sort(sortTypes)
-  )
-}
-
-function getRootType(type: SchemaType): SchemaType {
-  if (!type.type) {
-    return type
-  }
-  return getRootType(type.type)
+export function getSelectableTypes(schema: Schema, typeFilter: string): ObjectSchemaType[] {
+  // TODO: double check return type of `getSearchableTypes`
+  return (getSearchableTypes(schema) as ObjectSchemaType[])
+    .filter((type) => inTypeFilter(type, typeFilter))
+    .sort(sortTypes)
 }
 
 export function sortTypes(a: SchemaType, b: SchemaType): number {
   return (a.title ?? a.name).localeCompare(b.title ?? b.name)
+}
+
+function inTypeFilter(type: SchemaType, typeFilter: string): boolean {
+  return !typeFilter || (type.title ?? type.name).toLowerCase().includes(typeFilter?.toLowerCase())
 }
