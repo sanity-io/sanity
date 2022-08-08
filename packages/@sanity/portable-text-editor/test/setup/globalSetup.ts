74f060aec29af53f4622f93119367d245cc4a56f
Per-Kristian Nordnes <per.kristian.nordnes@gmail.com>
2022-08-08 15:18:04 +0200

    test(portable-text-editor): update test server start script for vite 3

--- packages/@sanity/portable-text-editor/test/setup/globalSetup.ts
+++ packages/@sanity/portable-text-editor/test/setup/globalSetup.ts
@@ -6,14 +6,14 @@ const testFolderPath = path.resolve(__dirname, '..')
 module.exports = async function globalSetup() {
   await setupDevServer([
     {
-      command: `vite ${testFolderPath}/web-server`,
+      command: `vite --port 3000 ${testFolderPath}/web-server`,
       launchTimeout: 10000,
-      port: 3000,
     },
     {
       command: `node -r esbuild-register ${testFolderPath}/ws-server`,
       launchTimeout: 10000,
       port: 3001,
+      debug: true,
     },
   ])
 }
