30243f171f233cee380244ca3143b8cbe3f76720
Espen Hovlandsdal <espen@hovlandsdal.com>
2022-08-07 23:41:23 -0700

    feat(vision): push toast indicating URL has been copied

--- packages/@sanity/vision/src/containers/VisionContainer.tsx
+++ packages/@sanity/vision/src/containers/VisionContainer.tsx
@@ -1,3 +1,4 @@
+import {useToast} from '@sanity/ui'
 import React from 'react'
 import {DelayedSpinner} from '../components/DelayedSpinner'
 import {VisionGui} from '../components/VisionGui'
@@ -5,6 +6,7 @@ import {useDatasets} from '../hooks/useDatasets'
 import type {VisionProps} from '../types'
 
 export function VisionContainer(props: VisionProps) {
+  const toast = useToast()
   const loadedDatasets = useDatasets(props.client)
   if (!loadedDatasets) {
     return <DelayedSpinner />
@@ -17,5 +19,5 @@ export function VisionContainer(props: VisionProps) {
       : // Otherwise use the loaded list, obviously
         loadedDatasets
 
-  return <VisionGui {...props} datasets={datasets} />
+  return <VisionGui {...props} datasets={datasets} toast={toast} />
 }
