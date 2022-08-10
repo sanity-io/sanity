86f8b4864f810f956e3108584de3e5ec39a4aa31
Herman Wikner <wiknerherman@gmail.com>
2022-08-10 16:19:31 +0200

    feat(form): add helper function to get correct field level in `inputResolver`

--- packages/sanity/src/form/studio/inputResolver/helpers.ts
+++ packages/sanity/src/form/studio/inputResolver/helpers.ts
@@ -1,6 +1,38 @@
 import {SchemaType} from '@sanity/types'
 import {get} from 'lodash'
+import {ArrayFieldProps, ObjectFieldProps} from '../../types'
 
 export function getOption(type: SchemaType, optionName: string) {
   return get(type.options, optionName)
+}
+
+const PSEUDO_OBJECTS = ['array', 'file', 'image', 'reference']
+const HIDDEN_FIELDS = ['asset', 'crop', 'hotspot', '_ref', '_weak']
+
+export function getObjectFieldLevel(field: ObjectFieldProps): number {
+  const {type, fields, options} = field.schemaType
+  const fieldType = type?.name || ''
+
+  const isPseudoObject = PSEUDO_OBJECTS.includes(fieldType)
+
+  const hasVisibleFields = fields?.filter((f) => !HIDDEN_FIELDS.includes(f.name)).length > 0
+  const hasListOptions = options?.list?.length > 0
+
+  if (hasVisibleFields || hasListOptions || !isPseudoObject) {
+    return field.level
+  }
+
+  return 0
+}
+
+export function getArrayFieldLevel(field: ArrayFieldProps): number {
+  const {options} = field.schemaType
+
+  const hasListOptions = (options?.list || [])?.length > 0
+
+  if (hasListOptions) {
+    return field.level
+  }
+
+  return 0
 }
