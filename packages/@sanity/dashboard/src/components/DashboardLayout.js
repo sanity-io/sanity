007e4f5792345d50be8558824beef8c1027fedb5
Fred Carlsen <fred@sjelfull.no>
2022-08-12 12:34:16 +0200

    feat(dashboard): add width layout option to dashboard

--- packages/@sanity/dashboard/src/components/DashboardLayout.js
+++ packages/@sanity/dashboard/src/components/DashboardLayout.js
@@ -1,18 +1,38 @@
 import PropTypes from 'prop-types'
-import React from 'react'
+import React, {useMemo} from 'react'
 import {Container} from '@sanity/ui'
 
 function DashboardLayout(props) {
+  const {width} = props
+  const formattedWidth = useMemo(() => getWidth(width), [width])
+
   return (
-    <Container width={4} padding={4} sizing="border" style={{minHeight: '100%'}}>
+    <Container
+      width={formattedWidth}
+      padding={4}
+      sizing="border"
+      style={{minHeight: '100%'}}
+      data-name="dashboard-layout"
+    >
       {props.children}
     </Container>
   )
 }
 
+function getWidth(value) {
+  const widthMap = {
+    small: 2,
+    medium: 3,
+    large: 4,
+  }
+
+  return widthMap?.[value] || widthMap.large
+}
+
 DashboardLayout.propTypes = {
   // eslint-disable-next-line react/forbid-prop-types
   children: PropTypes.any,
+  width: PropTypes.oneOf(['small', 'medium', 'large']),
 }
 
 DashboardLayout.defaultProps = {
