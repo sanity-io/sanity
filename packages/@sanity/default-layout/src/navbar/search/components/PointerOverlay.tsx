727f927a244c41c05c3ec6c76d47ea6f3ec25770
Robin Pyon <robin.pyon@gmail.com>
2022-08-12 16:32:27 +0100

    fix(default-layout): ensure pointer overlays don't appear on devices with hover capabilities

--- packages/@sanity/default-layout/src/navbar/search/components/PointerOverlay.tsx
+++ packages/@sanity/default-layout/src/navbar/search/components/PointerOverlay.tsx
@@ -1,20 +1,28 @@
-import {Box} from '@sanity/ui'
 import React, {forwardRef} from 'react'
 import styled from 'styled-components'
 
 /**
- * Simple hidden component which conditionally appears over command palette items to cancel
- * existing :hover states of any child elements.
+ * Simple component which conditionally appears over command list items to cancel
+ * existing :hover states for all child elements.
+ *
+ * It should only appear if hover capabilities are available (not on touch devices)
  */
 export const PointerOverlay = forwardRef<HTMLDivElement>(function PointerOverlay(_props, ref) {
   return <PointerOverlayWrapper aria-hidden="true" ref={ref} />
 })
 
-const PointerOverlayWrapper = styled(Box)`
+const PointerOverlayWrapper = styled.div`
   bottom: 0;
+  display: none;
   left: 0;
   position: absolute;
   right: 0;
   top: 0;
   z-index: 1;
+
+  @media (hover: hover) {
+    &[data-enabled='true'] {
+      display: block;
+    }
+  }
 `
