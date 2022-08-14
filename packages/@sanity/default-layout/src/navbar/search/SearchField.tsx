e757ffd00159d723e8b63b5e279812e04e6407ff
Robin Pyon <robin.pyon@gmail.com>
2022-08-14 17:03:35 +0100

    refactor(default-layout): extract placeholder positioning logic out of search popover, avoid passing ref

--- packages/@sanity/default-layout/src/navbar/search/SearchField.tsx
+++ packages/@sanity/default-layout/src/navbar/search/SearchField.tsx
@@ -1,15 +1,19 @@
 import {Portal} from '@sanity/ui'
-import React, {useCallback, useRef, useState} from 'react'
+import React, {useCallback, useEffect, useState} from 'react'
 import FocusLock from 'react-focus-lock'
 import {PlaceholderSearchInput} from './components/PlaceholderSearchInput'
 import {SearchPopover} from './components/SearchPopover'
+import {POPOVER_INPUT_PADDING, POPOVER_MAX_WIDTH} from './constants'
 import {useMeasureSearchResultsIndex} from './hooks/useMeasureSearchResultsIndex'
 import {useSearchHotkeys} from './hooks/useSearchHotkeys'
 
 export function SearchField() {
-  const childContainerRef = useRef<HTMLDivElement>(null)
-  const placeholderInputRef = useRef<HTMLInputElement>()
+  const [placeholderElement, setPlaceholderElement] = useState<HTMLInputElement | null>(null)
   const [open, setOpen] = useState(false)
+  const [popoverPosition, setPopoverPosition] = useState<{
+    x: number
+    y: number
+  }>(null)
 
   /**
    * Measure top-most visible search result index
@@ -29,6 +33,13 @@ export function SearchField() {
   const handleOpen = useCallback(() => setOpen(true), [])
 
   /**
+   * Update popover position on window resize based off current placeholder input
+   */
+  const handleWindowResize = useCallback(() => {
+    setPopoverPosition(calcDialogPosition(placeholderElement))
+  }, [placeholderElement])
+
+  /**
    * Bind hotkeys to open / close actions
    */
   useSearchHotkeys({
@@ -37,21 +48,52 @@ export function SearchField() {
     open,
   })
 
+  useEffect(() => {
+    setPopoverPosition(calcDialogPosition(placeholderElement))
+  }, [placeholderElement])
+
+  useEffect(() => {
+    window.addEventListener('resize', handleWindowResize)
+    return () => window.removeEventListener('resize', handleWindowResize)
+  }, [handleWindowResize])
+
   return (
     <>
-      <PlaceholderSearchInput onOpen={handleOpen} ref={placeholderInputRef} />
+      <PlaceholderSearchInput onOpen={handleOpen} ref={setPlaceholderElement} />
       {open && (
         <Portal>
           <FocusLock>
             <SearchPopover
-              childContainerRef={childContainerRef}
               initialSearchIndex={savedSearchIndex}
               onClose={handleClose}
-              placeholderRef={placeholderInputRef}
+              position={popoverPosition}
             />
           </FocusLock>
         </Portal>
       )}
     </>
   )
+}
+
+function calcDialogPosition(
+  element: HTMLElement
+): {
+  x: number | null
+  y: number
+} {
+  if (!element) {
+    return null
+  }
+
+  const placeholderRect = element.getBoundingClientRect()
+
+  // Offset positioning to account for dialog padding. This should ensure that our popover search input
+  // sits directly over the top of the existing placeholder input.
+  return {
+    x:
+      window.innerWidth - placeholderRect.x > POPOVER_MAX_WIDTH
+        ? placeholderRect.x - POPOVER_INPUT_PADDING
+        : null,
+    y: placeholderRect.y - POPOVER_INPUT_PADDING,
+  }
 }
