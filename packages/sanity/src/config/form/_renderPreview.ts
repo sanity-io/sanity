e68b3402f1857cdc4c19507b1f59d07127aafb2d
Marius Lundgård <studio@mariuslundgard.com>
2022-08-15 16:17:30 +0200

    fix(sanity): collect render middleware recursively

--- packages/sanity/src/config/form/_renderPreview.ts
+++ packages/sanity/src/config/form/_renderPreview.ts
@@ -2,8 +2,24 @@ import {SchemaType} from '@sanity/types'
 import {PreviewProps} from '../../components/previews'
 import {RenderPreviewCallback} from '../../form'
 import {defaultRenderPreview} from '../../form/studio/defaults'
-import {SourceOptions} from '../types'
+import {PluginOptions, SourceOptions} from '../types'
 import {_RenderMiddleware} from './_types'
+
+function _collectMiddleware(
+  middlewares: _RenderMiddleware<PreviewProps & {schemaType: SchemaType}, RenderPreviewCallback>[],
+  plugins: PluginOptions[]
+) {
+  for (const plugin of plugins) {
+    // Recursive
+    if (plugin.plugins) {
+      _collectMiddleware(middlewares, plugin.plugins)
+    }
+
+    if (plugin.form?.renderPreview) {
+      middlewares.push(plugin.form?.renderPreview)
+    }
+  }
+}
 
 /**
  * @internal
@@ -16,11 +32,7 @@ export function _createRenderPreview(config: SourceOptions): RenderPreviewCallba
     >[] = []
 
     if (config.plugins) {
-      for (const plugin of config.plugins) {
-        if (plugin.form?.renderPreview) {
-          middlewares.push(plugin.form?.renderPreview)
-        }
-      }
+      _collectMiddleware(middlewares, config.plugins)
     }
 
     if (config.form?.renderPreview) {
