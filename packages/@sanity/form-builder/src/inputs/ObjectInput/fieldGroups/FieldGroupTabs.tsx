900419aed9578f10268b7405e8bce0ddf0b63392
Fred Carlsen <fred@sjelfull.no>
2022-08-17 13:48:45 +0200

    fix(form-builder): make sure to forward ref for object input with tabs (#3522)
    Co-authored-by: Bjørge Næss <bjoerge@gmail.com>

--- packages/@sanity/form-builder/src/inputs/ObjectInput/fieldGroups/FieldGroupTabs.tsx
+++ packages/@sanity/form-builder/src/inputs/ObjectInput/fieldGroups/FieldGroupTabs.tsx
@@ -1,8 +1,10 @@
-import React, {useCallback, useEffect, useMemo, useState} from 'react'
+/* This is disabled to work around a bug with eslint-plugin-react */
+/* eslint-disable react/no-unused-prop-types */
+import React, {ForwardedRef, forwardRef, useCallback} from 'react'
 import {ElementQuery, Select, TabList} from '@sanity/ui'
 import {FieldGroup} from '@sanity/types/src'
 import styled from 'styled-components'
-import {GroupTab, GroupOption} from './GroupTab'
+import {GroupOption, GroupTab} from './GroupTab'
 
 interface FieldGroupTabsProps {
   inputId: string
@@ -30,39 +32,38 @@ const Root = styled(ElementQuery)`
     display: block;
   }
 `
-
 /* For medium to large screens, use TabList and Tab from Sanity UI  */
-const GroupTabs = ({
-  inputId,
-  groups,
-  onClick,
-  selectedName,
-  shouldAutoFocus = true,
-  disabled,
-}: FieldGroupTabsProps) => (
-  <TabList space={2} data-testid="field-group-tabs">
-    {groups
-      .map((group) => {
-        const {fields, ...restGroup} = group
+// disable eslint false positive
+// eslint-disable-next-line react/display-name
+const GroupTabs = forwardRef(
+  (
+    {inputId, groups, onClick, selectedName, shouldAutoFocus = true, disabled}: FieldGroupTabsProps,
+    forwardedRef: ForwardedRef<HTMLButtonElement>
+  ) => (
+    <TabList space={2} data-testid="field-group-tabs" ref={forwardedRef}>
+      {groups
+        .map((group) => {
+          const {fields, ...restGroup} = group
 
-        if (!fields || fields.length === 0) {
-          return null
-        }
-        return (
-          <GroupTab
-            key={`${inputId}-${group.name}-tab`}
-            aria-controls={`${inputId}-field-group-fields`}
-            onClick={onClick}
-            selected={selectedName === group.name ?? group.default}
-            autoFocus={selectedName === group.name && shouldAutoFocus}
-            parent={groups}
-            disabled={disabled}
-            {...restGroup}
-          />
-        )
-      })
-      .filter(Boolean)}
-  </TabList>
+          if (!fields || fields.length === 0) {
+            return null
+          }
+          return (
+            <GroupTab
+              key={`${inputId}-${group.name}-tab`}
+              aria-controls={`${inputId}-field-group-fields`}
+              onClick={onClick}
+              selected={selectedName === group.name ?? group.default}
+              autoFocus={selectedName === group.name && shouldAutoFocus}
+              parent={groups}
+              disabled={disabled}
+              {...restGroup}
+            />
+          )
+        })
+        .filter(Boolean)}
+    </TabList>
+  )
 )
 
 /* For small screens, use Select from Sanity UI  */
@@ -114,24 +115,27 @@ const GroupSelect = ({
   )
 }
 
-export const FieldGroupTabs = React.memo(function FieldGroupTabs({
-  onClick,
-  disabled = false,
-  ...props
-}: FieldGroupTabsProps) {
-  const {groups} = props
+// disable eslint false positive
+// eslint-disable-next-line react/display-name
+export const FieldGroupTabs = React.memo(
+  forwardRef(function FieldGroupTabs(
+    {onClick, disabled = false, ...props}: FieldGroupTabsProps,
+    forwardedRef: ForwardedRef<any>
+  ) {
+    const {groups} = props
 
-  const handleClick = useCallback(
-    (groupName) => {
-      onClick(groupName)
-    },
-    [onClick]
-  )
+    const handleClick = useCallback(
+      (groupName) => {
+        onClick(groupName)
+      },
+      [onClick]
+    )
 
-  return (
-    <Root data-testid="field-group-root">
-      <GroupTabs {...props} disabled={disabled} onClick={handleClick} />
-      <GroupSelect {...props} disabled={disabled} onSelect={handleClick} />
-    </Root>
-  )
-})
+    return (
+      <Root data-testid="field-group-root">
+        <GroupTabs {...props} ref={forwardedRef} disabled={disabled} onClick={handleClick} />
+        <GroupSelect {...props} disabled={disabled} onSelect={handleClick} />
+      </Root>
+    )
+  })
+)
