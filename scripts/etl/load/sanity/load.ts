c95e2e6dec3786ce44f8cbe2cac136ac2c767be5
Marius Lundgård <studio@mariuslundgard.com>
2022-08-23 14:51:24 +0200

    build: update `etl` script

--- scripts/etl/load/sanity/load.ts
+++ scripts/etl/load/sanity/load.ts
@@ -1,8 +1,9 @@
 import createSanityClient, {SanityDocument} from '@sanity/client'
+import {TransformResult} from '@sanity/tsdoc-to-portable-text'
 import chalk from 'chalk'
 import {config} from '../../config'
 
-export async function loadToSanity(docs: SanityDocument[]): Promise<void> {
+export async function loadToSanity(docs: TransformResult): Promise<void> {
   if (!config.sanity.token) {
     // prettier-ignore
     console.log(
@@ -21,7 +22,7 @@ export async function loadToSanity(docs: SanityDocument[]): Promise<void> {
   let tx = sanityClient.transaction()
 
   for (const doc of docs) {
-    tx = tx.createOrReplace(doc)
+    tx = tx.createOrReplace(doc as unknown as SanityDocument)
   }
 
   await tx.commit()
