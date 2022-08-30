c69a48c2ad21b18b4989810e09f27733fbf58bc6
Espen Hovlandsdal <espen@hovlandsdal.com>
2022-08-30 16:02:22 -0700

    refactor: optimize document form presence
    When any item within the presence stream changed location, an update was triggered - even when the result
    was an empty array. This lead to re-renders when any person in any document changed location, instead of
    only affecting the current document. In additional, changes to the `lastActiveAt` field is not interesting
    to us since we do not use this field anywhere. Ideally we would drop it from the types altogether, but that
    would be a breaking change.

--- packages/@sanity/base/src/datastores/presence/useDocumentPresence.tsx
+++ packages/@sanity/base/src/datastores/presence/useDocumentPresence.tsx
@@ -1,16 +1,29 @@
+import {isEqualWith} from 'lodash'
+import {distinctUntilChanged} from 'rxjs/operators'
 import type {User} from '@sanity/types'
 import {useState, useEffect} from 'react'
 import type {DocumentPresence} from './types'
 import {documentPresence, documentPresenceUsers} from './presence-store'
 
-export function useDocumentPresence(documentId): DocumentPresence[] {
+export interface UseDocumentPresenceOptions {
+  ignoreLastActiveUpdates?: boolean
+}
+
+export function useDocumentPresence(
+  documentId,
+  {ignoreLastActiveUpdates}: UseDocumentPresenceOptions = {}
+): DocumentPresence[] {
   const [presence, setPresence] = useState<DocumentPresence[]>([])
   useEffect(() => {
-    const subscription = documentPresence(documentId).subscribe(setPresence)
+    const presence$ = documentPresence(documentId)
+    const mapped$ = ignoreLastActiveUpdates
+      ? presence$.pipe(distinctUntilChanged((x, y) => isEqualWith(x, y, trueIfLastActiveAt)))
+      : presence$
+    const subscription = mapped$.subscribe(setPresence)
     return () => {
       subscription.unsubscribe()
     }
-  }, [documentId])
+  }, [documentId, ignoreLastActiveUpdates])
   return presence
 }
 
@@ -30,4 +43,18 @@ export function useDocumentPresenceUsers(documentId): User[] {
     }
   }, [documentId])
   return presence
+}
+
+/**
+ * For use with `isEqualWith` - returning `undefined` will use the default
+ * `isEqual` algorithm, while returning `true` will treat that path as
+ * equal. We do this for the `lastActiveAt` property at any level, in order
+ * to disregard the timestamp in equality comparisons, if specified
+ */
+function trueIfLastActiveAt(
+  a: unknown,
+  b: unknown,
+  key: string | number | symbol
+): true | undefined {
+  return key === 'lastActiveAt' ? true : undefined
 }
