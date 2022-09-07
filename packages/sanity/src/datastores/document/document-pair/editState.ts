1eab7ba89cee881f865296640b03c5890100da8c
Espen Hovlandsdal <espen@hovlandsdal.com>
2022-09-07 15:02:01 -0700

    fix(datastores): memoize edit state in document store instance (#3565)

--- packages/sanity/src/datastores/document/document-pair/editState.ts
+++ packages/sanity/src/datastores/document/document-pair/editState.ts
@@ -4,7 +4,6 @@ import {Observable} from 'rxjs'
 import {map, publishReplay, refCount, startWith} from 'rxjs/operators'
 import {HistoryStore} from '../../history'
 import {IdPair} from '../types'
-import {memoize} from '../utils/createMemoizer'
 import {operationArgs} from './operationArgs'
 import {isLiveEditEnabled} from './utils/isLiveEditEnabled'
 
@@ -18,37 +17,34 @@ export interface EditStateFor {
   ready: boolean
 }
 
-export const editState = memoize(
-  (
-    ctx: {
-      client: SanityClient
-      historyStore: HistoryStore
-      schema: Schema
-    },
-    idPair: IdPair,
-    typeName: string
-  ): Observable<EditStateFor> => {
-    const liveEdit = isLiveEditEnabled(ctx.schema, typeName)
-    return operationArgs(ctx, idPair, typeName).pipe(
-      map(({snapshots}) => ({
-        id: idPair.publishedId,
-        type: typeName,
-        draft: snapshots.draft,
-        published: snapshots.published,
-        liveEdit,
-        ready: true,
-      })),
-      startWith({
-        id: idPair.publishedId,
-        type: typeName,
-        draft: null,
-        published: null,
-        liveEdit,
-        ready: false,
-      }),
-      publishReplay(1),
-      refCount()
-    )
+export const editState = (
+  ctx: {
+    client: SanityClient
+    historyStore: HistoryStore
+    schema: Schema
   },
-  (_ctx, idPair, typeName) => idPair.publishedId + typeName
-)
+  idPair: IdPair,
+  typeName: string
+): Observable<EditStateFor> => {
+  const liveEdit = isLiveEditEnabled(ctx.schema, typeName)
+  return operationArgs(ctx, idPair, typeName).pipe(
+    map(({snapshots}) => ({
+      id: idPair.publishedId,
+      type: typeName,
+      draft: snapshots.draft,
+      published: snapshots.published,
+      liveEdit,
+      ready: true,
+    })),
+    startWith({
+      id: idPair.publishedId,
+      type: typeName,
+      draft: null,
+      published: null,
+      liveEdit,
+      ready: false,
+    }),
+    publishReplay(1),
+    refCount()
+  )
+}
