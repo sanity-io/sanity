3f34928024f212cd816e5bf19edf2b856c9767e6
Espen Hovlandsdal <espen@hovlandsdal.com>
2022-09-09 13:37:53 -0700

    feat(types): add missing `Geopoint` data type

--- packages/@sanity/types/src/schema/types.ts
+++ packages/@sanity/types/src/schema/types.ts
@@ -762,3 +762,30 @@ export interface ImageSchemaType extends Omit<ObjectSchemaType, 'options'> {
     sources?: AssetSource[]
   }
 }
+
+/**
+ * Geographical point representing a pair of latitude and longitude coordinates,
+ * stored as degrees, in the World Geodetic System 1984 (WGS 84) format. Also
+ * includes an optional `alt` property representing the altitude in meters.
+ */
+export interface Geopoint {
+  /**
+   * Type of the object. Must be `geopoint`.
+   */
+  _type: 'geopoint'
+
+  /**
+   * Latitude in degrees
+   */
+  lat: number
+
+  /**
+   * Longitude in degrees
+   */
+  lng: number
+
+  /**
+   * Altitude in meters
+   */
+  alt?: number
+}
