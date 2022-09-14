47efa75727d2088a63cadcee548ab9c5de575b5e
Snorre Eskeland Brekke <snorre.e.brekke@gmail.com>
2022-09-14 12:46:33 +0200

    fix: use sanity/ui checkerboard

--- packages/sanity/src/form/components/Checkerboard.tsx
+++ packages/sanity/src/form/components/Checkerboard.tsx
@@ -1,21 +1,10 @@
-import styled from 'styled-components'
-import {hues} from '@sanity/color'
-import type {ThemeProps} from '@sanity/ui'
+import React, {type PropsWithChildren} from 'react'
+import {Card, type CardProps} from '@sanity/ui'
 
-function gradientColor({theme}: ThemeProps) {
-  return theme.sanity.color.dark ? '#323232' : hues.gray[50].hex
+export const Checkerboard = ({children, ...rest}: PropsWithChildren<CardProps>) => {
+  return (
+    <Card __unstable_checkered {...rest}>
+      {children}
+    </Card>
+  )
 }
-
-function backgroundColor({theme}: ThemeProps) {
-  return theme.sanity.color.dark ? '#222222' : hues.gray[100].hex
-}
-
-export const Checkerboard = styled.div`
-  background-color: ${backgroundColor};
-  background-image: linear-gradient(45deg, ${gradientColor} 25%, transparent 25%),
-    linear-gradient(-45deg, ${gradientColor} 25%, transparent 25%),
-    linear-gradient(45deg, transparent 75%, ${gradientColor} 75%),
-    linear-gradient(-45deg, transparent 75%, ${gradientColor} 75%);
-  background-size: 16px 16px;
-  background-position: 0 0, 0 8px, 8px -8px, -8px 0;
-`
