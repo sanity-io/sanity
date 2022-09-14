4cd3fe8d9de9a514697a3717d3980b7032c98c91
Snorre Eskeland Brekke <snorre.e.brekke@gmail.com>
2022-09-14 13:00:31 +0200

    fix: checkerboard props

--- packages/sanity/src/form/components/Checkerboard.tsx
+++ packages/sanity/src/form/components/Checkerboard.tsx
@@ -1,7 +1,10 @@
-import React, {type PropsWithChildren} from 'react'
+import React from 'react'
 import {Card, type CardProps} from '@sanity/ui'
 
-export const Checkerboard = ({children, ...rest}: PropsWithChildren<CardProps>) => {
+export const Checkerboard = ({
+  children,
+  ...rest
+}: CardProps & Omit<React.HTMLProps<HTMLDivElement>, 'as' | 'height' | 'ref'>) => {
   return (
     <Card __unstable_checkered {...rest}>
       {children}
