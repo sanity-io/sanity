90a4b48de7bded1dedb833818ce5406ee3d06c9e
Herman Wikner <wiknerherman@gmail.com>
2022-09-14 13:13:51 +0200

    refactor(sanity): rename `ToolMenu` to `StudioToolMenu`, update API and add test id:s

---
+++ packages/sanity/src/studio/components/navbar/tools/StudioToolMenu.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import {ToolMenuProps} from '../../../../config'
+import {ToolCollapseMenu} from './ToolCollapseMenu'
+import {ToolVerticalMenu} from './ToolVerticalMenu'
+
+export function StudioToolMenu(props: ToolMenuProps) {
+  const {context, isSidebarOpen, ...restProps} = props
+
+  if (context === 'sidebar') {
+    return <ToolVerticalMenu isVisible={isSidebarOpen} {...restProps} />
+  }
+
+  return <ToolCollapseMenu {...restProps} />
+}
