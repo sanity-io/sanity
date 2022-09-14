e74b79b0cb32c7a3ea59bc8dc04913371a3a9e70
Herman Wikner <wiknerherman@gmail.com>
2022-09-14 13:16:46 +0200

    feat(sanity): implement `Layout` from `components.studio` in `Studio`

--- packages/sanity/src/studio/Studio.tsx
+++ packages/sanity/src/studio/Studio.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
 import {StudioProvider, StudioProviderProps} from './StudioProvider'
-import {StudioLayout} from './StudioLayout'
+import {useWorkspace} from './workspace'
 
 export type StudioProps = Omit<StudioProviderProps, 'children'>
+
+function StudioLayout() {
+  const {Layout} = useWorkspace().studio.components
+
+  return <Layout />
+}
 
 export function Studio(props: StudioProps) {
   return (
