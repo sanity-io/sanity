89d682e5296b9172cfbd2c2bea1d3fb35028982c
Herman Wikner <wiknerherman@gmail.com>
2022-09-14 13:21:56 +0200

    test(test-studio): update custom `ToolMenu`

--- dev/test-studio/components/navbar/ToolMenu.tsx
+++ dev/test-studio/components/navbar/ToolMenu.tsx
@@ -1,19 +1,19 @@
 import {Button, Flex} from '@sanity/ui'
 import React from 'react'
-import {ToolMenuProps, ToolLink} from 'sanity'
+import {ToolLink, ToolMenuProps} from 'sanity'
 
 export function ToolMenu(props: ToolMenuProps) {
-  const {context, tools, closeDrawer} = props
+  const {context, tools, closeSidebar} = props
 
   return (
-    <Flex gap={3} direction={context === 'drawer' ? 'column' : 'row'}>
+    <Flex gap={3} direction={context === 'sidebar' ? 'column' : 'row'}>
       {tools.map((tool) => (
         <ToolLink key={tool.name} name={tool.name}>
           {tool.title}
         </ToolLink>
       ))}
 
-      {context === 'drawer' && <Button text="Close drawer" onClick={closeDrawer} />}
+      {context === 'topbar' && <Button text="Close drawer" onClick={closeSidebar} />}
     </Flex>
   )
 }
