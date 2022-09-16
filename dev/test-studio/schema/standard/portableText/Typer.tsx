79674a68efb47471f81189a75a506e18194a7ca5
Bjørge Næss <bjoerge@gmail.com>
2022-09-16 14:33:56 +0200

    debug(test-studio): add a test scenario for background validation

---
+++ dev/test-studio/schema/standard/portableText/Typer.tsx
@@ -0,0 +1,31 @@
+import React, {useCallback, useEffect} from 'react'
+
+const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+function getRandomCharacter() {
+  return characters.charAt(Math.floor(Math.random() * characters.length))
+}
+
+export function Typer() {
+  const [text, setText] = React.useState('')
+  const [enabled, setEnabled] = React.useState(false)
+  const handleCheckboxChange = useCallback((e) => {
+    setEnabled(e.target.checked)
+  }, [])
+  useEffect(() => {
+    let i
+    if (enabled) {
+      i = setInterval(() => {
+        setText((t) => `${getRandomCharacter()} ${t} `.substring(0, 10))
+      }, 16)
+    }
+    return () => {
+      clearInterval(i)
+    }
+  }, [enabled])
+  return (
+    <div>
+      <div>Enable 60fps updates</div>
+      <input type="checkbox" checked={enabled} onChange={handleCheckboxChange} /> {text}
+    </div>
+  )
+}
