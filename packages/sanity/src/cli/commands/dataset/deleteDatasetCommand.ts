316ce0e6d2338c62f49e57d484cc113b466c5c62
Espen Hovlandsdal <espen@hovlandsdal.com>
2022-09-16 14:12:18 -0700

    feat(cli): add `--force` flag to dataset delete command

--- packages/sanity/src/cli/commands/dataset/deleteDatasetCommand.ts
+++ packages/sanity/src/cli/commands/dataset/deleteDatasetCommand.ts
@@ -1,14 +1,35 @@
+import yargs from 'yargs/yargs'
+import {hideBin} from 'yargs/helpers'
 import type {CliCommandDefinition} from '@sanity/cli'
 import {validateDatasetName} from '../../actions/dataset/validateDatasetName'
 
-const deleteDatasetCommand: CliCommandDefinition = {
+const helpText = `
+Options
+  --force Do not prompt for delete confirmation - forcefully delete
+
+Examples
+  sanity dataset delete
+  sanity dataset delete my-dataset
+  sanity dataset delete my-dataset --force
+`
+
+function parseCliFlags(args: {argv?: string[]}) {
+  return yargs(hideBin(args.argv || process.argv).slice(2)).option('force', {type: 'boolean'}).argv
+}
+
+interface DeleteDatasetFlags {
+  force?: boolean
+}
+
+const deleteDatasetCommand: CliCommandDefinition<DeleteDatasetFlags> = {
   name: 'delete',
   group: 'dataset',
-  helpText: '',
+  helpText,
   signature: '[datasetName]',
   description: 'Delete a dataset within your project',
   action: async (args, context) => {
     const {apiClient, prompt, output} = context
+    const {force} = await parseCliFlags(args)
     const [ds] = args.argsWithoutOptions
     if (!ds) {
       throw new Error('Dataset name must be provided')
@@ -20,15 +41,19 @@ const deleteDatasetCommand: CliCommandDefinition = {
       throw dsError
     }
 
-    await prompt.single({
-      type: 'input',
-      message:
-        'Are you ABSOLUTELY sure you want to delete this dataset?\n  Type the name of the dataset to confirm delete:',
-      filter: (input) => `${input}`.trim(),
-      validate: (input) => {
-        return input === dataset || 'Incorrect dataset name. Ctrl + C to cancel delete.'
-      },
-    })
+    if (force) {
+      output.warn(`'--force' used: skipping confirmation, deleting dataset "${dataset}"`)
+    } else {
+      await prompt.single({
+        type: 'input',
+        message:
+          'Are you ABSOLUTELY sure you want to delete this dataset?\n  Type the name of the dataset to confirm delete:',
+        filter: (input) => `${input}`.trim(),
+        validate: (input) => {
+          return input === dataset || 'Incorrect dataset name. Ctrl + C to cancel delete.'
+        },
+      })
+    }
 
     await apiClient().datasets.delete(dataset)
     output.print('Dataset deleted successfully')
