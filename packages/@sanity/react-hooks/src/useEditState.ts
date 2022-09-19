c81a61105cb7e00b8eba4359e224d2712f6984b3
Per-Kristian Nordnes <per.kristian.nordnes@gmail.com>
2022-09-19 14:21:50 +0200

    feature(react-hooks): add low priority modality to useEditState hook
    This will allowed to get a debounced result, for consumers that doesn't need to be immediately updated.

--- packages/@sanity/react-hooks/src/useEditState.ts
+++ packages/@sanity/react-hooks/src/useEditState.ts
@@ -4,10 +4,25 @@
 import type {EditStateFor} from '@sanity/base/_internal'
 import documentStore from 'part:@sanity/base/datastore/document'
 import {useMemoObservable} from 'react-rx'
+import {merge, timer} from 'rxjs'
+import {debounce, share, skip, take} from 'rxjs/operators'
 
-export function useEditState(publishedDocId: string, docTypeName: string): EditStateFor {
-  return useMemoObservable(() => documentStore.pair.editState(publishedDocId, docTypeName), [
-    publishedDocId,
-    docTypeName,
-  ]) as EditStateFor
+export function useEditState(
+  publishedDocId: string,
+  docTypeName: string,
+  priority: 'default' | 'low' = 'default'
+): EditStateFor {
+  return useMemoObservable(() => {
+    const base = documentStore.pair.editState(publishedDocId, docTypeName).pipe(share())
+    if (priority === 'low') {
+      return merge(
+        base.pipe(take(1)),
+        base.pipe(
+          skip(1),
+          debounce(() => timer(1000))
+        )
+      )
+    }
+    return documentStore.pair.editState(publishedDocId, docTypeName)
+  }, [publishedDocId, docTypeName, priority]) as EditStateFor
 }
