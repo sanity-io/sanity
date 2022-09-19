6d2e9b71156bc16559af8c6071399b4c9450aa8a
Bjørge Næss <bjoerge@gmail.com>
2022-09-19 12:46:40 +0200

    perf(validation): refactor validation to be based on observables instead of promises, run in idle callback

--- packages/@sanity/validation/src/index.ts
+++ packages/@sanity/validation/src/index.ts
@@ -5,3 +5,5 @@ import inferFromSchemaType from './inferFromSchemaType'
 
 export default {Rule: RuleClass, validateDocument, inferFromSchema, inferFromSchemaType}
 export {RuleClass as Rule, validateDocument, inferFromSchema, inferFromSchemaType}
+
+export {validateDocumentObservable} from './validateDocument'
