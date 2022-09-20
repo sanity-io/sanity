c367d789fe30efb9cde9f6d81133945d0334b5ac
Herman Wikner <wiknerherman@gmail.com>
2022-09-20 14:13:08 +0200

    feat(form): use components from `form.components` in `StudioFormBuilder`

--- packages/sanity/src/form/studio/StudioFormBuilderProvider.tsx
+++ packages/sanity/src/form/studio/StudioFormBuilderProvider.tsx
@@ -1,13 +1,14 @@
 /* eslint-disable camelcase */
 
 import {ObjectSchemaType, Path, ValidationMarker} from '@sanity/types'
-import React from 'react'
+import React, {useCallback} from 'react'
 import {useSource} from '../../studio'
 import {PatchChannel, PatchEvent} from '../patch'
 import {FormBuilderProvider} from '../FormBuilderProvider'
 import {ObjectMember, StateTree} from '../store'
-import {FormFieldGroup} from '../types'
+import {FieldProps, FormFieldGroup, InputProps, ItemProps} from '../types'
 import {FormFieldPresence} from '../../presence'
+import {PreviewProps} from '../../components/previews'
 
 /**
  * @alpha This API might change.
@@ -74,7 +75,17 @@ export function StudioFormBuilderProvider(props: StudioFormBuilderProviderProps)
     value,
   } = props
 
-  const {file, image, renderField, renderInput, renderItem, renderPreview} = useSource().form
+  const {components, file, image} = useSource().form
+
+  const {Input, Field, Preview, Item} = components
+
+  const renderInput = useCallback((inputProps: InputProps) => <Input {...inputProps} />, [Input])
+  const renderField = useCallback((fieldProps: FieldProps) => <Field {...fieldProps} />, [Field])
+  const renderItem = useCallback((itemProps: ItemProps) => <Item {...itemProps} />, [Item])
+  const renderPreview = useCallback(
+    (previewProps: PreviewProps) => <Preview {...previewProps} />,
+    [Preview]
+  )
 
   return (
     <FormBuilderProvider
