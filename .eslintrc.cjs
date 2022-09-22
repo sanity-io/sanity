bbe63d4c82b7e43fad92175621c74b6c7258e00f
Marius Lundgård <studio@mariuslundgard.com>
2022-09-23 01:12:28 +0200

    chore(sanity): configure eslint boundaries

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -54,6 +54,7 @@ module.exports = {
     {
       files: ['*.{ts,tsx}'],
       extends: [
+        'plugin:boundaries/recommended',
         'sanity',
         'sanity/react',
         'sanity/import',
@@ -62,16 +63,126 @@ module.exports = {
         'plugin:react-hooks/recommended',
         'prettier',
       ],
+      plugins: ['boundaries', 'import', '@typescript-eslint', 'prettier', 'react', 'tsdoc'],
       rules: {
         ...baseConfig.rules,
         '@typescript-eslint/no-dupe-class-members': ['error'],
         '@typescript-eslint/no-shadow': ['error'],
         '@typescript-eslint/no-unused-vars': ['warn'],
+        'boundaries/element-types': [
+          2,
+          {
+            default: 'disallow',
+            rules: [
+              {
+                // export
+                from: 'sanity/_internal',
+                allow: ['sanity/_internal__contents'],
+              },
+              {
+                from: 'sanity/_internal__contents',
+                allow: ['sanity', 'sanity/_internal__contents'],
+              },
+              {
+                // export
+                from: 'sanity/cli',
+                allow: ['sanity/cli__contents'],
+              },
+              {
+                from: 'sanity/cli__contents',
+                allow: ['sanity/cli__contents'],
+              },
+              {
+                // export
+                from: 'sanity',
+                allow: ['sanity__contents'],
+              },
+              {
+                from: 'sanity__contents',
+                allow: ['sanity__contents', 'sanity/router'],
+              },
+              {
+                // export
+                from: 'sanity/desk',
+                allow: ['sanity/desk__contents'],
+              },
+              {
+                from: 'sanity/desk__contents',
+                allow: ['sanity', 'sanity/desk__contents', 'sanity/router'],
+              },
+              {
+                // export
+                from: 'sanity/router',
+                allow: ['sanity/router__contents'],
+              },
+              {
+                from: 'sanity/router__contents',
+                allow: ['sanity/router__contents'],
+              },
+            ],
+          },
+        ],
         'no-undef': 'off',
         'no-dupe-class-members': 'off', // doesn't work with TS overrides
         'no-shadow': 'off',
         'no-unused-vars': 'off',
         'react/jsx-filename-extension': ['error', {extensions: ['.tsx']}],
+      },
+      settings: {
+        ...baseConfig.settings,
+        'boundaries/elements': [
+          {
+            type: 'sanity',
+            pattern: ['packages/sanity/exports/index.ts'],
+            mode: 'full',
+          },
+          {
+            type: 'sanity__contents',
+            pattern: ['packages/sanity/src/core/**/*.*'],
+            mode: 'full',
+          },
+          {
+            type: 'sanity/_internal',
+            pattern: ['packages/sanity/exports/_internal.ts'],
+            mode: 'full',
+          },
+          {
+            type: 'sanity/_internal__contents',
+            pattern: ['packages/sanity/src/_internal/**/*.*'],
+            mode: 'full',
+          },
+          {
+            type: 'sanity/cli',
+            pattern: ['packages/sanity/exports/cli.ts'],
+            mode: 'full',
+          },
+          {
+            type: 'sanity/cli__contents',
+            pattern: ['packages/sanity/src/cli/**/*.*'],
+            mode: 'full',
+          },
+          {
+            type: 'sanity/desk',
+            pattern: ['packages/sanity/exports/desk.ts'],
+            mode: 'file',
+          },
+          {
+            type: 'sanity/desk__contents',
+            pattern: ['packages/sanity/src/desk/**/*.*'],
+            mode: 'file',
+          },
+          {
+            type: 'sanity/router',
+            pattern: ['packages/sanity/exports/router.ts'],
+            mode: 'full',
+          },
+          {
+            type: 'sanity/router__contents',
+            pattern: ['packages/sanity/src/router/**/*.*'],
+            mode: 'full',
+          },
+        ],
+        'boundaries/include': ['packages/sanity/exports/*.*', 'packages/sanity/src/**/*.*'],
       },
     },
 
