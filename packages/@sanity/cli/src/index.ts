3d95732483acd547fb0a9b37686dd67c775a4fa5
Marius Lundgård <studio@mariuslundgard.com>
2022-09-23 14:28:05 +0200

    fix(cli): add missing exports

--- packages/@sanity/cli/src/index.ts
+++ packages/@sanity/cli/src/index.ts
@@ -1,3 +1,7 @@
 export * from './types'
 export * from './config'
+
+export type {CliClientOptions} from './cliClient'
 export {getCliClient} from './cliClient'
+
+export type {SanityClient} from '@sanity/client'
