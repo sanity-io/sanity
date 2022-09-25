bcbf6af192f745978764fc17ae8fdbe07597283b
Marius Lundgård <studio@mariuslundgard.com>
2022-09-25 11:55:18 +0200

    refactor(sanity): move asset sources to files

--- packages/sanity/src/form/studio/DefaultAssetSource/index.ts
+++ packages/sanity/src/form/studio/DefaultAssetSource/index.ts
@@ -1,18 +1,2 @@
-import {ImageIcon, DocumentsIcon} from '@sanity/icons'
-import {AssetSource} from '@sanity/types'
-import {FIXME} from '../../types'
-import {DefaultSource} from './DefaultSource'
-
-export const ImageSource: AssetSource = {
-  name: 'sanity-default',
-  title: 'Uploaded images',
-  component: DefaultSource as FIXME,
-  icon: ImageIcon as FIXME,
-}
-
-export const FileSource: AssetSource = {
-  name: 'sanity-default',
-  title: 'Uploaded files',
-  component: DefaultSource as FIXME,
-  icon: DocumentsIcon as FIXME,
-}
+export * from './FileAssetSource'
+export * from './ImageAssetSource'
