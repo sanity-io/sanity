045099cb45e36abfb92625db289fbd64eeb60b7d
Marius Lundgård <studio@mariuslundgard.com>
2022-10-01 13:08:48 +0200

    build(types): require TSDoc release tags

--- packages/@sanity/types/package.config.ts
+++ packages/@sanity/types/package.config.ts
@@ -1,4 +1,16 @@
 import {defineConfig} from '@sanity/pkg-utils'
 import baseConfig from '../../../package.config'
 
-export default defineConfig({...baseConfig})
+export default defineConfig({
+  ...baseConfig,
+
+  extract: {
+    ...baseConfig.extract,
+    rules: {
+      ...baseConfig.extract?.rules,
+      'ae-forgotten-export': 'error',
+      'ae-incompatible-release-tags': 'error',
+      'ae-missing-release-tag': 'error',
+    },
+  },
+})
