b213090ddf206a8ae781135e3fc09575ed94c3f1
Herman Wikner <wiknerherman@gmail.com>
2022-10-02 20:06:27 +0200

    test(sanity): add components API tests

---
+++ cypress/integration/config/formComponentsApi.test.js
@@ -0,0 +1,101 @@
+/* eslint-disable cypress/no-unnecessary-waiting */
+
+describe('Config: form components', () => {
+  beforeEach(() => {
+    cy.visit('custom-components/content/v3;formComponentsApi;id')
+    cy.viewport(1500, 2500)
+  })
+
+  describe('default field and input renders with customizations in config, plugin and schema', () => {
+    it('boolean', () => {
+      cy.wait(3000)
+
+      cy.get('[data-testid="field-plugin-component"]')
+        .find('[data-testid="field-config-component"]')
+        .find('[data-testid="field-schema-boolean"]')
+        .find('[data-testid="input-plugin-component"]')
+        .find('[data-testid="input-config-component"]')
+        .find('[data-testid="input-schema-boolean"]')
+        .find('[data-testid="boolean-input"]')
+        .should('be.visible')
+    })
+
+    it('string', () => {
+      cy.wait(3000)
+
+      cy.get('[data-testid="field-plugin-component"]')
+        .find('[data-testid="field-config-component"]')
+        .find('[data-testid="field-schema-string"]')
+        .find('[data-testid="input-plugin-component"]')
+        .find('[data-testid="input-config-component"]')
+        .find('[data-testid="input-schema-string"]')
+        .find('[data-testid="string-input"]')
+        .should('be.visible')
+    })
+
+    it('reference', () => {
+      cy.wait(3000)
+
+      cy.get('[data-testid="field-plugin-component"]')
+        .find('[data-testid="field-config-component"]')
+        .find('[data-testid="field-schema-reference"]')
+        .find('[data-testid="input-plugin-component"]')
+        .find('[data-testid="input-config-component"]')
+        .find('[data-testid="input-schema-reference"]')
+        .find('[data-testid="reference-input"]')
+        .should('be.visible')
+    })
+
+    it('image', () => {
+      cy.wait(3000)
+
+      cy.get('[data-testid="field-plugin-component"]')
+        .find('[data-testid="field-config-component"]')
+        .find('[data-testid="field-schema-image"]')
+        .find('[data-testid="input-plugin-component"]')
+        .find('[data-testid="input-config-component"]')
+        .find('[data-testid="input-schema-image"]')
+        .find('[data-testid="image-input"]')
+        .should('be.visible')
+    })
+
+    it('array primitives', () => {
+      cy.wait(3000)
+
+      cy.get('[data-testid="field-plugin-component"]')
+        .find('[data-testid="field-config-component"]')
+        .find('[data-testid="field-schema-array-primitives"]')
+        .find('[data-testid="input-plugin-component"]')
+        .find('[data-testid="input-config-component"]')
+        .find('[data-testid="input-schema-array-primitives"]')
+        .find('[data-testid="array-primitives-input"]')
+        .should('be.visible')
+    })
+
+    it('array', () => {
+      cy.wait(3000)
+
+      cy.get('[data-testid="field-plugin-component"]')
+        .find('[data-testid="field-config-component"]')
+        .find('[data-testid="field-schema-array-objects"]')
+        .find('[data-testid="input-plugin-component"]')
+        .find('[data-testid="input-config-component"]')
+        .find('[data-testid="input-schema-array-objects"]')
+        .find('[data-testid="array-input"]')
+        .should('be.visible')
+    })
+
+    it('pte', () => {
+      cy.wait(3000)
+
+      cy.get('[data-testid="field-plugin-component"]')
+        .find('[data-testid="field-config-component"]')
+        .find('[data-testid="field-schema-pte"]')
+        .find('[data-testid="input-plugin-component"]')
+        .find('[data-testid="input-config-component"]')
+        .find('[data-testid="input-schema-pte"]')
+        .find('[data-testid="pt-editor"]')
+        .should('be.visible')
+    })
+  })
+})
