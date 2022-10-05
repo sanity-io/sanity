e0cbaff029452e5f38c533501f9a8b93c66a3412
Espen Hovlandsdal <espen@hovlandsdal.com>
2022-10-05 15:37:13 -0700

    chore(cli): address a few lint issues in tests

--- packages/@sanity/cli/test/shared/describe.ts
+++ packages/@sanity/cli/test/shared/describe.ts
@@ -2,8 +2,8 @@ import {cliUserToken, hasBuiltCli} from './environment'
 
 // From jest typings, not exposed by jest
 interface DoneCallback {
-  (...args: any[]): any
-  fail(error?: string | {message: string}): any
+  (...args: unknown[]): unknown
+  fail(error?: string | {message: string}): unknown
 }
 
 type ProvidesCallback = ((cb: DoneCallback) => void | undefined) | (() => Promise<unknown>)
@@ -12,7 +12,7 @@ type ProvidesCallback = ((cb: DoneCallback) => void | undefined) | (() => Promis
 export const describeCliTest = cliUserToken && hasBuiltCli ? describe : describe.skip
 
 // test.concurrent() runs even if the parent describe is skipped, so we need to wrap it as well
-export const testConcurrent = (name: string, testFn: ProvidesCallback, timeout = 30000) => {
+export const testConcurrent = (name: string, testFn: ProvidesCallback, timeout = 30000): void => {
   const tester = cliUserToken && hasBuiltCli ? test.concurrent : test.concurrent.skip
   return tester(name, testFn, timeout)
 }
