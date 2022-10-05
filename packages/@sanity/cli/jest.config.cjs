b31386978cc9b38fb09ae768234eef748796bbf7
Espen Hovlandsdal <espen@hovlandsdal.com>
2022-10-05 15:27:48 -0700

    chore(cli): alphabetize jest config properties

--- packages/@sanity/cli/jest.config.cjs
+++ packages/@sanity/cli/jest.config.cjs
@@ -4,11 +4,11 @@ const {createJestConfig} = require('../../../test/config.cjs')
 
 module.exports = createJestConfig({
   displayName: require('./package.json').name,
-  testEnvironment: 'node',
-  rootDir: __dirname,
   globalSetup: '<rootDir>/test/shared/globalSetup.ts',
   globalTeardown: '<rootDir>/test/shared/globalTeardown.ts',
-  slowTestThreshold: 60000,
-  testTimeout: 30000,
+  rootDir: __dirname,
   setupFilesAfterEnv: ['<rootDir>/test/shared/setupAfterEnv.ts'],
+  slowTestThreshold: 60000,
+  testEnvironment: 'node',
+  testTimeout: 30000,
 })
