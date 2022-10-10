9aeb4a45af88842b3f54fa3cfcb903ee96ab79ed
Marius Lundgård <studio@mariuslundgard.com>
2022-10-10 13:37:04 +0200

    refactor(cli): add runnable file to support `esbuild-register`

--- packages/@sanity/cli/package.config.ts
+++ packages/@sanity/cli/package.config.ts
@@ -1,38 +1,34 @@
 /* eslint-disable no-sync */
 import fs from 'fs'
 import path from 'path'
-import {defineConfig, PkgExports} from '@sanity/pkg-utils'
+import {defineConfig} from '@sanity/pkg-utils'
 import baseConfig from '../../../package.config'
+
+const workersDir = path.join(__dirname, 'src', 'workers')
+
+const workerNames = fs
+  .readdirSync(workersDir)
+  .filter((file) => file.endsWith('.ts'))
+  .map((file) => path.basename(file, '.ts'))
 
 export default defineConfig({
   ...baseConfig,
-  exports: (existingExports) => ({
-    ...existingExports,
-    './cli': {
+
+  bundles: [
+    {
       source: './src/cli.ts',
       require: './lib/cli.js',
-      default: './lib/cli.js',
     },
-    ...getWorkerExports(),
-  }),
-  runtime: 'node',
-})
+    {
+      source: './src/run.ts',
+      require: './lib/run.js',
+    },
 
-function getWorkerExports(): PkgExports {
-  const workersDir = path.join(__dirname, 'src', 'workers')
-  const sourceFiles = fs
-    .readdirSync(workersDir)
-    .filter((file) => file.endsWith('.ts'))
-    .map((file) => path.basename(file, '.ts'))
-
-  const workers: PkgExports = {}
-  for (const name of sourceFiles) {
-    workers[`./workers/${name}`] = {
+    ...workerNames.map((name) => ({
       source: `./src/workers/${name}.ts`,
       require: `./lib/workers/${name}.js`,
-      default: `./lib/workers/${name}.js`,
-    }
-  }
+    })),
+  ],
 
-  return workers
-}
+  runtime: 'node',
+})
