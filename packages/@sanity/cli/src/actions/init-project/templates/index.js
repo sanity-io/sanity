c38725917938e01829d2bb6fc3e309f75b714368
RitaDias <rita@sanity.io>
2022-10-14 09:05:07 +0200

    feat: add empty schema (#3756)

--- packages/@sanity/cli/src/actions/init-project/templates/index.js
+++ packages/@sanity/cli/src/actions/init-project/templates/index.js
@@ -1,6 +1,7 @@
 module.exports = {
   blog: require('./blog'),
   clean: require('./clean'),
+  empty: require('./empty'),
   'get-started': require('./get-started'),
   'getting-started-pets': require('./getting-started-pets'),
   'getting-started-dashboard': require('./getting-started-dashboard'),
---
+++ packages/@sanity/cli/templates/empty/schemas/schema.js
@@ -0,0 +1,16 @@
+// First, we must import the schema creator
+import createSchema from 'part:@sanity/base/schema-creator'
+
+// Then import schema types from any plugins that might expose them
+import schemaTypes from 'all:part:@sanity/base/schema-type'
+
+// Then we give our schema to the builder and provide the result to Sanity
+export default createSchema({
+  // We name our schema
+  name: 'default',
+  // Then proceed to concatenate our document type
+  // to the ones provided by any plugins that are installed
+  types: schemaTypes.concat([
+    /* Your types here! */
+  ]),
+})
