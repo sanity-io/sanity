ec51e40c5d8666419d11f93ecb78d52d63fcf3bf
Espen Hovlandsdal <espen@hovlandsdal.com>
2022-10-14 17:00:35 -0700

    fix(cli): fix using incorrect builtin module in bundle

--- packages/@sanity/cli/package.config.ts
+++ packages/@sanity/cli/package.config.ts
@@ -1,7 +1,9 @@
 /* eslint-disable no-sync */
 import fs from 'fs'
 import path from 'path'
+import isBuiltin from 'is-builtin-module'
 import {defineConfig} from '@sanity/pkg-utils'
+import {nodeResolve} from '@rollup/plugin-node-resolve'
 import baseConfig from '../../../package.config'
 
 const workersDir = path.join(__dirname, 'src', 'workers')
@@ -13,6 +15,25 @@ const workerNames = fs
 
 export default defineConfig({
   ...baseConfig,
+
+  // This is a workaround for an issue where the `readable-stream` package imports
+  // from `string_decoder/` instead of `string_decoder` (note the trailing slash),
+  // which _should_ tell rollup not to use the built-in `string_decoder` module,
+  // which it unfortunately does not. Hoping we can get rid of this hack soon:
+  // https://github.com/rollup/plugins/issues/1211
+  // https://github.com/sindresorhus/is-builtin-module/issues/7
+  // https://github.com/sindresorhus/builtin-modules/pull/17
+  rollup: {
+    plugins: (plugins) => [
+      ...plugins.filter((plugin) => plugin.name !== 'node-resolve'),
+      nodeResolve({
+        browser: false,
+        extensions: ['.cjs', '.mjs', '.js', '.jsx', '.json', '.node'],
+        preferBuiltins: false,
+        resolveOnly: (moduleName) => moduleName === 'string_decoder' || !isBuiltin(moduleName),
+      }),
+    ],
+  },
 
   bundles: [
     {
