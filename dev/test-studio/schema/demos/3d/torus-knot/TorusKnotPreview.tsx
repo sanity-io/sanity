8784f6cb661f0c8aa8dcdf0d41a9201ae7c317fb
Cody Olsen <81981+stipsan@users.noreply.github.com>
2022-10-29 03:49:37 +0200

    chore(3D): stop animating on pointer leave

--- dev/test-studio/schema/demos/3d/torus-knot/TorusKnotPreview.tsx
+++ dev/test-studio/schema/demos/3d/torus-knot/TorusKnotPreview.tsx
@@ -1,5 +1,5 @@
 import {Canvas, Vector3} from '@react-three/fiber'
-import React, {memo, Suspense, useRef} from 'react'
+import React, {memo, startTransition, Suspense, useEffect, useRef, useState} from 'react'
 import styled from 'styled-components'
 import {Box} from '@sanity/ui'
 
@@ -24,14 +24,41 @@ export default memo(function TorusKnotPreview(props: Props) {
   const {base, colorA, colorB, position = {x: -1.75, y: 0, z: 0}} = props
   const {x, y, z} = position
   const canvasRef = useRef<HTMLCanvasElement>(null)
-  const frameloop = useLazyFrameloop(canvasRef)
+  const lazyFrameloop = useLazyFrameloop(canvasRef)
+  const [frameloop, setFrameloop] = useState<'always' | 'demand'>('always')
+
+  // eslint-disable-next-line consistent-return
+  useEffect(() => {
+    if (canvasRef.current) {
+      // run animation loop on hover
+      const paneItem = canvasRef.current.closest('[data-ui=PaneItem]')
+      if (paneItem) {
+        const onEnter = () => startTransition(() => setFrameloop('always'))
+        const onLeave = () => startTransition(() => setFrameloop('demand'))
+        paneItem.addEventListener('pointerenter', onEnter)
+        paneItem.addEventListener('pointerleave', onLeave)
+        return () => {
+          paneItem.removeEventListener('pointerenter', onEnter)
+          paneItem.removeEventListener('pointerleave', onLeave)
+        }
+      }
+    }
+  }, [])
+
   return (
     <Container>
       <Canvas
         ref={canvasRef}
         linear
         camera={{position: {x, y, z} as Vector3, fov: 80, near: 0.001}}
-        frameloop={frameloop}
+        frameloop={
+          // eslint-disable-next-line no-nested-ternary
+          lazyFrameloop === 'never'
+            ? 'never'
+            : lazyFrameloop === 'demand' && frameloop === 'always'
+            ? 'demand'
+            : frameloop
+        }
       >
         <Suspense fallback={null}>
           <View element={{base, colorA, colorB, position: {x, y, z}}} preview />
