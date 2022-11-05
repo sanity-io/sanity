ead02bb92f50fba46d45cb8b205718ea187800b6
Robin Pyon <robin.pyon@gmail.com>
2022-11-05 23:33:55 +0000

    feat(studio): use sanity ui menu button for selectors, add reducer action specifically for changing operators

--- packages/sanity/src/core/studio/components/navbar/search/components/filters/filter/SelectOperators.tsx
+++ packages/sanity/src/core/studio/components/navbar/search/components/filters/filter/SelectOperators.tsx
@@ -1,52 +1,119 @@
-import {Inline, Select} from '@sanity/ui'
-import React, {ChangeEvent} from 'react'
+import {CheckmarkIcon, SelectIcon} from '@sanity/icons'
+import {Box, Button, Flex, Inline, Menu, MenuButton, MenuDivider, MenuItem, Text} from '@sanity/ui'
+import React, {useCallback, useId} from 'react'
+import styled from 'styled-components'
+import {useSearchState} from '../../../contexts/search/useSearchState'
 import {getFilter} from '../../../definitions/filters'
-import {getOperator, OperatorType} from '../../../definitions/operators'
+import {getOperator, Operator, OperatorType} from '../../../definitions/operators'
 import type {SearchFilterState} from '../../../types'
 
-interface SelectOperatorsProps {
+interface OperatorsMenuButtonProps {
   filter: SearchFilterState
-  onChange: (event: ChangeEvent<HTMLSelectElement>) => void
-  value?: OperatorType
+  operator?: Operator
 }
 
-export function SelectOperators({filter, onChange, value}: SelectOperatorsProps) {
+const IconWrapperBox = styled(Box)<{$visible: boolean}>`
+  visibility: ${({$visible}) => ($visible ? 'visible' : 'hidden')};
+`
+
+function CustomMenuItem({
+  onClick,
+  operator,
+  selected,
+}: {
+  onClick: (operatorType: OperatorType) => void
+  operator: Operator
+  selected: boolean
+}) {
+  const handleClick = useCallback(() => onClick(operator.type), [onClick, operator.type])
+
+  return (
+    <MenuItem onClick={handleClick} padding={3} selected={selected} tone="default">
+      <Flex align="center" justify="space-between" gap={4}>
+        <Inline space={1}>
+          <Text size={1} weight="regular">
+            {operator.label}
+          </Text>
+        </Inline>
+        <IconWrapperBox $visible={selected}>
+          <Text size={1}>
+            <CheckmarkIcon />
+          </Text>
+        </IconWrapperBox>
+      </Flex>
+    </MenuItem>
+  )
+}
+
+export function OperatorsMenuButton({filter, operator}: OperatorsMenuButtonProps) {
   const operatorItems = getFilter(filter.filterType)?.operators
 
-  if (!operatorItems || operatorItems.length < 1) {
+  const menuButtonId = useId()
+
+  const {dispatch} = useSearchState()
+
+  const handleClick = useCallback(
+    (operatorType: OperatorType) => {
+      dispatch({
+        key: filter._key,
+        operatorType,
+        type: 'TERMS_FILTERS_SET_OPERATOR',
+      })
+    },
+    [dispatch, filter._key]
+  )
+
+  if (!operator || !operatorItems || operatorItems.length < 1) {
     return null
   }
 
   return (
     <Inline>
-      <Select fontSize={1} onChange={onChange} radius={2} value={value}>
-        {operatorItems.map((item, index) => {
-          if (item.type === 'item') {
-            const operator = getOperator(item.name)
-            return (
-              <option
+      <MenuButton
+        button={
+          <Button mode="ghost" padding={3}>
+            <Flex align="center" gap={2} justify="space-between">
+              <Text size={1} weight="regular">
+                {operator.label}
+              </Text>
+              <Box marginLeft={1}>
+                <Text size={1}>
+                  <SelectIcon />
+                </Text>
+              </Box>
+            </Flex>
+          </Button>
+        }
+        id={menuButtonId || ''}
+        menu={
+          <Menu>
+            {operatorItems.map((item, index) => {
+              if (item.type === 'item') {
+                const menuOperator = getOperator(item.name)
+                if (!menuOperator) {
+                  return null
+                }
+                return (
+                  <CustomMenuItem
+                    // eslint-disable-next-line react/no-array-index-key
+                    key={index}
+                    onClick={handleClick}
+                    operator={menuOperator}
+                    selected={operator.type === item.name}
+                  />
+                )
+              }
+              if (item.type === 'divider') {
                 // eslint-disable-next-line react/no-array-index-key
-                key={index}
-                value={item.name}
-              >
-                {operator?.label}
-              </option>
-            )
-          }
-          if (item.type === 'divider') {
-            return (
-              <option
-                disabled
-                // eslint-disable-next-line react/no-array-index-key
-                key={index}
-              >
-                -
-              </option>
-            )
-          }
-          return null
-        })}
-      </Select>
+                return <MenuDivider key={index} />
+              }
+              return null
+            })}
+          </Menu>
+        }
+        placement="bottom-start"
+        popover={{portal: false, radius: 2}}
+      />
     </Inline>
   )
 }
