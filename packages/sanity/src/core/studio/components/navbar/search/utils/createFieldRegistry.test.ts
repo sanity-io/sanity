25e96d9dd30e52da07379bbdce41bb73916bf6ac
Robin Pyon <robin.pyon@gmail.com>
2022-11-07 16:14:42 +0000

    fix(studio): handle inline objects when creating inline registry, stub tests

---
+++ packages/sanity/src/core/studio/components/navbar/search/utils/createFieldRegistry.test.ts
@@ -0,0 +1,8 @@
+describe('createFieldRegistry', () => {
+  it.todo('should create a flattened list of all available fields')
+  it.todo('should ignore hidden fields')
+  it.todo('should only traverse nested object fields up until a certain depth')
+  it.todo('should correctly traverse recursive documents')
+})
+
+export {}
