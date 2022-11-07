dc0f4128e8203e43cd43aef433121b869682da77
Robin Pyon <robin.pyon@gmail.com>
2022-11-07 03:56:28 +0000

    fix(studio): dont constrain reference autocomplete, use published IDs when searching on references

--- packages/sanity/src/core/studio/components/navbar/search/components/filters/filter/inputTypes/Reference.tsx
+++ packages/sanity/src/core/studio/components/navbar/search/components/filters/filter/inputTypes/Reference.tsx
@@ -1,11 +1,12 @@
 import React, {useCallback} from 'react'
+import {getPublishedId} from '../../../../../../../../util'
 import {OperatorInputComponentProps} from '../../../../definitions/operators'
 import {ReferenceAutocomplete} from '../ReferenceAutocomplete'
 
 export function FieldInputReference({onChange, value}: OperatorInputComponentProps<string>) {
   const handleChange = useCallback(
     (documentId: string | null) => {
-      onChange(documentId)
+      onChange(documentId && getPublishedId(documentId))
     },
     [onChange]
   )
