d2f72521888eee23a98f97bd2a78ccac09518117
Robin Pyon <robin.pyon@gmail.com>
2022-11-07 03:40:23 +0000

    fix(studio): ensure hasSearchableTerms works with undefined term.filters

--- packages/sanity/src/core/studio/components/navbar/search/utils/hasSearchableTerms.ts
+++ packages/sanity/src/core/studio/components/navbar/search/utils/hasSearchableTerms.ts
@@ -1,5 +1,9 @@
 import type {SearchTerms} from '../../../../../search'
 
 export function hasSearchableTerms(terms: SearchTerms): boolean {
-  return terms.query.trim() !== '' || terms.filter?.trim() !== '' || terms.types.length > 0
+  return (
+    terms.query.trim() !== '' ||
+    (terms.filter && terms.filter?.trim() !== '') ||
+    terms.types.length > 0
+  )
 }
