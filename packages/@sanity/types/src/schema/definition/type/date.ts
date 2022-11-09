585226d4bbb98063413ac6c6069affc9074c6b2c
Snorre Eskeland Brekke <snorre.e.brekke@gmail.com>
2022-11-09 14:51:24 +0100

    fix: added min & max validation to DateRule

--- packages/@sanity/types/src/schema/definition/type/date.ts
+++ packages/@sanity/types/src/schema/definition/type/date.ts
@@ -9,7 +9,16 @@ export interface DateOptions {
 
 /** @public */
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface DateRule extends RuleDef<DateRule, string> {}
+export interface DateRule extends RuleDef<DateRule, string> {
+  /**
+   * @param minDate - Minimum date (inclusive). minDate should be in ISO 8601 format.
+   */
+  min: (minDate: string) => DateRule
+  /**
+   * @param maxDate - Maximum date (inclusive). maxDate should be in ISO 8601 format.
+   */
+  max: (maxDate: string) => DateRule
+}
 
 /** @public */
 export interface DateDefinition extends BaseSchemaDefinition {
