5b840e774464c90a31d7b75f057183af8e70035c
Espen Hovlandsdal <espen@hovlandsdal.com>
2022-11-21 16:44:37 +0700

    chore(test-studio): migrate some input components

--- dev/test-studio/schema/debug/components/CustomFontStringInput.tsx
+++ dev/test-studio/schema/debug/components/CustomFontStringInput.tsx
@@ -1,22 +1,22 @@
-import {set} from 'sanity'
+import {set, StringInputProps} from 'sanity'
 import React from 'react'
 import styles from './CustomFontStringInput.module.css'
 
-export default class CustomStringInput extends React.Component {
+export default class CustomStringInput extends React.Component<StringInputProps> {
   handleChange = (event) => {
     this.props.onChange(set(event.target.value))
   }
 
   render() {
-    const {value, type} = this.props
+    const {value, schemaType} = this.props
     return (
       <div>
-        <h3>{type.title}</h3>
-        <p>{type.description}</p>
+        <h3>{schemaType.title}</h3>
+        <p>{schemaType.description}</p>
         <input
           type="text"
           className={styles.input}
-          placeholder={type.placeholder}
+          placeholder={schemaType.placeholder}
           onChange={this.handleChange}
           value={value}
         />
