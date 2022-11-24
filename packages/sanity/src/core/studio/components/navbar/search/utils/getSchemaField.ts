79bcf05beef5d3dc9440452581232ec058ea87a7
Robin Pyon <robin.pyon@gmail.com>
2022-11-24 10:24:27 +0000

    fix(studio): fix issue with recursive getSchemaField not correctly returning the field on the last path

--- packages/sanity/src/core/studio/components/navbar/search/utils/getSchemaField.ts
+++ packages/sanity/src/core/studio/components/navbar/search/utils/getSchemaField.ts
@@ -3,19 +3,17 @@ import {isObjectSchemaType, ObjectField, SchemaType} from '@sanity/types'
 export function getSchemaField(
   schemaType: SchemaType,
   fieldPath: string
-  // paths: string[]
 ): ObjectField<SchemaType> | undefined {
   const paths = fieldPath.split('.')
   const firstPath = paths[0]
-  if (firstPath) {
-    if (isObjectSchemaType(schemaType)) {
-      const field = schemaType.fields.find((f) => f.name === firstPath)
-      if (field) {
-        if (isObjectSchemaType(field)) {
-          return getSchemaField(field, paths.slice(1).join('.'))
-        }
-        return field
+  if (firstPath && isObjectSchemaType(schemaType)) {
+    const field = schemaType.fields.find((f) => f.name === firstPath)
+    if (field) {
+      const nextPath = paths.slice(1).join('.')
+      if (nextPath) {
+        return getSchemaField(field.type, nextPath)
       }
+      return field
     }
   }
   return undefined
