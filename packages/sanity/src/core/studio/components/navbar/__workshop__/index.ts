36e3b28d47a84fa908fecd36667bfacd6518412f
Robin Pyon <robin.pyon@gmail.com>
2022-11-24 16:15:04 +0000

    chore(studio): use new story / defineScope signature

--- packages/sanity/src/core/studio/components/navbar/__workshop__/index.ts
+++ packages/sanity/src/core/studio/components/navbar/__workshop__/index.ts
@@ -1,20 +1,24 @@
 import {defineScope} from '@sanity/ui-workshop'
 import {lazy} from 'react'
 
-export default defineScope('sanity/studio/navbar/base', 'Base', [
-  {
-    name: 'navbar',
-    title: 'Navbar',
-    component: lazy(() => import('./NavbarStory')),
-  },
-  {
-    name: 'changelog-dialog',
-    title: 'ChangelogDialog',
-    component: lazy(() => import('./ChangelogDialogStory')),
-  },
-  {
-    name: 'workspacePreview',
-    title: 'WorkspacePreview',
-    component: lazy(() => import('./WorkspacePreviewStory')),
-  },
-])
+export default defineScope({
+  name: 'sanity/studio/navbar/base',
+  title: 'Base',
+  stories: [
+    {
+      name: 'navbar',
+      title: 'Navbar',
+      component: lazy(() => import('./NavbarStory')),
+    },
+    {
+      name: 'changelog-dialog',
+      title: 'ChangelogDialog',
+      component: lazy(() => import('./ChangelogDialogStory')),
+    },
+    {
+      name: 'workspacePreview',
+      title: 'WorkspacePreview',
+      component: lazy(() => import('./WorkspacePreviewStory')),
+    },
+  ],
+})
--- packages/sanity/src/core/studio/components/navbar/search/__workshop__/index.ts
+++ packages/sanity/src/core/studio/components/navbar/search/__workshop__/index.ts
@@ -1,20 +1,24 @@
 import {defineScope} from '@sanity/ui-workshop'
 import {lazy} from 'react'
 
-export default defineScope('sanity/studio/navbar/search', 'Search', [
-  {
-    name: 'miniReferenceInput',
-    title: 'MiniReferenceInput',
-    component: lazy(() => import('./MiniReferenceInputStory')),
-  },
-  {
-    name: 'searchPopover',
-    title: 'SearchPopover',
-    component: lazy(() => import('./SearchPopoverStory')),
-  },
-  {
-    name: 'searchDialog',
-    title: 'SearchDialog',
-    component: lazy(() => import('./SearchDialogStory')),
-  },
-])
+export default defineScope({
+  name: 'sanity/studio/navbar/search',
+  title: 'Search',
+  stories: [
+    {
+      name: 'miniReferenceInput',
+      title: 'MiniReferenceInput',
+      component: lazy(() => import('./MiniReferenceInputStory')),
+    },
+    {
+      name: 'searchPopover',
+      title: 'SearchPopover',
+      component: lazy(() => import('./SearchPopoverStory')),
+    },
+    {
+      name: 'searchDialog',
+      title: 'SearchDialog',
+      component: lazy(() => import('./SearchDialogStory')),
+    },
+  ],
+})
