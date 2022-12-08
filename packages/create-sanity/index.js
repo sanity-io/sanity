2759e932e3a2a24bb6cfc2feaba5ff6cf312f7ee
Per-Kristian Nordnes <per.kristian.nordnes@gmail.com>
2022-12-08 10:56:12 +0100

    fix(create-sanity): fix Windows issue

--- packages/create-sanity/index.js
+++ packages/create-sanity/index.js
@@ -3,4 +3,4 @@ const cli = require.resolve('@sanity/cli/bin/sanity')
 const childProcess = require('child_process')
 
 const args = process.argv.slice(2)
-childProcess.spawn(cli, ['init', ...args.concat('--from-create')], {stdio: 'inherit'})
+childProcess.spawn('node', [cli, 'init', '', ...args.concat('--from-create')], {stdio: 'inherit'})
