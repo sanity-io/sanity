c2efecbd00d063a712031c8e391b9e838a0edad7
Per-Kristian Nordnes <per.kristian.nordnes@gmail.com>
2022-12-12 15:48:37 +0100

    refactor(core/fields/diffs): use new types in Portable-text-diffs

--- packages/sanity/src/core/field/types/portableText/diff/types.ts
+++ packages/sanity/src/core/field/types/portableText/diff/types.ts
@@ -1,27 +1,10 @@
-import {ObjectSchemaType, SchemaType} from '@sanity/types'
+import {PortableTextTextBlock} from '@sanity/types'
 import {ObjectDiff} from '../../../types'
 
-export type PortableTextBlock = {
-  _key: string
-  _type: string
-  children: PortableTextChild[]
-  markDefs?: {_key: string; _type: string}[]
-  style?: string
+export type PortableTextDiff = ObjectDiff & {
+  displayValue: PortableTextTextBlock
+  origin: ObjectDiff
 }
-
-export type PortableTextChild = {
-  _key: string
-  _type: string
-  marks?: string[]
-  text?: string
-}
-
-export type SpanTypeSchema = SchemaType & {
-  decorators?: {title: string; value: string}[]
-  annotations?: ObjectSchemaType[]
-}
-
-export type PortableTextDiff = ObjectDiff & {displayValue: PortableTextBlock; origin: ObjectDiff}
 
 export type MarkSymbolMap = Record<string, string[]>
 
