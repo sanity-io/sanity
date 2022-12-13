a369882165ecfc4b5c689fef09476477611728b5
Espen Hovlandsdal <espen@hovlandsdal.com>
2022-12-13 11:16:45 -0800

    fix(server): make build entries plugin compatible with vite 4

--- packages/@sanity/server/src/vite/plugin-sanity-build-entries.ts
+++ packages/@sanity/server/src/vite/plugin-sanity-build-entries.ts
@@ -3,14 +3,24 @@ import type {SanityMonorepo} from '../sanityMonorepo'
 import {renderDocument} from '../renderDocument'
 
 interface ViteOutputBundle {
-  [fileName: string]: ViteRenderedChunk
+  [fileName: string]: ViteRenderedChunk | ViteRenderedAsset
+}
+
+interface ViteRenderedAsset {
+  type: 'asset'
 }
 
 interface ViteRenderedChunk {
+  type: 'chunk'
+  name: string
+  fileName: string
+  facadeModuleId: string | null
   code: string
   imports: string[]
   viteMetadata: ChunkMetadata
 }
+
+const entryChunkId = '.sanity/runtime/app.js'
 
 export function sanityBuildEntries(options: {
   cwd: string
@@ -19,29 +29,36 @@ export function sanityBuildEntries(options: {
 }): Plugin {
   const {cwd, monorepo, basePath} = options
 
-  let entryChunkRef: string
-
   return {
     name: '@sanity/server/build-entries',
     apply: 'build',
 
     buildStart() {
-      entryChunkRef = this.emitFile({
+      this.emitFile({
         type: 'chunk',
-        id: '.sanity/runtime/app.js',
+        id: entryChunkId,
         name: 'sanity',
       })
     },
 
     async generateBundle(_options, outputBundle) {
       const bundle = outputBundle as unknown as ViteOutputBundle
+      const entryFile = Object.values(bundle).find(
+        (file) =>
+          file.type === 'chunk' &&
+          file.name === 'sanity' &&
+          file.facadeModuleId?.endsWith(entryChunkId)
+      )
 
-      const entryFileName = this.getFileName(entryChunkRef)
-      const entryFile = bundle[entryFileName]
       if (!entryFile) {
-        throw new Error(`Failed to find entry file in bundle (${entryFileName})`)
+        throw new Error(`Failed to find entry file in bundle (${entryChunkId})`)
       }
 
+      if (entryFile.type !== 'chunk') {
+        throw new Error('Entry file is not a chunk')
+      }
+
+      const entryFileName = entryFile.fileName
       const entryPath = [basePath.replace(/\/+$/, ''), entryFileName].join('/')
 
       let css: string[] = []
@@ -52,7 +69,10 @@ export function sanityBuildEntries(options: {
         for (const key of entryFile.imports) {
           // Traverse all CSS assets that isn't loaded by the runtime and
           // need <link> tags in the HTML template
-          const importedCss = bundle[key]?.viteMetadata?.importedCss
+          const entry = bundle[key]
+          const importedCss =
+            entry && entry.type === 'chunk' ? entry.viteMetadata.importedCss : undefined
+
           if (importedCss) {
             css.push(...importedCss)
           }
