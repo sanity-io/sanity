bb61fa1ef9da1dd4c14afdf0051d5e5a15d6372f
Herman Wikner <wiknerherman@gmail.com>
2022-12-15 16:57:55 +0100

    dev(test-studio): update custom logo component

--- dev/test-studio/components/Branding.tsx
+++ dev/test-studio/components/Branding.tsx
@@ -1,6 +1,10 @@
-import {Text} from '@sanity/ui'
+import {Box, Text} from '@sanity/ui'
 import React from 'react'
 
 export function Branding() {
-  return <Text weight="bold">Test Studio&trade;</Text>
+  return (
+    <Box padding={3}>
+      <Text weight="bold">Test Studio&trade;</Text>
+    </Box>
+  )
 }
