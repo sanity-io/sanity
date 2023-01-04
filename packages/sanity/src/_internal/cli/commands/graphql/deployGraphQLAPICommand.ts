93d30808bda8b877b015b0b65da4543aec768d8b
Espen Hovlandsdal <espen@hovlandsdal.com>
2023-01-04 13:58:52 -0800

    fix(cli): reintroduce deprecated graphql deployment flags

--- packages/sanity/src/_internal/cli/commands/graphql/deployGraphQLAPICommand.ts
+++ packages/sanity/src/_internal/cli/commands/graphql/deployGraphQLAPICommand.ts
@@ -6,15 +6,29 @@ Options
   --force Deploy API without confirming breaking changes
   --api <api-id> Only deploy API with this ID. Can be specified multiple times.
 
+The following options will override any setting from the CLI configuration file
+(sanity.cli.js/sanity.cli.ts) - and applies to ALL defined APIs defined in that
+configuration file. Tread with caution!
+
+  --tag Deploy API(s) to given tag (defaults to 'default')
+  --dataset <name> Deploy API for the given dataset
+  --generation <gen1|gen2|gen3> API generation to deploy (defaults to 'gen3')
+  --non-null-document-fields Use non-null document fields (_id, _type etc)
+  --playground Enable GraphQL playground for easier debugging
+  --no-playground Disable GraphQL playground
+
 Examples
   # Deploy all defined GraphQL APIs
   sanity graphql deploy
 
-  # Validate defined GraphQL APIs and check for breaking changes
+  # Validate defined GraphQL APIs, check for breaking changes, skip deploy
   sanity graphql deploy --dry-run
 
   # Deploy only the GraphQL APIs with the IDs "staging" and "ios"
   sanity graphql deploy --api staging --api ios
+
+  # Deploy all defined GraphQL APIs, overriding any playground setting
+  sanity graphql deploy --playground
 `
 
 const deployGraphQLAPICommand: CliCommandDefinition = {
