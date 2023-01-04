ad9890119c82414d065679b46769f010ef9ee0d0
Espen Hovlandsdal <espen@hovlandsdal.com>
2023-01-04 15:26:58 -0800

    fix(create-sanity): resolve cli module without `module.resolve`

--- packages/create-sanity/index.js
+++ packages/create-sanity/index.js
@@ -1,6 +1,9 @@
 #!/usr/bin/env node
-const cli = require.resolve('@sanity/cli/bin/sanity')
+const path = require('path')
 const childProcess = require('child_process')
+const resolvePkg = require('resolve-pkg')
 
 const args = process.argv.slice(2)
+const cliDir = resolvePkg('@sanity/cli', {cwd: __dirname})
+const cli = path.join(cliDir, 'bin', 'sanity')
 childProcess.spawn('node', [cli, 'init', '', ...args.concat('--from-create')], {stdio: 'inherit'})
