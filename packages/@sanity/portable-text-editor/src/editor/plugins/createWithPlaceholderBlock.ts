14bca9e25e624264358b8a568f5eacbd0e7f0dcd
Per-Kristian Nordnes <per.kristian.nordnes@gmail.com>
2023-01-09 12:12:10 +0100

    fix(portable-text-editor): set placeholder directly
    Don't make an operation for inserting the placeholder block. We don't want these to end up as patches.
    Also read old selection before calling onChange.

--- packages/@sanity/portable-text-editor/src/editor/plugins/createWithPlaceholderBlock.ts
+++ packages/@sanity/portable-text-editor/src/editor/plugins/createWithPlaceholderBlock.ts
@@ -1,8 +1,6 @@
 import {Transforms, Descendant} from 'slate'
 import {PortableTextMemberSchemaTypes, PortableTextSlateEditor} from '../../types/editor'
 import {debugWithName} from '../../utils/debug'
-import {withoutPatching} from '../../utils/withoutPatching'
-import {withoutSaving} from './createWithUndoRedo'
 
 const debug = debugWithName('plugin:withPlaceholderBlock')
 
@@ -38,25 +36,20 @@ export function createWithPlaceholderBlock({
     const {onChange} = editor
     // Make sure there's a placeholder block present if the editor's children become empty
     editor.onChange = () => {
+      const hadSelection = !!editor.selection
       onChange()
       if (editor.children.length === 0) {
-        const hadSelection = !!editor.selection
-        withoutPatching(editor, () => {
-          withoutSaving(editor, () => {
-            debug('Inserting placeholder block')
-            Transforms.deselect(editor)
-            Transforms.insertNodes(editor, editor.createPlaceholderBlock(), {
-              at: [0],
-            })
-            if (hadSelection) {
-              Transforms.select(editor, {
-                focus: {path: [0, 0], offset: 0},
-                anchor: {path: [0, 0], offset: 0},
-              })
-              editor.onChange()
-            }
+        debug('Inserting placeholder block')
+        Transforms.deselect(editor)
+        // Set the value directly without using transforms as we don't want this to be producing any patches
+        editor.children = [editor.createPlaceholderBlock()]
+        if (hadSelection) {
+          Transforms.select(editor, {
+            focus: {path: [0, 0], offset: 0},
+            anchor: {path: [0, 0], offset: 0},
           })
-        })
+        }
+        editor.onChange()
       }
     }
     return editor
