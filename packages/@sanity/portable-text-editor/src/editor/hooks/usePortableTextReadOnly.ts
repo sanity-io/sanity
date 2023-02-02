812008ae803d6d2af281ba91445bcc2af4f5a10a
Per-Kristian Nordnes <per.kristian.nordnes@gmail.com>
2023-02-02 10:53:12 +0100

    chore(portable-text-editor): small comment adjustments + add missing TS return def.

--- packages/@sanity/portable-text-editor/src/editor/hooks/usePortableTextReadOnly.ts
+++ packages/@sanity/portable-text-editor/src/editor/hooks/usePortableTextReadOnly.ts
@@ -3,13 +3,11 @@ import {createContext, useContext} from 'react'
 /**
  * A React context for sharing the editor's readOnly status.
  */
-
 export const PortableTextEditorReadOnlyContext = createContext<boolean>(false)
 
 /**
  * Get the current editor selection from the React context.
  */
-
 export const usePortableTextEditorReadOnlyStatus = (): boolean => {
   const readOnly = useContext(PortableTextEditorReadOnlyContext)
 
