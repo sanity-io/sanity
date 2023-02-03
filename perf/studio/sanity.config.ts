03a82916fc61139bf85296e7d016baccefe75971
Bjørge Næss <bjoerge@gmail.com>
2023-02-03 18:54:15 +0100

    fix(perf): use single source of truth for perf studio config

--- perf/studio/sanity.config.ts
+++ perf/studio/sanity.config.ts
@@ -1,5 +1,6 @@
 import {defineConfig} from 'sanity'
 import {deskTool} from 'sanity/desk'
+import {STUDIO_DATASET, STUDIO_PROJECT_ID} from '../config'
 import {simple} from './schema/simple'
 import {deepObject} from './schema/deepObject'
 import {deepArray} from './schema/deepArray'
@@ -9,7 +10,7 @@ export default defineConfig({
   plugins: [deskTool() as FIXME /* 🤷 */],
   title: 'Perf test Studio',
   name: 'default',
-  projectId: 'qk0wb6qx',
-  dataset: 'test',
+  projectId: STUDIO_PROJECT_ID,
+  dataset: STUDIO_DATASET,
   schema: {types: [simple, deepObject, deepArray]},
 })
