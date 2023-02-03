5bcda1db9c3470612d6909b17d8ff6cd627288ad
Bjørge Næss <bjoerge@gmail.com>
2023-02-03 16:24:17 +0100

    fix(perf): read the actual env var name from process.env

--- perf/utils/env.ts
+++ perf/utils/env.ts
@@ -1,5 +1,5 @@
 export function getEnv(name: string) {
-  const envVar = process.env.PERF_TEST_SANITY_TOKEN
+  const envVar = process.env[name]
   if (!envVar) {
     throw new Error(`Missing ${name} environment variable`)
   }
