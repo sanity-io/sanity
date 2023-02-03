2b129908cef68d630674068738cf598e6929e430
Bjørge Næss <bjoerge@gmail.com>
2023-02-03 19:05:26 +0100

    fix(perf): allow configuring headless mode via env var

--- perf/utils/env.ts
+++ perf/utils/env.ts
@@ -1,6 +1,8 @@
-export function getEnv(name: string) {
+export function getEnv(name: string, optional: true): string | undefined
+export function getEnv(name: string): string
+export function getEnv(name: string, optional?: boolean) {
   const envVar = process.env[name]
-  if (!envVar) {
+  if (!optional && !envVar) {
     throw new Error(`Missing ${name} environment variable`)
   }
   return envVar
