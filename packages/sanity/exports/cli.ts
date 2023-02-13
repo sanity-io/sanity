c03ef40ae4271f18f2f1fa1f6b116cc49e9cc77a
Espen Hovlandsdal <espen@hovlandsdal.com>
2023-02-13 15:56:40 +0100

    feat(cli): expose `getStudioEnvironmentVariables` method, add dotenv handling

--- packages/sanity/exports/cli.ts
+++ packages/sanity/exports/cli.ts
@@ -1,3 +1,5 @@
 export {createCliConfig, defineCliConfig, getCliClient} from '@sanity/cli'
 
 export type {CliClientOptions, CliConfig} from '@sanity/cli'
+
+export {getStudioEnvironmentVariables} from '../src/_internal/cli/server/getStudioEnvironmentVariables'
