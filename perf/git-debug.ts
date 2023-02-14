be8efe769a1506ce81776eed010fde87e40827a4
Bjørge Næss <bjoerge@gmail.com>
2023-02-14 13:48:29 +0100

    add more debug output to workflow

--- perf/git-debug.ts
+++ perf/git-debug.ts
@@ -1,6 +1,7 @@
 import {getCurrentBranch, getCurrentBranchSync} from './utils/gitUtils'
 import {getEnv} from './utils/env'
 
-console.log('ENV:', getEnv('CURRENT_BRANCH', true) || getCurrentBranchSync())
+console.log('ENV:', getEnv('CURRENT_BRANCH', true))
+console.log('git says: ', getCurrentBranchSync())
 getCurrentBranch().then(console.log)
 console.log('------------------')
