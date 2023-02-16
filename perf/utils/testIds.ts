b7036232efcdca96336d1cb237dd34b304f7ec2b
Bjørge Næss <bjoerge@gmail.com>
2023-02-16 18:03:43 +0100

    fix(perf): validate that testIds are valid sanity document ids and fix wrong reference id

--- perf/utils/testIds.ts
+++ perf/utils/testIds.ts
@@ -1,5 +1,16 @@
 type TestId<T extends string> = T & {__brand: 'test-id'}
+
+function isValidSanityId(id: string) {
+  return /^[a-z0-9_-]+$/.test(id)
+}
+
 function defineId<T extends string>(testId: T) {
+  if (isValidSanityId(testId)) {
+    throw new Error(
+      `"${testId}" is not a valid test ID. A test ID must be all lowercase and a valid Sanity document ID`
+    )
+  }
+
   return testId as TestId<T>
 }
 
