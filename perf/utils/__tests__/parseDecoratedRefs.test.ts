7a9350fb4011ecb70ddad93d761135272639a929
Bjørge Næss <bjoerge@gmail.com>
2023-02-16 13:13:24 +0100

    fixup! fix(ci): extract branches and tags from git ref

--- perf/utils/__tests__/parseDecoratedRefs.test.ts
+++ perf/utils/__tests__/parseDecoratedRefs.test.ts
@@ -3,6 +3,10 @@ import {parseDecoratedRefs} from '../gitUtils'
 test('parseDecoratedRefs', () => {
   expect(parseDecoratedRefs('')).toEqual({branches: [], tags: []})
   expect(parseDecoratedRefs('HEAD -> ')).toEqual({branches: [], tags: []})
+  expect(parseDecoratedRefs('HEAD, origin/perf-testing')).toEqual({
+    branches: ['origin/perf-testing'],
+    tags: [],
+  })
   expect(parseDecoratedRefs('HEAD -> test, tag: v3.0.0, v3-current')).toEqual({
     branches: ['test', 'v3-current'],
     tags: ['v3.0.0'],
