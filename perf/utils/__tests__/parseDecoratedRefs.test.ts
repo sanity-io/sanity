a35419de17567b0bbfa4a5be33bdee19d708ca82
Bjørge Næss <bjoerge@gmail.com>
2023-02-16 13:25:01 +0100

    fix(perf): strip origin/ when parsing refs

--- perf/utils/__tests__/parseDecoratedRefs.test.ts
+++ perf/utils/__tests__/parseDecoratedRefs.test.ts
@@ -4,7 +4,7 @@ test('parseDecoratedRefs', () => {
   expect(parseDecoratedRefs('')).toEqual({branches: [], tags: []})
   expect(parseDecoratedRefs('HEAD -> ')).toEqual({branches: [], tags: []})
   expect(parseDecoratedRefs('HEAD, origin/perf-testing')).toEqual({
-    branches: ['origin/perf-testing'],
+    branches: ['perf-testing'],
     tags: [],
   })
   expect(parseDecoratedRefs('HEAD -> test, tag: v3.0.0, v3-current')).toEqual({
