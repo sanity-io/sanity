e7518152408106ba4f12aa3dde7be6457500a111
Bjørge Næss <bjoerge@gmail.com>
2023-02-16 12:33:51 +0100

    debug: add more debug logging

--- perf/git-debug.ts
+++ perf/git-debug.ts
@@ -1,7 +1,8 @@
-import {getCurrentBranch, getCurrentBranchSync} from './utils/gitUtils'
+import {ALL_FIELDS, getCurrentBranch, getCurrentBranchSync, getGitInfoSync} from './utils/gitUtils'
 import {getEnv} from './utils/env'
 
 console.log('ENV:', getEnv('CURRENT_BRANCH', true))
 console.log('git says: ', getCurrentBranchSync())
 getCurrentBranch().then(console.log)
+console.log(getGitInfoSync(ALL_FIELDS))
 console.log('------------------')
