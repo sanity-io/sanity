83da7d1e8c76e0a6573e1fa1fe1311369799d770
Bjørge Næss <bjoerge@gmail.com>
2023-03-03 13:52:46 +0100

    chore(build): remove cypress (#4235)

--- cypress/integration/form-builder/booleanInput.js
+++
@@ -1,7 +0,0 @@
-describe('@sanity/form-builder: BooleanInput', () => {
-  it('should have a height of 35px if no description', () => {
-    cy.visit('/test/desk/input-standard;booleansTest;bd99e58a-845f-4d52-b54a-56a9b7af3be1')
-
-    cy.getField('switchIndeterminate2').invoke('outerHeight').should('be.eq', 35)
-  })
-})
