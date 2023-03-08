5885284bd172d2c3854d452d728f8d9483cc828d
Marius Lundgård <studio@mariuslundgard.com>
2023-03-08 12:01:32 +0100

    test: add document inspector example

--- dev/test-studio/sanity.config.ts
+++ dev/test-studio/sanity.config.ts
@@ -36,6 +36,7 @@ import {
 import {googleTheme} from './themes/google'
 import {vercelTheme} from './themes/vercel'
 import {GoogleLogo, TailwindLogo, VercelLogo} from './components/workspaceLogos'
+import {customInspector} from './inspectors/custom'
 
 const sharedSettings = definePlugin({
   name: 'sharedSettings',
@@ -55,6 +56,13 @@ const sharedSettings = definePlugin({
   },
   document: {
     actions: documentActions,
+    inspectors: (prev, ctx) => {
+      if (ctx.documentType === 'inspectorsTest') {
+        return [customInspector, ...prev]
+      }
+
+      return prev
+    },
     newDocumentOptions,
   },
   plugins: [
