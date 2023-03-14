87da83f0b70ed77874c6a8f2cc89db9e42b528ad
vicbergquist <victoria.bergquist@gmail.com>
2023-03-14 16:21:02 +0100

    docs: add docblock for useDataset hook
    docs: add docblock for useProjectId hook
    
    docs: add docblock for useFormValue hook
    
    docs: add docblock for useSchema hook

--- packages/sanity/src/core/hooks/useProjectId.ts
+++ packages/sanity/src/core/hooks/useProjectId.ts
@@ -1,7 +1,17 @@
 import {useSource} from '../studio'
 
 /**
- * @beta
+ * React hook that returns the current project id
+ *
+ * @public
+ * @returns The current project id
+ * @example Using the `useProjectId` hook
+ * ```ts
+ * function MyComponent() {
+ *   const projectId = useProjectId()
+ *   // ... do something with the project id ...
+ * }
+ * ```
  */
 export function useProjectId(): string {
   return useSource().projectId
