af6bf47de9dc91126a210235003f7dfe7abfd8eb
vicbergquist <victoria.bergquist@gmail.com>
2023-03-14 16:21:02 +0100

    docs: add docblock for useDataset hook
    docs: add docblock for useProjectId hook
    
    docs: add docblock for useFormValue hook
    
    docs: add docblock for useSchema hook

--- packages/sanity/src/core/hooks/useDataset.ts
+++ packages/sanity/src/core/hooks/useDataset.ts
@@ -1,7 +1,17 @@
 import {useSource} from '../studio'
 
 /**
- * @beta
+ * React hook that returns the name of the current dataset
+ *
+ * @public
+ * @returns The name of the current dataset
+ * @example Using the `useDataset` hook
+ * ```ts
+ * function MyComponent() {
+ *   const dataset = useDataset()
+ *   // ... do something with the dataset name ...
+ * }
+ * ```
  */
 export function useDataset(): string {
   return useSource().dataset
--- packages/sanity/src/core/hooks/useSchema.ts
+++ packages/sanity/src/core/hooks/useSchema.ts
@@ -2,7 +2,17 @@ import {Schema} from '@sanity/types'
 import {useSource} from '../studio'
 
 /**
+ * React hook that returns the schema registry for the current project
+ *
  * @public
+ * @returns The schema registry for the current project
+ * @example Using the `useSchema` hook
+ * ```ts
+ * function MyComponent() {
+ *   const schema = useSchema()
+ *   // ... do something with the schema ...
+ * }
+ * ```
  */
 export function useSchema(): Schema {
   return useSource().schema
