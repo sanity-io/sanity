2cf2931b816f031b4d7bb06bf5158adc93259b6e
Herman Wikner <wiknerherman@gmail.com>
2023-03-16 14:15:27 +0100

    feat(core): improve global create new ui

---
+++ packages/sanity/src/core/studio/components/navbar/new-document/NewDocumentButton.style.ts
@@ -0,0 +1,58 @@
+import {Box, Popover, Dialog, Flex, Card} from '@sanity/ui'
+import styled, {css} from 'styled-components'
+
+export const StyledPopover = styled(Popover)(() => {
+  return css`
+    [data-ui='Popover__wrapper'] {
+      width: 320px;
+      display: flex;
+      flex-direction: column;
+      border-radius: ${({theme}) => theme.sanity.radius[3]}px;
+      position: relative;
+      overflow: hidden;
+    }
+  `
+})
+
+// We are using `flex-start` to make sure that the dialogs doesn't jump around when
+// the content changes. This is because the dialog is centered by default, and
+// when the content changes, the dialog will jump to the center of the screen.
+export const StyledDialog = styled(Dialog)(() => {
+  return css`
+    [data-ui='DialogCard'] {
+      justify-content: flex-start;
+    }
+  `
+})
+
+export const TooltipContentBox = styled(Box)`
+  max-width: 300px;
+`
+
+export const RootFlex = styled(Flex)``
+
+export const PopoverHeaderCard = styled(Card)`
+  min-height: max-content;
+`
+
+export const DialogHeaderCard = styled(Card)`
+  min-height: max-content;
+`
+
+export const PopoverListFlex = styled(Flex)(
+  (props: {$maxDisplayedItems: number; $itemHeight: number}) => {
+    const {$maxDisplayedItems, $itemHeight} = props
+
+    // Calculate the max height of the list.
+    // We want the max height to be the height of the list items multiplied by the max number of items.
+    return css`
+      --item-height: ${$itemHeight}px;
+      --max-items: ${$maxDisplayedItems};
+      --list-padding: 0.5rem;
+
+      position: relative;
+      max-height: calc(var(--item-height) * var(--max-items) + var(--list-padding));
+      min-height: calc((var(--item-height) * 1));
+    `
+  }
+)
---
+++ packages/sanity/src/core/studio/components/navbar/new-document/NewDocumentList.tsx
@@ -0,0 +1,130 @@
+import {Flex, Inline, Spinner, Text} from '@sanity/ui'
+import React, {useCallback} from 'react'
+import {CurrentUser} from '@sanity/types'
+import styled from 'styled-components'
+import {CommandList} from '../../../../components'
+import {supportsTouch} from '../../../../util'
+import {NewDocumentOption, PreviewLayout} from './types'
+import {INLINE_PREVIEW_HEIGHT, NewDocumentListOption} from './NewDocumentListOption'
+
+const ContentFlex = styled(Flex)`
+  min-height: 100px;
+`
+
+const QueryString = styled.strong`
+  word-break: break-all;
+`
+
+export interface NewDocumentListProps {
+  currentUser: CurrentUser | null
+  loading: boolean
+  onDocumentClick: () => void
+  options: NewDocumentOption[]
+  preview: PreviewLayout
+  searchQuery: string
+  textInputElement: HTMLInputElement | null
+}
+
+export function NewDocumentList(props: NewDocumentListProps) {
+  const {currentUser, loading, onDocumentClick, options, preview, searchQuery, textInputElement} =
+    props
+  const hasOptions = options.length > 0 && !loading
+
+  const handleDocumentClick = useCallback(() => {
+    onDocumentClick()
+  }, [onDocumentClick])
+
+  const getItemDisabled = useCallback(
+    (index: number) => options[index]?.hasPermission === false,
+    [options]
+  )
+
+  const renderItem = useCallback(
+    (item: NewDocumentOption) => {
+      return (
+        <NewDocumentListOption
+          currentUser={currentUser}
+          key={item.id}
+          onClick={handleDocumentClick}
+          option={item}
+          preview={preview}
+        />
+      )
+    },
+    [currentUser, handleDocumentClick, preview]
+  )
+
+  // Render loading state
+  if (loading) {
+    return (
+      <ContentFlex
+        align="center"
+        flex={1}
+        height="fill"
+        justify="center"
+        padding={4}
+        sizing="border"
+      >
+        <Inline space={2}>
+          <Spinner muted size={1} />
+
+          <Text muted size={1}>
+            Loading…
+          </Text>
+        </Inline>
+      </ContentFlex>
+    )
+  }
+
+  // Render no search results state
+  if (!hasOptions && searchQuery) {
+    return (
+      <ContentFlex
+        align="center"
+        flex={1}
+        height="fill"
+        justify="center"
+        padding={4}
+        sizing="border"
+      >
+        <Text align="center" muted size={1}>
+          No results found for <QueryString>“{searchQuery}”</QueryString>
+        </Text>
+      </ContentFlex>
+    )
+  }
+
+  // Render no options state
+  if (!hasOptions) {
+    return (
+      <ContentFlex
+        align="center"
+        flex={1}
+        height="fill"
+        justify="center"
+        padding={4}
+        sizing="border"
+      >
+        <Text align="center" muted size={1}>
+          No documents found
+        </Text>
+      </ContentFlex>
+    )
+  }
+
+  return (
+    <CommandList
+      activeItemDataAttr="data-hovered"
+      ariaLabel="New document"
+      autoFocus={!supportsTouch}
+      getItemDisabled={getItemDisabled}
+      inputElement={textInputElement}
+      itemHeight={INLINE_PREVIEW_HEIGHT}
+      items={options}
+      overscan={5}
+      padding={preview === 'inline' ? 1 : 2}
+      paddingBottom={0}
+      renderItem={renderItem}
+    />
+  )
+}
