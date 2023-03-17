815eac80973166c5215d9853c7b43fb03f42a333
Bjørge Næss <bjoerge@gmail.com>
2023-03-17 14:50:07 +0100

    fix(perf-runner): support comparing with local build

--- perf/queries.ts
+++ perf/queries.ts
@@ -3,4 +3,4 @@
  */
 export const currentBranch = `*[_type=='branch' && name == $branch] {
   "deployments": *[_type == 'deployment' && name=="performance-studio" && status=="succeeded" && branch._ref == ^._id]
-} [count(deployments) > 0].deployments[]  | order(_createdAt asc) [0...10] {"id": _id, url}`
+} [count(deployments) > 0].deployments[]  | order(_createdAt asc) [0...$count] {"id": _id, url}`
