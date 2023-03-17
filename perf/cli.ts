b06b4d3ab30c348773190390c2e7b7ce0bff7b68
Bjørge Næss <bjoerge@gmail.com>
2023-03-17 13:59:07 +0100

    refactor(perf): convert testCurrentBranch into a CLI

--- perf/cli.ts
+++ perf/cli.ts
@@ -1,22 +1,35 @@
+import {parseArgs} from 'node:util'
 import globby from 'globby'
+import {config} from 'dotenv'
+import {createClient} from '@sanity/client'
 import {run} from './runner/runner'
-import {studioMetricsClient} from './config/studioMetricsClient'
-import {perfStudioClient} from './config/perfStudioClient'
 import {findEnv, readEnv} from './config/envVars'
 import * as queries from './queries'
 import {getCurrentBranchSync} from './runner/utils/gitUtils'
+import {STUDIO_DATASET, STUDIO_PROJECT_ID} from './config/constants'
 
-const headless = findEnv('PERF_TEST_HEADLESS') !== 'false'
+config({path: `${__dirname}/.env`})
 
-async function main() {
+async function main(args: {branch?: string; headless?: boolean}) {
   const testFiles = await globby(`${__dirname}/tests/**/*.test.ts`)
-  const currentBranch = findEnv('PERF_TEST_BRANCH') || getCurrentBranchSync()
+  const branch = args.branch || findEnv('PERF_TEST_BRANCH') || getCurrentBranchSync()
+  const headless = args.headless ?? findEnv('PERF_TEST_HEADLESS') !== 'false'
+
+  const studioMetricsClient = createClient({
+    projectId: 'c1zuxvqn',
+    dataset: 'production',
+    token: readEnv('PERF_TEST_METRICS_TOKEN'),
+    apiVersion: '2023-02-03',
+    useCdn: false,
+  })
+
   const deployments = await studioMetricsClient.fetch(queries.currentBranch, {
-    branch: currentBranch,
+    branch,
+    headless,
   })
 
   if (deployments.length === 0) {
-    console.error('No deployments found for branch %s', currentBranch)
+    console.error('No deployments found for branch %s', branch)
     process.exit(0)
   }
   // eslint-disable-next-line no-console
@@ -25,10 +38,18 @@ async function main() {
   if (deployments.length === 1) {
     console.error(
       'Only a single deployment found for current branch (%s). Two or more deployments are required in order to run the performance tests',
-      currentBranch
+      branch
     )
     process.exit(0)
   }
+
+  const perfStudioClient = createClient({
+    projectId: STUDIO_PROJECT_ID,
+    dataset: STUDIO_DATASET,
+    token: readEnv('PERF_TEST_SANITY_TOKEN'),
+    apiVersion: '2023-02-03',
+    useCdn: false,
+  })
 
   return run({
     testFiles,
@@ -42,7 +63,21 @@ async function main() {
   })
 }
 
-main().then(
+const {values: args} = parseArgs({
+  args: process.argv.slice(2),
+  options: {
+    branch: {
+      type: 'string',
+      short: 'b',
+    },
+    headless: {
+      type: 'boolean',
+      short: 'h',
+    },
+  },
+})
+
+main(args).then(
   () => {
     // eslint-disable-next-line no-console
     console.log('Ran performance test suite')
