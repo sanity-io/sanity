35fd1f62cc0b433206cc2aaa652b613b2e4be25a
Bjørge Næss <bjoerge@gmail.com>
2023-03-20 17:06:25 +0100

    fix(perf-runner): include base branch in query to get recent deployments

--- perf/queries.ts
+++ perf/queries.ts
@@ -2,5 +2,8 @@
  * Get all recent deployments for a given branch
  */
 export const branchDeploymentsQuery = `*[_type=='branch' && name == $branch] {
-  "deployments": *[_type == 'deployment' && name=="performance-studio" && status=="succeeded" && branch._ref == ^._id]
+  "branchId": _id,
+  "baseBranchId": *[_type=='branch' && name == ^.base][0]._id
+} {
+  "deployments": *[_type == 'deployment' && name=="performance-studio" && status=="succeeded" && (branch._ref in [^.branchId, ^.baseBranchId])]
 } [count(deployments) > 0].deployments[]  | order(_createdAt asc) [0...$count] {_id, deploymentId, url, label}`
