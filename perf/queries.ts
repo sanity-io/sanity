b4172732facc797ef2cf9287048c20abbab22fbe
Binoy Patel <me@binoy.io>
2023-03-21 11:11:23 -0400

    fix(perf-runner): use single deployment from base in query
    - remove perf/yarn.lock

--- perf/queries.ts
+++ perf/queries.ts
@@ -5,5 +5,8 @@ export const branchDeploymentsQuery = `*[_type=='branch' && name == $branch] {
   "branchId": _id,
   "baseBranchId": *[_type=='branch' && name == ^.base][0]._id
 } {
-  "deployments": *[_type == 'deployment' && name=="performance-studio" && status=="succeeded" && (branch._ref in [^.branchId, ^.baseBranchId])]
-} [count(deployments) > 0].deployments[]  | order(_createdAt asc) [0...$count] {_id, deploymentId, url, label}`
+  "deployments": [
+    ...*[_type == 'deployment' && name=="performance-studio" && status=="succeeded" && (branch._ref in [^.branchId])],
+    *[_type == 'deployment' && name=="performance-studio" && status=="succeeded" && (branch._ref in [^.baseBranchId])][0]
+    ]
+} [count(deployments) > 0].deployments[] | order(_createdAt asc) [0...$count] {_id, deploymentId, url, label}`
