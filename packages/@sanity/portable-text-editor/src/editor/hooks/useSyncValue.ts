b253bbc7e92d5c8fd02d58ef3ad2199619fc3647
Per-Kristian Nordnes <per.kristian.nordnes@gmail.com>
2023-03-21 17:24:18 +0100

    refactor(portable-text-editor): make useSyncValue more readable
    This code was a bit hard to read. Extract some of the code into own functions and make variable names more sensible. Also add some comments.

--- packages/@sanity/portable-text-editor/src/editor/hooks/useSyncValue.ts
+++ packages/@sanity/portable-text-editor/src/editor/hooks/useSyncValue.ts
@@ -2,7 +2,7 @@
 import React, {useMemo, useRef} from 'react'
 import {PortableTextBlock} from '@sanity/types'
 import {isEqual} from 'lodash'
-import {Editor, Transforms, Node} from 'slate'
+import {Editor, Transforms, Node, Descendant} from 'slate'
 import {PortableTextEditor} from '../PortableTextEditor'
 import {PortableTextSlateEditor} from '../../types/editor'
 import {debugWithName} from '../../utils/debug'
@@ -90,91 +90,37 @@ export function useSyncValue(
           withoutPatching(slateEditor, () => {
             Editor.withoutNormalizing(slateEditor, () => {
               const childrenLength = slateEditor.children.length
+              // Remove blocks that have become superfluous
               if (slateValueFromProps.length < childrenLength) {
                 Array.from(Array(childrenLength - slateValueFromProps.length)).forEach(
                   (_, index) => {
-                    const bIndex = childrenLength - 1 - index
-                    if (bIndex > 0) {
+                    const blockIndex = childrenLength - 1 - index
+                    if (blockIndex > 0) {
                       Transforms.removeNodes(slateEditor, {
-                        at: [bIndex],
+                        at: [blockIndex],
                       })
                     }
                   }
                 )
               }
-              slateValueFromProps.forEach((c, i) => {
-                const oldBlock = slateEditor.children[i]
-                if (oldBlock && !isEqual(c, oldBlock)) {
-                  // If the blocks have the same _key, update it.
-                  if (oldBlock._key === c._key) {
-                    debug('Updating changed block', oldBlock, c)
-                    Transforms.setNodes(slateEditor, c as Partial<Node>, {at: [i]})
-                    // Text block must have their children updated as well (setNode does not target a node's children)
-                    if (slateEditor.isTextBlock(c) && slateEditor.isTextBlock(oldBlock)) {
-                      const bChildrenLength = oldBlock.children.length
-                      if (c.children.length < bChildrenLength) {
-                        Array.from(Array(bChildrenLength - c.children.length)).forEach(
-                          (_, index) => {
-                            const cIndex = bChildrenLength - 1 - index
-                            if (cIndex > 0) {
-                              debug('Removing child')
-                              Transforms.removeNodes(slateEditor, {
-                                at: [i, cIndex],
-                              })
-                            }
-                          }
-                        )
-                      }
-                      c.children.forEach((bc, bi) => {
-                        const oldChild = oldBlock.children[bi]
-                        if (!isEqual(bc, oldChild)) {
-                          if (bc._key === oldChild?._key) {
-                            debug('Updating changed child', bc)
-                            Transforms.setNodes(slateEditor, bc as Partial<Node>, {at: [i, bi]})
-                            // If it's a inline block, also update the void text node key
-                            if (bc._type !== 'span') {
-                              debug('Updating changed inline object child', bc)
-                              Transforms.setNodes(
-                                slateEditor,
-                                {_key: `${c._key}-void-child`},
-                                {
-                                  at: [i, bi, 0],
-                                  voids: true,
-                                }
-                              )
-                            }
-                          } else if (oldChild) {
-                            debug('Replacing child', bc)
-                            Transforms.removeNodes(slateEditor, {at: [i, bi]})
-                            withPreserveKeys(slateEditor, () => {
-                              Transforms.insertNodes(slateEditor, bc as Node, {at: [i, bi]})
-                            })
-                          } else if (!oldChild) {
-                            debug('Inserting new child', bc)
-                            withPreserveKeys(slateEditor, () => {
-                              Transforms.insertNodes(slateEditor, bc as Node, {at: [i, bi]})
-                            })
-                          }
-                        }
-                      })
-                    }
+              // Go through all of the blocks and see if they need to be updated
+              slateValueFromProps.forEach((currentBlock, currentBlockIndex) => {
+                const oldBlock = slateEditor.children[currentBlockIndex]
+                const hasChanges = oldBlock && !isEqual(currentBlock, oldBlock)
+                if (hasChanges) {
+                  if (oldBlock._key === currentBlock._key) {
+                    debug('Updating block', oldBlock, currentBlock)
+                    _updateBlock(slateEditor, currentBlock, oldBlock, currentBlockIndex)
                   } else {
-                    debug('Replacing block', oldBlock, c)
-                    const currentSel = slateEditor.selection
-                    Transforms.deselect(slateEditor)
-                    Transforms.removeNodes(slateEditor, {at: [i]})
-                    withPreserveKeys(slateEditor, () => {
-                      Transforms.insertNodes(slateEditor, c, {at: [i]})
-                    })
-                    if (currentSel) {
-                      Transforms.select(slateEditor, currentSel)
-                    }
+                    debug('Replacing block', oldBlock, currentBlock)
+                    _replaceBlock(slateEditor, currentBlock, currentBlockIndex)
                   }
                 }
+                // Insert new blocks exceeding the original value.
                 if (!oldBlock) {
-                  debug('Adding new block', c)
+                  debug('Adding new block', currentBlock)
                   withPreserveKeys(slateEditor, () => {
-                    Transforms.insertNodes(slateEditor, c, {at: [i]})
+                    Transforms.insertNodes(slateEditor, currentBlock, {at: [currentBlockIndex]})
                   })
                 }
               })
@@ -186,4 +132,104 @@ export function useSyncValue(
     },
     [editor, isPending, readOnly, slateEditor]
   )
+}
+
+/**
+ * This code is moved out of the above algorithm to keep complexity down.
+ * @internal
+ */
+function _replaceBlock(
+  slateEditor: PortableTextSlateEditor,
+  currentBlock: Descendant,
+  currentBlockIndex: number
+) {
+  // While replacing the block, temporarily deselect the editor,
+  // then optimistically try to restore the selection afterwards.
+  const currentSel = slateEditor.selection
+  Transforms.deselect(slateEditor)
+  Transforms.removeNodes(slateEditor, {at: [currentBlockIndex]})
+  withPreserveKeys(slateEditor, () => {
+    Transforms.insertNodes(slateEditor, currentBlock, {at: [currentBlockIndex]})
+  })
+  if (currentSel) {
+    Transforms.select(slateEditor, currentSel)
+  }
+}
+
+/**
+ * This code is moved out of the above algorithm to keep complexity down.
+ * @internal
+ */
+function _updateBlock(
+  slateEditor: PortableTextSlateEditor,
+  currentBlock: Descendant,
+  oldBlock: Descendant,
+  currentBlockIndex: number
+) {
+  // Update the root props on the block
+  Transforms.setNodes(slateEditor, currentBlock as Partial<Node>, {
+    at: [currentBlockIndex],
+  })
+  // Text block's need to have their children updated as well (setNode does not target a node's children)
+  if (slateEditor.isTextBlock(currentBlock) && slateEditor.isTextBlock(oldBlock)) {
+    const oldBlockChildrenLength = oldBlock.children.length
+    if (currentBlock.children.length < oldBlockChildrenLength) {
+      // Remove any children that have become superfluous
+      Array.from(Array(oldBlockChildrenLength - currentBlock.children.length)).forEach(
+        (_, index) => {
+          const childIndex = oldBlockChildrenLength - 1 - index
+          if (childIndex > 0) {
+            debug('Removing child')
+            Transforms.removeNodes(slateEditor, {
+              at: [currentBlockIndex, childIndex],
+            })
+          }
+        }
+      )
+    }
+    currentBlock.children.forEach((currentBlockChild, currentBlockChildIndex) => {
+      const oldBlockChild = oldBlock.children[currentBlockChildIndex]
+      const isChildChanged = !isEqual(currentBlockChild, oldBlockChild)
+      if (isChildChanged) {
+        // Update if this is the same child
+        if (currentBlockChild._key === oldBlockChild?._key) {
+          debug('Updating changed child', currentBlockChild)
+          Transforms.setNodes(slateEditor, currentBlockChild as Partial<Node>, {
+            at: [currentBlockIndex, currentBlockChildIndex],
+          })
+          // If it's a inline block, also update the void text node key
+          if (currentBlockChild._type !== 'span') {
+            debug('Updating changed inline object child', currentBlockChild)
+            Transforms.setNodes(
+              slateEditor,
+              {_key: `${currentBlock._key}-void-child`},
+              {
+                at: [currentBlockIndex, currentBlockChildIndex, 0],
+                voids: true,
+              }
+            )
+          }
+          // Replace the child if _key's are different
+        } else if (oldBlockChild) {
+          debug('Replacing child', currentBlockChild)
+          Transforms.removeNodes(slateEditor, {
+            at: [currentBlockIndex, currentBlockChildIndex],
+          })
+          withPreserveKeys(slateEditor, () => {
+            Transforms.insertNodes(slateEditor, currentBlockChild as Node, {
+              at: [currentBlockIndex, currentBlockChildIndex],
+            })
+          })
+          // Insert it if it didn't exist before
+        } else if (!oldBlockChild) {
+          debug('Inserting new child', currentBlockChild)
+          withPreserveKeys(slateEditor, () => {
+            Transforms.insertNodes(slateEditor, currentBlockChild as Node, {
+              at: [currentBlockIndex, currentBlockChildIndex],
+            })
+          })
+        }
+      }
+    })
+  }
 }
