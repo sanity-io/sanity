9a03202c9e3c17c9274f9144b9fb460ca0705eb1
Bjørge Næss <bjoerge@gmail.com>
2023-03-29 13:26:46 +0200

    fix(perf): make sure form callbacks are stable

--- packages/sanity/src/core/form/studio/contexts/FormCallbacks.tsx
+++ packages/sanity/src/core/form/studio/contexts/FormCallbacks.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/no-unused-prop-types */
 import {Path} from '@sanity/types'
-import React, {memo, useContext} from 'react'
+import React, {memo, useCallback, useContext, useMemo, useRef} from 'react'
 import {FormPatch, PatchEvent} from '../../patch'
 
 /** @internal */
@@ -24,8 +24,66 @@ export const FormCallbacksContext = React.createContext<FormCallbacksValue | nul
 export const FormCallbacksProvider = memo(function FormCallbacksProvider(
   props: FormCallbacksValue & {children: React.ReactNode}
 ) {
+  const ref = useRef<FormCallbacksValue>(props)
+  ref.current = props
+
+  const transformPatches = useCallback(
+    (patches: FormPatch[]) => {
+      return ref.current?.transformPatches?.(patches) ?? []
+    },
+    [ref]
+  )
+
+  const onChange = useCallback((patchEvent: PatchEvent) => {
+    ref.current.onChange(patchEvent)
+  }, [])
+
+  const onPathFocus = useCallback((path: Path) => {
+    ref.current.onPathFocus(path)
+  }, [])
+  const onPathBlur = useCallback((path: Path) => {
+    ref.current.onPathBlur(path)
+  }, [])
+  const onPathOpen = useCallback((path: Path) => {
+    ref.current.onPathOpen(path)
+  }, [])
+  const onSetPathCollapsed = useCallback((path: Path, collapsed: boolean) => {
+    ref.current.onSetPathCollapsed(path, collapsed)
+  }, [])
+  const onSetFieldSetCollapsed = useCallback((path: Path, collapsed: boolean) => {
+    ref.current.onSetFieldSetCollapsed(path, collapsed)
+  }, [])
+  const onFieldGroupSelect = useCallback((path: Path, fieldGroupName: string) => {
+    ref.current.onFieldGroupSelect(path, fieldGroupName)
+  }, [])
+
+  const contextValue: FormCallbacksValue = useMemo(
+    () => ({
+      transformPatches,
+      onChange,
+      onPathFocus,
+      onPathBlur,
+      onPathOpen,
+      onSetPathCollapsed,
+      onSetFieldSetCollapsed,
+      onFieldGroupSelect,
+    }),
+    [
+      onChange,
+      onFieldGroupSelect,
+      onPathBlur,
+      onPathFocus,
+      onPathOpen,
+      onSetFieldSetCollapsed,
+      onSetPathCollapsed,
+      transformPatches,
+    ]
+  )
+
   return (
-    <FormCallbacksContext.Provider value={props}>{props.children}</FormCallbacksContext.Provider>
+    <FormCallbacksContext.Provider value={contextValue}>
+      {props.children}
+    </FormCallbacksContext.Provider>
   )
 })
 
