45d6b8563f42bab42db0bc12fa4d7c83cc2ca376
Marius Lundgård <studio@mariuslundgard.com>
2023-04-13 15:44:52 +0200

    refactor(desk): convert validation to document inspector

---
+++ packages/sanity/src/desk/panes/document/inspectors/validation/getPathTypes.ts
@@ -0,0 +1,129 @@
+/* eslint-disable no-loop-func */
+
+import {Path, SanityDocument, SchemaType} from '@sanity/types'
+import {isArray, isRecord} from 'sanity'
+
+export function getPathTypes(options: {
+  path: Path
+  schemaType: SchemaType
+  value: Partial<SanityDocument> | null
+}): SchemaType[] {
+  const {path, schemaType, value} = options
+  const result: SchemaType[] = []
+
+  let s = schemaType
+  let v: unknown = value
+
+  for (const segment of path) {
+    // field name
+    if (typeof segment === 'string') {
+      if (!isRecord(v) && v !== undefined) {
+        throw new Error(`Parent value is not an object, cannot get path segment: .${segment}`)
+      }
+
+      if (s.jsonType !== 'object') {
+        throw new Error(
+          `Parent type is not an object schema type, cannot get path segment: .${segment}`
+        )
+      }
+
+      v = v?.[segment]
+
+      const field = s.fields.find((f) => f.name === segment)
+
+      if (!field) {
+        throw new Error(`Field type not found: .${segment}`)
+      }
+
+      s = field.type
+
+      result.push(s)
+
+      continue
+    }
+
+    // array item index
+    if (typeof segment === 'number') {
+      if (!isArray(v) && v !== undefined) {
+        throw new Error(`Parent value is not an array, cannot get path segment: [${segment}]`)
+      }
+
+      if (s.jsonType !== 'array') {
+        throw new Error(
+          `Parent type is not an array schema type, cannot get path segment: [${segment}]`
+        )
+      }
+
+      v = v?.[segment]
+
+      const itemType = s.of.find((ofType) => {
+        if (typeof v === 'string') {
+          return ofType.jsonType === 'string'
+        }
+
+        if (typeof v === 'number') {
+          return ofType.jsonType === 'number'
+        }
+
+        if (typeof v === 'boolean') {
+          return ofType.jsonType === 'boolean'
+        }
+
+        if (isRecord(v)) {
+          return ofType.name === v?._type
+        }
+
+        return false
+      })
+
+      if (!itemType) {
+        throw new Error(`Item type not found: [${segment}]`)
+      }
+
+      s = itemType
+
+      result.push(s)
+
+      continue
+    }
+
+    // array item key
+    if (isRecord(segment) && segment._key) {
+      if (!isArray(v)) {
+        throw new Error(
+          `Parent value is not an anrray, cannot get path segment: [_key == ${segment}]`
+        )
+      }
+
+      if (s.jsonType !== 'array') {
+        throw new Error(
+          `Parent type is not an array schema type, cannot get path segment: .${segment}`
+        )
+      }
+
+      const values = v ?? []
+
+      v = values.find((i) => isRecord(i) && i._key === segment._key)
+
+      if (!isRecord(v)) {
+        throw new Error(`Array item not found: [_key == ${segment._key}]`)
+      }
+
+      const ofType = s.of.find((i) => isRecord(v) && i.name === v?._type)
+
+      if (!ofType) {
+        throw new Error(`Array item type not found: .${v?._type}`)
+      }
+
+      s = ofType
+
+      result.push(s)
+
+      continue
+    }
+
+    throw new Error(`Invalid path segment: ${JSON.stringify(segment)}`)
+  }
+
+  return result
+}
---
+++ packages/sanity/src/desk/panes/document/inspectors/validation/index.ts
@@ -0,0 +1,57 @@
+import {CheckmarkCircleIcon, ErrorOutlineIcon, WarningOutlineIcon} from '@sanity/icons'
+import {useMemo} from 'react'
+import {VALIDATION_INSPECTOR_NAME} from '../../constants'
+import {ValidationInspector} from './ValidationInspector'
+import {
+  DocumentInspector,
+  DocumentInspectorMenuItem,
+  DocumentInspectorUseMenuItemProps,
+  FormNodeValidation,
+  isValidationError,
+  isValidationWarning,
+  useValidationStatus,
+} from 'sanity'
+
+function useMenuItem(props: DocumentInspectorUseMenuItemProps): DocumentInspectorMenuItem {
+  const {documentId, documentType} = props
+  const {validation: validationMarkers} = useValidationStatus(documentId, documentType)
+
+  const validation: FormNodeValidation[] = useMemo(
+    () =>
+      validationMarkers.map((item) => ({
+        level: item.level,
+        message: item.item.message,
+        path: item.path,
+      })),
+    [validationMarkers]
+  )
+
+  const hasErrors = validation.some(isValidationError)
+  const hasWarnings = validation.some(isValidationWarning)
+
+  const icon = useMemo(() => {
+    if (hasErrors) return ErrorOutlineIcon
+    if (hasWarnings) return WarningOutlineIcon
+    return CheckmarkCircleIcon
+  }, [hasErrors, hasWarnings])
+
+  const tone = useMemo(() => {
+    if (hasErrors) return 'critical'
+    if (hasWarnings) return 'caution'
+    return 'positive'
+  }, [hasErrors, hasWarnings])
+
+  return {
+    hidden: validation.length === 0,
+    icon,
+    title: 'Validation',
+    tone,
+    showAsAction: true,
+  }
+}
+
+export const validationInspector: DocumentInspector = {
+  name: VALIDATION_INSPECTOR_NAME,
+  component: ValidationInspector,
+  useMenuItem,
+}
