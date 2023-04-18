09afa59ad69870da94a096d3d466e504d5a919c0
Binoy Patel <me@binoy.io>
2023-04-18 11:20:05 -0400

    fix(core): add virtualization to array of objects and references (#4299)
    * fix(core): memoize object input
    
    * fix(core): wip virtualize array list
    
    * fix(core): update virtualization configuration
    
    - account for custom height components
    - fix height issue and account for grid gap in the calculation
    - cleanup ref code in DocumentPanel
    - change handleDragEnd function to directly pass the index
    
    * fix(core): fix sorting on list
    
    * fix(core): fix dragging elements past virtual list
    
    * fix(core): reorganize imports and remove unused
    
    * Canary Version Bump
    
    * fix(core): fix padding issue with arrays
    
    * fix(core): use item key instead of index
    
    * fix(core): fix scrolling to error from validation menu
    
    * fix(core): fix issue when edit modal is closed scrolling breaks
    
    * fix(core): fix PR feedback
    
    * fix(core): fixes issue with focus and opening object item modals

--- packages/sanity/src/core/form/inputs/arrays/ArrayOfObjectsInput/List/ListArrayInput.tsx
+++ packages/sanity/src/core/form/inputs/arrays/ArrayOfObjectsInput/List/ListArrayInput.tsx
@@ -1,7 +1,14 @@
-/* eslint-disable react/jsx-handler-names */
-import {Card, Stack, Text} from '@sanity/ui'
-import React, {useCallback, useMemo} from 'react'
+import {Card, Stack, Text, useBoundaryElement, useTheme} from '@sanity/ui'
+import {isKeySegment} from '@sanity/types'
+import React, {useCallback, useMemo, useRef, useState} from 'react'
 import shallowEquals from 'shallow-equals'
+import {
+  defaultRangeExtractor,
+  useVirtualizer,
+  VirtualizerOptions,
+  type Range,
+} from '@tanstack/react-virtual'
+import type {DragStartEvent} from '@dnd-kit/core'
 import {Item, List} from '../../common/list'
 import {ArrayOfObjectsInputProps, ObjectItem} from '../../../../types'
 import {ArrayOfObjectsItem} from '../../../../members'
@@ -26,12 +33,17 @@ export function ListArrayInput<Item extends ObjectItem>(props: ArrayOfObjectsInp
     onInsert,
     onItemMove,
     onUpload,
+    focusPath,
     renderPreview,
     renderField,
     renderItem,
     renderInput,
     arrayFunctions: ArrayFunctions = ArrayOfObjectsFunctions,
   } = props
+
+  // Stores the index of the item being dragged
+  const [activeDragItemIndex, setActiveDragItemIndex] = useState<number | null>(null)
+  const {space} = useTheme().sanity
 
   const handlePrepend = useCallback(
     (item: Item) => {
@@ -47,15 +59,109 @@ export function ListArrayInput<Item extends ObjectItem>(props: ArrayOfObjectsInp
     [onInsert]
   )
 
-  const sortable = schemaType.options?.sortable !== false
-
   const memberKeys = useMemoCompare(
     useMemo(() => members.map((member) => member.key), [members]),
     shallowEquals
   )
 
+  const boundaryElement = useBoundaryElement()
+  const parentRef = useRef<HTMLDivElement>(null)
+
+  const focusPathKey = useMemo(() => {
+    return isKeySegment(focusPath[0]) ? focusPath[0]._key : focusPath[0]
+  }, [focusPath])
+
+  /**
+   * This is a custom range extractor that adds the activeDragItemIndex and focusedItem to the range
+   * so that the item is always rendered and it can perform it's own actions
+   * Note: When adding an index make sure the range includes all index or the scroll will jump
+   */
+  const rangeExtractor = useCallback(
+    (range: Range) => {
+      const newRange = {...range}
+
+      // Update start and end indexes based on activeDragItemIndex
+      if (activeDragItemIndex !== null) {
+        newRange.startIndex = Math.min(range.startIndex, activeDragItemIndex)
+        newRange.endIndex = Math.max(range.endIndex, activeDragItemIndex)
+      }
+
+      // Update start and end indexes based on focusPathKey
+      if (focusPathKey) {
+        const index = memberKeys.findIndex((key) => key === focusPathKey)
+        if (index !== -1) {
+          newRange.startIndex = Math.min(newRange.startIndex, index)
+          newRange.endIndex = Math.max(newRange.endIndex, index)
+        }
+      }
+
+      return defaultRangeExtractor(newRange)
+    },
+    [activeDragItemIndex, focusPathKey, memberKeys]
+  )
+
+  /**
+   * It observes the scroll element and calls the callback function with the difference between the
+   * scroll's scrollTop and the offsetTop of the parent element of the observed element.
+   * it return an unsubscribe function that removes the event listener when called.
+   */
+  const observeElementOffset = useCallback<
+    VirtualizerOptions<HTMLElement, Element>['observeElementOffset']
+  >((instance, cb) => {
+    if (!instance.scrollElement) {
+      return undefined
+    }
+
+    const scroll = instance.scrollElement
+
+    const handleScroll = () => {
+      const itemOffset = parentRef.current?.offsetTop ?? 0
+      cb(scroll.scrollTop - itemOffset)
+    }
+
+    handleScroll()
+
+    instance.scrollElement.addEventListener('scroll', handleScroll, {
+      capture: false,
+      passive: true,
+    })
+
+    return () => {
+      scroll.removeEventListener('scroll', handleScroll)
+    }
+  }, [])
+
+  // This is the estimated size of an item in the list. The reason this is an estimate is because
+  // custom components can have different dimensions and the library recalculate the size of the element
+  const estimateSize = useCallback(() => 53, [])
+
+  const virtualizer = useVirtualizer({
+    count: members.length,
+    estimateSize,
+    getScrollElement: useCallback(() => boundaryElement.element, [boundaryElement.element]),
+    observeElementOffset,
+    rangeExtractor,
+    getItemKey: useCallback((index: number) => memberKeys[index], [memberKeys]),
+  })
+
+  const items = virtualizer.getVirtualItems()
+
+  const handleItemMoveStart = useCallback((event: DragStartEvent) => {
+    const {active} = event
+    setActiveDragItemIndex(active.data.current?.sortable?.index)
+  }, [])
+
+  const handleItemMoveEnd = useCallback(() => {
+    setActiveDragItemIndex(null)
+  }, [])
+
+  const sortable = schemaType.options?.sortable !== false
+
+  const listGridGap = 1
+  const paddingY = 1
+
   return (
-    <Stack space={3}>
+    <Stack space={3} ref={parentRef}>
       <UploadTargetCard
         types={schemaType.of}
         resolveUploader={resolveUploader}
@@ -71,35 +177,66 @@ export function ListArrayInput<Item extends ObjectItem>(props: ArrayOfObjectsInp
               </Text>
             </Card>
           ) : (
-            <Card border radius={1}>
+            <Card
+              border
+              radius={1}
+              style={{
+                // This is not memoized since it changes on scroll so it will change anyways making memo useless
+                // Account for grid gap
+                height: `${
+                  virtualizer.getTotalSize() + items.length * space[listGridGap] + space[paddingY]
+                }px`,
+                width: '100%',
+                position: 'relative',
+              }}
+            >
               <List
                 axis="y"
-                gap={1}
-                paddingY={1}
+                gap={listGridGap}
+                paddingY={paddingY}
                 items={memberKeys}
                 onItemMove={onItemMove}
+                onItemMoveStart={handleItemMoveStart}
+                onItemMoveEnd={handleItemMoveEnd}
                 sortable={sortable}
+                style={{
+                  // This is not memoized since it changes on scroll so it will change anyways making memo useless
+                  position: 'absolute',
+                  top: 0,
+                  left: 0,
+                  width: '100%',
+                  transform: `translateY(${items[0].start}px)`,
+                }}
               >
-                {members.map((member) => (
-                  <Item key={member.key} sortable={sortable} id={member.key}>
-                    {member.kind === 'item' && (
-                      <ArrayOfObjectsItem
-                        member={member}
-                        renderItem={renderItem}
-                        renderField={renderField}
-                        renderInput={renderInput}
-                        renderPreview={renderPreview}
-                      />
-                    )}
-                    {member.kind === 'error' && (
-                      <ErrorItem
-                        sortable={sortable}
-                        member={member}
-                        onRemove={() => props.onItemRemove(member.key)}
-                      />
-                    )}
-                  </Item>
-                ))}
+                {items.map((virtualRow) => {
+                  const member = members[virtualRow.index]
+                  return (
+                    <Item
+                      ref={virtualizer.measureElement}
+                      key={virtualRow.key}
+                      sortable={sortable}
+                      data-index={virtualRow.index}
+                      id={member.key}
+                    >
+                      {member.kind === 'item' && (
+                        <ArrayOfObjectsItem
+                          member={member}
+                          renderItem={renderItem}
+                          renderField={renderField}
+                          renderInput={renderInput}
+                          renderPreview={renderPreview}
+                        />
+                      )}
+                      {member.kind === 'error' && (
+                        <ErrorItem
+                          sortable={sortable}
+                          member={member}
+                          onRemove={() => props.onItemRemove(member.key)}
+                        />
+                      )}
+                    </Item>
+                  )
+                })}
               </List>
             </Card>
           )}
--- packages/sanity/src/core/form/inputs/arrays/common/list.tsx
+++ packages/sanity/src/core/form/inputs/arrays/common/list.tsx
@@ -1,17 +1,22 @@
 import {Box, Card, Grid} from '@sanity/ui'
-import React, {ComponentProps, useCallback, useMemo} from 'react'
+import React, {
+  type ComponentProps,
+  type ForwardedRef,
+  forwardRef,
+  memo,
+  useCallback,
+  useMemo,
+} from 'react'
 import styled, {css} from 'styled-components'
 import {
-  AutoScrollOptions,
   closestCenter,
   DndContext,
-  DragEndEvent,
   KeyboardSensor,
   PointerSensor,
-  SensorOptions,
   useSensor,
   useSensors,
 } from '@dnd-kit/core'
+import type {SensorOptions, AutoScrollOptions, DragStartEvent, DragEndEvent} from '@dnd-kit/core'
 import {
   horizontalListSortingStrategy,
   SortableContext,
@@ -19,7 +24,7 @@ import {
   useSortable,
   verticalListSortingStrategy,
 } from '@dnd-kit/sortable'
-import {CSS, Transition} from '@dnd-kit/utilities'
+import {CSS} from '@dnd-kit/utilities'
 import {restrictToHorizontalAxis, restrictToVerticalAxis} from '@dnd-kit/modifiers'
 import {SortableItemIdContext} from './DragHandle'
 import {restrictToParentElementWithMargins} from './dndkit-modifier/restrictToParentElementWithMargins'
@@ -60,7 +65,7 @@ function sortingStrategy(axis: Axis) {
   return axis === 'x' ? horizontalListSortingStrategy : verticalListSortingStrategy
 }
 
-function SortableList(props: ListProps) {
+const SortableList = memo(function SortableList(props: ListProps) {
   const {items, axis, onItemMove, onItemMoveStart, onItemMoveEnd, children, ...rest} = props
 
   const sensors = useSensors(useSensor(PointerSensor), useSensor(KeyboardSensor, SENSOR_OPTIONS))
@@ -71,14 +76,14 @@ function SortableList(props: ListProps) {
 
       if (active.id !== over?.id) {
         onItemMove?.({
-          fromIndex: items.indexOf(active.id as string),
-          toIndex: items.indexOf(over?.id as string),
+          fromIndex: active.data.current?.sortable?.index,
+          toIndex: over?.data.current?.sortable?.index,
         })
       }
 
       onItemMoveEnd?.()
     },
-    [items, onItemMove, onItemMoveEnd]
+    [onItemMove, onItemMoveEnd]
   )
   const modifiers = useMemo(
     () => [restrictToParentElementWithMargins({y: 4}), ...(axis ? [restrictToAxis(axis)] : [])],
@@ -99,9 +104,12 @@ function SortableList(props: ListProps) {
       </SortableContext>
     </DndContext>
   )
-}
+})
 
-function SortableListItem(props: ItemProps) {
+const SortableListItem = forwardRef<HTMLDivElement, ItemProps>(function SortableListItem(
+  props,
+  ref
+) {
   const {id, children, disableTransition} = props
   const {setNodeRef, transform, transition, active} = useSortable({
     id,
@@ -119,24 +127,39 @@ function SortableListItem(props: ItemProps) {
       } as const),
     [transform, transition, active]
   )
+
+  // This sets the ref on the component for both sorting and for virtualizer
+  const setRef = useCallback(
+    (node: HTMLDivElement | null) => {
+      setNodeRef(node)
+      if (typeof ref === 'function') {
+        ref(node)
+      } else if (ref) {
+        ref.current = node
+      }
+    },
+    [ref, setNodeRef]
+  )
+
   return (
     <ListItem
-      ref={setNodeRef}
+      ref={setRef}
       style={style}
       $moving={isActive}
       className={isActive ? MOVING_ITEM_CLASS_NAME : ''}
+      data-index={props['data-index']}
     >
       {children}
     </ListItem>
   )
-}
+})
 
 interface ListProps extends ComponentProps<typeof Grid> {
   sortable?: boolean
   axis?: Axis
   items: string[]
   onItemMove?: (event: {fromIndex: number; toIndex: number}) => void
-  onItemMoveStart?: () => void
+  onItemMoveStart?: (event: DragStartEvent) => void
   onItemMoveEnd?: () => void
   children?: React.ReactNode
 }
@@ -172,13 +195,17 @@ interface ItemProps {
   sortable?: boolean
   disableTransition?: boolean
   children?: React.ReactNode
+  'data-index'?: number
 }
 
-export function Item(props: ItemProps & ComponentProps<typeof Card>) {
+export const Item = forwardRef(function Item(
+  props: ItemProps & ComponentProps<typeof Card>,
+  ref: ForwardedRef<HTMLDivElement>
+) {
   const {sortable, ...rest} = props
   return (
     <SortableItemIdContext.Provider value={props.id}>
-      {sortable ? <SortableListItem {...rest} /> : <ListItem {...rest} />}
+      {sortable ? <SortableListItem ref={ref} {...rest} /> : <ListItem ref={ref} {...rest} />}
     </SortableItemIdContext.Provider>
   )
-}
+})
