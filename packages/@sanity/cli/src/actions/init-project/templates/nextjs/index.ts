bec3a25609d9e15d3de28ce1e2b20023795b6f5b
Benjamin Akar <16625915+benja@users.noreply.github.com>
2023-04-28 12:27:42 +0200

    refactor: remove studio global style temporarily

--- packages/@sanity/cli/src/actions/init-project/templates/nextjs/index.ts
+++ packages/@sanity/cli/src/actions/init-project/templates/nextjs/index.ts
@@ -40,11 +40,6 @@ import { NextStudio } from 'next-sanity/studio'
 import { NextStudioHead } from 'next-sanity/studio/head'
 import { StudioLayout, StudioProvider } from 'sanity'
 import config from ':configPath:'
-import { createGlobalStyle } from 'styled-components'
-
-const GlobalStyle = createGlobalStyle(({ theme }) => ({
-  html: { backgroundColor: theme.sanity.color.base.bg },
-}))
 
 export default function StudioPage() {
   return (
@@ -53,12 +48,7 @@ export default function StudioPage() {
         <NextStudioHead favicons={false} />
       </Head>
 
-      <NextStudio config={config}>
-        <StudioProvider config={config}>
-          <GlobalStyle />
-          <StudioLayout />
-        </StudioProvider>
-      </NextStudio>
+      <NextStudio config={config} />
     </>
   )
 }`
