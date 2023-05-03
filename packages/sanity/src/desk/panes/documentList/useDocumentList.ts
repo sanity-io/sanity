b7dc549093eb1598d2921b8a805bf8115521c232
Herman Wikner <wiknerherman@gmail.com>
2023-05-03 12:19:02 +0200

    fix(desk): add debounce to listener and general fixes in `useDocumentList`

--- packages/sanity/src/desk/panes/documentList/useDocumentList.ts
+++ packages/sanity/src/desk/panes/documentList/useDocumentList.ts
@@ -1,7 +1,11 @@
 import {useEffect, useState, useCallback, useMemo} from 'react'
-import {EMPTY, concat, of, switchMap} from 'rxjs'
+import {EMPTY, concat, debounce, of, switchMap, timer} from 'rxjs'
 import {DocumentListPaneItem, QueryResult, SortOrder} from './types'
-import {isSimpleTypeFilter, removePublishedWithDrafts} from './helpers'
+import {
+  getTypeNameFromSingleTypeFilter,
+  isSimpleTypeFilter,
+  removePublishedWithDrafts,
+} from './helpers'
 import {DEFAULT_ORDERING, FULL_LIST_LIMIT, PARTIAL_PAGE_LIMIT} from './constants'
 import {getQueryResults} from './getQueryResults'
 import {useDocumentTypeNames} from './hooks'
@@ -57,6 +61,7 @@ export function useDocumentList(opts: UseDocumentListOpts): DocumentListState {
 
   const documents = result?.documents
 
+  // Filter out published documents that have drafts to avoid duplicates in the list.
   const items = useMemo(
     () => (documents ? removePublishedWithDrafts(documents) : EMPTY_ARRAY),
     [documents]
@@ -75,27 +80,51 @@ export function useDocumentList(opts: UseDocumentListOpts): DocumentListState {
   // A state to keep track of whether we should fetch the full list of documents.
   const [shouldFetchFullList, setShouldFetchFullList] = useState<boolean>(false)
 
-  // Check if the filter is a simple filter (i.e. a filter that only contains a single document type).
+  // Check if the filter is a simple type filter, e.g. `*[_type == "book"]` or `*[_type == $type]`
   const isSimpleFilter = isSimpleTypeFilter(filter)
 
-  // Fetch the names of all document types that match the filter and params.
-  // This allows us to search for documents of all types in the list.
-  const {data: fetchedTypeNames, loading: loadingDocumentTypes} = useDocumentTypeNames({
+  // Get the type name from the filter, if it is a simple type filter.
+  const typeNameFromFilter = useMemo(
+    () => getTypeNameFromSingleTypeFilter(filter, paramsProp),
+    [filter, paramsProp]
+  )
+
+  // If the filter is a simple type filter, we can disable fetching the document types names.
+  // This is because we already know the type name from the filter and params.
+  const disableFetchTypeNames = Boolean(typeNameFromFilter && isSimpleFilter)
+
+  // If the filter is not a simple type filter, we need to fetch the document types names
+  // to use in the search query and in the listener for the list change.
+  const {data: documentTypes, loading: loadingDocumentTypes} = useDocumentTypeNames({
     filter,
     params: paramsProp,
-    // Disable the hook if the filter is a simple filter.
-    disabled: isSimpleFilter,
+    disabled: disableFetchTypeNames,
   })
 
-  const isLoading = isInitialLoading || Boolean(loading && result === null)
+  // The document types names are used to build the search query and to listen for changes.
+  // 1. If the filter is a simple type filter, we can use the type name directly.
+  // 2. If the filter is a complex filter, we need to use the document types names that we have fetched.
+  const uniqueTypesNames = useMemo(() => {
+    if (disableFetchTypeNames && typeNameFromFilter) return [typeNameFromFilter]
 
-  // The document types to use for the search query.
-  const typeNames = useMemo(() => {
-    const typeFromParams = paramsProp?.type as string
+    return [...new Set(documentTypes || EMPTY_ARRAY)]
+  }, [documentTypes, typeNameFromFilter, disableFetchTypeNames])
 
-    return [...new Set((isSimpleFilter ? [typeFromParams] : fetchedTypeNames) || EMPTY_ARRAY)]
-  }, [isSimpleFilter, fetchedTypeNames, paramsProp?.type])
+  const noDocumentTypes = uniqueTypesNames.length === 0 && !loadingDocumentTypes
 
+  const isLoading =
+    Boolean(isInitialLoading && !noDocumentTypes) ||
+    Boolean(loading && result === null && noDocumentTypes)
+
+  // The search is ready to be interacted with when:
+  // 1. The filter is a simple type filter and we have the type name from the filter.
+  // 2. Or when the filter is not a simple type filter and we have fetched the document types names.
+  const simpleFilterSearchReady = uniqueTypesNames.length > 0
+  const complexFilterSearchReady = simpleFilterSearchReady && !loadingDocumentTypes
+  const isSearchReady = isSimpleFilter ? simpleFilterSearchReady : complexFilterSearchReady
+
+  // This function is triggered when the user has scrolled to the bottom of the list
+  // and we need to fetch more items.
   const onListChange = useCallback(() => {
     if (isLoading || hasFullList || shouldFetchFullList) return
 
@@ -142,12 +171,12 @@ export function useDocumentList(opts: UseDocumentListOpts): DocumentListState {
     const sort = sortOrder?.by || DEFAULT_ORDERING?.by
     const extendedProjection = sortOrder?.extendedProjection || DEFAULT_ORDERING?.extendedProjection
     const limit = shouldFetchFullList ? FULL_LIST_LIMIT : PARTIAL_PAGE_LIMIT
-    const types = typeNames.flatMap((name) => schema.get(name) || []) as SearchableType[]
+    const types = uniqueTypesNames.flatMap((name) => schema.get(name) || []) as SearchableType[]
 
     const terms: SearchTerms = {
       query: searchQuery || '',
       types,
-      filter: isSimpleFilter ? undefined : filter,
+      filter,
     }
 
     const options: SearchOptions & WeightedSearchOptions = {
@@ -161,31 +190,34 @@ export function useDocumentList(opts: UseDocumentListOpts): DocumentListState {
     return createSearchQuery(terms, options)
   }, [
     filter,
-    isSimpleFilter,
     paramsProp,
     schema,
     searchQuery,
     shouldFetchFullList,
     sortOrder?.by,
     sortOrder?.extendedProjection,
-    typeNames,
+    uniqueTypesNames,
   ])
 
+  // The query is too big to be used with a listener. To work around this, we use a
+  // listener to listen for mutations on the document types, and then re-run the query
+  // to get the updated list of documents when a mutation occurs.
   const listener$ = useMemo(() => {
-    if (typeNames.length === 0) {
+    if (uniqueTypesNames.length === 0) {
       return EMPTY
     }
 
-    return client.observable.listen(
-      `*[_type in $types]`,
-      {types: typeNames},
-      {events: ['welcome', 'mutation']}
-    )
-  }, [client.observable, typeNames])
+    return client.observable
+      .listen(`*[_type in $types]`, {types: uniqueTypesNames}, {events: ['welcome', 'mutation']})
+      .pipe(
+        // Add debounce to prevent multiple requests when multiple mutations occur.
+        // Skip the debounce when the event is 'welcome' to prevent a delay in the initial load.
+        debounce((value) => (value.type === 'welcome' ? of('') : timer(1000)))
+      )
+  }, [client.observable, uniqueTypesNames])
 
   const getQueryResultsProps = useMemo(() => ({client, query, params}), [client, params, query])
 
-  // Set up the document list listener
   useEffect(() => {
     setResult((prev) => ({...prev, loading: true}))
 
@@ -208,25 +240,21 @@ export function useDocumentList(opts: UseDocumentListOpts): DocumentListState {
   }, [apiVersion, getQueryResultsProps, handleSetResult, listener$])
 
   const reset = useCallback(() => {
-    setResult(INITIAL_STATE)
     setHasFullList(false)
-    setShouldFetchFullList(false)
     setIsLazyLoading(false)
+    setResult(INITIAL_STATE)
+    setShouldFetchFullList(false)
   }, [])
 
   useEffect(() => {
     reset()
-  }, [reset, filter, paramsProp, sortOrder, searchQuery, typeNames])
-
-  const isSearchReady = isSimpleFilter
-    ? typeNames.length > 0
-    : typeNames.length > 0 && !loadingDocumentTypes
+  }, [reset, filter, paramsProp, sortOrder, searchQuery, uniqueTypesNames])
 
   return {
     error,
     hasMaxItems,
     isLazyLoading,
-    isLoading: isSimpleFilter ? isLoading : Boolean(!isSearchReady || isLoading),
+    isLoading,
     isSearchReady,
     items,
     onListChange,
