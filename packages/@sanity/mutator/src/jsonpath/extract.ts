a13acd0f5f976cf159b9000de6ce4d03b5b80355
Noah Gentile <noah.gentile@gmail.com>
2023-05-06 10:27:43 -0700

    docs(mutator): adding inline docs for `extract`

--- packages/@sanity/mutator/src/jsonpath/extract.ts
+++ packages/@sanity/mutator/src/jsonpath/extract.ts
@@ -1,5 +1,13 @@
 import {extractAccessors} from './extractAccessors'
 
+/**
+ * Extracts a value for the given JsonPath
+ *
+ * @param path - Path to extract
+ * @param value - Value to extract from
+ * @returns An array of objects from the given path
+ * @internal
+ */
 export function extract(path: string, value: unknown): unknown[] {
   const accessors = extractAccessors(path, value)
   return accessors.map((acc) => acc.get())
