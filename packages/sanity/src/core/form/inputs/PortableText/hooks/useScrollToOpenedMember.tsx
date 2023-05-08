ac3096db27af76cca7bec6a325efa09acbfa52b4
Bjørge Næss <bjoerge@gmail.com>
2023-05-08 13:49:14 +0200

    debug: find and verify valid focused item

--- packages/sanity/src/core/form/inputs/PortableText/hooks/useScrollToOpenedMember.tsx
+++ packages/sanity/src/core/form/inputs/PortableText/hooks/useScrollToOpenedMember.tsx
@@ -1,7 +1,11 @@
 import {PortableTextEditor, usePortableTextEditor} from '@sanity/portable-text-editor'
 import {Path} from '@sanity/types'
+import {isEqual} from '@sanity/util/paths'
 import {useEffect} from 'react'
 import scrollIntoView from 'scroll-into-view-if-needed'
+import {te} from 'date-fns/locale'
+import {isMemberArrayOfObjects, isMemberObject} from '../../../members/object/fields/asserters'
+import {isObjectField} from '../../../utils/asserters'
 import {usePortableTextMemberItems} from './usePortableTextMembers'
 
 interface Props {
@@ -33,17 +37,47 @@ export function useScrollToOpenedMember(props: Props): void {
         scrollMode: 'if-needed',
       })
       // If this is a text block (and not a child within), place the cursor in the beginning of it.
-      const isChildPath = focusPath.slice(editorRootPath.length).length > 1
-      if (memberItem.kind === 'textBlock' && !isChildPath) {
-        const relativePath = memberItem.member.item.path.slice(editorRootPath.length).slice(0, 1)
-        // "auto-close" regular text blocks or they get sticky here when trying to focus on an other field
-        // There is no natural way of closing them (however opening something else would close them)
-        PortableTextEditor.select(editor, {
-          anchor: {path: relativePath, offset: 0},
-          focus: {path: relativePath, offset: 0},
-        })
-        PortableTextEditor.focus(editor)
-        onItemClose()
+      // const isChildPath = focusPath.slice(editorRootPath.length).length > 1
+      if (memberItem.kind === 'textBlock') {
+        const isChildPath = focusPath.slice(editorRootPath.length).length > 1
+
+        if (isChildPath) {
+          const childrenField = memberItem.member.item.members.find(
+            (member) => member.kind === 'field' && member.name === 'children'
+          )
+          const childMembers =
+            childrenField?.kind === 'field' && isMemberArrayOfObjects(childrenField)
+              ? childrenField.field.members
+              : null
+
+          const focusedSpan = childMembers?.flatMap((member) =>
+            member.kind === 'item' &&
+            member.item.schemaType.name === 'span' &&
+            isEqual(member.item.path.slice(editorRootPath.length), focusPath)
+              ? [member.item]
+              : []
+          )[0]
+
+          const relativePath = focusedSpan?.path.slice(editorRootPath.length)
+          if (relativePath) {
+            console.log('set focus at span', relativePath)
+            PortableTextEditor.focus(editor)
+            PortableTextEditor.select(editor, {
+              anchor: {path: relativePath, offset: 0},
+              focus: {path: relativePath, offset: 0},
+            })
+          }
+        } else {
+          const relativePath = memberItem.member.item.path.slice(editorRootPath.length).slice(0, 1)
+          // "auto-close" regular text blocks or they get sticky here when trying to focus on an other field
+          // There is no natural way of closing them (however opening something else would close them)
+          PortableTextEditor.select(editor, {
+            anchor: {path: relativePath, offset: 0},
+            focus: {path: relativePath, offset: 0},
+          })
+          PortableTextEditor.focus(editor)
+          onItemClose()
+        }
       }
     }
   }, [
