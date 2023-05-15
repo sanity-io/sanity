e5bed2e04ce91fbcb3abeeb627c00e700a0ae063
Binoy Patel <me@binoy.io>
2023-05-15 15:02:22 +0530

    fix(core): editing reference input in PTE does not show value

--- packages/sanity/src/core/form/inputs/ReferenceInput/ReferenceItem.tsx
+++ packages/sanity/src/core/form/inputs/ReferenceInput/ReferenceItem.tsx
@@ -186,6 +186,15 @@ export function ReferenceItem<Item extends ReferenceItemValue = ReferenceItemVal
     [refType?.name, value?._ref]
   )
 
+  const handleReplace = useCallback(() => {
+    if (hasRef && isEditing) {
+      onPathFocus([])
+    } else {
+      onPathFocus(['_ref'])
+    }
+    onChange(unset(['_ref']))
+  }, [hasRef, isEditing, onChange, onPathFocus])
+
   const menu = useMemo(
     () =>
       readOnly ? null : (
@@ -201,9 +210,7 @@ export function ReferenceItem<Item extends ReferenceItemValue = ReferenceItemVal
                     <MenuItem
                       text={hasRef && isEditing ? 'Cancel replace' : 'Replace'}
                       icon={hasRef && isEditing ? CloseIcon : ReplaceIcon}
-                      onClick={
-                        hasRef && isEditing ? () => onPathFocus([]) : () => onPathFocus(['_ref'])
-                      }
+                      onClick={handleReplace}
                     />
                     <MenuItem text="Duplicate" icon={DuplicateIcon} onClick={handleDuplicate} />
                     <InsertMenu onInsert={handleInsert} types={insertableTypes} />
@@ -231,11 +238,11 @@ export function ReferenceItem<Item extends ReferenceItemValue = ReferenceItemVal
       onRemove,
       hasRef,
       isEditing,
+      handleReplace,
       handleDuplicate,
       handleInsert,
       insertableTypes,
       OpenLink,
-      onPathFocus,
     ]
   )
 
