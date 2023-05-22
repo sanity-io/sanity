d9d318acc5b61f5ecc19fa194d17e44d52d85641
Per-Kristian Nordnes <per.kristian.nordnes@gmail.com>
2023-05-22 10:24:36 +0200

    refactor(form/inputs): simplify tenary statement
    As suggested by @binoy14 in his review.

--- packages/sanity/src/core/form/inputs/PortableText/object/Annotation.tsx
+++ packages/sanity/src/core/form/inputs/PortableText/object/Annotation.tsx
@@ -96,10 +96,7 @@ export function Annotation(props: AnnotationProps) {
     [Markers, markers, renderCustomMarkers, text, validation]
   )
 
-  const presence =
-    memberItem?.node.presence && memberItem?.node.presence.length > 0
-      ? memberItem.node.presence
-      : EMPTY_ARRAY
+  const presence = memberItem?.node.presence?.length ? memberItem.node.presence : EMPTY_ARRAY
 
   const isOpen = Boolean(memberItem?.member.open)
   const input = memberItem?.input
