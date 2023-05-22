29d5c39b1b5f9cd4384bd1a75aa4c67ded9cb10f
Per-Kristian Nordnes <per.kristian.nordnes@gmail.com>
2023-05-22 10:24:36 +0200

    refactor(form/inputs): simplify tenary statement
    As suggested by @binoy14 in his review.

--- packages/sanity/src/core/form/inputs/PortableText/object/InlineObject.tsx
+++ packages/sanity/src/core/form/inputs/PortableText/object/InlineObject.tsx
@@ -76,11 +76,7 @@ export const InlineObject = (props: InlineObjectProps) => {
 
   const isOpen = Boolean(memberItem?.member.open)
   const input = memberItem?.input
-  const presence =
-    memberItem?.node.presence && memberItem?.node.presence.length > 0
-      ? memberItem.node.presence
-      : EMPTY_ARRAY
-
+  const presence = memberItem?.node.presence?.length ? memberItem.node.presence : EMPTY_ARRAY
   const componentProps: BlockProps | undefined = useMemo(
     () => ({
       __unstable_boundaryElement: boundaryElement || undefined,
