sanity-io/sanity

commit 75dfb9ca1bbb6bc21cad9c8139e4a9d87f9522ad

fix(form/inputs): fix re-rendering issue with PT-input block object
As the memberItem input is given new props all the time, use a ref to the input as a hook dep for rendering block objects.

packages/sanity/src/core/form/inputs/PortableText/object/Annotation.tsx
@@ -1,7 +1,7 @@
 import {PortableTextEditor, usePortableTextEditor} from '@sanity/portable-text-editor'
 import {ObjectSchemaType, Path, PortableTextObject} from '@sanity/types'
 import {Tooltip} from '@sanity/ui'
-import React, {ComponentType, useCallback, useMemo} from 'react'
+import React, {ComponentType, useCallback, useMemo, useRef} from 'react'
 import {pathToString} from '../../../../field'
 import {BlockAnnotationProps, RenderCustomMarkers} from '../../../types'
 import {DefaultMarkers} from '../_legacyDefaultParts/Markers'
@@ -99,13 +99,14 @@ export function Annotation(props: AnnotationProps) {
   const presence = memberItem?.node.presence?.length ? memberItem.node.presence : EMPTY_ARRAY
 
   const isOpen = Boolean(memberItem?.member.open)
-  const input = memberItem?.input
+  const inputRef = useRef(memberItem?.input)
+  inputRef.current = memberItem?.input // Update at every render as other Annotation props updates
 
   const componentProps = useMemo(
     (): BlockAnnotationProps => ({
       __unstable_boundaryElement: boundaryElement || undefined,
       __unstable_referenceElement: memberItem?.elementRef?.current || undefined,
-      children: input,
+      children: inputRef.current,
       focused,
       markers,
       onClose,
@@ -128,7 +129,7 @@ export function Annotation(props: AnnotationProps) {
       boundaryElement,
       editor.schemaTypes.block,
       focused,
-      input,
+      inputRef,
       isOpen,
       markers,
       markersToolTip,

packages/sanity/src/core/form/inputs/PortableText/object/BlockObject.tsx
@@ -6,7 +6,14 @@ import {
 } from '@sanity/portable-text-editor'
 import {ObjectSchemaType, Path, PortableTextBlock} from '@sanity/types'
 import {Tooltip, Flex, ResponsivePaddingProps, Box} from '@sanity/ui'
-import React, {ComponentType, PropsWithChildren, useCallback, useMemo, useState} from 'react'
+import React, {
+  ComponentType,
+  PropsWithChildren,
+  useCallback,
+  useMemo,
+  useRef,
+  useState,
+} from 'react'
 import {PatchArg} from '../../../patch'
 import {BlockProps, RenderCustomMarkers, RenderPreviewCallback} from '../../../types'
 import {RenderBlockActionsCallback} from '../types'
@@ -143,14 +150,15 @@ export function BlockObject(props: BlockObjectProps) {
   )
 
   const isOpen = Boolean(memberItem?.member.open)
-  const input = memberItem?.input
+  const inputRef = useRef(memberItem?.input)
+  inputRef.current = memberItem?.input // Update at every render as other BlockObjectProps updates
 
   const CustomComponent = schemaType.components?.block as ComponentType<BlockProps> | undefined
   const componentProps: BlockProps = useMemo(
     () => ({
       __unstable_boundaryElement: boundaryElement || undefined,
       __unstable_referenceElement: memberItem?.elementRef?.current || undefined,
-      children: input,
+      children: inputRef.current,
       focused,
       markers,
       onClose,
@@ -172,7 +180,6 @@ export function BlockObject(props: BlockObjectProps) {
     [
       boundaryElement,
       focused,
-      input,
       isOpen,
       markers,
       memberItem?.elementRef,

packages/sanity/src/core/form/inputs/PortableText/object/InlineObject.tsx
@@ -4,7 +4,7 @@ import {
   usePortableTextEditor,
 } from '@sanity/portable-text-editor'
 import {ObjectSchemaType, Path, PortableTextBlock, PortableTextChild} from '@sanity/types'
-import React, {useCallback, useEffect, useMemo, useState} from 'react'
+import React, {useCallback, useEffect, useMemo, useRef, useState} from 'react'
 import {Tooltip} from '@sanity/ui'
 import {BlockProps, RenderCustomMarkers, RenderPreviewCallback} from '../../../types'
 import {useFormBuilder} from '../../../useFormBuilder'
@@ -75,13 +75,17 @@ export const InlineObject = (props: InlineObjectProps) => {
   }, [memberItem, onItemOpen])
 
   const isOpen = Boolean(memberItem?.member.open)
-  const input = memberItem?.input
-  const presence = memberItem?.node.presence?.length ? memberItem.node.presence : EMPTY_ARRAY
+
+  const inputRef = useRef(memberItem?.input)
+  inputRef.current = memberItem?.input // Update at every render as other Annotation props updates
+
+  const presence = memberItem?.node.presence || EMPTY_ARRAY
+
   const componentProps: BlockProps | undefined = useMemo(
     () => ({
       __unstable_boundaryElement: boundaryElement || undefined,
       __unstable_referenceElement: memberItem?.elementRef?.current || undefined,
-      children: input,
+      children: inputRef.current,
       focused,
       onClose: onItemClose,
       onOpen,
@@ -104,7 +108,7 @@ export const InlineObject = (props: InlineObjectProps) => {
     [
       boundaryElement,
       focused,
-      input,
+      inputRef,
       isOpen,
       markers,
       memberItem,

commit c32c80d59bf7a1f13b69deccf072b9e0eeae18f9

fix(core/form/inputs): fix rendering regression for PT-input
There was a rendering regression in 75dfb9ca1bbb6bc21cad9c8139e4a9d87f9522ad where nested PT-inputs (PT in PT) would not
properly update their form inputs.

Revert the use of refs for the input in the PT node components, and let the PortableTextInput figure that out instead.

I have added a condition that will only update the input if the member is open or it's value has changed.

packages/sanity/src/core/form/inputs/PortableText/PortableTextInput.tsx
@@ -199,9 +199,13 @@ export function PortableTextInput(props: PortableTextInputProps) {
       }
 
       if (existingItem) {
+        // Only update the input if the node is open or the value has changed
+        // This is a performance optimization.
+        if (item.member.open || existingItem.node.value !== item.node.value) {
+          existingItem.input = input
+        }
         existingItem.member = item.member
         existingItem.node = item.node
-        existingItem.input = input
         return existingItem
       }
 

packages/sanity/src/core/form/inputs/PortableText/object/Annotation.tsx
@@ -1,7 +1,7 @@
 import {PortableTextEditor, usePortableTextEditor} from '@sanity/portable-text-editor'
 import {ObjectSchemaType, Path, PortableTextObject} from '@sanity/types'
 import {Tooltip} from '@sanity/ui'
-import React, {ComponentType, useCallback, useMemo, useRef} from 'react'
+import React, {ComponentType, useCallback, useMemo} from 'react'
 import {pathToString} from '../../../../field'
 import {BlockAnnotationProps, RenderCustomMarkers} from '../../../types'
 import {DefaultMarkers} from '../_legacyDefaultParts/Markers'
@@ -99,14 +99,15 @@ export function Annotation(props: AnnotationProps) {
   const presence = memberItem?.node.presence || EMPTY_ARRAY
 
   const isOpen = Boolean(memberItem?.member.open)
-  const inputRef = useRef(memberItem?.input)
-  inputRef.current = memberItem?.input // Update at every render as other Annotation props updates
+  const input = memberItem?.input
+  const nodePath = memberItem?.node.path || EMPTY_ARRAY
+  const referenceElement = memberItem?.elementRef?.current
 
   const componentProps = useMemo(
     (): BlockAnnotationProps => ({
       __unstable_boundaryElement: boundaryElement || undefined,
-      __unstable_referenceElement: memberItem?.elementRef?.current || undefined,
-      children: inputRef.current,
+      __unstable_referenceElement: referenceElement || undefined,
+      children: input,
       focused,
       markers,
       onClose,
@@ -115,7 +116,7 @@ export function Annotation(props: AnnotationProps) {
       onRemove,
       open: isOpen,
       parentSchemaType: editor.schemaTypes.block,
-      path: memberItem?.node.path || path,
+      path: nodePath,
       presence,
       readOnly: Boolean(readOnly),
       renderDefault: DefaultAnnotationComponent,
@@ -129,18 +130,18 @@ export function Annotation(props: AnnotationProps) {
       boundaryElement,
       editor.schemaTypes.block,
       focused,
-      inputRef,
+      input,
       isOpen,
       markers,
       markersToolTip,
-      memberItem,
+      nodePath,
       onClose,
       onOpen,
       onPathFocus,
       onRemove,
-      path,
       presence,
       readOnly,
+      referenceElement,
       schemaType,
       selected,
       text,

packages/sanity/src/core/form/inputs/PortableText/object/BlockObject.tsx
@@ -6,14 +6,7 @@ import {
 } from '@sanity/portable-text-editor'
 import {ObjectSchemaType, Path, PortableTextBlock} from '@sanity/types'
 import {Tooltip, Flex, ResponsivePaddingProps, Box} from '@sanity/ui'
-import React, {
-  ComponentType,
-  PropsWithChildren,
-  useCallback,
-  useMemo,
-  useRef,
-  useState,
-} from 'react'
+import React, {ComponentType, PropsWithChildren, useCallback, useMemo, useState} from 'react'
 import {PatchArg} from '../../../patch'
 import {BlockProps, RenderCustomMarkers, RenderPreviewCallback} from '../../../types'
 import {RenderBlockActionsCallback} from '../types'
@@ -150,15 +143,16 @@ export function BlockObject(props: BlockObjectProps) {
   )
 
   const isOpen = Boolean(memberItem?.member.open)
-  const inputRef = useRef(memberItem?.input)
-  inputRef.current = memberItem?.input // Update at every render as other BlockObjectProps updates
+  const input = memberItem?.input
+  const nodePath = memberItem?.node.path || EMPTY_ARRAY
+  const referenceElement = memberItem?.elementRef?.current
 
   const CustomComponent = schemaType.components?.block as ComponentType<BlockProps> | undefined
   const componentProps: BlockProps = useMemo(
     () => ({
       __unstable_boundaryElement: boundaryElement || undefined,
-      __unstable_referenceElement: memberItem?.elementRef?.current || undefined,
-      children: inputRef.current,
+      __unstable_referenceElement: referenceElement || undefined,
+      children: input,
       focused,
       markers,
       onClose,
@@ -167,7 +161,7 @@ export function BlockObject(props: BlockObjectProps) {
       onRemove,
       open: isOpen,
       parentSchemaType,
-      path: memberItem?.node.path || EMPTY_ARRAY,
+      path: nodePath,
       presence,
       readOnly: Boolean(readOnly),
       renderDefault: DefaultBlockObjectComponent,
@@ -179,16 +173,17 @@ export function BlockObject(props: BlockObjectProps) {
     }),
     [
       boundaryElement,
+      referenceElement,
+      input,
       focused,
-      isOpen,
       markers,
-      memberItem?.elementRef,
-      memberItem?.node.path,
       onClose,
       onOpen,
       onPathFocus,
       onRemove,
+      isOpen,
       parentSchemaType,
+      nodePath,
       presence,
       readOnly,
       renderPreview,

packages/sanity/src/core/form/inputs/PortableText/object/InlineObject.tsx
@@ -4,7 +4,7 @@ import {
   usePortableTextEditor,
 } from '@sanity/portable-text-editor'
 import {ObjectSchemaType, Path, PortableTextBlock, PortableTextChild} from '@sanity/types'
-import React, {useCallback, useEffect, useMemo, useRef, useState} from 'react'
+import React, {useCallback, useEffect, useMemo, useState} from 'react'
 import {Tooltip} from '@sanity/ui'
 import {BlockProps, RenderCustomMarkers, RenderPreviewCallback} from '../../../types'
 import {useFormBuilder} from '../../../useFormBuilder'
@@ -75,17 +75,17 @@ export const InlineObject = (props: InlineObjectProps) => {
   }, [memberItem, onItemOpen])
 
   const isOpen = Boolean(memberItem?.member.open)
-
-  const inputRef = useRef(memberItem?.input)
-  inputRef.current = memberItem?.input // Update at every render as other Annotation props updates
+  const input = memberItem?.input
+  const nodePath = memberItem?.node.path || EMPTY_ARRAY
+  const referenceElement = memberItem?.elementRef?.current
 
   const presence = memberItem?.node.presence || EMPTY_ARRAY
 
   const componentProps: BlockProps | undefined = useMemo(
     () => ({
       __unstable_boundaryElement: boundaryElement || undefined,
-      __unstable_referenceElement: memberItem?.elementRef?.current || undefined,
-      children: inputRef.current,
+      __unstable_referenceElement: referenceElement || undefined,
+      children: input,
       focused,
       onClose: onItemClose,
       onOpen,
@@ -95,7 +95,7 @@ export const InlineObject = (props: InlineObjectProps) => {
       markers,
       member: memberItem?.member,
       parentSchemaType,
-      path: memberItem?.member.item.path || EMPTY_ARRAY,
+      path: nodePath,
       presence,
       readOnly: Boolean(readOnly),
       renderDefault: DefaultInlineObjectComponent,
@@ -108,10 +108,11 @@ export const InlineObject = (props: InlineObjectProps) => {
     [
       boundaryElement,
       focused,
-      inputRef,
+      input,
       isOpen,
       markers,
-      memberItem,
+      memberItem?.member,
+      nodePath,
       onItemClose,
       onOpen,
       onPathFocus,
@@ -119,6 +120,7 @@ export const InlineObject = (props: InlineObjectProps) => {
       parentSchemaType,
       presence,
       readOnly,
+      referenceElement,
       renderPreview,
       schemaType,
       selected,