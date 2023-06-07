e4779a6b84de7ad7b2c05e5cd0ad9c0c562a2013
Noah Gentile <nkgentile@users.noreply.github.com>
2023-06-07 13:43:08 -0700

    feat(mutator): export `extract` method (#4461)

--- packages/@sanity/mutator/src/jsonpath/extract.ts
+++ packages/@sanity/mutator/src/jsonpath/extract.ts
@@ -1,5 +1,13 @@
 import {extractAccessors} from './extractAccessors'
 
+/**
+ * Extracts values matching the given JsonPath
+ *
+ * @param path - Path to extract
+ * @param value - Value to extract from
+ * @returns An array of values matching the given path
+ * @public
+ */
 export function extract(path: string, value: unknown): unknown[] {
   const accessors = extractAccessors(path, value)
   return accessors.map((acc) => acc.get())
