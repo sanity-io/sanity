2f9cbc701de90ffefa7c2c8056615244899608bd
Michael Wain <michael@sanity.io>
2023-06-12 21:28:24 +0100

    feat(vision): add perspective support

---
+++ packages/@sanity/vision/src/perspectives.ts
@@ -0,0 +1,8 @@
+import type {ClientPerspective} from '@sanity/client'
+
+export const PERSPECTIVES = ['raw', 'previewDrafts', 'published'] satisfies ClientPerspective[]
+export const DEFAULT_PERSPECTIVE = PERSPECTIVES[0]
+
+export function isPerspective(p: string): p is ClientPerspective {
+  return PERSPECTIVES.includes(p as ClientPerspective)
+}
