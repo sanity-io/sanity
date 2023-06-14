1bc96aab686ca27d4502d66a5d984c716608dc37
Per-Kristian Nordnes <per.kristian.nordnes@gmail.com>
2023-06-14 13:26:12 +0200

    refactor(portable-text-editor): refactor undo/redo plugin
    This will update the plugin to use the new diff-match-patch library which will support all UTF-8 chars.
    
    I have also refactored and improved the operation adjustment  quite a bit.
    
    Provided several new tests for undo/redo in collab mode.

--- packages/@sanity/portable-text-editor/src/utils/withPreserveKeys.ts
+++ packages/@sanity/portable-text-editor/src/utils/withPreserveKeys.ts
@@ -3,9 +3,10 @@ import {Editor} from 'slate'
 export const PRESERVE_KEYS: WeakMap<Editor, boolean | undefined> = new WeakMap()
 
 export function withPreserveKeys(editor: Editor, fn: () => void): void {
+  const prev = isPreservingKeys(editor)
   PRESERVE_KEYS.set(editor, true)
   fn()
-  PRESERVE_KEYS.set(editor, false)
+  PRESERVE_KEYS.set(editor, prev)
 }
 
 export function isPreservingKeys(editor: Editor): boolean | undefined {
