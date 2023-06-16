f957741a07202553bd4f60670ab154e46c01ed06
RitaDias <rita@sanity.io>
2023-06-16 15:54:22 +0200

    docs: update export for type to single re-export
    Co-authored-by: Espen Hovlandsdal <espen@hovlandsdal.com>

--- packages/sanity/exports/cli.ts
+++ packages/sanity/exports/cli.ts
@@ -2,5 +2,4 @@ export {createCliConfig, defineCliConfig, getCliClient} from '@sanity/cli'
 
 export type {CliClientOptions, CliConfig} from '@sanity/cli'
 
-export type {studioEnvVariablesOptions} from '../src/_internal/cli/server/getStudioEnvironmentVariables'
-export {getStudioEnvironmentVariables} from '../src/_internal/cli/server/getStudioEnvironmentVariables'
+export {getStudioEnvironmentVariables, type studioEnvVariablesOptions} from '../src/_internal/cli/server/getStudioEnvironmentVariables'
