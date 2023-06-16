c56c19f5bd6c512caad627dfcc4c5b3b7dd1e34d
RitaDias <anarita151@gmail.com>
2023-06-16 11:30:56 +0200

    docs(cli): update docs for getStudioEnvironmentVariables

--- packages/sanity/exports/cli.ts
+++ packages/sanity/exports/cli.ts
@@ -2,4 +2,5 @@ export {createCliConfig, defineCliConfig, getCliClient} from '@sanity/cli'
 
 export type {CliClientOptions, CliConfig} from '@sanity/cli'
 
+export type {studioEnvVariablesOptions} from '../src/_internal/cli/server/getStudioEnvironmentVariables'
 export {getStudioEnvironmentVariables} from '../src/_internal/cli/server/getStudioEnvironmentVariables'
