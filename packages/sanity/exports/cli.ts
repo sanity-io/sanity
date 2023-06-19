031125b37bc411a23135ceed3e81435e73f579de
RitaDias <anarita151@gmail.com>
2023-06-19 09:35:37 +0200

    chore: linting

--- packages/sanity/exports/cli.ts
+++ packages/sanity/exports/cli.ts
@@ -2,4 +2,7 @@ export {createCliConfig, defineCliConfig, getCliClient} from '@sanity/cli'
 
 export type {CliClientOptions, CliConfig} from '@sanity/cli'
 
-export {getStudioEnvironmentVariables, type studioEnvVariablesOptions} from '../src/_internal/cli/server/getStudioEnvironmentVariables'
+export {
+  getStudioEnvironmentVariables,
+  type studioEnvVariablesOptions,
+} from '../src/_internal/cli/server/getStudioEnvironmentVariables'
