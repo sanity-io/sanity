06d5928d78593786aebf440cf21a6c514ea58a2e
RitaDias <rita@sanity.io>
2023-06-21 13:44:06 +0200

    docs(cli): update docs for getStudioEnvironmentVariables (#4597)
    * docs(cli): update docs for getStudioEnvironmentVariables
    
    * docs: update export for type to single re-export
    
    Co-authored-by: Espen Hovlandsdal <espen@hovlandsdal.com>
    
    * chore: linting
    
    * fix: rename interface to pascalcase
    
    ---------
    
    Co-authored-by: Espen Hovlandsdal <espen@hovlandsdal.com>

--- packages/sanity/exports/cli.ts
+++ packages/sanity/exports/cli.ts
@@ -2,4 +2,7 @@ export {createCliConfig, defineCliConfig, getCliClient} from '@sanity/cli'
 
 export type {CliClientOptions, CliConfig} from '@sanity/cli'
 
-export {getStudioEnvironmentVariables} from '../src/_internal/cli/server/getStudioEnvironmentVariables'
+export {
+  getStudioEnvironmentVariables,
+  type StudioEnvVariablesOptions,
+} from '../src/_internal/cli/server/getStudioEnvironmentVariables'
--- packages/sanity/src/_internal/cli/server/getStudioEnvironmentVariables.ts
+++ packages/sanity/src/_internal/cli/server/getStudioEnvironmentVariables.ts
@@ -4,29 +4,46 @@ import {loadEnv} from '@sanity/cli'
 const envPrefix = 'SANITY_STUDIO_'
 
 /**
+ * The params for the `getStudioEnvironmentVariables` function that gets Studio focused environment variables.
+ *
+ * @public
+ */
+export interface StudioEnvVariablesOptions {
+  /**
+   * When specified adds a prefix to the environment variable keys,
+   * eg: `getStudioEnvironmentVariables({prefix: 'process.env.'})`
+   */
+  prefix?: string
+  /**
+   * When specified includes environment variables from dotenv files (`.env`), in the same way the studio does.
+   * A `mode` must be specified, usually `development`
+   * or `production`, which will load the corresponding `.env.development` or `.env.production`.
+   * To specify where to look for the dotenv files, specify `options.envFile.envDir`.
+   */
+  envFile?: {mode: string; envDir?: string} | false
+  /**
+   * When specified, JSON-encodes the values, which is handy if you want to pass
+   * this to a bundlers hardcoded defines, such as Vite's `define` or Webpack's `DefinePlugin`.
+   */
+  jsonEncode?: boolean
+}
+
+/**
  * Get environment variables prefixed with SANITY_STUDIO_, as an object.
  *
- * Specify `options.prefix` to add a prefix to the environment variable keys,
- * eg: `getStudioEnvironmentVariables({prefix: 'process.env.'})`
- *
- * Specify `options.envFile` to include environment variables from dotenv files (`.env`),
- * in the same way the studio does. A `mode` must be specified, usually `development`
- * or `production`, which will load the corresponding `.env.development` or `.env.production`.
- * To specify where to look for the dotenv files, specify `options.envFile.envDir`.
- *
- * Specify `options.jsonEncode` to JSON-encode the values, which is handy if you want to pass
- * this to a bundlers hardcoded defines, such as Vite's `define` or Webpack's `DefinePlugin`.
- *
  * @param options - Options for the environment variable loading
+ *  {@link StudioEnvVariablesOptions}
  * @returns Object of studio environment variables
+ *
+ * @example
+ * ```tsx
+ * getStudioEnvironmentVariables({prefix: 'process.env.', jsonEncode: true})
+ * ```
+ *
  * @public
  */
 export function getStudioEnvironmentVariables(
-  options: {
-    prefix?: string
-    envFile?: {mode: string; envDir?: string} | false
-    jsonEncode?: boolean
-  } = {}
+  options: StudioEnvVariablesOptions = {}
 ): Record<string, string> {
   const {prefix = '', envFile = false, jsonEncode = false} = options
   const fullEnv = envFile
