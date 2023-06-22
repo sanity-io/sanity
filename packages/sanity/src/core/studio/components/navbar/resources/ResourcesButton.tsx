749dd820acd2a159aeb24c9075aed050473dea6e
Nina Andal Aarvik <nina@sanity.io>
2023-06-22 09:24:17 +0200

    fix(core): add tooltip to resourcesButton

--- packages/sanity/src/core/studio/components/navbar/resources/ResourcesButton.tsx
+++ packages/sanity/src/core/studio/components/navbar/resources/ResourcesButton.tsx
@@ -1,6 +1,6 @@
 import {HelpCircleIcon} from '@sanity/icons'
-import {Button, Menu, MenuButton} from '@sanity/ui'
-import React, {useCallback, useState} from 'react'
+import {Box, Button, Flex, Menu, MenuButton, Text, Tooltip} from '@sanity/ui'
+import React from 'react'
 import styled from 'styled-components'
 import {useColorScheme} from '../../../colorScheme'
 import {useGetHelpResources} from './helper-functions/hooks'
@@ -14,23 +14,33 @@ const StyledMenu = styled(Menu)`
 export function ResourcesButton() {
   const {scheme} = useColorScheme()
 
-  const [open, setOpen] = useState<boolean>(false)
-
-  const handleOpen = useCallback(() => setOpen(!open), [open])
   const {value, error, isLoading} = useGetHelpResources()
 
   return (
-    <>
-      <MenuButton
-        button={<Button icon={HelpCircleIcon} onClick={handleOpen} mode="bleed" fontSize={2} />}
-        id="menu-button-resources"
-        menu={
-          <StyledMenu>
-            <ResourcesMenuItems error={error} isLoading={isLoading} value={value} />
-          </StyledMenu>
+    <Flex>
+      <Tooltip
+        content={
+          <Box padding={2}>
+            <Text size={1}>Help and Resources </Text>
+          </Box>
         }
-        popover={{constrainSize: true, placement: 'bottom', portal: true, scheme}}
-      />
-    </>
+        scheme={scheme}
+        placement="bottom"
+        portal
+      >
+        <div>
+          <MenuButton
+            button={<Button icon={HelpCircleIcon} mode="bleed" fontSize={2} />}
+            id="menu-button-resources"
+            menu={
+              <StyledMenu>
+                <ResourcesMenuItems error={error} isLoading={isLoading} value={value} />
+              </StyledMenu>
+            }
+            popover={{constrainSize: true, placement: 'bottom', portal: true, scheme}}
+          />
+        </div>
+      </Tooltip>
+    </Flex>
   )
 }
