068dbcfadb1571ddeacc69a7640db377240f0cd9
Fred Carlsen <fred@sjelfull.no>
2023-06-23 21:54:27 +0200

    test(playwright): add inline object

--- packages/sanity/playwright-ct/FormBuilderStory.tsx
+++ packages/sanity/playwright-ct/FormBuilderStory.tsx
@@ -57,10 +57,25 @@ const SCHEMA_TYPES = [
         type: 'array',
         name: 'body',
         of: [
-          defineArrayMember({type: 'block'}),
+          defineArrayMember({
+            type: 'block',
+            of: [
+              defineArrayMember({
+                type: 'object', 
+                title: 'Inline Object',
+                fields: [
+                defineField({
+                  type: 'string',
+                  name: 'title',
+                  title: 'Title',
+                }),
+              ]}),
+            ]
+          }),
           defineArrayMember({
             name: 'object',
             type: 'object',
+            title: 'Object',
             fields: [{type: 'string', name: 'title', title: 'Title'}],
             preview: {
               select: {
