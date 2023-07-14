19eac575b02b3ed6a202cd3bfa10ce9137265d1c
Espen Hovlandsdal <espen@hovlandsdal.com>
2023-07-14 16:01:13 -0700

    feat(cli): bootstrap new studios with prettier v3

--- packages/@sanity/cli/src/studioDependencies.ts
+++ packages/@sanity/cli/src/studioDependencies.ts
@@ -21,7 +21,7 @@ export const studioDependencies = {
     '@types/react': '^18.0.25',
     '@types/styled-components': '^5.1.26',
     eslint: '^8.6.0',
-    prettier: '^2.8.8',
+    prettier: '^3.0.0',
     typescript: '^4.9.5', // Peer dependency of eslint-config-studio (implicitly)
   },
 }
