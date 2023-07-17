321161958af502409c57b6abd5951d6fceb3143c
Bjørge Næss <bjoerge@gmail.com>
2023-07-17 13:27:24 +0200

    fix(schema): remove projectId and __experimental_search from CDR type definitions

--- packages/@sanity/types/src/schema/definition/type/crossDatasetReference.ts
+++ packages/@sanity/types/src/schema/definition/type/crossDatasetReference.ts
@@ -12,12 +12,9 @@ export interface CrossDatasetReferenceDefinition extends BaseSchemaDefinition {
     title?: string
     icon?: ComponentType
     preview?: PreviewConfig
-    // eslint-disable-next-line camelcase
-    __experimental_search?: {path: string | string[]; weight?: number; mapWith?: string}[]
   }[]
 
   dataset: string
-  projectId: string
   studioUrl?: (document: {id: string; type?: string}) => string | null
   tokenId?: string
   options?: ReferenceOptions
