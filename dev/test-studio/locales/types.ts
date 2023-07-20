938fcaef71dc496c75ba58a957fcf7faad8d506e
Espen Hovlandsdal <espen@hovlandsdal.com>
2023-07-19 19:55:37 -0700

    refactor(i18n): drop unused resource key test

--- dev/test-studio/locales/types.ts
+++
@@ -1,9 +0,0 @@
-// eslint-disable-next-line import/no-unassigned-import
-import 'sanity'
-import {testStudioLocaleNamespace, TestStudioLocaleResourceKeys} from './index'
-
-declare module 'sanity' {
-  interface SanityLanguageResources {
-    [testStudioLocaleNamespace]: TestStudioLocaleResourceKeys
-  }
-}
