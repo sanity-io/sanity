42d15aa54b99c10b5c59eca22a26eecdc7e945f3
Espen Hovlandsdal <espen@hovlandsdal.com>
2023-07-24 14:18:06 -0700

    fix(core): allow `null` and `role`-less user in `userHasRole` fn
    This allows it to be used directly from a conditional property callback,
    where `null` is valid since a studio may not require authenticated users.
    In addition, the conditional property callback context have dropped the
    legacy `user` property, which meant the signature did not match.

--- packages/sanity/src/core/util/userHasRole.ts
+++ packages/sanity/src/core/util/userHasRole.ts
@@ -4,13 +4,14 @@ import type {CurrentUser} from '@sanity/types'
  * Checks whether or not the given user has the role with the given ID
  *
  * @param user - The user to check (currently only the current user is supported)
+ *   If `null` is passed, this function always returns `false`.
  * @param roleId - The ID of the role to check for
  *
  * @returns true if the user has the role, false otherwise
  *
  * @example
  * Fetch the current user and check if they have the role "administrator":
- * ```
+ * ```ts
  * import {userHasRole, useCurrentUser} from 'sanity'
  *
  * export function MyComponent() {
@@ -21,6 +22,9 @@ import type {CurrentUser} from '@sanity/types'
  * ```
  * @public
  */
-export function userHasRole(user: CurrentUser, roleId: string): boolean {
-  return user.roles.some((role) => role.name === roleId)
+export function userHasRole(
+  user: (Omit<CurrentUser, 'role'> & {role?: string}) | null,
+  roleId: string
+): boolean {
+  return user !== null && user.roles.some((role) => role.name === roleId)
 }
