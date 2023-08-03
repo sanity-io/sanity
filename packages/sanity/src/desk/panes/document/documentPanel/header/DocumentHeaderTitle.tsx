7d97baab736d6b7afe134892f7553a2fbf4b0394
Binoy Patel <me@binoy.io>
2023-08-03 13:27:02 -0400

    fix(core): fix tests

--- packages/sanity/src/desk/panes/document/documentPanel/header/DocumentHeaderTitle.tsx
+++ packages/sanity/src/desk/panes/document/documentPanel/header/DocumentHeaderTitle.tsx
@@ -1,8 +1,14 @@
 import React, {ReactElement} from 'react'
 import {useDocumentTitle} from '../../useDocumentTitle'
+import {useDocumentPane} from '../../useDocumentPane'
 
 export function DocumentHeaderTitle(): ReactElement {
+  const {connectionState} = useDocumentPane()
   const {error, title} = useDocumentTitle()
+
+  if (connectionState !== 'connected') {
+    return <></>
+  }
 
   if (error) {
     return <>{error}</>
