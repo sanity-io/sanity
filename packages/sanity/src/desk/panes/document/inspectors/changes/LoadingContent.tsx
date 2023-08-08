12594707559c3fc0803abd36267061337d92a19b
RitaDias <anarita151@gmail.com>
2023-08-08 12:24:37 +0200

    feat(desk): add i18n primitives to LoadingContent

--- packages/sanity/src/desk/panes/document/inspectors/changes/LoadingContent.tsx
+++ packages/sanity/src/desk/panes/document/inspectors/changes/LoadingContent.tsx
@@ -1,15 +1,19 @@
 import React from 'react'
 import {Box, Flex, Spinner, Text} from '@sanity/ui'
 import {Delay} from '../../../../components'
+import {deskLocaleNamespace} from '../../../../i18n'
+import {useTranslation} from 'sanity'
 
 export function LoadingContent() {
+  const {t} = useTranslation(deskLocaleNamespace)
+
   return (
     <Delay ms={300}>
       <Flex align="center" direction="column" height="fill" justify="center" paddingTop={3}>
         <Spinner muted />
         <Box marginTop={3}>
           <Text align="center" muted size={1}>
-            Loading changes
+            {t('desk.review-changes.loading-changes')}
           </Text>
         </Box>
       </Flex>
