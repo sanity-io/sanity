162e5dc28e08cd69df7ee8b0060864b24654906d
RitaDias <anarita151@gmail.com>
2023-08-09 11:10:44 +0200

    feat(desk): add i18n primitives to timelineItem + removal of unused consts

--- packages/sanity/src/desk/panes/document/timeline/constants.ts
+++ packages/sanity/src/desk/panes/document/timeline/constants.ts
@@ -8,17 +8,6 @@ import {
   AddCircleIcon,
 } from '@sanity/icons'
 
-export const TIMELINE_LABELS: {[key: string]: string | undefined} = {
-  create: 'created',
-  delete: 'deleted',
-  discardDraft: 'discarded draft',
-  initial: 'created',
-  editDraft: 'edited',
-  editLive: 'live edited',
-  publish: 'published',
-  unpublish: 'unpublished',
-}
-
 export const TIMELINE_ICON_COMPONENTS: {[key: string]: IconComponent | undefined} = {
   create: AddCircleIcon,
   delete: TrashIcon,
