c712d844494a66491ad0d05457bc4d9c362927c8
Fred Carlsen <fred@sjelfull.no>
2023-08-11 16:26:51 +0200

    fix(core): add offline state to `useConnectionState`

--- packages/sanity/src/core/hooks/useConnectionState.ts
+++ packages/sanity/src/core/hooks/useConnectionState.ts
@@ -1,29 +1,46 @@
-import {distinctUntilChanged, map, mapTo, startWith, switchMap} from 'rxjs/operators'
-import {of, timer} from 'rxjs'
+import {
+  distinctUntilChanged,
+  map,
+  startWith,
+  switchMap,
+  filter,
+  mergeWith,
+  mapTo,
+} from 'rxjs/operators'
+import {of, timer, NEVER} from 'rxjs'
 import {useMemoObservable} from 'react-rx'
-import {useDocumentStore} from '../store'
+import {ErrorStatus, useConnectionStatusStore, useDocumentStore} from '../store'
 
 /** @internal */
-export type ConnectionState = 'connecting' | 'reconnecting' | 'connected'
+export type ConnectionState = 'connecting' | 'reconnecting' | 'connected' | 'offline'
 
 const INITIAL: ConnectionState = 'connecting'
 
 /** @internal */
 export function useConnectionState(publishedDocId: string, docTypeName: string): ConnectionState {
   const documentStore = useDocumentStore()
+  const {connectionStatus$} = useConnectionStatusStore()
 
   return useMemoObservable(
-    () =>
-      documentStore.pair.documentEvents(publishedDocId, docTypeName).pipe(
+    () => {
+      const documentEvents$ = documentStore.pair.documentEvents(publishedDocId, docTypeName).pipe(
         map((ev: {type: string}) => ev.type),
         map((eventType) => eventType !== 'reconnect'),
         switchMap((isConnected) =>
           isConnected ? of('connected') : timer(200).pipe(mapTo('reconnecting'))
-        ),
-        startWith(INITIAL as any),
-        distinctUntilChanged()
-      ),
-    [documentStore.pair, publishedDocId, docTypeName],
+        )
+      )
+
+      const connectionStatusUpdate$ = connectionStatus$.pipe(
+        filter((status): status is ErrorStatus => status.type === 'error'),
+        map((status) => (status.isOffline ? 'offline' : NEVER))
+      )
+
+      return documentEvents$
+        .pipe(mergeWith(connectionStatusUpdate$))
+        .pipe(startWith(INITIAL as any), distinctUntilChanged())
+    },
+    [documentStore.pair, connectionStatus$, publishedDocId, docTypeName],
     INITIAL
   )
 }
