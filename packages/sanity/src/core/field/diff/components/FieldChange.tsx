823f0f336195f5c5057de3acd652862b69b74575
RitaDias <anarita151@gmail.com>
2023-08-14 11:02:32 +0200

    fix(core): add 'action' subgroup to core (change panel)

--- packages/sanity/src/core/field/diff/components/FieldChange.tsx
+++ packages/sanity/src/core/field/diff/components/FieldChange.tsx
@@ -102,13 +102,15 @@ export function FieldChange(
                 <PopoverWrapper
                   content={
                     <Box padding={3} sizing="border">
-                      {t('review-changes.revert-button-prompt')}
+                      {t('review-changes.action.revert-changes-description')}
                       <Grid columns={2} gap={2} marginTop={2}>
                         <Button mode="ghost" onClick={closeRevertChangesConfirmDialog}>
-                          <Text align="center">{t('review-changes.revert-button-cancel')}</Text>
+                          <Text align="center">{t('review-changes.action.revert-all-cancel')}</Text>
                         </Button>
                         <Button tone="critical" onClick={handleRevertChanges}>
-                          <Text align="center">{t('review-changes.revert-button-change')}</Text>
+                          <Text align="center">
+                            {t('review-changes.action.revert-changes-confirm-change', {count: 1})}
+                          </Text>
                         </Button>
                       </Grid>
                     </Box>
