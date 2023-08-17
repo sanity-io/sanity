14eb8598e1d9687b380dbf53154ddcab441dfe62
RitaDias <anarita151@gmail.com>
2023-08-17 15:12:03 +0200

    feat(core): add i18n primitives to Instructions

--- packages/sanity/src/core/studio/components/navbar/search/components/Instructions.tsx
+++ packages/sanity/src/core/studio/components/navbar/search/components/Instructions.tsx
@@ -1,16 +1,18 @@
 import {ControlsIcon} from '@sanity/icons'
 import {Flex, Inline, Text} from '@sanity/ui'
 import React from 'react'
+import {useTranslation} from '../../../../../i18n'
 
 export function Instructions() {
+  const {t} = useTranslation()
+
   return (
     <Flex align="center" direction="column" gap={4} paddingX={4} paddingY={5}>
       <Inline space={3}>
-        <Text muted>Use</Text>
+        <Text muted>{t('navbar.search.instructions')}</Text>
         <Text muted>
           <ControlsIcon />
         </Text>
-        <Text muted>to refine your search</Text>
       </Inline>
     </Flex>
   )
