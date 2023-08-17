725b1b6c2ff85f06c12d4e10a1e86ce91ba9d104
RitaDias <anarita151@gmail.com>
2023-08-17 11:58:05 +0200

    feat(vision): add i18n primitives to tryParseParams

--- packages/@sanity/vision/src/util/tryParseParams.ts
+++ packages/@sanity/vision/src/util/tryParseParams.ts
@@ -1,13 +1,17 @@
 import JSON5 from 'json5'
+import {TFunction} from 'sanity'
 
-export function tryParseParams(val: string): Record<string, unknown> | Error {
+export function tryParseParams(
+  val: string,
+  t: TFunction<'vision', undefined>,
+): Record<string, unknown> | Error {
   try {
     const parsed = val ? JSON5.parse(val) : {}
     return typeof parsed === 'object' && parsed && !Array.isArray(parsed) ? parsed : {}
   } catch (err) {
     // JSON5 always prefixes the error message with JSON5:, so we remove it
     // to clean up the error tooltip
-    err.message = `Parameters are not valid JSON:\n\n${err.message.replace('JSON5:', '')}`
+    err.message = `${t('query.error.params-invalid-json')}:\n\n${err.message.replace('JSON5:', '')}`
     return err
   }
 }
