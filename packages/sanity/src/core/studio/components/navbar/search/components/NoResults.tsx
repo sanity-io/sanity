e6738a2cba91114b69790e52612b9cbdbbce794a
RitaDias <anarita151@gmail.com>
2023-08-17 14:52:41 +0200

    feat(core): add i18n primitives to NoResults

--- packages/sanity/src/core/studio/components/navbar/search/components/NoResults.tsx
+++ packages/sanity/src/core/studio/components/navbar/search/components/NoResults.tsx
@@ -1,15 +1,18 @@
 import {Container, Stack, Text} from '@sanity/ui'
 import React from 'react'
+import {useTranslation} from '../../../../../i18n'
 
 export function NoResults() {
+  const {t} = useTranslation()
+
   return (
     <Container width={0}>
       <Stack aria-live="assertive" space={4} paddingX={4} paddingY={5}>
         <Text align="center" muted weight="semibold">
-          No results found
+          {t('navbar.search.no-results-title')}
         </Text>
         <Text align="center" muted size={1}>
-          Try another keyword or adjust your filters
+          {t('navbar.search.no-results-description')}
         </Text>
       </Stack>
     </Container>
