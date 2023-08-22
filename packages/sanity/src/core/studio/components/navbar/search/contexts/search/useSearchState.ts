11b3330e6ba49e497f031500256771bff4188839
RitaDias <anarita151@gmail.com>
2023-08-22 12:49:03 +0200

    feat(core): add i18n primitives to useSearchState

--- packages/sanity/src/core/studio/components/navbar/search/contexts/search/useSearchState.ts
+++ packages/sanity/src/core/studio/components/navbar/search/contexts/search/useSearchState.ts
@@ -1,10 +1,13 @@
 import {useContext} from 'react'
+import {useTranslation} from '../../../../../../i18n'
 import {SearchContext, SearchContextValue} from './SearchContext'
 
 export function useSearchState(): SearchContextValue {
   const context = useContext(SearchContext)
+  const {t} = useTranslation()
+
   if (context === undefined) {
-    throw new Error('useSearchState must be used within an SearchProvider')
+    throw new Error(t('navbar.search.error.use-search-state-not-used-within-provider'))
   }
   return context
 }
