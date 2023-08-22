1cbbad92e3f9996532bcbbdcdefdc86075a8036a
RitaDias <anarita151@gmail.com>
2023-08-22 12:29:38 +0200

    feat(core): add i18n primitives to Boolean

--- packages/sanity/src/core/studio/components/navbar/search/components/filters/filter/inputs/boolean/Boolean.tsx
+++ packages/sanity/src/core/studio/components/navbar/search/components/filters/filter/inputs/boolean/Boolean.tsx
@@ -2,11 +2,13 @@ import {Select} from '@sanity/ui'
 import React, {ChangeEvent, useCallback} from 'react'
 import {useSearchState} from '../../../../../contexts/search/useSearchState'
 import {type OperatorInputComponentProps} from '../../../../../definitions/operators/operatorTypes'
+import {useTranslation} from '../../../../../../../../../i18n'
 
 export function SearchFilterBooleanInput({onChange, value}: OperatorInputComponentProps<boolean>) {
   const {
     state: {fullscreen},
   } = useSearchState()
+  const {t} = useTranslation()
 
   const handleChange = useCallback(
     (event: ChangeEvent<HTMLSelectElement>) => {
@@ -22,8 +24,8 @@ export function SearchFilterBooleanInput({onChange, value}: OperatorInputCompone
       radius={2}
       value={String(value ?? true)}
     >
-      <option value="true">True</option>
-      <option value="false">False</option>
+      <option value="true">{t('navbar.search.true')}</option>
+      <option value="false">{t('navbar.search.false')}</option>
     </Select>
   )
 }
