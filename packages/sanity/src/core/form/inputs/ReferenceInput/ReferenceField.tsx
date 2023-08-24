0a2438cde252e63b25b63d1ef5f1e3eb0ba2b38c
Nina Andal Aarvik <nina@sanity.io>
2023-08-24 10:25:53 +0200

    fix(core): remove padding on ReferenceField (#4842)

--- packages/sanity/src/core/form/inputs/ReferenceInput/ReferenceField.tsx
+++ packages/sanity/src/core/form/inputs/ReferenceInput/ReferenceField.tsx
@@ -302,7 +302,7 @@ export function ReferenceField(props: ReferenceFieldProps) {
         validation={props.validation}
       >
         {isEditing ? (
-          <Box paddingY={2}>{children}</Box>
+          <Box>{children}</Box>
         ) : (
           <Card shadow={1} radius={1} padding={1} tone={tone}>
             <Stack space={1}>
