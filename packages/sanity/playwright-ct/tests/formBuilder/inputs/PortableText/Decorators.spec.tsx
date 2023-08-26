9a5c5c3e82f4a0ff019acd122a064e3529632eb2
Per-Kristian Nordnes <per.kristian.nordnes@gmail.com>
2023-08-26 14:04:20 +0200

    test(playwright-ct): simplify decorators tests
    Use a lookup map and iterate on each default decorator.

--- packages/sanity/playwright-ct/tests/formBuilder/inputs/PortableText/Decorators.spec.tsx
+++ packages/sanity/playwright-ct/tests/formBuilder/inputs/PortableText/Decorators.spec.tsx
@@ -4,15 +4,42 @@ import React from 'react'
 import {testHelpers} from '../../../utils/testHelpers'
 import {DecoratorsStory} from './DecoratorsStory'
 
-test.beforeEach(({browserName}) => {
-  test.skip(
-    browserName === 'webkit' && Os.platform() === 'linux',
-    'Skipping Webkit for Linux which currently is flaky with this test.',
-  )
-})
+const DEFAULT_DECORATORS = [
+  {
+    name: 'strong',
+    title: 'Strong',
+    hotkey: 'b',
+  },
+  {
+    name: 'em',
+    title: 'Italic',
+    hotkey: 'i',
+  },
+  {
+    name: 'underline',
+    title: 'Underline',
+    hotkey: 'u',
+  },
+  {
+    name: 'code',
+    title: 'Code',
+    hotkey: "'",
+  },
+  {
+    name: 'strike',
+    title: 'Strike',
+    hotkey: undefined, // Currently not defined
+  },
+]
 
 test.describe('Decorators', () => {
   test.describe('Keyboard shortcuts', () => {
+    test.beforeEach(({browserName}) => {
+      test.skip(
+        browserName === 'webkit' && Os.platform() === 'linux',
+        'Skipping Webkit for Linux which currently is flaky with this test.',
+      )
+    })
     test('Render default styles with keyboard shortcuts', async ({mount, page}) => {
       const {getModifierKey, getFocusedPortableTextEditor, insertPortableText, toggleHotkey} =
         testHelpers({
@@ -22,48 +49,31 @@ test.describe('Decorators', () => {
       const $pte = await getFocusedPortableTextEditor('field-body')
       const modifierKey = getModifierKey()
 
-      // Bold
-      await toggleHotkey('b', modifierKey)
-      await insertPortableText('bold text 123', $pte)
-      await toggleHotkey('b', modifierKey)
-      await expect($pte.locator('[data-mark="strong"]', {hasText: 'bold text'})).toBeVisible()
-
-      // Italic
-      await toggleHotkey('i', modifierKey)
-      await insertPortableText('italic text', $pte)
-      await toggleHotkey('i', modifierKey)
-      await expect($pte.locator('[data-mark="em"]', {hasText: 'italic text'})).toBeVisible()
-
-      // Underline
-      await toggleHotkey('u', modifierKey)
-      await insertPortableText('underlined text', $pte)
-      await toggleHotkey('u', modifierKey)
-      await expect(
-        $pte.locator('[data-mark="underline"]', {
-          hasText: 'underlined text',
-        }),
-      ).toBeVisible()
-
-      // Code
-      await toggleHotkey("'", modifierKey)
-      await insertPortableText('code text', $pte)
-      await expect($pte.locator('[data-mark="code"]', {hasText: 'code text'})).toBeVisible()
-      await toggleHotkey("'", modifierKey)
+      // eslint-disable-next-line max-nested-callbacks
+      for (const decorator of DEFAULT_DECORATORS) {
+        if (decorator.hotkey) {
+          await toggleHotkey(decorator.hotkey, modifierKey)
+          await insertPortableText(`${decorator.name} text 123`, $pte)
+          await toggleHotkey(decorator.hotkey, modifierKey)
+          await expect(
+            $pte.locator(`[data-mark="${decorator.name}"]`, {
+              hasText: `${decorator.name} text 123`,
+            }),
+          ).toBeVisible()
+        }
+      }
     })
   })
 
-  test.describe('Menu bar', () => {
-    test('Should display all default decorators', async ({mount, page}) => {
+  test.describe('Toolbar', () => {
+    test('Should display all default decorator buttons', async ({mount, page}) => {
       const {getFocusedPortableTextInput} = testHelpers({page})
       await mount(<DecoratorsStory />)
       const $portableTextInput = await getFocusedPortableTextInput('field-body')
 
-      // Assertion: All icons in the menu bar should be visible
-      const ICONS = ['bold', 'code', 'italic', 'underline']
-      for (const icon of ICONS) {
-        await expect(
-          $portableTextInput.getByRole('button').locator(`[data-sanity-icon="${icon}"]`),
-        ).toBeVisible()
+      // Assertion: All buttons in the menu bar should be visible
+      for (const decorator of DEFAULT_DECORATORS) {
+        await expect($portableTextInput.getByRole('button', {name: decorator.title})).toBeVisible()
       }
     })
   })
