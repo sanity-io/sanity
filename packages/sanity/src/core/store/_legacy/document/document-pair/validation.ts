4fbb59fcbea5c94f74409b7efca5b36adb3350e7
Bjørge Næss <bjoerge@gmail.com>
2023-08-28 10:28:48 -0700

    fix(core): wait for availability status to arrive before passing on document existence status (#4860)
    * fix(core): wait for availability status to arrive before passing on document existence status
    
    * fixup! fix(core): wait for availability status to arrive before passing on document existence status

--- packages/sanity/src/core/store/_legacy/document/document-pair/validation.ts
+++ packages/sanity/src/core/store/_legacy/document/document-pair/validation.ts
@@ -148,7 +148,9 @@ export const validation = memoize(
     const getDocumentExists: GetDocumentExists = ({id}) =>
       lastValueFrom(
         referenceExistence$.pipe(
-          first(),
+          // If the id is not present as key in the `referenceExistence` map it means it's existence status
+          // isn't yet loaded, so we want to wait until it is
+          first((referenceExistence) => id in referenceExistence),
           map((referenceExistence) => referenceExistence[id]),
         ),
       )
