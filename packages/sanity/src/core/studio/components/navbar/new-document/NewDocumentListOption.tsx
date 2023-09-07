45667c2cd65e22ce65d013e2f2a54281402632c0
Espen Hovlandsdal <espen@hovlandsdal.com>
2023-09-07 10:03:53 -0700

    fix(core): inconsistent navbar prefix

--- packages/sanity/src/core/studio/components/navbar/new-document/NewDocumentListOption.tsx
+++ packages/sanity/src/core/studio/components/navbar/new-document/NewDocumentListOption.tsx
@@ -53,7 +53,7 @@ export function NewDocumentListOption(props: NewDocumentListOptionProps) {
         <TooltipContentBox padding={2}>
           <InsufficientPermissionsMessage
             currentUser={currentUser}
-            operationLabel={t('nav-bar.new-document.error.unable-to-create-document')}
+            operationLabel={t('navbar.new-document.error.unable-to-create-document')}
           />
         </TooltipContentBox>
       }
