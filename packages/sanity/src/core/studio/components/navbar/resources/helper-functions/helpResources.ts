fa1158828d82667085c7d2fe71f9364fa29fb9bd
Espen Hovlandsdal <espen@hovlandsdal.com>
2023-09-13 14:08:29 -0700

    refactor(i18n): prepare to send locale for help & resources request

--- packages/sanity/src/core/studio/components/navbar/resources/helper-functions/helpResources.ts
+++ packages/sanity/src/core/studio/components/navbar/resources/helper-functions/helpResources.ts
@@ -1,5 +1,4 @@
 import type {Observable} from 'rxjs'
-import {map} from 'rxjs/operators'
 import {SanityClient} from '@sanity/client'
 import {SANITY_VERSION} from '../../../../../version'
 import {ResourcesResponse} from './types'
@@ -8,21 +7,20 @@ import {ResourcesResponse} from './types'
  * @internal Not a stable API yet
  * @hidden
  */
-export function getHelpResources(client: SanityClient): Observable<ResourcesResponse> {
-  const request$ = client
-    .withConfig({apiVersion: '1'})
-    .observable.request<ResourcesResponse>({
-      url: '/help',
-      /*
+export function getHelpResources(
+  client: SanityClient,
+  locale: string,
+): Observable<ResourcesResponse> {
+  // @todo: Send `locale` as query parameter once API supports it
+  return client.withConfig({apiVersion: '1'}).observable.request<ResourcesResponse>({
+    url: '/help',
+    /*
       query and tag is used by analytics for tracking.
       Builds to: `{m: ['sanity@3.0.2']}' and serializes to: `?m=sanity@3.0.2`.
       Final format will be e.g. https://api.sanity.io/v1/help?tag=sanity.studio.module.version-check&m=sanity%403.0.2
       */
-      query: {m: [`sanity@${SANITY_VERSION}`]},
-      tag: 'module.version-check',
-      json: true,
-    })
-    .pipe(map((result) => ({...result})))
-
-  return request$
+    query: {m: [`sanity@${SANITY_VERSION}`]},
+    tag: 'module.version-check',
+    json: true,
+  })
 }
