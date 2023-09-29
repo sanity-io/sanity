b57c07f0d3f59cff1b07d8d42a65499cdc865aaa
Tommy Petty <tommy.petty@gmail.com>
2023-09-29 15:52:42 -0400

    Prep work for making e2e more isolated

--- test/e2e/helpers/createUniqueDocument.ts
+++ test/e2e/helpers/createUniqueDocument.ts
@@ -1,18 +1,16 @@
-import {SanityDocument, SanityDocumentStub} from '@sanity/client'
+import {SanityClient, SanityDocument, SanityDocumentStub} from '@sanity/client'
 import {uuid} from '@sanity/uuid'
-import {testSanityClient} from './sanityClient'
 
-export async function createUniqueDocument({
-  _type,
-  _id,
-  ...restProps
-}: SanityDocumentStub): Promise<Partial<SanityDocument>> {
+export async function createUniqueDocument(
+  client: SanityClient,
+  {_type, _id, ...restProps}: SanityDocumentStub,
+): Promise<Partial<SanityDocument>> {
   const doc = {
     _type,
     _id: _id || uuid(),
     ...restProps,
   }
 
-  await testSanityClient.create(doc, {visibility: 'async'})
+  await client.create(doc, {visibility: 'async'})
   return doc
 }
--- test/e2e/tests/desk/inspectors.spec.ts
+++ test/e2e/tests/desk/inspectors.spec.ts
@@ -1,85 +1,90 @@
+/* eslint-disable max-nested-callbacks */
 import {test, expect} from '@playwright/test'
-import {createUniqueDocument, testSanityClient} from '../../helpers'
+import {createUniqueDocument, withDefaultClient} from '../../helpers'
 
-test.describe.skip('sanity/desk: document inspectors', () => {
-  test('open and close custom inspector', async ({page}) => {
-    await page.goto('/test/content/input-debug;inspectorsTest;inspectors-test')
+withDefaultClient((context) => {
+  test.describe.skip('sanity/desk: document inspectors', () => {
+    test('open and close custom inspector', async ({page}) => {
+      await page.goto('/test/content/input-debug;inspectorsTest;inspectors-test')
 
-    // Click to open inspector
-    await page
-      .locator('[data-ui="StatusButton"][aria-label="Custom inspector"]')
-      .click({timeout: 0})
-
-    // Expect button to be selected and inspector to be visible
-    await expect(
-      page.locator('[data-ui="StatusButton"][aria-label="Custom inspector"][data-selected]'),
-    ).toBeVisible()
-    await expect(page.locator('aside[data-ui="DocumentInspectorPanel"]')).toBeVisible()
-    await expect(page.locator('aside[data-ui="DocumentInspectorPanel"] h1')).toContainText(
-      'Custom inspector',
-    )
-
-    // Click to close inspector
-    await page.locator('button[aria-label="Close custom inspector"]').click()
-
-    expect(
+      // Click to open inspector
       await page
         .locator('[data-ui="StatusButton"][aria-label="Custom inspector"]')
-        .evaluate((el) => el.getAttribute('data-selected')),
-    ).toBe(null)
-  })
+        .click({timeout: 0})
 
-  test('open "Validation" inspector', async ({page}) => {
-    // create published document
-    const uniqueDoc = await createUniqueDocument({_type: 'validationTest'})
-    const id = uniqueDoc._id!
+      // Expect button to be selected and inspector to be visible
+      await expect(
+        page.locator('[data-ui="StatusButton"][aria-label="Custom inspector"][data-selected]'),
+      ).toBeVisible()
+      await expect(page.locator('aside[data-ui="DocumentInspectorPanel"]')).toBeVisible()
+      await expect(page.locator('aside[data-ui="DocumentInspectorPanel"] h1')).toContainText(
+        'Custom inspector',
+      )
 
-    // create draft document
-    await createUniqueDocument({
-      _type: 'inspectorsTest',
-      _id: `drafts.${id}`,
-      name: 'Edited by e2e test runner',
+      // Click to close inspector
+      await page.locator('button[aria-label="Close custom inspector"]').click()
+
+      expect(
+        await page
+          .locator('[data-ui="StatusButton"][aria-label="Custom inspector"]')
+          .evaluate((el) => el.getAttribute('data-selected')),
+      ).toBe(null)
     })
 
-    await page.goto(`/test/content/input-debug;validationTest;${id}`)
+    test('open "Validation" inspector', async ({page}) => {
+      // create published document
+      const uniqueDoc = await createUniqueDocument(context.client, {_type: 'validationTest'})
+      const id = uniqueDoc._id!
 
-    // Click to open inspector
-    await page.locator('[data-ui="StatusButton"][aria-label="Validation"]').click({timeout: 0})
+      // create draft document
+      await createUniqueDocument(context.client, {
+        _type: 'inspectorsTest',
+        _id: `drafts.${id}`,
+        name: 'Edited by e2e test runner',
+      })
 
-    // Expect button to be selected and inspector to be visible
-    await expect(
-      page.locator('[data-ui="StatusButton"][aria-label="Validation"][data-selected]'),
-    ).toBeVisible()
-    await expect(page.locator('aside[data-ui="DocumentInspectorPanel"]')).toBeVisible()
-    await expect(page.locator('aside[data-ui="DocumentInspectorPanel"] h1')).toContainText(
-      'Validation',
-    )
-  })
+      await page.goto(`/test/content/input-debug;validationTest;${id}`)
 
-  test('open "Review changes" inspector', async ({page}) => {
-    // create published document
-    const uniqueDoc = await createUniqueDocument({_type: 'inspectorsTest'})
-    const id = uniqueDoc._id!
+      // Click to open inspector
+      await page.locator('[data-ui="StatusButton"][aria-label="Validation"]').click({timeout: 0})
 
-    // create draft document
-    await createUniqueDocument({
-      _type: 'inspectorsTest',
-      _id: `drafts.${id}`,
-      name: 'Edited by e2e test runner',
+      // Expect button to be selected and inspector to be visible
+      await expect(
+        page.locator('[data-ui="StatusButton"][aria-label="Validation"][data-selected]'),
+      ).toBeVisible()
+      await expect(page.locator('aside[data-ui="DocumentInspectorPanel"]')).toBeVisible()
+      await expect(page.locator('aside[data-ui="DocumentInspectorPanel"] h1')).toContainText(
+        'Validation',
+      )
     })
 
-    await page.goto(`/test/content/input-debug;inspectorsTest;${id}`)
+    test('open "Review changes" inspector', async ({page}) => {
+      // create published document
+      const uniqueDoc = await createUniqueDocument(context.client, {_type: 'inspectorsTest'})
+      const id = uniqueDoc._id!
 
-    // Click to open inspector
-    await page.locator('[data-testid="review-changes-button"]').click()
+      // create draft document
+      await createUniqueDocument(context.client, {
+        _type: 'inspectorsTest',
+        _id: `drafts.${id}`,
+        name: 'Edited by e2e test runner',
+      })
 
-    // Expect button to be selected and inspector to be visible
-    await expect(page.locator('[data-testid="review-changes-button"][data-selected]')).toBeVisible()
-    await expect(page.locator('aside[data-ui="DocumentInspectorPanel"]')).toBeVisible()
-    await expect(page.locator('aside[data-ui="DocumentInspectorPanel"] h1')).toContainText(
-      'Review changes',
-    )
+      await page.goto(`/test/content/input-debug;inspectorsTest;${id}`)
 
-    await testSanityClient.delete(id)
+      // Click to open inspector
+      await page.locator('[data-testid="review-changes-button"]').click()
+
+      // Expect button to be selected and inspector to be visible
+      await expect(
+        page.locator('[data-testid="review-changes-button"][data-selected]'),
+      ).toBeVisible()
+      await expect(page.locator('aside[data-ui="DocumentInspectorPanel"]')).toBeVisible()
+      await expect(page.locator('aside[data-ui="DocumentInspectorPanel"] h1')).toContainText(
+        'Review changes',
+      )
+
+      await context.client.delete(id)
+    })
   })
 })
