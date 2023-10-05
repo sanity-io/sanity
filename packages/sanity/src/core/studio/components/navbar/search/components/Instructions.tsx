b204882e16213d44ebd94529635983e9c73f4aa3
Espen Hovlandsdal <espen@hovlandsdal.com>
2023-10-04 19:02:12 -0700

    refactor(i18n): drop use of `<Trans />` component

--- packages/sanity/src/core/studio/components/navbar/search/components/Instructions.tsx
+++ packages/sanity/src/core/studio/components/navbar/search/components/Instructions.tsx
@@ -1,8 +1,7 @@
 import {ControlsIcon} from '@sanity/icons'
 import {Flex, Inline, Text} from '@sanity/ui'
 import React from 'react'
-import {Trans} from 'react-i18next'
-import {useTranslation} from '../../../../../i18n'
+import {Translate, useTranslation} from '../../../../../i18n'
 
 export function Instructions() {
   const {t} = useTranslation()
@@ -11,10 +10,12 @@ export function Instructions() {
     <Flex align="center" direction="column" gap={4} paddingX={4} paddingY={5}>
       <Inline space={3}>
         <Text muted>
-          <Trans
+          <Translate
             t={t}
             i18nKey="search.instructions"
-            components={[<ControlsIcon key={0} style={{padding: '0 0.25rem'}} />]}
+            components={{
+              ControlsIcon: () => <ControlsIcon key={0} style={{padding: '0 0.25rem'}} />,
+            }}
           />
         </Text>
       </Inline>
