1f2b462552639be4faa011f047c8a18448052374
Herman Wikner <wiknerherman@gmail.com>
2023-10-13 11:48:10 +0200

    refactor: remove unused context

--- packages/sanity/src/desk/comments/src/context/client/CommentsClientContext.ts
+++
@@ -1,8 +0,0 @@
-import {createContext} from 'react'
-import {CommentsClientContextValue} from './types'
-
-/**
- * @beta
- * @hidden
- */
-export const CommentsClientContext = createContext<CommentsClientContextValue | null>(null)
