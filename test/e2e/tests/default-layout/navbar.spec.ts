42cf7ff9fb694edb4b4f57b5f84e8581a3c41edc
Binoy Patel <me@binoy.io>
2023-10-16 11:05:52 -0400

    chore(test): test failed e2e test

--- test/e2e/tests/default-layout/navbar.spec.ts
+++ test/e2e/tests/default-layout/navbar.spec.ts
@@ -3,6 +3,6 @@ import {test, expect} from '@playwright/test'
 test.describe('@sanity/default-layout: Navbar', () => {
   test('render ActionModal on top of pane headers in desk tool', async ({page}) => {
     page.goto('/test/content')
-    await expect(page.locator('data-testid=navbar')).toBeVisible()
+    await expect(page.locator('data-testid=navbar')).toBeHidden()
   })
 })
