861d1e823b86d9ebaf6b1439f92ee7eea5a20a75
Per-Kristian Nordnes <per.kristian.nordnes@gmail.com>
2023-10-16 16:47:54 +0200

    test(playwright-ct): add test for focus tracking in PT-input

---
+++ packages/sanity/playwright-ct/tests/formBuilder/inputs/PortableText/FocusTrackingStory.tsx
@@ -0,0 +1,80 @@
+/* eslint-disable react/jsx-no-bind */
+import {Path, defineArrayMember, defineField, defineType} from '@sanity/types'
+import React, {useEffect, useState} from 'react'
+import {SanityDocument} from '@sanity/client'
+import {TestWrapper} from '../../utils/TestWrapper'
+
+const SCHEMA_TYPES = [
+  defineType({
+    type: 'document',
+    name: 'test',
+    title: 'Test',
+    fields: [
+      defineField({
+        type: 'array',
+        name: 'body',
+        of: [
+          defineArrayMember({
+            type: 'block',
+            of: [
+              defineArrayMember({
+                type: 'object',
+                name: 'inlineObjectWithTextProperty',
+                fields: [
+                  defineField({
+                    type: 'string',
+                    name: 'text',
+                    components: {
+                      input: (inputProps) => (
+                        <div data-testid="inlineTextInputField">
+                          {inputProps.renderDefault(inputProps)}
+                        </div>
+                      ),
+                    },
+                  }),
+                ],
+              }),
+            ],
+          }),
+          defineArrayMember({
+            type: 'object',
+            name: 'testObjectBlock',
+            fields: [{type: 'string', name: 'text'}],
+            components: {
+              input: (inputProps) => (
+                <div data-testid="objectBlockInputField">
+                  {inputProps.renderDefault(inputProps)}
+                </div>
+              ),
+            },
+          }),
+        ],
+      }),
+    ],
+  }),
+]
+
+export function FocusTrackingStory({
+  focusPath: focusPathFromProps,
+  document,
+}: {
+  focusPath?: Path
+  document?: SanityDocument
+}) {
+  const [focusPath, setFocusPath] = useState<Path>(focusPathFromProps || [])
+  useEffect(() => {
+    ;(window as any).__setFocusPath = (path: Path) => {
+      setFocusPath(path)
+    }
+    return () => {
+      ;(window as any).__setFocusPath = undefined
+    }
+  }, [])
+  return (
+    <div data-focus-path={JSON.stringify(focusPath)}>
+      <TestWrapper schemaTypes={SCHEMA_TYPES} focusPath={focusPath} document={document} />
+    </div>
+  )
+}
+
+export default FocusTrackingStory
