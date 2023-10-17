8f9d4056ec17f7d8572aaec158e4b1d313b1b3c5
Espen Hovlandsdal <espen@hovlandsdal.com>
2023-10-16 21:14:16 -0700

    feat(i18n): use i18n primitives for inspect dialog, document pane menu items

--- packages/sanity/src/desk/panes/document/inspectDialog/constants.ts
+++ packages/sanity/src/desk/panes/document/inspectDialog/constants.ts
@@ -1,3 +1,3 @@
-export const VIEW_MODE_PARSED = {id: 'parsed', title: 'Parsed'}
-export const VIEW_MODE_RAW = {id: 'raw', title: 'Raw JSON'}
+export const VIEW_MODE_PARSED = {id: 'parsed', title: 'document-inspector.view-mode.parsed'}
+export const VIEW_MODE_RAW = {id: 'raw', title: 'document-inspector.view-mode.raw-json'}
 export const VIEW_MODES = [VIEW_MODE_PARSED, VIEW_MODE_RAW]
