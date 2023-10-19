e96b449894967d1ecbad7f752c09bc9b0dc2c9b3
Tommy Petty <tommy.petty@gmail.com>
2023-10-19 14:01:04 -0400

    feat(i18n): add localization for UnknownPaneType

--- packages/sanity/src/desk/panes/unknown/UnknownPaneType.tsx
+++ packages/sanity/src/desk/panes/unknown/UnknownPaneType.tsx
@@ -1,7 +1,8 @@
 import {Box, Text} from '@sanity/ui'
 import React from 'react'
 import {Pane, PaneContent, PaneHeader} from '../../components/pane'
-import {isRecord} from 'sanity'
+import {deskLocaleNamespace} from '../../i18n'
+import {isRecord, useTranslation} from 'sanity'
 
 interface UnknownPaneProps {
   isSelected: boolean
@@ -15,6 +16,7 @@ interface UnknownPaneProps {
 export function UnknownPane(props: UnknownPaneProps) {
   const {isSelected, pane, paneKey} = props
   const type = (isRecord(pane) && pane.type) || null
+  const {t} = useTranslation(deskLocaleNamespace)
 
   return (
     <Pane id={paneKey} selected={isSelected}>
@@ -23,11 +25,11 @@ export function UnknownPane(props: UnknownPaneProps) {
         <Box padding={4}>
           {typeof type === 'string' ? (
             <Text as="p" muted>
-              Structure item of type <code>{type}</code> is not a known entity.
+              {t('panes.unknown-pane-type.unknown-type.text', {type: type})}
             </Text>
           ) : (
             <Text as="p" muted>
-              Structure item is missing required <code>type</code> property.
+              {t('panes.unknown-pane-type.missing-type.text')}
             </Text>
           )}
         </Box>
