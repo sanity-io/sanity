f87050fba1c29551ac83968c291b421a0c3f156c
Espen Hovlandsdal <espen@hovlandsdal.com>
2023-10-20 11:50:05 -0700

    feat(i18n): allow certain simple HTML tags when using Translate component

--- dev/test-studio/components/TranslateExample.tsx
+++ dev/test-studio/components/TranslateExample.tsx
@@ -1,27 +1,35 @@
 import {Translate, useTranslation} from 'sanity'
 import React from 'react'
-import {Card, Text} from '@sanity/ui'
+import {Card, Stack, Text} from '@sanity/ui'
 import {InfoFilledIcon} from '@sanity/icons'
 
 export function TranslateExample() {
   const {t} = useTranslation('testStudio')
   return (
     <Card padding={4}>
-      <Text>
-        <Translate
-          t={t}
-          i18nKey="translate.example"
-          components={{
-            Icon: () => <InfoFilledIcon />,
-            Red: ({children}) => <span style={{color: 'red'}}>{children}</span>,
-            Bold: ({children}) => <b>{children}</b>,
-          }}
-          values={{
-            keyword: 'something',
-            duration: '30',
-          }}
-        />
-      </Text>
+      <Stack space={4}>
+        <Text>{t('use-translation.with-html')}</Text>
+
+        <Text>
+          <Translate t={t} i18nKey="use-translation.with-html" />
+        </Text>
+
+        <Text>
+          <Translate
+            t={t}
+            i18nKey="translate.example"
+            components={{
+              Icon: () => <InfoFilledIcon />,
+              Red: ({children}) => <span style={{color: 'red'}}>{children}</span>,
+              Bold: ({children}) => <b>{children}</b>,
+            }}
+            values={{
+              keyword: 'something',
+              duration: '30',
+            }}
+          />
+        </Text>
+      </Stack>
     </Card>
   )
 }
