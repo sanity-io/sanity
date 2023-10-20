51955338938530736fe1d261bb67965a7c03609f
Per-Kristian Nordnes <per.kristian.nordnes@gmail.com>
2023-10-20 13:23:23 +0200

    test(playwright-ct): update props after refactor

--- packages/sanity/playwright-ct/tests/comments/CommentInputStory.tsx
+++ packages/sanity/playwright-ct/tests/comments/CommentInputStory.tsx
@@ -1,5 +1,6 @@
 import React, {useState} from 'react'
 import {CurrentUser, PortableTextBlock} from '@sanity/types'
+import {noop} from 'lodash'
 import {CommentInput} from '../../../src/desk/comments/src/components/pte/comment-input/CommentInput'
 import {TestWrapper} from '../formBuilder/utils/TestWrapper'
 
@@ -28,12 +29,9 @@ export function CommentsInputStory() {
         onChange={setValue}
         value={value}
         mentionOptions={{data: [], error: null, loading: false}}
-        onEditDiscard={() => {
-          // ...
-        }}
-        onSubmit={() => {
-          // ...
-        }}
+        onDiscardConfirm={noop}
+        onDiscardCancel={noop}
+        onSubmit={noop}
       />
     </TestWrapper>
   )
