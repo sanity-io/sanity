fca92536bb434b773c61ca540e785fcfdf33dc46
Per-Kristian Nordnes <per.kristian.nordnes@gmail.com>
2023-10-25 17:22:06 +0200

    test(playwrigh-ct): add test for comment submission

--- packages/sanity/playwright-ct/tests/comments/CommentInput.spec.tsx
+++ packages/sanity/playwright-ct/tests/comments/CommentInput.spec.tsx
@@ -23,9 +23,27 @@ test.describe('Comments', () => {
     test('Should bring up mentions menu when typing @', async ({mount, page}) => {
       await mount(<CommentsInputStory />)
       const $editable = page.getByTestId('comment-input-editable')
-      await expect($editable).toBeEditable()
-      await page.keyboard.type(`@`)
+      await $editable.waitFor({state: 'visible'})
+      await page.keyboard.type('@')
       await expect(page.getByTestId('comments-mentions-menu')).toBeVisible()
+    })
+
+    test('Should be able to submit', async ({mount, page}) => {
+      const {insertPortableText} = testHelpers({page})
+      let submitted = false
+      const onSubmit = () => {
+        submitted = true
+      }
+      await mount(<CommentsInputStory onSubmit={onSubmit} />)
+      const $editable = page.getByTestId('comment-input-editable')
+      await expect($editable).toBeEditable()
+      // Test that blank comments can't be submitted
+      await page.keyboard.press('Enter')
+      expect(submitted).toBe(false)
+      await insertPortableText('This is a comment!', $editable)
+      await expect($editable).toHaveText('This is a comment!')
+      await page.keyboard.press('Enter')
+      expect(submitted).toBe(true)
     })
   })
 })
