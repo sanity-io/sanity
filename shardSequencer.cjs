d8e726a7905bb2027f66cbafa6684c716554ba48
Per-Kristian Nordnes <per.kristian.nordnes@gmail.com>
2023-10-27 22:17:38 +0200

    ci: remove shard sequencer, it was GitHub cache :tableflip:

--- shardSequencer.cjs
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-const Sequencer = require('@jest/test-sequencer').default
-
-module.exports = class JestShardSequencer extends Sequencer {
-  shard(tests) {
-    return tests.filter((test) => !test.path.includes('e2e'))
-  }
-}
