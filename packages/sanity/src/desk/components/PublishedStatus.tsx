2013203d084e4767442380339d3508afc7c786a0
Nina Andal Aarvik <nina@sanity.io>
2023-10-31 10:14:07 +0100

    fix(desk): edit logic for published- and edit status message in document list  (#5067)
    * fix(desk): edit logic for published- and change status message in document list
    
    * fix(desk): remove a TimeAgo component and use hook for PublishAction tooltip
    
    * fix(core): remove TimeAgo components, use useTimeAgo for reference previews
    
    * Update packages/sanity/src/core/form/inputs/ReferenceInput/ReferencePreview.tsx
    
    Co-authored-by: Robin Pyon <robinpyon@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Robin Pyon <robinpyon@users.noreply.github.com>

--- packages/sanity/src/desk/components/PublishedStatus.tsx
+++ packages/sanity/src/desk/components/PublishedStatus.tsx
@@ -2,26 +2,22 @@ import React from 'react'
 import {PublishIcon} from '@sanity/icons'
 import {PreviewValue, SanityDocument} from '@sanity/types'
 import {Box, Text, Tooltip} from '@sanity/ui'
-import {TimeAgo} from './TimeAgo'
-import {TextWithTone} from 'sanity'
+import {TextWithTone, useTimeAgo} from 'sanity'
 
 export function PublishedStatus(props: {document?: PreviewValue | Partial<SanityDocument> | null}) {
   const {document} = props
   const updatedAt = document && '_updatedAt' in document && document._updatedAt
   const statusLabel = document ? 'Published' : 'Not published'
 
+  // Label with abbreviations and suffix
+  const lastUpdatedTimeAgo = useTimeAgo(updatedAt || '', {minimal: true, agoSuffix: true})
+
   return (
     <Tooltip
       portal
       content={
         <Box padding={2}>
-          <Text size={1}>
-            {document ? (
-              <>Published {updatedAt && <TimeAgo time={updatedAt} />}</>
-            ) : (
-              <>Not published</>
-            )}
-          </Text>
+          <Text size={1}>{document ? `Published ${lastUpdatedTimeAgo}` : 'Not published'}</Text>
         </Box>
       }
     >
