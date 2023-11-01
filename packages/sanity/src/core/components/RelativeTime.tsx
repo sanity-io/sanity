1f9141ad7fd5bccc8c6cb192a8e93dfc805f71b7
Rico Kahler <ricokahler@gmail.com>
2023-11-01 17:16:47 -0500

    chore(core): add release tags to RelativeTime

--- packages/sanity/src/core/components/RelativeTime.tsx
+++ packages/sanity/src/core/components/RelativeTime.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import {type RelativeTimeOptions, useRelativeTime} from '../hooks/useRelativeTime'
 
+/**
+ * @internal
+ */
 export interface RelativeTimeProps extends RelativeTimeOptions {
   time: string | Date
 }
 
+/**
+ * @internal
+ */
 export function RelativeTime({time, ...options}: RelativeTimeProps) {
   const timestamp = time instanceof Date ? time : new Date(time)
   const timeAgo = useRelativeTime(timestamp, options)
