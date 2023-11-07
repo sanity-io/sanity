0d590deb29d7e7c1fdb337b194b778c4bca81209
Herman Wikner <wiknerherman@gmail.com>
2023-11-07 09:16:21 +0100

    feat(desk): delay comments onboarding popover visibility (#5133)

--- packages/sanity/src/desk/comments/src/components/onboarding/CommentsOnboardingPopover.tsx
+++ packages/sanity/src/desk/comments/src/components/onboarding/CommentsOnboardingPopover.tsx
@@ -1,9 +1,24 @@
 import {Box, Button, Flex, Popover, PopoverProps, Stack, Text} from '@sanity/ui'
 import React from 'react'
-import styled from 'styled-components'
+import styled, {keyframes} from 'styled-components'
 
 const Root = styled(Box)`
   max-width: 280px;
+`
+
+const fadeInKeyFrame = keyframes`
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+`
+
+const StyledPopover = styled(Popover)`
+  opacity: 0;
+  // Fade in the popover after 500ms
+  animation: ${fadeInKeyFrame} 200ms 500ms forwards;
 `
 
 interface CommentsOnboardingPopoverProps extends Omit<PopoverProps, 'content'> {
@@ -15,7 +30,7 @@ export function CommentsOnboardingPopover(props: CommentsOnboardingPopoverProps)
   const {onDismiss} = props
 
   return (
-    <Popover
+    <StyledPopover
       content={
         <Root padding={4}>
           <Stack space={3}>
