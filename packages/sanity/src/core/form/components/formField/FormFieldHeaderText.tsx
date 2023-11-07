e69303e3b149ce116f0479c90c270455cec4c54c
Espen Hovlandsdal <espen@hovlandsdal.com>
2023-11-07 10:25:54 -0800

    feat(i18n): translate form field components (#5137)

--- packages/sanity/src/core/form/components/formField/FormFieldHeaderText.tsx
+++ packages/sanity/src/core/form/components/formField/FormFieldHeaderText.tsx
@@ -1,8 +1,7 @@
-/* eslint-disable camelcase */
-
-import {FormNodeValidation} from '@sanity/types'
+import type {FormNodeValidation} from '@sanity/types'
 import {Box, Flex, Stack, Text} from '@sanity/ui'
 import React, {memo} from 'react'
+import {useTranslation} from '../../../i18n'
 import {createDescriptionId} from '../../members/common/createDescriptionId'
 import {FormFieldValidationStatus} from './FormFieldValidationStatus'
 
@@ -29,13 +28,18 @@ export const FormFieldHeaderText = memo(function FormFieldHeaderText(
   props: FormFieldHeaderTextProps,
 ) {
   const {description, inputId, title, validation = EMPTY_ARRAY} = props
+  const {t} = useTranslation()
   const hasValidations = validation.length > 0
 
   return (
     <Stack space={2}>
       <Flex>
         <Text as="label" htmlFor={inputId} weight="semibold" size={1}>
-          {title || <span style={{color: 'var(--card-muted-fg-color)'}}>Untitled</span>}
+          {title || (
+            <span style={{color: 'var(--card-muted-fg-color)'}}>
+              {t('form.field.untitled-field-label')}
+            </span>
+          )}
         </Text>
 
         {hasValidations && (
