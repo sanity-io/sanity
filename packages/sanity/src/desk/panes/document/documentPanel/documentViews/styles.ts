05d2ce5bf44ccd43b75caaad4b538e1366a8a13f
Nina Andal Aarvik <nina@sanity.io>
2023-11-08 11:31:26 +0000

    Revert "fix(desk): add interface to documentViews styles props"
    This reverts commit f9894498aa1733f3ec07959aea5f3b531d8b7ffc.

--- packages/sanity/src/desk/panes/document/documentPanel/documentViews/styles.ts
+++ packages/sanity/src/desk/panes/document/documentPanel/documentViews/styles.ts
@@ -5,14 +5,8 @@ export const Root = styled(Box)`
   position: relative;
 `
 
-interface TitleProps {
-  $titleHeight?: number
-  $muted?: boolean
-  theme: Theme
-}
-
-export const Title = styled.h6<TitleProps>`
-  ${({$titleHeight, $muted, theme}) => {
+export const Title = styled.h6(
+  ({theme, $titleHeight, $muted}: {theme: Theme; $titleHeight?: number; $muted?: boolean}) => {
     const {fontSize, lineHeight} = theme.sanity.fonts.heading.sizes[5]
 
     return css`
@@ -26,5 +20,5 @@ export const Title = styled.h6<TitleProps>`
       height: ${$titleHeight ? `${$titleHeight}px` : 'auto'};
       ${$muted ? `color: ${theme.sanity.color.card.disabled.fg};` : ''}
     `
-  }}
-`
+  },
+)
