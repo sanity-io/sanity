2b8f21c192d75021bb955a1c1a0a24dc763970dc
Robin Pyon <robin.pyon@gmail.com>
2023-11-08 01:27:08 +0000

    feat: initial pass of webkit-specific scrollbar styles

--- packages/sanity/src/core/studio/GlobalStyle.tsx
+++ packages/sanity/src/core/studio/GlobalStyle.tsx
@@ -1,9 +1,32 @@
 import {createGlobalStyle} from 'styled-components'
 
+const SCROLLBAR_SIZE = 14 // px
+const SCROLLBAR_BORDER_SIZE = 4 // px
+
 export const GlobalStyle = createGlobalStyle(({theme}) => {
   const {color, fonts} = theme.sanity
 
   return {
+    '::-webkit-scrollbar': {
+      width: `${SCROLLBAR_SIZE}px`,
+      height: `${SCROLLBAR_SIZE}px`,
+    },
+
+    '::-webkit-scrollbar-thumb': {
+      backgroundClip: 'content-box',
+      backgroundColor: color.muted.transparent.disabled.fg,
+      borderRadius: `${SCROLLBAR_SIZE}px`,
+      border: `${SCROLLBAR_BORDER_SIZE}px solid transparent`,
+    },
+
+    '::-webkit-scrollbar-thumb:hover': {
+      backgroundColor: color.muted.transparent.enabled.fg,
+    },
+
+    '::-webkit-scrollbar-track': {
+      background: 'transparent',
+    },
+
     html: {
       backgroundColor: color.base.bg,
     },
