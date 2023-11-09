ae339b0aaea4c0f15d6c90476b119f1e4d6e00d2
Robin Pyon <robin.pyon@gmail.com>
2023-11-09 23:42:46 +0000

    feat: always render workspace icon, except in single workspace setups with no custom icon provided

--- packages/sanity/src/core/config/types.ts
+++ packages/sanity/src/core/config/types.ts
@@ -678,7 +678,13 @@ export interface WorkspaceSummary {
   type: 'workspace-summary'
   name: string
   title: string
+  /**
+   * User supplied component if provided, otherwise falls back to
+   * an automatically generated default icon.
+   */
   icon: ReactNode
+  /** Returns true if a custom icon has been provided in studio config */
+  customIcon: boolean
   subtitle?: string
   basePath: string
   auth: AuthStore
