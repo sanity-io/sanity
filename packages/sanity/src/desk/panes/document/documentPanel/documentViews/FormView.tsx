3439753f1aadcdb97bcd0d5ea702a2625e5e55d7
Nina Andal Aarvik <nina@sanity.io>
2023-11-13 19:35:56 +0000

    feat: introduce large document titles from MVI-1 (#5161)
    * feat(core): adds title to document panel (#4782)
    
    * feat(core): avoid layout shift when editing title (#4828)
    
    * feat(core): avoid layout shift when editing title
    
    Fixes SDX-672
    
    * refactor(core): use line clamp for the title truncating
    
    * fix(desk): add interface to styles and max lines 4
    
    * fix(desk): fix merge conflict error
    
    * fix(desk): remove max 4 lines of header
    
    * fix(desk): add responsive title fontsize
    
    * fix(desk): remove header snapshotting
    
    * fix: tweak title margins, remove deprecated word-break CSS property
    
    ---------
    
    Co-authored-by: Binoy Patel <me@binoy.io>

--- packages/sanity/src/desk/panes/document/documentPanel/documentViews/FormView.tsx
+++ packages/sanity/src/desk/panes/document/documentPanel/documentViews/FormView.tsx
@@ -1,9 +1,11 @@
-import {Box, Container, Text, focusFirstDescendant} from '@sanity/ui'
+import {Box, Container, ElementQuery, Text, focusFirstDescendant} from '@sanity/ui'
 import React, {forwardRef, useEffect, useMemo, useCallback, useState} from 'react'
 import {tap} from 'rxjs/operators'
 import {LoadingBlock} from '../../../../../ui/loadingBlock'
+import {useDocumentTitle} from '../../useDocumentTitle'
 import {useDocumentPane} from '../../useDocumentPane'
 import {useConditionalToast} from './useConditionalToast'
+import {Title} from './styles'
 import {
   DocumentMutationEvent,
   DocumentRebaseEvent,
@@ -47,6 +49,7 @@ export const FormView = forwardRef<HTMLDivElement, FormViewProps>(function FormV
   } = useDocumentPane()
   const documentStore = useDocumentStore()
   const presence = useDocumentPresence(documentId)
+  const {title} = useDocumentTitle()
 
   // The `patchChannel` is an INTERNAL publish/subscribe channel that we use to notify form-builder
   // nodes about both remote and local patches.
@@ -152,6 +155,14 @@ export const FormView = forwardRef<HTMLDivElement, FormViewProps>(function FormV
     >
       <PresenceOverlay margins={margins}>
         <Box as="form" onSubmit={preventDefault} ref={setRef}>
+          {/* This is the document title */}
+          <Box marginBottom={5}>
+            <ElementQuery>
+              <Title forwardedAs="h2" muted={!title}>
+                {title ?? 'Untitled'}
+              </Title>
+            </ElementQuery>
+          </Box>
           {formState === null ? (
             <Box padding={2}>
               <Text>This form is hidden</Text>
