c36e77f898748fb307d4ed7b9979fd606d76b28f
Robin Pyon <robin.pyon@gmail.com>
2023-11-14 22:13:22 +0000

    feat: create separate form header component, render document type

--- packages/sanity/src/desk/panes/document/documentPanel/documentViews/styles.ts
+++ packages/sanity/src/desk/panes/document/documentPanel/documentViews/styles.ts
@@ -1,40 +1,6 @@
 import {Box} from '@sanity/ui'
-import styled, {css} from 'styled-components'
+import styled from 'styled-components'
 
 export const Root = styled(Box)`
   position: relative;
-`
-
-// Use CSS container queries to conditionally render headings at different sizes.
-// Hide if container queries are not supported.
-export const TitleContainer = styled(Box)`
-  ${({theme}) => {
-    return css`
-      @supports not (container-type: inline-size) {
-        display: none !important;
-      }
-
-      container-type: inline-size;
-
-      [data-heading] {
-        font-size: ${theme.sanity.fonts.heading.sizes[2].fontSize}px;
-        line-height: ${theme.sanity.fonts.heading.sizes[2].lineHeight}px;
-        overflow-wrap: break-word;
-      }
-
-      @container (min-width: 420px) {
-        [data-heading] {
-          font-size: ${theme.sanity.fonts.heading.sizes[3].fontSize}px;
-          line-height: ${theme.sanity.fonts.heading.sizes[3].lineHeight}px;
-        }
-      }
-
-      @container (min-width: 560px) {
-        [data-heading] {
-          font-size: ${theme.sanity.fonts.heading.sizes[4].fontSize}px;
-          line-height: ${theme.sanity.fonts.heading.sizes[4].lineHeight}px;
-        }
-      }
-    `
-  }}
 `
