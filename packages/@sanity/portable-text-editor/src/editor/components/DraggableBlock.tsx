25abdad0465c7c8097105115f328f6a83642e7e4
Per-Kristian Nordnes <per.kristian.nordnes@gmail.com>
2023-11-16 15:37:08 +0100

    feat(portable-text-editor): make it possible to drag and drop regular text blocks

--- packages/@sanity/portable-text-editor/src/editor/components/DraggableBlock.tsx
+++ packages/@sanity/portable-text-editor/src/editor/components/DraggableBlock.tsx
@@ -40,7 +40,6 @@ export const DraggableBlock = ({children, element, readOnly, blockRef}: Draggabl
   const editor = useSlateStatic()
   const dragGhostRef: MutableRefObject<undefined | HTMLElement> = useRef()
   const [isDragOver, setIsDragOver] = useState(false)
-  const isVoid = useMemo(() => Editor.isVoid(editor, element), [editor, element])
   const isInline = useMemo(() => Editor.isInline(editor, element), [editor, element])
 
   const [blockElement, setBlockElement] = useState<HTMLElement | null>(null)
@@ -164,10 +163,6 @@ export const DraggableBlock = ({children, element, readOnly, blockRef}: Draggabl
   // Note: this is called for the dragging block
   const handleDrag = useCallback(
     (event: DragEvent) => {
-      if (!isVoid) {
-        IS_DRAGGING_BLOCK_ELEMENT.delete(editor)
-        return
-      }
       IS_DRAGGING.set(editor, true)
       IS_DRAGGING_BLOCK_ELEMENT.set(editor, element)
       event.stopPropagation() // Stop propagation so that leafs don't get this and take focus/selection!
@@ -178,13 +173,13 @@ export const DraggableBlock = ({children, element, readOnly, blockRef}: Draggabl
         target.style.opacity = '1'
       }
     },
-    [editor, element, isVoid],
+    [editor, element],
   )
 
   // Note: this is called for the dragging block
   const handleDragStart = useCallback(
     (event: DragEvent) => {
-      if (!isVoid || isInline) {
+      if (isInline) {
         debug('Not dragging block')
         IS_DRAGGING_BLOCK_ELEMENT.delete(editor)
         IS_DRAGGING.set(editor, false)
@@ -225,7 +220,7 @@ export const DraggableBlock = ({children, element, readOnly, blockRef}: Draggabl
       }
       handleDrag(event)
     },
-    [blockElement, editor, handleDrag, isInline, isVoid],
+    [blockElement, editor, handleDrag, isInline],
   )
 
   const isDraggingOverFirstBlock =
@@ -271,7 +266,7 @@ export const DraggableBlock = ({children, element, readOnly, blockRef}: Draggabl
 
   return (
     <div
-      draggable={isVoid}
+      draggable
       onDragStart={handleDragStart}
       onDrag={handleDrag}
       onDragOver={handleDragOver}
