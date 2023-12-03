0b1b62fa596b0929089386efbe137b1163c17867
Robin Pyon <robin.pyon@gmail.com>
2023-12-03 19:18:52 +0000

    fix: localize create new button tooltip in document pane headers

--- packages/sanity/src/desk/components/paneHeaderActions/PaneHeaderCreateButton.tsx
+++ packages/sanity/src/desk/components/paneHeaderActions/PaneHeaderCreateButton.tsx
@@ -116,7 +116,7 @@ export function PaneHeaderCreateButton({templateItems}: PaneHeaderCreateButtonPr
           mode="bleed"
           disabled={disabled}
           data-testid="action-intent-button"
-          tooltipProps={disabled ? null : {content: 'Create new document'}}
+          tooltipProps={disabled ? null : {content: t('pane-header.create-new-button.tooltip')}}
         />
       </InsufficientPermissionsMessageTooltip>
     )
@@ -129,8 +129,7 @@ export function PaneHeaderCreateButton({templateItems}: PaneHeaderCreateButtonPr
           icon={AddIcon}
           mode="bleed"
           data-testid="multi-action-intent-button"
-          // @todo: localize
-          tooltipProps={{content: 'Create new document'}}
+          tooltipProps={{content: t('pane-header.create-new-button.tooltip')}}
         />
       }
       id="create-menu"
