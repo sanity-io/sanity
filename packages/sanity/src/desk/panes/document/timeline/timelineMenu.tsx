8a9929798bf676ad75987ee00915e4089c810870
Espen Hovlandsdal <espen@hovlandsdal.com>
2023-12-04 17:58:38 -0800

    fix(i18n): include timestamp in timeline operation revision label

--- packages/sanity/src/desk/panes/document/timeline/timelineMenu.tsx
+++ packages/sanity/src/desk/panes/document/timeline/timelineMenu.tsx
@@ -133,16 +133,22 @@ export function TimelineMenu({chunk, mode, placement}: TimelineMenuProps) {
     </>
   )
 
+  const formatParams = {
+    timestamp: {dateStyle: 'medium', timeStyle: 'short'},
+  }
+
   const revLabel = chunk
-    ? t(TIMELINE_ITEM_I18N_KEY_MAPPING[chunk.type])
+    ? t(TIMELINE_ITEM_I18N_KEY_MAPPING[chunk.type], {
+        context: 'timestamp',
+        timestamp: new Date(chunk?.endTimestamp),
+        formatParams,
+      })
     : t('timeline.latest-version')
 
   const sinceLabel = chunk
     ? t('timeline.since', {
         timestamp: new Date(chunk?.endTimestamp),
-        formatParams: {
-          timestamp: {dateStyle: 'medium', timeStyle: 'short'},
-        },
+        formatParams,
       })
     : t('timeline.since-version-missing')
 
