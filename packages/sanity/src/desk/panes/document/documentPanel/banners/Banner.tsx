c106d1eebd1d2074f862b744a62098a6a6fe6e37
Robin Pyon <robin.pyon@gmail.com>
2023-12-07 02:08:48 +0000

    feat: DRY document banners and update styles, create dedicated spacer button

---
+++ packages/sanity/src/desk/panes/document/documentPanel/banners/Banner.tsx
@@ -0,0 +1,49 @@
+import {ButtonTone, Card, CardTone, Container, Flex, Text} from '@sanity/ui'
+import React from 'react'
+import {Button} from '../../../../../ui'
+import {SpacerButton} from '../../../../../ui/spacerButton'
+
+interface BannerProps {
+  action?: {
+    as?: React.ElementType | keyof JSX.IntrinsicElements
+    icon?: React.ComponentType
+    onClick?: () => void
+    text: string
+    tone?: ButtonTone
+  }
+  content: React.ReactNode
+  icon?: React.ComponentType
+  tone?: CardTone
+}
+
+export function Banner(props: BannerProps) {
+  const {action, content, icon: Icon, tone = 'transparent', ...rest} = props
+
+  return (
+    <Card borderBottom paddingX={4} paddingY={2} tone={tone} {...rest}>
+      <Flex align="center" gap={3}>
+        {Icon && (
+          <Text size={0}>
+            <Icon />
+          </Text>
+        )}
+
+        <Flex align="center" flex={1} gap={2} paddingY={3}>
+          {content}
+        </Flex>
+
+        <SpacerButton />
+
+        {action && (
+          <Button
+            as={action?.as}
+            mode="ghost"
+            onClick={action?.onClick}
+            text={action.text}
+            tone={action.tone || 'default'}
+          />
+        )}
+      </Flex>
+    </Card>
+  )
+}
