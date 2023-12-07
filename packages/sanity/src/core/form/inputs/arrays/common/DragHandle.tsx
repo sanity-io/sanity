4c6d56eca05986622ae27e2d3f95a36449bdb508
Robin Pyon <robin.pyon@gmail.com>
2023-12-07 17:31:14 +0000

    fix: localize drag handle button, use correct delay values

--- packages/sanity/src/core/form/inputs/arrays/common/DragHandle.tsx
+++ packages/sanity/src/core/form/inputs/arrays/common/DragHandle.tsx
@@ -3,6 +3,7 @@ import React, {useContext} from 'react'
 import {DragHandleIcon} from '@sanity/icons'
 import {useSortable} from '@dnd-kit/sortable'
 import {Button, ButtonProps} from '../../../../../ui'
+import {useTranslation} from '../../../../i18n'
 
 const DragHandleButton = styled(Button)<{grid?: boolean}>`
   cursor: ${(props) => (props.grid ? 'move' : 'ns-resize')};
@@ -19,12 +20,16 @@ interface DragHandleProps {
 export const DragHandle = function DragHandle(props: DragHandleProps) {
   const id = useContext(SortableItemIdContext)!
   const {listeners, attributes} = useSortable({id})
+  const {t} = useTranslation()
   const {mode = 'bleed', ...rest} = props
 
   return (
     <DragHandleButton
       icon={DragHandleIcon}
-      tooltipProps={{content: 'Drag to reorder', delay: 1000}}
+      tooltipProps={{
+        content: t('inputs.array.action.drag.tooltip'),
+        delay: {open: 1000},
+      }}
       mode={mode}
       data-ui="DragHandleButton"
       {...attributes}
