bac82dac5bbb8bab333953b5cc6ef999bb595205
Robin Pyon <robin.pyon@gmail.com>
2023-12-07 00:46:19 +0000

    feat: update font styles for no search results + insufficient permissions messages

--- packages/sanity/src/core/studio/components/navbar/search/components/NoResults.tsx
+++ packages/sanity/src/core/studio/components/navbar/search/components/NoResults.tsx
@@ -8,7 +8,7 @@ export function NoResults() {
   return (
     <Container width={0}>
       <Stack aria-live="assertive" space={4} paddingX={4} paddingY={5}>
-        <Text align="center" muted weight="medium">
+        <Text align="center" muted size={1} weight="medium">
           {t('search.no-results-title')}
         </Text>
         <Text align="center" muted size={1}>
