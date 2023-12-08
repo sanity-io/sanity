00b140fd8b343147ffc9efb7fcb5ac5449669880
Herman Wikner <wiknerherman@gmail.com>
2023-12-08 11:09:20 +0100

    feat(core): introduce `document.components.layout`

---
+++ packages/sanity/src/core/config/form/types.ts
@@ -0,0 +1,16 @@
+import {ComponentType} from 'react'
+import {PreviewProps} from '../../components'
+import {InputProps, FieldProps, ItemProps, BlockProps, BlockAnnotationProps} from '../../form'
+
+/**
+ * @hidden
+ * @beta */
+export interface FormComponents {
+  annotation?: ComponentType<BlockAnnotationProps>
+  block?: ComponentType<BlockProps>
+  field?: ComponentType<FieldProps>
+  inlineBlock?: ComponentType<BlockProps>
+  input?: ComponentType<InputProps>
+  item?: ComponentType<ItemProps>
+  preview?: ComponentType<PreviewProps>
+}
