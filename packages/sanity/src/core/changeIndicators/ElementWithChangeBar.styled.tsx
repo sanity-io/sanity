0935cd58fa2b5c53c17bd53a0fba027597825a89
Marius Lundgård <studio@mariuslundgard.com>
2023-12-13 10:31:37 +0100

    fix(studio): update change indicator

--- packages/sanity/src/core/changeIndicators/ElementWithChangeBar.styled.tsx
+++ packages/sanity/src/core/changeIndicators/ElementWithChangeBar.styled.tsx
@@ -1,13 +1,12 @@
-import styled, {css} from 'styled-components'
-import {Theme} from '@sanity/ui'
+/* eslint-disable camelcase */
 
-interface ThemeContext {
-  theme: Theme
-}
+import styled, {css} from 'styled-components'
+import {getTheme_v2} from '@sanity/ui/theme'
 
 interface RootProps {
   $changed?: boolean
   $disabled?: boolean
+  $hasFocus?: boolean
   $isReviewChangeOpen: boolean
   $withHoverEffect?: boolean
 }
@@ -15,30 +14,45 @@ interface RootProps {
 const animationSpeed = 250
 
 export const ChangeBarWrapper = styled.div<RootProps>(
-  ({$changed, $disabled, $isReviewChangeOpen}) => {
+  ({$changed, $disabled, $hasFocus, $isReviewChangeOpen}) => {
     if ($disabled)
       return css`
-        ${ChangeBar} {
+        ${ChangeBarMarker}:after {
           display: none;
         }
       `
 
     return css`
-      --change-bar-offset: 2px;
+      --change-bar-offset: 4px;
 
       display: flex;
       position: relative;
 
+      ${ChangeBarMarker}:after {
+        opacity: 0.5;
+      }
+
       @media (hover: hover) {
         &:hover {
           z-index: 10;
+          ${ChangeBarMarker}:after {
+            opacity: 1;
+          }
         }
       }
 
       /* hide when field is not changed */
+      ${$hasFocus &&
+      css`
+        ${ChangeBarMarker}:after {
+          opacity: 1;
+        }
+      `}
+
+      /* hide when field is not changed */
       ${!$changed &&
       css`
-        ${ChangeBar} {
+        ${ChangeBarMarker}:after {
           opacity: 0;
           pointer-events: none;
         }
@@ -67,77 +81,71 @@ export const ChangeBar = styled.div<{$zIndex: number}>`
   z-index: ${({$zIndex}) => $zIndex};
 `
 
-export const ChangeBarMarker = styled.div(({theme}: ThemeContext) => {
-  /* these colours aren't freely available on the current theme */
-  const notSelectedColor = theme.sanity.color.spot.yellow
-  const screenMedium = theme.sanity.media[0]
+export const ChangeBarMarker = styled.div((props) => {
+  const {media} = getTheme_v2(props.theme)
 
   return css`
     position: absolute;
-    top: 0;
+    top: -1px;
     left: var(--change-bar-offset);
-    width: 2px;
-    bottom: 0;
-    background-color: ${notSelectedColor};
-    border-radius: 1px;
+    width: 1px;
+    bottom: -1px;
+    background-color: var(--card-bg-color);
 
-    @media (min-width: ${screenMedium}px) {
+    @media (min-width: ${media[0]}px) {
       display: unset;
+    }
+
+    &:after {
+      content: '';
+      display: block;
+      position: absolute;
+      top: 1px;
+      left: 0;
+      width: 1px;
+      bottom: 1px;
+      background-color: var(--card-badge-caution-dot-color);
+      border-radius: 0.5px;
     }
   `
 })
 
-export const ChangeBarButton = styled.button<{$withHoverEffect?: boolean}>(
-  ({theme, $withHoverEffect}) => {
-    /* these colours aren't freely available on the current theme */
-    const notSelectedColor = theme.sanity.color.spot.yellow
+export const ChangeBarButton = styled.button<{$withHoverEffect?: boolean}>((props) => {
+  const {$withHoverEffect} = props
 
-    return css`
-      appearance: none;
+  return css`
+    appearance: none;
+    border: 0;
+    outline: 0;
+    display: block;
+    padding: 0;
+    background: transparent;
+    opacity: 0;
+    position: absolute;
+    height: 100%;
+    cursor: pointer;
+    pointer-events: all;
+    left: calc(-0.25rem + var(--change-bar-offset));
+    width: calc(1rem - 1px);
+    transition: opacity ${animationSpeed}ms;
+
+    &:focus {
       border: 0;
       outline: 0;
-      display: block;
-      padding: 0;
-      background: transparent;
-      opacity: 0;
-      position: absolute;
-      height: 100%;
-      cursor: pointer;
-      pointer-events: all;
-      left: calc(-0.25rem + var(--change-bar-offset));
-      width: 1rem;
-      transition: opacity ${animationSpeed}ms;
+    }
 
-      &:focus {
-        border: 0;
-        outline: 0;
-      }
+    &:focus {
+      border: 0;
+      outline: 0;
+    }
 
-      &:after {
-        content: '';
-        width: 16px;
-        height: calc(100% + 14px);
-        display: block;
-        position: absolute;
-        top: -7px;
-        left: -3px;
-        border-radius: 8px;
-        background: ${notSelectedColor};
-      }
-
-      &:focus {
-        border: 0;
-        outline: 0;
-      }
-
-      ${$withHoverEffect &&
-      css`
-        @media (hover: hover) {
-          &:hover {
-            opacity: 0.2;
-          }
+    ${$withHoverEffect &&
+    css`
+      @media (hover: hover) {
+        &:hover {
+          opacity: 0.2;
         }
-      `}
-    `
-  },
-)
+      }
+    `}
+  `
+})
