b44f50f99fc6dbd27f0678bb7627a83047bd4783
Marius Lundgård <studio@mariuslundgard.com>
2023-12-13 10:31:37 +0100

    fix(studio): update change indicator

--- packages/sanity/src/core/changeIndicators/constants.ts
+++ packages/sanity/src/core/changeIndicators/constants.ts
@@ -10,7 +10,7 @@ export const ARROW_MARGIN_X = 8
 export const ARROW_MARGIN_Y = 2
 export const ARROW_SIZE = 4
 export const ARROW_THRESHOLD = 12
-export const STROKE_WIDTH = 2
+export const STROKE_WIDTH = 1
 
 export const DEBUG = false
 export const DEBUG_LAYER_BOUNDS = DEBUG || false
