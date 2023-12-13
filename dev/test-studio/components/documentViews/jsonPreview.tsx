7a68d4ee86648449e232daa1bee18fd09590b049
Marius Lundgård <studio@mariuslundgard.com>
2023-12-13 10:42:26 +0100

    test(test-studio): update config

--- dev/test-studio/components/documentViews/jsonPreview.tsx
+++ dev/test-studio/components/documentViews/jsonPreview.tsx
@@ -1,10 +1,14 @@
-import {Card, Code} from '@sanity/ui'
+import {Box, Card, Code} from '@sanity/ui'
 import React from 'react'
 
 export function JSONPreviewDocumentView(props: any) {
   return (
-    <Card padding={4} sizing="border" style={{minHeight: '100%'}} tone="transparent">
-      <Code language="json">{JSON.stringify(props.document.displayed, null, 2)}</Code>
+    <Card overflow="auto" style={{minHeight: '100%'}} tone="transparent">
+      <Box padding={4}>
+        <Code language="json" size={1}>
+          {JSON.stringify(props.document.displayed, null, 2)}
+        </Code>
+      </Box>
     </Card>
   )
 }
