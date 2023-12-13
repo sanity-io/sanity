2083fd31d2ea0e32fd5047ab4c012604d58faa7c
Nina Andal Aarvik <nina@sanity.io>
2023-12-13 12:00:09 +0200

    fix(core): add timestamps to all preview components (#5352)
    * fix(core): add timestamps to all preview components
    
    * fix(core): add comments

--- packages/sanity/src/core/preview/utils/getPreviewPaths.ts
+++ packages/sanity/src/core/preview/utils/getPreviewPaths.ts
@@ -1,11 +1,18 @@
 import {SchemaType} from '@sanity/types'
 import {PreviewPath} from '../types'
 
+const DEFAULT_PREVIEW_PATHS: PreviewPath[] = [['_createdAt'], ['_updatedAt']]
+
 /** @internal */
 export function getPreviewPaths(preview: SchemaType['preview']): PreviewPath[] | undefined {
   const selection = preview?.select
 
   if (!selection) return undefined
 
-  return Object.values(selection).map((value) => String(value).split('.'))
+  // Transform the selection dot-notation paths into array paths.
+  // Example: ['object.title', 'name'] => [['object', 'title'], ['name']]
+  const paths = Object.keys(selection).map((value) => String(value).split('.')) || []
+
+  // Return the paths with the default preview paths appended.
+  return paths.concat(DEFAULT_PREVIEW_PATHS)
 }
