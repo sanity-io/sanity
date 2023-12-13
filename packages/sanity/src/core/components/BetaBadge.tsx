9251f5ff560331d16cab9f8ce0624e7e8c1959be
Marius Lundgård <studio@mariuslundgard.com>
2023-12-13 10:32:07 +0100

    fix(studio): update beta badge

--- packages/sanity/src/core/components/BetaBadge.tsx
+++ packages/sanity/src/core/components/BetaBadge.tsx
@@ -1,31 +1,16 @@
-import React, {PropsWithChildren} from 'react'
-import {hues} from '@sanity/color'
+import {HTMLProps} from 'react'
 import {Badge, BadgeProps} from '@sanity/ui'
-import styled, {css} from 'styled-components'
-
-const StyledBadge = styled(Badge)(({theme}) => {
-  const fgTint = theme.sanity.color.dark ? 50 : 700
-  const bgTint = theme.sanity.color.dark ? 700 : 100
-  const bg = hues.purple[bgTint].hex
-  const fg = hues.purple[fgTint].hex
-
-  return css`
-    background-color: ${bg};
-    box-shadow: inset 0 0 0 1px ${bg};
-    color: ${fg};
-  `
-})
 
 /** @internal */
 export type BetaBadgeProps = Omit<BadgeProps, 'mode' | 'tone'>
 
 /** @internal */
-export function BetaBadge(props: PropsWithChildren<BetaBadgeProps>) {
+export function BetaBadge(props: BetaBadgeProps & Omit<HTMLProps<HTMLDivElement>, 'ref'>) {
   const {fontSize = 1, children = 'Beta', ...rest} = props
 
   return (
-    <StyledBadge {...rest} fontSize={fontSize}>
+    <Badge {...rest} fontSize={fontSize} radius={2} tone="primary">
       {children}
-    </StyledBadge>
+    </Badge>
   )
 }
