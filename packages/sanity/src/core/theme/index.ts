4032151a9c417fb13c018e977fefdf7bd8a1e4ee
Marius Lundgård <studio@mariuslundgard.com>
2023-12-14 12:24:40 +0100

    fix(studio): update default studio theme

--- packages/sanity/src/core/theme/index.ts
+++ packages/sanity/src/core/theme/index.ts
@@ -1,11 +1,10 @@
-import {studioTheme} from '@sanity/ui'
+import {buildTheme} from '@sanity/ui/theme'
 import {StudioTheme} from './types'
 
 export * from './_legacy'
 export * from './types'
 
 /** @internal */
-export const defaultTheme: StudioTheme = {
-  ...studioTheme,
-  focusRing: {offset: -1, width: 2},
-}
+export const defaultTheme: StudioTheme = buildTheme({
+  container: [320, 640, 960, 1280, 1600, 1920],
+})
