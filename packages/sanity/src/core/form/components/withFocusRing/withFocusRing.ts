74f1a23f6d70e2fabd83676e47d991184a2be4da
Marius Lundgård <studio@mariuslundgard.com>
2023-12-14 10:52:28 +0100

    refactor(form): use theme v2

--- packages/sanity/src/core/form/components/withFocusRing/withFocusRing.ts
+++ packages/sanity/src/core/form/components/withFocusRing/withFocusRing.ts
@@ -1,29 +1,35 @@
+/* eslint-disable camelcase */
+
 import styled, {css} from 'styled-components'
 import {rem, Theme} from '@sanity/ui'
 import {ComponentType} from 'react'
+import {getTheme_v2} from '@sanity/ui/theme'
 import {focusRingBorderStyle, focusRingStyle} from './helpers'
 
 export function withFocusRing<Props>(component: ComponentType<Props>) {
   return styled(component)<Props & {$border?: boolean}>(
     (props: {theme: Theme; $border?: boolean}) => {
-      const border = {width: props.$border ? 1 : 0, color: 'var(--card-border-color)'}
+      const {$border} = props
+      const {card, color, radius} = getTheme_v2(props.theme)
+
+      const border = {width: $border ? 1 : 0, color: 'var(--card-border-color)'}
+
       return css`
         --card-focus-box-shadow: ${focusRingBorderStyle(border)};
 
-        border-radius: ${rem(props.theme.sanity.v2?.radius[1] ?? props.theme.sanity.radius[1])};
+        border-radius: ${rem(radius[1])};
         outline: none;
         box-shadow: var(--card-focus-box-shadow);
 
         &:focus {
           --card-focus-box-shadow: ${focusRingStyle({
             border,
-            base: {bg: props.theme.sanity.v2?.color.bg ?? props.theme.sanity.color.base.bg},
+            base: color,
             focusRing: {
+              ...card.focusRing,
               // An offset of 0 is needed to avoid the focus ring overlap the border of the inner items, the theme has an offset of -1
               // Detected in empty array items.
               offset: 0,
-              width:
-                props.theme.sanity.v2?.card.focusRing.width ?? props.theme.sanity.focusRing.width,
             },
           })};
         }
