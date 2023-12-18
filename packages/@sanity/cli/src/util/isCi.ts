0727413bb2c5ab3fca80f593bb55e528ada61669
Bjørge Næss <bjoerge@gmail.com>
2023-12-18 14:43:39 +0100

    fix(cli): parse trueish env vars when detecting ci environment

--- packages/@sanity/cli/src/util/isCi.ts
+++ packages/@sanity/cli/src/util/isCi.ts
@@ -1,5 +1,7 @@
 /* eslint-disable no-process-env */
+import {isTrueish} from './isTrueish'
+
 export const isCi =
-  process.env.CI || // Travis CI, CircleCI, Gitlab CI, Appveyor, CodeShip
-  process.env.CONTINUOUS_INTEGRATION || // Travis CI
+  isTrueish(process.env.CI) || // Travis CI, CircleCI, Gitlab CI, Appveyor, CodeShip
+  isTrueish(process.env.CONTINUOUS_INTEGRATION) || // Travis CI
   process.env.BUILD_NUMBER // Jenkins, TeamCity
