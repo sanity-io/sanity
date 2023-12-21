2cedcaf85699411b221ccddf5dfb5c4f71ee60a2
Binoy Patel <me@binoy.io>
2023-12-21 10:44:08 -0500

    chore(test): update cli tests to use staging env (#5404)
    * chore(test): update cli tests to use staging env
    
    * chore(test): update test user name
    
    * chore(tests): rename token env var name
    
    * chore(test): disable telemetry tests for now
    
    * chore(test): fix PR feedback
    
    * fixup! chore(test): disable telemetry tests for now
    
    * chore(test): fix env var
    
    * chore(test): re-enable telemetry tests
    
    ---------
    
    Co-authored-by: Bjørge Næss <bjoerge@gmail.com>

--- packages/@sanity/cli/test/__fixtures__/v2/script.js
+++ packages/@sanity/cli/test/__fixtures__/v2/script.js
@@ -3,7 +3,7 @@
 import client from 'part:@sanity/base/client'
 
 client
-  .withConfig({apiVersion: '2022-09-09'})
+  .withConfig({apiVersion: '2022-09-09', apiHost: 'https://api.sanity.work'})
   .users.getById('me')
   .then(
     (user) => console.log(JSON.stringify({user, env: process.env}, null, 2)),
