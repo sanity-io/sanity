749444d19b3908c89a160760e131349a38f6d8ed
Espen Hovlandsdal <espen@hovlandsdal.com>
2024-01-02 12:46:10 -0800

    feat(cli): add desk rename codemod

--- packages/@sanity/cli/src/actions/codemod/mods/index.ts
+++ packages/@sanity/cli/src/actions/codemod/mods/index.ts
@@ -1,7 +1,9 @@
 import {partsTypeDirective} from './partsTypeDirective'
 import {reactIconsV3} from './reactIconsV3'
+import {deskRename} from './deskRename'
 
 export default {
   reactIconsV3,
   partsTypeDirective,
+  deskRename,
 }
