7ecef287b9f6b14bc82541ebed2c00b6d9ea1715
Pedro Bonamin <pedrobonamin@gmail.com>
2024-01-03 15:46:22 +0100

    fix(theme): update studio theme,  support only color and fonts customizations

--- packages/sanity/src/core/studio/StudioThemeProvider.tsx
+++ packages/sanity/src/core/studio/StudioThemeProvider.tsx
@@ -2,11 +2,25 @@
 
 import React from 'react'
 import {ThemeProvider, LayerProvider} from '@sanity/ui'
+import {RootTheme} from '@sanity/ui/theme'
+import {StudioTheme, defaultTheme} from '../theme'
+import {usesLegacyThemerFonts} from '../theme/themer/themerFonts'
 import {useActiveWorkspace} from './activeWorkspaceMatcher'
 import {ColorSchemeSetValueContext, ColorSchemeValueContext} from './colorScheme'
 
 interface StudioThemeProviderProps {
   children: React.ReactNode
+}
+
+function getThemeValues(theme: StudioTheme): RootTheme {
+  return {
+    ...defaultTheme,
+    v2: theme.v2,
+    fonts: usesLegacyThemerFonts(theme.fonts)
+      ? defaultTheme.fonts
+      : theme.fonts ?? defaultTheme.fonts,
+    color: theme.color ?? defaultTheme.color,
+  }
 }
 
 /** @internal */
@@ -18,7 +32,7 @@ export function StudioThemeProvider({children}: StudioThemeProviderProps) {
     return (
       <ColorSchemeSetValueContext.Provider value={false}>
         <ColorSchemeValueContext.Provider value={scheme}>
-          <ThemeProvider scheme={scheme} theme={theme}>
+          <ThemeProvider scheme={scheme} theme={getThemeValues(theme)}>
             <LayerProvider>{children}</LayerProvider>
           </ThemeProvider>
         </ColorSchemeValueContext.Provider>
@@ -27,7 +41,7 @@ export function StudioThemeProvider({children}: StudioThemeProviderProps) {
   }
 
   return (
-    <ThemeProvider theme={theme}>
+    <ThemeProvider theme={getThemeValues(theme)}>
       <LayerProvider>{children}</LayerProvider>
     </ThemeProvider>
   )
--- packages/sanity/src/core/theme/index.ts
+++ packages/sanity/src/core/theme/index.ts
@@ -5,6 +5,4 @@ export * from './_legacy'
 export * from './types'
 
 /** @internal */
-export const defaultTheme: StudioTheme = buildTheme({
-  container: [320, 640, 960, 1280, 1600, 1920],
-})
+export const defaultTheme: StudioTheme = buildTheme()
