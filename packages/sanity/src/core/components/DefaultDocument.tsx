47d84a2134a70829eaba59c853eab0403e390869
Nina Andal Aarvik <nina@sanity.io>
2024-01-05 11:32:08 +0100

    fix(core): remove variable fonts in global styles (#5456)

--- packages/sanity/src/core/components/DefaultDocument.tsx
+++ packages/sanity/src/core/components/DefaultDocument.tsx
@@ -6,20 +6,60 @@ import {Favicons} from './Favicons'
 
 const globalStyles = `
   @font-face {
-    font-family: 'Inter';
+    font-family: Inter;
     font-style: normal;
-    font-weight: 100 900;
+    font-weight: 400;
     font-display: swap;
-    src: url('https://studio-static.sanity.io/InterVariable.ttf');
-    font-named-instance: 'Regular';
+    src: url("https://studio-static.sanity.io/Inter-Regular.woff2") format("woff2");
   }
   @font-face {
-    font-family: 'Inter';
+    font-family: Inter;
     font-style: italic;
-    font-weight: 100 900;
+    font-weight: 400;
     font-display: swap;
-    src: url('https://studio-static.sanity.io/InterVariable-Italic.ttf');
-    font-named-instance: 'Italic';
+    src: url("https://studio-static.sanity.io/Inter-Italic.woff2") format("woff2");
+  }
+  @font-face {
+    font-family: Inter;
+    font-style: normal;
+    font-weight: 500;
+    font-display: swap;
+    src: url("https://studio-static.sanity.io/Inter-Medium.woff2") format("woff2");
+  }
+  @font-face {
+    font-family: Inter;
+    font-style: italic;
+    font-weight: 500;
+    font-display: swap;
+    src: url("https://studio-static.sanity.io/Inter-MediumItalic.woff2") format("woff2");
+  }
+  @font-face {
+    font-family: Inter;
+    font-style: normal;
+    font-weight: 600;
+    font-display: swap;
+    src: url("https://studio-static.sanity.io/Inter-SemiBold.woff2") format("woff2");
+  }
+  @font-face {
+    font-family: Inter;
+    font-style: italic;
+    font-weight: 600;
+    font-display: swap;
+    src: url("https://studio-static.sanity.io/Inter-SemiBoldItalic.woff2") format("woff2");
+  }
+  @font-face {
+    font-family: Inter;
+    font-style: normal;
+    font-weight: 700;
+    font-display: swap;
+    src: url("https://studio-static.sanity.io/Inter-Bold.woff2") format("woff2");
+  }
+  @font-face {
+    font-family: Inter;
+    font-style: italic;
+    font-weight: 700;
+    font-display: swap;
+    src: url("https://studio-static.sanity.io/Inter-BoldItalic.woff2") format("woff2");
   }
   html {
     background-color: #f1f3f6;
