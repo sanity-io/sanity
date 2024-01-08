68e6eda9c8c55b40146c63d03854081138d7dca7
Herman Wikner <wiknerherman@gmail.com>
2024-01-08 12:20:14 +0100

    feat(comments): add reaction components

--- packages/sanity/src/structure/comments/src/components/list/CommentsListItemLayout.tsx
+++ packages/sanity/src/structure/comments/src/components/list/CommentsListItemLayout.tsx
@@ -10,32 +10,24 @@ import {
   CommentDocument,
   CommentEditPayload,
   CommentMessage,
+  CommentReactionOption,
   CommentStatus,
   MentionOptionsHookValue,
 } from '../../types'
 import {FLEX_GAP} from '../constants'
 import {hasCommentMessageValue, useCommentHasChanged} from '../../helpers'
 import {AVATAR_HEIGHT, CommentsAvatar, SpacerAvatar} from '../avatars'
+import {CommentReactionsBar} from '../reactions'
 import {CommentsListItemContextMenu} from './CommentsListItemContextMenu'
 import {TimeAgoOpts, useTimeAgo, useUser, useDidUpdate} from 'sanity'
 
+const stopPropagation = (e: React.MouseEvent<HTMLDivElement>) => e.stopPropagation()
+
 const ContextMenuBox = styled(Box)``
 
-function StopPropagationWrap(props: React.PropsWithChildren<{isParent?: boolean}>) {
-  const {children, isParent} = props
-
-  const handleClick = useCallback((event: React.MouseEvent<HTMLDivElement>) => {
-    event.stopPropagation()
-  }, [])
-
-  return (
-    <ContextMenuBox data-root-menu={isParent ? 'true' : 'false'} onClick={handleClick}>
-      {children}
-    </ContextMenuBox>
-  )
-}
-
 const SKELETON_INLINE_STYLE: React.CSSProperties = {width: '50%'}
+
+const EMPTY_ARRAY: [] = []
 
 const TimeText = styled(Text)(({theme}) => {
   const isDark = theme.sanity.color.dark
@@ -120,6 +112,7 @@ interface CommentsListItemLayoutProps {
   onDelete: (id: string) => void
   onEdit: (id: string, message: CommentEditPayload) => void
   onInputKeyDown?: (event: React.KeyboardEvent<Element>) => void
+  onReactionSelect?: (id: string, reaction: CommentReactionOption) => void
   onStatusChange?: (id: string, status: CommentStatus) => void
   readOnly?: boolean
 }
@@ -141,6 +134,7 @@ export function CommentsListItemLayout(props: CommentsListItemLayoutProps) {
     onDelete,
     onEdit,
     onInputKeyDown,
+    onReactionSelect,
     onStatusChange,
     readOnly,
   } = props
@@ -155,6 +149,20 @@ export function CommentsListItemLayout(props: CommentsListItemLayoutProps) {
 
   const hasChanges = useCommentHasChanged(value)
   const hasValue = useMemo(() => hasCommentMessageValue(value), [value])
+
+  // Filter out reactions that's been optimistically removed from the comment.
+  const reactions = useMemo(
+    () =>
+      (comment?.reactions?.filter((r) => r?._optimisticState !== 'removed') || EMPTY_ARRAY).filter(
+        (r) => {
+          // Filter out reactions that might have to incorrect format
+          return 'userId' in r && 'shortName' in r
+        },
+      ),
+    [comment?.reactions],
+  )
+
+  const hasReactions = Boolean(reactions?.length)
 
   const commentInputRef = useRef<CommentInputHandle>(null)
 
@@ -217,6 +225,13 @@ export function CommentsListItemLayout(props: CommentsListItemLayoutProps) {
     cancelEdit()
   }, [cancelEdit])
 
+  const handleReactionSelect = useCallback(
+    (reaction: CommentReactionOption) => {
+      onReactionSelect?.(_id, reaction)
+    },
+    [_id, onReactionSelect],
+  )
+
   const handleEditSubmit = useCallback(() => {
     onEdit(_id, {message: value})
     setIsEditing(false)
@@ -250,6 +265,10 @@ export function CommentsListItemLayout(props: CommentsListItemLayoutProps) {
   useDidUpdate(isEditing, handleCloseMenu)
 
   useClickOutside(handleClickOutside, [rootElement])
+
+  const currentUserReactionsNames = useMemo(() => {
+    return reactions.filter((r) => r.userId === currentUser?.id).map((r) => r.shortName)
+  }, [currentUser?.id, reactions])
 
   const name = user?.displayName ? (
     <Text size={1} weight="medium" textOverflow="ellipsis" title={user.displayName}>
@@ -291,21 +310,23 @@ export function CommentsListItemLayout(props: CommentsListItemLayoutProps) {
           </Flex>
 
           {!isEditing && !displayError && (
-            <StopPropagationWrap isParent={isParent}>
+            <ContextMenuBox data-root-menu={isParent ? 'true' : 'false'} onClick={stopPropagation}>
               <CommentsListItemContextMenu
                 canDelete={canDelete}
                 canEdit={canEdit}
+                currentUserReactionsNames={currentUserReactionsNames}
                 isParent={isParent}
                 onCopyLink={handleCopyLink}
                 onDeleteStart={handleDelete}
                 onEditStart={toggleEdit}
                 onMenuClose={handleMenuClose}
                 onMenuOpen={handleMenuOpen}
+                onReactionSelect={handleReactionSelect}
                 onStatusChange={handleOpenStatusChange}
                 readOnly={readOnly}
                 status={comment.status}
               />
-            </StopPropagationWrap>
+            </ContextMenuBox>
           )}
         </Flex>
 
@@ -337,6 +358,20 @@ export function CommentsListItemLayout(props: CommentsListItemLayoutProps) {
             <SpacerAvatar />
 
             <CommentMessageSerializer blocks={message} />
+          </Flex>
+        )}
+
+        {hasReactions && (
+          <Flex gap={FLEX_GAP} marginTop={2}>
+            <SpacerAvatar />
+
+            <Box onClick={stopPropagation}>
+              <CommentReactionsBar
+                currentUser={currentUser}
+                onSelect={handleReactionSelect}
+                reactions={reactions}
+              />
+            </Box>
           </Flex>
         )}
       </InnerStack>
---
+++ packages/sanity/src/structure/comments/src/components/reactions/CommentReactionsMenuButton.tsx
@@ -0,0 +1,111 @@
+import React, {cloneElement, useCallback, useMemo, useState} from 'react'
+import {Card, useClickOutside} from '@sanity/ui'
+import {CommentReactionOption, CommentReactionShortNames} from '../../types'
+import {Popover, PopoverProps} from '../../../../../ui-components'
+import {CommentReactionsMenu} from './CommentReactionsMenu'
+
+const POPOVER_FALLBACK_PLACEMENTS: PopoverProps['fallbackPlacements'] = ['top', 'bottom']
+
+export interface CommentReactionsMenuButtonProps {
+  onMenuClose?: () => void
+  onMenuOpen?: () => void
+  onSelect: (option: CommentReactionOption) => void
+  options: CommentReactionOption[]
+  renderMenuButton: (props: {open: boolean}) => React.ReactElement
+  selectedOptionNames: CommentReactionShortNames[]
+}
+
+export function CommentReactionsMenuButton(props: CommentReactionsMenuButtonProps) {
+  const {options, onSelect, selectedOptionNames, onMenuClose, onMenuOpen, renderMenuButton} = props
+  const [buttonElement, setButtonElement] = useState<HTMLButtonElement | null>(null)
+  const [popoverElement, setPopoverElement] = useState<HTMLDivElement | null>(null)
+
+  const [open, setOpen] = useState<boolean>(false)
+
+  const handleClick = useCallback(() => {
+    const next = !open
+    setOpen(next)
+
+    if (next) {
+      onMenuOpen?.()
+    } else {
+      onMenuClose?.()
+    }
+  }, [onMenuClose, onMenuOpen, open])
+
+  const handleClose = useCallback(() => {
+    if (!open) return
+
+    setOpen(false)
+    onMenuClose?.()
+    buttonElement?.focus()
+  }, [buttonElement, onMenuClose, open])
+
+  const handleClickOutside = useCallback(handleClose, [handleClose])
+
+  const handleKeyDown = useCallback(
+    (event: React.KeyboardEvent<HTMLDivElement>) => {
+      const {key, shiftKey} = event
+
+      if ((shiftKey && key === 'Tab') || key === 'Escape' || key === 'Tab') {
+        handleClose()
+      }
+    },
+    [handleClose],
+  )
+
+  useClickOutside(handleClickOutside, [popoverElement, buttonElement])
+
+  const handleSelect = useCallback(
+    (option: CommentReactionOption) => {
+      onSelect(option)
+      handleClose()
+    },
+    [handleClose, onSelect],
+  )
+
+  const button = useMemo(() => {
+    // Get the button element from the renderMenuButton function.
+    const btn = renderMenuButton({open})
+
+    // Clone the button element and add the necessary props.
+    return cloneElement(btn, {
+      'aria-expanded': open,
+      'aria-haspopup': 'true',
+      id: 'reactions-menu-button',
+      onClick: handleClick,
+      ref: setButtonElement,
+    })
+  }, [handleClick, open, renderMenuButton])
+
+  const popoverContent = (
+    <Card
+      aria-labelledby="reactions-menu-button"
+      onKeyDown={handleKeyDown}
+      padding={1}
+      radius={3}
+      tone="default"
+    >
+      <CommentReactionsMenu
+        onSelect={handleSelect}
+        options={options}
+        selectedOptionNames={selectedOptionNames}
+      />
+    </Card>
+  )
+
+  return (
+    <Popover
+      constrainSize
+      content={popoverContent}
+      fallbackPlacements={POPOVER_FALLBACK_PLACEMENTS}
+      open={open}
+      placement="bottom"
+      portal
+      ref={setPopoverElement}
+      tone="default"
+    >
+      {button}
+    </Popover>
+  )
+}
