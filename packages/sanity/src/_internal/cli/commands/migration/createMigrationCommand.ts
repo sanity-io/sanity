31ebd74634461a4ac5522b8a7434a10c43952e61
Bjørge Næss <bjoerge@gmail.com>
2024-01-09 15:57:49 +0100

    feat(cli): scaffold create/run migration cli commands

---
+++ packages/sanity/src/_internal/cli/commands/migration/createMigrationCommand.ts
@@ -0,0 +1,32 @@
+import type {CliCommandDefinition} from '@sanity/cli'
+
+const helpText = `
+Options
+  --type <type> Type of migration (incremental/full)
+
+Examples
+  sanity migration create
+  sanity migration create <name>
+  sanity migration create <name> --type incremental
+`
+
+const allowedTypes = ['incremental', 'full']
+
+interface MigrateFlags {
+  type?: 'incremental' | 'full'
+}
+
+const createMigrationCommand: CliCommandDefinition<MigrateFlags> = {
+  name: 'create',
+  group: 'migration',
+  signature: '[NAME]',
+  helpText,
+  description: 'Create a new content migration within your project',
+  action: async (args, context) => {
+    const {output, prompt} = context
+    const flags = args.extOptions
+
+    output.print('scaffold %s migration', flags.type || 'incremental')
+  },
+}
+export default createMigrationCommand
---
+++ packages/sanity/src/_internal/cli/commands/migration/migrationGroup.ts
@@ -0,0 +1,6 @@
+export default {
+  name: 'migration',
+  signature: '[COMMAND]',
+  isGroupRoot: true,
+  description: 'Scaffold and run migrations against your project dataset',
+}
