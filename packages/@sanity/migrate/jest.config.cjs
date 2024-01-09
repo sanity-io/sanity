5f301a017d23ef742cb40362380d9c9e28e3e653
Bjørge Næss <bjoerge@gmail.com>
2024-01-09 14:43:11 +0100

    test(migrate): make sure uint8array-extras package gets transpiled from esm

--- packages/@sanity/migrate/jest.config.cjs
+++ packages/@sanity/migrate/jest.config.cjs
@@ -4,4 +4,6 @@ const {createJestConfig} = require('../../../test/config.cjs')
 
 module.exports = createJestConfig({
   displayName: require('./package.json').name,
+  testEnvironment: 'node',
+  transformIgnorePatterns: ['node_modules/(?!(uint8array-extras)/)'],
 })
