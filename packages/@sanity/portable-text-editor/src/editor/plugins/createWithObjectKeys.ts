7366c3be5b71b8c8c0c7ad803975b7e109349a3d
Per-Kristian Nordnes <per.kristian.nordnes@gmail.com>
2024-01-11 16:39:54 +0100

    refactor(portable-text-editor): preserve keys on undo/redo

--- packages/@sanity/portable-text-editor/src/editor/plugins/createWithObjectKeys.ts
+++ packages/@sanity/portable-text-editor/src/editor/plugins/createWithObjectKeys.ts
@@ -14,11 +14,18 @@ export function createWithObjectKeys(
   return function withKeys(editor: PortableTextSlateEditor): PortableTextSlateEditor {
     PRESERVE_KEYS.set(editor, false)
     const {apply, normalizeNode} = editor
+
+    // The apply function can be called with a scope (withPreserveKeys) that will
+    // preserve keys for the produced nodes if they have a _key property set already.
+    // The default behavior is to always generate a new key here.
+    // For example, when undoing and redoing we want to retain the keys, but
+    // when we create a new bold span by splitting a non-bold-span we want the produced node to get a new key.
     editor.apply = (operation) => {
       if (operation.type === 'split_node') {
+        const withNewKey = !isPreservingKeys(editor) || !('_key' in operation.properties)
         operation.properties = {
           ...operation.properties,
-          _key: keyGenerator(),
+          ...(withNewKey ? {_key: keyGenerator()} : {}),
         }
       }
       if (operation.type === 'insert_node') {
