8c0f8a35fd0a52cd73b3f5f4a918665f6a575f0d
Ash <ash@sanity.io>
2024-01-19 15:17:50 +0000

    test(migrate): add tests for `parseJSON`

---
+++ packages/@sanity/migrate/src/it-utils/__test__/json.test.ts
@@ -0,0 +1,34 @@
+import {parseJSON} from '../json'
+
+test('parse JSON', async () => {
+  const gen = async function* () {
+    yield '{"someString": "string"}'
+    yield '{"someNumber": 42}'
+  }
+
+  const it = parseJSON(gen(), {})
+
+  expect(await it.next()).toEqual({value: {someString: 'string'}, done: false})
+  expect(await it.next()).toEqual({value: {someNumber: 42}, done: false})
+  expect(await it.next()).toEqual({value: undefined, done: true})
+})
+
+test('parse JSON with interrupting error', async () => {
+  const gen = async function* () {
+    yield '{"someString": "string"}'
+    yield '{"someString": "str{"error":{"description":"Some error"}}'
+  }
+
+  const it = parseJSON(gen())
+
+  expect(await it.next()).toEqual({value: {someString: 'string'}, done: false})
+
+  await expect(async () => {
+    await it.next()
+  }).rejects.toThrowErrorMatchingInlineSnapshot(`
+"Error parsing JSON: Some error
+
+{\\"error\\":{\\"description\\":\\"Some error\\"}}
+"
+`)
+})
