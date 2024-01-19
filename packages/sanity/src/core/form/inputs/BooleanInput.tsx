aeef3831614e04a5a388764cafae4b9ebc9ba601
Binoy Patel <me@binoy.io>
2024-01-19 11:20:58 -0500

    fix(core): remove unnecessary fragment

--- packages/sanity/src/core/form/inputs/BooleanInput.tsx
+++ packages/sanity/src/core/form/inputs/BooleanInput.tsx
@@ -29,35 +29,33 @@ export function BooleanInput(props: BooleanInputProps) {
   const tone: CardTone | undefined = readOnly ? 'transparent' : undefined
 
   return (
-    <>
-      <Card border data-testid="boolean-input" radius={2} tone={tone}>
-        <Flex>
-          <ZeroLineHeightBox padding={3}>
-            <LayoutSpecificInput
-              label={schemaType.title}
-              {...elementProps}
-              checked={checked}
-              disabled={readOnly}
-              indeterminate={indeterminate}
-              style={{margin: -4}}
-            />
-          </ZeroLineHeightBox>
-          <Box flex={1} paddingY={3}>
-            <FormFieldHeaderText
-              description={schemaType.description}
-              inputId={id}
-              validation={validation}
-              title={schemaType.title}
-              deprecated={schemaType.deprecated}
-            />
-          </Box>
-          <CenterAlignedBox paddingX={3} paddingY={1}>
-            <FormFieldStatus maxAvatars={1} position="top">
-              {/*<FieldPresence maxAvatars={1} presence={presence} />*/}
-            </FormFieldStatus>
-          </CenterAlignedBox>
-        </Flex>
-      </Card>
-    </>
+    <Card border data-testid="boolean-input" radius={2} tone={tone}>
+      <Flex>
+        <ZeroLineHeightBox padding={3}>
+          <LayoutSpecificInput
+            label={schemaType.title}
+            {...elementProps}
+            checked={checked}
+            disabled={readOnly}
+            indeterminate={indeterminate}
+            style={{margin: -4}}
+          />
+        </ZeroLineHeightBox>
+        <Box flex={1} paddingY={3}>
+          <FormFieldHeaderText
+            description={schemaType.description}
+            inputId={id}
+            validation={validation}
+            title={schemaType.title}
+            deprecated={schemaType.deprecated}
+          />
+        </Box>
+        <CenterAlignedBox paddingX={3} paddingY={1}>
+          <FormFieldStatus maxAvatars={1} position="top">
+            {/*<FieldPresence maxAvatars={1} presence={presence} />*/}
+          </FormFieldStatus>
+        </CenterAlignedBox>
+      </Flex>
+    </Card>
   )
 }
