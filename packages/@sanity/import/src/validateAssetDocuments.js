28200d77af4cf8e93469b01bea96edb450e67a00
Bjørge Næss <bjoerge@gmail.com>
2024-01-20 10:28:50 +0100

    chore: manually fix remaining non-autofixable eslint issues

--- packages/@sanity/import/src/validateAssetDocuments.js
+++ packages/@sanity/import/src/validateAssetDocuments.js
@@ -1,5 +1,5 @@
-const debug = require('debug')('sanity:import:asset-validation')
 const {generateHelpUrl} = require('@sanity/generate-help-url')
+const debug = require('debug')('sanity:import:asset-validation')
 const pMap = require('p-map')
 const urlExists = require('./util/urlExists')
 
