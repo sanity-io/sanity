852f87a4b13318041ba9b4ee1b83b60453c3d8c0
Robin Pyon <robin.pyon@gmail.com>
2024-01-22 16:40:30 +0000

    chore: initial pass of ui-component workshop stories

--- packages/sanity/src/ui-components/__workshop__/index.ts
+++ packages/sanity/src/ui-components/__workshop__/index.ts
@@ -6,9 +6,49 @@ export default defineScope({
   title: 'Studio UI',
   stories: [
     {
+      name: 'button',
+      title: 'Button',
+      component: lazy(() => import('./ButtonStory')),
+    },
+    {
+      name: 'dialog',
+      title: 'Dialog',
+      component: lazy(() => import('./DialogStory')),
+    },
+    {
+      name: 'menu-button',
+      title: 'MenuButton',
+      component: lazy(() => import('./MenuButtonStory')),
+    },
+    {
+      name: 'menu-group',
+      title: 'MenuGroup',
+      component: lazy(() => import('./MenuGroupStory')),
+    },
+    {
       name: 'menu-item',
       title: 'MenuItem',
       component: lazy(() => import('./MenuItemStory')),
+    },
+    {
+      name: 'popover',
+      title: 'Popover',
+      component: lazy(() => import('./PopoverStory')),
+    },
+    {
+      name: 'tab',
+      title: 'Tab',
+      component: lazy(() => import('./TabStory')),
+    },
+    {
+      name: 'tooltip',
+      title: 'Tooltip',
+      component: lazy(() => import('./TooltipStory')),
+    },
+    {
+      name: 'tooltip-delay-group-provider',
+      title: 'TooltipDelayGroupProvider',
+      component: lazy(() => import('./TooltipDelayGroupProviderStory')),
     },
   ],
 })
