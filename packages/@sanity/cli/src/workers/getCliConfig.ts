48be65e94d5ba2c38bf29caa7c762fa61bc8ff90
Bjørge Næss <bjoerge@gmail.com>
2024-01-22 02:36:25 +0100

    fix: run prettier

--- packages/@sanity/cli/src/workers/getCliConfig.ts
+++ packages/@sanity/cli/src/workers/getCliConfig.ts
@@ -5,11 +5,10 @@ import {getCliConfig} from '../util/getCliConfig'
 // We're communicating with a parent process through a message channel
 getCliConfig(workerData, {forked: false})
   .then((config) => parentPort?.postMessage({type: 'config', config}))
-  .catch(
-    (error) =>
-      parentPort?.postMessage({
-        type: 'error',
-        error: error instanceof Error ? error.stack : error,
-        errorType: error && (error.type || error.name),
-      }),
+  .catch((error) =>
+    parentPort?.postMessage({
+      type: 'error',
+      error: error instanceof Error ? error.stack : error,
+      errorType: error && (error.type || error.name),
+    }),
   )
