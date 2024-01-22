9711ca601088c0335845616b032bf6552cd362fd
Robin Pyon <robin.pyon@gmail.com>
2024-01-22 16:36:43 +0000

    refactor: remove size prop from studio UI tabs

--- packages/sanity/src/ui-components/tab/Tab.tsx
+++ packages/sanity/src/ui-components/tab/Tab.tsx
@@ -10,17 +10,7 @@ import React, {forwardRef} from 'react'
 export type TabProps = Pick<
   UITabProps,
   'aria-controls' | 'focused' | 'icon' | 'id' | 'label' | 'selected' | 'tone'
-> & {
-  size?: 'default' | 'small'
-}
-
-const SMALL_TAB_PROPS = {
-  padding: 2,
-}
-
-const DEFAULT_TAB_PROPS = {
-  padding: 3,
-}
+>
 
 /**
  * Customized Sanity UI <Tab> with limited layout options.
@@ -28,20 +18,8 @@ const DEFAULT_TAB_PROPS = {
  * @internal
  */
 export const Tab = forwardRef(function Tab(
-  {
-    size = 'small',
-    tone = 'default',
-    ...props
-  }: TabProps & Omit<React.HTMLProps<HTMLButtonElement>, 'as' | 'size'>,
+  {tone = 'default', ...props}: TabProps & Omit<React.HTMLProps<HTMLButtonElement>, 'as' | 'size'>,
   ref: React.ForwardedRef<HTMLButtonElement>,
 ) {
-  return (
-    <UITab
-      {...props}
-      {...(size === 'default' ? DEFAULT_TAB_PROPS : SMALL_TAB_PROPS)}
-      muted
-      ref={ref}
-      tone={tone}
-    />
-  )
+  return <UITab {...props} muted padding={2} ref={ref} tone={tone} />
 })
