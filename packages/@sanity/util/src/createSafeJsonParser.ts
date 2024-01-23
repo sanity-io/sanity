1eed03078d368f8f6d4712329991c23ea3291cc9
Ash <ash@sanity.io>
2024-01-23 10:09:06 +0000

    chore(util): update comment

--- packages/@sanity/util/src/createSafeJsonParser.ts
+++ packages/@sanity/util/src/createSafeJsonParser.ts
@@ -7,7 +7,10 @@ type Parser<Type> = (line: string) => Type
 /**
  * Create a safe JSON parser that is able to handle lines interrupted by an error object.
  *
+ * This may occur when streaming NDJSON from the Export HTTP API.
+ *
  * @internal
+ * @see {@link https://github.com/sanity-io/sanity/pull/1787 | Initial pull request}
  */
 export function createSafeJsonParser<Type>({errorLabel}: Options): Parser<Type> {
   return function safeJsonParser(line) {
