c21e9e4d3050563d0fbdb4310bb457cfec7b3009
Ash <ash@sanity.io>
2024-01-24 13:14:04 +0000

    feat(migrate): use safe JSON parser when streaming from Export HTTP API (#5542)
    * refactor(util): create shared JSON parser for `@sanity/export` and `@sanity/migrate`
    
    * feat(migrate): add safe JSON parser
    
    * chore(util): update comment
    
    * chore(export): update comment
    
    * feat(migrate): use safe JSON parser when streaming from Export HTTP API
    
    * feat(migrate): add JSON options and type parameter to NDJSON utility
    
    * refactor(migrate): use type parameter
    
    * fix(migrate): add missing export

--- packages/@sanity/migrate/src/_exports/index.ts
+++ packages/@sanity/migrate/src/_exports/index.ts
@@ -1,6 +1,7 @@
 export {fromExportArchive} from '../sources/fromExportArchive'
 export {fromExportEndpoint} from '../sources/fromExportEndpoint'
 export {fromDocuments} from '../sources/fromDocuments'
+export {safeJsonParser} from '../sources/fromExportEndpoint'
 export * from '../types'
 export * from '../defineMigration'
 export * from '../it-utils'
--- packages/@sanity/migrate/src/it-utils/ndjson.ts
+++ packages/@sanity/migrate/src/it-utils/ndjson.ts
@@ -1,8 +1,14 @@
 import {split} from './split'
 import {decodeText} from './decodeText'
-import {parseJSON} from './json'
+import {type JSONOptions, parseJSON} from './json'
 import {filter} from './filter'
 
-export function ndjson(it: AsyncIterableIterator<Uint8Array>) {
-  return parseJSON(filter(split(decodeText(it), '\n'), (line) => Boolean(line && line.trim())))
+export function ndjson<Type>(
+  it: AsyncIterableIterator<Uint8Array>,
+  options?: JSONOptions<Type>,
+): AsyncIterableIterator<Type> {
+  return parseJSON(
+    filter(split(decodeText(it), '\n'), (line) => Boolean(line && line.trim())),
+    options,
+  )
 }
