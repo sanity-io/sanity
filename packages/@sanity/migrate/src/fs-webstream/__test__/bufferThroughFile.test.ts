e889c48aa3a3f76a4071b4dffdd4a0c56b0113f2
Bjørge Næss <bjoerge@gmail.com>
2024-01-29 10:00:10 +0100

    fix(migrate): simplify bufferThroughFile

--- packages/@sanity/migrate/src/fs-webstream/__test__/bufferThroughFile.test.ts
+++ packages/@sanity/migrate/src/fs-webstream/__test__/bufferThroughFile.test.ts
@@ -1,89 +1,200 @@
 /* eslint-disable no-constant-condition */
 import {stat} from 'node:fs/promises'
 
+import path from 'node:path'
 import {bufferThroughFile} from '../bufferThroughFile'
-import {parse} from '../../it-utils'
+import {decodeText, parse} from '../../it-utils'
 import {streamToAsyncIterator} from '../../utils/streamToAsyncIterator'
 import {asyncIterableToStream} from '../../utils/asyncIterableToStream'
+import {lastValueFrom} from '../../it-utils/lastValueFrom'
+import {firstValueFrom} from '../../it-utils/firstValueFrom'
 
 const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms))
 
-test('stops buffering when the consumer is done', async () => {
-  const encoder = new TextEncoder()
-  async function* gen() {
-    for (let n = 0; n < 1000; n++) {
-      yield encoder.encode(`{"foo": ${n},`)
-      // simulate a bit of delay in the producer (which is often the case)
+let id = 0
+const getTestBufferFileName = () => path.join(__dirname, '.tmp', `buffer-${id++}.ndjson`)
+
+describe('using primary stream', () => {
+  test('stops buffering when the consumer is done', async () => {
+    const encoder = new TextEncoder()
+
+    async function* gen() {
+      for (let n = 0; n < 100; n++) {
+        yield encoder.encode(`{"foo": ${n},`)
+        // simulate a bit of delay in the producer (which is often the case)
+        await sleep(1)
+        yield encoder.encode(`"bar": ${n}, "baz": ${n}}`)
+        yield encoder.encode('\n')
+      }
+    }
+
+    const bufferFile = getTestBufferFileName()
+    const abortController = new AbortController()
+    const createReader = bufferThroughFile(asyncIterableToStream(gen()), bufferFile, {
+      signal: abortController.signal,
+    })
+    const fileBufferStream = createReader()
+    const lines = []
+    for await (const chunk of parse(decodeText(streamToAsyncIterator(fileBufferStream)))) {
+      lines.push(chunk)
+      if (lines.length === 3) {
+        // we only pick 3 lines and break out of the iteration. This should stop the buffering
+        break
+      }
+      // simulate a slow consumer
+      // (the bufferThroughFile stream should still continue to write to the file as fast as possible)
       await sleep(10)
-      yield encoder.encode(`"bar": ${n}, "baz": ${n}}`)
-      yield encoder.encode('\n')
     }
-  }
 
-  const bufferFile = `${__dirname}/partial.ndjson`
-  const fileBufferStream = bufferThroughFile(asyncIterableToStream(gen()), bufferFile)
+    expect(lines).toEqual([
+      {bar: 0, baz: 0, foo: 0},
+      {bar: 1, baz: 1, foo: 1},
+      {bar: 2, baz: 2, foo: 2},
+    ])
 
-  const lines = []
-  for await (const chunk of parse(streamToAsyncIterator(fileBufferStream))) {
-    lines.push(chunk)
-    if (lines.length === 3) {
-      // we only pick 3 lines and break out of the iteration. This should stop the buffering
-      break
+    // Note: the stream needs to be explicitly aborted, otherwise the source stream will run to completion
+    // would be nice if there was a way to "unref()" the file handle to prevent it from blocking the process,
+    // but I don't think there is
+    abortController.abort()
+
+    // This asserts that buffer file contains more bytes than the 3 lines above represents
+    const bufferFileSize = (await stat(bufferFile)).size
+
+    expect(bufferFileSize).toBeGreaterThan(90)
+    // but not the full 100 lines
+    expect(bufferFileSize).toBeLessThan(3270)
+  })
+
+  test('it runs to completion if consumer needs it', async () => {
+    const encoder = new TextEncoder()
+
+    async function* gen() {
+      for (let n = 0; n < 100; n++) {
+        yield encoder.encode(`{"foo": ${n},`)
+        // simulate a bit of delay in the producer (which is often the case)
+        await sleep(1)
+        yield encoder.encode(`"bar": ${n}, "baz": ${n}}`)
+        yield encoder.encode('\n')
+      }
     }
-    // simulate a slow consumer
-    // (the bufferThroughFile stream should still continue to write to the file as fast as possible)
-    await sleep(50)
-  }
 
-  expect(lines).toEqual([
-    {bar: 0, baz: 0, foo: 0},
-    {bar: 1, baz: 1, foo: 1},
-    {bar: 2, baz: 2, foo: 2},
-  ])
+    const bufferFile = getTestBufferFileName()
+    const controller = new AbortController()
+    const createReader = bufferThroughFile(asyncIterableToStream(gen()), bufferFile, {
+      signal: controller.signal,
+    })
+    const fileBufferStream = createReader()
+    const lines = []
+    for await (const chunk of parse(decodeText(streamToAsyncIterator(fileBufferStream)))) {
+      if (lines.length < 3) {
+        // in contrast to the test above, we don't break out of the iteration early, but let it run to completion
+        lines.push(chunk)
+      }
+    }
 
-  // Note: the stream needs to be explicitly cancelled, otherwise the source stream will run to completion
-  // would be nice if there was a way to "unref()" the file handle to prevent it from blocking the process,
-  // but I don't think there is
-  await fileBufferStream.cancel()
+    expect(lines).toEqual([
+      {bar: 0, baz: 0, foo: 0},
+      {bar: 1, baz: 1, foo: 1},
+      {bar: 2, baz: 2, foo: 2},
+    ])
 
-  // This asserts that buffer file contains more bytes than the 3 lines above represents
-  const bufferFileSize = (await stat(bufferFile)).size
-
-  expect(bufferFileSize).toBeGreaterThan(90)
-  // but not the full 1000 lines
-  expect(bufferFileSize).toBe(311)
+    // This asserts that buffer file contains all the yielded lines
+    expect((await stat(bufferFile)).size).toBe(3270)
+  })
 })
 
-test('it runs to completion if consumer needs it', async () => {
-  const encoder = new TextEncoder()
-  async function* gen() {
-    for (let n = 0; n < 100; n++) {
-      yield encoder.encode(`{"foo": ${n},`)
-      // simulate a bit of delay in the producer (which is often the case)
-      await sleep(1)
-      yield encoder.encode(`"bar": ${n}, "baz": ${n}}`)
-      yield encoder.encode('\n')
+describe('using secondary stream', () => {
+  test('stops buffering when the consumer is done', async () => {
+    const encoder = new TextEncoder()
+
+    async function* gen() {
+      for (let n = 0; n < 100; n++) {
+        yield encoder.encode(`{"foo": ${n},`)
+        // simulate a bit of delay in the producer (which is often the case)
+        yield encoder.encode(`"bar": ${n}, "baz": ${n}}`)
+        yield encoder.encode('\n')
+      }
     }
-  }
 
-  const bufferFile = `${__dirname}/full.ndjson`
-  const fileBufferStream = bufferThroughFile(asyncIterableToStream(gen()), bufferFile)
+    const bufferFile = getTestBufferFileName()
+    const abortController = new AbortController()
+    const createReader = bufferThroughFile(asyncIterableToStream(gen()), bufferFile, {
+      signal: abortController.signal,
+    })
+    const fileBufferStream = createReader()
 
-  const lines = []
-  for await (const chunk of parse(streamToAsyncIterator(fileBufferStream))) {
-    if (lines.length < 3) {
-      // in contrast to the test above, we don't break out of the iteration early, but let it run to completion
+    const lines = []
+    for await (const chunk of parse(decodeText(streamToAsyncIterator(fileBufferStream)))) {
       lines.push(chunk)
+      lines.push(await lastValueFrom(parse(decodeText(streamToAsyncIterator(createReader())))))
+      if (lines.length === 6) {
+        break
+      }
     }
-  }
-  await fileBufferStream.cancel()
 
-  expect(lines).toEqual([
-    {bar: 0, baz: 0, foo: 0},
-    {bar: 1, baz: 1, foo: 1},
-    {bar: 2, baz: 2, foo: 2},
-  ])
+    abortController.abort()
 
-  // This asserts that buffer file contains all the yielded lines
-  expect((await stat(bufferFile)).size).toBe(3270)
+    expect(lines).toEqual([
+      {bar: 0, baz: 0, foo: 0},
+      {bar: 99, baz: 99, foo: 99},
+      {bar: 1, baz: 1, foo: 1},
+      {bar: 99, baz: 99, foo: 99},
+      {bar: 2, baz: 2, foo: 2},
+      {bar: 99, baz: 99, foo: 99},
+    ])
+  })
+
+  test('ends when the primary stream completes', async () => {
+    const encoder = new TextEncoder()
+
+    async function* gen() {
+      for (let n = 0; n < 100; n++) {
+        yield encoder.encode(`{"foo": ${n},`)
+        yield encoder.encode(`"bar": ${n}, "baz": ${n}}`)
+        yield encoder.encode('\n')
+      }
+    }
+
+    const bufferFile = getTestBufferFileName()
+    const createReader = bufferThroughFile(asyncIterableToStream(gen()), bufferFile)
+    const primary = createReader()
+    const first = firstValueFrom(parse(decodeText(streamToAsyncIterator(primary))))
+    const last = lastValueFrom(parse(decodeText(streamToAsyncIterator(createReader()))))
+
+    expect(await first).toEqual({bar: 0, baz: 0, foo: 0})
+
+    expect(await last).toEqual({bar: 99, baz: 99, foo: 99})
+  })
+
+  test('throws if a new stream is created after abortion', async () => {
+    const encoder = new TextEncoder()
+
+    async function* gen() {
+      for (let n = 0; n < 100; n++) {
+        yield encoder.encode(`{"foo": ${n},`)
+        yield encoder.encode(`"bar": ${n}, "baz": ${n}}`)
+        yield encoder.encode('\n')
+      }
+    }
+
+    const bufferFile = getTestBufferFileName()
+    const controller = new AbortController()
+    const createReader = bufferThroughFile(asyncIterableToStream(gen()), bufferFile, {
+      signal: controller.signal,
+    })
+    const primary = createReader()
+    const first = await firstValueFrom(parse(decodeText(streamToAsyncIterator(primary))))
+
+    expect(first).toEqual({bar: 0, baz: 0, foo: 0})
+
+    await primary.cancel()
+
+    controller.abort()
+
+    await expect(() =>
+      lastValueFrom(parse(decodeText(streamToAsyncIterator(createReader())))),
+    ).rejects.toThrowErrorMatchingInlineSnapshot(
+      `"Cannot create new buffered readers on aborted stream"`,
+    )
+  })
 })
--- packages/@sanity/migrate/src/fs-webstream/bufferThroughFile.ts
+++ packages/@sanity/migrate/src/fs-webstream/bufferThroughFile.ts
@@ -4,69 +4,134 @@ const CHUNK_SIZE = 1024
 
 /**
  * Takes a source stream that will be drained and written to the provided file name as fast as possible.
- * and returns a readable stream that reads from the same buffer file. The returned stream can be read at any rate.
- * Note: the returned stream needs to be explicitly cancelled, otherwise it will run to completion and potentially
- * prevent the process from exiting until the whole source stream has been written.
+ * and returns a function that can be called to create multiple readable stream on top of the buffer file.
+ * It will start pulling data from the source stream once the first readableStream is created, writing to the buffer file in the background.
+ * The readable streams and can be read at any rate (but will not receive data faster than the buffer file is written to).
+ * Note: by default, buffering will run to completion, and this may prevent the process from exiting after done reading from the
+ * buffered streams. To stop writing to the buffer file, an AbortSignal can be provided and once it's controller aborts, the buffer file will
+ * stop. After the signal is aborted, no new buffered readers can be created.
+ *
  * @param source - The source readable stream. Will be drained as fast as possible.
  * @param filename - The filename to write to.
+ * @param options - Optional AbortSignal to stop writing to the buffer file.
+ * @returns A function that can be called multiple times to create a readable stream on top of the buffer file.
  */
-export function bufferThroughFile(source: ReadableStream, filename: string) {
-  let fileHandle: FileHandle
-  let totalBytesRead = 0
-  let totalBytesWritten = 0
+export function bufferThroughFile(
+  source: ReadableStream<Uint8Array | string>,
+  filename: string,
+  options?: {signal: AbortSignal},
+) {
+  const signal = options?.signal
 
+  let writeHandle: FileHandle
+  let readHandle: Promise<FileHandle> | null
+
+  // Whether the all data has been written to the buffer file.
   let bufferDone = false
-  let readerDone = false
 
-  async function pump(reader: ReadableStreamDefaultReader) {
+  signal?.addEventListener('abort', async () => {
+    await Promise.all([
+      writeHandle && writeHandle.close(),
+      readHandle && (await readHandle).close(),
+    ])
+  })
+
+  // Number of active readers. When this reaches 0, the read handle will be closed.
+  let readerCount = 0
+  let ready: Promise<void>
+
+  async function pump(reader: ReadableStreamDefaultReader<Uint8Array | string>) {
     try {
       // eslint-disable-next-line no-constant-condition
       while (true) {
         const {done, value} = await reader.read()
-        if (done || readerDone) return
-        await fileHandle.write(value)
-        totalBytesWritten += value.byteLength
+        if (done || signal?.aborted) {
+          // if we're done reading, or the primary reader has been cancelled, stop writing to the buffer file
+          return
+        }
+        await writeHandle.write(value)
       }
     } finally {
+      await writeHandle.close()
+      bufferDone = true
       reader.releaseLock()
     }
   }
 
-  async function tryReadFromBuffer() {
-    const {bytesRead, buffer} = await fileHandle.read(
-      new Uint8Array(CHUNK_SIZE),
-      0,
-      CHUNK_SIZE,
-      totalBytesRead,
-    )
-    if (bytesRead === 0 && !bufferDone && !readerDone) {
-      // we're waiting for more data to be written to the buffer file, try again
-      return tryReadFromBuffer()
+  function createBufferedReader() {
+    let totalBytesRead = 0
+
+    return async function tryReadFromBuffer(handle: FileHandle) {
+      const {bytesRead, buffer} = await handle.read(
+        new Uint8Array(CHUNK_SIZE),
+        0,
+        CHUNK_SIZE,
+        totalBytesRead,
+      )
+      if (bytesRead === 0 && !bufferDone && !signal?.aborted) {
+        // we're waiting for more data to be written to the buffer file, try again
+        return tryReadFromBuffer(handle)
+      }
+      totalBytesRead += bytesRead
+      return {bytesRead, buffer}
     }
-    return {bytesRead, buffer}
   }
 
-  return new ReadableStream({
-    async start() {
-      fileHandle = await open(filename, 'w+')
-      // Note: do not await this, as it will block the stream from starting
-      pump(source.getReader()).then(() => {
-        bufferDone = true
-      })
-    },
-    async pull(controller) {
-      const {bytesRead, buffer} = await tryReadFromBuffer()
-      if (bytesRead === 0 && bufferDone) {
-        await fileHandle.close()
-        controller.close()
-      } else {
-        totalBytesRead += bytesRead
-        controller.enqueue(buffer.subarray(0, bytesRead))
-      }
-    },
-    async cancel() {
-      readerDone = true
-      await fileHandle?.close()
-    },
-  })
+  function init(): Promise<void> {
+    if (!ready) {
+      ready = (async () => {
+        writeHandle = await open(filename, 'w')
+        // start pumping data from the source stream to the buffer file
+        // note, don't await this, as it will block the ReadableStream.start() method
+        pump(source.getReader())
+      })()
+    }
+    return ready
+  }
+
+  function getReadHandle(): Promise<FileHandle> {
+    if (!readHandle) {
+      readHandle = open(filename, 'r')
+    }
+    return readHandle
+  }
+
+  function onReaderStart() {
+    readerCount++
+  }
+  async function onReaderEnd() {
+    readerCount--
+    if (readerCount === 0 && readHandle) {
+      const handle = readHandle
+      readHandle = null
+      await (await handle).close()
+    }
+  }
+
+  return () => {
+    const readChunk = createBufferedReader()
+
+    return new ReadableStream<Uint8Array>({
+      async start() {
+        if (signal?.aborted) {
+          throw new Error('Cannot create new buffered readers on aborted stream')
+        }
+        onReaderStart()
+        await init()
+        await getReadHandle()
+      },
+      async pull(controller) {
+        if (!readHandle) {
+          throw new Error('Cannot read from closed handle')
+        }
+        const {bytesRead, buffer} = await readChunk(await readHandle)
+        if (bytesRead === 0 && bufferDone) {
+          await onReaderEnd()
+          controller.close()
+        } else {
+          controller.enqueue(buffer.subarray(0, bytesRead))
+        }
+      },
+    })
+  }
 }
