73de603399f3e6cf97df956f77f9f572bdfe02ba
Bjørge Næss <bjoerge@gmail.com>
2024-01-29 09:39:42 +0100

    refactor(migrate): rename function

--- packages/@sanity/migrate/src/sources/fromExportArchive.ts
+++ packages/@sanity/migrate/src/sources/fromExportArchive.ts
@@ -1,15 +1,15 @@
 import {maybeDecompress} from '../fs-webstream/maybeDecompress'
 
 import {untar} from '../tar-webstream/untar'
-import {streamAsyncIterator} from '../utils/streamToAsyncIterator'
+import {streamToAsyncIterator} from '../utils/streamToAsyncIterator'
 import {readFileAsWebStream} from '../fs-webstream/readFileAsWebStream'
 
 export async function* fromExportArchive(path: string) {
-  for await (const [header, entry] of streamAsyncIterator(
+  for await (const [header, entry] of streamToAsyncIterator(
     untar(await maybeDecompress(readFileAsWebStream(path))),
   )) {
     if (header.type === 'file' && header.name.endsWith('.ndjson')) {
-      for await (const chunk of streamAsyncIterator(entry)) {
+      for await (const chunk of streamToAsyncIterator(entry)) {
         yield chunk
       }
     }
