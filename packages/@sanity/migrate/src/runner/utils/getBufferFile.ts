338bf269c95aa9926ba8cd349714c9b5e7e5f0ae
Bjørge Næss <bjoerge@gmail.com>
2024-01-30 22:16:40 +0100

    fix(migrate): remove mkdirp in favor of node:fs (#5619)

--- packages/@sanity/migrate/src/runner/utils/getBufferFile.ts
+++ packages/@sanity/migrate/src/runner/utils/getBufferFile.ts
@@ -1,6 +1,6 @@
 import {tmpdir} from 'node:os'
 import path from 'node:path'
-import mkdirp from 'mkdirp'
+import {mkdir} from 'node:fs/promises'
 
 export async function createBufferFile() {
   const bufferDir = path.join(
@@ -8,6 +8,7 @@ export async function createBufferFile() {
     'sanity-migrate',
     `${Date.now()}-${Math.random().toString(36).slice(2)}`,
   )
-  await mkdirp(bufferDir)
+
+  await mkdir(bufferDir, {recursive: true})
   return path.join(bufferDir, `snapshot.ndjson`)
 }
