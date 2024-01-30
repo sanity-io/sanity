91cb05bdb6076c367efcc6c2e59dec7b4a74aaef
Herman Wikner <wiknerherman@gmail.com>
2024-01-30 17:13:01 +0100

    fix(comments): improve overlapping logic

--- packages/sanity/src/structure/comments/src/utils/inline-comments/currentSelectionOverlapsWithComment.ts
+++ packages/sanity/src/structure/comments/src/utils/inline-comments/currentSelectionOverlapsWithComment.ts
@@ -1,9 +1,53 @@
 import {EditorSelection} from '@sanity/portable-text-editor'
 import * as PathUtils from '@sanity/util/paths'
 
+interface OffsetStringPath {
+  offset: number
+  stringPath: string
+}
+
+interface OffsetSelection {
+  anchor: OffsetStringPath
+  focus: OffsetStringPath
+}
+
 interface CurrentSelectionIsOverlappingWithCommentProps {
   currentSelection?: EditorSelection
   addedCommentsSelections?: EditorSelection[]
+}
+
+function mapSelectionToOffset(selection?: EditorSelection): OffsetSelection {
+  if (!selection) {
+    return {
+      anchor: {offset: 0, stringPath: ''},
+      focus: {offset: 0, stringPath: ''},
+    }
+  }
+
+  const anchor = {
+    offset: selection.anchor.offset || 0,
+    stringPath: PathUtils.toString(selection.anchor.path || []),
+  }
+
+  const focus = {
+    offset: selection.focus.offset || 0,
+    stringPath: PathUtils.toString(selection.focus.path || []),
+  }
+
+  // Swap anchor and focus if focus is greater than anchor
+  return focus.offset < anchor.offset ? {anchor: focus, focus: anchor} : {anchor, focus}
+}
+
+function isSelectionInside(
+  commentSelection: OffsetSelection,
+  compareValue: OffsetSelection,
+): boolean {
+  return (
+    (compareValue.anchor.offset > commentSelection.anchor.offset &&
+      compareValue.anchor.offset < commentSelection.focus.offset) ||
+    (compareValue.focus.offset > commentSelection.anchor.offset &&
+      compareValue.focus.offset < commentSelection.focus.offset)
+  )
 }
 
 export function currentSelectionIsOverlappingWithComment(
@@ -11,48 +55,22 @@ export function currentSelectionIsOverlappingWithComment(
 ): boolean {
   const {currentSelection, addedCommentsSelections = []} = props
 
-  const compareValue = {
-    anchor: {
-      offset: currentSelection?.anchor.offset || 0,
-      stringPath: PathUtils.toString(currentSelection?.anchor.path || []),
-    },
-    focus: {
-      offset: currentSelection?.focus.offset || 0,
-      stringPath: PathUtils.toString(currentSelection?.focus.path || []),
-    },
-  }
+  const compareValue = mapSelectionToOffset(currentSelection)
 
-  const commentSelections = addedCommentsSelections?.map((selection) => {
-    return {
-      anchor: {
-        offset: selection?.anchor.offset || 0,
-        stringPath: PathUtils.toString(selection?.anchor.path || []),
-      },
-      focus: {
-        offset: selection?.focus.offset || 0,
-        stringPath: PathUtils.toString(selection?.focus.path || []),
-      },
-    }
-  })
+  const commentSelections = addedCommentsSelections?.map(mapSelectionToOffset) || []
 
-  const overlaps = commentSelections?.some((commentSelection) => {
-    const val =
-      // Check if the current selection's anchor is inside the comment selection
-      (compareValue.anchor.offset > commentSelection.anchor.offset &&
-        compareValue.anchor.offset < commentSelection.focus.offset) ||
-      // Check if the current selection's focus is inside the comment selection
-      (compareValue.focus.offset > commentSelection.anchor.offset &&
-        compareValue.focus.offset < commentSelection.focus.offset) ||
-      // Check if the current selection completely envelops the comment selection
-      (compareValue.anchor.offset < commentSelection.anchor.offset &&
-        compareValue.focus.offset > commentSelection.focus.offset) ||
-      // Check if the current selection wraps around the comment selection
+  const overlaps = commentSelections.some((commentSelection) => {
+    const isInside = isSelectionInside(commentSelection, compareValue)
+    const envelops =
+      compareValue.anchor.offset < commentSelection.anchor.offset &&
+      compareValue.focus.offset > commentSelection.focus.offset
+    const wrapsAround =
       (compareValue.anchor.offset < commentSelection.anchor.offset &&
         compareValue.focus.offset > commentSelection.anchor.offset) ||
       (compareValue.anchor.offset < commentSelection.focus.offset &&
         compareValue.focus.offset > commentSelection.focus.offset)
 
-    return val && compareValue.anchor.stringPath === commentSelection.anchor.stringPath
+    return isInside || envelops || wrapsAround
   })
 
   return overlaps
