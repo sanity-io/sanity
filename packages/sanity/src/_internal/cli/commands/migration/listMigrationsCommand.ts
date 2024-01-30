5d9a556f9d680323b7f32a210553cf7784770446
Bjørge Næss <bjoerge@gmail.com>
2024-01-30 10:22:40 +0100

    fix(sanity): improve error handling on 'sanity migration run' without id

--- packages/sanity/src/_internal/cli/commands/migration/listMigrationsCommand.ts
+++ packages/sanity/src/_internal/cli/commands/migration/listMigrationsCommand.ts
@@ -1,6 +1,6 @@
 import path from 'path'
 import {readdir} from 'node:fs/promises'
-import type {CliCommandDefinition} from '@sanity/cli'
+import type {CliCommandContext, CliCommandDefinition} from '@sanity/cli'
 import {register} from 'esbuild-register/dist/node'
 import {Migration} from '@sanity/migrate'
 import {Table} from 'console-table-printer'
@@ -21,45 +21,48 @@ const createMigrationCommand: CliCommandDefinition<CreateFlags> = {
   helpText,
   description: 'List available migrations',
   action: async (args, context) => {
-    const {output, workDir} = context
-
-    if (!__DEV__) {
-      register({
-        target: `node${process.version.slice(1)}`,
-      })
-    }
-
-    const directories = (
-      await readdir(path.join(workDir, MIGRATIONS_DIRECTORY), {withFileTypes: true})
-    ).filter((ent) => ent.isDirectory())
-
-    const migrationModules = directories
-      .map((ent) => {
-        const candidates = resolveMigrationScript(workDir, ent.name)
-        const found = candidates.find((candidate) => candidate.mod?.default)
-        if (!found) {
-          return null
-        }
-        return {
-          dirname: ent.name,
-          migration: found.mod.default as Migration,
-        }
-      })
-      .filter(Boolean) as {dirname: string; migration: Migration}[]
+    const {workDir, output} = context
+    const migrations = await resolveMigrations(workDir)
     const table = new Table({
-      title: `Found ${migrationModules.length} migrations in project`,
+      title: `Found ${migrations.length} migrations in project`,
       columns: [
         {name: 'id', title: 'ID', alignment: 'left'},
         {name: 'title', title: 'Title', alignment: 'left'},
       ],
     })
 
-    migrationModules.forEach((definedMigration) => {
+    migrations.forEach((definedMigration) => {
       table.addRow({id: definedMigration.dirname, title: definedMigration.migration.title})
     })
     table.printTable()
-    output.print(`\nRun "sanity migration run <MIGRATION ID>" to run a migration`)
+    output.print('\nRun `sanity migration run <ID>` to run a migration')
   },
+}
+
+export async function resolveMigrations(workDir: string) {
+  if (!__DEV__) {
+    register({
+      target: `node${process.version.slice(1)}`,
+    })
+  }
+
+  const directories = (
+    await readdir(path.join(workDir, MIGRATIONS_DIRECTORY), {withFileTypes: true})
+  ).filter((ent) => ent.isDirectory())
+
+  return directories
+    .map((ent) => {
+      const candidates = resolveMigrationScript(workDir, ent.name)
+      const found = candidates.find((candidate) => candidate.mod?.default)
+      if (!found) {
+        return null
+      }
+      return {
+        dirname: ent.name,
+        migration: found.mod.default as Migration,
+      }
+    })
+    .filter(Boolean) as {dirname: string; migration: Migration}[]
 }
 
 export default createMigrationCommand
