60e417a2ddcedaa2d16b337da24021e6bc3c6a7d
Pedro Bonamin <pedrobonamin@gmail.com>
2024-01-30 09:28:58 +0100

    chore(comments-upsell): move CommentsUpsellProvider to CommentsStudioLayout

--- packages/sanity/src/structure/comments/plugin/studio-layout/CommentsStudioLayout.tsx
+++ packages/sanity/src/structure/comments/plugin/studio-layout/CommentsStudioLayout.tsx
@@ -1,11 +1,24 @@
 import React from 'react'
-import {CommentsOnboardingProvider, CommentsSetupProvider} from '../../src'
-import {LayoutProps} from 'sanity'
+import {CommentsOnboardingProvider, CommentsSetupProvider, CommentsUpsellProvider} from '../../src'
+import {ConditionalWrapper} from '../../../../ui-components/conditionalWrapper'
+import {LayoutProps, useFeatureEnabled} from 'sanity'
 
 export function CommentsStudioLayout(props: LayoutProps) {
+  // TODO: Remove _enabled before merging
+  const {enabled: _enabled, isLoading} = useFeatureEnabled('studioComments')
+  const enabled = false
+
   return (
     <CommentsSetupProvider>
-      <CommentsOnboardingProvider>{props.renderDefault(props)}</CommentsOnboardingProvider>
+      <CommentsOnboardingProvider>
+        <ConditionalWrapper
+          condition={!enabled && !isLoading}
+          // eslint-disable-next-line react/jsx-no-bind
+          wrapper={(children) => <CommentsUpsellProvider>{children}</CommentsUpsellProvider>}
+        >
+          {props.renderDefault(props)}
+        </ConditionalWrapper>
+      </CommentsOnboardingProvider>
     </CommentsSetupProvider>
   )
 }
