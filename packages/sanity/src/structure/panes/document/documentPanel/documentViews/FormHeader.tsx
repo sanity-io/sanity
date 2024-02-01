4e2507b078e912d9f9b6d6de1cd615bdaf772a58
Simeon Griggs <simeon@hey.com>
2024-02-01 20:31:13 +0000

    feat: apply text balance to document titles (#5641)

--- packages/sanity/src/structure/panes/document/documentPanel/documentViews/FormHeader.tsx
+++ packages/sanity/src/structure/panes/document/documentPanel/documentViews/FormHeader.tsx
@@ -33,6 +33,7 @@ export const TitleContainer = styled(Stack)`
         font-size: ${theme.sanity.fonts.heading.sizes[4].fontSize}px;
         line-height: ${theme.sanity.fonts.heading.sizes[4].lineHeight}px;
         overflow-wrap: break-word;
+        text-wrap: pretty;
       }
 
       @container (max-width: 560px) {
