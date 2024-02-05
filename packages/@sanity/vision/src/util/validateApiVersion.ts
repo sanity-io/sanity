175ece0d0c73aee44b4f46d242b2555f28ab1d8c
Espen Hovlandsdal <espen@hovlandsdal.com>
2024-02-05 15:18:15 -0800

    fix(vision): explicitly allow `v1` as valid API version

--- packages/@sanity/vision/src/util/validateApiVersion.ts
+++ packages/@sanity/vision/src/util/validateApiVersion.ts
@@ -4,6 +4,7 @@ export function validateApiVersion(apiVersion: string): boolean {
   const isValidApiVersion =
     parseableApiVersion.length > 0 &&
     (parseableApiVersion === 'X' ||
+      parseableApiVersion === '1' ||
       (/^\d{4}-\d{2}-\d{2}$/.test(parseableApiVersion) && !isNaN(Date.parse(parseableApiVersion))))
 
   return isValidApiVersion
