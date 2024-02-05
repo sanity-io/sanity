6317b49d23de69cc4025f47267dc084fb08a7ca6
Espen Hovlandsdal <espen@hovlandsdal.com>
2024-02-05 10:18:43 -0800

    fix(comments): improve text wrapping logic inside of user list

--- packages/sanity/src/structure/comments/src/components/reactions/CommentReactionsUsersTooltip.tsx
+++ packages/sanity/src/structure/comments/src/components/reactions/CommentReactionsUsersTooltip.tsx
@@ -14,14 +14,24 @@ const ContentStack = styled(Stack)`
   max-width: 180px;
 `
 
+const TextGroup = styled.div`
+  display: inline-block;
+`
+
 const InlineText = styled(Text).attrs({size: TEXT_SIZE})`
   display: inline-block !important;
+
+  & > span {
+    white-space: break-spaces;
+  }
 `
 
 const TextBox = styled(Box)`
   line-height: 1;
   text-align: center;
 `
+
+const LEADING_NON_WHITESPACE_RE = /^\S+/
 
 interface UserDisplayNameProps {
   currentUserId: string
@@ -64,38 +74,73 @@ export function CommentReactionsUsersTooltip(props: CommentReactionsUsersTooltip
   )
 }
 
+function FormattedUserList({currentUserId, userIds}: {currentUserId: string; userIds: string[]}) {
+  const listFormat = useListFormat({style: 'long', type: 'conjunction'})
+  if (userIds.length === 0) return null
+
+  /**
+   * We need to do some surgery on the list: in some locales (such as en-US), the literal segments
+   * can contain oxford commas, which we want to include as the part of the element preceeding it.
+   * This ensures that we do not wrap to a new line that starts with a comma. In general, we should
+   * not special case on _comma_ per se, but rather by the presence of a non-whitespace character.
+   */
+  const parts = listFormat.formatToParts(userIds)
+  const elements: JSX.Element[] = []
+  for (let i = 0; i < parts.length; i++) {
+    const item = parts[i]
+
+    if (item.type === 'literal') {
+      // Add literals as-is - the next case will rewrite literals to exclude leading non-whitespace
+      elements.push(<InlineText key={`literal-${i}`}>{item.value}</InlineText>)
+      continue
+    }
+
+    const nextItem = parts[i + 1]
+    const nextLeadsWithNonWhitespace =
+      nextItem && nextItem.type === 'literal' && LEADING_NON_WHITESPACE_RE.test(nextItem.value)
+    if (nextLeadsWithNonWhitespace) {
+      // This is the 'oxford comma' case, where we want to include any leading non-whitespace from
+      // the literal as trailing characters to the element we are currently adding.
+      const [nonWhitespace = ''] = nextItem.value.match(LEADING_NON_WHITESPACE_RE) || []
+
+      elements.push(
+        // Key (value) is user ID, thus unique
+        <TextGroup key={item.value}>
+          <InlineText weight="medium">
+            <UserDisplayName currentUserId={currentUserId} isFirst={i === 0} userId={item.value} />
+          </InlineText>
+          <InlineText>{nonWhitespace}</InlineText>
+        </TextGroup>,
+      )
+
+      // Rewrite the next item to not contain this leading non-whitespace
+      nextItem.value = nextItem.value.slice(nonWhitespace.length)
+      continue
+    }
+
+    // Literals have been taken care of and returns early, so the only remaining case is that we're
+    // in an element that does _not_ have a leading non-whitespace literal following it.
+    elements.push(
+      // Key (value) is user ID, thus unique
+      <InlineText weight="medium" key={item.value}>
+        <UserDisplayName currentUserId={currentUserId} isFirst={i === 0} userId={item.value} />
+      </InlineText>,
+    )
+  }
+
+  return elements
+}
+
 export function CommentReactionsUsersTooltipContent(
   props: Omit<CommentReactionsUsersTooltipProps, 'children'>,
 ) {
   const {currentUser, reactionName, userIds} = props
   const {t} = useTranslation(commentsLocaleNamespace)
-  const listFormat = useListFormat({style: 'long', type: 'conjunction'})
 
   const UserList = useCallback(() => {
-    const len = userIds.length
-
-    if (len === 0 || !currentUser) return null
-
-    return (
-      <>
-        {listFormat.formatToParts(userIds).map((item, index) =>
-          item.type === 'element' ? (
-            <InlineText weight="medium" key={item.value}>
-              <UserDisplayName
-                currentUserId={currentUser.id}
-                isFirst={index === 0}
-                userId={item.value}
-              />
-            </InlineText>
-          ) : (
-            <>
-              <InlineText key={item.value}> {item.value} </InlineText>{' '}
-            </>
-          ),
-        )}
-      </>
-    )
-  }, [currentUser, listFormat, userIds])
+    if (!currentUser) return null
+    return <FormattedUserList currentUserId={currentUser.id} userIds={userIds} />
+  }, [currentUser, userIds])
 
   return (
     <ContentStack padding={1}>
