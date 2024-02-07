ffab9314c2f7c502866c45fe55d140a406a6d181
Ash <ash@sanity.io>
2024-02-07 09:18:57 +0000

    feat(cli): print project root path suggestion
    When executing a command in a subdirectory of a Studio project, a warning will be printed
    suggesting the user moves to the root Studio project directory.

--- packages/sanity/src/_internal/cli/util/ensureCwdIsProjectRoot.ts
+++ packages/sanity/src/_internal/cli/util/ensureCwdIsProjectRoot.ts
@@ -1,15 +1,15 @@
+import path from 'node:path'
 import {type CliCommandContext} from '@sanity/cli'
+import {isModernCliConfig} from './isModernCliConfig'
 
 interface EnsureCwdIsProjectRootApi {
   cwdIsProjectRoot: boolean
   printCwdProjectRootWarning: () => void
 }
 
-export function ensureCwdIsProjectRoot({
-  cliConfig,
-  output,
-  chalk,
-}: CliCommandContext): EnsureCwdIsProjectRootApi {
+export function ensureCwdIsProjectRoot(context: CliCommandContext): EnsureCwdIsProjectRootApi {
+  const {cliConfig, output, chalk, workDir} = context
+
   return {
     cwdIsProjectRoot: typeof cliConfig !== 'undefined',
     printCwdProjectRootWarning() {
@@ -21,6 +21,20 @@ export function ensureCwdIsProjectRoot({
           ].join('\n'),
         ),
       )
+
+      if (isModernCliConfig(context) && context.projectRootPath) {
+        output.print()
+        output.print(
+          chalk.grey(`We found a Studio project at ${chalk.cyan(context.projectRootPath)}.`),
+        )
+        output.print(
+          chalk.grey(
+            `Run ${chalk.cyan(
+              `cd ${path.relative(workDir, context.projectRootPath)}`,
+            )} to move there, and then try this command again.`,
+          ),
+        )
+      }
     },
   }
 }
