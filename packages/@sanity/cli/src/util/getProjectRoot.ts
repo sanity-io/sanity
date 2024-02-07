bfaa35a74da3a569f391b25874102e3781e24471
Ash <ash@sanity.io>
2024-02-07 13:59:00 +0000

    fix(cli): downgrade `find-up` to avoid ESM issues

--- packages/@sanity/cli/src/util/getProjectRoot.ts
+++ packages/@sanity/cli/src/util/getProjectRoot.ts
@@ -1,9 +1,9 @@
 import {dirname} from 'node:path'
+import findUp from 'find-up'
 
 const CONFIG_FILENAMES = ['sanity.config.ts', 'sanity.config.js', 'sanity.cli.ts', 'sanity.cli.js']
 
 export async function getProjectRoot(): Promise<string | undefined> {
-  const {findUp} = await import('find-up')
   const rootConfigPath = await findUp(CONFIG_FILENAMES)
 
   if (rootConfigPath) {
