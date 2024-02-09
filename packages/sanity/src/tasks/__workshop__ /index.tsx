3400dec7cdfc0e0a92df417f64f496ff677acc9c
Nina Andal Aarvik <nina@sanity.io>
2024-02-09 14:47:15 +0100

    fix(tasks): setup workshop

---
+++ packages/sanity/src/tasks/__workshop__ /index.tsx
@@ -0,0 +1,24 @@
+import {defineScope} from '@sanity/ui-workshop'
+import {lazy} from 'react'
+
+export default defineScope({
+  name: 'tasks',
+  title: 'Tasks',
+  stories: [
+    {
+      name: 'tasks-layout',
+      title: 'TasksLayout',
+      component: lazy(() => import('./TasksLayoutStory')),
+    },
+    {
+      name: 'tasks-create',
+      title: 'TasksCreate',
+      component: lazy(() => import('./TasksCreateStory')),
+    },
+    // {
+    //   name: 'tasks-tablist',
+    //   title: 'TabsList',
+    //   component: lazy(() => import('./TasksTabsStory')),
+    // },
+  ],
+})
