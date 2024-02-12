5a8ee4eebdcdea021f376535806dd7c8139871fe
Fred Carlsen <fred@sjelfull.no>
2024-02-12 17:50:37 +0100

    fix(tasks): reset to created tab on delete
    Signed-off-by: Fred Carlsen <fred@sjelfull.no>

--- packages/sanity/src/tasks/src/tasks/components/edit/TaskEdit.tsx
+++ packages/sanity/src/tasks/src/tasks/components/edit/TaskEdit.tsx
@@ -4,16 +4,17 @@ import {useTasks} from '../../context'
 import {TasksCreate} from '../create/TasksCreate'
 
 interface TaskEditProps {
+  onDelete: () => void
   onCancel: () => void
   selectedTask: string | null
 }
 export function TaskEdit(props: TaskEditProps) {
-  const {onCancel, selectedTask} = props
+  const {onCancel, onDelete, selectedTask} = props
   const {data} = useTasks()
   const task = useMemo(() => data.find((t) => t._id === selectedTask), [selectedTask, data])
   if (!task) {
     return <Text>Task not found</Text>
   }
 
-  return <TasksCreate onCancel={onCancel} mode="edit" initialValues={task} />
+  return <TasksCreate onCancel={onCancel} onDelete={onDelete} mode="edit" initialValues={task} />
 }
