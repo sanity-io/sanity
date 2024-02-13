51ed18d28f14dfeabae2b100a8111693bb4a821c
Nina Andal Aarvik <nina@sanity.io>
2024-02-13 10:35:23 +0100

    fix(structure): use useDocumentTitle hook for toast message (#5719)

--- packages/sanity/src/structure/panes/document/DocumentOperationResults.tsx
+++ packages/sanity/src/structure/panes/document/DocumentOperationResults.tsx
@@ -5,20 +5,21 @@ import {Translate, useDocumentOperationEvent, useTranslation} from 'sanity'
 import {usePaneRouter} from '../../components'
 import {structureLocaleNamespace} from '../../i18n'
 import {useDocumentPane} from './useDocumentPane'
+import {useDocumentTitle} from './useDocumentTitle'
 
 const IGNORE_OPS = ['patch', 'commit']
 
 export const DocumentOperationResults = memo(function DocumentOperationResults() {
   const {push: pushToast} = useToast()
-  const {documentId, documentType, displayed} = useDocumentPane()
+  const {documentId, documentType} = useDocumentPane()
+  const {title} = useDocumentTitle()
   const event: any = useDocumentOperationEvent(documentId, documentType)
   const prevEvent = useRef(event)
   const paneRouter = usePaneRouter()
   const {t} = useTranslation(structureLocaleNamespace)
 
   //Truncate the document title and add "..." if it is over 25 characters
-  const documentTitleBase =
-    (displayed?.title as string) || t('panes.document-operation-results.operation-undefined-title')
+  const documentTitleBase = title || t('panes.document-operation-results.operation-undefined-title')
   const documentTitle =
     documentTitleBase.length > 25 ? `${documentTitleBase.slice(0, 25)}...` : documentTitleBase
 
