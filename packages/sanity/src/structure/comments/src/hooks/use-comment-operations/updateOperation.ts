71dc2b4759ce36b54c1920e03576dd2b9ef161d0
Herman Wikner <wiknerherman@gmail.com>
2024-02-14 16:38:09 +0100

    feat(comments): add optional throttle option to update operation

--- packages/sanity/src/structure/comments/src/hooks/use-comment-operations/updateOperation.ts
+++ packages/sanity/src/structure/comments/src/hooks/use-comment-operations/updateOperation.ts
@@ -1,18 +1,54 @@
 import {type SanityClient} from '@sanity/client'
 import {uuid} from '@sanity/uuid'
+import {throttle} from 'lodash'
 
 import {type CommentUpdatePayload} from '../../types'
+
+const THROTTLE_TIME_MS = 1000
 
 interface UpdateOperationProps {
   client: SanityClient
   comment: CommentUpdatePayload
+  throttle: boolean | undefined
   id: string
   onUpdate?: (id: string, comment: CommentUpdatePayload) => void
   transactionId: string | undefined
 }
 
+async function postCommentUpdate(props: UpdateOperationProps) {
+  const {client, id, comment, transactionId: transactionIdProp} = props
+
+  // Fall back to generating a new transaction id if none is provided
+  const transactionId = transactionIdProp || uuid()
+  const patch = client?.patch(id).set(comment)
+  const transaction = client.transaction().transactionId(transactionId).patch(patch)
+
+  // If the update contains a status, we'll update the status of all replies
+  // to the comment as well.
+  if (comment.status) {
+    await transaction.commit()
+
+    await client
+      .patch({query: `*[_type == "comment" && parentCommentId == "${id}"]`})
+      .set({
+        status: comment.status,
+      })
+      .commit()
+
+    return
+  }
+
+  // Else we'll just update the comment itself
+  await transaction.commit()
+}
+
+const throttlePostCommentUpdate = throttle(postCommentUpdate, THROTTLE_TIME_MS, {
+  trailing: true,
+  leading: false,
+})
+
 export async function updateOperation(props: UpdateOperationProps): Promise<void> {
-  const {client, id, comment, onUpdate, transactionId: transactionIdProp} = props
+  const {id, comment, onUpdate, throttle: throttleProp} = props
 
   const hasEditedMessage = 'message' in comment
 
@@ -27,26 +63,16 @@ export async function updateOperation(props: UpdateOperationProps): Promise<void
 
   onUpdate?.(id, nextComment)
 
-  // Fall back to generating a new transaction id if none is provided
-  const transactionId = transactionIdProp || uuid()
-
-  const patch = client?.patch(id).set(nextComment)
-  const transaction = client.transaction().transactionId(transactionId).patch(patch)
-
-  // If the update contains a status, we'll update the status of all replies
-  // to the comment as well.
-  if (comment.status) {
-    await transaction.commit()
-    await client
-      .patch({query: `*[_type == "comment" && parentCommentId == "${id}"]`})
-      .set({
-        status: comment.status,
-      })
-      .commit()
-
+  if (!throttleProp) {
+    await postCommentUpdate({
+      ...props,
+      comment: nextComment,
+    })
     return
   }
 
-  // Else we'll just update the comment itself
-  await transaction.commit()
+  await throttlePostCommentUpdate({
+    ...props,
+    comment: nextComment,
+  })
 }
