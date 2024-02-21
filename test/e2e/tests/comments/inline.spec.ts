dfcce0d08e52f5573863ba50a1f9ea863a231ba0
Herman Wikner <wiknerherman@gmail.com>
2024-02-21 13:41:07 +0100

    test(comments): update inline comment creation e2e

--- test/e2e/tests/comments/inline.spec.ts
+++ test/e2e/tests/comments/inline.spec.ts
@@ -1,6 +1,10 @@
 import {expect} from '@playwright/test'
 import {test} from '@sanity/test'
 
+const WAIT_OPTIONS = {
+  timeout: 20 * 1000, // 20 seconds
+}
+
 // This text is in the initial value of the body field of the document (i.e the "commentsCI" document type)
 const PTE_CONTENT_TEXT = 'This is some text in the body field'
 
@@ -9,27 +13,30 @@ test.describe('Inline comments:', () => {
     // 1. Create a new draft document
     await createDraftDocument('/test/content/input-ci;commentsCI')
 
-    // 2. Activate the editor interface to ensure it's ready for user interaction.
+    // 2. Click the overlay to active the editor.
+    await page.waitForSelector('[data-testid="activate-overlay"]', WAIT_OPTIONS)
     await page.click('[data-testid="activate-overlay"]')
 
-    // 3. Locate and select the text within the editor that the comment will be attached to.
+    // 3. Select all text in the editor.
     const editor = page.locator('[data-testid="pt-editor"]')
     const textBox = editor.locator('div[role="textbox"]')
     textBox.selectText()
 
-    // 4. Click the button to initiate the comment process, opening the input area for the comment.
-    await page.waitForSelector('[data-testid="inline-comment-button"]')
+    // 4. Click on the floating comment button to start authoring a comment.
+    await page.waitForSelector('[data-testid="inline-comment-button"]', WAIT_OPTIONS)
     const button = page.locator('[data-testid="inline-comment-button"]')
     button.click()
 
     // 5. Ensure the comment input field is displayed and the selected text is visually marked for commenting.
-    await page.waitForSelector('[data-inline-comment-state="authoring"]')
+    await page.waitForSelector('[data-testid="comment-input"]', WAIT_OPTIONS)
     const commentInput = page.locator('[data-testid="comment-input"]')
-    const authoringDecorator = page.locator('[data-inline-comment-state="authoring"]')
-    await expect(authoringDecorator).toHaveText(PTE_CONTENT_TEXT)
     await expect(commentInput).toBeVisible()
 
-    // 6. Author the comment and submit it using the send button.
+    await page.waitForSelector('[data-inline-comment-state="authoring"]', WAIT_OPTIONS)
+    const authoringDecorator = page.locator('[data-inline-comment-state="authoring"]')
+    await expect(authoringDecorator).toHaveText(PTE_CONTENT_TEXT)
+
+    // 6. Author the comment and submit it by clicking the send button.
     const commentInputTextBox = commentInput.locator('div[role="textbox"]')
     await commentInputTextBox.fill('This is a comment')
     const sendButton = commentInput.locator('[data-testid="comment-input-send-button"]')
