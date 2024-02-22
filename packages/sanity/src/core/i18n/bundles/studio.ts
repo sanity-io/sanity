0794d72069360e281b0cb90c394e7598e3aa0dc4
Nina Andal Aarvik <nina@sanity.io>
2024-02-22 16:15:26 +0100

    fix(core): add aria label to action button pte toolbar (#5653)
    * fix(core): add aria label to action button pte toolbar
    
    * fix(core): change aria label for pte toolbar action buttons
    
    * fix(core): change aria-label

--- packages/sanity/src/core/i18n/bundles/studio.ts
+++ packages/sanity/src/core/i18n/bundles/studio.ts
@@ -1589,6 +1589,8 @@ export const studioLocaleStrings = defineLocalesResources('studio', {
   'timeline.since': 'Since: {{timestamp, datetime}}',
   /** Label for missing change version for timeline menu dropdown are showing */
   'timeline.since-version-missing': 'Since: unknown version',
+  /**Aria label for the action buttons in the PTE toolbar */
+  'toolbar.portable-text.action-button-aria-label': '{{action}}',
   /** Label for the button showed after trial ended */
   'user-menu.action.free-trial-finished': 'Upgrade from free',
   /** Label for button showing the free trial days left */
