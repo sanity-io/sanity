5f771158351dccda299024254829ac870904f94a
Bjørge Næss <bjoerge@gmail.com>
2024-02-23 18:01:39 +0100

    Revert "fix(core): add aria label to action button pte toolbar (#5653)"
    This reverts commit 0794d72069360e281b0cb90c394e7598e3aa0dc4.

--- packages/sanity/src/core/form/inputs/PortableText/toolbar/ActionMenu.tsx
+++ packages/sanity/src/core/form/inputs/PortableText/toolbar/ActionMenu.tsx
@@ -89,9 +89,6 @@ export const ActionMenu = memo(function ActionMenu(props: ActionMenuProps) {
         const active = activeKeys.includes(action.key)
         return (
           <CollapseMenuButton
-            aria-label={t('toolbar.portable-text.action-button-aria-label', {
-              action: action.title || action.key,
-            })}
             data-testid={`action-button-${action.key}`}
             disabled={disabled || annotationDisabled}
             mode="bleed"
