a14572386725288443b5dbd5217022b1a3d9baac
Ash <ash@sanity.io>
2024-03-04 10:27:02 +0000

    refactor(core): remove unused offset

--- packages/sanity/src/core/studio/components/navbar/search/contexts/search/SearchProvider.tsx
+++ packages/sanity/src/core/studio/components/navbar/search/contexts/search/SearchProvider.tsx
@@ -176,7 +176,6 @@ export function SearchProvider({children, fullscreen}: SearchProviderProps) {
             `findability-filter-count:${completeFilters.length}`,
           ],
           limit: SEARCH_LIMIT,
-          // offset: pageIndex * SEARCH_LIMIT,
           skipSortByScore: ordering.ignoreScore,
           sort: [ordering.sort],
           cursor: cursor || undefined,
