8a4acacf180b771a95916b4b8e94b720d5553739
Pedro Bonamin <pedrobonamin@gmail.com>
2024-03-04 12:03:29 +0100

    fix(tasks): updates post rebase with base branch

--- packages/sanity/src/tasks/__workshop__/TasksCreateStory.tsx
+++ packages/sanity/src/tasks/__workshop__/TasksCreateStory.tsx
@@ -2,14 +2,11 @@ import {AddonDatasetProvider} from 'sanity'
 
 import {TasksForm, TasksProvider} from '../src'
 
-function noop() {
-  return null
-}
 export default function TasksCreateStory() {
   return (
     <AddonDatasetProvider>
       <TasksProvider>
-        <TasksForm onCancel={noop} mode="create" />
+        <TasksForm documentId="fake-id" mode="create" />
       </TasksProvider>
     </AddonDatasetProvider>
   )
