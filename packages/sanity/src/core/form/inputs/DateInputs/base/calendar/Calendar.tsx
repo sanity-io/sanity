cc38891a88bc3ff06691b5a4805f44ef226b8815
Tommy Petty <tommy.petty@gmail.com>
2024-03-05 13:52:06 -0500

    fix(core): remove prev and next month buttons in calendar input

--- packages/sanity/src/core/form/inputs/DateInputs/base/calendar/Calendar.tsx
+++ packages/sanity/src/core/form/inputs/DateInputs/base/calendar/Calendar.tsx
@@ -202,7 +202,6 @@ export const Calendar = forwardRef(function Calendar(
         <Flex>
           <Box flex={1}>
             <CalendarMonthSelect
-              moveFocusedDate={moveFocusedDate}
               onChange={handleFocusedMonthChange}
               labels={{
                 goToPreviousMonth: labels.goToPreviousMonth,
@@ -333,7 +332,6 @@ function CalendarTimePresetButton(props: {
 }
 
 function CalendarMonthSelect(props: {
-  moveFocusedDate: (by: number) => void
   onChange: (e: FormEvent<HTMLSelectElement>) => void
   value?: number
   monthNames: MonthNames
@@ -342,25 +340,10 @@ function CalendarMonthSelect(props: {
     goToNextMonth: string
   }
 }) {
-  const {moveFocusedDate, onChange, value, labels, monthNames} = props
-
-  const handlePrevMonthClick = useCallback(() => moveFocusedDate(-1), [moveFocusedDate])
-
-  const handleNextMonthClick = useCallback(() => moveFocusedDate(1), [moveFocusedDate])
+  const {onChange, value, labels, monthNames} = props
 
   return (
     <Flex flex={1} gap={1}>
-      <Button
-        aria-label={labels.goToPreviousMonth}
-        onClick={handlePrevMonthClick}
-        mode="bleed"
-        icon={ChevronLeftIcon}
-        tooltipProps={{content: 'Previous month'}}
-        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-        // @ts-ignore - Button with specific styling requirements
-        {...CALENDAR_ICON_BUTTON_PROPS}
-      />
-
       <Box flex={1}>
         <Select fontSize={1} radius={2} value={value} onChange={onChange} padding={2}>
           {monthNames.map((monthName, i) => (
@@ -371,16 +354,6 @@ function CalendarMonthSelect(props: {
           ))}
         </Select>
       </Box>
-      <Button
-        aria-label={labels.goToNextMonth}
-        mode="bleed"
-        icon={ChevronRightIcon}
-        onClick={handleNextMonthClick}
-        tooltipProps={{content: 'Next month'}}
-        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-        // @ts-ignore - Button with specific styling requirements
-        {...CALENDAR_ICON_BUTTON_PROPS}
-      />
     </Flex>
   )
 }
