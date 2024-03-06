c8f7e32b1d543c807296f3bfa19ff03a69248782
Sindre Gulseth <sgulseth@gmail.com>
2024-03-06 14:51:53 +0100

    feat: add cmd to generate a JSON representation of schema

---
+++ packages/sanity/src/_internal/cli/commands/schema/extractSchemaCommand.ts
@@ -0,0 +1,32 @@
+import {type CliCommandDefinition} from '@sanity/cli'
+
+const description = 'Extracts a JSON representation of a Sanity schema within a Studio context.'
+
+const helpText = `
+**Note**: This command is experimental and subject to change.
+
+Options
+  --workspace <name> The name of the workspace to generate a schema for
+  --path Optional path to specify destination of the schema file
+  --enforce-required-fields Makes the schema generated treat fields marked as required as non-optional. Defaults to false.
+
+Examples
+  # Extracts schema types in a Sanity project with more than one workspace
+  sanity schema extract --workspace default
+`
+
+const extractSchemaCommand: CliCommandDefinition = {
+  name: 'extract',
+  group: 'schema',
+  signature: '',
+  description,
+  helpText,
+  hideFromHelp: true,
+  action: async (args, context) => {
+    const mod = await import('../../actions/schema/extractAction')
+
+    return mod.default(args, context)
+  },
+} satisfies CliCommandDefinition
+
+export default extractSchemaCommand
---
+++ packages/sanity/src/_internal/cli/threads/extractSchema.ts
@@ -0,0 +1,73 @@
+import {isMainThread, parentPort, workerData as _workerData} from 'node:worker_threads'
+
+import {extractSchema} from '@sanity/schema/_internal'
+import {type Workspace} from 'sanity'
+
+import {getStudioWorkspaces} from '../util/getStudioWorkspaces'
+import {mockBrowserEnvironment} from '../util/mockBrowserEnvironment'
+
+export interface ExtractSchemaWorkerData {
+  workDir: string
+  workspaceName?: string
+  enforceRequiredFields?: boolean
+}
+
+export interface ExtractSchemaWorkerResult {
+  schema: ReturnType<typeof extractSchema>
+}
+
+if (isMainThread || !parentPort) {
+  throw new Error('This module must be run as a worker thread')
+}
+
+const opts = _workerData as ExtractSchemaWorkerData
+const cleanup = mockBrowserEnvironment(opts.workDir)
+
+async function main() {
+  try {
+    const workspaces = await getStudioWorkspaces({basePath: opts.workDir})
+
+    const workspace = getWorkspace({workspaces, workspaceName: opts.workspaceName})
+
+    const {types} = workspace.schema._original || {types: []}
+
+    const schema = extractSchema(types, {
+      enforceRequiredFields: opts.enforceRequiredFields,
+    })
+
+    parentPort?.postMessage({
+      schema,
+    } satisfies ExtractSchemaWorkerResult)
+  } finally {
+    cleanup()
+  }
+}
+
+main()
+
+function getWorkspace({
+  workspaces,
+  workspaceName,
+}: {
+  workspaces: Workspace[]
+  workspaceName?: string
+}): Workspace {
+  if (workspaces.length === 0) {
+    throw new Error('No studio configuration found')
+  }
+
+  if (workspaces.length === 1) {
+    return workspaces[0]
+  }
+
+  if (workspaceName === undefined) {
+    throw new Error(
+      `Multiple workspaces found. Please specify which workspace to use with '--workspace'.`,
+    )
+  }
+  const workspace = workspaces.find((w) => w.name === workspaceName)
+  if (!workspace) {
+    throw new Error(`Could not find workspace "${workspaceName}"`)
+  }
+  return workspace
+}
