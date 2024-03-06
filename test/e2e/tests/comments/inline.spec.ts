777b035088e05fee6c2fda88e65a5435b94c3bb1
Herman Wikner <wiknerherman@gmail.com>
2024-03-06 15:46:51 +0100

    test(comments): remove comment in e2e test

--- test/e2e/tests/comments/inline.spec.ts
+++ test/e2e/tests/comments/inline.spec.ts
@@ -84,7 +84,6 @@ async function inlineCommentCreationTest(props: InlineCommentCreationTestProps) 
   await expect(commentsListItemReferencedValue).toHaveText(PTE_CONTENT_TEXT)
 }
 
-// The tests will be enabled when the inline comments feature is ready to be used.
 test.describe('Inline comments:', () => {
   test('should create inline comment', async ({page, createDraftDocument, browserName}) => {
     // For now, only test in Chromium due to flakiness in Firefox and WebKit
