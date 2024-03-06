b94decd27a7e2b28a0de8176f0955b8a288b36bf
Carolina Gonzalez <carolina.nicole.gonzalez@gmail.com>
2024-03-06 12:16:22 -0500

    refactor: simplify backends per feedback

--- packages/sanity/src/core/store/key-value/backends/server.ts
+++ packages/sanity/src/core/store/key-value/backends/server.ts
@@ -20,98 +20,56 @@ export function serverBackend({client: _client}: ServerBackendOptions): Backend 
   const client = _client.withConfig({...DEFAULT_STUDIO_CLIENT_OPTIONS, apiVersion: 'vX'})
 
   const keyValueLoader = new DataLoader<string, KeyValueStoreValue | null>(async (keys) => {
-    const value = await client
+    const keyValuePairs = await client
       .request<KeyValuePair[]>({
         uri: `/users/me/keyvalue/${keys.join(',')}`,
         withCredentials: true,
       })
       .catch((error) => {
-        if (error.response?.statusCode === 404) {
-          return Array(keys.length).fill(null)
-        }
-        throw error
+        console.error('Error fetching data:', error)
+        return Array(keys.length).fill(null)
       })
-    const keyValuePairs = value.reduce(
-      (acc, next) => {
-        if (next?.key) {
-          acc[next.key] = next.value
-        }
-        return acc
-      },
-      {} as Record<string, KeyValueStoreValue | null>,
-    )
 
-    const result = keys.map((key) => keyValuePairs[key] || null)
-    return result
+    return keyValuePairs.map((pair) => pair.value)
   })
 
+  const getKeys = (keys: string[]) => {
+    return from(keyValueLoader.loadMany(keys))
+  }
+
+  const setKeys = (keyValuePairs: KeyValuePair[]) => {
+    return from(
+      client.request<KeyValuePair[]>({
+        method: 'PUT',
+        uri: `/users/me/keyvalue`,
+        body: keyValuePairs,
+        withCredentials: true,
+      }),
+    ).pipe(
+      map((response) => {
+        response.forEach((pair) => {
+          keyValueLoader.clear(pair.key)
+          keyValueLoader.prime(pair.key, pair.value)
+        })
+        return response.map((pair) => pair.value)
+      }),
+      catchError((error) => {
+        console.error('Error setting data:', error)
+        return of(Array(keyValuePairs.length).fill(null))
+      }),
+    )
+  }
+
   return {
-    getKey: (key: string, defaultValue: unknown) => {
-      return from(keyValueLoader.load(key)).pipe(
-        map((value) => {
-          return value ?? defaultValue
-        }),
-        catchError((error) => {
-          console.error('Error fetching data:', error)
-          return of(defaultValue)
-        }),
-      )
+    getKey: (key: string) => {
+      return getKeys([key]).pipe(map((values) => values[0]))
     },
     setKey: (key: string, nextValue: unknown) => {
-      return from(
-        client.request<KeyValuePair[]>({
-          method: 'PUT',
-          uri: `/users/me/keyvalue`,
-          body: [{key, value: nextValue}],
-          withCredentials: true,
-        }),
-      ).pipe(
-        map((response) => {
-          const newValue = response[0].value
-          if (newValue) {
-            keyValueLoader.clear(key)
-            keyValueLoader.prime(key, newValue)
-          }
-          return newValue
-        }),
-        catchError((error) => {
-          console.error('Error setting data:', error)
-          return of(null)
-        }),
+      return setKeys([{key, value: nextValue as KeyValueStoreValue}]).pipe(
+        map((values) => values[0]),
       )
     },
-    getKeys: (keys: string[], defValues: unknown[]) => {
-      return from(keyValueLoader.loadMany(keys)).pipe(
-        map((values) => {
-          return values.map((value, i) => value ?? defValues[i])
-        }),
-        catchError((error) => {
-          console.error('Error fetching data:', error)
-          return of(defValues)
-        }),
-      )
-    },
-    setKeys: (keyValuePairs: KeyValuePair[]) => {
-      return from(
-        client.request<KeyValuePair[]>({
-          method: 'PUT',
-          uri: `/users/me/keyvalue`,
-          body: keyValuePairs,
-          withCredentials: true,
-        }),
-      ).pipe(
-        map((response) => {
-          response.forEach((pair) => {
-            keyValueLoader.clear(pair.key)
-            keyValueLoader.prime(pair.key, pair.value)
-          })
-          return response.map((pair: KeyValuePair) => pair.value)
-        }),
-        catchError((error) => {
-          console.error('Error setting data:', error)
-          return of(Array(keyValuePairs.length).fill(null))
-        }),
-      )
-    },
+    getKeys,
+    setKeys,
   }
 }
