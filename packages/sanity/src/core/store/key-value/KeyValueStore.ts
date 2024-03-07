46dcaa8e49245c57e9171c9919387112a97b0053
cngonzalez <carolina.nicole.gonzalez@gmail.com>
2024-03-07 10:12:30 -0700

    feat(core): fetch structure tool settings from backend (#5901)

--- packages/sanity/src/core/store/key-value/KeyValueStore.ts
+++ packages/sanity/src/core/store/key-value/KeyValueStore.ts
@@ -1,18 +1,19 @@
-import {merge, type Observable, Subject} from 'rxjs'
+import {type SanityClient} from '@sanity/client'
+import {merge, Observable, Subject} from 'rxjs'
 import {filter, map, switchMap} from 'rxjs/operators'
 
-import {resolveBackend} from './backends/resolve'
+import {serverBackend} from './backends/server'
 import {type KeyValueStore, type KeyValueStoreValue} from './types'
 
 /** @internal */
-export function createKeyValueStore(): KeyValueStore {
-  const storageBackend = resolveBackend()
+export function createKeyValueStore({client}: {client: SanityClient}): KeyValueStore {
+  const storageBackend = serverBackend({client})
 
   const setKey$ = new Subject<{key: string; value: KeyValueStoreValue}>()
 
   const updates$ = setKey$.pipe(
     switchMap((event) =>
-      storageBackend.set(event.key, event.value).pipe(
+      storageBackend.setKey(event.key, event.value).pipe(
         map((nextValue) => ({
           key: event.key,
           value: nextValue,
@@ -21,12 +22,9 @@ export function createKeyValueStore(): KeyValueStore {
     ),
   )
 
-  const getKey = (
-    key: string,
-    defaultValue: KeyValueStoreValue,
-  ): Observable<KeyValueStoreValue> => {
+  const getKey = (key: string): Observable<KeyValueStoreValue> => {
     return merge(
-      storageBackend.get(key, defaultValue),
+      storageBackend.getKey(key),
       updates$.pipe(
         filter((update) => update.key === key),
         map((update) => update.value),
@@ -34,8 +32,28 @@ export function createKeyValueStore(): KeyValueStore {
     ) as Observable<KeyValueStoreValue>
   }
 
-  const setKey = (key: string, value: KeyValueStoreValue) => {
+  const setKey = (key: string, value: KeyValueStoreValue): Observable<KeyValueStoreValue> => {
+    /*
+     * The backend returns the result of the set operation, so we can just pass that along.
+     * Most utils do not use it (they will take advantage of local state first) but it reflects the
+     * backend function and could be useful for debugging.
+     */
+    const response = new Observable<KeyValueStoreValue>((subscriber) => {
+      const subscription = storageBackend.setKey(key, value).subscribe({
+        next: (nextValue) => {
+          subscriber.next(nextValue as KeyValueStoreValue)
+          subscriber.complete()
+        },
+        //storageBackend should handle its own errors, we're just passing along the result.
+        error: (err) => {
+          subscriber.error(err)
+        },
+      })
+      return () => subscription.unsubscribe()
+    })
+
     setKey$.next({key, value})
+    return response
   }
 
   return {getKey, setKey}
