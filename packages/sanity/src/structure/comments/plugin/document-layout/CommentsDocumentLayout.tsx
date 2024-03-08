8b0d1ccf69790953a13294a3111d2c3c246fb9d6
Herman Wikner <wiknerherman@gmail.com>
2024-03-08 14:11:23 +0100

    fix(comments): remove eslint ignore comment

--- packages/sanity/src/structure/comments/plugin/document-layout/CommentsDocumentLayout.tsx
+++ packages/sanity/src/structure/comments/plugin/document-layout/CommentsDocumentLayout.tsx
@@ -43,7 +43,6 @@ function CommentsDocumentLayoutInner(props: DocumentLayoutProps) {
       documentType={documentType}
       isCommentsOpen={inspector?.name === COMMENTS_INSPECTOR_NAME}
       onCommentsOpen={handleOpenCommentsInspector}
-      // eslint-disable-next-line @sanity/i18n/no-attribute-string-literals
       sortOrder="desc"
       type="field"
     >
