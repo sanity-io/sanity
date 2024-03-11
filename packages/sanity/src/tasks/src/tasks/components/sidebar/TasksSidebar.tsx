76fa4706d1069419c6545682c4b2042acae7309c
Herman Wikner <wiknerherman@gmail.com>
2024-03-11 16:34:07 +0100

    feat(tasks): responsive sidebar (#5949)

--- packages/sanity/src/tasks/src/tasks/components/sidebar/TasksSidebar.tsx
+++ packages/sanity/src/tasks/src/tasks/components/sidebar/TasksSidebar.tsx
@@ -1,5 +1,4 @@
 import {Box, Card, Flex, Spinner} from '@sanity/ui'
-import {AnimatePresence, motion, type Transition, type Variants} from 'framer-motion'
 import {useCallback, useMemo} from 'react'
 import {useCurrentUser} from 'sanity'
 import styled from 'styled-components'
@@ -10,11 +9,7 @@ import {TaskSidebarContent} from './TasksSidebarContent'
 import {TasksSidebarHeader} from './TasksSidebarHeader'
 
 const SidebarRoot = styled(Card)`
-  width: 360px;
   flex: 1;
-  box-shadow:
-    0px 6px 8px -4px rgba(134, 144, 160, 0.2),
-    0px 12px 17px -1px rgba(134, 144, 160, 0.14);
 `
 
 const SidebarContent = styled.div`
@@ -22,20 +17,13 @@ const SidebarContent = styled.div`
   overflow: scroll;
 `
 
-const VARIANTS: Variants = {
-  hidden: {opacity: 0, x: 0},
-  visible: {opacity: 1, x: 0},
-}
-
-const TRANSITION: Transition = {duration: 0.2}
-
 /**
  * @internal
  */
 export function TasksStudioSidebarInner() {
   const {activeDocument, data, isLoading} = useTasks()
   const {state, setActiveTab, setViewMode} = useTasksNavigation()
-  const {activeTabId, viewMode, isOpen, selectedTask} = state
+  const {activeTabId, viewMode, selectedTask} = state
   const currentUser = useCurrentUser()
 
   const onTaskSelect = useCallback((id: string) => setViewMode({type: 'edit', id}), [setViewMode])
@@ -58,37 +46,32 @@ export function TasksStudioSidebarInner() {
   }, [activeDocument?.documentId, activeTabId, data, currentUser])
 
   return (
-    <AnimatePresence initial={false}>
-      {isOpen && (
-        <motion.div variants={VARIANTS} transition={TRANSITION} initial="hidden" animate="visible">
-          <SidebarRoot borderLeft height="fill" marginLeft={1}>
-            <TasksSidebarHeader items={filteredList} />
-            <SidebarContent>
-              {viewMode === 'list' ? (
-                <>
-                  {isLoading ? (
-                    <Box padding={3}>
-                      <Flex align="center" justify="center">
-                        <Spinner />
-                      </Flex>
-                    </Box>
-                  ) : (
-                    <TaskSidebarContent
-                      items={filteredList}
-                      onTaskSelect={onTaskSelect}
-                      setActiveTabId={setActiveTab}
-                      activeTabId={activeTabId}
-                    />
-                  )}
-                </>
-              ) : (
-                <TasksFormBuilder key={selectedTask} />
-              )}
-            </SidebarContent>
-          </SidebarRoot>
-        </motion.div>
-      )}
-    </AnimatePresence>
+    <SidebarRoot height="fill" flex={1}>
+      <TasksSidebarHeader items={filteredList} />
+
+      <SidebarContent>
+        {viewMode === 'list' ? (
+          <>
+            {isLoading ? (
+              <Box padding={3}>
+                <Flex align="center" justify="center">
+                  <Spinner />
+                </Flex>
+              </Box>
+            ) : (
+              <TaskSidebarContent
+                items={filteredList}
+                onTaskSelect={onTaskSelect}
+                setActiveTabId={setActiveTab}
+                activeTabId={activeTabId}
+              />
+            )}
+          </>
+        ) : (
+          <TasksFormBuilder key={selectedTask} />
+        )}
+      </SidebarContent>
+    </SidebarRoot>
   )
 }
 
