ffdc66a5df260bc3af3c2561a83e6abd547f160d
cngonzalez <carolina.nicole.gonzalez@gmail.com>
2024-03-12 06:42:36 -0700

    feat(core): store recent search history in backend (#5940)

--- test/e2e/tests/navbar/search.spec.ts
+++ test/e2e/tests/navbar/search.spec.ts
@@ -1,7 +1,14 @@
 import {expect} from '@playwright/test'
 import {test} from '@sanity/test'
 
-test('searching creates saved searches', async ({page, createDraftDocument, baseURL}) => {
+const SEARCH_KEY = 'studio.search.recent'
+test('searching creates saved searches', async ({
+  page,
+  createDraftDocument,
+  baseURL,
+  sanityClient,
+}) => {
+  const {dataset} = sanityClient.config()
   await createDraftDocument('/test/content/book')
 
   await page.getByTestId('field-title').getByTestId('string-input').fill('A searchable title')
@@ -12,17 +19,13 @@ test('searching creates saved searches', async ({page, createDraftDocument, base
   await page.getByTestId('search-results').click()
 
   //search query should be saved
-  const localStorage = await page.evaluate(() => window.localStorage)
-  const keyMatch = Object.keys(localStorage).find((key) => key.startsWith('search::recent'))
-  const savedSearches = JSON.parse(localStorage[keyMatch!]).recentSearches
+  const savedSearches = await sanityClient
+    .request({
+      uri: `/users/me/keyvalue/${SEARCH_KEY}.${dataset}`,
+      withCredentials: true,
+    })
+    .then((res) => res.recentSearches)
   expect(savedSearches[0].terms.query).toBe('A se')
-
-  //search query should be saved after browsing
-  await page.goto('https://example.com')
-  await page.goto(baseURL ?? '/test/content')
-  const postNavigationLocalStorage = await page.evaluate(() => window.localStorage)
-  const postNavigationSearches = JSON.parse(postNavigationLocalStorage[keyMatch!]).recentSearches
-  expect(postNavigationSearches[0].terms.query).toBe('A se')
 
   //search should save multiple queries
   await page.getByTestId('studio-search').click()
@@ -36,8 +39,12 @@ test('searching creates saved searches', async ({page, createDraftDocument, base
   await page.getByTestId('search-results').isVisible()
   await page.getByTestId('search-results').click()
 
-  const secondSearchStorage = await page.evaluate(() => window.localStorage)
-  const secondSearches = JSON.parse(secondSearchStorage[keyMatch!]).recentSearches
+  const secondSearches = await sanityClient
+    .request({
+      uri: `/users/me/keyvalue/${SEARCH_KEY}.${dataset}`,
+      withCredentials: true,
+    })
+    .then((res) => res.recentSearches)
   expect(secondSearches[0].terms.query).toBe('A searchable')
   expect(secondSearches[1].terms.query).toBe('A search')
   expect(secondSearches[2].terms.query).toBe('A se')
