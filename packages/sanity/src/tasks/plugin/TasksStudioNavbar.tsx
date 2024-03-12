31263cad7c9d672755a6467d6ef8f68c5ba7c8c5
Herman Wikner <wiknerherman@gmail.com>
2024-03-12 12:39:00 +0100

    refactor(tasks): use navbar actions

--- packages/sanity/src/tasks/plugin/TasksStudioNavbar.tsx
+++ packages/sanity/src/tasks/plugin/TasksStudioNavbar.tsx
@@ -1,14 +1,53 @@
+import {PanelRightIcon, TaskIcon} from '@sanity/icons'
+import {useCallback, useMemo} from 'react'
 import {type NavbarProps} from 'sanity'
 
-import {TasksNavbarButton, useTasksEnabled} from '../src'
+import {useTasks, useTasksEnabled} from '../src'
+
+const EMPTY_ARRAY: [] = []
+
+function TasksStudioNavbarInner(props: NavbarProps) {
+  const {toggleOpen, isOpen} = useTasks()
+
+  const handleAction = useCallback(() => {
+    toggleOpen()
+  }, [toggleOpen])
+
+  const actions = useMemo((): NavbarProps['__internal_actions'] => {
+    return [
+      ...(props?.__internal_actions || EMPTY_ARRAY),
+      {
+        icon: PanelRightIcon,
+        location: 'topbar',
+        name: 'tasks-topbar',
+        onAction: handleAction,
+        selected: isOpen,
+        title: 'Tasks',
+      },
+      {
+        icon: TaskIcon,
+        location: 'sidebar',
+        name: 'tasks-sidebar',
+        onAction: handleAction,
+        selected: isOpen,
+        title: 'Tasks',
+      },
+    ]
+  }, [handleAction, isOpen, props?.__internal_actions])
+
+  return props.renderDefault({
+    ...props,
+    // eslint-disable-next-line camelcase
+    __internal_actions: actions,
+  })
+}
 
 export function TasksStudioNavbar(props: NavbarProps) {
   const {enabled} = useTasksEnabled()
 
-  if (!enabled) return props.renderDefault(props)
-  return props.renderDefault({
-    ...props,
-    // eslint-disable-next-line camelcase
-    __internal_rightSectionNode: <TasksNavbarButton />,
-  })
+  if (!enabled) {
+    return props.renderDefault(props)
+  }
+
+  return <TasksStudioNavbarInner {...props} />
 }
