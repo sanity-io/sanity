c1ab6ed30b61f266ddf92de69d09303b41fdbab9
Bjørge Næss <bjoerge@gmail.com>
2024-03-13 13:01:03 +0100

    fix: add mutate to vitest workspace

--- vitest.workspace.ts
+++ vitest.workspace.ts
@@ -1,3 +1,3 @@
 import {defineWorkspace} from 'vitest/config'
 
-export default defineWorkspace(['packages/@sanity/migrate'])
+export default defineWorkspace(['packages/@sanity/migrate', 'packages/@sanity/mutate'])
