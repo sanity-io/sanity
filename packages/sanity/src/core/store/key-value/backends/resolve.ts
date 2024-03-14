79da333dbee50dbaa19d30a5609be60377346162
Carolina Gonzalez <carolina.nicole.gonzalez@gmail.com>
2024-03-14 13:19:24 -0400

    chore: remove unused resolve logic

--- packages/sanity/src/core/store/key-value/backends/resolve.ts
+++
@@ -1,9 +0,0 @@
-import {supportsLocalStorage} from '../../../util/supportsLocalStorage'
-import {localStorageBackend} from './localStorage'
-import {memoryBackend} from './memory'
-import {type Backend} from './types'
-
-export function resolveBackend(): Backend {
-  //TODO: add check for "server"
-  return supportsLocalStorage ? localStorageBackend : memoryBackend
-}
