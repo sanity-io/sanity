5482b8e071aa3e2fcc0f46a61c660ca3d71f4b03
cngonzalez <carolina.nicole.gonzalez@gmail.com>
2024-03-14 06:16:41 -0700

    fix: prevent key value store from firing setKey for each open subscription (#5997)

--- packages/sanity/src/core/store/key-value/KeyValueStore.ts
+++ packages/sanity/src/core/store/key-value/KeyValueStore.ts
@@ -1,6 +1,6 @@
 import {type SanityClient} from '@sanity/client'
-import {merge, Observable, Subject} from 'rxjs'
-import {filter, map, switchMap} from 'rxjs/operators'
+import {merge, type Observable, Subject} from 'rxjs'
+import {filter, map, shareReplay, switchMap, take} from 'rxjs/operators'
 
 import {serverBackend} from './backends/server'
 import {type KeyValueStore, type KeyValueStoreValue} from './types'
@@ -12,14 +12,15 @@ export function createKeyValueStore({client}: {client: SanityClient}): KeyValueS
   const setKey$ = new Subject<{key: string; value: KeyValueStoreValue}>()
 
   const updates$ = setKey$.pipe(
-    switchMap((event) =>
-      storageBackend.setKey(event.key, event.value).pipe(
+    switchMap((event) => {
+      return storageBackend.setKey(event.key, event.value).pipe(
         map((nextValue) => ({
           key: event.key,
           value: nextValue,
         })),
-      ),
-    ),
+      )
+    }),
+    shareReplay(1),
   )
 
   const getKey = (key: string): Observable<KeyValueStoreValue> => {
@@ -33,27 +34,18 @@ export function createKeyValueStore({client}: {client: SanityClient}): KeyValueS
   }
 
   const setKey = (key: string, value: KeyValueStoreValue): Observable<KeyValueStoreValue> => {
+    setKey$.next({key, value})
+
     /*
      * The backend returns the result of the set operation, so we can just pass that along.
      * Most utils do not use it (they will take advantage of local state first) but it reflects the
      * backend function and could be useful for debugging.
      */
-    const response = new Observable<KeyValueStoreValue>((subscriber) => {
-      const subscription = storageBackend.setKey(key, value).subscribe({
-        next: (nextValue) => {
-          subscriber.next(nextValue as KeyValueStoreValue)
-          subscriber.complete()
-        },
-        //storageBackend should handle its own errors, we're just passing along the result.
-        error: (err) => {
-          subscriber.error(err)
-        },
-      })
-      return () => subscription.unsubscribe()
-    })
-
-    setKey$.next({key, value})
-    return response
+    return updates$.pipe(
+      filter((update) => update.key === key),
+      map((update) => update.value as KeyValueStoreValue),
+      take(1),
+    )
   }
 
   return {getKey, setKey}
