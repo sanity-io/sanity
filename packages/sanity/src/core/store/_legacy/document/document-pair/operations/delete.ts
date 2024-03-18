c5d4173fc12db769c7b8f14b853a5b2b2b5f306e
Bjørge Næss <bjoerge@gmail.com>
2024-03-18 14:01:38 +0100

    feat(sanity): use actions API when deleting documents
    Note: still work in progress, pending backend support for `skipCrossDatasetReferenceValidation`

--- packages/sanity/src/core/store/_legacy/document/document-pair/operations/delete.ts
+++ packages/sanity/src/core/store/_legacy/document/document-pair/operations/delete.ts
@@ -1,21 +1,30 @@
-import {isLiveEditEnabled} from '../utils/isLiveEditEnabled'
 import {type OperationImpl} from './types'
 
 export const del: OperationImpl<[], 'NOTHING_TO_DELETE'> = {
   disabled: ({snapshots}) => (snapshots.draft || snapshots.published ? false : 'NOTHING_TO_DELETE'),
-  execute: ({client, schema, idPair, typeName}) => {
-    const tx = client.observable.transaction().delete(idPair.publishedId)
+  execute: ({client: globalClient, schema, idPair, typeName}) => {
+    const vXClient = globalClient.withConfig({apiVersion: 'X'})
 
-    if (isLiveEditEnabled(schema, typeName)) {
-      return tx.commit({tag: 'document.delete'})
-    }
+    const {dataset} = globalClient.config()
 
-    return tx.delete(idPair.draftId).commit({
+    return vXClient.observable.request({
+      url: `/data/actions/${dataset}`,
+      method: 'post',
       tag: 'document.delete',
       // this disables referential integrity for cross-dataset references. we
       // have this set because we warn against deletes in the `ConfirmDeleteDialog`
       // UI. This operation is run when "delete anyway" is clicked
-      skipCrossDatasetReferenceValidation: true,
+      //@todo awaiting backend support for this
+      // query: {skipCrossDatasetReferenceValidation: 'true'},
+      body: {
+        actions: [
+          {
+            actionType: 'sanity.action.document.delete',
+            draftId: idPair.draftId,
+            publishedId: idPair.publishedId,
+          },
+        ],
+      },
     })
   },
 }
