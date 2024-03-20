9133b213717d06cd7b59acccc71a649cb0391f86
Pedro Bonamin <pedrobonamin@gmail.com>
2024-03-20 16:26:57 +0100

    fix(tasks): add new sanityTasks flag

--- packages/sanity/src/tasks/src/tasks/context/enabled/TasksEnabledProvider.tsx
+++ packages/sanity/src/tasks/src/tasks/context/enabled/TasksEnabledProvider.tsx
@@ -12,22 +12,33 @@ interface TaksEnabledProviderProps {
  * @internal
  */
 export function TasksEnabledProvider({children}: TaksEnabledProviderProps) {
-  const {enabled, isLoading} = useFeatureEnabled('studioTasks')
+  const {enabled, isLoading} = useFeatureEnabled('sanityTasks')
+
+  // Staging flag - keeping here until the new flag is added to staging.
+  const {enabled: stagingIsEnabled, isLoading: stagingIsLoading} = useFeatureEnabled('studioTasks')
+
   const isWorkspaceEnabled = useWorkspace().tasks?.enabled
 
   const value: TasksEnabledContextValue = useMemo(() => {
-    if (!enabled || !isWorkspaceEnabled || isLoading) {
+    if (!isWorkspaceEnabled || isLoading || stagingIsLoading) {
       return {
         enabled: false,
         mode: null,
       }
     }
+    // The staging check will be removed when the sanityTasks flag is added to staging.
+    if (stagingIsEnabled || enabled) {
+      return {
+        enabled: true,
+        mode: 'default',
+      }
+    }
 
     return {
-      enabled: true,
-      mode: 'default',
+      enabled: false,
+      mode: null,
     }
-  }, [enabled, isLoading, isWorkspaceEnabled])
+  }, [enabled, isLoading, isWorkspaceEnabled, stagingIsEnabled, stagingIsLoading])
 
   return <TasksEnabledContext.Provider value={value}>{children}</TasksEnabledContext.Provider>
 }
