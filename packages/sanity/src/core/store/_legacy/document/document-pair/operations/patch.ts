82a767dd5f3b2b6af3df3feb7adc937df0dd5432
Bjørge Næss <bjoerge@gmail.com>
2024-03-22 14:47:37 +0100

    fix(sanity): align patching with new semantics wrt creating and concurrent deletes

--- packages/sanity/src/core/store/_legacy/document/document-pair/operations/patch.ts
+++ packages/sanity/src/core/store/_legacy/document/document-pair/operations/patch.ts
@@ -1,7 +1,6 @@
 import {isLiveEditEnabled} from '../utils/isLiveEditEnabled'
 import {type OperationImpl} from './types'
 
-// todo: we could also consider exposing 'mutate' directly
 export const patch: OperationImpl<[patches: any[], initialDocument?: Record<string, any>]> = {
   disabled: (): false => false,
   execute: (
@@ -11,23 +10,35 @@ export const patch: OperationImpl<[patches: any[], initialDocument?: Record<stri
   ): void => {
     if (isLiveEditEnabled(schema, typeName)) {
       // No drafting, so patch and commit the published document
-      published.mutate([
-        published.createIfNotExists({
-          _type: typeName,
-          ...initialDocument,
-        }),
-        ...published.patch(patches),
-      ])
+      const patchMutation = published.patch(patches)
+      // Note: if the document doesn't exist on the server yet, we need to create it first. We only want to do this if we can't see it locally
+      // if it's been deleted on the server we want that to become a mutation error when submitting.
+      const mutations = snapshots.published
+        ? patchMutation
+        : [
+            published.createIfNotExists({
+              _type: typeName,
+              ...initialDocument,
+            }),
+          ]
+      // No drafting, so patch and commit the published document
+      published.mutate(mutations)
     } else {
-      draft.mutate([
-        draft.createIfNotExists({
-          ...initialDocument,
-          ...snapshots.published,
-          _id: idPair.draftId,
-          _type: typeName,
-        }),
-        ...draft.patch(patches),
-      ])
+      const patchMutation = draft.patch(patches)
+      const mutations = snapshots.draft
+        ? patchMutation
+        : [
+            // If there's no draft, the user's edits will be based on the published document in the form in front of them
+            // so before patching it we need to make sure it's created based on the current published version first.
+            draft.createIfNotExists({
+              ...initialDocument,
+              ...snapshots.published,
+              _id: idPair.draftId,
+              _type: typeName,
+            }),
+            ...patchMutation,
+          ]
+      draft.mutate(mutations)
     }
   },
 }
