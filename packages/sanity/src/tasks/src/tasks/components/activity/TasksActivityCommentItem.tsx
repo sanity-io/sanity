a4b8b2a2978286e0f3187b7b5a5b8359c2b92598
Pedro Bonamin <pedrobonamin@gmail.com>
2024-03-27 12:38:39 +0100

    fix(tasks): reduce padding on tasks comment reply edx-1209

--- packages/sanity/src/tasks/src/tasks/components/activity/TasksActivityCommentItem.tsx
+++ packages/sanity/src/tasks/src/tasks/components/activity/TasksActivityCommentItem.tsx
@@ -1,3 +1,7 @@
+// eslint-disable-next-line camelcase
+import {getTheme_v2} from '@sanity/ui/theme'
+import styled, {css} from 'styled-components'
+
 import {CommentsListItem, type CommentsListItemProps} from '../../../../../structure/comments'
 import {ActivityItem} from './TasksActivityItem'
 
@@ -12,19 +16,31 @@ interface TasksActivityCommentItemProps extends Omit<CommentsListItemProps, 'mod
   // ...
 }
 
+const CommentListItemRoot = styled.div((props) => {
+  const theme = getTheme_v2(props.theme)
+  return css`
+    [data-ui='comments-list-item'] {
+      padding-right: ${theme.space[2]}px;
+      &:focus-within {
+        padding-bottom: ${theme.space[2]}px;
+      }
+    }
+  `
+})
 export function TasksActivityCommentItem(props: TasksActivityCommentItemProps) {
   const {parentComment} = props
 
   return (
     <ActivityItem userId={parentComment.authorId}>
-      <CommentsListItem
-        {...props}
-        avatarConfig={COMMENTS_LIST_ITEM_AVATAR_CONFIG}
-        canReply
-        innerPadding={1}
-        isSelected={false}
-        mode="default" // TODO: set dynamic mode?
-      />
+      <CommentListItemRoot>
+        <CommentsListItem
+          {...props}
+          avatarConfig={COMMENTS_LIST_ITEM_AVATAR_CONFIG}
+          canReply
+          isSelected={false}
+          mode="default"
+        />
+      </CommentListItemRoot>
     </ActivityItem>
   )
 }
