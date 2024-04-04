783092baab81f0c04d95fcc8aa926682f472ad9b
Pedro Bonamin <pedrobonamin@gmail.com>
2024-04-04 14:29:43 +0200

    fix(core): persist dragHandle in grid layout

--- packages/sanity/src/core/form/inputs/arrays/common/DragHandle.tsx
+++ packages/sanity/src/core/form/inputs/arrays/common/DragHandle.tsx
@@ -44,7 +44,6 @@ export const DragHandle = function DragHandle(props: DragHandleProps) {
       {...attributes}
       {...listeners}
       disabled={readOnly}
-      // {...(readOnly ? {disabled: true} : {...attributes, ...listeners})}
     />
   )
 }
