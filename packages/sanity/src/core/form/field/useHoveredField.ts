67c52e2d96d1d8184887881e39859e8f32377fbb
Binoy Patel <me@binoy.io>
2024-04-04 16:41:49 -0400

    refactor(core): move HoveredFieldContext to singleton

--- packages/sanity/src/core/form/field/useHoveredField.ts
+++ packages/sanity/src/core/form/field/useHoveredField.ts
@@ -1,6 +1,5 @@
 import {useContext} from 'react'
-
-import {HoveredFieldContext, type HoveredFieldContextValue} from './HoveredFieldContext'
+import {HoveredFieldContext, type HoveredFieldContextValue} from 'sanity/_singleton'
 
 /** @internal */
 export function useHoveredField(): HoveredFieldContextValue {
