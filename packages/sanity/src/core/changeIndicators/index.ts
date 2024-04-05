e9788135b9ba41c10cab1785a2fd7eaa7183c5c7
Binoy Patel <me@binoy.io>
2024-04-05 14:29:52 -0400

    chore(core): remove unusued ChangeIndicatorContext (#6214)

--- packages/sanity/src/core/changeIndicators/index.ts
+++ packages/sanity/src/core/changeIndicators/index.ts
@@ -1,6 +1,5 @@
 export * from './ChangeFieldWrapper'
 export * from './ChangeIndicator'
-export * from './ChangeIndicatorContext'
 export * from './ConnectorContext'
 export * from './overlay/ChangeConnectorRoot'
 export * from './tracker'
