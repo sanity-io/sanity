548952e1abc0923a6687a8046785120bd38bb4bb
Binoy Patel <me@binoy.io>
2024-04-05 17:40:43 -0400

    refactor(core): move UserColorManagerContext to singletons

--- packages/sanity/src/_singletons/user-color/UserColorManagerContext.ts
+++ packages/sanity/src/_singletons/user-color/UserColorManagerContext.ts
@@ -1,5 +1,4 @@
 import {createContext} from 'react'
-
-import {type UserColorManager} from './types'
+import type {UserColorManager} from 'sanity'
 
 export const UserColorManagerContext = createContext<UserColorManager | null>(null)
