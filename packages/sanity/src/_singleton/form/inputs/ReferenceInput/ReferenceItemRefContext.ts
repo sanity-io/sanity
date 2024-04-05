121e08ed3025ed7a887035097a2ce95f06719550
Binoy Patel <me@binoy.io>
2024-04-05 11:44:22 -0400

    refactor(core): move ReferenceItemRefContext to singleton

---
+++ packages/sanity/src/_singleton/form/inputs/ReferenceInput/ReferenceItemRefContext.ts
@@ -0,0 +1,16 @@
+import {createContext, type MutableRefObject} from 'react'
+
+/**
+ * @internal
+ */
+export interface ReferenceItemRef {
+  menuRef: MutableRefObject<HTMLDivElement | null>
+  containerRef: MutableRefObject<HTMLDivElement | null>
+}
+
+/**
+ * This is a way to store ref of the menu as well as the container of the ReferenceItem
+ * so it can be used down the tree for clickOutside handling
+ * @internal
+ */
+export const ReferenceItemRefContext = createContext<ReferenceItemRef | null>(null)
