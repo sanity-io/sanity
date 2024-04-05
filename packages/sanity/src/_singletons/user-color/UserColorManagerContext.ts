d0789952492f7894c1f947043be352266efd2a81
Binoy Patel <me@binoy.io>
2024-04-05 17:49:34 -0400

    fix(singletons): fix build issues and move CalendarContext interface back

--- packages/sanity/src/_singletons/user-color/UserColorManagerContext.ts
+++ packages/sanity/src/_singletons/user-color/UserColorManagerContext.ts
@@ -1,4 +1,7 @@
 import {createContext} from 'react'
 import type {UserColorManager} from 'sanity'
 
+/**
+ * @internal
+ */
 export const UserColorManagerContext = createContext<UserColorManager | null>(null)
