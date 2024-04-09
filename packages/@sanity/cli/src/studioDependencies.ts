61dbbe28b6c05e943c4caf660457f82583e7ba5f
Cody Olsen <81981+stipsan@users.noreply.github.com>
2024-04-09 18:01:30 +0200

    fix: bump `styled-components` used by `npm create sanity` (#6281)

--- packages/@sanity/cli/src/studioDependencies.ts
+++ packages/@sanity/cli/src/studioDependencies.ts
@@ -10,7 +10,7 @@ export const studioDependencies = {
     // Non-Sanity dependencies
     react: '^18.2.0',
     'react-dom': '^18.2.0',
-    'styled-components': '^6.0.7',
+    'styled-components': '^6.1.8',
   },
 
   devDependencies: {
