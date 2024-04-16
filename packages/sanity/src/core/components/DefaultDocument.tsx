17a293bd970fcd358d202b3f6d3e62dfe999f67e
Binoy Patel <me@binoy.io>
2024-04-16 16:51:48 -0400

    chore: bump package url

--- packages/sanity/src/core/components/DefaultDocument.tsx
+++ packages/sanity/src/core/components/DefaultDocument.tsx
@@ -151,12 +151,12 @@ export function DefaultDocument(props: DefaultDocumentProps): ReactElement {
             dangerouslySetInnerHTML={{
               __html: `{
               "imports": {
-                "react": "https://unpkg.com/sanity@3.37.3-canary.140/dist/react.mjs",
-                "sanity": "https://unpkg.com/sanity@3.37.3-canary.140/dist/index.mjs",
-                "sanity/_singletons": "https://unpkg.com/sanity@3.37.3-canary.140/dist/_singletons.mjs",
-                "sanity/structure": "https://unpkg.com/sanity@3.37.3-canary.140/dist/structure.mjs",
-                "sanity/router": "https://unpkg.com/sanity@3.37.3-canary.140/dist/router.mjs",
-                "styled-components": "https://unpkg.com/sanity@3.37.3-canary.140/dist/styled-components.mjs"
+                "react": "https://unpkg.com/sanity@3.37.3-canary.143/dist/react.mjs",
+                "sanity": "https://unpkg.com/sanity@3.37.3-canary.143/dist/index.mjs",
+                "sanity/_singletons": "https://unpkg.com/sanity@3.37.3-canary.143/dist/_singletons.mjs",
+                "sanity/structure": "https://unpkg.com/sanity@3.37.3-canary.143/dist/structure.mjs",
+                "sanity/router": "https://unpkg.com/sanity@3.37.3-canary.143/dist/router.mjs",
+                "styled-components": "https://unpkg.com/sanity@3.37.3-canary.143/dist/styled-components.mjs"
               }
             }`,
             }}
