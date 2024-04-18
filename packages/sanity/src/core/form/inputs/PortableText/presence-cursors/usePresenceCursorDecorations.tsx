a301d8975d95253a10885b88d592bd7e08d46110
Herman Wikner <wiknerherman@gmail.com>
2024-04-18 16:01:13 +0200

    fix(core): use `useFormFieldPresence` in `usePresenceCursorDecorations`

--- packages/sanity/src/core/form/inputs/PortableText/presence-cursors/usePresenceCursorDecorations.tsx
+++ packages/sanity/src/core/form/inputs/PortableText/presence-cursors/usePresenceCursorDecorations.tsx
@@ -1,9 +1,10 @@
 import {type RangeDecoration} from '@sanity/portable-text-editor'
 import {type Path} from '@sanity/types'
-import {isEqual, uniqWith} from 'lodash'
+import {startsWith} from '@sanity/util/paths'
+import {isEqual} from 'lodash'
 import {useMemo} from 'react'
 
-import {useChildPresence} from '../../../studio/contexts/Presence'
+import {useFormFieldPresence} from '../../../studio/contexts/Presence'
 import {UserPresenceCursor} from './UserPresenceCursor'
 
 export interface PresenceCursorDecorationsHookProps {
@@ -14,13 +15,15 @@ export function usePresenceCursorDecorations(
   props: PresenceCursorDecorationsHookProps,
 ): RangeDecoration[] {
   const {path} = props
-  const childPresence = useChildPresence(path)
+  const fieldPresence = useFormFieldPresence()
+
+  const currentPresence = useMemo(
+    () => fieldPresence.filter((p) => startsWith(path, p.path) && !isEqual(path, p.path)),
+    [path, fieldPresence],
+  )
 
   return useMemo((): RangeDecoration[] => {
-    const decorations: RangeDecoration[] = uniqWith(
-      childPresence.filter((presence) => Boolean(presence?.selection)),
-      isEqual,
-    ).map((presence) => {
+    const decorations: RangeDecoration[] = currentPresence.map((presence) => {
       if (!presence.selection) {
         return null
       }
@@ -33,5 +36,5 @@ export function usePresenceCursorDecorations(
     }) as RangeDecoration[]
 
     return decorations.filter(Boolean)
-  }, [childPresence])
+  }, [currentPresence])
 }
