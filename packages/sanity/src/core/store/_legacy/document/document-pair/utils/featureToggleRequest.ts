2723846eca5c60b6622b96e9da0ba1d2cb8c44e0
Carolina Gonzalez <carolina.nicole.gonzalez@gmail.com>
2024-04-19 11:56:53 -0400

    refactor: use shareReplay

--- packages/sanity/src/core/store/_legacy/document/document-pair/utils/featureToggleRequest.ts
+++ packages/sanity/src/core/store/_legacy/document/document-pair/utils/featureToggleRequest.ts
@@ -3,25 +3,15 @@
 import {type SanityClient} from '@sanity/client'
 import {map, type Observable, of} from 'rxjs'
 
-import {type IdPair} from '../../types'
-import {memoize} from '../../utils/createMemoizer'
-
 //in the "real" code, this would be observable.request, to a URI
-export const featureToggleRequest: (
+export const featureToggleRequest = (
   client: SanityClient,
-  idPair: IdPair,
   serverActionsEnabled: boolean,
-) => Observable<boolean> = memoize(
-  (client: SanityClient, idPair: IdPair, serverActionsEnabled: boolean): Observable<boolean> => {
-    if (!serverActionsEnabled) {
-      return of(false)
-    }
-    return client.observable
-      .fetch('*[_id == $id][0]', {id: '20449512-1e9c-44f0-a509-417c901fbbbd'})
-      .pipe(map((doc) => doc.name.split('CONTROL FOR SERVER ACTIONS: ')[1] === 'enabled'))
-  },
-  (client: SanityClient, idPair: IdPair, serverActionsEnabled: boolean) => {
-    const config = client.config()
-    return `${config.dataset ?? ''}-${config.projectId ?? ''}-${idPair.publishedId}-${serverActionsEnabled ? '-serverActionsEnabled' : ''}`
-  },
-)
+): Observable<boolean> => {
+  if (!serverActionsEnabled) {
+    return of(false)
+  }
+  return client.observable
+    .fetch('*[_id == $id][0]', {id: '20449512-1e9c-44f0-a509-417c901fbbbd'})
+    .pipe(map((doc) => doc.name.split('CONTROL FOR SERVER ACTIONS: ')[1] === 'enabled'))
+}
