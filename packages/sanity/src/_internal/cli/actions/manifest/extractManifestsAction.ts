37e7b310267eeb6070148de623f1ee90caf4c27f
Ash <ash@sanity.io>
2024-04-22 17:31:43 +0100

    refactor(sanity): use manifest schemas from `@sanity/manifest`

--- packages/sanity/src/_internal/cli/actions/manifest/extractManifestsAction.ts
+++ packages/sanity/src/_internal/cli/actions/manifest/extractManifestsAction.ts
@@ -3,8 +3,8 @@ import {dirname, join, resolve} from 'node:path'
 import {Worker} from 'node:worker_threads'
 
 import {type CliCommandAction} from '@sanity/cli'
+import {type ManifestV1, type ManifestV1Workspace} from '@sanity/manifest'
 import readPkgUp from 'read-pkg-up'
-import z from 'zod'
 
 import {
   type ExtractSchemaWorkerData,
@@ -13,23 +13,6 @@ import {
 
 const MANIFEST_FILENAME = 'v1.studiomanifest.json'
 const SCHEMA_FILENAME_PREFIX = 'schema-'
-
-const ManifestSchema = z.object({manifestVersion: z.number()})
-
-const ManifestV1WorkspaceSchema = z.object({
-  name: z.string(),
-  dataset: z.string(),
-  schema: z.string(),
-})
-
-type ManifestV1Workspace = z.infer<typeof ManifestV1WorkspaceSchema>
-
-const ManifestV1Schema = ManifestSchema.extend({
-  createdAt: z.date(),
-  workspaces: z.array(ManifestV1WorkspaceSchema),
-})
-
-type ManifestV1 = z.infer<typeof ManifestV1Schema>
 
 const extractManifests: CliCommandAction = async (_args, context) => {
   const {output, workDir, chalk} = context
@@ -119,7 +102,7 @@ export default extractManifests
 function externalizeSchemas(
   schemas: ExtractSchemaWorkerResult[],
   staticPath: string,
-): Promise<z.infer<typeof ManifestV1WorkspaceSchema>[]> {
+): Promise<ManifestV1Workspace[]> {
   const output = schemas.reduce<Promise<ManifestV1Workspace>[]>((workspaces, workspace) => {
     return [...workspaces, externalizeSchema(workspace, staticPath)]
   }, [])
@@ -130,7 +113,7 @@ function externalizeSchemas(
 async function externalizeSchema(
   workspace: ExtractSchemaWorkerResult,
   staticPath: string,
-): Promise<z.infer<typeof ManifestV1WorkspaceSchema>> {
+): Promise<ManifestV1Workspace> {
   const encoder = new TextEncoder()
   const schemaString = JSON.stringify(workspace.schema, null, 2)
   const hash = await crypto.subtle.digest('SHA-1', encoder.encode(schemaString))
