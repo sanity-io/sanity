6113f58102da781257af253dde40de579185930d
Binoy Patel <me@binoy.io>
2024-04-24 14:04:32 -0400

    fix: build the correct react-dom server lib

--- packages/sanity/package.bundle.ts
+++ packages/sanity/package.bundle.ts
@@ -14,7 +14,7 @@ export default defineConfig(() => {
           'react': './node_modules/react/cjs/react.production.min.js',
           'react-dom': './node_modules/react-dom/cjs/react-dom.production.min.js',
           'react-dom_server':
-            './node_modules/react-dom/cjs/react-dom-server.browser.production.min.js',
+            './node_modules/react-dom/cjs/react-dom-server-legacy.browser.production.min.js',
           'react_jsx-runtime': './node_modules/react/cjs/react-jsx-runtime.production.min.js',
           'router': './src/_exports/router.ts',
           'styled-components': './node_modules/styled-components/dist/styled-components.esm.js',
