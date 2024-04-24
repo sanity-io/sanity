b46d446031d65255146e3c7c956675482951e427
Binoy Patel <me@binoy.io>
2024-04-24 13:03:24 -0400

    fix: depcheck issues and bundle react-dom/server

--- packages/sanity/package.bundle.ts
+++ packages/sanity/package.bundle.ts
@@ -13,6 +13,8 @@ export default defineConfig(() => {
           'presentation': './src/_exports/presentation.ts',
           'react': './node_modules/react/cjs/react.production.min.js',
           'react-dom': './node_modules/react-dom/cjs/react-dom.production.min.js',
+          'react-dom_server':
+            './node_modules/react-dom/cjs/react-dom-server.browser.production.min.js',
           'react_jsx-runtime': './node_modules/react/cjs/react-jsx-runtime.production.min.js',
           'router': './src/_exports/router.ts',
           'styled-components': './node_modules/styled-components/dist/styled-components.esm.js',
