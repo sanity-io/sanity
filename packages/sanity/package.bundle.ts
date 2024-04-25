35793ef03001e64e402bafd1fa4693632f6e7896
Binoy Patel <me@binoy.io>
2024-04-25 15:14:36 -0400

    feat: move shared exports another module

--- packages/sanity/package.bundle.ts
+++ packages/sanity/package.bundle.ts
@@ -7,18 +7,11 @@ export default defineConfig(() => {
     build: {
       lib: {
         entry: {
-          '_singletons': './src/_exports/_singletons.ts',
-          'sanity': './src/_exports/index.ts',
-          'desk': './src/_exports/desk.ts',
-          'presentation': './src/_exports/presentation.ts',
-          'react': './node_modules/react/cjs/react.production.min.js',
-          'react-dom': './node_modules/react-dom/cjs/react-dom.production.min.js',
-          'react-dom_server':
-            './node_modules/react-dom/cjs/react-dom-server-legacy.browser.production.min.js',
-          'react_jsx-runtime': './node_modules/react/cjs/react-jsx-runtime.production.min.js',
-          'router': './src/_exports/router.ts',
-          'styled-components': './node_modules/styled-components/dist/styled-components.esm.js',
-          'structure': './src/_exports/structure.ts',
+          _singletons: './src/_exports/_singletons.ts',
+          sanity: './src/_exports/index.ts',
+          desk: './src/_exports/desk.ts',
+          presentation: './src/_exports/presentation.ts',
+          structure: './src/_exports/structure.ts',
         },
       },
     },
