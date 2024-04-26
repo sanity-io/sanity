6cc195aa66f6451ab5ccf789ec781f197bd4e174
Cody Olsen <stipsan@gmail.com>
2024-04-26 10:56:46 +0200

    chore: support HMR same way test-studio does

--- dev/test-next-studio/next.config.mjs
+++ dev/test-next-studio/next.config.mjs
@@ -14,17 +14,60 @@ const config = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  transpilePackages: ['sanity-test-studio'],
+  transpilePackages: [
+    '@sanity/block-tools',
+    '@sanity/cli',
+    '@sanity/diff',
+    '@sanity/migrate',
+    '@sanity/mutator',
+    '@sanity/portable-text-editor',
+    '@sanity/schema',
+    '@sanity/types',
+    '@sanity/util',
+    '@sanity/vision',
+    'sanity-test-studio',
+    'sanity',
+  ],
   // eslint-disable-next-line @typescript-eslint/no-shadow
   webpack(config) {
     config.resolve.alias = {
       ...config.resolve.alias,
-      '@sanity/vision': requireResolve('@sanity/vision'),
-      'sanity/desk': requireResolve('sanity/desk'),
-      'sanity/presentation': requireResolve('sanity/presentation'),
-      'sanity/router': requireResolve('sanity/router'),
-      'sanity/structure': requireResolve('sanity/structure'),
-      'sanity': requireResolve('sanity'),
+      '@sanity/block-tools': requireResolve('../../packages/@sanity/block-tools/src/index.ts'),
+      '@sanity/diff': requireResolve('../../packages/@sanity/diff/src/index.ts'),
+      '@sanity/cli': requireResolve('../../packages/@sanity/cli/src/index.ts'),
+      '@sanity/mutator': requireResolve('../../packages/@sanity/mutator/src/index.ts'),
+      '@sanity/portable-text-editor': requireResolve(
+        '../../packages/@sanity/portable-text-editor/src/index.ts',
+      ),
+      '@sanity/schema/_internal': requireResolve(
+        '../../packages/@sanity/schema/src/_exports/_internal.ts',
+      ),
+      '@sanity/schema': requireResolve('../../packages/@sanity/schema/src/_exports/index.ts'),
+      '@sanity/migrate': requireResolve('../../packages/@sanity/migrate/src/_exports/index.ts'),
+      '@sanity/types': requireResolve('../../packages/@sanity/types/src/index.ts'),
+      '@sanity/util/fs': requireResolve('../../packages/@sanity/util/src/_exports/fs.ts'),
+      '@sanity/util/client': requireResolve('../../packages/@sanity/util/src/_exports/client.ts'),
+      '@sanity/util/concurrency-limiter': requireResolve(
+        '../../packages/@sanity/util/src/_exports/concurrency-limiter.ts',
+      ),
+      '@sanity/util/content': requireResolve('../../packages/@sanity/util/src/_exports/content.ts'),
+      '@sanity/util/createSafeJsonParser': requireResolve(
+        '../../packages/@sanity/util/src/_exports/createSafeJsonParser.ts',
+      ),
+      '@sanity/util/legacyDateFormat': requireResolve(
+        '../../packages/@sanity/util/src/_exports/legacyDateFormat.ts',
+      ),
+      '@sanity/util/paths': requireResolve('../../packages/@sanity/util/src/_exports/paths.ts'),
+      '@sanity/util': requireResolve('../../packages/@sanity/util/src/_exports/index.ts'),
+      '@sanity/vision': requireResolve('../../packages/@sanity/vision/src/index.ts'),
+      'sanity/_internal': requireResolve('../../packages/sanity/src/_exports/_internal.ts'),
+      'sanity/cli': requireResolve('../../packages/sanity/src/_exports/cli.ts'),
+      'sanity/desk': requireResolve('../../packages/sanity/src/_exports/desk.ts'),
+      'sanity/presentation': requireResolve('../../packages/sanity/src/_exports/presentation.ts'),
+      'sanity/router': requireResolve('../../packages/sanity/src/_exports/router.ts'),
+      'sanity/structure': requireResolve('../../packages/sanity/src/_exports/structure.ts'),
+      'sanity/migrate': requireResolve('../../packages/sanity/src/_exports/migrate.ts'),
+      'sanity': requireResolve('../../packages/sanity/src/_exports/index.ts'),
     }
     return config
   },
