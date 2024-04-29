ffa68ec009c45e4fc14559cb04410bdcea376da0
RitaDias <rita@sanity.io>
2024-04-29 12:52:42 +0200

    fix(@sanity): issue where hidden unicode characters were bloating document in PTE (#6440)
    * fix(portable-text-editor): issue shown in tests re stega. use duplicate code
    
    * test(playwright-ct): add test
    
    * chore(sanity): remove prettier linting
    
    * test(sanity): fix missing snapshot
    
    * test(sanity): update test after realising the test would pass always if comparing object number
    
    * chore: test unicode removal
    
    * chore: test unicode removal
    
    * chore(@sanity): remove old solution
    
    * fix(@sanity/block-tools): unicode issue. remove vercel/stega and move to block-tools
    
    * test(@sanity/block-tools): for unicode
    
    * fix(@sanity/block-tools): utf8 characters weren't beign filtered. using the vercel/stega
    
    * chore: update lock file
    
    * (chore): update pnpm lock

--- packages/sanity/playwright-ct/tests/formBuilder/inputs/PortableText/copyPaste/CopyPaste.spec.tsx
+++ packages/sanity/playwright-ct/tests/formBuilder/inputs/PortableText/copyPaste/CopyPaste.spec.tsx
@@ -4,7 +4,13 @@ import {type Path, type SanityDocument} from '@sanity/types'
 
 import {testHelpers} from '../../../../utils/testHelpers'
 import CopyPasteStory from './CopyPasteStory'
-import {GDOCS_INPUT, NORMALIZED_INPUT_SNAPSHOT, REMOVED_INPUT_SNAPSHOT} from './input'
+import {
+  CLEANED_UNICODE_INPUT_SNAPSHOT,
+  GDOCS_INPUT,
+  NORMALIZED_INPUT_SNAPSHOT,
+  REMOVED_INPUT_SNAPSHOT,
+  UNICODE_TEXT,
+} from './input'
 
 export type UpdateFn = () => {focusPath: Path; document: SanityDocument}
 
@@ -66,6 +72,33 @@ test.describe('Portable Text Input', () => {
       const snapshotLength = NORMALIZED_INPUT_SNAPSHOT.length
 
       await expect(documentState?.bodyNormalized?.length || 0).toEqual(snapshotLength)
+    })
+  })
+
+  test.describe('Should be able to paste text that has hidden unicode characters without bloating the PTE', () => {
+    test(`Removed unicode characters`, async ({mount, page}) => {
+      const {getFocusedPortableTextEditor, insertPortableTextCopyPaste, waitForDocumentState} =
+        testHelpers({page})
+
+      await mount(<CopyPasteStory document={document} />)
+
+      const $pte = await getFocusedPortableTextEditor('field-body')
+
+      await insertPortableTextCopyPaste(UNICODE_TEXT, $pte)
+
+      const documentState = await waitForDocumentState((documentStateValue) => {
+        return (documentStateValue?.body?.length || 0) > 0
+      })
+
+      // strigify is needed in these cases in order to get the correct length for the content within the children
+      // prettier-ignore
+      const bodyLength = await JSON.stringify(documentState?.body).length || 0
+      // prettier-ignore
+      const snapshotLength = JSON.stringify(CLEANED_UNICODE_INPUT_SNAPSHOT).length
+
+      // Ideally we would compare the snapshot with the document, but the keys will be different each time
+      // We therefore compare the length of the body to the snapshot length here instead.
+      await expect(bodyLength).toEqual(snapshotLength)
     })
   })
 })
