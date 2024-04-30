0306fdd5f9077c42f9175bc200ed87e9257235f3
Pedro Bonamin <pedrobonamin@gmail.com>
2024-04-30 09:32:02 +0200

    fix(pte): preserve block key when pressing enter at start of block

--- packages/@sanity/portable-text-editor/src/editor/plugins/createWithHotKeys.ts
+++ packages/@sanity/portable-text-editor/src/editor/plugins/createWithHotKeys.ts
@@ -219,18 +219,31 @@ export function createWithHotkeys(
           return
         }
 
-        // Enter from another style than the first (default one)
-        if (
-          editor.isTextBlock(focusBlock) &&
-          focusBlock.style &&
-          focusBlock.style !== types.styles[0].value
-        ) {
+        if (editor.isTextBlock(focusBlock)) {
+          // Enter from another style than the first (default one)
           const [, end] = Range.edges(editor.selection)
-          const endAtEndOfNode = Editor.isEnd(editor, end, end.path)
-          if (endAtEndOfNode) {
+          if (focusBlock.style && focusBlock.style !== types.styles[0].value) {
+            const endAtEndOfNode = Editor.isEnd(editor, end, end.path)
+            if (endAtEndOfNode) {
+              Editor.insertNode(editor, createEmptyBlock())
+              event.preventDefault()
+              editor.onChange()
+              return
+            }
+          }
+          // If it's at the start of block, we want to preserve the current block key and insert a new one in the current position instead of splitting the node.
+          const isEndAtStartOfNode = Editor.isStart(editor, end, end.path)
+          if (isEndAtStartOfNode) {
             Editor.insertNode(editor, createEmptyBlock())
+            const [nextBlockPath] = Path.next(focusBlockPath)
+            Transforms.select(editor, {
+              anchor: {path: [nextBlockPath, 0], offset: 0},
+              focus: {path: [nextBlockPath, 0], offset: 0},
+            })
+
             event.preventDefault()
             editor.onChange()
+
             return
           }
         }
