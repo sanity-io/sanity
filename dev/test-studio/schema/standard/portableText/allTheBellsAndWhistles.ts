1257c8e4bca3bc79b0221274e04d3e55199934dd
Fred Carlsen <fred@sjelfull.no>
2024-04-30 16:48:38 +0200

    feat(form/inputs): add support for image drop+paste in PTE input
    Signed-off-by: Fred Carlsen <fred@sjelfull.no>

--- dev/test-studio/schema/standard/portableText/allTheBellsAndWhistles.ts
+++ dev/test-studio/schema/standard/portableText/allTheBellsAndWhistles.ts
@@ -1,4 +1,4 @@
-import {BellIcon, ColorWheelIcon, ImageIcon, InfoOutlineIcon} from '@sanity/icons'
+import {BellIcon, ColorWheelIcon, DocumentPdfIcon, ImageIcon, InfoOutlineIcon} from '@sanity/icons'
 import {type Rule} from '@sanity/types'
 import {defineArrayMember, defineField, defineType} from 'sanity'
 
@@ -13,6 +13,9 @@ export const ptAllTheBellsAndWhistlesType = defineType({
     {
       name: 'whitespace',
       title: 'Whitespace',
+      options: {
+        collapsed: true,
+      },
     },
   ],
   fields: [
@@ -36,7 +39,7 @@ export const ptAllTheBellsAndWhistlesType = defineType({
           marks: {
             // decorators: [{title: 'Strong', value: 'strong'}],
             annotations: [
-              {
+              defineField({
                 type: 'object',
                 name: 'link',
                 title: 'Link',
@@ -44,13 +47,13 @@ export const ptAllTheBellsAndWhistlesType = defineType({
                 //   modal: {type: 'dialog'},
                 // },
                 fields: [
-                  {
+                  defineField({
                     type: 'url',
                     name: 'href',
                     title: 'URL',
-                    validation: (rule: Rule) =>
+                    validation: (rule) =>
                       rule
-                        .custom((url: string, context: any) => {
+                        .custom((url: string | undefined, context: any) => {
                           if (!url && !context.parent.reference) {
                             return 'Inline Link: Requires a reference or URL'
                           }
@@ -61,7 +64,7 @@ export const ptAllTheBellsAndWhistlesType = defineType({
                           scheme: ['http', 'https', 'mailto', 'tel'],
                           allowRelative: true,
                         }),
-                  },
+                  }),
                   defineField({
                     title: 'Linked Book',
                     name: 'reference',
@@ -77,8 +80,8 @@ export const ptAllTheBellsAndWhistlesType = defineType({
                     initialValue: false,
                   },
                 ],
-              },
-              {
+              }),
+              defineField({
                 type: 'object',
                 name: 'color',
                 title: 'Color',
@@ -91,7 +94,7 @@ export const ptAllTheBellsAndWhistlesType = defineType({
                     validation: (rule: Rule) => rule.required(),
                   },
                 ],
-              },
+              }),
             ],
           },
           of: [
@@ -118,6 +121,21 @@ export const ptAllTheBellsAndWhistlesType = defineType({
               ],
             }),
           ],
+        }),
+
+        defineField({
+          type: 'file',
+          icon: DocumentPdfIcon,
+          name: 'pdfFile',
+          title: 'PDF file',
+          options: {
+            accept: 'application/pdf',
+          },
+          preview: {
+            select: {
+              title: 'asset.originalFilename',
+            },
+          },
         }),
 
         defineField({
@@ -169,6 +187,29 @@ export const ptAllTheBellsAndWhistlesType = defineType({
               description: 'Not implemented in most surfaces.',
               name: 'isLarge',
               type: 'boolean',
+            }),
+          ],
+        }),
+
+        defineField({
+          name: 'imageObject',
+          title: 'Image object',
+          type: 'object',
+          icon: ImageIcon,
+          fields: [
+            defineField({
+              type: 'image',
+              icon: ImageIcon,
+              name: 'image',
+              title: 'Image',
+              options: {
+                hotspot: true,
+              },
+              preview: {
+                select: {
+                  media: 'asset',
+                },
+              },
             }),
           ],
         }),
