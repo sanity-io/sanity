bb5d2518a2c8583c32090af8b60794c84b9fa94d
Jordan Lawrence <jordanl17@me.com>
2024-05-02 15:19:21 +0100

    fix(pte): reorg test cases

--- packages/@sanity/portable-text-editor/src/editor/plugins/__tests__/withPortableTextMarkModel.test.tsx
+++ packages/@sanity/portable-text-editor/src/editor/plugins/__tests__/withPortableTextMarkModel.test.tsx
@@ -869,63 +869,6 @@ Array [
         expect(PortableTextEditor.isAnnotationActive(editor, 'link')).toBe(false)
       })
     })
-
-    it('should return active list styles that cover the whole selection', async () => {
-      const editorRef: RefObject<PortableTextEditor> = createRef()
-      const initialValue = [
-        {
-          _key: 'a',
-          _type: 'myTestBlockType',
-          children: [
-            {
-              _key: 'a1',
-              _type: 'span',
-              marks: [],
-              text: '12',
-            },
-          ],
-          markDefs: [],
-          style: 'normal',
-        },
-        {
-          _key: 'b',
-          _type: 'myTestBlockType',
-          children: [
-            {
-              _key: '2',
-              _type: 'span',
-              marks: [],
-              text: '34',
-              level: 1,
-              listItem: 'bullet',
-            },
-          ],
-          markDefs: [],
-          style: 'normal',
-        },
-      ]
-      const onChange = jest.fn()
-      await waitFor(() => {
-        render(
-          <PortableTextEditorTester
-            onChange={onChange}
-            ref={editorRef}
-            schemaType={schemaType}
-            value={initialValue}
-          />,
-        )
-      })
-      const editor = editorRef.current!
-      expect(editor).toBeDefined()
-      await waitFor(() => {
-        PortableTextEditor.focus(editor)
-        PortableTextEditor.select(editor, {
-          focus: {path: [{_key: 'a'}, 'children', {_key: 'a1'}], offset: 0},
-          anchor: {path: [{_key: '2'}, 'children', {_key: '2'}], offset: 2},
-        })
-        expect(PortableTextEditor.hasListStyle(editor, 'bullet')).toBe(false)
-      })
-    })
   })
 
   describe('removing annotations', () => {
