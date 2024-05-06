97aae6beebe977f4068e38bd4263f94fee0aa7aa
Jordan Lawrence <jordanl17@me.com>
2024-05-06 17:14:02 +0100

    fix(studio): adding tooltip to read-only bool inputs

--- packages/sanity/src/core/form/inputs/BooleanInput.tsx
+++ packages/sanity/src/core/form/inputs/BooleanInput.tsx
@@ -1,6 +1,9 @@
 import {Box, Card, type CardTone, Checkbox, Flex, Switch} from '@sanity/ui'
+import {useMemo} from 'react'
 import {styled} from 'styled-components'
 
+import {Tooltip} from '../../../ui-components'
+import {useTranslation} from '../../i18n/hooks/useTranslation'
 import {FormFieldHeaderText} from '../components/formField/FormFieldHeaderText'
 import {FormFieldStatus} from '../components/formField/FormFieldStatus'
 import {type BooleanInputProps} from '../types'
@@ -23,6 +26,7 @@ const ZeroLineHeightBox = styled(Box)`
  * @beta
  */
 export function BooleanInput(props: BooleanInputProps) {
+  const {t} = useTranslation()
   const {id, value, schemaType, readOnly, elementProps, validation} = props
   const layout = schemaType.options?.layout || 'switch'
 
@@ -33,19 +37,26 @@ export function BooleanInput(props: BooleanInputProps) {
 
   const tone: CardTone | undefined = readOnly ? 'transparent' : undefined
 
+  const input = useMemo(
+    () => (
+      <ZeroLineHeightBox padding={3}>
+        <LayoutSpecificInput
+          label={schemaType.title}
+          {...elementProps}
+          checked={checked}
+          readOnly={readOnly}
+          indeterminate={indeterminate}
+          style={{margin: -4}}
+        />
+      </ZeroLineHeightBox>
+    ),
+    [LayoutSpecificInput, checked, elementProps, indeterminate, readOnly, schemaType.title],
+  )
+
   return (
     <Root border data-testid="boolean-input" radius={2} tone={tone}>
       <Flex>
-        <ZeroLineHeightBox padding={3}>
-          <LayoutSpecificInput
-            label={schemaType.title}
-            {...elementProps}
-            checked={checked}
-            disabled={readOnly}
-            indeterminate={indeterminate}
-            style={{margin: -4}}
-          />
-        </ZeroLineHeightBox>
+        {readOnly ? <Tooltip content={t('inputs.boolean.disabled')}>{input}</Tooltip> : input}
         <Box flex={1} paddingY={2}>
           <FormFieldHeaderText
             deprecated={schemaType.deprecated}
