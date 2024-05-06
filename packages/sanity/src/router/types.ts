1ab2c155911297f469b37c2a4c982679e295a382
Bjørge Næss <bjoerge@gmail.com>
2024-05-06 15:38:09 +0200

    feat(sanity): add support for sticky query params

--- packages/sanity/src/router/types.ts
+++ packages/sanity/src/router/types.ts
@@ -261,6 +261,11 @@ export interface RouterContextValue {
   navigateUrl: (opts: {path: string; replace?: boolean}) => void
 
   /**
+   * Navigates to the current URL with the sticky url search param set to the given value
+   */
+  navigateStickyParam: (param: string, value: string, options?: NavigateOptions) => void
+
+  /**
    * Navigates to the given router state.
    * See {@link RouterState} and {@link NavigateOptions}
    */
@@ -276,4 +281,9 @@ export interface RouterContextValue {
    * The current router state. See {@link RouterState}
    */
   state: RouterState
+
+  /**
+   * The current router state. See {@link RouterState}
+   */
+  stickyParams: Record<string, string | undefined>
 }
