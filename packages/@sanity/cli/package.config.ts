394b2be3ef04e12f91bb625def90afe98e5ef3fe
James Garbutt <43081j@users.noreply.github.com>
2024-05-06 15:52:28 +0100

    feat(cli): remove is-builtin-module
    This removes the `is-builtin-module` package since all node versions
    within our `engine` constraint already have such a function built in.
    
    This also allows us to remove the temporary workaround we had around
    `string_decoder/` since the built in function will not have such a bug.

--- packages/@sanity/cli/package.config.ts
+++ packages/@sanity/cli/package.config.ts
@@ -1,11 +1,11 @@
 /* eslint-disable no-sync */
 import fs from 'node:fs'
+import {isBuiltin} from 'node:module'
 import path from 'node:path'
 
 import baseConfig from '@repo/package.config'
 import {nodeResolve} from '@rollup/plugin-node-resolve'
 import {defineConfig} from '@sanity/pkg-utils'
-import isBuiltin from 'is-builtin-module'
 
 const workersDir = path.join(__dirname, 'src', 'workers')
 
@@ -17,13 +17,6 @@ const workerNames = fs
 export default defineConfig({
   ...baseConfig,
 
-  // This is a workaround for an issue where the `readable-stream` package imports
-  // from `string_decoder/` instead of `string_decoder` (note the trailing slash),
-  // which _should_ tell rollup not to use the built-in `string_decoder` module,
-  // which it unfortunately does not. Hoping we can get rid of this hack soon:
-  // https://github.com/rollup/plugins/issues/1211
-  // https://github.com/sindresorhus/is-builtin-module/issues/7
-  // https://github.com/sindresorhus/builtin-modules/pull/17
   rollup: {
     plugins: (plugins) => [
       ...plugins.filter((plugin) => plugin.name !== 'node-resolve'),
@@ -31,7 +24,7 @@ export default defineConfig({
         browser: false,
         extensions: ['.cjs', '.mjs', '.js', '.jsx', '.json', '.node'],
         preferBuiltins: false,
-        resolveOnly: (moduleName) => moduleName === 'string_decoder' || !isBuiltin(moduleName),
+        resolveOnly: (moduleName) => !isBuiltin(moduleName),
       }),
     ],
   },
