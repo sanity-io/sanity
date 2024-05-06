0687d2532c94c3c929443efa6f88c233a43070d7
Bjørge Næss <bjoerge@gmail.com>
2024-05-06 15:46:44 +0200

    feat(sanity): add 'perspective' dropdown

--- packages/sanity/src/core/search/weighted/createSearchQuery.ts
+++ packages/sanity/src/core/search/weighted/createSearchQuery.ts
@@ -186,7 +186,7 @@ export function createSearchQuery(
       __limit: limit,
       ...(params || {}),
     },
-    options: {tag},
+    options: {tag, perspective: searchOpts.perspective},
     searchSpec: specs,
     terms,
   }
