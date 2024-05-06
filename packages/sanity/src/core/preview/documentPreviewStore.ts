f2b4117472851b4c9943ce2146798fe99cb54dc4
Bjørge Næss <bjoerge@gmail.com>
2024-05-06 15:46:44 +0200

    feat(sanity): add 'perspective' dropdown

--- packages/sanity/src/core/preview/documentPreviewStore.ts
+++ packages/sanity/src/core/preview/documentPreviewStore.ts
@@ -26,8 +26,7 @@ import {
 export type ObserveForPreviewFn = (
   value: Previewable,
   type: PreviewableType,
-  viewOptions?: PrepareViewOptions,
-  apiConfig?: ApiConfig,
+  options?: {viewOptions?: PrepareViewOptions; apiConfig?: ApiConfig; perspective?: string},
 ) => Observable<PreparedSnapshot>
 
 /**
