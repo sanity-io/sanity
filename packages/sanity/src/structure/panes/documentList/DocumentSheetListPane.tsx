1476643b766e031d91b25195f869c9a2b8906711
Jordan Lawrence <jordanl17@me.com>
2024-05-07 14:56:54 +0100

    feat(structure): resolving a default export to named

--- packages/sanity/src/structure/panes/documentList/DocumentSheetListPane.tsx
+++ packages/sanity/src/structure/panes/documentList/DocumentSheetListPane.tsx
@@ -1,8 +1,6 @@
 import {Text} from '@sanity/ui'
 
-function DocumentSheetListPane() {
+export function DocumentSheetListPane() {
   // eslint-disable-next-line i18next/no-literal-string
   return <Text>SheetList view</Text>
 }
-
-export default DocumentSheetListPane
