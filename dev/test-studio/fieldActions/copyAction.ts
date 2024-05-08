62064f6bdc156070628e0de9e0cfe295657f86a0
Fred Carlsen <fred@sjelfull.no>
2024-05-08 23:10:53 +0200

    fixup! feat(form): copy paste of document and fields prototype
    Signed-off-by: Fred Carlsen <fred@sjelfull.no>

--- dev/test-studio/fieldActions/copyAction.ts
+++ dev/test-studio/fieldActions/copyAction.ts
@@ -1,6 +1,7 @@
 import {CopyIcon} from '@sanity/icons'
 import {useCallback} from 'react'
 import {
+  Path,
   SanityDocument,
   defineDocumentFieldAction,
   useDocumentStore,
@@ -11,30 +12,54 @@ import {
 import {defineActionItem} from './define'
 import {useToast} from '@sanity/ui'
 
+export interface CopyActionResult {
+  documentId: string
+  documentType: string
+  schemaTypeName: string
+  docValue: unknown
+  isDocument: boolean
+  isArray: boolean
+  isObject: boolean
+  path: Path
+}
+
 export const copyAction = defineDocumentFieldAction({
   name: 'test/copy',
-  useAction({documentId, documentType, path}) {
+  useAction({documentId, documentType, path, schemaType}) {
+    const {jsonType, name: schemaTypeName} = schemaType
     // const docValue = useFormValue(path) as SanityDocument
     const getter = useGetFormValue()
     const toast = useToast()
     const onAction = useCallback(() => {
       // const documentStore = useDocumentStore()
       const docValue = getter(path)
-      // const docValue: string[] = []
-      const value = JSON.stringify({documentId, documentType, path, docValue})
-      // const pair = documentStore
-      // .checkoutPair({draftId: documentId, publishedId: documentId}).published.
+      const isDocument = schemaType?.type?.name === 'document'
+      const isArray = schemaType?.type?.name === 'array'
+      const isObject = schemaType?.type?.name === 'document'
+      // const name: string = schemaType?.type?.name || schemaType?.name
+      const payloadValue: CopyActionResult = {
+        schemaTypeName,
+        documentId,
+        documentType,
+        path,
+        docValue,
+        isDocument,
+        isArray,
+        isObject,
+      }
+
+      const label = isDocument ? `Document` : `Field`
 
       // navigator.clipboard.writeText(value)
-      localStorage.setItem('copiedField', value)
+      localStorage.setItem('copyActionResult', JSON.stringify(payloadValue))
 
       toast.push({
         status: 'success',
-        title: 'Field copied',
+        title: `${label} copied`,
       })
 
       // eslint-disable-next-line no-console
-      console.log('comment', {documentId, documentType, path, docValue})
+      console.log('comment', {payloadValue, jsonType})
     }, [documentId, documentType, path, toast])
 
     return defineActionItem({
