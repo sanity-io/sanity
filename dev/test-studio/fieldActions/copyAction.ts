68ecbce69b5e24ac02d81512d625d4956b560391
Fred Carlsen <fred@sjelfull.no>
2024-05-08 21:51:15 +0200

    feat(form): copy paste of document and fields prototype
    edx-1263
    
    Signed-off-by: Fred Carlsen <fred@sjelfull.no>

--- dev/test-studio/fieldActions/copyAction.ts
+++ dev/test-studio/fieldActions/copyAction.ts
@@ -1,16 +1,66 @@
 import {CopyIcon} from '@sanity/icons'
 import {useCallback} from 'react'
-import {defineDocumentFieldAction} from 'sanity'
+import {
+  Path,
+  SanityDocument,
+  defineDocumentFieldAction,
+  useDocumentStore,
+  useFormValue,
+  useGetFormValue,
+} from 'sanity'
 
 import {defineActionItem} from './define'
+import {useToast} from '@sanity/ui'
+
+export interface CopyActionResult {
+  documentId: string
+  documentType: string
+  schemaTypeName: string
+  docValue: unknown
+  isDocument: boolean
+  isArray: boolean
+  isObject: boolean
+  path: Path
+}
 
 export const copyAction = defineDocumentFieldAction({
   name: 'test/copy',
-  useAction({documentId, documentType, path}) {
+  useAction({documentId, documentType, path, schemaType}) {
+    const {jsonType, name: schemaTypeName} = schemaType
+    // const docValue = useFormValue(path) as SanityDocument
+    const getter = useGetFormValue()
+    const toast = useToast()
     const onAction = useCallback(() => {
+      // const documentStore = useDocumentStore()
+      const docValue = getter(path)
+      const isDocument = schemaType?.type?.name === 'document'
+      const isArray = schemaType?.type?.name === 'array'
+      const isObject = schemaType?.type?.name === 'document'
+      // const name: string = schemaType?.type?.name || schemaType?.name
+      const payloadValue: CopyActionResult = {
+        schemaTypeName,
+        documentId,
+        documentType,
+        path,
+        docValue,
+        isDocument,
+        isArray,
+        isObject,
+      }
+
+      const label = isDocument ? `Document` : `Field`
+
+      // navigator.clipboard.writeText(value)
+      localStorage.setItem('copyActionResult', JSON.stringify(payloadValue))
+
+      toast.push({
+        status: 'success',
+        title: `${label} copied`,
+      })
+
       // eslint-disable-next-line no-console
-      console.log('comment', {documentId, documentType, path})
-    }, [documentId, documentType, path])
+      console.log('comment', {payloadValue, jsonType})
+    }, [documentId, documentType, path, toast])
 
     return defineActionItem({
       type: 'action',
