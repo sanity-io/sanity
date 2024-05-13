f4c7cfe24a5f321a806ccacb4c5ac5936652972d
Binoy Patel <me@binoy.io>
2024-05-13 16:31:22 -0400

    chore(ci): add a circleci config to bundle packages (#6446)
    * chore(ci): add a circleci config to bundle packages
    
    * fix: store escaped package names in the manifest
    
    * chore: run job only on current

--- packages/@repo/shared-modules.bundle/package.bundle.ts
+++ packages/@repo/shared-modules.bundle/package.bundle.ts
@@ -6,13 +6,16 @@ export default defineConfig(() => {
   return mergeConfig(defaultConfig, {
     build: {
       lib: {
+        // NOTE: IF ANY OF THIS CHANGES MAKE SURE TO UPDATE `scripts/uploadBundles.ts`
         entry: {
-          'react': './node_modules/react/cjs/react.production.min.js',
-          'react-dom': './node_modules/react-dom/cjs/react-dom.production.min.js',
-          'react-dom_server':
+          'react/index': './node_modules/react/cjs/react.production.min.js',
+          'react-dom/index': './node_modules/react-dom/cjs/react-dom.production.min.js',
+          'react-dom/client': './node_modules/react-dom/cjs/react-dom.production.min.js',
+          'react-dom/server':
             './node_modules/react-dom/cjs/react-dom-server-legacy.browser.production.min.js',
-          'react_jsx-runtime': './node_modules/react/cjs/react-jsx-runtime.production.min.js',
-          'styled-components': './node_modules/styled-components/dist/styled-components.esm.js',
+          'react/jsx-runtime': './node_modules/react/cjs/react-jsx-runtime.production.min.js',
+          'styled-components/index':
+            './node_modules/styled-components/dist/styled-components.esm.js',
         },
       },
     },
