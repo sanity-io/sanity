8296ebbad6abd586f8d9f5220916fd3d9cace05f
Pedro Bonamin <pedrobonamin@gmail.com>
2024-05-14 14:36:59 +0200

    wip: use global pair listener

--- packages/sanity/src/structure/panes/documentList/useDocumentSheetList.ts
+++ packages/sanity/src/structure/panes/documentList/useDocumentSheetList.ts
@@ -1,31 +1,47 @@
 import {useMemo} from 'react'
-import {getPublishedId, useSearchState} from 'sanity'
-
-import {useDocumentSheetListStore} from './useDocumentSheetListStore'
+import {useMemoObservable} from 'react-rx'
+import {combineLatest} from 'rxjs'
+import {type EditStateFor, getPublishedId, useDocumentStore, useSearchState} from 'sanity'
 
 interface DocumentSheetListOptions {
   /**The schemaType.name  */
   typeName: string
 }
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+function useEditStateList(publishedDocIds: string[], docTypeName: string): EditStateFor[] {
+  const documentStore = useDocumentStore()
+  return useMemoObservable(() => {
+    return combineLatest(
+      publishedDocIds.map((publishedDocId) =>
+        documentStore.pair.editState(publishedDocId, docTypeName),
+      ),
+    )
+  }, [documentStore.pair, publishedDocIds, docTypeName]) as EditStateFor[]
+}
+
 export function useDocumentSheetList({typeName}: DocumentSheetListOptions) {
   const {state} = useSearchState()
 
   const items = useMemo(() => {
-    const map = new Map()
-    state.result.hits.forEach((h) => map.set(getPublishedId(h.hit._id), h.hit))
-    return map
+    return state.result.hits.map((h) => getPublishedId(h.hit._id))
   }, [state.result.hits])
 
-  // The store is listening to all the documents that match with the _type filter.
-  const {data, isLoading} = useDocumentSheetListStore({
-    filter: `_type == "${typeName}"`,
-  })
+  const docs = useEditStateList(items, typeName)
+  const tableData = useMemo(() => {
+    return (docs?.filter((d) => d.ready) || []).map((d) => {
+      if (d.draft) {
+        return d.draft
+      }
+      if (d.published) {
+        return d.published
+      }
+      return {
+        _type: d.type,
+        _id: d.id,
+      }
+    })
+  }, [docs])
 
-  // Only return the documents that match with the serverSide filter items.
-  const documents = useMemo(() => {
-    return data.filter((doc) => items.has(getPublishedId(doc._id)))
-  }, [data, items])
-
-  return {data: documents, isLoading}
+  return {data: tableData, isLoading: false}
 }
