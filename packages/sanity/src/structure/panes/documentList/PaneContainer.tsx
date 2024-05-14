b863b24ecf6a2e979668b077387eba5b0a7d3026
Pedro Bonamin <pedrobonamin@gmail.com>
2024-05-14 10:31:43 +0200

    feat(structure): add virtualised documentSheetList and sheetListStore

--- packages/sanity/src/structure/panes/documentList/PaneContainer.tsx
+++ packages/sanity/src/structure/panes/documentList/PaneContainer.tsx
@@ -112,7 +112,7 @@ export const PaneContainer = memo(function PaneContainer(
 
   const isSheetListLayout = layout === 'sheetList'
   const paneLayout = isSheetListLayout ? (
-    <DocumentSheetListPane />
+    <DocumentSheetListPane {...props} />
   ) : (
     <DocumentListPane {...props} sortOrder={sortOrderRaw} layout={layout} />
   )
