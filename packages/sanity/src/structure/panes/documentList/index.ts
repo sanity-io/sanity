22980c263b689b5846d51ecb594f706bcaefaacb
Cody Olsen <81981+stipsan@users.noreply.github.com>
2024-05-15 23:33:51 +0200

    fix: regression crashing the document list pane in presentation (#6684)
    * chore: test if removing the default alias fixes it
    
    * Revert "chore: test if removing the default alias fixes it"
    
    This reverts commit 0917e5c4689ea7fe426b134c2c50f262dea71f01.
    
    * fix: re-export PaneContainer as DocumentListPane like the lazy import
    
    * chore: remove old `@sanity/presentation` repro that isn't needed anymore
    
    * chore: update CODEOWNERS with internals used by presentation

--- packages/sanity/src/structure/panes/documentList/index.ts
+++ packages/sanity/src/structure/panes/documentList/index.ts
@@ -1,1 +1,9 @@
-export * from './DocumentListPane'
+import {type ComponentProps} from 'react'
+
+import {PaneContainer} from './PaneContainer'
+
+export {PaneContainer as DocumentListPane}
+/**
+ * @internal
+ */
+export type DocumentListPaneProps = ComponentProps<typeof PaneContainer>
