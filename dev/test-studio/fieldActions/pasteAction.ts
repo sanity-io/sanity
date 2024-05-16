41fea8523af85a3daaeabc3efbc575140d40a593
Fred Carlsen <fred@sjelfull.no>
2024-05-16 13:54:49 +0200

    refactor(dev): use new copy paste api in actions
    Signed-off-by: Fred Carlsen <fred@sjelfull.no>

--- dev/test-studio/fieldActions/pasteAction.ts
+++ dev/test-studio/fieldActions/pasteAction.ts
@@ -1,62 +1,23 @@
 import {ClipboardIcon} from '@sanity/icons'
 import {useCallback} from 'react'
-import {PatchEvent, defineDocumentFieldAction, set, useFormCallbacks, useGetFormValue} from 'sanity'
+import {defineDocumentFieldAction, useCopyPasteAction} from 'sanity'
 
 import {defineActionItem} from './define'
-import {useDocumentPane} from 'sanity/structure'
-import {useToast} from '@sanity/ui'
-import {CopyActionResult} from './copyAction'
-import {filter, pickBy} from 'lodash'
 
 export const pasteAction = defineDocumentFieldAction({
   name: 'test/paste',
   useAction({documentId, documentType, path, schemaType}) {
-    const toast = useToast()
-    const getter = useGetFormValue()
-    // const {onChange} = useFormCallbacks()
-    const {onChange} = useDocumentPane()
+    const {onPaste} = useCopyPasteAction({documentId, documentType, path, schemaType})
     const onAction = useCallback(() => {
-      // const value = navigator.clipboard.read()
-      const value = localStorage.getItem('copyActionResult')
-      if (!value) {
-        return
-      }
-      const parsed = JSON.parse(value) as CopyActionResult
-      const {schemaTypeName, isDocument, isArray, isObject, path} = parsed
-
-      const keysToDelete = ['_id', '_type', '_createdAt', '_updatedAt', '_rev']
-      const isArrayValue = Array.isArray(parsed.docValue)
-      const newValue = isArrayValue
-        ? parsed.docValue
-        : pickBy(parsed.docValue as {}, (_value, key) => !keysToDelete.includes(key))
-      const isTargetDocument = schemaType?.type?.name === 'document'
-      const targetName = schemaType.name
-      const targetTypeLabel = isTargetDocument ? 'document' : 'field'
-
-      if (isDocument && (!isTargetDocument || documentType !== schemaTypeName)) {
-        toast.push({
-          status: 'error',
-          title: `Cannot paste document of type ${schemaTypeName} into ${targetTypeLabel} of type ${targetName}`,
-        })
-
-        return
-      }
-
-      onChange(PatchEvent.from(set(newValue, path)))
-
-      toast.push({
-        status: 'success',
-        title: `${isDocument ? `Document` : `Field`} updated`,
-      })
-      // eslint-disable-next-line no-console
-      console.log('paste', {documentId, documentType, path, parsed})
-    }, [documentId, documentType, path])
+      onPaste()
+    }, [onPaste])
 
     return defineActionItem({
       type: 'action',
       icon: ClipboardIcon,
       onAction,
       title: 'Paste',
+      hidden: path.length === 0,
     })
   },
 })
