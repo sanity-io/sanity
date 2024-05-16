08d9d2e7727beb9a1eb264dc427fe2e568b9e8f3
Fred Carlsen <fred@sjelfull.no>
2024-05-16 13:54:49 +0200

    refactor(dev): use new copy paste api in actions
    Signed-off-by: Fred Carlsen <fred@sjelfull.no>

--- dev/test-studio/documentActions/actions/CopyDocumentAction.tsx
+++ dev/test-studio/documentActions/actions/CopyDocumentAction.tsx
@@ -1,54 +1,31 @@
 import {CopyIcon} from '@sanity/icons'
-import {useToast} from '@sanity/ui'
-import {useCallback, useMemo} from 'react'
+import {useCallback} from 'react'
 import {
   type DocumentActionComponent,
   InsufficientPermissionsMessage,
+  useCopyPasteAction,
   useCurrentUser,
   useDocumentPairPermissions,
 } from 'sanity'
 
 /** @internal */
-export const CopyDocumentAction: DocumentActionComponent = ({
-  id,
-  type,
-  draft,
-  published,
-  onComplete,
-}) => {
-  const toast = useToast()
+export const CopyDocumentAction: DocumentActionComponent = ({id, type, onComplete}) => {
+  const {onCopy} = useCopyPasteAction({
+    documentId: id,
+    documentType: type,
+    path: [],
+    schemaType: type,
+  })
+  const handle = useCallback(() => {
+    onCopy()
+    onComplete()
+  }, [onCopy, onComplete])
   const [permissions, isPermissionsLoading] = useDocumentPairPermissions({
     id,
     type,
     permission: 'update',
   })
-
   const currentUser = useCurrentUser()
-  const doc = draft || published || {}
-  const filteredDoc = useMemo(() => {
-    return Object.keys(doc).reduce((filtered, prop) => {
-      if (!prop.startsWith('_')) {
-        // @ts-ignore
-        filtered[prop] = doc[prop]
-      }
-      return filtered
-    }, {})
-  }, [draft, published])
-
-  const handle = useCallback(async () => {
-    const payload = {type, path: [], docValue: filteredDoc}
-    console.log('copying document', payload)
-
-    const value = JSON.stringify(payload)
-    localStorage.setItem('copiedDocument', value)
-
-    toast.push({
-      status: 'success',
-      title: 'Document copied',
-    })
-
-    onComplete()
-  }, [onComplete, type, filteredDoc])
 
   if (!isPermissionsLoading && !permissions?.granted) {
     return {
