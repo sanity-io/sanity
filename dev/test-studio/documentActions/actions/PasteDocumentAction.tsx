da909182ebcdd11cb1df61d630d5ca049043e513
Fred Carlsen <fred@sjelfull.no>
2024-05-16 13:54:49 +0200

    refactor(dev): use new copy paste api in actions
    Signed-off-by: Fred Carlsen <fred@sjelfull.no>

--- dev/test-studio/documentActions/actions/PasteDocumentAction.tsx
+++ dev/test-studio/documentActions/actions/PasteDocumentAction.tsx
@@ -1,76 +1,37 @@
 import {ClipboardIcon} from '@sanity/icons'
-import {useToast} from '@sanity/ui'
-import {useCallback, useState} from 'react'
-import {filter, firstValueFrom} from 'rxjs'
+import {useCallback} from 'react'
 import {
   type DocumentActionComponent,
   InsufficientPermissionsMessage,
+  useCopyPasteAction,
   useCurrentUser,
-  useDocumentOperation,
   useDocumentPairPermissions,
-  useDocumentStore,
-  PatchEvent,
-  set,
 } from 'sanity'
 
 /** @internal */
 export const PasteDocumentAction: DocumentActionComponent = ({id, type, onComplete}) => {
-  const documentStore = useDocumentStore()
-  const {patch} = useDocumentOperation(id, type)
-  const [isPasting, setPasting] = useState(false)
-  const toast = useToast()
+  const {onPaste} = useCopyPasteAction({
+    documentId: id,
+    documentType: type,
+    path: [],
+    schemaType: type,
+  })
+  const handle = useCallback(() => {
+    onPaste()
+    onComplete()
+  }, [onPaste, onComplete])
+
   const [permissions, isPermissionsLoading] = useDocumentPairPermissions({
     id,
     type,
     permission: 'update',
   })
-
   const currentUser = useCurrentUser()
-
-  const handle = useCallback(async () => {
-    setPasting(true)
-
-    // // set up the listener before executing
-    const pasteSuccess = firstValueFrom(
-      documentStore.pair
-        .operationEvents(id, type)
-        .pipe(filter((e) => e.op === 'patch' && e.type === 'success')),
-    )
-
-    const value = localStorage.getItem('copiedDocument')
-    if (!value) {
-      return
-    }
-    const parsed = JSON.parse(value) as any
-    const newValue = parsed.docValue
-
-    toast.push({
-      status: 'success',
-      title: 'Field updated',
-    })
-
-    delete newValue?.['_id']
-    delete newValue?.['_type']
-    delete newValue?.['_createdAt']
-    delete newValue?.['_updatedAt']
-    delete newValue?.['_rev']
-
-    patch.execute([PatchEvent.from(set(newValue, []))])
-
-    await pasteSuccess
-
-    toast.push({
-      status: 'success',
-      title: 'Document updated',
-    })
-
-    onComplete()
-  }, [onComplete, type])
 
   if (!isPermissionsLoading && !permissions?.granted) {
     return {
       icon: ClipboardIcon,
-      disabled: true,
+      disabled: isPermissionsLoading,
       label: 'Paste',
       title: (
         <InsufficientPermissionsMessage context="duplicate-document" currentUser={currentUser} />
@@ -86,5 +47,3 @@ export const PasteDocumentAction: DocumentActionComponent = ({id, type, onComple
     onHandle: handle,
   }
 }
-
-// CopyDocumentAction.action = 'duplicate'
--- dev/test-studio/fieldActions/copyAction.ts
+++ dev/test-studio/fieldActions/copyAction.ts
@@ -1,16 +1,8 @@
 import {CopyIcon} from '@sanity/icons'
 import {useCallback} from 'react'
-import {
-  Path,
-  SanityDocument,
-  defineDocumentFieldAction,
-  useDocumentStore,
-  useFormValue,
-  useGetFormValue,
-} from 'sanity'
+import {defineDocumentFieldAction, type Path, useCopyPasteAction} from 'sanity'
 
 import {defineActionItem} from './define'
-import {useToast} from '@sanity/ui'
 
 export interface CopyActionResult {
   documentId: string
@@ -26,47 +18,17 @@ export interface CopyActionResult {
 export const copyAction = defineDocumentFieldAction({
   name: 'test/copy',
   useAction({documentId, documentType, path, schemaType}) {
-    const {jsonType, name: schemaTypeName} = schemaType
-    // const docValue = useFormValue(path) as SanityDocument
-    const getter = useGetFormValue()
-    const toast = useToast()
+    const {onCopy} = useCopyPasteAction({documentId, documentType, path, schemaType})
     const onAction = useCallback(() => {
-      // const documentStore = useDocumentStore()
-      const docValue = getter(path)
-      const isDocument = schemaType?.type?.name === 'document'
-      const isArray = schemaType?.type?.name === 'array'
-      const isObject = schemaType?.type?.name === 'document'
-      // const name: string = schemaType?.type?.name || schemaType?.name
-      const payloadValue: CopyActionResult = {
-        schemaTypeName,
-        documentId,
-        documentType,
-        path,
-        docValue,
-        isDocument,
-        isArray,
-        isObject,
-      }
-
-      const label = isDocument ? `Document` : `Field`
-
-      // navigator.clipboard.writeText(value)
-      localStorage.setItem('copyActionResult', JSON.stringify(payloadValue))
-
-      toast.push({
-        status: 'success',
-        title: `${label} copied`,
-      })
-
-      // eslint-disable-next-line no-console
-      console.log('comment', {payloadValue, jsonType})
-    }, [documentId, documentType, path, toast])
+      onCopy()
+    }, [onCopy])
 
     return defineActionItem({
       type: 'action',
       icon: CopyIcon,
       onAction,
       title: 'Copy',
+      hidden: path.length === 0,
     })
   },
 })
