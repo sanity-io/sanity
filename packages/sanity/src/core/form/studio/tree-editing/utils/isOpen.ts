23808319f54dcad5e2ba66540ca1e64e6849df8e
RitaDias <anarita151@gmail.com>
2024-05-16 13:04:28 +0200

    feat(core): update condition for navigation

--- packages/sanity/src/core/form/studio/tree-editing/utils/isOpen.ts
+++ packages/sanity/src/core/form/studio/tree-editing/utils/isOpen.ts
@@ -1,11 +1,17 @@
 import {toString} from '@sanity/util/paths'
-import {type ObjectSchemaType, type Path} from 'sanity'
+import {isObjectSchemaType, type ObjectSchemaType, type Path} from 'sanity'
 
 import {getSchemaField} from './getSchemaField'
 
 export function isOpen(schemaType: ObjectSchemaType, focusPath: Path): boolean {
-  const startSegment = focusPath[0]
-  if (!startSegment) return false
+  const [startSegment, secondSegment] = focusPath
+  /* 
+  If the first path is empty or
+  if the second segment does not have a key then it means that we are clicking on the array input and not on an item pr
+  if the schemaType is not an object schema type then the dialog should not open
+  */
+  if (!startSegment || !secondSegment?.hasOwnProperty('_key') || !isObjectSchemaType(schemaType))
+    return false
 
   return getSchemaField(schemaType, toString([startSegment]))?.type.jsonType === 'array'
 }
