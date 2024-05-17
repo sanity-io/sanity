b00c8c946e74d5e2ce004def5f913189c0c0809f
Jordan Lawrence <jordanl17@me.com>
2024-05-17 13:44:24 +0100

    feat(structure): testing for useDocumentSheetColumns with initial cols

---
+++ packages/sanity/src/structure/panes/documentList/__tests__/useDocumentSheetColumns.test.ts
@@ -0,0 +1,51 @@
+import {describe, expect, it, jest} from '@jest/globals'
+import {renderHook} from '@testing-library/react'
+
+import {useDocumentSheetColumns} from '../useDocumentSheetColumns'
+
+jest.mock('sanity', () => ({
+  ...(jest.requireActual('sanity') || {}),
+  useDocumentPreviewStore: jest.fn().mockReturnValue({}),
+}))
+
+describe('useDocumentSheetColumns', () => {
+  it('returns initial column visibilities', () => {
+    const mockSchemaType = {
+      name: 'author',
+      title: 'Author',
+      type: 'document',
+      fields: [
+        {name: 'name', type: {name: 'string'}},
+        {name: 'nickname', type: {name: 'string'}},
+        {name: 'email', type: {name: 'string'}},
+        {name: 'age', type: {name: 'number'}},
+        {
+          name: 'address',
+          type: {
+            name: 'object',
+            fields: [
+              {name: 'street', type: {name: 'string'}},
+              {name: 'country', type: {name: 'string'}},
+            ],
+          },
+        },
+        {name: 'phone number', type: {name: 'number'}},
+        {name: 'has pet', type: {name: 'boolean'}},
+      ],
+    }
+
+    const {result} = renderHook(() => useDocumentSheetColumns(mockSchemaType))
+    expect(result.current.initialColumnsVisibility).toEqual({
+      'Preview': true,
+      'selected': true,
+      'name': true,
+      'nickname': true,
+      'email': true,
+      'age': true,
+      'address.street': true,
+      'address.country': false,
+      'phone number': false,
+      'has pet': false,
+    })
+  })
+})
