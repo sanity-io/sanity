47532988b28af97501fa645a1d5d80f99bfcfa1e
Herman Wikner <wiknerherman@gmail.com>
2024-05-20 11:04:35 +0200

    feat(core): improve breadcrumbs in tree editing

--- packages/sanity/src/core/form/studio/tree-editing/utils/build-tree-editing-state/buildBreadcrumbsState.ts
+++ packages/sanity/src/core/form/studio/tree-editing/utils/build-tree-editing-state/buildBreadcrumbsState.ts
@@ -1,3 +1,4 @@
+import {isEqual} from 'lodash'
 import {
   type ArraySchemaType,
   EMPTY_ARRAY,
@@ -13,42 +14,44 @@ interface BuildBreadcrumbsStateProps {
   arrayValue: Record<string, unknown>[]
   itemPath: Path
   parentPath: Path
-  title: string
 }
 
 export function buildBreadcrumbsState(props: BuildBreadcrumbsStateProps): TreeEditingBreadcrumb {
-  const {arraySchemaType, arrayValue, itemPath, parentPath, title} = props
+  const {arraySchemaType, arrayValue, itemPath, parentPath} = props
+
+  const items = arrayValue.map((arrayItem) => {
+    const nestedItemPath = [...parentPath, {_key: arrayItem._key}] as Path
+    const nestedItemType = arrayItem?._type as string
+
+    const nestedItemSchemaField = arraySchemaType?.of?.find(
+      (type) => type.name === nestedItemType,
+    ) as ObjectSchemaType
+
+    // Is anonymous object (no _type field)
+    if (!nestedItemType) {
+      return {
+        path: nestedItemPath,
+        title: 'Unknown', // todo: what should we do here?
+        children: EMPTY_ARRAY,
+      }
+    }
+
+    const nestedPreviewTitleKey = nestedItemSchemaField?.preview?.select?.title || ''
+    const nestedTitle =
+      arrayItem?.[nestedPreviewTitleKey] || getSchemaTypeTitle(nestedItemSchemaField)
+
+    return {
+      path: nestedItemPath,
+      title: String(nestedTitle || 'Untitled'),
+      children: EMPTY_ARRAY,
+    }
+  })
+
+  const selectedItemTitle = items.find((item) => isEqual(item.path, itemPath))?.title
 
   return {
     path: itemPath,
-    title: (title || 'Untitled') as string,
-    children: arrayValue.map((arrayItem) => {
-      const nestedItemPath = [...parentPath, {_key: arrayItem._key}] as Path
-      const nestedItemType = arrayItem?._type as string
-
-      const nestedItemSchemaField = arraySchemaType?.of?.find(
-        (type) => type.name === nestedItemType,
-      ) as ObjectSchemaType
-
-      // Is anonymous object (no _type field)
-      if (!nestedItemType) {
-        return {
-          path: nestedItemPath,
-          title: 'Unknown', // todo: what should we do here?
-          children: EMPTY_ARRAY,
-        }
-      }
-
-      const nestedPreviewTitleKey = nestedItemSchemaField?.preview?.select?.title
-      const nestedTitle = nestedPreviewTitleKey
-        ? arrayItem?.[nestedPreviewTitleKey]
-        : getSchemaTypeTitle(nestedItemSchemaField)
-
-      return {
-        path: nestedItemPath,
-        title: nestedTitle as string,
-        children: EMPTY_ARRAY,
-      }
-    }),
+    title: (selectedItemTitle || 'Untitled') as string,
+    children: items,
   }
 }
