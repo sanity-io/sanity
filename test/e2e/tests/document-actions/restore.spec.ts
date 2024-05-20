98a43531c7f85db6924bf66ad1aacd5a6778e92b
Ash <ash@sanity.io>
2024-05-20 11:29:17 +0100

    test(e2e): add test for removed `restore` document action

--- test/e2e/tests/document-actions/restore.spec.ts
+++ test/e2e/tests/document-actions/restore.spec.ts
@@ -97,3 +97,44 @@ test(`respects overridden restore action`, async ({page, createDraftDocument}) =
   await confirmButton.click()
   await expect(title).toHaveText(titleA)
 })
+
+test(`respects removed restore action`, async ({page, createDraftDocument}) => {
+  const titleA = 'Title A'
+  const titleB = 'Title B'
+
+  const documentStatus = page.getByTestId('pane-footer-document-status')
+  const publishButton = page.getByTestId('action-Publish')
+  const restoreButton = page.getByTestId('action-Restore')
+  const timelineMenuOpenButton = page.getByTestId('timeline-menu-open-button')
+  const timelineItemButton = page.getByTestId('timeline-item-button')
+  const previousRevisionButton = timelineItemButton.nth(2)
+  const title = page.getByTestId('document-panel-document-title')
+  const titleInput = page.getByTestId('field-title').getByTestId('string-input')
+
+  await createDraftDocument('/test/content/input-debug;removeRestoreActionTest')
+  await titleInput.fill(titleA)
+
+  // Wait for the document to be published.
+  await page.waitForTimeout(1_000)
+  await publishButton.click()
+  await expect(documentStatus).toContainText('Published just now')
+
+  // Change the title.
+  await titleInput.fill(titleB)
+  await expect(title).toHaveText(titleB)
+
+  // Wait for the document to be published.
+  await page.waitForTimeout(1_000)
+  await publishButton.click()
+  await expect(documentStatus).toContainText('Published just now')
+
+  // Pick the previous revision from the revision timeline.
+  await timelineMenuOpenButton.click()
+  await expect(previousRevisionButton).toBeVisible()
+  await previousRevisionButton.click({force: true})
+
+  await expect(titleInput).toHaveValue(titleA)
+
+  // Ensure the restore button is not displayed.
+  await expect(restoreButton).not.toBeVisible()
+})
