6bc16a62265d71b9e9629c74ee86354cf938974d
Ash <ash@sanity.io>
2024-05-20 11:25:48 +0100

    test(e2e): add test for custom `restore` document action

--- test/e2e/tests/document-actions/restore.spec.ts
+++ test/e2e/tests/document-actions/restore.spec.ts
@@ -44,3 +44,56 @@ test(`documents can be restored to an earlier revision`, async ({page, createDra
   await confirmButton.click()
   await expect(title).toHaveText(titleA)
 })
+
+test(`respects overridden restore action`, async ({page, createDraftDocument}) => {
+  const titleA = 'Title A'
+  const titleB = 'Title B'
+
+  const publishKeypress = () => page.locator('body').press('Control+Alt+p')
+  const documentStatus = page.getByTestId('pane-footer-document-status')
+  const restoreButton = page.getByTestId('action-Restore')
+  const customRestoreButton = page.getByRole('button').getByText('Custom restore')
+  const confirmButton = page.getByTestId('confirm-dialog-confirm-button')
+  const timelineMenuOpenButton = page.getByTestId('timeline-menu-open-button')
+  const timelineItemButton = page.getByTestId('timeline-item-button')
+  const previousRevisionButton = timelineItemButton.nth(2)
+  const titleInput = page.getByTestId('field-title').getByTestId('string-input')
+
+  await createDraftDocument('/test/content/input-debug;documentActionsTest')
+  const title = page.getByTestId('document-panel-document-title')
+  await titleInput.fill(titleA)
+
+  // Wait for the document to be published.
+  //
+  // Note: This is invoked using the publish keyboard shortcut, because the publish document action
+  // has been overridden for the `documentActionsTest` type, and is not visible without opening the
+  // document actions menu.
+  await page.waitForTimeout(1_000)
+  await publishKeypress()
+  await expect(documentStatus).toContainText('Published just now')
+
+  // Change the title.
+  await titleInput.fill(titleB)
+  await expect(title).toHaveText(titleB)
+
+  // Wait for the document to be published.
+  await page.waitForTimeout(1_000)
+  await publishKeypress()
+  await expect(documentStatus).toContainText('Published just now')
+
+  // Pick the previous revision from the revision timeline.
+  await timelineMenuOpenButton.click()
+  await expect(previousRevisionButton).toBeVisible()
+  await previousRevisionButton.click({force: true})
+
+  await expect(titleInput).toHaveValue(titleA)
+
+  // Ensure the custom restore button is rendered instead of the default restore button.
+  await expect(customRestoreButton).toBeVisible()
+  await expect(restoreButton).not.toBeVisible()
+
+  // Ensure the custom restore action can invoke the system restore action.
+  await customRestoreButton.click()
+  await confirmButton.click()
+  await expect(title).toHaveText(titleA)
+})
