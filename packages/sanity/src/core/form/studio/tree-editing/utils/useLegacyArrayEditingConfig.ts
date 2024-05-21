3888413b09eb59e70245ff5bef0a569472bf9558
RitaDias <anarita151@gmail.com>
2024-05-21 13:15:36 +0200

    docs(sanity): add comments

--- packages/sanity/src/core/form/studio/tree-editing/utils/useLegacyArrayEditingConfig.ts
+++ packages/sanity/src/core/form/studio/tree-editing/utils/useLegacyArrayEditingConfig.ts
@@ -1,5 +1,10 @@
 import {useSource} from 'sanity'
 
+/**
+ * A utility hook to check if the array editing dialog should be open (based on config)
+ * @returns Returns true if the dialog should be open
+ * @internal
+ */
 export function useLegacyArrayEditingConfig(): boolean {
   const {document} = useSource()
 
