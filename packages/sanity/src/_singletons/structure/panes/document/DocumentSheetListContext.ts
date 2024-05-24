c3435122bcf70f45c5c8e87a403410a27d5a1db5
Jordan Lawrence <jordanl17@me.com>
2024-05-24 03:46:00 +0100

    feat(structure): fixing mock for pane test

--- packages/sanity/src/_singletons/structure/panes/document/DocumentSheetListContext.ts
+++ packages/sanity/src/_singletons/structure/panes/document/DocumentSheetListContext.ts
@@ -2,4 +2,5 @@ import {createContext} from 'react'
 
 import type {SheetListContextValue} from '../../../../structure/panes/documentList/sheetList/SheetListProvider'
 
+/** @internal */
 export const SheetListContext = createContext<SheetListContextValue | undefined>(undefined)
