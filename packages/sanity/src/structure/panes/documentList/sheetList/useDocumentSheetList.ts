92c73989abff96a3e3a8d664dcb01d6aba21f917
Pedro Bonamin <pedrobonamin@gmail.com>
2024-05-24 14:35:07 +0200

    feat(structure): add sheet list publish action pending items: - validate user permissions for publish - validate document

--- packages/sanity/src/structure/panes/documentList/sheetList/useDocumentSheetList.ts
+++ packages/sanity/src/structure/panes/documentList/sheetList/useDocumentSheetList.ts
@@ -1,6 +1,7 @@
 import {useMemo} from 'react'
-import {getPublishedId, useSearchState} from 'sanity'
+import {getDraftId, getPublishedId, useSearchState} from 'sanity'
 
+import {type DocumentSheetTableRow} from './types'
 import {useDocumentSheetListStore} from './useDocumentSheetListStore'
 
 interface DocumentSheetListOptions {
@@ -8,7 +9,10 @@ interface DocumentSheetListOptions {
   typeName: string
 }
 
-export function useDocumentSheetList({typeName}: DocumentSheetListOptions) {
+export function useDocumentSheetList({typeName}: DocumentSheetListOptions): {
+  data: DocumentSheetTableRow[]
+  isLoading: boolean
+} {
   const {state} = useSearchState()
 
   const items = useMemo(() => {
@@ -18,14 +22,38 @@ export function useDocumentSheetList({typeName}: DocumentSheetListOptions) {
   }, [state.result.hits])
 
   // The store is listening to all the documents that match with the _type filter.
-  const {data, isLoading} = useDocumentSheetListStore({
+  const {
+    data,
+    isLoading,
+    documents: allDocuments,
+  } = useDocumentSheetListStore({
     filter: `_type == "${typeName}"`,
   })
 
   // Only return the documents that match with the serverSide filter items.
-  const documents = useMemo(() => {
-    return data.filter((doc) => items.has(getPublishedId(doc._id)))
-  }, [data, items])
+  const documents: DocumentSheetTableRow[] = useMemo(() => {
+    return data
+      .filter((doc) => items.has(getPublishedId(doc._id)))
+      .map((doc) => {
+        const draftId = getDraftId(doc._id)
+        const publishedId = getPublishedId(doc._id)
+        const draft = allDocuments[draftId]
+        const published = allDocuments[publishedId]
+        return {
+          ...doc,
+          __metadata: {
+            idPair: {
+              draftId,
+              publishedId,
+            },
+            snapshots: {
+              draft,
+              published,
+            },
+          },
+        }
+      })
+  }, [data, items, allDocuments])
 
   return {data: documents, isLoading}
 }
