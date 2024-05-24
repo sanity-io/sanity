dd3e17b0758850ae3ae3f46f2e30f20d540bcf88
Christian Hamburger Grøngaard <christian.groengaard@sanity.io>
2024-05-24 12:36:15 +0200

    chore(page-building): use array options.insertMenu

--- dev/page-building-studio/schemaTypes/index.ts
+++ dev/page-building-studio/schemaTypes/index.ts
@@ -1,6 +1,6 @@
 import {hero} from './hero'
 import {logoCarousel} from './logoCarousel'
-import {page} from './page'
+import {page, pageOneBlockType} from './page'
 import {testimonial, testimonials} from './testimonials'
 
-export const schemaTypes = [page, hero, logoCarousel, testimonial, testimonials]
+export const schemaTypes = [page, pageOneBlockType, hero, logoCarousel, testimonial, testimonials]
--- dev/page-building-studio/schemaTypes/page.ts
+++ dev/page-building-studio/schemaTypes/page.ts
@@ -15,6 +15,54 @@ export const page = defineType({
       title: 'Blocks',
       type: 'array',
       of: [{type: 'hero'}, {type: 'logo-carousel'}, {type: 'testimonials'}],
+      options: {
+        insertMenu: {
+          layout: 'full',
+          groups: [
+            {
+              name: 'intro',
+              title: 'Intro',
+              of: ['hero'],
+            },
+            {
+              name: 'storytelling',
+              title: 'Storytelling',
+            },
+            {
+              name: 'upsell',
+              title: 'Upsell',
+              of: ['testimonials', 'hero'],
+            },
+          ],
+        },
+      },
+    }),
+  ],
+})
+
+export const pageOneBlockType = defineType({
+  type: 'document',
+  name: 'pageOneBlockType',
+  title: 'Page (One Block Type)',
+  groups: [{name: 'meta'}, {name: 'blocks'}],
+  fields: [
+    defineField({
+      type: 'string',
+      name: 'title',
+      title: 'Title',
+      group: 'meta',
+    }),
+    defineField({
+      name: 'blocks',
+      title: 'Blocks',
+      type: 'array',
+      group: 'blocks',
+      of: [{type: 'hero'}],
+      options: {
+        insertMenu: {
+          layout: 'full',
+        },
+      },
     }),
   ],
 })
