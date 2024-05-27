eed385728a0f97e0579b82b7d6a4af2b1221e525
Herman Wikner <wiknerherman@gmail.com>
2024-05-27 19:18:10 +0200

    feat(core): keep legacy array editing in portable text editor

--- packages/sanity/src/core/form/studio/tree-editing/utils/useTreeArrayEditingEnabled.ts
+++ packages/sanity/src/core/form/studio/tree-editing/utils/useTreeArrayEditingEnabled.ts
@@ -1,13 +1,31 @@
+import {useContext, useMemo} from 'react'
 import {useSource} from 'sanity'
+import {PortableTextAwareContext} from 'sanity/_singletons'
+
+interface TreeArrayEditingEnabledHookValue {
+  /**
+   * A boolean indicating whether tree array editing is enabled.
+   */
+  enabled: boolean
+  /**
+   * A boolean indicating whether the legacy array editing should be used.
+   */
+  legacyEditing: boolean
+}
 
 /**
- * A utility hook to check if the tree array editing dialog should be open (based on config)
- * @returns Returns true if the dialog should be open
  * @internal
+ * A hook that provides information about whether tree array editing is enabled.
  */
-export function useTreeArrayEditingEnabled(): boolean {
+export function useTreeArrayEditingEnabled(): TreeArrayEditingEnabledHookValue {
   const {features} = useSource()
+  const hasEditorParent = useContext(PortableTextAwareContext)?.hasEditorParent
 
-  // it either has the value of the `enabled` property or defaults to `true`
-  return features?.beta?.treeArrayEditing?.enabled ?? false
+  return useMemo(
+    (): TreeArrayEditingEnabledHookValue => ({
+      enabled: features?.beta?.treeArrayEditing?.enabled === true,
+      legacyEditing: Boolean(hasEditorParent),
+    }),
+    [features, hasEditorParent],
+  )
 }
