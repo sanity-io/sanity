9c8b6652a54164e2329764880ee40fb5577513dd
Binoy Patel <me@binoy.io>
2024-05-28 14:42:45 -0400

    refactor(cli): updates the compare function to use previous work

---
+++ packages/sanity/src/_internal/cli/util/getAutoUpdatesImportMap.ts
@@ -0,0 +1,25 @@
+/**
+ * @internal
+ */
+export interface AutoUpdatesImportMap {
+  'sanity': string
+  'sanity/': string
+  '@sanity/vision': string
+  '@sanity/vision/': string
+}
+
+const MODULES_HOST = 'https://api.sanity.work'
+
+/**
+ * @internal
+ */
+export function getAutoUpdateImportMap(version: string): AutoUpdatesImportMap {
+  const autoUpdatesImports = {
+    'sanity': `${MODULES_HOST}/v1/modules/sanity/default/${version}`,
+    'sanity/': `${MODULES_HOST}/v1/modules/sanity/default/${version}/`,
+    '@sanity/vision': `${MODULES_HOST}/v1/modules/@sanity__vision/default/${version}`,
+    '@sanity/vision/': `${MODULES_HOST}/v1/modules/@sanity__vision/default/${version}/`,
+  }
+
+  return autoUpdatesImports
+}
