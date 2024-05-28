879cbf7b8320dd1fd1672db1f1cefac83e8e3296
RitaDias <anarita151@gmail.com>
2024-05-28 09:59:41 +0200

    docs(core): append some comments for clearer logic

--- packages/sanity/src/core/form/studio/tree-editing/utils/useTreeArrayEditingEnabled.ts
+++ packages/sanity/src/core/form/studio/tree-editing/utils/useTreeArrayEditingEnabled.ts
@@ -9,6 +9,8 @@ interface TreeArrayEditingEnabledHookValue {
   enabled: boolean
   /**
    * A boolean indicating whether the legacy array editing should be used.
+   * Specifically it's currently used to add an exception for PTEs since they are
+   * currently outside of the scope of the tree array editing feature.
    */
   legacyEditing: boolean
 }
@@ -16,6 +18,8 @@ interface TreeArrayEditingEnabledHookValue {
 /**
  * @internal
  * A hook that provides information about whether tree array editing is enabled.
+ * This hook needs to exist while we have the two type of solutions for array editing available
+ * and the PTE is not yet fully integrated with the tree array editing feature.
  */
 export function useTreeArrayEditingEnabled(): TreeArrayEditingEnabledHookValue {
   const {features} = useSource()
