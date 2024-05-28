115b2e4f5ddc8edfd1d00fb26d0c2c7e3d63796e
Per-Kristian Nordnes <per.kristian.nordnes@gmail.com>
2024-05-28 13:46:52 +0200

    test(core): add value transfer test for global copy/paste

---
+++ packages/sanity/src/core/studio/copyPaste/__test__/valueTransfer.test.ts
@@ -0,0 +1,39 @@
+import {beforeEach, describe, expect, jest, test} from '@jest/globals'
+import {type CopyActionResult} from 'sanity'
+
+import {transferValue} from '../valueTransfer'
+import {schema} from './schema'
+
+beforeEach(() => {
+  jest.resetModules()
+  jest.clearAllMocks()
+})
+
+describe('transferValue', () => {
+  test('can copy portable text field', () => {
+    const docValue = {
+      body: [
+        {
+          _key: 'someKey',
+          _type: 'customNamedBlock',
+          children: [{_key: 'someOtherKey', _type: 'span', text: 'Hello'}],
+        },
+      ],
+    }
+    const copyActionResult: CopyActionResult = {
+      documentId: '123',
+      documentType: 'author',
+      schemaTypeName: 'author',
+      path: ['bio'],
+      docValue,
+      isDocument: true,
+      isArray: false,
+      isObject: false,
+    }
+    const targetValue = transferValue(copyActionResult, {
+      targetDocumentType: 'author',
+      targetSchemaType: schema.get('author')!,
+    })
+    expect(targetValue).toEqual(docValue)
+  })
+})
