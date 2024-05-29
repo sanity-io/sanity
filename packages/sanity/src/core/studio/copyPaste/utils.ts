947788e2f9c95ccd3419f4c9d02b907305ffd5ae
Fred Carlsen <fred@sjelfull.no>
2024-05-29 15:40:47 +0200

    refactor: prepare for using clipboard insted of LS
    Signed-off-by: Fred Carlsen <fred@sjelfull.no>

--- packages/sanity/src/core/studio/copyPaste/utils.ts
+++ packages/sanity/src/core/studio/copyPaste/utils.ts
@@ -7,10 +7,40 @@ export const getLocalStorageItem = (key: string): CopyActionResult | null => {
   return item ? JSON.parse(item) : null
 }
 
+export const getClipboardItem = async (): Promise<CopyActionResult | null> => {
+  const value = await window.navigator.clipboard.readText().then((text) => {
+    return parseCopyResult(text)
+  })
+
+  if (!isCopyPasteResult(value)) {
+    return null
+  }
+
+  return value
+}
+
+export const writeClipboardItem = (value: CopyActionResult): void => {
+  window.navigator.clipboard.writeText(JSON.stringify(value))
+}
+
 export const setLocalStorageItem = (key: string, value: CopyActionResult): void => {
   localStorage.setItem(key, JSON.stringify(value))
 }
 
 export function getLocalStorageKey(projectId: string) {
   return `${LS_RESULT_KEY}:${projectId}`
+}
+
+export function isCopyPasteResult(value: any): value is CopyActionResult {
+  const normalized = typeof value === 'string' ? parseCopyResult(value) : value
+
+  return typeof normalized === 'object' && normalized?._type === 'copyResult'
+}
+
+export function parseCopyResult(value: any): CopyActionResult | null {
+  try {
+    return JSON.parse(value)
+  } catch (_) {
+    return null
+  }
 }
