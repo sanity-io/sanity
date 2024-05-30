701f05248f32e69bf40c76244238c63041516c6c
Binoy Patel <me@binoy.io>
2024-05-30 16:00:15 -0400

    feat(cli): enable autoUpdates by default on new projects

--- packages/@sanity/cli/src/actions/init-project/createCliConfig.ts
+++ packages/@sanity/cli/src/actions/init-project/createCliConfig.ts
@@ -9,7 +9,12 @@ export default defineCliConfig({
   api: {
     projectId: '%projectId%',
     dataset: '%dataset%'
-  }
+  },
+  /**
+   * Enable auto-updates for studios.
+   * Learn more at https://www.sanity.io/docs/cli#auto-updates
+   */
+  autoUpdates: true,
 })
 `
 
