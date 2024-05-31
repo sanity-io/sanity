2a82da720fd320dded02fa1b7f28560279df755f
RitaDias <anarita151@gmail.com>
2024-05-31 13:19:20 +0200

    test(e2e): add breadcrumb test

--- test/e2e/tests/tree-editing/TreeEditingNavigation.spec.ts
+++ test/e2e/tests/tree-editing/TreeEditingNavigation.spec.ts
@@ -64,6 +64,8 @@ test.describe('navigation - tree sidebar', () => {
     modal.getByTestId('string-input').fill('Albert, the whale')
     await page.getByRole('button', {name: 'Done'}).click()
 
+    await expect(modal).not.toBeVisible()
+
     // second element
     await page.getByTestId('field-animals').getByRole('button', {name: 'Add item'}).click()
     modal.getByTestId('string-input').fill('Lucy, the cat')
@@ -85,7 +87,61 @@ test.describe('navigation - tree sidebar', () => {
     // Wait for the animation to change form to finish
     await waitForOpacityChange(page, '[data-testid="tree-editing-dialog-content"]', 5000)
 
-    //await page.waitForTimeout(500) // Hack, need to wait for animation to finish
+    // make sure that item is selected on nav tree
+    await expect(
+      await page.getByRole('treeitem', {name: 'Lucy, the cat'}).getByTestId('side-menu-item'),
+    ).toHaveAttribute('data-selected')
+
+    // make sure first input has the right data
+    await expect(await modal.getByTestId('string-input').inputValue()).toBe('Lucy, the cat')
+
+    // make sure breadcrumb shows the right item
+    await expect(await modal.locator('#tree-breadcrumb-menu-button').textContent()).toBe(
+      'Lucy, the cat',
+    )
+  })
+})
+
+test.describe('navigation - breadcrumb', () => {
+  test.beforeEach(async ({page, createDraftDocument}) => {
+    // set up an array with two items: Albert, the whale and Lucy, the cat
+    await createDraftDocument('/test/content/input-debug;objectsDebug')
+    const modal = await page.getByTestId('tree-editing-dialog')
+
+    // first element
+    await page.getByTestId('field-animals').getByRole('button', {name: 'Add item'}).click()
+    modal.getByTestId('string-input').fill('Albert, the whale')
+    await page.getByRole('button', {name: 'Done'}).click()
+
+    await expect(modal).not.toBeVisible()
+
+    // second element
+    await page.getByTestId('field-animals').getByRole('button', {name: 'Add item'}).click()
+    modal.getByTestId('string-input').fill('Lucy, the cat')
+    await page.getByRole('button', {name: 'Done'}).click()
+  })
+
+  // first level array item test
+  test(`opening the first item, you should be able to navigate to the second array item`, async ({
+    page,
+  }) => {
+    const modal = await page.getByTestId('tree-editing-dialog')
+
+    await page.getByRole('button', {name: 'Albert, the whale'}).click()
+
+    // click the breacrumb
+    await page.locator('#tree-breadcrumb-menu-button').click()
+    await page.waitForSelector('[data-ui="Popover__wrapper"]', {state: 'attached'})
+
+    // navigate on second array item in breadcrumb
+    // for some reason when trying to click playwright doesn't like the dropdown menu and doesn't let me select any
+    await page.keyboard.press('ArrowDown')
+    await page.keyboard.press('Enter')
+
+    await page.getByTestId('tree-editing-sidebar-toggle').click()
+
+    // Wait for the animation to change form to finish
+    await waitForOpacityChange(page, '[data-testid="tree-editing-dialog-content"]', 5000)
 
     // make sure that item is selected on nav tree
     await expect(
