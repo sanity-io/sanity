6bbdd02875a6054940bd9a419b6e598ae6ff5b98
Pedro Bonamin <pedrobonamin@gmail.com>
2024-06-04 10:37:44 -0300

    fix(structure): update sheet list select column

--- packages/sanity/typings/tanstack-table.d.ts
+++ packages/sanity/typings/tanstack-table.d.ts
@@ -6,4 +6,11 @@ declare module '@tanstack/react-table' {
     selectedAnchor: number | null
     setSelectedAnchor: (anchorRowIndex: number | null) => void
   }
+  interface ColumnMeta<TData extends RowData, TValue> {
+    /**
+     * Allows for custom headers to be rendered in the table
+     * opting out from the default header rendering.
+     */
+    customHeader?: boolean
+  }
 }
