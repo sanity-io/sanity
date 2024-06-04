dd1beb83a509bd5317f93621c9027c4de6aaf468
Pedro Bonamin <pedrobonamin@gmail.com>
2024-06-04 13:10:03 -0300

    feat(structure): edx-1313 - clicking on a row opens the document

--- packages/sanity/typings/tanstack-table.d.ts
+++ packages/sanity/typings/tanstack-table.d.ts
@@ -1,10 +1,13 @@
 import {type RowData} from '@tanstack/react-table'
 
+import {type BaseStructureToolPaneProps} from '../src/structure/panes/types'
+
 declare module '@tanstack/react-table' {
   interface TableMeta<TData extends RowData> {
     patchDocument: (documentId: string, fieldId: string, value: any) => void
     selectedAnchor: number | null
     setSelectedAnchor: (anchorRowIndex: number | null) => void
+    paneProps: BaseStructureToolPaneProps<'documentList'>
   }
   interface ColumnMeta<TData extends RowData, TValue> {
     /**
@@ -12,5 +15,6 @@ declare module '@tanstack/react-table' {
      * opting out from the default header rendering.
      */
     customHeader?: boolean
+    borderWidth?: number
   }
 }
