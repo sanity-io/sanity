a2eaea6c6f0a1562fb22c0bd51479fbf3f7e8827
Herman Wikner <wiknerherman@gmail.com>
2024-06-05 11:45:57 +0200

    feat(core): improve preview handling

--- packages/sanity/src/core/form/studio/tree-editing/components/search/TreeEditingSearchMenu.tsx
+++ packages/sanity/src/core/form/studio/tree-editing/components/search/TreeEditingSearchMenu.tsx
@@ -1,10 +1,58 @@
 import {Box, Button, Stack, Text} from '@sanity/ui'
 import {isEqual} from 'lodash'
 import {useCallback} from 'react'
-import {CommandList, type Path, supportsTouch, useTranslation} from 'sanity'
+import {
+  CommandList,
+  getSchemaTypeTitle,
+  type Path,
+  supportsTouch,
+  unstable_useValuePreview as useValuePreview,
+  useTranslation,
+} from 'sanity'
 
 import {type TreeEditingMenuItem} from '../../types'
 import {ITEM_HEIGHT} from './constants'
+
+interface ResultItemProps {
+  item: TreeEditingMenuItem
+  onPathSelect: (path: Path) => void
+  isFirst: boolean
+}
+
+function ResultItem(props: ResultItemProps): JSX.Element {
+  const {item, onPathSelect, isFirst} = props
+
+  const {value} = useValuePreview({
+    value: item.value,
+    schemaType: item.schemaType,
+  })
+
+  const title = value?.title || 'Untitled'
+
+  return (
+    <Stack marginTop={isFirst ? undefined : 1}>
+      <Button
+        mode="bleed"
+        // eslint-disable-next-line react/jsx-no-bind
+        onClick={() => onPathSelect(item.path)}
+      >
+        <Stack space={2} flex={1}>
+          <Box>
+            <Text size={1} textOverflow="ellipsis">
+              <b>{title}</b>
+            </Text>
+          </Box>
+
+          <Box>
+            <Text muted size={0} textOverflow="ellipsis">
+              {getSchemaTypeTitle(item.parentSchemaType)}
+            </Text>
+          </Box>
+        </Stack>
+      </Button>
+    </Stack>
+  )
+}
 
 interface TreeEditingSearchMenuProps {
   items: TreeEditingMenuItem[]
@@ -20,29 +68,7 @@ export function TreeEditingSearchMenu(props: TreeEditingSearchMenuProps): JSX.El
     (item: TreeEditingMenuItem) => {
       const isFirst = isEqual(item.path, items[0].path)
 
-      return (
-        <Stack marginTop={isFirst ? undefined : 1}>
-          <Button
-            mode="bleed"
-            // eslint-disable-next-line react/jsx-no-bind
-            onClick={() => onPathSelect(item.path)}
-          >
-            <Stack space={2} flex={1}>
-              <Box>
-                <Text size={1} textOverflow="ellipsis">
-                  <b>{item.title}</b>
-                </Text>
-              </Box>
-
-              <Box>
-                <Text muted size={0} textOverflow="ellipsis">
-                  {item.parentTitle}
-                </Text>
-              </Box>
-            </Stack>
-          </Button>
-        </Stack>
-      )
+      return <ResultItem isFirst={isFirst} item={item} onPathSelect={onPathSelect} />
     },
     [items, onPathSelect],
   )
