159d13ef2285eeaf6a6142d3ae0b805efc0510e3
Carolina Gonzalez <carolina.nicole.gonzalez@gmail.com>
2024-06-05 10:45:31 -0700

    chore: update internal tag to include comment

--- packages/sanity/src/core/environment/hasSanityPackageInImportMap.ts
+++ packages/sanity/src/core/environment/hasSanityPackageInImportMap.ts
@@ -1,19 +1,17 @@
-/*
- * The presence of the sanity module in the importmap script
- * indicates that the studio was probably built with an auto-updates flag.
+/**
+ * Returns whether or not there is a `sanity` entry in an import map in the current document,
+ * which usually means that this studio is "auto updating".
+ * @internal
  */
-
-/** @internal */
 export const hasSanityPackageInImportMap = () => {
   if (typeof document === 'undefined' || !('querySelectorAll' in document)) {
     return false
   }
   const importMapEntries = document.querySelectorAll('script[type="importmap"]')
-  return Array.from(importMapEntries)
-    .some((entry) => {
-      if (!entry.textContent) return false
-      const importMap = JSON.parse(entry.textContent)
-      const imports = importMap.imports || {}
-      return 'sanity' in imports
-    })
+  return Array.from(importMapEntries).some((entry) => {
+    if (!entry.textContent) return false
+    const importMap = JSON.parse(entry.textContent)
+    const imports = importMap.imports || {}
+    return 'sanity' in imports
+  })
 }
