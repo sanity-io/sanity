09a0aa064530b24791694df409c1225e38c6a040
Ash <ash@sanity.io>
2024-06-06 09:25:41 -0700

    wip

--- packages/sanity/src/_internal/cli/actions/manifest/extractManifestAction.ts
+++ packages/sanity/src/_internal/cli/actions/manifest/extractManifestAction.ts
@@ -126,8 +126,8 @@ async function externalizeSchema(
 ): Promise<ManifestV1Workspace> {
   const schemaString = JSON.stringify(workspace.schema, null, 2)
   const hash = createHash('sha1').update(schemaString).digest('hex')
-  // const filename = `${hash.slice(0, 8)}${SCHEMA_FILENAME_SUFFIX}`
-  const filename = `${workspace.name}${SCHEMA_FILENAME_SUFFIX}`
+  const filename = `${hash.slice(0, 8)}${SCHEMA_FILENAME_SUFFIX}`
+  // const filename = `${workspace.name}${SCHEMA_FILENAME_SUFFIX}`
 
   await writeFile(join(staticPath, filename), schemaString)
 
