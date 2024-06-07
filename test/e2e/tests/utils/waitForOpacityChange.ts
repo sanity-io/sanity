9d3ef4dc0f5a48ba9500926a5af973118b6d7449
RitaDias <anarita151@gmail.com>
2024-06-07 09:44:10 +0200

    chore(test): style

--- test/e2e/tests/utils/waitForOpacityChange.ts
+++ test/e2e/tests/utils/waitForOpacityChange.ts
@@ -1,7 +1,11 @@
 import {type Page} from '@playwright/test'
 
 /** used for detecting changes of opacity, especially useful when testing with motion */
-export async function waitForOpacityChange(page: Page, selector: string, timeout = 10000) {
+export async function waitForOpacityChange(
+  page: Page,
+  selector: string,
+  timeout = 10000,
+): Promise<void> {
   await page.waitForFunction(
     (sel) => {
       const element = document.querySelector(sel)
