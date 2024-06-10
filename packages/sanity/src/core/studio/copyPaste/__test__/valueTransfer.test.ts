af01e552aeae8c859c6129950e78e6a0c2d2d379
Per-Kristian Nordnes <per.kristian.nordnes@gmail.com>
2024-06-10 14:13:31 +0200

    refactor(core): use new value transfer function for copy/paste + tests (#6878)

--- packages/sanity/src/core/studio/copyPaste/__test__/valueTransfer.test.ts
+++ packages/sanity/src/core/studio/copyPaste/__test__/valueTransfer.test.ts
@@ -1,6 +1,7 @@
 import {beforeEach, describe, expect, jest, test} from '@jest/globals'
-import {type CopyActionResult} from 'sanity'
+import {omit} from 'lodash'
 
+import {resolveSchemaTypeForPath} from '../resolveSchemaTypeForPath'
 import {transferValue} from '../valueTransfer'
 import {schema} from './schema'
 
@@ -10,9 +11,11 @@ beforeEach(() => {
 })
 
 describe('transferValue', () => {
-  test('can copy portable text field', () => {
-    const docValue = {
-      body: [
+  test('cannot copy from one type to another if the schema json type is different', () => {
+    const sourceValue = {
+      _type: 'author',
+      _id: 'xxx',
+      bio: [
         {
           _key: 'someKey',
           _type: 'customNamedBlock',
@@ -20,21 +23,112 @@ describe('transferValue', () => {
         },
       ],
     }
-    const copyActionResult: CopyActionResult = {
-      _type: 'copyResult',
-      documentId: '123',
-      documentType: 'author',
-      schemaTypeName: 'author',
-      path: ['bio'],
-      docValue,
-      isDocument: true,
-      isArray: false,
-      isObject: false,
-    }
-    const targetValue = transferValue(copyActionResult, {
-      targetDocumentType: 'author',
-      targetSchemaType: schema.get('author')!,
+    const transferValueResult = transferValue({
+      sourceRootSchemaType: schema.get('author')!,
+      sourcePath: [],
+      sourceValue,
+      targetRootSchemaType: schema.get('editor')!,
+      targetPath: ['bio'],
     })
-    expect(targetValue).toEqual(docValue)
+    expect(transferValueResult.errors.length).toEqual(1)
+    expect(transferValueResult.errors[0].message).toEqual(
+      'Source and target schema types are not compatible',
+    )
   })
+
+  describe('objects', () => {
+    test('can copy object', () => {
+      const sourceValue = {
+        _type: 'author',
+        _id: 'xxx',
+        bio: [
+          {
+            _key: 'someKey',
+            _type: 'customNamedBlock',
+            children: [{_key: 'someOtherKey', _type: 'span', text: 'Hello'}],
+          },
+        ],
+      }
+      const transferValueResult = transferValue({
+        sourceRootSchemaType: schema.get('author')!,
+        sourcePath: [],
+        sourceValue,
+        targetRootSchemaType: schema.get('editor')!,
+        targetPath: [],
+      })
+      expect(transferValueResult?.targetValue).toEqual({
+        ...omit(sourceValue, ['_id']),
+        _type: 'editor',
+      })
+    })
+
+    test('can copy array of numbers', () => {
+      const sourceValue = {
+        _type: 'author',
+        _id: 'xxx',
+        favoriteNumbers: [1, 2, 3, 4, 'foo'],
+      }
+      const transferValueResult = transferValue({
+        sourceRootSchemaType: schema.get('author')!,
+        sourcePath: ['favoriteNumbers'],
+        sourceValue,
+        targetRootSchemaType: schema.get('editor')!,
+        targetPath: ['favoriteNumbers'],
+      })
+      expect(transferValueResult?.targetValue).toEqual([1, 2, 3, 4])
+    })
+
+    test('can copy nested objects', () => {
+      const sourceValue = {_type: 'nestedObject', _key: 'yyy', title: 'item', items: []}
+      const schemaTypeAtPath = resolveSchemaTypeForPath(schema.get('author')!, ['nestedTest'])
+      const transferValueResult = transferValue({
+        sourceRootSchemaType: schemaTypeAtPath!,
+        sourcePath: [],
+        sourceValue,
+        targetRootSchemaType: schema.get('editor')!,
+        targetPath: ['nestedTest'],
+      })
+      expect(transferValueResult?.targetValue).toEqual({_type: 'nestedObject', title: 'item'})
+    })
+
+    test('can copy image objects', () => {
+      const sourceValue = {
+        _type: 'image',
+        asset: {
+          _ref: 'image-e4be7fa20bb20c271060a46bca82b9e84907a13a-320x320-jpg',
+          _type: 'reference',
+        },
+      }
+      const schemaTypeAtPath = resolveSchemaTypeForPath(schema.get('author')!, ['profileImage'])
+      const transferValueResult = transferValue({
+        sourceRootSchemaType: schemaTypeAtPath!,
+        sourcePath: [],
+        sourceValue,
+        targetRootSchemaType: schema.get('editor')!,
+        targetPath: ['profileImage'],
+      })
+      expect(transferValueResult?.targetValue).toEqual(sourceValue)
+    })
+  })
+  // test.only('can copy nested objects', () => {
+  //   const sourceValue = {
+  //     _type: 'nestedObject',
+  //     title: 'root',
+  //     objectList: [{_type: 'nestedObject', _key: 'yyy', title: 'item', items: []}],
+  //   }
+  //   const transferValueResult = transferValue({
+  //     sourceRootSchemaType: schema.get('author')!,
+  //     sourcePath: ['nestedTest', 'objectList', {_key: 'yyy'}],
+  //     sourceValue,
+  //     targetRootSchemaType: schema.get('editor')!,
+  //     targetPath: ['nestedTest'],
+  //   })
+  //   expect(transferValueResult.errors.length).toEqual(0)
+  //   expect(transferValueResult?.targetValue).toEqual({
+  //     _type: 'nestedObject',
+  //     _key: 'yyy',
+  //     title: 'item',
+  //     items: [],
+  //   })
+  // })
 })
