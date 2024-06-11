640c1ca58a5d5c616203f70330eaa1ba03e617b0
Binoy Patel <me@binoy.io>
2024-06-11 17:45:14 -0400

    chore(cli): Update copy for build command unattended mode
    Co-authored-by: Espen Hovlandsdal <espen@hovlandsdal.com>

--- packages/sanity/src/_internal/cli/commands/build/buildCommand.ts
+++ packages/sanity/src/_internal/cli/commands/build/buildCommand.ts
@@ -5,7 +5,7 @@ const helpText = `
 Options
   --source-maps Enable source maps for built bundles (increases size of bundle)
   --no-minify Skip minifying built JavaScript (speeds up build, increases size of bundle)
-  -y, --yes Unattended mode, use 'y' for prompts and use only flags for choices
+  -y, --yes Unattended mode, answers "yes" to any "yes/no" prompt and otherwise uses defaults
 
 Examples
   sanity build
