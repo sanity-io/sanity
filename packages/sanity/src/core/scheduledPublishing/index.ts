43711d78a2f5d97dfcfe647a4961567b8a60270b
Pedro Bonamin <46196328+pedrobonamin@users.noreply.github.com>
2024-06-11 07:02:05 -0300

    fix(scheduled-publishing): export EditScheduleForm from core (#6872)
    * fix(scheduled-publishing): export EditScheduleForm from core
    
    * fix(scheduled-publishing):  add release tag to EditScheduleForm
    
    * fix(scheduled-publishing): move internal tag beneath the description

--- packages/sanity/src/core/scheduledPublishing/index.ts
+++ packages/sanity/src/core/scheduledPublishing/index.ts
@@ -1,2 +1,3 @@
+export * from './components/editScheduleForm/EditScheduleForm'
 export * from './plugin/documentActions/schedule/ScheduleAction'
 export * from './plugin/documentBadges/scheduled/ScheduledBadge'
