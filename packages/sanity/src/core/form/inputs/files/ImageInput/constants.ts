014b137228601c3510f6e1a8331c2e9c9419f795
Cody Olsen <stipsan@gmail.com>
2024-06-12 22:27:52 +0200

    refactor: extract `ImageInput-> renderAsset ` into FC

---
+++ packages/sanity/src/core/form/inputs/files/ImageInput/constants.ts
@@ -0,0 +1,13 @@
+import {type MenuButtonProps} from '../../../../../ui-components'
+
+/**
+ * @internal
+ * @hidden
+ */
+export const ASSET_FIELD_PATH = ['asset'] as const
+
+/**
+ * @internal
+ * @hidden
+ */
+export const ASSET_IMAGE_MENU_POPOVER: MenuButtonProps['popover'] = {portal: true} as const
