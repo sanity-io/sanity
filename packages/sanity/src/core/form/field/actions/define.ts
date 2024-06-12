2b2e2da03cf78f2883e1a5a236b178c3ddf2b2fc
Fred Carlsen <fred@sjelfull.no>
2024-06-12 16:27:05 +0200

    feat(core): move copy paste field actions to core
    fixes edx-1512
    fixes edx-1513
    
    Signed-off-by: Fred Carlsen <fred@sjelfull.no>

---
+++ packages/sanity/src/core/form/field/actions/define.ts
@@ -0,0 +1,9 @@
+import {type DocumentFieldActionGroup, type DocumentFieldActionItem} from 'sanity'
+
+export function defineActionItem(node: DocumentFieldActionItem): DocumentFieldActionItem {
+  return node
+}
+
+export function defineActionGroup(node: DocumentFieldActionGroup): DocumentFieldActionGroup {
+  return node
+}
