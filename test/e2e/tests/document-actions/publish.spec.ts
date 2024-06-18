1ffc92f1d083f415e5cf8ae528009ad16174642e
Ash <ash@sanity.io>
2024-06-18 10:07:38 +0100

    test(e2e): ensure custom publish action can patch document before publication
    Co-authored-by: Bjørge Næss <bjoerge@gmail.com>

--- test/e2e/tests/document-actions/publish.spec.ts
+++ test/e2e/tests/document-actions/publish.spec.ts
@@ -20,3 +20,30 @@ test(`document panel displays correct title for published document`, async ({
   // Ensure the correct title is displayed after publishing.
   expect(page.getByTestId('document-panel-document-title')).toHaveText(title)
 })
+
+test(`custom publish action can patch document before publication`, async ({
+  page,
+  createDraftDocument,
+}) => {
+  const title = 'Test Title'
+
+  const publishKeypress = () => page.locator('body').press('Control+Alt+p')
+  const documentStatus = page.getByTestId('pane-footer-document-status')
+  const titleInput = page.getByTestId('field-title').getByTestId('string-input')
+  const publishedAtInput = page.getByTestId('field-publishedAt').getByTestId('date-input')
+
+  await createDraftDocument('/test/content/input-debug;documentActionsTest')
+  await titleInput.fill(title)
+
+  // Wait for the document to be published.
+  //
+  // Note: This is invoked using the publish keyboard shortcut, because the publish document action
+  // has been overridden for the `documentActionsTest` type, and is not visible without opening the
+  // document actions menu.
+  await page.waitForTimeout(1_000)
+  await publishKeypress()
+  await expect(documentStatus).toContainText('Published just now')
+
+  // Ensure the custom publish action succeeded in setting the `publishedAt` field.
+  await expect(publishedAtInput).toHaveValue(/.*/)
+})
