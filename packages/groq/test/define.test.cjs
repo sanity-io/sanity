678bd683266543b4501ea3c2ba37d1c2f941bdd9
Romeo Van Snick <romeo@romeovansnick.be>
2024-06-19 10:16:16 +0200

    feat(typegen): groq/define module with a defineQuery helper

---
+++ packages/groq/test/define.test.cjs
@@ -0,0 +1,17 @@
+'use strict'
+// Integration test for the Node.js CJS runtime
+
+const {strict: assert} = require('node:assert')
+
+const {defineQuery} = require('groq/define')
+
+assert.equal(typeof defineQuery, 'function')
+
+assert.equal(defineQuery(`foo${'bar'}`), `foo${'bar'}`)
+assert.equal(defineQuery(`${'bar'}`), `${'bar'}`)
+assert.equal(defineQuery(``), ``)
+assert.equal(defineQuery(`${'foo'}`), `${'foo'}`)
+assert.equal(defineQuery(`${/foo/}bar`), `${/foo/}bar`)
+assert.equal(defineQuery(`${'foo'}bar${347}`), `${'foo'}bar${347}`)
+assert.equal(defineQuery(`${'foo'}bar${347}${/qux/}`), `${'foo'}bar${347}${/qux/}`)
+assert.equal(defineQuery(`${'foo'}${347}qux`), `${'foo'}${347}qux`)
