5dcbef3e9680d48d512b1d1202eff4df5a359ee9
Fred Carlsen <fred@sjelfull.no>
2024-06-20 17:12:00 +0200

    test(form): add component tests for copy paste
    Signed-off-by: Fred Carlsen <fred@sjelfull.no>

---
+++ packages/sanity/playwright-ct/tests/fixtures/copyPasteFixture.ts
@@ -0,0 +1,91 @@
+import {test as base} from '@playwright/experimental-ct-react'
+
+export const test = base.extend<{
+  getClipboardItemByMimeTypeAsText: (mimeType: string) => Promise<string | null>
+  setClipboardItems: (items: ClipboardItem[]) => Promise<void>
+  getClipboardItems: () => Promise<ClipboardItem[]>
+  getClipboardItemsAsText: () => Promise<string>
+  clipboardItems: {items: ClipboardItem[]}
+}>({
+  clipboardItems: [{items: [] as ClipboardItem[]}, {scope: 'test'}],
+
+  page: async ({page, clipboardItems}, use) => {
+    const setupClipboardMocks = () => {
+      return page.addInitScript((items) => {
+        const mockClipboard = {
+          read: () => {
+            return Promise.resolve((window as any).__clipboardItems)
+          },
+          write: (newItems: ClipboardItem[]) => {
+            ;(window as any).__clipboardItems = newItems
+
+            return Promise.resolve()
+          },
+          readText: () => {
+            const textItem = items.find((item) => item.types.includes('text/plain'))
+            return textItem
+              ? textItem.getType('text/plain').then((blob: Blob) => blob.text())
+              : Promise.resolve('')
+          },
+          writeText: (text: string) => {
+            const textBlob = new Blob([text], {type: 'text/plain'})
+            items.push(new ClipboardItem({'text/plain': textBlob}))
+            return Promise.resolve()
+          },
+        }
+        Object.defineProperty(Object.getPrototypeOf(navigator), 'clipboard', {
+          value: mockClipboard,
+          writable: false,
+        })
+        ;(window as any).__clipboardItems = items
+      }, clipboardItems.items)
+    }
+
+    await setupClipboardMocks()
+
+    page.on('framenavigated', async () => {
+      await setupClipboardMocks()
+    })
+
+    await use(page)
+  },
+
+  setClipboardItems: async ({clipboardItems}, use) => {
+    await use(async (items: ClipboardItem[]) => {
+      clipboardItems.items = items
+    })
+  },
+
+  getClipboardItems: async ({page}, use) => {
+    await use(() => {
+      return page.evaluate(() => navigator.clipboard.read())
+    })
+  },
+
+  getClipboardItemsAsText: async ({page}, use) => {
+    await use(async () => {
+      return page.evaluate(async () => {
+        const items = await navigator.clipboard.read()
+        const textItem = items.find((item) => item.types.includes('text/plain'))
+
+        return textItem
+          ? textItem.getType('text/plain').then((blob: Blob) => blob.text())
+          : Promise.resolve('')
+      })
+    })
+  },
+
+  getClipboardItemByMimeTypeAsText: async ({page}, use) => {
+    await use(async (mimeType: string) => {
+      return page.evaluate(async (mime) => {
+        const items = await navigator.clipboard.read()
+        const textItem = items.find((item) => item.types.includes(mime))
+        const content = textItem ? textItem.getType(mime).then((blob: Blob) => blob.text()) : null
+
+        return content
+      }, mimeType)
+    })
+  },
+})
+
+export const {expect} = test
--- packages/sanity/playwright-ct/tests/formBuilder/inputs/PortableText/copyPaste/CopyPasteStory.tsx
+++ packages/sanity/playwright-ct/tests/formBuilder/inputs/PortableText/copyPaste/CopyPasteStory.tsx
@@ -65,6 +65,95 @@ const SCHEMA_TYPES = [
           }),
         ],
       }),
+      defineField({
+        type: 'string',
+        name: 'title',
+        title: 'Title',
+      }),
+      defineField({
+        type: 'object',
+        name: 'objectWithColumns',
+        title: 'Object with columns',
+        options: {
+          columns: 4,
+        },
+        fields: [
+          {
+            type: 'string',
+            title: 'String 1',
+            description: 'this is a king kong description',
+            name: 'string1',
+          },
+          {
+            type: 'string',
+            title: 'String 2',
+            name: 'string2',
+          },
+          {
+            type: 'number',
+            title: 'Number 1',
+            name: 'number1',
+          },
+          {
+            type: 'number',
+            title: 'Number 2',
+            name: 'number2',
+          },
+          {
+            type: 'image',
+            title: 'Image 1',
+            name: 'image1',
+          },
+          {
+            name: 'file',
+            type: 'file',
+            title: 'File',
+          },
+        ],
+      }),
+      defineField({
+        name: 'arrayOfPrimitives',
+        type: 'array',
+        of: [
+          {
+            type: 'string',
+            title: 'A string',
+          },
+          {
+            type: 'number',
+            title: 'A number',
+          },
+          {
+            type: 'boolean',
+            title: 'A boolean',
+          },
+        ],
+      }),
+      defineField({
+        name: 'arrayOfMultipleTypes',
+        title: 'Array of multiple types',
+        type: 'array',
+        of: [
+          {
+            type: 'image',
+          },
+          {
+            type: 'object',
+            name: 'color',
+            title: 'Color with a long title',
+            fields: [
+              {
+                name: 'title',
+                type: 'string',
+              },
+              {
+                name: 'name',
+                type: 'string',
+              },
+            ],
+          },
+        ],
+      }),
     ],
   }),
 ]
