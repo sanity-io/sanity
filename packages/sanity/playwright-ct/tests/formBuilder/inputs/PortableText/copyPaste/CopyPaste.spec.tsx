5f8a6bd8323816cc6316af39854cfa4b7a2f4006
Fred Carlsen <fred@sjelfull.no>
2024-06-20 17:12:00 +0200

    test(form): add component tests for copy paste
    Signed-off-by: Fred Carlsen <fred@sjelfull.no>

--- packages/sanity/playwright-ct/tests/formBuilder/inputs/PortableText/copyPaste/CopyPaste.spec.tsx
+++ packages/sanity/playwright-ct/tests/formBuilder/inputs/PortableText/copyPaste/CopyPaste.spec.tsx
@@ -1,9 +1,10 @@
 /* eslint-disable max-nested-callbacks */
 import path from 'node:path'
 
-import {expect, test} from '@playwright/experimental-ct-react'
+// import {expect, test} from '@playwright/experimental-ct-react'
 import {type Path, type SanityDocument} from '@sanity/types'
 
+import {expect, test} from '../../../../fixtures'
 import {testHelpers} from '../../../../utils/testHelpers'
 import CopyPasteStory from './CopyPasteStory'
 import {
@@ -23,6 +24,26 @@ const document: SanityDocument = {
   _updatedAt: new Date().toISOString(),
   _rev: '123',
   body: [],
+  arrayOfPrimitives: ['One', 'Two', true],
+  arrayOfMultipleTypes: [
+    {
+      _key: '6724abb6eee4',
+      _type: 'color',
+      title: 'Alright, testing this. Testing this as well tresting to typing here ee e',
+    },
+    {
+      _key: 'b361e6162fcc',
+      _type: 'book',
+      reviewsInline: [
+        {
+          _key: '0f1e4ae3c464',
+          _type: 'review',
+          title: 'Okay',
+        },
+      ],
+      title: 'Foo! bar hello iam typing testing to type i am testing to type in this dialog',
+    },
+  ],
 }
 
 test.describe('Portable Text Input', () => {
@@ -152,5 +173,270 @@ test.describe('Portable Text Input', () => {
 
       await expect(page.getByText(`upload this file here`)).toBeVisible()
     })
+  })
+})
+
+test.describe('Object input', () => {
+  test(`Copy and paste via field actions`, async ({
+    browserName,
+    getClipboardItemsAsText,
+    mount,
+    page,
+  }) => {
+    await mount(<CopyPasteStory document={document} />)
+
+    await expect(page.getByTestId(`field-objectWithColumns`)).toBeVisible()
+
+    const $object = page.getByTestId('field-objectWithColumns').locator(`[tabindex="0"]`).first()
+
+    await expect($object).toBeVisible()
+
+    await page
+      .getByTestId('field-objectWithColumns.string1')
+      .locator('input')
+      .fill('A string to copy')
+
+    await page
+      .getByTestId('field-objectWithColumns.string2')
+      .locator('input')
+      .fill('This is the second field')
+
+    // https://github.com/microsoft/playwright/pull/30572
+    // maybe part of 1.44
+    // await page.keyboard.press('ControlOrMeta+C')
+    let $fieldActions = page
+      .getByTestId('field-actions-menu-objectWithColumns')
+      .getByTestId('field-actions-trigger')
+
+    await $fieldActions.focus()
+    await expect($fieldActions).toBeFocused()
+    await $fieldActions.click()
+
+    await expect(page.getByRole('menuitem', {name: 'Copy field'})).toBeVisible()
+    await page.getByRole('menuitem', {name: 'Copy field'}).click()
+
+    if (browserName === 'firefox') {
+      await expect(page.getByText(`Your browser doesn't support this action (yet)`)).toBeVisible()
+
+      return
+    }
+
+    await expect(page.getByText(`Field Object with columns copied`)).toBeVisible()
+
+    // Check that the plain text version is set
+    await expect(await getClipboardItemsAsText()).toContain('A string to copy')
+    await expect(await getClipboardItemsAsText()).toContain('This is the second field')
+
+    await page.getByTestId('field-objectWithColumns.string1').locator('input').focus()
+    await page.keyboard.press('Meta+A')
+    await page.keyboard.press('Delete')
+
+    $fieldActions = page
+      .getByTestId('field-actions-menu-objectWithColumns')
+      .getByTestId('field-actions-trigger')
+
+    await $fieldActions.focus()
+
+    await expect($fieldActions).toBeVisible()
+
+    await $fieldActions.click()
+
+    await expect(page.getByRole('menuitem', {name: 'Paste field'})).toBeVisible()
+    await page.getByRole('menuitem', {name: 'Paste field'}).click()
+
+    await expect(page.getByText(`Field Object with columns updated`)).toBeVisible()
+
+    await expect(page.getByTestId('field-objectWithColumns.string1').locator('input')).toHaveValue(
+      'A string to copy',
+    )
+  })
+
+  test(`Copy via keyboard shortcut`, async ({
+    browserName,
+    getClipboardItemsAsText,
+    mount,
+    page,
+  }) => {
+    await mount(<CopyPasteStory document={document} />)
+
+    await expect(page.getByTestId(`field-objectWithColumns`)).toBeVisible()
+
+    const $object = page.getByTestId('field-objectWithColumns').locator(`[tabindex="0"]`).first()
+
+    await expect($object).toBeVisible()
+
+    await page
+      .getByTestId('field-objectWithColumns.string1')
+      .locator('input')
+      .fill('A string to copy')
+
+    await page
+      .getByTestId('field-objectWithColumns.string2')
+      .locator('input')
+      .fill('This is the second field')
+
+    // https://github.com/microsoft/playwright/pull/30572
+    // maybe part of 1.44
+    // await page.keyboard.press('ControlOrMeta+C')
+    await $object.focus()
+    await expect($object).toBeFocused()
+    await $object.press('ControlOrMeta+C')
+
+    if (browserName === 'firefox') {
+      await expect(page.getByText(`Your browser doesn't support this action (yet)`)).toBeVisible()
+
+      return
+    }
+
+    await expect(page.getByText(`Field Object with columns copied`)).toBeVisible()
+
+    // Check that the plain text version is set
+    await expect(await getClipboardItemsAsText()).toContain('A string to copy')
+    await expect(await getClipboardItemsAsText()).toContain('This is the second field')
+
+    await $object.focus()
+    await expect($object).toBeFocused()
+    await $object.press('ControlOrMeta+V')
+
+    await expect(page.getByText(`Field Object with columns updated`)).toBeVisible()
+
+    await expect(page.getByTestId('field-objectWithColumns.string1').locator('input')).toHaveValue(
+      'A string to copy',
+    )
+  })
+})
+
+test.describe('String input', () => {
+  test(`Copy and pasting via field actions`, async ({
+    browserName,
+    getClipboardItemsAsText,
+    mount,
+    page,
+  }) => {
+    await mount(<CopyPasteStory document={document} />)
+
+    await expect(page.getByTestId(`field-title`)).toBeVisible()
+
+    await page.getByTestId('field-title').locator('input').fill('A string to copy')
+
+    // https://github.com/microsoft/playwright/pull/30572
+    // maybe part of 1.44
+    // await page.keyboard.press('ControlOrMeta+C')
+    let $fieldActions = page
+      .getByTestId('field-actions-menu-title')
+      .getByTestId('field-actions-trigger')
+
+    await $fieldActions.focus()
+    await expect($fieldActions).toBeFocused()
+    await $fieldActions.click()
+
+    await expect(page.getByRole('menuitem', {name: 'Copy field'})).toBeVisible()
+    await page.getByRole('menuitem', {name: 'Copy field'}).click()
+
+    if (browserName === 'firefox') {
+      await expect(page.getByText(`Your browser doesn't support this action (yet)`)).toBeVisible()
+
+      return
+    }
+
+    await expect(page.getByText(`Field Title copied`)).toBeVisible()
+
+    // Check that the plain text version is set
+    await expect(await getClipboardItemsAsText()).toContain('A string to copy')
+
+    await page.getByTestId('field-title').locator('input').fill('')
+
+    // Trigger the field actions menu
+    $fieldActions = page
+      .getByTestId('field-actions-menu-title')
+      .getByTestId('field-actions-trigger')
+
+    await $fieldActions.focus()
+    await expect($fieldActions).toBeFocused()
+    await $fieldActions.click()
+
+    // Click on the "Paste field" option in the menu
+    await expect(page.getByRole('menuitem', {name: 'Paste field'})).toBeVisible()
+    await page.getByRole('menuitem', {name: 'Paste field'}).click()
+
+    // Verify that the field content is updated with the pasted value
+    await expect(page.getByText(`Field Title updated`)).toBeVisible()
+    await expect(page.getByTestId('field-title').locator('input')).toHaveValue('A string to copy')
+  })
+})
+
+test.describe('Array input', () => {
+  test(`Copy and pasting via field actions`, async ({
+    browserName,
+    getClipboardItemsAsText,
+    mount,
+    page,
+  }) => {
+    await mount(<CopyPasteStory document={document} />)
+
+    await expect(page.getByTestId(`field-arrayOfPrimitives`)).toBeVisible()
+
+    // https://github.com/microsoft/playwright/pull/30572
+    // maybe part of 1.44
+    // await page.keyboard.press('ControlOrMeta+C')
+    const $fieldActions = page
+      .getByTestId('field-actions-menu-arrayOfPrimitives')
+      .getByTestId('field-actions-trigger')
+
+    await $fieldActions.focus()
+    await expect($fieldActions).toBeFocused()
+    await $fieldActions.click()
+
+    await expect(page.getByRole('menuitem', {name: 'Copy field'})).toBeVisible()
+    await page.getByRole('menuitem', {name: 'Copy field'}).click()
+
+    if (browserName === 'firefox') {
+      await expect(page.getByText(`Your browser doesn't support this action (yet)`)).toBeVisible()
+
+      return
+    }
+
+    await expect(page.getByText(`Field Array of primitives copied`)).toBeVisible()
+
+    // Check that the plain text version is set
+    await expect(await getClipboardItemsAsText()).toContain('One, Two')
+
+    const $rowActionTrigger = page.locator('[id="arrayOfPrimitives[0]-menuButton"]')
+
+    await $rowActionTrigger.focus()
+    await expect($rowActionTrigger).toBeFocused()
+    await $rowActionTrigger.click()
+
+    const $removeButton = page.getByRole('menuitem', {name: 'Remove'}).first()
+
+    await expect($removeButton).toBeVisible()
+
+    await $removeButton.click()
+
+    expect(
+      page.getByTestId(`field-arrayOfPrimitives`).getByTestId('string-input').first(),
+    ).not.toHaveValue('One')
+    expect(
+      page.getByTestId(`field-arrayOfPrimitives`).getByTestId('string-input').first(),
+    ).toHaveValue('Two')
+
+    await $fieldActions.focus()
+    await expect($fieldActions).toBeFocused()
+    await $fieldActions.click()
+
+    await expect(page.getByRole('menuitem', {name: 'Paste field'})).toBeVisible()
+    await page.getByRole('menuitem', {name: 'Paste field'}).click()
+
+    await expect(page.getByText(`Field Array of primitives updated`)).toBeVisible()
+
+    expect(
+      page.getByTestId(`field-arrayOfPrimitives`).getByTestId('string-input').first(),
+    ).toHaveValue('One')
+
+    // $fieldActions = page
+    //   .getByTestId('field-actions-menu-arrayOfPrimitives')
+    //   .getByTestId('field-actions-trigger')
+
+    // arrayOfPrimitives[0]-menuButton
   })
 })
