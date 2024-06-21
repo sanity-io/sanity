b4cac451e9714012390ba1e85af3f3334faf3ac8
Fred Carlsen <fred@sjelfull.no>
2024-06-21 14:19:00 +0200

    test(form): stabilise copy paste e2e tests
    Signed-off-by: Fred Carlsen <fred@sjelfull.no>

--- packages/sanity/playwright-ct/tests/fixtures/index.ts
+++ packages/sanity/playwright-ct/tests/fixtures/index.ts
@@ -2,7 +2,8 @@ import {test as base} from '@playwright/experimental-ct-react'
 import {mergeTests} from '@playwright/test'
 
 import {test as copyPasteFixture} from './copyPasteFixture'
+import {test as scrollToTopFixture} from './scrollToTopFixture'
 
-export const test = mergeTests(base, copyPasteFixture)
+export const test = mergeTests(base, copyPasteFixture, scrollToTopFixture)
 
 export {expect} from '@playwright/test'
