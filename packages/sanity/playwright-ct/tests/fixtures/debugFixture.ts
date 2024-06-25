f7b157de149b9b4827be73beaf33eec37f2f8abf
Fred Carlsen <fred@sjelfull.no>
2024-06-25 19:28:07 +0200

    feat(playwright-ct): add debug fixture
    Signed-off-by: Fred Carlsen <fred@sjelfull.no>

---
+++ packages/sanity/playwright-ct/tests/fixtures/debugFixture.ts
@@ -0,0 +1,31 @@
+import {test as base} from '@playwright/experimental-ct-react'
+
+export const test = base.extend<{
+  logActiveElement: () => Promise<{
+    tagName: string
+    id: string
+    className: string
+    name: string
+    attributes: Record<string, string>
+  }>
+}>({
+  logActiveElement: async ({page}, use) => {
+    await use(async () => {
+      const activeElementInfo = await page.evaluate(() => {
+        const active = document.activeElement as HTMLElement
+        return {
+          tagName: active.tagName,
+          id: active.id,
+          className: active.className,
+          name: active.nodeName,
+          attributes: Object.fromEntries(
+            Array.from(active.attributes).map((attr) => [attr.name, attr.value]),
+          ),
+        }
+      })
+      return Promise.resolve(activeElementInfo)
+    })
+  },
+})
+
+export {expect} from '@playwright/test'
