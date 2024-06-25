f04e529d0399e9d94289bf88ba6265b90a504787
Fred Carlsen <fred@sjelfull.no>
2024-06-25 19:28:07 +0200

    feat(playwright-ct): add debug fixture
    Signed-off-by: Fred Carlsen <fred@sjelfull.no>

--- packages/sanity/playwright-ct/tests/fixtures/index.ts
+++ packages/sanity/playwright-ct/tests/fixtures/index.ts
@@ -2,8 +2,9 @@ import {test as base} from '@playwright/experimental-ct-react'
 import {mergeTests} from '@playwright/test'
 
 import {test as copyPasteFixture} from './copyPasteFixture'
+import {test as debugFixture} from './debugFixture'
 import {test as scrollToTopFixture} from './scrollToTopFixture'
 
-export const test = mergeTests(base, copyPasteFixture, scrollToTopFixture)
+export const test = mergeTests(base, copyPasteFixture, scrollToTopFixture, debugFixture)
 
 export {expect} from '@playwright/test'
