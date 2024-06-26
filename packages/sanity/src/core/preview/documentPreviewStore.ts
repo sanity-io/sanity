6f6b170f75de1346a6cc925f544ee43fde3d2dec
Ash <ash@sanity.io>
2024-06-26 14:34:46 +0100

    feat(sanity): add `_version` field to preview observation

--- packages/sanity/src/core/preview/documentPreviewStore.ts
+++ packages/sanity/src/core/preview/documentPreviewStore.ts
@@ -132,7 +132,7 @@ export function createDocumentPreviewStore({
     id: string,
     apiConfig?: ApiConfig,
   ): Observable<string | undefined> {
-    return observePaths({_type: 'reference', _ref: id}, ['_type'], apiConfig).pipe(
+    return observePaths({_type: 'reference', _ref: id}, ['_type', '_version'], apiConfig).pipe(
       map((res) => (isRecord(res) && typeof res._type === 'string' ? res._type : undefined)),
       distinctUntilChanged(),
     )
