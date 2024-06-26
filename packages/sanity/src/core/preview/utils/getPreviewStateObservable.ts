b6e62b77f35004749219c041ebc5985a4cadff6d
Ash <ash@sanity.io>
2024-06-26 14:52:07 +0100

    feat(sanity): reflect checked out bundle in document lists and list previews

--- packages/sanity/src/core/preview/utils/getPreviewStateObservable.ts
+++ packages/sanity/src/core/preview/utils/getPreviewStateObservable.ts
@@ -10,6 +10,7 @@ export interface PreviewState {
   isLoading?: boolean
   draft?: PreviewValue | Partial<SanityDocument> | null
   published?: PreviewValue | Partial<SanityDocument> | null
+  version?: PreviewValue | Partial<SanityDocument> | null
 }
 
 const isLiveEditEnabled = (schemaType: SchemaType) => schemaType.liveEdit === true
@@ -24,22 +25,32 @@ export function getPreviewStateObservable(
   schemaType: SchemaType,
   documentId: string,
   title: ReactNode,
+  bundlePerspective?: string,
 ): Observable<PreviewState> {
   const draft$ = isLiveEditEnabled(schemaType)
     ? of({snapshot: null})
     : documentPreviewStore.observeForPreview({_id: getDraftId(documentId)}, schemaType)
 
+  // TODO: Create `getVersionId` abstraction
+  const version$ = bundlePerspective
+    ? documentPreviewStore.observeForPreview(
+        {_type: 'reference', _ref: [bundlePerspective, getPublishedId(documentId, true)].join('.')},
+        schemaType,
+      )
+    : of({snapshot: null})
+
   const published$ = documentPreviewStore.observeForPreview(
-    {_id: getPublishedId(documentId)},
+    {_id: getPublishedId(documentId, documentId.startsWith(`${bundlePerspective}.`))},
     schemaType,
   )
 
-  return combineLatest([draft$, published$]).pipe(
-    map(([draft, published]) => ({
+  return combineLatest([draft$, published$, version$]).pipe(
+    map(([draft, published, version]) => ({
       draft: draft.snapshot ? {title, ...(draft.snapshot || {})} : null,
       isLoading: false,
       published: published.snapshot ? {title, ...(published.snapshot || {})} : null,
+      version: version.snapshot ? {title, ...(version.snapshot || {})} : null,
     })),
-    startWith({draft: null, isLoading: true, published: null}),
+    startWith({draft: null, isLoading: true, published: null, version: null}),
   )
 }
