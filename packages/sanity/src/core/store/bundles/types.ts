c85934b4f6205a9ba755658a052661741f03a58c
Pedro Bonamin <pedrobonamin@gmail.com>
2024-06-28 14:09:42 +0200

    feat(corel): include useBundleOperations and story

--- packages/sanity/src/core/store/bundles/types.ts
+++ packages/sanity/src/core/store/bundles/types.ts
@@ -1,10 +1,12 @@
 import {type SanityDocument} from '@sanity/types'
+import {type ButtonTone} from '@sanity/ui'
 
 export interface BundleDocument extends SanityDocument {
   _type: 'bundle'
   title: string
+  name: string
   description?: string
-  color?: string
+  tone?: ButtonTone
   icon?: string
   authorId: string
 }
