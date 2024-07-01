4d5ccc64ab151b244f0e9ebeeed352664c506c73
RitaDias <rita@sanity.io>
2024-07-01 13:08:57 +0200

    feat(corel): doc and structure updates (#7039)
    * docs(sanity): update comment on getAllVersionsOfDocument
    
    * fix(sanity): listing existing versions didn't account for published
    
    * chore(sanity): move latest version up (temp)
    
    * feat(sanity): update navigation of perspective + add "current badge"
    
    * feat(sanity): add global version navigation
    
    * refactor(sanity): hide document version picker, update the document icon
    
    * feat(sanity): update studio nav bar (reduce create button + spacing)
    
    * chore(sanity): general cleanup
    
    * feat(sanity): add version provider, hide only the version picker in document
    
    * chore(sanity): clean up version context
    
    * chore(sanity): clean up, move components & files to single folder
    
    * chore(sanity): clean up, move components & files to single folder
    
    * chore(sanity): add ready button to document footer
    
    * refactor(sanity): remove isVersion and add frontend guardrails
    
    * chore(sanity): clean duplicate component
    
    * chore(sanity): clean up missing dependencies
    
    * refactor(sanity): update button
    
    * refactor(sanity): clear up naming
    
    * chore(sanity): hide ready
    
    * chore(sanity): ready todos
    
    * feat(sanity): add bundle modal + update colours + remove add new version from doc
    
    * refactor(sanity): rename from release to bundle
    
    * refactor(sanity): clean up code for types (bundleform)
    
    * refactor(sanity): update method
    
    * refactor(sanity): update drafts name (draft -> drafts) in LATEST & context
    
    * refactor(sanity): use handleBundleChange instead of two separate methods
    
    * refactor(sanity): update title to use LATEST const
    
    * refactor(sanity): update draft filtering and checks (global and document level)
    
    * refactor(sanity): update context to rely on router & update query to match existing bundles (MOCK)
    
    * refactor(sanity): update handle methods to have callback
    
    * feat(sanity): add guardrails for title naming on bundle create
    
    * docs(sanity): update comments to potentially move things to plugin
    
    * refactor(sanity): merge version badges & icons
    
    * refactor(sanity): use speakingurl instead of toSlug

---
+++ packages/sanity/src/core/versions/util/dummyGetters.ts
@@ -0,0 +1,51 @@
+import {type SanityClient, type SanityDocument} from 'sanity'
+import speakingurl from 'speakingurl'
+
+import {type Version} from '../types'
+import {BUNDLES} from './const'
+
+/* MOSTLY TEMPORARY FUNCTIONS / DUMMY DATA */
+
+/**
+ * Returns all versions of a document
+ *
+ * @param documentId - document id
+ * @param client - sanity client
+ * @returns array of SanityDocuments versions from a specific doc
+ */
+export async function getAllVersionsOfDocument(
+  client: SanityClient,
+  documentId: string,
+): Promise<Version[]> {
+  // remove all versions, get just id (anything anything after first .)
+  const id = documentId.replace(/^[^.]*\./, '')
+
+  const query = `*[_id match "*${id}*"]`
+
+  return await client.fetch(query, {}, {tag: 'document.list-versions'}).then((documents) => {
+    return documents.map((doc: SanityDocument) => {
+      const sluggedName = getVersionName(doc._id)
+      const bundle = BUNDLES.find((b) => b.name === sluggedName)
+      return {
+        name: speakingurl(sluggedName),
+        title: bundle?.title || sluggedName,
+        tone: bundle?.tone || 'default',
+        icon: bundle?.icon || 'cube',
+      }
+    })
+  })
+}
+
+export function getVersionName(documentId: string): string {
+  if (documentId.indexOf('.') === -1) return 'Published'
+  const version = documentId.slice(0, documentId.indexOf('.'))
+  return version
+}
+
+export function versionDocumentExists(documentVersions: Version[], name: string): boolean {
+  return documentVersions.some((version) => version.name === name)
+}
+
+export function isDraftOrPublished(versionName: string): boolean {
+  return speakingurl(versionName) === 'drafts' || speakingurl(versionName) === 'published'
+}
