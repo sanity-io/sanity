bb081583c15e6e9b877a83f2d57d34cbb7761ade
Jordan Lawrence <jordanl17@me.com>
2024-07-02 13:07:07 +0100

    feat(releases): initialising releases plugin (#7048)
    * docs(sanity): update comment on getAllVersionsOfDocument
    
    * fix(sanity): listing existing versions didn't account for published
    
    * chore(sanity): move latest version up (temp)
    
    * feat(sanity): update navigation of perspective + add "current badge"
    
    * feat(sanity): add global version navigation
    
    * refactor(sanity): hide document version picker, update the document icon
    
    * feat(sanity): update studio nav bar (reduce create button + spacing)
    
    * chore(sanity): general cleanup
    
    * feat(sanity): add version provider, hide only the version picker in document
    
    * feat(releases): create releases tool as default plugin
    
    * feat(releases): styling and components for basic releases overview
    
    * feat(releases): adding release detail page and routing
    
    * feat(releases): uri decoding releaseID
    
    * chore(sanity): clean up version context
    
    * chore(sanity): clean up, move components & files to single folder
    
    * chore(sanity): clean up, move components & files to single folder
    
    * feat(corel): add bundles store
    
    * chore(sanity): add ready button to document footer
    
    * refactor(sanity): remove isVersion and add frontend guardrails
    
    * chore(sanity): clean duplicate component
    
    * chore(sanity): clean up missing dependencies
    
    * refactor(sanity): update button
    
    * chore(corel): use rxjs in bundlesStore listener
    
    * refactor(sanity): clear up naming
    
    * chore(sanity): hide ready
    
    * chore(sanity): ready todos
    
    * feat(corel): add retry to initialFetch
    
    * feat(releases): remove the ready concepts
    
    * feat(corel): include useBundleOperations and story
    
    * fix(corel): update imports paths
    
    * feat(sanity): add bundle modal + update colours + remove add new version from doc
    
    * refactor(sanity): rename from release to bundle
    
    * refactor(sanity): clean up code for types (bundleform)
    
    * refactor(releases): refactor releases to bundles
    
    * feat(releases): using bundle ops to fetch and create add-on bundles
    
    * refactor(releases): type guard for at least one bundle returned
    
    * fix(releases): fixing issue where overview header not hidden when no bundles
    
    * feat(releases): search filters list of releases
    
    * refactor(releases): minor refactor of hooks in BundlesOverview
    
    * refactor(sanity): update method
    
    * feat(releases): addijg concept loosely of bundle documents
    
    * feat(releases): testing for BundlesTable
    
    * feat(releases): testing for BundlesTable
    
    * feat(releases): testing for releases overview page
    
    * refactor(release): fixing types on the custom queries
    
    * refactor(releases): Creating BundleIcon
    
    * refactor(releases): minor refactor and tidy to component logic
    
    * refactor(releases): minor format changes
    
    * refactor(releases): create bundle dialog creates itself and then returns
    
    * refactor(releases): create bundle dialog creates itself and then returns
    
    * feat(releases): minor style update to match prototype on the mode picker
    
    * feat(releases): minor style update to match prototype on the mode picker
    
    * refactor(releases): moving search into table header as per prototype
    
    * chore(corel): add eslintrc change, remove progress icon and update types (#7050)
    
    ---------
    
    Co-authored-by: RitaDias <anarita151@gmail.com>
    Co-authored-by: Pedro Bonamin <pedrobonamin@gmail.com>
    Co-authored-by: Pedro Bonamin <46196328+pedrobonamin@users.noreply.github.com>

---
+++ packages/sanity/src/core/releases/plugin/index.ts
@@ -0,0 +1,25 @@
+import {route} from 'sanity/router'
+
+import {definePlugin} from '../../config'
+import {ReleasesTool} from '../tool/ReleasesTool'
+
+/**
+ * @internal
+ */
+export const RELEASES_NAME = 'sanity/releases'
+
+/**
+ * @internal
+ */
+export const releases = definePlugin({
+  name: RELEASES_NAME,
+
+  tools: [
+    {
+      name: 'releases',
+      title: 'Releases',
+      component: ReleasesTool,
+      router: route.create('/', [route.create('/:bundleId')]),
+    },
+  ],
+})
