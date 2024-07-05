96cb3f1581e03e531a898296d6fe6ca55dbed95d
RitaDias <rita@sanity.io>
2024-07-05 12:49:17 +0200

    refactor(COREL): refactor version context & renaming (#7071)
    * feat(sanity): update types, add icon and hue picker to bundles, update uis
    
    * feat(sanity): add date picker to bundleform
    
    * refactor(sanity): update publishAt fields in menus
    
    * chore(sanity): update LATEST type to Partial
    
    * refactor(sanity): make single bundle menu + clean up
    
    * chore(sanity): remove warnings comments
    
    * refactor(sanity): move version provider to its own file + organise + add useVersion
    
    * refactor(sanity): remove provider + context, move logic to hook
    
    * chore(sanity): remove comments + change name from setCurrentVersion to setGlobalBundle
    
    * chore(sanity): rename useVersion to useBundle
    
    * chore(sanity): update comments
    
    * chore(sanity): Rename currentVersion
    
    * refactor(sanity): rename currentBudnle and setGlobalBundle
    
    * chore(sanity): rename versions to bundles in core directory
    
    * chore(sanity): rename to GlobalPerspectiveMenu and move to navbar directory
    
    * chore(sanity): rename to DocumentPerspectiveMenu and move to navbar directory
    
    * chore(sanity): remove isDraft from usePerspective, add new util
    
    * refactor(sanity): remove isDraft
    
    * refactor(sanity): use current global in the bundle badge

--- packages/sanity/src/structure/panes/document/documentPanel/header/perspective/DocumentPerspectiveMenu.tsx
+++ packages/sanity/src/structure/panes/document/documentPanel/header/perspective/DocumentPerspectiveMenu.tsx
@@ -1,29 +1,23 @@
-/* eslint-disable no-warning-comments */
 import {ChevronDownIcon} from '@sanity/icons'
 import {Box, Button} from '@sanity/ui'
-import {useCallback, useContext, useEffect, useState} from 'react'
+import {useCallback, useEffect, useState} from 'react'
 import {DEFAULT_STUDIO_CLIENT_OPTIONS, useClient} from 'sanity'
 
-import {
-  VersionContext,
-  type VersionContextValue,
-} from '../../../../_singletons/core/form/VersionContext'
-import {useBundles} from '../../../store/bundles/BundlesProvider'
-import {type BundleDocument} from '../../../store/bundles/types'
-import {getAllVersionsOfDocument} from '../../util/dummyGetters'
-import {BundleBadge} from '../BundleBadge'
-import {BundleMenu} from '../BundleMenu'
+import {BundleBadge} from '../../../../../../core/bundles/components/BundleBadge'
+import {BundleMenu} from '../../../../../../core/bundles/components/BundleMenu'
+import {usePerspective} from '../../../../../../core/bundles/hooks/usePerspective'
+import {LATEST} from '../../../../../../core/bundles/util/const'
+import {getAllVersionsOfDocument} from '../../../../../../core/bundles/util/dummyGetters'
+import {useBundles} from '../../../../../../core/store/bundles/BundlesProvider'
+import {type BundleDocument} from '../../../../../../core/store/bundles/types'
 
-// TODO A LOT OF DOCUMENTED CODE IS RELATED TO SEARCH AND CREATING BUNDLE FROM HERE
-// STILL NEED TO DECIDE IF WE KEEP IT OR NOT
-export function DocumentVersionMenu(props: {documentId: string}): JSX.Element {
+export function DocumentPerspectiveMenu(props: {documentId: string}): JSX.Element {
   const {documentId} = props
   const client = useClient(DEFAULT_STUDIO_CLIENT_OPTIONS)
 
-  // TODO MAKE SURE THIS IS HOW WE WANT TO DO THIS
-  const {currentVersion, isDraft} = useContext<VersionContextValue>(VersionContext)
+  const {currentGlobalBundle} = usePerspective()
 
-  const {title, hue, icon} = currentVersion
+  const {title, hue, icon} = currentGlobalBundle
 
   const {data: bundles} = useBundles()
 
@@ -44,16 +38,14 @@ export function DocumentVersionMenu(props: {documentId: string}): JSX.Element {
     fetchVersionsInner()
   }, [fetchVersions])
 
-  /* TODO Version Badge should only show when the current opened document is in a version */
-
   return (
     <>
-      {currentVersion && !isDraft && (
+      {/* FIXME Version Badge should only show when the current opened document is in a version, RIGHT
+      NOW IT'S USING THE GLOBAL */}
+      {currentGlobalBundle && currentGlobalBundle.name === LATEST.name && (
         <BundleBadge hue={hue} title={title} icon={icon} padding={2} />
       )}
-
-      {/**
-       * TODO IS THIS STILL NEEDED? VS THE PICKER IN STUDIO NAVBAR? */}
+      {/** TODO IS THIS STILL NEEDED? VS THE PICKER IN STUDIO NAVBAR? */}
       <Box flex="none">
         <BundleMenu
           button={<Button icon={ChevronDownIcon} mode="bleed" padding={2} space={2} />}
