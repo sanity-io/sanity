801e61bca6a950a9046735e73be688a8e5846b3b
RitaDias <rita@sanity.io>
2024-07-05 10:19:26 +0200

    feat(COREL): update types to bundle + add create bundle functionality (#7047)
    * refactor(sanity): update types to bundle
    
    * feat(sanity): add ability to create releases from dialog + update types
    
    * feat(sanity): update list on global and document to use store
    
    * feat(sanity): update types, add icon and hue picker to bundles, update uis
    
    * feat(sanity): add date picker to bundleform
    
    * refactor(sanity): update publishAt fields in menus
    
    * refactor(sanity): update type in dummyGetter
    
    * chore(sanity): remove BUNDLES const
    
    * chore(sanity): update LATEST type to Partial
    
    * chore(sanity): update validation for bundle date creation
    
    * chore(sanity): clean up code
    
    * refactor(sanity): add archived filter
    
    * refactor(sanity): make single bundle menu + clean up
    
    * chore(sanity): add missing properties
    
    * feat(sanity): add loading & remove unused code
    
    * feat(sanity): add loading to document version, update filter
    
    * chore(sanity): rename VersionBadge to BundleBadge
    
    * chore(sanity): clean up methods and style
    
    * chore(sanity): remove unused import
    
    * refactor(sanity): use Bundle provider instead of store
    
    * chore(sanity): update bundleRow to use the new name for badge
    
    * chore(sanity): clean up code
    
    * refactor(sanity): re-add oncancel and oncreate props in dialog
    
    * refactor(sanity): add scroll to bundle menu
    
    * refactor(sanity): move from archived to archivedAt
    
    * refactor(sanity): only show the list of bundles if it has more than published / drafts
    
    * chore(sanity): remove warnings comments

--- packages/sanity/src/core/versions/components/panes/DocumentVersionMenu.tsx
+++ packages/sanity/src/core/versions/components/panes/DocumentVersionMenu.tsx
@@ -1,64 +1,39 @@
 /* eslint-disable no-warning-comments */
-/* eslint-disable i18next/no-literal-string */
-
-import {CheckmarkIcon, ChevronDownIcon} from '@sanity/icons'
-import {
-  Box,
-  Button,
-  Flex,
-  Label,
-  Menu,
-  MenuButton,
-  MenuDivider,
-  MenuItem,
-  Stack,
-  Text,
-} from '@sanity/ui'
+import {ChevronDownIcon} from '@sanity/icons'
+import {Box, Button} from '@sanity/ui'
 import {useCallback, useContext, useEffect, useState} from 'react'
 import {DEFAULT_STUDIO_CLIENT_OPTIONS, useClient} from 'sanity'
-import {useRouter} from 'sanity/router'
 
 import {
   VersionContext,
   type VersionContextValue,
 } from '../../../../_singletons/core/form/VersionContext'
-import {type Version} from '../../types'
-import {BUNDLES, LATEST} from '../../util/const'
-import {getAllVersionsOfDocument, isDraftOrPublished} from '../../util/dummyGetters'
-import {VersionBadge} from '../VersionBadge'
+import {useBundles} from '../../../store/bundles/BundlesProvider'
+import {type BundleDocument} from '../../../store/bundles/types'
+import {getAllVersionsOfDocument} from '../../util/dummyGetters'
+import {BundleBadge} from '../BundleBadge'
+import {BundleMenu} from '../BundleMenu'
 
 // TODO A LOT OF DOCUMENTED CODE IS RELATED TO SEARCH AND CREATING BUNDLE FROM HERE
 // STILL NEED TO DECIDE IF WE KEEP IT OR NOT
-export function DocumentVersionMenu(props: {
-  documentId: string
-  documentType: string
-}): JSX.Element {
-  const {documentId, documentType} = props
-  //const {newVersion} = useDocumentOperation(documentId, documentType)
+export function DocumentVersionMenu(props: {documentId: string}): JSX.Element {
+  const {documentId} = props
   const client = useClient(DEFAULT_STUDIO_CLIENT_OPTIONS)
-  const router = useRouter()
+
+  // TODO MAKE SURE THIS IS HOW WE WANT TO DO THIS
   const {currentVersion, isDraft} = useContext<VersionContextValue>(VersionContext)
-  const {name} = currentVersion
-  // const toast = useToast()
 
-  const [documentVersions, setDocumentVersions] = useState<Version[]>([])
+  const {title, hue, icon} = currentVersion
 
-  /*// search
-  const [addVersionTitle, setAddVersionTitle] = useState('')
-  const addVersionName = speakingurl(addVersionTitle)
+  const {data: bundles} = useBundles()
 
-  // use to prevent adding a version when you're already in that version
-  const addVersionExists = BUNDLES.some((r) => r.name.toLocaleLowerCase() === addVersionName)
-
-  // list of available bundles
-  const bundleOptionsList = BUNDLES.filter((r) =>
-    r.title.toLowerCase().includes(addVersionTitle.toLowerCase()),
-  )*/
+  // TODO MAKE SURE THIS IS HOW WE WANT TO DO THIS
+  const [documentVersions, setDocumentVersions] = useState<BundleDocument[]>([])
 
   const fetchVersions = useCallback(async () => {
-    const response = await getAllVersionsOfDocument(client, documentId)
+    const response = await getAllVersionsOfDocument(bundles, client, documentId)
     setDocumentVersions(response)
-  }, [client, documentId])
+  }, [bundles, client, documentId])
 
   // DUMMY FETCH -- NEEDS TO BE REPLACED -- USING GROQ from utils
   useEffect(() => {
@@ -69,198 +44,21 @@ export function DocumentVersionMenu(props: {
     fetchVersionsInner()
   }, [fetchVersions])
 
-  /* used for the search of bundles when writing a new version name */
-  /*const handleAddVersionChange = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
-    setAddVersionTitle(event.target.value)
-  }, [])
-
-  const handleAddVersion = useCallback(
-    (versionName: string) => () => {
-      const nameSlugged = speakingurl(versionName)
-
-      // only add to version if there isn't already a version in that bundle of this doc
-      if (versionDocumentExists(documentVersions, versionName)) {
-        toast.push({
-          status: 'error',
-          title: `There's already a version of this document in the bundle ${title}`,
-        })
-        return
-      }
-      const bundleId = `${nameSlugged}.${documentId}`
-
-      newVersion.execute(bundleId)
-    },
-    [documentVersions, documentId, newVersion, toast, title],
-  )*/
-
-  const handleChangeToVersion = useCallback(
-    (version: Version) => () => {
-      const {name: versionName} = version
-
-      if (isDraftOrPublished(versionName)) {
-        router.navigateStickyParam('perspective', '')
-      } else {
-        router.navigateStickyParam('perspective', `bundle.${versionName}`)
-      }
-    },
-    [router],
-  )
-
-  const onMenuOpen = useCallback(async () => {
-    //setAddVersionTitle('')
-    fetchVersions()
-  }, [fetchVersions])
-
   /* TODO Version Badge should only show when the current opened document is in a version */
 
   return (
     <>
       {currentVersion && !isDraft && (
-        <VersionBadge
-          tone={currentVersion.tone}
-          title={currentVersion.title}
-          icon={currentVersion.icon}
-          padding={2}
-        />
+        <BundleBadge hue={hue} title={title} icon={icon} padding={2} />
       )}
 
-      {/** TODO IS THIS STILL NEEDED? VS THE PICKER IN STUDIO NAVBAR? */}
-
+      {/**
+       * TODO IS THIS STILL NEEDED? VS THE PICKER IN STUDIO NAVBAR? */}
       <Box flex="none">
-        <MenuButton
+        <BundleMenu
           button={<Button icon={ChevronDownIcon} mode="bleed" padding={2} space={2} />}
-          id="version-menu"
-          onOpen={onMenuOpen}
-          menu={
-            <Menu padding={0} space={0}>
-              <Stack padding={1}>
-                {/* localize text */}
-                <MenuItem
-                  iconRight={isDraft ? CheckmarkIcon : <CheckmarkIcon style={{opacity: 0}} />}
-                  onClick={handleChangeToVersion(LATEST)}
-                  pressed={isDraft}
-                  text={LATEST.title}
-                />
-              </Stack>
-
-              <MenuDivider />
-
-              {BUNDLES.length > 0 && (
-                <>
-                  <Stack padding={1} space={1}>
-                    <Box padding={3} paddingBottom={2}>
-                      {/*                      
-                      localize text
-                      */}
-                      <Label muted size={1}>
-                        {/* localize text */}
-                        Versions
-                      </Label>
-                    </Box>
-
-                    {documentVersions
-                      .filter((b) => !isDraftOrPublished(b.name))
-                      .map((b) => (
-                        <MenuItem
-                          key={b.name}
-                          href=""
-                          onClick={handleChangeToVersion(b)}
-                          padding={1}
-                          pressed={name === b.name}
-                        >
-                          <Flex>
-                            {<VersionBadge tone={b.tone} icon={b.icon} padding={2} />}
-
-                            <Box flex={1} padding={2} style={{minWidth: 100}}>
-                              <Text size={1} weight="medium">
-                                {b.title}
-                              </Text>
-                            </Box>
-
-                            {
-                              <Box padding={2}>
-                                <Text muted size={1}>
-                                  {/* localize text */}
-                                  {b.publishAt
-                                    ? `a date will be here ${b.publishAt}`
-                                    : 'No target date'}
-                                </Text>
-                              </Box>
-                            }
-
-                            <Box padding={2}>
-                              <Text size={1}>
-                                {<CheckmarkIcon style={{opacity: b.name === name ? 1 : 0}} />}
-                              </Text>
-                            </Box>
-                          </Flex>
-                        </MenuItem>
-                      ))}
-                  </Stack>
-
-                  <MenuDivider />
-                </>
-              )}
-
-              {/*<Stack padding={1} space={1}>
-                <Box>
-                  <TextInput
-                    border={false}
-                    fontSize={1}
-                    onChange={handleAddVersionChange}
-                    placeholder="Create version"
-                    value={addVersionTitle}
-                  />
-                </Box>
-
-                {addVersionTitle && (
-                  <>
-                    {bundleOptionsList.map((r) => (
-                      <MenuItem
-                        disabled={r.name === addVersionName}
-                        key={r.name}
-                        onClick={handleAddVersion(r.name)}
-                        padding={1}
-                      >
-                        <Flex>
-                          <VersionBadge tone={r.tone} icon={r.icon} padding={2} />
-
-                          <Box flex={1} padding={2} style={{minWidth: 100}}>
-                            <Text size={1} weight="medium">
-                              {r.name === 'draft' ? LATEST.title : r.title}
-                            </Text>
-                          </Box>
-
-                          <Box padding={2}>
-                            <Text muted size={1}>
-                              {r.publishAt
-                                ? `a date will be here ${r.publishAt}`
-                                : 'No target date'}
-                            </Text>
-                          </Box>
-                        </Flex>
-                      </MenuItem>
-                    ))}
-
-                    // localize text
-                    <MenuItem
-                      disabled={addVersionExists}
-                      icon={AddIcon}
-                      onClick={handleAddVersion(addVersionName)}
-                      text={<>Create version: "{addVersionTitle}"</>}
-                    />
-                  </></Menu>
-                )}
-              </Stack>*/}
-            </Menu>
-          }
-          popover={{
-            constrainSize: true,
-            fallbackPlacements: [],
-            placement: 'bottom-start',
-            portal: true,
-            tone: 'default',
-          }}
+          bundles={documentVersions}
+          loading={!documentVersions}
         />
       </Box>
     </>
