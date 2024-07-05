d873e6ae67bb96e3388f1fa50727e6c40b173995
RitaDias <rita@sanity.io>
2024-07-05 12:49:17 +0200

    refactor(COREL): refactor version context & renaming (#7071)
    * feat(sanity): update types, add icon and hue picker to bundles, update uis
    
    * feat(sanity): add date picker to bundleform
    
    * refactor(sanity): update publishAt fields in menus
    
    * chore(sanity): update LATEST type to Partial
    
    * refactor(sanity): make single bundle menu + clean up
    
    * chore(sanity): remove warnings comments
    
    * refactor(sanity): move version provider to its own file + organise + add useVersion
    
    * refactor(sanity): remove provider + context, move logic to hook
    
    * chore(sanity): remove comments + change name from setCurrentVersion to setGlobalBundle
    
    * chore(sanity): rename useVersion to useBundle
    
    * chore(sanity): update comments
    
    * chore(sanity): Rename currentVersion
    
    * refactor(sanity): rename currentBudnle and setGlobalBundle
    
    * chore(sanity): rename versions to bundles in core directory
    
    * chore(sanity): rename to GlobalPerspectiveMenu and move to navbar directory
    
    * chore(sanity): rename to DocumentPerspectiveMenu and move to navbar directory
    
    * chore(sanity): remove isDraft from usePerspective, add new util
    
    * refactor(sanity): remove isDraft
    
    * refactor(sanity): use current global in the bundle badge

--- packages/sanity/src/structure/panes/document/statusBar/DocumentStatusBarActions.tsx
+++ packages/sanity/src/structure/panes/document/statusBar/DocumentStatusBarActions.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable no-warning-comments */
 /* eslint-disable camelcase */
-import {Flex, Hotkeys, LayerProvider, Stack, Text} from '@sanity/ui'
-import {memo, useContext, useMemo, useState} from 'react'
+import {Flex, LayerProvider, Stack, Text} from '@sanity/ui'
+import {memo, useMemo, useState} from 'react'
 import {
   type DocumentActionComponent,
   type DocumentActionDescription,
@@ -11,11 +11,9 @@ import {
   useTimelineSelector,
 } from 'sanity'
 
-import {
-  VersionContext,
-  type VersionContextValue,
-} from '../../../../_singletons/core/form/VersionContext'
-import {BundleActions} from '../../../../core/versions/components/panes/BundleActions'
+import {BundleActions} from '../../../../core/bundles/components/panes/BundleActions'
+import {usePerspective} from '../../../../core/bundles/hooks/usePerspective'
+import {LATEST} from '../../../../core/bundles/util/const'
 import {Button, Tooltip} from '../../../../ui-components'
 import {RenderActionCollectionState} from '../../../components'
 import {HistoryRestoreAction} from '../../../documentActions'
@@ -67,9 +65,8 @@ function DocumentStatusBarActionsInner(props: DocumentStatusBarActionsInnerProps
 
   /* Version / Bundling handling */
 
-  // eslint-disable-next-line no-warning-comments
   // TODO MAKE SURE THIS IS HOW WE WANT TO DO THIS
-  const {currentVersion, isDraft} = useContext<VersionContextValue>(VersionContext)
+  const {currentGlobalBundle} = usePerspective()
 
   return (
     <Flex align="center" gap={1}>
@@ -78,7 +75,7 @@ function DocumentStatusBarActionsInner(props: DocumentStatusBarActionsInnerProps
         <LayerProvider zOffset={200}>
           <Tooltip disabled={!tooltipContent} content={tooltipContent} placement="top">
             <Stack>
-              {isDraft ? (
+              {currentGlobalBundle.name === LATEST.name ? (
                 <Button
                   data-testid={`action-${firstActionState.label}`}
                   disabled={
@@ -94,7 +91,7 @@ function DocumentStatusBarActionsInner(props: DocumentStatusBarActionsInnerProps
               ) : (
                 /** TODO DO WE STILL NEED THIS OR CAN WE MOVE THIS TO THE PLUGIN? */
                 <BundleActions
-                  currentVersion={currentVersion}
+                  currentGlobalBundle={currentGlobalBundle}
                   documentId={documentId}
                   documentType={documentType}
                 />
@@ -107,7 +104,7 @@ function DocumentStatusBarActionsInner(props: DocumentStatusBarActionsInnerProps
        * TODO DO WE STILL NEED THIS OR CAN WE MOVE THIS TO THE PLUGIN?
        * SPECIFICALLY FOR ISDRAFT
        */}
-      {showMenu && menuActionStates.length > 0 && isDraft && (
+      {showMenu && menuActionStates.length > 0 && currentGlobalBundle.name === LATEST.name && (
         <ActionMenuButton actionStates={menuActionStates} disabled={disabled} />
       )}
       {firstActionState && firstActionState.dialog && (
