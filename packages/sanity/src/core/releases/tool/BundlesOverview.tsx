41bf20fedba61a74abba54518c7dbe5c82ad8f63
Ash <ash@sanity.io>
2024-07-07 21:10:42 +0100

    refactor(sanity): move bundle metadata store to resource cache
    Co-authored-by: Pedro Bonamin <pedrobonamin@gmail.com>

--- packages/sanity/src/core/releases/tool/BundlesOverview.tsx
+++ packages/sanity/src/core/releases/tool/BundlesOverview.tsx
@@ -6,7 +6,7 @@ import {type MouseEventHandler, useCallback, useEffect, useMemo, useState} from 
 import {Button as StudioButton} from '../../../ui-components'
 import {CreateBundleDialog} from '../../bundles/components/dialog/CreateBundleDialog'
 import {LoadingBlock} from '../../components/loadingBlock/LoadingBlock'
-import {useBundles} from '../../store/bundles/BundlesProvider'
+import {useBundles} from '../../store/bundles'
 import {type BundleDocument} from '../../store/bundles/types'
 import {BundlesTable} from '../components/BundlesTable/BundlesTable'
 import {containsBundles} from '../types/bundle'
