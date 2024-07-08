1c5c343bffbc0253d5cb970b52111b0d04bbdeee
Pedro Bonamin <46196328+pedrobonamin@users.noreply.github.com>
2024-07-08 08:59:52 +0200

    fix(corel): update uses of collate, add typeguard for bundles and add release tag to addonDatasetProvider (#7074)
    * fix(corel): update uses of collate, add typeguard for bundles and add release tag to addonDatasetProvider
    
    * fix(corel): update imports for bundleRow test
    
    * chore(corel): disable publish tests
    
    * fix(corel): update failing tests
    
    * fix(corel): update bundle actions definition
    
    * fix(corel): remove not used file
    
    * fix(corel): update DocumentHeaderTitle test
    
    * fix(corel): fix lint issues

--- packages/sanity/src/core/bundles/hooks/usePerspective.tsx
+++ packages/sanity/src/core/bundles/hooks/usePerspective.tsx
@@ -1,22 +1,25 @@
 import {useRouter} from 'sanity/router'
 
-import {useBundlesStore} from '../../store/bundles'
+import {useBundles} from '../../store/bundles/BundlesProvider'
 import {type BundleDocument} from '../../store/bundles/types'
 import {LATEST} from '../util/const'
 
 /**
  * @internal
  */
-export interface VersionContextValue {
+export interface PerspectiveValue {
   /* Return the current global bundle */
   currentGlobalBundle: Partial<BundleDocument>
   /* Change the perspective in the studio based on the perspective name */
   setPerspective: (name: string) => void
 }
 
-export function usePerspective(): VersionContextValue {
+/**
+ * @internal
+ */
+export function usePerspective(): PerspectiveValue {
   const router = useRouter()
-  const {data: bundles} = useBundlesStore()
+  const {data: bundles} = useBundles()
 
   const setPerspective = (name: string | undefined) => {
     if (name === 'drafts') {
