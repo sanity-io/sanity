d621870aa010a7f391564d4c8147a97fd1a21495
RitaDias <anarita151@gmail.com>
2024-07-08 13:00:08 +0200

    chore(sanity): update createWrapper

--- packages/sanity/src/core/bundles/util/tests/createWrapper.tsx
+++ packages/sanity/src/core/bundles/util/tests/createWrapper.tsx
@@ -2,12 +2,9 @@ import {studioTheme, ThemeProvider} from '@sanity/ui'
 import {type ReactNode} from 'react'
 
 import {createTestProvider} from '../../../../../test/testUtils/TestProvider'
-import {releasesUsEnglishLocaleBundle} from '../../../releases/i18n'
 
 export const createWrapper = async () => {
-  const TestProvider = await createTestProvider({
-    resources: [releasesUsEnglishLocaleBundle],
-  })
+  const TestProvider = await createTestProvider()
   return function Wrapper({children}: {children: ReactNode}): JSX.Element {
     return (
       <ThemeProvider theme={studioTheme}>
