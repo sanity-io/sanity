2190d33c36dc2a00fab47e20bf0508fd6c9f831c
Espen Hovlandsdal <espen@hovlandsdal.com>
2024-07-10 11:48:03 -0700

    fix(core): cancel telemetry consent check on app unmount

---
+++ packages/sanity/src/core/studio/MaybeEnableErrorReporting.ts
@@ -0,0 +1,40 @@
+import {useEffect} from 'react'
+
+import {type ErrorReporter} from '../error/errorReporter'
+import {useClient} from '../hooks'
+
+const CONSENT_CLIENT_OPTIONS = {apiVersion: '2024-05-28'}
+
+/**
+ * React component that checks for the users' telemetry consent, enabling or disabling error reporting
+ * on the passed error reporter accordingly. Reporter should default to being disabled/buffer
+ * events until the consent status is checked.
+ *
+ * Needs to be mounted within a SourceProvider to work correctly.
+ *
+ * @param props - Takes an error reporting instance to enable or disable based on the users' consent
+ * @returns Explicitly return null. Component only has side effects.
+ */
+export function MaybeEnableErrorReporting(props: {errorReporter: ErrorReporter}): null {
+  const {errorReporter} = props
+  const client = useClient(CONSENT_CLIENT_OPTIONS)
+
+  useEffect(() => {
+    const request = client.observable.request({uri: '/intake/telemetry-status'}).subscribe({
+      next: (res) => {
+        if (res?.status === 'granted') {
+          errorReporter.enable()
+        } else {
+          errorReporter.disable()
+        }
+      },
+      error: (err) => {
+        console.error('Error fetching telemetry status', err)
+      },
+    })
+
+    return () => request.unsubscribe()
+  }, [client, errorReporter])
+
+  return null
+}
