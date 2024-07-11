841a9cbc3d62364eea1a2e3ac18e10fd6e2f02e6
Pedro Bonamin <46196328+pedrobonamin@users.noreply.github.com>
2024-07-11 17:02:32 +0200

    feat(corel): releases details screen. (#7092)
    * fix(corel): update releases router to use bundle name
    
    * feat(corel): add bundle details screen
    
    * chore: update @sanity/icons package
    
    * feat(corel): releases DocumentRow updates
    
    * fix(corel): update BundleIconEditorPicker types
    
    * fix(corel): update bundles table test
    
    * fix(corel): add filter in documents table
    
    * chore(corel): update file location and naming
    
    * feat(corel): add document actions to document row
    
    * fix(core): remove references to bundles in useListener hook
    
    * fix(corel): add search params to IntentLink
    
    * chore(corel): remove router type change and restore previous pnpm-lock file
    
    * fix(corel): update bundlesTable tests

---
+++ packages/sanity/src/core/releases/tool/detail/documentTable/DocumentRow.tsx
@@ -0,0 +1,177 @@
+import {CheckmarkCircleIcon, EmptyIcon, Progress50Icon} from '@sanity/icons'
+import {type SanityDocument} from '@sanity/types'
+import {AvatarStack, Box, Card, Flex, Text} from '@sanity/ui'
+import {
+  type Dispatch,
+  type ForwardedRef,
+  forwardRef,
+  type SetStateAction,
+  useEffect,
+  useMemo,
+} from 'react'
+import {getPublishedId, RelativeTime, SanityDefaultPreview, UserAvatar} from 'sanity'
+import {IntentLink} from 'sanity/router'
+
+import {Tooltip} from '../../../../../ui-components'
+import {type BundleDocument} from '../../../../store/bundles/types'
+import {DocumentActions} from './DocumentActions'
+import {useDocumentPreviewValues} from './useDocumentPreviewValues'
+import {useVersionHistory} from './useVersionHistory'
+
+const DOCUMENT_STATUS = {
+  ready: {
+    icon: CheckmarkCircleIcon,
+    color: 'var(--card-badge-positive-icon-color)',
+    text: 'This document is marked as ready',
+  },
+  noChanges: {
+    icon: EmptyIcon,
+    color: '',
+    text: "This document doesn't have changes in this release",
+  },
+  edited: {
+    icon: Progress50Icon,
+    color: 'var(--card-badge-caution-icon-color)',
+    text: 'This document has been edited in this release',
+  },
+} as const
+
+const DocumentStatus = ({status}: {status: keyof typeof DOCUMENT_STATUS}) => {
+  const Icon = DOCUMENT_STATUS[status].icon
+  return (
+    <Box paddingX={2}>
+      <Text muted size={1}>
+        <Tooltip content={DOCUMENT_STATUS[status].text} portal>
+          {Icon && <Icon style={{color: DOCUMENT_STATUS[status].color}} />}
+        </Tooltip>
+      </Text>
+    </Box>
+  )
+}
+
+const getDocumentStatus = (document: SanityDocument): keyof typeof DOCUMENT_STATUS => {
+  if (document._state === 'ready') {
+    return 'ready'
+  }
+  if (document._updatedAt === document._createdAt) {
+    return 'noChanges'
+  }
+  return 'edited'
+}
+
+export function DocumentRow(props: {
+  searchTerm: string
+  document: SanityDocument
+  release: BundleDocument
+  setCollaborators: Dispatch<SetStateAction<string[]>>
+}) {
+  const {document, release, searchTerm, setCollaborators} = props
+  const documentId = document._id
+  const documentTypeName = document._type
+  const {previewValues, isLoading} = useDocumentPreviewValues({document, release})
+
+  const history = useVersionHistory(documentId, document?._rev)
+
+  useEffect(() => {
+    setCollaborators((pre) => Array.from(new Set([...pre, ...history.editors])))
+  }, [history.editors, setCollaborators])
+
+  const LinkComponent = useMemo(
+    () =>
+      // eslint-disable-next-line @typescript-eslint/no-shadow
+      forwardRef(function LinkComponent(linkProps, ref: ForwardedRef<HTMLAnchorElement>) {
+        return (
+          <IntentLink
+            {...linkProps}
+            intent="edit"
+            params={{
+              id: getPublishedId(documentId, true),
+              type: documentTypeName,
+            }}
+            searchParams={[['perspective', `bundle.${release.name}`]]}
+            ref={ref}
+          />
+        )
+      }),
+    [documentId, documentTypeName, release.name],
+  )
+
+  if (searchTerm) {
+    // Early return to filter out documents that don't match the search term
+    const fallbackTitle = typeof document.title === 'string' ? document.title : 'Untitled'
+    const title = typeof previewValues.title === 'string' ? previewValues.title : fallbackTitle
+    if (!title.toLowerCase().includes(searchTerm.toLowerCase())) return null
+  }
+
+  return (
+    <Card border radius={3}>
+      <Flex style={{margin: -1}}>
+        <Box flex={1} padding={1}>
+          <Card as={LinkComponent} radius={2} data-as="a">
+            <SanityDefaultPreview {...previewValues} isPlaceholder={isLoading} />
+          </Card>
+        </Box>
+
+        {/* Created */}
+        <Flex align="center" paddingX={2} paddingY={3} sizing="border" style={{width: 130}}>
+          {document._createdAt && (
+            <Flex align="center" gap={2}>
+              {history.createdBy && <UserAvatar size={0} user={history.createdBy} />}
+              <Text muted size={1}>
+                <RelativeTime time={document._createdAt} useTemporalPhrase minimal />
+              </Text>
+            </Flex>
+          )}
+        </Flex>
+
+        {/* Updated */}
+        <Flex align="center" paddingX={2} paddingY={3} sizing="border" style={{width: 130}}>
+          {document._updatedAt && (
+            <Flex align="center" gap={2}>
+              {history.lastEditedBy && <UserAvatar size={0} user={history.lastEditedBy} />}
+              <Text muted size={1}>
+                <RelativeTime time={document._updatedAt} useTemporalPhrase minimal />
+              </Text>
+            </Flex>
+          )}
+        </Flex>
+
+        {/* Published */}
+        <Flex align="center" paddingX={2} paddingY={3} sizing="border" style={{width: 130}}>
+          {/* TODO: How to get the publishedAt date from the document, consider history API */}
+          {/* {document._publishedAt && (
+            <Flex align="center" gap={2}>
+              <UserAvatar size={0} user={document._publishedBy} />
+              <Text muted size={1}>
+                <RelativeTime time={document._publishedAt} />
+              </Text>
+            </Flex>
+          )} */}
+
+          {!document._publishedAt && (
+            <Text muted size={1}>
+              &nbsp;
+            </Text>
+          )}
+        </Flex>
+
+        {/* Contributors */}
+        <Flex align="center" paddingX={2} paddingY={3} sizing="border" style={{width: 100}}>
+          <AvatarStack maxLength={3} size={0}>
+            {history.editors?.map((userId) => <UserAvatar key={userId} user={userId} />)}
+          </AvatarStack>
+        </Flex>
+
+        {/* Status */}
+        <Flex align="center" paddingX={2} paddingY={3} sizing="border" style={{width: 60}}>
+          <DocumentStatus status={getDocumentStatus(document)} />
+        </Flex>
+
+        {/* Actions is empty - don't render yet */}
+        <Flex align="center" flex="none" padding={3}>
+          <DocumentActions document={document} />
+        </Flex>
+      </Flex>
+    </Card>
+  )
+}
---
+++ packages/sanity/src/core/releases/tool/detail/documentTable/DocumentTable.tsx
@@ -0,0 +1,75 @@
+import {type SanityDocument} from '@sanity/types'
+import {Stack} from '@sanity/ui'
+import {type Dispatch, type SetStateAction, useMemo, useState} from 'react'
+import {styled} from 'styled-components'
+
+import {type BundleDocument} from '../../../../store/bundles/types'
+import {DocumentHeader} from './DocumentHeader'
+import {DocumentRow} from './DocumentRow'
+import {type DocumentSort} from './types'
+
+const RowStack = styled(Stack)({
+  '& > *:not(:first-child)': {
+    borderTopLeftRadius: 0,
+    borderTopRightRadius: 0,
+    marginTop: -1,
+  },
+
+  '& > *:not(:last-child)': {
+    borderBottomLeftRadius: 0,
+    borderBottomRightRadius: 0,
+  },
+})
+
+export function DocumentTable(props: {
+  documents: SanityDocument[]
+  release: BundleDocument
+  setCollaborators: Dispatch<SetStateAction<string[]>>
+}) {
+  const {documents, release, setCollaborators} = props
+  // Filter will happen at the DocumentRow level because we don't have access here to the preview values.
+  const [searchTerm, setSearchTerm] = useState<string>('')
+  const [sort, setSort] = useState<DocumentSort>({property: '_updatedAt', order: 'desc'})
+
+  const sortedDocuments = useMemo(() => {
+    const sorted = [...documents]
+
+    sorted.sort((a, b) => {
+      const aDateString = a[sort.property]
+      const bDateString = b[sort.property]
+
+      const aDate = typeof aDateString === 'string' ? Date.parse(aDateString) : 0
+      const bDate = typeof bDateString === 'string' ? Date.parse(bDateString) : 0
+
+      if (sort.order === 'asc') {
+        return aDate - bDate
+      }
+
+      return bDate - aDate
+    })
+    return sorted
+  }, [documents, sort])
+
+  return (
+    <Stack space={1}>
+      <DocumentHeader
+        setSort={setSort}
+        sort={sort}
+        searchTerm={searchTerm}
+        setSearchTerm={setSearchTerm}
+      />
+
+      <RowStack>
+        {sortedDocuments.map((d) => (
+          <DocumentRow
+            searchTerm={searchTerm}
+            document={d}
+            key={d._id}
+            release={release}
+            setCollaborators={setCollaborators}
+          />
+        ))}
+      </RowStack>
+    </Stack>
+  )
+}
