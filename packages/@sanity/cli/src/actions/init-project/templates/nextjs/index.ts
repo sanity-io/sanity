d870848c37c29554bbe1ad5e802f445fb81143bc
Simeon Griggs <simeon@hey.com>
2024-07-11 14:16:33 +0100

    fix: change urlFor source type

--- packages/@sanity/cli/src/actions/init-project/templates/nextjs/index.ts
+++ packages/@sanity/cli/src/actions/init-project/templates/nextjs/index.ts
@@ -120,14 +120,14 @@ export const client = createClient({
 `
 
 const imageTS = `import createImageUrlBuilder from '@sanity/image-url'
-import type { Image } from 'sanity'
+import { SanityImageSource } from "@sanity/image-url/lib/types/types";
 
 import { dataset, projectId } from '../env'
 
 // https://www.sanity.io/docs/image-url
 const builder = createImageUrlBuilder({ projectId, dataset })
 
-export const urlFor = (source: Image) => {
+export const urlFor = (source: SanityImageSource) => {
   return builder.image(source)
 }
 `
