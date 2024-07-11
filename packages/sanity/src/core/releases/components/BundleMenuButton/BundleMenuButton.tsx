42ac92b21fdf41e1b96a2b6955a7d30412e96e95
Pedro Bonamin <46196328+pedrobonamin@users.noreply.github.com>
2024-07-11 09:58:40 +0200

    feat(corel): remove version documents when deleting bundle (#7107)

--- packages/sanity/src/core/releases/components/BundleMenuButton/BundleMenuButton.tsx
+++ packages/sanity/src/core/releases/components/BundleMenuButton/BundleMenuButton.tsx
@@ -1,8 +1,9 @@
 import {ArchiveIcon, EllipsisHorizontalIcon, TrashIcon} from '@sanity/icons'
-import {Button, Menu, MenuButton, MenuItem, Spinner} from '@sanity/ui'
+import {Button, Menu, MenuButton, MenuItem, Spinner, Text, useToast} from '@sanity/ui'
 import {useState} from 'react'
 import {useRouter} from 'sanity/router'
 
+import {Dialog} from '../../../../ui-components'
 import {type BundleDocument} from '../../../store/bundles/types'
 import {useBundleOperations} from '../../../store/bundles/useBundleOperations'
 
@@ -15,18 +16,32 @@ export const BundleMenuButton = ({bundle}: Props) => {
   const router = useRouter()
   const isBundleArchived = !!bundle?.archivedAt
   const [isPerformingOperation, setIsPerformingOperation] = useState(false)
+  const [showDiscardDialog, setShowDiscardDialog] = useState(false)
+  const [discardStatus, setDiscardStatus] = useState<'idle' | 'discarding' | 'error'>('idle')
 
   const bundleMenuDisabled = !bundle
+  const toast = useToast()
 
   const handleOnDeleteBundle = async () => {
     if (bundleMenuDisabled) return
-
-    setIsPerformingOperation(true)
-    await deleteBundle(bundle._id)
-    setIsPerformingOperation(false)
-    if (router.state.bundleId) {
-      // navigate back to bundle overview
-      router.navigate({bundleId: undefined})
+    try {
+      setDiscardStatus('discarding')
+      await deleteBundle(bundle)
+      setDiscardStatus('idle')
+      if (router.state.bundleId) {
+        // navigate back to bundle overview
+        router.navigate({bundleId: undefined})
+      }
+    } catch (e) {
+      setDiscardStatus('error')
+      toast.push({
+        closable: true,
+        status: 'error',
+        title: 'Failed to delete bundle',
+        description: e.message,
+      })
+    } finally {
+      setShowDiscardDialog(false)
     }
   }
 
@@ -41,36 +56,58 @@ export const BundleMenuButton = ({bundle}: Props) => {
     setIsPerformingOperation(false)
   }
 
+  // TODO: Replace this with the count once it's available. Wait for @jordanl17 change on that.
+  const count = 2
+
   return (
-    <MenuButton
-      button={
-        <Button
-          disabled={bundleMenuDisabled || isPerformingOperation}
-          icon={isPerformingOperation ? Spinner : EllipsisHorizontalIcon}
-          mode="bleed"
-          padding={2}
-          aria-label="Release menu"
-        />
-      }
-      id="bundle-menu"
-      menu={
-        <Menu>
-          <MenuItem
-            onClick={handleOnToggleArchive}
-            // TODO: swap line once UnaryIcon is available
-            // icon={isBundleArchived ? UnarchiveIcon : ArchiveIcon}
-            icon={isBundleArchived ? ArchiveIcon : ArchiveIcon}
-            text={isBundleArchived ? 'Unarchive' : 'Archive'}
+    <>
+      <MenuButton
+        button={
+          <Button
+            disabled={bundleMenuDisabled || isPerformingOperation}
+            icon={isPerformingOperation ? Spinner : EllipsisHorizontalIcon}
+            mode="bleed"
+            padding={2}
+            aria-label="Release menu"
           />
-          <MenuItem onClick={handleOnDeleteBundle} icon={TrashIcon} text="Delete" />
-        </Menu>
-      }
-      popover={{
-        constrainSize: true,
-        fallbackPlacements: [],
-        placement: 'bottom',
-        portal: true,
-      }}
-    />
+        }
+        id="bundle-menu"
+        menu={
+          <Menu>
+            <MenuItem
+              onClick={handleOnToggleArchive}
+              icon={isBundleArchived ? ArchiveIcon : ArchiveIcon}
+              text={isBundleArchived ? 'Unarchive' : 'Archive'}
+            />
+            <MenuItem onClick={() => setShowDiscardDialog(true)} icon={TrashIcon} text="Delete" />
+          </Menu>
+        }
+        popover={{
+          constrainSize: true,
+          fallbackPlacements: [],
+          placement: 'bottom',
+          portal: true,
+        }}
+      />
+      {showDiscardDialog && (
+        <Dialog
+          id="discard-version-dialog"
+          header={`Are you sure you want to delete the ${bundle?.title} release?`}
+          onClose={() => setShowDiscardDialog(false)}
+          footer={{
+            confirmButton: {
+              tone: 'default',
+              onClick: handleOnDeleteBundle,
+              loading: discardStatus === 'discarding',
+              disabled: discardStatus === 'discarding',
+            },
+          }}
+        >
+          <Text muted size={1}>
+            This will also delete {count} document version{count > 1 ? 's' : ''}.
+          </Text>
+        </Dialog>
+      )}
+    </>
   )
 }
