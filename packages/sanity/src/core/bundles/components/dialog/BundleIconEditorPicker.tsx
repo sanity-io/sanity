62e0c3fc835381fa36070eeec4bdd09f711bfa7f
RitaDias <rita@sanity.io>
2024-07-12 13:11:31 +0200

    test(COREL): add core tests (#7119)
    * test(sanity): try to find solution for test
    
    * wip: add theme provider to test
    
    * test(sanity): add test to bundleForm & add createWrapper in core
    
    * test(sanity): add test to BundleIconEditorPicker
    
    * chore(sanity): update createWrapper
    
    * test(sanity): update bundleForm test
    
    * test(sanity) add createBundleDialog tests
    
    * test(sanity) add BundleMenu tests
    
    * refactor(sanity): add prop to createWrapper to access TestProviderOptions
    
    ---------
    
    Co-authored-by: Ash <ash@sanity.io>

--- packages/sanity/src/core/bundles/components/dialog/BundleIconEditorPicker.tsx
+++ packages/sanity/src/core/bundles/components/dialog/BundleIconEditorPicker.tsx
@@ -1,6 +1,16 @@
 import {COLOR_HUES, type ColorHueKey} from '@sanity/color'
 import {icons, type IconSymbol, SearchIcon} from '@sanity/icons'
-import {Avatar, Box, Button, Flex, Popover, Stack, TextInput, useClickOutside} from '@sanity/ui'
+import {
+  Avatar,
+  Box,
+  Button,
+  Container,
+  Flex,
+  Popover,
+  Stack,
+  TextInput,
+  useClickOutside,
+} from '@sanity/ui'
 import {useCallback, useState} from 'react'
 import {styled} from 'styled-components'
 
@@ -60,7 +70,7 @@ export function BundleIconEditorPicker(props: {
   return (
     <Popover
       content={
-        <>
+        <Container data-testid="popover-content">
           <Flex gap={1} padding={1}>
             {COLOR_HUES.map((hue) => (
               <Button
@@ -69,6 +79,7 @@ export function BundleIconEditorPicker(props: {
                 onClick={handleHueChange(hue)}
                 padding={1}
                 selected={value.hue === hue}
+                data-testid={`hue-button-${hue}`}
               >
                 <Avatar color={hue} size={0} />
               </Button>
@@ -96,11 +107,12 @@ export function BundleIconEditorPicker(props: {
                     mode="bleed"
                     onClick={handleIconChange(key as IconSymbol)}
                     padding={2}
+                    data-testId={`icon-button-${key}`}
                   />
                 ))}
             </IconPickerFlex>
           </StyledStack>
-        </>
+        </Container>
       }
       open={open}
       placement="bottom-start"
@@ -115,6 +127,7 @@ export function BundleIconEditorPicker(props: {
           ref={setButton}
           selected={open}
           radius="full"
+          data-testid="icon-picker-button"
         >
           <BundleBadge hue={value.hue} icon={value.icon} />
         </Button>
