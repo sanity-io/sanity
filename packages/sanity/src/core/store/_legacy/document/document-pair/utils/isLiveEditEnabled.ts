45adbf1b0941dd1d1d7579f233e1ea59416455e6
Ash <ash@sanity.io>
2024-07-14 17:42:58 +0100

    feat(sanity): #TEMPORARY #DELETE handle versions in `DocumentPaneProvider`

--- packages/sanity/src/core/store/_legacy/document/document-pair/utils/isLiveEditEnabled.ts
+++ packages/sanity/src/core/store/_legacy/document/document-pair/utils/isLiveEditEnabled.ts
@@ -1,4 +1,5 @@
 import {type Schema} from '@sanity/types'
 
 export const isLiveEditEnabled = (schema: Schema, typeName: string): boolean =>
+  (globalThis as any).__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED_LIVE_EDIT_OVERRIDE ??
   schema.get(typeName)?.liveEdit === true
