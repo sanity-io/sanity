96b7e8f5b3d0aeec184632f39e24024f0ecd99ed
RitaDias <rita@sanity.io>
2024-07-15 15:23:04 +0200

    refactor(COREL): update title & date validation (#7148)
    * refactor(sanity): refactor error setting
    
    * refactor(sanity): refactor title error setting
    
    * refactor(sanity): refactor date error setting
    
    * docs(sanity): update comments
    
    * chore(sanity): clean up
    
    * test(sanity): update bundleiconeditorpicker test
    
    * test(sanity): update bundleForm test
    
    * test(sanity): update bundleForm & createBundleDialog test
    
    * test: update form and update tests

--- packages/sanity/src/core/bundles/components/dialog/__tests__/BundleForm.test.tsx
+++ packages/sanity/src/core/bundles/components/dialog/__tests__/BundleForm.test.tsx
@@ -1,17 +1,25 @@
 import {beforeEach, describe, expect, it, jest} from '@jest/globals'
 import {fireEvent, render, screen} from '@testing-library/react'
-import {type BundleDocument} from 'sanity'
+import {type BundleDocument, useDateTimeFormat} from 'sanity'
 
+import {useBundles} from '../../../../store/bundles'
 import {createWrapper} from '../../../util/tests/createWrapper'
 import {BundleForm} from '../BundleForm'
 
-jest.mock('sanity', () => ({
-  useDateTimeFormat: jest.fn().mockReturnValue({format: jest.fn().mockReturnValue('Mocked date')}),
-  useTranslation: jest.fn().mockReturnValue({t: jest.fn().mockReturnValue('Mocked translation')}),
+jest.mock('../../../../../core/hooks/useDateTimeFormat', () => ({
+  useDateTimeFormat: jest.fn(),
 }))
+
+jest.mock('../../../../store/bundles', () => ({
+  useBundles: jest.fn(),
+}))
+
+const mockUseBundleStore = useBundles as jest.Mock<typeof useBundles>
+const mockUseDateTimeFormat = useDateTimeFormat as jest.Mock
 
 describe('BundleForm', () => {
   const onChangeMock = jest.fn()
+  const onErrorMock = jest.fn()
   const valueMock: Partial<BundleDocument> = {
     title: '',
     description: '',
@@ -22,12 +30,36 @@ describe('BundleForm', () => {
 
   beforeEach(async () => {
     onChangeMock.mockClear()
+    onErrorMock.mockClear()
+
+    // Mock the data returned by useBundles hook
+    const mockData: BundleDocument[] = [
+      {
+        description: 'What a spring drop, allergies galore 🌸',
+        _updatedAt: '2024-07-12T10:39:32Z',
+        _rev: 'HdJONGqRccLIid3oECLjYZ',
+        authorId: 'pzAhBTkNX',
+        title: 'Spring Drop',
+        icon: 'heart-filled',
+        _id: 'db76c50e-358b-445c-a57c-8344c588a5d5',
+        _type: 'bundle',
+        name: 'spring-drop',
+        hue: 'magenta',
+        _createdAt: '2024-07-02T11:37:51Z',
+      },
+      // Add more mock data if needed
+    ]
+    mockUseBundleStore.mockReturnValue({data: mockData, loading: false, dispatch: jest.fn()})
+
+    mockUseDateTimeFormat.mockReturnValue({format: jest.fn().mockReturnValue('Mocked date')})
 
     const wrapper = await createWrapper()
-    render(<BundleForm onChange={onChangeMock} value={valueMock} />, {wrapper})
+    render(<BundleForm onChange={onChangeMock} value={valueMock} onError={onErrorMock} />, {
+      wrapper,
+    })
   })
 
-  it('should render the form fields', async () => {
+  it('should render the form fields', () => {
     expect(screen.getByTestId('bundle-form-title')).toBeInTheDocument()
     expect(screen.getByTestId('bundle-form-description')).toBeInTheDocument()
     expect(screen.getByTestId('bundle-form-publish-at')).toBeInTheDocument()
@@ -59,5 +91,42 @@ describe('BundleForm', () => {
     fireEvent.change(publishAtInput, {target: {value: ' '}})
 
     expect(onChangeMock).toHaveBeenCalledWith({...valueMock, publishAt: ''})
+  })
+
+  it('should show an error when the title is "drafts"', () => {
+    const titleInput = screen.getByTestId('bundle-form-title')
+
+    fireEvent.change(titleInput, {target: {value: 'drafts'}})
+
+    expect(screen.getByTestId('input-validation-icon-error')).toBeInTheDocument()
+  })
+
+  it('should show an error when the title is "published"', () => {
+    const titleInput = screen.getByTestId('bundle-form-title')
+    fireEvent.change(titleInput, {target: {value: 'published'}})
+
+    expect(screen.getByTestId('input-validation-icon-error')).toBeInTheDocument()
+  })
+
+  it('should show an error when the bundle already exists', () => {
+    const titleInput = screen.getByTestId('bundle-form-title')
+    fireEvent.change(titleInput, {target: {value: 'Spring Drop'}})
+
+    expect(screen.getByTestId('input-validation-icon-error')).toBeInTheDocument()
+  })
+
+  it('should show an error when the title is empty', () => {
+    const titleInput = screen.getByTestId('bundle-form-title')
+    fireEvent.change(titleInput, {target: {value: 'test'}}) // Set a valid title first
+    fireEvent.change(titleInput, {target: {value: ' '}}) // remove the title
+
+    expect(screen.getByTestId('input-validation-icon-error')).toBeInTheDocument()
+  })
+
+  it('should show an error when the publishAt input value is invalid', () => {
+    const publishAtInput = screen.getByTestId('bundle-form-publish-at')
+    fireEvent.change(publishAtInput, {target: {value: 'invalid-date'}})
+
+    expect(screen.getByTestId('input-validation-icon-error')).toBeInTheDocument()
   })
 })
