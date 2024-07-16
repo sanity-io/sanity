c927f429b0d9cb5637a67ed29558f93ec5775fb3
Pedro Bonamin <pedrobonamin@gmail.com>
2024-07-16 20:43:07 +0200

    chore(corel): add migration script and handlers

--- packages/sanity/src/core/releases/tool/ReleasesTool.tsx
+++ packages/sanity/src/core/releases/tool/ReleasesTool.tsx
@@ -1,12 +1,15 @@
 import {useRouter} from 'sanity/router'
 
 import {ReleaseDetail} from './detail/ReleaseDetail'
+import {MigrateBundlesSlug} from './Migrate'
 import {ReleasesOverview} from './ReleasesOverview'
 
 export function ReleasesTool() {
   const router = useRouter()
 
   const {bundleSlug} = router.state
+  // TODO: Remove this after merging the PR and running the migration script
+  if (bundleSlug === 'migrate') return <MigrateBundlesSlug />
   if (bundleSlug) return <ReleaseDetail />
 
   return <ReleasesOverview />
