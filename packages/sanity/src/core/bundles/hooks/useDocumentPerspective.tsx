b5fea980afe89d6fefc72a4aa2ccf44e82e1afb9
RitaDias <anarita151@gmail.com>
2024-07-19 14:18:59 +0200

    refactor: update fetch and query

--- packages/sanity/src/core/bundles/hooks/useDocumentPerspective.tsx
+++ packages/sanity/src/core/bundles/hooks/useDocumentPerspective.tsx
@@ -1,6 +1,6 @@
-import {type SanityDocument} from '@sanity/client'
+import {type ListenOptions, type SanityDocument} from '@sanity/client'
 import {useCallback, useEffect, useMemo, useState} from 'react'
-import {catchError, of, tap} from 'rxjs'
+import {catchError, of} from 'rxjs'
 import {DEFAULT_STUDIO_CLIENT_OPTIONS, getBundleSlug, useBundles, useClient} from 'sanity'
 
 export interface DocumentPerspectiveProps {
@@ -13,6 +13,12 @@ export interface DocumentPerspectiveState {
   loading: boolean
 }
 
+const LISTEN_OPTIONS: ListenOptions = {
+  events: ['welcome', 'mutation', 'reconnect'],
+  includeResult: true,
+  visibility: 'query',
+}
+
 export function useDocumentPerspective(props: DocumentPerspectiveProps): DocumentPerspectiveState {
   const {documentId} = props
 
@@ -23,7 +29,9 @@ export function useDocumentPerspective(props: DocumentPerspectiveProps): Documen
   const [loading, setLoading] = useState<boolean>(client !== null)
   const {data: bundles} = useBundles()
   const pureDocumentId =
-    documentId.indexOf('.') > 0 ? documentId.slice(0, documentId.indexOf('.')) : documentId
+    documentId.indexOf('.') > 0
+      ? documentId.slice(documentId.indexOf('.') + 1, documentId.length)
+      : documentId
 
   const QUERY = `*[sanity::versionOf("${pureDocumentId}")]`
 
@@ -36,6 +44,7 @@ export function useDocumentPerspective(props: DocumentPerspectiveProps): Documen
     try {
       const res = await client.fetch(QUERY)
 
+      // build the document bundles
       const documentBundles = res
         ?.map((r: SanityDocument) =>
           bundles?.find((b) => r._version && getBundleSlug(r._id) === b.slug),
@@ -53,10 +62,7 @@ export function useDocumentPerspective(props: DocumentPerspectiveProps): Documen
   const listener$ = useMemo(() => {
     if (!client) return of()
 
-    const events$ = client.observable.listen(QUERY).pipe(
-      //map((event) => ({event})),
-      // eslint-disable-next-line no-console
-      tap((event) => console.log('Event:', event)), // This will log each event to the console
+    const events$ = client.observable.listen(QUERY, {}, LISTEN_OPTIONS).pipe(
       catchError((err) => {
         setError(err)
         return of() // return an empty observable on error
@@ -66,34 +72,31 @@ export function useDocumentPerspective(props: DocumentPerspectiveProps): Documen
     return events$
   }, [QUERY, client])
 
-  const handleListenerEvent = useCallback(async (event) => {
-    // eslint-disable-next-line no-console
-    console.log('Handle Listener Event:', {event})
+  const handleListenerEvent = useCallback(
+    async (event) => {
+      if (event.type === 'welcome') {
+        setLoading(true)
+        await initialFetch()
+        setLoading(false)
+      }
 
-    if (event.type === 'mutation') {
-      /*const prev = event.result
+      if (event.type === 'mutation') {
+        const prev = event.result
+        const exists = state?.find((b) => b.slug === getBundleSlug(prev._id))
 
-        const updatedBundles = state?.map((b: SanityDocument) =>
-          b.slug === getBundleSlug(prev._id) ? prev : b,
-        )
-
-        setState(updatedBundles || [])*/
-      //setState()
-    }
-    // Handle the event here (e.g., update the state with new data)
-    //initialFetch()
-  }, [])
+        if (exists) {
+          const updatedBundles = state?.map((b: SanityDocument) => (exists ? prev : b))
+          setState(updatedBundles || [])
+        } else {
+          setState([...(state || []), prev])
+        }
+      }
+    },
+    [initialFetch, state],
+  )
 
   useEffect(() => {
-    initialFetch()
-
-    const sub = listener$.subscribe({
-      next: handleListenerEvent,
-      error: (err) => {
-        console.error('Listener Error:', err)
-        setError(err as Error)
-      },
-    })
+    const sub = listener$.subscribe(handleListenerEvent)
 
     return () => {
       sub?.unsubscribe()
--- packages/sanity/src/structure/panes/document/documentPanel/header/perspective/DocumentPerspectiveMenu.tsx
+++ packages/sanity/src/structure/panes/document/documentPanel/header/perspective/DocumentPerspectiveMenu.tsx
@@ -1,12 +1,11 @@
 import {ChevronDownIcon} from '@sanity/icons'
 import {Box, Button} from '@sanity/ui'
-import {useCallback, useEffect, useMemo} from 'react'
+import {useCallback, useMemo} from 'react'
 import {
   BundleBadge,
   type BundleDocument,
   BundleMenu,
   DEFAULT_STUDIO_CLIENT_OPTIONS,
-  getAllVersionsOfDocument,
   getBundleSlug,
   useBundles,
   useClient,
@@ -33,23 +32,6 @@ export function DocumentPerspectiveMenu(props: {documentId: string}): JSX.Elemen
 
   const router = useRouter()
 
-  // TODO MAKE SURE THIS IS HOW WE WANT TO DO THIS
-  //const [documentVersions, setDocumentVersions] = useState<BundleDocument[]>([])
-
-  const fetchVersions = useCallback(async () => {
-    const response = await getAllVersionsOfDocument(bundles, client, documentId)
-    //setDocumentVersions(response)
-  }, [bundles, client, documentId])
-
-  // DUMMY FETCH -- NEEDS TO BE REPLACED -- USING GROQ from utils
-  useEffect(() => {
-    const fetchVersionsInner = async () => {
-      fetchVersions()
-    }
-
-    fetchVersionsInner()
-  }, [fetchVersions])
-
   const handleBundleClick = useCallback(() => {
     router.navigateIntent('release', {slug})
   }, [router, slug])
@@ -68,15 +50,14 @@ export function DocumentPerspectiveMenu(props: {documentId: string}): JSX.Elemen
         </BadgeButton>
       )}
       {/** TODO IS THIS STILL NEEDED? VS THE PICKER IN STUDIO NAVBAR? */}
-      {documentVersions && documentVersions.length > 0 && (
-        <Box flex="none">
-          <BundleMenu
-            button={<Button icon={ChevronDownIcon} mode="bleed" padding={2} space={2} />}
-            bundles={documentVersions as BundleDocument[]}
-            loading={!documentVersions}
-          />
-        </Box>
-      )}
+
+      <Box flex="none">
+        <BundleMenu
+          button={<Button icon={ChevronDownIcon} mode="bleed" padding={2} space={2} />}
+          bundles={documentVersions as BundleDocument[]}
+          loading={!documentVersions}
+        />
+      </Box>
     </>
   )
 }
