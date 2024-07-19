139f6f2fcf40fac9d326a0aadddef74564f22199
RitaDias <anarita151@gmail.com>
2024-07-19 11:23:07 +0200

    refactor: update dummygetters to reflect corel

--- packages/sanity/src/core/bundles/util/dummyGetters.ts
+++ packages/sanity/src/core/bundles/util/dummyGetters.ts
@@ -27,12 +27,13 @@ export async function getAllVersionsOfDocument(
     return documents.map((doc: SanityDocument) => {
       const sluggedName = getBundleSlug(doc._id)
       const bundle = bundles?.find((b) => b.slug === sluggedName)
-      return {
+      const version: Partial<BundleDocument> = {
         slug: speakingurl(sluggedName),
         title: bundle?.title || sluggedName,
         hue: bundle?.hue || 'gray',
         icon: bundle?.icon || 'cube',
       }
+      return version
     })
   })
 }
