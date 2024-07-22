74e7db34ab70da74a4968f67ddcbb363f4d054df
Bjørge Næss <bjoerge@gmail.com>
2024-07-22 11:26:40 +0200

    feat(preview): add `unstable_observeDocument(s)` to preview store (#7176)
    * chore: remove futile circular dependency workaround
    
    * refactor(core): lift global listener out of preview store
    
    * fix(preview): cleanup code so it matches current conventions + add some docs
    
    * feat(preview): add unstable_observeDocument + unstable_observeDocuments to preview store
    
    * fix: use includeMutations: false
    
    * fix: expect ts error on includeMutations: false for now

--- packages/sanity/src/core/preview/documentPreviewStore.ts
+++ packages/sanity/src/core/preview/documentPreviewStore.ts
@@ -1,14 +1,16 @@
-import {type SanityClient} from '@sanity/client'
+import {type MutationEvent, type SanityClient, type WelcomeEvent} from '@sanity/client'
 import {type PrepareViewOptions, type SanityDocument} from '@sanity/types'
-import {type Observable} from 'rxjs'
-import {distinctUntilChanged, map} from 'rxjs/operators'
+import {combineLatest, type Observable} from 'rxjs'
+import {distinctUntilChanged, filter, map} from 'rxjs/operators'
 
 import {isRecord} from '../util'
-import {create_preview_availability} from './availability'
+import {createPreviewAvailabilityObserver} from './availability'
+import {createGlobalListener} from './createGlobalListener'
+import {createObserveDocument} from './createObserveDocument'
 import {createPathObserver} from './createPathObserver'
 import {createPreviewObserver} from './createPreviewObserver'
 import {create_preview_documentPair} from './documentPair'
-import {create_preview_observeFields} from './observeFields'
+import {createObserveFields} from './observeFields'
 import {
   type ApiConfig,
   type DraftsModelDocument,
@@ -31,6 +33,11 @@ export type ObserveForPreviewFn = (
 ) => Observable<PreparedSnapshot>
 
 /**
+ * The document preview store supports subscribing to content for previewing purposes.
+ * Documents observed by this store will be kept in sync and receive real-time updates from all collaborators,
+ * but has no support for optimistic updates, so any local edits will require a server round-trip before becoming visible,
+ * which means this store is less suitable for real-time editing scenarios.
+ *
  * @hidden
  * @beta */
 export interface DocumentPreviewStore {
@@ -51,6 +58,19 @@ export interface DocumentPreviewStore {
     id: string,
     paths: PreviewPath[],
   ) => Observable<DraftsModelDocument<T>>
+
+  /**
+   * Observe a complete document with the given ID
+   * @hidden
+   * @beta
+   */
+  unstable_observeDocument: (id: string) => Observable<SanityDocument | undefined>
+  /**
+   * Observe a list of complete documents with the given IDs
+   * @hidden
+   * @beta
+   */
+  unstable_observeDocuments: (ids: string[]) => Observable<(SanityDocument | undefined)[]>
 }
 
 /** @internal */
@@ -63,18 +83,19 @@ export function createDocumentPreviewStore({
   client,
 }: DocumentPreviewStoreOptions): DocumentPreviewStore {
   const versionedClient = client.withConfig({apiVersion: '1'})
+  const globalListener = createGlobalListener(versionedClient).pipe(
+    filter(
+      (event): event is MutationEvent | WelcomeEvent =>
+        // ignore reconnect events for now until we know that downstream consumers can handle them
+        event.type === 'mutation' || event.type === 'welcome',
+    ),
+  )
+  const invalidationChannel = globalListener.pipe(
+    map((event) => (event.type === 'welcome' ? {type: 'connected' as const} : event)),
+  )
 
-  // NOTE: this is workaroudn for circumventing a circular dependency between `observePaths` and
-  // `observeFields`.
-  // eslint-disable-next-line camelcase
-  const __proxy_observePaths: ObservePathsFn = (value, paths, apiConfig) => {
-    return observePaths(value, paths, apiConfig)
-  }
-
-  const {observeFields} = create_preview_observeFields({
-    observePaths: __proxy_observePaths,
-    versionedClient,
-  })
+  const observeDocument = createObserveDocument({client, mutationChannel: globalListener})
+  const observeFields = createObserveFields({client: versionedClient, invalidationChannel})
 
   const {observePaths} = createPathObserver({observeFields})
 
@@ -88,21 +109,23 @@ export function createDocumentPreviewStore({
     )
   }
 
-  // const {createPreviewObserver} = create_preview_createPreviewObserver(observeDocumentTypeFromId)
   const observeForPreview = createPreviewObserver({observeDocumentTypeFromId, observePaths})
-  const {observeDocumentPairAvailability} = create_preview_availability(
+  const {observeDocumentPairAvailability} = createPreviewAvailabilityObserver(
     versionedClient,
     observePaths,
   )
   const {observePathsDocumentPair} = create_preview_documentPair(versionedClient, observePaths)
 
   // @todo: explain why the API is like this now, and that it should not be like this in the future!
+
   return {
     observePaths,
     observeForPreview,
     observeDocumentTypeFromId,
 
-    // eslint-disable-next-line camelcase
+    unstable_observeDocument: observeDocument,
+    unstable_observeDocuments: (ids: string[]) =>
+      combineLatest(ids.map((id) => observeDocument(id))),
     unstable_observeDocumentPairAvailability: observeDocumentPairAvailability,
     unstable_observePathsDocumentPair: observePathsDocumentPair,
   }
