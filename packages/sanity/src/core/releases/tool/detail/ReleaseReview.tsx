6c89da916861c54fc9b9b1a5e9c4e54e561618c9
Pedro Bonamin <46196328+pedrobonamin@users.noreply.github.com>
2024-07-22 14:21:23 +0200

    feat(corel): add search input to release review screen (#7213)
    * feat(corel): add search input to release review screen
    
    * chore(corel): add tests for release review search

--- packages/sanity/src/core/releases/tool/detail/ReleaseReview.tsx
+++ packages/sanity/src/core/releases/tool/detail/ReleaseReview.tsx
@@ -1,10 +1,16 @@
+import {SearchIcon} from '@sanity/icons'
 import {type SanityDocument} from '@sanity/types'
-import {Stack} from '@sanity/ui'
+import {Container, Flex, Stack, Text, TextInput} from '@sanity/ui'
+import {useState} from 'react'
+import {styled} from 'styled-components'
 
 import {type BundleDocument} from '../../../store/bundles/types'
 import {type DocumentHistory} from './documentTable/useReleaseHistory'
 import {DocumentDiffContainer} from './review/DocumentDiffContainer'
 
+const InputContainer = styled(Container)`
+  margin: 0;
+`
 export function ReleaseReview({
   documents,
   release,
@@ -14,16 +20,38 @@ export function ReleaseReview({
   release: BundleDocument
   documentsHistory: Map<string, DocumentHistory>
 }) {
+  const [searchTerm, setSearchTerm] = useState('')
+
   return (
-    <Stack space={[5, 6]}>
-      {documents.map((document) => (
-        <DocumentDiffContainer
-          key={document._id}
-          document={document}
-          release={release}
-          history={documentsHistory.get(document._id)}
-        />
-      ))}
+    <Stack space={5}>
+      <Flex justify="space-between" align="center">
+        <Text size={1} weight="semibold">
+          Changes to published documents
+        </Text>
+        <InputContainer width={0}>
+          <TextInput
+            fontSize={1}
+            icon={SearchIcon}
+            placeholder="Search documents"
+            radius={3}
+            value={searchTerm}
+            onChange={(event) => setSearchTerm(event.currentTarget.value)}
+            onClear={() => setSearchTerm('')}
+            clearButton={!!searchTerm}
+          />
+        </InputContainer>
+      </Flex>
+      <Stack space={[5, 6]}>
+        {documents.map((document) => (
+          <DocumentDiffContainer
+            key={document._id}
+            searchTerm={searchTerm}
+            document={document}
+            release={release}
+            history={documentsHistory.get(document._id)}
+          />
+        ))}
+      </Stack>
     </Stack>
   )
 }
