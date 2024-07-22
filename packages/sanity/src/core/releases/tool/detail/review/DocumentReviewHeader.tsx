00d201313b8c1cce5e508d1aba05b81b7395545e
Pedro Bonamin <46196328+pedrobonamin@users.noreply.github.com>
2024-07-22 15:38:31 +0200

    feat: add documents validation (#7207)
    * feat(corel): add validation to document list
    
    * fix: create documentsValidation function to support a list of documents
    
    * fix: refactor document-pair validation function to use validateDocumentWithReferences
    
    * fix(corel): update ui to show validation errors
    
    * fix(corel): create a new map instance to force re render
    
    * fix: update types
    
    * chore(corel): replace map for object in bundle documents validation
    
    * fix(corel): update release review test
    
    * chore(corel): replace map for object in documents history

--- packages/sanity/src/core/releases/tool/detail/review/DocumentReviewHeader.tsx
+++ packages/sanity/src/core/releases/tool/detail/review/DocumentReviewHeader.tsx
@@ -1,11 +1,12 @@
 import {type PreviewValue, type SanityDocument} from '@sanity/types'
-import {AvatarStack, Box, Flex} from '@sanity/ui'
+import {AvatarStack, Box, Card, Flex} from '@sanity/ui'
 
 import {RelativeTime} from '../../../../components/RelativeTime'
 import {UserAvatar} from '../../../../components/userAvatar/UserAvatar'
 import {type BundleDocument} from '../../../../store/bundles/types'
 import {Chip} from '../../../components/Chip'
 import {ReleaseDocumentPreview} from '../../../components/ReleaseDocumentPreview'
+import {type DocumentValidationStatus} from '../bundleDocumentsValidation'
 
 export function DocumentReviewHeader({
   previewValues,
@@ -13,11 +14,13 @@ export function DocumentReviewHeader({
   isLoading,
   history,
   release,
+  validation,
 }: {
   document: SanityDocument
   previewValues: PreviewValue
   isLoading: boolean
   release: BundleDocument
+  validation?: DocumentValidationStatus
   history?: {
     createdBy: string
     lastEditedBy: string
@@ -25,43 +28,49 @@ export function DocumentReviewHeader({
   }
 }) {
   return (
-    <Flex justify="space-between" align="center">
-      <Box padding={1} style={{flex: '1'}}>
-        <ReleaseDocumentPreview
-          documentId={document._id}
-          documentTypeName={document._type}
-          releaseSlug={release.slug}
-          previewValues={previewValues}
-          isLoading={isLoading}
-        />
-      </Box>
-      <Flex gap={2} padding={3} style={{flexShrink: 0}}>
-        {history?.createdBy && (
-          <Chip
-            avatar={<UserAvatar size={0} user={history?.createdBy} />}
-            text={
-              <span>
-                Created <RelativeTime time={document._createdAt} useTemporalPhrase />
-              </span>
-            }
+    <Card
+      tone={validation?.hasError ? 'critical' : 'default'}
+      radius={3}
+      style={{marginBottom: 20}}
+    >
+      <Flex justify="space-between" align="center">
+        <Box padding={1} style={{flex: '1'}}>
+          <ReleaseDocumentPreview
+            documentId={document._id}
+            documentTypeName={document._type}
+            releaseSlug={release.slug}
+            previewValues={previewValues}
+            isLoading={isLoading}
           />
-        )}
-        {history?.lastEditedBy && (
-          <Chip
-            avatar={<UserAvatar size={0} user={history?.lastEditedBy} />}
-            text={
-              <span>
-                Edited <RelativeTime time={document._updatedAt} useTemporalPhrase />
-              </span>
-            }
-          />
-        )}
-        <Box padding={1}>
-          <AvatarStack size={0} style={{margin: -1}}>
-            {history?.editors?.map((userId) => <UserAvatar key={userId} user={userId} />)}
-          </AvatarStack>
         </Box>
+        <Flex gap={2} padding={3} style={{flexShrink: 0}}>
+          {history?.createdBy && (
+            <Chip
+              avatar={<UserAvatar size={0} user={history?.createdBy} />}
+              text={
+                <span>
+                  Created <RelativeTime time={document._createdAt} useTemporalPhrase />
+                </span>
+              }
+            />
+          )}
+          {history?.lastEditedBy && (
+            <Chip
+              avatar={<UserAvatar size={0} user={history?.lastEditedBy} />}
+              text={
+                <span>
+                  Edited <RelativeTime time={document._updatedAt} useTemporalPhrase />
+                </span>
+              }
+            />
+          )}
+          <Box padding={1}>
+            <AvatarStack size={0} style={{margin: -1}}>
+              {history?.editors?.map((userId) => <UserAvatar key={userId} user={userId} />)}
+            </AvatarStack>
+          </Box>
+        </Flex>
       </Flex>
-    </Flex>
+    </Card>
   )
 }
