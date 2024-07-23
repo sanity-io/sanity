2c35087448807b9370a9d91fa6de6722d55ee67d
Cody Olsen <81981+stipsan@users.noreply.github.com>
2024-07-23 13:12:32 +0200

    fix(WorkspacesProvider): run `prepareConfig` in a passive effect (#7226)

--- packages/sanity/src/core/studio/StudioProvider.tsx
+++ packages/sanity/src/core/studio/StudioProvider.tsx
@@ -1,5 +1,5 @@
 import {ToastProvider} from '@sanity/ui'
-import {type ReactNode} from 'react'
+import {type ReactNode, useMemo} from 'react'
 import Refractor from 'react-refractor'
 import bash from 'refractor/lang/bash.js'
 import javascript from 'refractor/lang/javascript.js'
@@ -62,17 +62,20 @@ export function StudioProvider({
   // mounted React component that is shared across embedded and standalone studios.
   errorReporter.initialize()
 
-  const _children = (
-    <WorkspaceLoader LoadingComponent={LoadingBlock} ConfigErrorsComponent={ConfigErrorsScreen}>
-      <StudioTelemetryProvider config={config}>
-        <LocaleProvider>
-          <PackageVersionStatusProvider>
-            <MaybeEnableErrorReporting errorReporter={errorReporter} />
-            <ResourceCacheProvider>{children}</ResourceCacheProvider>
-          </PackageVersionStatusProvider>
-        </LocaleProvider>
-      </StudioTelemetryProvider>
-    </WorkspaceLoader>
+  const _children = useMemo(
+    () => (
+      <WorkspaceLoader LoadingComponent={LoadingBlock} ConfigErrorsComponent={ConfigErrorsScreen}>
+        <StudioTelemetryProvider config={config}>
+          <LocaleProvider>
+            <PackageVersionStatusProvider>
+              <MaybeEnableErrorReporting errorReporter={errorReporter} />
+              <ResourceCacheProvider>{children}</ResourceCacheProvider>
+            </PackageVersionStatusProvider>
+          </LocaleProvider>
+        </StudioTelemetryProvider>
+      </WorkspaceLoader>
+    ),
+    [children, config],
   )
 
   return (
@@ -80,7 +83,7 @@ export function StudioProvider({
       <ToastProvider paddingY={7} zOffset={Z_OFFSET.toast}>
         <ErrorLogger />
         <StudioErrorBoundary>
-          <WorkspacesProvider config={config} basePath={basePath}>
+          <WorkspacesProvider config={config} basePath={basePath} LoadingComponent={LoadingBlock}>
             <ActiveWorkspaceMatcher
               unstable_history={history}
               NotFoundComponent={NotFoundScreen}
