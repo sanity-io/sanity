1447d26eac7d7f95b67f7549da1c48da529fac25
RitaDias <rita@sanity.io>
2024-07-23 10:20:21 +0200

    refactor: update GROQ query for document versions (#7208)
    * temp: initial set up
    
    * refactor: update fetch and query
    
    * refactor: update documentPerspectiveMenu & BundleActions
    
    * refactor: remove all getDocumentVersions (old)
    
    * refactor: add guardrail to welcome event
    
    * chore: update types on hook
    
    * chore: rename hook
    
    * chore: remove unused code
    
    * test: add basic tests
    
    * test: rename dummGetters
    
    * chore: add guardrail
    
    * docs: add comment to hook
    
    * test: update test
    
    * refactor: use getpublishedid
    
    * test: fix test
    
    * chore: update the types

--- packages/sanity/src/core/bundles/components/panes/BundleActions.tsx
+++ packages/sanity/src/core/bundles/components/panes/BundleActions.tsx
@@ -1,18 +1,12 @@
 import {AddIcon, CheckmarkIcon} from '@sanity/icons'
 import {useToast} from '@sanity/ui'
-import {useCallback, useEffect, useRef, useState} from 'react'
+import {useCallback, useEffect, useState} from 'react'
 import {filter, firstValueFrom} from 'rxjs'
-import {
-  DEFAULT_STUDIO_CLIENT_OPTIONS,
-  useClient,
-  useDocumentOperation,
-  useDocumentStore,
-} from 'sanity'
+import {useDocumentOperation, useDocumentStore, useDocumentVersions} from 'sanity'
 
 import {Button} from '../../../../ui-components'
-import {useBundles} from '../../../store/bundles'
 import {type BundleDocument} from '../../../store/bundles/types'
-import {getAllVersionsOfDocument, versionDocumentExists} from '../../util/dummyGetters'
+import {versionDocumentExists} from '../../util/util'
 
 interface BundleActionsProps {
   currentGlobalBundle: BundleDocument
@@ -26,40 +20,25 @@ interface BundleActionsProps {
 export function BundleActions(props: BundleActionsProps): JSX.Element {
   const {currentGlobalBundle, documentId, documentType} = props
   const {slug, title} = currentGlobalBundle
-  const {data: bundles, loading} = useBundles()
+  const {data: documentVersions} = useDocumentVersions({documentId})
   const documentStore = useDocumentStore()
 
-  const [documentVersions, setDocumentVersions] = useState<BundleDocument[]>([])
-  const [creatingVersion, setCreatingVersion] = useState(false)
-  const [isInVersion, setIsInVersion] = useState(false)
-  const client = useClient(DEFAULT_STUDIO_CLIENT_OPTIONS)
+  const [creatingVersion, setCreatingVersion] = useState<boolean>(false)
+  const [isInVersion, setIsInVersion] = useState<boolean>(false)
+
   const toast = useToast()
   const {newVersion} = useDocumentOperation(documentId, documentType)
-  const dummyFetch = useRef<Promise<void> | undefined>()
 
-  const fetchVersions = useCallback(async () => {
-    if (!loading) {
-      const response = await getAllVersionsOfDocument(bundles, client, documentId)
-      setDocumentVersions(response)
-    }
-  }, [loading, bundles, client, documentId])
-
-  // DUMMY FETCH -- NEEDS TO BE REPLACED -- USING GROQ from utils
   useEffect(() => {
-    const fetchVersionsInner = async () => {
-      if (!dummyFetch.current) {
-        dummyFetch.current = fetchVersions()
-      }
-      await dummyFetch.current
-      setIsInVersion(versionDocumentExists(documentVersions, slug))
+    if (documentVersions) {
+      const exists = versionDocumentExists(documentVersions, slug) ?? false
+      setIsInVersion(exists)
     }
-
-    fetchVersionsInner()
-  }, [bundles, documentId, fetchVersions, documentVersions, slug])
+  }, [documentVersions, slug])
 
   const handleAddVersion = useCallback(async () => {
     // only add to version if there isn't already a version in that bundle of this doc
-    if (versionDocumentExists(documentVersions, slug)) {
+    if (documentVersions && versionDocumentExists(documentVersions, slug)) {
       toast.push({
         status: 'error',
         title: `There's already a version of this document in the bundle ${title}`,
@@ -105,6 +84,7 @@ export function BundleActions(props: BundleActionsProps): JSX.Element {
       tone="primary"
       onClick={handleAddVersion}
       disabled={isInVersion || creatingVersion}
+      loading={!documentVersions}
     />
   )
 }
