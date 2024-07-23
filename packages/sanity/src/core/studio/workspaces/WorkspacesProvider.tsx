6a11c9bb663a51aadc7b3b2cc8f35a62c2b627fa
Cody Olsen <81981+stipsan@users.noreply.github.com>
2024-07-23 13:12:32 +0200

    fix(WorkspacesProvider): run `prepareConfig` in a passive effect (#7226)

--- packages/sanity/src/core/studio/workspaces/WorkspacesProvider.tsx
+++ packages/sanity/src/core/studio/workspaces/WorkspacesProvider.tsx
@@ -1,17 +1,33 @@
-import {type ReactNode, useMemo} from 'react'
+import {type ComponentType, type ReactNode, useEffect, useState} from 'react'
 import {WorkspacesContext} from 'sanity/_singletons'
 
 import {type Config, prepareConfig} from '../../config'
+import {type WorkspacesContextValue} from './WorkspacesContext'
 
 /** @internal */
 export interface WorkspacesProviderProps {
   config: Config
   children: ReactNode
   basePath?: string
+  LoadingComponent: ComponentType
 }
 
 /** @internal */
-export function WorkspacesProvider({config, children, basePath}: WorkspacesProviderProps) {
-  const {workspaces} = useMemo(() => prepareConfig(config, {basePath}), [config, basePath])
+export function WorkspacesProvider({
+  config,
+  children,
+  basePath,
+  LoadingComponent,
+}: WorkspacesProviderProps) {
+  const [workspaces, setWorkspaces] = useState<WorkspacesContextValue | null>(null)
+
+  useEffect(() => {
+    setWorkspaces(prepareConfig(config, {basePath}).workspaces)
+  }, [basePath, config])
+
+  if (workspaces === null) {
+    return <LoadingComponent />
+  }
+
   return <WorkspacesContext.Provider value={workspaces}>{children}</WorkspacesContext.Provider>
 }
