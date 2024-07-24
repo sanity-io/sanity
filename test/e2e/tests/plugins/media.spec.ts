8939f2d1657246b30e45123824fca286c2849775
RitaDias <anarita151@gmail.com>
2024-07-24 14:27:42 +0200

    test(dev): add test

---
+++ test/e2e/tests/plugins/media.spec.ts
@@ -0,0 +1,17 @@
+import {expect} from '@playwright/test'
+import {test} from '@sanity/test'
+
+test('media plugin should open', async ({page, createDraftDocument}) => {
+  await createDraftDocument('/test/content/input-standard;imagesTest')
+  const imageInput = page.getByTestId('change-bar__field-wrapper').nth(2)
+
+  expect(await imageInput).toBeVisible()
+
+  expect(await page.locator('#mainImage_assetImageButton')).toBeVisible()
+  await page.locator('#mainImage_assetImageButton').click()
+
+  expect(await page.getByTestId('file-input-multi-button-media')).toBeVisible()
+  await page.getByTestId('file-input-browse-button-media').click()
+
+  expect(await page.getByTestId('media-plugin')).toBeVisible()
+})
