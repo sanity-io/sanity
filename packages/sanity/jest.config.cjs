917dc59ae4abb865b91ee2b106f62924eef597c7
Rico Kahler <ricokahler@gmail.com>
2024-07-24 15:47:05 -0500

    test: adds jest config for CLI tests within the `sanity` module (#7243)

--- packages/sanity/jest.config.cjs
+++ packages/sanity/jest.config.cjs
@@ -1,11 +1,19 @@
-'use strict'
-
+const path = require('node:path')
 const {createJestConfig} = require('../../test/config.cjs')
+
+const cliPath = path.resolve(__dirname, './src/_internal/cli')
 
 module.exports = createJestConfig({
   displayName: require('./package.json').name,
   globalSetup: '<rootDir>/test/setup/global.ts',
   setupFiles: ['<rootDir>/test/setup/environment.ts'],
   setupFilesAfterEnv: ['<rootDir>/test/setup/afterEnv.ts'],
-  modulePathIgnorePatterns: ['<rootDir>/playwright-ct'],
+  modulePathIgnorePatterns: [
+    '<rootDir>/playwright-ct',
+    cliPath, // the CLI has its own jest config
+  ],
+  projects: [
+    __dirname, // self
+    cliPath, // cli
+  ],
 })
