91f91a242254cbef08b15f56819484e9423bcea0
Pedro Bonamin <pedrobonamin@gmail.com>
2024-07-24 09:34:55 +0200

    feat(core): add getDocumentIsInPerspective function

--- packages/sanity/src/core/bundles/util/util.ts
+++ packages/sanity/src/core/bundles/util/util.ts
@@ -12,6 +12,34 @@ export function getBundleSlug(documentId: string): string {
   return version
 }
 
+/**
+ * @beta
+ * @param documentId - The document id, e.g. `my-document-id` or `drafts.my-document-id` or `summer.my-document-id`
+ * @param perspective - The current perspective, e.g. `bundle.summer` or undefined, it can be obtained from `useRouter().stickyParams.perspective`
+ * @returns boolean - `true` if the document is in the current perspective.
+ * e.g:
+ * - document: `summer.my-document-id`, perspective: `bundle.summer` : **true**
+ * - document: `my-document-id`, perspective: `bundle.summer` : **false**
+ * - document: `summer.my-document-id`perspective: `bundle.winter` : **false**
+ * - document: `summer.my-document-id`, perspective: `undefined` : **false**
+ * - document: `my-document-id`, perspective: `undefined` : **true**
+ * - document: `drafts.my-document-id`, perspective: `undefined` : **true**
+ */
+export function getDocumentIsInPerspective(
+  documentId: string,
+  perspective: string | undefined,
+): boolean {
+  const bundleSlug = getBundleSlug(documentId)
+
+  if (!perspective) return bundleSlug === 'Published' || bundleSlug === 'drafts'
+
+  if (!perspective.startsWith('bundle.')) return false
+  // perspective is `bundle.${bundleSlug}`
+
+  if (bundleSlug === 'Published') return false
+  return bundleSlug === perspective.replace('bundle.', '')
+}
+
 export function versionDocumentExists(
   documentVersions: BundleDocument[] = [],
   slug: string,
