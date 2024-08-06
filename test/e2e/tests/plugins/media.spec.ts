eee9d64f4b4657beb4e8b48fd3959b462c220d78
RitaDias <anarita151@gmail.com>
2024-08-06 10:03:03 +0200

    test(e2e): update e2e media tests

--- test/e2e/tests/plugins/media.spec.ts
+++ test/e2e/tests/plugins/media.spec.ts
@@ -1,17 +1,31 @@
 import {expect} from '@playwright/test'
 import {test} from '@sanity/test'
 
-test('media plugin should open', async ({page, createDraftDocument}) => {
+test('media plugin should open from input', async ({page, createDraftDocument}) => {
   await createDraftDocument('/test/content/input-standard;imagesTest')
-  const imageInput = page.getByTestId('change-bar__field-wrapper').nth(2)
 
-  expect(await imageInput).toBeVisible()
+  // wait for input to be visible
+  await expect(await page.getByTestId('change-bar__field-wrapper').nth(2)).toBeVisible()
 
-  expect(await page.locator('#mainImage_assetImageButton')).toBeVisible()
+  // wait for menu to be visible and click menu button
+  await expect(await page.locator('#mainImage_assetImageButton')).toBeVisible()
   await page.locator('#mainImage_assetImageButton').click()
 
-  expect(await page.getByTestId('file-input-multi-button-media')).toBeVisible()
+  // wait for menu to open, click the menu item for media
+  await expect(await page.getByTestId('file-input-browse-button-media')).toBeVisible()
   await page.getByTestId('file-input-browse-button-media').click()
 
-  expect(await page.getByTestId('virtuoso-scroller')).toBeVisible()
+  // check that it didn't crash
+  await expect(await page.getByRole('button', {name: 'Insert image imagesTest'})).toBeVisible()
+})
+
+test('open media plugin from navbar', async ({page, createDraftDocument}) => {
+  await createDraftDocument('/test/')
+  await expect(page.getByTestId('parent-config-studio-tool-menu')).toBeVisible()
+
+  // click media plugin
+  await page.getByRole('link', {name: 'Media'}).click()
+
+  // check that it didn't crash
+  await expect(await page.getByRole('button', {name: 'Browse Assets Upload assets'})).toBeVisible()
 })
