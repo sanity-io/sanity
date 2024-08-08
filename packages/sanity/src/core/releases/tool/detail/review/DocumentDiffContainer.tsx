e57d4514777976046a53a8c7cde1165e34653f60
Pedro Bonamin <46196328+pedrobonamin@users.noreply.github.com>
2024-08-08 09:33:25 +0200

    feat(corel): virtualize content releases plugin lists (#7301)
    * feat(core): virtualise the content releases lists
    
    * feat(corel): lift preview values state to useBundles hook
    
    * fix(core): update corel sort actions
    
    * fix(core): update useBundleDocuments return value
    
    * fix(core): rename BundleDocumentResult to DocumentInBundleResult
    
    * fix(corel): remove Row prop from releases Table
    
    * fix(core): update how preview is calculated in useBundleDocuments, reuse the fetched document
    
    * feat(core): update corel summary and overview virtual tables
    
    * fix(core): update release summary types import
    
    * feat(core): virtualize review changes screen - corel-91

--- packages/sanity/src/core/releases/tool/detail/review/DocumentDiffContainer.tsx
+++ packages/sanity/src/core/releases/tool/detail/review/DocumentDiffContainer.tsx
@@ -1,4 +1,4 @@
-import {type ObjectSchemaType, type SanityDocument} from '@sanity/types'
+import {type ObjectSchemaType} from '@sanity/types'
 import {Card, Flex} from '@sanity/ui'
 
 import {LoadingBlock} from '../../../../components/loadingBlock/LoadingBlock'
@@ -6,61 +6,61 @@ import {useSchema} from '../../../../hooks/useSchema'
 import {useObserveDocument} from '../../../../preview/useObserveDocument'
 import {type BundleDocument} from '../../../../store/bundles/types'
 import {getPublishedId} from '../../../../util/draftUtils'
-import {type DocumentValidationStatus} from '../bundleDocumentsValidation'
-import {useDocumentPreviewValues} from '../documentTable/useDocumentPreviewValues'
 import {type DocumentHistory} from '../documentTable/useReleaseHistory'
 import {DocumentReviewHeader} from '../review/DocumentReviewHeader'
+import {type DocumentInBundleResult} from '../useBundleDocuments'
 import {DocumentDiff} from './DocumentDiff'
 
-export function DocumentDiffContainer({
-  document,
-  release,
-  history,
-  searchTerm,
-  validation,
-}: {
-  document: SanityDocument
-  release: BundleDocument
-  history?: DocumentHistory
-  searchTerm: string
-  validation?: DocumentValidationStatus
-}) {
+function DocumentDiffExpanded({document}: {document: DocumentInBundleResult['document']}) {
   const publishedId = getPublishedId(document._id, true)
+
   const schema = useSchema()
   const schemaType = schema.get(document._type) as ObjectSchemaType
   if (!schemaType) {
     throw new Error(`Schema type "${document._type}" not found`)
   }
-  const {document: baseDocument, loading: baseDocumentLoading} = useObserveDocument(
-    publishedId,
-    schemaType,
-  )
-  const {previewValues, isLoading} = useDocumentPreviewValues({document, release})
 
-  if (searchTerm) {
-    // Early return to filter out documents that don't match the search term
-    const fallbackTitle = typeof document.title === 'string' ? document.title : 'Untitled'
-    const title = typeof previewValues.title === 'string' ? previewValues.title : fallbackTitle
-    if (!title.toLowerCase().includes(searchTerm.toLowerCase())) return null
-  }
+  const {document: baseDocument, loading: baseDocumentLoading} = useObserveDocument(publishedId)
 
+  if (baseDocumentLoading) return <LoadingBlock />
+
+  return <DocumentDiff baseDocument={baseDocument} document={document} schemaType={schemaType} />
+}
+
+export function DocumentDiffContainer({
+  document,
+  history,
+  release,
+  previewValues,
+  validation,
+  isExpanded,
+  toggleIsExpanded,
+}: {
+  document: DocumentInBundleResult['document']
+  history?: DocumentHistory
+  release: BundleDocument
+  previewValues: DocumentInBundleResult['previewValues']
+  validation?: DocumentInBundleResult['validation']
+  isExpanded: boolean
+  toggleIsExpanded: () => void
+}) {
   return (
-    <Card border radius={3}>
+    <Card border radius={3} data-testid={`doc-differences-${document._id}`}>
       <DocumentReviewHeader
         document={document}
-        previewValues={previewValues}
-        isLoading={!!isLoading}
+        isLoading={previewValues.isLoading}
+        previewValues={previewValues.values}
         history={history}
         release={release}
         validation={validation}
+        isExpanded={isExpanded}
+        toggleIsExpanded={toggleIsExpanded}
       />
-      <Flex justify="center" padding={4}>
-        {baseDocumentLoading ? (
-          <LoadingBlock />
-        ) : (
-          <DocumentDiff baseDocument={baseDocument} document={document} schemaType={schemaType} />
-        )}
-      </Flex>
+      {isExpanded && (
+        <Flex justify="center" padding={4}>
+          <DocumentDiffExpanded document={document} />
+        </Flex>
+      )}
     </Card>
   )
 }
