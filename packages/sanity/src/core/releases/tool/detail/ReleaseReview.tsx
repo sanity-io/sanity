f296171db91a093e81310e3930f0c0ce0d6e8c3f
Pedro Bonamin <46196328+pedrobonamin@users.noreply.github.com>
2024-08-08 09:33:25 +0200

    feat(corel): virtualize content releases plugin lists (#7301)
    * feat(core): virtualise the content releases lists
    
    * feat(corel): lift preview values state to useBundles hook
    
    * fix(core): update corel sort actions
    
    * fix(core): update useBundleDocuments return value
    
    * fix(core): rename BundleDocumentResult to DocumentInBundleResult
    
    * fix(corel): remove Row prop from releases Table
    
    * fix(core): update how preview is calculated in useBundleDocuments, reuse the fetched document
    
    * feat(core): update corel summary and overview virtual tables
    
    * fix(core): update release summary types import
    
    * feat(core): virtualize review changes screen - corel-91

--- packages/sanity/src/core/releases/tool/detail/ReleaseReview.tsx
+++ packages/sanity/src/core/releases/tool/detail/ReleaseReview.tsx
@@ -1,32 +1,74 @@
 import {SearchIcon} from '@sanity/icons'
-import {type SanityDocument} from '@sanity/types'
-import {Container, Flex, Stack, Text, TextInput} from '@sanity/ui'
-import {useState} from 'react'
+import {Box, Container, Flex, Text, TextInput} from '@sanity/ui'
+import {useVirtualizer} from '@tanstack/react-virtual'
+import {type RefObject, useCallback, useMemo, useState} from 'react'
 import {styled} from 'styled-components'
 
 import {type BundleDocument} from '../../../store/bundles/types'
-import {type DocumentValidationStatus} from './bundleDocumentsValidation'
 import {type DocumentHistory} from './documentTable/useReleaseHistory'
 import {DocumentDiffContainer} from './review/DocumentDiffContainer'
+import {type DocumentInBundleResult} from './useBundleDocuments'
 
 const InputContainer = styled(Container)`
   margin: 0;
 `
+const VirtualizerRoot = styled.div`
+  position: relative;
+  width: 100%;
+`
+
+const VirtualizerTrack = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+`
+
+// Estimation of a document with 1 change
+const REVIEW_ITEM_ESTIMATED_HEIGHT = 140
+
 export function ReleaseReview({
   documents,
   release,
   documentsHistory,
-  validation,
+  scrollContainerRef,
 }: {
-  documents: SanityDocument[]
+  documents: DocumentInBundleResult[]
   release: BundleDocument
   documentsHistory: Record<string, DocumentHistory>
-  validation: Record<string, DocumentValidationStatus>
+  scrollContainerRef: RefObject<HTMLDivElement>
 }) {
   const [searchTerm, setSearchTerm] = useState('')
+  const [expandedItems, setIsExpandedItems] = useState<Record<string, boolean>>({})
+
+  const toggleIsExpanded = useCallback((documentId: string) => {
+    setIsExpandedItems((prev) => {
+      if (typeof prev[documentId] === 'boolean') {
+        return {...prev, [documentId]: !prev[documentId]}
+      }
+      return {...prev, [documentId]: false}
+    })
+  }, [])
+
+  const filteredList = useMemo(() => {
+    return documents.filter(({previewValues, document}) => {
+      const fallbackTitle = typeof document.title === 'string' ? document.title : 'Untitled'
+      const title =
+        typeof previewValues.values.title === 'string' ? previewValues.values.title : fallbackTitle
+      return title.toLowerCase().includes(searchTerm.toLowerCase())
+    })
+  }, [searchTerm, documents])
+
+  const virtualizer = useVirtualizer({
+    count: filteredList.length,
+    getScrollElement: () => scrollContainerRef.current,
+    estimateSize: () => REVIEW_ITEM_ESTIMATED_HEIGHT,
+    overscan: 4,
+  })
+  const items = virtualizer.getVirtualItems()
 
   return (
-    <Stack space={5}>
+    <Flex direction="column" gap={5} paddingY={6}>
       <Flex justify="space-between" align="center">
         <Text size={1} weight="semibold">
           Changes to published documents
@@ -44,18 +86,34 @@ export function ReleaseReview({
           />
         </InputContainer>
       </Flex>
-      <Stack space={[5, 6]}>
-        {documents.map((document) => (
-          <DocumentDiffContainer
-            key={document._id}
-            searchTerm={searchTerm}
-            document={document}
-            release={release}
-            history={documentsHistory[document._id]}
-            validation={validation[document._id]}
-          />
-        ))}
-      </Stack>
-    </Stack>
+      <VirtualizerRoot style={{height: virtualizer.getTotalSize()}}>
+        <VirtualizerTrack style={{transform: `translateY(${items[0]?.start ?? 0}px)`}}>
+          {items.map((virtualRow) => {
+            const {document, validation, previewValues} = filteredList[virtualRow.index]
+
+            return (
+              <Box
+                paddingBottom={[5, 6]}
+                key={virtualRow.key}
+                data-index={virtualRow.index}
+                ref={virtualizer.measureElement}
+              >
+                <DocumentDiffContainer
+                  key={document._id}
+                  release={release}
+                  history={documentsHistory[document._id]}
+                  document={document}
+                  validation={validation}
+                  previewValues={previewValues}
+                  isExpanded={expandedItems[document._id] ?? true}
+                  // eslint-disable-next-line react/jsx-no-bind
+                  toggleIsExpanded={() => toggleIsExpanded(document._id)}
+                />
+              </Box>
+            )
+          })}
+        </VirtualizerTrack>
+      </VirtualizerRoot>
+    </Flex>
   )
 }
