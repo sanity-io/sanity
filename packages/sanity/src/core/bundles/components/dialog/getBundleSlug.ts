d42538d6c537c6a0407f2ba76e05d0e56b0a2ea7
Jordan Lawrence <jordanl17@me.com>
2024-08-12 16:14:49 +0100

    feat(releases): suffix to bundle slug

---
+++ packages/sanity/src/core/bundles/components/dialog/getBundleSlug.ts
@@ -0,0 +1,42 @@
+import {useCallback} from 'react'
+import speakingurl from 'speakingurl'
+
+import {useBundles} from '../../../store/bundles'
+
+export function useGetBundleSlug() {
+  const {data: bundles} = useBundles()
+
+  // From existing bundles:
+  // if slug doesn't exist, return -1
+  // if slug does exist, return the highest suffix number (or 0 if no suffix)
+  const getMaxSuffixForSlug = useCallback(
+    (baseSlug: string): number => {
+      if (!bundles) return -1
+
+      const suffixRegex = new RegExp(`^${baseSlug}(?:-(\\d+))?$`)
+      return bundles.reduce((maxSlugSuffix, {slug}) => {
+        const isBaseSlugMatch = slug.match(suffixRegex)
+        if (!isBaseSlugMatch) return maxSlugSuffix
+
+        const suffixNumber = parseInt(isBaseSlugMatch[1] || '0', 10)
+        return Math.max(maxSlugSuffix, suffixNumber)
+      }, -1)
+    },
+    [bundles],
+  )
+
+  const generateSlugFromTitle = useCallback(
+    (pickedTitle: string) => {
+      const newSlug = speakingurl(pickedTitle)
+      const existingSlugMaxSuffix = getMaxSuffixForSlug(newSlug)
+
+      // newSlug doesn't exist yet
+      if (existingSlugMaxSuffix === -1) return newSlug
+
+      return `${newSlug}-${existingSlugMaxSuffix + 1}`
+    },
+    [getMaxSuffixForSlug],
+  )
+
+  return generateSlugFromTitle
+}
