08fe78813070c85d29761ddf41e388494841f359
Rune Botten <rbotten@gmail.com>
2024-08-12 19:35:22 -0700

    fix(core): avoid unnecessary re-renders in useDocumentPresence

--- packages/sanity/src/core/store/_legacy/presence/useDocumentPresence.tsx
+++ packages/sanity/src/core/store/_legacy/presence/useDocumentPresence.tsx
@@ -1,15 +1,20 @@
-import {useEffect, useState} from 'react'
+import {isEqual} from 'lodash'
+import {useEffect, useReducer} from 'react'
 
 import {usePresenceStore} from '../datastores'
 import {type DocumentPresence} from './types'
 
+function presenceReducer(state: DocumentPresence[], next: DocumentPresence[]): DocumentPresence[] {
+  return isEqual(state, next) ? state : next
+}
+
 /** @internal */
 export function useDocumentPresence(documentId: string): DocumentPresence[] {
   const presenceStore = usePresenceStore()
-  const [presence, setPresence] = useState<DocumentPresence[]>([])
+  const [presence, dispatch] = useReducer(presenceReducer, [])
 
   useEffect(() => {
-    const subscription = presenceStore.documentPresence(documentId).subscribe(setPresence)
+    const subscription = presenceStore.documentPresence(documentId).subscribe(dispatch)
     return () => {
       subscription.unsubscribe()
     }
