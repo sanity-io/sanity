6b331ef241acf498ddb37026a9b1b80db087eee3
Pedro Bonamin <46196328+pedrobonamin@users.noreply.github.com>
2024-08-13 12:14:28 +0200

    feat(core): document diff review for new documents (#7356)
    * feat(core): update corel new document review difference
    
    * fix(core): add memoKey to corel documents in bundle, memoize components

--- packages/sanity/src/core/releases/tool/detail/__tests__/ReleaseSummary.test.tsx
+++ packages/sanity/src/core/releases/tool/detail/__tests__/ReleaseSummary.test.tsx
@@ -27,6 +27,7 @@ const timeNow = new Date()
 
 const releaseDocuments: DocumentInBundleResult[] = [
   {
+    memoKey: 'key123',
     document: {
       _id: '123',
       _type: 'document',
@@ -51,6 +52,7 @@ const releaseDocuments: DocumentInBundleResult[] = [
     },
   },
   {
+    memoKey: 'key123',
     document: {
       _id: '456',
       _type: 'document',
