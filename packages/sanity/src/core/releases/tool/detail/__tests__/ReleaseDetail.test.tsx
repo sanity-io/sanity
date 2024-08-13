29094e9993a31b147c8315ef14acca18ea07fe98
Jordan Lawrence <jordanl17@me.com>
2024-08-13 11:26:51 +0100

    feat(releases, core): supporting remotely deleted bundles (#7342)
    * feat(releases, core): supporting remotely deleted bundles
    
    * test(releases, core): testing for remotely deleted bundles
    
    * test(releases, core): further tests for remote deleted
    
    * test(releases, core): fixing failures due to translation tests
    
    * fix(releases): table prop type
    
    * test(releases, core): resolving mocking issue for sanity useTranslation
    
    * fix(releases, core): fixing incorrect formatted translation
    
    * chore(releases, core): fixing imports in tests
    
    * chore(releases, core): fixing lint error in DocumentHeaderTitle
    
    * chore(releases, core): PR comments

--- packages/sanity/src/core/releases/tool/detail/__tests__/ReleaseDetail.test.tsx
+++ packages/sanity/src/core/releases/tool/detail/__tests__/ReleaseDetail.test.tsx
@@ -11,7 +11,7 @@ import {ReleaseDetail} from '../ReleaseDetail'
 import {useBundleDocuments} from '../useBundleDocuments'
 
 jest.mock('../../../../store/bundles', () => ({
-  useBundles: jest.fn().mockReturnValue({data: [], loading: false}),
+  useBundles: jest.fn().mockReturnValue({data: [], loading: false, deletedBundles: {}}),
 }))
 
 jest.mock('../../../../store/bundles/useBundleOperations', () => ({
@@ -93,7 +93,12 @@ const publishAgnosticTests = () => {
 describe('ReleaseDetail', () => {
   describe('when loading bundles', () => {
     beforeEach(async () => {
-      mockUseBundles.mockReturnValue({data: [], loading: true, dispatch: jest.fn()})
+      mockUseBundles.mockReturnValue({
+        data: [],
+        loading: true,
+        dispatch: jest.fn(),
+        deletedBundles: {},
+      })
       await renderTest()
     })
 
@@ -130,6 +135,7 @@ describe('ReleaseDetail', () => {
         ],
         loading: false,
         dispatch: jest.fn(),
+        deletedBundles: {},
       })
       mockUseListener.mockReturnValue({
         documents: [],
@@ -177,6 +183,7 @@ describe('after bundles have loaded', () => {
         ],
         loading: false,
         dispatch: jest.fn(),
+        deletedBundles: {},
       })
       mockUseListener.mockReturnValue({
         documents: [
@@ -376,6 +383,7 @@ describe('after bundles have loaded', () => {
         ],
         loading: false,
         dispatch: jest.fn(),
+        deletedBundles: {},
       })
       mockUseListener.mockReturnValue({
         documents: [
@@ -408,6 +416,59 @@ describe('after bundles have loaded', () => {
 
     it('should not show the review changes button', () => {
       expect(screen.queryByText('Review changes')).toBeNull()
+    })
+  })
+
+  describe('with deleted release', () => {
+    beforeEach(async () => {
+      mockUseBundles.mockReturnValue({
+        data: [],
+        loading: false,
+        dispatch: jest.fn(),
+        deletedBundles: {
+          'test-bundle-slug': {
+            title: 'Test bundle',
+            publishedAt: undefined,
+            archivedAt: undefined,
+            _id: 'test-id',
+            _createdAt: new Date().toISOString(),
+            _type: 'bundle',
+            slug: 'test-bundle-slug',
+            hue: 'blue',
+            icon: 'string',
+            authorId: 'author-id',
+            _updatedAt: new Date().toISOString(),
+            _rev: 'abc',
+          },
+        },
+      })
+      await renderTest()
+    })
+
+    publishAgnosticTests()
+
+    it('should not show publish button', () => {
+      expect(screen.queryByText('Publish all')).toBeNull()
+    })
+
+    it('should disable Bundle menu', () => {
+      expect(screen.getByTestId('release-menu-button')).toBeDisabled()
+    })
+  })
+
+  describe('with missing release', () => {
+    beforeEach(async () => {
+      mockUseBundles.mockReturnValue({
+        data: [],
+        loading: false,
+        dispatch: jest.fn(),
+        deletedBundles: {},
+      })
+      await renderTest()
+    })
+
+    it('should show missing release message', () => {
+      screen.getByText('Release not found: test-bundle-slug')
     })
   })
 })
