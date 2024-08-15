3bee7b4a058330767d5bf4b118503e86475cd17e
Bjørge Næss <bjoerge@gmail.com>
2024-08-15 10:28:18 +0200

    fixup! refactor(preview): extract global listener, refactor preview APIs and improve typings

--- packages/sanity/src/core/preview/constants.tsx
+++ packages/sanity/src/core/preview/constants.tsx
@@ -8,7 +8,7 @@ export const INCLUDE_FIELDS = [...INCLUDE_FIELDS_QUERY, '_key']
  * How long to wait after the last subscriber has unsubscribed before resetting the observable and disconnecting the listener
  * We want to keep the listener alive for a short while after the last subscriber has unsubscribed to avoid unnecessary reconnects
  */
-export const LISTENER_RESET_DELAY = 2000
+export const LISTENER_RESET_DELAY = 10_000
 
 export const AVAILABILITY_READABLE = {
   available: true,
