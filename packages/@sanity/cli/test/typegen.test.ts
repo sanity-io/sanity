c11e51fdb20169282f6fdcdd51905096ecb98cac
Sindre Gulseth <sgulseth@gmail.com>
2024-08-20 12:16:18 +0200

    feat(typegen): set overload client methods to default to true (#7390)

--- packages/@sanity/cli/test/typegen.test.ts
+++ packages/@sanity/cli/test/typegen.test.ts
@@ -70,7 +70,7 @@ describeCliTest('CLI: `sanity typegen`', () => {
     expect(types.toString()).toMatchSnapshot()
   })
 
-  test('sanity typegen generate: with overloadClientMethods', async () => {
+  test('sanity typegen generate: with overloadClientMethods false', async () => {
     // Write a prettier config to the output folder, with single quotes. The defeault is double quotes.
     await writeFile(`${studiosPath}/v3/out/.prettierrc`, '{\n  "singleQuote": true\n}\n')
     const result = await runSanityCmdCommand('v3', [
@@ -86,7 +86,7 @@ describeCliTest('CLI: `sanity typegen`', () => {
     )
 
     const types = await readFile(`${studiosPath}/v3/out/types.ts`)
-    expect(types.toString()).toContain(`'person'`)
+    expect(types.toString()).not.toContain(`Query TypeMap`)
     expect(types.toString()).toMatchSnapshot()
   })
 })
