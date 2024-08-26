feb873ce766e734790d39fdfb686e01372bce8ce
Drew Lyton <31733517+drewlyton@users.noreply.github.com>
2024-08-26 16:06:25 -0400

    Update packages/sanity/src/core/i18n/bundles/studio.ts
    Co-authored-by: Jon Eide Johnsen <joneidejohnsen@gmail.com>

--- packages/sanity/src/core/i18n/bundles/studio.ts
+++ packages/sanity/src/core/i18n/bundles/studio.ts
@@ -1099,7 +1099,7 @@ export const studioLocaleStrings = defineLocalesResources('studio', {
   'package-version.new-package-available.reload-button': 'Push to reload',
   /** Title of the alert for studio users when packages in their studio are out-of-date */
   'package-version.new-package-available.title': 'Sanity Studio is ready to update!',
-  /** Label for action to manage members of the current studio project */
+  /** Label for action to invite members to the current studio project */
   'presence.action.manage-members': 'Invite collaborators',
   /** Accessibility label for presence menu button */
   'presence.aria-label': 'Global presence',
