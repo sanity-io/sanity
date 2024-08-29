ffa60c1bab328f0964a33af5fec850ace93a7e76
Jordan Lawrence <jordanl17@me.com>
2024-08-29 14:20:16 +0100

    feat(sanity): add always present document action to copy url to clipboard (#7416)
    * feat: add always present document action to copy url to clipboard
    
    * chore: add comment to menuitems
    
    * feat: add check for navigator presence
    
    * feat: add tracking for copy document URL
    
    * fix: update to use intent URL instead of just current URL
    
    * feat: resolve intent link on demand
    
    * revert: go back to just copying current URL
    
    * chore: add comment explanation

--- packages/sanity/src/structure/i18n/resources.ts
+++ packages/sanity/src/structure/i18n/resources.ts
@@ -351,7 +351,7 @@ const structureLocaleStrings = defineLocalesResources('structure', {
   /** The text when a generic operation succeeded (fallback, generally not shown)  */
   'panes.document-operation-results.operation-success': 'Successfully performed {{op}} on document',
   /** The text when copy URL operation succeeded  */
-  'panes.document-operation-results.operation-success_copy-url': 'Document URL copied to clipboard',  
+  'panes.document-operation-results.operation-success_copy-url': 'Document URL copied to clipboard',
   /** The text when a delete operation succeeded  */
   'panes.document-operation-results.operation-success_delete':
     'The document was successfully deleted',
