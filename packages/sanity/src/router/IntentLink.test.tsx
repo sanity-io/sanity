ca0568bc41b32468e3750455b0ff0e31dbdf45ad
Ash <ash@sanity.io>
2024-08-29 16:14:04 +0100

    feat(sanity): preserve current sticky parameters when resolving intent links

--- packages/sanity/src/router/IntentLink.test.tsx
+++ packages/sanity/src/router/IntentLink.test.tsx
@@ -1,5 +1,6 @@
 import {describe, expect, it} from '@jest/globals'
 import {render} from '@testing-library/react'
+import {noop} from 'lodash'
 
 import {IntentLink} from './IntentLink'
 import {route} from './route'
@@ -28,6 +29,70 @@ describe('IntentLink', () => {
     // Component should render the query param in the href
     expect(component.container.querySelector('a')?.href).toContain(
       '/test/intent/edit/id=document-id-123;type=document-type/?perspective=bundle.summer-drop',
+    )
+  })
+
+  it('should preserve sticky parameters when resolving intent link', () => {
+    const router = route.create('/test', [route.intents('/intent')])
+    const component = render(
+      <IntentLink
+        intent="edit"
+        params={{
+          id: 'document-id-123',
+          type: 'document-type',
+        }}
+      />,
+      {
+        wrapper: ({children}) => (
+          <RouterProvider
+            onNavigate={noop}
+            router={router}
+            state={{
+              _searchParams: [['perspective', 'bundle.summer-drop']],
+            }}
+          >
+            {children}
+          </RouterProvider>
+        ),
+      },
+    )
+    // Component should render the query param in the href
+    expect(component.container.querySelector('a')?.href).toContain(
+      '/test/intent/edit/id=document-id-123;type=document-type/?perspective=bundle.summer-drop',
+    )
+  })
+
+  it('should allow sticky parameters to be overridden when resolving intent link', () => {
+    const router = route.create('/test', [route.intents('/intent')])
+    const component = render(
+      <IntentLink
+        intent="edit"
+        params={{
+          id: 'document-id-123',
+          type: 'document-type',
+        }}
+        searchParams={[['perspective', `bundle.autumn-drop`]]}
+      />,
+      {
+        wrapper: ({children}) => (
+          <RouterProvider
+            onNavigate={noop}
+            router={router}
+            state={{
+              _searchParams: [['perspective', 'bundle.summer-drop']],
+            }}
+          >
+            {children}
+          </RouterProvider>
+        ),
+      },
+    )
+    // Component should render the query param in the href
+    expect(component.container.querySelector('a')?.href).toContain(
+      '/test/intent/edit/id=document-id-123;type=document-type/?perspective=bundle.autumn-drop',
+    )
+    expect(component.container.querySelector('a')?.href).not.toContain(
+      'perspective=bundle.summer-drop',
     )
   })
 })
