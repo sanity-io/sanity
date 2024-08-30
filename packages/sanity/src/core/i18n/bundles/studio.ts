eae214cd6f47201f9e1e4e3cc362ead96aba2cc9
RitaDias <anarita151@gmail.com>
2024-08-30 14:12:28 +0200

    feat(sanity): add left side information on footer (revision status line)

--- packages/sanity/src/core/i18n/bundles/studio.ts
+++ packages/sanity/src/core/i18n/bundles/studio.ts
@@ -376,7 +376,7 @@ export const studioLocaleStrings = defineLocalesResources('studio', {
   /** Label to show in the document footer indicating the published date of the document */
   'document-status.published': 'Published {{date}}',
   /** Label to show in the document footer indicating the revision from date of the document */
-  'document-status.revision-from': 'Revision from {{date}}',
+  'document-status.revision-from': 'Revision from <em>{{date}}</em>',
 
   /** The value of the <code>_key</code> property must be a unique string. */
   'form.error.duplicate-keys-alert.details.additional-description':
