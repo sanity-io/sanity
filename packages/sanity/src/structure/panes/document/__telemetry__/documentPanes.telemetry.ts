e6b0614c0b65ceafbcdf97ee92ef7006aaf4e19d
RitaDias <rita@sanity.io>
2024-09-05 11:51:32 +0200

    feat(sanity): add telemetry when creating a draft (#7459)

--- packages/sanity/src/structure/panes/document/__telemetry__/documentPanes.telemetry.ts
+++ packages/sanity/src/structure/panes/document/__telemetry__/documentPanes.telemetry.ts
@@ -8,3 +8,13 @@ export const DocumentURLCopied = defineEvent({
   version: 1,
   description: 'User copied document URL to clipboard',
 })
+
+/**
+ * When a draft is successfully created
+ * @internal
+ */
+export const CreatedDraft = defineEvent({
+  name: 'Create a new draft',
+  version: 1,
+  description: 'User created a new draft',
+})
