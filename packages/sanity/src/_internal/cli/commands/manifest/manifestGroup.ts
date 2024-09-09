4bad06ecb145ff39b0481660ea86d1625d9ed2e7
Snorre Eskeland Brekke <snorre.e.brekke@gmail.com>
2024-09-09 13:29:58 +0200

    chore: wording change

--- packages/sanity/src/_internal/cli/commands/manifest/manifestGroup.ts
+++ packages/sanity/src/_internal/cli/commands/manifest/manifestGroup.ts
@@ -2,5 +2,5 @@ export default {
   name: 'manifest',
   signature: '[COMMAND]',
   isGroupRoot: true,
-  description: 'Extracts the studio configuration as one more JSON manifest files.',
+  description: 'Interacts with the studio configuration.',
 }
