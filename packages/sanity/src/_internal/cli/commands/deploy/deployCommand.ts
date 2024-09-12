8b0eeff8c38b7512de2da00e75183e96bab0deef
Carolina Gonzalez <carolina@sanity.io>
2024-09-12 13:31:51 -0400

    chore: add help text for auto-updates in the studio (#7461)

--- packages/sanity/src/_internal/cli/commands/deploy/deployCommand.ts
+++ packages/sanity/src/_internal/cli/commands/deploy/deployCommand.ts
@@ -9,6 +9,7 @@ import {type DeployStudioActionFlags} from '../../actions/deploy/deployAction'
 const helpText = `
 Options
   --source-maps Enable source maps for built bundles (increases size of bundle)
+  --auto-updates Enable auto updates of studio versions
   --no-minify Skip minifying built JavaScript (speeds up build, increases size of bundle)
   --no-build Don't build the studio prior to deploy, instead deploying the version currently in \`dist/\`
 
