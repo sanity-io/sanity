450f606d9eb7db8cd62106785f71703b4907da66
pedrobonamin <pedrobonamin@gmail.com>
2024-09-12 17:48:27 +0200

    feat(core): add studio announcements telemetry events

--- packages/sanity/src/core/studio/studioAnnouncements/StudioAnnouncements.tsx
+++ packages/sanity/src/core/studio/studioAnnouncements/StudioAnnouncements.tsx
@@ -1,18 +1,26 @@
 import {createClient} from '@sanity/client'
+import {useTelemetry} from '@sanity/telemetry/react'
 import {useCallback, useEffect, useMemo, useState} from 'react'
 import {SANITY_VERSION} from 'sanity'
 
+import {
+  StudioAnnouncementCardClicked,
+  StudioAnnouncementCardDismissed,
+  StudioAnnouncementCardSeen,
+  StudioAnnouncementModalDismissed,
+} from './__telemetry__/studioAnnouncements.telemetry'
 import {studioAnnouncementQuery} from './query'
 import {StudioAnnouncementCard} from './StudioAnnouncementCard'
 import {StudioAnnouncementDialog} from './StudioAnnouncementDialog'
 import {type StudioAnnouncementDocument} from './types'
 import {isValidAudience} from './utils'
-
 /**
  * @internal
  * @hidden
  */
 export function StudioAnnouncements() {
+  const telemetry = useTelemetry()
+
   const [isDialogOpen, setIsDialogOpen] = useState(false)
   const [isCardOpen, setIsCardOpen] = useState(false)
   const [unseenDocuments, setUnseenDocuments] = useState<StudioAnnouncementDocument[]>([])
@@ -40,27 +48,36 @@ export function StudioAnnouncements() {
         setUnseenDocuments(unseen)
         if (unseen.length > 0) {
           setIsCardOpen(true)
+          telemetry.log(StudioAnnouncementCardSeen)
         }
       })
-  }, [client])
+  }, [client, telemetry])
 
+  /**
+   * TODO: Remove this before merging
+   */
   useEffect(() => {
     if (!isCardOpen) {
       setTimeout(() => setIsCardOpen(true), 1000)
     }
   }, [isCardOpen])
 
-  const handleCardClose = useCallback(() => {
+  const handleCardDismiss = useCallback(() => {
     setIsCardOpen(false)
-  }, [])
+    telemetry.log(StudioAnnouncementCardDismissed)
+  }, [telemetry])
+
   const handleCardClick = useCallback(() => {
     setIsDialogOpen(true)
     setIsCardOpen(false)
-  }, [])
+    telemetry.log(StudioAnnouncementCardClicked)
+  }, [telemetry])
 
   const handleDialogClose = useCallback(() => {
     setIsDialogOpen(false)
-  }, [])
+    telemetry.log(StudioAnnouncementModalDismissed)
+  }, [telemetry])
+
   if (!unseenDocuments.length) return null
   return (
     <>
@@ -69,7 +86,7 @@ export function StudioAnnouncements() {
         announcementType={unseenDocuments[0].announcementType}
         onCardClick={handleCardClick}
         isOpen={isCardOpen}
-        onCardClose={handleCardClose}
+        onCardDismiss={handleCardDismiss}
       />
       {isDialogOpen && (
         <StudioAnnouncementDialog unseenDocuments={unseenDocuments} onClose={handleDialogClose} />
