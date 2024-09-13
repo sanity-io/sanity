d30f9b03795bdf4f8586e879a77e2422a14c564b
Ash <ash@sanity.io>
2024-09-13 11:32:34 +0100

    feat(sanity): move perspective resolution to `resolvePerspective` function

---
+++ packages/sanity/src/core/util/resolvePerspective.ts
@@ -0,0 +1,42 @@
+/**
+ * If the provided perspective has the `bundle.` prefix, returns it with this
+ * prefix removed.
+ *
+ * @internal
+ */
+export function resolveBundlePerspective(perspective?: string): string | undefined {
+  return perspective?.split(/^bundle./).at(1)
+}
+
+/**
+ * Given a system perspective, or a bundle name prefixed with `bundle.`, returns
+ * an object with either `perspective` or `bundlePerspective` properties that
+ * may be submitted directly to Content Lake APIs.
+ *
+ * @internal
+ */
+export function resolvePerspectiveOptions(
+  perspective: string | undefined,
+  transformPerspectives: (perspectives: string[], isSystemPerspective: boolean) => string[] = (
+    perspectives,
+  ) => perspectives,
+):
+  | {perspective: string; bundlePerspective?: never}
+  | {perspective?: never; bundlePerspective: string}
+  | Record<PropertyKey, never> {
+  if (typeof perspective === 'undefined') {
+    return {}
+  }
+
+  const bundlePerspective = resolveBundlePerspective(perspective)
+
+  if (typeof bundlePerspective === 'string') {
+    return {
+      bundlePerspective: transformPerspectives([bundlePerspective], false).join(','),
+    }
+  }
+
+  return {
+    perspective: transformPerspectives([perspective], true).join(','),
+  }
+}
