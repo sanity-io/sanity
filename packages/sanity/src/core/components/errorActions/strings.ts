7809762b794c6b8eb6233e0292febe84ed03ac2a
Ash <ash@sanity.io>
2024-09-18 14:39:24 +0100

    feat(sanity): add `ErrorActions` component

---
+++ packages/sanity/src/core/components/errorActions/strings.ts
@@ -0,0 +1,10 @@
+// These strings are not internationalized because `ErrorActions` is used inside
+// `StudioErrorBoundary`, which is rendered outside of `LocaleProvider`.
+export const strings = {
+  'retry.title': 'Retry',
+  'copy-error-details.description': 'These technical details may be useful for developers.',
+  'copy-error-details.title': 'Copy error details',
+  'copy-error-details.toast.get-failed': 'Failed to get error details',
+  'copy-error-details.toast.copy-failed': 'Failed to copy error details',
+  'copy-error-details.toast.succeeded': 'Copied error details to clipboard',
+} as const
