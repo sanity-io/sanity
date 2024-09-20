203b71dee8e4f061efab8bd32c5fd14efcd9dc36
Bjørge Næss <bjoerge@gmail.com>
2024-09-20 21:15:05 +0200

    chore(test): move `@sanity/mutator` to vitest

--- vitest.workspace.ts
+++ vitest.workspace.ts
@@ -1,3 +1,7 @@
 import {defineWorkspace} from 'vitest/config'
 
-export default defineWorkspace(['packages/@sanity/migrate', 'packages/@sanity/block-tools'])
+export default defineWorkspace([
+  'packages/@sanity/migrate',
+  'packages/@sanity/block-tools',
+  'packages/@sanity/mutator',
+])
