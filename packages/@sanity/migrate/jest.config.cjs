da26bdac3752268e147cda14a317ea7f7a7fb054
Bjørge Næss <bjoerge@gmail.com>
2024-09-20 17:11:18 +0200

    chore(test): move `@sanity/migrate` to vitest

--- packages/@sanity/migrate/jest.config.cjs
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-const {createJestConfig} = require('../../../test/config.cjs')
-
-module.exports = createJestConfig({
-  displayName: require('./package.json').name,
-  testEnvironment: 'node',
-  transformIgnorePatterns: ['node_modules/(?!(uint8array-extras)/)'],
-})
--- packages/@sanity/migrate/src/mutations/__tests__/mutate.test.ts
+++ packages/@sanity/migrate/src/mutations/__tests__/mutate.test.ts
@@ -1,4 +1,4 @@
-import {expect, test} from '@jest/globals'
+import {expect, test} from 'vitest'
 
 import {at, create, createIfNotExists, createOrReplace, del, patch} from '../creators'
 import {inc, insert, set, setIfMissing, unset} from '../operations/creators'
