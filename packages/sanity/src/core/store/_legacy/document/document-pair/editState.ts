f97b30e70ee35d48a2c4d2fea69b7779137fcc53
pedrobonamin <pedrobonamin@gmail.com>
2024-09-23 11:09:13 +0200

    feat(core): preserve visited documents in document-store

--- packages/sanity/src/core/store/_legacy/document/document-pair/editState.ts
+++ packages/sanity/src/core/store/_legacy/document/document-pair/editState.ts
@@ -1,7 +1,7 @@
 import {type SanityClient} from '@sanity/client'
 import {type SanityDocument, type Schema} from '@sanity/types'
 import {combineLatest, type Observable} from 'rxjs'
-import {map, publishReplay, refCount, startWith, switchMap} from 'rxjs/operators'
+import {map, publishReplay, refCount, startWith, switchMap, take} from 'rxjs/operators'
 
 import {type IdPair, type PendingMutationsEvent} from '../types'
 import {memoize} from '../utils/createMemoizer'
@@ -38,36 +38,49 @@ export const editState = memoize(
     },
     idPair: IdPair,
     typeName: string,
+    visited$: Observable<(SanityDocument | undefined)[]>,
   ): Observable<EditStateFor> => {
     const liveEdit = isLiveEditEnabled(ctx.schema, typeName)
-    return snapshotPair(ctx.client, idPair, typeName, ctx.serverActionsEnabled).pipe(
-      switchMap((versions) =>
-        combineLatest([
-          versions.draft.snapshots$,
-          versions.published.snapshots$,
-          versions.transactionsPendingEvents$.pipe(
-            map((ev: PendingMutationsEvent) => (ev.phase === 'begin' ? LOCKED : NOT_LOCKED)),
-            startWith(NOT_LOCKED),
+    return visited$.pipe(
+      take(1),
+      map((visited) => {
+        return {
+          draft: visited.find((doc) => doc?._id === idPair.draftId) || null,
+          published: visited.find((doc) => doc?._id === idPair.publishedId) || null,
+        }
+      }),
+      switchMap((visitedPair) => {
+        return snapshotPair(ctx.client, idPair, typeName, ctx.serverActionsEnabled).pipe(
+          switchMap((versions) =>
+            combineLatest([
+              versions.draft.snapshots$,
+              versions.published.snapshots$,
+              versions.transactionsPendingEvents$.pipe(
+                // eslint-disable-next-line max-nested-callbacks
+                map((ev: PendingMutationsEvent) => (ev.phase === 'begin' ? LOCKED : NOT_LOCKED)),
+                startWith(NOT_LOCKED),
+              ),
+            ]),
           ),
-        ]),
-      ),
-      map(([draftSnapshot, publishedSnapshot, transactionSyncLock]) => ({
-        id: idPair.publishedId,
-        type: typeName,
-        draft: draftSnapshot,
-        published: publishedSnapshot,
-        liveEdit,
-        ready: true,
-        transactionSyncLock,
-      })),
-      startWith({
-        id: idPair.publishedId,
-        type: typeName,
-        draft: null,
-        published: null,
-        liveEdit,
-        ready: false,
-        transactionSyncLock: null,
+          map(([draftSnapshot, publishedSnapshot, transactionSyncLock]) => ({
+            id: idPair.publishedId,
+            type: typeName,
+            draft: draftSnapshot,
+            published: publishedSnapshot,
+            liveEdit,
+            ready: true,
+            transactionSyncLock,
+          })),
+          startWith({
+            id: idPair.publishedId,
+            type: typeName,
+            draft: visitedPair.draft,
+            published: visitedPair.published,
+            liveEdit,
+            ready: false,
+            transactionSyncLock: null,
+          }),
+        )
       }),
       publishReplay(1),
       refCount(),
