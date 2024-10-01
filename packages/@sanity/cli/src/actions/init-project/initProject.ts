ce923275d2b8683583219af0833e5c9792c9d971
svirs <shur@sanity.io>
2024-10-01 03:11:41 +0200

    wrap in Promise.allSettled

--- packages/@sanity/cli/src/actions/init-project/initProject.ts
+++ packages/@sanity/cli/src/actions/init-project/initProject.ts
@@ -594,26 +594,31 @@ export default async function initSanity(
 
   trace.log({step: 'importTemplateDataset', selectedOption: shouldImport ? 'yes' : 'no'})
 
-  // record that template files attempted to be created locally
-  await apiClient({api: {projectId: projectId}})
-    .request<SanityProject>({uri: `/projects/${projectId}`})
-    .then((project: SanityProject) => {
-      if (!project?.metadata?.cliInitializedAt) {
-        return apiClient({api: {projectId}}).request({
-          method: 'PATCH',
-          uri: `/projects/${projectId}`,
-          body: {metadata: {cliInitializedAt: new Date().toISOString()}},
-        })
-      }
-      return Promise.resolve()
-    })
-    .catch(() => {
-      // Non-critical update
-      debug('Failed to update cliInitializedAt metadata')
-    })
+  const [_, bootstrapPromise] = await Promise.allSettled([
+    // record template files attempted to be created locally
+    apiClient({api: {projectId: projectId}})
+      .request<SanityProject>({uri: `/projects/${projectId}`})
+      .then((project: SanityProject) => {
+        if (!project?.metadata?.cliInitializedAt) {
+          return apiClient({api: {projectId}}).request({
+            method: 'PATCH',
+            uri: `/projects/${projectId}`,
+            body: {metadata: {cliInitializedAt: new Date().toISOString()}},
+          })
+        }
+        return Promise.resolve()
+      })
+      .catch(() => {
+        // Non-critical update
+        debug('Failed to update cliInitializedAt metadata')
+      }),
+    // Bootstrap Sanity, creating required project files, manifests etc
+    bootstrapTemplate(templateOptions, context),
+  ])
 
-  // Bootstrap Sanity, creating required project files, manifests etc
-  await bootstrapTemplate(templateOptions, context)
+  if (bootstrapPromise.status === 'rejected' && bootstrapPromise.reason instanceof Error) {
+    throw bootstrapPromise.reason
+  }
 
   let pkgManager: PackageManager
 
