aeb9e71ee778850542dedd9340149dc61d9107d5
Rico Kahler <ricokahler@gmail.com>
2024-10-01 16:34:03 -0500

    test: add efps to CI  (#7556)
    * test: revamp eFPS suite
    
    * chore: add efps github actions
    
    * chore: use different token for efps tests
    
    * test: add comment with perf report result
    
    * test: keep playwright install script
    
    * feat: add workflow dispatch inputs
    
    * fix: use `requestAnimationFrame` instead
    
    ---------
    
    Co-authored-by: Binoy Patel <me@binoy.io>

--- perf/efps/helpers/aggregateLatencies.ts
+++ perf/efps/helpers/aggregateLatencies.ts
@@ -1,4 +1,6 @@
-export function calculatePercentile(numbers: number[], percentile: number): number {
+import {type EfpsResult} from '../types'
+
+function calculatePercentile(numbers: number[], percentile: number): number {
   // Sort the array in ascending order
   const sorted = numbers.slice().sort((a, b) => a - b)
 
@@ -18,4 +20,13 @@ export function calculatePercentile(numbers: number[], percentile: number): numb
 
   const fraction = index - lowerIndex
   return lowerValue + (upperValue - lowerValue) * fraction
+}
+
+export function aggregateLatencies(values: number[]): EfpsResult['latency'] {
+  return {
+    p50: calculatePercentile(values, 0.5),
+    p75: calculatePercentile(values, 0.75),
+    p90: calculatePercentile(values, 0.9),
+    p99: calculatePercentile(values, 0.99),
+  }
 }
