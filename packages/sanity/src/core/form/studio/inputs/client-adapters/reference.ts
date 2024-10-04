53020c1236a3f64c49e3c126259550d61e877f12
RitaDias <rita@sanity.io>
2024-10-04 10:33:05 +0200

    refactor(sanity): fix type issues with collate, update tests and readQueryResult (#7587)
    ### Description
    
    - Re-adds `QueryResult` interface
    - Changing the type in the collate to not explode when versions are
    present.
    
    When rebasing against `next` something went wrong specifically around
    this [PR](https://github.com/sanity-io/sanity/pull/7555/files). I wasn't
    able to fix it by using rebasing however this unblocks us and allows us
    to continue work.
    
    I imagine that the fix will likely be more around making sure that the
    collate prop type stayed the same (making the changes in the
    ENABLE_LRU_MEMO changes however I don't know the larger impact of making
    that change, so, for now I've set this up.
    
    Folks that are more knowledgeable around this should weight in before I
    merge since this is more like a temporary bandaid than a fix 🥲
    
    Ash set up a branch `corel-pre-rebase-20241003` in case if you want to
    give the rebase another stab to avoid some of these issues. Keep in mind
    that you'll need to cherry pick
    [StickParams](https://github.com/sanity-io/sanity/pull/7429) commits as
    well

--- packages/sanity/src/core/form/studio/inputs/client-adapters/reference.ts
+++ packages/sanity/src/core/form/studio/inputs/client-adapters/reference.ts
@@ -243,7 +243,7 @@ export function referenceSearch(
   })
   return search(textTerm, {includeDrafts: true}).pipe(
     map(({hits}) => hits.map(({hit}) => hit)),
-    map((docs) => collate(docs)),
+    map((docs) => collate({documents: docs})),
     // pick the 100 best matches
     map((collated) => collated.slice(0, 100)),
     mergeMap((collated) => {
--- packages/sanity/src/core/util/draftUtils.test.ts
+++ packages/sanity/src/core/util/draftUtils.test.ts
@@ -1,5 +1,5 @@
 import {describe, expect, it, test} from '@jest/globals'
-import {type SanityDocument} from '@sanity/types'
+import {type SanityDocument, type SanityDocumentLike} from '@sanity/types'
 
 import {
   collate,
@@ -11,12 +11,12 @@ import {
 } from './draftUtils'
 
 test('collate()', () => {
-  const foo = {_type: 'foo', _id: 'foo'}
-  const fooDraft = {_type: 'foo', _id: 'drafts.foo'}
-  const barDraft = {_type: 'foo', _id: 'drafts.bar'}
-  const baz = {_type: 'foo', _id: 'baz'}
+  const foo = {_type: 'foo', _id: 'foo'} as SanityDocumentLike
+  const fooDraft = {_type: 'foo', _id: 'drafts.foo'} as SanityDocumentLike
+  const barDraft = {_type: 'foo', _id: 'drafts.bar'} as SanityDocumentLike
+  const baz = {_type: 'foo', _id: 'baz'} as SanityDocumentLike
 
-  expect(collate([foo, fooDraft, barDraft, baz])).toEqual([
+  expect(collate({documents: [foo, fooDraft, barDraft, baz]})).toEqual([
     {type: 'foo', id: 'foo', draft: fooDraft, published: foo},
     {type: 'foo', id: 'bar', draft: barDraft},
     {type: 'foo', id: 'baz', published: baz},
