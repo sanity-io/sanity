7a0eb013213de17b9adebd9711018cdb0dd1ccf9
Bjørge Næss <bjoerge@gmail.com>
2024-10-08 12:15:48 +0200

    fixup! feat: propagate `PairListenerOptions`; add telemetry for `OutOfSyncError` errors

--- packages/sanity/src/core/store/_legacy/document/__telemetry__/documentOutOfSyncEvents.telemetry.ts
+++ packages/sanity/src/core/store/_legacy/document/__telemetry__/documentOutOfSyncEvents.telemetry.ts
@@ -1,7 +1,7 @@
 import {defineEvent} from '@sanity/telemetry'
 
 export const DocumentOutOfSync = defineEvent<{errorName: string}>({
-  name: 'Document out of sync',
+  name: 'Document Desynced',
   version: 1,
   description: 'Occurs when a "hole" in events from the document pair listener is detected.',
 })
