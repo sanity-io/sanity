16cff2b7f536e40720438a47d5580085df3c62c7
Rita <anarita151@gmail.com>
2024-10-14 09:42:20 +0200

    chore(sanity): remove unused imports

--- packages/sanity/src/structure/panes/document/documentPanel/header/perspective/DocumentPerspectiveMenu.tsx
+++ packages/sanity/src/structure/panes/document/documentPanel/header/perspective/DocumentPerspectiveMenu.tsx
@@ -1,21 +1,12 @@
-import {ChevronDownIcon, DotIcon} from '@sanity/icons'
+import {DotIcon} from '@sanity/icons'
 // eslint-disable-next-line no-restricted-imports -- Bundle Button requires more fine-grained styling than studio button
 import {Text} from '@sanity/ui'
 // eslint-disable-next-line camelcase
-import {getTheme_v2} from '@sanity/ui/theme'
-import {memo, useCallback, useMemo} from 'react'
-import {
-  type BundleDocument,
-  getVersionFromId,
-  ReleaseBadge,
-  useDateTimeFormat,
-  usePerspective,
-  useTranslation,
-} from 'sanity'
-import {IntentLink} from 'sanity/router'
-import {css, styled} from 'styled-components'
+import {memo, useCallback} from 'react'
+import {getVersionFromId, useDateTimeFormat, usePerspective, useTranslation} from 'sanity'
+import {styled} from 'styled-components'
 
-import {Button, Button as StudioButton, Tooltip} from '../../../../../../ui-components'
+import {Button, Tooltip} from '../../../../../../ui-components'
 import {usePaneRouter} from '../../../../../components'
 import {useDocumentPane} from '../../../useDocumentPane'
 
@@ -31,64 +22,16 @@ const Chip = styled(Button)`
   }
 `
 
-const BadgeButton = styled(Button)((props) => {
-  const theme = getTheme_v2(props.theme)
-  const mode = props.mode || 'default'
-  const tone = props.tone || 'default'
-  const color = theme.color.button[mode][tone]
-
-  return css`
-    cursor: pointer;
-    border-radius: 999px !important;
-    @media (hover: hover) {
-      &:hover {
-        text-decoration: none !important;
-        background-color: ${color.hovered.bg};
-      }
-    }
-  `
-})
-
-const ReleaseLink = ({release}: {release: Partial<BundleDocument>}) => {
-  const {hue, title, icon, _id: releaseId} = release
-
-  return (
-    <BadgeButton
-      mode="bleed"
-      radius="full"
-      data-testid="button-document-release"
-      intent="release"
-      params={{id: releaseId}}
-      target="_blank"
-      rel="noopener noreferrer"
-      as={IntentLink}
-    >
-      <ReleaseBadge hue={hue} title={title} icon={icon} padding={2} />
-    </BadgeButton>
-  )
-}
-
 export const DocumentPerspectiveMenu = memo(function DocumentPerspectiveMenu() {
   const paneRouter = usePaneRouter()
-  const {t} = useTranslation()
+  const {t} = useTranslation() // @todo add and update translations
   const {currentGlobalBundle, setPerspective} = usePerspective(paneRouter.perspective)
   const dateTimeFormat = useDateTimeFormat({
     dateStyle: 'medium',
     timeStyle: 'short',
   })
 
-  const {documentVersions, existsInBundle, editState, displayed, ...other} = useDocumentPane()
-
-  const releasesMenuButton = useMemo(
-    () => (
-      <StudioButton
-        tooltipProps={{content: t('release.version-list.tooltip')}}
-        icon={ChevronDownIcon}
-        mode="bleed"
-      />
-    ),
-    [t],
-  )
+  const {documentVersions, existsInBundle, editState, displayed} = useDocumentPane()
 
   const handleBundleChange = useCallback(
     (bundleId: string) => () => {
