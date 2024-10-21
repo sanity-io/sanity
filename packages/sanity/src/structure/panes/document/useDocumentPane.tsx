aba2ae5f17761766acdb000b71142f788211e43b
Binoy Patel <me@binoy.io>
2024-10-21 12:58:41 -0400

    test(core): use context selector for DocumentPaneProvider

--- packages/sanity/src/structure/panes/document/useDocumentPane.tsx
+++ packages/sanity/src/structure/panes/document/useDocumentPane.tsx
@@ -1,15 +1,17 @@
-import {useContext} from 'react'
 import {DocumentPaneContext} from 'sanity/_singletons'
+import {type Context, useContextSelector} from 'use-context-selector'
 
 import {type DocumentPaneContextValue} from './DocumentPaneContext'
 
 /** @internal */
-export function useDocumentPane(): DocumentPaneContextValue {
-  const documentPane = useContext(DocumentPaneContext)
-
-  if (!documentPane) {
-    throw new Error('DocumentPane: missing context value')
-  }
+export function useDocumentPane<T = DocumentPaneContextValue>(
+  selector?: (value: DocumentPaneContextValue) => T,
+): T {
+  const selectorFn = selector || ((value: DocumentPaneContextValue) => value as T)
+  const documentPane = useContextSelector<DocumentPaneContextValue, T>(
+    DocumentPaneContext as Context<DocumentPaneContextValue>,
+    selectorFn,
+  )
 
   return documentPane
 }
