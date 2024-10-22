0c70b9a21b0dedeaf9269b7d017d72248a576c6c
Snorre Eskeland Brekke <snorre.e.brekke@gmail.com>
2024-10-22 22:27:23 +0200

    chore: fetchCreateCompatibleAppId tests

--- packages/sanity/src/core/create/studio-app/useStudioAppIdStore.ts
+++ packages/sanity/src/core/create/studio-app/useStudioAppIdStore.ts
@@ -1,29 +1,13 @@
 import {useEffect, useState} from 'react'
-import {type SanityClient, useClient} from 'sanity'
+import {useClient} from 'sanity'
 
 import {useSanityCreateConfig} from '../context/useSanityCreateConfig'
 import {type AppIdCache} from './appIdCache'
-import {getStudioManifest} from './manifest'
-
-// @ts-expect-error: __SANITY_STAGING__ is a global env variable set by the vite config
-const isStaging = typeof __SANITY_STAGING__ !== 'undefined' && __SANITY_STAGING__ === true
-const internalUrlSuffix = isStaging ? 'studio.sanity.work' : 'sanity.studio'
+import {fetchCreateCompatibleAppId} from './fetchCreateCompatibleAppId'
 
 export interface ResolvedStudioAppId {
   loading: boolean
   appId?: string
-}
-
-interface StudioAppResponse {
-  id: string
-  title?: string
-  type: 'studio' | string
-  urlType: 'internal' | 'external' | string
-  appHost: string
-}
-
-export function getStudioUrl(app: StudioAppResponse): string {
-  return app.urlType === 'internal' ? `https://${app.appHost}.${internalUrlSuffix}` : app.appHost
 }
 
 /**
@@ -50,7 +34,12 @@ export function useStudioAppIdStore(cache: AppIdCache): ResolvedStudioAppId {
       try {
         const entry = await cache.get({
           projectId,
-          appIdFetcher: (id) => fetchCreateCompatibleAppId(id, client, config.fallbackStudioOrigin),
+          appIdFetcher: (id) =>
+            fetchCreateCompatibleAppId({
+              projectId: id,
+              client,
+              fallbackOrigin: config.fallbackStudioOrigin,
+            }),
         })
         if (mounted) setAppId(entry?.appId)
       } catch (err) {
@@ -73,35 +62,4 @@ export function useStudioAppIdStore(cache: AppIdCache): ResolvedStudioAppId {
     loading,
     appId,
   }
-}
-
-export async function fetchCreateCompatibleAppId(
-  projectId: string,
-  client: SanityClient,
-  fallbackOrigin?: string,
-): Promise<string | undefined> {
-  const apps = (await client.request({
-    method: 'GET',
-    url: `/projects/${projectId}/user-applications`,
-  })) as StudioAppResponse[]
-
-  const origin = window.location?.origin
-
-  const appsWithStudioUrl = apps.map((app) => ({...app, studioUrl: getStudioUrl(app)}))
-  const appMatchingOrigin = appsWithStudioUrl.find((app) => {
-    return app && origin && app.studioUrl?.startsWith(origin)
-  })
-
-  if (appMatchingOrigin?.appHost) {
-    const manifest = await getStudioManifest(getStudioUrl(appMatchingOrigin))
-    if (manifest) {
-      return appMatchingOrigin.id
-    }
-  }
-
-  const appMatchingFallback = appsWithStudioUrl.find((app) => {
-    return app && fallbackOrigin && app.studioUrl?.startsWith(`https://${fallbackOrigin}`)
-  })
-
-  return appMatchingFallback?.id
 }
