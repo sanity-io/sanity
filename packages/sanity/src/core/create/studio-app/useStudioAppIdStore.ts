3b0a450fa5f8a58c76a6d66618faa9ee144b4881
Snorre Eskeland Brekke <snorre.e.brekke@gmail.com>
2024-10-22 22:59:01 +0200

    chore: useStudioAppIdStore tests

--- packages/sanity/src/core/create/studio-app/useStudioAppIdStore.ts
+++ packages/sanity/src/core/create/studio-app/useStudioAppIdStore.ts
@@ -1,8 +1,8 @@
-import {useEffect, useState} from 'react'
-import {useClient} from 'sanity'
+import {useEffect, useMemo, useState} from 'react'
+import {useClient, useSource} from 'sanity'
 
 import {useSanityCreateConfig} from '../context/useSanityCreateConfig'
-import {type AppIdCache} from './appIdCache'
+import {type AppIdCache, type AppIdFetcher} from './appIdCache'
 import {fetchCreateCompatibleAppId} from './fetchCreateCompatibleAppId'
 
 export interface ResolvedStudioAppId {
@@ -16,31 +16,48 @@ export interface ResolvedStudioAppId {
  * @internal
  */
 export function useStudioAppIdStore(cache: AppIdCache): ResolvedStudioAppId {
-  const [loading, setLoading] = useState(false)
   const client = useClient({apiVersion: '2024-09-01'})
   const config = useSanityCreateConfig()
+  const {projectId} = useSource()
+
+  const appIdFetcher: AppIdFetcher = useMemo(() => {
+    return (pId) =>
+      fetchCreateCompatibleAppId({
+        projectId: pId,
+        client,
+        fallbackOrigin: config.fallbackStudioOrigin,
+      })
+  }, [client, config.fallbackStudioOrigin])
+
+  return useStudioAppIdStoreInner({
+    projectId,
+    cache,
+    appIdFetcher,
+    enabled: config.startInCreateEnabled,
+  })
+}
+
+export function useStudioAppIdStoreInner(props: {
+  cache: AppIdCache
+  projectId: string
+  enabled: boolean
+  appIdFetcher: AppIdFetcher
+}): ResolvedStudioAppId {
+  const {cache, enabled, appIdFetcher, projectId} = props
+  const [loading, setLoading] = useState(false)
 
   const [appId, setAppId] = useState<string | undefined>()
 
   useEffect(() => {
     let mounted = true
     async function getAppId() {
-      const projectId = client.config().projectId
       if (!projectId) {
         return
       }
       setLoading(true)
 
       try {
-        const entry = await cache.get({
-          projectId,
-          appIdFetcher: (id) =>
-            fetchCreateCompatibleAppId({
-              projectId: id,
-              client,
-              fallbackOrigin: config.fallbackStudioOrigin,
-            }),
-        })
+        const entry = await cache.get({projectId, appIdFetcher})
         if (mounted) setAppId(entry?.appId)
       } catch (err) {
         if (mounted) setAppId(undefined)
@@ -49,14 +66,14 @@ export function useStudioAppIdStore(cache: AppIdCache): ResolvedStudioAppId {
       }
     }
 
-    if (config.startInCreateEnabled) {
+    if (enabled) {
       getAppId().catch(console.error)
     }
     return () => {
       mounted = false
       setLoading(false)
     }
-  }, [setLoading, client, cache, config])
+  }, [setLoading, appIdFetcher, cache, enabled, projectId])
 
   return {
     loading,
