19a622b777927a5ebe7595cf2f1e2d2a55e87c78
Jordan Lawrence <jordanl17@me.com>
2024-10-25 02:50:42 +0100

    feat: animating the releases nav, to spice things up

--- packages/sanity/src/core/releases/navbar/ReleasesNav.tsx
+++ packages/sanity/src/core/releases/navbar/ReleasesNav.tsx
@@ -1,6 +1,7 @@
 import {CalendarIcon, CloseIcon} from '@sanity/icons'
 // eslint-disable-next-line no-restricted-imports -- Bundle Button requires more fine-grained styling than studio button
 import {Box, Button, Card, Flex, Stack, Text} from '@sanity/ui'
+import {AnimatePresence, motion} from 'framer-motion'
 import {type PropsWithChildren, useCallback, useMemo} from 'react'
 import {useTranslation} from 'react-i18next'
 import {IntentLink, useRouterState} from 'sanity/router'
@@ -78,26 +79,33 @@ export function ReleasesNav(): JSX.Element {
       : 'default'
 
     return (
-      <Button
-        as={releasesIntentLink}
-        data-as="a"
-        rel="noopener noreferrer"
-        mode="bleed"
-        padding={0}
-        radius="full"
-        style={{maxWidth: '180px'}}
+      <motion.div
+        initial={{width: 0, opacity: 0}}
+        animate={{width: 'auto', opacity: 1}}
+        exit={{width: 0, opacity: 0}}
+        transition={{duration: 0.25, ease: 'easeInOut'}}
       >
-        <Flex align="flex-start" gap={0}>
-          <Box flex="none">
-            <ReleaseAvatar padding={2} tone={tone} />
-          </Box>
-          <Stack flex={1} paddingY={2} paddingRight={2} space={2}>
-            <Text size={1} textOverflow="ellipsis" weight="medium">
-              {currentGlobalBundle.metadata?.title}
-            </Text>
-          </Stack>
-        </Flex>
-      </Button>
+        <Button
+          as={releasesIntentLink}
+          data-as="a"
+          rel="noopener noreferrer"
+          mode="bleed"
+          padding={0}
+          radius="full"
+          style={{maxWidth: '180px'}}
+        >
+          <Flex align="flex-start" gap={0}>
+            <Box flex="none">
+              <ReleaseAvatar padding={2} tone={tone} />
+            </Box>
+            <Stack flex={1} paddingY={2} paddingRight={2} space={2}>
+              <Text size={1} textOverflow="ellipsis" weight="medium">
+                {currentGlobalBundle.metadata?.title}
+              </Text>
+            </Stack>
+          </Flex>
+        </Button>
+      </motion.div>
     )
   }, [currentGlobalBundle])
 
@@ -105,7 +113,7 @@ export function ReleasesNav(): JSX.Element {
     <Card flex="none" border marginRight={1} radius="full" tone="inherit" style={{margin: -1}}>
       <Flex gap={0}>
         <Box flex="none">{releasesToolLink}</Box>
-        {currentGlobalPerspectiveLabel}
+        <AnimatePresence>{currentGlobalPerspectiveLabel}</AnimatePresence>
         <GlobalPerspectiveMenu />
         {currentGlobalBundle._id !== LATEST._id && (
           <div>
