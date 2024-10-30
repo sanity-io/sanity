0740e430328795f8c0069d6d89186088425b9f4e
Jordan Lawrence <jordanl17@me.com>
2024-10-30 14:54:32 +0000

    fix: workspace picker menu always uses default tone

--- packages/sanity/src/core/studio/components/navbar/workspace/WorkspaceMenuButton.tsx
+++ packages/sanity/src/core/studio/components/navbar/workspace/WorkspaceMenuButton.tsx
@@ -24,6 +24,7 @@ const POPOVER_PROPS: MenuButtonProps['popover'] = {
   constrainSize: true,
   fallbackPlacements: ['bottom-end', 'bottom'],
   placement: 'bottom-end',
+  tone: 'default',
 }
 
 export function WorkspaceMenuButton() {
