51db20ac6c40bc14073696f7725b6dda3a43b80e
Binoy Patel <me@binoy.io>
2024-11-04 11:27:52 -0500

    test(cli): migrate cli tests to vitest (#7731)

--- vitest.workspace.ts
+++ vitest.workspace.ts
@@ -3,6 +3,7 @@ import {defineWorkspace} from 'vitest/config'
 export default defineWorkspace([
   'packages/@sanity/migrate',
   'packages/@sanity/block-tools',
+  'packages/@sanity/cli',
   'packages/@sanity/mutator',
   'packages/@sanity/schema',
   'packages/@sanity/types',
