0f32341be9aab83961f16ae285845a13ba431f6a
Jordan Lawrence <jordanl17@me.com>
2024-11-05 12:44:50 +0000

    chore(scripts): reordering blocks in release notes template script (#7758)

--- scripts/printReleaseNotesTemplate.ts
+++ scripts/printReleaseNotesTemplate.ts
@@ -13,12 +13,6 @@ const PREV_RELEASE =
 const CHANGELOG_COMMAND = `git log --pretty=format:'%aN | %s | %h' --abbrev-commit --reverse ${PREV_RELEASE}..origin/${BASE_BRANCH}`
 
 const TEMPLATE = `
-# Install or upgrade Sanity Studio
-
-To initiate a new Sanity Studio project or upgrade an existing one, please refer to our comprehensive guide on [Installing and Upgrading Sanity Studio](https://www.sanity.io/docs/upgrade).
-
-If you are updating from a version earlier than [3.37.0](https://www.sanity.io/changelog/93306939-973b-4e48-bb63-89a6b91d70b3), you should also check out [this article](https://www.sanity.io/help/upgrade-packages) to ensure your dependencies are up to date.
-
 # ✨ Highlights
 
 ## Awesome feature X
@@ -37,6 +31,12 @@ A few words about the awesome feature Y, preferably with screengifs
 - Fixes 🐞
 - Fixes 🐛
 - Fixes 🦗
+
+# Install or upgrade Sanity Studio
+
+To initiate a new Sanity Studio project or upgrade an existing one, please refer to our comprehensive guide on [Installing and Upgrading Sanity Studio](https://www.sanity.io/docs/upgrade).
+
+If you are updating from a version earlier than [3.37.0](https://www.sanity.io/changelog/93306939-973b-4e48-bb63-89a6b91d70b3), you should also check out [this article](https://www.sanity.io/help/upgrade-packages) to ensure your dependencies are up to date.
 
 # 📓 Full changelog
 Author | Message | Commit
