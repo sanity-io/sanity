77d5e01ebd9fdb0bc196975ba13a6b93ae87bacb
Rita <anarita151@gmail.com>
2024-11-20 14:46:18 +0100

    test(sanity): update useVersionOperations tests

--- packages/sanity/src/core/releases/hooks/__tests__/useVersionOperations.test.tsx
+++ packages/sanity/src/core/releases/hooks/__tests__/useVersionOperations.test.tsx
@@ -1,13 +1,50 @@
-import {beforeEach, describe, it, vi} from 'vitest'
+import {act, renderHook} from '@testing-library/react'
+import {beforeEach, describe, expect, it, vi} from 'vitest'
+
+import {createTestProvider} from '../../../../../test/testUtils/TestProvider'
+import {useReleaseOperationsMockReturn} from '../../store/__tests__/__mocks/useReleaseOperations.mock'
+import {useVersionOperations} from '../useVersionOperations'
+import {usePerspectiveMockReturn} from './__mocks__/usePerspective.mock'
+
+vi.mock('../../store/useReleaseOperations', () => ({
+  useReleaseOperations: vi.fn(() => useReleaseOperationsMockReturn),
+}))
+
+vi.mock('../usePerspective', () => ({
+  usePerspective: vi.fn(() => usePerspectiveMockReturn),
+}))
 
 describe('useVersionOperations', () => {
   beforeEach(() => {
     vi.clearAllMocks()
   })
 
-  it.todo('should create a version successfully', async () => {})
+  it('should create a version successfully', async () => {
+    const wrapper = await createTestProvider()
+    const {result} = renderHook(() => useVersionOperations(), {wrapper})
 
-  it.todo('should discard a version successfully')
+    await act(async () => {
+      await result.current.createVersion('releaseId', 'documentId')
+    })
 
-  it.todo('should handle discard version failure')
+    expect(useReleaseOperationsMockReturn.createVersion).toHaveBeenCalledWith(
+      'releaseId',
+      'documentId',
+    )
+    expect(usePerspectiveMockReturn.setPerspectiveFromReleaseId).toHaveBeenCalledWith('releaseId')
+  })
+
+  it('should discard a version successfully', async () => {
+    const wrapper = await createTestProvider()
+    const {result} = renderHook(() => useVersionOperations(), {wrapper})
+
+    await act(async () => {
+      await result.current.discardVersion('releaseId', 'documentId')
+    })
+
+    expect(useReleaseOperationsMockReturn.discardVersion).toHaveBeenCalledWith(
+      'releaseId',
+      'documentId',
+    )
+  })
 })
