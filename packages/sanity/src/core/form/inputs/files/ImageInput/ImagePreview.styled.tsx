f5e85ae1a4de1f6551bf2a1c701b50e2ad9a6576
Marius Lundgård <studio@mariuslundgard.com>
2024-11-29 13:00:21 +0100

    fix(sanity): support future UI tones (#7916)

--- packages/sanity/src/core/form/inputs/files/ImageInput/ImagePreview.styled.tsx
+++ packages/sanity/src/core/form/inputs/files/ImageInput/ImagePreview.styled.tsx
@@ -1,6 +1,5 @@
-import {Card, type CardTone, Flex, rgba, studioTheme} from '@sanity/ui'
-import {useColorSchemeValue} from 'sanity'
-import {css, styled} from 'styled-components'
+import {Card, Flex} from '@sanity/ui'
+import {styled} from 'styled-components'
 
 export const RatioBox = styled(Card)`
   position: relative;
@@ -18,24 +17,17 @@ export const RatioBox = styled(Card)`
   }
 `
 
-export const Overlay = styled(Flex)<{
-  $tone: Exclude<CardTone, 'inherit'>
-}>(({$tone}) => {
-  const colorScheme = useColorSchemeValue()
-  const textColor = studioTheme.color[colorScheme][$tone].card.enabled.fg
-  const backgroundColor = rgba(studioTheme.color[colorScheme][$tone].card.enabled.bg, 0.8)
-
-  return css`
-    position: absolute;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    backdrop-filter: blur(10px);
-    color: ${$tone ? textColor : ''};
-    background-color: ${backgroundColor};
-  `
-})
+export const Overlay = styled(Card)`
+  display: flex;
+  justify-content: flex-end;
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  backdrop-filter: blur(10px);
+  background-color: color-mix(in srgb, transparent, var(--card-bg-color) 80%);
+`
 
 export const FlexOverlay = styled(Flex)`
   position: absolute;
