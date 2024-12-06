dc03852645924947e34173a82337680f5ff778e9
Jordan Lawrence <jordanl17@me.com>
2024-12-06 14:09:13 +0000

    chore: hiding the useIsReleaseActive hook

--- packages/sanity/src/core/releases/hooks/useIsReleaseActive.ts
+++ packages/sanity/src/core/releases/hooks/useIsReleaseActive.ts
@@ -1,6 +1,7 @@
 import {isDraftPerspective, isPublishedPerspective} from '../util/util'
 import {usePerspective} from './usePerspective'
 
+/** @internal */
 export const useIsReleaseActive = () => {
   const {selectedPerspective} = usePerspective()
 
