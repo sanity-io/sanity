2761dbb292c25bc99bd7313e3792158b176a519d
pedrobonamin <pedrobonamin@gmail.com>
2024-12-09 16:00:51 +0100

    fix(core): update getDocumentAtRevision cache

--- packages/sanity/src/core/store/events/getDocumentAtRevision.ts
+++ packages/sanity/src/core/store/events/getDocumentAtRevision.ts
@@ -1,10 +1,10 @@
 import {type SanityClient, type SanityDocument} from '@sanity/client'
-import {type Observable, of} from 'rxjs'
-import {catchError, map, startWith, tap} from 'rxjs/operators'
+import {type Observable} from 'rxjs'
+import {catchError, map, shareReplay, startWith} from 'rxjs/operators'
 
 import {type EventsStoreRevision} from './types'
 
-const documentRevisionCache: Record<string, SanityDocument> = Object.create(null)
+const documentRevisionCache: Record<string, Observable<EventsStoreRevision>> = Object.create(null)
 
 export function getDocumentAtRevision({
   client,
@@ -17,27 +17,27 @@ export function getDocumentAtRevision({
 }): Observable<EventsStoreRevision | null> {
   const cacheKey = `${documentId}@${revisionId}`
   const dataset = client.config().dataset
-  if (documentRevisionCache[cacheKey]) {
-    return of({document: documentRevisionCache[cacheKey], loading: false, revisionId: revisionId})
+  if (!documentRevisionCache[cacheKey]) {
+    documentRevisionCache[cacheKey] = client.observable
+      .request<{documents: SanityDocument[]}>({
+        url: `/data/history/${dataset}/documents/${documentId}?revision=${revisionId}`,
+        tag: 'get-document-revision',
+      })
+      .pipe(
+        map((response) => {
+          const document = response.documents[0]
+          return {document: document, loading: false, revisionId: revisionId}
+        }),
+
+        catchError((error: Error) => {
+          // TODO: Handle error
+          console.error('Error fetching document at revision', error)
+          return [{document: null, loading: false, revisionId: revisionId}]
+        }),
+        startWith({document: null, loading: true, revisionId: revisionId}),
+        shareReplay(1),
+      )
   }
-  return client.observable
-    .request<{documents: SanityDocument[]}>({
-      url: `/data/history/${dataset}/documents/${documentId}?revision=${revisionId}`,
-      tag: 'get-document-revision',
-    })
-    .pipe(
-      map((response) => {
-        const document = response.documents[0]
-        return {document: document, loading: false, revisionId: revisionId}
-      }),
-      tap((resp) => {
-        documentRevisionCache[cacheKey] = resp.document
-      }),
-      catchError((error: Error) => {
-        // TODO: Handle error
-        console.error('Error fetching document at revision', error)
-        return [{document: null, loading: false, revisionId: revisionId}]
-      }),
-      startWith({document: null, loading: true, revisionId: revisionId}),
-    )
+
+  return documentRevisionCache[cacheKey]
 }
