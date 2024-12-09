ba2ec9b1104ddadaf5cf3c9fe055f6d438fcd85f
Robin Pyon <robin.pyon@gmail.com>
2024-12-09 16:10:32 +0000

    fix: failing test in appIdCache

--- packages/sanity/src/core/create/studio-app/__tests__/appIdCache.test.ts
+++ packages/sanity/src/core/create/studio-app/__tests__/appIdCache.test.ts
@@ -5,11 +5,13 @@ import {createAppIdCache} from '../appIdCache'
 describe('appIdCache', () => {
   it(`invokes fetch function and caches the result`, async () => {
     const cache = createAppIdCache()
-    const appIdFetcher = vi.fn((projectId) => Promise.resolve(`${projectId}-appId`))
+    const appIdFetcher = vi.fn((projectId) =>
+      Promise.resolve({appId: `${projectId}-appId`, studioApps: []}),
+    )
     const result1 = await cache.get({projectId: 'projectId', appIdFetcher})
     const result2 = await cache.get({projectId: 'projectId', appIdFetcher})
 
-    expect(result1).toEqual({appId: 'projectId-appId'})
+    expect(result1).toEqual({appId: 'projectId-appId', studioApps: []})
     expect(result1).toEqual(result2)
     expect(appIdFetcher).toHaveBeenCalledOnce()
   })
