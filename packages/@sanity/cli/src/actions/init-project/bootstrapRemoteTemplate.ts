60537c85754978fda074611d17add9295e511576
Rostislav Melkumyan <hello@rosti.no>
2024-12-10 15:07:54 +0100

    feat(cli): use `@vercel/frameworks` in `bootstrapRemoteTemplate` (#8001)

--- packages/@sanity/cli/src/actions/init-project/bootstrapRemoteTemplate.ts
+++ packages/@sanity/cli/src/actions/init-project/bootstrapRemoteTemplate.ts
@@ -1,6 +1,9 @@
 import {mkdir} from 'node:fs/promises'
 import {join} from 'node:path'
 
+import {type Framework, frameworks} from '@vercel/frameworks'
+import {detectFrameworkRecord, LocalFileSystemDetector} from '@vercel/fs-detectors'
+
 import {debug} from '../../debug'
 import {type CliCommandContext} from '../../types'
 import {
@@ -9,7 +12,6 @@ import {
   downloadAndExtractRepo,
   generateSanityApiReadToken,
   getMonoRepo,
-  isNextJsTemplate,
   type RepoInfo,
   tryApplyPackageName,
   validateRemoteTemplate,
@@ -56,12 +58,16 @@ export async function bootstrapRemoteTemplate(
   const readToken = needsReadToken
     ? await generateSanityApiReadToken('API Read Token', variables.projectId, apiClient)
     : undefined
-  const isNext = await isNextJsTemplate(outputPath)
-  const envName = isNext ? '.env.local' : '.env'
 
-  for (const folder of packages ?? ['']) {
-    const path = join(outputPath, folder)
-    await applyEnvVariables(path, {...variables, readToken}, envName)
+  for (const pkg of packages ?? ['']) {
+    const packagePath = join(outputPath, pkg)
+    const packageFramework: Framework | null = await detectFrameworkRecord({
+      fs: new LocalFileSystemDetector(packagePath),
+      frameworkList: frameworks as readonly Framework[],
+    })
+    // Next.js uses `.env.local` for local environment variables
+    const envName = packageFramework?.slug === 'nextjs' ? '.env.local' : '.env'
+    await applyEnvVariables(packagePath, {...variables, readToken}, envName)
   }
 
   debug('Setting package name to %s', packageName)
