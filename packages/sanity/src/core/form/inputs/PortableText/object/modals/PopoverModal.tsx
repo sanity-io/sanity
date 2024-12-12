5bcd296db51679f5645302d7172d6f98c6ff07f1
Per-Kristian Nordnes <per.kristian.nordnes@gmail.com>
2024-12-12 11:58:21 +0100

    feat(core): Implement focus locking inside the Portable Text Input Popover Modal
    The PopoverModal should have the same kind of focus locking as other dialogs

--- packages/sanity/src/core/form/inputs/PortableText/object/modals/PopoverModal.tsx
+++ packages/sanity/src/core/form/inputs/PortableText/object/modals/PopoverModal.tsx
@@ -3,6 +3,7 @@
 import {CloseIcon} from '@sanity/icons'
 import {Box, Flex, Text, useClickOutsideEvent, useGlobalKeyDown} from '@sanity/ui'
 import {type ReactNode, useCallback, useRef, useState} from 'react'
+import FocusLock from 'react-focus-lock'
 import {type PortableTextEditorElement} from 'sanity/_singletons'
 
 import {Button, type PopoverProps} from '../../../../../../ui-components'
@@ -42,26 +43,33 @@ export function PopoverEditDialog(props: PopoverEditDialogProps): ReactNode {
       referenceBoundary={referenceBoundary}
       referenceElement={referenceElement}
       width={width}
+      autoFocus
     />
   )
 }
 
 function Content(props: PopoverEditDialogProps) {
   const {onClose, referenceBoundary, referenceElement, title, autoFocus} = props
+  const isClosedRef = useRef(false)
+
+  const handleClose = useCallback(() => {
+    isClosedRef.current = true
+    onClose()
+  }, [onClose])
 
   useGlobalKeyDown(
     useCallback(
       (event: KeyboardEvent) => {
         if (event.key === 'Escape') {
-          onClose()
+          handleClose()
         }
       },
-      [onClose],
+      [handleClose],
     ),
   )
 
   useClickOutsideEvent(
-    onClose,
+    handleClose,
     () => [referenceElement],
     () => referenceBoundary,
   )
@@ -70,42 +78,44 @@ function Content(props: PopoverEditDialogProps) {
   const [contentElement, setContentElement] = useState<HTMLDivElement | null>(null)
   const containerElement = useRef<HTMLDivElement | null>(null)
 
-  useEffect(() => {
-    // When rendered, focus on the first input element in the content
-    if (contentElement) {
-      contentElement.querySelector('input')?.focus()
-    }
-  }, [contentElement])
+  const handleFocusLockWhiteList = useCallback((element: HTMLElement) => {
+    // This is needed in order for focusLock not to trap focus in the
+    // popover when closing the popover and focus is to be returned to the editor
+    if (isClosedRef.current) return false
+    return Boolean(element.contentEditable) || Boolean(containerElement.current?.contains(element))
+  }, [])
 
   return (
     <VirtualizerScrollInstanceProvider
       scrollElement={contentElement}
       containerElement={containerElement}
     >
-      <Flex ref={containerElement} direction="column" height="fill">
-        <ContentHeaderBox flex="none" padding={1}>
-          <Flex align="center">
-            <Box flex={1} padding={2}>
-              <Text weight="medium">{title}</Text>
-            </Box>
+      <FocusLock autoFocus whiteList={handleFocusLockWhiteList}>
+        <Flex ref={containerElement} direction="column" height="fill">
+          <ContentHeaderBox flex="none" padding={1}>
+            <Flex align="center">
+              <Box flex={1} padding={2}>
+                <Text weight="medium">{title}</Text>
+              </Box>
 
-            <Button
-              autoFocus={Boolean(autoFocus)}
-              icon={CloseIcon}
-              mode="bleed"
-              onClick={onClose}
-              tooltipProps={{content: 'Close'}}
-            />
-          </Flex>
-        </ContentHeaderBox>
-        <ContentScrollerBox flex={1}>
-          <PresenceOverlay margins={[0, 0, 1, 0]}>
-            <Box padding={3} ref={setContentElement}>
-              {props.children}
-            </Box>
-          </PresenceOverlay>
-        </ContentScrollerBox>
-      </Flex>
+              <Button
+                autoFocus={Boolean(autoFocus)}
+                icon={CloseIcon}
+                mode="bleed"
+                onClick={handleClose}
+                tooltipProps={{content: 'Close'}}
+              />
+            </Flex>
+          </ContentHeaderBox>
+          <ContentScrollerBox flex={1}>
+            <PresenceOverlay margins={[0, 0, 1, 0]}>
+              <Box padding={3} ref={setContentElement}>
+                {props.children}
+              </Box>
+            </PresenceOverlay>
+          </ContentScrollerBox>
+        </Flex>
+      </FocusLock>
     </VirtualizerScrollInstanceProvider>
   )
 }
