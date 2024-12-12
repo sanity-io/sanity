cc88fc80a63db8ceeb2589397b31fe937e8ed53d
Jordan Lawrence <jordanl17@me.com>
2024-12-12 13:29:27 +0000

    fix: type on mock

--- packages/sanity/src/core/releases/store/__tests__/__mocks/createReleaseOperationsStore.mock.ts
+++ packages/sanity/src/core/releases/store/__tests__/__mocks/createReleaseOperationsStore.mock.ts
@@ -16,6 +16,7 @@ export const createReleaseOperationsStoreReturn: Mocked<ReleaseOperationsStore> 
   unschedule: vi.fn(),
   updateRelease: vi.fn(),
   deleteRelease: vi.fn(),
+  revertRelease: vi.fn(),
   unpublishVersion: vi.fn(),
 }
 
