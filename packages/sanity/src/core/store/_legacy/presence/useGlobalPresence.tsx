a9f286dc0fe4c58d3a2e435995282d5ca3f23063
Cody Olsen <stipsan@gmail.com>
2024-12-13 16:23:40 +0100

    fix: `WebSocket is closed before the connection is established` warning

--- packages/sanity/src/core/store/_legacy/presence/useGlobalPresence.tsx
+++ packages/sanity/src/core/store/_legacy/presence/useGlobalPresence.tsx
@@ -1,20 +1,19 @@
-import {useEffect, useState} from 'react'
+import {startTransition, useEffect, useReducer} from 'react'
+import {useObservable} from 'react-rx'
+import {of} from 'rxjs'
 
 import {usePresenceStore} from '../datastores'
 import {type GlobalPresence} from './types'
 
+const initial: GlobalPresence[] = []
+const fallback = of(initial)
+
 /** @internal */
 export function useGlobalPresence(): GlobalPresence[] {
-  const [presence, setPresence] = useState<GlobalPresence[]>([])
+  const [mounted, mount] = useReducer(() => true, false)
+  // Using `startTransition` here ensures that rapid re-renders that affect the deps used by `usePresenceStore` delay the transition to `mounted=true`, thus avoiding creating websocket connections that will be closed immediately.
+  useEffect(() => startTransition(mount), [])
+
   const presenceStore = usePresenceStore()
-
-  useEffect(() => {
-    const subscription = presenceStore.globalPresence$.subscribe(setPresence)
-
-    return () => {
-      subscription.unsubscribe()
-    }
-  }, [presenceStore])
-
-  return presence
+  return useObservable(mounted ? presenceStore.globalPresence$ : fallback, initial)
 }
