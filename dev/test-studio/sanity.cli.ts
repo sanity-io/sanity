f5cd7df9280b7820ee1cfeb45709e6cec325cde0
Cody Olsen <stipsan@gmail.com>
2024-12-13 11:37:04 +0100

    chore: add `dev:million-lint` command

--- dev/test-studio/sanity.cli.ts
+++ dev/test-studio/sanity.cli.ts
@@ -3,6 +3,20 @@ import path from 'node:path'
 import {defineCliConfig} from 'sanity/cli'
 import {type UserConfig} from 'vite'
 
+const millionLintEnabled = process.env.REACT_MILLION_LINT === 'true'
+const millionInclude: string[] = []
+try {
+  if (millionLintEnabled) {
+    for (const filePath of require('./.react-compiler-bailout-report.json')) {
+      millionInclude.push(`**/${filePath}`)
+    }
+  }
+} catch (err) {
+  throw new Error('Failed to read lint report, did you run `pnpm report:react-compiler-bailout`?', {
+    cause: err,
+  })
+}
+
 export default defineCliConfig({
   api: {
     projectId: 'ppsg7ml5',
@@ -13,12 +27,34 @@ export default defineCliConfig({
   // A) `SANITY_STUDIO_REACT_STRICT_MODE=false pnpm dev`
   // B) creating a `.env` file locally that sets the same env variable as above
   reactStrictMode: true,
-  reactCompiler: {target: '18'},
+  reactCompiler: millionLintEnabled
+    ? {
+        target: '18',
+        sources: (filename) => {
+          if (filename.includes('node_modules')) {
+            return false
+          }
+          return millionInclude.every(
+            (pattern) => !filename.endsWith(`/${pattern.split('**/')[1]}`),
+          )
+        },
+      }
+    : {target: '18'},
   vite(viteConfig: UserConfig): UserConfig {
     const reactProductionProfiling = process.env.REACT_PRODUCTION_PROFILING === 'true'
 
     return {
       ...viteConfig,
+      plugins: millionLintEnabled
+        ? [
+            require('@million/lint').vite({
+              filter: {
+                include: millionInclude,
+              },
+            }),
+            ...(viteConfig.plugins || []),
+          ]
+        : viteConfig.plugins,
       optimizeDeps: {
         ...viteConfig.optimizeDeps,
         include: ['react/jsx-runtime'],
