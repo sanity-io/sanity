57150e7116eb47065912d274c85d023585f61627
Cody Olsen <81981+stipsan@users.noreply.github.com>
2024-12-18 20:48:17 +0100

    fix: `WebSocket is closed before the connection is established` warning (#8042)

--- packages/sanity/src/core/store/_legacy/presence/useDocumentPresence.tsx
+++ packages/sanity/src/core/store/_legacy/presence/useDocumentPresence.tsx
@@ -1,19 +1,19 @@
-import {useEffect, useState} from 'react'
+import {startTransition, useEffect, useReducer} from 'react'
+import {useObservable} from 'react-rx'
+import {of} from 'rxjs'
 
 import {usePresenceStore} from '../datastores'
 import {type DocumentPresence} from './types'
 
+const initial: DocumentPresence[] = []
+const fallback = of(initial)
+
 /** @internal */
 export function useDocumentPresence(documentId: string): DocumentPresence[] {
+  const [mounted, mount] = useReducer(() => true, false)
+  // Using `startTransition` here ensures that rapid re-renders that affect the deps used by `usePresenceStore` delay the transition to `mounted=true`, thus avoiding creating websocket connections that will be closed immediately.
+  useEffect(() => startTransition(mount), [])
+
   const presenceStore = usePresenceStore()
-  const [presence, setPresence] = useState<DocumentPresence[]>([])
-
-  useEffect(() => {
-    const subscription = presenceStore.documentPresence(documentId).subscribe(setPresence)
-    return () => {
-      subscription.unsubscribe()
-    }
-  }, [documentId, presenceStore])
-
-  return presence
+  return useObservable(mounted ? presenceStore.documentPresence(documentId) : fallback, initial)
 }
