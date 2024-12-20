8f61ce3e80148d44debee5217433a41741e70ac8
Bjørge Næss <bjoerge@gmail.com>
2024-12-20 14:49:09 +0100

    fix(core): re-subscribes to shared pair listener opens a new connection (#8120)

--- packages/sanity/src/core/store/_legacy/document/document-pair/memoizedPair.ts
+++ packages/sanity/src/core/store/_legacy/document/document-pair/memoizedPair.ts
@@ -1,12 +1,14 @@
 import {type SanityClient} from '@sanity/client'
-import {Observable} from 'rxjs'
-import {publishReplay, refCount} from 'rxjs/operators'
+import {merge, type Observable, of, ReplaySubject, share, timer} from 'rxjs'
 
 import {type PairListenerOptions} from '../getPairListener'
 import {type IdPair} from '../types'
 import {memoize} from '../utils/createMemoizer'
 import {checkoutPair, type Pair} from './checkoutPair'
 import {memoizeKeyGen} from './memoizeKeyGen'
+
+// How long to keep listener connected for after last unsubscribe
+const LISTENER_RESET_DELAY = 10_000
 
 export const memoizedPair: (
   client: SanityClient,
@@ -22,12 +24,18 @@ export const memoizedPair: (
     serverActionsEnabled: Observable<boolean>,
     pairListenerOptions?: PairListenerOptions,
   ): Observable<Pair> => {
-    return new Observable<Pair>((subscriber) => {
-      const pair = checkoutPair(client, idPair, serverActionsEnabled, pairListenerOptions)
-      subscriber.next(pair)
-
-      return pair.complete
-    }).pipe(publishReplay(1), refCount())
+    const pair = checkoutPair(client, idPair, serverActionsEnabled, pairListenerOptions)
+    return merge(
+      of(pair),
+      // makes sure the pair listener is kept alive for as long as there are subscribers
+      pair._keepalive,
+    ).pipe(
+      share({
+        connector: () => new ReplaySubject(1),
+        resetOnComplete: true,
+        resetOnRefCountZero: () => timer(LISTENER_RESET_DELAY),
+      }),
+    )
   },
   memoizeKeyGen,
 )
