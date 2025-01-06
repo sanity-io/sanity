3918d6addee35719d8f6629b823f8ebdfbe324c3
Bjørge Næss <bjoerge@gmail.com>
2025-01-06 12:13:16 +0100

    chore(test-studio): add debug schema for patch-on-mount

--- dev/test-studio/structure/constants.ts
+++ dev/test-studio/structure/constants.ts
@@ -55,6 +55,7 @@ export const DEBUG_INPUT_TYPES = [
   'fieldsetsTest',
   'fieldValidationInferReproDoc',
   'focusTest',
+  'patchOnMountDebug',
   'formInputDebug',
   'initialValuesTest',
   'inspectorsTest',
