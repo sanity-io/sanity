sanity-io/sanity

commit 876d873b284d040dcf54884b2e45ed5cb8f5ec68

fix(core): re-subscribes to shared pair listener opens a new connection (#8120)

packages/sanity/src/core/store/_legacy/document/document-pair/checkoutPair.ts
@@ -2,7 +2,7 @@ import {type Action, type SanityClient} from '@sanity/client'
 import {type Mutation} from '@sanity/mutator'
 import {type SanityDocument} from '@sanity/types'
 import {omit} from 'lodash'
-import {EMPTY, from, merge, type Observable, Subject} from 'rxjs'
+import {EMPTY, from, merge, type Observable} from 'rxjs'
 import {filter, map, mergeMap, share, take, tap} from 'rxjs/operators'
 
 import {
@@ -65,7 +65,7 @@ export interface Pair {
   transactionsPendingEvents$: Observable<PendingMutationsEvent>
   published: DocumentVersion
   draft: DocumentVersion
-  complete: () => void
+  _keepalive: Observable<never>
 }
 
 function setVersion<T>(version: 'draft' | 'published') {
@@ -204,10 +204,7 @@ export function checkoutPair(
 ): Pair {
   const {publishedId, draftId} = idPair
 
-  const listenerEventsConnector = new Subject<ListenerEvent>()
-  const listenerEvents$ = getPairListener(client, idPair, pairListenerOptions).pipe(
-    share({connector: () => listenerEventsConnector}),
-  )
+  const listenerEvents$ = getPairListener(client, idPair, pairListenerOptions).pipe(share())
 
   const reconnect$ = listenerEvents$.pipe(
     filter((ev) => ev.type === 'reconnect'),
@@ -255,6 +252,8 @@ export function checkoutPair(
       consistency$: published.consistency$,
       remoteSnapshot$: published.remoteSnapshot$.pipe(map(setVersion('published'))),
     },
-    complete: () => listenerEventsConnector.complete(),
+    // Use this to keep the mutation pipeline active.
+    // It won't ever emit any events, but it will prevent the eventsource connection from completing for as long as it is subscribed to
+    _keepalive: commits$.pipe(mergeMap(() => EMPTY)),
   }
 }

packages/sanity/src/core/store/_legacy/document/document-pair/memoizedPair.ts
@@ -1,12 +1,14 @@
 import {type SanityClient} from '@sanity/client'
-import {Observable} from 'rxjs'
-import {publishReplay, refCount} from 'rxjs/operators'
+import {merge, type Observable, of, ReplaySubject, share, timer} from 'rxjs'
 
 import {type PairListenerOptions} from '../getPairListener'
 import {type IdPair} from '../types'
 import {memoize} from '../utils/createMemoizer'
 import {checkoutPair, type Pair} from './checkoutPair'
 import {memoizeKeyGen} from './memoizeKeyGen'
+
+// How long to keep listener connected for after last unsubscribe
+const LISTENER_RESET_DELAY = 10_000
 
 export const memoizedPair: (
   client: SanityClient,
@@ -22,12 +24,18 @@ export const memoizedPair: (
     serverActionsEnabled: Observable<boolean>,
     pairListenerOptions?: PairListenerOptions,
   ): Observable<Pair> => {
-    return new Observable<Pair>((subscriber) => {
-      const pair = checkoutPair(client, idPair, serverActionsEnabled, pairListenerOptions)
-      subscriber.next(pair)
-
-      return pair.complete
-    }).pipe(publishReplay(1), refCount())
+    const pair = checkoutPair(client, idPair, serverActionsEnabled, pairListenerOptions)
+    return merge(
+      of(pair),
+      // makes sure the pair listener is kept alive for as long as there are subscribers
+      pair._keepalive,
+    ).pipe(
+      share({
+        connector: () => new ReplaySubject(1),
+        resetOnComplete: true,
+        resetOnRefCountZero: () => timer(LISTENER_RESET_DELAY),
+      }),
+    )
   },
   memoizeKeyGen,
 )

packages/sanity/src/core/store/_legacy/document/getPairListener.ts
@@ -2,10 +2,9 @@
 import {type SanityClient} from '@sanity/client'
 import {type SanityDocument} from '@sanity/types'
 import {groupBy} from 'lodash'
-import {defer, merge, type Observable, of, throwError, timer} from 'rxjs'
+import {defer, merge, type Observable, of, throwError} from 'rxjs'
 import {catchError, concatMap, filter, map, mergeMap, scan, share} from 'rxjs/operators'
 
-import {LISTENER_RESET_DELAY} from '../../../preview/constants'
 import {shareReplayLatest} from '../../../preview/utils/shareReplayLatest'
 import {debug} from './debug'
 import {
@@ -96,7 +95,6 @@ export function getPairListener(
         //filter((event) => Math.random() < 0.99 || event.type !== 'mutation'),
         shareReplayLatest({
           predicate: (event) => event.type === 'welcome' || event.type === 'reconnect',
-          resetOnRefCountZero: () => timer(LISTENER_RESET_DELAY),
         }),
       ),
   ) as Observable<WelcomeEvent | MutationEvent | ReconnectEvent>

commit ce0badc8c2f2a3e65647705348d21789b712cc10

fixup! fix(sanity): fix race condition regression introduced by #8120

packages/sanity/src/core/store/_legacy/document/document-pair/checkoutPair.ts
@@ -2,7 +2,7 @@ import {type Action, type SanityClient} from '@sanity/client'
 import {type Mutation} from '@sanity/mutator'
 import {type SanityDocument} from '@sanity/types'
 import {omit} from 'lodash'
-import {EMPTY, from, merge, type Observable, Subject} from 'rxjs'
+import {EMPTY, from, merge, type Observable} from 'rxjs'
 import {filter, map, mergeMap, share, take, tap} from 'rxjs/operators'
 
 import {
@@ -65,7 +65,6 @@ export interface Pair {
   transactionsPendingEvents$: Observable<PendingMutationsEvent>
   published: DocumentVersion
   draft: DocumentVersion
-  complete: () => void
 }
 
 function setVersion<T>(version: 'draft' | 'published') {
@@ -204,10 +203,7 @@ export function checkoutPair(
 ): Pair {
   const {publishedId, draftId} = idPair
 
-  const listenerEventsConnector = new Subject<ListenerEvent>()
-  const listenerEvents$ = getPairListener(client, idPair, pairListenerOptions).pipe(
-    share({connector: () => listenerEventsConnector}),
-  )
+  const listenerEvents$ = getPairListener(client, idPair, pairListenerOptions).pipe(share())
 
   const reconnect$ = listenerEvents$.pipe(
     filter((ev) => ev.type === 'reconnect'),
@@ -255,6 +251,5 @@ export function checkoutPair(
       consistency$: published.consistency$,
       remoteSnapshot$: published.remoteSnapshot$.pipe(map(setVersion('published'))),
     },
-    complete: () => listenerEventsConnector.complete(),
   }
 }

packages/sanity/src/core/store/_legacy/document/document-pair/memoizedPair.ts
@@ -1,5 +1,6 @@
 import {type SanityClient} from '@sanity/client'
-import {Observable, ReplaySubject, share, timer} from 'rxjs'
+import {EMPTY, merge, Observable, of, ReplaySubject, share, timer} from 'rxjs'
+import {mergeMap} from 'rxjs/operators'
 
 import {type PairListenerOptions} from '../getPairListener'
 import {type IdPair} from '../types'
@@ -26,9 +27,13 @@ export const memoizedPair: (
   ): Observable<Pair> => {
     return new Observable<Pair>((subscriber) => {
       const pair = checkoutPair(client, idPair, serverActionsEnabled, pairListenerOptions)
-      subscriber.next(pair)
-
-      return pair.complete
+      return merge(
+        of(pair),
+        // merge in draft events and published events to makes sure they receive
+        // the events they need for as long as the pair is subscribed to
+        pair.draft.events.pipe(mergeMap(() => EMPTY)),
+        pair.published.events.pipe(mergeMap(() => EMPTY)),
+      ).subscribe(subscriber)
     }).pipe(
       share({
         connector: () => new ReplaySubject(1),