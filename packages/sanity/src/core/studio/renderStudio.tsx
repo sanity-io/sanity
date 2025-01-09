9ba86c7d9c0a8932e6922a3750edda7a08d1a74d
Bjørge Næss <bjoerge@gmail.com>
2025-01-09 17:00:25 +0100

    {wip} saved 2025-01-9 17:00

--- packages/sanity/src/core/studio/renderStudio.tsx
+++ packages/sanity/src/core/studio/renderStudio.tsx
@@ -1,8 +1,4 @@
-import {StrictMode} from 'react'
-import {createRoot} from 'react-dom/client'
-
 import {type Config} from '../config'
-import {Studio} from './Studio'
 
 interface RenderStudioOptions {
   basePath?: string
@@ -35,24 +31,6 @@ export function renderStudio(
   config: Config,
   options: RenderStudioOptions | boolean = false,
 ): () => void {
-  if (!rootElement) {
-    throw new Error('Missing root element to mount application into')
-  }
-
-  const opts = typeof options === 'boolean' ? {reactStrictMode: options} : options
-  const {reactStrictMode = false, basePath} = opts
-
-  const root = createRoot(rootElement)
-
-  root.render(
-    reactStrictMode ? (
-      <StrictMode>
-        <Studio config={config} basePath={basePath} unstable_globalStyles />
-      </StrictMode>
-    ) : (
-      <Studio config={config} basePath={basePath} unstable_globalStyles />
-    ),
-  )
-
-  return () => root.unmount()
+  rootElement.innerHTML = 'Hello world'
+  return () => {}
 }
