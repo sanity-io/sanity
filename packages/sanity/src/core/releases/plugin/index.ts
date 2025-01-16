3417abab1dab0a994788a6be735adbc14b57f412
Ash <ash@sanity.io>
2025-01-16 14:59:04 +0000

    feat(sanity): do not switch off Actions API in Releases plugin

--- packages/sanity/src/core/releases/plugin/index.ts
+++ packages/sanity/src/core/releases/plugin/index.ts
@@ -57,8 +57,4 @@ export const releases = definePlugin({
   document: {
     actions: (actions, context) => resolveDocumentActions(actions, context),
   },
-  // eslint-disable-next-line camelcase
-  __internal_serverDocumentActions: {
-    enabled: false,
-  },
 })
