edbb298918988a556c2316501636601b42bb5b22
Ash <ash@sanity.io>
2025-01-16 15:00:02 +0000

    feat(test-studio): enable Actions API

--- dev/test-studio/sanity.config.ts
+++ dev/test-studio/sanity.config.ts
@@ -168,6 +168,10 @@ const defaultWorkspace = defineConfig({
   },
   basePath: '/test',
   icon: SanityMonogram,
+  // eslint-disable-next-line camelcase
+  __internal_serverDocumentActions: {
+    enabled: true,
+  },
   scheduledPublishing: {
     enabled: true,
     inputDateTimeFormat: 'MM/dd/yy h:mm a',
