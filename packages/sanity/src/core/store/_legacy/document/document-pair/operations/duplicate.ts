283978acd75a78827d950f2f37db1d05a93d74b1
pedrobonamin <pedrobonamin@gmail.com>
2025-01-20 11:19:51 +0100

    fix(core): remove nexted ternary from duplicate action

--- packages/sanity/src/core/store/_legacy/document/document-pair/operations/duplicate.ts
+++ packages/sanity/src/core/store/_legacy/document/document-pair/operations/duplicate.ts
@@ -1,3 +1,4 @@
+import {type SanityDocument} from '@sanity/types'
 import {omit} from 'lodash'
 
 import {getDraftId, getVersionFromId, getVersionId} from '../../../../../util'
@@ -6,9 +7,31 @@ import {type OperationImpl} from './types'
 
 const omitProps = ['_createdAt', '_updatedAt']
 
+const getDocumentToDuplicateId = ({
+  versionSnapshot,
+  dupeId,
+  liveEdit,
+}: {
+  versionSnapshot?: SanityDocument | null | undefined
+  dupeId: string
+  liveEdit: boolean
+}) => {
+  if (versionSnapshot) {
+    // When duplicating a version document we need to create it with a version id.
+    // We get the version from the snapshot id and create a new version id for the duplicate.
+    const versionId = getVersionFromId(versionSnapshot._id)
+    if (versionId) return getVersionId(dupeId, versionId)
+  }
+
+  if (liveEdit) {
+    return dupeId
+  }
+
+  return getDraftId(dupeId)
+}
+
 export const duplicate: OperationImpl<[baseDocumentId: string], 'NOTHING_TO_DUPLICATE'> = {
   disabled: ({snapshots}) => {
-    if (snapshots.version) return false
     return snapshots.published || snapshots.draft || snapshots.version
       ? false
       : 'NOTHING_TO_DUPLICATE'
@@ -20,14 +43,11 @@ export const duplicate: OperationImpl<[baseDocumentId: string], 'NOTHING_TO_DUPL
       throw new Error('cannot execute on empty document')
     }
 
-    // When duplicating a version document we need to create it with a version id
-    const versionId = snapshots.version?._id ? getVersionFromId(snapshots.version._id) : null
-    // eslint-disable-next-line no-nested-ternary
-    const _id = versionId
-      ? getVersionId(dupeId, versionId)
-      : isLiveEditEnabled(schema, typeName)
-        ? dupeId
-        : getDraftId(dupeId)
+    const _id = getDocumentToDuplicateId({
+      versionSnapshot: snapshots.version,
+      dupeId,
+      liveEdit: isLiveEditEnabled(schema, typeName),
+    })
 
     return client.observable.create(
       {
