cbbebd58d1aad40802a55e98625e69fbaa9638ca
Cody Olsen <81981+stipsan@users.noreply.github.com>
2025-01-20 11:08:56 +0100

    fix(presentation): always show all documents in use (#8320)

--- packages/sanity/src/presentation/editor/DocumentListPane.tsx
+++ packages/sanity/src/presentation/editor/DocumentListPane.tsx
@@ -50,7 +50,7 @@ export function DocumentListPane(props: {
     () =>
       refs
         .filter((r) => getPublishedId(r._id) !== mainDocumentState?.document?._id)
-        .map((r) => r._id),
+        .map((r) => getPublishedId(r._id)),
     [mainDocumentState, refs],
   )
 
