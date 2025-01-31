29bc9c3939a2e2161e868453e6a71abdae6fbd4f
Ash <ash@sanity.io>
2025-01-31 16:38:51 +0000

    refactor(sanity): remove unnecessary logic in `ReleasesStudioLayout`
    The plugin is only executed if it is enabled. Therefore, it is
    unnecessary to check whether it's enabled in the `ReleasesStudioLayout`
    component, which is part of the plugin.

--- packages/sanity/src/core/releases/plugin/ReleasesStudioLayout.tsx
+++ packages/sanity/src/core/releases/plugin/ReleasesStudioLayout.tsx
@@ -4,17 +4,8 @@ import {type LayoutProps} from '../../config'
 import {AddonDatasetProvider} from '../../studio'
 import {ReleasesMetadataProvider} from '../contexts/ReleasesMetadataProvider'
 
-export const ReleasesStudioLayout: ComponentType<LayoutProps> = (props) => {
-  // TODO: Replace for useReleasesEnabled
-  const {enabled} = {enabled: true}
-
-  if (!enabled) {
-    return props.renderDefault(props)
-  }
-
-  return (
-    <AddonDatasetProvider>
-      <ReleasesMetadataProvider>{props.renderDefault(props)}</ReleasesMetadataProvider>
-    </AddonDatasetProvider>
-  )
-}
+export const ReleasesStudioLayout: ComponentType<LayoutProps> = (props) => (
+  <AddonDatasetProvider>
+    <ReleasesMetadataProvider>{props.renderDefault(props)}</ReleasesMetadataProvider>
+  </AddonDatasetProvider>
+)
