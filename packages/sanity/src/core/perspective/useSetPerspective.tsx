0fb67579f6d91e302872f79c658135639a4de3fd
pedrobonamin <pedrobonamin@gmail.com>
2025-01-31 16:35:26 +0100

    fix(core):  remove explicit check for 'r' in perspective

--- packages/sanity/src/core/perspective/useSetPerspective.tsx
+++ packages/sanity/src/core/perspective/useSetPerspective.tsx
@@ -9,19 +9,9 @@ export function useSetPerspective() {
   const router = useRouter()
   const setPerspective = useCallback(
     (releaseId: 'published' | 'drafts' | ReleaseId | undefined) => {
-      let perspectiveParam = ''
-
-      if (!releaseId || releaseId === 'drafts') {
-        perspectiveParam = ''
-      } else if (releaseId === 'published' || releaseId.startsWith('r')) {
-        perspectiveParam = releaseId
-      } else {
-        throw new Error(`Invalid releaseId: ${releaseId}`)
-      }
-
       router.navigateStickyParams({
         excludedPerspectives: '',
-        perspective: perspectiveParam,
+        perspective: releaseId === 'drafts' ? '' : releaseId,
       })
     },
     [router],
