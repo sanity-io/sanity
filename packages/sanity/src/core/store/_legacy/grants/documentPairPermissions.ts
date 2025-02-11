76b7b114def295e5e11cf6161d5a6da8150d9d0f
Rita <anarita151@gmail.com>
2025-02-11 14:11:34 +0100

    fix(core): update documentPairPermission to allow for versions

--- packages/sanity/src/core/store/_legacy/grants/documentPairPermissions.ts
+++ packages/sanity/src/core/store/_legacy/grants/documentPairPermissions.ts
@@ -33,6 +33,7 @@ interface PairPermissionsOptions {
   grantsStore: GrantsStore
   permission: DocumentPermission
   draft: SanityDocument | null
+  version: SanityDocument | null
   published: SanityDocument | null
   liveEdit: boolean
 }
@@ -41,6 +42,7 @@ function getPairPermissions({
   grantsStore,
   permission,
   draft,
+  version,
   published,
   liveEdit,
 }: PairPermissionsOptions): Array<[string, Observable<PermissionCheckResult>]> {
@@ -51,8 +53,10 @@ function getPairPermissions({
   //
   // note: this should _not_ be used if the draft and published versions should
   // be considered separately/explicitly in the permissions.
-  const effectiveVersion = draft || published
-  const effectiveVersionType = effectiveVersion === draft ? 'draft' : 'published'
+  const effectiveVersion = version || draft || published
+  const effectiveVersionType =
+    // eslint-disable-next-line no-nested-ternary
+    effectiveVersion === version ? version : effectiveVersion === draft ? 'draft' : 'published'
 
   const {checkDocumentPermission} = grantsStore
 
@@ -74,6 +78,12 @@ function getPairPermissions({
       if (liveEdit) return []
 
       return [['delete draft document', checkDocumentPermission('update', draft)]]
+    }
+
+    case 'discardVersion': {
+      if (liveEdit) return []
+
+      return [['delete version', checkDocumentPermission('update', version || null)]]
     }
 
     case 'publish': {
@@ -157,6 +167,7 @@ function getPairPermissions({
 export type DocumentPermission =
   | 'delete'
   | 'discardDraft'
+  | 'discardVersion'
   | 'publish'
   | 'unpublish'
   | 'update'
@@ -190,7 +201,7 @@ export function getDocumentPairPermissions({
   permission,
   type,
   serverActionsEnabled,
-  version,
+  version: v,
   pairListenerOptions,
 }: DocumentPairPermissionsOptions): Observable<PermissionCheckResult> {
   // this case was added to fix a crash that would occur if the `schemaType` was
@@ -206,21 +217,24 @@ export function getDocumentPairPermissions({
 
   return snapshotPair(
     client,
-    getIdPair(id, {version}),
+    getIdPair(id, {version: v}),
     type,
     serverActionsEnabled,
     pairListenerOptions,
   ).pipe(
     switchMap((pair) =>
-      combineLatest([pair.draft.snapshots$, pair.published.snapshots$]).pipe(
-        map(([draft, published]) => ({draft, published})),
-      ),
+      combineLatest([
+        pair.draft.snapshots$,
+        pair.published.snapshots$,
+        pair.version?.snapshots$ || of(null),
+      ]).pipe(map(([draft, published, version]) => ({draft, published, version}))),
     ),
-    switchMap(({draft, published}) => {
+    switchMap(({draft, published, version}) => {
       const pairPermissions = getPairPermissions({
         grantsStore,
         permission,
         draft,
+        version,
         published,
         liveEdit,
       }).map(([label, observable]) =>
