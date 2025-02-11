9ea63c3cf360d6f065f74304718ffa7b0c208faf
Bjørge Næss <bjoerge@gmail.com>
2025-02-11 16:18:02 +0100

    fix(sanity): hide Live badge when editing a version of a live editable document

--- packages/sanity/src/structure/documentBadges/LiveEditBadge.ts
+++ packages/sanity/src/structure/documentBadges/LiveEditBadge.ts
@@ -2,9 +2,9 @@ import {type DocumentBadgeComponent} from 'sanity'
 
 /** @internal */
 export const LiveEditBadge: DocumentBadgeComponent = (props) => {
-  const {liveEditSchemaType} = props
+  const {liveEditSchemaType, version} = props
 
-  if (liveEditSchemaType) {
+  if (liveEditSchemaType && !version) {
     return {
       label: 'Live',
       color: 'danger',
