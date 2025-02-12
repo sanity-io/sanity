339ba005c309924aab3e2c7049f32d1ebf2af90f
Marius Gundersen <me@mariusgundersen.net>
2025-02-12 10:16:38 +0100

    fix(structure): Don't modify EMPTY_PARAMS object

--- packages/sanity/src/structure/constants.ts
+++ packages/sanity/src/structure/constants.ts
@@ -3,7 +3,7 @@
  */
 export const _DEBUG = false
 
-export const EMPTY_PARAMS = {}
+export const EMPTY_PARAMS = Object.freeze({})
 export const LOADING_PANE = Symbol('LOADING_PANE')
 
 export const DOCUMENT_PANEL_PORTAL_ELEMENT = 'documentPanelPortalElement'
