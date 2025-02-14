6cc51f0df0340020b6b53188c04ec6afce05aa74
pedrobonamin <pedrobonamin@gmail.com>
2025-02-14 12:20:08 +0100

    fix(core): change publish now release action wording

--- packages/sanity/src/core/releases/i18n/resources.ts
+++ packages/sanity/src/core/releases/i18n/resources.ts
@@ -31,7 +31,7 @@ const releasesLocaleStrings = {
   /** Action text for unscheduling a release */
   'action.unschedule': 'Unschedule for publishing',
   /** Action text for publishing all documents in a release (and the release itself) */
-  'action.publish-all-documents': 'Publish all documents',
+  'action.publish-all-documents': 'Run release',
   /** Text for the review changes button in release tool */
   'action.review': 'Review changes',
   /** Action text for reverting a release */
