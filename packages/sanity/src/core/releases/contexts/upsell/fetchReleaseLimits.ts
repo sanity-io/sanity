a094e3d69b1e849ca2847f484558078b576470da
Jordan Lawrence <jordanl17@me.com>
2025-02-19 11:22:33 +0000

    chore: toggle stub or real API

--- packages/sanity/src/core/releases/contexts/upsell/fetchReleaseLimits.ts
+++ packages/sanity/src/core/releases/contexts/upsell/fetchReleaseLimits.ts
@@ -1,17 +1,21 @@
 import {type SanityClient} from '@sanity/client'
 import {type Observable} from 'rxjs'
 
+import {fetchReleasesLimits} from './ReleasesUpsellProvider'
+
 interface ReleaseLimits {
   orgActiveReleaseCount: number
   datasetReleaseLimit: number
   orgActiveReleaseLimit: number | null
 }
 
+const USE_STUB = false
+
 /**
  * @internal
  * fetches subscriptions for this project
  */
-export function fetchReleaseLimits({
+export function _fetchReleaseLimits({
   versionedClient,
 }: {
   versionedClient: SanityClient
@@ -22,8 +26,9 @@ export function fetchReleaseLimits({
       apiHost: 'https://api.sanity.work',
     })
     .observable.request<ReleaseLimits>({
-      uri: '/features',
-      //   uri: `project/${versionedClient.config().projectId}/new-content-release-allowed`,
+      uri: `project/${versionedClient.config().projectId}/new-content-release-allowed`,
       tag: 'new-content-release-allowed',
     })
 }
+
+export const fetchReleaseLimits = USE_STUB ? fetchReleasesLimits : _fetchReleaseLimits
