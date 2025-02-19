1010d9ee3aac6ab5666a7c2f2b1d5d9603847f63
Rita <anarita151@gmail.com>
2025-02-19 14:32:24 +0100

    docs(core): update checkWithPermissionGuard comment

--- packages/sanity/src/core/releases/store/createReleasePermissionsStore.ts
+++ packages/sanity/src/core/releases/store/createReleasePermissionsStore.ts
@@ -28,7 +28,7 @@ export function createReleasePermissionsStore(): useReleasePermissionsValue {
   /**
    * Checks if a release action can be performed by running a dry run of the given action
    *
-   * @param action - any of the actions from the ReleaseOperationStore, e.g. publishRelease should send in also the needed props
+   * @param action - any of the actions from the {@link ReleaseOperationStore}, e.g. publishRelease should send in also the needed props
    * @param args - the arguments to pass to the action (release id, etc)
    * @returns true or false depending if the user can perform the action
    */
