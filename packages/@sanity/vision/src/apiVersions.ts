39c07882d62ee123672990e1b222575e492d4ce3
Geoff Ball <geoff@gball.dev>
2025-02-19 21:09:13 -0700

    feat(vision): add API version 2025-02-19

--- packages/@sanity/vision/src/apiVersions.ts
+++ packages/@sanity/vision/src/apiVersions.ts
@@ -4,6 +4,7 @@ export const API_VERSIONS = [
   'v2021-03-25',
   'v2021-10-21',
   'v2022-03-07',
+  'v2025-02-19',
   `v${new Date().toISOString().split('T')[0]}`,
 ]
 export const [DEFAULT_API_VERSION] = API_VERSIONS.slice(-1)
