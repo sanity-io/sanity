ea842b1fea949aa56dc48a4add73a9723e6af758
Rita <anarita151@gmail.com>
2025-02-28 09:11:51 +0100

    chore(core): revert change on VersionInlineBadge

--- packages/sanity/src/core/releases/components/VersionInlineBadge.tsx
+++ packages/sanity/src/core/releases/components/VersionInlineBadge.tsx
@@ -12,7 +12,6 @@ const StyledVersionInlineBadge = styled.span<{$tone?: BadgeTone}>((props) => {
     color: var(--card-badge-${$tone ?? 'default'}-fg-color);
     background-color: var(--card-badge-${$tone ?? 'default'}-bg-color);
     border-radius: 3px;
-    padding: 0px 2px;
     text-decoration: none;
     padding: 0px 2px;
     font-weight: 500;
