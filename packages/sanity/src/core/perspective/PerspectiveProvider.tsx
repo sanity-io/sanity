71b68d5eb282665da3f48e305f7ae3abc37f5694
pedrobonamin <pedrobonamin@gmail.com>
2025-03-03 19:15:59 +0100

    fix(core): add isReleaseLocked or willBeUnpublished checks to document form

--- packages/sanity/src/core/perspective/PerspectiveProvider.tsx
+++ packages/sanity/src/core/perspective/PerspectiveProvider.tsx
@@ -3,8 +3,8 @@ import {PerspectiveContext} from 'sanity/_singletons'
 
 import {getReleasesPerspectiveStack} from '../releases/hooks/utils'
 import {useActiveReleases} from '../releases/store/useActiveReleases'
-import {getReleaseIdFromReleaseDocumentId} from '../releases/util/getReleaseIdFromReleaseDocumentId'
 import {EMPTY_ARRAY} from '../util/empty'
+import {getSelectedPerspective} from './getSelectedPerspective'
 import {type PerspectiveContextValue, type ReleaseId, type SelectedPerspective} from './types'
 
 /**
@@ -21,14 +21,10 @@ export function PerspectiveProvider({
 }) {
   const {data: releases} = useActiveReleases()
 
-  const selectedPerspective: SelectedPerspective = useMemo(() => {
-    if (!selectedPerspectiveName) return 'drafts'
-    if (selectedPerspectiveName === 'published') return 'published'
-    const selectedRelease = releases.find(
-      (release) => getReleaseIdFromReleaseDocumentId(release._id) === selectedPerspectiveName,
-    )
-    return selectedRelease || 'drafts'
-  }, [selectedPerspectiveName, releases])
+  const selectedPerspective: SelectedPerspective = useMemo(
+    () => getSelectedPerspective(selectedPerspectiveName, releases),
+    [selectedPerspectiveName, releases],
+  )
 
   const perspectiveStack = useMemo(
     () =>
