cc225f2f2d265d3d0da2ce8a56526aab022b7582
Rita <anarita151@gmail.com>
2025-03-03 08:14:33 +0100

    chore(code): add lint tag

--- packages/sanity/src/core/form/useDocumentForm.ts
+++ packages/sanity/src/core/form/useDocumentForm.ts
@@ -111,6 +111,7 @@ interface DocumentFormValue {
  *
  * Use this as a base point to create your own form.
  */
+// eslint-disable-next-line max-statements
 export function useDocumentForm(options: DocumentFormOptions): DocumentFormValue {
   const {
     documentType,
