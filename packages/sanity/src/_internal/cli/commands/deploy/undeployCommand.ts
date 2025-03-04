ab116ea5e64796e8eeaffbb9437baf1fd6ae0af1
Noah Gentile <nkgentile@users.noreply.github.com>
2025-03-04 10:50:48 -0800

    feat(cli): add unattended mode flag to `sanity undeploy` (#8130)

--- packages/sanity/src/_internal/cli/commands/deploy/undeployCommand.ts
+++ packages/sanity/src/_internal/cli/commands/deploy/undeployCommand.ts
@@ -4,9 +4,15 @@ import {
   type CliCommandDefinition,
 } from '@sanity/cli'
 
+import {type UndeployStudioActionFlags} from '../../actions/deploy/undeployAction'
+
 const helpText = `
+Options
+  -y, --yes Unattended mode, answers "yes" to any "yes/no" prompt and otherwise uses defaults
+
 Examples
   sanity undeploy
+  sanity undeploy --yes
 `
 
 const undeployCommand: CliCommandDefinition = {
@@ -14,7 +20,7 @@ const undeployCommand: CliCommandDefinition = {
   signature: '',
   description: 'Removes the deployed Sanity Studio from Sanity hosting',
   action: async (
-    args: CliCommandArguments<Record<string, unknown>>,
+    args: CliCommandArguments<UndeployStudioActionFlags>,
     context: CliCommandContext,
   ) => {
     const mod = await import('../../actions/deploy/undeployAction')
