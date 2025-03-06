403da8f370a865b738fe41411953e4a7f11b57cc
Per-Kristian Nordnes <per.kristian.nordnes@gmail.com>
2025-03-06 13:08:09 +0100

    test(core): support @testing-library DOM matcher types in vitest

---
+++ packages/sanity/test/vitest.setup.ts
@@ -0,0 +1,4 @@
+// Support for @testing-library/jest-dom via vitest
+// See https://github.com/testing-library/jest-dom/issues/515#issuecomment-1682870017
+// eslint-disable-next-line import/no-unassigned-import
+import '@testing-library/jest-dom'
