f8e0ac04c268f369d1275fb1c5512d75bb2d5009
Per-Kristian Nordnes <per.kristian.nordnes@gmail.com>
2025-03-06 10:33:59 +0100

    doc(core): update comment

--- packages/sanity/src/core/form/studio/inputs/globalDocumentReference/constants.ts
+++ packages/sanity/src/core/form/studio/inputs/globalDocumentReference/constants.ts
@@ -1,2 +1,2 @@
-// TODO: change this to 'v2025-02-19' when the API is released
+// TODO: change this to 'vXXXX-XX-XX' when the GDR API is released
 export const globalDocumentReferenceApiVersion = 'vX'
