eef276658a36c8c119786060fa0617524b330357
Per-Kristian Nordnes <per.kristian.nordnes@gmail.com>
2025-03-06 14:26:19 +0100

    fix(core): bump document pair action api min version
    This is needed to be able to write global document references through mutations.

--- packages/sanity/src/core/store/_legacy/document/document-pair/utils/actionsApiClient.ts
+++ packages/sanity/src/core/store/_legacy/document/document-pair/utils/actionsApiClient.ts
@@ -3,7 +3,7 @@ import {type SanityClient} from '@sanity/client'
 import {RELEASES_STUDIO_CLIENT_OPTIONS} from '../../../../../releases/util/releasesClient'
 import {type IdPair} from '../../types'
 
-const ACTIONS_API_MINIMUM_VERSION = 'X'
+const ACTIONS_API_MINIMUM_VERSION = '2025-02-19'
 
 export function actionsApiClient(client: SanityClient, idPair: IdPair): SanityClient {
   if (idPair.versionId) {
