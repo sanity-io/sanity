66e75fa650f03aed863bd4224e8cd76102731b64
pedrobonamin <pedrobonamin@gmail.com>
2025-03-10 11:54:13 +0100

    chore(vision): move vision result display to it's own component

--- packages/@sanity/vision/src/components/VisionGui.tsx
+++ packages/@sanity/vision/src/components/VisionGui.tsx
@@ -5,7 +5,7 @@ import {ErrorOutlineIcon, PlayIcon, StopIcon} from '@sanity/icons'
 import {Box, Button, Card, Flex, Hotkeys, Text, type ToastContextValue, Tooltip} from '@sanity/ui'
 import {isHotkey} from 'is-hotkey-esm'
 import {type ChangeEvent, createRef, PureComponent, type RefObject} from 'react'
-import {type TFunction, Translate} from 'sanity'
+import {type TFunction} from 'sanity'
 
 import {API_VERSIONS, DEFAULT_API_VERSION} from '../apiVersions'
 import {VisionCodeMirror, type VisionCodeMirrorHandle} from '../codemirror/VisionCodeMirror'
@@ -19,37 +19,23 @@ import {
 } from '../perspectives'
 import {type VisionProps} from '../types'
 import {encodeQueryString} from '../util/encodeQueryString'
-import {getCsvBlobUrl, getJsonBlobUrl} from '../util/getBlobUrl'
 import {getLocalStorage, type LocalStorageish} from '../util/localStorage'
 import {parseApiQueryString, type ParsedApiQueryString} from '../util/parseApiQueryString'
 import {prefixApiVersion} from '../util/prefixApiVersion'
 import {ResizeObserver} from '../util/resizeObserver'
 import {tryParseParams} from '../util/tryParseParams'
 import {validateApiVersion} from '../util/validateApiVersion'
-import {DelayedSpinner} from './DelayedSpinner'
 import {ParamsEditor, type ParamsEditorChangeEvent} from './ParamsEditor'
-import {QueryErrorDialog} from './QueryErrorDialog'
-import {ResultView} from './ResultView'
-import {SaveCsvButton, SaveJsonButton} from './SaveResultButtons'
 import {
   ControlsContainer,
-  DownloadsCard,
-  InputBackgroundContainer,
   InputBackgroundContainerLeft,
   InputContainer,
-  Result,
-  ResultContainer,
-  ResultFooter,
-  ResultInnerContainer,
-  ResultOuterContainer,
   Root,
-  SaveResultLabel,
   SplitpaneContainer,
   StyledLabel,
-  TimingsCard,
-  TimingsTextContainer,
 } from './VisionGui.styled'
 import {VisionGuiHeader} from './VisionGuiHeader'
+import {VisionGuiResult} from './VisionGuiResult'
 
 function nodeContains(node: Node, other: EventTarget | Node | null): boolean {
   if (!node || !other) {
@@ -679,9 +665,6 @@ export class VisionGui extends PureComponent<VisionGuiProps, VisionGuiState> {
       rawParams,
       url,
     } = this.state
-    const hasResult = !error && !queryInProgress && typeof queryResult !== 'undefined'
-    const jsonUrl = hasResult ? getJsonBlobUrl(queryResult) : ''
-    const csvUrl = hasResult ? getCsvBlobUrl(queryResult) : ''
 
     return (
       <Root
@@ -846,78 +829,16 @@ export class VisionGui extends PureComponent<VisionGuiProps, VisionGuiState> {
                 </InputContainer>
               </SplitPane>
             </Box>
-            <ResultOuterContainer direction="column" data-testid="vision-result">
-              <ResultInnerContainer flex={1}>
-                <ResultContainer
-                  flex={1}
-                  overflow="hidden"
-                  tone={error ? 'critical' : 'default'}
-                  $isInvalid={Boolean(error)}
-                >
-                  <Result overflow="auto">
-                    <InputBackgroundContainer>
-                      <Box marginLeft={3}>
-                        <StyledLabel muted>{t('result.label')}</StyledLabel>
-                      </Box>
-                    </InputBackgroundContainer>
-                    <Box padding={3} paddingTop={5}>
-                      {(queryInProgress || (listenInProgress && listenMutations.length === 0)) && (
-                        <Box marginTop={3}>
-                          <DelayedSpinner />
-                        </Box>
-                      )}
-                      {error && <QueryErrorDialog error={error} />}
-                      {hasResult && <ResultView data={queryResult} datasetName={dataset} />}
-                      {listenInProgress && listenMutations.length > 0 && (
-                        <ResultView data={listenMutations} datasetName={dataset} />
-                      )}
-                    </Box>
-                  </Result>
-                </ResultContainer>
-              </ResultInnerContainer>
-              {/* Execution time */}
-              <ResultFooter justify="space-between" direction={['column', 'column', 'row']}>
-                <TimingsCard paddingX={4} paddingY={3} sizing="border">
-                  <TimingsTextContainer align="center">
-                    <Box>
-                      <Text muted>
-                        {t('result.execution-time-label')}:{' '}
-                        {typeof queryTime === 'number'
-                          ? `${queryTime}ms`
-                          : t('result.timing-not-applicable')}
-                      </Text>
-                    </Box>
-                    <Box marginLeft={4}>
-                      <Text muted>
-                        {t('result.end-to-end-time-label')}:{' '}
-                        {typeof e2eTime === 'number'
-                          ? `${e2eTime}ms`
-                          : t('result.timing-not-applicable')}
-                      </Text>
-                    </Box>
-                  </TimingsTextContainer>
-                </TimingsCard>
-
-                {hasResult && (
-                  <DownloadsCard paddingX={4} paddingY={3} sizing="border">
-                    <SaveResultLabel muted>
-                      <Translate
-                        components={{
-                          SaveResultButtons: () => (
-                            <>
-                              <SaveJsonButton blobUrl={jsonUrl} />
-                              <SaveCsvButton blobUrl={csvUrl} />
-                            </>
-                          ),
-                        }}
-                        i18nKey="result.save-result-as-format"
-                        t={t}
-                      />
-                    </SaveResultLabel>
-                  </DownloadsCard>
-                )}
-              </ResultFooter>
-            </ResultOuterContainer>
+            <VisionGuiResult
+              error={error}
+              queryInProgress={queryInProgress}
+              queryResult={queryResult}
+              listenInProgress={listenInProgress}
+              listenMutations={listenMutations}
+              dataset={dataset}
+              queryTime={queryTime}
+              e2eTime={e2eTime}
+            />
           </SplitPane>
         </SplitpaneContainer>
       </Root>
