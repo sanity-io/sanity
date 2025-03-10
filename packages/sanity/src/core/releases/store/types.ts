db83d84070e69c84a3397465804717dedb45b756
Ash <ash@sanity.io>
2025-03-10 16:26:21 +0000

    feat(sanity): add `isReleaseType` type guard (#8875)

--- packages/sanity/src/core/releases/store/types.ts
+++ packages/sanity/src/core/releases/store/types.ts
@@ -10,7 +10,21 @@ import {type ReleasesReducerAction, type ReleasesReducerState} from './reducer'
 /**
  * @beta
  */
-export type ReleaseType = 'asap' | 'scheduled' | 'undecided'
+export const releaseTypes = ['asap', 'scheduled', 'undecided'] as const
+
+/**
+ * @beta
+ */
+export type ReleaseType = (typeof releaseTypes)[number]
+
+/**
+ * @beta
+ */
+export function isReleaseType(maybeReleaseType: unknown): maybeReleaseType is ReleaseType {
+  return (
+    typeof maybeReleaseType === 'string' && releaseTypes.includes(maybeReleaseType as ReleaseType)
+  )
+}
 
 /**
  * @beta
