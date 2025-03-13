bbcd58faf99478c4302685dbc6acddb7bfa31fe3
Eoin Falconer <eoin.falc@gmail.com>
2025-03-13 22:55:15 +0000

    chore: revert tests

--- test/e2e/tests/desk/deprecatedFields.spec.ts
+++ test/e2e/tests/desk/deprecatedFields.spec.ts
@@ -21,30 +21,17 @@ const allTypes = [
   'namedDeprecatedObject',
   'namedDeprecatedArray',
 ]
-test.describe('deprecated field indicators', () => {
-  // Use a single document across all tests
-  let documentCreated = false
 
-  test.beforeEach(async ({createDraftDocument}) => {
-    // Only create the document for the first test
-    if (!documentCreated) {
-      await createDraftDocument('/test/content/input-debug;deprecatedFields')
-      documentCreated = true
-    }
+for (const type of allTypes) {
+  test(`${type} type shows deprecated message`, async ({page, createDraftDocument}) => {
+    await createDraftDocument('/test/content/input-debug;deprecatedFields')
+
+    await expect(await page.getByTestId(`deprecated-badge-${type}`)).toBeVisible()
+
+    const deprecatedBadge = page.getByTestId(`deprecated-badge-${type}`)
+    const deprecatedMessage = page.getByTestId(`deprecated-message-${type}`)
+
+    await expect(deprecatedMessage).toBeVisible()
+    await expect(deprecatedBadge).toHaveText('deprecated')
   })
-
-  for (const type of allTypes) {
-    test(`${type} type shows deprecated message`, async ({page}) => {
-      // await createDraftDocument('/test/content/input-debug;deprecatedFields')
-      // wait for the next assertion to be created
-      await page.waitForSelector(`[data-testid="deprecated-badge-${type}"]`)
-      await expect(page.getByTestId(`deprecated-badge-${type}`)).toBeVisible()
-
-      const deprecatedBadge = page.getByTestId(`deprecated-badge-${type}`)
-      const deprecatedMessage = page.getByTestId(`deprecated-message-${type}`)
-
-      await expect(deprecatedMessage).toBeVisible()
-      await expect(deprecatedBadge).toHaveText('deprecated')
-    })
-  }
-})
+}
