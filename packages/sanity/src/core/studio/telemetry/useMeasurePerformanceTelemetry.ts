d32305df94b98de5a34b8209aec77cee650a353a
Bjørge Næss <bjoerge@gmail.com>
2025-03-17 18:48:40 +0100

    feat(telemetry): track INP performance from the field

---
+++ packages/sanity/src/core/studio/telemetry/useMeasurePerformanceTelemetry.ts
@@ -0,0 +1,61 @@
+import {useTelemetry} from '@sanity/telemetry/react'
+import {useCallback, useEffect} from 'react'
+
+import {PerformanceINPMeasured} from '../__telemetry__/performance.telemetry'
+
+function getElementIdentifier(node: Node | null) {
+  if (!node) {
+    return null
+  }
+  if (!(node instanceof Element)) {
+    return node.nodeName
+  }
+  // Note: Deliberately using classList instead of className here since className isn't always a string
+  // See https://developer.mozilla.org/en-US/docs/Web/API/Element/className#notes
+  const {nodeName, classList, id} = node
+  return (
+    nodeName.toLowerCase() +
+    (id ? `#${id}` : '') +
+    (classList ? `.${classList.value.replaceAll(' ', '.')}` : '')
+  )
+}
+
+function isPerformanceEventTiming(entry: PerformanceEntry): entry is PerformanceEventTiming {
+  return entry.entryType === 'event'
+}
+
+/**
+ * @internal
+ */
+export function useMeasurePerformanceTelemetry() {
+  const telemetry = useTelemetry()
+  const onEvent = useCallback(
+    (list: PerformanceObserverEntryList, observer: PerformanceObserver) => {
+      const entries = list.getEntries()
+
+      let maxEntry: PerformanceEventTiming | undefined = undefined
+      for (const entry of entries) {
+        if (!isPerformanceEventTiming(entry)) continue
+        if (entry.duration > (maxEntry?.duration || 0)) {
+          maxEntry = entry
+        }
+      }
+      if (!maxEntry) {
+        return
+      }
+      telemetry.log(PerformanceINPMeasured, {
+        target: getElementIdentifier(maxEntry.target),
+        interaction: maxEntry.name,
+        duration: maxEntry.duration,
+      })
+    },
+    [telemetry],
+  )
+  useEffect(() => {
+    const observer = new PerformanceObserver(onEvent)
+    observer.observe({type: 'event', buffered: true})
+    return () => {
+      observer.disconnect()
+    }
+  }, [onEvent])
+}
