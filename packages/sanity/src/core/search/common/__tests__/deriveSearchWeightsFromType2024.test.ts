6f90a10afba8a19453d16cf7b425320557df8308
Bjørge Næss <bjoerge@gmail.com>
2025-03-20 11:40:02 +0100

    add timeouts

--- packages/sanity/src/core/search/common/__tests__/deriveSearchWeightsFromType2024.test.ts
+++ packages/sanity/src/core/search/common/__tests__/deriveSearchWeightsFromType2024.test.ts
@@ -4,50 +4,54 @@ import {describe, expect, it} from 'vitest'
 import {createSchema} from '../../../schema/createSchema'
 import {deriveSearchWeightsFromType2024} from '../deriveSearchWeightsFromType2024'
 
-describe('deriveSearchWeightsFromType2024', () => {
-  it('works for schemas that branch out a lot', () => {
-    // schema of 60 "components" with 10 fields each
-    const range = [...Array(60).keys()]
+describe(
+  'deriveSearchWeightsFromType2024',
+  () => {
+    it('works for schemas that branch out a lot', () => {
+      // schema of 60 "components" with 10 fields each
+      const range = [...Array(60).keys()]
 
-    const componentRefs = range.map((index) => ({type: `component_${index}`}))
-    const components = range.map((index) =>
-      defineType({
-        name: `component_${index}`,
-        type: 'object',
-        fields: [
-          ...[...Array(10).keys()].map((fieldIndex) =>
-            defineField({name: `component_${index}_field_${fieldIndex}`, type: 'string'}),
-          ),
-          defineField({name: `children_${index}`, type: 'array', of: [...componentRefs]}),
-        ],
-      }),
-    )
-
-    const schema = createSchema({
-      name: 'default',
-      types: [
-        ...components,
+      const componentRefs = range.map((index) => ({type: `component_${index}`}))
+      const components = range.map((index) =>
         defineType({
-          name: 'testType',
-          type: 'document',
+          name: `component_${index}`,
+          type: 'object',
           fields: [
-            defineField({
-              name: 'components',
-              type: 'array',
-              of: [...componentRefs],
-            }),
+            ...[...Array(10).keys()].map((fieldIndex) =>
+              defineField({name: `component_${index}_field_${fieldIndex}`, type: 'string'}),
+            ),
+            defineField({name: `children_${index}`, type: 'array', of: [...componentRefs]}),
           ],
         }),
-      ],
-    })
+      )
 
-    expect(
-      deriveSearchWeightsFromType2024({
-        schemaType: schema.get('testType')!,
-        maxDepth: 5,
-      }),
-    ).toMatchObject({
-      typeName: 'testType',
+      const schema = createSchema({
+        name: 'default',
+        types: [
+          ...components,
+          defineType({
+            name: 'testType',
+            type: 'document',
+            fields: [
+              defineField({
+                name: 'components',
+                type: 'array',
+                of: [...componentRefs],
+              }),
+            ],
+          }),
+        ],
+      })
+
+      expect(
+        deriveSearchWeightsFromType2024({
+          schemaType: schema.get('testType')!,
+          maxDepth: 5,
+        }),
+      ).toMatchObject({
+        typeName: 'testType',
+      })
     })
-  })
-})
+  },
+  {timeout: 10_000},
+)
