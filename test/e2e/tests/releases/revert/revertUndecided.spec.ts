6676de4ac86515eb9f45e2e0d6e7717535dff99f
Rita <anarita151@gmail.com>
2025-03-20 11:03:54 +0100

    test(e2e): add revert undecided tests

---
+++ test/e2e/tests/releases/revert/revertUndecided.spec.ts
@@ -0,0 +1,151 @@
+import {expect} from '@playwright/test'
+import {test} from '@sanity/test'
+
+import {speciesDocumentNameASAP} from '../utils/__fixtures__/documents'
+import {partialUndecidedReleaseMetadata} from '../utils/__fixtures__/releases'
+import {
+  archiveAndDeleteRelease,
+  createDocument,
+  createRelease,
+  deleteRelease,
+  getRandomReleaseId,
+} from '../utils/methods'
+import {
+  publishAndConfirmReleaseMenu,
+  revertAndConfirmRelease,
+  scheduleAndConfirmReleaseMenu,
+} from '../utils/release-detail-ui-methods'
+
+// skip firefox due to flakyness
+const SKIP_BROWSERS = ['firefox']
+const skipIfBrowser = (browserName: string) => {
+  test.skip(SKIP_BROWSERS.includes(browserName), `Skip ${browserName} due to flakiness`)
+}
+test.describe('Revert Undecided', () => {
+  const undecidedReleaseIdTestOne: string = getRandomReleaseId()
+
+  test.beforeEach(async ({sanityClient, browserName}) => {
+    skipIfBrowser(browserName)
+    const dataset = sanityClient.config().dataset
+
+    await createRelease({
+      sanityClient,
+      dataset,
+      releaseId: undecidedReleaseIdTestOne,
+      metadata: partialUndecidedReleaseMetadata,
+    })
+  })
+
+  test.afterEach(async ({sanityClient, browserName, page}) => {
+    skipIfBrowser(browserName)
+    const dataset = sanityClient.config().dataset
+    // used for the new releases that have been created from the revert
+    const currentPageReleaseId = page.url().split('/').pop()
+
+    await Promise.all(
+      [
+        deleteRelease({sanityClient, dataset, releaseId: undecidedReleaseIdTestOne}),
+        currentPageReleaseId
+          ? archiveAndDeleteRelease({sanityClient, dataset, releaseId: currentPageReleaseId})
+          : null,
+      ].filter(Boolean),
+    )
+  })
+
+  // Schedule -> Wait -> Revert -> ASAP release
+  test('schedule Undecided release, wait for it to be published. When reverted, should be ASAP release', async ({
+    page,
+    sanityClient,
+    _testContext,
+    browserName,
+  }) => {
+    skipIfBrowser(browserName)
+    test.setTimeout(180000)
+    test.slow()
+    const versionDocumentId = _testContext.getUniqueDocumentId()
+
+    await createDocument(sanityClient, {
+      ...speciesDocumentNameASAP,
+      name: 'published',
+      _id: `${versionDocumentId}`,
+    })
+
+    await createDocument(sanityClient, {
+      ...speciesDocumentNameASAP,
+      _id: `versions.${undecidedReleaseIdTestOne}.${versionDocumentId}`,
+    })
+
+    await page.goto(`test/releases/${undecidedReleaseIdTestOne}`)
+
+    // schedule the release for (1 minute and 10 seconds) from now
+    // this seems to be the lowest interval that works without hanging / flaking
+    await scheduleAndConfirmReleaseMenu({
+      page,
+      date: new Date(new Date().setSeconds(new Date().getSeconds() + 70)),
+    })
+
+    // retention policy card
+    await expect(page.getByTestId('retention-policy-card')).toBeVisible()
+
+    await revertAndConfirmRelease({page})
+
+    await expect(page.getByTestId('revert-stage-success-link')).toBeVisible()
+    await page.getByTestId('revert-stage-success-link').click()
+
+    await expect(page.getByTestId('retention-policy-card')).not.toBeVisible()
+
+    await expect(
+      page.getByRole('button', {
+        name: `Reverting "${partialUndecidedReleaseMetadata.title}"`,
+        exact: true,
+      }),
+    ).toBeVisible()
+    await expect(page.getByTestId('release-type-picker')).toHaveText('ASAP')
+  })
+
+  // Publish -> Revert -> ASAP release
+  test('publish Undecided release, when reverted, should be ASAP release', async ({
+    page,
+    sanityClient,
+    _testContext,
+    browserName,
+  }) => {
+    skipIfBrowser(browserName)
+    test.setTimeout(180000)
+    test.slow()
+    const versionDocumentId = _testContext.getUniqueDocumentId()
+
+    await createDocument(sanityClient, {
+      ...speciesDocumentNameASAP,
+      name: 'published',
+      _id: `${versionDocumentId}`,
+    })
+
+    await createDocument(sanityClient, {
+      ...speciesDocumentNameASAP,
+      _id: `versions.${undecidedReleaseIdTestOne}.${versionDocumentId}`,
+    })
+
+    await page.goto(`test/releases/${undecidedReleaseIdTestOne}`)
+
+    await publishAndConfirmReleaseMenu({page})
+
+    // retention policy card
+    await expect(page.getByTestId('retention-policy-card')).toBeVisible()
+
+    await revertAndConfirmRelease({page})
+
+    await expect(page.getByTestId('revert-stage-success-link')).toBeVisible()
+    await page.getByTestId('revert-stage-success-link').click()
+
+    await expect(page.getByTestId('retention-policy-card')).not.toBeVisible()
+
+    await expect(
+      page.getByRole('button', {
+        name: `Reverting "${partialUndecidedReleaseMetadata.title}"`,
+        exact: true,
+      }),
+    ).toBeVisible()
+    await expect(page.getByTestId('release-type-picker')).toHaveText('ASAP')
+  })
+})
