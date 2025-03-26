66d89c3bb67193bfcbf0b3bf3c745c621ee88ba6
Jordan Lawrence <jordanl17@me.com>
2025-03-26 16:52:06 +0000

    test: trying to fix issue with not recording videos

--- test/e2e/tests/fixtures/harFixture.ts
+++ test/e2e/tests/fixtures/harFixture.ts
@@ -1,43 +1,29 @@
 /* eslint-disable react-hooks/rules-of-hooks */
-import fs from 'node:fs'
 import path from 'node:path'
 
-import {test as sanityFixtures} from '@sanity/test'
+import {test as base} from '@sanity/test'
 
-export const test = sanityFixtures.extend({
-  context: async ({browser}, use, testInfo) => {
+export const test = base.extend<{
+  harRecorder: void
+}>({
+  harRecorder: async ({browser}, use, testInfo) => {
     const harPath = path.join(testInfo.outputDir, 'network.har')
-    const videoPath = path.join(testInfo.outputDir, 'video.webm')
 
     const context = await browser.newContext({
       recordHar: {path: harPath},
-      recordVideo: {dir: testInfo.outputDir},
     })
 
-    await use(context) // give the context to the test
-
+    const page = await context.newPage()
+    await page.goto('about:blank')
     await context.close()
 
-    if (testInfo.status === testInfo.expectedStatus) {
-      try {
-        fs.unlinkSync(harPath)
-        fs.unlinkSync(videoPath)
-      } catch {
-        // ignore
-      }
-    } else {
+    if (testInfo.status !== testInfo.expectedStatus) {
       await testInfo.attach('Network traffic', {
         path: harPath,
         contentType: 'application/json',
       })
-
-      // ✅ Manually attach the video
-      if (fs.existsSync(videoPath)) {
-        await testInfo.attach('Video recording', {
-          path: videoPath,
-          contentType: 'video/webm',
-        })
-      }
     }
+
+    await use(undefined)
   },
 })
