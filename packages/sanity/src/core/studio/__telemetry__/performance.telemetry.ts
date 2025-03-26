e04d6f733e53cdf64556c5cfe243b80183c334c3
Bjørge Næss <bjoerge@gmail.com>
2025-03-26 11:34:41 +0100

    refactor(telemetry): tweak max sample rate

--- packages/sanity/src/core/studio/__telemetry__/performance.telemetry.ts
+++ packages/sanity/src/core/studio/__telemetry__/performance.telemetry.ts
@@ -8,9 +8,8 @@ export interface PerformanceINPMeasuredData {
 
 export const PerformanceINPMeasured = defineEvent<PerformanceINPMeasuredData>({
   name: 'Performance INP Measured',
-  //@ts-expect-error not yet supported, see https://github.com/sanity-io/telemetry/pull/6
-  // Do not sample more often than every 10s
-  maxSampleRate: 10_000,
+  // Sample at most every minute
+  maxSampleRate: 60_000,
   version: 1,
   description: 'Performance INP (Interaction to Next Paint) measurement happened',
 })
