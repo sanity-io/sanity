816830c31ba54213a1b72f698fa8893d4b361fc4
pedrobonamin <pedrobonamin@gmail.com>
2025-04-03 17:05:26 +0200

    fix(test): update array and imageArrayDrag tests

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -259,7 +259,13 @@ const config = {
             paths: [
               {
                 name: '@playwright/test',
-                importNames: ['test'],
+                importNames: ['test', 'default'],
+                message:
+                  'Please use named imports, e.g. `import {test} from "studio-test"` instead.',
+              },
+              {
+                name: '@sanity/test',
+                importNames: ['test', 'default'],
                 message:
                   'Please use named imports, e.g. `import {test} from "studio-test"` instead.',
               },
