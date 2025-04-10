21871839defe663a20722912dcdde48f31a9c1df
Simon MacDonald <simon.macdonald@gmail.com>
2025-04-10 14:33:33 -0400

    fix: log command uses name
    Signed-off-by: Simon MacDonald <simon.macdonald@gmail.com>

--- packages/@sanity/cli/src/commands/functions/logsFunctionsCommand.ts
+++ packages/@sanity/cli/src/commands/functions/logsFunctionsCommand.ts
@@ -2,16 +2,17 @@ import {type CliCommandDefinition} from '../../types'
 
 const helpText = `
 Options
-  --id <id> The ID of the function to retrieve logs for
+  --name <name> The name of the function to retrieve logs for
 
 Examples
   # Retrieve logs for Sanity Function abcd1234
-  sanity functions logs --id abcd1234
+  sanity functions logs --name echo
 `
 
 const defaultFlags = {
-  id: undefined,
+  name: '',
   limit: 50,
+  json: false,
 }
 
 const logsFunctionsCommand: CliCommandDefinition = {
@@ -31,24 +32,69 @@ const logsFunctionsCommand: CliCommandDefinition = {
       requireProject: false,
     })
 
-    if (flags.id) {
-      const token = client.config().token
-      const {blueprintsActions} = await import('@sanity/runtime-cli')
-      const blueprintConfig = blueprintsActions.blueprint.readConfigFile()
+    if (flags.name === '') {
+      print('You must provide a function name')
+      return
+    }
 
-      if (token && blueprintConfig?.projectId) {
-        const {functionsActions} = await import('@sanity/runtime-cli')
-        const result = await functionsActions.logs.logs(
-          flags.id,
-          {limit: flags.limit},
-          {token, projectId: blueprintConfig.projectId},
-        )
-        print(JSON.stringify(result, null, 2))
+    const token = client.config().token
+    const {blueprintsActions} = await import('@sanity/runtime-cli')
+
+    const {deployedStack} = await blueprintsActions.blueprint.readBlueprintOnDisk({
+      getStack: true,
+      token,
+    })
+
+    if (!deployedStack) print('Stack not found') // returns
+
+    const blueprintConfig = blueprintsActions.blueprint.readConfigFile()
+    const projectId = blueprintConfig?.projectId
+
+    const func = deployedStack?.resources?.find(
+      (r) => r?.type?.startsWith('sanity.function.') && r.name === flags.name,
+    )
+
+    if (!func) throw Error(`Unable to find function ${flags.name}`)
+
+    if (token && projectId) {
+      const {functionsActions} = await import('@sanity/runtime-cli')
+      const {ok, error, logs, total} = await functionsActions.logs.logs(
+        func.externalId,
+        {limit: flags.limit},
+        {token, projectId},
+      )
+
+      if (!ok) {
+        print(`Error: ${error || 'Unknown error'}`)
+        return
+      }
+
+      const filteredLogs = logs.filter(
+        (entry: {level: string; message: string}) => entry.level && entry.message,
+      )
+
+      if (filteredLogs.length === 0) {
+        print(`No logs found for function ${flags.name}`)
+        return
+      }
+
+      if (flags.json) {
+        print(JSON.stringify(filteredLogs, null, 2))
       } else {
-        print('You must run this command from a blueprints project')
+        print(`Found ${total} log entries for function ${flags.name}`)
+        if (logs.length < total) {
+          print(`Here are the last ${filteredLogs.length} entries`)
+        }
+        print('\n')
+
+        for (const log of filteredLogs) {
+          const {time, level, message} = log
+          const date = new Date(time)
+          print(`${date.toLocaleDateString()} ${date.toLocaleTimeString()} ${level} ${message}`)
+        }
       }
     } else {
-      print('You must provide a function ID')
+      print('You must run this command from a blueprints project')
     }
   },
 }
