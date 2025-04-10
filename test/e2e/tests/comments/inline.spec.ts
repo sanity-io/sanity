ddcc036186553a5aa561a2881860bfaff26268dd
pedrobonamin <pedrobonamin@gmail.com>
2025-04-10 15:59:43 +0200

    fix(test): use getByTestId

--- test/e2e/tests/comments/inline.spec.ts
+++ test/e2e/tests/comments/inline.spec.ts
@@ -22,20 +22,18 @@ async function inlineCommentCreationTest(props: InlineCommentCreationTestProps) 
   await createDraftDocument('/content/input-ci;commentsCI')
 
   // 2. Click the editor
-  const editor = page.locator('[data-testid="pt-editor"]')
+  const editor = page.getByTestId('pt-editor')
   await editor.click()
 
   // 3. Select all text in the editor.
   await page.getByTestId('pt-editor').waitFor(WAIT_OPTIONS)
-  const textbox = page
-    .locator('[data-testid="pt-editor"]')
-    .locator('div[role="textbox"][contenteditable=true]')
+  const textbox = page.getByTestId('pt-editor').locator('div[role="textbox"][contenteditable=true]')
   await textbox.selectText()
 
   // 4. Click on the floating comment button to start authoring a comment.
   await page.getByTestId('inline-comment-button').waitFor({...WAIT_OPTIONS, state: 'visible'})
 
-  const button = page.locator('[data-testid="inline-comment-button"]')
+  const button = page.getByTestId('inline-comment-button')
   await expect(button).toBeVisible()
   button.click({
     delay: 1000,
@@ -43,7 +41,7 @@ async function inlineCommentCreationTest(props: InlineCommentCreationTestProps) 
 
   // 5. Ensure the comment input field is displayed and the selected text is visually marked for commenting.
   await page.getByTestId('comment-input').waitFor({...WAIT_OPTIONS, state: 'visible'})
-  const commentInput = page.locator('[data-testid="comment-input"]')
+  const commentInput = page.getByTestId('comment-input')
   await expect(commentInput).toBeVisible()
 
   await page.locator('[data-inline-comment-state="authoring"]').waitFor(WAIT_OPTIONS)
@@ -53,7 +51,7 @@ async function inlineCommentCreationTest(props: InlineCommentCreationTestProps) 
   // 6. Author the comment and submit it by clicking the send button.
   const commentInputTextBox = commentInput.locator('div[role="textbox"]')
   await commentInputTextBox.fill('This is a comment')
-  const sendButton = commentInput.locator('[data-testid="comment-input-send-button"]')
+  const sendButton = commentInput.getByTestId('comment-input-send-button')
   await sendButton.click({delay: 1000})
 
   // 7. Verify the comment has been successfully added by checking for the presence of the
@@ -64,16 +62,14 @@ async function inlineCommentCreationTest(props: InlineCommentCreationTestProps) 
 
   // 8. Verify that the comments list is visible after the comment has been added.
   await page.getByTestId('comments-list').waitFor(WAIT_OPTIONS)
-  const commentsList = page.locator('[data-testid="comments-list"]')
+  const commentsList = page.getByTestId('comments-list')
   await expect(commentsList).toBeVisible()
 
   // 9. Verify that the comment appears within the list and correctly references the intended content.
   await page.getByTestId('comments-list-item').waitFor(WAIT_OPTIONS)
-  const commentsListItem = page.locator('[data-testid="comments-list-item"]')
+  const commentsListItem = page.getByTestId('comments-list-item')
   await expect(commentsListItem).toBeVisible()
-  const commentsListItemReferencedValue = page.locator(
-    '[data-testid="comments-list-item-referenced-value"]',
-  )
+  const commentsListItemReferencedValue = page.getByTestId('comments-list-item-referenced-value')
   await expect(commentsListItemReferencedValue).toHaveText(PTE_CONTENT_TEXT)
 }
 
