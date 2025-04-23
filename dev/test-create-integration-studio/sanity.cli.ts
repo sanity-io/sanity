2f1c4bc7c7d5dac1ca20bce8208084754720203e
pedrobonamin <pedrobonamin@gmail.com>
2025-04-23 13:54:21 +0200

    chore(test-studio): moves create-integration to staging env

--- dev/test-create-integration-studio/sanity.cli.ts
+++ dev/test-create-integration-studio/sanity.cli.ts
@@ -2,10 +2,10 @@ import {defineCliConfig} from 'sanity/cli'
 
 export default defineCliConfig({
   api: {
-    projectId: 'ppsg7ml5',
-    dataset: 'test',
+    projectId: 'exx11uqh',
+    dataset: 'playground',
   },
 
-  studioHost: 'create-integration-test',
+  studioHost: 'canvas-integration-staging',
   autoUpdates: false,
 })
--- dev/test-create-integration-studio/sanity.config.ts
+++ dev/test-create-integration-studio/sanity.config.ts
@@ -1,16 +1,13 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import {codeInput} from '@sanity/code-input'
+import {visionTool} from '@sanity/vision'
 import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
 
-import {CustomAction} from './CustomAction'
 import {schemaTypes} from './schema'
 
 const baseConfig = defineConfig({
-  plugins: [structureTool(), codeInput()],
-  document: {
-    actions: [CustomAction],
-  },
+  plugins: [structureTool(), codeInput(), visionTool()],
   projectId: 'ppsg7ml5',
   dataset: 'test',
   schema: {types: schemaTypes},
@@ -19,40 +16,15 @@ const baseConfig = defineConfig({
 export default defineConfig([
   {
     ...baseConfig,
-    title: 'Fallback origin',
-    name: 'fallback',
-    basePath: '/fallback',
-    beta: {
-      create: {
-        fallbackStudioOrigin: 'create-integration-test.sanity.studio',
-      },
-    },
-  },
-  {
-    ...baseConfig,
-    title: 'No fallback origin',
-    name: 'no-fallback',
-    basePath: '/no-fallback',
-  },
-  {
-    ...baseConfig,
-    title: 'Invalid fallback origin',
-    name: 'invalid-fallback',
-    basePath: '/invalid-fallback',
-    beta: {
-      create: {
-        fallbackStudioOrigin: 'does-not-exist',
-      },
-    },
-  },
-  {
-    ...baseConfig,
-    title: 'Opt out',
-    name: 'opt-out',
-    basePath: '/opt-out',
-    beta: {
-      create: {
-        startInCreateEnabled: false,
+    projectId: 'exx11uqh',
+    dataset: 'playground',
+    title: 'Playground',
+    name: 'playground',
+    basePath: '/playground',
+    apiHost: 'https://api.sanity.work',
+    features: {
+      canvas: {
+        enabled: true,
       },
     },
   },
