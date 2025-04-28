f4cbf8a6dd1bb170ebc0332713addeae8b1ba1c6
Bjørge Næss <bjoerge@gmail.com>
2025-04-28 11:00:06 +0200

    fix(cli): add multiple choice for upgrading when running sanity build/deploy

--- packages/sanity/src/_internal/cli/actions/build/buildAction.ts
+++ packages/sanity/src/_internal/cli/actions/build/buildAction.ts
@@ -77,18 +77,35 @@ export default async function buildSanityStudio(
 
     // If it is in unattended mode, we don't want to prompt
     if (result?.length && !unattendedMode) {
-      const shouldUpgrade = await prompt.single({
-        type: 'confirm',
+      const choice = await prompt.single({
+        type: 'list',
         message: chalk.yellow(
           `The following local package versions are different from the versions currently served at runtime.\n` +
             `When using auto updates, we recommend that you test locally with the same versions before deploying. \n\n` +
             `${result.map((mod) => ` - ${mod.pkg} (local version: ${mod.installed}, runtime version: ${mod.remote})`).join('\n')} \n\n` +
-            `Do you want to upgrade local versions instead?`,
+            `Do you want to upgrade local versions before deploying?`,
         ),
-        default: false,
+        choices: [
+          {
+            type: 'choice',
+            value: 'upgrade-and-proceed',
+            name: `Upgrade and proceed with ${args.groupOrCommand}`,
+          },
+          {
+            type: 'choice',
+            value: 'upgrade',
+            name: `Upgrade only. You will need to run the ${args.groupOrCommand} command again`,
+          },
+          {type: 'choice', name: 'Cancel', value: 'cancel'},
+        ],
+        default: 'upgrade-and-proceed',
       })
 
-      if (shouldUpgrade) {
+      if (choice === 'cancel') {
+        return {didCompile: false}
+      }
+
+      if (choice === 'upgrade' || choice === 'upgrade-and-proceed') {
         await upgradePackages(
           {
             packageManager: (await getPackageManagerChoice(workDir, {interactive: false})).chosen,
@@ -96,6 +113,10 @@ export default async function buildSanityStudio(
           },
           context,
         )
+
+        if (choice !== 'upgrade-and-proceed') {
+          return {didCompile: false}
+        }
       }
     }
   }
