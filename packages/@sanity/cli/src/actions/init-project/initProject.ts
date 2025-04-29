d4fb4e0daa1226e469fdce53c5491c6f86e87cf2
Rune Botten <rbotten@gmail.com>
2025-04-29 07:05:49 -0700

    fix(cli): Support unattended init within nextjs (#9198)

--- packages/@sanity/cli/src/actions/init-project/initProject.ts
+++ packages/@sanity/cli/src/actions/init-project/initProject.ts
@@ -290,6 +290,8 @@ export default async function initSanity(
     print('')
   }
 
+  const isNextJs = detectedFramework?.slug === 'nextjs'
+
   const flags = await prepareFlags()
 
   // We're authenticated, now lets select or create a project (for studios) or org (for core apps)
@@ -313,14 +315,16 @@ export default async function initSanity(
   }
 
   let initNext = false
-  const isNextJs = detectedFramework?.slug === 'nextjs'
   if (isNextJs) {
-    initNext = await prompt.single({
-      type: 'confirm',
-      message:
-        'Would you like to add configuration files for a Sanity project in this Next.js folder?',
-      default: true,
-    })
+    initNext =
+      unattended ||
+      (await prompt.single({
+        type: 'confirm',
+        message:
+          'Would you like to add configuration files for a Sanity project in this Next.js folder?',
+        default: true,
+      }))
+
     trace.log({
       step: 'useDetectedFramework',
       selectedOption: initNext ? 'yes' : 'no',
@@ -1229,12 +1233,15 @@ export default async function initSanity(
 
     if (unattended) {
       debug('Unattended mode, validating required options')
-      const requiredForUnattended = ['dataset', 'output-path'] as const
-      requiredForUnattended.forEach((flag) => {
-        if (!cliFlags[flag]) {
-          throw new Error(`\`--${flag}\` must be specified in unattended mode`)
-        }
-      })
+
+      if (!cliFlags['dataset' as const]) {
+        throw new Error(`\`--dataset\` must be specified in unattended mode`)
+      }
+
+      // output-path is not used in unattended mode within nextjs
+      if (!isNextJs && !cliFlags['output-path' as const]) {
+        throw new Error(`\`--output-path\` must be specified in unattended mode`)
+      }
 
       if (!cliFlags.project && !createProjectName) {
         throw new Error(
