4e7ac6ecc110ec0432467bd8a60c69dde7a4223a
Ash <ash@sanity.io>
2025-05-05 01:33:51 +0100

    feat(sanity): add `condition` prop to `CapabilityGate` (#9334)

--- packages/sanity/src/core/components/CapabilityGate.tsx
+++ packages/sanity/src/core/components/CapabilityGate.tsx
@@ -6,6 +6,7 @@ import {type Capability} from '../store/renderingContext/types'
 
 type Props = PropsWithChildren<{
   capability: Capability
+  condition?: 'available' | 'unavailable'
 }>
 
 /**
@@ -16,12 +17,20 @@ type Props = PropsWithChildren<{
  * providing a capability that may be overriden by the Studio rendering context. If the rendering
  * context provides this capability, the local implementation will not be rendered.
  */
-export const CapabilityGate: ComponentType<Props> = ({children, capability}) => {
+export const CapabilityGate: ComponentType<Props> = ({
+  children,
+  capability,
+  condition = 'unavailable',
+}) => {
   const renderingContextStore = useRenderingContextStore()
   const renderingContextCapabilities = useObservable(renderingContextStore.capabilities, {})
   const renderingContextHasCapability = renderingContextCapabilities[capability] === true
 
-  if (renderingContextHasCapability) {
+  if (condition === 'available' && !renderingContextHasCapability) {
+    return null
+  }
+
+  if (condition === 'unavailable' && renderingContextHasCapability) {
     return null
   }
 
