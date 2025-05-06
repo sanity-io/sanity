04e3f340cd598f40bc389c60e611fb8e331ce776
Bjørge Næss <bjoerge@gmail.com>
2025-05-06 15:10:43 +0200

    chore(test-studio): add a few more cases to the error reporting test tool

--- dev/test-studio/plugins/error-reporting-test/ErrorReportingTest.tsx
+++ dev/test-studio/plugins/error-reporting-test/ErrorReportingTest.tsx
@@ -1,8 +1,13 @@
 import {Button, Card, Flex, Stack} from '@sanity/ui'
-import {useCallback, useState} from 'react'
+import {lazy, useCallback, useEffect, useRef, useState} from 'react'
 
 function triggerCustomErrorOnEvent() {
   throw new Error('Custom error triggered')
+}
+
+function triggerImportError() {
+  const filename = '/does-not-exist.js'
+  import(filename)
 }
 
 function triggerTypeErrorOnEvent(evt: any) {
@@ -26,14 +31,47 @@ function triggerPromiseError() {
 export function ErrorReportingTest() {
   const [doRenderError, setRenderError] = useState(false)
   const handleShouldRenderWithError = useCallback(() => setRenderError(true), [])
+  const [triggerReactLazyImportError, setTriggerReactLazyImportError] = useState(false)
+  const [triggerEffectError, setTriggerEffectError] = useState(false)
+  const handleTriggerReactLazyImportError = useCallback(
+    () => setTriggerReactLazyImportError(true),
+    [],
+  )
+  const handleTriggerEffectError = useCallback(() => setTriggerEffectError(true), [])
+  const [triggerResizeObserverLoop, setTriggerResizeObserverLoop] = useState(false)
+  const handleTriggerResizeObserveLoop = useCallback(() => setTriggerResizeObserverLoop(true), [])
+
+  useEffect(() => {
+    if (triggerEffectError) {
+      throw new Error('Error triggered in effect')
+    }
+  }, [triggerEffectError])
 
   return (
     <Card padding={5}>
       <Flex>
         <Stack space={4}>
           <Button
+            text="Trigger import error from react callback"
+            onClick={triggerImportError}
+            tone="primary"
+          />
+
+          <Button
+            text="Trigger import error from React.lazy"
+            onClick={handleTriggerReactLazyImportError}
+            tone="primary"
+          />
+
+          <Button
             text="Trigger custom error on event handler"
             onClick={triggerCustomErrorOnEvent}
+            tone="primary"
+          />
+
+          <Button
+            text="Trigger error in useEffect"
+            onClick={handleTriggerEffectError}
             tone="primary"
           />
 
@@ -69,11 +107,17 @@ export function ErrorReportingTest() {
         </Stack>
       </Flex>
 
+      {triggerReactLazyImportError && <ReactLazyError />}
       {doRenderError && <WithRenderError />}
       {triggerResizeObserverLoop && <ResizeObserverLoop />}
     </Card>
   )
 }
+
+const ReactLazyError = lazy(() => {
+  const name = '/does-not-exist.js'
+  return import(name)
+})
 
 function WithRenderError({text}: any) {
   return <div>{text.toUpperCase()}</div>
