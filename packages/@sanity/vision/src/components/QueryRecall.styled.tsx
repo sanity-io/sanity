d79bb049d8c4905e7a8eac1f2d8ec3f5e0ed66d1
ChrisLaRocque <larocque.christopher@gmail.com>
2025-05-09 09:01:06 -0400

    Update QueryRecall.styled.tsx

--- packages/@sanity/vision/src/components/QueryRecall.styled.tsx
+++ packages/@sanity/vision/src/components/QueryRecall.styled.tsx
@@ -1,5 +1,5 @@
 import {Box, Stack} from '@sanity/ui'
-import styled from 'styled-components'
+import {styled} from 'styled-components'
 
 export const FixedHeader = styled(Stack)`
   position: sticky;
