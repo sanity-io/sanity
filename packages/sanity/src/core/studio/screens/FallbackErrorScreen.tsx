12ff0e012166709609f0330550c0780b7ca3b666
Ash <ash@sanity.io>
2025-05-13 12:32:49 +0100

    debug

--- packages/sanity/src/core/studio/screens/FallbackErrorScreen.tsx
+++ packages/sanity/src/core/studio/screens/FallbackErrorScreen.tsx
@@ -29,6 +29,7 @@ export function FallbackErrorScreen(props: {
               <Heading>{heading}</Heading>
               <Text>An error occurred that Sanity Studio was unable to recover from.</Text>
               <pre>{error.toString()}</pre>
+              <pre>{stack}</pre>
               {isProd && (
                 <Text>
                   <strong>To report this error,</strong> copy the error details and share them with
