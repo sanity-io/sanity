7843505ce9bad455034f4f529b1f5d5e2f462fc3
Jordan Lawrence <jordanl17@me.com>
2025-05-14 11:58:14 +0100

    chore: marking use of useClient() with no options as deprecated

--- packages/sanity/src/core/hooks/useClient.ts
+++ packages/sanity/src/core/hooks/useClient.ts
@@ -43,7 +43,7 @@ export function useClient(): SanityClient
  * }
  * ```
  */
-export function useClient(clientOptions?: SourceClientOptions): SanityClient
+export function useClient(clientOptions: SourceClientOptions): SanityClient
 export function useClient(clientOptions?: SourceClientOptions): SanityClient {
   const source = useSource()
   if (!clientOptions) {
