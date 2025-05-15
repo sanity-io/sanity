d63bb393a4c1781ccfd941f2f978a574212904ac
pedrobonamin <pedrobonamin@gmail.com>
2025-05-15 17:27:03 +0200

    fix(structure): add version information to history panel

--- packages/sanity/src/structure/panes/document/inspectors/changes/ChangesTabs.tsx
+++ packages/sanity/src/structure/panes/document/inspectors/changes/ChangesTabs.tsx
@@ -1,9 +1,17 @@
-import {CloseIcon} from '@sanity/icons'
-import {Card, Flex, TabList, TabPanel} from '@sanity/ui'
-import {type DocumentInspectorProps, useSource, useTranslation} from 'sanity'
+import {CloseIcon, InfoOutlineIcon} from '@sanity/icons'
+import {Box, Card, Flex, TabList, TabPanel, Text} from '@sanity/ui'
+import {useState} from 'react'
+import {
+  type DocumentInspectorProps,
+  isReleaseDocument,
+  Translate,
+  usePerspective,
+  useSource,
+  useTranslation,
+} from 'sanity'
 import {styled} from 'styled-components'
 
-import {Button, Tab} from '../../../../../ui-components'
+import {Button, Tab, Tooltip} from '../../../../../ui-components'
 import {usePaneRouter} from '../../../../components/paneRouter/usePaneRouter'
 import {structureLocaleNamespace} from '../../../../i18n'
 import {HISTORY_INSPECTOR_NAME} from '../../constants'
@@ -27,9 +35,10 @@ const isValidTab = (tab: string | undefined): tab is (typeof TABS)[number] =>
 export function ChangesTabs(props: DocumentInspectorProps) {
   const {params, setParams} = usePaneRouter()
   const source = useSource()
-
+  const [parentRef, setParentRef] = useState<HTMLDivElement | null>(null)
   const {t} = useTranslation(structureLocaleNamespace)
   const isReady = params?.inspect === HISTORY_INSPECTOR_NAME
+  const {selectedPerspective} = usePerspective()
 
   const paneRouterTab = isValidTab(params?.changesInspectorTab)
     ? params.changesInspectorTab
@@ -43,8 +52,21 @@ export function ChangesTabs(props: DocumentInspectorProps) {
       since: tab === 'history' ? undefined : params?.since,
     })
 
+  // eslint-disable-next-line no-nested-ternary
+  const perspectiveName = isReleaseDocument(selectedPerspective)
+    ? selectedPerspective.metadata.title
+    : selectedPerspective === 'drafts'
+      ? 'Draft'
+      : 'Published'
+
   return (
-    <FadeInFlex direction="column" padding={0} height="fill" data-ready={isReady ? '' : undefined}>
+    <FadeInFlex
+      direction="column"
+      padding={0}
+      height="fill"
+      data-ready={isReady ? '' : undefined}
+      ref={setParentRef}
+    >
       <Card paddingBottom={1}>
         <Flex align="center" padding={3} gap={2}>
           <TabList space={1} flex={1}>
@@ -70,6 +92,41 @@ export function ChangesTabs(props: DocumentInspectorProps) {
             onClick={props.onClose}
             tooltipProps={{content: t('document-inspector.close-button.tooltip')}}
           />
+        </Flex>
+      </Card>
+      <Card padding={2} marginBottom={3} marginX={3} tone="neutral" border radius={3}>
+        <Flex gap={2} align="flex-start">
+          <Tooltip
+            portal
+            placement="bottom-end"
+            boundaryElement={parentRef}
+            content={
+              <Box flex={1} padding={1}>
+                <Text size={1}>
+                  <Translate
+                    t={t}
+                    values={{
+                      perspective: perspectiveName,
+                    }}
+                    i18nKey="changes.banner.tooltip"
+                  />
+                </Text>
+              </Box>
+            }
+          >
+            <Text size={0} muted>
+              <InfoOutlineIcon fontSize={1} />
+            </Text>
+          </Tooltip>
+          <Text size={0} muted>
+            <Translate
+              t={t}
+              values={{
+                perspective: perspectiveName,
+              }}
+              i18nKey="changes.banner.description"
+            />
+          </Text>
         </Flex>
       </Card>
 
