c329ad10fa81f68c7f0e19df31b27a61c4a0816e
Magnus Holm <judofyr@gmail.com>
2025-05-20 09:30:40 +0200

    feat(schema): introduce internal 'own props' on each type
    This adds a new property (`_internal_ownProps`) which contains the
    properties on a type which has _not_ been inherited. This is intended
    to be used when we're serializing the schema since then we'd like
    each type to be as "minimal" as possible and match the definitions.

--- packages/@sanity/schema/src/legacy/types/string.ts
+++ packages/@sanity/schema/src/legacy/types/string.ts
@@ -1,7 +1,8 @@
 import {pick} from 'lodash'
 
 import primitivePreview from '../preview/primitivePreview'
-import {DEFAULT_OVERRIDEABLE_FIELDS} from './constants'
+import {DEFAULT_OVERRIDEABLE_FIELDS, OWN_PROPS_NAME} from './constants'
+import {hiddenGetter} from './utils'
 
 const OVERRIDABLE_FIELDS = [...DEFAULT_OVERRIDEABLE_FIELDS]
 
@@ -17,10 +18,16 @@ export const StringType = {
     return STRING_CORE
   },
   extend(subTypeDef: any) {
-    const parsed = Object.assign(pick(STRING_CORE, OVERRIDABLE_FIELDS), subTypeDef, {
-      type: STRING_CORE,
+    const ownProps = {
+      ...subTypeDef,
       preview: primitivePreview,
+    }
+
+    const parsed = Object.assign(pick(STRING_CORE, OVERRIDABLE_FIELDS), ownProps, {
+      type: STRING_CORE,
     })
+
+    hiddenGetter(parsed, OWN_PROPS_NAME, ownProps)
 
     return subtype(parsed)
 
@@ -30,9 +37,11 @@ export const StringType = {
           return parent
         },
         extend: (extensionDef: any) => {
-          const current = Object.assign({}, parent, pick(extensionDef, OVERRIDABLE_FIELDS), {
+          const subOwnProps = pick(extensionDef, OVERRIDABLE_FIELDS)
+          const current = Object.assign({}, parent, subOwnProps, {
             type: parent,
           })
+          hiddenGetter(current, OWN_PROPS_NAME, subOwnProps)
           return subtype(current)
         },
       }
--- packages/@sanity/schema/src/legacy/types/url.ts
+++ packages/@sanity/schema/src/legacy/types/url.ts
@@ -1,7 +1,8 @@
 import {pick} from 'lodash'
 
 import primitivePreview from '../preview/primitivePreview'
-import {DEFAULT_OVERRIDEABLE_FIELDS} from './constants'
+import {DEFAULT_OVERRIDEABLE_FIELDS, OWN_PROPS_NAME} from './constants'
+import {hiddenGetter} from './utils'
 
 const OVERRIDABLE_FIELDS = [...DEFAULT_OVERRIDEABLE_FIELDS]
 
@@ -17,10 +18,17 @@ export const UrlType = {
     return URL_CORE
   },
   extend(subTypeDef: any) {
-    const parsed = Object.assign(pick(URL_CORE, OVERRIDABLE_FIELDS), subTypeDef, {
-      type: URL_CORE,
+    const ownProps = {
+      ...subTypeDef,
       preview: primitivePreview,
+    }
+
+    const parsed = Object.assign(pick(URL_CORE, OVERRIDABLE_FIELDS), ownProps, {
+      type: URL_CORE,
     })
+
+    hiddenGetter(parsed, OWN_PROPS_NAME, ownProps)
+
     return subtype(parsed)
 
     function subtype(parent: any) {
@@ -29,9 +37,11 @@ export const UrlType = {
           return parent
         },
         extend: (extensionDef: any) => {
-          const current = Object.assign({}, parent, pick(extensionDef, OVERRIDABLE_FIELDS), {
+          const subOwnownProps = pick(extensionDef, OVERRIDABLE_FIELDS)
+          const current = Object.assign({}, parent, subOwnownProps, {
             type: parent,
           })
+          hiddenGetter(current, OWN_PROPS_NAME, subOwnownProps)
           return subtype(current)
         },
       }
