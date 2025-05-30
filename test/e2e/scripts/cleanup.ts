84bcdee84b42d47f68ba7fbddc0fddc84f5e508a
Jordan Lawrence <jordanl17@me.com>
2025-05-30 01:03:54 +0100

    test: narrow scope of env var requirements for e2e dataset cleanup

--- test/e2e/scripts/cleanup.ts
+++ test/e2e/scripts/cleanup.ts
@@ -1,5 +1,25 @@
 /* eslint-disable no-console */
-import {SANITY_E2E_PROJECT_ID, SANITY_E2E_SESSION_TOKEN} from '../env'
+/* eslint-disable no-process-env */
+/* eslint-disable no-process-exit */
+
+import {loadEnvFiles} from '../../../scripts/utils/loadEnvFiles'
+
+loadEnvFiles()
+
+const SANITY_E2E_SESSION_TOKEN = process.env.SANITY_E2E_SESSION_TOKEN!
+const SANITY_E2E_PROJECT_ID = process.env.SANITY_E2E_PROJECT_ID!
+
+if (!SANITY_E2E_SESSION_TOKEN) {
+  console.error('Missing `SANITY_E2E_SESSION_TOKEN` environment variable.')
+  console.error('See `test/e2e/README.md` for details.')
+  process.exit(1)
+}
+
+if (!SANITY_E2E_PROJECT_ID) {
+  console.error('Missing `SANITY_E2E_PROJECT_ID` environment variable.')
+  console.error('See `test/e2e/README.md` for details.')
+  process.exit(1)
+}
 
 const API_BASE_URL = `https://${SANITY_E2E_PROJECT_ID}.api.sanity.work/v2025-05-22`
 
