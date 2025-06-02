5bd7da10fa79fa2c5c618d79ab068e67c886cf36
Bjørge Næss <bjoerge@gmail.com>
2025-06-02 14:31:49 +0100

    fixup! fixup! chore(ci): fix missing env var for cleanup script

--- test/e2e/envVars.ts
+++ test/e2e/envVars.ts
@@ -5,15 +5,11 @@ loadEnvFiles()
 type KnownEnvVar = 'SANITY_E2E_PROJECT_ID' | 'SANITY_E2E_DATASET' | 'SANITY_E2E_SESSION_TOKEN'
 
 export function readEnv(name: KnownEnvVar): string {
-  const val = findEnv(name)
+  const val = process.env[name]
   if (val === undefined) {
     throw new Error(
       `Missing required environment variable "${name}" 'See \`test/e2e/README.md\` for details.'`,
     )
   }
   return val
-}
-
-export function findEnv(name: KnownEnvVar): string | undefined {
-  return process.env[name]
 }
