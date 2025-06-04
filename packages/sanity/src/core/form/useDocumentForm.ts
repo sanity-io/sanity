fa7f811080aa088dfc594a286f70fb2cecd4ee52
Rita <anarita151@gmail.com>
2025-06-04 13:43:50 +0200

    chore(structure, core): update unusued consts

--- packages/sanity/src/core/form/useDocumentForm.ts
+++ packages/sanity/src/core/form/useDocumentForm.ts
@@ -149,7 +149,7 @@ export function useDocumentForm(options: DocumentFormOptions): DocumentFormValue
   const schema = useSchema()
   const presenceStore = usePresenceStore()
   const {data: releases} = useActiveReleases()
-  const {data: documentVersions, loading: documentVersionsLoading} = useDocumentVersions({
+  const {data: documentVersions} = useDocumentVersions({
     documentId,
   })
   const {selectedReleaseId} = usePerspective()
@@ -302,8 +302,10 @@ export function useDocumentForm(options: DocumentFormOptions): DocumentFormValue
     isDraftId(previousId || '') ? getPublishedId(previousId || '') : previousId || '',
   )
   const formDocumentValue = useMemo(() => {
-    if (value._system?.delete && document) {
-      return getFormDocumentValue ? getFormDocumentValue(document) : value
+    if (value._system?.delete) {
+      if (document) return getFormDocumentValue ? getFormDocumentValue(document) : value
+
+      return {}
     }
     return getFormDocumentValue ? getFormDocumentValue(value) : value
   }, [getFormDocumentValue, value, document])
