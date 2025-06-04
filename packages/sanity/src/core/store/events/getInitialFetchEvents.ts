221c5c13bb239f8002c7d87ae5d3a03f42bd2f62
pedrobonamin <pedrobonamin@gmail.com>
2025-06-04 09:25:37 +0200

    fix(core): history cleared event should be the last event in the events list

--- packages/sanity/src/core/store/events/getInitialFetchEvents.ts
+++ packages/sanity/src/core/store/events/getInitialFetchEvents.ts
@@ -96,12 +96,14 @@ export function getInitialFetchEvents({client, documentId}: InitialFetchEventsOp
         )
         const needsHistoryClearedEvent = events.length === 0 && transactions.length > 0
         if (needsHistoryClearedEvent) {
-          const newTimestamp = new Date(editEvents[0].timestamp).getTime() - 1
+          const clearedEventTimestamp =
+            new Date(editEvents[editEvents.length - 1].timestamp).getTime() - 1
+
           editEvents.unshift({
             type: 'historyCleared',
             documentId,
             id: HISTORY_CLEARED_EVENT_ID,
-            timestamp: new Date(newTimestamp).toISOString(),
+            timestamp: new Date(clearedEventTimestamp).toISOString(),
             author: '',
             documentVariantType,
           })
