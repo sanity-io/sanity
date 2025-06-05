88321264dd315df9ecb66d3103218dc773c98987
Jordan Lawrence <jordanl17@me.com>
2025-06-05 11:26:06 +0100

    fix(core): restore delete document uses `lastRevision` (#9552)

--- packages/sanity/src/core/store/_legacy/document/document-pair/operations/restore.ts
+++ packages/sanity/src/core/store/_legacy/document/document-pair/operations/restore.ts
@@ -1,9 +1,10 @@
+import {type DocumentRevision} from '../../../history'
 import {isLiveEditEnabled} from '../utils/isLiveEditEnabled'
 import {type OperationImpl} from './types'
 
-export const restore: OperationImpl<[fromRevision: string]> = {
+export const restore: OperationImpl<[fromRevision: DocumentRevision]> = {
   disabled: (): false => false,
-  execute: ({historyStore, schema, idPair, typeName}, fromRevision: string) => {
+  execute: ({historyStore, schema, idPair, typeName}, fromRevision: DocumentRevision) => {
     const targetId = isLiveEditEnabled(schema, typeName) ? idPair.publishedId : idPair.draftId
     return historyStore.restore(idPair.publishedId, targetId, fromRevision)
   },
--- packages/sanity/src/core/store/_legacy/document/document-pair/serverOperations/restore.ts
+++ packages/sanity/src/core/store/_legacy/document/document-pair/serverOperations/restore.ts
@@ -1,9 +1,13 @@
+import {type DocumentRevision} from '../../../history'
 import {type OperationImpl} from '../operations/types'
 import {isLiveEditEnabled} from '../utils/isLiveEditEnabled'
 
-export const restore: OperationImpl<[fromRevision: string]> = {
+export const restore: OperationImpl<[fromRevision: DocumentRevision]> = {
   disabled: (): false => false,
-  execute: ({snapshots, historyStore, schema, idPair, typeName}, fromRevision: string) => {
+  execute: (
+    {snapshots, historyStore, schema, idPair, typeName},
+    fromRevision: DocumentRevision,
+  ) => {
     const targetId = isLiveEditEnabled(schema, typeName) ? idPair.publishedId : idPair.draftId
     return historyStore.restore(idPair.publishedId, targetId, fromRevision, {
       fromDeleted: !snapshots.draft && !snapshots.published,
