88a1cc9f3127a58bd35c1684d394e81f973e9672
Cody Olsen <stipsan@gmail.com>
2025-06-12 11:49:20 +0200

    refactor: eslint + oxlint `@sanity/diff`

---
+++ packages/@sanity/diff/eslint.config.mjs
@@ -0,0 +1,22 @@
+import {dirname, resolve} from 'node:path'
+import {fileURLToPath} from 'node:url'
+
+import baseConfig from '@repo/eslint-config'
+import {defineConfig} from 'eslint/config'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+const ROOT_PATH = resolve(__dirname, '../../..')
+
+export default defineConfig([
+  ...baseConfig,
+  {
+    name: '@sanity/diff/overrides',
+    rules: {
+      'complexity': 'off',
+      'max-depth': 'off',
+      'id-length': 'off',
+      'import/no-extraneous-dependencies': ['error', {packageDir: [ROOT_PATH, __dirname]}],
+    },
+  },
+])
