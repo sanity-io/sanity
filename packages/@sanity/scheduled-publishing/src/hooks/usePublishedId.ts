eda6a305a253ef27a7fe98ed8b19ad4c678219ee
Rita <anarita151@gmail.com>
2025-06-12 09:51:41 +0200

    chore(@sanity): update to use es2021 like the monorepo

--- packages/@sanity/scheduled-publishing/src/hooks/usePublishedId.ts
+++ packages/@sanity/scheduled-publishing/src/hooks/usePublishedId.ts
@@ -1,5 +1,5 @@
 import {useMemo} from 'react'
 
 export function usePublishedId<T extends string | undefined>(id: T): T {
-  return useMemo(() => (id ? id.replace('drafts.', '') : undefined) as T, [id])
+  return useMemo(() => (id ? id.replaceAll('drafts.', '') : undefined) as T, [id])
 }
