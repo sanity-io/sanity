ccd9546253b70946eabb95bb1b9fa60196119789
Cole Peters <cole@colepeters.com>
2025-06-12 16:02:27 -0500

    feat(cli): Improve guidance after SDK app init

--- packages/@sanity/cli/src/packageManager/installPackages.ts
+++ packages/@sanity/cli/src/packageManager/installPackages.ts
@@ -18,32 +18,44 @@ export async function installDeclaredPackages(
     encoding: 'utf8',
     env: getPartialEnvWithNpmPath(cwd),
     cwd,
-    stdio: 'inherit',
+    stdio: 'pipe',
   }
 
-  const npmArgs = ['install', '--legacy-peer-deps']
+  // results of running execa with the selected package manager
   let result: ExecaReturnValue<string> | undefined
-  if (packageManager === 'npm') {
-    output.print(`Running 'npm ${npmArgs.join(' ')}'`)
-    result = await execa('npm', npmArgs, execOptions)
-  } else if (packageManager === 'yarn') {
-    const yarnArgs = ['install']
-    output.print(`Running 'yarn ${yarnArgs.join(' ')}'`)
-    result = await execa('yarn', yarnArgs, execOptions)
-  } else if (packageManager === 'pnpm') {
-    const pnpmArgs = ['install']
-    output.print(`Running 'pnpm ${pnpmArgs.join(' ')}'`)
-    result = await execa('pnpm', pnpmArgs, execOptions)
-  } else if (packageManager === 'bun') {
-    const bunArgs = ['install']
-    output.print(`Running 'bun ${bunArgs.join(' ')}'`)
-    result = await execa('bun', bunArgs, execOptions)
-  } else if (packageManager === 'manual') {
-    output.print(`Manual installation selected - run 'npm ${npmArgs.join(' ')}' or similar`)
+
+  type PackageManagerLibs = Exclude<PackageManager, 'manual'>
+  type InstallerArgs = {[key in PackageManagerLibs]: string[]}
+
+  const installerArgs: InstallerArgs = {
+    npm: ['install', '--legacy-peer-deps'],
+    yarn: ['install'],
+    pnpm: ['install'],
+    bun: ['install'],
   }
 
-  if (result?.exitCode || result?.failed) {
-    throw new Error('Dependency installation failed')
+  async function handleInstall(cmd: PackageManager, args: InstallerArgs[PackageManagerLibs]) {
+    // Start a spinner for the install process
+    const progress = output.spinner(`Running ${cmd} ${args.join(' ')}\n`).start()
+
+    // Perform the install command with execa
+    result = await execa(cmd, args, execOptions)
+
+    // If the install fails, log execa's stdout and throw…
+    if (result?.exitCode || result?.failed) {
+      progress.fail()
+      output.print(result.stdout)
+      throw new Error('Dependency installation failed')
+    } else {
+      // …otherwise, just mark the install as successful
+      progress.succeed()
+    }
+  }
+
+  if (packageManager === 'manual') {
+    output.print(`Manual installation selected — run 'npm ${installerArgs.npm} or equivalent'`)
+  } else {
+    await handleInstall(packageManager, installerArgs[packageManager])
   }
 }
 
