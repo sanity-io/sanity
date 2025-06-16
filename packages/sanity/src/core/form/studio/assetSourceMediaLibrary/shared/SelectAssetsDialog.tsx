7d1edb0758521178fccaed8a0491d51cefbb498f
Per-Kristian Nordnes <per.kristian.nordnes@gmail.com>
2025-06-16 15:10:23 +0200

    feat(core): add realtime validation to ML asset picker

--- packages/sanity/src/core/form/studio/assetSourceMediaLibrary/shared/SelectAssetsDialog.tsx
+++ packages/sanity/src/core/form/studio/assetSourceMediaLibrary/shared/SelectAssetsDialog.tsx
@@ -1,9 +1,21 @@
-import {type AssetFromSource, type FileSchemaType, type ImageSchemaType} from '@sanity/types'
-import {Flex, Stack, useTheme, useToast} from '@sanity/ui'
+import {
+  type AssetFromSource,
+  type FileSchemaType,
+  type ImageSchemaType,
+  type SanityDocument,
+  type ValidationMarker,
+} from '@sanity/types'
+import {Box, Card, Flex, Text, useTheme, useToast} from '@sanity/ui'
 import {type ReactNode, useCallback, useState} from 'react'
 
+import {useDocumentPane} from '../../../../../structure/panes/document/useDocumentPane'
 import {Button} from '../../../../../ui-components'
+import {useClient} from '../../../../hooks'
+import {useSchema} from '../../../../hooks/useSchema'
 import {useTranslation} from '../../../../i18n'
+import {useWorkspace} from '../../../../studio'
+import {validateItem} from '../../../../validation/validateDocument'
+import {FormFieldValidationStatus} from '../../../components/formField/FormFieldValidationStatus'
 import {useAuthType} from '../hooks/useAuthType'
 import {useLinkAssets} from '../hooks/useLinkAssets'
 import {useMediaLibraryId} from '../hooks/useMediaLibraryId'
@@ -52,6 +64,44 @@ export function SelectAssetsDialog(props: SelectAssetsDialogProps): ReactNode {
 
   const [assetSelection, setAssetSelection] = useState<AssetSelectionItem[]>(props.selection)
   const [didSelect, setDidSelect] = useState(false)
+  const [validation, setValidation] = useState([] as ValidationMarker[])
+
+  const client = useClient({apiVersion: mediaLibraryConfig.__internal.apiVersion})
+  const workspace = useWorkspace()
+
+  const schema = useSchema()
+
+  const {value: document} = useDocumentPane()
+
+  const validateSelection = useCallback(
+    async (assetSelectionItem: AssetSelectionItem) => {
+      const value = {
+        _type: 'mainImage',
+        media: {
+          _ref: `media-library:${libraryId}:${assetSelectionItem.asset._id}`,
+          _type: 'globalDocumentReference',
+          _weak: true,
+        },
+      }
+      const getClient = () => client
+      const result = await validateItem({
+        value: value,
+        getClient,
+        path: [],
+        schema: schema,
+        type: schemaType,
+        parent: document,
+        i18n: workspace.i18n,
+        environment: 'studio',
+        document: document as SanityDocument,
+        getDocumentExists: async () => {
+          return true
+        },
+      })
+      return result
+    },
+    [client, document, libraryId, schema, schemaType, workspace.i18n],
+  )
 
   const pluginApiVersion = mediaLibraryConfig.__internal.pluginApiVersion
   const appBasePath = mediaLibraryConfig.__internal.appBasePath
@@ -76,17 +126,36 @@ export function SelectAssetsDialog(props: SelectAssetsDialogProps): ReactNode {
       console.error(error)
       setDidSelect(false)
     }
-  }, [assetSelection, onClose, onSelect, onLinkAssets, t, toast])
+  }, [assetSelection, onLinkAssets, onSelect, onClose, toast, t])
 
   const handleClose = useCallback(() => {
     onClose()
   }, [onClose])
 
-  const handlePluginMessage = useCallback((message: PluginPostMessage) => {
-    if (message.type === 'assetSelection') {
-      setAssetSelection(message.selection)
-    }
-  }, [])
+  const handleAssetSelection = useCallback(
+    async (assetSelectionItems: AssetSelectionItem[]) => {
+      if (assetSelectionItems.length === 0) {
+        setValidation([])
+        return
+      }
+      const validationResult = await validateSelection(assetSelectionItems[0])
+      const hasErrors = validationResult.some((marker) => marker.level === 'error')
+      if (hasErrors) {
+        setValidation(validationResult)
+      }
+    },
+    [validateSelection],
+  )
+
+  const handlePluginMessage = useCallback(
+    (message: PluginPostMessage) => {
+      if (message.type === 'assetSelection') {
+        setAssetSelection(message.selection)
+        handleAssetSelection(message.selection)
+      }
+    },
+    [handleAssetSelection],
+  )
 
   const {setIframe} = usePluginPostMessage(appHost, handlePluginMessage)
   if (!open) {
@@ -95,18 +164,34 @@ export function SelectAssetsDialog(props: SelectAssetsDialogProps): ReactNode {
 
   return (
     <AppDialog
-      animate
-      header={dialogHeaderTitle}
+      // header={dialogHeaderTitle}
       id="media-library-plugin-dialog-select-assets"
-      onClose={handleClose}
+      // onClose={handleClose}
       open
       ref={ref}
       data-testid="media-library-plugin-dialog-select-assets"
       width={3}
       footer={
-        <Flex width="full" gap={3} justify="flex-end" padding={2}>
-          <Stack space={3}>
-            <Flex width="full" gap={3} justify="flex-end" padding={3}>
+        <Card
+          width="full"
+          height="fill"
+          padding={3}
+          radius={2}
+          shadow={1}
+          style={{position: 'relative', minHeight: '2dvh'}}
+          tone="primary"
+        >
+          <Flex width="full" gap={3} justify="flex-start" padding={3}>
+            <Box flex={1}>
+              <Text size={1} weight="semibold">
+                {dialogHeaderTitle}
+              </Text>
+            </Box>
+
+            <Flex width="full" gap={3} justify="flex-end" align="center">
+              {validation.length > 0 && (
+                <FormFieldValidationStatus fontSize={2} placement="top" validation={validation} />
+              )}
               <Button
                 mode="ghost"
                 onClick={handleClose}
@@ -116,13 +201,16 @@ export function SelectAssetsDialog(props: SelectAssetsDialogProps): ReactNode {
               <Button
                 onClick={handleSelect}
                 loading={didSelect}
-                disabled={assetSelection.length === 0}
+                disabled={
+                  assetSelection.length === 0 ||
+                  validation.some((marker) => marker.level === 'error')
+                }
                 text={t('asset-source.dialog.button.select')}
                 size="large"
               />
             </Flex>
-          </Stack>
-        </Flex>
+          </Flex>
+        </Card>
       }
     >
       <Iframe ref={setIframe} src={iframeUrl} />
