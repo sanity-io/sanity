14e4f7a1b813e424c705d7c27567b0b4a4e89b23
Per-Kristian Nordnes <per.kristian.nordnes@gmail.com>
2025-06-16 15:00:03 +0200

    feat: fullscreen ML dialog

--- packages/sanity/src/core/form/studio/assetSourceMediaLibrary/shared/Dialog.tsx
+++ packages/sanity/src/core/form/studio/assetSourceMediaLibrary/shared/Dialog.tsx
@@ -3,6 +3,13 @@ import {Dialog} from '@sanity/ui'
 import {styled} from 'styled-components'
 
 export const AppDialog = styled(Dialog)`
+  [data-ui='Card'] {
+    position: absolute;
+    top: 0;
+    right: 0;
+    left: 0;
+    bottom: 0;
+  }
   [data-ui='Card']:first-child {
     flex: 1;
   }
