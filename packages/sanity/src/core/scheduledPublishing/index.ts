4372cdabe8fdb99dc91209ecd5a32a1b333012db
Rita <anarita151@gmail.com>
2025-06-17 13:24:44 +0200

    test(core): fix issue with exports
    chore: update linting rules

--- packages/sanity/src/core/scheduledPublishing/index.ts
+++
@@ -1,5 +0,0 @@
-/**
- * This are util files that were initially created to help with the scheduled-publishing plugin but grew beyond just the scheduled publishing.
- * They likely should be kept and then case by case check if you should remove them.
- */
-export * from './contexts'
