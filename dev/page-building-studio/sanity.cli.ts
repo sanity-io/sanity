cf87c38c49388ab2d65e74b00b26dbde0294fa44
Cody Olsen <81981+stipsan@users.noreply.github.com>
2025-06-30 13:18:26 +0200

    chore: enable profiling of page building studio (#9853)

--- dev/page-building-studio/sanity.cli.ts
+++ dev/page-building-studio/sanity.cli.ts
@@ -1,4 +1,5 @@
 import {defineCliConfig} from 'sanity/cli'
+import {type UserConfig} from 'vite'
 
 export default defineCliConfig({
   api: {
@@ -6,4 +7,20 @@ export default defineCliConfig({
     dataset: 'page-building',
   },
   reactCompiler: {target: '18'},
+  vite(viteConfig: UserConfig): UserConfig {
+    return {
+      ...viteConfig,
+      esbuild: {
+        ...viteConfig.esbuild,
+        minifyIdentifiers: false,
+      },
+      resolve: {
+        ...viteConfig.resolve,
+        alias: {
+          ...viteConfig.resolve?.alias,
+          'react-dom/client': require.resolve('react-dom/profiling'),
+        },
+      },
+    }
+  },
 })
