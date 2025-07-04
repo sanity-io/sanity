b330d31abe06241e1e521b5deddf843ee73deb6e
Rita <anarita151@gmail.com>
2025-07-04 09:27:59 +0200

    refactor(core): make DocTitle (inspector title) use the same logic as the rest of places that require the documentTitle

--- packages/sanity/src/structure/components/DocTitle.tsx
+++ packages/sanity/src/structure/components/DocTitle.tsx
@@ -1,7 +1,8 @@
 import {type SanityDocumentLike} from '@sanity/types'
-import {unstable_useValuePreview as useValuePreview, useSchema, useTranslation} from 'sanity'
+import {useSchema, useTranslation} from 'sanity'
 
 import {structureLocaleNamespace} from '../i18n'
+import {useDocumentTitle} from '../panes'
 
 export interface DocTitleProps {
   document: SanityDocumentLike
@@ -12,23 +13,15 @@ export function DocTitle(props: DocTitleProps) {
   const schema = useSchema()
   const schemaType = schema.get(documentValue._type)
   const {t} = useTranslation(structureLocaleNamespace)
-
-  const {error, value} = useValuePreview({
-    schemaType: schemaType!,
-    value: documentValue,
-  })
+  const {title} = useDocumentTitle()
 
   if (!schemaType) {
     return <code>{t('doc-title.unknown-schema-type.text', {schemaType: documentValue._type})}</code>
   }
 
-  if (error) {
-    return <>{t('doc-title.error.text', {errorMessage: error.message})}</>
-  }
-
   return (
     <>
-      {value?.title || (
+      {title || (
         <span style={{color: 'var(--card-muted-fg-color)'}}>{t('doc-title.fallback.text')}</span>
       )}
     </>
