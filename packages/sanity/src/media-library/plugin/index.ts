7e261419d6a3048733d682c677a325514395c3ca
Ash <ash@sanity.io>
2025-07-10 14:36:56 +0100

    refactor: remove obsolete bindings

--- packages/sanity/src/media-library/plugin/index.ts
+++ packages/sanity/src/media-library/plugin/index.ts
@@ -9,16 +9,6 @@ export const MEDIA_LIBRARY_NAME = 'sanity/media-library'
 /**
  * @internal
  */
-export const MEDIA_LIBRARY_TOOL_NAME = 'media-library'
-
-/**
- * @internal
- */
-export const MEDIA_LIBRARY_INTENT = 'media-library'
-
-/**
- * @internal
- */
 export const mediaLibrary = definePlugin({
   name: MEDIA_LIBRARY_NAME,
   schema: {
