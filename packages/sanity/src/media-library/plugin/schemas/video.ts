eec335d0cf6185e99514ddcc0795d4dbe8c804df
Ash <ash@sanity.io>
2025-07-14 11:24:01 +0100

    fix(sanity): ensure global document reference preview configuration is present for serialized `sanity.video` schema type

--- packages/sanity/src/media-library/plugin/schemas/video.ts
+++ packages/sanity/src/media-library/plugin/schemas/video.ts
@@ -24,7 +24,18 @@ export const video = defineType({
       to: [
         {
           type: 'sanity.asset',
-          preview: {},
+          // Note: the preview configuration is not used in Studio, but it must be present in order
+          // to satisfy global document reference schema validation.
+          //
+          // If an empty object is supplied, the preview configuration will be omitted when the
+          // schema is extracted. Studio will be completely functional, but an error will occur
+          // when another service (e.g. Agent Actions) attempts to parse the serialized schema and
+          // finds the required preview configuration is absent.
+          preview: {
+            select: {
+              title: '_id',
+            },
+          },
         },
       ],
       resourceType: 'media-library',
