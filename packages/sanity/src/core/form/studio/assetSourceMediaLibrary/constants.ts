16e847408b2ecaaa0448fedf50dc7b4c58958b58
James Warner <jmswrnr@users.noreply.github.com>
2025-07-31 16:55:28 +0100

    chore: set this back

--- packages/sanity/src/core/form/studio/assetSourceMediaLibrary/constants.ts
+++ packages/sanity/src/core/form/studio/assetSourceMediaLibrary/constants.ts
@@ -14,5 +14,5 @@ export const CDN_HOST_PRODUCTION = 'https://sanity-cdn.com'
 export const API_HOST_PRODUCTION = 'https://api.sanity.io'
 
 // Local dev Media Library server
-export const IS_LOCAL_DEV = true && isDev // Set boolean to true to work against local Media Library dev server (but keep isDev check to avoid accidentally committing it)
+export const IS_LOCAL_DEV = false && isDev // Set boolean to true to work against local Media Library dev server (but keep isDev check to avoid accidentally committing it)
 export const LOCAL_DEV_FRONTEND_HOST = 'http://localhost:3001'
