8d8f522b2b340606003f972f2c4e6414c3fbca8d
Bjørge Næss <bjoerge@gmail.com>
2025-08-01 13:52:13 +0200

    fix(sanity): deprecate i18n strings no longer in use

--- packages/sanity/src/core/i18n/bundles/studio.ts
+++ packages/sanity/src/core/i18n/bundles/studio.ts
@@ -1248,9 +1248,9 @@ export const studioLocaleStrings = defineLocalesResources('studio', {
   'new-document.open-dialog-aria-label': 'Create new document',
   /** Title for "Create new document" dialog */
   'new-document.title': 'Create new document',
-  /** Label for button that will make the browser reload when users' studio version is out-of-date */
+  /** @deprecated Label for button that will make the browser reload when users' studio version is out-of-date */
   'package-version.new-package-available.reload-button': 'Push to reload',
-  /** Title of the alert for studio users when packages in their studio are out-of-date */
+  /** @deprecated Title of the alert for studio users when packages in their studio are out-of-date */
   'package-version.new-package-available.title': 'Sanity Studio is ready to update!',
   /** Label for action to invite members to the current studio project */
   'presence.action.manage-members': 'Invite members',
