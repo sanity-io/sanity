73a871f7c40fa504453c8f41511fdd7847d8aa2b
Rita <anarita151@gmail.com>
2025-08-11 14:22:23 +0200

    test(core): update search test

--- packages/sanity/src/core/releases/tool/detail/__tests__/ReleaseSummary.test.tsx
+++ packages/sanity/src/core/releases/tool/detail/__tests__/ReleaseSummary.test.tsx
@@ -88,15 +88,16 @@ const releaseDocuments = [
   },
 ]
 
+type ScrollableProps = {scrollContainerRef: {current: HTMLDivElement | null}}
+
 const ScrollContainer: FC<PropsWithChildren> = ({children}) => {
   const [ref, setRef] = useState<HTMLDivElement | null>(null)
 
   return (
     <div style={{height: '400px'}} ref={setRef}>
-      {cloneElement(
-        children as ReactElement<{scrollContainerRef: RefObject<HTMLDivElement | null>}>,
-        {scrollContainerRef: {current: ref}},
-      )}
+      {cloneElement(children as ReactElement<ScrollableProps>, {
+        scrollContainerRef: {current: ref},
+      })}
     </div>
   )
 }
@@ -133,8 +134,11 @@ describe('ReleaseSummary', () => {
   setupVirtualListEnv()
 
   describe('for an active release', () => {
+    let onSearchTermChange: (value: string) => void
+
     beforeEach(async () => {
-      await renderTest({})
+      onSearchTermChange = vi.fn()
+      await renderTest({onSearchTermChange})
       await vi.waitFor(() => screen.getByTestId('document-table-card'), {
         timeout: 5000,
         interval: 500,
@@ -184,9 +188,7 @@ describe('ReleaseSummary', () => {
         })
       })
 
-      const [searchedFirstDocument] = screen.getAllByTestId('table-row')
-
-      within(searchedFirstDocument).getByText('Second document')
+      expect(onSearchTermChange).toHaveBeenCalledWith('Second')
     })
 
     it('Allows for adding a document to an active release', () => {
