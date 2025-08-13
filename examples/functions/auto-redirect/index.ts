fd0d501c03dba2c6a8692e023fd0d33497b15d58
Jon Burbridge <jonathan.burbridge@sanity.io>
2025-08-13 10:28:43 +0100

    docs(examples): add auto redirect function

---
+++ examples/functions/auto-redirect/index.ts
@@ -0,0 +1,57 @@
+import { createClient } from "@sanity/client";
+import { documentEventHandler } from "@sanity/functions";
+
+export const handler = documentEventHandler(async ({ context, event }) => {
+  const client = createClient({
+    ...context.clientOptions,
+    useCdn: false,
+    apiVersion: "2025-05-08",
+  });
+
+  
+  const { before, after } = event.data;
+  const slug = after?.slug?.current;
+  const beforeSlug = before?.slug?.current;
+
+  if (!slug || !beforeSlug) {
+    console.log("No slug or beforeSlug");
+    return;
+  }
+  if (slug !== beforeSlug) {
+    // check if redirect already exists
+    const existingRedirect = await client.fetch(
+      `*[_type == "redirect" && source.current == "${beforeSlug}"][0]`
+    );
+    if (existingRedirect) {
+      console.log(`Redirect already exists for source ${beforeSlug}`);
+      return;
+    }
+    // check for loops
+    const loopRedirect = await client.fetch(
+      `*[_type == "redirect" && source.current == "${slug}" && destination.current == "${beforeSlug}"][0]`
+    );
+    if (loopRedirect) {
+      console.log("Redirect loop detected");
+      return;
+    }
+    const redirect = {
+      _type: "redirect",
+      source: {
+        current: beforeSlug,
+      },
+      destination: {
+        current: slug,
+      },
+      permanent: true,
+    };
+
+    try {
+      const res = await client.create(redirect)
+      console.log(`🔗 Redirect from ${beforeSlug} to ${slug} was created ${JSON.stringify(res)}`);
+    } catch (error) {
+      console.log(error);
+    }
+  } else {
+    console.log("Slug did not change");
+  }
+});
