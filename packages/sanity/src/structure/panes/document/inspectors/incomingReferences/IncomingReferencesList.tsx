a7e900c6c21778aa489d227c0834a63efbea3616
pedrobonamin <pedrobonamin@gmail.com>
2025-08-13 11:43:48 +0200

    feat(structure): add incoming references inspector

---
+++ packages/sanity/src/structure/panes/document/inspectors/incomingReferences/IncomingReferencesList.tsx
@@ -0,0 +1,126 @@
+// TODO: Remove this eslint-disable
+/* eslint-disable i18next/no-literal-string */
+import {Box, Card, Stack, Text} from '@sanity/ui'
+import {useMemo} from 'react'
+import {useObservable} from 'react-rx'
+import {filter, map, type Observable, startWith} from 'rxjs'
+import {mergeMapArray} from 'rxjs-mergemap-array'
+import {
+  type DocumentPreviewStore,
+  getPublishedId,
+  LoadingBlock,
+  type PublishedId,
+  type SanityDocument,
+  useDocumentPreviewStore,
+  useSchema,
+} from 'sanity'
+
+import {useDocumentPane} from '../../useDocumentPane'
+import {IncomingReferenceDocument} from './IncomingReferenceDocument'
+
+const TypeTitle = ({type}: {type: string}) => {
+  const schema = useSchema()
+  const schemaType = schema.get(type)
+  return (
+    <Box padding={2}>
+      <Text size={1} weight="medium">
+        {schemaType?.title}
+      </Text>
+    </Box>
+  )
+}
+
+const INITIAL_STATE = {
+  list: [],
+  loading: true,
+}
+function getIncomingReferences({
+  publishedId,
+  documentPreviewStore,
+}: {
+  publishedId: PublishedId
+  documentPreviewStore: DocumentPreviewStore
+}): Observable<{
+  list: {
+    type: string
+    documents: SanityDocument[]
+  }[]
+  loading: boolean
+}> {
+  return documentPreviewStore
+    .unstable_observeDocumentIdSet(`references("${publishedId}")`, {
+      insert: 'prepend',
+    })
+    .pipe(
+      map((state) => state.documentIds),
+      mergeMapArray((id: string) => {
+        return documentPreviewStore.unstable_observeDocument(id).pipe(
+          filter(Boolean),
+          map((doc) => doc),
+        )
+      }),
+      // Remove duplicates due to different versions of the same document.
+      map((documents) => {
+        const seenPublishedId: string[] = []
+        return documents.filter((doc) => {
+          const pId = getPublishedId(doc._id)
+          if (seenPublishedId.includes(pId)) return false
+
+          seenPublishedId.push(pId)
+          return true
+        })
+      }),
+      map((documents) => {
+        const types = documents.map((doc) => doc._type)
+        const uniqueTypes = [...new Set(types)].sort((a, b) => a.localeCompare(b))
+        return uniqueTypes.map((type) => ({
+          type,
+          documents: documents.filter((doc) => doc._type === type),
+        }))
+      }),
+      map((list) => ({list, loading: false})),
+      startWith(INITIAL_STATE),
+    )
+}
+
+export function IncomingReferencesList() {
+  const {documentId} = useDocumentPane()
+  const documentPreviewStore = useDocumentPreviewStore()
+  const publishedId = getPublishedId(documentId)
+  const references$ = useMemo(
+    () => getIncomingReferences({publishedId, documentPreviewStore}),
+    [publishedId, documentPreviewStore],
+  )
+  const references = useObservable(references$, INITIAL_STATE)
+
+  if (references.loading) {
+    return <LoadingBlock showText title={'Loading documents'} />
+  }
+
+  if (references.list.length === 0) {
+    return (
+      <Box padding={2}>
+        <Card border radius={2} padding={1} tone="default">
+          <Box paddingY={3} paddingX={2}>
+            <Text size={1} muted>
+              No incoming references found
+            </Text>
+          </Box>
+        </Card>
+      </Box>
+    )
+  }
+
+  return (
+    <>
+      {references.list.map((type) => (
+        <Stack key={type.type} padding={2} space={1} marginBottom={2}>
+          <TypeTitle type={type.type} />
+          {type.documents.map((document) => (
+            <IncomingReferenceDocument key={document._id} document={document} />
+          ))}
+        </Stack>
+      ))}
+    </>
+  )
+}
