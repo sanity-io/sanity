22f2ead73c8d849819178df0bf3616f44d66c1cd
pedrobonamin <pedrobonamin@gmail.com>
2025-08-13 15:46:46 +0200

    feat(structure): deep link to reference from incoming references view

--- packages/sanity/src/core/form/inputs/ReferenceInput/ReferenceInputPreview.tsx
+++ packages/sanity/src/core/form/inputs/ReferenceInput/ReferenceInputPreview.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable complexity */
 import {LaunchIcon as OpenInNewTabIcon, SyncIcon as ReplaceIcon, TrashIcon} from '@sanity/icons'
 import {type Reference} from '@sanity/types'
 import {Box, Card, type CardTone, Flex, Menu, MenuDivider, Stack} from '@sanity/ui'
@@ -7,8 +8,10 @@ import {
   type ForwardedRef,
   forwardRef,
   useCallback,
+  useEffect,
   useMemo,
   useRef,
+  useState,
 } from 'react'
 import {IntentLink} from 'sanity/router'
 
@@ -16,6 +19,7 @@ import {MenuButton, MenuItem, TooltipDelayGroupProvider} from '../../../../ui-co
 import {ContextMenuButton} from '../../../components/contextMenuButton'
 import {useTranslation} from '../../../i18n'
 import {EMPTY_ARRAY} from '../../../util/empty'
+import {withFocusRing} from '../../components/withFocusRing/withFocusRing'
 import {useDidUpdate} from '../../hooks/useDidUpdate'
 import {set, unset} from '../../patch'
 import {PreviewReferenceValue} from './PreviewReferenceValue'
@@ -26,6 +30,8 @@ import {ReferenceStrengthMismatchAlertStrip} from './ReferenceStrengthMismatchAl
 import {type ReferenceInfo, type ReferenceInputProps} from './types'
 import {type Loadable, useReferenceInfo} from './useReferenceInfo'
 import {useReferenceInput} from './useReferenceInput'
+
+const WithFocusRingCard = withFocusRing(Card)
 
 function getTone({
   readOnly,
@@ -219,12 +225,19 @@ export function ReferenceInputPreview(props: ReferenceInputProps & {children: Re
     [onPathFocus],
   )
 
+  const [cardRef, setCardRef] = useState<HTMLDivElement | null>(null)
+  useEffect(() => {
+    if (focused && cardRef) {
+      cardRef.focus()
+    }
+  }, [focused, cardRef])
+
   if (isEditing) {
     return <Box>{children}</Box>
   }
 
   return (
-    <Card border radius={2} padding={1} tone={tone}>
+    <WithFocusRingCard border $radius={2} padding={1} tone={tone} ref={setCardRef} tabIndex={-1}>
       <Stack space={1}>
         <Flex gap={1} align="center" style={{lineHeight: 0}}>
           <TooltipDelayGroupProvider>
@@ -255,6 +268,6 @@ export function ReferenceInputPreview(props: ReferenceInputProps & {children: Re
         </Flex>
         {footer}
       </Stack>
-    </Card>
+    </WithFocusRingCard>
   )
 }
