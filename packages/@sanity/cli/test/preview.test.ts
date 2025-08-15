2a3e2d64f2518f9cd5edfff3c6c974f3b688d61f
Bjørge Næss <bjoerge@gmail.com>
2025-08-15 09:45:30 +0200

    refactor(test): refactor testServerCommand, remove flake

--- packages/@sanity/cli/test/preview.test.ts
+++ packages/@sanity/cli/test/preview.test.ts
@@ -9,32 +9,36 @@ import {runSanityCmdCommand, studioNames, studiosPath} from './shared/environmen
 describeCliTest('CLI: `sanity preview`', () => {
   describe.each(studioNames)('%s', (name) => {
     testConcurrent('preview (no basepath)', async () => {
-      const {html: previewHtml, stderr} = await testServerCommand({
+      const {html: previewHtml} = await testServerCommand({
         command: 'preview',
         args: ['--port', '3330', '../../static'],
         basePath: '/',
         port: 3330,
         cwd: path.join(studiosPath, name),
         expectedTitle: 'Sanity Static',
+        expectedOutput: ({stderr}) => {
+          expect(stderr).toContain('Could not determine base path from index.html') // Warning
+        },
       })
       expect(previewHtml).toContain('<h1>This is static.</h1>')
-      expect(stderr).toContain('Could not determine base path from index.html') // Warning
     })
 
     testConcurrent('preview (infers basepath)', async () => {
-      const {html: previewHtml, stdout} = await testServerCommand({
+      const {html: previewHtml} = await testServerCommand({
         command: 'preview',
         args: ['--port', '3456', '../../static-basepath'],
         basePath: '/some-base-path',
         port: 3456,
         cwd: path.join(studiosPath, name),
         expectedTitle: 'Sanity Static, Base Pathed',
+        expectedOutput: ({stdout}) => {
+          expect(stdout).toContain('Using resolved base path from static build')
+          expect(stdout).toContain('/some-base-path')
+          expect(stdout).toContain(':3456/some-base-path')
+        },
       })
 
       expect(previewHtml).toContain('<h1>This is static, served from a base path.</h1>')
-      expect(stdout).toContain('Using resolved base path from static build')
-      expect(stdout).toContain('/some-base-path')
-      expect(stdout).toContain(':3456/some-base-path')
     })
 
     testConcurrent('preview (root basepath)', async () => {
