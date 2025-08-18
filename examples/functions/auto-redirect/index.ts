eff92b48197061b3a3dddcca568db6d75763f720
Jon Burbridge <jonathan.burbridge@sanity.io>
2025-08-18 10:37:17 +0100

    style: remove Negated condition

--- examples/functions/auto-redirect/index.ts
+++ examples/functions/auto-redirect/index.ts
@@ -16,41 +16,41 @@ export const handler = documentEventHandler(async ({context, event}) => {
     console.log('No slug or beforeSlug')
     return
   }
-  if (slug !== beforeSlug) {
-    // check if redirect already exists
-    const existingRedirect = await client.fetch(
-      `*[_type == "redirect" && source.current == "${beforeSlug}"][0]`,
-    )
-    if (existingRedirect) {
-      console.log(`Redirect already exists for source ${beforeSlug}`)
-      return
-    }
-    // check for loops
-    const loopRedirect = await client.fetch(
-      `*[_type == "redirect" && source.current == "${slug}" && destination.current == "${beforeSlug}"][0]`,
-    )
-    if (loopRedirect) {
-      console.log('Redirect loop detected')
-      return
-    }
-    const redirect = {
-      _type: 'redirect',
-      source: {
-        current: beforeSlug,
-      },
-      destination: {
-        current: slug,
-      },
-      permanent: true,
-    }
-
-    try {
-      const res = await client.create(redirect)
-      console.log(`🔗 Redirect from ${beforeSlug} to ${slug} was created ${JSON.stringify(res)}`)
-    } catch (error) {
-      console.log(error)
-    }
-  } else {
+  if (slug === beforeSlug) {
     console.log('Slug did not change')
+    return
+  }
+  // check if redirect already exists
+  const existingRedirect = await client.fetch(
+    `*[_type == "redirect" && source.current == "${beforeSlug}"][0]`,
+  )
+  if (existingRedirect) {
+    console.log(`Redirect already exists for source ${beforeSlug}`)
+    return
+  }
+  // check for loops
+  const loopRedirect = await client.fetch(
+    `*[_type == "redirect" && source.current == "${slug}" && destination.current == "${beforeSlug}"][0]`,
+  )
+  if (loopRedirect) {
+    console.log('Redirect loop detected')
+    return
+  }
+  const redirect = {
+    _type: 'redirect',
+    source: {
+      current: beforeSlug,
+    },
+    destination: {
+      current: slug,
+    },
+    permanent: true,
+  }
+
+  try {
+    const res = await client.create(redirect)
+    console.log(`🔗 Redirect from ${beforeSlug} to ${slug} was created ${JSON.stringify(res)}`)
+  } catch (error) {
+    console.log(error)
   }
 })
