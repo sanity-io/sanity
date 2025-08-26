c045b43fe0c693a3a7375a86035cac3cf166df37
Jordan Lawrence <jordanl17@me.com>
2025-08-26 18:18:50 +0100

    test: add temporary failing test to trigger retry workflow

--- packages/@sanity/util/test/createSafeJsonParser.test.ts
+++ packages/@sanity/util/test/createSafeJsonParser.test.ts
@@ -11,6 +11,14 @@ test('parse JSON', () => {
   expect(parse('{"someNumber": 42}')).toEqual({someNumber: 42})
 })
 
+// Temporary test to trigger GitHub Actions retry workflow
+// TODO: Remove this test after verifying retry functionality
+test('TEMP: trigger retry workflow - will fail on first attempt', () => {
+  // This test will always fail to trigger the retry workflow
+  // Remove this test once retry functionality is verified
+  expect(true).toBe(false)
+})
+
 test('parse JSON with interrupting error', () => {
   expect(() => parse('{"someString": "str{"error":{"description":"Some error"}}'))
     .toThrowErrorMatchingInlineSnapshot(`
