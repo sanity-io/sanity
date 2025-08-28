dd06c1676e89157d8b593159798df046b34b2560
Taylor Beseda <tbeseda@gmail.com>
2025-08-28 08:35:58 -0600

    move defineEvent to a .telemetry.ts

--- packages/@sanity/cli/src/commands/blueprints/addBlueprintsCommand.ts
+++ packages/@sanity/cli/src/commands/blueprints/addBlueprintsCommand.ts
@@ -1,15 +1,5 @@
 import {type CliCommandDefinition} from '../../types'
-import {defineEvent} from '@sanity/telemetry'
-
-// create a telemetry event for when the user uses the --example flag with the blueprints add command
-export const BlueprintsAddExampleUsed = defineEvent<{
-  example: string // name of the example
-  resourceType: string // always 'function'
-}>({
-  version: 1,
-  name: 'Blueprints Add Example Used',
-  description: 'User used --example flag with blueprints add command',
-})
+import {BlueprintsAddExampleUsed} from './blueprints.telemetry'
 
 const helpText = `
 Arguments
