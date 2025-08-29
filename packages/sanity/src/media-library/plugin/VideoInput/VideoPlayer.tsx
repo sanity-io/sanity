6d48099016404cb012eafe17f25874aa03814fe9
Fred Carlsen <fred@sjelfull.no>
2025-08-29 15:43:12 +0200

    refactor(sanity): use video playback info endpoint for previews

--- packages/sanity/src/media-library/plugin/VideoInput/VideoPlayer.tsx
+++ packages/sanity/src/media-library/plugin/VideoInput/VideoPlayer.tsx
@@ -3,18 +3,22 @@ import 'player.style/sutro'
 
 import MuxPlayer from '@mux/mux-player-react'
 
+import {type VideoPlaybackTokens} from './types'
+
 type VideoPlayerProps = {
   playbackId: string
   aspectRatio?: number
   muted?: boolean
   disabled?: boolean
+  tokens?: VideoPlaybackTokens
 }
 
-export function VideoPlayer({playbackId, ..._props}: VideoPlayerProps) {
+export function VideoPlayer({playbackId, tokens, ..._props}: VideoPlayerProps) {
   return (
     <MuxPlayer
       theme={'sutro' as const}
       playbackId={playbackId}
+      tokens={tokens}
       autoPlay={false}
       loop={false}
       style={{
