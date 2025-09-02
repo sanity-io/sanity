162e5c8a9c114d3d747f5b1e1ebcd08761814ef6
Jordan Lawrence <jordanl17@me.com>
2025-09-02 02:36:53 +0100

    chore: removing unused e2e utils

--- test/e2e/tests/inputs/reference.spec.ts
+++ test/e2e/tests/inputs/reference.spec.ts
@@ -1,11 +1,7 @@
 import {expect} from '@playwright/test'
 
 import {withDefaultClient} from '../../helpers'
-import {
-  expectCreatedStatus,
-  expectEditedStatus,
-  expectPublishedStatus,
-} from '../../helpers/documentStatusAssertions'
+import {expectEditedStatus, expectPublishedStatus} from '../../helpers/documentStatusAssertions'
 import {test} from '../../studio-test'
 
 withDefaultClient((context) => {
