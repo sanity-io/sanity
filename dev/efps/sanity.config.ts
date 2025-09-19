9c4827b1168b22a279a74486199a047dcf014d09
Rita <anarita151@gmail.com>
2025-09-19 11:26:59 +0200

    chore: remove unused deps and default workspace

--- dev/efps/sanity.config.ts
+++ dev/efps/sanity.config.ts
@@ -1,30 +1,11 @@
-import {visionTool} from '@sanity/vision'
 import {defineConfig} from 'sanity'
-import {structureTool} from 'sanity/structure'
 
-import {schemaTypes} from './schemaTypes'
 import {articleEfps} from './tests/article/sanity.config'
 import {recipeEfps} from './tests/recipe/sanity.config'
 import {singleStringEfps} from './tests/singleString/sanity.config'
 import {syntheticEfps} from './tests/synthetic/sanity.config'
 
-export const defaultConfig = defineConfig({
-  name: 'default',
-  title: 'Studio Performance - efps tests',
-  basePath: '/structure',
-  projectId: 'b8j69ts2',
-  dataset: 'production',
-  apiHost: 'https://api.sanity.work',
-
-  plugins: [structureTool(), visionTool()],
-
-  schema: {
-    types: schemaTypes,
-  },
-})
-
 export default defineConfig([
-  defaultConfig,
   {...articleEfps, basePath: '/article'},
   {...recipeEfps, basePath: '/recipe'},
   {...syntheticEfps, basePath: '/synthetic'},
