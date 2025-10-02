4f8a90b8c28b70672fc490013c1cd465c7fa5f79
pedrobonamin <pedrobonamin@gmail.com>
2025-10-02 12:21:42 +0200

    chore: small tweaks to cleanup code changes

--- packages/sanity/src/core/form/studio/inputs/reference/resolveUserDefinedFilter.tsx
+++ packages/sanity/src/core/form/studio/inputs/reference/resolveUserDefinedFilter.tsx
@@ -8,7 +8,6 @@ import {get} from '@sanity/util/paths'
 
 import {type Source} from '../../../../config'
 
-// TODO: Use thsi
 export async function resolveUserDefinedFilter(
   options: ReferenceOptions | undefined,
   document: SanityDocument,
