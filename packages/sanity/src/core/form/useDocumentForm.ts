50f4d47dab72ae8d40f0ff80c1780f5079e732d5
Jordan Lawrence <jordanl17@me.com>
2025-10-09 09:51:41 +0100

    fix: validate published document when it is displayed

--- packages/sanity/src/core/form/useDocumentForm.ts
+++ packages/sanity/src/core/form/useDocumentForm.ts
@@ -163,9 +163,6 @@ export function useDocumentForm(options: DocumentFormOptions): DocumentFormValue
 
   const telemetry = useTelemetry()
 
-  const {validation: validationRaw} = useValidationStatus(documentId, documentType, releaseId)
-  const validation = useUnique(validationRaw)
-
   // if it only has versions then we need to make sure that whatever the first document that is allowed
   // is a version document, but also that it has the right order
   // this will make sure that then the right document appears and so does the right chip within the document header
@@ -238,6 +235,14 @@ export function useDocumentForm(options: DocumentFormOptions): DocumentFormValue
     selectedPerspectiveName,
     onlyHasVersions,
   ])
+
+  const {validation: validationRaw} = useValidationStatus(
+    documentId,
+    documentType,
+    releaseId,
+    value._id,
+  )
+  const validation = useUnique(validationRaw)
 
   const {previousId: upstreamId} = useDocumentIdStack({
     strict: true,
