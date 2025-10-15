1fbaa51cabb96f5ecbc514335a7c4d99f2cb5ddd
pedrobonamin <pedrobonamin@gmail.com>
2025-10-15 14:54:07 +0200

    fix: update decide schema types, copy from sanity/client

--- dev/test-studio/schema/decideSchema/types.ts
+++ dev/test-studio/schema/decideSchema/types.ts
@@ -1,19 +1,48 @@
-export interface DecideObject<T = unknown> {
+// TODO: Import this from "@sanity/client"
+
+const stringOperators = [
+  'equals',
+  'not-equals',
+  'contains',
+  'not-contains',
+  'is-empty',
+  'is-not-empty',
+] as const
+
+const numberOperators = [
+  'equals',
+  'not-equals',
+  'is-empty',
+  'is-not-empty',
+  '>',
+  '<',
+  '>=',
+  '<=',
+] as const
+
+/**
+ * @internal
+ */
+export interface DecideField<T = unknown> {
   _type: 'sanity.decideField'
   default?: T
-  variants?: Array<{
-    _key: string
-    _type: 'variant'
-    value?: T
-    anyOf?: Array<DecideRule>
-  }>
+  variants?: Array<{_key: string; _type: 'variant'; value?: T; anyOf?: Array<DecideRule>}>
 }
 
-export interface DecideRule {
-  property: string
-  operator: string
-  targetValue: string
-  and?: DecideRule[]
-  _key: string
-  _type: 'rule'
-}
+export type DecideRule =
+  | {
+      property: string
+      operator: (typeof stringOperators)[number]
+      targetValue: string
+      and?: DecideRule[]
+      _key: string
+      _type: 'rule'
+    }
+  | {
+      property: string
+      operator: (typeof numberOperators)[number]
+      targetValue: number
+      and?: DecideRule[]
+      _key: string
+      _type: 'rule'
+    }
