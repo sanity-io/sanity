5d288556e63a544584909b46fbfb569e2d20075b
Robin Pyon <robin.pyon@gmail.com>
2025-10-23 17:31:23 +0100

    refactor: remove duplicate exports from document/fieldActions

--- packages/sanity/src/core/config/document/fieldActions/index.ts
+++ packages/sanity/src/core/config/document/fieldActions/index.ts
@@ -2,9 +2,5 @@ import {copyAction} from '../../../form/field/actions/copyAction'
 import {pasteAction} from '../../../form/field/actions/pasteAction'
 import {type DocumentFieldAction} from './types'
 
-export * from './define'
-export * from './reducer'
-export * from './types'
-
 /** @internal */
 export const initialDocumentFieldActions: DocumentFieldAction[] = [copyAction, pasteAction]
