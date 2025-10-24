fa232f4e471e8dd00944941901c009837611dd08
Bjørge Næss <bjoerge@gmail.com>
2025-10-24 16:17:12 +0200

    chore(design-studio): bring design studio back to a working state

--- dev/design-studio/sanity.config.ts
+++ dev/design-studio/sanity.config.ts
@@ -1,7 +1,6 @@
 import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
 
-import {themePreviewTool} from './plugins/theme-preview-tool'
 import {schemaTypes} from './schemaTypes'
 import {structure} from './structure'
 import {templates} from './templates'
@@ -9,14 +8,13 @@ import {templates} from './templates'
 export default defineConfig({
   plugins: [
     structureTool({structure}),
-    themePreviewTool(),
     // @todo
     // visionTool(),
   ],
   name: 'default',
   title: 'Design Studio',
-  projectId: 'vr2mh9ho',
-  dataset: 'production',
+  projectId: 'ppsg7ml5',
+  dataset: 'design-studio',
   schema: {
     templates,
     types: schemaTypes,
