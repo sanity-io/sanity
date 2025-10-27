735a3aa8cba8ee684797bb41b58beefe9efff68e
Bjørge Næss <bjoerge@gmail.com>
2025-10-27 10:01:59 +0100

    chore: remove eslint-config-studio usage

--- dev/efps/eslint.config.mjs
+++ dev/efps/eslint.config.mjs
@@ -1,11 +1,9 @@
 import baseConfig from '@repo/eslint-config'
-import studio from '@sanity/eslint-config-studio'
 import {defineConfig} from 'eslint/config'
 
 export default defineConfig([
   {
     ignores: ['builds/**/*', 'builds/**/assets/**/*', 'results/**/*'],
   },
-  ...studio,
   ...baseConfig,
 ])
