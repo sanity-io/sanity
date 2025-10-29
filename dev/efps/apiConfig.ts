50d926e8a27a82f81ac445bfb0b01504aebbe671
Bjørge Næss <bjoerge@gmail.com>
2025-10-29 01:59:00 +0100

    fix(ci): spell out process.env vars for bundling

--- dev/efps/apiConfig.ts
+++ dev/efps/apiConfig.ts
@@ -1,7 +1,9 @@
-import {readEnvVar} from './readEnvVar'
-
+export function fail(name: string): never {
+  throw new Error(`Missing required environment variable "${name}"`)
+}
 export const apiConfig = {
-  projectId: readEnvVar('SANITY_STUDIO_PERF_EFPS_PROJECT_ID'),
-  dataset: readEnvVar('SANITY_STUDIO_PERF_EFPS_DATASET'),
-  apiHost: readEnvVar('SANITY_STUDIO_PERF_EFPS_API_HOST'),
-} as const
+  projectId:
+    process.env.SANITY_STUDIO_PERF_EFPS_PROJECT_ID || fail('SANITY_STUDIO_PERF_EFPS_PROJECT_ID'),
+  dataset: process.env.SANITY_STUDIO_PERF_EFPS_DATASET || fail('SANITY_STUDIO_PERF_EFPS_DATASET'),
+  apiHost: process.env.SANITY_STUDIO_PERF_EFPS_API_HOST || fail('SANITY_STUDIO_PERF_EFPS_API_HOST'),
+}
