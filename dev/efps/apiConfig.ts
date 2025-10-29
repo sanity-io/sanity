eccf2253426fb154ca34b2a5b2bba67439a3447d
Bjørge Næss <bjoerge@gmail.com>
2025-10-29 12:37:26 +0100

    fix(ci): efps local dev improvements

--- dev/efps/apiConfig.ts
+++ dev/efps/apiConfig.ts
@@ -3,7 +3,12 @@ export function fail(name: string): never {
 }
 export const apiConfig = {
   projectId:
-    process.env.SANITY_STUDIO_PERF_EFPS_PROJECT_ID || fail('SANITY_STUDIO_PERF_EFPS_PROJECT_ID'),
-  dataset: process.env.SANITY_STUDIO_PERF_EFPS_DATASET || fail('SANITY_STUDIO_PERF_EFPS_DATASET'),
-  apiHost: process.env.SANITY_STUDIO_PERF_EFPS_API_HOST || fail('SANITY_STUDIO_PERF_EFPS_API_HOST'),
+    process.env.SANITY_STUDIO_EFPS_EXPERIMENT_PROJECT_ID ||
+    fail('SANITY_STUDIO_EFPS_EXPERIMENT_PROJECT_ID'),
+  dataset:
+    process.env.SANITY_STUDIO_EFPS_EXPERIMENT_DATASET ||
+    fail('SANITY_STUDIO_EFPS_EXPERIMENT_DATASET'),
+  apiHost:
+    process.env.SANITY_STUDIO_EFPS_EXPERIMENT_API_HOST ||
+    fail('SANITY_STUDIO_EFPS_EXPERIMENT_API_HOST'),
 }
