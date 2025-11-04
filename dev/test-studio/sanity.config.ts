7d9e99a3ac7b36bc7593dd93f5d378593474577e
Pedro Bonamin <46196328+pedrobonamin@users.noreply.github.com>
2025-11-04 10:46:14 +0100

    chore(core): adds `scheduledDrafts` config option (off by default) (#10946)

--- dev/test-studio/sanity.config.ts
+++ dev/test-studio/sanity.config.ts
@@ -5,13 +5,7 @@ import {BookIcon} from '@sanity/icons'
 import {SanityMonogram} from '@sanity/logos'
 import {debugSecrets} from '@sanity/preview-url-secret/sanity-plugin-debug-secrets'
 import {visionTool} from '@sanity/vision'
-import {
-  DECISION_PARAMETERS_SCHEMA,
-  defineConfig,
-  definePlugin,
-  QUOTA_EXCLUDED_RELEASES_ENABLED,
-  type WorkspaceOptions,
-} from 'sanity'
+import {DECISION_PARAMETERS_SCHEMA, defineConfig, definePlugin, type WorkspaceOptions} from 'sanity'
 import {defineDocuments, defineLocations, presentationTool} from 'sanity/presentation'
 import {structureTool} from 'sanity/structure'
 import {unsplashAssetSource, UnsplashIcon} from 'sanity-plugin-asset-source-unsplash'
@@ -234,7 +228,9 @@ const defaultWorkspace = defineConfig({
   mediaLibrary: {
     enabled: true,
   },
-  [QUOTA_EXCLUDED_RELEASES_ENABLED]: true,
+  scheduledDrafts: {
+    enabled: false,
+  },
   [DECISION_PARAMETERS_SCHEMA]: {
     audiences: ['aud-a', 'aud-b', 'aud-c'],
     locales: ['en-GB', 'en-US'],
@@ -379,7 +375,9 @@ export default defineConfig([
     mediaLibrary: {
       enabled: true,
     },
-    [QUOTA_EXCLUDED_RELEASES_ENABLED]: true,
+    scheduledDrafts: {
+      enabled: true,
+    },
   },
   {
     name: 'growth',
@@ -399,7 +397,9 @@ export default defineConfig([
     mediaLibrary: {
       enabled: true,
     },
-    [QUOTA_EXCLUDED_RELEASES_ENABLED]: true,
+    scheduledDrafts: {
+      enabled: true,
+    },
   },
   {
     name: 'media-library-playground',
