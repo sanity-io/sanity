86336ab1840bce2b20ea6a34121012430a0e8ba0
Rita <anarita151@gmail.com>
2025-11-04 13:39:16 +0100

    test: add e2e tests

---
+++ e2e/tests/focused-document/focused.spec.ts
@@ -0,0 +1,115 @@
+import {expect} from '@playwright/test'
+
+import {test} from '../../studio-test'
+
+test.describe('focused document', () => {
+  test.beforeEach(async ({page, createDraftDocument}) => {
+    test.slow()
+    await createDraftDocument('/content/input-standard;referenceTest')
+
+    expect(page.getByTestId('focus-pane-button-focus')).toBeVisible()
+    expect(page.getByTestId('field-title').getByTestId('string-input')).toBeVisible()
+
+    await page.getByTestId('field-title').getByTestId('string-input').fill('Test Author')
+  })
+
+  test(`focus pane button shows the pane breadcrumb`, async ({page}) => {
+    await page.getByTestId('focus-pane-button-focus').click()
+
+    await expect(page.getByTestId('document-header-breadcrumb')).toBeVisible()
+    await expect(
+      page.getByTestId('document-header-breadcrumb').getByText('Test Author'),
+    ).toBeVisible()
+  })
+
+  test(`focus pane button collapses the pane breadcrumb when the focus pane button is clicked again`, async ({
+    page,
+  }) => {
+    await page.getByTestId('focus-pane-button-focus').click()
+
+    await expect(page.getByTestId('document-header-breadcrumb')).toBeVisible()
+    await expect(
+      page.getByTestId('document-header-breadcrumb').getByText('Test Author'),
+    ).toBeVisible()
+
+    await page.getByTestId('focus-pane-button-collapse').click()
+
+    await expect(page.getByTestId('document-header-breadcrumb')).not.toBeVisible()
+  })
+
+  test('navigation to referenced document works when document is focused', async ({page}) => {
+    test.slow()
+
+    await page.getByTestId('focus-pane-button-focus').click()
+
+    await expect(page.locator('#selfOrEmpty-selectTypeMenuButton')).toBeVisible()
+    await page.locator('#selfOrEmpty-selectTypeMenuButton').click()
+    await expect(
+      page.getByTestId('create-new-document-select-reference-test-selectTypeMenuItem'),
+    ).toBeVisible()
+    await page.getByTestId('create-new-document-select-reference-test-selectTypeMenuItem').click()
+
+    await expect(page.getByTestId('document-panel-document-title')).toBeVisible()
+
+    expect(page.getByTestId('field-title').getByTestId('string-input')).toBeVisible()
+
+    await page.getByTestId('field-title').getByTestId('string-input').fill('Test Name')
+
+    await expect(page.getByTestId('document-header-breadcrumb')).toBeVisible()
+    await expect(
+      page.getByTestId('document-header-breadcrumb').getByText('Test Author'),
+    ).toBeVisible()
+    await expect(
+      page.getByTestId('document-header-breadcrumb').getByText('Test Name'),
+    ).toBeVisible()
+  })
+
+  test('focus pane hides previous and after panes in the breadcrumb', async ({page}) => {
+    await expect(page.locator('[data-testid="pane-header"]')).toHaveCount(4)
+    await page.getByTestId('focus-pane-button-focus').click()
+
+    await expect(page.getByTestId('document-header-breadcrumb')).toBeVisible()
+
+    await expect(page.locator('[data-testid="pane-header"]')).toHaveCount(1)
+  })
+
+  test('pane focuses on the middle pane, will close references to the right', async ({page}) => {
+    test.slow()
+
+    // set first reference
+    await expect(page.locator('#selfOrEmpty-selectTypeMenuButton')).toBeVisible()
+    await page.locator('#selfOrEmpty-selectTypeMenuButton').click()
+    await expect(
+      page.getByTestId('create-new-document-select-reference-test-selectTypeMenuItem'),
+    ).toBeVisible()
+    await page.getByTestId('create-new-document-select-reference-test-selectTypeMenuItem').click()
+
+    await expect(page.getByTestId('document-panel-document-title').nth(1)).toBeVisible()
+
+    expect(page.getByTestId('field-title').getByTestId('string-input').nth(1)).toBeVisible()
+
+    await page.getByTestId('field-title').getByTestId('string-input').nth(1).fill('Test Name')
+
+    // Reopen the first document pane
+    await page.getByTestId('pane-header').nth(3).click()
+
+    await expect(page.getByTestId('document-panel-document-title').nth(0)).toBeVisible()
+    await expect(page.getByTestId('document-panel-document-title').nth(0)).toContainText(
+      'Test Author',
+    )
+
+    await expect(page.locator('[data-testid="pane-header"]')).toHaveCount(6)
+    expect(page.getByTestId('focus-pane-button-focus')).toBeVisible()
+    await page.getByTestId('focus-pane-button-focus').click()
+
+    await expect(page.locator('[data-testid="pane-header"]')).toHaveCount(1)
+    await expect(page.getByTestId('document-header-breadcrumb')).toBeVisible()
+    await expect(page.getByTestId('document-header-breadcrumb')).toBeVisible()
+    await expect(
+      page.getByTestId('document-header-breadcrumb').getByText('Test Author'),
+    ).toBeVisible()
+    await expect(
+      page.getByTestId('document-header-breadcrumb').getByText('Test Name'),
+    ).not.toBeVisible()
+  })
+})
