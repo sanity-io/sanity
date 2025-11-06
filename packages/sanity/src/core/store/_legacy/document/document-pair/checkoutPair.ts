2e0fc7e5209712e67cbd0463827ec43501f35479
Bjørge Næss <bjoerge@gmail.com>
2025-11-06 20:25:31 +0100

    chore(sanity): enable resumability for document pair listener

--- packages/sanity/src/core/store/_legacy/document/document-pair/checkoutPair.ts
+++ packages/sanity/src/core/store/_legacy/document/document-pair/checkoutPair.ts
@@ -30,6 +30,8 @@ import {
   type MutationEvent,
   type PendingMutationsEvent,
   type ReconnectEvent,
+  type ResetEvent,
+  type WelcomeBackEvent,
   type WelcomeEvent,
 } from '../types'
 import {actionsApiClient} from './utils/actionsApiClient'
@@ -53,7 +55,7 @@ export type WithVersion<T> = T & {version: DocumentVariantType}
  * @hidden
  * @beta */
 export type DocumentVersionEvent = WithVersion<
-  ReconnectEvent | BufferedDocumentEvent | WelcomeEvent
+  ReconnectEvent | BufferedDocumentEvent | WelcomeEvent | WelcomeBackEvent | ResetEvent
 >
 
 /**
@@ -254,7 +256,7 @@ export function checkoutPair(
   const listenerEvents$ = getPairListener(client, idPair, {onSyncErrorRecovery, tag}).pipe(share())
 
   const connectionChangeEvents$ = listenerEvents$.pipe(
-    filter((ev) => ev.type === 'reconnect' || ev.type === 'welcome'),
+    filter((ev) => ev.type === 'reconnect' || ev.type === 'welcome' || ev.type === 'welcomeback'),
   )
 
   const draft = createBufferedDocument(
