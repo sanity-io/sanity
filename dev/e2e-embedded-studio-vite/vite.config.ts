43768586c3ad7d82d28e22191f125ba4a994dcfa
pedrobonamin <pedrobonamin@gmail.com>
2025-11-11 11:37:05 +0100

    chore: cleanup

--- dev/e2e-embedded-studio-vite/vite.config.ts
+++ dev/e2e-embedded-studio-vite/vite.config.ts
@@ -2,7 +2,6 @@ import {getViteAliases} from '@repo/dev-aliases/vite'
 import react from '@vitejs/plugin-react'
 import {defineConfig} from 'vite'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
