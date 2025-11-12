95996ff8dc944fc644aaf5c391d69f9b857ceab8
Matthew Ritter <matthew.ritter@sanity.io>
2025-11-12 14:53:47 -0500

    fix: refactor asset limit upsell provider

--- packages/sanity/src/core/limits/context/assets/AssetLimitUpsellProvider.tsx
+++ packages/sanity/src/core/limits/context/assets/AssetLimitUpsellProvider.tsx
@@ -1,59 +1,19 @@
-import {type PropsWithChildren, useCallback, useContext, useMemo, useState} from 'react'
+import {type PropsWithChildren, useContext} from 'react'
 import {AssetLimitUpsellContext, type AssetLimitUpsellContextValue} from 'sanity/_singletons'
 
-import {useUpsellData} from '../../../hooks/useUpsellData'
-import {type UpsellDialogViewedInfo} from '../../../studio'
+import {getDialogPropsFromContext, useUpsellContext} from '../../../hooks/useUpsellContext'
 import {UpsellDialog} from '../../../studio/upsell/UpsellDialog'
 
 export function AssetLimitUpsellProvider({children}: PropsWithChildren) {
-  const [upsellDialogOpen, setUpsellDialogOpen] = useState(false)
-  const {upsellData, telemetryLogs} = useUpsellData({
+  const contextValue = useUpsellContext({
     dataUri: '/journey/asset-limit',
     feature: 'asset-limits',
   })
 
-  const handleOpenDialog = useCallback(
-    (source: UpsellDialogViewedInfo['source']) => {
-      setUpsellDialogOpen(true)
-      telemetryLogs.dialogViewed(source)
-    },
-    [telemetryLogs],
-  )
-
-  const handleClose = useCallback(() => {
-    setUpsellDialogOpen(false)
-    telemetryLogs.dialogDismissed()
-  }, [telemetryLogs])
-
-  const handlePrimaryButtonClick = useCallback(() => {
-    telemetryLogs.dialogPrimaryClicked()
-  }, [telemetryLogs])
-
-  const handleSecondaryButtonClick = useCallback(() => {
-    telemetryLogs.dialogSecondaryClicked()
-  }, [telemetryLogs])
-
-  const ctxValue = useMemo<AssetLimitUpsellContextValue>(
-    () => ({
-      upsellDialogOpen,
-      handleOpenDialog,
-      upsellData,
-      telemetryLogs,
-    }),
-    [handleOpenDialog, upsellDialogOpen, upsellData, telemetryLogs],
-  )
-
   return (
-    <AssetLimitUpsellContext.Provider value={ctxValue}>
+    <AssetLimitUpsellContext.Provider value={contextValue}>
       {children}
-      {upsellData && upsellDialogOpen && (
-        <UpsellDialog
-          data={upsellData}
-          onClose={handleClose}
-          onPrimaryClick={handlePrimaryButtonClick}
-          onSecondaryClick={handleSecondaryButtonClick}
-        />
-      )}
+      <UpsellDialog {...getDialogPropsFromContext(contextValue)} />
     </AssetLimitUpsellContext.Provider>
   )
 }
