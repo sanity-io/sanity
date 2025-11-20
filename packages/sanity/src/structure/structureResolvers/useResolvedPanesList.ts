d64b5a780e6d3f7f29c71610e24b025a46f4d5d0
Rita <anarita151@gmail.com>
2025-11-20 08:54:26 +0000

    fix: Adds fix to stop breaking in presentation

--- packages/sanity/src/structure/structureResolvers/useResolvedPanesList.ts
+++ packages/sanity/src/structure/structureResolvers/useResolvedPanesList.ts
@@ -1,12 +1,26 @@
-import {useContext} from 'react'
+import {useContext, useMemo} from 'react'
 
 import {ResolvedPanesContext} from './provider/ResolvedPanesProvider'
 import {type Panes} from './useResolvedPanes'
 
 export function useResolvedPanesList(): Panes {
   const context = useContext(ResolvedPanesContext)
-  if (!context) {
-    throw new Error('useResolvedPanesContext must be used within ResolvedPanesProvider')
-  }
-  return context
+
+  // This allows components to be used outside of ResolvedPanesProvider
+  // Such as Presensation which doesn't need to be concerned about the structure of the panes
+  // As it always travels down the nearest pane to the document.
+  const defaultValue = useMemo<Panes>(
+    () => ({
+      paneDataItems: [],
+      routerPanes: [],
+      resolvedPanes: [],
+      maximizedPane: null,
+      setMaximizedPane: () => {
+        // noop
+      },
+    }),
+    [],
+  )
+
+  return context ?? defaultValue
 }
