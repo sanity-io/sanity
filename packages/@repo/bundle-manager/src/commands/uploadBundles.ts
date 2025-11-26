2be3548d46a4fe7570f12305a2e739bb3cd4bafd
Cody Olsen <81981+stipsan@users.noreply.github.com>
2025-11-26 20:31:44 +0100

    fix: ensure module server uploads `.mjs` files (#11281)

--- packages/@repo/bundle-manager/src/commands/uploadBundles.ts
+++ packages/@repo/bundle-manager/src/commands/uploadBundles.ts
@@ -23,7 +23,6 @@ const storage = new Storage({
 const bucket = storage.bucket(readEnv<KnownEnvVar>('GCLOUD_BUCKET'))
 
 const mimeTypes: Record<string, string | undefined> = {
-  '.js': 'application/javascript',
   '.mjs': 'application/javascript',
   '.map': 'application/json',
 }
