357e7cd3bf6503086a47022f487303fafdac136c
Per-Kristian Nordnes <per.kristian.nordnes@gmail.com>
2025-11-27 15:50:07 +0100

    fix(core): only validate .media validation markers
    Only .media validation rules should be considered when validation a selection for the Media Library plugin.
    
    Other validation rules on the field (like required alt text on image) should not make the selection invalid.

--- packages/sanity/src/core/form/studio/assetSourceMediaLibrary/shared/validation.ts
+++ packages/sanity/src/core/form/studio/assetSourceMediaLibrary/shared/validation.ts
@@ -1,25 +1,24 @@
 import {type ValidationMarker} from '@sanity/types'
 
 /**
- * Ignore asset required validation markers.
+ * Ignore anything but the 'media' validation markers
  *
- * This is used to still allow asset selection when validating
- * asset source media library assets.
+ * This is used to still allow an asset selection even if there are other validation errors on the field,
  *
  * @param validationMarkers - The validation markers to filter.
  * @returns The filtered validation markers.
  */
-export function ignoreAssetRequiredValidation(validationMarkers: ValidationMarker[]) {
+export function filterMediaValidationMarkers(validationMarkers: ValidationMarker[]) {
   return validationMarkers.filter((marker) => {
     if (
       '__internal_metadata' in marker &&
       typeof marker.__internal_metadata === 'object' &&
       marker.__internal_metadata !== null &&
       'name' in marker.__internal_metadata &&
-      marker.__internal_metadata.name === 'assetRequired'
+      marker.__internal_metadata?.name === 'media'
     ) {
-      return false
+      return true
     }
-    return true
+    return false
   })
 }
