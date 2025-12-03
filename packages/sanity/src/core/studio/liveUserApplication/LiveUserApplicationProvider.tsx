a830c41574511920434d26511e8f4a6aa66f20e0
Dain Cilke <dain.cilke@gmail.com>
2025-12-03 14:56:39 +0100

    feat(userApplications): add live user application provider

---
+++ packages/sanity/src/core/studio/liveUserApplication/LiveUserApplicationProvider.tsx
@@ -0,0 +1,55 @@
+import {type ReactNode, useEffect, useMemo, useState} from 'react'
+import {LiveUserApplicationContext} from 'sanity/_singletons'
+
+import {type UserApplication, useUserApplicationCache} from '../../store/userApplications'
+import {useWorkspaces} from '../workspaces'
+import {findUserApplication} from './liveUserApplication'
+
+/** @internal */
+interface LiveUserApplicationProviderProps {
+  children: ReactNode
+}
+
+/**
+ * Provider that automatically uploads the studio manifest when the Studio loads.
+ * This runs once when all workspaces are available and includes all workspace information.
+ *
+ * @internal
+ */
+export function LiveUserApplicationProvider({children}: LiveUserApplicationProviderProps) {
+  const [userApplication, setUserApplication] = useState<UserApplication | undefined>(undefined)
+
+  const workspaces = useWorkspaces()
+  const userApplicationCache = useUserApplicationCache()
+
+  useEffect(() => {
+    let hasSubscriber = true
+    findUserApplication(userApplicationCache, workspaces)
+      .then((found) => {
+        if (hasSubscriber) {
+          setUserApplication(found)
+        }
+      })
+      .catch((error) => {
+        console.error('Error when determining live user application id:', error)
+        if (hasSubscriber) {
+          setUserApplication(undefined)
+        }
+      })
+    return () => {
+      hasSubscriber = false
+    }
+  }, [userApplicationCache, workspaces])
+
+  const contextValue = useMemo(
+    () => ({
+      userApplication,
+    }),
+    [userApplication],
+  )
+  return (
+    <LiveUserApplicationContext.Provider value={contextValue}>
+      {children}
+    </LiveUserApplicationContext.Provider>
+  )
+}
