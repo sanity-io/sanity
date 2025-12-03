037bc0822dcc3bdc31f2678e5b6cf73f281214f7
pedrobonamin <pedrobonamin@gmail.com>
2025-12-03 13:42:44 +0100

    chore(core): update format

--- packages/sanity/src/core/form/types/inputProps.ts
+++ packages/sanity/src/core/form/types/inputProps.ts
@@ -85,8 +85,8 @@ export interface BaseInputProps {
 export interface ObjectInputProps<
   T = Record<string, any>,
   S extends ObjectSchemaType = ObjectSchemaType,
-> extends BaseInputProps,
-    Omit<ObjectFormNode<T, S>, '_allMembers' | 'displayInlineChanges'> {
+>
+  extends BaseInputProps, Omit<ObjectFormNode<T, S>, '_allMembers' | 'displayInlineChanges'> {
   /**
    * @hidden
    * @beta */
@@ -195,8 +195,8 @@ export interface ObjectInputProps<
 export interface ArrayOfObjectsInputProps<
   T extends {_key: string} = {_key: string},
   S extends ArraySchemaType = ArraySchemaType,
-> extends BaseInputProps,
-    Omit<ArrayOfObjectsFormNode<T[], S>, 'displayInlineChanges'> {
+>
+  extends BaseInputProps, Omit<ArrayOfObjectsFormNode<T[], S>, 'displayInlineChanges'> {
   /**
    * @hidden
    * @beta */
@@ -331,8 +331,8 @@ export type ArrayOfPrimitivesElementType<T extends any[]> = T extends (infer K)[
 export interface ArrayOfPrimitivesInputProps<
   T extends string | boolean | number = string | boolean | number,
   S extends ArraySchemaType = ArraySchemaType,
-> extends BaseInputProps,
-    Omit<ArrayOfPrimitivesFormNode<T[], S>, 'displayInlineChanges'> {
+>
+  extends BaseInputProps, Omit<ArrayOfPrimitivesFormNode<T[], S>, 'displayInlineChanges'> {
   /**
    * @hidden
    * @beta */
@@ -452,8 +452,7 @@ export interface ComplexElementProps {
  * @hidden
  * @public */
 export interface StringInputProps<S extends StringSchemaType = StringSchemaType>
-  extends BaseInputProps,
-    Omit<StringFormNode<S>, 'displayInlineChanges'> {
+  extends BaseInputProps, Omit<StringFormNode<S>, 'displayInlineChanges'> {
   /**
    * @hidden
    * @beta */
@@ -469,8 +468,7 @@ export interface StringInputProps<S extends StringSchemaType = StringSchemaType>
  * @hidden
  * @public */
 export interface NumberInputProps<S extends NumberSchemaType = NumberSchemaType>
-  extends BaseInputProps,
-    Omit<NumberFormNode<S>, 'displayInlineChanges'> {
+  extends BaseInputProps, Omit<NumberFormNode<S>, 'displayInlineChanges'> {
   /**
    * @hidden
    * @beta */
@@ -486,8 +484,7 @@ export interface NumberInputProps<S extends NumberSchemaType = NumberSchemaType>
  * @hidden
  * @public */
 export interface BooleanInputProps<S extends BooleanSchemaType = BooleanSchemaType>
-  extends BaseInputProps,
-    Omit<BooleanFormNode<S>, 'displayInlineChanges'> {
+  extends BaseInputProps, Omit<BooleanFormNode<S>, 'displayInlineChanges'> {
   /**
    * @hidden
    * @beta */
@@ -518,8 +515,10 @@ export type PrimitiveInputProps = StringInputProps | BooleanInputProps | NumberI
  *
  * @public
  * */
-export interface PortableTextInputProps
-  extends ArrayOfObjectsInputProps<PortableTextBlock, ArraySchemaType<PortableTextBlock>> {
+export interface PortableTextInputProps extends ArrayOfObjectsInputProps<
+  PortableTextBlock,
+  ArraySchemaType<PortableTextBlock>
+> {
   /**
    * A React Ref that can reference the underlying editor instance
    */
