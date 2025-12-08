c3fe7f28cd6eb6e42d217f1bdef5ec32eb65092e
Bjørge Næss <bjoerge@gmail.com>
2025-12-08 13:19:29 +0100

    chore(structure): hoist default value to outer scope

--- packages/sanity/src/structure/structureResolvers/useResolvedPanesList.ts
+++ packages/sanity/src/structure/structureResolvers/useResolvedPanesList.ts
@@ -1,26 +1,22 @@
-import {useContext, useMemo} from 'react'
+import {useContext} from 'react'
 import {ResolvedPanesContext} from 'sanity/_singletons'
 
 import {type Panes} from './useResolvedPanes'
 
+const DEFAULT_VALUE = {
+  paneDataItems: [],
+  routerPanes: [],
+  resolvedPanes: [],
+  maximizedPane: null,
+  setMaximizedPane: () => {
+    // noop
+  },
+}
 export function useResolvedPanesList(): Panes {
   const context = useContext(ResolvedPanesContext)
 
   // This allows components to be used outside of ResolvedPanesProvider
   // Such as Presensation which doesn't need to be concerned about the structure of the panes
   // As it always travels down the nearest pane to the document.
-  const defaultValue = useMemo<Panes>(
-    () => ({
-      paneDataItems: [],
-      routerPanes: [],
-      resolvedPanes: [],
-      maximizedPane: null,
-      setMaximizedPane: () => {
-        // noop
-      },
-    }),
-    [],
-  )
-
-  return context ?? defaultValue
+  return context ?? DEFAULT_VALUE
 }
