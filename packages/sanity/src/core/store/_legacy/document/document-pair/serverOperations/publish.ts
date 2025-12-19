a0b47a9932b69daab414092d76a302d765996fab
Bjørge Næss <bjoerge@gmail.com>
2025-12-19 20:41:50 +0100

    fix(releases): pass through publish action in version context (#11598)

--- packages/sanity/src/core/store/_legacy/document/document-pair/serverOperations/publish.ts
+++ packages/sanity/src/core/store/_legacy/document/document-pair/serverOperations/publish.ts
@@ -9,21 +9,21 @@ export const publish: OperationImpl<[], DisabledReason> = {
     if (isLiveEditEnabled(schema, typeName)) {
       return 'LIVE_EDIT_ENABLED'
     }
-    if (!snapshots.draft) {
+    if (!snapshots.draft && !snapshots.version) {
       return snapshots.published ? 'ALREADY_PUBLISHED' : 'NO_CHANGES'
     }
     return false
   },
   execute: ({client, idPair, snapshots}) => {
     // The editor must be able to see the draft they are choosing to publish.
-    if (!snapshots.draft) {
-      throw new Error('cannot execute "publish" when draft is missing')
+    if (!snapshots.draft && !snapshots.version) {
+      throw new Error('cannot execute "publish" when draft or version is missing')
     }
 
     return actionsApiClient(client, idPair).observable.action(
       {
         actionType: 'sanity.action.document.publish',
-        draftId: idPair.draftId,
+        draftId: (snapshots.draft?._id || snapshots.version?._id)!,
         publishedId: idPair.publishedId,
         // Optimistic locking using `ifPublishedRevisionId` ensures that concurrent publish action
         // invocations do not override each other.
