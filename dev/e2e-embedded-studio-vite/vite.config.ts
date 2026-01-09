8c12ca9ef473a2e2a0440639abce5fdf77f60e14
Cody Olsen <stipsan@gmail.com>
2026-01-09 02:30:48 +0100

    chore: get rid of `@repo/dev-aliases`

--- dev/e2e-embedded-studio-vite/vite.config.ts
+++ dev/e2e-embedded-studio-vite/vite.config.ts
@@ -1,14 +1,13 @@
-import {getViteAliases} from '@repo/dev-aliases/vite'
 import react from '@vitejs/plugin-react'
 import {defineConfig} from 'vite'
 
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      ...getViteAliases(),
-    },
-  },
+  plugins: [
+    react({babel: {plugins: ['babel-plugin-react-compiler'], generatorOpts: {compact: true}}}),
+  ],
+  // Needed due to the monorepo setup, optimizeDeps will cause duplication of context providers when it chunks lazy imports so we have to disable optimization
+  optimizeDeps: {exclude: ['sanity']},
+  resolve: {dedupe: ['react', 'react-dom', 'sanity', 'styled-components']},
   define: {
     'import.meta.env.SANITY_E2E_PROJECT_ID': JSON.stringify(process.env.SANITY_E2E_PROJECT_ID),
     'import.meta.env.SANITY_E2E_DATASET': JSON.stringify(process.env.SANITY_E2E_DATASET),
--- dev/embedded-studio/vite.config.ts
+++ dev/embedded-studio/vite.config.ts
@@ -1,13 +1,12 @@
-import {getViteAliases} from '@repo/dev-aliases/vite'
 import react from '@vitejs/plugin-react'
 import {defineConfig} from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      ...getViteAliases(),
-    },
-  },
+  plugins: [
+    react({babel: {plugins: ['babel-plugin-react-compiler'], generatorOpts: {compact: true}}}),
+  ],
+  // Needed due to the monorepo setup, optimizeDeps will cause duplication of context providers when it chunks lazy imports so we have to disable optimization
+  optimizeDeps: {exclude: ['sanity']},
+  resolve: {dedupe: ['react', 'react-dom', 'sanity', 'styled-components']},
 })
