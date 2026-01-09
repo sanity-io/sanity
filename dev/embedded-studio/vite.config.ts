f6e03ca93ff3f19a66388a44f60ef975356e4aef
Cody Olsen <stipsan@gmail.com>
2026-01-09 14:17:26 +0100

    chore: handle embedded studio

--- dev/embedded-studio/vite.config.ts
+++ dev/embedded-studio/vite.config.ts
@@ -3,10 +3,5 @@ import {defineConfig} from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react({babel: {plugins: ['babel-plugin-react-compiler'], generatorOpts: {compact: true}}}),
-  ],
-  // Needed due to the monorepo setup, optimizeDeps will cause duplication of context providers when it chunks lazy imports so we have to disable optimization
-  optimizeDeps: {exclude: ['sanity']},
-  resolve: {dedupe: ['react', 'react-dom', 'sanity', 'styled-components']},
+  plugins: [react()],
 })
