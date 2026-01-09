fd7d81fb3fd14cc951ed157c90ace2723a5a0c8d
Cody Olsen <stipsan@gmail.com>
2026-01-09 14:09:58 +0100

    chore: fix embeddable e2e

--- dev/e2e-embedded-studio-vite/vite.config.ts
+++ dev/e2e-embedded-studio-vite/vite.config.ts
@@ -2,12 +2,7 @@ import react from '@vitejs/plugin-react'
 import {defineConfig} from 'vite'
 
 export default defineConfig({
-  plugins: [
-    react({babel: {plugins: ['babel-plugin-react-compiler'], generatorOpts: {compact: true}}}),
-  ],
-  // Needed due to the monorepo setup, optimizeDeps will cause duplication of context providers when it chunks lazy imports so we have to disable optimization
-  optimizeDeps: {exclude: ['sanity']},
-  resolve: {dedupe: ['react', 'react-dom', 'sanity', 'styled-components']},
+  plugins: [react()],
   define: {
     'import.meta.env.SANITY_E2E_PROJECT_ID': JSON.stringify(process.env.SANITY_E2E_PROJECT_ID),
     'import.meta.env.SANITY_E2E_DATASET': JSON.stringify(process.env.SANITY_E2E_DATASET),
