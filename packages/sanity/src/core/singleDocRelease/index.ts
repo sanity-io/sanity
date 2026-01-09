92c1e9926fc1d077578c1dab690098e016b7ca54
Jordan Lawrence <jordanl17@me.com>
2026-01-09 13:46:44 +0000

    fix: correct export order for useScheduledDraftDocument in SingleDocRelease module

--- packages/sanity/src/core/singleDocRelease/index.ts
+++ packages/sanity/src/core/singleDocRelease/index.ts
@@ -1,4 +1,4 @@
 export {SingleDocReleaseProvider, useSingleDocRelease} from './context/SingleDocReleaseProvider'
-export {useScheduledDraftDocument} from './hooks/useScheduledDraftDocument'
 export {usePausedScheduledDraft} from './hooks/usePausedScheduledDraft'
+export {useScheduledDraftDocument} from './hooks/useScheduledDraftDocument'
 export {useScheduledDraftsEnabled} from './hooks/useScheduledDraftsEnabled'
