e4d969e94f738c5588ccec7e64214ea85edfd029
Cody Olsen <stipsan@gmail.com>
2026-01-19 12:13:47 +0100

    chore(deps): bump cli test fixture versions

--- packages/@sanity/cli/test/__fixtures__/remote-template/sanity.config.ts
+++ packages/@sanity/cli/test/__fixtures__/remote-template/sanity.config.ts
@@ -2,10 +2,8 @@ import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
 
 export default defineConfig({
-  name: 'sanity-template-astro-clean',
-  title: 'Sanity Astro Starter',
-  projectId: process.env.SANITY_STUDIO_PROJECT_ID,
-  dataset: process.env.SANITY_STUDIO_DATASET,
+  projectId: process.env.SANITY_STUDIO_PROJECT_ID!,
+  dataset: process.env.SANITY_STUDIO_DATASET!,
   plugins: [structureTool()],
   schema: {
     types: [],
