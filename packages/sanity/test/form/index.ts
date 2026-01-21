4176feb79e120ed6dbf5873157bf1a0fb208e28c
Cody Olsen <stipsan@gmail.com>
2026-01-21 16:54:02 +0100

    chore: WIP

--- packages/sanity/test/form/index.ts
+++ packages/sanity/test/form/index.ts
@@ -1,9 +1,18 @@
-export * from './renderArrayOfObjectsInput'
-export * from './renderBooleanInput'
-export * from './renderCrossDatasetReferenceInput'
-export * from './renderFileInput'
-export * from './renderGlobalDocumentReferenceInput'
-export * from './renderImageInput'
-export * from './renderNumberInput'
-export * from './renderObjectInput'
-export * from './renderStringInput'
+export {
+  renderArrayOfObjectsInput,
+  type TestRenderArrayOfObjectInputCallback,
+} from './renderArrayOfObjectsInput'
+export {renderBooleanInput, type TestRenderBooleanInputCallback} from './renderBooleanInput'
+export {
+  renderCrossDatasetReferenceInput,
+  type TestRenderCrossDatasetReferenceInputCallback,
+} from './renderCrossDatasetReferenceInput'
+export {renderFileInput, type TestRenderFileInputCallback} from './renderFileInput'
+export {
+  renderGlobalDocumentReferenceInput,
+  type TestRenderGlobalDocumentReferenceInputCallback,
+} from './renderGlobalDocumentReferenceInput'
+export {renderImageInput, type TestRenderImageInputCallback} from './renderImageInput'
+export {renderNumberInput, type TestRenderNumberInputCallback} from './renderNumberInput'
+export {renderObjectInput, type TestRenderObjectInputCallback} from './renderObjectInput'
+export {renderStringInput, type TestRenderStringInputCallback} from './renderStringInput'
