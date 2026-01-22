cc36df66c7fac7cbf7a95a5b3db99c1f03f245f1
Rita <anarita151@gmail.com>
2026-01-22 14:12:37 +0100

    chore: add use case

--- dev/test-studio/structure/constants.ts
+++ dev/test-studio/structure/constants.ts
@@ -65,6 +65,7 @@ export const DEBUG_INPUT_TYPES = [
   'fieldsetsTest',
   'fieldValidationInferReproDoc',
   'focusTest',
+  'hiddenFieldValidationTest',
   'patchOnMountDebug',
   'formInputDebug',
   'initialValuesTest',
