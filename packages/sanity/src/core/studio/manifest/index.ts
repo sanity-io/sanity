4b67bf862c1890c716e2aa7e063b5ed45f72eec0
Dain Cilke <dain.cilke@gmail.com>
2026-01-23 15:52:55 +0100

    fix: resolve lint errors and update exports snapshot

--- packages/sanity/src/core/studio/manifest/index.ts
+++ packages/sanity/src/core/studio/manifest/index.ts
@@ -1,7 +1,7 @@
 /**
  * @internal
  */
-export {LiveManifestRegisterProvider} from './LiveManifestRegisterProvider'
 export {generateStudioManifest, type GenerateStudioManifestOptions} from './generateStudioManifest'
+export {LiveManifestRegisterProvider} from './LiveManifestRegisterProvider'
 export type {ManifestWorkspaceInput, StudioManifest, StudioWorkspaceManifest} from './types'
 export {uploadSchema} from './uploadSchema'
