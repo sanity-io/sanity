a7e779b01cbae5861c806df054e54144a2d14186
Kristoffer Brabrand <kristoffer@brabrand.no>
2026-01-29 23:06:20 +0100

    refactor: use code generation throught the @sanity/codegen package

--- packages/@sanity/cli/src/index.ts
+++ packages/@sanity/cli/src/index.ts
@@ -4,13 +4,3 @@ export * from './config'
 export * from './types'
 export {loadEnv} from './util/loadEnv'
 export type {SanityClient} from '@sanity/client'
-
-// Typegen API exports
-export {
-  type GenerateTypesOptions,
-  type GenerateTypesResult,
-  generateTypesToFile,
-} from './actions/typegen/typegenApi'
-
-// Typegen telemetry exports
-export {TypegenWatchModeTrace, TypesGeneratedTrace} from './actions/typegen/generate.telemetry'
