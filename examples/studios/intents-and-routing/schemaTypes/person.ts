4d5e9a77e54b7422922c6f54f1fde1ae001a7d8f
Bjørge Næss <bjoerge@gmail.com>
2026-01-29 14:23:02 +0100

    docs: add studio intents and routing example (#10414)

---
+++ examples/studios/intents-and-routing/schemaTypes/person.ts
@@ -0,0 +1,35 @@
+import {defineField, defineType} from 'sanity'
+
+export default defineType({
+  name: 'person',
+  title: 'Person',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'name',
+      title: 'Name',
+      type: 'string',
+      description: 'Please use "Firstname Lastname" format',
+    }),
+    defineField({
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'name',
+        maxLength: 100,
+      },
+    }),
+    defineField({
+      name: 'image',
+      title: 'Image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    }),
+  ],
+  preview: {
+    select: {title: 'name', media: 'image'},
+  },
+})
