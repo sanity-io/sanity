9204913067487f93322c919532498cbfd61976da
Bjørge Næss <bjoerge@gmail.com>
2026-02-04 16:42:49 +0100

    chore(tst-studio): add schema reproducing the issue

--- dev/test-studio/structure/constants.ts
+++ dev/test-studio/structure/constants.ts
@@ -47,6 +47,7 @@ export const DEBUG_INPUT_TYPES = [
   'decorations',
   'collapsibleObjects',
   'commentsDebug',
+  'domAttrsAsFieldNames',
   'conditionalFieldsTest',
   'conditionalFieldsWithPathTest',
   'customInputsTest',
