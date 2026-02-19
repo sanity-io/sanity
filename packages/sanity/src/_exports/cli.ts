2b89aa4d9b4970b97bdb44808e6aa85a8069ceae
Espen Hovlandsdal <espen@hovlandsdal.com>
2026-02-19 09:21:46 -0800

    refactor: sort exports

--- packages/sanity/src/_exports/cli.ts
+++ packages/sanity/src/_exports/cli.ts
@@ -1,3 +1,3 @@
-export {getStudioEnvironmentVariables, type StudioEnvVariablesOptions} from '@sanity/cli/_internal'
 export type {CliClientOptions, CliConfig} from '@sanity/cli'
 export {createCliConfig, defineCliConfig, getCliClient} from '@sanity/cli'
+export {getStudioEnvironmentVariables, type StudioEnvVariablesOptions} from '@sanity/cli/_internal'
