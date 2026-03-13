74b5d62137b0f42a2eeaa5ffe9e28d9b58115299
Rita <anarita151@gmail.com>
2026-03-13 10:04:39 +0100

    chore: update use case and move to debug inputs

--- dev/test-studio/schema/debug/components/DialogWrappedObjectInput.tsx
+++ dev/test-studio/schema/debug/components/DialogWrappedObjectInput.tsx
@@ -1,23 +1,48 @@
-import {Button, Dialog, Stack} from '@sanity/ui'
-import {useId, useState} from 'react'
-import {type ObjectInputProps} from 'sanity'
+'use client'
 
-export function DialogWrappedObjectInput(props: ObjectInputProps) {
-  const [isOpen, setIsOpen] = useState(false)
-  const dialogId = useId()
+import {EditIcon} from '@sanity/icons'
+import {Box, Button, Card, Dialog, Stack, Text} from '@sanity/ui'
+import {useState} from 'react'
+import {type InputProps} from 'sanity'
+
+export function ModalObjectInput(props: InputProps) {
+  const {value, renderDefault, schemaType, readOnly} = props
+  const [open, setOpen] = useState(false)
+
+  const _title = (typeof schemaType?.title === 'string' ? schemaType.title : undefined) ?? 'Edit'
+
+  const hasValue = value !== undefined && value !== null
 
   return (
     <Stack space={3}>
-      <Button text="Edit in dialog" mode="ghost" onClick={() => setIsOpen(true)} />
-      {isOpen && (
+      <Card padding={3} radius={2} shadow={1} tone="transparent">
+        <Stack space={3}>
+          <Box>
+            {hasValue && (
+              <Text size={1} muted>
+                Configured
+              </Text>
+            )}
+          </Box>
+          <Button
+            icon={EditIcon}
+            mode="ghost"
+            onClick={() => setOpen(true)}
+            text={hasValue ? 'Edit' : 'Configure'}
+            disabled={readOnly}
+          />
+        </Stack>
+      </Card>
+
+      {open && (
         <Dialog
-          open={isOpen}
-          id={dialogId}
-          header="Object debug: Dialog-wrapped renderDefault"
-          onClose={() => setIsOpen(false)}
-          width={2}
+          open
+          id={'modal-object-input'}
+          header={_title}
+          onClose={() => setOpen(false)}
+          width={4}
         >
-          {props.renderDefault(props)}
+          <Box padding={4}>{renderDefault(props)}</Box>
         </Dialog>
       )}
     </Stack>
