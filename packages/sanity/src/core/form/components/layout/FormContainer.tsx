2969943c0ef2b63767edd8af85f70d42792e2ec5
pedrobonamin <pedrobonamin@gmail.com>
2026-03-13 10:21:24 +0100

    fix: formContainer width and padding

--- packages/sanity/src/core/form/components/layout/FormContainer.tsx
+++ packages/sanity/src/core/form/components/layout/FormContainer.tsx
@@ -7,8 +7,8 @@ import {styled} from 'styled-components'
 export const FormContainer = styled.div`
     box-sizing: border-box;
     margin-inline: auto;
-    padding-inline: ${vars.space[4]}px;
-    padding-block-start: ${vars.space[5]}px;
-    padding-block-end: ${vars.space[9]}px;
-    max-width: calc(${vars.container[1]}px + (var(--formGutterSize, 0px) * 2) + (var(--formGutterGap, 0px) * 2));
+    padding-inline: ${vars.space[4]};
+    padding-block-start: ${vars.space[5]};
+    padding-block-end: ${vars.space[9]};
+    max-width: calc(${vars.container[1]} + (var(--formGutterSize, 0px) * 2) + (var(--formGutterGap, 0px) * 2));
   `
