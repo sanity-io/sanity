89f40a8af71542562e5e5d9bd8422fb0848b033c
Pedro Bonamin <46196328+pedrobonamin@users.noreply.github.com>
2026-03-20 16:14:07 +0100

    chore(structure): remove sheetList (#12477)

--- packages/sanity/src/structure/panes/documentList/__tests__/PaneContainer.test.tsx
+++ packages/sanity/src/structure/panes/documentList/__tests__/PaneContainer.test.tsx
@@ -1,6 +1,6 @@
-import {act, render, screen} from '@testing-library/react'
+import {render, screen} from '@testing-library/react'
 import {defineConfig, type PerspectiveContextValue, useSearchState} from 'sanity'
-import {describe, expect, it, type Mock, vi} from 'vitest'
+import {describe, it, type Mock, vi} from 'vitest'
 
 import {createTestProvider} from '../../../../../test/testUtils/TestProvider'
 import {structureUsEnglishLocaleBundle} from '../../../i18n'
@@ -17,10 +17,6 @@ vi.mock('../../../useStructureTool', () => ({
 }))
 vi.mock('../../../components/pane/usePaneLayout', () => ({
   usePaneLayout: vi.fn().mockReturnValue({panes: [], mount: vi.fn()}),
-}))
-
-vi.mock('../sheetList/useDocumentSheetList', () => ({
-  useDocumentSheetList: vi.fn().mockReturnValue({data: [], isLoading: false}),
 }))
 
 vi.mock('sanity', async (importOriginal) => ({
@@ -73,62 +69,5 @@ describe('PaneContainer', () => {
     )
 
     await screen.findByTestId('document-list-pane')
-    expect(screen.queryByTestId('document-sheet-list-pane')).toBeNull()
-  })
-
-  it('should show the document sheet list pane when the sheet layout is selected', async () => {
-    const mockDispatch = vi.fn()
-    const config = defineConfig({
-      projectId: 'test',
-      dataset: 'test',
-      schema: {
-        types: [
-          {
-            type: 'document',
-            name: 'author',
-            fields: [{type: 'string', name: 'name'}],
-          },
-        ],
-      },
-    })
-
-    const wrapper = await createTestProvider({
-      config,
-      resources: [structureUsEnglishLocaleBundle],
-    })
-    mockUseStructureToolSetting.mockReturnValue(['sheetList', vi.fn()])
-    // Mock return value for useSearchState
-    mockUseSearchState.mockReturnValue({
-      state: {
-        result: {hits: [], error: null, loading: false},
-      },
-      dispatch: mockDispatch,
-    })
-
-    render(
-      <PaneContainer
-        paneKey="paneKey"
-        index={1}
-        itemId="123"
-        pane={
-          {
-            id: 'author',
-            schemaTypeName: 'author',
-            options: {},
-          } as DocumentListPaneNode
-        }
-      />,
-      {wrapper},
-    )
-    act(() => {
-      expect(mockDispatch).toHaveBeenCalledWith({
-        type: 'TERMS_TYPE_ADD',
-        schemaType: expect.objectContaining({
-          name: 'author',
-        }),
-      })
-    })
-    screen.getByTestId('document-sheet-list-pane')
-    expect(screen.queryByTestId('document-list-pane')).toBeNull()
   })
 })
