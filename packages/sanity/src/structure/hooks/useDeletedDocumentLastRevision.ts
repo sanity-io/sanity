c32ac36c5f166b31568dea2c7fffd3ff1b39b53c
pedrobonamin <pedrobonamin@gmail.com>
2026-03-20 18:28:03 +0100

    fix: prevent runtime error when using DocumentPaneWithlegacyTimeLine

--- packages/sanity/src/structure/hooks/useDeletedDocumentLastRevision.ts
+++ packages/sanity/src/structure/hooks/useDeletedDocumentLastRevision.ts
@@ -1,14 +1,27 @@
-import {useMemo} from 'react'
+import {useContext, useMemo} from 'react'
 import {useObservable} from 'react-rx'
 import {of} from 'rxjs'
-import {isDeleteDocumentVersionEvent, useEvents, isDeleteDocumentGroupEvent} from 'sanity'
+import {isDeleteDocumentVersionEvent, isDeleteDocumentGroupEvent} from 'sanity'
+import {EventsContext} from 'sanity/_singletons'
 
+/**
+ * This fallback is only necessary until we remove the DocumentPaneWithLegacyTimelineStore option.
+ * Because if users use that option, the EventsContext will not be available.
+ * Once we remove the DocumentPaneWithLegacyTimelineStore option, we can remove this fallback.
+ */
+const EventsContextFallback = {
+  events: [],
+  lastNonDeletedRevId: null,
+  getDocumentAtRevision: () => of({document: null, loading: false}),
+}
 /**
  * Returns the last revision of a deleted document.
  * It will only work in case the document is deleted to avoid overfetching the document history endpoint
  */
 export const useDeletedDocumentLastRevision = () => {
-  const {events, lastNonDeletedRevId, getDocumentAtRevision} = useEvents()
+  const {events, lastNonDeletedRevId, getDocumentAtRevision} =
+    useContext(EventsContext) || EventsContextFallback
+
   const isDeleted =
     events.length > 0 &&
     (isDeleteDocumentVersionEvent(events[0]) || isDeleteDocumentGroupEvent(events[0]))
