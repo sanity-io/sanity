bed3d938aee530ebf992014e3bd2961238a81d4a
Gustav Hansen <gustav.hansen@sanity.io>
2026-03-26 13:30:49 +0100

    feat: workbench entry

---
+++ packages/sanity/src/_exports/workbench.ts
@@ -0,0 +1,6 @@
+/**
+ * Note: we are forwarding the workbench render function from the workbench paackage,
+ * to avoid having to install the workbench package as a dependency in the user project.
+ */
+
+export {renderWorkbench} from '@sanity/workbench/_internal'
