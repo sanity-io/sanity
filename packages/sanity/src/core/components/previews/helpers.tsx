44bc72ce59fc9905ed376c658f50021129ecc8e9
Ash <ash@sanity.io>
2026-05-21 11:46:21 +0100

    fix(sanity): set correct dimensions for PTE block image preview

--- packages/sanity/src/core/components/previews/helpers.tsx
+++ packages/sanity/src/core/components/previews/helpers.tsx
@@ -1,6 +1,9 @@
+import {tryGetImageDimensions} from '@sanity/asset-utils'
 import {type ElementType, isValidElement, type ReactNode} from 'react'
 import {isValidElementType} from 'react-is'
+import {getDevicePixelRatio} from 'use-device-pixel-ratio'
 
+import {PREVIEW_SIZES} from './constants'
 import {type PreviewProps, type PreviewLayoutKey, type PreviewMediaDimensions} from './types'
 
 export function renderPreviewMedia<Layout = PreviewLayoutKey>(
@@ -46,4 +49,24 @@ export function renderPreviewNode<Layout = PreviewLayoutKey>(
 
   // @todo: find out why `value` isn't infered as `ReactNode` here
   return (value as any) || fallbackNode
+}
+
+export function resolveBlockImageDimensions(
+  source: Parameters<typeof tryGetImageDimensions>[0],
+): PreviewMediaDimensions | undefined {
+  const sourceDimensions = tryGetImageDimensions(source)
+
+  if (typeof sourceDimensions === 'undefined') {
+    return undefined
+  }
+
+  const max = PREVIEW_SIZES.blockImage.media
+  const scale = Math.min(max.width / sourceDimensions.width, 1)
+
+  return {
+    width: Math.round(sourceDimensions.width * scale),
+    height: Math.round(sourceDimensions.height * scale),
+    fit: 'max',
+    dpr: getDevicePixelRatio(),
+  }
 }
