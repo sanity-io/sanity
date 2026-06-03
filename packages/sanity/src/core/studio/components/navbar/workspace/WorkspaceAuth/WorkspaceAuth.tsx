163d020a12e3e4229aa51234d6b9ac38b753d73f
Pedro Bonamin <46196328+pedrobonamin@users.noreply.github.com>
2026-06-03 14:16:13 +0200

    fix(core): increase AuthenticateScreen width (#12935)

--- packages/sanity/src/core/studio/components/navbar/workspace/WorkspaceAuth/WorkspaceAuth.tsx
+++ packages/sanity/src/core/studio/components/navbar/workspace/WorkspaceAuth/WorkspaceAuth.tsx
@@ -1,6 +1,9 @@
 import {AddIcon, ArrowLeftIcon} from '@sanity/icons'
-import {Box, Flex, Stack} from '@sanity/ui'
+import {Box, Container, Flex, rem, Stack} from '@sanity/ui'
+// eslint-disable-next-line camelcase
+import {getTheme_v2} from '@sanity/ui/theme'
 import {useCallback, useState} from 'react'
+import {styled} from 'styled-components'
 
 import {Button} from '../../../../../../ui-components'
 import {useTranslation} from '../../../../../i18n'
@@ -10,6 +13,15 @@ import {WORKSPACES_DOCS_URL} from '../constants'
 import {WorkspacePreview} from '../WorkspacePreview'
 import {Layout} from './Layout'
 import {WorkspaceAuthCard} from './WorkspaceAuthCard'
+
+const StyledContainer = styled(Container)((props) => {
+  const theme = getTheme_v2(props.theme)
+  const {container} = theme
+  return {
+    width: 'auto',
+    minWidth: rem(container[0]),
+  }
+})
 
 export function WorkspaceAuth() {
   const {visibleWorkspaces} = useVisibleWorkspaces()
@@ -46,74 +58,78 @@ export function WorkspaceAuth() {
 
   if (LoginComponent && selectedWorkspace) {
     return (
-      <Stack space={2}>
-        {visibleWorkspaces.length > 1 && (
-          <Flex>
-            <Button
-              icon={ArrowLeftIcon}
-              mode="bleed"
-              onClick={handleBack}
-              text={t('workspaces.action.choose-another-workspace')}
-            />
-          </Flex>
-        )}
-
-        <Layout
-          header={
-            <Box padding={3}>
-              <WorkspacePreview
-                icon={selectedWorkspace.icon}
-                title={selectedWorkspace.title}
-                subtitle={selectedWorkspace?.subtitle}
+      <Container width={0}>
+        <Stack space={2}>
+          {visibleWorkspaces.length > 1 && (
+            <Flex>
+              <Button
+                icon={ArrowLeftIcon}
+                mode="bleed"
+                onClick={handleBack}
+                text={t('workspaces.action.choose-another-workspace')}
               />
-            </Box>
-          }
-        >
-          <Stack padding={2} paddingBottom={3} paddingTop={4}>
-            <LoginComponent
-              key={selectedWorkspaceName}
-              projectId={selectedWorkspace.projectId}
-              redirectPath={
-                window.location.pathname.startsWith(selectedWorkspace.basePath)
-                  ? // NOTE: the fragment cannot be preserved because it's used
-                    // to transfer an sid to a token
-                    `${window.location.pathname}${window.location.search}`
-                  : selectedWorkspace.basePath
-              }
-            />
-          </Stack>
-        </Layout>
-      </Stack>
+            </Flex>
+          )}
+
+          <Layout
+            header={
+              <Box padding={3}>
+                <WorkspacePreview
+                  icon={selectedWorkspace.icon}
+                  title={selectedWorkspace.title}
+                  subtitle={selectedWorkspace?.subtitle}
+                />
+              </Box>
+            }
+          >
+            <Stack padding={2} paddingBottom={3} paddingTop={4}>
+              <LoginComponent
+                key={selectedWorkspaceName}
+                projectId={selectedWorkspace.projectId}
+                redirectPath={
+                  window.location.pathname.startsWith(selectedWorkspace.basePath)
+                    ? // NOTE: the fragment cannot be preserved because it's used
+                      // to transfer an sid to a token
+                      `${window.location.pathname}${window.location.search}`
+                    : selectedWorkspace.basePath
+                }
+              />
+            </Stack>
+          </Layout>
+        </Stack>
+      </Container>
     )
   }
 
   return (
-    <Layout
-      header={t('workspaces.choose-your-workspace-label')}
-      footer={
-        <Stack padding={1}>
-          <Button
-            as="a"
-            href={WORKSPACES_DOCS_URL}
-            icon={AddIcon}
-            mode="bleed"
-            rel="noopener noreferrer"
-            size="large"
-            target="__blank"
-            text={t('workspaces.action.add-workspace')}
-          />
+    <StyledContainer width={1}>
+      <Layout
+        header={t('workspaces.choose-your-workspace-label')}
+        footer={
+          <Stack padding={1}>
+            <Button
+              as="a"
+              href={WORKSPACES_DOCS_URL}
+              icon={AddIcon}
+              mode="bleed"
+              rel="noopener noreferrer"
+              size="large"
+              target="__blank"
+              text={t('workspaces.action.add-workspace')}
+            />
+          </Stack>
+        }
+      >
+        <Stack space={1} paddingX={1} paddingY={2}>
+          {visibleWorkspaces.map((workspace) => (
+            <WorkspaceAuthCard
+              key={workspace.name}
+              workspace={workspace}
+              onSelect={(state) => handleCardSelect(workspace.name, state)}
+            />
+          ))}
         </Stack>
-      }
-    >
-      <Stack space={1} paddingX={1} paddingY={2}>
-        {visibleWorkspaces.map((workspace) => (
-          <WorkspaceAuthCard
-            key={workspace.name}
-            workspace={workspace}
-            onSelect={(state) => handleCardSelect(workspace.name, state)}
-          />
-        ))}
-      </Stack>
-    </Layout>
+      </Layout>
+    </StyledContainer>
   )
 }
