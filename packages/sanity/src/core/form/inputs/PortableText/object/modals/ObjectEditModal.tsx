782b68d94977486703f3ac23d681925a0f27fe35
pedrobonamin <pedrobonamin@gmail.com>
2026-06-04 10:49:53 +0200

    fix(core): update portableText dialogs to use default width 1

--- packages/sanity/src/core/form/inputs/PortableText/object/modals/ObjectEditModal.tsx
+++ packages/sanity/src/core/form/inputs/PortableText/object/modals/ObjectEditModal.tsx
@@ -69,7 +69,7 @@ export function ObjectEditModal(props: {
       type="dialog"
       onClose={onClose}
       header={modalTitle}
-      width={1}
+      width={modalWidth ?? 1}
       autofocus={autoFocus}
     >
       {props.children}
