5e1d3ed7773de059ec3d542216136359737e6c87
Jordan Lawrence <jordanl17@me.com>
2026-06-13 03:01:15 +0100

    perf(core): lazy-load search filter input components from operator definitions

--- packages/sanity/src/core/studio/components/navbar/search/definitions/operators/assetOperators.ts
+++ packages/sanity/src/core/studio/components/navbar/search/definitions/operators/assetOperators.ts
@@ -1,7 +1,26 @@
-import {SearchButtonValueReference} from '../../components/filters/common/ButtonValue'
-import {SearchFilterAssetInput} from '../../components/filters/filter/inputs/asset/Asset'
+import {type ComponentType, lazy} from 'react'
+
 import {defineSearchOperator} from './operatorTypes'
 import {toJSON} from './operatorUtils'
+
+// Operator definitions are evaluated pre-auth via prepareConfig; lazy-load filter input
+// components to keep them out of the eager bundle.
+const SearchButtonValueReference = lazy(() =>
+  import('../../components/filters/common/ButtonValue').then((m) => ({
+    default: m.SearchButtonValueReference,
+  })),
+) as ComponentType<any>
+// SearchFilterAssetInput is a factory; create one lazy component per variant.
+const SearchFilterAssetFileInput = lazy(() =>
+  import('../../components/filters/filter/inputs/asset/Asset').then((m) => ({
+    default: m.SearchFilterAssetInput('file'),
+  })),
+) as ComponentType<any>
+const SearchFilterAssetImageInput = lazy(() =>
+  import('../../components/filters/filter/inputs/asset/Asset').then((m) => ({
+    default: m.SearchFilterAssetInput('image'),
+  })),
+) as ComponentType<any>
 
 // @todo: don't manually cast `buttonValueComponent` and `inputComponent` once
 // we understand why `npm run etl` fails with 'Unable to follow symbol' errors
@@ -13,7 +32,7 @@ export const assetOperators = {
     groqFilter: ({fieldPath, value}) =>
       value?._ref && fieldPath ? `${fieldPath}.asset._ref == ${toJSON(value._ref)}` : null,
     initialValue: null,
-    inputComponent: SearchFilterAssetInput('file'),
+    inputComponent: SearchFilterAssetFileInput,
     type: 'assetFileEqual',
   }),
   assetFileNotEqual: defineSearchOperator({
@@ -23,7 +42,7 @@ export const assetOperators = {
     groqFilter: ({fieldPath, value}) =>
       value?._ref && fieldPath ? `${fieldPath}.asset._ref != ${toJSON(value._ref)}` : null,
     initialValue: null,
-    inputComponent: SearchFilterAssetInput('file'),
+    inputComponent: SearchFilterAssetFileInput,
     type: 'assetFileNotEqual',
   }),
   assetImageEqual: defineSearchOperator({
@@ -33,7 +52,7 @@ export const assetOperators = {
     groqFilter: ({fieldPath, value}) =>
       value?._ref && fieldPath ? `${fieldPath}.asset._ref == ${toJSON(value._ref)}` : null,
     initialValue: null,
-    inputComponent: SearchFilterAssetInput('image'),
+    inputComponent: SearchFilterAssetImageInput,
     type: 'assetImageEqual',
   }),
   assetImageNotEqual: defineSearchOperator({
@@ -43,7 +62,7 @@ export const assetOperators = {
     groqFilter: ({fieldPath, value}) =>
       value?._ref && fieldPath ? `${fieldPath}.asset._ref != ${toJSON(value._ref)}` : null,
     initialValue: null,
-    inputComponent: SearchFilterAssetInput('image'),
+    inputComponent: SearchFilterAssetImageInput,
     type: 'assetImageNotEqual',
   }),
 }
