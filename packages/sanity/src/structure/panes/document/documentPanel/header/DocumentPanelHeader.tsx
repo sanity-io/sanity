23f525b084727f22e8fa2664b75651d400d2ac1b
pedrobonamin <pedrobonamin@gmail.com>
2026-07-08 15:35:53 +0200

    fix: temporarily restore documents perspective list

--- packages/sanity/src/structure/panes/document/documentPanel/header/DocumentPanelHeader.tsx
+++ packages/sanity/src/structure/panes/document/documentPanel/header/DocumentPanelHeader.tsx
@@ -131,7 +131,9 @@ export const DocumentPanelHeader = memo(
 
     const menuButtonNodes = useMemo(() => menuNodes.filter(isMenuNodeButton), [menuNodes])
     const contextMenuNodes = useMemo(() => menuNodes.filter(isNotMenuNodeButton), [menuNodes])
-    const hasDocumentGroupInventory = beta?.documentGroupInventory?.enabled === true
+    // TODO: Disabled for now in this branch until inventory supports variants
+    // DO NOT MERGE THIS CHANGE TO MAIN
+    const hasDocumentGroupInventory = beta?.documentGroupInventory?.enabled === true && false
 
     const {collapsed, isLast} = usePane()
     // Prevent focus if this is the last (non-collapsed) pane.
