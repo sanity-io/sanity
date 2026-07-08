204a7c5addb4463beae20afdd27b6b2ec7b08661
Ash <ash@sanity.io>
2026-07-08 11:59:25 +0100

    chore(ailf): add configure-list-preview ailf task

---
+++ packages/@repo/ailf/.ailf/tasks/configure-list-preview.reference.ts
@@ -0,0 +1,61 @@
+import {defineConfig, defineType, defineField} from 'sanity'
+
+export default defineConfig({
+  name: 'default',
+  title: 'Blog',
+  projectId: 'xxxxxxxx',
+  dataset: 'production',
+  schema: {
+    types: [
+      defineType({
+        name: 'author',
+        title: 'Author',
+        type: 'document',
+        fields: [
+          defineField({
+            name: 'name',
+            title: 'Name',
+            type: 'string',
+          }),
+        ],
+      }),
+      defineType({
+        name: 'post',
+        title: 'Post',
+        type: 'document',
+        fields: [
+          defineField({
+            name: 'title',
+            title: 'Title',
+            type: 'string',
+          }),
+          defineField({
+            name: 'coverImage',
+            title: 'Cover image',
+            type: 'image',
+          }),
+          defineField({
+            name: 'author',
+            title: 'Author',
+            type: 'reference',
+            to: [{type: 'author'}],
+          }),
+        ],
+        preview: {
+          select: {
+            title: 'title',
+            authorName: 'author.name',
+            media: 'coverImage',
+          },
+          prepare({title, authorName, media}) {
+            return {
+              title,
+              subtitle: authorName,
+              media,
+            }
+          },
+        },
+      }),
+    ],
+  },
+})
