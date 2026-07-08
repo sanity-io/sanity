7c98e5318d84a9a1c7adc24ef8100dee6e4174a9
Pedro Bonamin <46196328+pedrobonamin@users.noreply.github.com>
2026-07-08 15:09:44 +0200

    Update types.ts

--- packages/sanity/src/core/releases/tool/detail/types.ts
+++ packages/sanity/src/core/releases/tool/detail/types.ts
@@ -4,6 +4,6 @@ import {type BundleDocument} from './useBundleDocuments'
  * A single document in a release, along with its validation status.
  * Returned by {@link useReleaseDocuments}.
  *
- * @internal
+ * @public
  */
 export type DocumentInRelease = BundleDocument
