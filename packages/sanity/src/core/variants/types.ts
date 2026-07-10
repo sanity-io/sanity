63ac3f0e744fef24db8840cf58c6e7b16da7d495
Ash <ash@sanity.io>
2026-07-10 10:55:51 +0100

    chore(sanity): add dedicated `VariantId` type

--- packages/sanity/src/core/variants/types.ts
+++ packages/sanity/src/core/variants/types.ts
@@ -2,9 +2,14 @@ import {type SanityDocument, type PortableTextBlock} from '@sanity/types'
 
 import {type VARIANT_DOCUMENT_TYPE, type VARIANT_DOCUMENTS_PATH} from './store/constants'
 
+/**
+ * @internal
+ */
+export type VariantId = `${typeof VARIANT_DOCUMENTS_PATH}.${string}`
+
 export interface SystemVariant extends SanityDocument {
   _type: typeof VARIANT_DOCUMENT_TYPE
-  _id: `${typeof VARIANT_DOCUMENTS_PATH}.${string}`
+  _id: VariantId
   name?: string
   conditions: Record<string, string>
   priority: number // defaults to 0.
