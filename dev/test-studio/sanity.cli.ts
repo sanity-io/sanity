9b17e134e0477beab16dc9df4a95c9ab898e5502
Cody Olsen <stipsan@gmail.com>
2026-07-13 12:47:33 +0200

    chore: use vite bundled dev mode

--- dev/test-studio/sanity.cli.ts
+++ dev/test-studio/sanity.cli.ts
@@ -22,10 +22,7 @@ export default defineCliConfig({
         projectId: 'ppsg7ml5',
         dataset: 'test',
       },
-  // Can be overriden by:
-  // A) `SANITY_STUDIO_REACT_STRICT_MODE=false pnpm dev`
-  // B) creating a `.env` file locally that sets the same env variable as above
-  reactStrictMode: true,
+  unstable_bundledDev: true,
   reactCompiler: {
     target: '19',
     // By default the compiler is loaded up on all workspace files, even sanity/lib/structure.js which is pre-compiled with `@sanity/pkg-utils`,
@@ -49,33 +46,6 @@ export default defineCliConfig({
 
     let nextConfig = mergeConfig(viteConfig, {
       plugins: [vanillaExtractPlugin()],
-      server: {
-        warmup: {
-          clientFiles: [
-            /**
-             * Since the test studio on the monorepo is using src files for `sanity`, `sanity/structure`, `@sanity/vision`, etc,
-             * it's not enough with the default `./.sanity/runtime/app.js` warmup file,
-             * we have to add a few more to avoid the initial "waterfall of reload doom" scenario.
-             * The ones we add here are from lazy loaded import() calls that are discovered late due to our file structure.
-             * They're not a problem in production, as our npm bundling hoists the dynamic imports to the top level entrypoint so vite discovers them early.
-             */
-            // https://github.com/sanity-io/sanity/blob/f6357dbe1e19076286c06de8d2c272058c0dc01e/packages/sanity/src/structure/structureTool.ts#L108
-            './node_modules/sanity/src/structure/components/structureTool/StructureToolBoundary.tsx',
-            // https://github.com/sanity-io/sanity/blob/f6357dbe1e19076286c06de8d2c272058c0dc01e/packages/sanity/src/presentation/plugin.tsx#L26
-            './node_modules/sanity/src/presentation/PresentationToolGrantsCheck.tsx',
-            // https://github.com/sanity-io/sanity/blob/f6357dbe1e19076286c06de8d2c272058c0dc01e/packages/sanity/src/presentation/plugin.tsx#L27
-            './node_modules/sanity/src/presentation/loader/BroadcastDisplayedDocument.tsx',
-            // https://github.com/sanity-io/sanity/blob/f6357dbe1e19076286c06de8d2c272058c0dc01e/packages/sanity/src/structure/panes/StructureToolPane.tsx#L26
-            './node_modules/sanity/src/structure/panes/userComponent/UserComponentPane.tsx',
-            // https://github.com/sanity-io/sanity/blob/f6357dbe1e19076286c06de8d2c272058c0dc01e/packages/sanity/src/structure/panes/StructureToolPane.tsx#L27
-            './node_modules/sanity/src/structure/panes/document/DocumentPane.tsx',
-            // https://github.com/sanity-io/sanity/blob/f6357dbe1e19076286c06de8d2c272058c0dc01e/packages/sanity/src/structure/panes/StructureToolPane.tsx#L28
-            './node_modules/sanity/src/structure/panes/documentList/PaneContainer.tsx',
-            // https://github.com/sanity-io/sanity/blob/f6357dbe1e19076286c06de8d2c272058c0dc01e/packages/sanity/src/structure/panes/StructureToolPane.tsx#L29
-            './node_modules/sanity/src/structure/panes/list/ListPane.tsx',
-          ],
-        },
-      },
       // Needed due to the monorepo setup, optimizeDeps will cause duplication of context providers when it chunks lazy imports so we have to disable optimization
       optimizeDeps: {exclude: ['sanity']},
       build: {
