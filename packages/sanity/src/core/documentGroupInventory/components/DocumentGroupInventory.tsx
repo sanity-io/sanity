e5bc01cfca364f370564286640e11fe2cfbbd675
Ash <ash@sanity.io>
2026-07-20 15:58:50 +0100

    chore(sanity): dull tone of document group inventory text button

--- packages/sanity/src/core/documentGroupInventory/components/DocumentGroupInventory.tsx
+++ packages/sanity/src/core/documentGroupInventory/components/DocumentGroupInventory.tsx
@@ -628,7 +628,7 @@ const TextButton = styled.button(({theme}) => {
     padding: 0;
     outline: none;
     all: unset;
-    color: ${color.link.fg};
+    color: ${color.button.ghost.neutral.enabled.fg};
 
     * {
       color: inherit;
