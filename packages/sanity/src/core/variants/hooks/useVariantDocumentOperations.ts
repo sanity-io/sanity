aec0ddf1c872cb3b3623df77e056c2d3cb94575c
Ash <ash@sanity.io>
2026-07-22 09:36:40 +0100

    chore(sanity): return `SingleActionResult` from `createVariantDocument`

--- packages/sanity/src/core/variants/hooks/useVariantDocumentOperations.ts
+++ packages/sanity/src/core/variants/hooks/useVariantDocumentOperations.ts
@@ -1,3 +1,4 @@
+import {type SanityDocumentLike} from '@sanity/types'
 import {useCallback} from 'react'
 
 import {useClient} from '../../hooks/useClient'
@@ -12,18 +13,16 @@ import {type SystemVariant} from '../types'
 export function useVariantDocumentOperations() {
   const client = useClient(VARIANTS_STUDIO_CLIENT_OPTIONS)
 
-  const createVariantDocument = useCallback(
-    async (options: {
-      baseId: string
-      baseRevisionId?: string
-      variant: SystemVariant
-      selectedPerspective: TargetPerspective
-    }) => {
-      await createVariantScopedDocument({
+  const createVariantDocument = useCallback<
+    (
+      options: Omit<Parameters<typeof createVariantScopedDocument>[0], 'client'>,
+    ) => ReturnType<typeof createVariantScopedDocument>
+  >(
+    (options) =>
+      createVariantScopedDocument({
         client,
         ...options,
-      })
-    },
+      }),
     [client],
   )
 
