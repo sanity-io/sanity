fafd6830f4771e628f2b439bbefe7f6da4703aea
laurenashpole <lauren.ashpole@sanity.io>
2026-08-07 11:07:14 -0400

    chore: swap out @sanity-labs/ui-poc for ui5

--- packages/sanity/src/structure/panes/documentList/DocumentListPaneSearchOrdering.tsx
+++ packages/sanity/src/structure/panes/documentList/DocumentListPaneSearchOrdering.tsx
@@ -1,8 +1,8 @@
-import {Box} from '@sanity-labs/ui-poc'
 import {SortIcon} from '@sanity/icons/Sort'
 import {Menu, MenuDivider, Text} from '@sanity/ui'
 import {memo, useId} from 'react'
 import {useGetI18nText, useTranslation} from 'sanity'
+import {Box} from 'ui5'
 
 import {Button} from '../../../ui-components/button/Button'
 import {MenuButton} from '../../../ui-components/menuButton/MenuButton'
