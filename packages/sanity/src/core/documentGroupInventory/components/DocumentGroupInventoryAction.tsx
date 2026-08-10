9cba8a6ded85526c415692f7e33a610963391dd7
Pedro Bonamin <46196328+pedrobonamin@users.noreply.github.com>
2026-08-10 14:23:42 +0200

    remove empty tooltipprops

--- packages/sanity/src/core/documentGroupInventory/components/DocumentGroupInventoryAction.tsx
+++ packages/sanity/src/core/documentGroupInventory/components/DocumentGroupInventoryAction.tsx
@@ -73,7 +73,6 @@ export const DocumentGroupInventoryAction: ComponentType<
           tone="neutral"
           onClick={() => setIsDocumentGroupInventoryActive(!isDocumentGroupInventoryActive)}
           icon={RhombusIcon}
-          tooltipProps={{}}
           mode="ghost"
         />
       </Popover>
