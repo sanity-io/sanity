e63212d55cbcfe9012df4c4c781d4a42edb9039b
Lauren Ashpole <lauren.ashpole@sanity.io>
2026-08-14 10:47:38 -0400

    chore: migrate box in core tasks (#14015)

--- packages/sanity/src/core/tasks/components/list/EmptyStates.tsx
+++ packages/sanity/src/core/tasks/components/list/EmptyStates.tsx
@@ -1,7 +1,8 @@
 import {AddIcon} from '@sanity/icons/Add'
-import {Box, Flex, Stack, Text} from '@sanity/ui'
+import {Flex, Stack, Text} from '@sanity/ui'
 import {useCallback} from 'react'
 import {styled} from 'styled-components'
+import {Box} from 'ui5'
 
 import {Button} from '../../../../ui-components/button/Button'
 import {useTranslation} from '../../../i18n/hooks/useTranslation'
