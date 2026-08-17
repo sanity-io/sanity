aea1ea6ac635385133033f4b1dbcd66e7f1768f8
laurenashpole <lauren.ashpole@sanity.io>
2026-08-17 10:45:25 -0400

    chore: migrate box in misc directories in structure

--- packages/sanity/src/structure/components/confirmDeleteDialog/ConfirmDeleteDialogBody.styles.tsx
+++ packages/sanity/src/structure/components/confirmDeleteDialog/ConfirmDeleteDialogBody.styles.tsx
@@ -1,7 +1,8 @@
 import {InfoOutlineIcon} from '@sanity/icons/InfoOutline'
-import {Box, Flex, Inline, rem, Text} from '@sanity/ui'
+import {Flex, Inline, rem, Text} from '@sanity/ui'
 import {useTranslation} from 'sanity'
 import {styled} from 'styled-components'
+import {Box} from 'ui5'
 
 import {Tooltip} from '../../../ui-components/tooltip/Tooltip'
 import {structureLocaleNamespace} from '../../i18n'
