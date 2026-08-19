70302468f57b762e78e856f3160c99e95454c42d
Ash <ash@sanity.io>
2026-08-19 16:09:31 +0100

    refactor(sanity): migrate gutter-field anchor to Vanilla Extract

--- packages/sanity/src/core/form/components/FormFieldGutter.tsx
+++ packages/sanity/src/core/form/components/FormFieldGutter.tsx
@@ -1,7 +1,10 @@
+import {assignInlineVars} from '@vanilla-extract/dynamic'
 import {type ComponentType} from 'react'
 
 import {type BaseFieldProps} from '../types/fieldProps'
+import {pathToAnchorIdent} from '../utils/pathToAnchorIdent'
 import {FormDivergenceIndicator} from './FormDivergenceIndicator'
+import {formFieldAnchor, formFieldAnchorPath} from './FormFieldAnchor.css'
 
 /**
  * @internal
@@ -14,5 +17,10 @@ export type FormFieldGutterProps = Pick<BaseFieldProps, 'path'>
  * @internal
  */
 export const FormFieldGutter: ComponentType<FormFieldGutterProps> = ({path}) => (
-  <FormDivergenceIndicator path={path} />
+  <div
+    className={formFieldAnchor}
+    style={assignInlineVars({[formFieldAnchorPath]: pathToAnchorIdent('input', path)})}
+  >
+    <FormDivergenceIndicator path={path} />
+  </div>
 )
