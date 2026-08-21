e9e70ea2e981be108634e7ed0b4203b78f7d1e61
laurenashpole <lauren.ashpole@sanity.io>
2026-08-21 11:19:40 -0400

    chore: final box migration

--- packages/@sanity/vision/src/components/VisionGuiResult.tsx
+++ packages/@sanity/vision/src/components/VisionGuiResult.tsx
@@ -1,6 +1,7 @@
 import {type MutationEvent} from '@sanity/client'
-import {Box, Text} from '@sanity/ui'
+import {Text} from '@sanity/ui'
 import {Translate, useTranslation} from 'sanity'
+import {Box} from 'ui5'
 
 import {visionLocaleNamespace} from '../i18n'
 import {getCsvBlobUrl, getJsonBlobUrl} from '../util/getBlobUrl'
