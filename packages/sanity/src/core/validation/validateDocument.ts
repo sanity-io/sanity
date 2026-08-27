283f16e0644bfb512464b4430705fe7e58551aa2
Jonah Snider <jonah@jonahsnider.com>
2026-08-27 06:49:42 -0700

    fix(sanity): honor validation concurrency options (#14143)
    Signed-off-by: Jonah Snider <jonah@jonahsnider.com>

--- packages/sanity/src/core/validation/validateDocument.ts
+++ packages/sanity/src/core/validation/validateDocument.ts
@@ -39,13 +39,6 @@ const DEFAULT_MAX_FETCH_CONCURRENCY = 25
 // NOTE: ensure to update the TSDoc and CLI help test if this is changed
 const DEFAULT_MAX_CUSTOM_VALIDATION_CONCURRENCY = 5
 
-let _limitConcurrency: ReturnType<typeof createClientConcurrencyLimiter> | undefined
-const getConcurrencyLimiter = (maxConcurrency: number) => {
-  if (_limitConcurrency) return _limitConcurrency
-  _limitConcurrency = createClientConcurrencyLimiter(maxConcurrency)
-  return _limitConcurrency
-}
-
 const isRecord = (maybeRecord: unknown): maybeRecord is Record<string, unknown> =>
   typeof maybeRecord === 'object' && maybeRecord !== null && !Array.isArray(maybeRecord)
 
@@ -159,11 +152,11 @@ export interface ValidateDocumentOptions {
   maxCustomValidationConcurrency?: number
 
   /**
-   * The amount of allowed inflight fetch requests at once. You may need to up
-   * this value if you have complex custom validations that require many
-   * `client.fetch` requests at once. It's possible for custom validator to
-   * stall if there are not enough concurrent fetch requests available to
-   * fullfil the custom validation. This is 25 by default.
+   * The amount of allowed inflight fetch requests at once for this validation.
+   * You may need to up this value if you have complex custom validations that require many
+   * `client.fetch` requests at once. It's possible for a custom validator to
+   * stall if there are not enough concurrent fetch requests available to fulfill
+   * the custom validation. This is 25 by default.
    */
   maxFetchConcurrency?: number
 
@@ -191,7 +184,7 @@ export function validateDocument({
 }: ValidateDocumentOptions): Promise<ValidationMarker[]> {
   // oxlint-disable-next-line no-deprecated -- will fix in follow up PR
   const getClient = options.getClient || workspace.getClient
-  const limitConcurrency = getConcurrencyLimiter(
+  const limitConcurrency = createClientConcurrencyLimiter(
     maxFetchConcurrency ?? DEFAULT_MAX_FETCH_CONCURRENCY,
   )
   const getConcurrencyLimitedClient = (clientOptions: SourceClientOptions) =>
@@ -244,7 +237,7 @@ export function validateDocumentObservable({
   currentUser,
 }: ValidateDocumentObservableOptions): Observable<ValidationMarker[]> {
   if (typeof document?._type !== 'string') {
-    throw new Error(`Tried to validated a value without a '_type'`)
+    throw new Error(`Tried to validate a value without a '_type'`)
   }
 
   const documentType = schema.get(document._type)
